20b985516f3e4841aee4057e168ae675558af10b
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-01-02 21:33:57 -0800

    fix local declarations to get unsigned comparisons correct (4.3BSD/lib/154)
    SCCS-vsn: lib/libc/stdlib/malloc.c 5.8

--- usr/src/lib/libc/stdlib/malloc.c
+++ usr/src/lib/libc/stdlib/malloc.c
@@ -16,7 +16,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)malloc.c	5.7 (Berkeley) %G%";
+static char sccsid[] = "@(#)malloc.c	5.8 (Berkeley) %G%";
 #endif /* LIBC_SCCS and not lint */
 
 /*
@@ -110,8 +110,8 @@ malloc(nbytes)
 	unsigned nbytes;
 {
   	register union overhead *op;
-  	register int bucket;
-	register unsigned amt, n;
+  	register int bucket, n;
+	register unsigned amt;
 
 	/*
 	 * First time malloc is called, setup page size and
@@ -277,7 +277,8 @@ realloc(cp, nbytes)
 	char *cp; 
 	unsigned nbytes;
 {   
-  	register u_int onb, i;
+  	register u_int onb;
+	register int i;
 	union overhead *op;
   	char *res;
 	int was_alloced = 0;
