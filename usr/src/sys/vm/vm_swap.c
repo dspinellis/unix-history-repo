c652976b677eb84c94f8b97b99d92a8900735bd8
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-01-13 23:14:50 -0800

    have to account for rawread (that does not use a b_vp)
    SCCS-vsn: sys/vm/vm_swap.c 7.10

--- usr/src/sys/vm/vm_swap.c
+++ usr/src/sys/vm/vm_swap.c
@@ -14,7 +14,7 @@
  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  *
- *	@(#)vm_swap.c	7.9 (Berkeley) %G%
+ *	@(#)vm_swap.c	7.10 (Berkeley) %G%
  */
 
 #include "param.h"
