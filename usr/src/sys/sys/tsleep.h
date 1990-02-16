fa7292fecf4a88f11eb234d616a1870bd31bafbe
Marc Teitelbaum <marc@ucbvax.Berkeley.EDU>
1990-02-15 22:28:16 -0800

    checkpoint work in progress (this file will probably go away anyway)...
    SCCS-vsn: sys/sys/tsleep.h 7.2

--- usr/src/sys/sys/tsleep.h
+++ usr/src/sys/sys/tsleep.h
@@ -14,7 +14,7 @@
  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  *
- *	@(#)tsleep.h	7.1 (Berkeley) %G%
+ *	@(#)tsleep.h	7.2 (Berkeley) %G%
  */
 #ifndef _TSLEEP_
 #define _TSLEEP_
@@ -33,6 +33,67 @@
 #define	SLP_TTYOUT_CLIST	11
 #define	SLP_PTS_OPEN	12
 #define	SLP_PTC_WRITE	13
+#define	SLP_SO_LINGER	14
+#define	SLP_SO_SBWAIT	15
+#define	SLP_SO_ACCEPT	16
+#define	SLP_SO_ACCEPT2	17
+#define	SLP_EXLCK	18
+#define	SLP_SHLCK	19
+#define	SLP_ISO_CONSOUT	20
+#define	SLP_ISO_CONSCONN 21
+#define	SLP_NFS_IOD	22
+#define	SLP_SO_SBLOCK	23
+#define	SLP_TU_OPN	24
+#define	SLP_MFS		25
+#define	SLP_HP_OPEN	26
+#define	SLP_AD_GETW	27
+#define	SLP_PCAT_OUT	28
+#define	SLP_PCAT_CLIST	29
+#define	SLP_DH_OPN	30
+#define	SLP_DHU_OPN	31
+#define	SLP_DMFL_ASLP	32
+#define	SLP_DMFL_ERROR	33
+#define	SLP_DMX_OPN	34
+#define	SLP_DN_REG	35
+#define	SLP_DN_PAUSE	36
+#define	SLP_DZ_OPN	37
+#define	SLP_IK_BUSY	38
+#define	SLP_LP_OUT	39
+#define	SLP_LP_CLIST	40
+#define	SLP_NP_SLP	41
+#define	SLP_PS_REFRESH	42
+#define	SLP_PS_MAP	43
+#define	SLP_TM_OPN	44
+#define	SLP_UDA_OPN	45
+#define SLP_UT_OPN	46
+#define	SLP_UU_OPN	47
+#define	SLP_VS_WAIT	48
+#define SLP_VS_USRWAIT	49
+#define	SLP_VS_START	50
+#define SLP_VS_ABORT	51
+#define SLP_VS_PWRUP	52
+#define	SLP_VS_IOBCTL	53
+#define	SLP_VS_FIB	54
+#define	SLP_VS_FIBRET	55
+#define	SLP_VS_INITF	56
+#define SLP_VS_INITDEV	57
+#define	SLP_DR_WAIT	58
+#define	SLP_DR_RESET	59
+#define	SLP_DR_ACTV	60
+#define	SLP_HD_OPN	61
+#define	SLP_MP_1OPN	62
+#define	SLP_MP_POPN	63
+#define	SLP_MP_OPN	64
+#define	SLP_MP_1CLS	65
+#define	SLP_MP_CLS	66
+#define	SLP_MP_BRK	67
+#define	SLP_MP_STDL	68
+#define	SLP_MP_DLWAIT	69
+#define	SLP_VD_OPN	70
+#define	SLP_VX_OPN	71
+#define	SLP_VX_CLS	72
+#define	SLP_VX_PARAM	73
+#endif
 
 #ifdef SLP_MSGLIST
 char *slp_mesg[] = {
@@ -50,126 +111,65 @@ char *slp_mesg[] = {
 	"tty_clists",
 	"tty_ptsopen",
 	"tty_ptcwrite",	/* 13 */
-#define	SLP_SO_LINGER	14
 	"so_linger",
-#define	SLP_SO_SBWAIT	15
 	"so_sbwait",
-#define	SLP_SO_ACCEPT	16
 	"so_accept",
-#define	SLP_SO_ACCEPT2	17
 	"so_accept2",
-#define	SLP_EXLCK	18
 	"exlock",
-#define	SLP_SHLCK	19
 	"shlock",
-#define	SLP_ISO_CONSOUT	20
 	"iso_cons",
-#define	SLP_ISO_CONSCONN 21
 	"iso_consconn",
-#define	SLP_NFS_IOD	22
 	"nfs_iod",
-#define	SLP_SO_SBLOCK	23
 	"so_sblock",
-#define	SLP_TU_OPN	24
 	"tu58_opn",
-#define	SLP_MFS		25
 	"mfs_idle",
-#define	SLP_HP_OPEN	26
 	"hp_open",
-#define	SLP_AD_GETW	27
 	"ad_getw",
-#define	SLP_PCAT_OUT	28
 	"pcat_out",
-#define	SLP_PCAT_CLIST	29
 	"pcat_clist",
-#define	SLP_DH_OPN	30
 	"dh_opn",
-#define	SLP_DHU_OPN	31
 	"dhu_opn",
-#define	SLP_DMFL_ASLP	32
 	"dmfl_aslp",
-#define	SLP_DMFL_ERROR	33
 	"dmfl_err",
-#define	SLP_DMX_OPN	34
 	"dmx_opn",
-#define	SLP_DN_REG	35
 	"dn_reg",
-#define	SLP_DN_PAUSE	36
 	"dn_pause",
-#define	SLP_DZ_OPN	37
 	"dz_opn",
-#define	SLP_IK_BUSY	38
 	"ik_busy",
-#define	SLP_LP_OUT	39
 	"lp_out",
-#define	SLP_LP_CLIST	40
 	"lp_clist",
-#define	SLP_NP_SLP	41
 	"np_slp",
-#define	SLP_PS_REFRESH	42
 	"ps_refresh",
-#define	SLP_PS_MAP	43
 	"ps_map",
-#define	SLP_TM_OPN	44
 	"tm_opn",
-#define	SLP_UDA_OPN	45
 	"uda_opn",
-#define SLP_UT_OPN	46
 	"ut_opn",
-#define	SLP_UU_OPN	47
 	"uu_opn",
-#define	SLP_VS_WAIT	48
 	"vs_wait",
-#define SLP_VS_USRWAIT	49
 	"vs_usrwait",
-#define	SLP_VS_START	50
 	"vs_start",
-#define SLP_VS_ABORT	51
 	"vs_abort",
-#define SLP_VS_PWRUP	52
 	"vs_pwrup",
-#define	SLP_VS_IOBCTL	53
 	"vs_iobctl",
-#define	SLP_VS_FIB	54
 	"vs_fib",
-#define	SLP_VS_FIBRET	55
 	"vs_fibret",
-#define	SLP_VS_INITF	56
 	"vs_initf",
-#define SLP_VS_INITDEV	57
 	"vs_initdev",
-#define	SLP_DR_WAIT	58
 	"dr_wait",
-#define	SLP_DR_RESET	59
 	"dr_reset",
-#define	SLP_DR_ACTV	60
 	"dr_actv",
-#define	SLP_HD_OPN	61
 	"hd_opn",
-#define	SLP_MP_1OPN	62
 	"mp_1opn",
-#define	SLP_MP_POPN	63
 	"mp_popn",
-#define	SLP_MP_OPN	64
 	"mp_opn",
-#define	SLP_MP_1CLS	65
 	"mp_1cls",
-#define	SLP_MP_CLS	66
 	"mp_cls",
-#define	SLP_MP_BRK	67
 	"mp_brk",
-#define	SLP_MP_STDL	68
 	"mp_stdl",
-#define	SLP_MP_DLWAIT	69
 	"mp_dlwait",
-#define	SLP_VD_OPN	70
 	"vd_opn",
-#define	SLP_VX_OPN	71
 	"vx_opn",
-#define	SLP_VX_CLS	72
 	"vx_cls",
-#define	SLP_VX_PARAM	73
 	"vx_param",
 };
-#endif
 #endif
