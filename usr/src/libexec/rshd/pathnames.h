bf664590a69479c89c0f5a418ddcf6bb3030b395
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-02-15 17:21:23 -0800

    remove nologin, it's paths.h now
    SCCS-vsn: libexec/rshd/pathnames.h 5.3
    SCCS-vsn: sbin/shutdown/pathnames.h 5.3
    SCCS-vsn: usr.bin/login/pathnames.h 5.4
    SCCS-vsn: usr.bin/uucp/includes/pathnames.h 5.2

--- usr/src/libexec/rshd/pathnames.h
+++ usr/src/libexec/rshd/pathnames.h
@@ -14,10 +14,9 @@
  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  *
- *	@(#)pathnames.h	5.2 (Berkeley) %G%
+ *	@(#)pathnames.h	5.3 (Berkeley) %G%
  */
 
 #include <paths.h>
 
 #define	_PATH_DEFPATH	"PATH=/bin:/usr/bin:"
-#define	_PATH_NOLOGIN	"/etc/nologin"
