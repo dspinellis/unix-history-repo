c330d679ba5575e0f74c73a5abfe31a6f4bf76ae
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-02-15 17:19:28 -0800

    add nologin
    SCCS-vsn: include/paths.h 5.5

--- usr/src/include/paths.h
+++ usr/src/include/paths.h
@@ -14,7 +14,7 @@
  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  *
- *	@(#)paths.h	5.4 (Berkeley) %G%
+ *	@(#)paths.h	5.5 (Berkeley) %G%
  */
 
 #define	_PATH_BSHELL	"/bin/sh"
@@ -26,6 +26,7 @@
 #define	_PATH_KMEM	"/dev/kmem"
 #define	_PATH_MAN	"/usr/man"
 #define	_PATH_MEM	"/dev/mem"
+#define	_PATH_NOLOGIN	"/etc/nologin"
 #define	_PATH_SENDMAIL	"/usr/sbin/sendmail"
 #define	_PATH_TMP	"/tmp/"
 #define	_PATH_TTY	"/dev/tty"
