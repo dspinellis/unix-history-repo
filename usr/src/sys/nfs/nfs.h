bb3a248a4f2c7d66d88c37059a0cb95a7cd33910
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-02-16 21:40:49 -0800

    generalize a number of constants
    SCCS-vsn: sys/nfs/nfs.h 7.5

--- usr/src/sys/nfs/nfs.h
+++ usr/src/sys/nfs/nfs.h
@@ -17,22 +17,29 @@
  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  *
- *	@(#)nfs.h	7.4 (Berkeley) %G%
+ *	@(#)nfs.h	7.5 (Berkeley) %G%
  */
 
 /*
  * Tunable constants for nfs
  */
+
 #define	MAX_IOVEC	10
-#define	NFS_TIMEO	10	/* Timeout in .1 sec intervals */
-#define	NFS_MAXTIMEO	600	/* Max timeout to backoff too in .1 sec */
-#define	NFS_ATTRTIMEO	5	/* Attribute cache timeout in sec */
-#define	NFS_RETRANS	10	/* Num of retrans for soft mounts */
-#define	NFS_WSIZE	8192	/* Max. write data size <= 8192 */
-#define	NFS_RSIZE	8192	/* Max. read data size <= 8192 */
+#define NFS_HZ		10		/* Ticks per second for NFS timeouts */
+#define	NFS_TIMEO	(1*NFS_HZ)	/* Default timeout = 1 second */
+#define	NFS_MINTIMEO	(NFS_HZ/2)	/* Min timeout to use */
+#define	NFS_MAXTIMEO	(60*NFS_HZ)	/* Max timeout to backoff to */
+#define	NFS_MAXREXMIT	100		/* Stop counting after this many */
+#define	NFS_MAXWINDOW	1024		/* Max number of outstanding requests */
+#define	NFS_RETRANS	10		/* Num of retrans for soft mounts */
+#define NFS_FISHY	6		/* Host not responding at this count */
+#define	NFS_ATTRTIMEO	5		/* Attribute cache timeout in sec */
+#define	NFS_WSIZE	8192		/* Max. write data size <= 8192 */
+#define	NFS_RSIZE	8192		/* Max. read data size <= 8192 */
 #define	MAX_READDIR	NFS_RSIZE	/* Max. size of directory read */
-#define	MAX_ASYNCDAEMON	20	/* Max. number of async_daemons runnable */
+#define	MAX_ASYNCDAEMON	20		/* Max. number async_daemons runnable */
 #define	NMOD(a)		((a) % nfs_asyncdaemons)
+
 
 /*
  * Nfs outstanding request list element
@@ -46,12 +53,18 @@ struct nfsreq {
 	struct vnode	*r_vp;
 	int		r_msiz;
 	u_long		r_xid;
-	u_long		r_inaddr;
-	u_long		r_retry;
-	u_long		r_timeout;
-	u_long		r_timer;
+	short		r_flags;	/* flags on request, see below */
+	short		r_retry;	/* max retransmission count */
+	short		r_rexmit;	/* current retrans count */
+	short		r_timer;	/* tick counter on reply */
+	short		r_timerinit;	/* reinit tick counter on reply */
 };
 
+/* Flag values for r_flags */
+#define R_TIMING	0x01		/* timing request (in mntp) */
+#define R_SENT		0x02		/* request has been sent */
+
+#ifdef	KERNEL
 /*
  * Silly rename structure that hangs off the nfsnode until the name
  * can be removed by nfs_inactive()
@@ -65,6 +78,7 @@ struct sillyrename {
 /* And its flag values */
 #define REMOVE		0
 #define	RMDIR		1
+#endif	/* KERNEL */
 
 /*
  * Stats structure
@@ -85,6 +99,10 @@ struct nfsstats {
 	int	srvrpccnt[NFS_NPROCS];
 	int	srvrpc_errs;
 	int	srv_errs;
+	int	rpcrequests;
+	int	rpctimeouts;
+	int	rpcunexpected;
+	int	rpcinvalid;
 	int	srvcache_inproghits;
 	int	srvcache_idemdonehits;
 	int	srvcache_nonidemdonehits;
@@ -93,4 +111,4 @@ struct nfsstats {
 
 #ifdef KERNEL
 struct nfsstats nfsstats;
-#endif
+#endif /* KERNEL */
