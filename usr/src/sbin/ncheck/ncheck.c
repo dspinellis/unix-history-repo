b3d6d453a4eb9e51066f5e3f9b76eb2988eaee71
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-02-21 00:11:31 -0800

    typo
    SCCS-vsn: sbin/ncheck/ncheck.c 5.10

--- usr/src/sbin/ncheck/ncheck.c
+++ usr/src/sbin/ncheck/ncheck.c
@@ -11,7 +11,7 @@ char copyright[] =
 #endif not lint
 
 #ifndef lint
-static char sccsid[] = "@(#)ncheck.c	5.9 (Berkeley) %G%";
+static char sccsid[] = "@(#)ncheck.c	5.10 (Berkeley) %G%";
 #endif not lint
 
 /*
@@ -22,7 +22,7 @@ static char sccsid[] = "@(#)ncheck.c	5.9 (Berkeley) %G%";
 #define	MAXNINDIR	(MAXBSIZE / sizeof (daddr_t))
 
 #include <sys/param.h>
-#include <sys/time.h
+#include <sys/time.h>
 #include <sys/vnode.h>
 #include <ufs/inode.h>
 #include <ufs/fs.h>
