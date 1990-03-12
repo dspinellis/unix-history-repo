0498b8205cfd118f26656564f82e5e5e8f6c6e50
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-03-12 01:03:31 -0800

    add Berkeley specific copyright notice
    SCCS-vsn: usr.bin/make/lst.lib/lstAppend.c 5.2
    SCCS-vsn: usr.bin/make/lst.lib/lstAtEnd.c 5.2
    SCCS-vsn: usr.bin/make/lst.lib/lstAtFront.c 5.2
    SCCS-vsn: usr.bin/make/lst.lib/lstClose.c 5.2
    SCCS-vsn: usr.bin/make/lst.lib/lstConcat.c 5.2
    SCCS-vsn: usr.bin/make/lst.lib/lstCur.c 5.2
    SCCS-vsn: usr.bin/make/lst.lib/lstDatum.c 5.2
    SCCS-vsn: usr.bin/make/lst.lib/lstDeQueue.c 5.2
    SCCS-vsn: usr.bin/make/lst.lib/lstDestroy.c 5.2
    SCCS-vsn: usr.bin/make/lst.lib/lstDupl.c 5.2
    SCCS-vsn: usr.bin/make/lst.lib/lstEnQueue.c 5.2
    SCCS-vsn: usr.bin/make/lst.lib/lstFake.c 5.2
    SCCS-vsn: usr.bin/make/lst.lib/lstFind.c 5.2
    SCCS-vsn: usr.bin/make/lst.lib/lstFindFrom.c 5.2
    SCCS-vsn: usr.bin/make/lst.lib/lstFirst.c 5.2
    SCCS-vsn: usr.bin/make/lst.lib/lstForEach.c 5.2
    SCCS-vsn: usr.bin/make/lst.lib/lstForEachFrom.c 5.2
    SCCS-vsn: usr.bin/make/lst.lib/lstIndex.c 5.2
    SCCS-vsn: usr.bin/make/lst.lib/lstInit.c 5.2
    SCCS-vsn: usr.bin/make/lst.lib/lstInsert.c 5.2
    SCCS-vsn: usr.bin/make/lst.lib/lstIsAtEnd.c 5.2
    SCCS-vsn: usr.bin/make/lst.lib/lstIsEmpty.c 5.2
    SCCS-vsn: usr.bin/make/lst.lib/lstLast.c 5.2
    SCCS-vsn: usr.bin/make/lst.lib/lstLength.c 5.2
    SCCS-vsn: usr.bin/make/lst.lib/lstMember.c 5.2
    SCCS-vsn: usr.bin/make/lst.lib/lstMove.c 5.2
    SCCS-vsn: usr.bin/make/lst.lib/lstNext.c 5.2
    SCCS-vsn: usr.bin/make/lst.lib/lstOpen.c 5.2
    SCCS-vsn: usr.bin/make/lst.lib/lstPred.c 5.2
    SCCS-vsn: usr.bin/make/lst.lib/lstPrev.c 5.2
    SCCS-vsn: usr.bin/make/lst.lib/lstRemove.c 5.2
    SCCS-vsn: usr.bin/make/lst.lib/lstReplace.c 5.2
    SCCS-vsn: usr.bin/make/lst.lib/lstSetCirc.c 5.2
    SCCS-vsn: usr.bin/make/lst.lib/lstSucc.c 5.2

--- usr/src/usr.bin/make/lst.lib/lstAtEnd.c
+++ usr/src/usr.bin/make/lst.lib/lstAtEnd.c
@@ -1,22 +1,31 @@
+/*
+ * Copyright (c) 1988, 1989, 1990 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to Berkeley by
+ * Adam de Boor.
+ *
+ * Redistribution and use in source and binary forms are permitted
+ * provided that the above copyright notice and this paragraph are
+ * duplicated in all such forms and that any documentation,
+ * advertising materials, and other materials related to such
+ * distribution and use acknowledge that the software was developed
+ * by the University of California, Berkeley.  The name of the
+ * University may not be used to endorse or promote products derived
+ * from this software without specific prior written permission.
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+#ifndef lint
+static char sccsid[] = "@(#)lstAtEnd.c	5.2 (Berkeley) %G%";
+#endif /* not lint */
+
 /*-
  * LstAtEnd.c --
  *	Add a node at the end of the list
- *
- * Copyright (c) 1988 by the Regents of the University of California
- *
- * Permission to use, copy, modify, and distribute this
- * software and its documentation for any purpose and without
- * fee is hereby granted, provided that the above copyright
- * notice appears in all copies.  Neither the University of California nor
- * Adam de Boor makes any representations about the suitability of this
- * software for any purpose.  It is provided "as is" without
- * express or implied warranty.
- *
  */
-#ifndef lint
-static char *rcsid =
-"$Id: lstAtEnd.c,v 1.3 88/11/17 20:51:48 adam Exp $ SPRITE (Berkeley)";
-#endif lint
 
 #include	"lstInt.h"
 	
--- usr/src/usr.bin/make/lst.lib/lstMove.c
+++ usr/src/usr.bin/make/lst.lib/lstMove.c
@@ -1,22 +1,32 @@
+/*
+ * Copyright (c) 1988, 1989, 1990 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to Berkeley by
+ * Adam de Boor.
+ *
+ * Redistribution and use in source and binary forms are permitted
+ * provided that the above copyright notice and this paragraph are
+ * duplicated in all such forms and that any documentation,
+ * advertising materials, and other materials related to such
+ * distribution and use acknowledge that the software was developed
+ * by the University of California, Berkeley.  The name of the
+ * University may not be used to endorse or promote products derived
+ * from this software without specific prior written permission.
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+#ifndef lint
+static char sccsid[] = "@(#)lstMove.c	5.2 (Berkeley) %G%";
+#endif /* not lint */
+
 /*-
  * LstMove.c --
  *	Move an existing node after or before one in the same or different
  *	list.
- *
- * Copyright (c) 1988 by University of California Regents
- *
- * Permission to use, copy, modify, and distribute this
- * software and its documentation for any purpose and without
- * fee is hereby granted, provided that the above copyright
- * notice appears in all copies.  Neither the University of California nor
- * Adam de Boor makes any representations about the suitability of this
- * software for any purpose.  It is provided "as is" without
- * express or implied warranty.
  */
-#ifndef lint
-static char *rcsid =
-"$Id: lstMove.c,v 1.6 89/06/13 15:01:48 adam Exp $ SPRITE (Berkeley)";
-#endif lint
 
 #include	"lstInt.h"
 
