553f191eba0bcf7694af45c1b9ad9e0edc6e9eb0
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-03-11 23:59:15 -0800

    add Berkeley specific copyright
    SCCS-vsn: usr.bin/make/buf.h 5.2

--- usr/src/usr.bin/make/buf.h
+++ usr/src/usr.bin/make/buf.h
@@ -1,18 +1,30 @@
+/*
+ * Copyright (c) 1988, 1989, 1990 The Regents of the University of California.
+ * Copyright (c) 1988, 1989 by Adam de Boor
+ * Copyright (c) 1989 by Berkeley Softworks
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to Berkeley by
+ * Adam de Boor.
+ *
+ * Redistribution and use in source and binary forms are permitted
+ * provided that the above copyright notice and this paragraph are
+ * duplicated in all such forms and that any documentation,
+ * advertising materials, and other materials related to such
+ * distribution and use acknowledge that the software was developed
+ * by the University of California, Berkeley.  The name of the
+ * University may not be used to endorse or promote products derived
+ * from this software without specific prior written permission.
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ *	@(#)buf.h	5.2 (Berkeley) %G%
+ */
+
 /*-
  * buf.h --
  *	Header for users of the buf library.
- *
- * Copyright (c) 1987 by the Regents of the University of California
- *
- * Permission to use, copy, modify, and distribute this
- * software and its documentation for any purpose and without
- * fee is hereby granted, provided that the above copyright
- * notice appear in all copies.  The University of California
- * makes no representations about the suitability of this
- * software for any purpose.  It is provided "as is" without
- * express or implied warranty.
- *
- *	"$Id: buf.h,v 2.1 89/07/03 15:49:36 adam Exp $ SPRITE (Berkeley)"
  */
 
 #ifndef _BUF_H
