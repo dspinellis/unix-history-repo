c06aa7dc28676b10fcfdce0adb0dae2c39a43cea
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-03-12 00:01:04 -0800

    add Berkeley specific copyright notice
    SCCS-vsn: usr.bin/make/bit.h 5.2

--- usr/src/usr.bin/make/bit.h
+++ usr/src/usr.bin/make/bit.h
@@ -1,16 +1,34 @@
 /*
+ * Copyright (c) 1988, 1989, 1990 The Regents of the University of California.
+ * Copyright (c) 1988, 1989 by Adam de Boor
+ * Copyright (c) 1989 by Berkeley Softworks
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to Berkeley by
+ * Adam de Boor.
+ *
+ * Redistribution and use in source and binary forms are permitted
+ * provided that the above copyright notice and this paragraph are
+ * duplicated in all such forms and that any documentation,
+ * advertising materials, and other materials related to such
+ * distribution and use acknowledge that the software was developed
+ * by the University of California, Berkeley.  The name of the
+ * University may not be used to endorse or promote products derived
+ * from this software without specific prior written permission.
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ *	@(#)bit.h	5.2 (Berkeley) %G%
+ */
+
+/*
  * bit.h --
  *
  *	Definition of macros for setting and clearing bits in an array
  *	of integers.
  *
  *	It is assumed that "int" is 32 bits wide.
- *
- * Copyright 1987 Regents of the University of California
- * All rights reserved.
- *
- *
- * $Id: bit.h,v 2.1 89/07/03 15:49:11 adam Exp $ SPRITE (Berkeley)
  */
 
 #ifndef _BIT
