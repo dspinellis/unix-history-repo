531d6be8d47daf394c4e75c00628a004fc5046f0
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-03-12 17:07:24 -0800

    remove SYSV and is68k ifdef's
    SCCS-vsn: usr.bin/make/cond.c 5.3
    SCCS-vsn: usr.bin/make/job.c 5.4
    SCCS-vsn: usr.bin/make/main.c 5.6

--- usr/src/usr.bin/make/cond.c
+++ usr/src/usr.bin/make/cond.c
@@ -21,7 +21,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)cond.c	5.2 (Berkeley) %G%";
+static char sccsid[] = "@(#)cond.c	5.3 (Berkeley) %G%";
 #endif /* not lint */
 
 /*-
@@ -33,11 +33,7 @@ static char sccsid[] = "@(#)cond.c	5.2 (Berkeley) %G%";
  *
  */
 
-#ifdef SYSV
-#include    <string.h>
-#else
 #include    <strings.h>
-#endif
 #include    "make.h"
 #include    <buf.h>
 #include    <ctype.h>
