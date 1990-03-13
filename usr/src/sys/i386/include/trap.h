14437eca08279306ec77c6d77a33ee223e04a27c
Bill Joy <bill@ucbvax.Berkeley.EDU>
1990-03-12 23:48:27 -0800

    date and time created 90/03/12 15:48:27 by bill
    SCCS-vsn: sys/i386/include/trap.h 1.1

---
+++ usr/src/sys/i386/include/trap.h
@@ -0,0 +1,48 @@
+/*	trap.h	1.3	87/07/11	*/
+
+/*
+ * Trap type values
+ * also known in trap.c for name strings
+ */
+
+#define	T_RESADFLT	0	/* reserved addressing */
+#define	T_PRIVINFLT	1	/* privileged instruction */
+#define	T_RESOPFLT	2	/* reserved operand */
+#define	T_BPTFLT	3	/* breakpoint instruction */
+#define	T_SYSCALL	5	/* system call (kcall) */
+#define	T_ARITHTRAP	6	/* arithmetic trap */
+#define	T_ASTFLT	7	/* system forced exception */
+#define	T_SEGFLT	8	/* segmentation (limit) fault */
+#define	T_PROTFLT	9	/* protection fault */
+#define	T_TRCTRAP	10	/* trace trap */
+#define	T_PAGEFLT	12	/* page fault */
+#define	T_TABLEFLT	13	/* page table fault */
+#define	T_ALIGNFLT	14	/* alignment fault */
+#define	T_KSPNOTVAL	15	/* kernel stack pointer not valid */
+#define	T_BUSERR	16	/* bus error */
+#define	T_KDBTRAP	17	/* kernel debugger trap */
+#define	T_DIVIDE	18	/* integer divide fault */
+#define	T_DEBUG	19	/* debug fault/trap catchall */
+#define	T_NMI	20	/* non-maskable trap */
+#define	T_OFLOW	21	/* overflow trap */
+#define	T_BOUND	22	/* bound instruction fault */
+#define	T_DNA	23	/* device not available fault */
+#define	T_DOUBLEFLT	24	/* double fault */
+#define	T_FPOPFLT	25	/* fp coprocessor operand fetch fault */
+#define	T_TSSFLT	26	/* invalid tss fault */
+#define	T_SEGNPFLT	27	/* segment not present fault */
+#define	T_STKFLT	28	/* stack fault */
+#define	T_RESERVED	29	/* stack fault */
+
+/* definitions for <sys/signal.h> */
+#define	    ILL_RESAD_FAULT	T_RESADFLT
+#define	    ILL_PRIVIN_FAULT	T_PRIVINFLT
+#define	    ILL_RESOP_FAULT	T_RESOPFLT
+#define	    ILL_ALIGN_FAULT	T_ALIGNFLT
+
+/* codes for SIGFPE/ARITHTRAP */
+#define	    FPE_INTOVF_TRAP	0x1	/* integer overflow */
+#define	    FPE_INTDIV_TRAP	0x2	/* integer divide by zero */
+#define	    FPE_FLTDIV_TRAP	0x3	/* floating/decimal divide by zero */
+#define	    FPE_FLTOVF_TRAP	0x4	/* floating overflow */
+#define	    FPE_FLTUND_TRAP	0x5	/* floating underflow */
