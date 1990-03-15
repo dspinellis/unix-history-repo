77ca3affa3612a8f26623ca66a604d17a9aec431
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-03-15 01:06:59 -0800

    redo for/from POSIX 1003.1; add Berkeley specific copyright notice
    SCCS-vsn: sys/sys/stat.h 7.4

--- usr/src/sys/sys/stat.h
+++ usr/src/sys/sys/stat.h
@@ -1,45 +1,92 @@
 /*
- * Copyright (c) 1982, 1986 Regents of the University of California.
- * All rights reserved.  The Berkeley software License Agreement
- * specifies the terms and conditions for redistribution.
+ * Copyright (c) 1982, 1986, 1989 The Regents of the University of California.
+ * All rights reserved.
  *
- *	@(#)stat.h	7.3 (Berkeley) %G%
+ * Redistribution and use in source and binary forms are permitted
+ * provided that the above copyright notice and this paragraph are
+ * duplicated in all such forms and that any documentation,
+ * advertising materials, and other materials related to such
+ * distribution and use acknowledge that the software was developed
+ * by the University of California, Berkeley.  The name of the
+ * University may not be used to endorse or promote products derived
+ * from this software without specific prior written permission.
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ *	@(#)stat.h	7.4 (Berkeley) %G%
  */
 
-struct	stat
+struct stat
 {
-	dev_t	st_dev;
-	ino_t	st_ino;
-	unsigned short st_mode;
-	short	st_nlink;
-	uid_t	st_uid;
-	gid_t	st_gid;
-	dev_t	st_rdev;
-	off_t	st_size;
-	time_t	st_atime;
-	int	st_spare1;
-	time_t	st_mtime;
-	int	st_spare2;
-	time_t	st_ctime;
-	int	st_spare3;
-	long	st_blksize;
-	long	st_blocks;
-	u_long	st_flags;
-	u_long	st_gen;
+	dev_t	st_dev;			/* inode's device */
+	ino_t	st_ino;			/* inode's number */
+	mode_t	st_mode;		/* inode protection mode */
+	nlink_t	st_nlink;		/* number of hard links */
+	uid_t	st_uid;			/* user ID of the file's owner */
+	gid_t	st_gid;			/* group ID of the file's group */
+	dev_t	st_rdev;		/* device type */
+	off_t	st_size;		/* file size, in bytes */
+	time_t	st_atime;		/* time of last access */
+	long	st_spare1;
+	time_t	st_mtime;		/* time of last data modification */
+	long	st_spare2;
+	time_t	st_ctime;		/* time of last file status change */
+	long	st_spare3;
+	long	st_blksize;		/* optimal blocksize for I/O */
+	long	st_blocks;		/* blocks allocated for file */
+	u_long	st_flags;		/* user defined flags for file */
+	u_long	st_gen;			/* file generation number */
 };
 
-#define	S_IFMT	0170000		/* type of file */
-#define		S_IFDIR	0040000	/* directory */
-#define		S_IFCHR	0020000	/* character special */
-#define		S_IFBLK	0060000	/* block special */
-#define		S_IFREG	0100000	/* regular */
-#define		S_IFLNK	0120000	/* symbolic link */
-#define		S_IFSOCK 0140000/* socket */
-#define	S_ISUID	0004000		/* set user id on execution */
-#define	S_ISGID	0002000		/* set group id on execution */
-#define	S_ISVTX	0001000		/* save swapped text even after use */
-#define	S_IREAD	0000400		/* read permission, owner */
-#define	S_IWRITE 0000200	/* write permission, owner */
-#define	S_IEXEC	0000100		/* execute/search permission, owner */
+#define	S_ISUID	0004000			/* set user id on execution */
+#define	S_ISGID	0002000			/* set group id on execution */
+#ifndef _POSIX_SOURCE
+#define	S_ISTXT	0001000			/* sticky bit */
+#endif
+
+#define	S_IRWXU	0000700			/* RWX mask for owner */
+#define	S_IRUSR	0000400			/* R for owner */
+#define	S_IWUSR	0000200			/* W for owner */
+#define	S_IXUSR	0000100			/* X for owner */
+
+#ifndef _POSIX_SOURCE
+#define	S_IREAD		S_IRUSR
+#define	S_IWRITE	S_IWUSR
+#define	S_IEXEC		S_IXUSR
+#endif
+
+#define	S_IRWXG	0000070			/* RWX mask for group */
+#define	S_IRGRP	0000040			/* R for group */
+#define	S_IWGRP	0000020			/* W for group */
+#define	S_IXGRP	0000010			/* X for group */
+
+#define	S_IRWXO	0000007			/* RWX mask for other */
+#define	S_IROTH	0000004			/* R for other */
+#define	S_IWOTH	0000002			/* W for other */
+#define	S_IXOTH	0000001			/* X for other */
+
+#ifndef _POSIX_SOURCE
+#define	S_IFMT	 0170000		/* type of file */
+#define	S_IFIFO	 0010000		/* named pipe (fifo) */
+#define	S_IFCHR	 0020000		/* character special */
+#define	S_IFDIR	 0040000		/* directory */
+#define	S_IFBLK	 0060000		/* block special */
+#define	S_IFREG	 0100000		/* regular */
+#define	S_IFLNK	 0120000		/* symbolic link */
+#define	S_IFSOCK 0140000		/* socket */
+
+#define	S_ISVTX	 0001000		/* save swapped text even after use */
 
 #define S_BLKSIZE	512	/* block size used in the stat struct */
+#endif
+
+#define	S_ISDIR(m)	((m & 0170000) == 0040000)	/* directory */
+#define	S_ISCHR(m)	((m & 0170000) == 0020000)	/* char special */
+#define	S_ISBLK(m)	((m & 0170000) == 0060000)	/* block special */
+#define	S_ISREG(m)	((m & 0170000) == 0100000)	/* regular file */
+#define	S_ISFIFO(m)	((m & 0170000) == 0010000)	/* fifo */
+#ifndef _POSIX_SOURCE
+#define	S_ISLNK(m)	((m & 0170000) == 0120000)	/* symbolic link */
+#define	S_ISSOCK(m)	((m & 0170000) == 0140000)	/* socket */
+#endif
