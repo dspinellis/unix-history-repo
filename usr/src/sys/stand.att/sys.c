e505d140867f6d17d30e35174504a2cd8436fd5a
Bill Joy <root@ucbvax.Berkeley.EDU>
1990-03-16 02:10:22 -0800

    specify pathnames for includes
    SCCS-vsn: sys/stand.att/sys.c 7.8

--- usr/src/sys/stand.att/sys.c
+++ usr/src/sys/stand.att/sys.c
@@ -3,14 +3,16 @@
  * All rights reserved.  The Berkeley software License Agreement
  * specifies the terms and conditions for redistribution.
  *
- *	@(#)sys.c	7.7 (Berkeley) %G%
+ *	@(#)sys.c	7.8 (Berkeley) %G%
  */
 
-#include "param.h"
-#include "inode.h"
-#include "fs.h"
-#include "dir.h"
-#include "reboot.h"
+#include "sys/param.h"
+#include "sys/time.h"
+#include "sys/vnode.h"
+#include "ufs/inode.h"
+#include "ufs/fs.h"
+#include "ufs/dir.h"
+#include "sys/reboot.h"
 #include "saio.h"
 
 #define	isdigit(c)	((u_int)((c) - '0') <= 9)
