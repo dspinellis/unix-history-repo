c9371b24f7263e9a664c9efb137605e49753cb7d
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-03-16 20:01:28 -0800

    rework for pmake 2.1
    SCCS-vsn: share/mk/bsd.prog.mk 5.5

--- usr/src/share/mk/bsd.prog.mk
+++ usr/src/share/mk/bsd.prog.mk
@@ -1,155 +1,161 @@
+# A Makefile for the BSD source tree.
+#
+#	@(#)bsd.prog.mk	5.5 (Berkeley) %G%
+#
 
-MAKE=	pmake	# for now...
-CPP=	newcpp
+# user defines:
+#	BINDIR, BINGRP, BINMODE, BINOWN
+#		binary target directory, group, mode, owner
+#	CLEANFILES
+#		list of files to be removed for the target clean; used,
+#		for example, to specify .c's produced from .y's.
+#	LDLIB	the list of libraries that the program loads, in the format
+#		expected by the loader.
+#	LINKS	list of binary links of the form "link target link target";
+#		for example, "/bin/[ /usr/bin/test" would link /bin/[ to
+#		/usr/bin/test"; not particularly labor saving, but prevents
+#		needing your own install target.
+#	MANDIR, MANMODE
+#		manual page installation directory, mode
+#	MLINKS	list of man page links of the form "link target link target",
+#		with some trickiness so the suffix specifies the directory to
+#		use.  For example, "a.1 b.2 c.3 d.4" would link ${MANDIR}1/a.0
+#		to ${MANDIR}2/b.0 and ${MANDIR}3/c.0 to ${MANDIR}4/d.0.
+#	PROG	program name
+#	SHAREDSTRINGS
+#		objects share strings using ${XSTR}
+#	SRCS	list of .c sources if the program has multiple files.
+#	SRCLIB	the list of libraries that the program depends on; normally
+#		from the LIB* list in this file.
+#	STRIP	strip flag
+#
+# user macros:
+#	STDALL	standard all target
+#	STDCLEAN
+#		standard clean target
+#	STDCLEANDIR
+#		standard cleandir target
+#	STDDEPEND
+#		standard depend target
+#	STDLINT
+#		standard lint target
+#	STDINSTALL
+#		standard install target
+#	STDTAGS	standard tags target
 
-# libraries used for dependency lines
-LIBUTIL=	/usr/lib/libutil.a
-LIBMATH=	/usr/lib/libm.a
-LIBKRB=		/usr/lib/libkrb.a
-LIBDES=		/usr/lib/libdes.a
+# permit a hierarchy of Makefile include files
+.if exists(../Make.include)
+.include "../Make.include"
+.endif
+
+# name of the dependency file
+DEPENDFILE=	.depend
+
+# standard libraries
+LIBC=		/lib/libc.a
 LIBCOMPAT=	/usr/lib/libcompat.a
+LIBDES=		/usr/lib/libdes.a
+LIBKRB=		/usr/lib/libkrb.a
+LIBMATH=	/usr/lib/libm.a
+LIBUTIL=	/usr/lib/libutil.a
 
-# any specified manual pages -- if none have been specified at this point,
-# we make assumptions about what they are and what they're called.
-MANALL=	${MAN1} ${MAN2} ${MAN3} ${MAN4} ${MAN5} ${MAN6} ${MAN7} ${MAN8}
+# read-only version of standard .c.o rule
+READONLY: .USE
+	${CC} ${CFLAGS} -c -R ${.IMPSRC}
 
-# user defines:
-#	SUBDIR -- the list of subdirectories to be processed
-#
-# if SUBDIR is set, we're in a makefile that processes subdirectories;
-# for all the standard targets, change to the subdirectory and make the
-# target.  If making one of the subdirectories, change to it and make
-# the target "all".  Machine dependent subdirectories take precedence
-# over standard subdirectories.
-.if defined(SUBDIR)
-all depend clean cleandir lint install tags:
-	@for entry in ${SUBDIR}; do
-		(echo "====> $$entry"
-		if test -d $${entry}.${MACHINE}; then
-			cd $${entry}.${MACHINE}
-		else
-			cd $${entry}
-		fi
-		${MAKE} ${.TARGET})
-	done
-
-${SUBDIR}:
-	@if test -d ${.TARGET}.${MACHINE}; then
-		cd ${.TARGET}.${MACHINE}
-	else
-		cd ${.TARGET}
-	fi
-	${MAKE} all
-
-.else	# !SUBDIR
-
-# user defines:
-#	SHAREDSTRINGS -- boolean variable, if sharing strings in objects.
-#
-# if SHAREDSTRINGS is defined, use XSTR to build objects.
-#
+# if the user defines SHAREDSTRINGS, they want objects to share strings.
+# Turn off parallel makes (the strings file is single threaded) and
+# rewrite the .c.o rule to use XSTR to build the objects.
 .if defined(SHAREDSTRINGS)
 .NOTPARALLEL:
-XSTR=	xstr
+XSTR=/usr/bin/xstr
 .c.o:
-	${CC} -E ${.INCLUDES} ${CFLAGS} ${.IMPSRC} | ${XSTR} -c -
-	@${CC} ${.INCLUDES} ${CFLAGS} -c x.c -o ${.TARGET}
+	${CC} -E ${CFLAGS} ${.IMPSRC} | ${XSTR} -c -
+	@${CC} ${CFLAGS} -c x.c -o ${.TARGET}
 	@rm -f x.c
 .endif
 
-# the default target is all
+# the default target.
 .MAIN: all
 
-# user defines:
-#	PROGC	-- the name of a program composed of a single source module
-#	PROGO	-- the name of a program composed of several object modules
-#	SRCLIB	-- the list of libraries that the program depends on;
-#		   normally from the LIB* list at the top of this file.
-#	LDLIB	-- the list of libraries that the program loads, in the
-#		   format expected by the loader.
+# manual pages -- if the Makefile doesn't specify one, assume that they're
+# named in a standard way, i.e. it's in section 1 with the same name as the
+# program.
+MANALL=	${MAN1} ${MAN2} ${MAN3} ${MAN4} ${MAN5} ${MAN6} ${MAN7} ${MAN8}
 
-all: ${PROGC} ${PROGO}
+# In the BSD source tree, each program Makefile should specify PROG, the
+# name of the program.
+all: ${PROG}
 
-# if the program is composed of a single source module, that module is
-# C source with the same name as the program.  If no manual pages have
-# been defined, it's in section 1 with the same name as the program.
-.if defined(PROGC)
-SRCS=	${PROGC}.c
-
-.if !defined(MAN1) && !defined(MAN2) && !defined(MAN3) && !defined(MAN4) && !defined(MAN5) && !defined(MAN6) && !defined(MAN7) && !defined(MAN8)
-MAN1=	${PROGC}.0
-.endif
-
-${PROGC}: ${SRCS} ${LIBC} ${SRCLIB}
-	${CC} ${CFLAGS} -o ${.TARGET} ${SRCS} ${LDLIB}
-
-.depend: ${SRCS}
-	set -
-	${CPP} -M ${CFLAGS:M-[ID]*} ${.INCLUDES} ${.ALLSRC} |
-		sed "s/^/${PROGC}: /" > ${DEPENDFILE}.new
-	mv ${DEPENDFILE}.new ${DEPENDFILE}
-
-.endif	# PROGC
+.if defined(SRCS)
 
 # if the program is composed of several object modules, the modules are
-# the list of sources with the .o's translated to .c's.  If no manual
-# pages have been defined, it's in section 1 with the same name as the
-# program.  Objects depend on their C source counterparts.
-.if defined(PROGO)
-
+# the list of sources with the .o's translated to .c's.
 OBJS=	${SRCS:.c=.o}
 
-.if !defined(MAN1) && !defined(MAN2) && !defined(MAN3) && !defined(MAN4) && !defined(MAN5) && !defined(MAN6) && !defined(MAN7) && !defined(MAN8)
-MAN1=	${PROGO}.0
+.if !defined(MAN1) && !defined(MAN2) && !defined(MAN3) && !defined(MAN4) && \
+	!defined(MAN5) && !defined(MAN6) && !defined(MAN7) && !defined(MAN8)
+MAN1=	${PROG}.0
 .endif
 
-${PROGO}: ${OBJS} ${LIBC} ${SRCLIB}
+${PROG}: ${OBJS} ${LIBC} ${SRCLIB}
 	${CC} ${LDFLAGS} -o ${.TARGET} ${OBJS} ${LDLIB}
 
 .depend: ${SRCS}
-	set -
-	set ${OBJS}
-	rm -f ${DEPENDFILE}.new
-	for entry in ${.ALLSRC}; do
-		${CPP} -M ${CFLAGS:M-[ID]*} ${.INCLUDES} $$entry |
-			sed "s/^/$$1: /" >> ${DEPENDFILE}.new
+	@set ${OBJS}
+	@for entry in ${.ALLSRC}; do
+		echo "$$1:" \
+		    `${CPP} -M ${CFLAGS:M-[ID]*} ${.INCLUDES} $$entry`
 		shift
-	done
-	mv ${DEPENDFILE}.new ${DEPENDFILE}
+	done > ${DEPENDFILE}
 
 ${OBJS}: ${.PREFIX}.c
 
-.endif	# PROGO
+.else
 
+SRCS= ${PROG}.c
+
+.if !defined(MAN1) && !defined(MAN2) && !defined(MAN3) && !defined(MAN4) && \
+	!defined(MAN5) && !defined(MAN6) && !defined(MAN7) && !defined(MAN8)
+MAN1=	${PROG}.0
+.endif
+
+${PROG}: ${SRCS} ${LIBC} ${SRCLIB}
+	${CC} ${CFLAGS} -o ${.TARGET} ${SRCS} ${LDLIB}
+
+.depend: ${SRCS}
+	echo "${PROG}:" \
+	    `${CPP} -M ${CFLAGS:M-[ID]*} ${.INCLUDES} ${.ALLSRC}` \
+		    > ${DEPENDFILE}
+
+.endif	# defined(SRCS)
+
+# standard targets
 depend: .depend
 
-# user defines:
-#	CLEANFILES	-- list of files to be removed for the target clean;
-#			   used, for example, to specify .c's produced from
-#			   .y's.
-JUNKFILES=	a.out Errs errs mklog core
-clean:
-	rm -f ${PROGC} ${PROGO} ${OBJS} ${CLEANFILES} ${JUNKFILES}
+STDCLEAN: .USE
+	rm -f a.out Errs errs mklog core ${CLEANFILES} ${PROG} ${OBJS}
 
-TAGSFILE=	tags
-cleandir: clean
+clean: STDCLEAN
+
+STDCLEANDIR: .USE
 	rm -f ${MANALL} ${TAGSFILE} ${DEPENDFILE}
 
-LINTFLAGS=	-chapbx
-lint: ${SRCS}
+cleandir: clean STDCLEANDIR
+
+LINTFLAGS=-chapbx
+STDLINT: .USE
 	lint ${LINTFLAGS} ${CFLAGS} ${.ALLSRC}
 
-tags: ${SRCS}
+lint: ${SRCS} STDLINT
+
+TAGSFILE=tags
+STDTAGS: .USE
 	ctags ${.ALLSRC}
 
-# user defines:
-#	MDIR	-- default manual page installation directory
-#	MANMODE	-- default manual page installation mode
-#	STRIP	-- default strip flag
-#	BINMODE	-- default binary installation mode
-#	BINOWN	-- default binary owner
-#	BINGRP	-- default binary group
-MDIR?=		/usr/share/man/cat
+tags: ${SRCS} STDTAGS
+
+MANDIR?=	/usr/share/man/cat
 MANMODE?=	444
 STRIP?=		-s
 BINMODE?=	755
@@ -157,84 +163,69 @@ BINOWN?=	bin
 BINGRP?=	bin
 
 # install target -- creates manual pages, then installs the binaries,
-# manual pages, and manual page links.
-install: ${MANALL}
+# links, manual pages, and manual page links.
+STDINSTALL: .USE
 	install ${STRIP} -o ${BINOWN} -g ${BINGRP} -m ${BINMODE} \
-	    ${PROGC} ${PROGO} ${DESTDIR}${DIR}
+	    ${PROG} ${DESTDIR}${BINDIR}
 .if defined(MAN1)
 	install -c -o ${BINOWN} -g ${BINGRP} -m ${MANMODE} ${MAN1} \
-	    ${DESTDIR}${MDIR}1
+	    ${DESTDIR}${MANDIR}1
 .endif
 .if defined(MAN2)
 	install -c -o ${BINOWN} -g ${BINGRP} -m ${MANMODE} ${MAN2} \
-	    ${DESTDIR}${MDIR}2
+	    ${DESTDIR}${MANDIR}2
 .endif
 .if defined(MAN3)
 	install -c -o ${BINOWN} -g ${BINGRP} -m ${MANMODE} ${MAN3} \
-	    ${DESTDIR}${MDIR}3
+	    ${DESTDIR}${MANDIR}3
 .endif
 .if defined(MAN4)
 	install -c -o ${BINOWN} -g ${BINGRP} -m ${MANMODE} ${MAN4} \
-	    ${DESTDIR}${MDIR}4
+	    ${DESTDIR}${MANDIR}4
 .endif
 .if defined(MAN5)
 	install -c -o ${BINOWN} -g ${BINGRP} -m ${MANMODE} ${MAN5} \
-	    ${DESTDIR}${MDIR}5
+	    ${DESTDIR}${MANDIR}5
 .endif
 .if defined(MAN6)
 	install -c -o ${BINOWN} -g ${BINGRP} -m ${MANMODE} ${MAN6} \
-	    ${DESTDIR}${MDIR}6
+	    ${DESTDIR}${MANDIR}6
 .endif
 .if defined(MAN7)
 	install -c -o ${BINOWN} -g ${BINGRP} -m ${MANMODE} ${MAN7} \
-	    ${DESTDIR}${MDIR}7
+	    ${DESTDIR}${MANDIR}7
 .endif
 .if defined(MAN8)
 	install -c -o ${BINOWN} -g ${BINGRP} -m ${MANMODE} ${MAN8} \
-	    ${DESTDIR}${MDIR}8
+	    ${DESTDIR}${MANDIR}8
 .endif
-# user defines:
-#	LINKS	-- list of binary links of the form "link target
-#		   link target"; for example, "/bin/[ /usr/bin/test"
-#		   would link /bin/[ to /usr/bin/test"; not particularly
-#		   labor saving, but prevents needing your own install
-#		   target.
-#	MLINKS	-- list of manual page linkes of the form "link target
-#		   link target"; for example, "a.1 b.2 c.3 d.4" would
-#		   link ${MDIR}1/a.0 to ${MDIR}2/b.0 and ${MDIR}3/c.0
-#		   to ${MDIR}4/d.0.
 .if defined(LINKS)
-	@set ${LINKS}
-	@while :; do
-		if `test $$# -lt 2`; then
-			break;
-		fi
-		t=$$1
-		shift
-		l=$$1
-		shift
-		echo $$l -\> $$t
-		rm -f $$t
-		ln ${DESTDIR}$$l ${DESTDIR}$$t
-	done
+	@set ${LINKS}; \
+	while test $$# -ge 2; do \
+		t=$$1; \
+		shift; \
+		l=$$1; \
+		shift; \
+		echo $$l -\> $$t; \
+		rm -f $$t; \
+		ln ${DESTDIR}$$l ${DESTDIR}$$t; \
+	done; true
 .endif	# LINKS
 .if defined(MLINKS)
-	@set ${MLINKS}
-	@while :; do
-		if `test $$# -lt 2`; then
-			break;
-		fi
-		name=$$1
-		shift
-		dir=${MDIR}`expr $$name : '[^\.]*\.\(.*\)'`
-		t=${DESTDIR}$${dir}/`expr $$name : '\([^\.]*\)'`.0
-		name=$$1
-		shift
-		dir=${MDIR}`expr $$name : '[^\.]*\.\(.*\)'`
-		l=${DESTDIR}$${dir}/`expr $$name : '\([^\.]*\)'`.0
-		echo $$l -\> $$t
-		rm -f $$t
-		ln $$l $$t
-	done
+	@set ${MLINKS}; \
+	while test $$# -ge 2; do \
+		name=$$1; \
+		shift; \
+		dir=${MANDIR}`expr $$name : '[^\.]*\.\(.*\)'`; \
+		t=${DESTDIR}$${dir}/`expr $$name : '\([^\.]*\)'`.0; \
+		name=$$1; \
+		shift; \
+		dir=${MANDIR}`expr $$name : '[^\.]*\.\(.*\)'`; \
+		l=${DESTDIR}$${dir}/`expr $$name : '\([^\.]*\)'`.0; \
+		echo $$l -\> $$t; \
+		rm -f $$t; \
+		ln $$l $$t; \
+	done; true
 .endif	# MLINKS
-.endif	# SUBDIR
+
+install: ${MANALL} STDINSTALL
