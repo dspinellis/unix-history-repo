3c743149b681e33529d72c4c38db843c8129d5dc
Mike Karels <karels@ucbvax.Berkeley.EDU>
1990-04-04 04:52:36 -0800

    rm setjmp, longjmp
    SCCS-vsn: sys/vax/vax/locore.s 7.22

--- usr/src/sys/vax/vax/locore.s
+++ usr/src/sys/vax/vax/locore.s
@@ -3,7 +3,7 @@
  * All rights reserved.  The Berkeley software License Agreement
  * specifies the terms and conditions for redistribution.
  *
- *	@(#)locore.s	7.21 (Berkeley) %G%
+ *	@(#)locore.s	7.22 (Berkeley) %G%
  */
 
 #include "psl.h"
