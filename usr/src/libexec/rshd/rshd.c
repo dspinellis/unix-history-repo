556add83a41acc23ffa0b094cdda9293a93725d6
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-04-03 17:37:33 -0800

    include from kerberosIV, not /usr/include
    SCCS-vsn: libexec/rshd/rshd.c 5.29

--- usr/src/libexec/rshd/rshd.c
+++ usr/src/libexec/rshd/rshd.c
@@ -22,7 +22,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)rshd.c	5.28 (Berkeley) %G%";
+static char sccsid[] = "@(#)rshd.c	5.29 (Berkeley) %G%";
 #endif /* not lint */
 
 /* From:
@@ -66,7 +66,7 @@ int	error();
 int	sent_null;
 
 #ifdef	KERBEROS
-#include <krb.h>
+#include <kerberosIV/krb.h>
 #define	VERSION_SIZE	9
 #define SECURE_MESSAGE  "This rsh session is using DES encryption for all transmissions.\r\n"
 #define	OPTIONS		"alnkvx"
