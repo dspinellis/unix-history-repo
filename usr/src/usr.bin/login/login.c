71935ada9c29a6624ee93eb08f13da5dcfb0496d
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-04-03 17:34:24 -0800

    include from kerberosIV directory
    SCCS-vsn: usr.bin/login/login.c 5.51

--- usr/src/usr.bin/login/login.c
+++ usr/src/usr.bin/login/login.c
@@ -22,7 +22,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)login.c	5.50 (Berkeley) %G%";
+static char sccsid[] = "@(#)login.c	5.51 (Berkeley) %G%";
 #endif /* not lint */
 
 /*
