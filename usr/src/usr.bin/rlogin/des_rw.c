a5a5918dbc6bfce7e2dc6bdd1ef0f98e4ce6430a
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-04-03 19:06:17 -0800

    include files moved to kerberosIV, not /usr/include
    SCCS-vsn: usr.bin/rlogin/rlogin.c 5.22
    SCCS-vsn: usr.bin/rlogin/des_rw.c 5.2
    SCCS-vsn: usr.bin/rlogin/kcmd.c 5.3

--- usr/src/usr.bin/rlogin/des_rw.c
+++ usr/src/usr.bin/rlogin/des_rw.c
@@ -26,10 +26,9 @@
 static char sccsid[] = "@(#)des_rw.c	1.7 (Berkeley) 2/6/89";
 #endif /* not lint */
 
-#include <sys/types.h>
 #include <sys/param.h>
-#include <krb.h>
-#include <des.h>
+#include <kerberosIV/krb.h>
+#include <kerberosIV/des.h>
 
 extern long		random();
 static unsigned char	des_inbuf[10240], storage[10240], *store_ptr;
