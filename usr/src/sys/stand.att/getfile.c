ca583a55a7d681eda9a162aabad194163fb0b0b4
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-04-04 23:37:23 -0800

    delete unneeded header files
    SCCS-vsn: sys/stand.att/getfile.c 7.4
    SCCS-vsn: sys/stand.att/ls.c 7.8

--- usr/src/sys/stand.att/getfile.c
+++ usr/src/sys/stand.att/getfile.c
@@ -14,14 +14,11 @@
  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  *
- *	@(#)getfile.c	7.3 (Berkeley) %G%
+ *	@(#)getfile.c	7.4 (Berkeley) %G%
  */
 
 #include "sys/param.h"
 #include "sys/time.h"
-#include "sys/vnode.h"
-#include "ufs/inode.h"
-#include "ufs/fs.h"
 #include "saio.h"
 
 getfile(prompt, mode)
