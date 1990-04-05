7c557e1f8935737b9b09d8c27e14f7f68d20f7da
Mike Karels <karels@ucbvax.Berkeley.EDU>
1990-04-05 04:20:12 -0800

    new tsleep, unwind on signal; server exit when signal received
    SCCS-vsn: sys/nfs/nfs_syscalls.c 7.11

--- usr/src/sys/nfs/nfs_syscalls.c
+++ usr/src/sys/nfs/nfs_syscalls.c
@@ -17,7 +17,7 @@
  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  *
- *	@(#)nfs_syscalls.c	7.10 (Berkeley) %G%
+ *	@(#)nfs_syscalls.c	7.11 (Berkeley) %G%
  */
 
 #include "param.h"
