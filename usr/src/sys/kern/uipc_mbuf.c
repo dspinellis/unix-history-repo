5e585f4d9cf30840f944bede2ed9b3f1467a509c
Keith Sklower <sklower@ucbvax.Berkeley.EDU>
1990-04-05 22:16:21 -0800

    remove unneeded (and harmful) header file dir.h
    SCCS-vsn: sys/kern/uipc_mbuf.c 7.13

--- usr/src/sys/kern/uipc_mbuf.c
+++ usr/src/sys/kern/uipc_mbuf.c
@@ -13,11 +13,10 @@
  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  *
- *	@(#)uipc_mbuf.c	7.12 (Berkeley) %G%
+ *	@(#)uipc_mbuf.c	7.13 (Berkeley) %G%
  */
 
 #include "param.h"
-#include "dir.h"
 #include "user.h"
 #include "proc.h"
 #include "cmap.h"
