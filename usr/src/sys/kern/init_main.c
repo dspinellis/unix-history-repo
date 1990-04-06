a09156c71efccfdf6ff161e4a9d5c32bf6c052f4
Marc Teitelbaum <marc@ucbvax.Berkeley.EDU>
1990-04-05 21:47:23 -0800

    swapper and pagedaemon have names set in p_comm.
    SCCS-vsn: sys/kern/init_main.c 7.22

--- usr/src/sys/kern/init_main.c
+++ usr/src/sys/kern/init_main.c
@@ -3,7 +3,7 @@
  * All rights reserved.  The Berkeley software License Agreement
  * specifies the terms and conditions for redistribution.
  *
- *	@(#)init_main.c	7.21 (Berkeley) %G%
+ *	@(#)init_main.c	7.22 (Berkeley) %G%
  */
 
 #include "param.h"
@@ -61,6 +61,7 @@ main(firstaddr)
 	 * set up system process 0 (swapper)
 	 */
 	p = &proc[0];
+	bcopy("swapper", p->p_comm, sizeof ("swapper"));
 	p->p_p0br = u.u_pcb.pcb_p0br;
 	p->p_szpt = 1;
 	p->p_addr = uaddr(p);
@@ -216,6 +217,7 @@ main(firstaddr)
 	if (newproc(0)) {
 		proc[2].p_flag |= SLOAD|SSYS;
 		proc[2].p_dsize = u.u_dsize = nswbuf*CLSIZE*KLMAX; 
+		bcopy("pagedaemon", proc[2].p_comm, sizeof ("pagedaemon"));
 		pageout();
 		/*NOTREACHED*/
 	}
