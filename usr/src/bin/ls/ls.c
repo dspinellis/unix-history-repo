d12c4e2a877289fc42a2a9d70ee12d1d3c49ca1c
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-04-10 00:58:28 -0800

    sccs include now replaces the line
    SCCS-vsn: bin/ls/ls.c 5.39

--- usr/src/bin/ls/ls.c
+++ usr/src/bin/ls/ls.c
@@ -5,7 +5,7 @@
  * This code is derived from software contributed to Berkeley by
  * Michael Fischbein.
  *
-%sccs.include.redist.c%
+ * %sccs.include.redist.c%
  */
 
 #ifndef lint
@@ -15,7 +15,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)ls.c	5.38 (Berkeley) %G%";
+static char sccsid[] = "@(#)ls.c	5.39 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/param.h>
