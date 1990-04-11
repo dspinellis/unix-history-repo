b1758572d07b1c028bd7fdf85f9d94c7d6cc7edd
Mike Karels <karels@ucbvax.Berkeley.EDU>
1990-04-10 18:30:34 -0800

    fds can be local; KNF
    SCCS-vsn: lib/libc/gen/popen.c 5.13

--- usr/src/lib/libc/gen/popen.c
+++ usr/src/lib/libc/gen/popen.c
@@ -19,7 +19,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)popen.c	5.12 (Berkeley) %G%";
+static char sccsid[] = "@(#)popen.c	5.13 (Berkeley) %G%";
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/param.h>
@@ -31,47 +31,46 @@ static char sccsid[] = "@(#)popen.c	5.12 (Berkeley) %G%";
 #include <paths.h>
 
 static pid_t *pids;
-static int fds;
 
 FILE *
 popen(program, type)
 	char *program, *type;
 {
 	FILE *iop;
-	int pdes[2], pid;
+	int pdes[2], fds, pid;
 	char *malloc();
 
 	if (*type != 'r' && *type != 'w' || type[1])
-		return(NULL);
+		return (NULL);
 
 	if (pids == NULL) {
 		if ((fds = getdtablesize()) <= 0)
-			return(NULL);
+			return (NULL);
 		if ((pids = (pid_t *)malloc((u_int)(fds * sizeof(int)))) == NULL)
-			return(NULL);
+			return (NULL);
 		bzero((char *)pids, fds * sizeof(pid_t));
 	}
 	if (pipe(pdes) < 0)
-		return(NULL);
+		return (NULL);
 	switch (pid = vfork()) {
 	case -1:			/* error */
-		(void)close(pdes[0]);
-		(void)close(pdes[1]);
-		return(NULL);
+		(void) close(pdes[0]);
+		(void) close(pdes[1]);
+		return (NULL);
 		/* NOTREACHED */
 	case 0:				/* child */
 		if (*type == 'r') {
 			if (pdes[1] != STDOUT_FILENO) {
-				(void)dup2(pdes[1], STDOUT_FILENO);
-				(void)close(pdes[1]);
+				(void) dup2(pdes[1], STDOUT_FILENO);
+				(void) close(pdes[1]);
 			}
-			(void)close(pdes[0]);
+			(void) close(pdes[0]);
 		} else {
 			if (pdes[0] != STDIN_FILENO) {
-				(void)dup2(pdes[0], STDIN_FILENO);
-				(void)close(pdes[0]);
+				(void) dup2(pdes[0], STDIN_FILENO);
+				(void) close(pdes[0]);
 			}
-			(void)close(pdes[1]);
+			(void) close(pdes[1]);
 		}
 		execl(_PATH_BSHELL, "sh", "-c", program, NULL);
 		_exit(127);
@@ -80,13 +79,13 @@ popen(program, type)
 	/* parent; assume fdopen can't fail...  */
 	if (*type == 'r') {
 		iop = fdopen(pdes[0], type);
-		(void)close(pdes[1]);
+		(void) close(pdes[1]);
 	} else {
 		iop = fdopen(pdes[1], type);
-		(void)close(pdes[0]);
+		(void) close(pdes[0]);
 	}
 	pids[fileno(iop)] = pid;
-	return(iop);
+	return (iop);
 }
 
 pclose(iop)
@@ -104,13 +103,13 @@ pclose(iop)
 	 * returns an error.
 	 */
 	if (pids == NULL || pids[fdes = fileno(iop)] == 0)
-		return(-1);
-	(void)fclose(iop);
+		return (-1);
+	(void) fclose(iop);
 	omask = sigblock(sigmask(SIGINT)|sigmask(SIGQUIT)|sigmask(SIGHUP));
 	do {
 		pid = waitpid(pids[fdes], &pstat, 0);
 	} while (pid == -1 && errno == EINTR);
-	(void)sigsetmask(omask);
+	(void) sigsetmask(omask);
 	pids[fdes] = 0;
-	return(pid == -1 ? -1 : pstat.w_status);
+	return (pid == -1 ? -1 : pstat.w_status);
 }
