d576f4d7d1089848452671a1efdfabbc46af78f6
Rick Adams <rick@ucbvax.Berkeley.EDU>
1990-04-16 15:10:41 -0800

    check if e_xfp is null before flushing it move load average into a global so dont need to recalculate for every job
    SCCS-vsn: usr.sbin/sendmail/src/daemon.c 5.32

--- usr/src/usr.sbin/sendmail/src/daemon.c
+++ usr/src/usr.sbin/sendmail/src/daemon.c
@@ -21,11 +21,13 @@
 
 #ifndef lint
 #ifdef DAEMON
-static char sccsid[] = "@(#)daemon.c	5.31 (Berkeley) %G% (with daemon mode)";
+static char sccsid[] = "@(#)daemon.c	5.32 (Berkeley) %G% (with daemon mode)";
 #else
-static char sccsid[] = "@(#)daemon.c	5.31 (Berkeley) %G% (without daemon mode)";
+static char sccsid[] = "@(#)daemon.c	5.32 (Berkeley) %G% (without daemon mode)";
 #endif
 #endif /* not lint */
+
+int la;	/* load average */
 
 #ifdef DAEMON
 
@@ -161,7 +163,6 @@ getrequests()
 		register int pid;
 		auto int lotherend;
 		extern int RefuseLA;
-		register int la;
 
 		/* see if we are rejecting connections */
 		while ((la = getla()) > RefuseLA)
@@ -402,7 +403,8 @@ again:
 		int on = 1;
 		(void) setsockopt(DaemonSocket, SOL_SOCKET, SO_DEBUG, (char *)&on, sizeof on);
 	}
-	(void) fflush(CurEnv->e_xfp);			/* for debugging */
+	if (CurEnv->e_xfp != NULL)
+		(void) fflush(CurEnv->e_xfp);		/* for debugging */
 	errno = 0;					/* for debugging */
 #ifdef NVMUNIX
 	bind(s, &SendmailAddress, sizeof SendmailAddress, 0);
@@ -448,8 +450,12 @@ again:
 			return (EX_TEMPFAIL);
 
 		  default:
-			message(Arpa_Info, "%s", errstring(sav_errno));
-			return (EX_UNAVAILABLE);
+			{
+				extern char *errstring();
+
+				message(Arpa_Info, "%s", errstring(sav_errno));
+				return (EX_UNAVAILABLE);
+			}
 		}
 	}
 
