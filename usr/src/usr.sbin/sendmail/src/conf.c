10b4127fd11930e13295bb757ee29ff7ba13d878
Rick Adams <rick@ucbvax.Berkeley.EDU>
1990-04-16 19:00:20 -0800

    forgot an extern int la; for load avarage.
    SCCS-vsn: usr.sbin/sendmail/src/conf.c 5.22

--- usr/src/usr.sbin/sendmail/src/conf.c
+++ usr/src/usr.sbin/sendmail/src/conf.c
@@ -17,7 +17,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)conf.c	5.21 (Berkeley) %G%";
+static char sccsid[] = "@(#)conf.c	5.22 (Berkeley) %G%";
 #endif /* not lint */
 
 # include <sys/ioctl.h>
