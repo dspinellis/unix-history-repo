ff5eaa6afe50f91b629ce5955af91c4e0207785b
Marc Teitelbaum <marc@ucbvax.Berkeley.EDU>
1990-04-18 20:24:40 -0800

    make work on block or character special
    SCCS-vsn: lib/libc/gen/devname.c 5.2

--- usr/src/lib/libc/gen/devname.c
+++ usr/src/lib/libc/gen/devname.c
@@ -1,61 +1,93 @@
-#include <sys/types.h>
-#include <sys/dir.h>
-#include <sys/stat.h>
-#include "pathnames.h"
-#include <stdio.h>
+/*
+ * Copyright (c) 1989 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms are permitted
+ * provided that the above copyright notice and this paragraph are
+ * duplicated in all such forms and that any documentation,
+ * advertising materials, and other materials related to such
+ * distribution and use acknowledge that the software was developed
+ * by the University of California, Berkeley.  The name of the
+ * University may not be used to endorse or promote products derived
+ * from this software without specific prior written permission.
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ */
 
-struct devs {
+#if defined(LIBC_SCCS) && !defined(lint)
+static char sccsid[] = "@(#)devname.c	5.2 (Berkeley) %G%";
+#endif /* LIBC_SCCS and not lint */
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/file.h>
+#include <dirent.h>
+#include <paths.h>
+
+static struct devs {
+	struct	devs *next;
 	dev_t	dev;
 	char	name[MAXNAMLEN+1];
-	struct	devs *next;
+	char	type;
 };
 
-struct devs *devhash[minor(~0)];
+#define	hash(x)	((x)&0xff)
+static struct devs *devhash[minor(~0)];
 
-int devinit;
-
-/* TODO - use a database file stored in /dev, - just check mod time on /dev 
-	hide data structures 
-*/
+static int devinit;
 
 char *
-devname(dev)
+devname(dev, type)
 	dev_t dev;
 {
 	struct devs *devp;
 
 	if (devinit == 0) {
-		DIR *dp = opendir(_PATH_DEV);
-		struct direct *entry;
+		register struct devs *devpp;
+		register struct dirent *entry;
 		struct stat sb;
-		struct devs *devpp;
+		DIR *dp = opendir(_PATH_DEV);
+		int savewd = open(".", O_RDONLY, 0);
+		int specialtype;
 
-		/* XXX XXX - can't chdir as a library routine (add flag?) */
-		if (dp == NULL || chdir(_PATH_DEV) == -1)
+		if (savewd == -1 || dp == NULL || chdir(_PATH_DEV) == -1)
 			return (NULL);
 		while ((entry = readdir(dp)) != NULL) {
 			if (stat(entry->d_name, &sb) == -1)
 				continue;
-			if ((sb.st_mode&S_IFMT) != S_IFCHR)
+			switch(sb.st_mode&S_IFMT) {
+			case S_IFCHR:
+				specialtype = 1;
+				break;
+			case S_IFBLK:
+				specialtype = 0;
+				break;
+			default:
 				continue;
+			}
 			devp = (struct devs *)malloc(sizeof (struct devs));
 			if (devp == NULL)
 				return (NULL);
+			devp->type = specialtype;
 			devp->dev = sb.st_rdev;
 			strcpy(devp->name, entry->d_name);
 			devp->next = NULL;
-			if ((devpp = devhash[minor(sb.st_rdev)]) == NULL)
-				devhash[minor(sb.st_rdev)] = devp;
+			if ((devpp = devhash[hash(sb.st_rdev)]) == NULL)
+				devhash[hash(sb.st_rdev)] = devp;
 			else {
 				for (;devpp->next != NULL; devpp = devpp->next)
 					;
 				devpp->next = devp;
 			}
 		}
-		devinit = 1; 	/* XXX - should have way to invalidate cache ?*/
+		fchdir(savewd);
+		close(savewd);
+		closedir(dp);
+		devinit = 1;
 	}
-	for (devp = devhash[minor(dev)]; devp != NULL; devp = devp->next)
-		if (dev == devp->dev)
+	for (devp = devhash[hash(dev)]; devp != NULL; devp = devp->next)
+		if (dev == devp->dev && type == devp->type)
 			return(devp->name);
 
 	return (NULL);
