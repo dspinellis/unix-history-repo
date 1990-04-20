70a8e95fa4995d7b3cd3e671d17622957acfb60a
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-04-19 17:03:45 -0800

    date and time created 90/04/19 10:03:45 by bostic
    SCCS-vsn: usr.sbin/sendmail/src/pathnames.h 5.1

---
+++ usr/src/usr.sbin/sendmail/src/pathnames.h
@@ -0,0 +1,11 @@
+/*-
+ * Copyright (c) 1990 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.redist.c%
+ *
+ *	@(#)pathnames.h	5.1 (Berkeley) %G%
+ */
+
+#define	_PATH_SENDMAILCF	"/etc/sendmail.cf";
+#define	_PATH_SENDMAILFC	"/etc/sendmail.fc";
