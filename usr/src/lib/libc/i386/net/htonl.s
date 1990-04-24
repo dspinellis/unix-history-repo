1a0ebe0a9f1928c6644710435da803137863c9e9
William Nesheim <william@ucbvax.Berkeley.EDU>
1990-04-24 03:01:37 -0800

    date and time created 90/04/23 20:01:37 by william
    SCCS-vsn: lib/libc/i386/net/htonl.s 5.1

---
+++ usr/src/lib/libc/i386/net/htonl.s
@@ -0,0 +1,24 @@
+/*-
+ * Copyright (c) 1990 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to Berkeley by
+ * William Jolitz.
+ *
+ * %sccs.include.redist.c%
+ */
+
+#if defined(LIBC_SCCS) && !defined(lint)
+	.asciz "@(#)htonl.s	5.1 (Berkeley) %G%"
+#endif /* LIBC_SCCS and not lint */
+
+/* hostorder = ntohl(netorder) */
+
+#include "DEFS.h"
+
+ENTRY(ntohl)
+	mov	%eax,4(sp)
+	xchg	%al,%ah
+	rol	$16,%eax
+	xchg	%al,%ah
+	ret
