63c3a9d7181176b5bc7bbc0bc0f53a2814d1a968
William Nesheim <william@ucbvax.Berkeley.EDU>
1990-04-24 02:39:04 -0800

    date and time created 90/04/23 19:39:04 by william
    SCCS-vsn: lib/libc/i386/sys/getppid.s 5.1

---
+++ usr/src/lib/libc/i386/sys/getppid.s
@@ -0,0 +1,19 @@
+/*-
+ * Copyright (c) 1990 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to Berkeley by
+ * William Jolitz.
+ *
+ * %sccs.include.redist.c%
+ */
+
+#if defined(SYSLIBC_SCCS) && !defined(lint)
+	.asciz "@(#)getppid.s	5.1 (Berkeley) %G%"
+#endif /* SYSLIBC_SCCS and not lint */
+
+#include "SYS.h"
+
+PSEUDO(getppid,getpid)
+	movl	%edx,%eax
+	ret			/* ppid = getppid(); */
