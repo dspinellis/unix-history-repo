4cadad92fc13ddc853fdd950f0dcb583b804ed83
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-04-28 19:54:02 -0800

    don't need paths.h anymore
    SCCS-vsn: old/implog/pathnames.h 5.4

--- usr/src/old/implog/pathnames.h
+++ usr/src/old/implog/pathnames.h
@@ -14,9 +14,7 @@
  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  *
- *	@(#)pathnames.h	5.3 (Berkeley) %G%
+ *	@(#)pathnames.h	5.4 (Berkeley) %G%
  */
-
-#include <paths.h>
 
 #define	_PATH_IMPLOG	"/var/log/implog"
