b05b8431f768945899882f3eb8b5b87ba892c811
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-04-29 22:50:28 -0800

    date and time created 90/04/29 15:50:28 by bostic
    SCCS-vsn: libexec/mail.local/pathnames.h 5.1

---
+++ usr/src/libexec/mail.local/pathnames.h
@@ -0,0 +1,14 @@
+/*-
+ * Copyright (c) 1990 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.redist.c%
+ *
+ *	@(#)pathnames.h	5.1 (Berkeley) %G%
+ */
+
+#include <paths.h>
+
+#undef _PATH_TMP
+#define _PATH_TMP	"/tmp/maXXXXX"
+#define _PATH_MAILDIR	"/var/mail/"
