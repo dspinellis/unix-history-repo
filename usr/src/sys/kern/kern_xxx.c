8d18fe0a7dc70628305b6224872195bd2ebe7e18
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-05-03 02:05:53 -0800

    add oldquota call
    SCCS-vsn: sys/kern/kern_xxx.c 7.10

--- usr/src/sys/kern/kern_xxx.c
+++ usr/src/sys/kern/kern_xxx.c
@@ -3,7 +3,7 @@
  * All rights reserved.  The Berkeley software License Agreement
  * specifies the terms and conditions for redistribution.
  *
- *	@(#)kern_xxx.c	7.9 (Berkeley) %G%
+ *	@(#)kern_xxx.c	7.10 (Berkeley) %G%
  */
 
 #include "param.h"
@@ -69,6 +69,12 @@ reboot()
 }
 
 ovhangup()
+{
+
+	RETURN (EINVAL);
+}
+
+oldquota()
 {
 
 	RETURN (EINVAL);
