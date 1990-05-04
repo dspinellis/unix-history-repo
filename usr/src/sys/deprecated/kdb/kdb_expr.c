f6fa1ad27048537b5d688934671609e7d7e8aec6
Keith Sklower <sklower@ucbvax.Berkeley.EDU>
1990-05-04 00:47:27 -0800

    first crack at torek changes, may need bug fixes
    SCCS-vsn: sys/deprecated/kdb/kdb_expr.c 7.6

--- usr/src/sys/deprecated/kdb/kdb_expr.c
+++ usr/src/sys/deprecated/kdb/kdb_expr.c
@@ -3,36 +3,36 @@
  * All rights reserved.  The Berkeley software License Agreement
  * specifies the terms and conditions for redistribution.
  *
- *	@(#)kdb_expr.c	7.5 (Berkeley) %G%
+ *	@(#)kdb_expr.c	7.6 (Berkeley) %G%
  */
 
 #include "../kdb/defs.h"
 
-char	*BADSYM;
-char	*BADVAR;
-char	*BADKET;
-char	*BADSYN;
-char	*NOCFN;
-char	*NOADR;
-char	*BADLOC;
+char	*kdbBADSYM;
+char	*kdbBADVAR;
+char	*kdbBADKET;
+char	*kdbBADSYN;
+char	*kdbNOCFN;
+char	*kdbNOADR;
+char	*kdbBADLOC;
 
-ADDR	lastframe;
-ADDR	savlastf;
-ADDR	savframe;
-ADDR	savpc;
-ADDR	callpc;
+ADDR	kdblastframe;
+ADDR	kdbsavlastf;
+ADDR	kdbsavframe;
+ADDR	kdbsavpc;
+ADDR	kdbcallpc;
 
-char	*lp;
-int	radix;
-char	isymbol[1024];
+char	*kdblp;
+int	kdbradix;
+char	kdbisymbol[1024];
 
-char	lastc, peekc;
+char	kdblastc, kdbpeekc;
 
-long	ditto;
-long	expv;
+long	kdbditto;
+long	kdbexpv;
 
 static long
-round(a,b)
+kdbround(a,b)
 	register long a, b;
 {
 	register long w;
@@ -44,35 +44,35 @@ round(a,b)
 }
 
 /* term | term dyadic expr |  */
-expr(a)
+kdbexpr(a)
 {
 	register rc;
 	register long lhs;
 
-	(void) rdc(); lp--; rc=term(a);
+	(void) kdbrdc(); kdblp--; rc=kdbterm(a);
 
 	while (rc) {
-		lhs = expv;
-		switch ((int)readchar()) {
+		lhs = kdbexpv;
+		switch ((int)kdbreadchar()) {
 		case '+':
-			(void) term(a|1); expv += lhs; break;
+			(void) kdbterm(a|1); kdbexpv += lhs; break;
 		case '-':
-			(void) term(a|1); expv = lhs - expv; break;
+			(void) kdbterm(a|1); kdbexpv = lhs - kdbexpv; break;
 		case '#':
-			(void) term(a|1); expv = round(lhs,expv); break;
+			(void) kdbterm(a|1); kdbexpv = kdbround(lhs,kdbexpv); break;
 		case '*':
-			(void) term(a|1); expv *= lhs; break;
+			(void) kdbterm(a|1); kdbexpv *= lhs; break;
 		case '%':
-			(void) term(a|1); expv = lhs/expv; break;
+			(void) kdbterm(a|1); kdbexpv = lhs/kdbexpv; break;
 		case '&':
-			(void) term(a|1); expv &= lhs; break;
+			(void) kdbterm(a|1); kdbexpv &= lhs; break;
 		case '|':
-			(void) term(a|1); expv |= lhs; break;
+			(void) kdbterm(a|1); kdbexpv |= lhs; break;
 		case ')':
 			if ((a&2)==0)
-				error(BADKET);
+				kdberror(kdbBADKET);
 		default:
-			lp--;
+			kdblp--;
 			return (rc);
 		}
 	}
@@ -81,117 +81,117 @@ expr(a)
 
 /* item | monadic item | (expr) | */
 static
-term(a)
+kdbterm(a)
 {
 
-	switch ((int)readchar()) {
+	switch ((int)kdbreadchar()) {
 	case '*':
-		(void) term(a|1); expv=chkget(expv,DSP);
+		(void) kdbterm(a|1); kdbexpv=kdbchkget(kdbexpv,DSP);
 		return(1);
 	case '@':
-		(void) term(a|1); expv=chkget(expv,ISP);
+		(void) kdbterm(a|1); kdbexpv=kdbchkget(kdbexpv,ISP);
 		return(1);
 	case '-':
-		(void) term(a|1); expv = -expv;
+		(void) kdbterm(a|1); kdbexpv = -kdbexpv;
 		return(1);
 	case '~':
-		(void) term(a|1); expv = ~expv;
+		(void) kdbterm(a|1); kdbexpv = ~kdbexpv;
 		return(1);
 	case '#':
-		(void) term(a|1); expv = !expv;
+		(void) kdbterm(a|1); kdbexpv = !kdbexpv;
 		return(1);
 	case '(':
-		(void) expr(2);
-		if (*lp!=')')
-			error(BADSYN);
-		lp++;
+		(void) kdbexpr(2);
+		if (*kdblp!=')')
+			kdberror(kdbBADSYN);
+		kdblp++;
 		return(1);
 	}
-	lp--;
-	return (item(a));
+	kdblp--;
+	return (kdbitem(a));
 }
 
 /* name [ . local ] | number | . | ^ | <var | <register | 'x | | */
 static
-item(a)
+kdbitem(a)
 {
 	register base, d, regptr;
 	char savc;
 	register long frame;
 	register struct nlist *symp;
 
-	(void) readchar();
-	if (symchar(0)) {
-		readsym();
-		if (lastc=='.') {
-			frame = pcb.pcb_fp; lastframe = 0;
-			callpc = pcb.pcb_pc;
-			while (!errflg) {
-				savpc = callpc;
-				(void) findsym((long)callpc,ISYM);
-				if (eqsym(cursym->n_un.n_name,isymbol,'~'))
+	(void) kdbreadchar();
+	if (kdbsymchar(0)) {
+		kdbreadsym();
+		if (kdblastc=='.') {
+			frame = kdbpcb.pcb_fp; kdblastframe = 0;
+			kdbcallpc = kdbpcb.pcb_pc;
+			while (!kdberrflg) {
+				kdbsavpc = kdbcallpc;
+				(void) kdbfindsym((long)kdbcallpc,ISYM);
+				if (kdbeqsym(kdbcursym->n_un.n_name,kdbisymbol,'~'))
 					break;
-				callpc = getprevpc(frame);
-				lastframe = frame;
+				kdbcallpc = getprevpc(frame);
+				kdblastframe = frame;
 				frame = getprevframe(frame);
 				if (frame == NOFRAME)
-					error(NOCFN);
+					kdberror(kdbNOCFN);
 			}
-			savlastf = lastframe; savframe = frame;
-			(void) readchar();
-			if (symchar(0))
-				chkloc(expv=frame);
-		} else if ((symp=lookup(isymbol))==0)
-			error(BADSYM);
+			kdbsavlastf = kdblastframe; kdbsavframe = frame;
+			(void) kdbreadchar();
+			if (kdbsymchar(0))
+				kdbchkloc(kdbexpv=frame);
+		} else if ((symp=kdblookup(kdbisymbol))==0)
+			kdberror(kdbBADSYM);
 		else
-			expv = symp->n_value;
-		lp--;
+			kdbexpv = symp->n_value;
+		kdblp--;
 		return (1);
 	}
-	if (getnum())
+	if (kdbgetnum())
 		return (1);
-	switch (lastc) {
+	switch (kdblastc) {
 	case '.':
-		(void) readchar();
-		if (symchar(0)) {
-			lastframe=savlastf; callpc=savpc;
-			chkloc((long)savframe);
+		(void) kdbreadchar();
+		if (kdbsymchar(0)) {
+			kdblastframe=kdbsavlastf; kdbcallpc=kdbsavpc;
+			kdbchkloc((long)kdbsavframe);
 		} else
-			expv=dot;
-		lp--;
+			kdbexpv=kdbdot;
+		kdblp--;
 		break;
 	case '"':
-		expv=ditto;
+		kdbexpv=kdbditto;
 		break;
 	case '+':
-		expv=inkdot(dotinc);
+		kdbexpv=kdbinkdot(kdbdotinc);
 		break;
 	case '^':
-		expv=inkdot(-dotinc);
+		kdbexpv=kdbinkdot(-kdbdotinc);
 		break;
 	case '<':
-		savc=rdc();
-		if ((regptr=getreg(savc)) != -1)
-			expv = *(int *)regptr;
-		else if ((base=varchk(savc)) != -1)
-			expv=var[base];
+		savc=kdbrdc();
+		if ((regptr=kdbgetreg(savc)) != -1)
+			kdbexpv = *(int *)regptr;
+		else if ((base=kdbvarchk(savc)) != -1)
+			kdbexpv=kdbvar[base];
 		else
-			error(BADVAR);
+			kdberror(kdbBADVAR);
 		break;
 	case '\'':
-		d=4; expv=0;
-		while (quotchar()) {
+		d=4; kdbexpv=0;
+		while (kdbquotchar()) {
 		    if (d--) {
-		         expv <<= 8;
-			 expv |= lastc;
+		         kdbexpv <<= 8;
+			 kdbexpv |= kdblastc;
 		    } else
-			error(BADSYN);
+			kdberror(kdbBADSYN);
 		}
 		break;
 	default:
 		if (a)
-			error(NOADR);
-		lp--;
+			kdberror(kdbNOADR);
+		kdblp--;
 		return(0);
 	}
 	return (1);
@@ -199,65 +199,65 @@ item(a)
 
 /* service routines for expression reading */
 static
-getnum()
+kdbgetnum()
 {
 	register base,d,frpt;
 
-	if (!isdigit(lastc))
+	if (!isdigit(kdblastc))
 		return (0);
-	if ((base = radix) < 0)
+	if ((base = kdbradix) < 0)
 		base = -base;
-	expv = 0;
-	while (base>10 ? isxdigit(lastc) : isdigit(lastc)) {
+	kdbexpv = 0;
+	while (base>10 ? isxdigit(kdblastc) : isdigit(kdblastc)) {
 		register m = MAXINT/base;
 
-		if (expv>m)		/* avoid overflow */
-			expv = (expv-m)*base+m*base;
+		if (kdbexpv>m)		/* avoid overflow */
+			kdbexpv = (kdbexpv-m)*base+m*base;
 		else
-			expv *= base;
-		if ((d=convdig(lastc))>=base || d<0)
-			error(BADSYN);
-		expv += d; (void) readchar();
-		if (expv==0) {
-			if (lastc=='x' || lastc=='X') {
-				 base=16; (void) readchar();
-			} else if (lastc=='t' || lastc=='T') {
-				 base=10; (void) readchar();
-			} else if (lastc=='o' || lastc=='O') {
-				 base=8; (void) readchar();
+			kdbexpv *= base;
+		if ((d=kdbconvdig(kdblastc))>=base || d<0)
+			kdberror(kdbBADSYN);
+		kdbexpv += d; (void) kdbreadchar();
+		if (kdbexpv==0) {
+			if (kdblastc=='x' || kdblastc=='X') {
+				 base=16; (void) kdbreadchar();
+			} else if (kdblastc=='t' || kdblastc=='T') {
+				 base=10; (void) kdbreadchar();
+			} else if (kdblastc=='o' || kdblastc=='O') {
+				 base=8; (void) kdbreadchar();
 			}
 		}
 	}
-	if (lastc=='.' && (base==10 || expv==0)) {
+	if (kdblastc=='.' && (base==10 || kdbexpv==0)) {
 		frpt=0; base=10;
-		while (isdigit(readchar())) {
+		while (isdigit(kdbreadchar())) {
 			if (frpt)
 				continue;
 			frpt++;
-			if (lastc - '0' >= 5)
-				expv++;
+			if (kdblastc - '0' >= 5)
+				kdbexpv++;
 		}
 	}
-	peekc=lastc;
+	kdbpeekc=kdblastc;
 	return (1);
 }
 
 static
-readsym()
+kdbreadsym()
 {
 	register char *p;
 
-	p = isymbol;
+	p = kdbisymbol;
 	do {
-		if (p < &isymbol[sizeof(isymbol)-1])
-			*p++ = lastc;
-		(void) readchar();
-	} while (symchar(1));
+		if (p < &kdbisymbol[sizeof(kdbisymbol)-1])
+			*p++ = kdblastc;
+		(void) kdbreadchar();
+	} while (kdbsymchar(1));
 	*p++ = 0;
 }
 
 static
-convdig(c)
+kdbconvdig(c)
 	char c;
 {
 	if (isdigit(c))
@@ -268,17 +268,17 @@ convdig(c)
 }
 
 static
-symchar(dig)
+kdbsymchar(dig)
 {
 
-	if (lastc=='\\') {
-		(void) readchar();
+	if (kdblastc=='\\') {
+		(void) kdbreadchar();
 		return (1);
 	}
-	return (isalpha(lastc) || lastc=='_' || dig && isdigit(lastc));
+	return (isalpha(kdblastc) || kdblastc=='_' || dig && isdigit(kdblastc));
 }
 
-varchk(name)
+kdbvarchk(name)
 	register name;
 {
 	if (isdigit(name))
@@ -289,31 +289,31 @@ varchk(name)
 }
 
 static
-chkloc(frame)
+kdbchkloc(frame)
 	long frame;
 {
 
-	readsym();
+	kdbreadsym();
 	do {
-		if (localsym(frame)==0)
-			error(BADLOC);
-		expv=localval;
-	} while (!eqsym(cursym->n_un.n_name,isymbol,'~'));
+		if (kdblocalsym(frame)==0)
+			kdberror(kdbBADLOC);
+		kdbexpv=kdblocalval;
+	} while (!kdbeqsym(kdbcursym->n_un.n_name,kdbisymbol,'~'));
 }
 
-eqsym(s1, s2, c)
+kdbeqsym(s1, s2, c)
 	register char *s1, *s2;
 {
 
-	if (streq(s1,s2))
+	if (kdbstreq(s1,s2))
 		return (1);
-	if (*s1 == c && streq(s1+1, s2))
+	if (*s1 == c && kdbstreq(s1+1, s2))
 		return (1);
 	return (0);
 }
 
 static
-streq(s1, s2)
+kdbstreq(s1, s2)
 	char *s1, *s2;
 {
 
