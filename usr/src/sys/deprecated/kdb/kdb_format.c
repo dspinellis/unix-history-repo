514b67e2f5046d6917fdfa131afc8d2c34026f7e
Keith Sklower <sklower@ucbvax.Berkeley.EDU>
1990-05-04 00:47:40 -0800

    first crack at torek changes, may need bug fixes
    SCCS-vsn: sys/deprecated/kdb/kdb_format.c 7.5

--- usr/src/sys/deprecated/kdb/kdb_format.c
+++ usr/src/sys/deprecated/kdb/kdb_format.c
@@ -3,19 +3,19 @@
  * All rights reserved.  The Berkeley software License Agreement
  * specifies the terms and conditions for redistribution.
  *
- *	@(#)kdb_format.c	7.4 (Berkeley) %G%
+ *	@(#)kdb_format.c	7.5 (Berkeley) %G%
  */
 
 #include "../kdb/defs.h"
 
-char	*BADMOD;
-char	*ADWRAP;
+char	*kdbBADMOD;
+char	*kdbADWRAP;
 
-char	*lp;
-char	lastc,peekc;
-long	expv;
+char	*kdblp;
+char	kdblastc,kdbpeekc;
+long	kdbexpv;
 
-scanform(icount,ifp,itype,ptype)
+kdbscanform(icount,ifp,itype,ptype)
 	long icount;
 	char *ifp;
 {
@@ -27,11 +27,11 @@ scanform(icount,ifp,itype,ptype)
 
 	while (icount) {
 		fp=ifp;
-		savdot=dot; init=0;
-		if (!init && (exact=(findsym(dot,ptype)==0)) && maxoff)
-			printf("\n%s:%16t",cursym->n_un.n_name);
+		savdot=kdbdot; init=0;
+		if (!init && (exact=(kdbfindsym(kdbdot,ptype)==0)) && kdbmaxoff)
+			kdbprintf("\n%s:%16t",kdbcursym->n_un.n_name);
 		/*now loop over format*/
-		while (*fp && errflg==0) {
+		while (*fp && kdberrflg==0) {
 			if (isdigit(modifier = *fp)) {
 				fcount = 0;
 				while (isdigit(modifier = *fp++)) {
@@ -45,30 +45,30 @@ scanform(icount,ifp,itype,ptype)
 				break;
 #ifdef	ENTRYMASK
 			/* check for entry mask */
-			if (exact && dot==savdot && 
-			   (cursym->n_type&N_TYPE)==N_TEXT &&
-			   cursym->n_un.n_name[0]=='_' && *fp=='i') {
-				(void) exform(1,"x",itype,ptype);
+			if (exact && kdbdot==savdot && 
+			   (kdbcursym->n_type&N_TYPE)==N_TEXT &&
+			   kdbcursym->n_un.n_name[0]=='_' && *fp=='i') {
+				(void) kdbexform(1,"x",itype,ptype);
 				fp++;
-				printc(EOR);
+				kdbprintc(EOR);
 			} else
 #endif
-				fp = exform(fcount,fp,itype,ptype);
+				fp = kdbexform(fcount,fp,itype,ptype);
 		}
-		dotinc=dot-savdot;
-		dot=savdot;
+		kdbdotinc=kdbdot-savdot;
+		kdbdot=savdot;
 
-		if (errflg) {
+		if (kdberrflg) {
 			if (icount<0) {
-				 errflg=0;
+				 kdberrflg=0;
 				 break;
 			}
-			error(errflg);
+			kdberror(kdberrflg);
 		}
 		if (--icount)
-			dot=inkdot(dotinc);
-		if (mkfault)
-			error((char *)0);
+			kdbdot=kdbinkdot(kdbdotinc);
+		if (kdbmkfault)
+			kdberror((char *)0);
 	}
 }
 
@@ -78,7 +78,7 @@ scanform(icount,ifp,itype,ptype)
  * returns address of next format item
  */
 char *
-exform(fcount,ifp,itype,ptype)
+kdbexform(fcount,ifp,itype,ptype)
 	int fcount;
 	char *ifp;
 {
@@ -91,164 +91,164 @@ exform(fcount,ifp,itype,ptype)
 	  	fp = ifp; c = *fp;
 		longpr = (isupper(c) || c=='f' || c=='4' || c=='p');
 		if (itype != NSP && *fp != 'a') {
-			wx = get(dot, itype);
+			wx = kdbget(kdbdot, itype);
 			w = shorten(wx);
 		} else {
-			wx = w = dot;
+			wx = w = kdbdot;
 			if (itype == NSP &&
 			    (c == 'b' || c == 'B' ||
 			     c == 'c' || c == 'C' || c == '1'))
 				w = btol(wx);
 		}
-		if (errflg)
+		if (kdberrflg)
 			return (fp);
-		if (mkfault)
-			error((char *)0);
-		var[0] = wx;
+		if (kdbmkfault)
+			kdberror((char *)0);
+		kdbvar[0] = wx;
 		modifier = *fp++;
-		dotinc = (longpr ? sizeof (long):sizeof (short));
+		kdbdotinc = (longpr ? sizeof (long):sizeof (short));
 
-		if (charpos()==0 && modifier!='a')
-			printf("%16m");
+		if (kdbcharpos()==0 && modifier!='a')
+			kdbprintf("%16m");
 		switch (modifier) {
 
 		case SP: case TB:
 			break;
 
 		case 't': case 'T':
-			printf("%T",fcount); return (fp);
+			kdbprintf("%T",fcount); return (fp);
 
 		case 'r': case 'R':
-			printf("%M",fcount); return (fp);
+			kdbprintf("%M",fcount); return (fp);
 
 		case 'a':
-			psymoff(dot,ptype,":%16t"); dotinc=0; break;
+			kdbpsymoff(kdbdot,ptype,":%16t"); kdbdotinc=0; break;
 
 		case 'p':
-			psymoff(var[0],ptype,"%16t"); break;
+			kdbpsymoff(kdbvar[0],ptype,"%16t"); break;
 
 		case 'u':
-			printf("%-8u",w); break;
+			kdbprintf("%-8u",w); break;
 
 		case 'U':
-			printf("%-16U",wx); break;
+			kdbprintf("%-16U",wx); break;
 
 		case 'c': case 'C':
 			if (modifier == 'C')
-				printesc((int)byte(w));
+				kdbprintesc((int)byte(w));
 			else
-				printc((char)byte(w));
-			dotinc=1; break;
+				kdbprintc((char)byte(w));
+			kdbdotinc=1; break;
 
 		case 'b': case 'B':
-			printf("%-8o", byte(w)); dotinc=1; break;
+			kdbprintf("%-8o", byte(w)); kdbdotinc=1; break;
 
 		case '1':
-			printf("%-8R", byte(w)); dotinc=1; break;
+			kdbprintf("%-8R", byte(w)); kdbdotinc=1; break;
 
 		case 'w': case '2':
-			printf("%-8R", w); break;
+			kdbprintf("%-8R", w); break;
 
 		case 'W': case '4':
-			printf("%-16R", wx); break;
+			kdbprintf("%-16R", wx); break;
 
 		case 's': case 'S':
-			savdot=dot; dotinc=1;
-			while ((c=byte(get(dot,itype))) && errflg==0) {
-				dot=inkdot(1);
+			savdot=kdbdot; kdbdotinc=1;
+			while ((c=byte(kdbget(kdbdot,itype))) && kdberrflg==0) {
+				kdbdot=kdbinkdot(1);
 				if (modifier == 'S')
-					printesc((int)c);
+					kdbprintesc((int)c);
 				else
-					printc(c);
-				endline();
+					kdbprintc(c);
+				kdbendline();
 			}
-			dotinc=dot-savdot+1; dot=savdot; break;
+			kdbdotinc=kdbdot-savdot+1; kdbdot=savdot; break;
 
 		case 'x':
-			printf("%-8x",w); break;
+			kdbprintf("%-8x",w); break;
 
 		case 'X':
-			printf("%-16X", wx); break;
+			kdbprintf("%-16X", wx); break;
 
 		case 'z':
-			printf("%-8z",w); break;
+			kdbprintf("%-8z",w); break;
 
 		case 'Z':
-			printf("%-16Z", wx); break;
+			kdbprintf("%-16Z", wx); break;
 
 		case 'Y':
-			printf("%-24Y", wx); break;
+			kdbprintf("%-24Y", wx); break;
 
 		case 'q':
-			printf("%-8q", w); break;
+			kdbprintf("%-8q", w); break;
 
 		case 'Q':
-			printf("%-16Q", wx); break;
+			kdbprintf("%-16Q", wx); break;
 
 		case 'o':
-			printf("%-8o", w); break;
+			kdbprintf("%-8o", w); break;
 
 		case 'O':
-			printf("%-16O", wx); break;
+			kdbprintf("%-16O", wx); break;
 
 		case 'i': case 'I':
-			printins(itype,wx); printc(EOR); break;
+			kdbprintins(itype,wx); kdbprintc(EOR); break;
 
 		case 'd':
-			printf("%-8d", w); break;
+			kdbprintf("%-8d", w); break;
 
 		case 'D':
-			printf("%-16D", wx); break;
+			kdbprintf("%-16D", wx); break;
 
 		case 'n': case 'N':
-			printc('\n'); dotinc=0; break;
+			kdbprintc('\n'); kdbdotinc=0; break;
 
 		case '"':
-			dotinc=0;
+			kdbdotinc=0;
 			while (*fp != '"' && *fp)
-				printc(*fp++);
+				kdbprintc(*fp++);
 			if (*fp)
 				fp++;
 			break;
 
 		case '^':
-			dot=inkdot(-dotinc*fcount); return (fp);
+			kdbdot=kdbinkdot(-kdbdotinc*fcount); return (fp);
 
 		case '+':
-			dot=inkdot(fcount); return (fp);
+			kdbdot=kdbinkdot(fcount); return (fp);
 
 		case '-':
-			dot=inkdot(-fcount); return (fp);
+			kdbdot=kdbinkdot(-fcount); return (fp);
 
 		default:
-			error(BADMOD);
+			kdberror(kdbBADMOD);
 		}
 		if (itype!=NSP)
-			dot=inkdot(dotinc);
-		fcount--; endline();
+			kdbdot=kdbinkdot(kdbdotinc);
+		fcount--; kdbendline();
 	}
 	return (fp);
 }
 
 static
-printesc(c)
+kdbprintesc(c)
 	register int c;
 {
 
 	c &= STRIP;
 	if (c==0177 || c<SP)
-		printf("^%c", c ^ 0100);
+		kdbprintf("^%c", c ^ 0100);
 	else
-		printc(c);
+		kdbprintc(c);
 }
 
 long
-inkdot(incr)
+kdbinkdot(incr)
 {
 	register long newdot;
 
-	newdot=dot+incr;
-	if (addrwrap(dot, newdot))
-		error(ADWRAP);
+	newdot=kdbdot+incr;
+	if (addrwrap(kdbdot, newdot))
+		kdberror(kdbADWRAP);
 	return (newdot);
 }
