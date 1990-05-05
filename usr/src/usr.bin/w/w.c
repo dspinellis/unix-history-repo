a322d4fc8bb2f923190934127685521b5d3acbd6
Marc Teitelbaum <marc@ucbvax.Berkeley.EDU>
1990-05-04 22:47:53 -0800

    move proc_compare to ps directory
    SCCS-vsn: usr.bin/w/w.c 5.20

--- usr/src/usr.bin/w/w.c
+++ usr/src/usr.bin/w/w.c
@@ -11,7 +11,7 @@ char copyright[] =
 #endif not lint
 
 #ifndef lint
-static char sccsid[] = "@(#)w.c	5.19 (Berkeley) %G%";
+static char sccsid[] = "@(#)w.c	5.20 (Berkeley) %G%";
 #endif not lint
 
 /*
@@ -271,66 +271,6 @@ main(argc, argv)
 		printf("%.*s\n", argwidth, ep->args);
 	}
 }
-
-/*
- * Returns 1 if p2 is "better" than p1
- *
- * The algorithm for picking the "interesting" process is thus:
- *
- *	1) (Only foreground processes are eligable - implied)
- *	2) Runnable processes are favored over anything
- *	   else.  The runner with the highest cpu
- *	   utilization is picked (p_cpu).  Ties are
- *	   broken by picking the highest pid.
- *	3  Next, the sleeper with the shortest sleep
- *	   time is favored.  With ties, we pick out
- *	   just short-term sleepers (p_pri <= PZERO).
- *	   Further ties are broken by picking the highest
- *	   pid.
- *
- */
-#define isrun(p)	(((p)->p_stat == SRUN) || ((p)->p_stat == SIDL))
-proc_compare(p1, p2)
-	register struct proc *p1, *p2;
-{
-
-	if (p1 == NULL)
-		return (1);
-	/*
-	 * see if at least one of them is runnable
-	 */
-	switch (isrun(p1)<<1 | isrun(p2)) {
-	case 0x01:
-		return (1);
-	case 0x10:
-		return (0);
-	case 0x11:
-		/*
-		 * tie - favor one with highest recent cpu utilization
-		 */
-		if (p2->p_cpu > p1->p_cpu)
-			return (1);
-		if (p1->p_cpu > p2->p_cpu)
-			return (0);
-		return (p2->p_pid > p1->p_pid);	/* tie - return highest pid */
-	}
-	/* 
-	 * pick the one with the smallest sleep time
-	 */
-	if (p2->p_slptime > p1->p_slptime)
-		return (0);
-	if (p1->p_slptime > p2->p_slptime)
-		return (1);
-	/*
-	 * favor one sleeping in a non-interruptible sleep
-	 */
-	 if (p1->p_flag&SSINTR && (p2->p_flag&SSINTR) == 0)
-		 return (1);
-	 if (p2->p_flag&SSINTR && (p1->p_flag&SSINTR) == 0)
-		 return (0);
-	return(p2->p_pid > p1->p_pid);		/* tie - return highest pid */
-}
-
 
 struct stat *
 ttystat(line)
