550d64600bc29365e42f7f2e4e3ce1a27ba9f3c7
Keith Sklower <sklower@ucbvax.Berkeley.EDU>
1990-05-04 20:14:33 -0800

    use generic machinery for llifaddrs to replace themeselves by protocol specific ifaddrs by not stomping on ifa_rtrequest in the ll ifaddr.
    SCCS-vsn: sys/netiso/if_eon.c 7.7

--- usr/src/sys/netiso/if_eon.c
+++ usr/src/sys/netiso/if_eon.c
@@ -27,7 +27,7 @@ SOFTWARE.
 /*
  * $Header: if_eon.c,v 1.4 88/07/19 15:53:59 hagens Exp $ 
  * $Source: /usr/argo/sys/netiso/RCS/if_eon.c,v $ 
- *	@(#)if_eon.c	7.6 (Berkeley) %G% *
+ *	@(#)if_eon.c	7.7 (Berkeley) %G% *
  *
  *	EON rfc 
  *  Layer between IP and CLNL
