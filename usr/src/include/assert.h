965de38620afa2af3164f7a71ec878a22371c5a6
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-05-06 22:52:12 -0800

    redo from scratch, print out assertion as well as line/file
    SCCS-vsn: include/assert.h 4.3

--- usr/src/include/assert.h
+++ usr/src/include/assert.h
@@ -1,9 +1,23 @@
-/*	assert.h	4.2	85/01/21	*/
+/*-
+ * Copyright (c) 1990 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.redist.c%
+ *
+ *	@(#)assert.h	4.3 (Berkeley) %G%
+ */
 
-# ifndef NDEBUG
-# define _assert(ex)	{if (!(ex)){fprintf(stderr,"Assertion failed: file \"%s\", line %d\n", __FILE__, __LINE__);exit(1);}}
-# define assert(ex)	_assert(ex)
-# else
-# define _assert(ex)
-# define assert(ex)
-# endif
+#ifdef NDEBUG
+#define	assert
+#define	_assert
+#else
+#define	assert(expression) { \
+	if (!(expression)) { \
+		(void)fprintf(stderr, \
+		    "assertion \"%s\" failed: file \"%s\", line %d\n", \
+		    "expression", __FILE__, __LINE__); \
+		exit(2); \
+	} \
+}
+#define	_assert(expression)	assert(expression)
+#endif
