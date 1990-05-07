4bf5fda02a2ccb40fe7a2dcc2f2094491ce8efed
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-05-06 22:55:51 -0800

    no more AT&T code left, add Berkeley copyright
    SCCS-vsn: include/setjmp.h 4.3

--- usr/src/include/setjmp.h
+++ usr/src/include/setjmp.h
@@ -1,4 +1,11 @@
-/*	setjmp.h	4.2	89/10/16	*/
+/*-
+ * Copyright (c) 1990 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.redist.c%
+ *
+ *	@(#)setjmp.h	4.3 (Berkeley) %G%
+ */
 
 #ifndef _SETJMP_
 #define _SETJMP_
@@ -20,9 +27,8 @@ typedef int jmp_buf[_JBLEN];
 #ifdef __STDC__
 extern int setjmp(jmp_buf), _setjmp(jmp_buf);
 extern void longjmp(jmp_buf, int), _longjmp(jmp_buf, int);
-#else	/* !__STDC__ */
+#else
 extern int setjmp(), _setjmp();
 extern int longjmp(), _longjmp();
-#endif	/* !__STDC__ */
-
+#endif
 #endif
