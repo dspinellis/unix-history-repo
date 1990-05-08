74571815eeff8c597efd11cba0ad19607c94b377
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-05-08 05:56:02 -0800

    changes to support od(1), plus type "etc" -> "etb"
    SCCS-vsn: usr.bin/hexdump/conv.c 5.3

--- usr/src/usr.bin/hexdump/conv.c
+++ usr/src/usr.bin/hexdump/conv.c
@@ -16,7 +16,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)conv.c	5.2 (Berkeley) %G%";
+static char sccsid[] = "@(#)conv.c	5.3 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -27,6 +27,7 @@ conv_c(pr, p)
 	PR *pr;
 	u_char *p;
 {
+	extern int deprecated;
 	char buf[10], *str;
 
 	switch(*p) {
@@ -35,6 +36,8 @@ conv_c(pr, p)
 		goto strpr;
 	/* case '\a': */
 	case '\007':
+		if (deprecated)		/* od didn't know about \a */
+			break;
 		str = "\\a";
 		goto strpr;
 	case '\b':
@@ -53,17 +56,20 @@ conv_c(pr, p)
 		str = "\\t";
 		goto strpr;
 	case '\v':
+		if (deprecated)
+			break;
 		str = "\\v";
 		goto strpr;
 	default:
-		if (isprint(*p)) {
-			*pr->cchar = 'c';
-			(void)printf(pr->fmt, *p);
-		} else {
-			(void)sprintf(str = buf, "%03o", (int)*p);
-strpr:			*pr->cchar = 's';
-			(void)printf(pr->fmt, str);
-		}
+		break;
+	}
+	if (isprint(*p)) {
+		*pr->cchar = 'c';
+		(void)printf(pr->fmt, *p);
+	} else {
+		(void)sprintf(str = buf, "%03o", (int)*p);
+strpr:		*pr->cchar = 's';
+		(void)printf(pr->fmt, str);
 	}
 }
 
@@ -71,19 +77,27 @@ conv_u(pr, p)
 	PR *pr;
 	u_char *p;
 {
+	extern int deprecated;
 	static char *list[] = {
 		"nul", "soh", "stx", "etx", "eot", "enq", "ack", "bel",
 		 "bs",  "ht",  "lf",  "vt",  "ff",  "cr",  "so",  "si",
-		"dle", "dcl", "dc2", "dc3", "dc4", "nak", "syn", "etc",
+		"dle", "dcl", "dc2", "dc3", "dc4", "nak", "syn", "etb",
 		"can",  "em", "sub", "esc",  "fs",  "gs",  "rs",  "us",
 	};
 
+						/* od used nl, not lf */
 	if (*p <= 0x1f) {
 		*pr->cchar = 's';
-		(void)printf(pr->fmt, list[*p]);
-	} else if (*p == 0xff) {
+		if (deprecated && *p == 0x0a)
+			(void)printf(pr->fmt, "nl");
+		else
+			(void)printf(pr->fmt, list[*p]);
+	} else if (*p == 0x7f) {
 		*pr->cchar = 's';
 		(void)printf(pr->fmt, "del");
+	} else if (deprecated && *p == 0x20) {	/* od replace space with sp */
+		*pr->cchar = 's';
+		(void)printf(pr->fmt, " sp");
 	} else if (isprint(*p)) {
 		*pr->cchar = 'c';
 		(void)printf(pr->fmt, *p);
