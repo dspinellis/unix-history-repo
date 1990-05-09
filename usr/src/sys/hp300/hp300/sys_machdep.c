61d407ebf1b6088c3ddf678f6a1294a2268add14
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-05-09 01:15:43 -0800

    merge in hp300 support from Utah
    SCCS-vsn: sys/hp300/hp300/clockioctl.h 7.1
    SCCS-vsn: sys/hp300/hp300/clockreg.h 7.1
    SCCS-vsn: sys/hp/dev/cons.h 7.1
    SCCS-vsn: sys/hp300/include/cpu.h 7.1
    SCCS-vsn: sys/hp300/include/float.h 7.1
    SCCS-vsn: sys/hp300/include/frame.h 7.1
    SCCS-vsn: sys/hp300/hp300/isr.h 7.1
    SCCS-vsn: sys/hp300/include/mtpr.h 7.1
    SCCS-vsn: sys/hp300/include/param.h 7.1
    SCCS-vsn: sys/hp300/include/pcb.h 7.1
    SCCS-vsn: sys/hp300/hp300/pte.h 7.1
    SCCS-vsn: sys/hp300/include/psl.h 7.1
    SCCS-vsn: sys/hp300/include/reg.h 7.1
    SCCS-vsn: sys/hp300/include/trap.h 7.1
    SCCS-vsn: sys/hp300/include/vmparam.h 7.1
    SCCS-vsn: sys/hp300/hp300/autoconf.c 7.1
    SCCS-vsn: sys/hp300/hp300/clock.c 7.1
    SCCS-vsn: sys/hp300/hp300/conf.c 7.1
    SCCS-vsn: sys/hp/dev/cons.c 7.1
    SCCS-vsn: sys/hp300/hp300/dkbad.c 7.1
    SCCS-vsn: sys/hp300/hp300/genassym.c 7.1
    SCCS-vsn: sys/hp300/hp300/in_cksum.c 7.1
    SCCS-vsn: sys/hp300/hp300/kgdb_stub.c 7.1
    SCCS-vsn: sys/hp300/hp300/machdep.c 7.1
    SCCS-vsn: sys/hp300/hp300/mem.c 7.1
    SCCS-vsn: sys/hp300/hp300/ns_cksum.c 7.1
    SCCS-vsn: sys/hp300/hp300/swapgeneric.c 7.1
    SCCS-vsn: sys/hp300/hp300/sys_machdep.c 7.1
    SCCS-vsn: sys/hp300/hp300/trap.c 7.1
    SCCS-vsn: sys/hp300/hp300/vm_machdep.c 7.1
    SCCS-vsn: sys/hp300/hp300/locore.s 7.1
    SCCS-vsn: sys/hp300/hp300/oc_cksum.s 7.1
    SCCS-vsn: sys/hp300/hp300/vectors.s 7.1

---
+++ usr/src/sys/hp300/hp300/sys_machdep.c
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) 1982, 1986 Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.redist.c%
+ *
+ *	@(#)sys_machdep.c	7.1 (Berkeley) %G%
+ */
+
+#include "param.h"
+#include "systm.h"
+#include "user.h"
+#include "ioctl.h"
+#include "file.h"
+#include "proc.h"
+#include "uio.h"
+#include "kernel.h"
+#include "mtio.h"
+#include "buf.h"
+#include "trace.h"
+
+#include "pte.h"
+
+#ifdef TRACE
+int	nvualarm;
+
+vtrace()
+{
+	register struct a {
+		int	request;
+		int	value;
+	} *uap;
+	int vdoualarm();
+
+	uap = (struct a *)u.u_ap;
+	switch (uap->request) {
+
+	case VTR_DISABLE:		/* disable a trace point */
+	case VTR_ENABLE:		/* enable a trace point */
+		if (uap->value < 0 || uap->value >= TR_NFLAGS)
+			u.u_error = EINVAL;
+		else {
+			u.u_r.r_val1 = traceflags[uap->value];
+			traceflags[uap->value] = uap->request;
+		}
+		break;
+
+	case VTR_VALUE:		/* return a trace point setting */
+		if (uap->value < 0 || uap->value >= TR_NFLAGS)
+			u.u_error = EINVAL;
+		else
+			u.u_r.r_val1 = traceflags[uap->value];
+		break;
+
+	case VTR_UALARM:	/* set a real-time ualarm, less than 1 min */
+		if (uap->value <= 0 || uap->value > 60 * hz ||
+		    nvualarm > 5)
+			u.u_error = EINVAL;
+		else {
+			nvualarm++;
+			timeout(vdoualarm, (caddr_t)u.u_procp->p_pid,
+			    uap->value);
+		}
+		break;
+
+	case VTR_STAMP:
+		trace(TR_STAMP, uap->value, u.u_procp->p_pid);
+		break;
+	}
+}
+
+vdoualarm(arg)
+	int arg;
+{
+	register struct proc *p;
+
+	p = pfind(arg);
+	if (p)
+		psignal(p, 16);
+	nvualarm--;
+}
+#endif
---
+++ usr/src/sys/hp300/include/psl.h
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 1982, 1986 Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.redist.c%
+ *
+ *	@(#)psl.h	7.1 (Berkeley) %G%
+ */
+
+/*
+ * MC68000 program status word
+ */
+
+#define	PSL_C		0x0001		/* carry bit */
+#define	PSL_V		0x0002		/* overflow bit */
+#define	PSL_Z		0x0004		/* zero bit */
+#define	PSL_N		0x0008		/* negative bit */
+#define	PSL_X		0x0010		/* extend bit */
+#define	PSL_ALLCC	0x001F		/* all cc bits - unlikely */
+#define	PSL_IPL0	0x0000		/* interrupt priority level 0 */
+#define	PSL_IPL1	0x0100		/* interrupt priority level 1 */
+#define	PSL_IPL2	0x0200		/* interrupt priority level 2 */
+#define	PSL_IPL3	0x0300		/* interrupt priority level 3 */
+#define	PSL_IPL4	0x0400		/* interrupt priority level 4 */
+#define	PSL_IPL5	0x0500		/* interrupt priority level 5 */
+#define	PSL_IPL6	0x0600		/* interrupt priority level 6 */
+#define	PSL_IPL7	0x0700		/* interrupt priority level 7 */
+#define	PSL_S		0x2000		/* supervisor enable bit */
+#define	PSL_T		0x8000		/* trace enable bit */
+
+#define	PSL_LOWIPL	(PSL_S)
+#define	PSL_HIGHIPL	(PSL_S | PSL_IPL7)
+#define PSL_IPL		(PSL_IPL7)
+#define	PSL_USER	(0)
+
+#define	PSL_MBZ		0x58E0		/* must be zero bits */
+
+#define	PSL_USERSET	(0)
+#define	PSL_USERCLR	(PSL_S | PSL_IPL7 | PSL_MBZ)
