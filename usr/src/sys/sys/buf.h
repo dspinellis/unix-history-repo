0ade70d12ad9d8b82251877fd80be131377fcd1f
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-05-09 17:39:29 -0800

    update for hp300 support
    SCCS-vsn: sys/sys/buf.h 7.11

--- usr/src/sys/sys/buf.h
+++ usr/src/sys/sys/buf.h
@@ -2,19 +2,9 @@
  * Copyright (c) 1982, 1986, 1989 Regents of the University of California.
  * All rights reserved.
  *
- * Redistribution and use in source and binary forms are permitted
- * provided that the above copyright notice and this paragraph are
- * duplicated in all such forms and that any documentation,
- * advertising materials, and other materials related to such
- * distribution and use acknowledge that the software was developed
- * by the University of California, Berkeley.  The name of the
- * University may not be used to endorse or promote products derived
- * from this software without specific prior written permission.
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ * %sccs.include.redist.c%
  *
- *	@(#)buf.h	7.10 (Berkeley) %G%
+ *	@(#)buf.h	7.11 (Berkeley) %G%
  */
 
 /*
@@ -84,6 +74,7 @@ struct buf
 	struct	ucred *b_wcred;		/* ref to write credendtials */
 	int	b_dirtyoff;		/* offset in buffer of dirty region */
 	int	b_dirtyend;		/* offset of end of dirty region */
+	caddr_t	b_saveaddr;		/* original b_addr for PHYSIO */
 };
 
 #define	BQUEUES		4		/* number of free buffer queues */
