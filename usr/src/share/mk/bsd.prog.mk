c886f7b2d4391d04348f0cc8d7ef45f5ace305fb
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-05-11 00:21:03 -0800

    do bsd.global.mk inline
    SCCS-vsn: share/mk/bsd.prog.mk 5.10

--- usr/src/share/mk/bsd.prog.mk
+++ usr/src/share/mk/bsd.prog.mk
@@ -1,8 +1,13 @@
-.include <bsd.global.mk>
-
 .if exists(${.CURDIR}/../Makefile.inc)
 .include "${.CURDIR}/../Makefile.inc"
 .endif
+
+.SUFFIXES: .out .o .c .y .l .s .8 .7 .6 .5 .4 .3 .2 .1 .0
+
+.8.0 .7.0 .6.0 .5.0 .4.0 .3.0 .2.0 .1.0:
+	nroff -man -h ${.IMPSRC} > ${.TARGET}
+
+CFLAGS+=${COPTS}
 
 LIBC?=		/lib/libc.a
 LIBCOMPAT?=	/usr/lib/libcompat.a
@@ -19,6 +24,13 @@ LIBPLOT?=	/usr/lib/libplot.a
 LIBRPC?=	/usr/lib/sunrpc.a
 LIBTERM?=	/usr/lib/libterm.a
 LIBUTIL?=	/usr/lib/libutil.a
+
+.if defined(SHAREDSTRINGS)
+.c.o:
+	${CC} -E ${CFLAGS} ${.IMPSRC} | /usr/bin/xstr -c -
+	@${CC} ${CFLAGS} -c x.c -o ${.TARGET}
+	@rm -f x.c
+.endif
 
 .if defined(PROG)
 .if defined(SRCS)
