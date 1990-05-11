4b3ab651c5f4c75c44a2b597e742a79ac058036a
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-05-10 19:53:05 -0800

    add typedef for u_quad; merge in support for hp300 from Utah
    SCCS-vsn: sys/sys/types.h 7.8

--- usr/src/sys/sys/types.h
+++ usr/src/sys/sys/types.h
@@ -3,7 +3,7 @@
  * All rights reserved.  The Berkeley software License Agreement
  * specifies the terms and conditions for redistribution.
  *
- *	@(#)types.h	7.7 (Berkeley) %G%
+ *	@(#)types.h	7.8 (Berkeley) %G%
  */
 
 #ifndef _TYPES_
@@ -33,6 +33,13 @@ typedef	struct	label_t	{
 	int	val[14];
 } label_t;
 #endif
+#if defined(hp300)
+typedef	struct	_physadr { short r[1]; } *physadr;
+typedef	struct	label_t	{
+	int	val[15];	/* consistent with HP-UX */
+} label_t;
+#endif
+typedef	struct	_uquad { unsigned long val[2]; } u_quad;
 typedef	struct	_quad { long val[2]; } quad;
 typedef	long	daddr_t;
 typedef	char *	caddr_t;
