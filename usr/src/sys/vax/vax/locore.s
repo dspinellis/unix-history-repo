9957cc1844ff74ee69435f18091a8aa4667e3fc0
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-05-10 21:21:04 -0800

    use NKMEMCLUSTERS to size malloc arena; expand area by SMMMAXPGS
    SCCS-vsn: sys/vax/vax/locore.s 7.24

--- usr/src/sys/vax/vax/locore.s
+++ usr/src/sys/vax/vax/locore.s
@@ -3,7 +3,7 @@
  * All rights reserved.  The Berkeley software License Agreement
  * specifies the terms and conditions for redistribution.
  *
- *	@(#)locore.s	7.23 (Berkeley) %G%
+ *	@(#)locore.s	7.24 (Berkeley) %G%
  */
 
 #include "psl.h"
@@ -887,7 +887,10 @@ _/**/mname:	.globl	_/**/mname;		\
 	 * XXX: NEED way to compute kmem size from maxusers,
 	 * device complement
 	 */
-	SYSMAP(kmempt	,kmembase	,1000*CLSIZE 	)
+	SYSMAP(kmempt	,kmembase	,NKMEMCLUSTERS*CLSIZE )
+#ifdef	SYSVSHM
+				ADDMAP(	SHMMAXPGS	)
+#endif
 #ifdef	GPROF
 				ADDMAP( 600*CLSIZE	)
 #endif
