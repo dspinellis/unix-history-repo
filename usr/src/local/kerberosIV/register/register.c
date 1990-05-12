b841e2a73cdf333590ecde35df8a5ea866c578be
Kevin Fall <kfall@ucbvax.Berkeley.EDU>
1990-05-12 06:48:26 -0800

    clean up error message when unable to open keyfile
    SCCS-vsn: local/kerberosIV/register/register.c 1.10

--- usr/src/local/kerberosIV/register/register.c
+++ usr/src/local/kerberosIV/register/register.c
@@ -16,7 +16,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)register.c	1.9 (Berkeley) %G%";
+static char sccsid[] = "@(#)register.c	1.10 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -247,9 +247,9 @@ setup_key(local)
 
 	fd = open(namebuf, O_RDONLY);
 	if (fd < 0) {
-		fprintf(stderr, "couldn't open key file for local host %s\n",
-			inet_ntoa(local.sin_addr));
-		perror("open");
+		fprintf(stderr, "couldn't open key file %s for local host: ",
+			namebuf);
+		perror("");
 		exit(1);
 	}
 
