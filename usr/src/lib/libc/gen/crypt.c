3167161a915b45bdc173ef6206a78a4564f404de
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-05-12 00:53:04 -0800

    HP integration
    SCCS-vsn: lib/libc/gen/crypt.c 5.3

--- usr/src/lib/libc/gen/crypt.c
+++ usr/src/lib/libc/gen/crypt.c
@@ -1,5 +1,5 @@
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)crypt.c	5.2.1.1 (Berkeley) %G%";
+static char sccsid[] = "@(#)crypt.c	5.3 (Berkeley) %G%";
 #endif LIBC_SCCS and not lint
 
 /*
@@ -226,7 +226,7 @@ static	char	P[] = {
 /*
  * The current block, divided into 2 halves.
  */
-static	char	L[32], R[32];
+static	char	L[64], *R = L+32;
 static	char	tempL[32];
 static	char	f[32];
 
@@ -255,9 +255,12 @@ char *block;
 	 */
 	for (ii=0; ii<16; ii++) {
 		/*
-		 * Only encrypt for now.
+		 * Set direction
 		 */
-		i = ii;
+		if (edflag)
+			i = 15-ii;
+		else
+			i = ii;
 		/*
 		 * Save the R array,
 		 * which will be the new L.
