a658fb09ea256b9eb4f70bdbcf32f8bfd879152f
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-05-12 23:51:33 -0800

    date and time created 90/05/12 16:51:33 by bostic
    SCCS-vsn: lib/libc/hp300/sys/cerror.s 5.1

---
+++ usr/src/lib/libc/hp300/sys/cerror.s
@@ -0,0 +1,22 @@
+/*-
+ * Copyright (c) 1990 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to Berkeley by
+ * the Systems Programming Group of the University of Utah Computer
+ * Science Department.
+ *
+ * %sccs.include.redist.c%
+ */
+
+#if defined(LIBC_SCCS) && !defined(lint)
+	.asciz "@(#)cerror.s	5.1 (Berkeley) %G%"
+#endif /* LIBC_SCCS and not lint */
+
+#include "SYS.h"
+
+	.globl	_errno
+cerror:
+	movl	d0,_errno
+	movl	#-1,d0
+	rts
