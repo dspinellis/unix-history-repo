90a8d326801eae309f4aeb8ddd24c9275658b7b5
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-05-12 23:16:23 -0800

    date and time created 90/05/12 16:16:23 by bostic
    SCCS-vsn: lib/libc/hp300/string/strcmp.s 5.1

---
+++ usr/src/lib/libc/hp300/string/strcmp.s
@@ -0,0 +1,38 @@
+/*-
+ * Copyright (c) 1990 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to Berkeley by
+ * the Systems Programming Group of the University of Utah Computer
+ * Science Department.
+ *
+ * %sccs.include.redist.c%
+ */
+
+#if defined(LIBC_SCCS) && !defined(lint)
+	.asciz "@(#)strcmp.s	5.1 (Berkeley) %G%"
+#endif /* LIBC_SCCS and not lint */
+
+#include "DEFS.h"
+
+/*
+ * NOTE: this guy returns result compatible with the VAX assembly version.
+ * The C version on the portable gen directory returns different results
+ * (different signs!) when comparing chars with the high bit on.  Who is
+ * right??
+ */
+ENTRY(strcmp)
+	movl	sp@(4),a0	/* a0 = string1 */
+	movl	sp@(8),a1	/* a1 = string2 */
+scloop:
+	movb	a0@+,d0		/* get *string1 */
+	cmpb	a1@+,d0		/* compare a byte */
+	jne	scexit		/* not equal, break out */
+	tstb	d0		/* at end of string1? */
+	jne	scloop		/* no, keep going */
+	moveq	#0,d0		/* strings are equal */
+	rts
+scexit:
+	subb	a1@-,d0		/* *string1 - *string2 */
+	extbl	d0
+	rts
