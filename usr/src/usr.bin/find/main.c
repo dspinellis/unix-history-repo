0c3cc016f0bbc154e6b9e58e2f9652684082b19d
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-05-12 22:30:02 -0800

    date and time created 90/05/12 15:30:02 by bostic
    SCCS-vsn: usr.bin/find/main.c 5.1

---
+++ usr/src/usr.bin/find/main.c
@@ -0,0 +1,57 @@
+/*-
+ * Copyright (c) 1990 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.redist.c%
+ */
+
+#ifndef lint
+static char sccsid[] = "@(#)main.c	5.1 (Berkeley) %G%";
+#endif /* not lint */
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <fts.h>
+#include "find.h"
+
+newsyntax(argc, argvp)
+	int argc;
+	char ***argvp;
+{
+	extern char *optarg;
+	extern int depth, optind, xdev;
+	int ch;
+	char **argv, **cur;
+
+	cur = argv = *argvp;
+	while ((ch = getopt(argc, argv, "df:sx")) != EOF)
+		switch(ch) {
+		case 'd':
+			depth = 1;
+			break;
+		case 'f':
+			*cur++ = optarg;
+			break;
+		case 's':
+			ftsoptions &= ~FTS_PHYSICAL;
+			ftsoptions |= FTS_LOGICAL;
+			break;
+		case 'x':
+			xdev = 1;
+			ftsoptions &= ~FTS_NOSTAT;
+			break;
+		case '?':
+		default:
+			usage();
+		}
+
+	*argvp += optind;
+	if (cur == argv) {
+		if (!**argvp)
+			usage();
+		*cur++ = **argvp;
+		++*argvp;
+	}
+	*cur = NULL;
+}
