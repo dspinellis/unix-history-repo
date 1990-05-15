11a74693e67dd4e2bf5bfc2388d332a3e19bc8a1
Mike Karels <karels@ucbvax.Berkeley.EDU>
1990-05-14 22:16:08 -0800

    Release 0.1 changes
    SCCS-vsn: lib/libc/i386/gen/alloca.s 5.2
    SCCS-vsn: lib/libc/i386/gen/fixdfsi.s 5.2

--- usr/src/lib/libc/i386/gen/fixdfsi.s
+++ usr/src/lib/libc/i386/gen/fixdfsi.s
@@ -9,13 +9,13 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)fixdfsi.s	5.1 (Berkeley) %G%";
+static char sccsid[] = "@(#)fixdfsi.s	5.2 (Berkeley) %G%";
 #endif /* LIBC_SCCS and not lint */
 
 __fixdfsi(d)
 double d;
 {
 	int val;
-	asm("fistl %0" : "=g" (val) : "f" (d) );
+	asm("fldl %1 ; fistpl %0" : "=g" (val) : "g" (d) );
 	return(val);
 }
