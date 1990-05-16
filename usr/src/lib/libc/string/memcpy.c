5739378109034306acd0e3f976ff4ce213666fbb
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-05-15 21:12:49 -0800

    ANSI C version from Chris Torek
    SCCS-vsn: lib/libc/string/memcpy.c 5.5

--- usr/src/lib/libc/string/memcpy.c
+++ usr/src/lib/libc/string/memcpy.c
@@ -1,33 +1,30 @@
-/*
- * Copyright (c) 1985 Regents of the University of California.
+/*-
+ * Copyright (c) 1990 The Regents of the University of California.
  * All rights reserved.
  *
- * Redistribution and use in source and binary forms are permitted
- * provided that the above copyright notice and this paragraph are
- * duplicated in all such forms and that any documentation,
- * advertising materials, and other materials related to such
- * distribution and use acknowledge that the software was developed
- * by the University of California, Berkeley.  The name of the
- * University may not be used to endorse or promote products derived
- * from this software without specific prior written permission.
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
- * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ * This code is derived from software contributed to Berkeley by
+ * Chris Torek.
+ *
+ * %sccs.include.redist.c%
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)memcpy.c	5.4 (Berkeley) %G%";
+static char sccsid[] = "@(#)memcpy.c	5.5 (Berkeley) %G%";
 #endif /* LIBC_SCCS and not lint */
 
-char *
-memcpy(t, f, n)
-	register char *t, *f;
-	register n;
+#include <string.h>
+#include <sys/stdc.h>
+
+#undef memcpy
+
+/*
+ * Copy a block of memory.
+ */
+void *
+memcpy(dst, src, n)
+	void *dst;
+	const void *src;
+	size_t n;
 {
-	register char *p = t;
-
-	while (--n >= 0)
-		*t++ = *f++;
-
-	return (p);
+	return (memmove(dst, src, n));
 }
