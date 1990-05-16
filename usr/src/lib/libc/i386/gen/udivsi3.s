da84433cee0577e8a6849e315526bf52bf9e4184
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-05-15 16:08:59 -0800

    date and time created 90/05/15 09:08:59 by bostic
    SCCS-vsn: lib/libc/i386/gen/udivsi3.s 5.1

---
+++ usr/src/lib/libc/i386/gen/udivsi3.s
@@ -0,0 +1,20 @@
+/*-
+ * Copyright (c) 1990 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to Berkeley by
+ * William Jolitz.
+ *
+ * %sccs.include.redist.c%
+ */
+
+#if defined(LIBC_SCCS) && !defined(lint)
+	.asciz "@(#)udivsi3.s	5.1 (Berkeley) %G%"
+#endif /* LIBC_SCCS and not lint */
+
+	.globl ___udivsi3
+___udivsi3:
+	movl 4(%esp),%eax
+	xorl %edx,%edx
+	divl 8(%esp)
+	ret
