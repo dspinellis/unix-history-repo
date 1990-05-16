13d269ed8b93132545bd85f7ffdd133408d0d070
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-05-15 21:39:22 -0800

    ANSI C version from Chris Torek
    SCCS-vsn: lib/libc/string/memset.c 5.5

--- usr/src/lib/libc/string/memset.c
+++ usr/src/lib/libc/string/memset.c
@@ -1,33 +1,33 @@
-/*
- * Copyright (c) 1985 Regents of the University of California.
+/*-
+ * Copyright (c) 1990 The Regents of the University of California.
  * All rights reserved.
  *
- * Redistribution and use in source and binary forms are permitted
- * provided that the above copyright notice and this paragraph are
- * duplicated in all such forms and that any documentation,
- * advertising materials, and other materials related to such
- * distribution and use acknowledge that the software was developed
- * by the University of California, Berkeley.  The name of the
- * University may not be used to endorse or promote products derived
- * from this software without specific prior written permission.
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
- * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ * This code is derived from software contributed to Berkeley by
+ * Chris Torek.
+ *
+ * %sccs.include.redist.c%
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)memset.c	5.4 (Berkeley) %G%";
+static char sccsid[] = "@(#)memset.c	5.5 (Berkeley) %G%";
 #endif /* LIBC_SCCS and not lint */
 
-char *
-memset(s, c, n)
-	register char *s;
-	register c, n;
+#include <string.h>
+#include <sys/stdc.h>
+
+void *
+memset(dst, c, n)
+	void *dst;
+	register int c;
+	register size_t n;
 {
-	register char *p = s;
 
-	while (--n >= 0)
-		*s++ = c;
+	if (n != 0) {
+		register char *d = dst;
 
-	return (p);
+		do
+			*d++ = c;
+		while (--n != 0);
+	}
+	return (dst);
 }
