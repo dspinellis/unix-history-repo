ab950f0111c9a2c2998f8463406cfc3e5a976e2b
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-05-15 20:58:36 -0800

    minor cleanup, use int, not long
    SCCS-vsn: lib/libc/string/ffs.c 5.3

--- usr/src/lib/libc/string/ffs.c
+++ usr/src/lib/libc/string/ffs.c
@@ -1,35 +1,25 @@
-/*
- * Copyright (c) 1987 Regents of the University of California.
+/*-
+ * Copyright (c) 1990 The Regents of the University of California.
  * All rights reserved.
  *
- * Redistribution and use in source and binary forms are permitted
- * provided that the above copyright notice and this paragraph are
- * duplicated in all such forms and that any documentation,
- * advertising materials, and other materials related to such
- * distribution and use acknowledge that the software was developed
- * by the University of California, Berkeley.  The name of the
- * University may not be used to endorse or promote products derived
- * from this software without specific prior written permission.
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
- * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ * %sccs.include.redist.c%
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)ffs.c	5.2 (Berkeley) %G%";
+static char sccsid[] = "@(#)ffs.c	5.3 (Berkeley) %G%";
 #endif /* LIBC_SCCS and not lint */
 
 /*
  * ffs -- vax ffs instruction
  */
 ffs(mask)
-	register long mask;
+	register int mask;
 {
-	register int cnt;
+	register int bit;
 
 	if (mask == 0)
 		return(0);
-	for (cnt = 1; !(mask & 1); cnt++)
+	for (bit = 1; !(mask & 1); bit++)
 		mask >>= 1;
-	return(cnt);
+	return(bit);
 }
