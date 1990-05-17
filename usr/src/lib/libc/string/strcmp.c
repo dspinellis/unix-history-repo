71a30abcc11bff5194babf6558733222383461ec
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-05-17 01:31:56 -0800

    compare using unsigned characters
    SCCS-vsn: lib/libc/string/strcmp.c 5.4

--- usr/src/lib/libc/string/strcmp.c
+++ usr/src/lib/libc/string/strcmp.c
@@ -1,29 +1,29 @@
-/*
- * Copyright (c) 1988 Regents of the University of California.
+/*-
+ * Copyright (c) 1990 The Regents of the University of California.
  * All rights reserved.
  *
- * Redistribution and use in source and binary forms are permitted
- * provided that the above copyright notice and this paragraph are
- * duplicated in all such forms and that any documentation,
- * advertising materials, and other materials related to such
- * distribution and use acknowledge that the software was developed
- * by the University of California, Berkeley.  The name of the
- * University may not be used to endorse or promote products derived
- * from this software without specific prior written permission.
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
- * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ * This code is derived from software contributed to Berkeley by
+ * Chris Torek.
+ *
+ * %sccs.include.redist.c%
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)strcmp.c	5.3 (Berkeley) %G%";
+static char sccsid[] = "@(#)strcmp.c	5.4 (Berkeley) %G%";
 #endif /* LIBC_SCCS and not lint */
 
+#include <sys/stdc.h>
+#include <string.h>
+
+/*
+ * Compare strings.
+ */
+int
 strcmp(s1, s2)
-	register char *s1, *s2;
+	register const char *s1, *s2;
 {
-	for (; *s1 == *s2; ++s1, ++s2)
-		if (!*s1)
-			return(0);
-	return(*s1 - *s2);
+	while (*s1 == *s2++)
+		if (*s1++ == 0)
+			return (0);
+	return (*(unsigned char *)s1 - *(unsigned char *)--s2);
 }
