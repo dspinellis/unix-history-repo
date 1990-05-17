66df03aca4a5a7e1200f8e7d2cb9be5808837d02
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-05-17 01:37:18 -0800

    ANSI version
    SCCS-vsn: lib/libc/string/strcspn.c 5.5

--- usr/src/lib/libc/string/strcspn.c
+++ usr/src/lib/libc/string/strcspn.c
@@ -1,38 +1,42 @@
-/*
- * Copyright (c) 1985 Regents of the University of California.
+/*-
+ * Copyright (c) 1990 The Regents of the University of California.
  * All rights reserved.
  *
- * Redistribution and use in source and binary forms are permitted
- * provided that the above copyright notice and this paragraph are
- * duplicated in all such forms and that any documentation,
- * advertising materials, and other materials related to such
- * distribution and use acknowledge that the software was developed
- * by the University of California, Berkeley.  The name of the
- * University may not be used to endorse or promote products derived
- * from this software without specific prior written permission.
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
- * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ * This code is derived from software contributed to Berkeley by
+ * Chris Torek.
+ *
+ * %sccs.include.redist.c%
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)strcspn.c	5.4 (Berkeley) %G%";
+static char sccsid[] = "@(#)strcspn.c	5.5 (Berkeley) %G%";
 #endif /* LIBC_SCCS and not lint */
 
-strcspn(s, set)
-	register char *s, *set;
-{
-	register n = 0;
-	register char *p;
-	register c;
+#include <sys/stdc.h>
+#include <string.h>
 
-	while (c = *s++) {
-		for (p = set; *p; p++)
-			if (c == *p)
-				break;
-		if (*p)
-			return (n);
-		n++;
+/*
+ * Span the complement of string s2.
+ */
+size_t
+strcspn(s1, s2)
+	const char *s1;
+	register const char *s2;
+{
+	register const char *p, *spanp;
+	register char c, sc;
+
+	/*
+	 * Stop as soon as we find any character from s2.  Note that there
+	 * must be a NUL in s2; it suffices to stop when we find that, too.
+	 */
+	for (p = s1;;) {
+		c = *p++;
+		spanp = s2;
+		do {
+			if ((sc = *spanp++) == c)
+				return (p - 1 - s1);
+		} while (sc != 0);
 	}
-	return (n);
+	/* NOTREACHED */
 }
