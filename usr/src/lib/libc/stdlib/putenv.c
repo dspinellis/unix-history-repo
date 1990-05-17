4ef9385165412b4a26850e57eec8ed61a2b85094
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-05-16 20:51:44 -0800

    cleanup includes
    SCCS-vsn: lib/libc/stdlib/atoi.c 5.5
    SCCS-vsn: lib/libc/stdlib/atol.c 5.5
    SCCS-vsn: lib/libc/stdlib/putenv.c 5.2

--- usr/src/lib/libc/stdlib/putenv.c
+++ usr/src/lib/libc/stdlib/putenv.c
@@ -1,30 +1,21 @@
-/*
+/*-
  * Copyright (c) 1988 The Regents of the University of California.
  * All rights reserved.
  *
- * Redistribution and use in source and binary forms are permitted
- * provided that the above copyright notice and this paragraph are
- * duplicated in all such forms and that any documentation,
- * advertising materials, and other materials related to such
- * distribution and use acknowledge that the software was developed
- * by the University of California, Berkeley.  The name of the
- * University may not be used to endorse or promote products derived
- * from this software without specific prior written permission.
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ * %sccs.include.redist.c%
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)putenv.c	5.1 (Berkeley) %G%";
+static char sccsid[] = "@(#)putenv.c	5.2 (Berkeley) %G%";
 #endif /* LIBC_SCCS and not lint */
+
+#include <stdlib.h>
 
 putenv(str)
 	char *str;
 {
 	register char *equal;
 	int rval;
-	char *index();
 
 	if (!(equal = index(str, '=')))
 		return(1);
