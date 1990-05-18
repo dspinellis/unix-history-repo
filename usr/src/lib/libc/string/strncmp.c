fd2878cbda8973fd0895b26b35c27f58c889edd2
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-05-17 17:38:06 -0800

    ANSI version
    SCCS-vsn: lib/libc/string/strlen.c 5.4
    SCCS-vsn: lib/libc/string/strncmp.c 5.4

--- usr/src/lib/libc/string/strncmp.c
+++ usr/src/lib/libc/string/strncmp.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1988 Regents of the University of California.
+ * Copyright (c) 1989 The Regents of the University of California.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms are permitted
@@ -12,19 +12,29 @@
  * from this software without specific prior written permission.
  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
- * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)strncmp.c	5.3 (Berkeley) %G%";
+static char sccsid[] = "@(#)strncmp.c	5.4 (Berkeley) %G%";
 #endif /* LIBC_SCCS and not lint */
 
-strncmp(s1, s2, cnt)
-	register char *s1, *s2;
-	register int cnt;
+#include <sys/stdc.h>
+#include <string.h>
+
+int
+strncmp(s1, s2, n)
+	register const char *s1, *s2;
+	register size_t n;
 {
-	for (; cnt && *s1 == *s2; --cnt, ++s1, ++s2)
-		if (!*s1)
-			return(0);
-	return(cnt ? *s1 - *s2 : 0);
+
+	if (n == 0)
+		return (0);
+	do {
+		if (*s1 != *s2++)
+			return (*(unsigned char *)s1 - *(unsigned char *)--s2);
+		if (*s1++ == 0)
+			break;
+	} while (--n != 0);
+	return (0);
 }
