bb56a4a76049e4ebb6b4e28d28894134112a0d21
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-05-18 19:25:10 -0800

    ANSI doesn't allow promotions for the user
    SCCS-vsn: include/stdarg.h 5.2

--- usr/src/include/stdarg.h
+++ usr/src/include/stdarg.h
@@ -4,16 +4,26 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)stdarg.h	5.1 (Berkeley) %G%
+ *	@(#)stdarg.h	5.2 (Berkeley) %G%
  */
 
 typedef char *va_list;
 
-#define	__va_round(type) \
+/*
+ * ANSI says: "If there is no actual next argument, or if type is not
+ * compatible with the type of the actual next argument (as promoted
+ * according to the default argument promotions), the behavior is
+ * undefined."  We read this to mean that we're not allowed to do the
+ * promotion for the user, so shorts and chars drop core.
+ */
+#define	va_arg(ap, type) \
+	((type *)(ap += sizeof(type) < sizeof(int) ? \
+		abort() : sizeof(type)))[-1]
+
+#define	va_end(ap)
+
+#define	__va_promote(type) \
 	(((sizeof(type) + sizeof(int) - 1) / sizeof(int)) * sizeof(int))
 
-#define	va_arg(ap, type) \
-	((type *)(ap += __va_round(type)))[-1]
-#define	va_end(ap)
 #define	va_start(ap, last) \
-	(ap = ((char *)&(last) + __va_round(last)))
+	(ap = ((char *)&(last) + __va_promote(last)))
