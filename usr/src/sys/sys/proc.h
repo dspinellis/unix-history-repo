28198d883246d3ddd1003bd75b8e2df41a44f92a
Marc Teitelbaum <marc@ucbvax.Berkeley.EDU>
1990-05-18 23:00:45 -0800

    delete PROC_JOBC - now done inline in fixjobc()
    SCCS-vsn: sys/sys/proc.h 7.16

--- usr/src/sys/sys/proc.h
+++ usr/src/sys/sys/proc.h
@@ -3,7 +3,7 @@
  * All rights reserved.  The Berkeley software License Agreement
  * specifies the terms and conditions for redistribution.
  *
- *	@(#)proc.h	7.15 (Berkeley) %G%
+ *	@(#)proc.h	7.16 (Berkeley) %G%
  */
 
 /*
@@ -153,8 +153,6 @@ struct	prochd {
 int	whichqs;		/* bit mask summarizing non-empty qs's */
 
 #define SESS_LEADER(p)	((p)->p_session->s_leader == (p))
-#define PGRP_JOBC(p)	(((p)->p_pgrp != (p)->p_pptr->p_pgrp) && \
-			((p)->p_session == (p)->p_pptr->p_session))
 #endif
 
 /* stat codes */
