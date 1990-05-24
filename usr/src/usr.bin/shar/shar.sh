276028d16ec36ef011d05391f8a8c053f8fd13c9
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-05-23 18:19:31 -0800

    date and time created 90/05/23 11:19:31 by bostic
    SCCS-vsn: usr.bin/shar/shar.sh 5.1

---
+++ usr/src/usr.bin/shar/shar.sh
@@ -0,0 +1,41 @@
+#!/bin/sh -
+#
+# Copyright (c) 1990 The Regents of the University of California.
+# All rights reserved.
+#
+# %sccs.include.redist.sh%
+#
+#	@(#)shar.sh	5.1 (Berkeley) %G%
+#
+
+cat << EOF
+# This is a shell archive.  Save it in a file, remove anything before
+# this line, and then unpack it by entering "sh file".  Note, it may
+# create directories; files and directories will be owned by you and
+# have default permissions.
+#
+# This archive contains:
+#
+EOF
+
+for i
+do
+	echo "#	$i"
+done
+
+echo "#"
+
+for i
+do
+	if [ -d $i ]; then
+		echo "echo c - $i"
+		echo "mkdir $i > /dev/null 2>&1"
+	else
+		echo "echo x - $i"
+		echo "sed 's/^X//' >$i << 'END-of-$i'"
+		sed 's/^/X/' $i
+		echo "END-of-$i"
+	fi
+done
+echo exit
+echo ""
