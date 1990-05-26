c84f6f3c42bab46170d44f9bf72a1fd0c39be8d5
CSRG <csrg@ucbvax.Berkeley.EDU>
1990-05-26 15:52:22 -0800

    BSD 4_3_Net_2 development Work on file usr/src/lib/libg++/libg++/Binomial.cc Work on file usr/src/lib/libg++/libg++/EH.cc Work on file usr/src/lib/libg++/libg++/Erlang.cc Work on file usr/src/lib/libg++/libg++/Fix16.cc Work on file usr/src/lib/libg++/libg++/GetOpt.cc Work on file usr/src/lib/libg++/libg++/MLCG.cc Work on file usr/src/lib/libg++/libg++/Poisson.cc Work on file usr/src/lib/libg++/libg++/SFile.cc Work on file usr/src/lib/libg++/libg++/Uniform.cc Work on file usr/src/lib/libg++/libg++/chr.cc Work on file usr/src/lib/libg++/libg++/delete.cc Work on file usr/src/lib/libg++/libg++/gcd.cc Work on file usr/src/lib/libg++/libg++/hash.cc Work on file usr/src/lib/libg++/libg++/lg.cc Work on file usr/src/lib/libg++/libg++/fmtq.cc Work on file usr/src/lib/libg++/libg++/new.cc Work on file usr/src/lib/libg++/libg++/ioob.cc Work on file usr/src/lib/libg++/libg++/str.cc Work on file usr/src/lib/libg++/libg++/sqrt.cc Work on file usr/src/lib/libg++/libg++/xyzzy.cc
    Synthesized-from: CSRG/cd2/net.2

---
+++ usr/src/lib/libg++/libg++/chr.cc
@@ -0,0 +1,42 @@
+/* 
+Copyright (C) 1990 Free Software Foundation
+    written by Doug Lea (dl@rocky.oswego.edu)
+
+This file is part of GNU CC.
+
+GNU CC is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY.  No author or distributor
+accepts responsibility to anyone for the consequences of using it
+or for whether it serves any particular purpose or works at all,
+unless he says so in writing.  Refer to the GNU CC General Public
+License for full details.
+
+Everyone is granted permission to copy, modify and redistribute
+GNU CC, but only under the conditions described in the
+GNU CC General Public License.   A copy of this license is
+supposed to have been given to you along with GNU CC so you
+can know your rights and responsibilities.  It should be in a
+file named COPYING.  Among other things, the copyright notice
+and this notice must be preserved on all copies.  
+*/
+
+#ifdef __GNUG__
+#pragma implementation
+#endif
+#include <builtin.h>
+#include <AllocRing.h>
+
+extern AllocRing _libgxx_fmtq;
+
+char* chr(char ch, int width)
+{
+  int len = 1;
+  int wrksiz = len + width + 1;
+  char* fmtbase = (char *) _libgxx_fmtq.alloc(wrksiz);
+  char* fmt = fmtbase;
+  for (int blanks = width - len; blanks > 0; --blanks)
+   *fmt++ = ' ';
+  *fmt++ = ch;
+  *fmt = 0;
+  return fmtbase;
+}
