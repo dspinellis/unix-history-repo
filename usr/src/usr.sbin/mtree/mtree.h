45035cc7c99825f69368d5ca78b22662d8e55088
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-05-25 17:29:58 -0800

    integrate fts(3), don't need lots of this stuff
    SCCS-vsn: usr.sbin/mtree/mtree.h 5.3

--- usr/src/usr.sbin/mtree/mtree.h
+++ usr/src/usr.sbin/mtree/mtree.h
@@ -4,55 +4,51 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)mtree.h	5.2 (Berkeley) %G%
+ *	@(#)mtree.h	5.3 (Berkeley) %G%
  */
 
 #include <string.h>
 #include <stdlib.h>
 
-typedef struct _id {
-	struct _id *next;
-	u_int id;
-	u_long cnt;
-} ID;
-
 typedef struct _info {
-#define	F_CKSUM	0x001				/* check sum */
-#define	F_DMODE	0x002				/* directory mode */
-#define	F_FMODE	0x004				/* directory mode */
-#define	F_GROUP	0x008				/* group */
-#define	F_MODE	0x010				/* directory mode */
-#define	F_NLINK	0x020				/* number of links */
-#define	F_OWNER	0x040				/* owner */
-#define	F_SIZE	0x080				/* size */
-#define	F_SLINK	0x100				/* link count */
-#define	F_TYPE	0x200				/* file type */
-	u_int	flags;				/* items set */
-
 #define	F_BLOCK	0x001				/* block special */
 #define	F_CHAR	0x002				/* char special */
 #define	F_DIR	0x004				/* directory */
 #define	F_FILE	0x008				/* regular file */
 #define	F_LINK	0x010				/* symbolic link */
 #define	F_SOCK	0x020				/* socket */
-	u_int	type;				/* file type */
+#define	F_NONE	0x040				/* unknown */
+	u_short	type;				/* file type */
 
+#define	F_CKSUM	0x001				/* check sum */
+#define	F_GROUP	0x002				/* group */
+#define	F_IGN	0x004				/* ignore */
+#define	F_MODE	0x008				/* mode */
+#define	F_NLINK	0x010				/* number of links */
+#define	F_OWNER	0x020				/* owner */
+#define	F_SIZE	0x040				/* size */
+#define	F_SLINK	0x080				/* link count */
+#define	F_TYPE	0x100				/* file type */
+	u_short	flags;				/* items set */
+
+	off_t	st_size;
+	u_long	cksum;				/* check sum */
 	uid_t	st_uid;
 	gid_t	st_gid;
-	off_t	st_size;
+#define	MBITS	(S_ISUID|S_ISGID|S_ISTXT|S_IRWXU|S_IRWXG|S_IRWXO)
 	mode_t	st_mode;
 	nlink_t	st_nlink;
-	u_long	cksum;				/* check sum */
 	char	*slink;				/* symbolic link reference */
 } INFO;
 
 typedef struct _entry {
-	struct _entry	*next, *child, *parent;	/* tree links */
+	struct _entry	*child, *parent;	/* up, down */
+	struct _entry	*next, *prev;		/* left, right */
 	INFO	info;				/* node info structure */
 #define	F_DONE	0x01				/* directory done */
-#define	F_IGN	0x02				/* ignore this node on down */
-#define	F_VISIT	0x04				/* visited this node */
-	u_int	flags;				/* flags */
+#define	F_VISIT	0x02				/* visited this node */
+	u_char	flags;				/* flags */
 	char	*name;				/* node name */
-	int	nlen;				/* name length */
 } ENTRY;
+
+#define	RP(p)	(p->fts_path + 2)
