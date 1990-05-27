fdb951cfa0932aacdd715b869981e6a773b5c9f6
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-05-26 18:54:58 -0800

    add .even to guarantee jmp instruction is on even address
    SCCS-vsn: lib/libc/hp300/SYS.h 5.2

--- usr/src/lib/libc/hp300/SYS.h
+++ usr/src/lib/libc/hp300/SYS.h
@@ -8,7 +8,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)SYS.h	5.1 (Berkeley) %G%
+ *	@(#)SYS.h	5.2 (Berkeley) %G%
  */
 
 #include <sys/syscall.h>
@@ -30,7 +30,7 @@
 #else
 #define	ENTRY(x)	.globl _/**/x; .even; _/**/x:
 #endif PROF
-#define	SYSCALL(x)	err: jmp cerror; ENTRY(x); movl #SYS_/**/x,d0; \
+#define	SYSCALL(x)	.even; err: jmp cerror; ENTRY(x); movl #SYS_/**/x,d0; \
 			trap #0; jcs err
 #define	PSEUDO(x,y)	ENTRY(x); movl #SYS_/**/y,d0; trap #0;
 
