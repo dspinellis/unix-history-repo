7a0139c3864d915c958a171a0d1ec35768cf7f90
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-05-27 18:33:01 -0800

    don't lstat if not crossing a mount point
    SCCS-vsn: lib/libc/gen/getcwd.c 5.7

--- usr/src/lib/libc/gen/getcwd.c
+++ usr/src/lib/libc/gen/getcwd.c
@@ -16,87 +16,125 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)getcwd.c	5.6 (Berkeley) %G%";
+static char sccsid[] = "@(#)getcwd.c	5.7 (Berkeley) %G%";
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/param.h>
 #include <sys/stat.h>
 #include <dirent.h>
+#include <string.h>
+
+#define	ISDOT(dp) \
+	(dp->d_name[0] == '.' && (dp->d_name[1] == '\0' || \
+	    dp->d_name[1] == '.' && dp->d_name[2] == '\0'))
 
 char *
 getwd(store)
 	char *store;
 {
 	extern int errno;
-	register DIR *dir;
 	register struct dirent *dp;
-	register int first;
+	register DIR *dir;
+	register ino_t ino;
 	register char *pp, *pu;
-	struct stat s, tmp;
-	dev_t root_dev;
+	register int first;
+	struct stat s;
+	dev_t root_dev, dev;
 	ino_t root_ino;
 	int save_errno, found;
-	char path[MAXPATHLEN], up[MAXPATHLEN], *file, *strerror();
+	char path[MAXPATHLEN], up[MAXPATHLEN], *file;
 
+	/* save root values */
 	if (stat("/", &s)) {
 		file = "/";
 		goto err;
 	}
 	root_dev = s.st_dev;
 	root_ino = s.st_ino;
-	if (stat(".", &s)) {
-		file = ".";
-		goto err;
-	}
+
+	/* init path pointer; built from the end of the buffer */
 	pp = path + sizeof(path) - 1;
 	*pp = '\0';
+
+	/* special case first stat, it's ".", not ".." */
+	up[0] = '.';
+	up[1] = '\0';
+
 	for (pu = up, first = 1;; first = 0) {
-		if (root_dev == s.st_dev && root_ino == s.st_ino) {
+		/* stat current level */
+		if (lstat(up, &s)) {
+			file = up;
+			goto err;
+		}
+
+		/* save current node values */
+		ino = s.st_ino;
+		dev = s.st_dev;
+
+		/* check for root */
+		if (root_dev == dev && root_ino == ino) {
 			*store = '/';
 			(void) strcpy(store + 1, pp);
 			return (store);
 		}
+
 		*pu++ = '.';
 		*pu++ = '.';
 		*pu = '\0';
-		if (!(dir = opendir(up))) {
+
+		/* open and stat parent */
+		if (!(dir = opendir(up)) || fstat(dirfd(dir), &s)) {
 			file = up;
 			goto err;
 		}
+		found = save_errno = 0;
+
 		*pu++ = '/';
-		found = 0;
-		file = NULL;
-		while (errno = 0, dp = readdir(dir)) {
-			if (dp->d_name[0] == '.' && (dp->d_name[1] == '\0' ||
-			    dp->d_name[1] == '.' && dp->d_name[2] == '\0'))
-				continue;
-			bcopy(dp->d_name, pu, dp->d_namlen + 1);
-			if (lstat(up, &tmp)) {
-				file = dp->d_name;
+
+		/*
+		 * if it's a mount point you have to stat each element because
+		 * the inode number in the directory is for the entry in the
+		 * parent directory, not the inode number of the mounted file.
+		 */
+		if (s.st_dev == dev) {
+			while (dp = readdir(dir))
+				if (dp->d_fileno == ino)
+					goto hit;
+		} else {
+			while (dp = readdir(dir)) {
+				if (ISDOT(dp))
+					continue;
+				bcopy(dp->d_name, pu, dp->d_namlen + 1);
+				if (lstat(up, &s)) {
+					file = dp->d_name;
+					save_errno = errno;
+					errno = 0;
+					continue;
+				}
+				if (s.st_dev == dev && s.st_ino == ino) {
+hit:					if (!first)
+						*--pp = '/';
+					pp -= dp->d_namlen;
+					bcopy(dp->d_name, pp, dp->d_namlen);
+					found = 1;
+					break;
+				}
+			}
+			if (errno) {
+				file = up;
 				save_errno = errno;
-				continue;
-			}
-			if (tmp.st_dev == s.st_dev && tmp.st_ino == s.st_ino) {
-				if (!first)
-					*--pp = '/';
-				pp -= dp->d_namlen;
-				bcopy(dp->d_name, pp, dp->d_namlen);
-				found = 1;
-				break;
 			}
 		}
-		if (errno) {
-			save_errno = errno;
-			file = up;
-		}
-		closedir(dir);
+		(void) closedir(dir);
+
 		*pu = '\0';
+
 		if (!found) {
 			/*
 			 * We didn't find the current level in its parent
 			 * directory; figure out what to complain about.
 			 */
-			if (file) {
+			if (save_errno) {
 				errno = save_errno;
 				goto err;
 			}
@@ -104,14 +142,7 @@ getwd(store)
 				first ? "." : pp, up);
 			return ((char *)NULL);
 		}
-
-		/* stat "." at current level, then ascend */
-		if (lstat(up, &s)) {
-			file = up;
-			goto err;
-		}
 	}
-
 err:
 	(void) sprintf(store, "getwd: %s: %s", file, strerror(errno));
 	return ((char *)NULL);
