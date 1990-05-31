46cbdd72269b7452e3114fd6d4b1aa11843a1297
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-05-30 19:23:27 -0800

    new copyright notice; use ASMSTR instead of .ascsiz
    SCCS-vsn: lib/libc/vax/sys/_exit.s 5.7
    SCCS-vsn: lib/libc/vax/sys/_getlogin.s 5.4
    SCCS-vsn: lib/libc/vax/sys/getegid.s 5.7
    SCCS-vsn: lib/libc/vax/sys/geteuid.s 5.7
    SCCS-vsn: lib/libc/vax/sys/getppid.s 5.7
    SCCS-vsn: lib/libc/vax/sys/reboot.s 5.6
    SCCS-vsn: lib/libc/vax/sys/setlogin.s 5.4

--- usr/src/lib/libc/vax/sys/setlogin.s
+++ usr/src/lib/libc/vax/sys/setlogin.s
@@ -1,25 +1,15 @@
 /*
- * Copyright (c) 1989 Regents of the University of California.
+ * Copyright (c) 1983 The Regents of the University of California.
  * All rights reserved.
  *
- * Redistribution and use in source and binary forms are permitted
- * provided that the above copyright notice and this paragraph are
- * duplicated in all such forms and that any documentation,
- * advertising materials, and other materials related to such
- * distribution and use acknowledge that the software was developed
- * by the University of California, Berkeley.  The name of the
- * University may not be used to endorse or promote products derived
- * from this software without specific prior written permission.
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
- * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ * %sccs.include.redist.c%
  */
 
-#if defined(SYSLIBC_SCCS) && !defined(lint)
-	.asciz "@(#)setlogin.s	5.3 (Berkeley) %G%"
-#endif /* SYSLIBC_SCCS and not lint */
-
 #include "SYS.h"
+
+#if defined(LIBC_SCCS) && !defined(lint)
+	ASMSTR "@(#)setlogin.s	5.4 (Berkeley) %G%"
+#endif /* LIBC_SCCS and not lint */
 
 .globl	__logname_valid		/* in getlogin() */
 
