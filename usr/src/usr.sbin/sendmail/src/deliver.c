7915f5300618152c45c08d8771f97be3ec2f7d07
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-05-30 17:05:12 -0800

    unsigned sizeof test < 0; bug report 4.3BSD/usr.lib/156
    SCCS-vsn: usr.sbin/sendmail/src/deliver.c 5.37

--- usr/src/usr.sbin/sendmail/src/deliver.c
+++ usr/src/usr.sbin/sendmail/src/deliver.c
@@ -17,7 +17,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)deliver.c	5.36 (Berkeley) %G%";
+static char sccsid[] = "@(#)deliver.c	5.37 (Berkeley) %G%";
 #endif /* not lint */
 
 #include "sendmail.h"
@@ -221,7 +221,7 @@ deliver(e, firstto)
 			continue;
 
 		/* avoid overflowing tobuf */
-		if (sizeof tobuf - (strlen(to->q_paddr) + strlen(tobuf) + 2) < 0)
+		if (sizeof tobuf < (strlen(to->q_paddr) + strlen(tobuf) + 2))
 			break;
 
 		if (tTd(10, 1))
