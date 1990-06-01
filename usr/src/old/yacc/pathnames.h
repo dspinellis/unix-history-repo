a28b14df08cb34fb378c55665dcf6ee27dc7b54c
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-05-31 23:45:04 -0800

    rename to indicate deprecation
    SCCS-vsn: old/yacc/pathnames.h 5.2

--- usr/src/old/yacc/pathnames.h
+++ usr/src/old/yacc/pathnames.h
@@ -14,7 +14,7 @@
  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  *
- *	@(#)pathnames.h	5.1 (Berkeley) %G%
+ *	@(#)pathnames.h	5.2 (Berkeley) %G%
  */
 
-#define	_PATH_PARSER	"/usr/share/misc/yaccpar"
+#define	_PATH_PARSER	"/usr/share/misc/old.yaccpar"
