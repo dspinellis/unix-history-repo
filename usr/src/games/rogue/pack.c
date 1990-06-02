03f0a8a6463eb722903e1d9a8d9ac2e85971e10f
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-06-01 20:56:49 -0800

    new copyright notice
    SCCS-vsn: games/rogue/pack.c 5.3
    SCCS-vsn: games/snake/snscore/snscore.c 5.5
    SCCS-vsn: games/sail/assorted.c 5.4
    SCCS-vsn: games/rogue/play.c 5.3
    SCCS-vsn: games/rogue/random.c 5.3
    SCCS-vsn: games/sail/dr_1.c 5.4
    SCCS-vsn: games/rogue/ring.c 5.3
    SCCS-vsn: games/sail/dr_2.c 5.4
    SCCS-vsn: games/rogue/rogue.h 5.5
    SCCS-vsn: games/rogue/room.c 5.3
    SCCS-vsn: games/sail/dr_3.c 5.4
    SCCS-vsn: games/sail/dr_4.c 5.4
    SCCS-vsn: games/rogue/save.c 5.3
    SCCS-vsn: games/sail/dr_5.c 5.4
    SCCS-vsn: games/rogue/score.c 5.5
    SCCS-vsn: games/sail/dr_main.c 5.5
    SCCS-vsn: games/sail/driver.h 5.4
    SCCS-vsn: games/rogue/spec_hit.c 5.3
    SCCS-vsn: games/sail/extern.h 5.4
    SCCS-vsn: games/rogue/throw.c 5.3
    SCCS-vsn: games/rogue/trap.c 5.3
    SCCS-vsn: games/sail/game.c 5.4
    SCCS-vsn: games/rogue/use.c 5.3
    SCCS-vsn: games/sail/globals.c 5.4
    SCCS-vsn: games/rogue/zap.c 5.3
    SCCS-vsn: games/sail/lo_main.c 5.6
    SCCS-vsn: games/sail/machdep.h 5.5
    SCCS-vsn: games/sail/main.c 5.5
    SCCS-vsn: games/sail/misc.c 5.5
    SCCS-vsn: games/sail/parties.c 5.4
    SCCS-vsn: games/sail/pl_1.c 5.4
    SCCS-vsn: games/sail/pl_2.c 5.4
    SCCS-vsn: games/sail/pl_3.c 5.4
    SCCS-vsn: games/sail/pl_4.c 5.4
    SCCS-vsn: games/trek/abandon.c 5.4
    SCCS-vsn: games/sail/pl_5.c 5.4
    SCCS-vsn: games/trek/attack.c 5.4
    SCCS-vsn: games/sail/pl_6.c 5.4
    SCCS-vsn: games/trek/autover.c 5.5
    SCCS-vsn: games/sail/pl_7.c 5.6
    SCCS-vsn: games/trek/capture.c 5.4
    SCCS-vsn: games/trek/cgetc.c 5.4
    SCCS-vsn: games/sail/pl_main.c 5.4
    SCCS-vsn: games/trek/check_out.c 5.4
    SCCS-vsn: games/sail/player.h 5.4
    SCCS-vsn: games/trek/checkcond.c 5.4
    SCCS-vsn: games/sail/sync.c 5.6
    SCCS-vsn: games/trek/compkl.c 5.4
    SCCS-vsn: games/trek/computer.c 4.8
    SCCS-vsn: games/sail/version.c 5.4
    SCCS-vsn: games/trek/damage.c 5.4
    SCCS-vsn: games/wargames/wargames.sh 5.4
    SCCS-vsn: games/trek/damaged.c 5.4
    SCCS-vsn: games/trek/dcrept.c 5.4
    SCCS-vsn: games/trek/destruct.c 5.5
    SCCS-vsn: games/trek/dock.c 5.4
    SCCS-vsn: games/trek/dumpgame.c 4.6
    SCCS-vsn: games/trek/dumpme.c 5.4
    SCCS-vsn: games/trek/dumpssradio.c 5.4
    SCCS-vsn: games/trek/events.c 5.4
    SCCS-vsn: games/trek/externs.c 5.4
    SCCS-vsn: games/trek/getcodi.c 5.4
    SCCS-vsn: games/trek/getpar.c 4.8
    SCCS-vsn: games/trek/getpar.h 5.4
    SCCS-vsn: games/trek/help.c 5.4
    SCCS-vsn: games/trek/impulse.c 5.4
    SCCS-vsn: games/trek/initquad.c 5.4
    SCCS-vsn: games/trek/kill.c 5.4
    SCCS-vsn: games/trek/klmove.c 5.4
    SCCS-vsn: games/trek/lose.c 5.5
    SCCS-vsn: games/trek/lrscan.c 5.4
    SCCS-vsn: games/trek/main.c 5.5
    SCCS-vsn: games/trek/move.c 5.4
    SCCS-vsn: games/trek/nova.c 5.4
    SCCS-vsn: games/trek/out.c 5.4
    SCCS-vsn: games/trek/phaser.c 5.4
    SCCS-vsn: games/trek/play.c 5.5
    SCCS-vsn: games/trek/ram.c 5.5
    SCCS-vsn: games/trek/ranf.c 5.4
    SCCS-vsn: games/trek/rest.c 5.4
    SCCS-vsn: games/trek/schedule.c 5.4
    SCCS-vsn: games/trek/score.c 5.4
    SCCS-vsn: games/trek/setup.c 5.4
    SCCS-vsn: games/trek/setwarp.c 5.4
    SCCS-vsn: games/trek/shield.c 5.5
    SCCS-vsn: games/trek/snova.c 5.4
    SCCS-vsn: games/trek/srscan.c 5.4
    SCCS-vsn: games/trek/systemname.c 5.4
    SCCS-vsn: games/trek/torped.c 5.4
    SCCS-vsn: games/trek/trek.h 5.5
    SCCS-vsn: games/trek/utility.c 5.4
    SCCS-vsn: games/trek/visual.c 5.4
    SCCS-vsn: games/trek/warp.c 5.4
    SCCS-vsn: games/trek/win.c 5.4
    SCCS-vsn: games/worm/worm.c 5.7
    SCCS-vsn: games/worms/worms.c 5.8

--- usr/src/games/rogue/pack.c
+++ usr/src/games/rogue/pack.c
@@ -5,21 +5,11 @@
  * This code is derived from software contributed to Berkeley by
  * Timothy C. Stoehr.
  *
- * Redistribution and use in source and binary forms are permitted
- * provided that the above copyright notice and this paragraph are
- * duplicated in all such forms and that any documentation,
- * advertising materials, and other materials related to such
- * distribution and use acknowledge that the software was developed
- * by the University of California, Berkeley.  The name of the
- * University may not be used to endorse or promote products derived
- * from this software without specific prior written permission.
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)pack.c	5.2 (Berkeley) %G%";
+static char sccsid[] = "@(#)pack.c	5.3 (Berkeley) %G%";
 #endif /* not lint */
 
 /*
