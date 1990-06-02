ec759a55026a4a60d85b47cf492f3df4959adf00
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-06-01 20:15:30 -0800

    date and time created 90/06/01 13:15:30 by bostic
    SCCS-vsn: games/rogue/pathnames.h 5.1

---
+++ usr/src/games/rogue/pathnames.h
@@ -0,0 +1,12 @@
+/*-
+ * Copyright (c) 1990 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.redist.c%
+ *
+ *	@(#)pathnames.h	5.1 (Berkeley) %G%
+ */
+
+#define	_PATH_SCOREFILE	"/var/games/rogue.scores"
+#define	_PATH_LOCKFILE	"/var/games/rogue.lock"
+
