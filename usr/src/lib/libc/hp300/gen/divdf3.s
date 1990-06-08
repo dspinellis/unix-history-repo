a9002302bbfb0f7cc59fcb6bf822d197f7f3be22
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-06-08 00:50:00 -0800

    date and time created 90/06/07 17:50:00 by bostic
    SCCS-vsn: lib/libc/hp300/gen/divdf3.s 5.1

---
+++ usr/src/lib/libc/hp300/gen/divdf3.s
@@ -0,0 +1,25 @@
+/*-
+ * Copyright (c) 1990 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to Berkeley by
+ * the Systems Programming Group of the University of Utah Computer
+ * Science Department.
+ *
+ * %sccs.include.redist.c%
+ */
+
+#if defined(LIBC_SCCS) && !defined(lint)
+	.asciz "@(#)divdf3.s	5.1 (Berkeley) %G%"
+#endif /* LIBC_SCCS and not lint */
+
+#include "DEFS.h"
+
+/* double / double */
+ENTRY(__divdf3)
+	fmoved	sp@(4),fp0
+	fdivd	sp@(12),fp0
+	fmoved	fp0,sp@-
+	movel	sp@+,d0
+	movel	sp@+,d1
+	rts
