3fd0e810a682983970167ad7133b8fcd0df878dc
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-06-08 00:50:07 -0800

    date and time created 90/06/07 17:50:07 by bostic
    SCCS-vsn: lib/libc/hp300/gen/fixdfsi.s 5.1

---
+++ usr/src/lib/libc/hp300/gen/fixdfsi.s
@@ -0,0 +1,22 @@
+/*-
+ * Copyright (c) 1990 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to Berkeley by
+ * the Systems Programming Group of the University of Utah Computer
+ * Science Department.
+ *
+ * %sccs.include.redist.c%
+ */
+
+#if defined(LIBC_SCCS) && !defined(lint)
+	.asciz "@(#)fixdfsi.s	5.1 (Berkeley) %G%"
+#endif /* LIBC_SCCS and not lint */
+
+#include "DEFS.h"
+
+/* (int) double */
+ENTRY(__fixdfsi)
+	fintrzd	sp@(4),fp0
+	fmovel	fp0,d0
+	rts
