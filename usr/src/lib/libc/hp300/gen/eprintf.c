b89bd1be8fdb533f7554afebc2f8127d4703bb35
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-06-08 23:54:02 -0800

    it's a C file...
    SCCS-vsn: lib/libc/hp300/gen/eprintf.c 5.2

--- usr/src/lib/libc/hp300/gen/eprintf.c
+++ usr/src/lib/libc/hp300/gen/eprintf.c
@@ -10,7 +10,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-	.asciz "@(#)eprintf.c	5.1 (Berkeley) %G%"
+static char sccsid[] = "@(#)eprintf.c	5.2 (Berkeley) %G%";
 #endif /* LIBC_SCCS and not lint */
 
 #include <stdio.h>
