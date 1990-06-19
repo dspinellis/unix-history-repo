15148ec19504853c44eafc06e7a56dac40caf4ae
Kevin Fall <kfall@ucbvax.Berkeley.EDU>
1990-06-19 02:23:38 -0800

    change sizing on structures
    SCCS-vsn: usr.bin/passwd/kpasswd_proto.h 1.2

--- usr/src/usr.bin/passwd/kpasswd_proto.h
+++ usr/src/usr.bin/passwd/kpasswd_proto.h
@@ -23,14 +23,14 @@
  * (We hope this to be temporary until a real admin protocol is worked out)
  */
 
-#define	MSGSIZE	255
+#define	MAX_PW_LEN	256
 struct kpasswd_data {
-	C_Block	random_key;
-	char	secure_msg[MSGSIZE];
+	des_cblock random_key;
+	char secure_msg[MAX_PW_LEN];
 };
 struct update_data {
-	char	pw[255];
-	char	secure_msg[MSGSIZE];
+	char pw[MAX_PW_LEN];
+	char secure_msg[MAX_PW_LEN];
 };
 #define	SERVICE		"kpasswd"
 #define	SECURE_STRING	"Kerberos password update program -- 12/9/88 UC Berkeley"
