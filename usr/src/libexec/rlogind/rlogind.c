4e8ff895a22846c8ae565207b3d932d96574fb46
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-06-19 22:59:25 -0800

    TCSADFLUSH -> TCSAFLUSH
    SCCS-vsn: libexec/rlogind/rlogind.c 5.45

--- usr/src/libexec/rlogind/rlogind.c
+++ usr/src/libexec/rlogind/rlogind.c
@@ -22,7 +22,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)rlogind.c	5.44 (Berkeley) %G%";
+static char sccsid[] = "@(#)rlogind.c	5.45 (Berkeley) %G%";
 #endif /* not lint */
 
 #ifdef KERBEROS
