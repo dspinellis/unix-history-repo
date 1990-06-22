dbf21b4c536ec45897537cd88cf476fc37800908
Kevin Fall <kfall@ucbvax.Berkeley.EDU>
1990-06-21 21:31:19 -0800

    add ROOT login syslog message
    SCCS-vsn: libexec/rlogind/rlogind.c 5.46

--- usr/src/libexec/rlogind/rlogind.c
+++ usr/src/libexec/rlogind/rlogind.c
@@ -22,7 +22,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)rlogind.c	5.45 (Berkeley) %G%";
+static char sccsid[] = "@(#)rlogind.c	5.46 (Berkeley) %G%";
 #endif /* not lint */
 
 #ifdef KERBEROS
@@ -294,7 +294,9 @@ gotpty:
 	if (fchmod(t, 0))
 		fatal(f, line, 1);
 	(void)signal(SIGHUP, SIG_IGN);
-	vhangup();
+#ifdef	notdef
+vhangup();
+#endif
 	(void)signal(SIGHUP, SIG_DFL);
 	t = open(line, O_RDWR);
 	if (t < 0)
@@ -329,10 +331,16 @@ gotpty:
 		dup2(t, STDERR_FILENO);
 		(void)close(t);
 
-		if (authenticated)
+		if (authenticated) {
+			if (use_kerberos && (pwd->pw_uid == 0))
+				syslog(LOG_INFO|LOG_AUTH,
+				    "ROOT Kerberos login from %s.%s@%s on %s\n",
+				    kdata->pname, kdata->pinst, kdata->prealm,
+				    hp->h_name);
+
 			execl(_PATH_LOGIN, "login", "-p",
 			    "-h", hp->h_name, "-f", lusername, 0);
-		else
+		} else
 			execl(_PATH_LOGIN, "login", "-p",
 			    "-h", hp->h_name, lusername, 0);
 		fatal(STDERR_FILENO, _PATH_LOGIN, 1);
