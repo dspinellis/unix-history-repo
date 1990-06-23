4353a18681de5b5e002a4fe26c74c153024930cd
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-06-23 00:18:36 -0800

    have to pass error to ktrace
    SCCS-vsn: sys/kern/uipc_syscalls.c 7.17

--- usr/src/sys/kern/uipc_syscalls.c
+++ usr/src/sys/kern/uipc_syscalls.c
@@ -568,7 +568,7 @@ sendit(s, mp, flags, retsize)
 	if (ktriov != NULL) {
 		if (error == 0)
 			ktrgenio(u.u_procp->p_tracep, s, UIO_WRITE,
-				ktriov, *retsize);
+				ktriov, *retsize, error);
 		FREE(ktriov, M_TEMP);
 	}
 #endif
@@ -798,7 +798,7 @@ recvit(s, mp, namelenp, retsize)
 	if (ktriov != NULL) {
 		if (error == 0)
 			ktrgenio(u.u_procp->p_tracep, s, UIO_READ,
-				ktriov, len - auio.uio_resid);
+				ktriov, len - auio.uio_resid, error);
 		FREE(ktriov, M_TEMP);
 	}
 #endif
