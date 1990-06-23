0a2dcfba0bbd84c24131b956fbedbef6ce4b5383
Kevin Fall <kfall@ucbvax.Berkeley.EDU>
1990-06-23 01:55:37 -0800

    filesystem reorg
    SCCS-vsn: local/kerberosIV/register/pathnames.h 1.2

--- usr/src/local/kerberosIV/register/pathnames.h
+++ usr/src/local/kerberosIV/register/pathnames.h
@@ -1,4 +1,12 @@
+/*-
+ * Copyright (c) 1990 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.redist.c%
+ *
+ *	@(#)pathnames.h	1.2 (Berkeley) %G%
+ */
 
-#define	SERVER_KEYDIR	"/kerberos/register_keys"
+#define	SERVER_KEYDIR	"/etc/kerberosIV/register_keys"
 #define	CLIENT_KEYFILE	"/.update.key"
-#define	_PATH_KPASSWD	"/usr/local/kpasswd"
+#define	_PATH_KPASSWD	"/usr/bin/passwd"
