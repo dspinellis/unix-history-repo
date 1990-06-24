ed2bdf5ddf0a640d22bb366785f9ca1142f70ad0
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-06-23 23:20:43 -0800

    add Chris Torek's comment before removing
    SCCS-vsn: lib/libcompat/4.1/reset.c 4.2

--- usr/src/lib/libcompat/4.1/reset.c
+++ usr/src/lib/libcompat/4.1/reset.c
@@ -1,4 +1,20 @@
-/*	reset.c	4.1	83/06/30	*/
+/*	reset.c	4.2	90/06/23	*/
+
+/*
+ * This can't be written in C.  You have to longjmp from a context
+ * below (stackwise) the call to setjmp:
+ *
+ *	/* test reset/setexit *(/
+ *	main()
+ *	{
+ *		int i = setexit();
+ *		printf("i=%d\n", i);
+ *		if (i == 0)
+ *			reset(1);
+ *	}
+ *
+ * The above prints `longjmp botch' and dumps core.
+ */
 
 /*
  * Backwards compatible setexit/reset.
