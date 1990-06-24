4b84788324f5789a4ed6a74745bcf00138c43c16
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-06-23 21:53:58 -0800

    move lib.b to bc.library
    SCCS-vsn: usr.bin/bc/Makefile 5.5
    SCCS-vsn: usr.bin/bc/bc.1 6.4
    SCCS-vsn: usr.bin/bc/pathnames.h 5.3

--- usr/src/usr.bin/bc/pathnames.h
+++ usr/src/usr.bin/bc/pathnames.h
@@ -4,8 +4,8 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)pathnames.h	5.2 (Berkeley) %G%
+ *	@(#)pathnames.h	5.3 (Berkeley) %G%
  */
 
-#define	_PATH_LIBB	"/usr/share/misc/lib.b"
+#define	_PATH_LIBB	"/usr/share/misc/bc.library"
 #define	_PATH_DC	"/usr/bin/dc"
