2a45358ca9ca9b6e6668fb0503235ed9ef7d3b68
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-06-24 23:18:49 -0800

    get MAILDIR from paths.h
    SCCS-vsn: usr.bin/login/pathnames.h 5.5

--- usr/src/usr.bin/login/pathnames.h
+++ usr/src/usr.bin/login/pathnames.h
@@ -14,12 +14,11 @@
  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  *
- *	@(#)pathnames.h	5.4 (Berkeley) %G%
+ *	@(#)pathnames.h	5.5 (Berkeley) %G%
  */
 
 #include <paths.h>
 
 #define	_PATH_DEFPATH	"/bin:/usr/bin:/usr/games:"
 #define	_PATH_HUSHLOGIN	".hushlogin"
-#define	_PATH_MAILDIR	"/var/spool/mail"
 #define	_PATH_MOTDFILE	"/etc/motd"
