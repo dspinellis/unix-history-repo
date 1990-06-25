55e5edef3e689d5e8a7abf9974c2501313f1355c
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-06-25 06:30:09 -0800

    move _PATH_MAILDIR into <paths.h>
    SCCS-vsn: libexec/mail.local/pathnames.h 5.2
    SCCS-vsn: libexec/comsat/comsat.c 5.21

--- usr/src/libexec/comsat/comsat.c
+++ usr/src/libexec/comsat/comsat.c
@@ -12,7 +12,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)comsat.c	5.20 (Berkeley) %G%";
+static char sccsid[] = "@(#)comsat.c	5.21 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/param.h>
@@ -30,14 +30,10 @@ static char sccsid[] = "@(#)comsat.c	5.20 (Berkeley) %G%";
 #include <errno.h>
 #include <netdb.h>
 #include <syslog.h>
-#include <string.h>
 #include <ctype.h>
+#include <string.h>
+#include <paths.h>
 
-#include "pathnames.h"
-
-/*
- * comsat
- */
 int	debug = 0;
 #define	dsyslog	if (debug) syslog
 
--- usr/src/libexec/mail.local/pathnames.h
+++ usr/src/libexec/mail.local/pathnames.h
@@ -4,11 +4,10 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)pathnames.h	5.1 (Berkeley) %G%
+ *	@(#)pathnames.h	5.2 (Berkeley) %G%
  */
 
 #include <paths.h>
 
 #undef _PATH_TMP
 #define _PATH_TMP	"/tmp/maXXXXX"
-#define _PATH_MAILDIR	"/var/mail/"
