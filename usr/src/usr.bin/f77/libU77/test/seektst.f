a213d0e025283d38d0eda18faa4a649e928ae063
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-06-25 21:07:11 -0800

    date and time created 90/06/25 14:07:11 by bostic
    SCCS-vsn: usr.bin/f77/libU77/test/seektst.f 5.1

---
+++ usr/src/usr.bin/f77/libU77/test/seektst.f
@@ -0,0 +1,16 @@
+	character*20 word1, word2
+	integer ftell, fseek
+	open(1, file="/usr/dict/words")
+	iend = ftell(1)
+	iseek1 = fseek(1, iend/2, 0)
+	read(1,'(a)') word1
+	iword = ftell(1)
+	read(1,'(a)') word1
+	rewind 1
+	ibeg = ftell(1)
+	iseek2 = fseek(1, iword, 0)
+	read(1,'(a)') word2
+	write(*,*) ibeg, iword, ftell(1), iend
+	write(*,*) word1(:lnblnk(word1)), word2(:lnblnk(word2))
+	write(*,*) "seek status", iseek1, iseek2, fseek(1, -1, 0)
+	end
