63617b79e093c3d1c15f451c169ca4cd8373649f
Jim Bloom <bloom@ucbvax.Berkeley.EDU>
1990-06-27 20:29:49 -0800

    fix printing of time when ttl is 0
    SCCS-vsn: lib/libc/net/res_debug.c 5.30

--- usr/src/lib/libc/net/res_debug.c
+++ usr/src/lib/libc/net/res_debug.c
@@ -1,22 +1,14 @@
-/*
+/*-
  * Copyright (c) 1985, 1990 Regents of the University of California.
  * All rights reserved.
  *
- * Redistribution and use in source and binary forms are permitted
- * provided that the above copyright notice and this paragraph are
- * duplicated in all such forms and that any documentation,
- * advertising materials, and other materials related to such
- * distribution and use acknowledge that the software was developed
- * by the University of California, Berkeley.  The name of the
- * University may not be used to endorse or promote products derived
- * from this software without specific prior written permission.
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
- * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ * %sccs.include.redist.c%
+ *
+ *	@(#)res_debug.c	5.30 (Berkeley) %G%
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)res_debug.c	5.29 (Berkeley) %G%";
+static char sccsid[] = "@(#)res_debug.c	5.30 (Berkeley) %G%";
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/types.h>
@@ -453,6 +445,11 @@ p_time(value)
 {
 	int secs, mins, hours;
 	register char *p;
+
+	if (value == 0) {
+		strcpy(nbuf, "0 secs");
+		return(nbuf);
+	}
 
 	secs = value % 60;
 	value /= 60;
