c6c450362dec22b151e6b669a54f8a2222d6c123
Marc Teitelbaum <marc@ucbvax.Berkeley.EDU>
1990-06-29 07:14:52 -0800

    new interface
    SCCS-vsn: lib/libutil/pty.c 1.2
    SCCS-vsn: lib/libc/gen/daemon.c 5.2

--- usr/src/lib/libutil/pty.c
+++ usr/src/lib/libutil/pty.c
@@ -6,20 +6,31 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)pty.c	1.1 (Berkeley) %G%";
+static char sccsid[] = "@(#)pty.c	1.2 (Berkeley) %G%";
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/file.h>
+#include <sys/ioctl.h>
+#include <termios.h>
+#include <grp.h>
 #include <errno.h>
 
-openpty(amaster, aslave, name, flag)
+openpty(amaster, aslave, name, termp, winp)
 	int *amaster, *aslave;
 	char *name;
-	int flag;
+	struct termios *termp;
+	struct winsize *winp;
 {
 	register char *line = "/dev/ptyXX", *cp1, *cp2;
-	register master, slave;
+	register master, slave, ruid, ttygid;
+	struct group *gr;
 	extern errno;
+
+	if ((gr = getgrnam("tty")) != NULL)
+		ttygid = gr->gr_gid;
+	else
+		ttygid = -1;
+	ruid = getuid();
 
 	for (cp1 = "pqrs"; *cp1; cp1++) {
 		line[8] = *cp1;
@@ -27,20 +38,59 @@ openpty(amaster, aslave, name, flag)
 			line[9] = *cp2;
 			if ((master = open(line, O_RDWR, 0)) == -1) {
 				if (errno != EIO)
-					return (-1);
+					return (-1);	/* out of ptys */
 			} else {
 				line[5] = 't';
+				(void) chown(line, ruid, ttygid);
+				(void) chmod(line, 0620);
+				(void) revoke(line);
 				if ((slave = open(line, O_RDWR, 0)) != -1) {
 					*amaster = master;
 					*aslave = slave;
 					if (name)
 						strcpy(name, line);
+					if (termp)
+						(void) tcsetattr(slave, 
+							TCSAFLUSH, &termp);
+					if (winp)
+						(void) ioctl(slave, TIOCSWINSZ, 
+							(char *)&winp);
 					return (0);
 				}
-				close(master);
+				(void) close(master);
 				line[5] = 'p';
 			}
 		}
 	}
+	errno = ENOENT;	/* out of ptys */
 	return (-1);
+}
+
+forkpty(amaster, name, termp, winp)
+	int *amaster;
+	char *name;
+	struct termios *termp;
+	struct winsize *winp;
+{
+	int master, slave, pid;
+
+	if (openpty(&master, &slave, name, termp, winp) == -1)
+		return (-1);
+	switch (pid = fork()) {
+	case -1:
+		return (-1);
+	case 0:
+		/* 
+		 * child
+		 */
+		(void) close(master);
+		login_tty(slave);
+		return (0);
+	}
+	/*
+	 * parent
+	 */
+	*amaster = master;
+	(void) close(slave);
+	return (pid);
 }
