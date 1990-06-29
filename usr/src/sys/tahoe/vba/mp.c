5b422bf399acc85699c9cf47fb1766b929cebf2e
Marc Teitelbaum <marc@ucbvax.Berkeley.EDU>
1990-06-28 22:33:21 -0800

    ttysleep to catch revoked terminals
    SCCS-vsn: sys/tahoe/vba/mp.c 7.14
    SCCS-vsn: sys/tahoe/vba/vx.c 7.10

--- usr/src/sys/tahoe/vba/mp.c
+++ usr/src/sys/tahoe/vba/mp.c
@@ -17,7 +17,7 @@
  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  *
- *	@(#)mp.c	7.13 (Berkeley) %G%
+ *	@(#)mp.c	7.14 (Berkeley) %G%
  */
 
 #include "mp.h"
@@ -258,9 +258,8 @@ restart:
 	}
 	while ((mode&O_NONBLOCK) == 0 && (tp->t_cflag&CLOCAL) == 0 &&  
 	    (tp->t_state & TS_CARR_ON) == 0) {
-		if ((error = tsleep((caddr_t)&tp->t_rawq, TTIPRI | PCATCH,
-				    ttopen, 0)) ||
-		    (error = ttclosed(tp)))
+		if (error = ttysleep(tp, (caddr_t)&tp->t_rawq, TTIPRI | PCATCH,
+		    ttopen, 0))
 			goto bad;
 		/*
 		 * a mpclose() might have disabled port. if so restart
