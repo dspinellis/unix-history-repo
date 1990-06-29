587ef1b68550da633b94496b52af441a85ce4ee8
Marc Teitelbaum <marc@ucbvax.Berkeley.EDU>
1990-06-29 07:14:52 -0800

    new interface
    SCCS-vsn: lib/libutil/pty.c 1.2
    SCCS-vsn: lib/libc/gen/daemon.c 5.2

--- usr/src/lib/libc/gen/daemon.c
+++ usr/src/lib/libc/gen/daemon.c
@@ -6,12 +6,13 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)daemon.c	5.1 (Berkeley) %G%";
+static char sccsid[] = "@(#)daemon.c	5.2 (Berkeley) %G%";
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/file.h>
 
-daemon()
+daemon(nochdir, noclose)
+	int nochdir, noclose;
 {
 	int cpid;
 
@@ -19,13 +20,18 @@ daemon()
 		return (-1);
 	if (cpid)
 		exit(0);
-
 	(void) setsid();
-	(void) chdir("/");
-	(void) close(0);
-	(void) close(1);
-	(void) close(2);
-	(void) open("/", O_RDONLY, 0);
-	(void) dup2(0, 1);
-	(void) dup2(0, 2);
+	if (!nochdir)
+		(void) chdir("/");
+	if (!noclose) {
+		int devnull = open("/dev/null", O_RDWR, 0);
+
+		if (devnull != -1) {
+			(void) dup2(devnull, 0);
+			(void) dup2(devnull, 1);
+			(void) dup2(devnull, 2);
+			if (devnull > 2)
+				(void) close(devnull);
+		}
+	}
 }
