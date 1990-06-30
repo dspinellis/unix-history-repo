94b4152790621ca76a2430cdbbcd5dbfb4f8f058
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-06-30 00:04:16 -0800

    check in for Kirk; use getrusage, not times()
    SCCS-vsn: usr.bin/pascal/libpc/CLCK.c 1.4
    SCCS-vsn: usr.bin/pascal/libpc/PCEXIT.c 1.3
    SCCS-vsn: usr.bin/pascal/libpc/SCLCK.c 1.4

--- usr/src/usr.bin/pascal/libpc/PCEXIT.c
+++ usr/src/usr.bin/pascal/libpc/PCEXIT.c
@@ -6,29 +6,28 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)PCEXIT.c	1.2 (Berkeley) %G%";
+static char sccsid[] = "@(#)PCEXIT.c	1.3 (Berkeley) %G%";
 #endif /* not lint */
 
 #include "h00vars.h"
+#include <sys/time.h>
+#include <sys/resource.h>
 
 PCEXIT(code)
 
 	int	code;
 {
-	struct	{
-		long	usr_time;
-		long	sys_time;
-		long	child_usr_time;
-		long	child_sys_time;
-		} tbuf;
 	double l;
+	struct rusage ru;
 
 	PCLOSE(GLVL);
 	PFLUSH();
 	if (_stcnt > 0) {
-		times(&tbuf);
-		l = tbuf.usr_time;
-		l = l / HZ;
+		if (getrusage(RUSAGE_SELF, &ru) < 0)
+			exit(code);
+		l = ru.ru_utime.tv_usec;
+		l /= 1000000;
+		l += ru.ru_utime.tv_sec;
 		fprintf(stderr, "\n%1ld %s %04.2f seconds cpu time.\n",
 				_stcnt, "statements executed in", l);
 	}
