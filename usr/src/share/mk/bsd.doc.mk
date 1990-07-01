a3e8484241cc98298a17698044420a52e222119e
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-06-30 22:13:50 -0800

    first cut at doc include
    SCCS-vsn: share/mk/bsd.doc.mk 5.2

--- usr/src/share/mk/bsd.doc.mk
+++ usr/src/share/mk/bsd.doc.mk
@@ -1,22 +1,37 @@
-
-BINDIR=	/usr/share/doc
-
-MANGRP?=	bin
-MANOWN?=	bin
-MANMODE?=	444
+#	@(#)bsd.doc.mk	5.2 (Berkeley) %G%
 
 PRINTER=lz
-ROFF=	ditroff -P${PRINTER}
-TBL=	dtbl -P${PRINTER} ${PAGES}
 
-print:
-.if defined(DOTBL)
-	${TBL} |
-	${ROFF} ${MACROS} -t ${SRCS} | psdit > paper.${PRINTER}
-.else
-	${ROFF} ${MACROS} -t ${SRCS} | psdit > paper.${PRINTER}
+BIB?=		bib
+EQN?=		/usr/local/deqn -P${PRINTER}
+GREMLIN?=	grn -P${PRINTER}
+GRIND?=		vgrind -f
+INDXBIB?=	indxbib
+PIC?=		pic -P${PRINTER}
+REFER?=		refer
+ROFF?=		psroff -t ${MACROS} ${PAGES} -P${PRINTER}
+SOELIM?=	soelim
+TBL?=		dtbl -P${PRINTER}
+
+.PATH: ${.CURDIR}
+
+.if !target(print)
+print: paper.${PRINTER}
+	lpr -P${PRINTER} paper.${PRINTER}
 .endif
 
+clean cleandir:
+	rm -f paper.* [eE]rrs mklog ${CLEANFILES}
+
+FILES?=	${SRCS}
 install:
-	install -c -o ${MANOWN} -g ${MANGRP} -m ${MANMODE} \
-	    Makefile ${SRCS} ${EXTRA} ${DESTDIR}${BINDIR}/${DIR}
+	install -c -o ${BINOWN} -g ${BINGRP} -m 444 \
+	    Makefile ${FILES} ${EXTRA} ${DESTDIR}${BINDIR}/${DIR}
+
+spell: ${SRCS}
+	spell ${SRCS} | sort | comm -23 - spell.ok > paper.spell
+
+BINDIR?=	/usr/share/doc
+BINGRP?=	bin
+BINOWN?=	bin
+BINMODE?=	444
