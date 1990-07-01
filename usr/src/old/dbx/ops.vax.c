76c86d7396fe3e47dc0ce63cf14e7635b80b350a
Trent Hein <trent@ucbvax.Berkeley.EDU>
1990-06-30 18:50:26 -0800

    changed path to instrs.adb (vax)
    SCCS-vsn: old/dbx/ops.vax.c 5.10

--- usr/src/old/dbx/ops.vax.c
+++ usr/src/old/dbx/ops.vax.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)ops.vax.c	5.9 (Berkeley) %G%";
+static char sccsid[] = "@(#)ops.vax.c	5.10 (Berkeley) %G%";
 #endif /* not lint */
 
 /*
@@ -319,7 +319,7 @@ typedef struct {
 #endif
 
 #ifndef ADBINSTRS
-#define ADBINSTRS "../../bin/adb/adb.vax/instrs.adb"
+#define ADBINSTRS "../../bin/adb/adb.vax/obj/instrs.adb"
 #endif
 
 #define INSTTAB
