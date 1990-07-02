003159b4595be059e22c1116b47f97f897d7f537
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-07-01 21:55:18 -0800

    date and time created 90/07/01 14:55:18 by bostic
    SCCS-vsn: usr.sbin/sliplogin/pathnames.h 5.1

---
+++ usr/src/usr.sbin/sliplogin/pathnames.h
@@ -0,0 +1,12 @@
+/*-
+ * Copyright (c) 1990 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.redist.c%
+ *
+ *	@(#)pathnames.h	5.1 (Berkeley) %G%
+ */
+
+#define	_PATH_ACCESS	"/etc/slip.hosts"
+#define	_PATH_LOGIN	"/etc/slip.login"
+#define	_PATH_LOGOUT	"/etc/slip.logout"
