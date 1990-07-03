41e3c71d935e95464ae2f9fec2af97f461bccd9d
Edward Wang <edward@ucbvax.Berkeley.EDU>
1990-07-03 09:26:24 -0800

    use of null pointer
    SCCS-vsn: usr.bin/mail/send.c 5.22

--- usr/src/usr.bin/mail/send.c
+++ usr/src/usr.bin/mail/send.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)send.c	5.21 (Berkeley) %G%";
+static char sccsid[] = "@(#)send.c	5.22 (Berkeley) %G%";
 #endif /* not lint */
 
 #include "rcv.h"
@@ -42,11 +42,13 @@ send(mp, obuf, doign, prefix)
 	/*
 	 * Compute the prefix string, without trailing whitespace
 	 */
-	cp2 = 0;
-	for (cp = prefix; *cp; cp++)
-		if (*cp != ' ' && *cp != '\t')
-			cp2 = cp;
-	prefixlen = cp2 == 0 ? 0 : cp2 - prefix + 1;
+	if (prefix != NOSTR) {
+		cp2 = 0;
+		for (cp = prefix; *cp; cp++)
+			if (*cp != ' ' && *cp != '\t')
+				cp2 = cp;
+		prefixlen = cp2 == 0 ? 0 : cp2 - prefix + 1;
+	}
 	ibuf = setinput(mp);
 	count = mp->m_size;
 	ishead = 1;
