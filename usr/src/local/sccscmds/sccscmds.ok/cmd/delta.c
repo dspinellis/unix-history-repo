473466bd5fe951145239bcd191f46772e270f543
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-07-03 00:57:04 -0800

    log file moved
    SCCS-vsn: local/sccscmds/sccscmds.ok/cmd/delta.c 4.11

--- usr/src/local/sccscmds/sccscmds.ok/cmd/delta.c
+++ usr/src/local/sccscmds/sccscmds.ok/cmd/delta.c
@@ -1,11 +1,12 @@
 # include	"../hdr/defines.h"
 # include	"../hdr/had.h"
+# include	"pathnames.h"
 
-static char Sccsid[] = "@(#)delta.c	4.10	%G%";
+static char Sccsid[] = "@(#)delta.c	4.11	%G%";
 USXALLOC();
 
 # ifdef LOGDELTA
-char	LogFile[] = "/usr/adm/sccs-log";
+char	*LogFile = _PATH_SCCSLOG;
 FILE	*Logf;
 # endif
 
