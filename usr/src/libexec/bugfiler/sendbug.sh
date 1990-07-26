690e31be6828f89d47857c7353d3fcf927e70b1e
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-07-26 04:51:28 -0800

    new copyright notice
    SCCS-vsn: libexec/bugfiler/sendbug.sh 5.10

--- usr/src/libexec/bugfiler/sendbug.sh
+++ usr/src/libexec/bugfiler/sendbug.sh
@@ -1,23 +1,14 @@
 #!/bin/sh -
 #
-# Copyright (c) 1983 Regents of the University of California.
+# Copyright (c) 1983 The Regents of the University of California.
 # All rights reserved.
 #
-# Redistribution and use in source and binary forms are permitted
-# provided that the above copyright notice and this paragraph are
-# duplicated in all such forms and that any documentation,
-# advertising materials, and other materials related to such
-# distribution and use acknowledge that the software was developed
-# by the University of California, Berkeley.  The name of the
-# University may not be used to endorse or promote products derived
-# from this software without specific prior written permission.
-# THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
-# IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
-# WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+# %sccs.include.redist.sh%
 #
-#	@(#)sendbug.sh	5.9 (Berkeley) %G%
+#	@(#)sendbug.sh	5.10 (Berkeley) %G%
 #
-# Create a bug report and mail to '4bsd-bugs'.
+
+# create a bug report and mail it to '4bsd-bugs'.
 
 PATH=/bin:/sbin:/usr/sbin:/usr/bin
 export PATH
