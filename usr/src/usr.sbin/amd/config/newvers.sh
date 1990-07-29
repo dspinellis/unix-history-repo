3b36631ad392ba60b2176a0e7691cd398885c0e7
Jan-Simon Pendry <pendry@ucbvax.Berkeley.EDU>
1990-07-29 14:47:12 -0800

    Works with new make
    SCCS-vsn: usr.sbin/amd/config/newvers.sh 5.2

--- usr/src/usr.sbin/amd/config/newvers.sh
+++ usr/src/usr.sbin/amd/config/newvers.sh
@@ -12,7 +12,7 @@
 #
 # %sccs.include.redist.sh%
 #
-#	@(#)newvers.sh	5.1 (Berkeley) %G%
+#	@(#)newvers.sh	5.2 (Berkeley) %G%
 #
 PATH=/usr/ucb:/bin:/usr/bin
 if [ $# -ne 1 ]; then echo "Usage: newvers program" >&2; exit 1; fi
@@ -23,8 +23,8 @@ u=${USER-${LOGNAME-root}}
 h=`hostname`
 #h=`expr "$h" : '\([^.]*\)'`
 t=`date`
-r=`cat ../config/RELEASE`
-c=`sed 's/$/\\\\n\\\\/' ../text/COPYRIGHT`
+r=`cat $d../config/RELEASE`
+c=`sed 's/$/\\\\n\\\\/' $d../text/COPYRIGHT`
 if [ -z "$r" -o -z "$c" ]; then
 	echo ERROR: config file missing >&2
 	exit 1
