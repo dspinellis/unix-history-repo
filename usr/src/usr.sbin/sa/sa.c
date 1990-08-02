12afa0108765fbb5682b6ae3e3a7da23de0c6fc6
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-08-02 01:00:45 -0800

    fix bogus scaling (from ers@osf.org)
    SCCS-vsn: usr.sbin/sa/sa.c 4.13

--- usr/src/usr.sbin/sa/sa.c
+++ usr/src/usr.sbin/sa/sa.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *sccsid = "@(#)sa.c	4.12 (Berkeley) %G%";
+static char *sccsid = "@(#)sa.c	4.13 (Berkeley) %G%";
 #endif
 
 /*
@@ -276,7 +276,7 @@ int	(*cmp)();
 
 /* we assume pagesize is at least 1k */
 int	pgdiv;
-#define	pgtok(x)	((x) / pgdiv)
+#define	pgtok(x)	((x) * pgdiv)
 
 extern	tcmp(), ncmp(), bflgcmp(), dcmp(), Dcmp(), kcmp(), Kcmp();
 extern	double sum();
