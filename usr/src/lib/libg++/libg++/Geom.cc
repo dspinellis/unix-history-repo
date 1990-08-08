b0df42c497e4967ec132e9481eaa265c696bebb1
William F. Jolitz <wjolitz@soda.berkeley.edu>
1990-08-08 06:39:01 -0800

    386BSD 0.1 development Work on file usr/src/lib/libg++/libg++/DiscUnif.cc Work on file usr/src/lib/libg++/libg++/LogNorm.cc Work on file usr/src/lib/libg++/libg++/RndInt.cc Work on file usr/src/lib/libg++/libg++/HypGeom.cc Work on file usr/src/lib/libg++/libg++/NegExp.cc Work on file usr/src/lib/libg++/libg++/Geom.cc Work on file usr/src/lib/libg++/libg++/SmplStat.cc Work on file usr/src/lib/libg++/libg++/SmplHist.cc
    Co-Authored-By: Lynne Greer Jolitz <ljolitz@cardio.ucsf.edu>
    Synthesized-from: 386BSD-0.1

---
+++ usr/src/lib/libg++/libg++/Geom.cc
@@ -0,0 +1,36 @@
+/* 
+Copyright (C) 1988 Free Software Foundation
+    written by Dirk Grunwald (grunwald@cs.uiuc.edu)
+
+This file is part of GNU CC.
+
+GNU CC is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY.  No author or distributor
+accepts responsibility to anyone for the consequences of using it
+or for whether it serves any particular purpose or works at all,
+unless he says so in writing.  Refer to the GNU CC General Public
+License for full details.
+
+Everyone is granted permission to copy, modify and redistribute
+GNU CC, but only under the conditions described in the
+GNU CC General Public License.   A copy of this license is
+supposed to have been given to you along with GNU CC so you
+can know your rights and responsibilities.  It should be in a
+file named COPYING.  Among other things, the copyright notice
+and this notice must be preserved on all copies.  
+*/
+#ifdef __GNUG__
+#pragma implementation
+#endif
+#include <builtin.h>
+#include <Random.h>
+
+#include <Geom.h>
+
+double Geometric::operator()()
+{
+    int samples;
+    for (samples = 1; pGenerator -> asDouble() < pMean; samples++);
+    return((double) samples);
+}
+
