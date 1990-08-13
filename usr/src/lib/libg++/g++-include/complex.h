ebcb923828a000b3d006bda8b182b15f8c7a6368
William F. Jolitz <wjolitz@soda.berkeley.edu>
1990-08-13 11:12:27 -0800

    386BSD 0.0 development Work on file usr/src/lib/libg++/g++-include/strclass.h Work on file usr/src/lib/libg++/g++-include/complex.h
    Co-Authored-By: Lynne Greer Jolitz <ljolitz@cardio.ucsf.edu>
    Synthesized-from: 386BSD-0.0/src

---
+++ usr/src/lib/libg++/g++-include/complex.h
@@ -0,0 +1,9 @@
+#ifndef _complex_h
+#ifdef __GNUG__
+#pragma once
+#endif
+#define _complex_h
+#define __ATT_complex__
+#include <Complex.h>
+typedef class Complex complex;
+#endif
---
+++ usr/src/lib/libg++/g++-include/strclass.h
@@ -0,0 +1,8 @@
+#ifndef _strclass_h
+#ifdef __GNUG__
+#pragma once
+#endif
+#define _strclass_h
+#include <String.h>
+typedef class String string;
+#endif
