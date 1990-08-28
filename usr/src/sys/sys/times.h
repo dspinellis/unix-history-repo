4cc5e17524b624a89bc549a405f533ef7bbcb1d0
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-08-28 00:44:33 -0800

    bring it back from the dead; POSIX specified it
    SCCS-vsn: lib/libc/gen/times.c 5.3
    SCCS-vsn: sys/sys/times.h 7.2

--- usr/src/sys/sys/times.h
+++ usr/src/sys/sys/times.h
@@ -1,17 +1,28 @@
-/*
- * Copyright (c) 1982, 1986 Regents of the University of California.
- * All rights reserved.  The Berkeley software License Agreement
- * specifies the terms and conditions for redistribution.
+/*-
+ * Copyright (c) 1990 The Regents of the University of California.
+ * All rights reserved.
  *
- *	@(#)times.h	7.1 (Berkeley) %G%
+ * %sccs.include.redist.c%
+ *
+ *	@(#)times.h	7.2 (Berkeley) %G%
  */
 
-/*
- * Structure returned by times()
- */
+#include <machine/machtypes.h>
+
+#ifdef	_CLOCK_T_
+typedef	_CLOCK_T_	clock_t;
+#undef	_CLOCK_T_
+#endif
+
 struct tms {
-	time_t	tms_utime;		/* user time */
-	time_t	tms_stime;		/* system time */
-	time_t	tms_cutime;		/* user time, children */
-	time_t	tms_cstime;		/* system time, children */
+	clock_t tms_utime;	/* User CPU time */
+	clock_t tms_stime;	/* System CPU time */
+	clock_t tms_cutime;	/* User CPU time of terminated child procs */
+	clock_t tms_cstime;	/* System CPU time of terminated child procs */
 };
+
+#if __STDC__ || c_plusplus
+clock_t times(struct tms *);
+#else
+clock_t times();
+#endif
