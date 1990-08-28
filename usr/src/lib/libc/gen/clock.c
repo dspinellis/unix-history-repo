b2c8fb18211beae5586dc88e5c26d27436b3ec73
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-08-28 03:31:28 -0800

    rewrite to look like times()
    SCCS-vsn: lib/libc/gen/clock.c 5.4

--- usr/src/lib/libc/gen/clock.c
+++ usr/src/lib/libc/gen/clock.c
@@ -6,28 +6,25 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)clock.c	5.3 (Berkeley) %G%";
+static char sccsid[] = "@(#)clock.c	5.4 (Berkeley) %G%";
 #endif /* LIBC_SCCS and not lint */
 
-#include <machine/machlimits.h>
-#include <sys/types.h>
+#include <sys/param.h>
 #include <sys/time.h>
 #include <sys/resource.h>
+
+/*
+ * Convert usec to clock ticks; could do (usec * CLK_TCK) / 1000000,
+ * but this would overflow if we switch to nanosec.
+ */
+#define	CONVTCK(r)	(r.tv_sec * CLK_TCK + r.tv_usec / (1000000 / CLK_TCK))
 
 clock_t
 clock()
 {
-	struct rusage rusage;
-	clock_t val;
+	struct rusage ru;
 
-	if (getrusage(RUSAGE_SELF, &rusage))
+	if (getrusage(RUSAGE_SELF, &ru))
 		return ((clock_t) -1);
-	val = (rusage.ru_utime.tv_sec + rusage.ru_stime.tv_sec) * CLK_TCK;
-	/*
-	 * Convert usec to clock ticks; could do (usec * CLK_TCK) / 1000000,
-	 * but this would overflow if we switch to nanosec.
-	 */
-	val += (rusage.ru_utime.tv_usec + rusage.ru_stime.tv_usec) /
-		(1000000 / CLK_TCK);
-	return (val);
+	return((clock_t)((CONVTCK(ru.ru_utime) + CONVTCK(ru.ru_stime))));
 }
