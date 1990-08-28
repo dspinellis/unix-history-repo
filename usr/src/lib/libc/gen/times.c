d08b641a10d7b1aaaed03eebac463a3f134cffa1
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-08-28 00:44:33 -0800

    bring it back from the dead; POSIX specified it
    SCCS-vsn: lib/libc/gen/times.c 5.3
    SCCS-vsn: sys/sys/times.h 7.2

--- usr/src/lib/libc/gen/times.c
+++ usr/src/lib/libc/gen/times.c
@@ -1,46 +1,39 @@
-/*
- * Copyright (c) 1980 Regents of the University of California.
- * All rights reserved.  The Berkeley software License Agreement
- * specifies the terms and conditions for redistribution.
+/*-
+ * Copyright (c) 1990 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.redist.c%
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)times.c	5.2 (Berkeley) %G%";
-#endif LIBC_SCCS and not lint
+static char sccsid[] = "@(#)times.c	5.3 (Berkeley) %G%";
+#endif /* LIBC_SCCS and not lint */
 
 #include <sys/time.h>
+#include <sys/times.h>
 #include <sys/resource.h>
 
-/*
- * Backwards compatible times.
- */
-struct tms {
-	int	tms_utime;		/* user time */
-	int	tms_stime;		/* system time */
-	int	tms_cutime;		/* user time, children */
-	int	tms_cstime;		/* system time, children */
-};
-
+clock_t
 times(tmsp)
 	register struct tms *tmsp;
 {
 	struct rusage ru;
+	clock_t scale60();
 
 	if (getrusage(RUSAGE_SELF, &ru) < 0)
-		return (-1);
+		return ((clock_t)-1);
 	tmsp->tms_utime = scale60(&ru.ru_utime);
 	tmsp->tms_stime = scale60(&ru.ru_stime);
 	if (getrusage(RUSAGE_CHILDREN, &ru) < 0)
-		return (-1);
+		return ((clock_t)-1);
 	tmsp->tms_cutime = scale60(&ru.ru_utime);
 	tmsp->tms_cstime = scale60(&ru.ru_stime);
-	return (0);
+	return ((clock_t)0);
 }
 
-static
+static clock_t
 scale60(tvp)
 	register struct timeval *tvp;
 {
-
 	return (tvp->tv_sec * 60 + tvp->tv_usec / 16667);
 }
