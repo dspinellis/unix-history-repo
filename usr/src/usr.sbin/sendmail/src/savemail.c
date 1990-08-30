967c6729a2221e00cc6acaa29841da6965fd4953
Eric Allman <eric@ucbvax.Berkeley.EDU>
1990-08-29 21:39:55 -0800

    from Eric Wassenaar at NIKHEF-H, Amsterdam: reset EF_OLDSTYLE bit when returning mail
    SCCS-vsn: usr.sbin/sendmail/src/savemail.c 5.14
    SCCS-vsn: usr.sbin/sendmail/src/version.c 5.65

--- usr/src/usr.sbin/sendmail/src/savemail.c
+++ usr/src/usr.sbin/sendmail/src/savemail.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)savemail.c	5.13 (Berkeley) %G%";
+static char sccsid[] = "@(#)savemail.c	5.14 (Berkeley) %G%";
 #endif /* not lint */
 
 # include <sys/types.h>
