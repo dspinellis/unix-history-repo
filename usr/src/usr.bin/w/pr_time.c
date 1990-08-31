d8180f85feacee6913c3ac83278dd085b70bf406
Marc Teitelbaum <marc@ucbvax.Berkeley.EDU>
1990-08-30 18:55:10 -0800

    fixes from chris
    SCCS-vsn: bin/ps/ps.c 5.29
    SCCS-vsn: bin/ps/ps.1 6.8
    SCCS-vsn: lib/libc/gen/devname.c 5.7
    SCCS-vsn: usr.bin/w/pr_time.c 5.2

--- usr/src/usr.bin/w/pr_time.c
+++ usr/src/usr.bin/w/pr_time.c
@@ -6,12 +6,13 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)pr_time.c	5.1 (Berkeley) %G%";
+static char sccsid[] = "@(#)pr_time.c	5.2 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/types.h>
 #include <sys/time.h>
 #include <stdio.h>
+#include <string.h>
 
 #define	HR	(60 * 60)
 #define	DAY	(24 * HR)
@@ -28,17 +29,17 @@ prttime(tim, tail)
 	time_t tim;
 	char *tail;
 {
-	static char timebuf[32], *cp;
+	int mins;
+	static char timebuf[32];
 
 	if (tim >= 60) {
-		int mins = tim % 60;
-		sprintf(timebuf, "%2d:%02d", tim/60, mins);
+		mins = tim % 60;
+		(void) sprintf(timebuf, "%2d:%02d%s", (int)(tim / 60), mins,
+		    tail);
 	} else if (tim >= 0)
-		sprintf(timebuf, "    %2d", tim);
-	cp = timebuf;
-	while (*cp)
-		cp++;
-	sprintf(cp, "%s", tail);
+		(void) sprintf(timebuf, "    %2d%s", (int)tim, tail);
+	else
+		(void) strcpy(timebuf, tail);
 
 	return (timebuf);
 }
@@ -50,14 +51,14 @@ static char *month[] = { "Jan", "Feb", "Mar", "Apr", "May", "Jun",
 /* prtat prints a 12 hour time given a pointer to a time of day */
 char *
 attime(started)
-	long *started;
+	time_t *started;
 {
 	struct tm *p;
 	register int hr, pm;
 	static char prbuff[64];
 
 	if (now == 0)
-		time(&now);
+		(void) time(&now);
 	p = localtime(started);
 	hr = p->tm_hour;
 	pm = (hr > 11);
@@ -66,13 +67,13 @@ attime(started)
 	if (hr == 0)
 		hr = 12;
 	if (now - *started <= 18 * HR)
-		return (prttime(hr * 60 + p->tm_min, pm ? "pm" : "am"));
-	else if (now - *started <= 7 * DAY)
-		sprintf(prbuff, "%*s%d%s", hr < 10 ? 4 : 3,
+		return (prttime((time_t)hr * 60 + p->tm_min, pm ? "pm" : "am"));
+	if (now - *started <= 7 * DAY)
+		(void) sprintf(prbuff, "%*s%d%s", hr < 10 ? 4 : 3,
 			weekday[p->tm_wday], hr, pm ? "pm" : "am");
 	else
-		sprintf(prbuff, "%2d%s%2d", p->tm_mday, month[p->tm_mon], 
-			p->tm_year);
+		(void) sprintf(prbuff, "%2d%s%2d", p->tm_mday,
+			month[p->tm_mon], p->tm_year);
 
 	return (prbuff);
 }
