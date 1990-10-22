06d15241b50c594fa0650837af8886405a37da53
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-10-22 02:17:36 -0800

    use ifdef's that unifdef understands
    SCCS-vsn: usr.bin/rlogin/rlogin.c 5.32

--- usr/src/usr.bin/rlogin/rlogin.c
+++ usr/src/usr.bin/rlogin/rlogin.c
@@ -12,7 +12,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)rlogin.c	5.31 (Berkeley) %G%";
+static char sccsid[] = "@(#)rlogin.c	5.32 (Berkeley) %G%";
 #endif /* not lint */
 
 /*
@@ -136,11 +136,13 @@ main(argc, argv)
 		case 'l':
 			user = optarg;
 			break;
-#if defined(KERBEROS) && defined(CRYPT)
+#ifdef CRYPT
+#ifdef KERBEROS
 		case 'x':
 			encrypt = 1;
 			des_set_key(cred.session, schedule);
 			break;
+#endif
 #endif
 		case '?':
 		default:
@@ -368,21 +370,25 @@ writer()
 				continue;
 			}
 			if (c != escapechar)
-#if defined(KERBEROS) && defined(CRYPT)
+#ifdef CRYPT
+#ifdef KERBEROS
 				if (encrypt)
 					(void)des_write(rem, &escapechar, 1);
 				else
 #endif
+#endif
 					(void)write(rem, &escapechar, 1);
 		}
 
-#if defined(KERBEROS) && defined(CRYPT)
+#ifdef CRYPT
+#ifdef KERBEROS
 		if (encrypt) {
 			if (des_write(rem, &c, 1) == 0) {
 				msg("line gone");
 				break;
 			}
 		} else
+#endif
 #endif
 			if (write(rem, &c, 1) == 0) {
 				msg("line gone");
@@ -550,10 +556,12 @@ reader(omask)
 		rcvcnt = 0;
 		rcvstate = READING;
 
-#if defined(KERBEROS) && defined(CRYPT)
+#ifdef CRYPT
+#ifdef KERBEROS
 		if (encrypt)
 			rcvcnt = des_read(rem, rcvbuf, sizeof(rcvbuf));
 		else
+#endif
 #endif
 			rcvcnt = read(rem, rcvbuf, sizeof (rcvbuf));
 		if (rcvcnt == 0)
