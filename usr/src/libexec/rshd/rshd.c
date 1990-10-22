3ae6cb9dbfc2b010235d9cabf4a53a7bf1118f2d
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-10-22 04:25:18 -0800

    use ifdef's that unifdef can understand
    SCCS-vsn: libexec/rshd/rshd.c 5.36

--- usr/src/libexec/rshd/rshd.c
+++ usr/src/libexec/rshd/rshd.c
@@ -12,7 +12,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)rshd.c	5.35 (Berkeley) %G%";
+static char sccsid[] = "@(#)rshd.c	5.36 (Berkeley) %G%";
 #endif /* not lint */
 
 /* From:
@@ -430,7 +430,8 @@ fail:
 			error("Can't make pipe.\n");
 			exit(1);
 		}
-#if defined(KERBEROS) && defined(CRYPT)
+#ifdef CRYPT
+#ifdef KERBEROS
 		if (encrypt) {
 			if (pipe(pv1) < 0) {
 				error("Can't make 2nd pipe.\n");
@@ -442,13 +443,15 @@ fail:
 			}
 		}
 #endif
+#endif
 		pid = fork();
 		if (pid == -1)  {
 			error("Can't fork; try again.\n");
 			exit(1);
 		}
 		if (pid) {
-#if defined(KERBEROS) && defined(CRYPT)
+#ifdef CRYPT
+#ifdef KERBEROS
 			if (encrypt) {
 				static char msg[] = SECURE_MESSAGE;
 				(void) close(pv1[1]);
@@ -456,6 +459,7 @@ fail:
 				des_write(s, msg, sizeof(msg));
 
 			} else
+#endif
 #endif
 			{
 				(void) close(0); (void) close(1);
@@ -469,7 +473,8 @@ fail:
 				nfd = pv[0];
 			else
 				nfd = s;
-#if defined(KERBEROS) && defined(CRYPT)
+#ifdef CRYPT
+#ifdef KERBEROS
 			if (encrypt) {
 				FD_ZERO(&writeto);
 				FD_SET(pv2[0], &writeto);
@@ -479,13 +484,15 @@ fail:
 				nfd = MAX(nfd, pv1[0]);
 			} else
 #endif
+#endif
 				ioctl(pv[0], FIONBIO, (char *)&one);
 
 			/* should set s nbio! */
 			nfd++;
 			do {
 				ready = readfrom;
-#if defined(KERBEROS) && defined(CRYPT)
+#ifdef CRYPT
+#ifdef KERBEROS
 				if (encrypt) {
 					wready = writeto;
 					if (select(nfd, &ready,
@@ -494,15 +501,18 @@ fail:
 						break;
 				} else
 #endif
+#endif
 					if (select(nfd, &ready, (fd_set *)0,
 					  (fd_set *)0, (struct timeval *)0) < 0)
 						break;
 				if (FD_ISSET(s, &ready)) {
 					int	ret;
-#if defined(KERBEROS) && defined(CRYPT)
+#ifdef CRYPT
+#ifdef KERBEROS
 					if (encrypt)
 						ret = des_read(s, &sig, 1);
 					else
+#endif
 #endif
 						ret = read(s, &sig, 1);
 					if (ret <= 0)
@@ -517,17 +527,20 @@ fail:
 						shutdown(s, 1+1);
 						FD_CLR(pv[0], &readfrom);
 					} else {
-#if defined(KERBEROS) && defined(CRYPT)
+#ifdef CRYPT
+#ifdef KERBEROS
 						if (encrypt)
 							(void)
 							  des_write(s, buf, cc);
 						else
 #endif
+#endif
 							(void)
 							  write(s, buf, cc);
 					}
 				}
-#if defined(KERBEROS) && defined(CRYPT)
+#ifdef CRYPT
+#ifdef KERBEROS
 				if (encrypt && FD_ISSET(pv1[0], &ready)) {
 					errno = 0;
 					cc = read(pv1[0], buf, sizeof(buf));
@@ -548,17 +561,21 @@ fail:
 						(void) write(pv2[0], buf, cc);
 				}
 #endif
+#endif
 
 			} while (FD_ISSET(s, &readfrom) ||
-#if defined(KERBEROS) && defined(CRYPT)
+#ifdef CRYPT
+#ifdef KERBEROS
 			    (encrypt && FD_ISSET(pv1[0], &readfrom)) ||
+#endif
 #endif
 			    FD_ISSET(pv[0], &readfrom));
 			exit(0);
 		}
 		setpgrp(0, getpid());
 		(void) close(s); (void) close(pv[0]);
-#if defined(KERBEROS) && defined(CRYPT)
+#ifdef CRYPT
+#ifdef KERBEROS
 		if (encrypt) {
 			close(pv1[0]); close(pv2[0]);
 			dup2(pv1[1], 1);
@@ -566,6 +583,7 @@ fail:
 			close(pv1[1]);
 			close(pv2[1]);
 		}
+#endif
 #endif
 		dup2(pv[1], 2);
 		close(pv[1]);
