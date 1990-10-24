9272b3b00df06daefcfde180dbf3bed8ec49f71d
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-10-24 07:16:14 -0800

    add boolean type and defined values (for new VM)
    SCCS-vsn: sys/sys/types.h 7.12

--- usr/src/sys/sys/types.h
+++ usr/src/sys/sys/types.h
@@ -3,7 +3,7 @@
  * All rights reserved.  The Berkeley software License Agreement
  * specifies the terms and conditions for redistribution.
  *
- *	@(#)types.h	7.11 (Berkeley) %G%
+ *	@(#)types.h	7.12 (Berkeley) %G%
  */
 
 #ifndef _TYPES_
