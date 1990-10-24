770a31ffb2ada23d1faadd0ed578cc361b12bac2
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1990-10-23 18:30:43 -0800

    need to include types.h for function prototypes
    SCCS-vsn: sys/sys/fcntl.h 5.6

--- usr/src/sys/sys/fcntl.h
+++ usr/src/sys/sys/fcntl.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)fcntl.h	5.5 (Berkeley) %G%
+ *	@(#)fcntl.h	5.6 (Berkeley) %G%
  */
 
 #ifndef F_DUPFD
@@ -78,6 +78,11 @@
 #define	O_ACCMODE	(O_RDONLY|O_WRONLY|O_RDWR)
 
 #if __STDC__ || c_plusplus
+#ifdef KERNEL
+#include "types.h"
+#else
+#include <sys/types.h>
+#endif
 extern int fcntl(int, int, int);
 extern int creat(const char *, mode_t);
 extern int open(const char *, int, ...);
