268fc3f6766f6c3b03492c0cca9e58541d356c3d
Keith Sklower <sklower@ucbvax.Berkeley.EDU>
1990-10-31 21:56:05 -0800

    looutput is called with four args for now.
    SCCS-vsn: sys/netinet/if_ether.c 7.13

--- usr/src/sys/netinet/if_ether.c
+++ usr/src/sys/netinet/if_ether.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)if_ether.c	7.12 (Berkeley) %G%
+ *	@(#)if_ether.c	7.13 (Berkeley) %G%
  */
 
 /*
@@ -184,7 +184,7 @@ arpresolve(ac, m, destip, desten, usetrailers)
 		if (useloopback) {
 			sin.sin_family = AF_INET;
 			sin.sin_addr = *destip;
-			(void) looutput(&loif, m, (struct sockaddr *)&sin);
+			(void) looutput(&loif, m, (struct sockaddr *)&sin, 0);
 			/*
 			 * The packet has already been sent and freed.
 			 */
