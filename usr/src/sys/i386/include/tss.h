7494fe969e7d2eb4352adcbd1c411752d651f735
Bill Joy <bill@ucbvax.Berkeley.EDU>
1990-11-15 00:49:34 -0800

    correct types (*sigh*)
    SCCS-vsn: sys/i386/include/tss.h 5.2

--- usr/src/sys/i386/include/tss.h
+++ usr/src/sys/i386/include/tss.h
@@ -7,7 +7,7 @@
  *
  * %sccs.include.noredist.c%
  *
- *	@(#)tss.h	5.1 (Berkeley) %G%
+ *	@(#)tss.h	5.2 (Berkeley) %G%
  */
 
 /*
@@ -16,37 +16,37 @@
 
 
 struct i386tss {
-	int	tss_link;	/* actually 16 bits: top 16 bits must be zero */
-	int	tss_esp0; 	/* kernel stack pointer priviledge level 0 */
+	long	tss_link;	/* actually 16 bits: top 16 bits must be zero */
+	long	tss_esp0; 	/* kernel stack pointer priviledge level 0 */
 #define	tss_ksp	tss_esp0
-	int	tss_ss0;	/* actually 16 bits: top 16 bits must be zero */
-	int	tss_esp1; 	/* kernel stack pointer priviledge level 1 */
-	int	tss_ss1;	/* actually 16 bits: top 16 bits must be zero */
-	int	tss_esp2; 	/* kernel stack pointer priviledge level 2 */
-	int	tss_ss2;	/* actually 16 bits: top 16 bits must be zero */
-	struct  ptd *tss_cr3; 	/* page table directory */
+	long	tss_ss0;	/* actually 16 bits: top 16 bits must be zero */
+	long	tss_esp1; 	/* kernel stack pointer priviledge level 1 */
+	long	tss_ss1;	/* actually 16 bits: top 16 bits must be zero */
+	long	tss_esp2; 	/* kernel stack pointer priviledge level 2 */
+	long	tss_ss2;	/* actually 16 bits: top 16 bits must be zero */
+	long	tss_cr3; 	/* page table directory physical address */
 #define	tss_ptd	tss_cr3
-	int	tss_eip; 	/* program counter */
+	long	tss_eip; 	/* program counter */
 #define	tss_pc	tss_eip
-	int	tss_eflags; 	/* program status longword */
+	long	tss_eflags; 	/* program status longword */
 #define	tss_psl	tss_eflags
-	int	tss_eax; 
-	int	tss_ecx; 
-	int	tss_edx; 
-	int	tss_ebx; 
-	int	tss_esp; 	/* user stack pointer */
+	long	tss_eax; 
+	long	tss_ecx; 
+	long	tss_edx; 
+	long	tss_ebx; 
+	long	tss_esp; 	/* user stack pointer */
 #define	tss_usp	tss_esp
-	int	tss_ebp; 	/* user frame pointer */
+	long	tss_ebp; 	/* user frame pointer */
 #define	tss_fp	tss_ebp
-	int	tss_esi; 
-	int	tss_edi; 
-	int	tss_es;		/* actually 16 bits: top 16 bits must be zero */
-	int	tss_cs;		/* actually 16 bits: top 16 bits must be zero */
-	int	tss_ss;		/* actually 16 bits: top 16 bits must be zero */
-	int	tss_ds;		/* actually 16 bits: top 16 bits must be zero */
-	int	tss_fs;		/* actually 16 bits: top 16 bits must be zero */
-	int	tss_gs;		/* actually 16 bits: top 16 bits must be zero */
-	int	tss_ldt;	/* actually 16 bits: top 16 bits must be zero */
-	int	tss_ioopt;	/* options & io offset bitmap: currently zero */
+	long	tss_esi; 
+	long	tss_edi; 
+	long	tss_es;		/* actually 16 bits: top 16 bits must be zero */
+	long	tss_cs;		/* actually 16 bits: top 16 bits must be zero */
+	long	tss_ss;		/* actually 16 bits: top 16 bits must be zero */
+	long	tss_ds;		/* actually 16 bits: top 16 bits must be zero */
+	long	tss_fs;		/* actually 16 bits: top 16 bits must be zero */
+	long	tss_gs;		/* actually 16 bits: top 16 bits must be zero */
+	long	tss_ldt;	/* actually 16 bits: top 16 bits must be zero */
+	long	tss_ioopt;	/* options & io offset bitmap: currently zero */
 				/* XXX unimplemented .. i/o permission bitmap */
 };
