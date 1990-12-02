e7ee99b33e65c1022f880f204a83942e621db48e
Mike Karels <karels@ucbvax.Berkeley.EDU>
1990-12-01 22:22:38 -0800

    tty stop signals shouldn't stop sleeping procs in orphaned pgrps
    SCCS-vsn: sys/kern/kern_sig.c 7.24

--- usr/src/sys/kern/kern_sig.c
+++ usr/src/sys/kern/kern_sig.c
@@ -3,7 +3,7 @@
  * All rights reserved.  The Berkeley software License Agreement
  * specifies the terms and conditions for redistribution.
  *
- *	@(#)kern_sig.c	7.23 (Berkeley) %G%
+ *	@(#)kern_sig.c	7.24 (Berkeley) %G%
  */
 
 #include "param.h"
@@ -580,6 +580,16 @@ psignal(p, sig)
 	case SIGTSTP:
 	case SIGTTIN:
 	case SIGTTOU:
+		/*
+		 * If sending a tty stop signal to a member of an orphaned
+		 * process group, discard the signal here if the action
+		 * is default; don't stop the process below if sleeping,
+		 * and don't clear any pending SIGCONT.
+		 */
+		if (p->p_pgrp->pg_jobc == 0 && action == SIG_DFL)
+			return;
+		/* FALLTHROUGH */
+
 	case SIGSTOP:
 		p->p_sig &= ~sigmask(SIGCONT);
 		break;
