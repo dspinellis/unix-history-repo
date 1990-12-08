10d22e7bf466f8383231fa2f75c5fc63285f2f6a
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1990-12-07 23:19:38 -0800

    add Berkeley specific copyright
    SCCS-vsn: sys/conf/newvers.sh 7.4

--- usr/src/sys/conf/newvers.sh
+++ usr/src/sys/conf/newvers.sh
@@ -1,15 +1,18 @@
 #!/bin/sh -
 #
-# Copyright (c) 1980, 1986 Regents of the University of California.
-# All rights reserved.  The Berkeley software License Agreement
-# specifies the terms and conditions for redistribution.
+# Copyright (c) 1984, 1986, 1990 The Regents of the University of California.
+# All rights reserved.
 #
-#	@(#)newvers.sh	7.3 (Berkeley) %G%
+# %sccs.include.redist.sh%
 #
+#	@(#)newvers.sh	7.4 (Berkeley) %G%
+#
+
 if [ ! -r version ]
 then
 	echo 0 > version
 fi
+
 touch version
 v=`cat version` u=${USER-root} d=`pwd` h=`hostname` t=`date`
 ( echo "char sccs[] = \"@(#)4.3 BSD Reno #${v}: ${t} (${u}@${h}:${d})\\n\";" ;
