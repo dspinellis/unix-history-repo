f5d40d1913b06156f0eb3969cd05c91458dc8cfe
William Nesheim <william@ucbvax.Berkeley.EDU>
1990-12-17 22:54:55 -0800

    386 uses gas and gcc's cpp just like hp300
    SCCS-vsn: share/mk/sys.mk 5.10

--- usr/src/share/mk/sys.mk
+++ usr/src/share/mk/sys.mk
@@ -1,4 +1,4 @@
-#	@(#)sys.mk	5.9 (Berkeley) %G%
+#	@(#)sys.mk	5.10 (Berkeley) %G%
 
 unix=		We run UNIX.
 
@@ -10,7 +10,7 @@ AR=		ar
 ARFLAGS=	rl
 RANLIB=		ranlib
 
-.if (${MACHINE} == "hp300")
+.if (${MACHINE} == "hp300" || ${MACHINE} == "i386")
 AS=		/usr/local/lib/gcc-as
 .else
 AS=		as
@@ -21,7 +21,7 @@ AFLAGS=
 CC=		cc
 CFLAGS=		-O
 
-.if (${MACHINE} == "hp300")
+.if (${MACHINE} == "hp300" || ${MACHINE} == "i386")
 CPP=		/usr/local/lib/gcc-cpp -traditional
 .else
 CPP=		cpp
