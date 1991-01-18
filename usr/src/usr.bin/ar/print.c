8e37704a5ca5a5399fa51e7ef19c53ff9f1deccc
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-01-18 02:24:01 -0800

    date and time created 91/01/17 18:24:01 by bostic
    SCCS-vsn: usr.bin/ar/print.c 5.1

---
+++ usr/src/usr.bin/ar/print.c
@@ -0,0 +1,56 @@
+/*-
+ * Copyright (c) 1990 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to Berkeley by
+ * Hugh Smith at The University of Guelph.
+ *
+ * %sccs.include.redist.c%
+ */
+
+#ifndef lint
+static char sccsid[] = "@(#)print.c	5.1 (Berkeley) %G%";
+#endif /* not lint */
+
+#include <sys/param.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <stdio.h>
+#include "archive.h"
+
+extern CHDR chdr;			/* converted header */
+extern char *archive;			/* archive name */
+
+/*
+ * print --
+ *	Prints archive members on stdout - if member names given only
+ *	print those members, otherwise print all members.
+ */
+print(argv)
+	char **argv;
+{
+	CF cf;
+	register int afd, all;
+	int eval;
+
+	afd = open_archive(O_RDONLY);
+
+	SETCF(afd, archive, STDOUT_FILENO, "stdout", RPAD);
+	for (all = !*argv; get_header(afd);) {
+		if (!all && !files(argv)) {
+			SKIP(afd, chdr.size, archive);
+			continue;
+		}
+		if (options & AR_V) {
+			(void)printf("\n<%s>\n\n", chdr.name);
+			(void)fflush(stdout);
+		}
+		copyfile(&cf, chdr.size);
+		if (!all && !*argv)
+			break;
+	}
+	ORPHANS;
+	close_archive(afd);
+	return(eval);
+}
