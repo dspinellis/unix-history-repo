e7331233dece5c4d8ea5b0a8018135ee1ae9fefc
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-01-18 02:23:04 -0800

    new version
    SCCS-vsn: usr.bin/ar/ar.c 5.6
    SCCS-vsn: include/ar.h 5.2

--- usr/src/include/ar.h
+++ usr/src/include/ar.h
@@ -1,22 +1,29 @@
-/*
- * Copyright (c) 1980 Regents of the University of California.
- * All rights reserved.  The Berkeley software License Agreement
- * specifies the terms and conditions for redistribution.
+/*-
+ * Copyright (c) 1990 The Regents of the University of California.
+ * All rights reserved.
  *
- *	@(#)ar.h	5.1 (Berkeley) %G%
+ * This code is derived from software contributed to Berkeley by
+ * Hugh Smith at The University of Guelph.
+ *
+ *	@(#)ar.h	5.2 (Berkeley) %G%
  */
 
-#define	ARMAG	"!<arch>\n"
-#define	SARMAG	8
+/* Pre-4BSD archives had these magic numbers in them. */
+#define	OARMAG1	0177555
+#define	OARMAG2	0177545
 
-#define	ARFMAG	"`\n"
+#define	ARMAG		"!<arch>\n"	/* ar "magic number" */
+#define	SARMAG		8		/* strlen(ARMAG); */
+
+#define	AR_EFMT1	"#1/"		/* extended format #1 */
 
 struct ar_hdr {
-	char	ar_name[16];
-	char	ar_date[12];
-	char	ar_uid[6];
-	char	ar_gid[6];
-	char	ar_mode[8];
-	char	ar_size[10];
-	char	ar_fmag[2];
+	char ar_name[16];		/* name */
+	char ar_date[12];		/* modification time */
+	char ar_uid[6];			/* user id */
+	char ar_gid[6];			/* group id */
+	char ar_mode[8];		/* octal file permissions */
+	char ar_size[10];		/* size in bytes */
+#define	ARFMAG	"`\n"
+	char ar_fmag[2];		/* consistency check */
 };
