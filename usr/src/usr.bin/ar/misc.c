c42135923c0819cb4f9ee7eb31ad523688350c29
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-01-18 02:23:53 -0800

    date and time created 91/01/17 18:23:53 by bostic
    SCCS-vsn: usr.bin/ar/misc.c 5.1

---
+++ usr/src/usr.bin/ar/misc.c
@@ -0,0 +1,104 @@
+/*-
+ * Copyright (c) 1990 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to Berkeley by
+ * Hugh Smith at The University of Guelph.
+ *
+ * %sccs.include.redist.c%
+ */
+
+#ifndef lint
+static char sccsid[] = "@(#)misc.c	5.1 (Berkeley) %G%";
+#endif /* not lint */
+
+#include <sys/param.h>
+#include <sys/signal.h>
+#include <sys/errno.h>
+#include <dirent.h>
+#include <stdio.h>
+#include "pathnames.h"
+#include "archive.h"
+
+extern CHDR chdr;			/* converted header */
+extern char *archive;			/* archive name */
+char *tname = "temporary file";		/* temporary file "name" */
+
+tmp()
+{
+	extern char *envtmp;
+	sigset_t set, oset;
+	static int first;
+	int fd;
+	char path[MAXPATHLEN];
+
+	if (!first && !envtmp) {
+		envtmp = getenv("TMPDIR");
+		first = 1;
+	}
+
+	if (envtmp)
+		(void)sprintf(path, "%s/%s", envtmp, _NAME_ARTMP);
+	else
+		bcopy(_PATH_ARTMP, path, sizeof(_PATH_ARTMP));
+	
+	sigemptyset(&set);
+	sigaddset(&set, SIGHUP);
+	sigaddset(&set, SIGINT);
+	sigaddset(&set, SIGQUIT);
+	sigaddset(&set, SIGTERM);
+	(void)sigprocmask(SIG_BLOCK, &set, &oset);
+	if ((fd = mkstemp(path)) == -1)
+		error(tname);
+        (void)unlink(path);
+	(void)sigprocmask(SIG_SETMASK, &oset, (sigset_t *)NULL);
+	return(fd);
+}
+
+/*
+ * files --
+ *	See if the current file matches any file in the argument list; if it
+ * 	does, remove it from the argument list.
+ */
+files(argv)
+	char **argv;
+{
+	register char **list;
+
+	for (list = argv; *list; ++list)
+		if (compare(*list)) {
+			for (; list[0] = list[1]; ++list);
+			return(1);
+		}
+	return(0);
+}
+
+char *
+rname(path)
+	char *path;
+{
+	register char *ind;
+
+	return((ind = rindex(path, '/')) ? ind + 1 : path);
+}
+
+compare(dest)
+	char *dest;
+{
+	char *rname();
+
+	return(!strcmp(chdr.name, rname(dest)));
+}
+
+badfmt()
+{
+	errno = EFTYPE;
+	error(archive);
+}
+
+error(name)
+	char *name;
+{
+	(void)fprintf(stderr, "ar: %s: %s\n", name, strerror(errno));
+	exit(1);
+}
