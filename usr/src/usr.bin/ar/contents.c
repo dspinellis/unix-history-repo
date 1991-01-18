b87f1d680b76899e16f579855d06148a0f4e3476
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-01-18 02:23:38 -0800

    date and time created 91/01/17 18:23:38 by bostic
    SCCS-vsn: usr.bin/ar/contents.c 5.1

---
+++ usr/src/usr.bin/ar/contents.c
@@ -0,0 +1,65 @@
+/*-
+ * Copyright (c) 1990 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to Berkeley by
+ * Hugh Smith at The University of Guelph.
+ *
+ * %sccs.include.redist.c%
+ */
+
+#ifndef lint
+static char sccsid[] = "@(#)contents.c	5.1 (Berkeley) %G%";
+#endif /* not lint */
+
+#include <sys/param.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <tzfile.h>
+#include <dirent.h>
+#include <stdio.h>
+#include <ar.h>
+#include "archive.h"
+
+extern CHDR chdr;			/* converted header */
+extern char *archive;			/* archive name */
+
+/*
+ * contents --
+ *	Handles t[v] option - opens the archive and then reads headers,
+ *	skipping member contents.  If verbose option set, calls verbose
+ *	to do ls -l style display.
+ */
+contents(argv)
+	register char **argv;
+{
+	register int afd, all;
+	int eval;
+	struct tm *tp;
+	char buf[25];
+	
+	afd = open_archive(O_RDONLY);
+
+	for (all = !*argv; get_header(afd);) {
+		if (all || files(argv)) {
+			if (options & AR_V) {
+				(void)strmode(chdr.mode, buf);
+				(void)printf("%s %6d/%-6d %8ld ",
+				    buf + 1, chdr.uid, chdr.gid, chdr.size);
+				tp = localtime(&chdr.date);
+				(void)strftime(buf, sizeof(buf),
+				    "%b %e %H:%M %Y", tp);
+				(void)printf("%s %s\n", buf, chdr.name);
+			} else
+				(void)printf("%s\n", chdr.name);
+			if (!all && !*argv)
+				break;
+		}
+		SKIP(afd, chdr.size, archive);
+	} 
+	ORPHANS;
+	close_archive(afd);
+	return(eval);
+}
