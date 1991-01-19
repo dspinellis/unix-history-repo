4c104ee51ad3ca6bd5cccb1a55da243e4cfbe5c0
William Nesheim <william@ucbvax.Berkeley.EDU>
1991-01-18 22:32:26 -0800

    fixed just which copyright notice is included.
    SCCS-vsn: sys/i386/include/cpu.h 5.2
    SCCS-vsn: sys/i386/include/dkio.h 5.2
    SCCS-vsn: sys/i386/include/frame.h 5.2
    SCCS-vsn: sys/i386/include/npx.h 5.3
    SCCS-vsn: sys/i386/include/pcb.h 5.6
    SCCS-vsn: sys/i386/include/psl.h 5.2
    SCCS-vsn: sys/i386/include/pte.h 5.4
    SCCS-vsn: sys/i386/include/reg.h 5.5
    SCCS-vsn: sys/i386/include/trap.h 5.3
    SCCS-vsn: sys/i386/include/tss.h 5.4
    SCCS-vsn: sys/i386/include/vmparam.h 5.5
    SCCS-vsn: sys/i386/include/param.h 5.3

--- usr/src/sys/i386/include/cpu.h
+++ usr/src/sys/i386/include/cpu.h
@@ -6,9 +6,9 @@
  * This code is derived from software contributed to Berkeley by
  * William Jolitz.
  *
- * %sccs.include.noredist.c%
+ * %sccs.include.redist.c%
  *
- *	@(#)cpu.h	5.1 (Berkeley) %G%
+ *	@(#)cpu.h	5.2 (Berkeley) %G%
  */
 
 #define	resettodr()	/* no todr to set */
--- usr/src/sys/i386/include/dkio.h
+++ usr/src/sys/i386/include/dkio.h
@@ -5,9 +5,9 @@
  * This code is derived from software contributed to Berkeley by
  * William Jolitz.
  *
- * %sccs.include.noredist.c%
+ * %sccs.include.redist.c%
  *
- *	@(#)dkio.h	5.1 (Berkeley) %G%
+ *	@(#)dkio.h	5.2 (Berkeley) %G%
  */
 
 /*
