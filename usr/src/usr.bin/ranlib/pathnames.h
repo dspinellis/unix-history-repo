1e83ddcb3c937097ed0aafc7b9d0df36b8525d06
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-01-19 02:45:38 -0800

    date and time created 91/01/18 18:45:38 by bostic
    SCCS-vsn: usr.bin/ranlib/pathnames.h 5.1

---
+++ usr/src/usr.bin/ranlib/pathnames.h
@@ -0,0 +1,11 @@
+/*-
+ * Copyright (c) 1990 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to Berkeley by
+ * Hugh Smith at The University of Guelph.
+ *
+ *	@(#)pathnames.h	5.1 (Berkeley) %G%
+ */
+
+#define _PATH_RANTMP	"/tmp/ranlib.XXXXXX"
