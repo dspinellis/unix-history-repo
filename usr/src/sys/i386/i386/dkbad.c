7d8ebf2e1d797dec8b6b39ec085ba43413a807b4
William Nesheim <william@ucbvax.Berkeley.EDU>
1991-01-19 20:39:02 -0800

    change copyright notice
    SCCS-vsn: sys/i386/i386/dkbad.c 5.4

--- usr/src/sys/i386/i386/dkbad.c
+++ usr/src/sys/i386/i386/dkbad.c
@@ -5,9 +5,9 @@
  * This code is derived from software contributed to Berkeley by
  * William Jolitz.
  *
- * %sccs.include.noredist.c%
+ * %sccs.include.redist.c%
  *
- *	@(#)dkbad.c	5.3 (Berkeley) %G%
+ *	@(#)dkbad.c	5.4 (Berkeley) %G%
  */
 
 
