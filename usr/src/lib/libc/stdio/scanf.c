fea5d28a9157ba2c907d37989a9a9daf1cb53b14
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-01-21 05:28:59 -0800

    new stdio
    SCCS-vsn: lib/libc/stdio/scanf.3 6.3
    SCCS-vsn: lib/libc/stdio/scanf.c 5.3
    SCCS-vsn: lib/libc/stdio/setbuf.3 6.5

--- usr/src/lib/libc/stdio/scanf.c
+++ usr/src/lib/libc/stdio/scanf.c
@@ -1,33 +1,41 @@
+/*-
+ * Copyright (c) 1990 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to Berkeley by
+ * Chris Torek.
+ *
+ * %sccs.include.redist.c%
+ */
+
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)scanf.c	5.2 (Berkeley) %G%";
-#endif LIBC_SCCS and not lint
+static char sccsid[] = "@(#)scanf.c	5.3 (Berkeley) %G%";
+#endif /* LIBC_SCCS and not lint */
 
-#include	<stdio.h>
+#include <stdio.h>
+#if __STDC__
+#include <stdarg.h>
+#else
+#include <varargs.h>
+#endif
 
-scanf(fmt, args)
-char *fmt;
+#if __STDC__
+scanf(char const *fmt, ...)
+#else
+scanf(fmt, va_alist)
+	char *fmt;
+	va_dcl
+#endif
 {
-	return(_doscan(stdin, fmt, &args));
-}
+	int ret;
+	va_list ap;
 
-fscanf(iop, fmt, args)
-FILE *iop;
-char *fmt;
-{
-	return(_doscan(iop, fmt, &args));
-}
-
-sscanf(str, fmt, args)
-register char *str;
-char *fmt;
-{
-	FILE _strbuf;
-
-	_strbuf._flag = _IOREAD|_IOSTRG;
-	_strbuf._ptr = _strbuf._base = str;
-	_strbuf._cnt = 0;
-	while (*str++)
-		_strbuf._cnt++;
-	_strbuf._bufsiz = _strbuf._cnt;
-	return(_doscan(&_strbuf, fmt, &args));
+#if __STDC__
+	va_start(ap, fmt);
+#else
+	va_start(ap);
+#endif
+	ret = __svfscanf(stdin, fmt, ap);
+	va_end(ap);
+	return (ret);
 }
