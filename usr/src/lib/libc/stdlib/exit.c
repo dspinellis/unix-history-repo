654c4749f804466fa593415990524cd1a0555790
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-01-20 22:52:35 -0800

    new stdio version of exit.c; from Chris Torek
    SCCS-vsn: lib/libc/stdlib/exit.c 5.3

--- usr/src/lib/libc/stdlib/exit.c
+++ usr/src/lib/libc/stdlib/exit.c
@@ -1,11 +1,31 @@
+/*-
+ * Copyright (c) 1990 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.redist.c%
+ */
+
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)exit.c	5.2 (Berkeley) %G%";
-#endif LIBC_SCCS and not lint
+static char sccsid[] = "@(#)exit.c	5.3 (Berkeley) %G%";
+#endif /* LIBC_SCCS and not lint */
 
-exit(code)
-	int code;
+#include "atexit.h"
+
+void (*__cleanup)();
+
+/*
+ * Exit, flushing stdio buffers if necessary.
+ */
+exit(status)
+	int status;
 {
+	register struct atexit *p;
+	register int n;
 
-	_cleanup();
-	_exit(code);
+	for (p = __atexit; p; p = p->next)
+		for (n = p->ind; --n >= 0;)
+			(*p->fns[n])();
+	if (__cleanup)
+		(*__cleanup)();
+	_exit(status);
 }
