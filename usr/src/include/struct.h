a689510a78dc1f6461b60e67c9feefaf3d9da102
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-01-20 22:11:45 -0800

    Berkeley specific copyright, document each #define
    SCCS-vsn: include/struct.h 5.1

--- usr/src/include/struct.h
+++ usr/src/include/struct.h
@@ -1,9 +1,20 @@
-/*	struct.h	4.1	83/05/03	*/
-
-/*
- * access to information relating to the fields of a structure
+/*-
+ * Copyright (c) 1983 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.redist.c%
+ *
+ *	@(#)struct.h	5.1 (Berkeley) %G%
  */
 
-#define	fldoff(str, fld)	((int)&(((struct str *)0)->fld))
-#define	fldsiz(str, fld)	(sizeof(((struct str *)0)->fld))
-#define	strbase(str, ptr, fld)	((struct str *)((char *)(ptr)-fldoff(str, fld)))
+/* Offset of the field in the structure. */
+#define	fldoff(name, field) \
+	((int)&(((struct name *)0)->field))
+
+/* Size of the field in the structure. */
+#define	fldsiz(name, field) \
+	(sizeof(((struct name *)0)->field))
+
+/* Address of the structure from a field. */
+#define	strbase(name, addr, field) \
+	((struct name *)((char *)(addr) - fldoff(name, field)))
