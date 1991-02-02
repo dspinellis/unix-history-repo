94fe63e3903f5aea3f9be90b108c78a838d94fb5
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-02-02 03:28:46 -0800

    fdopen got messed up
    SCCS-vsn: lib/libc/stdio/fdopen.c 5.4

--- usr/src/lib/libc/stdio/fdopen.c
+++ usr/src/lib/libc/stdio/fdopen.c
@@ -9,13 +9,13 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)fdopen.c	5.3 (Berkeley) %G%";
+static char sccsid[] = "@(#)fdopen.c	5.4 (Berkeley) %G%";
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/types.h>
 #include <sys/file.h>
-#include <sys/errno.h>
 #include <stdio.h>
+#include <errno.h>
 #include "local.h"
 
 FILE *
@@ -23,29 +23,28 @@ fdopen(fd, mode)
 	int fd;
 	char *mode;
 {
-	static int nofile;
 	register FILE *fp;
-	int flags, oflags, fdflags, fdmode;
+	static int nofile;
+	int flags, oflags, fdflags, tmp;
 
 	if (nofile == 0)
 		nofile = getdtablesize();
 
-	if ((fdflags = fcntl(fd, F_GETFL, 0)) < 0)
+	if ((flags = __sflags(mode, &oflags)) == 0)
 		return (NULL);
 
 	/* Make sure the mode the user wants is a subset of the actual mode. */
-	fdmode = fdflags & O_ACCMODE;
-	if (fdmode != O_RDWR && fdmode != (oflags & O_ACCMODE)) {
+	if ((fdflags = fcntl(fd, F_GETFL, 0)) < 0)
+		return (NULL);
+	tmp = fdflags & O_ACCMODE;
+	if (tmp != O_RDWR && (tmp != (oflags & O_ACCMODE))) {
 		errno = EINVAL;
 		return (NULL);
 	}
 
 	if ((fp = __sfp()) == NULL)
 		return (NULL);
-
-	if ((fp->_flags = __sflags(mode, &oflags)) == 0) 
-		return (NULL);
-
+	fp->_flags = flags;
 	/*
 	 * If opened for appending, but underlying descriptor does not have
 	 * O_APPEND bit set, assert __SAPP so that __swrite() will lseek to
@@ -53,7 +52,6 @@ fdopen(fd, mode)
 	 */
 	if ((oflags & O_APPEND) && !(fdflags & O_APPEND))
 		fp->_flags |= __SAPP;
-
 	fp->_file = fd;
 	fp->_cookie = fp;
 	fp->_read = __sread;
