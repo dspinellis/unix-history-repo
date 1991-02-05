7444a0eabe0f7fb835b5a41be26cf7a7aa95ed2a
Chris Torek <torek@ucbvax.Berkeley.EDU>
1991-02-05 06:50:35 -0800

    tip.h now includes sys/time.h for us
    SCCS-vsn: usr.bin/tip/aculib/courier.c 5.6
    SCCS-vsn: usr.bin/tip/aculib/v831.c 5.4

--- usr/src/usr.bin/tip/aculib/v831.c
+++ usr/src/usr.bin/tip/aculib/v831.c
@@ -6,14 +6,12 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)v831.c	5.3 (Berkeley) %G%";
+static char sccsid[] = "@(#)v831.c	5.4 (Berkeley) %G%";
 #endif /* not lint */
 
 /*
  * Routines for dialing up on Vadic 831
  */
-#include <sys/time.h>
-
 #include "tip.h"
 
 int	v831_abort();
