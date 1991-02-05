5b13d4c6d13a31220080b4b4079faaa6d800524f
Chris Torek <torek@ucbvax.Berkeley.EDU>
1991-02-05 01:34:52 -0800

    fseek and ftell are declared correctly in <stdio.h>; do not redeclare
    SCCS-vsn: usr.bin/uucp/libuu/anlwrk.c 5.8

--- usr/src/usr.bin/uucp/libuu/anlwrk.c
+++ usr/src/usr.bin/uucp/libuu/anlwrk.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char sccsid[] = "@(#)anlwrk.c	5.7	(Berkeley) %G%";
+static char sccsid[] = "@(#)anlwrk.c	5.8	(Berkeley) %G%";
 #endif
 
 #include "uucp.h"
@@ -17,7 +17,6 @@ static char sccsid[] = "@(#)anlwrk.c	5.7	(Berkeley) %G%";
 
 int Nfiles = 0;
 char Filent[LLEN][NAMESIZE];
-long fseek(), ftell();
 extern int TransferSucceeded;
 
 /*LINTLIBRARY*/
