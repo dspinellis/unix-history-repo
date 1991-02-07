7dd021852f7c9c5d9b3e01a24201bdb07e7d2cd5
Mike Karels <karels@ucbvax.Berkeley.EDU>
1991-02-06 18:01:55 -0800

    fix old comment
    SCCS-vsn: sys/kern/kern_prot.c 7.18

--- usr/src/sys/kern/kern_prot.c
+++ usr/src/sys/kern/kern_prot.c
@@ -3,7 +3,7 @@
  * All rights reserved.  The Berkeley software License Agreement
  * specifies the terms and conditions for redistribution.
  *
- *	@(#)kern_prot.c	7.17 (Berkeley) %G%
+ *	@(#)kern_prot.c	7.18 (Berkeley) %G%
  */
 
 /*
