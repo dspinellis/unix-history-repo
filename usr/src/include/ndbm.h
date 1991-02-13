ec08afdadf7276616fa582d5d1a889edad5b6b2b
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-02-13 04:11:10 -0800

    new version from Margo Seltzer with the db(3) hash package add C function prototypes
    SCCS-vsn: include/ndbm.h 5.4

--- usr/src/include/ndbm.h
+++ usr/src/include/ndbm.h
@@ -1,73 +1,39 @@
-/*
- * Copyright (c) 1983 Regents of the University of California.
- * All rights reserved.  The Berkeley software License Agreement
- * specifies the terms and conditions for redistribution.
+/*-
+ * Copyright (c) 1990 The Regents of the University of California.
+ * All rights reserved.
  *
- *	@(#)ndbm.h	5.3 (Berkeley) %G%
+ * This code is derived from software contributed to Berkeley by
+ * Margo Seltzer.
+ *
+ * %sccs.include.redist.c%
+ *
+ *	@(#)ndbm.h	5.4 (Berkeley) %G%
  */
 
-/*
- * Hashed key data base library.
- */
-#define PBLKSIZ 1024
-#define DBLKSIZ 4096
+#include <sys/cdefs.h>
+#include <db.h>
+
+/* Map dbm interface onto hash(3). */
+#define DBM_RDONLY	O_RDONLY
+
+/* Flags to dbm_store(). */
+#define DBM_INSERT      0
+#define DBM_REPLACE     1
 
 typedef struct {
-	int	dbm_dirf;		/* open directory file */
-	int	dbm_pagf;		/* open page file */
-	int	dbm_flags;		/* flags, see below */
-	long	dbm_maxbno;		/* last ``bit'' in dir file */
-	long	dbm_bitno;		/* current bit number */
-	long	dbm_hmask;		/* hash mask */
-	long	dbm_blkptr;		/* current block for dbm_nextkey */
-	int	dbm_keyptr;		/* current key for dbm_nextkey */
-	long	dbm_blkno;		/* current page to read/write */
-	long	dbm_pagbno;		/* current page in pagbuf */
-	char	dbm_pagbuf[PBLKSIZ];	/* page file block buffer */
-	long	dbm_dirbno;		/* current block in dirbuf */
-	char	dbm_dirbuf[DBLKSIZ];	/* directory file block buffer */
-} DBM;
-
-#define _DBM_RDONLY	0x1	/* data base open read-only */
-#define _DBM_IOERR	0x2	/* data base I/O error */
-
-#define dbm_rdonly(db)	((db)->dbm_flags & _DBM_RDONLY)
-
-#define dbm_error(db)	((db)->dbm_flags & _DBM_IOERR)
-	/* use this one at your own risk! */
-#define dbm_clearerr(db)	((db)->dbm_flags &= ~_DBM_IOERR)
-
-/* for flock(2) and fstat(2) */
-#define dbm_dirfno(db)	((db)->dbm_dirf)
-#define dbm_pagfno(db)	((db)->dbm_pagf)
-
-typedef struct {
-	char	*dptr;
-	int	dsize;
+	char *dptr;
+	int dsize;
 } datum;
 
-/*
- * flags to dbm_store()
- */
-#define DBM_INSERT	0
-#define DBM_REPLACE	1
+typedef DB DBM;
 
-#if __STDC__ || c_plusplus
-extern DBM *dbm_open(const char *, int, int);
-extern void dbm_close(DBM *);
-extern datum dbm_fetch(DBM *, datum);
-extern datum dbm_firstkey(DBM *);
-extern datum dbm_nextkey(DBM *);
-extern long dbm_forder(DBM *, datum);
-extern int dbm_delete(DBM *, datum);
-extern int dbm_store(DBM *, datum, datum, int);
-#else
-extern DBM *dbm_open();
-extern void dbm_close();
-extern datum dbm_fetch();
-extern datum dbm_firstkey();
-extern datum dbm_nextkey();
-extern long dbm_forder();
-extern int dbm_delete();
-extern int dbm_store();
-#endif
+__BEGIN_DECLS
+void	 dbm_close __P((DBM *));
+int	 dbm_delete __P((DBM *, datum));
+datum	 dbm_fetch __P((DBM *, datum));
+datum	 dbm_firstkey __P((DBM *));
+long	 dbm_forder __P((DBM *, datum));
+datum	 dbm_nextkey __P((DBM *));
+DBM	*dbm_open __P((const char *, int, int));
+int	 dbm_store __P((DBM *, datum, datum, int));
+__END_DECLS
