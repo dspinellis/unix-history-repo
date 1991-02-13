5df30dfd947ab4e244d217b3ca33938e2c7e2908
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-02-13 04:13:26 -0800

    don't need to prepend key with type byte any more; should never have included sys/kinfo.h; use <paths.h> for /var/run pathname; wordsmith function prototypes
    SCCS-vsn: include/kvm.h 5.2

--- usr/src/include/kvm.h
+++ usr/src/include/kvm.h
@@ -4,24 +4,19 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)kvm.h	5.1 (Berkeley) %G%
+ *	@(#)kvm.h	5.2 (Berkeley) %G%
  */
 
-#include <sys/kinfo.h>
 #include <sys/cdefs.h>
 
-/*
- * type byte prepending key
- */
-#define	KVMDB_NLIST	1
-#define	KVMDB_DEVS	2
-
-#define	KVMDBDIR	"/var/run"
+/* Default version symbol. */
+#define	VRS_SYM		"_version"
+#define	VRS_KEY		"VERSION"
 
 __BEGIN_DECLS
-struct proc *kvm_nextproc __P((void));
-struct eproc *kvm_geteproc __P((const struct proc *));
-struct user *kvm_getu __P((const struct proc *));
-char *kvm_getargs __P((const struct proc *, const struct user *));
-char *kvm_geterr __P((void));
+char		*kvm_getargs __P((const struct proc *, const struct user *));
+struct eproc	*kvm_geteproc __P((const struct proc *));
+char		*kvm_geterr __P((void));
+struct user	*kvm_getu __P((const struct proc *));
+struct proc	*kvm_nextproc __P((void));
 __END_DECLS
