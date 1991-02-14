cd7fc06dbcce391b9a375f461783c5bd243baa03
Donn Seeley <donn@ucbvax.Berkeley.EDU>
1991-02-14 07:34:13 -0800

    G++'s parser isn't up to handling atexit()'s extended declaration...
    SCCS-vsn: include/stdlib.h 5.6

--- usr/src/include/stdlib.h
+++ usr/src/include/stdlib.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)stdlib.h	5.5 (Berkeley) %G%
+ *	@(#)stdlib.h	5.6 (Berkeley) %G%
  */
 
 #ifndef _STDLIB_H_
@@ -42,7 +42,7 @@ typedef struct {
 __BEGIN_DECLS
 void	 abort __P((void));
 int	 abs __P((int));
-int	 atexit __P((void (*_func)(void)));
+int	 atexit __P((void (*)(void)));
 double	 atof __P((const char *_nptr));
 int	 atoi __P((const char *_nptr));
 long	 atol __P((const char *_nptr));
