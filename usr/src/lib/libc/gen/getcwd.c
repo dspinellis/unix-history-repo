e8ea75d5fa4a2c8a0ae73b0e5772d7a9bdb6d284
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-02-20 16:26:10 -0800

    rename getwd to getcwd for POSIX.1 compatibility add System V, X/Open style path malloc'ing if user argument is NULL.
    SCCS-vsn: lib/libc/gen/getcwd.c 5.9
    SCCS-vsn: lib/libc/gen/getcwd.3 6.3

--- usr/src/lib/libc/gen/getcwd.c
+++ usr/src/lib/libc/gen/getcwd.c
@@ -1,17 +1,20 @@
 /*
- * Copyright (c) 1989 The Regents of the University of California.
+ * Copyright (c) 1989, 1991 The Regents of the University of California.
  * All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)getcwd.c	5.8 (Berkeley) %G%";
+static char sccsid[] = "@(#)getcwd.c	5.9 (Berkeley) %G%";
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/param.h>
 #include <sys/stat.h>
+#include <errno.h>
 #include <dirent.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #define	ISDOT(dp) \
@@ -19,121 +22,180 @@ static char sccsid[] = "@(#)getcwd.c	5.8 (Berkeley) %G%";
 	    dp->d_name[1] == '.' && dp->d_name[2] == '\0'))
 
 char *
-getwd(store)
-	char *store;
+getcwd(pt, size)
+	char *pt;
+	size_t size;
 {
-	extern int errno;
 	register struct dirent *dp;
 	register DIR *dir;
+	register dev_t dev;
 	register ino_t ino;
-	register char *pp, *pu;
 	register int first;
+	register char *bpt, *bup;
 	struct stat s;
-	dev_t root_dev, dev;
+	dev_t root_dev;
 	ino_t root_ino;
-	int save_errno, found;
-	char path[MAXPATHLEN], up[MAXPATHLEN], *file;
+	size_t ptsize, upsize;
+	int save_errno;
+	char *ept, *eup, *up;
 
-	/* save root values */
-	if (stat("/", &s)) {
-		file = "/";
-		goto err;
+	/*
+	 * If no buffer specified by the user, allocate one as necessary.
+	 * If a buffer is specified, the size has to be non-zero.  The path
+	 * is built from the end of the buffer backwards.
+	 */
+	if (pt) {
+		ptsize = 0;
+		if (!size) {
+			errno = EINVAL;
+			return((char *)NULL);
+		}
+		ept = pt + size;
+	} else {
+		if (!(pt = (char *)malloc(ptsize = 1024 - 4)))
+			return((char *)NULL);
+		ept = pt + ptsize;
 	}
-	root_dev = s.st_dev;
-	root_ino = s.st_ino;
+	bpt = ept - 1;
+	*ept = '\0';
 
-	/* init path pointer; built from the end of the buffer */
-	pp = path + sizeof(path) - 1;
-	*pp = '\0';
-
-	/* special case first stat, it's ".", not ".." */
+	/*
+	 * Allocate bytes (1024 - malloc space) for the string of "../"'s.
+	 * Should always be enough (it's 340 levels).  If it's not, allocate
+	 * as necessary.  Special * case the first stat, it's ".", not "..".
+	 */
+	if (!(up = (char *)malloc(upsize = 1024 - 4)))
+		goto err;
+	eup = up + MAXPATHLEN;
+	bup = up;
 	up[0] = '.';
 	up[1] = '\0';
 
-	for (pu = up, first = 1;; first = 0) {
-		/* stat current level */
-		if (lstat(up, &s)) {
-			file = up;
-			goto err;
-		}
+	/* Save root values, so know when to stop. */
+	if (stat("/", &s))
+		goto err;
+	root_dev = s.st_dev;
+	root_ino = s.st_ino;
 
-		/* save current node values */
+	errno = 0;			/* XXX readdir has no error return. */
+
+	for (first = 1;; first = 0) {
+		/* Stat the current level. */
+		if (lstat(up, &s))
+			goto err;
+
+		/* Save current node values. */
 		ino = s.st_ino;
 		dev = s.st_dev;
 
-		/* check for root */
+		/* Check for reaching root. */
 		if (root_dev == dev && root_ino == ino) {
-			*store = '/';
-			(void) strcpy(store + 1, pp);
-			return (store);
+			*--bpt = '/';
+			/*
+			 * It's unclear that it's a requirement to copy the
+			 * path to the beginning of the buffer, but it's always
+			 * been that way and stuff would probably break.
+			 */
+			(void)bcopy(bpt, pt, ept - bpt);
+			free(up);
+			return(pt);
 		}
-
-		*pu++ = '.';
-		*pu++ = '.';
-		*pu = '\0';
-
-		/* open and stat parent */
-		if (!(dir = opendir(up)) || fstat(dirfd(dir), &s)) {
-			file = up;
-			goto err;
-		}
-		found = save_errno = 0;
-
-		*pu++ = '/';
 
 		/*
-		 * if it's a mount point you have to stat each element because
+		 * Build pointer to the parent directory, allocating memory
+		 * as necessary.  Max length is 3 for "../", the largest
+		 * possible component name, plus a trailing NULL.
+		 */
+		if (bup + 3  + MAXNAMLEN + 1 >= eup) {
+			if (!(up = (char *)realloc(up, upsize *= 2)))
+				goto err;
+			eup = up + upsize;
+		}
+		*bup++ = '.';
+		*bup++ = '.';
+		*bup = '\0';
+
+		/* Open and stat parent directory. */
+		if (!(dir = opendir(up)) || fstat(dirfd(dir), &s))
+			goto err;
+
+		/* Add trailing slash for next directory. */
+		*bup++ = '/';
+
+		/*
+		 * If it's a mount point, have to stat each element because
 		 * the inode number in the directory is for the entry in the
 		 * parent directory, not the inode number of the mounted file.
 		 */
+		save_errno = 0;
 		if (s.st_dev == dev) {
-			while (dp = readdir(dir))
+			for (;;) {
+				if (!(dp = readdir(dir)))
+					goto notfound;
 				if (dp->d_fileno == ino)
-					goto hit;
-		} else {
-			while (dp = readdir(dir)) {
+					break;
+			}
+		} else
+			for (;;) {
+				if (!(dp = readdir(dir)))
+					goto notfound;
 				if (ISDOT(dp))
 					continue;
-				bcopy(dp->d_name, pu, dp->d_namlen + 1);
+				bcopy(dp->d_name, bup, dp->d_namlen + 1);
+
+				/* Save the first error for later. */
 				if (lstat(up, &s)) {
-					file = dp->d_name;
-					save_errno = errno;
+					if (!save_errno)
+						save_errno = errno;
 					errno = 0;
 					continue;
 				}
-				if (s.st_dev == dev && s.st_ino == ino) {
-hit:					if (!first)
-						*--pp = '/';
-					pp -= dp->d_namlen;
-					bcopy(dp->d_name, pp, dp->d_namlen);
-					found = 1;
+				if (s.st_dev == dev && s.st_ino == ino)
 					break;
-				}
 			}
-			if (errno) {
-				file = up;
-				save_errno = errno;
-			}
-		}
-		(void) closedir(dir);
 
-		*pu = '\0';
+		/*
+		 * Check for length of the current name, preceding slash,
+		 * leading slash.
+		 */
+		if (bpt - pt <= dp->d_namlen + (first ? 1 : 2)) {
+			size_t len, off;
 
-		if (!found) {
-			/*
-			 * We didn't find the current level in its parent
-			 * directory; figure out what to complain about.
-			 */
-			if (save_errno) {
-				errno = save_errno;
+			if (!ptsize) {
+				errno = ERANGE;
 				goto err;
 			}
-			(void) sprintf(store, "%s not found in %s?\n",
-				first ? "." : pp, up);
-			return ((char *)NULL);
+			off = bpt - pt;
+			len = ept - bpt;
+			if (!(pt = (char *)realloc(pt, ptsize *= 2)))
+				goto err;
+			bpt = pt + off;
+			ept = pt + ptsize;
+			(void)bcopy(bpt, ept - len, len);
+			bpt = ept - len;
 		}
+		if (!first)
+			*--bpt = '/';
+		bpt -= dp->d_namlen;
+		bcopy(dp->d_name, bpt, dp->d_namlen);
+		(void)closedir(dir);
+
+		/* Truncate any file name. */
+		*bup = '\0';
 	}
+
+notfound:
+	/*
+	 * If readdir set errno, use it, not any saved error; otherwise,
+	 * didn't find the current directory in its parent directory, set
+	 * errno to ENOENT.
+	 */
+	if (!errno)
+		errno = save_errno ? save_errno : ENOENT;
+	/* FALLTHROUGH */
 err:
-	(void) sprintf(store, "getwd: %s: %s", file, strerror(errno));
-	return ((char *)NULL);
+	if (ptsize)
+		free(pt);
+	free(up);
+	return((char *)NULL);
 }
