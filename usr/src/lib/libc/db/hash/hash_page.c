4728dbc8e3f9234515a23493913be26959bca724
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-02-22 01:15:39 -0800

    don't put namestr into text space -- Thanks ANSI!
    SCCS-vsn: lib/libc/db/hash/hash_page.c 5.4

--- usr/src/lib/libc/db/hash/hash_page.c
+++ usr/src/lib/libc/db/hash/hash_page.c
@@ -9,7 +9,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)hash_page.c	5.3 (Berkeley) %G%";
+static char sccsid[] = "@(#)hash_page.c	5.4 (Berkeley) %G%";
 #endif /* LIBC_SCCS and not lint */
 
 /******************************************************************************
@@ -781,7 +781,7 @@ static int
 open_temp()
 {
     sigset_t	set, oset;
-    char	*namestr = "_hashXXXXXX";
+    char	namestr[] = "_hashXXXXXX";
 
     /* Block signals; make sure file goes away at process exit. */
     sigemptyset(&set);
