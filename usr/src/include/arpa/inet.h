a4d74244a78412f69e25bd1457cc3e4231764d00
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-02-21 21:53:15 -0800

    first pass at function prototypes with cdefs.h
    SCCS-vsn: include/arpa/inet.h 5.5

--- usr/src/include/arpa/inet.h
+++ usr/src/include/arpa/inet.h
@@ -4,23 +4,18 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)inet.h	5.4 (Berkeley) %G%
+ *	@(#)inet.h	5.5 (Berkeley) %G%
  */
 
 /* External definitions for functions in inet(3) */
 
-#ifdef __STDC__
-extern unsigned long inet_addr(const char *);
-extern char *inet_ntoa(struct in_addr);
-extern struct in_addr inet_makeaddr(int , int);
-extern unsigned long inet_network(const char *);
-extern unsigned long inet_lnaof(struct in_addr);
-extern unsigned long inet_netof(struct in_addr);
-#else
-extern unsigned long inet_addr();
-extern char *inet_ntoa();
-extern struct in_addr inet_makeaddr();
-extern unsigned long inet_network();
-extern unsigned long inet_lnaof();
-extern unsigned long inet_netof();
-#endif
+#include <sys/cdefs.h>
+
+__BEGIN_DECLS
+extern unsigned long	 inet_addr __P((const char *));
+extern unsigned long	 inet_lnaof __P((struct in_addr));
+extern struct in_addr	 inet_makeaddr __P((int , int));
+extern unsigned long	 inet_netof __P((struct in_addr));
+extern unsigned long	 inet_network __P((const char *));
+extern char		*inet_ntoa __P((struct in_addr));
+__END_DECLS
