3908ab19e359394889e513848d1c69068c66be42
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-02-22 22:01:18 -0800

    use stdarg, not varargs
    SCCS-vsn: sys/sys/syslog.h 7.19

--- usr/src/sys/sys/syslog.h
+++ usr/src/sys/sys/syslog.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)syslog.h	7.18 (Berkeley) %G%
+ *	@(#)syslog.h	7.19 (Berkeley) %G%
  */
 
 #define	_PATH_LOG	"/dev/log"
@@ -139,7 +139,7 @@ CODE facilitynames[] = {
 #ifndef KERNEL
 
 #include <sys/cdefs.h>
-#include <varargs.h>
+#include <stdarg.h>
 
 __BEGIN_DECLS
 void	closelog __P((void));
