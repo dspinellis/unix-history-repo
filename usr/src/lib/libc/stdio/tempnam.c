2449b14920084269af9546e669446569da0e58b2
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-02-23 01:00:01 -0800

    cleanup, update, reimplement to work as correctly as possible
    SCCS-vsn: lib/libc/stdio/tmpfile.c 5.2
    SCCS-vsn: lib/libc/stdio/tmpnam.c 5.2
    SCCS-vsn: lib/libc/stdio/tempnam.c 5.1
    SCCS-vsn: lib/libc/stdio/tmpnam.3 5.11

--- usr/src/lib/libc/stdio/tempnam.c
+++ usr/src/lib/libc/stdio/tempnam.c
@@ -6,64 +6,53 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)tempnam.c	4.8 (Berkeley) %G%";
+static char sccsid[] = "@(#)tempnam.c	5.1 (Berkeley) %G%";
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/param.h>
+#include <errno.h>
 #include <stdio.h>
-
-#define	P_tmpdir	"/usr/tmp"
-
-FILE *
-tmpfile()
-{
-	FILE *fp;
-	char *f, *tmpnam();
-
-	if (!(f = tmpnam((char *)NULL)) || !(fp = fopen(f, "w+"))) {
-		fprintf(stderr, "tmpfile: cannot open %s.\n", f);
-		return(NULL);
-	}
-	(void)unlink(f);
-	return(fp);
-}
-
-char *
-tmpnam(s)
-	char *s;
-{
-	char *malloc(), *mktemp();
-
-	if (!s && !(s = malloc((u_int)MAXPATHLEN)))
-		return(NULL);
-	(void)sprintf(s, "%s/XXXXXX", P_tmpdir);
-	return(mktemp(s));
-}
+#include <stdlib.h>
+#include <unistd.h>
+#include <paths.h>
 
 char *
 tempnam(dir, pfx)
-	char *dir, *pfx;
+	const char *dir, *pfx;
 {
-	char *f, *name, *getenv(), *malloc(), *mktemp();
+	int sverrno;
+	char *f, *name;
 
-	if (!(name = malloc((u_int)MAXPATHLEN)))
+	if (!(name = malloc(MAXPATHLEN)))
 		return(NULL);
 
+	if (!pfx)
+		pfx = "tmp.";
+
 	if (f = getenv("TMPDIR")) {
-		(void)sprintf(name, "%s/%sXXXXXX", f, pfx ? "" : pfx);
+		(void)snprintf(name, MAXPATHLEN, "%s/%sXXXXXX", f, pfx);
 		if (f = mktemp(name))
 			return(f);
 	}
-	if (dir) {
-		(void)sprintf(name, "%s/%sXXXXXX", dir, pfx ? "" : pfx);
+
+	if (f = (char *)dir) {
+		(void)snprintf(name, MAXPATHLEN, "%s/%sXXXXXX", f, pfx);
 		if (f = mktemp(name))
 			return(f);
 	}
-	(void)sprintf(name, "%s/%sXXXXXX", P_tmpdir, pfx ? "" : pfx);
+
+	f = P_tmpdir;
+	(void)snprintf(name, MAXPATHLEN, "%s/%sXXXXXX", f, pfx);
 	if (f = mktemp(name))
 		return(f);
-	(void)sprintf(name, "/tmp/%sXXXXXX", pfx ? "" : pfx);
-	if (!(f = mktemp(name)))
-		(void)free(name);
-	return(f);
+
+	f = _PATH_TMP;
+	(void)snprintf(name, MAXPATHLEN, "%s/%sXXXXXX", f, pfx);
+	if (f = mktemp(name))
+		return(f);
+
+	sverrno = errno;
+	free(name);
+	errno = sverrno;
+	return(NULL);
 }
