48efbf763c7e0bf99d7f12b93d8ab69061e9eed9
Mike Karels <karels@ucbvax.Berkeley.EDU>
1991-02-22 20:35:21 -0800

    rearrange, put all open() and file f_flags definitions here, put fcntl stuff together, move all seek, etc. defs to unistd.h
    SCCS-vsn: sys/sys/fcntl.h 5.11

--- usr/src/sys/sys/fcntl.h
+++ usr/src/sys/sys/fcntl.h
@@ -4,17 +4,100 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)fcntl.h	5.10 (Berkeley) %G%
+ *	@(#)fcntl.h	5.11 (Berkeley) %G%
  */
 
 #ifndef _FCNTL_H_
 #define	_FCNTL_H_
 
+/*
+ * This file includes the definitions for open and fcntl
+ * described by POSIX for <fcntl.h>; it also includes
+ * related kernel definitions.
+ */
+
 #ifndef KERNEL
 #include <sys/types.h>
 #endif
 
-/* command values for fcntl(2) */
+/*
+ * File status flags: these are used by open(2), fcntl(2).
+ * They are also used in the kernel file structure,
+ * which is a superset of the open/fcntl flags.
+ * Open/fcntl flags begin with O_; kernel-internal-only
+ * flags begin with F.
+ */
+/* open-only flags */
+#define	O_RDONLY	0x0000		/* open for reading only */
+#define	O_WRONLY	0x0001		/* open for writing only */
+#define	O_RDWR		0x0002		/* open for reading and writing */
+#define	O_ACCMODE	0x0003		/* mask for above modes */
+
+#ifdef KERNEL
+/*
+ * Kernel encoding of open mode; separate read and write bits
+ * that are independently testable: 1 greater than the above.
+ */
+#define	FREAD		0x0001
+#define	FWRITE		0x0002
+#endif
+#define	O_NONBLOCK	0x0004		/* no delay */
+#define	O_APPEND	0x0008		/* set append mode */
+#ifdef KERNEL
+#define	FMARK		0x0010		/* mark during gc() */
+#define	FDEFER		0x0020		/* defer for next gc pass */
+#endif
+#ifndef _POSIX_SOURCE
+#define	O_ASYNC		0x0040		/* signal pgrp when data ready */
+#define	O_SHLOCK	0x0080		/* shared file lock present */
+#define	O_EXLOCK	0x0100		/* exclusive file lock present */
+#endif
+#define	O_CREAT		0x0200		/* create if nonexistant */
+#define	O_TRUNC		0x0400		/* truncate to zero length */
+#define	O_EXCL		0x0800		/* error if already exists */
+#define	O_FSYNC		0x1000		/* synchronous writes */
+
+/* defined by POSIX 1003.1; BSD default, so no bit required */
+#define	O_NOCTTY	0		/* don't assign controlling terminal */
+
+#ifdef KERNEL
+/* convert from open() flags to/from fflags; convert O_RD/WR to FREAD/FWRITE */
+#define	FFLAGS(oflags)	((oflags) + 1)
+#define	OFLAGS(fflags)	((fflags) - 1)
+
+/* bits to save after open */
+#define	FMASK		(FREAD|FWRITE|FAPPEND|FASYNC|FNONBLOCK)
+/* bits settable by fcntl(F_SETFL, ...) */
+#define	FCNTLFLAGS	(O_NONBLOCK|O_APPEND|O_ASYNC|O_FSYNC)
+#endif
+
+/*
+ * The O_* flags used to have only F* names, which were used in the kernel
+ * and by fcntl.  We retain the F* names for the kernel f_flags field
+ * and for backward compatibility for fcntl.
+ */
+#ifndef _POSIX_SOURCE
+#define	FAPPEND		O_APPEND
+#define	FASYNC		O_ASYNC
+#define	FNDELAY		O_NONBLOCK
+#define	O_NDELAY	O_NONBLOCK
+#endif
+
+#ifdef KERNEL
+#define	FCREAT		O_CREAT		/* not stored in f_flags */
+#define	FEXCL		O_EXCL		/* not stored in f_flags */
+#define	FTRUNC		O_TRUNC		/* not stored in f_flags */
+#define	FEXLOCK		O_EXLOCK
+#define	FSHLOCK		O_SHLOCK
+#define	FNONBLOCK	O_NONBLOCK
+#endif
+
+
+/*
+ * Constants used for fcntl(2)
+ */
+
+/* command values */
 #define	F_DUPFD		0		/* duplicate file descriptor */
 #define	F_GETFD		1		/* get file descriptor flags */
 #define	F_SETFD		2		/* set file descriptor flags */
@@ -41,6 +124,19 @@
 #define	F_POSIX		0x040	 	/* Use POSIX semantics for lock */
 #endif
 
+/*
+ * Advisory file segment locking data type -
+ * information passed to system by user
+ */
+struct flock {
+	short	l_type;		/* lock type: read/write, etc. */
+	short	l_whence;	/* type of l_start */
+	off_t	l_start;	/* starting offset */
+	off_t	l_len;		/* len = 0 means until end of file */
+	pid_t	l_pid;		/* lock owner */
+};
+
+
 #ifndef _POSIX_SOURCE
 /* lock operations for flock(2) */
 #define	LOCK_SH		0x01		/* shared file lock */
@@ -49,61 +145,14 @@
 #define	LOCK_UN		0x08		/* unlock file */
 #endif
 
-/* file status flags */
-#define	O_RDONLY	00000		/* open for reading only */
-#define	O_WRONLY	00001		/* open for writing only */
-#define	O_RDWR		00002		/* open for reading and writing */
-#define	O_NONBLOCK	00004		/* no delay */
-#ifndef _POSIX_SOURCE
-#define	O_NDELAY	O_NONBLOCK
-#define	FNDELAY		O_NONBLOCK
-#endif
-/*			00008		/* unused */
-#define	O_APPEND	00010		/* set append mode */
-#ifndef _POSIX_SOURCE
-#define	FAPPEND		O_APPEND
-#endif
-					/* kernel placeholders */
-#if !defined(_POSIX_SOURCE) && defined(KERNEL)
-#define	O_MARK		00020		/* mark during gc() */
-#define	O_DEFER		00040		/* defer for next gc pass */
-#endif
-/*			00080		/* unused */
-#ifndef _POSIX_SOURCE
-#define	O_ASYNC		00100		/* signal pgrp when data ready */
-#define	FASYNC		O_ASYNC
-#define	O_SHLOCK	00200		/* shared file lock present */
-#define	O_EXLOCK	00400		/* exclusive file lock present */
-/*			00800		/* unused */
-#endif
-#define	O_CREAT		01000		/* create if nonexistant */
-#define	O_TRUNC		02000		/* truncate to zero length */
-#define	O_EXCL		04000		/* error if already exists */
-/*			08000		/* unused */
-
-/* defined by POSIX 1003.1; BSD default, so no bit required */
-#define	O_NOCTTY	0		/* don't assign controlling terminal */
-
-/* mask for file access modes */
-#define	O_ACCMODE	(O_RDONLY|O_WRONLY|O_RDWR)
-
-/* file segment locking set data type - information passed to system by user */
-struct flock {
-	short	l_type;
-	short	l_whence;
-	size_t	l_start;
-	size_t	l_len;		/* len = 0 means until end of file */
-	short	l_sysid;
-	int	l_pid;		/* should be pid_t */
-};
 
 #ifndef KERNEL
 #include <sys/cdefs.h>
 
 __BEGIN_DECLS
+int	open __P((const char *, int, ...));
 int	creat __P((const char *, mode_t));
 int	fcntl __P((int, int, ...));
-int	open __P((const char *, int, ...));
 __END_DECLS
 #endif
 
