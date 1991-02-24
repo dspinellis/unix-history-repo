c32662e31faa14afa876c639b84705311d0243bf
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-02-24 00:37:02 -0800

    redo all the exec routines from scratch in C (well, ANSI C) changed path to look at "." last don't let execlp/execvp ignore execve errors changed name from execvp.c to exec.c
    SCCS-vsn: lib/libc/gen/exec.c 5.5

--- usr/src/lib/libc/gen/exec.c
+++ usr/src/lib/libc/gen/exec.c
@@ -1,90 +1,165 @@
-#if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)exec.c	5.4 (Berkeley) %G%";
-#endif LIBC_SCCS and not lint
-
-/*
- *	execlp(name, arg,...,0)	(like execl, but does path search)
- *	execvp(name, argv)	(like execv, but does path search)
+/*-
+ * Copyright (c) 1991 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.redist.c%
  */
+
+#if defined(LIBC_SCCS) && !defined(lint)
+static char sccsid[] = "@(#)exec.c	5.5 (Berkeley) %G%";
+#endif /* LIBC_SCCS and not lint */
+
+#include <sys/param.h>
+#include <sys/types.h>
 #include <errno.h>
-#include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include <stdio.h>
+#include <paths.h>
 
-static	char shell[] =	"/bin/sh";
+extern char **environ;
 
-int
-execlp(name, argv)
-const char *name, *argv;
+static char **buildargv(va_list ap, const char *arg, char ***envpp)
 {
-	return(execvp(name, &argv));
+	register size_t max, off;
+	register char **argv;
+
+	for (off = max = 0;; ++off) {
+		if (off >= max) {
+			max += 50;	/* Starts out at 0. */
+			max *= 2;	/* Ramp up fast. */
+			if (!(argv = realloc(argv, max * sizeof(char *))))
+				return(NULL);
+			if (off == 0) {
+				argv[0] = (char *)arg;
+				off = 1;
+			}
+		}
+		if (!(argv[off] = va_arg(ap, char *)))
+			break;
+	}
+	/* Get environment pointer if need user supposed to provide one. */
+	if (envpp)
+		*envpp = va_arg(ap, char **);
+	return(argv);
 }
 
-int
-execvp(name, argv)
-const char *name;
-char * const *argv;
+int execl(const char *name, const char *arg, ...)
 {
-	char *pathstr;
-	register char *cp;
-	char fname[128];
-	char *newargs[256], *execat();
-	int i;
-	register unsigned etxtbsy = 1;
-	register eacces = 0;
+	va_list ap;
+	int sverrno;
+	char **argv;
 
-	if ((pathstr = getenv("PATH")) == NULL)
-		pathstr = ":/bin:/usr/bin";
-	cp = index(name, '/')? "": pathstr;
-
-	do {
-		cp = execat(cp, name, fname);
-	retry:
-		execv(fname, argv);
-		switch(errno) {
-		case ENOEXEC:
-			newargs[0] = "sh";
-			newargs[1] = fname;
-			for (i=1; newargs[i+1]=argv[i]; i++) {
-				if (i>=254) {
-					errno = E2BIG;
-					return(-1);
-				}
-			}
-			execv(shell, newargs);
-			return(-1);
-		case ETXTBSY:
-			if (++etxtbsy > 5)
-				return(-1);
-			sleep(etxtbsy);
-			goto retry;
-		case EACCES:
-			eacces++;
-			break;
-		case ENOMEM:
-		case E2BIG:
-			return(-1);
-		}
-	} while (cp);
-	if (eacces)
-		errno = EACCES;
+	va_start(ap, arg);
+	if (argv = buildargv(ap, arg, (char ***)NULL))
+		(void)execve(name, argv, environ);
+	va_end(ap);
+	sverrno = errno;
+	free(argv);
+	errno = sverrno;
 	return(-1);
 }
 
-static char *
-execat(s1, s2, si)
-register char *s1, *s2;
-char *si;
+int execle(const char *name, const char *arg, ...)
 {
-	register char *s;
+	va_list ap;
+	int sverrno;
+	char **argv, **envp;
 
-	s = si;
-	while (*s1 && *s1 != ':')
-		*s++ = *s1++;
-	if (si != s)
-		*s++ = '/';
-	while (*s2)
-		*s++ = *s2++;
-	*s = '\0';
-	return(*s1? ++s1: 0);
+	va_start(ap, arg);
+	if (argv = buildargv(ap, arg, &envp))
+		(void)execve(name, argv, envp);
+	va_end(ap);
+	sverrno = errno;
+	free(argv);
+	errno = sverrno;
+	return(-1);
+}
+
+int execlp(const char *name, const char *arg, ...)
+{
+	va_list ap;
+	int sverrno;
+	char **argv;
+
+	va_start(ap, arg);
+	if (argv = buildargv(ap, arg, (char ***)NULL))
+		(void)execvp(name, argv);
+	va_end(ap);
+	sverrno = errno;
+	free(argv);
+	errno = sverrno;
+	return(-1);
+}
+
+int execv(const char *name, char * const *argv)
+{
+	(void)execve(name, argv, environ);
+	return(-1);
+}
+
+int execvp(const char *name, char * const *argv)
+{
+	register char *p;
+	int eacces, etxtbsy;
+	char *cur, *path, buf[MAXPATHLEN];
+
+	/* If it's an absolute or relative path name, it's easy. */
+	if (index(name, '/')) {
+		(void)execve(name, argv, environ);
+		return(-1);
+	}
+
+	/* Get the path we're searching. */
+	if (!(path = getenv("PATH")))
+		path = _PATH_DEFPATH;
+	cur = path = strdup(path);
+
+	eacces = etxtbsy = 0;
+	while (p = strsep(&cur, ":")) {
+		/*
+		 * It's a SHELL path -- double, leading and trailing colons
+		 * mean the current directory.
+		 */
+		if (!*p)
+			p = ".";
+		(void)snprintf(buf, sizeof(buf), "%s/%s", p, name);
+
+retry:		(void)execve(buf, argv, environ);
+		switch(errno) {
+		case EACCES:
+			eacces = 1;
+			break;
+		case ENOENT:
+			break;
+		case ENOEXEC: {
+			register size_t cnt;
+			register char **ap;
+
+			for (cnt = 0, ap = (char **)argv; *ap; ++ap, ++cnt);
+			if (ap = malloc((cnt + 2) * sizeof(char *))) {
+				bcopy(argv, ap + 2, cnt * sizeof(char *));
+				ap[0] = "sh";
+				ap[1] = buf;
+				(void)execve(_PATH_BSHELL, ap, environ);
+				free(ap);
+			}
+			goto done;
+		}
+		case ETXTBSY:
+			if (etxtbsy < 3)
+				(void)sleep(++etxtbsy);
+			goto retry;
+		default:
+			goto done;
+		}
+	}
+	if (eacces)
+		errno = EACCES;
+	else if (!errno)
+		errno = ENOENT;
+done:	free(path);
+	return(-1);
 }
