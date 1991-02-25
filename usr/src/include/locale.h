e926ad6f8707236b1225e18bc286b98553fa852a
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-02-24 18:40:28 -0800

    first pass at function prototypes with cdefs.h
    SCCS-vsn: include/locale.h 5.2

--- usr/src/include/locale.h
+++ usr/src/include/locale.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)locale.h	5.1 (Berkeley) %G%
+ *	@(#)locale.h	5.2 (Berkeley) %G%
  */
 
 #ifndef _LOCALE_H_
@@ -44,12 +44,11 @@ struct lconv {
 
 #define	_LC_LAST	6		/* marks end */
 
-#if __STDC__ || c_plusplus
-char	*setlocale(int _category, const char *_locale);
-struct	lconv *localeconv(void);
-#else
-char	*setlocale();
-struct	lconv *localeconv();
-#endif
+#include <sys/cdefs.h>
+
+__BEGIN_DECLS
+struct lconv	*localeconv __P((void));
+char		*setlocale __P((int, const char *));
+__END_DECLS
 
 #endif /* _LOCALE_H_ */
