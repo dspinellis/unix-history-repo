88311b40e3d367565775004df315c67486ddcb54
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-02-24 22:08:55 -0800

    put "." at the end of the default path
    SCCS-vsn: include/paths.h 5.12

--- usr/src/include/paths.h
+++ usr/src/include/paths.h
@@ -4,10 +4,10 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)paths.h	5.11 (Berkeley) %G%
+ *	@(#)paths.h	5.12 (Berkeley) %G%
  */
 
-#define	_PATH_DEFPATH	":/bin:/usr/bin"
+#define	_PATH_DEFPATH	"/bin:/usr/bin:"
 
 #define	_PATH_BSHELL	"/bin/sh"
 #define	_PATH_CONSOLE	"/dev/console"
