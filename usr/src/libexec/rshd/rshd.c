6510f6379639348d518b69c28d11a185803db712
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-03-02 18:12:14 -0800

    I messed up the default path stuff
    SCCS-vsn: libexec/rshd/rshd.c 5.38

--- usr/src/libexec/rshd/rshd.c
+++ usr/src/libexec/rshd/rshd.c
@@ -12,7 +12,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)rshd.c	5.37 (Berkeley) %G%";
+static char sccsid[] = "@(#)rshd.c	5.38 (Berkeley) %G%";
 #endif /* not lint */
 
 /*
@@ -113,8 +113,9 @@ main(argc, argv)
 char	username[20] = "USER=";
 char	homedir[64] = "HOME=";
 char	shell[64] = "SHELL=";
+char	path[100] = "PATH=";
 char	*envinit[] =
-	    {homedir, shell, _PATH_DEFPATH, username, 0};
+	    {homedir, shell, path, username, 0};
 char	**environ;
 
 doit(fromp)
@@ -410,6 +411,7 @@ doit(fromp)
 	(void) setuid((uid_t)pwd->pw_uid);
 	environ = envinit;
 	strncat(homedir, pwd->pw_dir, sizeof(homedir)-6);
+	strcat(path, _PATH_DEFPATH);
 	strncat(shell, pwd->pw_shell, sizeof(shell)-7);
 	strncat(username, pwd->pw_name, sizeof(username)-6);
 	cp = rindex(pwd->pw_shell, '/');
