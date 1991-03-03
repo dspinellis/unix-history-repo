e44db7f5ba45f979e17024c241004c00babca5e4
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-03-02 23:33:19 -0800

    ANSI
    SCCS-vsn: old/mkhosts/mkhosts.c 5.5

--- usr/src/old/mkhosts/mkhosts.c
+++ usr/src/old/mkhosts/mkhosts.c
@@ -12,7 +12,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)mkhosts.c	5.4 (Berkeley) %G%";
+static char sccsid[] = "@(#)mkhosts.c	5.5 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/file.h>
