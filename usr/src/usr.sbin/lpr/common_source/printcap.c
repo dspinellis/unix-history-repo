9a129999450f7a1e9905db16d14d225552ae2a37
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-03-04 21:33:07 -0800

    don't define BUFSIZ unless necessary
    SCCS-vsn: usr.sbin/lpr/common_source/printcap.c 5.7

--- usr/src/usr.sbin/lpr/common_source/printcap.c
+++ usr/src/usr.sbin/lpr/common_source/printcap.c
@@ -6,17 +6,17 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)printcap.c	5.6 (Berkeley) %G%";
+static char sccsid[] = "@(#)printcap.c	5.7 (Berkeley) %G%";
 #endif /* not lint */
+
+#include <ctype.h>
+#include <stdio.h>
+#include "pathnames.h"
 
 #ifndef BUFSIZ
 #define	BUFSIZ	1024
 #endif
 #define MAXHOP	32	/* max number of tc= indirections */
-
-#include <ctype.h>
-#include <stdio.h>
-#include "pathnames.h"
 
 /*
  * termcap - routines for dealing with the terminal capability data base
