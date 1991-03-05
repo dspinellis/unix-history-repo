a1eea032c53399a89793749d81c3c8cc5d4e0ba4
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-03-04 18:30:15 -0800

    fix for pcc
    SCCS-vsn: bin/ls/ls.c 5.45

--- usr/src/bin/ls/ls.c
+++ usr/src/bin/ls/ls.c
@@ -15,7 +15,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)ls.c	5.44 (Berkeley) %G%";
+static char sccsid[] = "@(#)ls.c	5.45 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/param.h>
@@ -208,7 +208,7 @@ main(argc, argv)
 		printfcn = printcol;
 
 	if (!argc) {
-		char dot[] = ".";	/* ANSI says it's not writable.*/
+		static char dot[] = ".";
 
 		argc = 1;
 		argv[0] = dot;
