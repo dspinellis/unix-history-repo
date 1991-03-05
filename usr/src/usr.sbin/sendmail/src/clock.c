f0678a4e3b4aa3329e0d71531ad63da85a2d14c9
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-03-04 21:43:07 -0800

    ANSI
    SCCS-vsn: usr.sbin/sendmail/src/clock.c 5.10

--- usr/src/usr.sbin/sendmail/src/clock.c
+++ usr/src/usr.sbin/sendmail/src/clock.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)clock.c	5.9 (Berkeley) %G%";
+static char sccsid[] = "@(#)clock.c	5.10 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -130,7 +130,7 @@ clrevent(ev)
 **		calls the next function in EventQueue.
 */
 
-void
+static void
 tick()
 {
 	register time_t now;
