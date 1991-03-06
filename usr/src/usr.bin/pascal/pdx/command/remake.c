6b7bfa8e1f99ec5f4d1c8031cf50b2f19d827e79
Donn Seeley <donn@ucbvax.Berkeley.EDU>
1991-03-06 08:40:33 -0800

    Fix mktemp() call.  Don't know why I bother...
    SCCS-vsn: usr.bin/pascal/pdx/command/remake.c 5.2

--- usr/src/usr.bin/pascal/pdx/command/remake.c
+++ usr/src/usr.bin/pascal/pdx/command/remake.c
@@ -5,7 +5,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)remake.c	5.1 (Berkeley) %G%";
+static char sccsid[] = "@(#)remake.c	5.2 (Berkeley) %G%";
 #endif not lint
 
 /*
@@ -34,7 +34,7 @@ remake()
 	if (strcmp(objname, "obj") != 0) {
 	    call("mv", stdin, stdout, "obj", objname, NIL);
 	}
-	tmpfile = mktemp("/tmp/pdxXXXX");
+	tmpfile = mktemp(strdup("/tmp/pdxXXXX"));
 	setout(tmpfile);
 	status();
 	unsetout();
