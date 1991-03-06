9aa19b171f40c69baa005fa5c3613920755ef5ab
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-03-06 00:26:15 -0800

    assembly stuff didn't work, and I don't really see any need for it.
    SCCS-vsn: share/mk/bsd.lib.mk 5.21

--- usr/src/share/mk/bsd.lib.mk
+++ usr/src/share/mk/bsd.lib.mk
@@ -1,4 +1,4 @@
-#	@(#)bsd.lib.mk	5.20 (Berkeley) %G%
+#	@(#)bsd.lib.mk	5.21 (Berkeley) %G%
 
 .if exists(${.CURDIR}/../Makefile.inc)
 .include "${.CURDIR}/../Makefile.inc"
@@ -132,12 +132,6 @@ lint:
 tags: ${SRCS}
 	-cd ${.CURDIR}; ctags -f /dev/stdout ${.ALLSRC:M*.c} | \
 	    sed "s;\${.CURDIR}/;;" > tags
-.if !empty(SRCS:M*.s)
-	-cd ${.CURDIR}; egrep -o "^ENTRY(.*)|^SYSCALL(.*)" ${.ALLSRC:M*.s} | \
-	    sed \
-	    "s;\([^:]*\):\([^(]*\)(\([^, )]*\)\(.*\);\3 \`pwd\`/\1 /^\2(\3\4$$/;" \
-	    >> tags; sort -o tags tags
-.endif
 .endif
 
 .include <bsd.man.mk>
