754b7f536dee3f62515cfdb1628fa5d4fa980d8b
Donn Seeley <donn@ucbvax.Berkeley.EDU>
1991-03-08 06:11:23 -0800

    Change add() to copy strings.
    SCCS-vsn: usr.bin/hexdump/parse.c 5.5

--- usr/src/usr.bin/hexdump/parse.c
+++ usr/src/usr.bin/hexdump/parse.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)parse.c	5.4 (Berkeley) %G%";
+static char sccsid[] = "@(#)parse.c	5.5 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -45,14 +45,18 @@ addfile(name)
 	(void)fclose(fp);
 }
 
-add(fmt)
-	char *fmt;
+add(xfmt)
+	char *xfmt;
 {
 	register char *p;
 	static FS **nextfs;
 	FS *tfs;
 	FU *tfu, **nextfu;
 	char savech, *savep, *emalloc(), *strdup();
+	char *fmt;
+
+	if ((fmt = strdup(xfmt)) == NULL)
+		nomem();
 
 	/* start new linked list of format units */
 	/* NOSTRICT */
