738f580053f5e149fab6ce53dd18d04ea8d230f8
Donn Seeley <donn@ucbvax.Berkeley.EDU>
1991-03-08 00:47:52 -0800

    Fixed mktemp() of a string constant.
    SCCS-vsn: old/refer/sortbib/sortbib.c 4.5

--- usr/src/old/refer/sortbib/sortbib.c
+++ usr/src/old/refer/sortbib/sortbib.c
@@ -1,9 +1,10 @@
 #ifndef lint
-static char *sccsid = "@(#)sortbib.c	4.4 (Berkeley) %G%";
+static char *sccsid = "@(#)sortbib.c	4.5 (Berkeley) %G%";
 #endif
 
 #include <stdio.h>
 #include <signal.h>
+#include <string.h>
 #include "pathnames.h"
 
 #define BUF BUFSIZ
@@ -44,8 +45,7 @@ char *argv[];
 	for (i = 1; i < argc; i++)		/* open files in arg list */
 		if ((fp[i-1] = fopen(argv[i], "r")) == NULL)
 			error(argv[i]);
-	tempfile = _PATH_TMPS;			/* tempfile for sorting keys */
-	mktemp(tempfile);
+	tempfile = mktemp(strdup(_PATH_TMPS));	/* tempfile for sorting keys */
 	if (signal(SIGINT,SIG_IGN) != SIG_IGN)	/* remove if interrupted */
 		signal(SIGINT, onintr);
 	if ((tfp = fopen(tempfile, "w")) == NULL)
