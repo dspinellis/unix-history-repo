2beb76d6c0436365d8c8f54b5b3c9c0ca0140368
Donn Seeley <donn@ucbvax.Berkeley.EDU>
1991-03-08 01:08:17 -0800

    #define's don't have to end in ';'...
    SCCS-vsn: libexec/bugfiler/pathnames.h 5.4

--- usr/src/libexec/bugfiler/pathnames.h
+++ usr/src/libexec/bugfiler/pathnames.h
@@ -4,9 +4,9 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)pathnames.h	5.3 (Berkeley) %G%
+ *	@(#)pathnames.h	5.4 (Berkeley) %G%
  */
 
 #define	MAIL_CMD	"/usr/sbin/sendmail -i -t -F \"Bugs Bunny\" -f owner-bugs"
 #undef _PATH_TMP
-#define	_PATH_TMP	"/tmp/BUG_XXXXXX";
+#define	_PATH_TMP	"/tmp/BUG_XXXXXX"
