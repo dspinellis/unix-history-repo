176d6fd28c7f92b37b2337c2a16e0d6d139c6ce0
William Nesheim <william@ucbvax.Berkeley.EDU>
1991-03-10 02:04:26 -0800

    add vm_offset_t, vm_size_t; create {hp300,i386,tahoe,vax}/include/ansi.h
    SCCS-vsn: sys/i386/include/types.h 7.5

--- usr/src/sys/i386/include/types.h
+++ usr/src/sys/i386/include/types.h
@@ -4,36 +4,21 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)types.h	7.4 (Berkeley) %G%
+ *	@(#)types.h	7.5 (Berkeley) %G%
  */
 
 #ifndef	_MACHTYPES_H_
 #define	_MACHTYPES_H_
 
-/*
- * Types which are fundamental to the implementation and may appear in
- * more than one standard header are defined here.  Standard headers
- * then use:
- *	#ifdef	_SIZE_T_
- *	typedef	_SIZE_T_ size_t;
- *	#undef	_SIZE_T_
- *	#endif
- *
- * Thanks, ANSI!
- */
-#define	_CLOCK_T_	unsigned long		/* clock() */
-#define	_PTRDIFF_T_	int			/* ptr1 - ptr2 */
-#define	_SIZE_T_	unsigned int		/* sizeof() */
-#define	_TIME_T_	long			/* time() */
-#define	_VA_LIST_	char *			/* va_list */
-#define	_WCHAR_T_	unsigned short		/* wchar_t */
-
-#if !defined(_ANSI_SOURCE) && !defined(_POSIX_SOURCE)
 typedef struct _physadr {
 	int r[1];
 } *physadr;
+
 typedef struct label_t {
 	int val[6];
 } label_t;
-#endif
+
+typedef	u_long	vm_offset_t;
+typedef	u_long	vm_size_t;
+
 #endif	/* _MACHTYPES_H_ */
