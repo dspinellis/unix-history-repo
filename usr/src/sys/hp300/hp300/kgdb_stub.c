b19529be176a726ef6967fdd11826ffc8e894770
Mike Karels <karels@ucbvax.Berkeley.EDU>
1991-03-12 23:04:24 -0800

    update from van using newer protocol; add kgdb_acc to avoid kernacc before kernel_map is set
    SCCS-vsn: sys/hp300/hp300/kgdb_stub.c 7.6

--- usr/src/sys/hp300/hp300/kgdb_stub.c
+++ usr/src/sys/hp300/hp300/kgdb_stub.c
@@ -4,14 +4,14 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)kgdb_stub.c	7.5 (Berkeley) %G%
+ *	@(#)kgdb_stub.c	7.6 (Berkeley) %G%
  */
 /*
  * "Stub" to allow remote cpu to debug over a serial line using gdb.
  */
 #ifdef KGDB
 #ifndef lint
-static char rcsid[] = "$Header: kgdb_stub.c,v 1.6 91/03/05 01:15:03 van Exp $";
+static char rcsid[] = "$Header: kgdb_stub.c,v 1.9 91/03/08 07:03:13 van Locked $";
 #endif
 
 #include "param.h"
@@ -22,18 +22,13 @@ static char rcsid[] = "$Header: kgdb_stub.c,v 1.6 91/03/05 01:15:03 van Exp $";
 #include "machine/reg.h"
 #include "frame.h"
 #include "buf.h"
-#include "../hp300/cons.h"
+#include "cons.h"
 
 #include "kgdb_proto.h"
 #include "machine/remote-sl.h"
 
-extern void printf();
-extern void bcopy();
 extern int kernacc();
 extern void chgkprot();
-
-/* (XXX from trap.c) user-mode flag in type */
-#define	USER	0x20
 
 #ifndef KGDBDEV
 #define KGDBDEV -1
@@ -156,7 +151,7 @@ computeSignal(type)
 {
 	int sigval;
 
-	switch (type &~ USER) {
+	switch (type) {
 	case T_BUSERR:
 		sigval = SIGBUS;
 		break;
@@ -219,58 +214,38 @@ computeSignal(type)
 #define GDB_SR 16
 #define GDB_PC 17
 
-/*
- * # of additional bytes in 680x0 exception frame format n.
- */
-static int frame_bytes[16] = {
-	0, 0, sizeof(struct fmt2), 0,
-	0, 0, 0, 0,
-	0, sizeof(struct fmt9), sizeof(struct fmtA), sizeof(struct fmtB),
-	0, 0, 0, 0
-};
-
-/*
- * Translate the values stored in the kernel frame struct to the format
- * understood by gdb.
- */
-static void
-regs_to_gdb(fp, gdb_regs)
-	struct frame *fp;
-	u_long *gdb_regs;
+static inline void
+kgdb_copy(register u_char *src, register u_char *dst, register u_int nbytes)
 {
-	bcopy((caddr_t)fp->f_regs, (caddr_t)gdb_regs, sizeof(fp->f_regs) + 8);
+	register u_char *ep = src + nbytes;
+
+	while (src < ep)
+		*dst++ = *src++;
 }
 
-/*
- * Convert gdb register values to kernel format.
- */
-static void
-gdb_to_regs(fp, gdb_regs)
-	struct frame *fp;
-	u_long *gdb_regs;
-{
-	bcopy((caddr_t)gdb_regs, (caddr_t)fp->f_regs, sizeof(fp->f_regs) - 4);
-	fp->f_sr = gdb_regs[GDB_SR];
-	fp->f_pc = gdb_regs[GDB_PC];
-}
+#define regs_to_gdb(fp, regs) \
+	(kgdb_copy((u_char *)((fp)->f_regs), (u_char *)(regs), REGISTER_BYTES))
+
+#define gdb_to_regs(fp, regs) \
+	(kgdb_copy((u_char *)(regs), (u_char *)((fp)->f_regs), REGISTER_BYTES))
 
 static u_long reg_cache[NUM_REGS];
+static u_char inbuffer[SL_MAXMSG+1];
+static u_char outbuffer[SL_MAXMSG];
 
 /*
  * This function does all command procesing for interfacing to 
  * a remote gdb.
  */
 int 
-kgdb_trap(int type, unsigned code, unsigned v, struct frame *frame)
+kgdb_trap(int type, struct frame *frame)
 {
-	int i;
-	u_long length;
-	caddr_t addr;
-	u_char *cp;
-	int out, in;
-	int inlen, outlen;
-	static u_char inbuffer[SL_MAXMSG+1];
-	static u_char outbuffer[SL_MAXMSG];
+	register u_long len;
+	u_char *addr;
+	register u_char *cp;
+	register u_char out, in;
+	register int outlen;
+	int inlen;
 	u_long gdb_regs[NUM_REGS];
 
 	if (kgdb_dev < 0) {
@@ -284,25 +259,31 @@ kgdb_trap(int type, unsigned code, unsigned v, struct frame *frame)
 		}
 		kgdb_getc = constab[major(kgdb_dev)].cn_getc;
 		kgdb_putc = constab[major(kgdb_dev)].cn_putc;
-		if (kgdb_getc == 0 || kgdb_putc == 0) {
+		if (kgdb_getc == 0 || kgdb_putc == 0)
 			return (0);
-		}
+		/*
+		 * If the packet that woke us up isn't a signal packet,
+		 * ignore it since there is no active debugger.  Also,
+		 * we check that it's not an ack to be sure that the 
+		 * remote side doesn't send back a response after the
+		 * local gdb has exited.  Otherwise, the local host
+		 * could trap into gdb if it's running a gdb kernel too.
+		 */
+#ifdef notdef
+		in = GETC;
+		if (KGDB_CMD(in) != KGDB_SIGNAL || (in & KGDB_ACK) != 0)
+			return (0);
+#endif
+		while (GETC != FRAME_END)
+			;
+
 		kgdb_active = 1;
 	}
 	/*
 	 * Stick frame regs into our reg cache then tell remote host
 	 * that an exception has occured.
 	 */
-	if ((type & USER) == 0)
-		/*
-		 * After a kernel mode trap, the saved sp points at the
-		 * PSW and is useless.  The correct saved sp should be
-		 * the top of the frame.
-		 */
-		frame->f_regs[SP] = (int)&frame->F_u +
-				    frame_bytes[frame->f_format];
 	regs_to_gdb(frame, gdb_regs);
-
 	outbuffer[0] = computeSignal(type);
 	kgdb_send(KGDB_SIGNAL, outbuffer, 1);
 
@@ -313,29 +294,40 @@ kgdb_trap(int type, unsigned code, unsigned v, struct frame *frame)
 			continue;
 
 		out = in | KGDB_ACK;
-		switch (in) {
+		switch (KGDB_CMD(in)) {
 
 		case KGDB_SIGNAL:
-			outbuffer[0] = computeSignal(type);
-			outlen = 1;
-			break;
+			/*
+			 * if this command came from a running gdb,
+			 * answer it -- the other guy has no way of
+			 * knowing if we're in or out of this loop
+			 * when he issues a "remote-signal".  (Note
+			 * that without the length check, we could
+			 * loop here forever if the ourput line is
+			 * looped back or the remote host is echoing.)
+			 */
+			if (inlen == 0) {
+				outbuffer[0] = computeSignal(type);
+				kgdb_send(KGDB_SIGNAL, outbuffer, 1);
+			}
+			continue;
 
 		case KGDB_REG_R:
 		case KGDB_REG_R | KGDB_DELTA:
 			cp = outbuffer;
 			outlen = 0;
-			for (i = inbuffer[0]; i < NUM_REGS; ++i) {
-				if (reg_cache[i] != gdb_regs[i] ||
+			for (len = inbuffer[0]; len < NUM_REGS; ++len) {
+				if (reg_cache[len] != gdb_regs[len] ||
 				    (in & KGDB_DELTA) == 0) {
 					if (outlen + 5 > SL_MAXMSG) {
 						out |= KGDB_MORE;
 						break;
 					}
-					cp[outlen] = i;
-					bcopy(&gdb_regs[i], 
-					      &cp[outlen + 1], 4);
+					cp[outlen] = len;
+					kgdb_copy((u_char *)&gdb_regs[len],
+						  &cp[outlen + 1], 4);
+					reg_cache[len] = gdb_regs[len];
 					outlen += 5;
-					reg_cache[i] = gdb_regs[i];
 				}
 			}
 			break;
@@ -343,10 +335,11 @@ kgdb_trap(int type, unsigned code, unsigned v, struct frame *frame)
 		case KGDB_REG_W:
 		case KGDB_REG_W | KGDB_DELTA:
 			cp = inbuffer;
-			for (i = 0; i < inlen; i += 5) {
-				register int j = cp[i];
+			for (len = 0; len < inlen; len += 5) {
+				register int j = cp[len];
 
-				bcopy(&cp[i + 1], &gdb_regs[j], 4);
+				kgdb_copy(&cp[len + 1],
+					  (u_char *)&gdb_regs[j], 4);
 				reg_cache[j] = gdb_regs[j];
 			}
 			gdb_to_regs(frame, gdb_regs);
@@ -354,32 +347,32 @@ kgdb_trap(int type, unsigned code, unsigned v, struct frame *frame)
 			break;
 				
 		case KGDB_MEM_R:
-			length = inbuffer[0];
-			bcopy(&inbuffer[1], &addr, 4);
-			if (length + 1 > SL_MAXMSG) {
+			len = inbuffer[0];
+			kgdb_copy(&inbuffer[1], (u_char *)&addr, 4);
+			if (len + 1 > SL_MAXMSG) {
 				outlen = 1;
 				outbuffer[0] = E2BIG;
-			} else if (!kernacc(addr, length, B_READ)) {
+			} else if (!kgdb_acc(addr, len, B_READ)) {
 				outlen = 1;
 				outbuffer[0] = EFAULT;
 			} else {
-				outlen = length + 1;
+				outlen = len + 1;
 				outbuffer[0] = 0;
-				bcopy(addr, &outbuffer[1], length);
+				kgdb_copy(addr, &outbuffer[1], len);
 			}
 			break;
 
 		case KGDB_MEM_W:
-			length = inlen - 4;
-			bcopy(inbuffer, &addr, 4);
+			len = inlen - 4;
+			kgdb_copy(inbuffer, (u_char *)&addr, 4);
 			outlen = 1;
-			if (!kernacc(addr, length, B_READ))
+			if (!kgdb_acc(addr, len, B_READ))
 				outbuffer[0] = EFAULT;
 			else {
 				outbuffer[0] = 0;
-				if (!kernacc(addr, length, B_WRITE))
-					chgkprot(addr, length, B_WRITE);
-				bcopy(&inbuffer[4], addr, length);
+				if (!kgdb_acc(addr, len, B_WRITE))
+					chgkprot(addr, len, B_WRITE);
+				kgdb_copy(&inbuffer[4], addr, len);
 			}
 			break;
 
@@ -404,5 +397,23 @@ kgdb_trap(int type, unsigned code, unsigned v, struct frame *frame)
 		/* Send the reply */
 		kgdb_send(out, outbuffer, outlen);
 	}
+}
+
+/*
+ * XXX do kernacc call if safe, otherwise attempt
+ * to simulate by simple bounds-checking.
+ */
+kgdb_acc(addr, len, rw)
+	caddr_t addr;
+{
+	extern char proc0paddr[], u[];		/* XXX! */
+	extern char *kernel_map;		/* XXX! */
+
+	if (kernel_map != NULL)
+		return (kernacc(addr, len, rw));
+	if (addr < proc0paddr + UPAGES * NBPG  ||
+	    u <= addr && addr < u + UPAGES * NBPG)
+		return (1);
+	return (0);
 }
 #endif
