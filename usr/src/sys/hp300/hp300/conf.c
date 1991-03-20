621ba41d5c98166e065191821c3cf8d96b89f2b9
Mike Karels <karels@ucbvax.Berkeley.EDU>
1991-03-20 00:12:36 -0800

    nodev split to enodev/enxio; rename /dev/tty; try to fix conflict with "fd"
    SCCS-vsn: sys/hp300/hp300/conf.c 7.4

--- usr/src/sys/hp300/hp300/conf.c
+++ usr/src/sys/hp300/hp300/conf.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)conf.c	7.3 (Berkeley) %G%
+ *	@(#)conf.c	7.4 (Berkeley) %G%
  */
 
 #include "sys/param.h"
@@ -14,31 +14,31 @@
 #include "sys/tty.h"
 #include "sys/conf.h"
 
-int	nulldev(), nodev(), rawread(), rawwrite(), swstrategy();
+int	nullop(), enxio(), enodev(), rawread(), rawwrite(), swstrategy();
 
 #include "ct.h"
 #if NCT > 0
 int	ctopen(),ctclose(),ctstrategy(),ctread(),ctwrite(),ctdump(),ctioctl();
 #else
-#define	ctopen		nodev
-#define	ctclose		nodev
-#define	ctstrategy	nodev
-#define	ctread		nodev
-#define	ctwrite		nodev
-#define	ctdump		nodev
-#define	ctioctl		nodev
+#define	ctopen		enxio
+#define	ctclose		enxio
+#define	ctstrategy	enxio
+#define	ctread		enxio
+#define	ctwrite		enxio
+#define	ctdump		enxio
+#define	ctioctl		enxio
 #endif
  
 #include "rd.h"
 #if NRD > 0
 int	rdopen(),rdstrategy(),rdread(),rdwrite(),rddump(),rdioctl(),rdsize();
 #else
-#define	rdopen		nodev
-#define	rdstrategy	nodev
-#define	rdread		nodev
-#define	rdwrite		nodev
-#define	rddump		nodev
-#define	rdioctl		nodev
+#define	rdopen		enxio
+#define	rdstrategy	enxio
+#define	rdread		enxio
+#define	rdwrite		enxio
+#define	rddump		enxio
+#define	rdioctl		enxio
 #define	rdsize		0
 #endif
  
@@ -46,12 +46,12 @@ int	rdopen(),rdstrategy(),rdread(),rdwrite(),rddump(),rdioctl(),rdsize();
 #if NSD > 0
 int	sdopen(),sdstrategy(),sdread(),sdwrite(),sddump(),sdioctl(),sdsize();
 #else
-#define	sdopen		nodev
-#define	sdstrategy	nodev
-#define	sdread		nodev
-#define	sdwrite		nodev
-#define	sddump		nodev
-#define	sdioctl		nodev
+#define	sdopen		enxio
+#define	sdstrategy	enxio
+#define	sdread		enxio
+#define	sdwrite		enxio
+#define	sddump		enxio
+#define	sdioctl		enxio
 #define	sdsize		0
 #endif
  
@@ -59,50 +59,36 @@ int	sdopen(),sdstrategy(),sdread(),sdwrite(),sddump(),sdioctl(),sdsize();
 #if NCD > 0
 int	cdopen(),cdstrategy(),cdread(),cdwrite(),cddump(),cdioctl(),cdsize();
 #else
-#define	cdopen		nodev
-#define	cdstrategy	nodev
-#define	cdread		nodev
-#define	cdwrite		nodev
-#define	cddump		nodev
-#define	cdioctl		nodev
+#define	cdopen		enxio
+#define	cdstrategy	enxio
+#define	cdread		enxio
+#define	cdwrite		enxio
+#define	cddump		enxio
+#define	cdioctl		enxio
 #define	cdsize		0
 #endif
  
-#include "fd.h"
-#if NFD > 0
-int	fdopen(),fdstrategy(),fdread(),fdwrite(),fddump(),fdioctl(),fdsize();
-#else
-#define	fdopen		nodev
-#define	fdstrategy	nodev
-#define	fdread		nodev
-#define	fdwrite		nodev
-#define	fddump		nodev
-#define	fdioctl		nodev
-#define	fdsize		0
-#endif
-
 struct bdevsw	bdevsw[] =
 {
 	{ ctopen,	ctclose,	ctstrategy,	ctioctl,	/*0*/
 	  ctdump,	0,		B_TAPE },
-	{ nodev,	nodev,		nodev,		nodev,		/*1*/
-	  nodev,	0,		0 },
-	{ rdopen,	nulldev,	rdstrategy,	rdioctl,	/*2*/
+	{ enxio,	enxio,		enxio,		enxio,		/*1*/
+	  enxio,	0,		0 },
+	{ rdopen,	nullop,		rdstrategy,	rdioctl,	/*2*/
 	  rddump,	rdsize,		0 },
-	{ nodev,	nodev,		swstrategy,	nodev,		/*3*/
-	  nodev,	0,		0 },
-	{ sdopen,	nulldev,	sdstrategy,	sdioctl,	/*4*/
+	{ enodev,	enodev,		swstrategy,	enodev,		/*3*/
+	  enodev,	0,		0 },
+	{ sdopen,	nullop,		sdstrategy,	sdioctl,	/*4*/
 	  sddump,	sdsize,		0 },
-	{ cdopen,	nulldev,	cdstrategy,	cdioctl,	/*5*/
+	{ cdopen,	nullop,		cdstrategy,	cdioctl,	/*5*/
 	  cddump,	cdsize,		0 },
-	{ fdopen,	nulldev,	fdstrategy,	fdioctl,	/*6*/
-	  fddump,	fdsize,		0 },
+	/* 6 was "fd" vnode device */
 };
 int	nblkdev = sizeof (bdevsw) / sizeof (bdevsw[0]);
 
-int	cnopen(),cnclose(),cnread(),cnwrite(),cnioctl(),cnselect();
+int	cnopen(), cnclose(), cnread(),  cnwrite(), cnioctl(), cnselect();
 
-int	syopen(),syread(),sywrite(),syioctl(),syselect();
+int	cttyopen(), cttyread(), cttywrite(), cttyioctl(), cttyselect();
 
 int 	mmrw();
 #define	mmselect	seltrue
@@ -114,29 +100,29 @@ int	ptcopen(),ptcclose(),ptcread(),ptcwrite(),ptcselect();
 int	ptyioctl();
 struct	tty pt_tty[];
 #else
-#define ptsopen		nodev
-#define ptsclose	nodev
-#define ptsread		nodev
-#define ptswrite	nodev
-#define ptcopen		nodev
-#define ptcclose	nodev
-#define ptcread		nodev
-#define ptcwrite	nodev
-#define ptyioctl	nodev
+#define ptsopen		enxio
+#define ptsclose	enxio
+#define ptsread		enxio
+#define ptswrite	enxio
+#define ptcopen		enxio
+#define ptcclose	enxio
+#define ptcread		enxio
+#define ptcwrite	enxio
+#define ptyioctl	enxio
 #define	pt_tty		0
-#define	ptcselect	nodev
-#define	ptsstop		nulldev
+#define	ptcselect	enxio
+#define	ptsstop		nullop
 #endif
 
 #include "ppi.h"
 #if NPPI > 0
 int	ppiopen(),ppiclose(),ppiread(),ppiwrite(),ppiioctl();
 #else
-#define ppiopen		nodev
-#define ppiclose	nodev
-#define ppiread		nodev
-#define ppiwrite	nodev
-#define ppiioctl	nodev
+#define ppiopen		enxio
+#define ppiclose	enxio
+#define ppiread		enxio
+#define ppiwrite	enxio
+#define ppiioctl	enxio
 #endif
 
 #include "ite.h"
@@ -144,12 +130,12 @@ int	ppiopen(),ppiclose(),ppiread(),ppiwrite(),ppiioctl();
 int	iteopen(),iteclose(),iteread(),itewrite(),iteioctl();
 struct	tty ite_tty[];
 #else
-#define iteopen		nodev
-#define iteclose	nodev
-#define iteread		nodev
-#define itewrite	nodev
-#define itestop		nulldev
-#define iteioctl	nodev
+#define iteopen		enxio
+#define iteclose	enxio
+#define iteread		enxio
+#define itewrite	enxio
+#define itestop		nullop
+#define iteioctl	enxio
 #define ite_tty		0
 #endif
 
@@ -158,12 +144,12 @@ struct	tty ite_tty[];
 int	dcaopen(),dcaclose(),dcaread(),dcawrite(),dcastop(),dcaioctl();
 struct	tty dca_tty[];
 #else
-#define dcaopen		nodev
-#define dcaclose	nodev
-#define dcaread		nodev
-#define dcawrite	nodev
-#define dcastop		nulldev
-#define dcaioctl	nodev
+#define dcaopen		enxio
+#define dcaclose	enxio
+#define dcaread		enxio
+#define dcawrite	enxio
+#define dcastop		nullop
+#define dcaioctl	enxio
 #define dca_tty		0
 #endif
 
@@ -172,12 +158,12 @@ struct	tty dca_tty[];
 int	dcmopen(),dcmclose(),dcmread(),dcmwrite(),dcmstop(),dcmioctl();
 struct	tty dcm_tty[];
 #else
-#define dcmopen		nodev
-#define dcmclose	nodev
-#define dcmread		nodev
-#define dcmwrite	nodev
-#define dcmstop		nulldev
-#define dcmioctl	nodev
+#define dcmopen		enxio
+#define dcmclose	enxio
+#define dcmread		enxio
+#define dcmwrite	enxio
+#define dcmstop		nullop
+#define dcmioctl	enxio
 #define dcm_tty		0
 #endif
 
@@ -185,13 +171,15 @@ struct	tty dcm_tty[];
 #if NCLOCK > 0
 int	clockopen(),clockclose(),clockioctl(),clockmap();
 #else
-#define clockopen	nodev
-#define clockclose	nodev
-#define clockioctl	nodev
-#define clockmap	nodev
+#define clockopen	enxio
+#define clockclose	enxio
+#define clockioctl	enxio
+#define clockmap	enxio
 #endif
 
 int	logopen(),logclose(),logread(),logioctl(),logselect();
+
+int	fdopen();
 
 int	ttselect(), seltrue();
 
@@ -201,66 +189,78 @@ int	hilopen(),hilclose(),hilioctl(),hilmap(),hilselect(),hilread();
 
 struct cdevsw	cdevsw[] =
 {
-	cnopen,		cnclose,	cnread,		cnwrite,	/*0*/
-	cnioctl,	nulldev,	nulldev,	NULL,
-	cnselect,	nodev,		NULL,
-	syopen,		nulldev,	syread,		sywrite,	/*1*/
-	syioctl,	nulldev,	nulldev,	NULL,
-	syselect,	nodev,		NULL,
-	nulldev,	nulldev,	mmrw,		mmrw,		/*2*/
-	nodev,		nulldev,	nulldev,	NULL,
-	mmselect,	nodev,		NULL,
-	nulldev,	nulldev,	rawread,	rawwrite,	/*3*/
-	nodev,		nodev,		nulldev,	NULL,
-	nodev,		nodev,		swstrategy,
-	ptsopen,	ptsclose,	ptsread,	ptswrite,	/*4*/
-	ptyioctl,	ptsstop,	nulldev,	pt_tty,
-	ttselect,	nodev,		NULL,
-	ptcopen,	ptcclose,	ptcread,	ptcwrite,	/*5*/
-	ptyioctl,	nulldev,	nulldev,	pt_tty,
-	ptcselect,	nodev,		NULL,
-	logopen,	logclose,	logread,	nodev,		/*6*/
-	logioctl,	nodev,		nulldev,	NULL,
-	logselect,	nodev,		NULL,
-	ctopen,		ctclose,	ctread,		ctwrite,	/*7*/
-	ctioctl,	nodev,		nulldev,	NULL,
-	seltrue,	nodev,		ctstrategy,
-	sdopen,		nulldev,	sdread,		sdwrite,	/*8*/
-	sdioctl,	nodev,		nulldev,	NULL,
-	seltrue,	nodev,		sdstrategy,
-	rdopen,		nulldev,	rdread,		rdwrite,	/*9*/
-	rdioctl,	nodev,		nulldev,	NULL,
-	seltrue,	nodev,		rdstrategy,
-	grfopen,	grfclose,	nulldev,	nulldev,	/*10*/
-	grfioctl,	nodev,		nulldev,	NULL,
-	grfselect,	grfmap,		NULL,
-	ppiopen,	ppiclose,	ppiread,	ppiwrite,	/*11*/
-	ppiioctl,	nodev,		nulldev,	NULL,
-	nodev,		nodev,		NULL,
-	dcaopen,	dcaclose,	dcaread,	dcawrite,	/*12*/
-	dcaioctl,	dcastop,	nulldev,	dca_tty,
-	ttselect,	nodev,		NULL,
-	iteopen,	iteclose,	iteread,	itewrite,	/*13*/
-	iteioctl,	nodev,		nulldev,	ite_tty,
-	ttselect,	nodev,		NULL,
-	hilopen,	hilclose,	hilread,	nulldev,	/*14*/
-	hilioctl,	nodev,		nulldev,	NULL,
-	hilselect,	hilmap,		NULL,
-	dcmopen,	dcmclose,	dcmread,	dcmwrite,	/*15*/
-	dcmioctl,	dcmstop,	nulldev,	dcm_tty,
-	ttselect,	nodev,		NULL,
-	nodev,		nodev,		nodev,		nodev,		/*16*/
-	nodev,		nodev,		nulldev,	NULL,
-	seltrue,	nodev,		NULL,
-	cdopen,		nulldev,	cdread,		cdwrite,	/*17*/
-	cdioctl,	nodev,		nulldev,	NULL,
-	seltrue,	nodev,		cdstrategy,
-	clockopen,	clockclose,	nulldev,	nulldev,	/*18*/
-	clockioctl,	nodev,		nulldev,	NULL,
-	nulldev,	clockmap,	NULL,
-	fdopen,		nulldev,	fdread,		fdwrite,	/*19*/
-	fdioctl,	nodev,		nulldev,	NULL,
-	seltrue,	nodev,		NULL,
+	{ cnopen,	cnclose,	cnread,		cnwrite,	/*0*/
+	  cnioctl,	nullop,		nullop,		NULL,
+	  cnselect,	enodev,		NULL },
+	{ cttyopen,	nullop,		cttyread,	cttywrite,	/*1*/
+	  cttyioctl,	nullop,		nullop,		NULL,
+	  cttyselect,	enodev,		NULL },
+	{ nullop,	nullop,		mmrw,		mmrw,		/*2*/
+	  enodev,	nullop,		nullop,		NULL,
+	  mmselect,	enodev,		NULL },
+	{ nullop,	nullop,		rawread,	rawwrite,	/*3*/
+	  enodev,	enodev,		nullop,		NULL,
+	  enodev,	enodev,		swstrategy },
+	{ ptsopen,	ptsclose,	ptsread,	ptswrite,	/*4*/
+	  ptyioctl,	ptsstop,	nullop,		pt_tty,
+	  ttselect,	enodev,		NULL },
+	{ ptcopen,	ptcclose,	ptcread,	ptcwrite,	/*5*/
+	  ptyioctl,	nullop,		nullop,		pt_tty,
+	  ptcselect,	enodev,		NULL },
+	{ logopen,	logclose,	logread,	enodev,		/*6*/
+	  logioctl,	enodev,		nullop,		NULL,
+	  logselect,	enodev,		NULL },
+	{ ctopen,	ctclose,	ctread,		ctwrite,	/*7*/
+	  ctioctl,	enodev,		nullop,		NULL,
+	  seltrue,	enodev,		ctstrategy },
+	{ sdopen,	nullop,		sdread,		sdwrite,	/*8*/
+	  sdioctl,	enodev,		nullop,		NULL,
+	  seltrue,	enodev,		sdstrategy },
+	{ rdopen,	nullop,		rdread,		rdwrite,	/*9*/
+	  rdioctl,	enodev,		nullop,		NULL,
+	  seltrue,	enodev,		rdstrategy },
+	{ grfopen,	grfclose,	nullop,		nullop,		/*10*/
+	  grfioctl,	enodev,		nullop,		NULL,
+	  grfselect,	grfmap,		NULL },
+	{ ppiopen,	ppiclose,	ppiread,	ppiwrite,	/*11*/
+	  ppiioctl,	enodev,		nullop,		NULL,
+	  enodev,	enodev,		NULL },
+	{ dcaopen,	dcaclose,	dcaread,	dcawrite,	/*12*/
+	  dcaioctl,	dcastop,	nullop,		dca_tty,
+	  ttselect,	enodev,		NULL },
+	{ iteopen,	iteclose,	iteread,	itewrite,	/*13*/
+	  iteioctl,	enodev,		nullop,		ite_tty,
+	  ttselect,	enodev,		NULL },
+	{ hilopen,	hilclose,	hilread,	nullop,		/*14*/
+	  hilioctl,	enodev,		nullop,		NULL,
+	  hilselect,	hilmap,		NULL },
+	{ dcmopen,	dcmclose,	dcmread,	dcmwrite,	/*15*/
+	  dcmioctl,	dcmstop,	nullop,		dcm_tty,
+	  ttselect,	enodev,		NULL },
+	{ enodev,	enodev,		enodev,		enodev,		/*16*/
+	  enodev,	enodev,		nullop,		NULL,
+	  seltrue,	enodev,		NULL },
+	{ cdopen,	nullop,		cdread,		cdwrite,	/*17*/
+	  cdioctl,	enodev,		nullop,		NULL,
+	  seltrue,	enodev,		cdstrategy },
+	{ clockopen,	clockclose,	nullop,		nullop,		/*18*/
+	  clockioctl,	enodev,		nullop,		NULL,
+	  nullop,	clockmap,	NULL },
+#ifdef notdef
+	{ fdopen,	nullop,		fdread,		fdwrite,	/*19*/
+	  fdioctl,	enodev,		nullop,		NULL,
+	  seltrue,	enodev,		NULL },
+#else
+	{ enodev,	enodev,		enodev,		enodev,		/*19*/
+	  enodev,	enodev,		nullop,		NULL,
+	  seltrue,	enodev,		NULL },
+#endif
+	{ enodev,	enodev,		enodev,		enodev,		/*20*/
+	  enodev,	enodev,		nullop,		NULL,
+	  seltrue,	enodev,		NULL },
+	{ fdopen,	enodev,		enodev,		enodev,		/*21*/
+	  enodev,	enodev,		enodev,		NULL,
+	  enodev,	enodev,		NULL },
 };
 int	nchrdev = sizeof (cdevsw) / sizeof (cdevsw[0]);
 
