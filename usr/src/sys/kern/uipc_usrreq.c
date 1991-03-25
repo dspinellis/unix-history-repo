c6e1f8ecdcdc4c3fe979cb00b466096fa81020de
Mike Karels <karels@ucbvax.Berkeley.EDU>
1991-03-24 22:43:47 -0800

    rm user.h
    SCCS-vsn: sys/kern/uipc_usrreq.c 7.24

--- usr/src/sys/kern/uipc_usrreq.c
+++ usr/src/sys/kern/uipc_usrreq.c
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 1982, 1986 Regents of the University of California.
  *
- *	@(#)uipc_usrreq.c	7.23 (Berkeley) %G%
+ *	@(#)uipc_usrreq.c	7.24 (Berkeley) %G%
  */
 
 #include "param.h"
