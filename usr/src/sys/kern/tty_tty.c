d23a82d7dd0d2be114330363d0673576f4f71c5a
Mike Karels <karels@ucbvax.Berkeley.EDU>
1991-03-24 23:17:21 -0800

    new device calling conventions
    SCCS-vsn: sys/kern/tty_tty.c 7.11

--- usr/src/sys/kern/tty_tty.c
+++ usr/src/sys/kern/tty_tty.c
@@ -3,7 +3,7 @@
  * All rights reserved.  The Berkeley software License Agreement
  * specifies the terms and conditions for redistribution.
  *
- *	@(#)tty_tty.c	7.10 (Berkeley) %G%
+ *	@(#)tty_tty.c	7.11 (Berkeley) %G%
  */
 
 /*
