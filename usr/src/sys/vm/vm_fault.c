6cde072cc0093d424b92554b1b2ad075d1ef865c
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1991-03-27 19:32:17 -0800

    copyright revision request from CMU
    SCCS-vsn: sys/vm/vm_fault.c 7.4

--- usr/src/sys/vm/vm_fault.c
+++ usr/src/sys/vm/vm_fault.c
@@ -1,5 +1,16 @@
 /* 
- * Copyright (c) 1987 Carnegie-Mellon University.
+ * Copyright (c) 1991 Regents of the University of California.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to Berkeley by
+ * The Mach Operating System project at Carnegie-Mellon University.
+ *
+ * %sccs.include.redist.c%
+ *
+ *	@(#)vm_fault.c	7.4 (Berkeley) %G%
+ *
+ *
+ * Copyright (c) 1987, 1990 Carnegie-Mellon University.
  * All rights reserved.
  *
  * Authors: Avadis Tevanian, Jr., Michael Wayne Young
@@ -14,25 +25,15 @@
  * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND 
  * FOR ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.
  * 
- * Carnegie Mellon requests users of this software to return to Carnegie
- * any improvements or extensions that they make and grant Carnegie
- * the rights to redistribute these changes.  We can be reached at
- * mach@cs.cmu.edu or
- *	Project Mach
- *	School of Computer Science
- *	Carnegie Mellon University
- *	Pittsburgh PA 15213-3890
+ * Carnegie Mellon requests users of this software to return to
  *
+ *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU
+ *  School of Computer Science
+ *  Carnegie Mellon University
+ *  Pittsburgh PA 15213-3890
  *
- * Copyright (c) 1991 Regents of the University of California.
- * All rights reserved.
- *
- * This code is derived from software contributed to Berkeley by
- * The Mach Operating System project at Carnegie-Mellon University.
- *
- * %sccs.include.redist.c%
- *
- *	@(#)vm_fault.c	7.3 (Berkeley) %G%
+ * any improvements or extensions that they make and grant Carnegie the
+ * rights to redistribute these changes.
  */
 
 /*
