f6401c3f721d1dfc78fa8ea957734c8cc2acb56e
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-04-02 13:44:24 -0800

    add XPG, Sun, Sys V math constants
    SCCS-vsn: include/math.h 5.6

--- usr/src/include/math.h
+++ usr/src/include/math.h
@@ -9,21 +9,35 @@
  * experiences (good or bad) with these elementary function codes, using
  * the sendbug(8) program, to the authors.
  *
- *	@(#)math.h	5.5 (Berkeley) %G%
+ *	@(#)math.h	5.6 (Berkeley) %G%
  */
 
 #ifndef	_MATH_H_
 #define	_MATH_H_
 
 #if defined(vax) || defined(tahoe)
-#define	HUGE_VAL	1.701411733192644270e38
+#define	HUGE_VAL	1.701411733192644270e+38
 #else
-#define	HUGE_VAL	1e500		/* positive infinity */
+#define	HUGE_VAL	1e500			/* IEEE: positive infinity */
 #endif
 
 #if !defined(_ANSI_SOURCE) && !defined(_POSIX_SOURCE)
 #define	HUGE	HUGE_VAL
 #endif
+
+#define	M_E		2.7182818284590452354	/* e */
+#define	M_LOG2E		1.4426950408889634074	/* log 2e */
+#define	M_LOG10E	0.43429448190325182765	/* log 10e */
+#define	M_LN2		0.69314718055994530942	/* log e2 */
+#define	M_LN10		2.30258509299404568402	/* log e10 */
+#define	M_PI		3.14159265358979323846	/* pi */
+#define	M_PI_2		1.57079632679489661923	/* pi/2 */
+#define	M_PI_4		0.78539816339744830962	/* pi/4 */
+#define	M_1_PI		0.31830988618379067154	/* 1/pi */
+#define	M_2_PI		0.63661977236758134308	/* 2/pi */
+#define	M_2_SQRTPI	1.12837916709551257390	/* 2/sqrt(pi) */
+#define	M_SQRT2		1.41421356237309504880	/* sqrt(2) */
+#define	M_SQRT1_2	0.70710678118654752440	/* 1/sqrt(2) */
 
 #include <sys/cdefs.h>
 
