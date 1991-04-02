aa3714748badcd1efe94794cd4c4057372502009
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-04-01 16:01:11 -0800

    data is a void *
    SCCS-vsn: include/db.h 5.9

--- usr/src/include/db.h
+++ usr/src/include/db.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)db.h	5.8 (Berkeley) %G%
+ *	@(#)db.h	5.9 (Berkeley) %G%
  */
 
 #ifndef _DB_H_
@@ -27,7 +27,7 @@
 
 /* key/data structure -- a data-base thang */
 typedef struct {
-	unsigned char *data;
+	void *data;
 	int size;
 } DBT;
 
