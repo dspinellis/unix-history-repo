accdb367dd9aa47dabe8b3a5b76d3e6f890f76ee
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-04-02 22:18:34 -0800

    get the SCCS id back
    SCCS-vsn: bin/csh/exec.c 5.10

--- usr/src/bin/csh/exec.c
+++ usr/src/bin/csh/exec.c
@@ -1,12 +1,13 @@
-/*
- * Copyright (c) 1980 Regents of the University of California.
- * All rights reserved.  The Berkeley Software License Agreement
- * specifies the terms and conditions for redistribution.
+/*-
+ * Copyright (c) 1980 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char *sccsid = "@(#)sh.exec.c	5.7 (Berkeley) 5/27/90";
-#endif
+static char sccsid[] = "@(#)exec.c	5.10 (Berkeley) %G%";
+#endif /* not lint */
 
 #include "sh.h"
 #include <sys/dir.h>
