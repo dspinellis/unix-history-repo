4aafafbf6dbdb728cfa453e41c77863d751474da
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-04-03 23:50:55 -0800

    move stdarg.h to machine dependent include directory
    SCCS-vsn: include/stdarg.h 5.6

--- usr/src/include/stdarg.h
+++ usr/src/include/stdarg.h
@@ -4,24 +4,12 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)stdarg.h	5.5 (Berkeley) %G%
+ *	@(#)stdarg.h	5.6 (Berkeley) %G%
  */
 
 #ifndef _STDARG_H
 #define	_STDARG_H
 
-typedef char *va_list;
-
-#define	va_arg(ap, type) \
-	((type *)(ap += sizeof(type) < sizeof(int) ? \
-		(abort(), 0) : sizeof(type)))[-1]
-
-#define	va_end(ap)
-
-#define	__va_promote(type) \
-	(((sizeof(type) + sizeof(int) - 1) / sizeof(int)) * sizeof(int))
-
-#define	va_start(ap, last) \
-	(ap = ((char *)&(last) + __va_promote(last)))
+#include <machine/stdarg.h>
 
 #endif /* !_STDARG_H */
