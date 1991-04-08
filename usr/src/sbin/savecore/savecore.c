3c1fa72444aba02bb9a3df551b009ff4b5b9348f
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-04-08 15:27:28 -0800

    savecore can print out the message as easily as /etc/rc
    SCCS-vsn: sbin/savecore/savecore.c 5.26

--- usr/src/sbin/savecore/savecore.c
+++ usr/src/sbin/savecore/savecore.c
@@ -12,7 +12,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)savecore.c	5.25 (Berkeley) %G%";
+static char sccsid[] = "@(#)savecore.c	5.26 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/param.h>
