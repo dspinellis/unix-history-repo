594f7da82270a230fb72bc36f40553e736ec321c
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-04-09 01:03:44 -0800

    new copyright; att/bsd/shared
    SCCS-vsn: games/fortune/tools/Do_spell 5.2
    SCCS-vsn: games/fortune/fortune/fortune.c 5.13
    SCCS-vsn: games/fortune/tools/Do_troff 5.4
    SCCS-vsn: games/fortune/fortune/pathnames.h 5.2
    SCCS-vsn: games/fortune/strfile/strfile.c 5.12
    SCCS-vsn: games/fortune/strfile/strfile.h 5.8
    SCCS-vsn: games/fortune/unstr/unstr.c 5.7
    SCCS-vsn: games/fortune/unstr/unstr.c 5.8
    SCCS-vsn: games/hangman/playgame.c 5.3
    SCCS-vsn: games/hangman/prdata.c 5.3
    SCCS-vsn: games/hangman/prman.c 5.3
    SCCS-vsn: games/hangman/prword.c 5.3
    SCCS-vsn: games/hangman/setup.c 5.4
    SCCS-vsn: games/monop/mon.dat 5.3
    SCCS-vsn: games/monop/monop.def 5.3
    SCCS-vsn: games/monop/monop.ext 5.3
    SCCS-vsn: games/monop/prop.dat 5.3
    SCCS-vsn: games/monop/brd.dat 5.4
    SCCS-vsn: games/quiz/quiz.6 6.2
    SCCS-vsn: games/quiz/quiz.c 4.8

--- usr/src/games/fortune/strfile/strfile.c
+++ usr/src/games/fortune/strfile/strfile.c
@@ -1,21 +1,11 @@
-/*
+/*-
  * Copyright (c) 1989 The Regents of the University of California.
  * All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Ken Arnold.
  *
- * Redistribution and use in source and binary forms are permitted
- * provided that the above copyright notice and this paragraph are
- * duplicated in all such forms and that any documentation,
- * advertising materials, and other materials related to such
- * distribution and use acknowledge that the software was developed
- * by the University of California, Berkeley.  The name of the
- * University may not be used to endorse or promote products derived
- * from this software without specific prior written permission.
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ * %sccs.include.redist.c%
  */
 
 #ifndef lint
@@ -25,7 +15,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)strfile.c	5.11 (Berkeley) %G%";
+static char sccsid[] = "@(#)strfile.c	5.12 (Berkeley) %G%";
 #endif /* not lint */
 
 # include	<machine/endian.h>
