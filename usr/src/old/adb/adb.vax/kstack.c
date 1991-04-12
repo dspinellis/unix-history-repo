864e51bc745cd0fd364e9ff8120fb72abd8755b6
Keith Sklower <sklower@ucbvax.Berkeley.EDU>
1991-04-11 18:49:15 -0800

    kernel reorg
    SCCS-vsn: old/adb/adb.vax/kstack.c 5.3

--- usr/src/old/adb/adb.vax/kstack.c
+++ usr/src/old/adb/adb.vax/kstack.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)kstack.c	5.2 (Berkeley) %G%";
+static char sccsid[] = "@(#)kstack.c	5.3 (Berkeley) %G%";
 #endif /* not lint */
 
 /*
@@ -18,7 +18,7 @@ static char sccsid[] = "@(#)kstack.c	5.2 (Berkeley) %G%";
 #include <ctype.h>
 #include <machine/pte.h>
 #include <machine/frame.h>
-#include <machine/rpb.h>
+#include <vax/vax/rpb.h>
 
 struct	pte *sbr;
 int	slr;
