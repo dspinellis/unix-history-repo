03c5e58125bf1ec842b7445b74370159d5c38086
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-04-12 20:40:37 -0800

    date and time created 91/04/12 13:40:37 by bostic
    SCCS-vsn: lib/libc/vax/sys/cache.lib/getppid.c 5.1

---
+++ usr/src/lib/libc/vax/sys/cache.lib/getppid.c
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 1983 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms are permitted
+ * provided that the above copyright notice and this paragraph are
+ * duplicated in all such forms and that any documentation,
+ * advertising materials, and other materials related to such
+ * distribution and use acknowledge that the software was developed
+ * by the University of California, Berkeley.  The name of the
+ * University may not be used to endorse or promote products derived
+ * from this software without specific prior written permission.
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+#if defined(LIBC_SCCS) && !defined(lint)
+	.asciz "@(#)getppid.c	5.1 (Berkeley) %G%"
+#endif /* LIBC_SCCS and not lint */
+
+#include "SYS.h"
+
+	.data
+	.globl	mypid, myppid
+myppid:
+	.long	0
+	.text
+
+ENTRY(getpid)
+	movl	myppid,r0	# check cache
+	beql	doit
+	ret
+doit:
+	chmk	$SYS_getpid
+	jcs	err
+	movl	r0,mypid	# cache pid
+	movl	r1,r0
+	movl	r0,myppid	# set cache
+	ret			# ppid = getppid();
+err:
+	jmp cerror;
