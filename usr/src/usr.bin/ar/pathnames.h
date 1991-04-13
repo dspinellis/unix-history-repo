0cc701f3d0574254e2686ec04d49eaf1e1f19fd5
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-04-12 21:29:11 -0800

    lost the redist include line somewhere
    SCCS-vsn: usr.bin/ar/archive.h 5.8
    SCCS-vsn: usr.bin/ar/pathnames.h 5.4

--- usr/src/usr.bin/ar/pathnames.h
+++ usr/src/usr.bin/ar/pathnames.h
@@ -1,11 +1,13 @@
 /*-
- * Copyright (c) 1990 The Regents of the University of California.
+ * Copyright (c) 1991 The Regents of the University of California.
  * All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Hugh Smith at The University of Guelph.
  *
- *	@(#)pathnames.h	5.3 (Berkeley) %G%
+ * %sccs.include.redist.c%
+ *
+ *	@(#)pathnames.h	5.4 (Berkeley) %G%
  */
 
 #define	_NAME_ARTMP	"ar.XXXXXX"
