eb24432572e2bc70b402e78ef5e16e47b745e14c
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-04-12 21:29:11 -0800

    lost the redist include line somewhere
    SCCS-vsn: usr.bin/ar/archive.h 5.8
    SCCS-vsn: usr.bin/ar/pathnames.h 5.4

--- usr/src/usr.bin/ar/archive.h
+++ usr/src/usr.bin/ar/archive.h
@@ -1,11 +1,13 @@
 /*-
- * Copyright (c) 1990 The Regents of the University of California.
+ * Copyright (c) 1991 The Regents of the University of California.
  * All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Hugh Smith at The University of Guelph.
  *
- *	@(#)archive.h	5.7 (Berkeley) %G%
+ * %sccs.include.redist.c%
+ *
+ *	@(#)archive.h	5.8 (Berkeley) %G%
  */
 
 /* Ar(1) options. */
