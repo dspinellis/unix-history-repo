5b3b4fae0ea95724a1295dc02ac175e7fbf996cc
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-04-12 21:00:04 -0800

    new copyright; att/bsd/shared
    SCCS-vsn: lib/libcompat/4.3/vax/nargs.s 5.4

--- usr/src/lib/libcompat/4.3/vax/nargs.s
+++ usr/src/lib/libcompat/4.3/vax/nargs.s
@@ -1,14 +1,13 @@
-/*
- * Copyright (c) 1980 Regents of the University of California.
- * All rights reserved.  The Berkeley software License Agreement
- * specifies the terms and conditions for redistribution.
+/*-
+ * Copyright (c) 1984 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.proprietary.c%
  */
 
-#ifdef LIBC_SCCS
-	.asciz	"@(#)nargs.s	5.3 (Berkeley) %G%"
-#endif LIBC_SCCS
-
-/* C library -- nargs */
+#if defined(LIBC_SCCS) && !defined(lint)
+	.asciz "@(#)nargs.s	5.4 (Berkeley) %G%"
+#endif /* LIBC_SCCS and not lint */
 
 #include "DEFS.h"
 
