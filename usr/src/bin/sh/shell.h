f13650a38cf96f0cacc6694029e63f91e047de9b
Marc Teitelbaum <marc@ucbvax.Berkeley.EDU>
1991-04-13 01:19:34 -0800

    cleanups
    SCCS-vsn: bin/sh/shell.h 5.4

--- usr/src/bin/sh/shell.h
+++ usr/src/bin/sh/shell.h
@@ -7,7 +7,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)shell.h	5.3 (Berkeley) %G%
+ *	@(#)shell.h	5.4 (Berkeley) %G%
  */
 
 /*
@@ -16,40 +16,41 @@
  *	SYMLINKS -> 1 if your system includes symbolic links, 0 otherwise.
  *	DIRENT -> 1 if your system has the SVR3 directory(3X) routines.
  *	UDIR -> 1 if you want the shell to simulate the /u directory.
+ *	ATTY -> 1 to include code for atty(1).
  *	SHORTNAMES -> 1 if your linker cannot handle long names.
  *	define BSD if you are running 4.2 BSD or later.
  *	define SYSV if you are running under System V.
- *	define DEBUG to turn on debugging.
+ *	define DEBUG=1 to compile in debugging (set global "debug" to turn on)
+ *	define DEBUG=2 to compile in and turn on debugging.
  *
  * When debugging is on, debugging info will be written to $HOME/trace and
  * a quit signal will generate a core dump.
  */
 
-#include <sys/cdefs.h>
 
 #define JOBS 1
 #define SYMLINKS 1
 #define DIRENT 1
 #define UDIR 0
 #define ATTY 0
-#define SHORTNAMES 0
 #define BSD
-/* #define SYSV */
-/* #define DEBUG */
-
-typedef void *pointer;
+#define DEBUG 1
 
 #ifdef __STDC__
+typedef void *pointer;
 #ifndef NULL
 #define NULL (void *)0
 #endif
 #else /* not __STDC__ */
+typedef char *pointer;
 #ifndef NULL
 #define NULL 0
 #endif
-#endif /* __STDC__ */
+#endif /*  not __STDC__ */
 #define STATIC	/* empty */
 #define MKINIT	/* empty */
+
+#include <sys/cdefs.h>
 
 extern char nullstr[1];		/* null string */
 
