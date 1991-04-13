362706d512d40786b2d4273c6b7a3657ecdbe448
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-04-12 23:12:25 -0800

    new copyright; att/bsd/shared
    SCCS-vsn: usr.bin/f77/pass1.vax/bb.c 5.4
    SCCS-vsn: usr.bin/f77/pass1.vax/conv.c 5.3
    SCCS-vsn: usr.bin/f77/pass1.vax/conv.h 5.2
    SCCS-vsn: usr.bin/f77/pass1.vax/data.c 5.3
    SCCS-vsn: usr.bin/f77/pass1.vax/data.h 5.2
    SCCS-vsn: usr.bin/f77/pass1.vax/defs.h 5.5
    SCCS-vsn: usr.bin/f77/pass1.vax/equiv.c 5.3
    SCCS-vsn: usr.bin/f77/pass1.vax/error.c 5.2
    SCCS-vsn: usr.bin/f77/pass1.vax/exec.c 5.7
    SCCS-vsn: usr.bin/f77/pass1.vax/expr.c 5.10
    SCCS-vsn: usr.bin/f77/pass1.vax/fio.h 5.2
    SCCS-vsn: usr.bin/f77/pass1.vax/fmt.c 5.2
    SCCS-vsn: usr.bin/f77/pass1.vax/format.h 5.2
    SCCS-vsn: usr.bin/f77/pass1.vax/ftypes.h 5.2
    SCCS-vsn: usr.bin/f77/pass1.vax/init.c 5.5
    SCCS-vsn: usr.bin/f77/pass1.vax/intr.c 5.4
    SCCS-vsn: usr.bin/f77/pass1.vax/io.c 5.5
    SCCS-vsn: usr.bin/f77/pass1.vax/io.h 5.2
    SCCS-vsn: usr.bin/f77/pass1.vax/lex.c 5.5
    SCCS-vsn: usr.bin/f77/pass1.vax/main.c 5.4
    SCCS-vsn: usr.bin/f77/pass1.vax/misc.c 5.3
    SCCS-vsn: usr.bin/f77/pass1.vax/optcse.c 5.3
    SCCS-vsn: usr.bin/f77/pass1.vax/optim.c 5.5
    SCCS-vsn: usr.bin/f77/pass1.vax/optim.h 5.2
    SCCS-vsn: usr.bin/f77/pass1.vax/optloop.c 5.3
    SCCS-vsn: usr.bin/f77/pass1.vax/paramset.c 5.3
    SCCS-vsn: usr.bin/f77/pass1.vax/pathnames.h 5.2
    SCCS-vsn: usr.bin/f77/pass1.vax/pccdefs.h 5.2
    SCCS-vsn: usr.bin/f77/pass1.vax/proc.c 5.9
    SCCS-vsn: usr.bin/f77/pass1.vax/put.c 5.3
    SCCS-vsn: usr.bin/f77/pass1.vax/putpcc.c 5.4
    SCCS-vsn: usr.bin/f77/pass1.vax/regalloc.c 5.7
    SCCS-vsn: usr.bin/f77/pass1.vax/setfmt.c 5.2
    SCCS-vsn: usr.bin/f77/pass1.vax/stab.c 5.4
    SCCS-vsn: usr.bin/f77/pass1.vax/vax.c 5.4
    SCCS-vsn: usr.bin/f77/pass1.vax/vaxx.c 5.2
    SCCS-vsn: usr.bin/f77/pass1.vax/DEBUGS 5.2
    SCCS-vsn: usr.bin/f77/pass1.vax/gram.dcl 5.6
    SCCS-vsn: usr.bin/f77/pass1.vax/gram.exec 5.3
    SCCS-vsn: usr.bin/f77/pass1.vax/gram.expr 5.3
    SCCS-vsn: usr.bin/f77/pass1.vax/gram.head 5.2
    SCCS-vsn: usr.bin/f77/pass1.vax/gram.io 5.2

--- usr/src/usr.bin/f77/pass1.vax/proc.c
+++ usr/src/usr.bin/f77/pass1.vax/proc.c
@@ -1,12 +1,13 @@
-/*
- * Copyright (c) 1980 Regents of the University of California.
- * All rights reserved.  The Berkeley software License Agreement
- * specifies the terms and conditions for redistribution.
+/*-
+ * Copyright (c) 1980 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.proprietary.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)proc.c	5.8 (Berkeley) %G%";
-#endif not lint
+static char sccsid[] = "@(#)proc.c	5.9 (Berkeley) %G%";
+#endif /* not lint */
 
 /*
  * proc.c
--- usr/src/usr.bin/f77/pass1.vax/regalloc.c
+++ usr/src/usr.bin/f77/pass1.vax/regalloc.c
@@ -1,12 +1,13 @@
-/*
- * Copyright (c) 1980 Regents of the University of California.
- * All rights reserved.  The Berkeley software License Agreement
- * specifies the terms and conditions for redistribution.
+/*-
+ * Copyright (c) 1980 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.proprietary.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)regalloc.c	5.6 (Berkeley) %G%";
-#endif not lint
+static char sccsid[] = "@(#)regalloc.c	5.7 (Berkeley) %G%";
+#endif /* not lint */
 
 /*
  * regalloc.c
