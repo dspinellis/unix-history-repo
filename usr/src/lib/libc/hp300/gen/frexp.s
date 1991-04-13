60b98276d9a0f96024922d9166a982624f335d3e
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-04-12 19:30:37 -0800

    another version (completely different) put under SCCS simply to keep around, frexp.c is the current version
    SCCS-vsn: lib/libc/hp300/gen/frexp.s 5.2

--- usr/src/lib/libc/hp300/gen/frexp.s
+++ usr/src/lib/libc/hp300/gen/frexp.s
@@ -1,34 +1,28 @@
-/*-
- * Copyright (c) 1990 The Regents of the University of California.
- * All rights reserved.
- *
- * This code is derived from software contributed to Berkeley by
- * the Systems Programming Group of the University of Utah Computer
- * Science Department.
- *
- * %sccs.include.redist.c%
- */
-
-#if defined(LIBC_SCCS) && !defined(lint)
-	.asciz "@(#)frexp.s	5.1 (Berkeley) %G%"
-#endif /* LIBC_SCCS and not lint */
-
-#include "DEFS.h"
-
-/*
- * double frexp(val, eptr)
- * returns: x s.t. val = x * (2 ** n), with n stored in *eptr
- */
-ENTRY(frexp)
-	fmoved		sp@(4),fp1
-	fgetmanx	fp1,fp0
-	fgetexpx	fp1
-	fmovel		fp1,d0
-	addql		#1,d0
-	movel		sp@(12),a0
-	movel		d0,a0@
-	fdivl		#2,fp0
-	fmoved		fp0,sp@-
-	movel		sp@+,d0
-	movel		sp@+,d1
+#NO_APP
+gcc_compiled.:
+.text
+	.even
+.globl _frexp
+_frexp:
+	link a6,#0
+	moveml #0x3000,sp@-
+	fmoved a6@(8),fp0
+	movel a6@(16),a0
+	fjeq L2
+	fmoved fp0,sp@-
+	movel sp@+,d0
+	movel sp@+,d1
+	bfextu d0{#1:#11},d2
+	addl #-1022,d2
+	movel d2,a0@
+	movel #1022,d3
+	bfins d3,d0{#1:#11}
+	jra L1
+L2:
+	clrl a0@
+	clrl d0
+	clrl d1
+L1:
+	moveml a6@(-8),#0xc
+	unlk a6
 	rts
