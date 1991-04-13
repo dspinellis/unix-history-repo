7e809f3f60a9e334481eb78e69178160c2ab4ac1
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-04-12 23:56:03 -0800

    from Donn Seeley -- from scratch, add Berkeley copyright
    SCCS-vsn: lib/libc/vax/stdlib/abs.s 5.5

--- usr/src/lib/libc/vax/stdlib/abs.s
+++ usr/src/lib/libc/vax/stdlib/abs.s
@@ -1,19 +1,17 @@
 /*-
- * Copyright (c) 1984 The Regents of the University of California.
+ * Copyright (c) 1991 The Regents of the University of California.
  * All rights reserved.
  *
- * %sccs.include.proprietary.c%
+ * %sccs.include.redist.c%
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-	.asciz "@(#)abs.s	5.4 (Berkeley) %G%"
+	.asciz "@(#)abs.s	5.5 (Berkeley) %G%"
 #endif /* LIBC_SCCS and not lint */
 
-#include "DEFS.h"
-
-ENTRY(abs, 0)
+ENTRY(abs)
 	movl	4(ap),r0
-	bgeq	1f
+	jgeq	1f
 	mnegl	r0,r0
 1:
 	ret
