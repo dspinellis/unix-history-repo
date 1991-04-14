db002ebd053ef33229f640072fcdf32d7dda933b
CSRG <csrg@ucbvax.Berkeley.EDU>
1991-04-14 03:38:58 -0800

    BSD 4_4 development Work on file usr/src/contrib/gawk-2.15.2/atari/stack.c
    Synthesized-from: CSRG/cd3/4.4

---
+++ usr/src/contrib/gawk-2.15.2/atari/stack.c
@@ -0,0 +1,6 @@
+/*
+ * This value indicates an amount of reserved memory for a stack
+ * in executables - see source of a startup code for details.
+ * It can be changed without recompilation with fixstk.ttp utility.
+ */
+long _stksize = 2L; /* keep half of memory */
