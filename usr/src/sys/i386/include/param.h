b03d3ea6460263b96176fe717319bbfa8e85b9b9
William Nesheim <william@ucbvax.Berkeley.EDU>
1991-04-16 00:15:49 -0800

    added mach macros
    SCCS-vsn: sys/i386/include/param.h 5.4

--- usr/src/sys/i386/include/param.h
+++ usr/src/sys/i386/include/param.h
@@ -7,7 +7,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)param.h	5.3 (Berkeley) %G%
+ *	@(#)param.h	5.4 (Berkeley) %G%
  */
 
 /*
@@ -96,6 +96,18 @@
  * For now though just use DEV_BSIZE.
  */
 #define	bdbtofsb(bn)	((bn) / (BLKDEV_IOSIZE/DEV_BSIZE))
+
+/*
+ * Mach derived conversion macros
+ */
+#define i386_round_pdr(x)	((((unsigned)(x)) + NBPDR - 1) & ~(NBPDR-1))
+#define i386_trunc_pdr(x)	((unsigned)(x) & ~(NBPDR-1))
+#define i386_round_page(x)	((((unsigned)(x)) + NBPG - 1) & ~(NBPG-1))
+#define i386_trunc_page(x)	((unsigned)(x) & ~(NBPG-1))
+#define i386_btod(x)		((unsigned)(x) >> PDRSHIFT)
+#define i386_dtob(x)		((unsigned)(x) << PDRSHIFT)
+#define i386_btop(x)		((unsigned)(x) >> PGSHIFT)
+#define i386_ptob(x)		((unsigned)(x) << PGSHIFT)
 
 #ifdef KERNEL
 #ifndef LOCORE
