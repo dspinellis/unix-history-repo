5a25d700ef1485cbb692b98044cb8c2bcc113a72
Chris Torek <torek@ucbvax.Berkeley.EDU>
1991-04-17 02:58:36 -0800

    my previous version was wrong; this one is right
    SCCS-vsn: lib/libc/stdlib/div.c 5.2
    SCCS-vsn: lib/libc/stdlib/ldiv.c 5.2

--- usr/src/lib/libc/stdlib/div.c
+++ usr/src/lib/libc/stdlib/div.c
@@ -9,39 +9,45 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)div.c	5.1 (Berkeley) %G%";
+static char sccsid[] = "@(#)div.c	5.2 (Berkeley) %G%";
 #endif /* LIBC_SCCS and not lint */
 
 #include <stdlib.h>		/* div_t */
 
-/*
- * I AM NOT SURE THIS IS COMPLETELY PORTABLE
- * (or that it is even right)
- */
 div_t
 div(num, denom)
 	int num, denom;
 {
 	div_t r;
 
-	/* avoid deep thought */
-	if (num > 0 && denom < 0) {
-		num = -num;
-		denom = -denom;
-	}
 	r.quot = num / denom;
 	r.rem = num % denom;
-	if (num < 0 && denom > 0) {
-		/*
-		 * Machine division and remainer may work either way.  The
-		 * ANSI standard says that |r.quot| < |n/d| (where n/d
-		 * computed in infinite precision).  If the remainder is
-		 * positive, we got the `wrong' answer, so fix it.
-		 */
-		if (r.rem > 0) {
-			r.quot++;
-			r.rem -= denom;
-		}
+	/*
+	 * The ANSI standard says that |r.quot| <= |n/d|, where
+	 * n/d is to be computed in infinite precision.  In other
+	 * words, we should always truncate the quotient towards
+	 * 0, never -infinity.
+	 *
+	 * Machine division and remainer may work either way when
+	 * one or both of n or d is negative.  If only one is
+	 * negative and r.quot has been truncated towards -inf,
+	 * r.rem will have the same sign as denom and the opposite
+	 * sign of num; if both are negative and r.quot has been
+	 * truncated towards -inf, r.rem will be positive (will
+	 * have the opposite sign of num).  These are considered
+	 * `wrong'.
+	 *
+	 * If both are num and denom are positive, r will always
+	 * be positive.
+	 *
+	 * This all boils down to:
+	 *	if num >= 0, but r.rem < 0, we got the wrong answer.
+	 * In that case, to get the right answer, add 1 to r.quot and
+	 * subtract denom from r.rem.
+	 */
+	if (num >= 0 && r.rem < 0) {
+		r.quot++;
+		r.rem -= denom;
 	}
 	return (r);
 }
