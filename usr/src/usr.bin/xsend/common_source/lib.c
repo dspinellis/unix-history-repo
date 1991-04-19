d3c4d8efa1cc43e0515edcbd860b5023b36a7b2a
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-04-19 02:03:15 -0800

    new copyright; att/bsd/shared
    SCCS-vsn: usr.bin/w/w.c 5.28
    SCCS-vsn: usr.bin/wall/wall.1 6.4
    SCCS-vsn: usr.bin/wc/wc.1 6.2
    SCCS-vsn: usr.bin/what/what.1 6.3
    SCCS-vsn: old/whereis/whereis.1 6.4
    SCCS-vsn: old/whereis/whereis.c 5.5
    SCCS-vsn: old/which/which.1 6.2
    SCCS-vsn: old/which/which.csh 5.5
    SCCS-vsn: usr.bin/who/who.1 6.4
    SCCS-vsn: old/whoami/whoami.1 6.3
    SCCS-vsn: usr.bin/window/windowrc 3.9
    SCCS-vsn: usr.bin/write/write.1 6.4
    SCCS-vsn: usr.bin/xsend/common_source/lib.c 4.3
    SCCS-vsn: usr.bin/xsend/common_source/xmail.h 4.2
    SCCS-vsn: usr.bin/xsend/enroll/enroll.c 4.4
    SCCS-vsn: usr.bin/xsend/xget/xget.c 4.6
    SCCS-vsn: usr.bin/xsend/xsend/xsend.1 6.3
    SCCS-vsn: usr.bin/xsend/xsend/xsend.c 4.8
    SCCS-vsn: usr.bin/yes/yes.1 6.3

--- usr/src/usr.bin/xsend/common_source/lib.c
+++ usr/src/usr.bin/xsend/common_source/lib.c
@@ -1,6 +1,10 @@
+/*-
+ * %sccs.include.proprietary.c%
+ */
+
 #ifndef lint
-static char sccsid[] = "@(#)lib.c	4.2 %G%";
-#endif
+static char sccsid[] = "@(#)lib.c	4.3 (Berkeley) %G%";
+#endif /* not lint */
 
 #include "xmail.h"
 #include "pathnames.h"
