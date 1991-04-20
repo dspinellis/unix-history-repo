1d2ca4217e26391c0487912e7b872abbbcea1581
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1991-04-20 00:03:16 -0800

    add VXLOCK as condition making VOP_ISLOCKED true
    SCCS-vsn: sys/sys/vnode.h 7.34

--- usr/src/sys/sys/vnode.h
+++ usr/src/sys/sys/vnode.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)vnode.h	7.33 (Berkeley) %G%
+ *	@(#)vnode.h	7.34 (Berkeley) %G%
  */
 
 #ifndef KERNEL
@@ -293,7 +293,8 @@ struct vnodeops {
 #define	VOP_BMAP(v,s,p,n)	(*((v)->v_op->vn_bmap))(v,s,p,n)
 #define	VOP_STRATEGY(b)		(*((b)->b_vp->v_op->vn_strategy))(b)
 #define	VOP_PRINT(v)		(*((v)->v_op->vn_print))(v)
-#define	VOP_ISLOCKED(v)		(*((v)->v_op->vn_islocked))(v)
+#define	VOP_ISLOCKED(v)		(((v)->v_flag & VXLOCK) || \
+				(*((v)->v_op->vn_islocked))(v))
 #define	VOP_ADVLOCK(v,p,o,l,f)	(*((v)->v_op->vn_advlock))(v,p,o,l,f)
 
 /*
