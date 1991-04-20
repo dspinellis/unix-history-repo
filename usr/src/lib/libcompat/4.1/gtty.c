23797ef6435c635c2e3969058cd9c6a9624c76d9
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-04-20 02:12:33 -0800

    new copyright; att/bsd/shared
    SCCS-vsn: lib/libcompat/4.1/ftime.c 5.3
    SCCS-vsn: lib/libcompat/4.1/getpw.c 5.3
    SCCS-vsn: lib/libcompat/4.1/gtty.c 5.3
    SCCS-vsn: lib/libcompat/4.1/stty.c 5.3
    SCCS-vsn: lib/libcompat/4.1/tell.c 5.3
    SCCS-vsn: lib/libcompat/4.1/vlimit.c 5.3
    SCCS-vsn: lib/libcompat/4.1/vtimes.c 5.4
    SCCS-vsn: lib/libcurses/curses.ext 5.6
    SCCS-vsn: lib/libmp/gcd.c 5.4
    SCCS-vsn: lib/libmp/madd.c 5.3
    SCCS-vsn: lib/libmp/mdiv.c 5.3
    SCCS-vsn: lib/libmp/mout.c 5.4
    SCCS-vsn: lib/libmp/mp.3 6.6
    SCCS-vsn: lib/libmp/msqrt.c 5.3
    SCCS-vsn: lib/libmp/mult.c 5.3
    SCCS-vsn: lib/libmp/pow.c 5.3
    SCCS-vsn: lib/libmp/util.c 5.3

--- usr/src/lib/libcompat/4.1/gtty.c
+++ usr/src/lib/libcompat/4.1/gtty.c
@@ -1,12 +1,13 @@
-/*
- * Copyright (c) 1980 Regents of the University of California.
- * All rights reserved.  The Berkeley software License Agreement
- * specifies the terms and conditions for redistribution.
+/*-
+ * Copyright (c) 1980 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.proprietary.c%
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)gtty.c	5.2 (Berkeley) %G%";
-#endif LIBC_SCCS and not lint
+static char sccsid[] = "@(#)gtty.c	5.3 (Berkeley) %G%";
+#endif /* LIBC_SCCS and not lint */
 
 /*
  * Writearound to old gtty system call.
--- usr/src/lib/libmp/mdiv.c
+++ usr/src/lib/libmp/mdiv.c
@@ -1,6 +1,10 @@
+/*-
+ * %sccs.include.proprietary.c%
+ */
+
 #ifndef lint
-static char sccsid[] = "@(#)mdiv.c	5.2 (Berkeley) %G%";
-#endif not lint
+static char sccsid[] = "@(#)mdiv.c	5.3 (Berkeley) %G%";
+#endif /* not lint */
 
 #include <mp.h>
 mdiv(a,b,q,r) MINT *a,*b,*q,*r;
