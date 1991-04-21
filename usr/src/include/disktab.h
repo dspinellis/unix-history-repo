5e37723bd10b4d9055bae72a9367febc75606678
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-04-20 23:40:37 -0800

    getdiskbyname prototype is in <sys/diskabel.h>
    SCCS-vsn: include/disktab.h 5.6

--- usr/src/include/disktab.h
+++ usr/src/include/disktab.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)disktab.h	5.5 (Berkeley) %G%
+ *	@(#)disktab.h	5.6 (Berkeley) %G%
  */
 
 #ifndef	_DISKTAB_H_
@@ -31,7 +31,5 @@ struct	disktab {
 		short	p_fsize;	/* frag size in bytes */
 	} d_partitions[8];
 };
-
-struct	disktab *getdiskbyname();
 
 #endif /* !_DISKTAB_H_ */
