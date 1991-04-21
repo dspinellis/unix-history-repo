c0ae9191082147d3e2e5b527b353a6e4b96ed716
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-04-20 21:15:50 -0800

    new copyright; att/bsd/shared
    SCCS-vsn: lib/libcompat/4.3/ecvt.3 6.4
    SCCS-vsn: lib/libcompat/4.3/ecvt.c 5.3
    SCCS-vsn: lib/libcompat/4.3/gcvt.c 5.3
    SCCS-vsn: lib/libcompat/4.3/regex.c 5.5
    SCCS-vsn: lib/libcompat/4.3/sibuf.c 5.3
    SCCS-vsn: lib/libcompat/4.3/sobuf.c 5.3
    SCCS-vsn: lib/libcompat/4.3/strout.c 5.3
    SCCS-vsn: old/libndbm/dbm.3 6.5
    SCCS-vsn: old/libndbm/ndbm.3 6.10

--- usr/src/lib/libcompat/4.3/regex.c
+++ usr/src/lib/libcompat/4.3/regex.c
@@ -1,12 +1,13 @@
-/*
- * Copyright (c) 1980 Regents of the University of California.
- * All rights reserved.  The Berkeley software License Agreement
- * specifies the terms and conditions for redistribution.
+/*-
+ * Copyright (c) 1980 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.proprietary.c%
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)regex.c	5.4 (Berkeley) %G%";
-#endif LIBC_SCCS and not lint
+static char sccsid[] = "@(#)regex.c	5.5 (Berkeley) %G%";
+#endif /* LIBC_SCCS and not lint */
 
 #include <unistd.h>
 
