2258fd494fcdb0569b8f10ee7f62bc6153ada8dd
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-04-20 21:21:40 -0800

    new copyright; att/bsd/shared
    SCCS-vsn: lib/libm/common_source/erf.c 5.3
    SCCS-vsn: lib/libm/common_source/j0.c 5.4
    SCCS-vsn: lib/libm/common_source/j1.c 5.4
    SCCS-vsn: lib/libm/common_source/jn.c 5.4
    SCCS-vsn: lib/libm/common_source/lgamma.c 5.4

--- usr/src/lib/libm/common_source/lgamma.c
+++ usr/src/lib/libm/common_source/lgamma.c
@@ -1,11 +1,12 @@
-/*
- * Copyright (c) 1985 Regents of the University of California.
- * All rights reserved.  The Berkeley software License Agreement
- * specifies the terms and conditions for redistribution.
+/*-
+ * Copyright (c) 1985 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.proprietary.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)lgamma.c	5.3 (Berkeley) %G%";
+static char sccsid[] = "@(#)lgamma.c	5.4 (Berkeley) %G%";
 #endif /* not lint */
 
 /*
