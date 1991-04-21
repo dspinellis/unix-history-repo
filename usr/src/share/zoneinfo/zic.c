bff1f8565561fd857640f3a15c57711257e62415
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-04-20 22:35:04 -0800

    new copyright; att/bsd/shared
    SCCS-vsn: share/zoneinfo/ialloc.c 5.3
    SCCS-vsn: share/zoneinfo/scheck.c 5.3
    SCCS-vsn: share/zoneinfo/tzfile.5 5.2
    SCCS-vsn: share/zoneinfo/zic.8 5.2
    SCCS-vsn: share/zoneinfo/zic.c 5.3

--- usr/src/share/zoneinfo/zic.c
+++ usr/src/share/zoneinfo/zic.c
@@ -1,8 +1,20 @@
+/*-
+ * Copyright (c) 1991 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to Berkeley by
+ * Arthur David Olson of the National Cancer Institute.
+ *
+ * %sccs.include.redist.c%
+ */
+
 #ifndef lint
-#ifndef NOID
+static char sccsid[] = "@(#)zic.c	5.3 (Berkeley) %G%";
+#endif /* not lint */
+
+#ifdef notdef
 static char	elsieid[] = "@(#)zic.c	4.12";
-#endif /* !defined NOID */
-#endif /* !defined lint */
+#endif
 
 #include <sys/types.h>
 #include <sys/cdefs.h>
