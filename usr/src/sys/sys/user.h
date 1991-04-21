abc6d35c564896bf11746872af68b597dc486447
Mike Karels <karels@ucbvax.Berkeley.EDU>
1991-04-21 00:57:04 -0800

    rm u.u_procp; change includes to mirror old for non-kernel; add kinfo_proc for core dumps, and redefinitions to fake out debuggers
    SCCS-vsn: sys/sys/user.h 7.18

--- usr/src/sys/sys/user.h
+++ usr/src/sys/sys/user.h
@@ -1,21 +1,26 @@
 /*
- * Copyright (c) 1982, 1986, 1989 Regents of the University of California.
+ * Copyright (c) 1982, 1986, 1989, 1991 Regents of the University of California.
  * All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)user.h	7.17 (Berkeley) %G%
+ *	@(#)user.h	7.18 (Berkeley) %G%
  */
 
+#include <machine/pcb.h>
 #ifndef KERNEL
+/* stuff that *used* to be included by user.h, or is now needed */
+#include <errno.h>
 #include <sys/time.h>
 #include <sys/resource.h>
+#include <sys/ucred.h>
 #include <sys/uio.h>
 #endif
-#include <machine/pcb.h>
-#include <sys/signalvar.h>
 #include <sys/resourcevar.h>
-#include <sys/namei.h>
+#include <sys/signalvar.h>
+#include <vm/vm.h>		/* XXX */
+#include <sys/kinfo_proc.h>
+
 
 /*
  * Per process structure containing data that
@@ -24,30 +29,37 @@
  
 struct	user {
 	struct	pcb u_pcb;
-	struct	proc *u_procp;		/* pointer to proc structure XXX */
 	label_t	u_ssave;		/* label variable for swapping XXX */
-#define curproc	u.u_procp		/* XXX */
 
 	struct	sigacts u_sigacts;	/* p_sigacts points here (use it!) */
-	struct	pstats u_stats;		/* rusage, profiling & timers */
+	struct	pstats u_stats;		/* p_stats points here (use it!) */
 
-/* 1.6 - resource controls */
-
-	int	u_spare[1];
-
+	/*
+	 * Remaining fields only for core dump and/or ptrace--
+	 * not valid at other times!
+	 */
+	struct	kinfo_proc u_kproc;	/* proc + eproc */
 };
 
-/* u_error codes */
-#ifndef KERNEL
-#include <errno.h>
-#endif
+/*
+ * Redefinitions to make the debuggers happy for now...
+ * This subterfuge brought to you by coredump() and procxmt().
+ * These fields are *only* valid at those times!
+ */
+#define	U_ar0	u_kproc.kp_proc.p_regs	/* copy of curproc->p_regs */
+#define	U_tsize	u_kproc.kp_eproc.e_vm.vm_tsize
+#define	U_dsize	u_kproc.kp_eproc.e_vm.vm_dsize
+#define	U_ssize	u_kproc.kp_eproc.e_vm.vm_ssize
+#define	U_sig	u_sigacts.ps_sig
+#define	U_code	u_sigacts.ps_code
 
 #ifdef KERNEL
 extern	struct user u;
-extern	struct user *swaputl;
-extern	struct user *forkutl;
-extern	struct user *xswaputl;
-extern	struct user *xswap2utl;
-extern	struct user *pushutl;
-extern	struct user *vfutl;
-#endif
+#else
+#define	u_ar0	U_ar0
+#define	u_tsize	U_tsize
+#define	u_dsize	U_dsize
+#define	u_ssize	U_ssize
+#define	u_sig	U_sig
+#define	u_code	U_code
+#endif /* KERNEL */
