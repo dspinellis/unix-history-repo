0efcc7df0d0f4db35a99bfe29bf44dfd26680445
Mike Karels <karels@ucbvax.Berkeley.EDU>
1991-04-20 23:44:31 -0800

    update from lbl: add SL_BUFSIZE, with room for checksum
    SCCS-vsn: sys/hp300/include/remote-sl.h 7.2

--- usr/src/sys/hp300/include/remote-sl.h
+++ usr/src/sys/hp300/include/remote-sl.h
@@ -15,7 +15,7 @@
  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  *
- * @(#) $Header: remote-sl.h,v 1.1 91/01/13 13:18:27 mccanne Exp $ (LBL)
+ * @(#) $Header: remote-sl.h,v 1.3 91/03/22 15:34:27 mccanne Exp $ (LBL)
  */
 
 #define FRAME_END		0xc0		/* Frame End */
@@ -33,9 +33,12 @@
 /*
  * Message limits.  SL_MAXMSG is the longest message that can be passed
  * down to the serial link.  The actual MTU is two times the max message
- * (since each byte might be escaped), plus the framing byte.  We add 2
- * to the message length to account for the type byte and check sum.
+ * (since each byte might be escaped), plus the two framing bytes.  We add 
+ * two to the message length to account for the type byte and check sum.
+ * SL_BUFSIZE is one character larger than SL_MAXMSG so we can stuff
+ * a checksum into the input buffer without special casing.
  */
 #define SL_MAXMSG 64
-#define SL_MTU ((2 * (SL_MAXMSG + 2) + 1))
+#define SL_BUFSIZE (SL_MAXMSG + 1)
+#define SL_MTU ((2 * (SL_MAXMSG + 2) + 2))
 
