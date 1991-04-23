6acdc9b82e1676c9977c521de69db6274475de02
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-04-22 22:03:08 -0800

    new copyright; att/bsd/shared
    SCCS-vsn: lib/libplot/bitgraph/arc.c 5.3
    SCCS-vsn: lib/libplot/bitgraph/bg.h 5.2
    SCCS-vsn: lib/libplot/bitgraph/box.c 5.3
    SCCS-vsn: lib/libplot/bitgraph/circle.c 5.3
    SCCS-vsn: lib/libplot/bitgraph/close.c 5.4
    SCCS-vsn: lib/libplot/bitgraph/cont.c 5.3
    SCCS-vsn: lib/libplot/bitgraph/dot.c 5.3
    SCCS-vsn: lib/libplot/bitgraph/erase.c 5.3
    SCCS-vsn: lib/libplot/bitgraph/label.c 5.3
    SCCS-vsn: lib/libplot/bitgraph/line.c 5.3
    SCCS-vsn: lib/libplot/bitgraph/linemod.c 5.3
    SCCS-vsn: lib/libplot/bitgraph/move.c 5.3
    SCCS-vsn: lib/libplot/bitgraph/open.c 5.4
    SCCS-vsn: lib/libplot/bitgraph/point.c 5.3
    SCCS-vsn: lib/libplot/bitgraph/space.c 5.3
    SCCS-vsn: lib/libplot/dumb/arc.c 5.2
    SCCS-vsn: lib/libplot/dumb/box.c 5.2
    SCCS-vsn: lib/libplot/dumb/circle.c 5.2
    SCCS-vsn: lib/libplot/dumb/close.c 5.3
    SCCS-vsn: lib/libplot/dumb/cont.c 5.2
    SCCS-vsn: lib/libplot/dumb/dot.c 5.2
    SCCS-vsn: lib/libplot/dumb/dumb.h 5.2
    SCCS-vsn: lib/libplot/dumb/erase.c 5.2
    SCCS-vsn: lib/libplot/dumb/label.c 5.2
    SCCS-vsn: lib/libplot/dumb/line.c 5.2
    SCCS-vsn: lib/libplot/dumb/linemod.c 5.2
    SCCS-vsn: lib/libplot/dumb/move.c 5.2
    SCCS-vsn: lib/libplot/dumb/open.c 5.3
    SCCS-vsn: lib/libplot/dumb/point.c 5.2
    SCCS-vsn: lib/libplot/dumb/space.c 5.2
    SCCS-vsn: lib/libplot/dumb/subr.c 5.2
    SCCS-vsn: lib/libplot/f77plot/arc.c 5.2
    SCCS-vsn: lib/libplot/f77plot/box.c 5.3
    SCCS-vsn: lib/libplot/f77plot/circle.c 5.2
    SCCS-vsn: lib/libplot/f77plot/clospl.c 5.3
    SCCS-vsn: lib/libplot/f77plot/cont.c 5.2
    SCCS-vsn: lib/libplot/f77plot/dot.c 5.2
    SCCS-vsn: lib/libplot/f77plot/erase.c 5.2
    SCCS-vsn: lib/libplot/f77plot/label.c 5.2
    SCCS-vsn: lib/libplot/f77plot/line.c 5.2
    SCCS-vsn: lib/libplot/f77plot/linemd.c 5.2
    SCCS-vsn: lib/libplot/f77plot/move.c 5.2
    SCCS-vsn: lib/libplot/f77plot/open.c 5.3
    SCCS-vsn: lib/libplot/f77plot/point.c 5.2
    SCCS-vsn: lib/libplot/f77plot/space.c 5.2

--- usr/src/lib/libplot/dumb/dumb.h
+++ usr/src/lib/libplot/dumb/dumb.h
@@ -1,11 +1,13 @@
+/*-
+ * Copyright (c) 1980 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.proprietary.c%
+ *
+ *	@(#)dumb.h	5.2 (Berkeley) %G%
+ */
+
 /*
- * Copyright (c) 1980 Regents of the University of California.
- * All rights reserved.  The Berkeley software License Agreement
- * specifies the terms and conditions for redistribution.
- *
- *	@(#)dumb.h	5.1 (Berkeley) %G%
- *
- *
  * This accepts plot file formats and produces the appropriate plots
  * for dumb terminals.  It can also be used for printing terminals and
  * lineprinter listings, although there is no way to specify number of
