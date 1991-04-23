eaef732060427fd39b6ce009b7eca0d8a7110d3c
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-04-22 21:54:51 -0800

    new copyright; att/bsd/shared
    SCCS-vsn: lib/libplot/aed/aed.h 5.2
    SCCS-vsn: lib/libplot/aed/arc.c 5.2
    SCCS-vsn: lib/libplot/aed/box.c 5.2
    SCCS-vsn: lib/libplot/aed/circle.c 5.2
    SCCS-vsn: lib/libplot/aed/close.c 5.2
    SCCS-vsn: lib/libplot/aed/cont.c 5.2
    SCCS-vsn: lib/libplot/aed/dot.c 5.2
    SCCS-vsn: lib/libplot/aed/erase.c 5.2
    SCCS-vsn: lib/libplot/aed/label.c 5.2
    SCCS-vsn: lib/libplot/aed/line.c 5.3
    SCCS-vsn: lib/libplot/aed/linemod.c 5.2
    SCCS-vsn: lib/libplot/aed/move.c 5.2
    SCCS-vsn: lib/libplot/aed/open.c 5.3
    SCCS-vsn: lib/libplot/aed/point.c 5.2
    SCCS-vsn: lib/libplot/aed/space.c 5.2
    SCCS-vsn: lib/libplot/aed/subr.c 5.2

--- usr/src/lib/libplot/aed/dot.c
+++ usr/src/lib/libplot/aed/dot.c
@@ -1,6 +1,13 @@
+/*-
+ * Copyright (c) 1983 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.proprietary.c%
+ */
+
 #ifndef lint
-static char sccsid[] = "@(#)dot.c	4.1 (Berkeley) 11/11/83";
-#endif
+static char sccsid[] = "@(#)dot.c	5.2 (Berkeley) %G%";
+#endif /* not lint */
 
 dot(){
 }
--- usr/src/lib/libplot/aed/subr.c
+++ usr/src/lib/libplot/aed/subr.c
@@ -1,6 +1,13 @@
+/*-
+ * Copyright (c) 1983 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.proprietary.c%
+ */
+
 #ifndef lint
-static char sccsid[] = "@(#)subr.c	4.1 (Berkeley) 11/11/83";
-#endif
+static char sccsid[] = "@(#)subr.c	5.2 (Berkeley) %G%";
+#endif /* not lint */
 
 #include "aed.h"
 
