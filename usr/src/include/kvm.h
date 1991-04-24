f4e5668961abf203081d71ab9da939c12bcb80a1
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-04-23 23:16:41 -0800

    prototype some more kvm functions
    SCCS-vsn: include/kvm.h 5.4

--- usr/src/include/kvm.h
+++ usr/src/include/kvm.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)kvm.h	5.3 (Berkeley) %G%
+ *	@(#)kvm.h	5.4 (Berkeley) %G%
  */
 
 #ifndef _KVM_H_
@@ -17,11 +17,17 @@
 #include <sys/cdefs.h>
 
 __BEGIN_DECLS
+struct proc;
+struct user;
 char		*kvm_getargs __P((const struct proc *, const struct user *));
 struct eproc	*kvm_geteproc __P((const struct proc *));
 char		*kvm_geterr __P((void));
+int		 kvm_getprocs __P((int, int));
 struct user	*kvm_getu __P((const struct proc *));
 struct proc	*kvm_nextproc __P((void));
+int		 kvm_nlist __P((struct nlist *));
+int		 kvm_openfiles __P((const char *, const char *, const char *));
+int		 kvm_read __P((void *, void *, int));
 __END_DECLS
 
 #endif /* !_KVM_H_ */
