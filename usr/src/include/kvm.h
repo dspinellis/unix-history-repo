6e9daf95523ec3e3a1bb63e78a357975cb0751c8
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-04-24 00:04:33 -0800

    ummm, well, we took a vote -- it's a void * agian
    SCCS-vsn: include/kvm.h 5.6

--- usr/src/include/kvm.h
+++ usr/src/include/kvm.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)kvm.h	5.5 (Berkeley) %G%
+ *	@(#)kvm.h	5.6 (Berkeley) %G%
  */
 
 #ifndef _KVM_H_
@@ -27,7 +27,7 @@ struct user	*kvm_getu __P((const struct proc *));
 struct proc	*kvm_nextproc __P((void));
 int		 kvm_nlist __P((struct nlist *));
 int		 kvm_openfiles __P((const char *, const char *, const char *));
-int		 kvm_read __P((off_t, void *, int));
+int		 kvm_read __P((void *, void *, int));
 __END_DECLS
 
 #endif /* !_KVM_H_ */
