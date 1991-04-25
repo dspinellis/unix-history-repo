ea92b07183946a720b22368d88e538bb8947617c
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-04-24 21:29:25 -0800

    original author Tom Truscott; add Berkeley copyright notice
    SCCS-vsn: usr.bin/uucp/uupoll/uupoll.c 5.7

--- usr/src/usr.bin/uucp/uupoll/uupoll.c
+++ usr/src/usr.bin/uucp/uupoll/uupoll.c
@@ -1,6 +1,19 @@
+/*-
+ * Copyright (c) 1986, 1991 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.redist.c%
+ */
+
 #ifndef lint
-static char sccsid[] = "@(#)uupoll.c	5.6	(Berkeley) %G%";
-#endif
+char copyright[] =
+"@(#) Copyright (c) 1986, 1991 The Regents of the University of California.\n\
+ All rights reserved.\n";
+#endif /* not lint */
+
+#ifndef lint
+static char sccsid[] = "@(#)uupoll.c	5.7 (Berkeley) %G%";
+#endif /* not lint */
 
 /*
  * Poll named system(s).
