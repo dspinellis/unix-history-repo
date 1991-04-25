995b583f66a4cf88f97dbf488709091575ac4cb4
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-04-24 21:31:21 -0800

    written by Mark Horton; add Berkeley specific copyright
    SCCS-vsn: usr.bin/uucp/uusend/uusend.c 5.4

--- usr/src/usr.bin/uucp/uusend/uusend.c
+++ usr/src/usr.bin/uucp/uusend/uusend.c
@@ -1,6 +1,19 @@
+/*-
+ * Copyright (c) 1980, 1991 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.redist.c%
+ */
+
 #ifndef lint
-static char sccsid[] = "@(#)uusend.c	5.3 (Berkeley) %G%";
-#endif
+char copyright[] =
+"@(#) Copyright (c) 1980, 1991 The Regents of the University of California.\n\
+ All rights reserved.\n";
+#endif /* not lint */
+
+#ifndef lint
+static char sccsid[] = "@(#)uusend.c	5.4 (Berkeley) %G%";
+#endif /* not lint */
 
 /*
  * uusend: primitive operation to allow uucp like copy of binary files
