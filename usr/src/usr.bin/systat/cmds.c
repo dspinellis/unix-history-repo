040e5879870c54f0ebc97c6c8db8a39824518632
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-04-25 21:29:06 -0800

    ANSI
    SCCS-vsn: usr.bin/systat/cmds.c 5.8
    SCCS-vsn: usr.bin/systat/main.c 5.9

--- usr/src/usr.bin/systat/cmds.c
+++ usr/src/usr.bin/systat/cmds.c
@@ -5,7 +5,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)cmds.c	5.7 (Berkeley) %G%";
+static char sccsid[] = "@(#)cmds.c	5.8 (Berkeley) %G%";
 #endif not lint
 
 /*
@@ -148,6 +148,7 @@ status()
           curcmd->c_name, naptime);
 }
 
+void
 suspend()
 {
         int oldmask;
