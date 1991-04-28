c6acfb769233c89adca06c205e0b7404b03ca577
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-04-27 23:34:12 -0800

    the .c's are really .h's
    SCCS-vsn: usr.bin/diction/style2/edict.c 4.4
    SCCS-vsn: usr.bin/diction/style2/ehash.c 4.5

--- usr/src/usr.bin/diction/style2/edict.c
+++ usr/src/usr.bin/diction/style2/edict.c
@@ -1,10 +1,8 @@
 /*-
  * %sccs.include.proprietary.c%
+ *
+ *	@(#)edict.c	4.4 (Berkeley) %G%
  */
-
-#ifndef lint
-static char sccsid[] = "@(#)edict.c	4.3 (Berkeley) %G%";
-#endif /* not lint */
 
 struct dict able_d[] = {
 "noihsaf",'Y',
