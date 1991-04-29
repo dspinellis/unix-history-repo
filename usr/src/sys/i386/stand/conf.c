26cfb27e252f9f211fd70f166d6cc127b6799d90
William Nesheim <william@ucbvax.Berkeley.EDU>
1991-04-29 00:00:37 -0800

    modified for common sys.c
    SCCS-vsn: sys/i386/stand/conf.c 7.2

--- usr/src/sys/i386/stand/conf.c
+++ usr/src/sys/i386/stand/conf.c
@@ -5,30 +5,34 @@
  * This code is derived from software contributed to Berkeley by
  * William Jolitz.
  *
- * %sccs.include.noredist.c%
+ * %sccs.include.redist.c%
  *
- *	@(#)conf.c	7.1 (Berkeley) %G%
+ *	@(#)conf.c	7.2 (Berkeley) %G%
  */
 
-
-/*#include "../machine/pte.h"*/
-
-#include "../h/param.h"
-#include "../h/inode.h"
-#include "../h/fs.h"
-
+#include "param.h"
 
 #include "saio.h"
+#ifdef BOOT
+extern exception;
+extern int debugflag;
+#endif
 
 devread(io)
 	register struct iob *io;
 {
 	int cc;
+	char c;
 
 	io->i_flgs |= F_RDDATA;
 	io->i_error = 0;
-	cc = (*devsw[major(io->i_ino.i_dev)].dv_strategy)(io, READ);
+	cc = (*devsw[io->i_dev].dv_strategy)(io, READ);
 	io->i_flgs &= ~F_TYPEMASK;
+
+#ifdef BOOT
+if(/*io->i_error || */(c=scankbd()))
+	_longjmp(&exception,1);
+#endif
 	return (cc);
 }
 
@@ -36,11 +40,16 @@ devwrite(io)
 	register struct iob *io;
 {
 	int cc;
+	char c;
 
 	io->i_flgs |= F_WRDATA;
 	io->i_error = 0;
-	cc = (*devsw[major(io->i_ino.i_dev)].dv_strategy)(io, WRITE);
+	cc = (*devsw[io->i_dev].dv_strategy)(io, WRITE);
 	io->i_flgs &= ~F_TYPEMASK;
+#ifdef BOOT
+if(/* io->i_error || */ (c=scankbd()))
+	_longjmp(&exception,1);
+#endif
 	return (cc);
 }
 
@@ -48,14 +57,14 @@ devopen(io)
 	register struct iob *io;
 {
 
-	(*devsw[major(io->i_ino.i_dev)].dv_open)(io);
+	(*devsw[io->i_dev].dv_open)(io);
 }
 
 devclose(io)
 	register struct iob *io;
 {
 
-	(*devsw[major(io->i_ino.i_dev)].dv_close)(io);
+	(*devsw[io->i_dev].dv_close)(io);
 }
 
 devioctl(io, cmd, arg)
@@ -64,7 +73,7 @@ devioctl(io, cmd, arg)
 	caddr_t arg;
 {
 
-	return ((*devsw[major(io->i_ino.i_dev)].dv_ioctl)(io, cmd, arg));
+	return ((*devsw[io->i_dev].dv_ioctl)(io, cmd, arg));
 }
 
 /*ARGSUSED*/
@@ -87,10 +96,13 @@ nullioctl(io, cmd, arg)
 
 int	nullsys(), nullioctl();
 int	wdstrategy(), wdopen()/*, wdioctl()*/;
+int	fdstrategy(), fdopen()/*, fdioctl()*/;
 
 struct devsw devsw[] = {
 	/*{ "xx",	xxstrategy,	xxopen,		nullsys, nullioctl },*/
 	{ "wd",	wdstrategy,	wdopen,		nullsys,/*wdioctl*/ nullioctl },
+	{ "", 0, 0, 0, 0 }, /* swapdev place holder */
+	{ "fd",	fdstrategy,	fdopen,		nullsys,/*fdioctl*/ nullioctl },
 	{ 0, 0, 0, 0, 0 },
 };
-int ndevs = 2 ;
+int ndevs = 3 ;
