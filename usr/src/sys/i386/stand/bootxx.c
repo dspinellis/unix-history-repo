7281a41555f8218cd43ff389cb66b8378cacc84c
William Nesheim <william@ucbvax.Berkeley.EDU>
1991-04-28 23:53:17 -0800

    merged to use features of common sys.c
    SCCS-vsn: sys/i386/stand/bootxx.c 7.2

--- usr/src/sys/i386/stand/bootxx.c
+++ usr/src/sys/i386/stand/bootxx.c
@@ -5,73 +5,76 @@
  * This code is derived from software contributed to Berkeley by
  * William Jolitz.
  *
- * %sccs.include.noredist.c%
+ * %sccs.include.redist.c%
  *
- *	@(#)bootxx.c	7.1 (Berkeley) %G%
+ *	@(#)bootxx.c	7.2 (Berkeley) %G%
  */
 
-#include "../h/param.h"
-#include "../h/inode.h"
-#include "../h/fs.h"
+#include "param.h"
 #include <a.out.h>
 #include "saio.h"
-#include "../h/reboot.h"
-#include "../h/dir.h"
-#include "../h/disk.h"
-#include "devvm.h"
+#include "reboot.h"
+#include "disklabel.h"
 
-extern	int howto, bootdev, unit, cyloffset, boottype;
-extern char bootprog[] ;
+char *bootprog = "/boot";
+extern int opendev, bootdev, cyloffset;
+extern struct disklabel disklabel;
+
 /*
- * Boot program... arguments passed in r10 and r11
- * are passed through to the full boot program.
+ * Boot program... loads /boot out of filesystem indicated by arguements.
+ * We assume an autoboot unless we detect a misconfiguration.
  */
 
-main()
+main(dev, unit, off)
 {
-	register int io, partition; register char *bp ;
+	register struct disklabel *lp;
+	register int io, partition, howto;
 
-#ifdef lint
-	howto = 0; devtype = 0;
-#endif
-	extern struct disklabel disklabel;
 
 	/* are we a disk, if so look at disklabel and do things */
-	if (bootdev == 0 || bootdev == 3) {
+	lp = &disklabel;
+	if (lp->d_magic == DISKMAGIC) {
 	    /*
-	     * Synthesize bootdev from unit, type and partition
-	     * from the ROM monitor.
-	     * It's dirty work, but someone's got to do it, and
-	     * we always seem to get it.
+	     * Synthesize bootdev from dev, unit, type and partition
+	     * information from the block 0 bootstrap.
+	     * It's dirty work, but someone's got to do it.
+	     * This will be used by the filesystem primatives, and
+	     * drivers. Ultimately, opendev will be created corresponding
+	     * to which drive to pass to top level bootstrap.
 	     */
 	    for (io = 0; io < 8; io++)
-		if (bootdev > 0) { /* XXX should check dk_type == DTYPE_SCSI */
-			if (disklabel.dk_partition[io].cyloff
-				== cyloffset * disklabel.dk_secpercyl)
+#ifdef notyetSCSI
+		if (lp->d_type == DTYPE_SCSI) {
+			if (lp->d_partitions[io].p_offset == off)
 				break;
-		} else {
-	
-			if (disklabel.dk_partition[io].cyloff == cyloffset)
+		} else
+#endif
+		if (lp->d_partitions[io].p_offset == off*lp->d_secpercyl)
 			break;
-		}
-	    if (io == 8) io = 0; /* probably a bad or non-existant disklabel */
-	    bootdev = makedev(bootdev, make_minor(unit, io));
-	} else { io = 0 ; howto = (howto&0x7) | 3 ; }
-	bp = bootprog ;
-	while (*bp != '0') bp++ ;	/* n-char device names instead of 2 */
-	*bp++ = unit % 10 + '0' ;
-	*bp += io % 10 ;
-/*	bootprog[3] = unit % 10 + '0';
-	bootprog[4] = io % 10 + 'a';*/
+
+	    if (io == 8) goto screwed;
+            cyloffset = off;
+	} else {
+screwed:
+		/* probably a bad or non-existant disklabel */
+		io = 0 ;
+		howto |= RB_SINGLE|RB_ASKNAME ;
+	}
+
+	/* construct bootdev */
+	/* currently, PC has no way of booting off alternate controllers */
+	bootdev = MAKEBOOTDEV(/*i_dev*/ dev, /*i_adapt*/0, /*i_ctlr*/0,
+	    unit, /*i_part*/io);
+
 	printf("loading %s\n", bootprog);
 	io = open(bootprog, 0);
 	if (io >= 0)
-		copyunix(io);
+		copyunix(io, howto);
 	_stop("boot failed\n");
 }
 
 /*ARGSUSED*/
-copyunix(io)
+copyunix(io, howto)
 	register io;	
 {
 	struct exec x;
@@ -82,23 +85,27 @@ copyunix(io)
 	if (i != sizeof x ||
 	    (x.a_magic != 0407 && x.a_magic != 0413 && x.a_magic != 0410))
 		_stop("Bad format\n");
+
 	if ((x.a_magic == 0413 || x.a_magic == 0410) &&
 	    lseek(io, 0x400, 0) == -1)
 		goto shread;
+
 	if (read(io, (char *)0, x.a_text) != x.a_text)
 		goto shread;
+
 	addr = (char *)x.a_text;
 	if (x.a_magic == 0413 || x.a_magic == 0410)
 		while ((int)addr & CLOFSET)
 			*addr++ = 0;
+
 	if (read(io, addr, x.a_data) != x.a_data)
 		goto shread;
+
 	addr += x.a_data;
-	x.a_bss += 128*512;	/* slop */
 	for (i = 0; i < x.a_bss; i++)
 		*addr++ = 0;
-	setregs();
- 	(*((int (*)()) x.a_entry))();
+
+ 	(*((int (*)()) x.a_entry))(howto, opendev, cyloffset);
 	return;
 shread:
 	_stop("Short read\n");
