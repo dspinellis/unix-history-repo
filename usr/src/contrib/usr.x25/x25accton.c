77e8c01254accf7861cba08b1673646b4a160d51
Keith Sklower <sklower@ucbvax.Berkeley.EDU>
1991-04-30 01:49:19 -0800

    versions for networking tape 2
    SCCS-vsn: contrib/usr.x25/x25accton.c 5.2
    SCCS-vsn: contrib/usr.x25/x25config.c 5.2

--- usr/src/contrib/usr.x25/x25accton.c
+++ usr/src/contrib/usr.x25/x25accton.c
@@ -1,4 +1,15 @@
 /*
+ * Copyright (c) 1990 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.redist.c%
+ *
+ *	@(#)x25accton.c	5.2 (Berkeley) %G%
+ */
+#include <sys/param.h>
+#include <sys/socket.h>
+#include <netccitt/x25.h>
+/*
  * Enable or disable X.25 accounting.
  */
 
@@ -10,4 +21,19 @@ main(argc, argv)
 		exit(1);
 	}
 	exit(0);
+}
+x25acct(name)
+char *name;
+{
+	int s = socket(AF_CCITT, SOCK_STREAM, 0);
+
+	if (s < 0)
+		return (s);
+	if (name)
+		return (setsockopt(s, CCITTPROTO_X25, PK_ACCTFILE,
+			name, strlen(name) + 1));
+	else
+		return (setsockopt(s, CCITTPROTO_X25, PK_ACCTFILE,
+			(char *)&name, 0));
+
 }
