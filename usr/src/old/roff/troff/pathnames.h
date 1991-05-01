c437f5218a82ade54e30abb8092df287c503a023
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-04-30 21:34:58 -0800

    move troff_font into /usr/old
    SCCS-vsn: old/roff/troff/pathnames.h 5.3

--- usr/src/old/roff/troff/pathnames.h
+++ usr/src/old/roff/troff/pathnames.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)pathnames.h	5.2 (Berkeley) %G%
+ *	@(#)pathnames.h	5.3 (Berkeley) %G%
  */
 
-#define	_PATH_FONTS	"/usr/libdata/troff_font/ftXX"
+#define	_PATH_FONTS	"/usr/old/libdata/troff_font/ftXX"
