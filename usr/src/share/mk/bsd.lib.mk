2baa3a52acf3aed2a2b5c952b2564f35b9db9044
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-04-30 23:50:43 -0800

    groff doesn't like -h
    SCCS-vsn: share/mk/bsd.lib.mk 5.24
    SCCS-vsn: share/mk/bsd.prog.mk 5.24

--- usr/src/share/mk/bsd.lib.mk
+++ usr/src/share/mk/bsd.lib.mk
@@ -1,4 +1,4 @@
-#	@(#)bsd.lib.mk	5.23 (Berkeley) %G%
+#	@(#)bsd.lib.mk	5.24 (Berkeley) %G%
 
 .if exists(${.CURDIR}/../Makefile.inc)
 .include "${.CURDIR}/../Makefile.inc"
@@ -23,7 +23,7 @@ BINMODE?=	555
 .SUFFIXES: .out .o .po .s .c .f .y .l .8 .7 .6 .5 .4 .3 .2 .1 .0
 
 .8.0 .7.0 .6.0 .5.0 .4.0 .3.0 .2.0 .1.0:
-	nroff -man -h ${.IMPSRC} > ${.TARGET}
+	nroff -mandoc ${.IMPSRC} > ${.TARGET}
 
 .c.o:
 	${CC} ${CFLAGS} -c ${.IMPSRC} 
