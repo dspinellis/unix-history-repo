85e3b87c1c2bd8a9ea00686c4c35822ea8918f95
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-05-02 23:26:01 -0800

    I lied, it needs user.h (actually, I was lied to...)
    SCCS-vsn: usr.bin/gcore/gcore.c 5.10

--- usr/src/usr.bin/gcore/gcore.c
+++ usr/src/usr.bin/gcore/gcore.c
@@ -12,7 +12,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)gcore.c	5.9 (Berkeley) %G%";
+static char sccsid[] = "@(#)gcore.c	5.10 (Berkeley) %G%";
 #endif /* not lint */
 
 /*
@@ -34,6 +34,7 @@ static char sccsid[] = "@(#)gcore.c	5.9 (Berkeley) %G%";
  */
 #include <machine/pte.h>
 #include <sys/param.h>
+#include <sys/user.h>
 #include <sys/proc.h>
 #include <sys/vm.h>
 #include <setjmp.h>
