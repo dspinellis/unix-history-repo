136d62b42559e8da945356c53b0fcc7959edc699
Steven McCanne <mccanne@ucbvax.Berkeley.EDU>
1991-05-03 04:26:58 -0800

    merged in new insn encodings
    SCCS-vsn: sys/net/bpf.h 1.3

--- usr/src/sys/net/bpf.h
+++ usr/src/sys/net/bpf.h
@@ -18,7 +18,7 @@
  * WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
  * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  *
- * @(#) $Header: bpf.h,v 1.19 91/01/30 18:20:21 mccanne Exp $ (LBL)
+ * @(#) $Header: bpf.h,v 1.20 91/04/24 22:06:24 mccanne Locked $ (LBL)
  *
  * This code is derived from the Stanford/CMU enet packet filter,
  * (net/enet.h) distributed with 4.3BSD Unix.
@@ -128,19 +128,60 @@ struct bpf_hdr {
 #define DLT_FDDI	10	/* FDDI */
 
 /*
- * The opcodes are defined as an enumeration.  However, they are stored
- * explicitly in the code array as 'u_short'.
+ * The instruction encondings.
  */
-enum bpf_code {
-#define OPDEF(opcode, opstr) opcode ,
-#include <net/bpfcodes.h>
-#undef OPDEF
-	/* this idea is borrowed from gcc */
-	LAST_AND_UNUSED_ENUM
-};
+/* classes <2:0> */
+#define BPF_CLASS(code) ((code) & 0x07)
+#define		BPF_LD		0x00
+#define		BPF_LDX		0x01
+#define		BPF_ST		0x02
+#define		BPF_STX		0x03
+#define		BPF_ALU		0x04
+#define		BPF_JMP		0x05
+#define		BPF_RET		0x06
+#define		BPF_MISC	0x07
 
-#define BPF_NCODES ((unsigned)LAST_AND_UNUSED_ENUM)
-#define BPF_VALIDCODE(code) ((unsigned)(code) < BPF_NCODES)
+/* ld/ldx fields */
+#define BPF_SIZE(code)	((code) & 0x18)
+#define		BPF_W		0x00
+#define		BPF_H		0x08
+#define		BPF_B		0x10
+#define BPF_MODE(code)	((code) & 0xe0)
+#define		BPF_IMM 	0x00
+#define		BPF_ABS		0x20
+#define		BPF_IND		0x40
+#define		BPF_MEM		0x60
+#define		BPF_LEN		0x80
+#define		BPF_MSH		0xa0
+
+/* alu/jmp fields */
+#define BPF_OP(code)	((code) & 0xf0)
+#define		BPF_ADD		0x00
+#define		BPF_SUB		0x10
+#define		BPF_MUL		0x20
+#define		BPF_DIV		0x30
+#define		BPF_OR		0x40
+#define		BPF_AND		0x50
+#define		BPF_LSH		0x60
+#define		BPF_RSH		0x70
+#define		BPF_NEG		0x80
+#define		BPF_JA		0x00
+#define		BPF_JEQ		0x10
+#define		BPF_JGT		0x20
+#define		BPF_JGE		0x30
+#define		BPF_JSET	0x40
+#define BPF_SRC(code)	((code) & 0x08)
+#define		BPF_K		0x00
+#define		BPF_X		0x08
+
+/* ret - BPF_K and BPF_X also apply */
+#define BPF_RVAL(code)	((code) & 0x18)
+#define		BPF_A		0x10
+
+/* misc */
+#define BPF_MISCOP(code) ((code) & 0xf8)
+#define		BPF_TAX		0x00
+#define		BPF_TXA		0x80
 
 /*
  * The instruction data structure.
@@ -153,10 +194,10 @@ struct bpf_insn {
 };
 
 /*
- * Macros for array initializers.
+ * Macros for insn array initializers.
  */
-#define BPF_STMT(code, k) { (u_short)code, 0, 0, k }
-#define BPF_JUMP(code, k, jt, jf) { (u_short)code, jt, jf, k }
+#define BPF_STMT(code, k) { (u_short)(code), 0, 0, k }
+#define BPF_JUMP(code, k, jt, jf) { (u_short)(code), jt, jf, k }
 
 #ifdef KERNEL
 extern u_int bpf_filter();
@@ -166,13 +207,6 @@ extern void bpf_mtap();
 #endif
 
 /*
- * These two macros are sensitive to the order in which the
- * opcodes appear in bpfcodes.h.
- */
-#define BPF_ISJUMP(code) ((unsigned)(code) <= (unsigned)EQOp)
-#define BPF_ISLEAF(code) ((unsigned)(code) >= (unsigned)RetOp)
-
-/*
- * Number of scratch memory words.
+ * Number of scratch memory words (for BPF_LD|BPF_MEM and BPF_ST).
  */
 #define BPF_MEMWORDS 16
