0d3e444bb5045fb2cccae09160355345da90a03f
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-05-02 20:34:29 -0800

    date and time created 91/05/02 13:34:29 by bostic
    SCCS-vsn: bin/stty/cchar.c 5.1

---
+++ usr/src/bin/stty/cchar.c
@@ -0,0 +1,51 @@
+/*-
+ * Copyright (c) 1991 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.redist.c%
+ */
+
+#ifndef lint
+static char sccsid[] = "@(#)cchar.c	5.1 (Berkeley) %G%";
+#endif /* not lint */
+
+#include <sys/types.h>
+#include <termios.h>
+#include <stddef.h>
+#include "stty.h"
+
+/*
+ * Special control characters.
+ *
+ * Cchars1 are the standard names, cchars2 are the old aliases.
+ * The first are displayed, but both are recognized on the
+ * command line.
+ */
+struct cchar cchars1[] = {
+	"discard",	VDISCARD, 	CDISCARD,
+	"dsusp", 	VDSUSP,		CDSUSP,
+	"eof",		VEOF,		CEOF,
+	"eol",		VEOL,		CEOL,
+	"eol2",		VEOL2,		CEOL,
+	"erase",	VERASE,		CERASE,
+	"intr",		VINTR,		CINTR,
+	"kill",		VKILL,		CKILL,
+	"lnext",	VLNEXT,		CLNEXT,
+	"quit",		VQUIT,		CQUIT,
+	"reprint",	VREPRINT, 	CREPRINT,
+	"start",	VSTART,		CSTART,
+	"status",	VSTATUS, 	CSTATUS,
+	"stop",		VSTOP,		CSTOP,
+	"susp",		VSUSP,		CSUSP,
+	"werase",	VWERASE,	CWERASE,
+	NULL
+};
+
+struct cchar cchars2[] = {
+	"brk",		VEOL,		CEOL,
+	"flush",	VDISCARD, 	CDISCARD,
+	"rprnt",	VREPRINT, 	CREPRINT,
+	"xoff",		VSTOP,		CSTOP,
+	"xon",		VSTART,		CSTART,
+	NULL
+};
