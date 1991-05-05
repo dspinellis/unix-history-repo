fcfc8a30c6b16bf477df7bd77f95cd030cb56e86
Mike Karels <karels@ucbvax.Berkeley.EDU>
1991-05-05 02:31:36 -0800

    u. is gone; new device calling conventions
    SCCS-vsn: sys/hp/dev/cons.c 7.6

--- usr/src/sys/hp/dev/cons.c
+++ usr/src/sys/hp/dev/cons.c
@@ -11,11 +11,11 @@
  *
  * from: Utah $Hdr: cons.c 1.1 90/07/09$
  *
- *	@(#)cons.c	7.5 (Berkeley) %G%
+ *	@(#)cons.c	7.6 (Berkeley) %G%
  */
 
 #include "sys/param.h"
-#include "sys/user.h"
+#include "sys/proc.h"
 #include "sys/systm.h"
 #include "sys/buf.h"
 #include "sys/ioctl.h"
@@ -83,22 +83,26 @@ cninit()
 	(*cp->cn_init)(cp);
 }
 
-cnopen(dev, flag)
+cnopen(dev, flag, mode, p)
 	dev_t dev;
+	int flag, mode;
+	struct proc *p;
 {
 	if (cn_tab == NULL)
-		return(0);
+		return (0);
 	dev = cn_tab->cn_dev;
-	return ((*cdevsw[major(dev)].d_open)(dev, flag));
+	return ((*cdevsw[major(dev)].d_open)(dev, flag, mode, p));
 }
  
-cnclose(dev, flag)
+cnclose(dev, flag, mode, p)
 	dev_t dev;
+	int flag, mode;
+	struct proc *p;
 {
 	if (cn_tab == NULL)
-		return(0);
+		return (0);
 	dev = cn_tab->cn_dev;
-	return ((*cdevsw[major(dev)].d_close)(dev, flag));
+	return ((*cdevsw[major(dev)].d_close)(dev, flag, mode, p));
 }
  
 cnread(dev, uio, flag)
@@ -106,7 +110,7 @@ cnread(dev, uio, flag)
 	struct uio *uio;
 {
 	if (cn_tab == NULL)
-		return(0);
+		return (0);
 	dev = cn_tab->cn_dev;
 	return ((*cdevsw[major(dev)].d_read)(dev, uio, flag));
 }
@@ -116,49 +120,51 @@ cnwrite(dev, uio, flag)
 	struct uio *uio;
 {
 	if (cn_tab == NULL)
-		return(0);
+		return (0);
 	dev = cn_tab->cn_dev;
 	return ((*cdevsw[major(dev)].d_write)(dev, uio, flag));
 }
  
-cnioctl(dev, cmd, data, flag)
+cnioctl(dev, cmd, data, flag, p)
 	dev_t dev;
 	caddr_t data;
+	struct proc *p;
 {
 	int error;
 
 	if (cn_tab == NULL)
-		return(0);
+		return (0);
 	/*
 	 * Superuser can always use this to wrest control of console
 	 * output from the "virtual" console.
 	 */
 	if (cmd == TIOCCONS && constty) {
-		error = suser(u.u_cred, &u.u_acflag);
+		error = suser(p->p_ucred, (u_short *) NULL);
 		if (error)
 			return (error);
 		constty = NULL;
 		return (0);
 	}
 	dev = cn_tab->cn_dev;
-	return ((*cdevsw[major(dev)].d_ioctl)(dev, cmd, data, flag));
+	return ((*cdevsw[major(dev)].d_ioctl)(dev, cmd, data, flag, p));
 }
 
 /*ARGSUSED*/
-cnselect(dev, rw)
+cnselect(dev, rw, p)
 	dev_t dev;
 	int rw;
+	struct proc *p;
 {
 	if (cn_tab == NULL)
-		return(1);
-	return(ttselect(cn_tab->cn_dev, rw));
+		return (1);
+	return (ttselect(cn_tab->cn_dev, rw, p));
 }
 
 cngetc()
 {
 	if (cn_tab == NULL)
-		return(0);
-	return((*cn_tab->cn_getc)(cn_tab->cn_dev));
+		return (0);
+	return ((*cn_tab->cn_getc)(cn_tab->cn_dev));
 }
 
 cnputc(c)
