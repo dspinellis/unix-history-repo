4c25dd5d848bc98d4b8949b3fac00226ca394a45
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-05-05 00:19:22 -0800

    new copyright; att/bsd/shared
    SCCS-vsn: sys/tahoe/stand/boot.c 7.6
    SCCS-vsn: sys/vax/stand/boot.c 7.15
    SCCS-vsn: sys/hp300/stand/boot.c 7.3
    SCCS-vsn: sys/vax/stand/bootxx.c 7.7

--- usr/src/sys/vax/stand/boot.c
+++ usr/src/sys/vax/stand/boot.c
@@ -1,9 +1,10 @@
-/*
- * Copyright (c) 1982, 1986 Regents of the University of California.
- * All rights reserved.  The Berkeley software License Agreement
- * specifies the terms and conditions for redistribution.
+/*-
+ * Copyright (c) 1982, 1986 The Regents of the University of California.
+ * All rights reserved.
  *
- *	@(#)boot.c	7.14 (Berkeley) %G%
+ * %sccs.include.redist.c%
+ *
+ *	@(#)boot.c	7.15 (Berkeley) %G%
  */
 
 #include "sys/param.h"
