d14f0869af72e6499c79249614e17cd3d88d4858
Mike Karels <karels@ucbvax.Berkeley.EDU>
1991-05-04 21:59:45 -0800

    attempt to clarify comment, move ansi/traditional cognates next to each other
    SCCS-vsn: sys/sys/cdefs.h 7.6

--- usr/src/sys/sys/cdefs.h
+++ usr/src/sys/sys/cdefs.h
@@ -1,10 +1,10 @@
 /*
- * Copyright (c) 1988 The Regents of the University of California.
+ * Copyright (c) 1991 The Regents of the University of California.
  * All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)cdefs.h	7.5 (Berkeley) %G%
+ *	@(#)cdefs.h	7.6 (Berkeley) %G%
  */
 
 #ifndef	_CDEFS_H_
@@ -19,29 +19,34 @@
 #endif
 
 /*
- * The __CONCAT macro is a bit tricky -- make sure you don't put
- * spaces in between its arguments, also you can't use strings in
- * code that's intended for historical compilers.
+ * The __CONCAT macro is used to concatenate parts of symbol names, e.g.
+ * with "#define OLD(foo) __CONCAT(old,foo)", OLD(foo) produces oldfoo.
+ * The __CONCAT macro is a bit tricky -- make sure you don't put spaces
+ * in between its arguments.  __CONCAT can also concatenate double-quoted
+ * strings produced by the __STRING macro, but this only works with ANSI C.
  */
 #if defined(__STDC__) || defined(__cplusplus)
 #define	__P(protos)	protos		/* full-blown ANSI C */
 #define	__CONCAT(x,y)	x ## y
 #define	__STRING(x)	#x
-#else					/* traditional style */
+
+#else	/* !(__STDC__ || __cplusplus) */
+#define	__P(protos)	()		/* traditional C preprocessor */
+#define	__CONCAT(x,y)	x/**/y
+#define	__STRING(x)	"x"
+
 #ifdef __GNUC__
 #define	const		__const		/* GCC: ANSI C with -traditional */
 #define	inline		__inline
 #define	signed		__signed
 #define	volatile	__volatile
-#else
+
+#else	/* !__GNUC__ */
 #define	const				/* delete ANSI C keywords */
 #define	inline
 #define	signed
 #define	volatile
-#endif
-#define	__P(protos)	()		/* traditional C preprocessor */
-#define	__CONCAT(x,y)	x/**/y
-#define	__STRING(x)	"x"
-#endif
+#endif	/* !__GNUC__ */
+#endif	/* !(__STDC__ || __cplusplus) */
 
 #endif /* !_CDEFS_H_ */
