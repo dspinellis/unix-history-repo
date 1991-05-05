069f2477a12b84f6f638604b57d5c7ec87dd416f
Mike Karels <karels@ucbvax.Berkeley.EDU>
1991-05-04 22:08:06 -0800

    (since 7.2:) change filedesc to point to one array of files/flags; initial allocation is in filedesc0 along with header, and expanded tables replace that one rather than augmenting it
    SCCS-vsn: sys/sys/filedesc.h 7.4

--- usr/src/sys/sys/filedesc.h
+++ usr/src/sys/sys/filedesc.h
@@ -4,26 +4,25 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)filedesc.h	7.3 (Berkeley) %G%
+ *	@(#)filedesc.h	7.4 (Berkeley) %G%
  */
-
-#ifndef _FILEDESC_H_
-#define _FILEDESC_H_
 
 /*
- * This structure is used for the management of descriptors.
- * It may be shared by multiple threads.
+ * This structure is used for the management of descriptors.  It may be
+ * shared by multiple processes.
  *
- * A process is initially started out with NDFILE worth of
- * descriptors, selected to be enough for typical applications
- * based on the historic limit of 20 open files. Additional
- * descriptors may be allocated up to a process' resource limit.
- * The initial expansion is set to NOEXTENT; each time it runs out,
- * it is doubled until the resource limit is reached. NOEXTENT should
- * be selected to be the biggest multiple of OFILESIZE (see below)
+ * A process is initially started out with NDFILE descriptors stored within
+ * this structure, selected to be enough for typical applications based on
+ * the historical limit of 20 open files (and the usage of descriptors by
+ * shells).  If these descriptors are exhausted, a larger descriptor table
+ * may be allocated, up to a process' resource limit; the internal arrays
+ * are then unused.  The initial expansion is set to NDEXTENT; each time
+ * it runs out, it is doubled until the resource limit is reached. NDEXTENT
+ * should be selected to be the biggest multiple of OFILESIZE (see below)
  * that will fit in a power-of-two sized piece of memory.
  */
-#define NOEXTENT	25		/* 125 bytes in 128-byte alloc. */ 
+#define NDFILE		20
+#define NDEXTENT	50		/* 250 bytes in 256-byte alloc. */ 
 
 struct filedesc {
 	struct	file **fd_ofiles;	/* file structures for open files */
@@ -31,10 +30,24 @@ struct filedesc {
 	struct	vnode *fd_cdir;		/* current directory */
 	struct	vnode *fd_rdir;		/* root directory */
 	int	fd_nfiles;		/* number of open files allocated */
-	int	fd_lastfile;		/* high-water mark of fd_ofiles */
-	int	fd_freefile;		/* approx. next free file */
+	u_short	fd_lastfile;		/* high-water mark of fd_ofiles */
+	u_short	fd_freefile;		/* approx. next free file */
 	u_short	fd_cmask;		/* mask for file creation */
 	u_short	fd_refcnt;		/* reference count */
+};
+
+/*
+ * Basic allocation of descriptors:
+ * one of the above, plus arrays for NDFILE descriptors.
+ */
+struct filedesc0 {
+	struct	filedesc fd_fd;
+	/*
+	 * These arrays are used when the number of open files is
+	 * <= NDFILE, and are then pointed to by the pointers above.
+	 */
+	struct	file *fd_dfiles[NDFILE];
+	char	fd_dfileflags[NDFILE];
 };
 
 /*
@@ -44,15 +57,17 @@ struct filedesc {
 #define	UF_MAPPED 	0x02		/* mapped from device */
 
 /*
- * Data structure access macros.
+ * Storage required per open file descriptor.
  */
-#define OFILE(fd, indx)	((fd)->fd_ofiles[indx])
-#define OFILEFLAGS(fd, indx) ((fd)->fd_ofileflags[indx])
 #define OFILESIZE (sizeof(struct file *) + sizeof(char))
 
+#ifdef KERNEL
 /*
  * Kernel global variables and routines.
  */
-extern struct filedesc *fdcopy();
-
-#endif /* !_FILEDESC_H_ */
+int	fdalloc __P((struct proc *p, int want, int *result));
+int	fdavail __P((struct proc *p, int n));
+int	falloc __P((struct proc *p, struct file **resultfp, int *resultfd));
+struct	filedesc *fdcopy __P((struct proc *p));
+void	fdfree __P((struct proc *p));
+#endif
