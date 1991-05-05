13fae383b480d5a17e255ecd898d51f0745d3f79
Mike Karels <karels@ucbvax.Berkeley.EDU>
1991-05-05 02:26:10 -0800

    add function prototypes
    SCCS-vsn: sys/sys/tprintf.h 7.2

--- usr/src/sys/sys/tprintf.h
+++ usr/src/sys/sys/tprintf.h
@@ -9,4 +9,7 @@
 
 typedef struct session *tpr_t;
 
-tpr_t	tprintf_open();
+tpr_t	tprintf_open __P((struct proc *));
+void	tprintf_close __P((tpr_t));
+
+void	tprintf __P((tpr_t, const char *fmt, ...));
