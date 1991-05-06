e156506147b0bc7a28aa9f73b525e13d19a3ac5c
William Nesheim <william@ucbvax.Berkeley.EDU>
1991-05-05 21:28:20 -0800

    bug in kprintn, buffer too small
    SCCS-vsn: sys/stand.att/printf.c 5.5

--- usr/src/sys/stand.att/printf.c
+++ usr/src/sys/stand.att/printf.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)printf.c	5.4 (Berkeley) %G%
+ *	@(#)printf.c	5.5 (Berkeley) %G%
  */
 
 /*
@@ -31,6 +31,7 @@
  */
 
 #include <sys/cdefs.h>
+#include <sys/types.h>
 
 /*
  * Note that stdarg.h and the ANSI style va_start macro is used for both
@@ -148,7 +149,8 @@ kprintn(ul, base)
 	unsigned long ul;
 	int base;
 {
-	char *p, buf[11];			/* hold 2^32 in base 8 */
+					/* hold a long in base 8 */
+	char *p, buf[(sizeof(long) * NBBY / 3) + 1];
 
 	p = buf;
 	do {
