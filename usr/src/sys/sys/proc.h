2360f0b7cbe9789713036a732e8ea00ece497e61
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-05-06 01:11:27 -0800

    new copyright; att/bsd/shared
    SCCS-vsn: sys/sys/proc.h 7.27
    SCCS-vsn: sys/sys/socketvar.h 7.17

--- usr/src/sys/sys/proc.h
+++ usr/src/sys/sys/proc.h
@@ -1,9 +1,10 @@
-/*
- * Copyright (c) 1982, 1986, 1989, 1991 Regents of the University of California.
- * All rights reserved.  The Berkeley software License Agreement
- * specifies the terms and conditions for redistribution.
+/*-
+ * Copyright (c) 1986, 1989, 1991 The Regents of the University of California.
+ * All rights reserved.
  *
- *	@(#)proc.h	7.26 (Berkeley) %G%
+ * %sccs.include.redist.c%
+ *
+ *	@(#)proc.h	7.27 (Berkeley) %G%
  */
 
 #ifndef _PROC_H_
