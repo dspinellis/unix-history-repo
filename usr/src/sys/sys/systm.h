0ce1f1f6966d0740e61cdd9460ba90d0d2c624c8
Mike Karels <karels@ucbvax.Berkeley.EDU>
1991-05-06 18:08:08 -0800

    prettiness patrol II: I like function grouping better than alphabetical; remove intstack, argdev, icode, other old/grotty stuff
    SCCS-vsn: sys/sys/systm.h 7.15

--- usr/src/sys/sys/systm.h
+++ usr/src/sys/sys/systm.h
@@ -4,11 +4,12 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)systm.h	7.14 (Berkeley) %G%
+ *	@(#)systm.h	7.15 (Berkeley) %G%
  */
 
 extern char *panicstr;		/* panic message */
-extern char *version;		/* system version */
+extern char version[];		/* system version */
+extern char copyright[];	/* system copyright */
 
 extern int nblkdev;		/* number of entries in bdevsw */
 extern int nchrdev;		/* number of entries in cdevsw */
@@ -24,11 +25,6 @@ extern u_char curpri;		/* priority of current process */
 extern int maxmem;		/* max memory per process */
 extern int physmem;		/* physical memory */
 
-extern int intstack[];		/* interrupt stack */
-
-extern int icode[];		/* user init code */
-extern int szicode;		/* size of icode */
-
 extern dev_t dumpdev;		/* dump device */
 extern long dumplo;		/* offset into dumpdev */
 
@@ -38,64 +34,61 @@ extern struct vnode *rootvp;	/* vnode equivalent to above */
 extern dev_t swapdev;		/* swapping device */
 extern struct vnode *swapdev_vp;/* vnode equivalent to above */
 
-extern dev_t argdev;		/* argument lists device */
-extern struct vnode *argdev_vp;	/* vnode equivalent to above */
-
 extern struct sysent {		/* system call table */
-	int sy_narg;		/* number of arguments */
-	int (*sy_call)();	/* implementing function */
+	int	sy_narg;	/* number of arguments */
+	int	(*sy_call)();	/* implementing function */
 } sysent[];
 
+extern int boothowto;		/* reboot flags, from console subsystem */
 #ifdef	KADB
 extern char *bootesym;		/* end of symbol info from boot */
 #endif
-extern int boothowto;		/* reboot flags, from console subsystem */
 
 /* casts to keep lint happy */
 #define	insque(q,p)	_insque((caddr_t)q,(caddr_t)p)
 #define	remque(q)	_remque((caddr_t)q)
 
 /*
- * General function prototypes.
+ * General function declarations.
  */
+int	nullop __P((void));
 int	enodev __P((void));
 int	enoioctl __P((void));
 int	enxio __P((void));
 int	eopnotsupp __P((void));
-int	nullop __P((void));
 int	seltrue __P((dev_t dev, int which, struct proc *p));
 
+void	panic __P((char *));
+void	tablefull __P((char *));
 void	addlog __P((const char *, ...));
 void	log __P((int, const char *, ...));
-void	panic __P((char *));
 void	printf __P((const char *, ...));
-void	tablefull __P((char *));
 void	ttyprintf __P((struct tty *, const char *, ...));
 
-int	bcmp __P((void *str1, void *str2, u_int len));
 void	bcopy __P((void *from, void *to, u_int len));
-void	bzero __P((void *buf, u_int len));
 void	ovbcopy __P((void *from, void *to, u_int len));
+void	bzero __P((void *buf, u_int len));
+int	bcmp __P((void *str1, void *str2, u_int len));
 int	strlen __P((char *string));
 
-int	copyin __P((void *udaddr, void *kaddr, u_int len));
-int	copyinstr __P((void *udaddr, void *kaddr, u_int len, u_int *done));
-int	copyout __P((void *kaddr, void *udaddr, u_int len));
-int	copyoutstr __P((void *kaddr, void *udaddr, u_int len, u_int *done));
 int	copystr __P((void *kfaddr, void *kdaddr, u_int len, u_int *done));
+int	copyinstr __P((void *udaddr, void *kaddr, u_int len, u_int *done));
+int	copyoutstr __P((void *kaddr, void *udaddr, u_int len, u_int *done));
+int	copyin __P((void *udaddr, void *kaddr, u_int len));
+int	copyout __P((void *kaddr, void *udaddr, u_int len));
 
 int	fubyte __P((void *base));
 #ifdef notdef
 int	fuibyte __P((void *base));
 #endif
-int	fuiword __P((void *base));
-int	fuword __P((void *base));
 int	subyte __P((void *base, int byte));
 int	suibyte __P((void *base, int byte));
-int	suiword __P((void *base, int word));
+int	fuword __P((void *base));
+int	fuiword __P((void *base));
 int	suword __P((void *base, int word));
+int	suiword __P((void *base, int word));
 
-int	ffs __P((long value));
-int	locc __P((int mask, char *cp, unsigned size));
 int	scanc __P((unsigned size, u_char *cp, u_char *table, int mask));
 int	skpc __P((int mask, int size, char *cp));
+int	locc __P((int mask, char *cp, unsigned size));
+int	ffs __P((long value));
