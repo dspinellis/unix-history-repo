efdbba10efd327a72f278bc9d70ecab148a62fa1
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-05-07 00:53:42 -0800

    change dev database to use "mode_t type dev_t dev" as the key
    SCCS-vsn: lib/libc/gen/devname.c 5.14

--- usr/src/lib/libc/gen/devname.c
+++ usr/src/lib/libc/gen/devname.c
@@ -6,7 +6,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)devname.c	5.13 (Berkeley) %G%";
+static char sccsid[] = "@(#)devname.c	5.14 (Berkeley) %G%";
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/types.h>
@@ -16,9 +16,14 @@ static char sccsid[] = "@(#)devname.c	5.13 (Berkeley) %G%";
 #include <paths.h>
 
 char *
-devname(dev)
+devname(dev, type)
 	dev_t dev;
+	mode_t type;
 {
+	struct {
+		mode_t type;
+		dev_t dev;
+	} bkey;
 	static DB *db;
 	static int failure;
 	DBT data, key;
@@ -26,13 +31,19 @@ devname(dev)
 	if (!db && !failure &&
 	    !(db = hash_open(_PATH_DEVDB, O_RDONLY, 0, NULL))) {
 		(void)fprintf(stderr,
-		    "ps: no device database %s\n", _PATH_DEVDB);
+		    "warning: no device database %s\n", _PATH_DEVDB);
 		failure = 1;
 	}
 	if (failure)
 		return("??");
 
-	key.data = (u_char *)&dev;
-	key.size = sizeof(dev);
+	/*
+	 * Keys are a mode_t followed by a dev_t.  The former is the type of
+	 * the file (mode & S_IFMT), the latter is the st_rdev field.
+	 */
+	bkey.dev = dev;
+	bkey.type = type;
+	key.data = &bkey;
+	key.size = sizeof(bkey);
 	return((db->get)(db, &key, &data, 0L) ? "??" : (char *)data.data);
 }
