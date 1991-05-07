f88533b66390586f5a39019492ca1f23b4c45650
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-05-06 23:41:59 -0800

    new copyright; att/bsd/shared
    SCCS-vsn: usr.sbin/sa/sa.c 4.14

--- usr/src/usr.sbin/sa/sa.c
+++ usr/src/usr.sbin/sa/sa.c
@@ -1,6 +1,19 @@
+/*-
+ * Copyright (c) 1991 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.proprietary.c%
+ */
+
 #ifndef lint
-static char *sccsid = "@(#)sa.c	4.13 (Berkeley) %G%";
-#endif
+char copyright[] =
+"@(#) Copyright (c) 1991 The Regents of the University of California.\n\
+ All rights reserved.\n";
+#endif /* not lint */
+
+#ifndef lint
+static char sccsid[] = "@(#)sa.c	4.14 (Berkeley) %G%";
+#endif /* not lint */
 
 /*
  *	Extensive modifications to internal data structures
