51f1abdee7d4033e53a963da5c40b1868ab4d5c6
Mike Karels <karels@ucbvax.Berkeley.EDU>
1991-05-06 17:48:15 -0800

    argdev is gone
    SCCS-vsn: sys/nfs/nfs_vfsops.c 7.31

--- usr/src/sys/nfs/nfs_vfsops.c
+++ usr/src/sys/nfs/nfs_vfsops.c
@@ -7,7 +7,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)nfs_vfsops.c	7.30 (Berkeley) %G%
+ *	@(#)nfs_vfsops.c	7.31 (Berkeley) %G%
  */
 
 #include "param.h"
@@ -197,8 +197,6 @@ nfs_mountroot()
 		swapdev_vp = vp;
 		VREF(vp);
 		swdevt[0].sw_vp = vp;
-		VREF(vp);
-		argdev_vp = vp;
 	}
 
 	/*
