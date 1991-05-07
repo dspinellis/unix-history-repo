ae53fa6227a3d4d2c3b7f22ce8a11755c13a8eda
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-05-06 22:37:27 -0800

    create files with default modes; ANSI cleanup
    SCCS-vsn: usr.bin/tee/tee.c 5.11

--- usr/src/usr.bin/tee/tee.c
+++ usr/src/usr.bin/tee/tee.c
@@ -12,7 +12,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)tee.c	5.10 (Berkeley) %G%";
+static char sccsid[] = "@(#)tee.c	5.11 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/types.h>
