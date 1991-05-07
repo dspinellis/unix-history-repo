feb41f918ea1b5f0fe01cb1da4117231acd32fe4
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-05-07 01:12:49 -0800

    new copyright; att/bsd/shared
    SCCS-vsn: usr.sbin/quot/quot.c 4.20

--- usr/src/usr.sbin/quot/quot.c
+++ usr/src/usr.sbin/quot/quot.c
@@ -1,6 +1,19 @@
+/*-
+ * Copyright (c) 1991 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.proprietary.c%
+ */
+
 #ifndef lint
-static char *sccsid = "@(#)quot.c	4.19 (Berkeley) 90/06/23";
-#endif
+char copyright[] =
+"@(#) Copyright (c) 1991 The Regents of the University of California.\n\
+ All rights reserved.\n";
+#endif /* not lint */
+
+#ifndef lint
+static char sccsid[] = "@(#)quot.c	4.20 (Berkeley) %G%";
+#endif /* not lint */
 
 /*
  * quot
