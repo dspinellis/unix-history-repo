e0d9fd43d08124e386f0a3c40784716dbaaab94e
Mike Hibler <hibler@ucbvax.Berkeley.EDU>
1991-05-07 16:46:18 -0800

    fd -> vn
    SCCS-vsn: sys/hp300/conf/devices.hp300 7.2
    SCCS-vsn: sys/hp300/conf/files.hp300 7.8
    SCCS-vsn: sys/dev/vnioctl.h 7.3
    SCCS-vsn: sys/dev/vn.c 7.5

--- usr/src/sys/dev/vnioctl.h
+++ usr/src/sys/dev/vnioctl.h
@@ -11,7 +11,7 @@
  *
  * from: Utah $Hdr: fdioctl.h 1.1 90/07/09$
  *
- *	@(#)vnioctl.h	7.2 (Berkeley) %G%
+ *	@(#)vnioctl.h	7.3 (Berkeley) %G%
  */
 
 /*
@@ -20,16 +20,16 @@
 
 #define FDISKFILE	"/etc/fdisks"	/* default config file */
 
-struct fd_ioctl {
-	char	*fd_file;	/* pathname of file to mount */
-	int	fd_size;	/* (returned) size of disk */
+struct vn_ioctl {
+	char	*vn_file;	/* pathname of file to mount */
+	int	vn_size;	/* (returned) size of disk */
 };
 
 /*
- * Before you can use a unit, it must be configured with FDIOCSET.
+ * Before you can use a unit, it must be configured with VNIOCSET.
  * The configuration persists across opens and closes of the device;
- * an FDIOCCLR must be used to reset a configuration.  An attempt to
- * FDIOCSET an already active unit will return EBUSY.
+ * an VNIOCCLR must be used to reset a configuration.  An attempt to
+ * VNIOCSET an already active unit will return EBUSY.
  */
-#define FDIOCSET	_IOWR('F', 0, struct fd_ioctl)	/* enable disk */
-#define FDIOCCLR	_IOW('F', 1, struct fd_ioctl)	/* disable disk */
+#define VNIOCSET	_IOWR('F', 0, struct vn_ioctl)	/* enable disk */
+#define VNIOCCLR	_IOW('F', 1, struct vn_ioctl)	/* disable disk */
