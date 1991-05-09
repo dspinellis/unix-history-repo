ef1ba5b37235999d4559c02c0a4d2dbb249b270b
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-05-08 20:55:26 -0800

    new copyright; att/bsd/shared
    SCCS-vsn: sys/hp300/hp300/kgdb_proto.h 1.3

--- usr/src/sys/hp300/hp300/kgdb_proto.h
+++ usr/src/sys/hp300/hp300/kgdb_proto.h
@@ -1,21 +1,15 @@
-/*
- * Copyright (c) 1990 Regents of the University of California.
+/*-
+ * Copyright (c) 1991 The Regents of the University of California.
  * All rights reserved.
  *
- * Redistribution and use in source and binary forms are permitted
- * provided that the above copyright notice and this paragraph are
- * duplicated in all such forms and that any documentation,
- * advertising materials, and other materials related to such
- * distribution and use acknowledge that the software was developed
- * by the University of California, Lawrence Berkeley Laboratory,
- * Berkeley, CA.  The name of the University may not be used to
- * endorse or promote products derived from this software without
- * specific prior written permission.
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
- * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ * This code is derived from software contributed to Berkeley by
+ * Steven McCanne of Lawrence Berkeley Laboratory.
  *
- * @(#) $Header: kgdb_proto.h,v 1.3 91/03/12 22:06:43 mccanne Exp $ (LBL)
+ * %sccs.include.redist.c%
+ *
+ *	@(#)kgdb_proto.h	1.3 (Berkeley) %G%
+ *
+ * $Header: kgdb_proto.h,v 1.3 91/03/12 22:06:43 mccanne Exp $ (LBL)
  */
 
 /*
