771943d369fe7d517f4a604e0279af531ab38ef5
Mike Karels <karels@ucbvax.Berkeley.EDU>
1991-05-08 20:57:54 -0800

    merge with stuff from pmap.h
    SCCS-vsn: sys/hp300/hp300/pte.h 7.3

--- usr/src/sys/hp300/hp300/pte.h
+++ usr/src/sys/hp300/hp300/pte.h
@@ -11,63 +11,43 @@
  *
  * from: Utah $Hdr: pte.h 1.11 89/09/03$
  *
- *	@(#)pte.h	7.2 (Berkeley) %G%
+ *	@(#)pte.h	7.3 (Berkeley) %G%
  */
 
 /*
- * HP300 page table entry
- *
- * There are two major kinds of pte's: those which have ever existed (and are
- * thus either now in core or on the swap device), and those which have
- * never existed, but which will be filled on demand at first reference.
- * There is a structure describing each.  There is also an ancillary
- * structure used in page clustering.
+ * HP300 hardware segment/page table entries
  */
 
-#ifndef LOCORE
-struct ste
-{
-unsigned int	sg_pfnum:20,		/* page table frame number */
-		:8,			/* reserved at 0 */
-		:1,			/* reserved at 1 */
-		sg_prot:1,		/* write protect bit */
-		sg_v:2;			/* valid bits */
+struct ste {
+	unsigned int	sg_pfnum:20;	/* page table frame number */
+	unsigned int	:8;		/* reserved at 0 */
+	unsigned int	:1;		/* reserved at 1 */
+	unsigned int	sg_prot:1;	/* write protect bit */
+	unsigned int	sg_v:2;		/* valid bits */
 };
 
-struct pte
-{
-unsigned int	pg_pfnum:20,		/* page frame number or 0 */
-		:3,
-		pg_fod:1,		/* is fill on demand (=0) */
-		:1,			/* reserved at zero */
-		pg_ci:1,		/* cache inhibit bit */
-		:1,			/* reserved at zero */
-		pg_m:1,			/* hardware modified (dirty) bit */
-		pg_u:1,			/* hardware used (reference) bit */
-		pg_prot:1,		/* write protect bit */
-		pg_v:2;			/* valid bit */
+struct pte {
+	unsigned int	pg_pfnum:20;	/* page frame number or 0 */
+	unsigned int	:3;
+	unsigned int	pg_w:1;		/* is wired */
+	unsigned int	:1;		/* reserved at zero */
+	unsigned int	pg_ci:1;	/* cache inhibit bit */
+	unsigned int	:1;		/* reserved at zero */
+	unsigned int	pg_m:1;		/* hardware modified (dirty) bit */
+	unsigned int	pg_u:1;		/* hardware used (reference) bit */
+	unsigned int	pg_prot:1;	/* write protect bit */
+	unsigned int	pg_v:2;		/* valid bit */
 };
 
-/* not used */
-struct hpte
-{
-unsigned int	pg_pfnum:20,
-		pg_high:12;		/* special for clustering */
-};
+typedef struct ste	st_entry_t;	/* segment table entry */
+typedef struct pte	pt_entry_t;	/* Mach page table entry */
 
-struct fpte
-{
-unsigned int	pg_blkno:22,		/* file system block number */
-		pg_fileno:1,		/* file mapped from or TEXT or ZERO */
-		pg_fod:1,		/* is fill on demand (=1) */
-		:6,
-		pg_v:2;
-};
-#endif
+#define	PT_ENTRY_NULL	((pt_entry_t *) 0)
+#define	ST_ENTRY_NULL	((st_entry_t *) 0)
 
-#define	SG_V		0x00000002
+#define	SG_V		0x00000002	/* segment is valid */
 #define	SG_NV		0x00000000
-#define	SG_PROT		0x00000004
+#define	SG_PROT		0x00000004	/* access protection mask */
 #define	SG_RO		0x00000004
 #define	SG_RW		0x00000000
 #define	SG_FRAME	0xfffff000
@@ -81,41 +61,27 @@ unsigned int	pg_blkno:22,		/* file system block number */
 #define	PG_PROT		0x00000004
 #define	PG_U		0x00000008
 #define	PG_M		0x00000010
-#define	PG_FOD		0x00000100
+#define	PG_W		0x00000100
 #define	PG_RO		0x00000004
 #define	PG_RW		0x00000000
 #define	PG_FRAME	0xfffff000
 #define	PG_CI		0x00000040
-#define	PG_PFNUM(x)	(((x) & PG_FRAME) >> PGSHIFT)
+#define PG_SHIFT	12
+#define	PG_PFNUM(x)	(((x) & PG_FRAME) >> PG_SHIFT)
 
-/*
- * Pseudo protections.
- * Note that PG_URKW is not defined intuitively, but it is currently only
- * used in vgetu() to initialize the u-area PTEs in the process address
- * space.  Since the kernel never accesses the u-area thru these we are ok.
- */
-#define	PG_KW		PG_RW
-#define	PG_URKR		PG_RO
-#define	PG_URKW		PG_RO
-#define	PG_UW		PG_RW
-
-#define	PG_FZERO	0
-#define	PG_FTEXT	1
-#define	PG_FMAX		(PG_FTEXT)
-
-/*
- * Pte related macros
- */
-#define	dirty(pte)	((pte)->pg_m)
+#define HP_STSIZE	HP_PAGE_SIZE	/* segment table size */
+#define HP_MAX_PTSIZE	HP_SEG_SIZE	/* max size of UPT */
+#define HP_MAX_KPTSIZE	0x100000	/* max memory to allocate to KPT */
+#define HP_PTBASE	0x10000000	/* UPT map base address */
+#define HP_PTMAXSIZE	0x70000000	/* UPT map maximum size */
 
 /*
  * Kernel virtual address to page table entry and to physical address.
  */
-#define	kvtopte(va) (&Sysmap[((unsigned)(va) &~ KERNBASE) >> PGSHIFT])
-#define	ptetokv(pt) ((((struct pte *)(pt) - Sysmap) << PGSHIFT) | KERNBASE)
-#define	kvtophys(x) ((kvtopte(x)->pg_pfnum << PGSHIFT) | ((int)(x) & PGOFSET))
+#define	kvtopte(va) \
+	(&Sysmap[((unsigned)(va) - VM_MIN_KERNEL_ADDRESS) >> PGSHIFT])
+#define	ptetokv(pt) \
+	((((pt_entry_t *)(pt) - Sysmap) << PGSHIFT) + VM_MIN_KERNEL_ADDRESS)
+#define	kvtophys(va) \
+	((kvtopte(va)->pg_pfnum << PGSHIFT) | ((int)(va) & PGOFSET))
 
-#if defined(KERNEL) && !defined(LOCORE)
-/* utilities defined in pmap.c */
-extern	struct pte *Sysmap;
-#endif /* defined(KERNEL) && !defined(LOCORE) */
