ec0758e2135fe4d6f0ae09e9afe7a936bd03bde0
William Nesheim <william@ucbvax.Berkeley.EDU>
1991-05-10 02:25:20 -0800

    scccs keywords, release no
    SCCS-vsn: sys/i386/i386/mem.c 7.1

--- usr/src/sys/i386/i386/mem.c
+++ usr/src/sys/i386/i386/mem.c
@@ -8,22 +8,10 @@
  * Science Department, and code derived from software contributed to
  * Berkeley by William Jolitz.
  *
- * Redistribution and use in source and binary forms are permitted provided
- * that: (1) source distributions retain this entire copyright notice and
- * comment, and (2) distributions including binaries display the following
- * acknowledgement:  ``This product includes software developed by the
- * University of California, Berkeley and its contributors'' in the
- * documentation or other materials provided with the distribution and in
- * all advertising materials mentioning features or use of this software.
- * Neither the name of the University nor the names of its contributors may
- * be used to endorse or promote products derived from this software without
- * specific prior written permission.
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR IMPLIED
- * WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ * %sccs.include.noredist.c%
  *
  * from: Utah $Hdr: mem.c 1.13 89/10/08$
- *	@(#)mem.c	5.6 (Berkeley) 1/19/91
+ *	@(#)mem.c	7.1 (Berkeley) %G%
  */
 
 /*
