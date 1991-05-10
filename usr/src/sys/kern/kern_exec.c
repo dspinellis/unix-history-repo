317ed0820a6f548f514e494bb19f87961268d609
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-05-10 04:18:15 -0800

    new copyright; att/bsd/shared
    SCCS-vsn: sys/kern/kern_acct.c 7.17
    SCCS-vsn: sys/kern/kern_exec.c 7.43

--- usr/src/sys/kern/kern_exec.c
+++ usr/src/sys/kern/kern_exec.c
@@ -1,9 +1,10 @@
-/*
- * Copyright (c) 1982, 1986, 1989, 1991 Regents of the University of California.
- * All rights reserved.  The Berkeley software License Agreement
- * specifies the terms and conditions for redistribution.
+/*-
+ * Copyright (c) 1982, 1986, 1991 The Regents of the University of California.
+ * All rights reserved.
  *
- *	@(#)kern_exec.c	7.42 (Berkeley) %G%
+ * %sccs.include.proprietary.c%
+ *
+ *	@(#)kern_exec.c	7.43 (Berkeley) %G%
  */
 
 #include "param.h"
