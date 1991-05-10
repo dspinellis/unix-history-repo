6d81d48afad24e87e66de1d98541ace0adef4556
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-05-10 02:00:17 -0800

    date and time created 91/05/09 19:00:17 by bostic
    SCCS-vsn: sys/i386/floppy/rm/rm.c 5.1

---
+++ usr/src/sys/i386/floppy/rm/rm.c
@@ -0,0 +1,27 @@
+/*-
+ * Copyright (c) 1991 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.redist.c%
+ */
+
+int eval;
+
+main(argc, argv)
+	int argc;
+	char **argv;
+{
+	if (*++argv && **argv == '-') {
+		err("no options available", 0);
+		_exit(1);
+	}
+	for (eval = 0; *argv; ++argv)
+		if (unlink(*argv)) {
+			err(*argv, 1);
+			eval = 1;
+		}
+	_exit(eval);
+}
+
+#define	PROGNAME	"rm: "
+#include "errfunction"
