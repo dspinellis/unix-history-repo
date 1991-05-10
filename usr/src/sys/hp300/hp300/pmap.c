cf5f2ba7e71ba9e3dee1d068e9cff9aea73984a3
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-05-10 06:14:59 -0800

    get the copyright right
    SCCS-vsn: sys/hp300/hp300/pmap.c 7.7

--- usr/src/sys/hp300/hp300/pmap.c
+++ usr/src/sys/hp300/hp300/pmap.c
@@ -9,7 +9,7 @@
  * The CMU software License Agreement specifies the terms and conditions
  * for use and redistribution.
  *
- *	@(#)pmap.c	7.6 (Berkeley) %G%
+ *	@(#)pmap.c	7.7 (Berkeley) %G%
  */
 
 /*
