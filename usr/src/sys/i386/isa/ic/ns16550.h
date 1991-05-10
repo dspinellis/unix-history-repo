4501391952b0ea02e2d9c92182ec9fa8f87be5df
William Nesheim <william@ucbvax.Berkeley.EDU>
1991-05-10 04:40:34 -0800

    date and time created 91/05/09 21:40:34 by william
    SCCS-vsn: sys/i386/isa/ic/ns16550.h 7.1

---
+++ usr/src/sys/i386/isa/ic/ns16550.h
@@ -0,0 +1,24 @@
+/*-
+ * Copyright (c) 1991 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.redist.c%
+ *
+ *	@(#)ns16550.h	7.1 (Berkeley) %G%
+ */
+
+/*
+ * NS16550 UART registers
+ */
+
+#define	com_data	0	/* data register (R/W) */
+#define	com_dlbl	0	/* divisor latch low (W) */
+#define	com_dlbh	1	/* divisor latch high (W) */
+#define	com_ier		1	/* interrupt enable (W) */
+#define	com_iir		2	/* interrupt identification (R) */
+#define	com_fifo	2	/* FIFO control (W) */
+#define	com_lctl	3	/* line control register (R/W) */
+#define	com_cfcr	3	/* line control register (R/W) */
+#define	com_mcr		4	/* modem control register (R/W) */
+#define	com_lsr		5	/* line status register (R/W) */
+#define	com_msr		6	/* modem status register (R/W) */
