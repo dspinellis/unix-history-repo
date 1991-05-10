258046c95ffe520ec5d7af4106bd8e709f7f7036
William Nesheim <william@ucbvax.Berkeley.EDU>
1991-05-10 02:19:26 -0800

    final version
    SCCS-vsn: sys/i386/i386/mem.c 5.7

--- usr/src/sys/i386/i386/mem.c
+++ usr/src/sys/i386/i386/mem.c
@@ -8,30 +8,44 @@
  * Science Department, and code derived from software contributed to
  * Berkeley by William Jolitz.
  *
- * %sccs.include.redist.c%
+ * Redistribution and use in source and binary forms are permitted provided
+ * that: (1) source distributions retain this entire copyright notice and
+ * comment, and (2) distributions including binaries display the following
+ * acknowledgement:  ``This product includes software developed by the
+ * University of California, Berkeley and its contributors'' in the
+ * documentation or other materials provided with the distribution and in
+ * all advertising materials mentioning features or use of this software.
+ * Neither the name of the University nor the names of its contributors may
+ * be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  *
  * from: Utah $Hdr: mem.c 1.13 89/10/08$
- *	@(#)mem.c	5.6 (Berkeley) %G%
+ *	@(#)mem.c	5.6 (Berkeley) 1/19/91
  */
 
 /*
  * Memory special file
  */
 
-#include "machine/pte.h"
-
 #include "param.h"
-#include "user.h"
 #include "conf.h"
 #include "buf.h"
 #include "systm.h"
-#include "vm.h"
-#include "cmap.h"
 #include "uio.h"
 #include "malloc.h"
 
 #include "machine/cpu.h"
 
+#include "vm/vm_param.h"
+#include "vm/lock.h"
+#include "vm/vm_statistics.h"
+#include "vm/pmap.h"
+#include "vm/vm_prot.h"
+
+extern        char *vmmap;            /* poor name! */
 /*ARGSUSED*/
 mmrw(dev, uio, flags)
 	dev_t dev;
@@ -58,14 +72,15 @@ mmrw(dev, uio, flags)
 /* minor device 0 is physical memory */
 		case 0:
 			v = uio->uio_offset;
-			*(int *)mmap = (v & PG_FRAME) | PG_V |
-				(uio->uio_rw == UIO_READ ? PG_KR : PG_KW);
-			load_cr3(u.u_pcb.pcb_cr3);
+			pmap_enter(pmap_kernel(), vmmap, v,
+				uio->uio_rw == UIO_READ ? VM_PROT_READ : VM_PROT_WRITE,
+				TRUE);
 			o = (int)uio->uio_offset & PGOFSET;
 			c = (u_int)(NBPG - ((int)iov->iov_base & PGOFSET));
 			c = MIN(c, (u_int)(NBPG - o));
 			c = MIN(c, (u_int)iov->iov_len);
 			error = uiomove((caddr_t)&vmmap[o], (int)c, uio);
+			pmap_remove(pmap_kernel(), vmmap, &vmmap[NBPG]);
 			continue;
 
 /* minor device 1 is kernel memory */
@@ -73,7 +88,7 @@ mmrw(dev, uio, flags)
 			c = iov->iov_len;
 			if (!kernacc((caddr_t)uio->uio_offset, c,
 			    uio->uio_rw == UIO_READ ? B_READ : B_WRITE))
-				goto fault;
+				return(EFAULT);
 			error = uiomove((caddr_t)uio->uio_offset, (int)c, uio);
 			continue;
 
@@ -99,6 +114,53 @@ mmrw(dev, uio, flags)
 			error = uiomove(zbuf, (int)c, uio);
 			continue;
 
+#ifdef notyet
+/* 386 I/O address space (/dev/ioport[bwl]) is a read/write access to seperate
+   i/o device address bus, different than memory bus. Semantics here are
+   very different than ordinary read/write, as if iov_len is a multiple
+   an implied string move from a single port will be done. Note that lseek
+   must be used to set the port number reliably. */
+		case 14:
+			if (iov->iov_len == 1) {
+				u_char tmp;
+				tmp = inb(uio->uio_offset);
+				error = uiomove (&tmp, iov->iov_len, uio);
+			} else {
+				if (!useracc((caddr_t)iov->iov_base,
+					iov->iov_len, uio->uio_rw))
+					return (EFAULT);
+				insb(uio->uio_offset, iov->iov_base,
+					iov->iov_len);
+			}
+			break;
+		case 15:
+			if (iov->iov_len == sizeof (short)) {
+				u_short tmp;
+				tmp = inw(uio->uio_offset);
+				error = uiomove (&tmp, iov->iov_len, uio);
+			} else {
+				if (!useracc((caddr_t)iov->iov_base,
+					iov->iov_len, uio->uio_rw))
+					return (EFAULT);
+				insw(uio->uio_offset, iov->iov_base,
+					iov->iov_len/ sizeof (short));
+			}
+			break;
+		case 16:
+			if (iov->iov_len == sizeof (long)) {
+				u_long tmp;
+				tmp = inl(uio->uio_offset);
+				error = uiomove (&tmp, iov->iov_len, uio);
+			} else {
+				if (!useracc((caddr_t)iov->iov_base,
+					iov->iov_len, uio->uio_rw))
+					return (EFAULT);
+				insl(uio->uio_offset, iov->iov_base,
+					iov->iov_len/ sizeof (long));
+			}
+			break;
+#endif
+
 		default:
 			return (ENXIO);
 		}
@@ -112,6 +174,4 @@ mmrw(dev, uio, flags)
 	if (zbuf)
 		free(zbuf, M_TEMP);
 	return (error);
-fault:
-	return (EFAULT);
 }
