1b6051e9d6230665ba063ece83fced7944168f8c
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-05-10 03:39:36 -0800

    date and time created 91/05/09 20:39:36 by bostic
    SCCS-vsn: sys/sys/resourcevar.h 7.1

---
+++ usr/src/sys/sys/resourcevar.h
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 1991 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.redist.c%
+ *
+ *	@(#)resourcevar.h	7.1 (Berkeley) %G%
+ */
+
+#ifndef	_RESOURCEVAR_H_		/* tmp for user.h */
+#define	_RESOURCEVAR_H_
+
+/*
+ * Kernel per-process accounting / statistics
+ * (not necessarily resident except when running).
+ */
+struct pstats {
+#define	pstat_startzero	p_ru
+	struct	rusage p_ru;		/* stats for this proc */
+	struct	rusage p_cru;		/* sum of stats for reaped children */
+#define	pstat_endzero	pstat_startcopy
+
+#define	pstat_startcopy	p_timer
+	struct	itimerval p_timer[3];	/* virtual-time timers */
+
+	struct uprof {			/* profile arguments */
+		short	*pr_base;	/* buffer base */
+		unsigned pr_size;	/* buffer size */
+		unsigned pr_off;	/* pc offset */
+		unsigned pr_scale;	/* pc scaling */
+	} p_prof;
+#define	pstat_endcopy	p_start
+	struct	timeval p_start;	/* starting time */
+};
+
+/*
+ * Kernel shareable process resource limits.  Because this structure
+ * is moderately large but changes infrequently, it is normally
+ * shared copy-on-write after forks.  If a group of processes
+ * ("threads") share modifications, the PL_SHAREMOD flag is set,
+ * and a copy must be made for the child of a new fork that isn't
+ * sharing modifications to the limits.
+ */
+struct plimit {
+	struct	rlimit pl_rlimit[RLIM_NLIMITS];
+	int	p_lflags;		/* below */
+	int	p_refcnt;		/* number of references */
+};
+
+/* pl_lflags: */
+#define	PL_SHAREMOD	0x01		/* modifications are shared */
+
+/* make copy of plimit structure */
+struct	plimit *limcopy __P((struct plimit *lim));
+#endif	/* !_RESOURCEVAR_H_ */
