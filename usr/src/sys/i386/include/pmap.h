3eaf0dcb30f3bbf9848ce45872dadfc29db9d3ae
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1991-05-10 18:27:29 -0800

    not derived from CMU
    SCCS-vsn: sys/i386/include/pmap.h 7.3

--- usr/src/sys/i386/include/pmap.h
+++ usr/src/sys/i386/include/pmap.h
@@ -3,40 +3,12 @@
  * All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
- * The Mach Operating System project at Carnegie-Mellon University,
  * the Systems Programming Group of the University of Utah Computer
  * Science Department and William Jolitz of UUNET Technologies Inc.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)pmap.h	7.2 (Berkeley) %G%
- */
-
-/*
- * Copyright (c) 1987 Carnegie-Mellon University.
- * All rights reserved.
- *
- * Authors: Avadis Tevanian, Jr., Michael Wayne Young
- * 
- * Permission to use, copy, modify and distribute this software and
- * its documentation is hereby granted, provided that both the copyright
- * notice and this permission notice appear in all copies of the
- * software, derivative works or modified versions, and any portions
- * thereof, and that both notices appear in supporting documentation.
- * 
- * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS" 
- * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND 
- * FOR ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.
- * 
- * Carnegie Mellon requests users of this software to return to
- *
- *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU
- *  School of Computer Science
- *  Carnegie Mellon University
- *  Pittsburgh PA 15213-3890
- *
- * any improvements or extensions that they make and grant Carnegie the
- * rights to redistribute these changes.
+ *	@(#)pmap.h	7.3 (Berkeley) %G%
  */
 
 /*
