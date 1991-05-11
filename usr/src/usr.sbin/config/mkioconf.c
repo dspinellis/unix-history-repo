ac553beeea5df6e7239c7780c924c9a2c6b18924
Mike Karels <karels@ucbvax.Berkeley.EDU>
1991-05-10 17:45:21 -0800

    pte has moved, isn't needed
    SCCS-vsn: usr.sbin/config/mkioconf.c 5.18

--- usr/src/usr.sbin/config/mkioconf.c
+++ usr/src/usr.sbin/config/mkioconf.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)mkioconf.c	5.17 (Berkeley) %G%";
+static char sccsid[] = "@(#)mkioconf.c	5.18 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <stdio.h>
@@ -449,7 +449,6 @@ hp300_ioconf()
 		perror(path("ioconf.c"));
 		exit(1);
 	}
-	fprintf(fp, "#include \"hp300/include/pte.h\"\n");
 	fprintf(fp, "#include \"sys/param.h\"\n");
 	fprintf(fp, "#include \"sys/buf.h\"\n");
 	fprintf(fp, "#include \"sys/map.h\"\n");
