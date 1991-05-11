99c891415351fefa7912df5d20c7ebea6214a7be
Eric Allman <eric@ucbvax.Berkeley.EDU>
1991-05-11 06:45:22 -0800

    fix read-only string problem in help command
    SCCS-vsn: usr.sbin/sendmail/src/srvrsmtp.c 5.31
    SCCS-vsn: usr.sbin/sendmail/src/version.c 5.67

--- usr/src/usr.sbin/sendmail/src/srvrsmtp.c
+++ usr/src/usr.sbin/sendmail/src/srvrsmtp.c
@@ -10,9 +10,9 @@
 
 #ifndef lint
 #ifdef SMTP
-static char sccsid[] = "@(#)srvrsmtp.c	5.30 (Berkeley) %G% (with SMTP)";
+static char sccsid[] = "@(#)srvrsmtp.c	5.31 (Berkeley) %G% (with SMTP)";
 #else
-static char sccsid[] = "@(#)srvrsmtp.c	5.30 (Berkeley) %G% (without SMTP)";
+static char sccsid[] = "@(#)srvrsmtp.c	5.31 (Berkeley) %G% (without SMTP)";
 #endif
 #endif /* not lint */
 
@@ -382,8 +382,6 @@ smtp()
 			break;
 
 		  case CMDHELP:		/* help -- give user info */
-			if (*p == '\0')
-				p = "SMTP";
 			help(p);
 			break;
 
@@ -523,8 +521,12 @@ help(topic)
 		return;
 	}
 
+	if (topic == NULL || *topic == '\0')
+		topic = "smtp";
+	else
+		makelower(topic);
+
 	len = strlen(topic);
-	makelower(topic);
 	noinfo = TRUE;
 
 	while (fgets(buf, sizeof buf, hf) != NULL)
