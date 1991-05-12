d650e4c809f632cc6ca8ca809fcc9a131e823c7c
CSRG <csrg@ucbvax.Berkeley.EDU>
1991-05-12 05:31:37 -0800

    BSD 4_3_Net_2 development Work on file usr/src/usr.bin/gdb/init.c
    Synthesized-from: CSRG/cd2/net.2

---
+++ usr/src/usr.bin/gdb/init.c
@@ -0,0 +1,24 @@
+void initialize_all_files () {
+_initialize_hp300bsd_dep();
+_initialize_pinsn();
+_initialize_blockframe ();
+_initialize_breakpoint ();
+_initialize_command ();
+_initialize_copying ();
+_initialize_core();
+_initialize_dbxread ();
+_initialize_expread ();
+_initialize_infcmd ();
+_initialize_inflow ();
+_initialize_infrun ();
+_initialize_printcmd ();
+_initialize_remote();
+_initialize_sl();
+_initialize_source ();
+_initialize_stack ();
+_initialize_symmisc ();
+_initialize_symtab ();
+_initialize_utils ();
+_initialize_valprint ();
+_initialize_values ();
+}
