06af8b6f265f5c32c2c6880885aa34b6b6a47786
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1991-05-16 04:02:44 -0800

    need to set reference count on copied credential
    SCCS-vsn: sys/kern/kern_fork.c 7.29

--- usr/src/sys/kern/kern_fork.c
+++ usr/src/sys/kern/kern_fork.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)kern_fork.c	7.28 (Berkeley) %G%
+ *	@(#)kern_fork.c	7.29 (Berkeley) %G%
  */
 
 #include "param.h"
@@ -159,6 +159,7 @@ again:
 	MALLOC(p2->p_cred, struct pcred *, sizeof(struct pcred),
 	    M_SUBPROC, M_WAITOK);
 	bcopy(p1->p_cred, p2->p_cred, sizeof(*p2->p_cred));
+	p2->p_cred->p_refcnt = 1;
 	crhold(p1->p_ucred);
 
 	p2->p_fd = fdcopy(p1);
