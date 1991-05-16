e85957c2ea3b3ba2cc71f131b2d4cdc17616a262
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1991-05-16 04:00:49 -0800

    use a name that will fit
    SCCS-vsn: sys/sys/malloc.h 7.25

--- usr/src/sys/sys/malloc.h
+++ usr/src/sys/sys/malloc.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)malloc.h	7.24 (Berkeley) %G%
+ *	@(#)malloc.h	7.25 (Berkeley) %G%
  */
 
 #ifndef _MALLOC_H_
@@ -107,7 +107,7 @@
 	"VM pager",	/* 36 M_VMPAGER */ \
 	"VM pgdata",	/* 37 M_VMPGDATA */ \
 	"file",		/* 38 M_FILE */ \
-	"file descrip",	/* 39 M_FILEDESC */ \
+	"file desc",	/* 39 M_FILEDESC */ \
 	"lockf",	/* 40 M_LOCKF */ \
 	"proc",		/* 41 M_PROC */ \
 	"subproc",	/* 42 M_PROC */ \
