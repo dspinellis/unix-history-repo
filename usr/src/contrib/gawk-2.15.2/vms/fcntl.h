e3262f2912df193a0c20a4e821eba38e430e9cb1
CSRG <csrg@ucbvax.Berkeley.EDU>
1991-05-16 18:16:22 -0800

    BSD 4_4_Lite2 development Work on file usr/src/contrib/gawk-2.15.2/vms/fcntl.h Work on file usr/src/contrib/gawk-2.15.2/vms/varargs.h
    Synthesized-from: CSRG/cd3/4.4BSD-Lite2

---
+++ usr/src/contrib/gawk-2.15.2/vms/fcntl.h
@@ -0,0 +1,10 @@
+/* "fcntl.h" -- constants for BSD-style I/O routines (ala VAX C's <file.h>) */
+#define O_RDONLY	0
+#define O_WRONLY	1
+#define O_RDWR		2
+#define O_NDELAY	4
+#define O_NOWAIT	4
+#define O_APPEND	8
+#define O_CREAT		0x0200
+#define O_TRUNC		0x0400
+#define O_EXCL		0x0800
