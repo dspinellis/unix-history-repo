9ebd5df637cabb8017e568f5a918025857e10181
Steve Sellgren <sellgren@ucbvax.Berkeley.EDU>
1991-05-18 00:16:00 -0800

    date and time created 91/05/17 17:16:00 by sellgren
    SCCS-vsn: admin/admin/4.3Tahoe/Add2.tahoe.86.for 1.1
    SCCS-vsn: admin/admin/4.3Tahoe/Add2.tahoe.86.gov 1.1

---
+++ usr/src/admin/admin/4.3Tahoe/Add2.tahoe.86.for
@@ -0,0 +1,46 @@
+.ND
+.ce 6
+ADDENDUM NUMBER TWO  
+.sp 1
+to
+.sp 1
+FOURTH BERKELEY SOFTWARE DISTRIBUTION
+.sp 2
+LICENSE (4/86) AGREEMENT BETWEEN
+.sp 2
+THE REGENTS OF THE UNIVERSITY OF CALIFORNIA AND FOREIGN LICENSEES
+.sp 2
+.IP 1.  
+The above-referenced License Agreement and Addendum
+Number One ("Agreement") entered into between The  
+Regent of the University of California ("Regents") and the below-
+named Licensee controls Licensee's use of release 4.3 of Regents'
+Fourth Berkeley Software Distribution ("4BSD"), which was distributed
+upon execution of the Agreement. The Tahoe version of 4.3BSD is hereby
+made available to Licensee under the same terms and conditions 
+set forth in the Agreement. 
+.sp 2
+.PP
+IN WITNESS WHEREOF, Regents and Licensee have executed this Addendum
+Number Two on the dates specified below. The effective date of this
+amendment shall be the date of execution by Regents.
+.sp 2
+.TS
+expand tab(~);
+cw(2.9375i) cw(0.75i) cw(2.9375i).
+THE REGENTS OF THE UNIVERSITY~~
+\v'-10p'\l'2.9375i'~~\l'2.9375i'
+\v'-7p'OF CALIFORNIA~~
+\v'-17p'\l'2.9375i'~~\l'2.9375i'
+\v'-15p'(LICENSOR)~~(LICENSEE)
+.T&
+lw(2.9375i) cw(0.75i) lw(2.9375i).
+~
+~
+~
+By\l'2.75i'~~By\l'2.75i'
+~
+Title\l'2.625i'~~Title\l'2.625i'
+~
+Date\l'2.625i'~~Date\l'2.625i'
+.TE
