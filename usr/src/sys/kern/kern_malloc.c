eb363597a2fede967fff3c48a1325a6b232e641b
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1991-05-19 21:37:12 -0800

    name resolution checking (need kern/kern_malloc.c 7.25.1.1, kern/vfs_lookup.c 7.31.1.1, kern/vfs_syscalls.c 7.70.1.1, nfs/nfs_serv.c 7.40.1.1, nfs/nfs_syscalls.c 7.26.1.1, ufs/ufs_vnops.c 7.64.1.1, and hp300/hp300/trap.c 7.14.1.1
    SCCS-vsn: sys/kern/kern_malloc.c 7.25.1.1
    SCCS-vsn: sys/kern/vfs_lookup.c 7.31.1.1

--- usr/src/sys/kern/kern_malloc.c
+++ usr/src/sys/kern/kern_malloc.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)kern_malloc.c	7.25 (Berkeley) %G%
+ *	@(#)kern_malloc.c	7.25.1.1 (Berkeley) %G%
  */
 
 #include "param.h"
@@ -37,8 +37,12 @@ malloc(size, type, flags)
 #ifdef KMEMSTATS
 	register struct kmemstats *ksp = &kmemstats[type];
 
+#ifdef DIAGNOSTIC
 	if (((unsigned long)type) > M_LAST)
 		panic("malloc - bogus type");
+	if (type == M_NAMEI)
+		curproc->p_spare[0]++;
+#endif
 #endif
 
 	indx = BUCKETINDX(size);
@@ -148,6 +152,8 @@ free(addr, type)
 	kup = btokup(addr);
 	size = 1 << kup->ku_indx;
 #ifdef DIAGNOSTIC
+	if (type == M_NAMEI)
+		curproc->p_spare[0]--;
 	if (size > NBPG * CLSIZE)
 		alloc = addrmask[BUCKETINDX(NBPG * CLSIZE)];
 	else
