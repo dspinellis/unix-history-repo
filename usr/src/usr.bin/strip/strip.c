74577aa04bf82cb92e855e4bd770fc1db0879687
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-05-26 18:07:13 -0800

    add -d option to strip debugging and empty symbols
    SCCS-vsn: usr.bin/strip/strip.c 5.7
    SCCS-vsn: usr.bin/strip/strip.1 6.6

--- usr/src/usr.bin/strip/strip.c
+++ usr/src/usr.bin/strip/strip.c
@@ -12,10 +12,11 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)strip.c	5.6 (Berkeley) %G%";
+static char sccsid[] = "@(#)strip.c	5.7 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <fcntl.h>
 #include <errno.h>
 #include <a.out.h>
@@ -24,24 +25,31 @@ static char sccsid[] = "@(#)strip.c	5.6 (Berkeley) %G%";
 #include <stdlib.h>
 #include <string.h>
 
-static void err __P((const char *fmt, ...));
-
 typedef struct exec EXEC;
+typedef struct nlist NLIST;
+
+void err __P((const char *fmt, ...));
+void s_stab __P((const char *, int, EXEC *));
+void s_sym __P((const char *, int, EXEC *));
+void usage __P((void));
+
+int eval;
 
 main(argc, argv)
 	int argc;
 	char *argv[];
 {
-	register off_t fsize;
-	register int fd, n, pagesize;
+	register int fd, nb;
 	EXEC head;
-	int ch, dstabs;
+	void (*sfcn)__P((const char *, int, EXEC *));
+	int ch;
+	char *fn;
 
-	dstabs = 0;
+	sfcn = s_sym;
 	while ((ch = getopt(argc, argv, "d")) != EOF)
 		switch(ch) {
 		case 'd':
-			dstabs = 1;
+			sfcn = s_stab;
 			break;
 		case '?':
 		default:
@@ -50,38 +58,174 @@ main(argc, argv)
 	argc -= optind;
 	argv += optind;
 
-	pagesize = getpagesize();
-
-	for (; *argv; ++argv) {
-		if ((fd = open(*argv, O_RDWR)) < 0 ||
-		    (n = read(fd, &head, sizeof(EXEC))) == -1) {
-			err("%s: %s", *argv, strerror(errno));
+	while (fn = *argv++) {
+		if ((fd = open(fn, O_RDWR)) < 0 ||
+		    (nb = read(fd, &head, sizeof(EXEC))) == -1) {
+			err("%s: %s", fn, strerror(errno));
 			continue;
 		}
-		if (n != sizeof(EXEC) || N_BADMAG(head)) {
-			err("%s: %s", *argv, strerror(EFTYPE));
+		if (nb != sizeof(EXEC) || N_BADMAG(head)) {
+			err("%s: %s", fn, strerror(EFTYPE));
 			continue;
 		}
-		if (!head.a_syms && !head.a_trsize && !head.a_drsize) {
-			(void)close(fd);
-			continue;
-		}
-		fsize = head.a_text + head.a_data;
-		if (head.a_magic == ZMAGIC)
-			fsize += pagesize - sizeof(EXEC);
-		head.a_syms = head.a_trsize = head.a_drsize = 0;
-
-		if (ftruncate(fd, fsize + sizeof(EXEC)) ||
-		    lseek(fd, 0L, SEEK_SET) == -1 ||
-		    write(fd, &head, sizeof(EXEC)) != sizeof(EXEC))
-			err("%s: %s", *argv, strerror(errno)); 
-		(void)close(fd);
+		sfcn(fn, fd, &head);
+		if (close(fd))
+			err("%s: %s", fn, strerror(errno));
 	}
-	exit(0);
+	exit(eval);
 }
 
+void
+s_sym(fn, fd, ep)
+	const char *fn;
+	int fd;
+	register EXEC *ep;
+{
+	static int pagesize = -1;
+	register off_t fsize;
+
+	/* If no symbols or data/text relocation info, quit. */
+	if (!ep->a_syms && !ep->a_trsize && !ep->a_drsize)
+		return;
+
+	/*
+	 * New file size is the header plus text and data segments; OMAGIC
+	 * and NMAGIC formats have the text/data immediately following the
+	 * header.  ZMAGIC format wastes the rest of of header page.
+	 */
+	if (ep->a_magic == ZMAGIC)
+		fsize = pagesize == -1 ? (pagesize = getpagesize()) : pagesize;
+	else
+		fsize = sizeof(EXEC);
+	fsize += ep->a_text + ep->a_data;
+
+	/* Set symbol size and relocation info values to 0. */
+	ep->a_syms = ep->a_trsize = ep->a_drsize = 0;
+
+	/* Rewrite the header and truncate the file. */
+	if (lseek(fd, 0L, SEEK_SET) == -1 ||
+	    write(fd, ep, sizeof(EXEC)) != sizeof(EXEC) ||
+	    ftruncate(fd, fsize))
+		err("%s: %s", fn, strerror(errno)); 
+}
+
+void
+s_stab(fn, fd, ep)
+	const char *fn;
+	int fd;
+	EXEC *ep;
+{
+	struct stat sb;
+	register NLIST *bsym2, *sym1, *sym2;
+	register u_long nsym1, nsym2;
+	register char *p, *bstr2, *str1, *str2;
+	register int len, symlen;
+	off_t fsize;
+	int nb;
+	char *bp;
+
+	/* Quit if no symbols. */
+	if (ep->a_syms == 0)
+		return;
+
+	bsym2 = NULL;
+	bp = bstr2 = NULL;
+
+	/* Read the file into memory.			XXX mmap */
+	if (fstat(fd, &sb))
+		goto syserr;
+	if ((bp = malloc(sb.st_size)) == NULL)
+		goto syserr;
+	if (lseek(fd, 0L, SEEK_SET) == -1)
+		goto syserr;
+	if ((nb = read(fd, bp, (int)sb.st_size)) == -1)
+		goto syserr;
+	if (nb != sb.st_size) {
+		errno = EIO;
+		goto syserr;
+	}
+
+	/*
+	 * Allocate space for new symbol and string tables.  Allocate before
+	 * reading the symbol tables so we can do it all in a single pass.
+	 * This loses if there weren't any symbols to strip, but that's life.
+	 */
+	sym1 = (NLIST *)(bp + N_SYMOFF(*ep));
+	if ((bsym2 = sym2 = malloc((u_int)ep->a_syms)) == NULL) {
+		err("%s", strerror(errno));
+		goto mem;
+	}
+	str1 = bp + N_STROFF(*ep);
+	if ((bstr2 = malloc((u_int)*(u_long *)str1)) == NULL) {
+		err("%s", strerror(errno));
+		goto mem;
+	}
+	str2 = bstr2 + sizeof(u_long);
+	symlen = sizeof(u_long);
+
+	/*
+	 * Read through the symbol table.  For each non-debugging symbol,
+	 * copy it into the new symbol and string tables.  Keep track of
+	 * how many symbols are copied and the length of the new string
+	 * table.
+	 */
+#define	strx	n_un.n_strx
+	nsym2 = 0;
+	for (nsym1 = ep->a_syms / sizeof(NLIST); nsym1--; ++sym1)
+		if (!(sym1->n_type & N_STAB) && sym1->strx) {
+			*sym2 = *sym1;
+			sym2->strx = str2 - bstr2;
+			p = str1 + sym1->strx;
+			len = strlen(p) + 1;
+			bcopy(p, str2, len);
+			symlen += len;
+			str2 += len;
+			++sym2;
+			++nsym2;
+		}
+
+	/* If no debugging symbols, quit. */
+	if (!nsym2)
+		goto mem;
+
+	/* Fill in new symbol table size. */
+	ep->a_syms = nsym2 * sizeof(NLIST);
+
+	/* Write out the header. */
+	if (lseek(fd, 0L, SEEK_SET) == -1 ||
+	    write(fd, ep, sizeof(EXEC)) != sizeof(EXEC))
+		goto syserr;
+
+	/* Write out the symbol table. */
+	if (lseek(fd, N_SYMOFF(*ep), SEEK_SET) == -1 ||
+	    write(fd, bsym2, ep->a_syms) != ep->a_syms)
+		goto syserr;
+
+	/* Fill in the new size of the string table. */
+	*(u_long *)bstr2 = symlen;
+
+	/* Write out the string table. */
+	if (write(fd, bstr2, symlen) != symlen)
+		goto syserr;
+
+	/* Truncate to the current length. */
+	if ((fsize = lseek(fd, 0L, SEEK_CUR)) == -1)
+		goto syserr;
+	if (ftruncate(fd, fsize))
+syserr:		err("%s: %s", fn, strerror(errno));
+
+mem:	if (bp)
+		free(bp);
+	if (bstr2)
+		free(bstr2);
+	if (bsym2)
+		free(bsym2);
+}
+
+void
 usage()
 {
+
 	(void)fprintf(stderr, "usage: strip [-d] file ...\n");
 	exit(1);
 }
@@ -111,4 +255,5 @@ err(fmt, va_alist)
 	(void)vfprintf(stderr, fmt, ap);
 	va_end(ap);
 	(void)fprintf(stderr, "\n");
+	eval = 1;
 }
