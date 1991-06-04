3bcc0b08e189ec966f7f5ab4a2b467de98725bf9
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-06-04 00:19:00 -0800

    vtail doesn't need to be global
    SCCS-vsn: bin/ps/extern.h 5.2

--- usr/src/bin/ps/extern.h
+++ usr/src/bin/ps/extern.h
@@ -4,13 +4,13 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)extern.h	5.1 (Berkeley) %G%
+ *	@(#)extern.h	5.2 (Berkeley) %G%
  */
 
 #include <sys/cdefs.h>
 
 extern VAR var[];
-extern struct varent *vhead, *vtail;
+extern struct varent *vhead;
 
 __BEGIN_DECLS
 void	 err __P((const char *, ...));
