b4c67accb8d6fdde6caa19b044a6fbcaf3e200dd
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-06-05 15:04:21 -0800

    break out rest of keywords into function table that can be bsearch'd
    SCCS-vsn: bin/stty/Makefile 5.4
    SCCS-vsn: bin/stty/extern.h 5.2
    SCCS-vsn: bin/stty/stty.c 5.27
    SCCS-vsn: bin/stty/stty.h 5.2

--- usr/src/bin/stty/extern.h
+++ usr/src/bin/stty/extern.h
@@ -4,13 +4,22 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)extern.h	5.1 (Berkeley) %G%
+ *	@(#)extern.h	5.2 (Berkeley) %G%
  */
 
-void checkredirect __P((void));
-void err __P((const char *, ...));
-void gprint __P((struct termios *, struct winsize *, int));
-void gread __P((struct termios *, char *));
-void optlist __P((void));
-void print __P((struct termios *, struct winsize *, int, enum FMT));
-void warn __P((const char *, ...));
+__BEGIN_DECLS
+int	c_cchars __P((const void *, const void *));
+int	c_modes __P((const void *, const void *));
+void	checkredirect __P((void));
+void	err __P((const char *, ...));
+struct key *
+	ksearch __P((char *));
+void	gprint __P((struct termios *, struct winsize *, int));
+void	gread __P((struct termios *, char *));
+void	optlist __P((void));
+void	print __P((struct termios *, struct winsize *, int, enum FMT));
+void	warn __P((const char *, ...));
+__END_DECLS
+
+extern struct cchar cchars1[], cchars2[];
+extern struct modes cmodes[], imodes[], lmodes[], omodes[];
--- usr/src/bin/stty/stty.h
+++ usr/src/bin/stty/stty.h
@@ -4,8 +4,30 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)stty.h	5.1 (Berkeley) %G%
+ *	@(#)stty.h	5.2 (Berkeley) %G%
  */
+
+#include <sys/ioctl.h>
+#include <termios.h>
+
+struct info {
+	int fd;					/* file descriptor */
+	int ldisc;				/* line discipline */
+	int off;				/* turn off */
+	int set;				/* need set */
+	int wset;				/* need window set */
+	char *arg;				/* argument */
+	struct termios t;			/* terminal info */
+	struct winsize win;			/* window info */
+};
+
+struct key {
+	char *name;				/* name */
+	void (*f) __P((struct info *));		/* function */
+#define	F_NEEDARG	0x01			/* needs an argument */
+#define	F_OFFOK		0x02			/* can turn off */
+	int flags;
+};
 
 struct modes {
 	char *name;
