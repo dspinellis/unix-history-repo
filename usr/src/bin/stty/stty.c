a41b9fb80aab22ee295d2a5577114f407644a8da
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-06-05 15:04:21 -0800

    break out rest of keywords into function table that can be bsearch'd
    SCCS-vsn: bin/stty/Makefile 5.4
    SCCS-vsn: bin/stty/extern.h 5.2
    SCCS-vsn: bin/stty/stty.c 5.27
    SCCS-vsn: bin/stty/stty.h 5.2

--- usr/src/bin/stty/stty.c
+++ usr/src/bin/stty/stty.c
@@ -12,12 +12,10 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)stty.c	5.26 (Berkeley) %G%";
+static char sccsid[] = "@(#)stty.c	5.27 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/types.h>
-#include <sys/ioctl.h>
-#include <termios.h>
 #include <fcntl.h>
 #include <errno.h>
 #include <unistd.h>
@@ -28,25 +26,22 @@ static char sccsid[] = "@(#)stty.c	5.26 (Berkeley) %G%";
 #include "stty.h"
 #include "extern.h"
 
-static void usage __P((void));
+static char usage[] = "usage: stty: [-eg] [-f file] [options]";
 
 main(argc, argv) 
 	int argc;
 	char **argv;
 {
-	extern char *optarg;
-	extern int opterr, optind;
-	extern struct cchar cchars1[], cchars2[];
-	extern struct modes cmodes[], imodes[], lmodes[], omodes[];
 	register struct modes *mp;
 	register struct cchar *cp;
-	struct winsize win;
-	struct termios t;
+	struct info i;
+	struct key *kp;
 	enum FMT fmt;
-	int ch, ctl, ldisc, set, tmp, wset;
+	int ch;
 
-	ctl = STDIN_FILENO;
 	fmt = NOTSET;
+	i.fd = STDIN_FILENO;
+
 	opterr = 0;
 	while (strspn(argv[optind], "-aefg") == strlen(argv[optind]) &&
 	    (ch = getopt(argc, argv, "aef:g")) != EOF)
@@ -58,7 +53,7 @@ main(argc, argv)
 			fmt = BSD;
 			break;
 		case 'f':
-			if ((ctl = open(optarg, O_RDONLY | O_NONBLOCK)) < 0)
+			if ((i.fd = open(optarg, O_RDONLY | O_NONBLOCK)) < 0)
 				err("%s: %s", optarg, strerror(errno));
 			break;
 		case 'g':
@@ -72,11 +67,11 @@ main(argc, argv)
 args:	argc -= optind;
 	argv += optind;
 
-	if (ioctl(ctl, TIOCGETD, &ldisc) < 0)
+	if (ioctl(i.fd, TIOCGETD, &i.ldisc) < 0)
 		err("TIOCGETD: %s", strerror(errno));
-	if (tcgetattr(ctl, &t) < 0)
+	if (tcgetattr(i.fd, &i.t) < 0)
 		err("tcgetattr: %s", strerror(errno));
-	if (ioctl(ctl, TIOCGWINSZ, &win) < 0)
+	if (ioctl(i.fd, TIOCGWINSZ, &i.win) < 0)
 		warn("TIOCGWINSZ: %s\n", strerror(errno));
 
 	checkredirect();			/* conversion aid */
@@ -88,216 +83,95 @@ args:	argc -= optind;
 		/* FALLTHROUGH */
 	case BSD:
 	case POSIX:
-		print(&t, &win, ldisc, fmt);
+		print(&i.t, &i.win, i.ldisc, fmt);
 		break;
 	case GFLAG:
-		gprint(&t, &win, ldisc);
+		gprint(&i.t, &i.win, i.ldisc);
 		break;
 	}
 	
-#define	CHK(s)	(**argv == s[0] && !strcmp(*argv, s))
-
-	for (set = wset = 0; *argv; ++argv) {
-		if (CHK("-nl")) {
-			t.c_iflag |= ICRNL;
-			t.c_oflag |= ONLCR;
-			set = 1;
-			continue;
-		}
-		if (CHK("all")) {
-			print(&t, &win, ldisc, BSD);
-			continue;
-		}
-		if (CHK("-cbreak"))
-			goto reset;
-		if (CHK("cbreak")) {
-			t.c_iflag | BRKINT|IXON|IMAXBEL;
-			t.c_oflag |= OPOST;
-			t.c_lflag |= ISIG|IEXTEN;
-			t.c_lflag &= ~ICANON;
-			set = 1;
-			continue;
-		}
-		if (CHK("cols")) {
-			if (!*++argv)
-				err("option requires an argument -- cols");
-			goto columns;
-		}
-		if (CHK("columns")) {
-			if (!*++argv)
-				err("option requires an argument -- columns");
-columns:		win.ws_col = atoi(*argv);
-			wset = 1;
-			continue;
-		}
-		if (CHK("cooked"))
-			goto reset;
-		if (CHK("dec")) {
-			t.c_cc[VERASE] = (u_char)0177;
-			t.c_cc[VKILL] = CTRL('u');
-			t.c_cc[VINTR] = CTRL('c');
-			t.c_lflag &= ~ECHOPRT;
-			t.c_lflag |= ECHOE|ECHOKE|ECHOCTL;
-			t.c_iflag &= ~IXANY;
-			set = 1;
-			continue;
-		}
-		if (CHK("everything")) {
-			print(&t, &win, ldisc, BSD);
-			continue;
-		}
-		if (CHK("-extproc")) {
-			tmp = 0;
-			ioctl(ctl, TIOCEXT, &tmp);
-			continue;
-		}
-		if (CHK("extproc")) {
-			tmp = 1;
-			ioctl(ctl, TIOCEXT, &tmp);
-			continue;
-		}
-		if (CHK("ispeed")) {
-			if (!*++argv)
-				err("option requires an argument -- ispeed");
-			cfsetispeed(&t, atoi(*argv));
-			set = 1;
-			continue;
-		}
-		if (CHK("new"))
-			goto tty;
-		if (CHK("nl")) {
-			t.c_iflag &= ~ICRNL;
-			t.c_oflag &= ~ONLCR;
-			set = 1;
-			continue;
-		}
-		if (CHK("old"))
-			goto tty;
-		if (CHK("ospeed")) {
-			if (!*++argv)
-				err("option requires an argument -- ospeed");
-			cfsetospeed(&t, atoi(*argv));
-			set = 1;
-			continue;
-		}
-		if (CHK("-raw"))
-			goto reset;
-		if (CHK("raw")) {
-			cfmakeraw(&t);
-			t.c_cflag &= ~(CSIZE|PARENB);
-			t.c_cflag |= CS8;
-			set = 1;
-			continue;
-		}
-		if (CHK("rows")) {
-			if (!*++argv)
-				err("option requires an argument -- rows");
-			win.ws_row = atoi(*argv);
-			wset = 1;
-			continue;
-		}
-		if (CHK("sane")) {
-reset:			t.c_cflag = TTYDEF_CFLAG | (t.c_cflag & CLOCAL);
-			t.c_iflag = TTYDEF_IFLAG;
-			t.c_iflag |= ICRNL;
-			/* preserve user-preference flags in lflag */
-#define	LKEEP	(ECHOKE|ECHOE|ECHOK|ECHOPRT|ECHOCTL|ALTWERASE|TOSTOP|NOFLSH)
-			t.c_lflag = TTYDEF_LFLAG | (t.c_lflag & LKEEP);
-			t.c_oflag = TTYDEF_OFLAG;
-			set = 1;
-			continue;
-		}
-		if (CHK("size")) {
-			(void)printf("%d %d\n", win.ws_row, win.ws_col);
-			continue;
-		}
-		if (CHK("speed")) {
-			(void)printf("%d\n", cfgetospeed(&t));
-			continue;
-		}
-		if (CHK("tty")) {
-tty:			tmp = TTYDISC;
-			if (ioctl(0, TIOCSETD, &tmp) < 0)
-				err("TIOCSETD: %s", strerror(errno));
+	for (i.set = i.wset = 0; *argv; ++argv) {
+		i.off = **argv == '-';
+		if (kp = ksearch(i.off ? *argv + 1 : *argv)) {
+			if (!(kp->flags & F_OFFOK) && i.off)
+				err("illegal option -- %s\n%s", *argv, usage);
+			if (kp->flags & F_NEEDARG && !(i.arg = *++argv))
+				err("option requires an argument -- %s\n%s",
+				    kp->name, usage);
+			kp->f(&i);
 			continue;
 		}
 		
+
+#define	CHK(s)	(**argv == s[0] && !strcmp(*argv, s))
 		for (mp = cmodes; mp->name; ++mp)
 			if (CHK(mp->name)) {
-				t.c_cflag &= ~mp->unset;
-				t.c_cflag |= mp->set;
-				set = 1;
+				i.t.c_cflag &= ~mp->unset;
+				i.t.c_cflag |= mp->set;
+				i.set = 1;
 				goto next;
 			}
 		for (mp = imodes; mp->name; ++mp)
 			if (CHK(mp->name)) {
-				t.c_iflag &= ~mp->unset;
-				t.c_iflag |= mp->set;
-				set = 1;
+				i.t.c_iflag &= ~mp->unset;
+				i.t.c_iflag |= mp->set;
+				i.set = 1;
 				goto next;
 			}
 		for (mp = lmodes; mp->name; ++mp)
 			if (CHK(mp->name)) {
-				t.c_lflag &= ~mp->unset;
-				t.c_lflag |= mp->set;
-				set = 1;
+				i.t.c_lflag &= ~mp->unset;
+				i.t.c_lflag |= mp->set;
+				i.set = 1;
 				goto next;
 			}
 		for (mp = omodes; mp->name; ++mp)
 			if (CHK(mp->name)) {
-				t.c_oflag &= ~mp->unset;
-				t.c_oflag |= mp->set;
-				set = 1;
+				i.t.c_oflag &= ~mp->unset;
+				i.t.c_oflag |= mp->set;
+				i.set = 1;
 				goto next;
 			}
-		for (cp = cchars1; cp->name; ++cp) {
-			if (!CHK(cp->name))
-				continue;
-			goto ccfound;
-		}
+
+		for (cp = cchars1; cp->name; ++cp)
+			if (CHK(cp->name))
+				goto ccfound;
 		for (cp = cchars2; cp->name; ++cp) {
 			if (!CHK(cp->name))
 				continue;
 ccfound:		if (!*++argv)
-				err("option requires an argument -- %s",
-				    cp->name);
+				err("option requires an argument -- %s\n%s",
+				    cp->name, usage);
 			if (CHK("undef") || CHK("<undef>"))
-				t.c_cc[cp->sub] = _POSIX_VDISABLE;
+				i.t.c_cc[cp->sub] = _POSIX_VDISABLE;
 			else if (**argv == '^')
-				t.c_cc[cp->sub] = 
+				i.t.c_cc[cp->sub] = 
 				    ((*argv)[1] == '?') ? 0177 :
 				    ((*argv)[1] == '-') ? _POSIX_VDISABLE :
 				    (*argv)[1] & 037;
 			else
-				t.c_cc[cp->sub] = **argv;
-			set = 1;
-			goto next;
+				i.t.c_cc[cp->sub] = **argv;
+			i.set = 1;
+			continue;
 		}
 
 		if (isdigit(**argv)) {
-			cfsetospeed(&t, atoi(*argv));
-			cfsetispeed(&t, atoi(*argv));
-			goto next;
+			cfsetospeed(&i.t, atoi(*argv));
+			cfsetispeed(&i.t, atoi(*argv));
+			continue;
 		}
 		if (!strncmp(*argv, "gfmt1", sizeof("gfmt1") - 1)) {
-			gread(&t, *argv + sizeof("gfmt1") - 1);
-			goto next;
+			gread(&i.t, *argv + sizeof("gfmt1") - 1);
+			continue;
 		}
 
-		err("illegal option -- %s", *argv);
+		err("illegal option -- %s\n%s", *argv, usage);
 next:		continue;
 	}
 
-	if (set && tcsetattr(ctl, 0, &t) < 0)
+	if (i.set && tcsetattr(i.fd, 0, &i.t) < 0)
 		err("tcsetattr: %s", strerror(errno));
-	if (wset && ioctl(ctl, TIOCSWINSZ, &win) < 0)
+	if (i.wset && ioctl(i.fd, TIOCSWINSZ, &i.win) < 0)
 		warn("TIOCSWINSZ: %s", strerror(errno));
 	exit(0);
-}
-
-static void
-usage()
-{
-	(void)fprintf(stderr, "usage: stty: [-eg] [-f file] [options]\n");
-	exit(1);
 }
