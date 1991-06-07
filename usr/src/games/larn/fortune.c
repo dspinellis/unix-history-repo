0678afdfa97bd338d4d323dbcecb0e835b3afe43
Steve Sellgren <sellgren@ucbvax.Berkeley.EDU>
1991-06-06 23:06:22 -0800

    Bostic's cleanup
    SCCS-vsn: games/larn/fortune.c 5.4

--- usr/src/games/larn/fortune.c
+++ usr/src/games/larn/fortune.c
@@ -1,63 +1,90 @@
+/*-
+ * Copyright (c) 1991 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.redist.c%
+ */
+
+#ifndef lint
+static char sccsid[] = "@(#)fortune.c	5.4 (Berkeley) %G%";
+#endif /* not lint */
+
 /* fortune.c		 Larn is copyrighted 1986 by Noah Morgan. */
+
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-
+#include <unistd.h>
+#include <stdlib.h>
 #include "header.h"
+#include "pathnames.h"
+
 /*
- *	function to return a random fortune from the fortune file
+ * function to return a random fortune from the fortune file
  */
-static char *base=0;	/* pointer to the fortune text */
-static char **flines=0;	/* array of pointers to each fortune */
-static int fd=0;		/* true if we have load the fortune info */
-static int nlines=0;	/* # lines in fortune database */
 
-char *fortune(file)
-	char *file;
-	{
+char *
+fortune()
+{
+	static int fd = -1;	/* true if we have load the fortune info */
+	static int nlines;	/* # lines in fortune database */
+	register int tmp;
 	register char *p;
-	register int lines,tmp;
-	struct stat stat;
-	char *malloc();
-	if (fd==0)
-		{
-		if ((fd=open(file,O_RDONLY)) < 0)	/* open the file */
-			return(0); /* can't find file */
+	char *base, **flines;
+	struct stat sb;
 
-	/* find out how big fortune file is and get memory for it */
-		stat.st_size = 16384;
-		if ((fstat(fd,&stat) < 0) || ((base=malloc(1+stat.st_size)) == 0))
-			{
-			close(fd); fd= -1; free((char*)base); return(0); 	/* can't stat file */
-			}
+	if (fd != -1)
+		return (flines[random() % nlines]);
 
-	/* read in the entire fortune file */
-		if (read(fd,base,stat.st_size) != stat.st_size)
-			{
-			close(fd); fd= -1; free((char*)base); return(0); 	/* can't read file */
-			}
-		close(fd);  base[stat.st_size]=0;	/* final NULL termination */
+	if ((fd = open(_PATH_FORTS, O_RDONLY)) < 0)
+		return (NULL);
 
-	/* count up all the lines (and NULL terminate) to know memory needs */
-		for (p=base,lines=0; p<base+stat.st_size; p++) /* count lines */
-			if (*p == '\n') *p=0,lines++;
-		nlines = lines;
+	/* Find out how big fortune file is and get memory for it. */
+	if ((fstat(fd, &sb) < 0) ||
+	    ((base = malloc(1 + sb.st_size)) == NULL)) {
+		(void)close(fd);
+		goto bad;
+	}
 
-	/* get memory for array of pointers to each fortune */
-		if ((flines=(char**)malloc(nlines*sizeof(char*))) == 0)
-			{
-			free((char*)base); fd= -1; return(0); /* malloc() failure */
-			}
+	/* Read in the entire fortune file. */
+	if (read(fd, base, sb.st_size) != sb.st_size) {
+		free(base);
+		goto bad;
+	}
+	base[sb.st_size] = '\0';	/* Final NULL termination. */
 
-	/* now assign each pointer to a line */
-		for (p=base,tmp=0; tmp<nlines; tmp++)
-			{
-			flines[tmp]=p;  while (*p++); /* advance to next line */
-			}
+	(void)close(fd);
+
+
+	/*
+	 * Count up all the lines (and NULL terminate) to know memory
+	 * needs.
+	 */
+	for (p = base; p < base + sb.st_size; p++)
+		if (*p == '\n') {
+			*p = '\0';
+			++nlines;
 		}
 
-	if (fd > 2)	/* if we have a database to look at */
-		return(flines[rund((nlines<=0)?1:nlines)]);
-	else 
-		return(0);
+	if (nlines <= 0) {
+		free(base);
+		goto bad;
 	}
+
+	/* Get memory for array of pointers to each fortune. */
+	if ((flines = malloc(nlines * sizeof(char *))) == NULL) {
+		free(base);
+		goto bad;
+	}
+
+	/* Now assign each pointer to a line. */
+	for (p = base, tmp = 0; tmp < nlines; ++tmp) {
+		flines[tmp] = p;
+		while (*p++);
+	}
+
+	return (flines[random() % nlines]);
+
+bad:	fd = -1;
+	return (NULL);
+}
