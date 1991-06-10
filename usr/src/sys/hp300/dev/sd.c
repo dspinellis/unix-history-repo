18725d49e37e7d48bdfa557ec64eee371e60b1e8
Mike Karels <karels@ucbvax.Berkeley.EDU>
1991-06-09 20:19:14 -0800

    make retry printf's conditional on debug
    SCCS-vsn: sys/hp300/dev/sd.c 7.7

--- usr/src/sys/hp300/dev/sd.c
+++ usr/src/sys/hp300/dev/sd.c
@@ -7,7 +7,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)sd.c	7.6 (Berkeley) %G%
+ *	@(#)sd.c	7.7 (Berkeley) %G%
  */
 
 /*
@@ -695,8 +695,11 @@ sdintr(unit, stat)
 #endif
 		retry = sderror(unit, sc, hp, stat);
 		if (retry && sdtab[unit].b_errcnt++ < SDRETRY) {
-			printf("sd%d: retry #%d\n",
-			       unit, sdtab[unit].b_errcnt);
+#ifdef DEBUG
+			if (sddebug & SDB_ERROR)
+				printf("sd%d: retry #%d\n",
+				       unit, sdtab[unit].b_errcnt);
+#endif
 			sdstart(unit);
 			return;
 		}
