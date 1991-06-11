e2c85d6812fbb17b3b4249425959b82b02adc3bc
Steve Sellgren <sellgren@ucbvax.Berkeley.EDU>
1991-06-10 20:39:14 -0800

    Why have this fortune file around?!
    SCCS-vsn: games/larn/fortune.c 5.5

--- usr/src/games/larn/fortune.c
+++ usr/src/games/larn/fortune.c
@@ -6,85 +6,56 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)fortune.c	5.4 (Berkeley) %G%";
+static char sccsid[] = "@(#)fortune.c	5.5 (Berkeley) %G%";
 #endif /* not lint */
 
 /* fortune.c		 Larn is copyrighted 1986 by Noah Morgan. */
-
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include "header.h"
-#include "pathnames.h"
 
 /*
  * function to return a random fortune from the fortune file
  */
 
+char *flines[] = {
+	"gem value = gem * 2 ^ perfection",
+	"sitting down can have unexpected results",
+	"don't pry into the affairs of others",
+	"drinking can be hazardous to your health",
+	"beware of the gusher!",
+	"some monsters are greedy",
+	"nymphs have light fingers",
+	"try kissing a disenchantress!",
+	"hammers and brains don't mix",
+	"what does a potion of cure dianthroritis taste like?",
+	"hit point gain/loss when raising a level depends on constitution",
+	"healing a mighty wizard can be exhilarating",
+	"be sure to pay your taxes",
+	"are Vampires afraid of something?",
+	"some dragons can fly",
+	"dos thou strive for perfection?",
+	"patience is a virtue, unless your daughter dies",
+	"what does the Eye of Larn see in its guardian?",
+	"a level 25 player casts like crazy!",
+	"energy rings affect spell regeneration",
+	"difficulty affects regeneration",
+	"control of the pesty spirits is most helpful",
+	"don't fall into a bottomless pit",
+	"dexterity allows you to carry more",
+	"you can get 2 points of WC for the price of one",
+	"never enter the dungeon naked!  the monsters will laugh at you!",
+	"did someone put itching powder in your armor?",
+	"you klutz!",
+	"avoid opening doors.  you never know whats on the other side.",
+	"infinite regeneration ---> temptation",
+	"the greatest weapon in the game has not the highest Weapon Class",
+	"you can't buy the most powerful scroll",
+	"identify things before you use them",
+	"there's more than one way through a wall"
+};
+
+#define NFORTUNES	34
+
 char *
 fortune()
 {
-	static int fd = -1;	/* true if we have load the fortune info */
-	static int nlines;	/* # lines in fortune database */
-	register int tmp;
-	register char *p;
-	char *base, **flines;
-	struct stat sb;
-
-	if (fd != -1)
-		return (flines[random() % nlines]);
-
-	if ((fd = open(_PATH_FORTS, O_RDONLY)) < 0)
-		return (NULL);
-
-	/* Find out how big fortune file is and get memory for it. */
-	if ((fstat(fd, &sb) < 0) ||
-	    ((base = malloc(1 + sb.st_size)) == NULL)) {
-		(void)close(fd);
-		goto bad;
-	}
-
-	/* Read in the entire fortune file. */
-	if (read(fd, base, sb.st_size) != sb.st_size) {
-		free(base);
-		goto bad;
-	}
-	base[sb.st_size] = '\0';	/* Final NULL termination. */
-
-	(void)close(fd);
-
-
-	/*
-	 * Count up all the lines (and NULL terminate) to know memory
-	 * needs.
-	 */
-	for (p = base; p < base + sb.st_size; p++)
-		if (*p == '\n') {
-			*p = '\0';
-			++nlines;
-		}
-
-	if (nlines <= 0) {
-		free(base);
-		goto bad;
-	}
-
-	/* Get memory for array of pointers to each fortune. */
-	if ((flines = malloc(nlines * sizeof(char *))) == NULL) {
-		free(base);
-		goto bad;
-	}
-
-	/* Now assign each pointer to a line. */
-	for (p = base, tmp = 0; tmp < nlines; ++tmp) {
-		flines[tmp] = p;
-		while (*p++);
-	}
-
-	return (flines[random() % nlines]);
-
-bad:	fd = -1;
-	return (NULL);
+	return (flines[random() % NFORTUNES]);
 }
