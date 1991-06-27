51a698f137c50692806327e1a7b0985cd655eb34
Keith Sklower <sklower@ucbvax.Berkeley.EDU>
1991-06-27 00:06:35 -0800

    play catchup with newvm changes
    SCCS-vsn: sys/netccitt/pk_usrreq.c 7.16
    SCCS-vsn: sys/netccitt/pk_acct.c 7.6

--- usr/src/sys/netccitt/pk_acct.c
+++ usr/src/sys/netccitt/pk_acct.c
@@ -9,19 +9,16 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)pk_acct.c	7.5 (Berkeley) %G%
+ *	@(#)pk_acct.c	7.6 (Berkeley) %G%
  */
 
 #include "param.h"
 #include "systm.h"
-#include "time.h"
+#include "namei.h"
 #include "proc.h"
-#include "user.h"
 #include "vnode.h"
 #include "kernel.h"
 #include "file.h"
-#include "acct.h"
-#include "uio.h"
 #include "socket.h"
 #include "socketvar.h"
 
@@ -42,35 +39,29 @@ pk_accton (path)
 	char *path;
 {
 	register struct vnode *vp = NULL;
-	register struct nameidata *ndp = &u.u_nd;
+	struct nameidata nd;
 	struct vnode *oacctp = pkacctp;
+	struct proc *p = curproc;
 	int error;
 
-	ndp -> ni_segflg = UIO_USERSPACE;
-	ndp -> ni_dirp = path;
-	if (error = vn_open (ndp, FREAD|FWRITE, 0))
+	if (path == 0)
+		goto close;
+	nd.ni_segflg = UIO_USERSPACE;
+	nd.ni_dirp = path;
+	if (error = vn_open (&nd, p, FWRITE, 0644))
 		return (error);
-	vp = ndp -> ni_vp;
+	vp = nd.ni_vp;
+	VOP_UNLOCK(vp);
 	if (vp -> v_type != VREG) {
 		vrele (vp);
 		return (EACCES);
 	}
 	pkacctp = vp;
-	if (oacctp)
-		vrele (oacctp);
-	return (0);
-}
-
-/* 
- *  Turn off packet accounting
- */
-
-pk_acctoff ()
-{
-	if (pkacctp) {
-		vrele (pkacctp);
-		pkacctp = 0;
+	if (oacctp) {
+	close:
+		error = vn_close (oacctp, FWRITE, p -> p_ucred, p);
 	}
+	return (error);
 }
 
 /* 
@@ -102,7 +93,7 @@ register struct pklcd *lcp;
 		acbuf.x25acct_revcharge = 1;
 	acbuf.x25acct_stime = lcp -> lcd_stime;
 	acbuf.x25acct_etime = time.tv_sec - acbuf.x25acct_stime;
-	acbuf.x25acct_uid = u.u_uid;
+	acbuf.x25acct_uid = curproc -> p_cred -> p_ruid;
 	acbuf.x25acct_psize = sa -> x25_opts.op_psize;
 	acbuf.x25acct_net = sa -> x25_net;
 	/*
@@ -123,6 +114,7 @@ register struct pklcd *lcp;
 	acbuf.x25acct_rxcnt = lcp -> lcd_rxcnt;
 
 	(void) vn_rdwr(UIO_WRITE, vp, (caddr_t)&acbuf, sizeof (acbuf),
-		(off_t)0, UIO_SYSSPACE, IO_UNIT|IO_APPEND, u.u_cred, (int *)0,
+		(off_t)0, UIO_SYSSPACE, IO_UNIT|IO_APPEND,
+		curproc -> p_ucred, (int *)0,
 		(struct proc *)0);
 }
