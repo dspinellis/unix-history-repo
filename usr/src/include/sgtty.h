005cd0aba4f3023faccad7567ecea15bee8309ac
Marc Teitelbaum <marc@ucbvax.Berkeley.EDU>
1991-06-26 18:40:35 -0800

    don't test for _IOCTL_H_
    SCCS-vsn: include/sgtty.h 5.2

--- usr/src/include/sgtty.h
+++ usr/src/include/sgtty.h
@@ -4,12 +4,10 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)sgtty.h	5.1 (Berkeley) %G%
+ *	@(#)sgtty.h	5.2 (Berkeley) %G%
  */
 
-#ifndef	_IOCTL_H_
 #ifndef USE_OLD_TTY
 #define	USE_OLD_TTY
 #endif
 #include <sys/ioctl.h>
-#endif /* !_IOCTL_H_ */
