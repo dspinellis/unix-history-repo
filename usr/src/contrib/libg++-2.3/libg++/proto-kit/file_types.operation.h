982f93d118b8c67a97c6ffc2557e3fa09b47bd3a
CSRG <csrg@ucbvax.Berkeley.EDU>
1991-06-27 23:24:53 -0800

    BSD 4_4_Lite1 development Work on file usr/src/contrib/libg++-2.3/libg++/proto-kit/Makefile Work on file usr/src/contrib/libg++-2.3/libg++/proto-kit/file_types.hierarchy Work on file usr/src/contrib/libg++-2.3/libg++/proto-kit/file_types.includes.h Work on file usr/src/contrib/libg++-2.3/libg++/proto-kit/file_types.operation.h Work on file usr/src/contrib/libg++-2.3/libg++/proto-kit/file_types.type.h Work on file usr/src/contrib/libg++-2.3/libg++/proto-kit/genclass.extnsn Work on file usr/src/contrib/libg++-2.3/libg++/proto-kit/hierarchy Work on file usr/src/contrib/libg++-2.3/libg++/proto-kit/make-defs-file Work on file usr/src/contrib/libg++-2.3/libg++/proto-kit/make-source-dependencies Work on file usr/src/contrib/libg++-2.3/libg++/proto-kit/make-types Work on file usr/src/contrib/libg++-2.3/libg++/proto-kit/prepend Work on file usr/src/contrib/libg++-2.3/libg++/proto-kit/prepend-header Work on file usr/src/contrib/libg++-2.3/libg++/proto-kit/prototype Work on file usr/src/contrib/libg++-2.3/libg++/proto-kit/prototype.dependencies
    Synthesized-from: CSRG/cd2/4.4BSD-Lite1

---
+++ usr/src/contrib/libg++-2.3/libg++/proto-kit/file_types.operation.h
@@ -0,0 +1,17 @@
+#define FILE_TYPES_OPERATION(OPERATION)  \
+OPERATION(Substrate) \
+OPERATION(SubstrateUnix) \
+OPERATION(SubstrateImmediate) \
+OPERATION(SubstrateSimple) \
+OPERATION(SubstrateUnixDirectory) \
+OPERATION(SubstrateUnixImmediate) \
+OPERATION(SubstrateUnixSimple) \
+OPERATION(SubstrateSimpleWhole) \
+OPERATION(SubstrateSimplePartial) \
+OPERATION(SubstrateUnixDirectoryImmediate) \
+OPERATION(SubstrateUnixDirectorySimple) \
+OPERATION(SubstrateUnixSimpleWhole) \
+OPERATION(SubstrateUnixSimplePartial) \
+OPERATION(SubstrateUnixDirectorySimpleWhole) \
+OPERATION(SubstrateUnixDirectorySimplePartial) \
+
