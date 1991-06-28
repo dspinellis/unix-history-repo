8fd5791b0ddef1f576c0bb93c214a94862e0f261
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-06-27 22:05:04 -0800

    correct error message for -h option by unprivileged user
    SCCS-vsn: usr.bin/login/login.c 5.71

--- usr/src/usr.bin/login/login.c
+++ usr/src/usr.bin/login/login.c
@@ -12,7 +12,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)login.c	5.70 (Berkeley) %G%";
+static char sccsid[] = "@(#)login.c	5.71 (Berkeley) %G%";
 #endif /* not lint */
 
 /*
@@ -122,7 +122,7 @@ main(argc, argv)
 		case 'h':
 			if (uid) {
 				(void)fprintf(stderr,
-				    "login: -h for super-user only.\n");
+				    "login: -h option: %s\n", strerror(EPERM));
 				exit(1);
 			}
 			if (rflag) {
