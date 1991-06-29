83d74cf08345484cff429045cf807eafa359c4c5
Mike Karels <karels@ucbvax.Berkeley.EDU>
1991-06-28 21:18:27 -0800

    we don't have page mode
    SCCS-vsn: sys/hp/dev/hil.c 7.9

--- usr/src/sys/hp/dev/hil.c
+++ usr/src/sys/hp/dev/hil.c
@@ -11,7 +11,7 @@
  *
  * from: Utah $Hdr: hil.c 1.33 89/12/22$
  *
- *	@(#)hil.c	7.8.1.1 (Berkeley) %G%
+ *	@(#)hil.c	7.9 (Berkeley) %G%
  */
 
 #include "sys/param.h"
@@ -1052,8 +1052,63 @@ hilq_to_b(q, cp, cc)
 	register struct clist *q;
 	register char *cp;
 {
+	register struct cblock *bp;
+	register int nc;
+	char *acp;
+	int s;
+	extern char cwaiting;
 
-	BODY_DELETED
+	if (cc <= 0)
+		return (0);
+	s = splhil();
+	if (q->c_cc <= 0) {
+		q->c_cc = 0;
+		q->c_cf = q->c_cl = NULL;
+		splx(s);
+		return (0);
+	}
+	acp = cp;
+
+	while (cc) {
+		nc = sizeof (struct cblock) - ((int)q->c_cf & CROUND);
+		nc = MIN(nc, cc);
+		nc = MIN(nc, q->c_cc);
+		(void) bcopy(q->c_cf, cp, (unsigned)nc);
+		q->c_cf += nc;
+		q->c_cc -= nc;
+		cc -= nc;
+		cp += nc;
+		if (q->c_cc <= 0) {
+			bp = (struct cblock *)(q->c_cf - 1);
+			bp = (struct cblock *)((int)bp & ~CROUND);
+			q->c_cf = q->c_cl = NULL;
+			spltty();
+			bp->c_next = cfreelist;
+			cfreelist = bp;
+			cfreecount += CBSIZE;
+			if (cwaiting) {
+				wakeup(&cwaiting);
+				cwaiting = 0;
+			}
+			break;
+		}
+		if (((int)q->c_cf & CROUND) == 0) {
+			bp = (struct cblock *)(q->c_cf);
+			bp--;
+			q->c_cf = bp->c_next->c_info;
+			spltty();
+			bp->c_next = cfreelist;
+			cfreelist = bp;
+			cfreecount += CBSIZE;
+			if (cwaiting) {
+				wakeup(&cwaiting);
+				cwaiting = 0;
+			}
+			splhil();
+		}
+	}
+	splx(s);
+	return (cp-acp);
 }
 
 /*
