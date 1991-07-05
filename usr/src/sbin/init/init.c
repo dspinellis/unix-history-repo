8a9349cce40d4fdda63d2eae2388c357f58ceec2
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1991-07-04 21:42:23 -0800

    check in for Kirk, going to look at the DB stuff
    SCCS-vsn: sbin/init/init.c 6.2

--- usr/src/sbin/init/init.c
+++ usr/src/sbin/init/init.c
@@ -15,7 +15,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)init.c	6.1 (Berkeley) %G%";
+static char sccsid[] = "@(#)init.c	6.2 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/types.h>
