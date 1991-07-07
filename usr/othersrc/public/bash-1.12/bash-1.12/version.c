62d04e8bad27dd66aed0f54621b3d0d635e1b73e
William F. Jolitz <wjolitz@soda.berkeley.edu>
1991-07-07 10:37:47 -0800

    386BSD 0.1 development Work on file usr/othersrc/public/bash-1.12/bash-1.12/version.c
    Co-Authored-By: Lynne Greer Jolitz <ljolitz@cardio.ucsf.edu>
    Synthesized-from: 386BSD-0.1

---
+++ usr/othersrc/public/bash-1.12/bash-1.12/version.c
@@ -0,0 +1,24 @@
+/* version.c -- distribution and version numbers. */
+
+/* Copyright (C) 1989 Free Software Foundation, Inc.
+
+   This file is part of GNU Bash, the Bourne Again SHell.
+
+   Bash is free software; you can redistribute it and/or modify it under
+   the terms of the GNU General Public License as published by the Free
+   Software Foundation; either version 1, or (at your option) any later
+   version.
+
+   Bash is distributed in the hope that it will be useful, but WITHOUT ANY
+   WARRANTY; without even the implied warranty of MERCHANTABILITY or
+   FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+   for more details.
+
+   You should have received a copy of the GNU General Public License along
+   with Bash; see the file COPYING.  If not, write to the Free Software
+   Foundation, 675 Mass Ave, Cambridge, MA 02139, USA. */
+
+#include "version.h"
+
+char *dist_version = DISTVERSION;
+int build_version = BUILDVERSION;
