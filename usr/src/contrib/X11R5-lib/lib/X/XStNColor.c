bba4cbfb86124d2b0520bf43305ee12e9e477cf7
CSRG <csrg@ucbvax.Berkeley.EDU>
1991-07-22 04:35:46 -0800

    BSD 4_4_Lite1 development Work on file usr/src/contrib/X11R5-lib/lib/X/XStNColor.c
    Synthesized-from: CSRG/cd2/4.4BSD-Lite1

---
+++ usr/src/contrib/X11R5-lib/lib/X/XStNColor.c
@@ -0,0 +1,80 @@
+/* $XConsortium: XStNColor.c,v 11.22 91/07/22 22:35:01 rws Exp $ */
+/* Copyright    Massachusetts Institute of Technology    1986	*/
+
+/*
+Permission to use, copy, modify, distribute, and sell this software and its
+documentation for any purpose is hereby granted without fee, provided that
+the above copyright notice appear in all copies and that both that
+copyright notice and this permission notice appear in supporting
+documentation, and that the name of M.I.T. not be used in advertising or
+publicity pertaining to distribution of the software without specific,
+written prior permission.  M.I.T. makes no representations about the
+suitability of this software for any purpose.  It is provided "as is"
+without express or implied warranty.
+*/
+
+#include <stdio.h>
+#include "Xlibint.h"
+#include "Xcmsint.h"
+
+extern void _XcmsRGB_to_XColor();
+
+#if NeedFunctionPrototypes
+XStoreNamedColor(
+register Display *dpy,
+Colormap cmap,
+_Xconst char *name, /* STRING8 */
+unsigned long pixel, /* CARD32 */
+int flags)  /* DoRed, DoGreen, DoBlue */
+#else
+XStoreNamedColor(dpy, cmap, name, pixel, flags)
+register Display *dpy;
+Colormap cmap;
+char *name; /* STRING8 */
+unsigned long pixel; /* CARD32 */
+int flags;  /* DoRed, DoGreen, DoBlue */
+#endif
+{
+    unsigned int nbytes;
+    register xStoreNamedColorReq *req;
+    XcmsCCC ccc;
+    XcmsColor cmsColor_exact;
+    XColor scr_def;
+
+    /*
+     * Let's Attempt to use TekCMS approach to Parse Color
+     */
+    if ((ccc = XcmsCCCOfColormap(dpy, cmap)) != (XcmsCCC)NULL) {
+	if (_XcmsResolveColorString(ccc, &name, &cmsColor_exact,
+		XcmsRGBFormat) != XcmsFailure) {
+	    _XcmsRGB_to_XColor(&cmsColor_exact, &scr_def, 1);
+	    scr_def.pixel = pixel;
+	    scr_def.flags = flags;
+	    XStoreColor(dpy, cmap, &scr_def);
+	    return;
+	}
+	/*
+	 * Otherwise we failed; or name was changed with yet another
+	 * name.  Thus pass name to the X Server.
+	 */
+    }
+
+    /*
+     * The TekCMS and i18n methods failed, so lets pass it to the server
+     * for parsing.
+     */
+
+    LockDisplay(dpy);
+    GetReq(StoreNamedColor, req);
+
+    req->cmap = cmap;
+    req->flags = flags;
+    req->pixel = pixel;
+    req->nbytes = nbytes = strlen(name);
+    req->length += (nbytes + 3) >> 2; /* round up to multiple of 4 */
+    Data(dpy, name, (long)nbytes);
+    UnlockDisplay(dpy);
+    SyncHandle();
+}
+
+
