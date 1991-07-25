f7281954260c14c030b968b7a470e055171e9954
Eric Allman <eric@ucbvax.Berkeley.EDU>
1991-07-25 14:47:50 -0800

    fix botch in file locking when checkpoints kick in
    SCCS-vsn: usr.sbin/sendmail/src/deliver.c 5.42
    SCCS-vsn: usr.sbin/sendmail/src/version.c 5.68

--- usr/src/usr.sbin/sendmail/src/deliver.c
+++ usr/src/usr.sbin/sendmail/src/deliver.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)deliver.c	5.41 (Berkeley) %G%";
+static char sccsid[] = "@(#)deliver.c	5.42 (Berkeley) %G%";
 #endif /* not lint */
 
 #include "sendmail.h"
