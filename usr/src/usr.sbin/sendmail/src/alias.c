670238b630da9dda07380ab331ab9d61fcf73747
Eric Allman <eric@ucbvax.Berkeley.EDU>
1991-07-27 00:46:17 -0800

    ugh...  fixes for older C compiler
    SCCS-vsn: usr.sbin/sendmail/src/alias.c 5.25

--- usr/src/usr.sbin/sendmail/src/alias.c
+++ usr/src/usr.sbin/sendmail/src/alias.c
@@ -6,24 +6,6 @@
  * %sccs.include.redist.c%
  */
 
-#ifndef lint
-#ifdef DBM
-static char sccsid[] = "@(#)alias.c	5.24 (Berkeley) %G% (with DBM)";
-#else
-#ifdef NEWDB
-static char sccsid[] = "@(#)alias.c	5.24 (Berkeley) %G% (with NEWDB)";
-#else
-static char sccsid[] = "@(#)alias.c	5.24 (Berkeley) %G% (without DBM)";
-#endif
-#endif
-#endif /* not lint */
-
-# ifdef DBM
-# ifdef NEWDB
-#   ERROR: must choose one of DBM or NEWDB compilation flags
-# endif
-# endif
-
 # include <sys/types.h>
 # include <sys/stat.h>
 # include <signal.h>
@@ -34,6 +16,24 @@ static char sccsid[] = "@(#)alias.c	5.24 (Berkeley) %G% (without DBM)";
 
 # ifdef NEWDB
 # include <db.h>
+# endif
+
+#ifndef lint
+#ifdef DBM
+static char sccsid[] = "@(#)alias.c	5.25 (Berkeley) %G% (with DBM)";
+#else
+#ifdef NEWDB
+static char sccsid[] = "@(#)alias.c	5.25 (Berkeley) %G% (with NEWDB)";
+#else
+static char sccsid[] = "@(#)alias.c	5.25 (Berkeley) %G% (without DBM)";
+#endif
+#endif
+#endif /* not lint */
+
+# ifdef DBM
+# ifdef NEWDB
+  ERROR ERROR ERROR: must choose one of DBM or NEWDB compilation flags
+# endif
 # endif
 
 /*
