ac703d41362feff1fc7c0eb6eb28e7f21fc4e84f
Keith Sklower <sklower@ucbvax.Berkeley.EDU>
1991-07-31 00:41:17 -0800

    change require by using routing vector instead of linked list
    SCCS-vsn: sys/netiso/iso.c 7.15

--- usr/src/sys/netiso/iso.c
+++ usr/src/sys/netiso/iso.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)iso.c	7.14 (Berkeley) %G%
+ *	@(#)iso.c	7.15 (Berkeley) %G%
  */
 
 /***********************************************************
@@ -80,14 +80,10 @@ int ether_output(), llc_rtrequest();
  *
  * NOTES:			
  */
-struct radix_node_head *iso_rnhead;
 iso_init()
 {
-	static iso_init_done;
-
-	if (iso_init_done == 0) {
-		iso_init_done++;
-		rn_inithead(&iso_rnhead, 48, AF_ISO);
+	if (rt_tables[AF_ISO] == 0) {
+		rn_inithead(rt_tables + AF_ISO, 48);
 	}
 }
 
