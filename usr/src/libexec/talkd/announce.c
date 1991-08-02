553ce5d70899cac0ac1e734e75fd33a6cefe231e
Marc Teitelbaum <marc@ucbvax.Berkeley.EDU>
1991-08-01 23:47:33 -0800

    make timeout dependent on RING_WAIT (time when client will try re-inviting the callee)
    SCCS-vsn: libexec/talkd/announce.c 5.11

--- usr/src/libexec/talkd/announce.c
+++ usr/src/libexec/talkd/announce.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)announce.c	5.10 (Berkeley) %G%";
+static char sccsid[] = "@(#)announce.c	5.11 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/types.h>
