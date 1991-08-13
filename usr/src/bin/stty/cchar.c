f58c50fe382e049ff656da31b3672fcd1aa88204
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-08-12 21:47:37 -0800

    picked and dropped a bit too fast...
    SCCS-vsn: bin/stty/cchar.c 5.5

--- usr/src/bin/stty/cchar.c
+++ usr/src/bin/stty/cchar.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)cchar.c	5.4 (Berkeley) %G%";
+static char sccsid[] = "@(#)cchar.c	5.5 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -76,7 +76,7 @@ csearch(argvp, ip)
 	if (!arg)
 		err("option requires an argument -- %s\n%s", name, usage);
 
-#define CHK(s)  (*name == s[0] && !strcmp(name, s))
+#define CHK(s)  (*arg == s[0] && !strcmp(arg, s))
 	if (CHK("undef") || CHK("<undef>"))
 		ip->t.c_cc[cp->sub] = _POSIX_VDISABLE;
 	else if (arg[0] == '^')
