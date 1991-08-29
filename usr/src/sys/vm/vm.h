05d49f85f895d5daa3009b7d5c1eeb5ba3494499
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1991-08-28 22:45:25 -0800

    vm_statistics.h is merged into vmmeter.h
    SCCS-vsn: sys/vm/vm.h 7.2

--- usr/src/sys/vm/vm.h
+++ usr/src/sys/vm/vm.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)vm.h	7.1 (Berkeley) %G%
+ *	@(#)vm.h	7.2 (Berkeley) %G%
  */
 
 #ifndef VM_H
@@ -16,8 +16,8 @@
 #include <vm/vm_inherit.h>
 #include <vm/vm_map.h>
 #include <vm/vm_object.h>
-#include <vm/vm_statistics.h>
 #include <vm/pmap.h>
+#include <sys/vmmeter.h>
 
 /*
  * Shareable process virtual address space.
