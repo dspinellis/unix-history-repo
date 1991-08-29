c2162a3dbced59b1a97e1fb6690a4ec96e251d13
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1991-08-28 22:42:03 -0800

    pmap_statistics moves in from deprecated vm_statistics.h
    SCCS-vsn: sys/vm/pmap.h 7.5

--- usr/src/sys/vm/pmap.h
+++ usr/src/sys/vm/pmap.h
@@ -44,6 +44,18 @@
 #ifndef	_PMAP_VM_
 #define	_PMAP_VM_
 
+/*
+ * Each machine dependent implementation is expected to
+ * keep certain statistics.  They may do this anyway they
+ * so choose, but are expected to return the statistics
+ * in the following structure.
+ */
+struct pmap_statistics {
+	long		resident_count;	/* # of pages mapped (total)*/
+	long		wired_count;	/* # of pages wired */
+};
+typedef struct pmap_statistics	*pmap_statistics_t;
+
 #include <machine/pmap.h>
 
 #ifdef KERNEL
