c71bf7f0411ea928b670459940d5ac3c41fd5290
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1991-08-29 00:07:47 -0800

    use PAGE_MASK macro
    SCCS-vsn: sys/vm/vm_mmap.c 7.8

--- usr/src/sys/vm/vm_mmap.c
+++ usr/src/sys/vm/vm_mmap.c
@@ -11,7 +11,7 @@
  *
  * from: Utah $Hdr: vm_mmap.c 1.3 90/01/21$
  *
- *	@(#)vm_mmap.c	7.7 (Berkeley) %G%
+ *	@(#)vm_mmap.c	7.8 (Berkeley) %G%
  */
 
 /*
@@ -119,7 +119,7 @@ smmap(p, uap, retval)
 	 * Size is implicitly rounded to a page boundary.
 	 */
 	addr = (vm_offset_t) uap->addr;
-	if ((uap->flags & MAP_FIXED) && (addr & page_mask) || uap->len < 0)
+	if ((uap->flags & MAP_FIXED) && (addr & PAGE_MASK) || uap->len < 0)
 		return(EINVAL);
 	size = (vm_size_t) round_page(uap->len);
 	/*
@@ -205,7 +205,7 @@ msync(p, uap, retval)
 		printf("msync(%d): addr %x len %x\n",
 		       p->p_pid, uap->addr, uap->len);
 #endif
-	if (((int)uap->addr & page_mask) || uap->len < 0)
+	if (((int)uap->addr & PAGE_MASK) || uap->len < 0)
 		return(EINVAL);
 	addr = oaddr = (vm_offset_t)uap->addr;
 	osize = (vm_size_t)uap->len;
@@ -274,7 +274,7 @@ munmap(p, uap, retval)
 #endif
 
 	addr = (vm_offset_t) uap->addr;
-	if ((addr & page_mask) || uap->len < 0)
+	if ((addr & PAGE_MASK) || uap->len < 0)
 		return(EINVAL);
 	size = (vm_size_t) round_page(uap->len);
 	if (size == 0)
@@ -320,7 +320,7 @@ mprotect(p, uap, retval)
 #endif
 
 	addr = (vm_offset_t) uap->addr;
-	if ((addr & page_mask) || uap->len < 0)
+	if ((addr & PAGE_MASK) || uap->len < 0)
 		return(EINVAL);
 	size = (vm_size_t) uap->len;
 	/*
