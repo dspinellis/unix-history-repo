29c13d8abad1b33bf71a2e8b9a1f30e192d8b42e
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-09-04 19:39:48 -0800

    Rev #2 (after ANSI/POSIX changes to hash)
    SCCS-vsn: lib/libc/db/test/hash.tests/tcreat3.c 5.2
    SCCS-vsn: lib/libc/db/test/hash.tests/tdel.c 5.3
    SCCS-vsn: lib/libc/db/test/hash.tests/thash4.c 5.3
    SCCS-vsn: lib/libc/db/test/hash.tests/tread2.c 5.3
    SCCS-vsn: lib/libc/db/test/hash.tests/tseq.c 5.2
    SCCS-vsn: lib/libc/db/test/hash.tests/tverify.c 5.3

--- usr/src/lib/libc/db/test/hash.tests/tdel.c
+++ usr/src/lib/libc/db/test/hash.tests/tdel.c
@@ -5,7 +5,33 @@
  * This code is derived from software contributed to Berkeley by
  * Margo Seltzer.
  *
- * %sccs.include.redist.c%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the University of
+ *	California, Berkeley and its contributors.
+ * 4. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
  */
 
 #ifndef lint
@@ -15,7 +41,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)tdel.c	5.2 (Berkeley) %G%";
+static char sccsid[] = "@(#)tdel.c	5.2 (Berkeley) 3/12/91";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -83,7 +109,7 @@ char **argv;
 			fgets(wp2, 8192, fp) &&
 			i++ < MAXWORDS) {
 		    key.size = strlen(wp1);
-		    stat = (dbp->delete)(dbp, &key);
+		    stat = (dbp->del)(dbp, &key, 0);
 		    if (stat) {
 			fprintf ( stderr, "Error retrieving %s\n", key.data );
 			exit(1);
