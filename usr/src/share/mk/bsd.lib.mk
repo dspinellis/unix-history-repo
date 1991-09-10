2228880a5b2c18db3ffccf0b4bbab71d93f25e88
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-09-09 22:23:22 -0800

    don't need to use old nroff anymore
    SCCS-vsn: share/mk/bsd.lib.mk 5.28
    SCCS-vsn: share/mk/bsd.prog.mk 5.28

--- usr/src/share/mk/bsd.lib.mk
+++ usr/src/share/mk/bsd.lib.mk
@@ -1,4 +1,4 @@
-#	@(#)bsd.lib.mk	5.27 (Berkeley) %G%
+#	@(#)bsd.lib.mk	5.28 (Berkeley) %G%
 
 .if exists(${.CURDIR}/../Makefile.inc)
 .include "${.CURDIR}/../Makefile.inc"
@@ -23,7 +23,7 @@ BINMODE?=	555
 .SUFFIXES: .out .o .po .s .c .f .y .l .8 .7 .6 .5 .4 .3 .2 .1 .0
 
 .8.0 .7.0 .6.0 .5.0 .4.0 .3.0 .2.0 .1.0:
-	/usr/old/bin/nroff -mandoc ${.IMPSRC} > ${.TARGET}
+	nroff -man ${.IMPSRC} > ${.TARGET}
 
 .c.o:
 	${CC} ${CFLAGS} -c ${.IMPSRC} 
