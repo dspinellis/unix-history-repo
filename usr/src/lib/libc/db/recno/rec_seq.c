5666b0fd5509d8b6465168cbb3cebe21a8ab7123
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-09-11 23:27:11 -0800

    redo cursor implementation, __rec_search interface
    SCCS-vsn: lib/libc/db/recno/rec_seq.c 5.2

--- usr/src/lib/libc/db/recno/rec_seq.c
+++ usr/src/lib/libc/db/recno/rec_seq.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)rec_seq.c	5.1 (Berkeley) %G%";
+static char sccsid[] = "@(#)rec_seq.c	5.2 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -14,7 +14,7 @@ static char sccsid[] = "@(#)rec_seq.c	5.1 (Berkeley) %G%";
 #include <db.h>
 #include <limits.h>
 #include <stdio.h>
-#include "../btree/btree.h"
+#include "recno.h"
 
 /*
  * __REC_SEQ -- Recno sequential scan interface.
@@ -37,7 +37,7 @@ __rec_seq(dbp, key, data, flags)
 	BTREE *t;
 	EPG *e;
 	recno_t nrec;
-	int exact, status;
+	int status;
 
 	t = dbp->internal;
 	switch(flags) {
@@ -55,11 +55,13 @@ __rec_seq(dbp, key, data, flags)
 		/* FALLTHROUGH */
 	case R_FIRST:
 		nrec = 1;
-		SET(t, BTF_SEQINIT);
 		break;
 	case R_PREV:
 		if (ISSET(t, BTF_SEQINIT)) {
-			nrec = t->bt_rcursor - 1;
+			if ((nrec = t->bt_rcursor - 1) == 0) {
+				errno = EINVAL;
+				return (RET_ERROR);
+			}
 			break;
 		}
 		/* FALLTHROUGH */
@@ -67,26 +69,27 @@ __rec_seq(dbp, key, data, flags)
 		if (t->bt_irec(t, MAX_REC_NUMBER) == RET_ERROR)
 			return (RET_ERROR);
 		nrec = t->bt_nrecs;
-		SET(t, BTF_SEQINIT);
 		break;
 	default:
 		errno = EINVAL;
 		return (RET_ERROR);
 	}
 	
-	if (nrec > t->bt_nrecs && (status = t->bt_irec(t, nrec)) != RET_SUCCESS)
-		return (status);
-
-	if ((e = __rec_search(t, nrec - 1, &exact)) == NULL)
-		return (RET_ERROR);
-
-	if (!exact) {
-		mpool_put(t->bt_mp, e->page, 0);
-		return (RET_SPECIAL);
+	if (nrec > t->bt_nrecs) {
+		if ((status = t->bt_irec(t, nrec)) != RET_SUCCESS)
+			return (status);
+		if (nrec > t->bt_nrecs)
+			return (RET_SPECIAL);
 	}
 
-	if ((status = __rec_ret(t, e, data)) == RET_SUCCESS)
+	if ((e = __rec_search(t, nrec - 1, SEARCH)) == NULL)
+		return (RET_ERROR);
+
+	if ((status = __rec_ret(t, e, data)) == RET_SUCCESS) {
 		t->bt_rcursor = nrec;
+		SET(t, BTF_SEQINIT);
+		UNSET(t, BTF_DELCRSR);
+	}
 	mpool_put(t->bt_mp, e->page, 0);
 	return (status);
 }
