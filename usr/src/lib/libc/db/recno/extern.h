6c91dfdbb80044a2939d5bde137664ae916889ef
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-09-11 23:21:02 -0800

    add __rec_dleaf; __rec_get takes const key; __rec_search takes argument for modifying parent pages during search
    SCCS-vsn: lib/libc/db/recno/extern.h 5.2

--- usr/src/lib/libc/db/recno/extern.h
+++ usr/src/lib/libc/db/recno/extern.h
@@ -4,23 +4,24 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)extern.h	5.1 (Berkeley) %G%
+ *	@(#)extern.h	5.2 (Berkeley) %G%
  */
+
+#include "../btree/extern.h"
 
 int	 __rec_close __P((DB *));
 int	 __rec_delete __P((const DB *, const DBT *, u_int));
+int	 __rec_dleaf __P((BTREE *, PAGE *, int));
 int	 __rec_fmap __P((BTREE *, recno_t));
 int	 __rec_fout __P((BTREE *));
 int	 __rec_fpipe __P((BTREE *, recno_t));
-int	 __rec_get __P((const DB *, DBT *, DBT *, u_int));
+int	 __rec_get __P((const DB *, const DBT *, DBT *, u_int));
 int	 __rec_iput __P((BTREE *, recno_t, const DBT *, u_int));
 int	 __rec_put __P((const DB *dbp, const DBT *, const DBT *, u_int));
 int	 __rec_ret __P((BTREE *, EPG *, DBT *));
-EPG	*__rec_search __P((BTREE *, recno_t, int *));
+EPG	*__rec_search __P((BTREE *, recno_t, enum SRCHOP));
 int	 __rec_seq __P((const DB *, DBT *, DBT *, u_int));
 int	 __rec_sync __P((const DB *));
 int	 __rec_vmap __P((BTREE *, recno_t));
 int	 __rec_vout __P((BTREE *));
 int	 __rec_vpipe __P((BTREE *, recno_t));
-
-#include "../btree/extern.h"
