c1fc29b4bb594f352d5fb0a0464dcd2d108bf3ce
Christos Zoulas <christos@ucbvax.Berkeley.EDU>
1991-09-16 01:00:30 -0800

    added $ to the list of history chars to keep
    SCCS-vsn: bin/csh/lex.c 5.23

--- usr/src/bin/csh/lex.c
+++ usr/src/bin/csh/lex.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)lex.c	5.22 (Berkeley) %G%";
+static char sccsid[] = "@(#)lex.c	5.23 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -1091,7 +1091,7 @@ gethent(sc)
 	    }
 	    np = lhsb;
 	    event = 0;
-	    while (!cmap(c, _ESC | _META | _Q | _Q1) && !any("{}:", c)) {
+	    while (!cmap(c, _ESC | _META | _Q | _Q1) && !any("${}:", c)) {
 		if (event != -1 && Isdigit(c))
 		    event = event * 10 + c - '0';
 		else
