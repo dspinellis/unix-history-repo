e509561cf53f80c367355a114d831c9a6a2e9928
Cynthia A. E. Livingston <cael@ucbvax.Berkeley.EDU>
1991-09-21 21:58:00 -0800

    date and time created 91/09/21 14:58:00 by cael
    SCCS-vsn: share/tmac/tmac.s 1.1

---
+++ usr/src/share/tmac/tmac.s
@@ -0,0 +1,15 @@
+.\"
+.\" Copyright (c) 1990 The Regents of the University of California.
+.\" All rights reserved.
+.\"
+.\" %sccs.include.redist.roff%
+.\"
+.\"	@(#)tmac.s	1.1 (Berkeley) %G%
+.\"
+.\" If groff, use groff -ms, else use local -ms (w/ditroff, troff, nroff)
+.ie g {\
+.	so tmac.s
+.\}
+.el \{
+.	so /usr/local/lib/tmac/tmac.s
+.\}
