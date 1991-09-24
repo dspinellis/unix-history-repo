3fa309308932049cd526cacb399f5402faa6d454
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-09-23 22:09:11 -0800

    hash_open -> dbopen, R_PUT no longer necessary
    SCCS-vsn: usr.sbin/sendmail/src/alias.c 5.26

--- usr/src/usr.sbin/sendmail/src/alias.c
+++ usr/src/usr.sbin/sendmail/src/alias.c
@@ -20,12 +20,12 @@
 
 #ifndef lint
 #ifdef DBM
-static char sccsid[] = "@(#)alias.c	5.25 (Berkeley) %G% (with DBM)";
+static char sccsid[] = "@(#)alias.c	5.26 (Berkeley) %G% (with DBM)";
 #else
 #ifdef NEWDB
-static char sccsid[] = "@(#)alias.c	5.25 (Berkeley) %G% (with NEWDB)";
+static char sccsid[] = "@(#)alias.c	5.26 (Berkeley) %G% (with NEWDB)";
 #else
-static char sccsid[] = "@(#)alias.c	5.25 (Berkeley) %G% (without DBM)";
+static char sccsid[] = "@(#)alias.c	5.26 (Berkeley) %G% (without DBM)";
 #endif
 #endif
 #endif /* not lint */
@@ -222,7 +222,7 @@ initaliases(aliasfile, init)
 # ifdef NEWDB
 		(void) strcpy(buf, aliasfile);
 		(void) strcat(buf, ".db");
-		AliasDBptr = hash_open(buf, O_RDONLY, DBMMODE, NULL);
+		AliasDBptr = dbopen(buf, O_RDONLY, DBMMODE, DB_HASH, NULL);
 		if (AliasDBptr == NULL)
 		{
 			syserr("initaliases: cannot open %s", buf);
@@ -256,7 +256,8 @@ initaliases(aliasfile, init)
 # ifdef NEWDB
 			(void) strcpy(buf, aliasfile);
 			(void) strcat(buf, ".db");
-			AliasDBptr = hash_open(buf, O_RDONLY, DBMMODE, NULL);
+			AliasDBptr =
+			    dbopen(buf, O_RDONLY, DBMMODE, DB_HASH, NULL);
 			if (AliasDBptr == NULL)
 			{
 				syserr("initaliases: cannot open %s", buf);
@@ -426,7 +427,8 @@ readaliases(aliasfile, init)
 # ifdef NEWDB
 		(void) strcpy(line, aliasfile);
 		(void) strcat(line, ".db");
-		dbp = hash_open(line, O_RDWR|O_CREAT|O_TRUNC, DBMMODE, NULL);
+		dbp = dbopen(line,
+		    O_RDWR|O_CREAT|O_TRUNC, DBMMODE, DB_HASH, NULL);
 		if (dbp == NULL)
 		{
 			syserr("readaliases: cannot create %s", line);
@@ -603,7 +605,7 @@ readaliases(aliasfile, init)
 # ifdef DBM
 			store(key, content);
 # else
-			if (dbp->put(dbp, &key, &content, R_PUT) != 0)
+			if (dbp->put(dbp, &key, &content, 0) != 0)
 				syserr("readaliases: db put (%s)", al.q_user);
 # endif
 		}
@@ -631,7 +633,7 @@ readaliases(aliasfile, init)
 		key.data = "@";
 # ifdef NEWDB
 		if (dbp->sync(dbp) != 0 ||
-		    dbp->put(dbp, &key, &key, R_PUT) != 0 ||
+		    dbp->put(dbp, &key, &key, 0) != 0 ||
 		    dbp->close(dbp) != 0)
 			syserr("readaliases: db close failure");
 # else
