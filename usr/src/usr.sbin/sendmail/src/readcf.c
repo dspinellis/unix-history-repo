6ebff832c2c7107bb1903bd827c159e5e52933da
Eric Allman <eric@ucbvax.Berkeley.EDU>
1991-09-30 22:28:18 -0800

    fix some debugging output
    SCCS-vsn: usr.sbin/sendmail/src/readcf.c 5.25

--- usr/src/usr.sbin/sendmail/src/readcf.c
+++ usr/src/usr.sbin/sendmail/src/readcf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)readcf.c	5.24 (Berkeley) %G%";
+static char sccsid[] = "@(#)readcf.c	5.25 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
