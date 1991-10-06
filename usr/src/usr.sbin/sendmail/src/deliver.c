a7edcf93abd9c20478a0a4d64eb5e87a3279b5f8
Eric Allman <eric@ucbvax.Berkeley.EDU>
1991-10-05 18:05:02 -0800

    if config level >=2, don't reset RES_DEFNAMES|RES_DNSRCH
    SCCS-vsn: usr.sbin/sendmail/src/deliver.c 5.44

--- usr/src/usr.sbin/sendmail/src/deliver.c
+++ usr/src/usr.sbin/sendmail/src/deliver.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)deliver.c	5.43 (Berkeley) %G%";
+static char sccsid[] = "@(#)deliver.c	5.44 (Berkeley) %G%";
 #endif /* not lint */
 
 #include "sendmail.h"
@@ -374,7 +374,8 @@ deliver(firstto, editfcn)
 	if (editfcn == NULL)
 		editfcn = putmessage;
 #ifdef NAMED_BIND
-	_res.options &= ~(RES_DEFNAMES | RES_DNSRCH);		/* XXX */
+	if (ConfigLevel < 2)
+		_res.options &= ~(RES_DEFNAMES | RES_DNSRCH);	/* XXX */
 #endif
 		}
 		if (Nmx >= 0)
