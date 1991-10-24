b039560f43fa415162ecfd6cdc223a88849d2a31
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-10-24 00:41:23 -0800

    add isblank, minor KNF whacks
    SCCS-vsn: lib/libc/locale/isctype.c 5.3

--- usr/src/lib/libc/locale/isctype.c
+++ usr/src/lib/libc/locale/isctype.c
@@ -6,7 +6,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)isctype.c	5.2 (Berkeley) %G%";
+static char sccsid[] = "@(#)isctype.c	5.3 (Berkeley) %G%";
 #endif /* LIBC_SCCS and not lint */
 
 #define _ANSI_LIBRARY
@@ -16,89 +16,96 @@ static char sccsid[] = "@(#)isctype.c	5.2 (Berkeley) %G%";
 isalnum(c)
 	int c;
 {
-	return((_ctype_ + 1)[c] & (_U|_L|_N));
+	return ((_ctype_ + 1)[c] & (_U|_L|_N));
 }
 
 #undef isalpha
 isalpha(c)
 	int c;
 {
-	return((_ctype_ + 1)[c] & (_U|_L));
+	return ((_ctype_ + 1)[c] & (_U|_L));
+}
+
+#undef isblank
+isblank(c)
+	int c;
+{
+	return (c == '\t' || c == ' ');
 }
 
 #undef iscntrl
 iscntrl(c)
 	int c;
 {
-	return((_ctype_ + 1)[c] & _C);
+	return ((_ctype_ + 1)[c] & _C);
 }
 
 #undef isdigit
 isdigit(c)
 	int c;
 {
-	return((_ctype_ + 1)[c] & _N);
+	return ((_ctype_ + 1)[c] & _N);
 }
 
 #undef isgraph
 isgraph(c)
 	int c;
 {
-	return((_ctype_ + 1)[c] & (_P|_U|_L|_N));
+	return ((_ctype_ + 1)[c] & (_P|_U|_L|_N));
 }
 
 #undef islower
 islower(c)
 	int c;
 {
-	return((_ctype_ + 1)[c] & _L);
+	return ((_ctype_ + 1)[c] & _L);
 }
 
 #undef isprint
 isprint(c)
 	int c;
 {
-	return((_ctype_ + 1)[c] & (_P|_U|_L|_N|_B));
+	return ((_ctype_ + 1)[c] & (_P|_U|_L|_N|_B));
 }
 
 #undef ispunct
 ispunct(c)
 	int c;
 {
-	return((_ctype_ + 1)[c] & _P);
+	return ((_ctype_ + 1)[c] & _P);
 }
 
 #undef isspace
 isspace(c)
 	int c;
 {
-	return((_ctype_ + 1)[c] & _S);
+	return ((_ctype_ + 1)[c] & _S);
 }
 
 #undef isupper
 isupper(c)
 	int c;
 {
-	return((_ctype_ + 1)[c] & _U);
+	return ((_ctype_ + 1)[c] & _U);
 }
 
 #undef isxdigit
 isxdigit(c)
 	int c;
 {
-	return((_ctype_ + 1)[c] & (_N|_X));
+	return ((_ctype_ + 1)[c] & (_N|_X));
 }
 
 #undef tolower
 tolower(c)
 	int c;
 {
-	return((c) - 'A' + 'a');
+	return ((c) - 'A' + 'a');
 }
 
 #undef toupper
 toupper(c)
 	int c;
 {
-	return((c) - 'a' + 'A');
+	return ((c) - 'a' + 'A');
 }
