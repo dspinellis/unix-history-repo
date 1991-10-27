47109377252f579bae0b53cf7a31d97af44b502d
Cynthia A. E. Livingston <cael@ucbvax.Berkeley.EDU>
1991-10-26 21:20:38 -0800

    modify exec'd pathnames to reflect move to /usr/old
    SCCS-vsn: old/refer/refer/pathnames.h 5.4

--- usr/src/old/refer/refer/pathnames.h
+++ usr/src/old/refer/refer/pathnames.h
@@ -4,17 +4,17 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)pathnames.h	5.3 (Berkeley) %G%
+ *	@(#)pathnames.h	5.4 (Berkeley) %G%
  */
 
 #include <paths.h>
 
 #define	_PATH_ALL	"/usr/dict/lookall/All"
-#define	_PATH_EIGN	"/usr/share/dict/eign"
-#define	_PATH_HUNT	"/usr/libexec/hunt"
+#define	_PATH_EIGN	"/usr/dict/eign"
+#define	_PATH_HUNT	"/usr/old/libexec/hunt"
 #define	_PATH_IND	"/var/db/Ind"
-#define	_PATH_LIB	"/usr/libexec"
-#define	_PATH_MKEY	"/usr/libexec/mkey"
+#define	_PATH_LIB	"/usr/old/libexec"
+#define	_PATH_MKEY	"/usr/old/libexec/mkey"
 #define	_PATH_PWD	"/bin/pwd"
 #define	_PATH_SORT	"/usr/bin/sort"
 #define	_PATH_TMPS	"/tmp/SbibXXXXXX"
