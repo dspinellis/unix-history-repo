9b731ee979201bb7cd74e122cf1697952926741c
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-10-28 00:12:17 -0800

    lint
    SCCS-vsn: usr.bin/finger/lprint.c 5.17

--- usr/src/usr.bin/finger/lprint.c
+++ usr/src/usr.bin/finger/lprint.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)lprint.c	5.16 (Berkeley) %G%";
+static char sccsid[] = "@(#)lprint.c	5.17 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -234,7 +234,7 @@ demi_print(str, oddfield)
 	return(oddfield);
 }
 
-static
+static int
 show_text(directory, file_name, header)
 	char *directory, *file_name, *header;
 {
