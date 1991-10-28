f2dd2ff989acd881ffc13434e7c4c7986b514798
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-10-28 00:15:25 -0800

    use btree instead of hashing, don't have to resort later
    SCCS-vsn: usr.bin/finger/sprint.c 5.11
    SCCS-vsn: usr.bin/finger/util.c 5.18

--- usr/src/usr.bin/finger/util.c
+++ usr/src/usr.bin/finger/util.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)util.c	5.17 (Berkeley) %G%";
+static char sccsid[] = "@(#)util.c	5.18 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/param.h>
@@ -28,6 +28,7 @@ static void	 find_idle_and_ttywrite __P((WHERE *));
 static void	 userinfo __P((PERSON *, struct passwd *));
 static WHERE	*walloc __P((PERSON *));
 
+int
 match(pw, user)
 	struct passwd *pw;
 	char *user;
@@ -127,10 +128,11 @@ PERSON *
 enter_person(pw)
 	register struct passwd *pw;
 {
-	PERSON *pn;
 	DBT data, key;
+	PERSON *pn;
 
-	if (db == NULL && (db = dbopen(NULL, O_RDWR, 0, DB_HASH, NULL)) == NULL)
+	if (db == NULL &&
+	    (db = dbopen(NULL, O_RDWR, 0, DB_BTREE, NULL)) == NULL)
 		err("%s", strerror(errno));
 
 	key.data = pw->pw_name;
