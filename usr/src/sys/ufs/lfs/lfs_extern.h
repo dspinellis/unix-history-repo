ae90dcaa139de65a00fe7e5e4bb72084dd4e614f
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-11-02 01:30:20 -0800

    checkpoint before integration
    SCCS-vsn: sys/ufs/lfs/lfs_extern.h 5.4

--- usr/src/sys/ufs/lfs/lfs_extern.h
+++ usr/src/sys/ufs/lfs/lfs_extern.h
@@ -4,50 +4,51 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)lfs_extern.h	5.3 (Berkeley) %G%
+ *	@(#)lfs_extern.h	5.4 (Berkeley) %G%
  */
 
 struct fid;
-struct inode;
-struct mount;
 struct nameidata;
 struct statfs;
 struct proc;
-struct ucred;
-struct vnode;
 
-daddr_t	 itod __P((LFS *, ino_t));
-int	 lfs_balloc __P((LFS *, VNODE *, daddr_t, int, BUF **));
-int	 lfs_bmap __P((struct inode *, daddr_t, daddr_t *));
-void	 lfs_bwrite __P((struct buf *));
-void	 lfs_cleaner __P((void));
-int	 lfs_fhtovp __P((struct mount *, struct fid *, struct vnode **));
+__BEGIN_DECLS
+u_long	 cksum __P((void *, size_t));				/* XXX */
+int	 lfs_blkatoff __P((struct inode *, off_t, char **, struct buf **));
+int	 lfs_bmap __P((INODE *, daddr_t, daddr_t *));
+int	 lfs_bwrite __P((BUF *));
+int	 lfs_fsync
+	     __P((struct vnode *, int, struct ucred *, int, struct proc *));
 u_long	 lfs_getversion __P((LFS *fs, ino_t));
-void	 lfs_hqueue __P((INODE *));
-ino_t	 lfs_ialloc __P((LFS *, struct inode *, struct inode **,
-	     struct ucred *));
-IFILE	*lfs_ientry __P((LFS *, ino_t));
-struct dinode *
-	 lfs_ifind __P((LFS *, ino_t, void *));
-void	 lfs_ifree __P((struct inode *));
-int	 lfs_inactive __P((struct vnode *, struct proc *));
+int	 lfs_ialloc __P((INODE *, int, UCRED *, INODE **));
+DINODE	*lfs_ifind __P((LFS *, ino_t, void *));
+void	 lfs_ifree __P((INODE *, ino_t, int));
+int	 lfs_iget __P((INODE *, ino_t, INODE **));
+int	 lfs_inactive __P((VNODE *, struct proc *));
 int	 lfs_init __P((void));
 void	 lfs_iset __P((INODE *, daddr_t, time_t));
-int	 lfs_lookup __P((struct vnode *, struct nameidata *, struct proc *));
-int	 lfs_mount __P((struct mount *, char *, caddr_t, struct nameidata *,
-	     struct proc *));
-int	 lfs_root __P((struct mount *, struct vnode **));
+daddr_t	 lfs_itod __P((LFS *, ino_t));
+int	 lfs_itrunc __P((INODE *, u_long, int));
+int	 lfs_iupdat
+	     __P((struct inode *, struct timeval *, struct timeval *, int));
+int	 lfs_makeinode __P((int, struct nameidata *, struct inode **));
+int	 lfs_mount
+	     __P((MOUNT *, char *, caddr_t, struct nameidata *, struct proc *));
+int	 lfs_mountroot __P((void));
+int	 lfs_read __P((struct vnode *, struct uio *, int, struct ucred *));
 int	 lfs_segwrite __P((MOUNT *, int));
-int	 lfs_statfs __P((struct mount *, struct statfs *, struct proc *));
-int	 lfs_sync __P((struct mount *, int));
-int	 lfs_unmount __P((struct mount *, int, struct proc *));
-int	 lfs_vcreate __P((struct mount *, ino_t, struct vnode **));
+int	 lfs_statfs __P((MOUNT *, struct statfs *, struct proc *));
+int	 lfs_sync __P((MOUNT *, int));
+int	 lfs_unmount __P((MOUNT *, int, struct proc *));
+int	 lfs_vcreate __P((MOUNT *, ino_t, VNODE **));
+int	 lfs_write __P((struct vnode *, struct uio *, int, struct ucred *));
 
 #ifdef DEBUG
-void	dump_super __P((LFS *));
-void	dump_dinode __P((struct dinode *));
-void	lfs_print_inumber __P((struct vnode *));
-void	lfs_spin __P((void));
+void	lfs_dump_dinode __P((DINODE *));
+void	lfs_dump_super __P((LFS *));
+int	lfs_umountdebug __P((struct mount *));
+int	lfs_vinvalbuf __P((struct vnode *));
 #endif
+__END_DECLS
 
 extern struct vnodeops lfs_vnodeops;
