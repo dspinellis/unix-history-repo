b9877daf8118ce043b9dc2238a30d658501a7e20
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-11-02 00:39:52 -0800

    LFS version 1; add LFS file system type
    SCCS-vsn: sys/kern/vfs_conf.c 7.4

--- usr/src/sys/kern/vfs_conf.c
+++ usr/src/sys/kern/vfs_conf.c
@@ -4,18 +4,18 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)vfs_conf.c	7.3 (Berkeley) %G%
+ *	@(#)vfs_conf.c	7.4 (Berkeley) %G%
  */
 
-#include "param.h"
-#include "mount.h"
+#include <sys/param.h>
+#include <sys/mount.h>
+#include <ufs/ffs/ffs_extern.h>
 
 /*
  * This specifies the filesystem used to mount the root.
  * This specification should be done by /etc/config.
  */
-extern int ufs_mountroot();
-int (*mountroot)() = ufs_mountroot;
+int (*mountroot)() = ffs_mountroot;
 
 /*
  * These define the root filesystem and device.
@@ -29,26 +29,35 @@ struct vnode *rootdir;
  */
 extern	struct vfsops ufs_vfsops;
 
-#ifdef NFS
-extern	struct vfsops nfs_vfsops;
+#ifdef LFS
+extern	struct vfsops lfs_vfsops;
 #endif
 
 #ifdef MFS
 extern	struct vfsops mfs_vfsops;
 #endif
 
+#ifdef NFS
+extern	struct vfsops nfs_vfsops;
+#endif
+
 struct vfsops *vfssw[] = {
-	(struct vfsops *)0,	/* 0 = MOUNT_NONE */
+	NULL,			/* 0 = MOUNT_NONE */
 	&ufs_vfsops,		/* 1 = MOUNT_UFS */
 #ifdef NFS
 	&nfs_vfsops,		/* 2 = MOUNT_NFS */
 #else
-	(struct vfsops *)0,
+	NULL,
 #endif
 #ifdef MFS
 	&mfs_vfsops,		/* 3 = MOUNT_MFS */
 #else
-	(struct vfsops *)0,
+	NULL,
 #endif
-	(struct vfsops *)0,	/* 4 = MOUNT_PC */
+	NULL,			/* 4 = MOUNT_PC */
+#ifdef LFS
+	&lfs_vfsops,		/* 5 = MOUNT_LFS */
+#else
+	NULL,
+#endif
 };
