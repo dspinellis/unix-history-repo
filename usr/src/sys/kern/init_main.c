06b4346f951f11b56d9d9fe7d14f248e1b4c3608
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-11-02 00:34:23 -0800

    LFS version 1; move ufs down a level
    SCCS-vsn: sys/kern/init_main.c 7.42

--- usr/src/sys/kern/init_main.c
+++ usr/src/sys/kern/init_main.c
@@ -2,7 +2,7 @@
  * Copyright (c) 1982, 1986 Regents of the University of California.
  * All rights reserved.  The Berkeley software License Agreement
  * specifies the terms and conditions for redistribution.
- *	@(#)init_main.c	7.41 (Berkeley) %G%
+ *	@(#)init_main.c	7.42 (Berkeley) %G%
  */
 
 #include "param.h"
