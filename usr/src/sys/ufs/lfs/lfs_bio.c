8275d06dcfde49a7362ee08637a39f3478398e54
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-11-02 01:37:02 -0800

    minor cleanup/explanation UFS/FFS split for LFS version 1; lfs moves down one level
    SCCS-vsn: sys/ufs/lfs/lfs_bio.c 7.1

--- usr/src/sys/ufs/lfs/lfs_bio.c
+++ usr/src/sys/ufs/lfs/lfs_bio.c
@@ -1,10 +1,10 @@
 /*
- * Copyright (c) 1982, 1986, 1989 Regents of the University of California.
+ * Copyright (c) 1991 Regents of the University of California.
  * All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)lfs_bio.c	5.4 (Berkeley) %G%
+ *	@(#)lfs_bio.c	7.1 (Berkeley) %G%
  */
 
 #include <sys/param.h>
@@ -12,23 +12,21 @@
 #include <sys/buf.h>
 #include <sys/resourcevar.h>
 
-#include <lfs/lfs.h>
-#include <lfs/lfs_extern.h>
+#include <ufs/lfs/lfs.h>
+#include <ufs/lfs/lfs_extern.h>
 
 /*
  * LFS version of bawrite, bdwrite, bwrite.  Set the delayed write flag and
  * use reassignbuf to move the buffer from the clean list to the dirty one,
  * then unlock the buffer.
+ *
+ * XXX No accounting for the cost of the write is currently done.
+ * XXX This is almost certainly wrong for synchronous operations, i.e. NFS.
  */
 int
 lfs_bwrite(bp)
 	register BUF *bp;
 {
-#ifdef DO_ACCOUNTING
-	Not included as this gets called from lots of places where the
-	current proc structure is probably wrong.  Ignore for now.
-	curproc->p_stats->p_ru.ru_oublock++;	/* XXX: no one paid yet */
-#endif
 	bp->b_flags &= ~(B_READ | B_DONE | B_ERROR);
 	bp->b_flags |= B_WRITE | B_DELWRI;
 	reassignbuf(bp, bp->b_vp);		/* XXX: do this inline? */
