2ca571ca8f4580e7f2549fb739f19d76c580597d
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-11-05 06:33:13 -0800

    break FFS into UFS/FFS/LFS; doesn't need -I/sys any more
    SCCS-vsn: usr.bin/fstat/Makefile 5.6
    SCCS-vsn: usr.bin/fstat/fstat.c 5.34

--- usr/src/usr.bin/fstat/fstat.c
+++ usr/src/usr.bin/fstat/fstat.c
@@ -12,7 +12,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)fstat.c	5.33 (Berkeley) %G%";
+static char sccsid[] = "@(#)fstat.c	5.34 (Berkeley) %G%";
 #endif /* not lint */
 
 /*
@@ -42,8 +42,8 @@ static char sccsid[] = "@(#)fstat.c	5.33 (Berkeley) %G%";
 #define NFS
 #include <sys/file.h>
 #include <sys/mount.h>
-#include <ufs/quota.h>
-#include <ufs/inode.h>
+#include <ufs/ufs/quota.h>
+#include <ufs/ufs/inode.h>
 #include <nfs/nfsv2.h>
 #include <nfs/nfs.h>
 #include <nfs/nfsnode.h>
