f43cf8d0570978bcd25ffd9980811ff2222ceed4
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-11-10 22:24:35 -0800

    Jim Oldroyd's version of quiz. date and time created 91/11/10 14:24:35 by bostic
    SCCS-vsn: games/quiz/quiz.h 5.1

---
+++ usr/src/games/quiz/quiz.h
@@ -0,0 +1,34 @@
+/*-
+ * Copyright (c) 1991 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to Berkeley by
+ * Jim R. Oldroyd at The Instruction Set.
+ *
+ * %sccs.include.redist.c%
+ *
+ *	@(#)quiz.h	5.1 (Berkeley) %G%
+ */
+
+#define	TRUE		1
+#define	FALSE		0
+
+/* Length of compiled regexp machine; increase if not big enough. */
+#define	RXP_LINE_SZ	8192
+
+/* Maximum line length for data files. */
+#define	LINE_SZ		1024
+
+/* Linked list for holding index and data file information. */
+typedef struct qentry {
+	struct qentry *q_next;		/* next one */
+	char	*q_text;		/* category text string from file */
+	int	 q_asked;		/* TRUE if question's been asked */
+	int	 q_answered;		/* TRUE if question's been answered */
+} QE;
+
+extern char rxperr[];
+
+int	 rxp_compile __P((char *));
+char	*rxp_expand __P((void));
+int	 rxp_match __P((char *));
