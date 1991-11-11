cbd9193bb9d2795d6f156c2d727c96fe79b7955b
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-11-10 22:24:27 -0800

    Jim Oldroyd's version of quiz
    SCCS-vsn: games/quiz/Makefile 5.1
    SCCS-vsn: games/quiz/pathnames.h 5.2
    SCCS-vsn: games/quiz/quiz.6 6.3
    SCCS-vsn: games/quiz/quiz.c 5.1

--- usr/src/games/quiz/pathnames.h
+++ usr/src/games/quiz/pathnames.h
@@ -4,7 +4,8 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)pathnames.h	5.1 (Berkeley) %G%
+ *	@(#)pathnames.h	5.2 (Berkeley) %G%
  */
 
-#define	_PATH_INDEX	"/usr/share/games/quiz.db/index"
+#define	_PATH_PAGER	"/usr/bin/more"
+#define	_PATH_QUIZIDX	"/usr/share/games/quiz.db/index"
