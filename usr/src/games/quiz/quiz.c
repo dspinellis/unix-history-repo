e3fb19f2e3e7124984439b63d8d951fe59480378
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-11-10 22:24:27 -0800

    Jim Oldroyd's version of quiz
    SCCS-vsn: games/quiz/Makefile 5.1
    SCCS-vsn: games/quiz/pathnames.h 5.2
    SCCS-vsn: games/quiz/quiz.6 6.3
    SCCS-vsn: games/quiz/quiz.c 5.1

--- usr/src/games/quiz/quiz.c
+++ usr/src/games/quiz/quiz.c
@@ -1,491 +1,353 @@
 /*-
- * %sccs.include.proprietary.c%
+ * Copyright (c) 1991 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to Berkeley by
+ * Jim R. Oldroyd at The Instruction Set.
+ *
+ * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)quiz.c	4.8 (Berkeley) %G%";
+char copyright[] =
+"@(#) Copyright (c) 1991 The Regents of the University of California.\n\
+ All rights reserved.\n";
 #endif /* not lint */
 
+#ifndef lint
+static char sccsid[] = "@(#)quiz.c	5.1 (Berkeley) %G%";
+#endif /* not lint */
+
+#include <sys/types.h>
+#include <errno.h>
+#include <time.h>
 #include <stdio.h>
-#include <signal.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include "quiz.h"
 #include "pathnames.h"
 
-#define NF 10
-#define NL 300
-#define NC 200
-#define SL 100
-#define NA 10
+static QE qlist;
+static int catone, cattwo, tflag;
+static u_int qsize;
 
-int tflag;
-int xx[NL];
-char score[NL];
-int rights;
-int wrongs;
-int guesses;
-FILE *input;
-int nl = 0;
-int na = NA;
-int inc;
-int ptr = 0;
-int nc = 0;
-char line[150];
-char response[100];
-char *tmp[NF];
-int select[NF];
+char	*appdstr __P((char *, char *));
+void	 downcase __P((char *));
+void	 err __P((const char *, ...));
+void	 get_cats __P((char *, char *));
+void	 get_file __P((char *));
+char	*next_cat __P((char *));
+void	 quiz __P((void));
+void	 score __P((u_int, u_int, u_int));
+void	 show_index __P((void));
+void	 usage __P((void));
 
-readline()
+int
+main(argc, argv)
+	int argc;
+	char *argv[];
 {
 	register int ch;
-	char *t;
-loop:
-	for(t=line;(ch=getc(input))!=-1;t++) {
-		*t = ch;
-		nc++;
-		if(*t==' '&&(t==line||t[-1]==' '))
-			t--;
-		if(*t=='\n') {
-			if(t[-1]=='\\')		/*inexact test*/
-				continue;
-			while(t>line&&t[-1]==' ')
-				*--t = '\n';
-			*++t = 0;
-			return(1);
-		}
-		if(t-line>=NC) {
-			printf("Too hard for me\n");
-			do {
-				if ((ch = getc(input)) == EOF)
-					return(0);
-			} while(ch!='\n');
-			*line = '\n';
-			goto loop;
-		}
-	}
-	return(0);
-}
+	char *indexfile;
 
-char *eu;
-char *ev;
-cmp(u,v)
-char *u,*v;
-{
-	int x;
-	eu = u;
-	ev = v;
-	x = disj(1);
-	if(x!=1)
-		return(x);
-	return(eat(1,0));
-}
-
-disj(s)
-{
-	int t, x;
-	char *u;
-	u = eu;
-	t = 0;
-	for(;;) {
-		x = string(s);
-		if(x>1)
-			return(x);
-		switch(*ev) {
-		case 0:
-		case ']':
-		case '}':
-			return(t|x&s);
-		case '|':
-			ev++;
-			t |= s;
-			s = 0;
-			continue;
-		}
-		if(s) eu = u;
-		if(string(0)>1)
-			return(2);
-		switch(*ev) {
-		case 0:
-		case ']':
-			return(0);
-		case '}':
-			return(1);
-		case '|':
-			ev++;
-			continue;
-		default:
-			return(2);
-		}
-	}
-}
-
-string(s)
-{
-	int x;
-	for(;;) {
-		switch(*ev) {
-		case 0:
-		case '|':
-		case ']':
-		case '}':
-			return(1);
-		case '\\':
-			ev++;
-			if(*ev==0)
-				return(2);
-			if(*ev=='\n') {
-				ev++;
-				continue;
-			}
-		default:
-			if(eat(s,*ev)==1)
-				continue;
-			return(0);
-		case '[':
-			ev++;
-			x = disj(s);
-			if(*ev!=']' || x>1)
-				return(2);
-			ev++;
-			if(s==0)
-				continue;
-			if(x==0)
-				return(0);
-			continue;
-		case '{':
-			ev++;
-			x = disj(s);
-			if(*ev!='}'||x>1)
-				return(2);
-			ev++;
-			continue;
-		}
-	}
-}
-
-eat(s,c)
-char c;
-{
-	if(*ev!=c)
-		return(2);
-	if(s==0) {
-		ev++;
-		return(1);
-	}
-	if(fold(*eu)!=fold(c))
-		return(0);
-	eu++;
-	ev++;
-	return(1);
-}
-
-fold(c)
-char c;
-{
-	if(c<'A'||c>'Z')
-		return(c);
-	return(c|040);
-}
-
-publish(t)
-char *t;
-{
-	ev = t;
-	pub1(1);
-}
-
-pub1(s)
-{
-	for(;;ev++){
-		switch(*ev) {
-		case '|':
-			s = 0;
-			ev;
-			continue;
-		case ']':
-		case '}':
-		case 0:
-			return;
-		case '[':
-		case '{':
-			ev++;
-			pub1(s);
-			ev;
-			continue;
-		case '\\':
-			if(*++ev=='\n')
-				continue;
-		default:
-			if(s)
-				putchar(*ev);
-		}
-	}
-}
-
-segment(u,w)
-char *u, *w[];
-{
-	char *s;
-	int i;
-	char *t;
-	s = u;
-	for(i=0;i<NF;i++) {
-		u = s;
-		t = w[i];
-		while(*s!=':'&&*s!='\n'&&s-u<SL) {
-			if(*s=='\\')  {
-				if(s[1] == '\n') {
-					s += 2;
-					continue;
-				}
-				*t++ = *s++;
-			}
-			*t++ = *s++;
-		}
-
-		while(*s!=':'&&*s!='\n')
-			s++;
-		*t = 0;
-		if(*s++=='\n') {
-			return(i+1);
-		}
-	}
-	printf("Too many facts about one thing\n");
-}
-
-perm(u,m,v,n,p)
-int p[];
-char *u[], *v[];
-{
-	int i, j;
-	int x;
-	for(i=0;i<m;i++) {
-		for(j=0;j<n;j++) {
-			x = cmp(u[i],v[j]);
-			if(x>1) badinfo();
-			if(x==0)
-				continue;
-			p[i] = j;
-			goto uloop;
-		}
-		return(0);
-uloop:		;
-	}
-	return(1);
-}
-
-find(u,m)
-char *u[];
-{
-	int n;
-	while(readline()){
-		n = segment(line,tmp);
-		if(perm(u,m,tmp+1,n-1,select))
-			return(1);
-	}
-	return(0);
-}
-
-readindex()
-{
-	xx[0] = nc = 0;
-	while(readline()) {
-		xx[++nl] = nc;
-		if(nl>=NL) {
-			printf("I've forgotten some of it;\n");
-			printf("I remember %d items.\n", nl);
-			break;
-		}
-	}
-}
-
-talloc()
-{
-	int i;
-	char *malloc();
-
-	for(i=0;i<NF;i++)
-		tmp[i] = malloc(SL);
-}
-
-void done();
-
-main(argc,argv)
-char *argv[];
-{
-	register j;
-	int i;
-	int x;
-	int z;
-	char *info;
-	int tvec[2];
-	char *t;
-	int count;
-	info = _PATH_INDEX;
-	time(tvec);
-	inc = tvec[1]&077774|01;
-loop:
-	if(argc>1&&*argv[1]=='-') {
-		switch(argv[1][1]) {
+	indexfile = _PATH_QUIZIDX;
+	while ((ch = getopt(argc, argv, "i:t")) != EOF)
+		switch(ch) {
 		case 'i':
-			if(argc>2) 
-				info = argv[2];
-			argc -= 2;
-			argv += 2;
-			goto loop;
+			indexfile = optarg;
+			break;
 		case 't':
 			tflag = 1;
-			argc--;
-			argv++;
-			goto loop;
-		}
-	}
-	input = fopen(info,"r");
-	if(input==NULL) {
-		printf("No info\n");
-		exit(0);
-	}
-	talloc();
-	if(argc<=2)
-		instruct(info);
-	signal(SIGINT,done);
-	argv[argc] = 0;
-	if(find(&argv[1],argc-1)==0)
-		dunno();
-	fclose(input);
-	input = fopen(tmp[0],"r");
-	if(input==NULL)
-		dunno();
-	readindex();
-	if(!tflag || na>nl)
-		na = nl;
-	setvbuf(stdout, (char *)NULL, _IONBF, 0);
-	for(;;) {
-		i = next();
-		fseek(input,xx[i]+0L,0);
-		z = xx[i+1]-xx[i];
-		for(j=0;j<z;j++)
-			line[j] = getc(input);
-		segment(line,tmp);
-		if(*tmp[select[0]] == '\0' || *tmp[select[1]] == '\0') {
-			score[i] = 1;
-			continue;
-		}
-		publish(tmp[select[0]]);
-		printf("\n");
-		for(count=0;;count++) {
-			if(query(response)==0) {
-				publish(tmp[select[1]]);
-				printf("\n");
-				if(count==0) wrongs++;
-				score[i] = tflag?-1:1;
-				break;
-			}
-			x = cmp(response,tmp[select[1]]);
-			if(x>1) badinfo();
-			if(x==1) {
-				printf("Right!\n");
-				if(count==0) rights++;
-				if(++score[i]>=1 && na<nl)
-					na++;
-				break;
-			}
-			printf("What?\n");
-			if(count==0) wrongs++;
-			score[i] = tflag?-1:1;
-		}
-		guesses += count;
-	}
-}
-
-query(r)
-char *r;
-{
-	char *t;
-	for(t=r;;t++) {
-		if(read(0,t,1)==0)
-			done();
-		if(*t==' '&&(t==r||t[-1]==' '))
-			t--;
-		if(*t=='\n') {
-			while(t>r&&t[-1]==' ')
-				*--t = '\n';
 			break;
+		case '?':
+		default:
+			usage();
 		}
-	}
-	*t = 0;
-	return(t-r);
-}
+	argc -= optind;
+	argv += optind;
 
-next()
-{
-	int flag;
-	inc = inc*3125&077777;
-	ptr = (inc>>2)%na;
-	flag = 0;
-	while(score[ptr]>0)
-		if(++ptr>=na) {
-			ptr = 0;
-			if(flag) done();
-			flag = 1;
-		}
-	return(ptr);
+	switch(argc) {
+	case 0:
+		get_file(indexfile);
+		show_index();
+		break;
+	case 2:
+		get_file(indexfile);
+		get_cats(argv[0], argv[1]);
+		quiz();
+		break;
+	default:
+		usage();
+	}
+	exit(0);
 }
 
 void
-done()
+get_file(file)
+	char *file;
 {
-	if (rights + wrongs) {
-		printf("\nRights %d, wrongs %d, ", rights, wrongs);
-		if (guesses)
-			printf("extra guesses %d, ", guesses);
-		printf("score %d%%\n",100 * rights / (rights + wrongs));
-	}
-	exit(0);
-}
-instruct(info)
-	char *info;
-{
-	char *t;
-	int i, n;
-	printf("Subjects:\n\n");
-	while(readline()) {
-		printf("-");
-		n = segment(line,tmp);
-		for(i=1;i<n;i++) {
-			printf(" ");
-			publish(tmp[i]);
+	register FILE *fp;
+	register QE *qp;
+	size_t len;
+	char *lp;
+
+	if ((fp = fopen(file, "r")) == NULL)
+		err("%s: %s", file, strerror(errno));
+
+	/*
+	 * XXX
+	 * Should really free up space from any earlier read list
+	 * but there are no reverse pointers to do so with.
+	 */
+	qp = &qlist;
+	qsize = 0;
+	while ((lp = fgetline(fp, &len)) != NULL) {
+		if (qp->q_text && qp->q_text[strlen(qp->q_text) - 1] == '\\')
+			qp->q_text = appdstr(qp->q_text, lp);
+		else {
+			if ((qp->q_next = malloc(sizeof(QE))) == NULL)
+				err("%s", strerror(errno));
+			qp = qp->q_next;
+			if ((qp->q_text = strdup(lp)) == NULL)
+				err("%s", strerror(errno));
+			qp->q_asked = qp->q_answered = FALSE;
+			qp->q_next = NULL;
+			++qsize;
 		}
-		printf("\n");
 	}
-	printf("\n");
-	input = fopen(info,"r");
-	if(input==NULL)
-		abort();
-	readline();
-	segment(line,tmp);
-	printf("For example,\n");
-	printf("    quiz ");
-	publish(tmp[1]);
-	printf(" ");
-	publish(tmp[2]);
-	printf("\nasks you a ");
-	publish(tmp[1]);
-	printf(" and you answer the ");
-	publish(tmp[2]);
-	printf("\n    quiz ");
-	publish(tmp[2]);
-	printf(" ");
-	publish(tmp[1]);
-	printf("\nworks the other way around\n");
-	printf("\nType empty line to get correct answer.\n");
-	exit(0);
+	(void)fclose(fp);
 }
 
-badinfo(){
-	printf("Bad info %s\n",line);
-}
-
-dunno()
+void
+show_index()
 {
-	printf("I don't know about that\n");
-	exit(0);
+	register QE *qp;
+	register char *p, *s;
+	FILE *pf;
+
+	if ((pf = popen(_PATH_PAGER, "w")) == NULL)
+		err("%s: %s", _PATH_PAGER, strerror(errno));
+	(void)fprintf(pf, "Subjects:\n\n");
+	for (qp = qlist.q_next; qp; qp = qp->q_next) {
+		for (s = next_cat(qp->q_text); s; s = next_cat(s)) {
+			if (!rxp_compile(s))
+				err("%s", rxperr);
+			if (p = rxp_expand())
+				(void)fprintf(pf, "%s ", p);
+		}
+		(void)fprintf(pf, "\n");
+	}
+	(void)fprintf(pf, "\n%s\n%s\n%s\n",
+"For example, \"quiz victim killer\" prints a victim's name and you reply",
+"with the killer, and \"quiz killer victim\" works the other way around.",
+"Type an empty line to get the correct answer.");
+	(void)pclose(pf);
+}
+
+void
+get_cats(cat1, cat2)
+	char *cat1, *cat2;
+{
+	register QE *qp;
+	int i;
+	char *s;
+
+	downcase(cat1);
+	downcase(cat2);
+	for (qp = qlist.q_next; qp; qp = qp->q_next) {
+		s = next_cat(qp->q_text);
+		catone = cattwo = i = 0;
+		while (s) {
+			if (!rxp_compile(s))
+				err("%s", rxperr);
+			i++;
+			if (rxp_match(cat1))
+				catone = i;
+			if (rxp_match(cat2))
+				cattwo = i;
+			s = next_cat(s);
+		}
+		if (catone && cattwo && catone != cattwo) {
+			if (!rxp_compile(qp->q_text))
+				err("%s", rxperr);
+			get_file(rxp_expand());
+			return;
+		}
+	}
+	err("invalid categories");
+}
+
+void
+quiz()
+{
+	register QE *qp;
+	register int i;
+	u_int guesses, rights, wrongs;
+	int next;
+	char *s, *t, question[LINE_SZ];
+	char *answer;
+
+	srandom(time(NULL));
+	guesses = rights = wrongs = 0;
+	for (;;) {
+		if (qsize == 0)
+			break;
+		next = random() % qsize;
+		qp = qlist.q_next;
+		for (i = 0; i < next; i++)
+			qp = qp->q_next;
+		while (qp && qp->q_answered)
+			qp = qp->q_next;
+		if (!qp) {
+			qsize = next;
+			continue;
+		}
+		if (tflag && random() % 100 > 20) {
+			/* repeat questions in tutorial mode */
+			while (qp && (!qp->q_asked || qp->q_answered))
+				qp = qp->q_next;
+			if (!qp)
+				continue;
+		}
+		s = qp->q_text;
+		for (i = 0; i < catone - 1; i++)
+			s = next_cat(s);
+		if (!rxp_compile(s))
+			err("%s", rxperr);
+		t = rxp_expand();
+		if (!t || *t == '\0') {
+			qp->q_answered = TRUE;
+			continue;
+		}
+		(void)strcpy(question, t);
+		s = qp->q_text;
+		for (i = 0; i < cattwo - 1; i++)
+			s = next_cat(s);
+		if (!rxp_compile(s))
+			err("%s", rxperr);
+		t = rxp_expand();
+		if (!t || *t == '\0') {
+			qp->q_answered = TRUE;
+			continue;
+		}
+		qp->q_asked = TRUE;
+		(void)printf("%s?\n", question);
+		for (;; ++guesses) {
+			if ((answer = fgetline(stdin, NULL)) == NULL) {
+				score(rights, wrongs, guesses);
+				exit(0);
+			}
+			downcase(answer);
+			if (rxp_match(answer)) {
+				(void)printf("Right!\n");
+				++rights;
+				qp->q_answered = TRUE;
+				break;
+			}
+			if (*answer == '\0') {
+				(void)printf("%s\n", t);
+				++wrongs;
+				if (!tflag)
+					qp->q_answered = TRUE;
+				break;
+			}
+			(void)printf("What?\n");
+		}
+	}
+	score(rights, wrongs, guesses);
+}
+
+char *
+next_cat(s)
+	register char *	s;
+{
+	for (;;)
+		switch (*s++) {
+		case '\0':
+			return (NULL);
+		case '\\':
+			break;
+		case ':':
+			return (s);
+		}
+	/* NOTREACHED */
+}
+
+char *
+appdstr(s, tp)
+	char *s;
+	register char *tp;
+{
+	register char *mp, *sp;
+	register int ch;
+	char *m;
+
+	if ((m = malloc(strlen(sp) + strlen(tp) + 1)) == NULL)
+		err("%s", strerror(errno));
+	for (mp = m, sp = s; *mp++ = *sp++;);
+
+	if (*(mp - 1) == '\\')
+		--mp;
+	while ((ch = *mp++ = *tp++) && ch != '\n');
+	*mp = '\0';
+
+	free(s);
+	return (m);
+}
+
+void
+score(r, w, g)
+	u_int r, w, g;
+{
+	(void)printf("Rights %d, wrongs %d,", r, w);
+	if (g)
+		(void)printf(" extra guesses %d,", g);
+	(void)printf(" score %d%%\n", (r + w + g) ? r * 100 / (r + w + g) : 0);
+}
+
+void
+downcase(p)
+	register char *p;
+{
+	register int ch;
+
+	for (; ch = *p; ++p)
+		if (isascii(ch) && isupper(ch))
+			*p = tolower(ch);
+}
+
+void
+usage()
+{
+	(void)fprintf(stderr, "quiz [-t] [-i file] category1 category2\n");
+	exit(1);
+}
+
+#if __STDC__
+#include <stdarg.h>
+#else
+#include <varargs.h>
+#endif
+
+void
+#if __STDC__
+err(const char *fmt, ...)
+#else
+err(fmt, va_alist)
+	char *fmt;
+        va_dcl
+#endif
+{
+	va_list ap;
+#if __STDC__
+	va_start(ap, fmt);
+#else
+	va_start(ap);
+#endif
+	(void)fprintf(stderr, "quiz: ");
+	(void)vfprintf(stderr, fmt, ap);
+	va_end(ap);
+	(void)fprintf(stderr, "\n");
+	exit(1);
 }
