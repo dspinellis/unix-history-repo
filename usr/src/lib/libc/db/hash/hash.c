12e1c1bd09b5a128c705b1cad40fb3cdfe592a41
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-11-12 18:34:52 -0800

    unnecessary assignment in BYTEORDER code
    SCCS-vsn: lib/libc/db/hash/hash.c 5.20

--- usr/src/lib/libc/db/hash/hash.c
+++ usr/src/lib/libc/db/hash/hash.c
@@ -9,7 +9,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)hash.c	5.19 (Berkeley) %G%";
+static char sccsid[] = "@(#)hash.c	5.20 (Berkeley) %G%";
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/param.h>
