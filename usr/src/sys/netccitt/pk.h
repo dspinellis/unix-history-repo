b7fe3a12b5b3cc245a6a48075ac831e20fd55913
Keith Sklower <sklower@ucbvax.Berkeley.EDU>
1991-11-13 18:48:39 -0800

    stupid bug in extracting LCN, fix by dkhusema@uni-erlangen.de
    SCCS-vsn: sys/netccitt/pk.h 7.9

--- usr/src/sys/netccitt/pk.h
+++ usr/src/sys/netccitt/pk.h
@@ -9,7 +9,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)pk.h	7.8 (Berkeley) %G%
+ *	@(#)pk.h	7.9 (Berkeley) %G%
  */
 
 /*
@@ -114,9 +114,9 @@ struct data_packet {
 #define PS(xp)		(((struct data_packet *)&xp -> packet_type)->ps)
 #define MBIT(xp)	(((struct data_packet *)&xp -> packet_type)->m_bit)
 #define LCN(xp)		(xp -> logical_channel_number + \
-	(xp -> lc_group_number ? (xp -> lc_group_number >> 8) : 0))
+	(xp -> lc_group_number ? (xp -> lc_group_number << 8) : 0))
 #define SET_LCN(xp, lcn) ((xp -> logical_channel_number = lcn), \
-	(xp -> lc_group_number = lcn > 255 ? lcn >> 8 : 0))
+	(xp -> lc_group_number = (lcn > 255 ? (lcn >> 8) : 0)))
 
 struct mbuf *pk_template ();
 
