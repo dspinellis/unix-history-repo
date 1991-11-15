1e20d1346abb905efb94a6d995379d9b705f57d6
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-11-14 18:22:21 -0800

    mv ld to /usr/old/bin
    SCCS-vsn: old/pcc/cc/pathnames.h 5.7

--- usr/src/old/pcc/cc/pathnames.h
+++ usr/src/old/pcc/cc/pathnames.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)pathnames.h	5.6 (Berkeley) %G%
+ *	@(#)pathnames.h	5.7 (Berkeley) %G%
  */
 
 #include <paths.h>
@@ -15,7 +15,7 @@
 #define	_PATH_CPP	"/usr/old/bin/cpp"
 #define	_PATH_CRT0	"/usr/lib/crt0.o"
 #define	_PATH_GCRT0	"/usr/lib/gcrt0.o"
-#define	_PATH_LD	"/usr/bin/ld"
+#define	_PATH_LD	"/usr/old/bin/ld"
 #define	_PATH_MCRT0	"/usr/lib/mcrt0.o"
 #define	_PATH_SCCOM	"/usr/old/libexec/sccom"
 #define	_PATH_USRNEW	"/usr/new"
