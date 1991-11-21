a6e3ff0f93fc5f77e6f8fdb558ad04c145b40e32
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1991-11-21 01:47:06 -0800

    delete unneeded seg.h
    SCCS-vsn: sys/hp300/hp300/trap.c 7.17

--- usr/src/sys/hp300/hp300/trap.c
+++ usr/src/sys/hp300/hp300/trap.c
@@ -11,13 +11,12 @@
  *
  * from: Utah $Hdr: trap.c 1.32 91/04/06$
  *
- *	@(#)trap.c	7.16 (Berkeley) %G%
+ *	@(#)trap.c	7.17 (Berkeley) %G%
  */
 
 #include "param.h"
 #include "systm.h"
 #include "proc.h"
-#include "seg.h"
 #include "acct.h"
 #include "kernel.h"
 #include "signalvar.h"
