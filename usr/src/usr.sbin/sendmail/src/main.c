540d12b0aa30e232652e7c2b067b6fe1587f85c4
Eric Allman <eric@ucbvax.Berkeley.EDU>
1991-11-22 21:54:54 -0800

    don't complain if sendmail.fc doesn't exist (this is not an error)
    SCCS-vsn: usr.sbin/sendmail/src/main.c 5.37

--- usr/src/usr.sbin/sendmail/src/main.c
+++ usr/src/usr.sbin/sendmail/src/main.c
@@ -13,7 +13,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)main.c	5.36 (Berkeley) %G%";
+static char sccsid[] = "@(#)main.c	5.37 (Berkeley) %G%";
 #endif /* not lint */
 
 #define	_DEFINE
