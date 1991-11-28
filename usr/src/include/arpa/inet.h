ea61956a0722e35c14fd609ef5ea6c1d9fe89794
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-11-27 19:18:53 -0800

    add inet_aton prototype
    SCCS-vsn: include/arpa/inet.h 5.8

--- usr/src/include/arpa/inet.h
+++ usr/src/include/arpa/inet.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)inet.h	5.7 (Berkeley) %G%
+ *	@(#)inet.h	5.8 (Berkeley) %G%
  */
 
 #ifndef _INET_H_
@@ -15,12 +15,13 @@
 #include <sys/cdefs.h>
 
 __BEGIN_DECLS
-extern unsigned long	 inet_addr __P((const char *));
-extern unsigned long	 inet_lnaof __P((struct in_addr));
-extern struct in_addr	 inet_makeaddr __P((u_long , u_long));
-extern unsigned long	 inet_netof __P((struct in_addr));
-extern unsigned long	 inet_network __P((const char *));
-extern char		*inet_ntoa __P((struct in_addr));
+unsigned long	 inet_addr __P((const char *));
+unsigned long	 inet_aton __P((const char *, struct in_addr *));
+unsigned long	 inet_lnaof __P((struct in_addr));
+struct in_addr	 inet_makeaddr __P((u_long , u_long));
+unsigned long	 inet_netof __P((struct in_addr));
+unsigned long	 inet_network __P((const char *));
+char		*inet_ntoa __P((struct in_addr));
 __END_DECLS
 
 #endif /* !_INET_H_ */
