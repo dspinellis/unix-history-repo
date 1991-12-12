644672c9146005340dfb3fcafcfaeccf86f91d88
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-12-12 01:56:19 -0800

    version 2; add cksum, make keywords flexible, new output format
    SCCS-vsn: usr.sbin/mtree/Makefile 5.5
    SCCS-vsn: usr.sbin/mtree/compare.c 5.8
    SCCS-vsn: usr.sbin/mtree/create.c 5.17
    SCCS-vsn: usr.sbin/mtree/mtree.8 5.11
    SCCS-vsn: usr.sbin/mtree/mtree.c 5.9
    SCCS-vsn: usr.sbin/mtree/mtree.h 5.8
    SCCS-vsn: usr.sbin/mtree/spec.c 5.15
    SCCS-vsn: usr.sbin/mtree/verify.c 5.10

--- usr/src/usr.sbin/mtree/verify.c
+++ usr/src/usr.sbin/mtree/verify.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)verify.c	5.9 (Berkeley) %G%";
+static char sccsid[] = "@(#)verify.c	5.10 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/param.h>
@@ -17,53 +17,61 @@ static char sccsid[] = "@(#)verify.c	5.9 (Berkeley) %G%";
 #include <errno.h>
 #include <stdio.h>
 #include "mtree.h"
+#include "extern.h"
 
-extern NODE *root;
+extern int crc_total, ftsoptions;
+extern int dflag, eflag, rflag, sflag, uflag;
+extern char fullpath[MAXPATHLEN];
 
+static NODE *root;
 static char path[MAXPATHLEN];
 
+static void	miss __P((NODE *, char *));
+static int	vwalk __P((void));
+
+int
 verify()
 {
-	vwalk();
+	int rval;
+
+	root = spec();
+	rval = vwalk();
 	miss(root, path);
+	return (rval);
 }
 
+static int
 vwalk()
 {
-	extern int ftsoptions, dflag, eflag, rflag;
 	register FTS *t;
 	register FTSENT *p;
 	register NODE *ep, *level;
+	int ftsdepth, specdepth, rval;
 	char *argv[2];
-	int ftsdepth = 0, specdepth = 0;
 
 	argv[0] = ".";
-	argv[1] = (char *)NULL;
-	if (!(t = fts_open(argv, ftsoptions, (int (*)())NULL))) {
-		(void)fprintf(stderr,
-		    "mtree: fts_open: %s.\n", strerror(errno));
-		exit(1);
-	}
+	argv[1] = NULL;
+	if ((t = fts_open(argv, ftsoptions, NULL)) == NULL)
+		err("fts_open: %s", strerror(errno));
 	level = root;
+	ftsdepth = specdepth = rval = 0;
 	while (p = fts_read(t)) {
 		switch(p->fts_info) {
 		case FTS_D:
-			if (!strcmp(p->fts_name, "."))
-				continue;
-			ftsdepth++; 
+			++ftsdepth; 
 			break;
 		case FTS_DP:
-			ftsdepth--; 
+			--ftsdepth; 
 			if (specdepth > ftsdepth) {
 				for (level = level->parent; level->prev;
 				      level = level->prev);  
-				specdepth--;
+				--specdepth;
 			}
 			continue;
 		case FTS_DNR:
 		case FTS_ERR:
 		case FTS_NS:
-			(void)fprintf(stderr, "mtree: %s: %s.\n",
+			(void)fprintf(stderr, "mtree: %s: %s\n",
 			    RP(p), strerror(errno));
 			continue;
 		default:
@@ -80,11 +88,12 @@ vwalk()
 					(void)fts_set(t, p, FTS_SKIP);
 					continue;
 				}
-				compare(ep->name, ep, p);
+				if (compare(ep->name, ep, p))
+					rval = MISMATCHEXIT;
 				if (ep->child && ep->type == F_DIR &&
 				    p->fts_info == FTS_D) {
 					level = ep->child;
-					specdepth++;
+					++specdepth;
 				}
 				break;
 			}
@@ -105,13 +114,17 @@ vwalk()
 		(void)fts_set(t, p, FTS_SKIP);
 	}
 	(void)fts_close(t);
+	if (sflag)
+		(void)fprintf(stderr,
+		    "mtree: %s checksum: %lu\n", fullpath, crc_total);
+	return (rval);
 }
 
+static void
 miss(p, tail)
 	register NODE *p;
 	register char *tail;
 {
-	extern int dflag, uflag;
 	register int create;
 	register char *tp;
 
@@ -128,9 +141,12 @@ miss(p, tail)
 
 		create = 0;
 		if (!(p->flags & F_VISIT) && uflag)
-#define	MINBITS	(F_GROUP|F_MODE|F_OWNER)
-			if ((p->flags & MINBITS) != MINBITS)
-				(void)printf(" (not created -- group, mode or owner not specified)");
+			if (!(p->flags & (F_UID | F_UNAME)))
+			    (void)printf(" (not created: user not specified)");
+			else if (!(p->flags & (F_GID | F_GNAME)))
+			    (void)printf(" (not created: group not specified)");
+			else if (!(p->flags & F_MODE))
+			    (void)printf(" (not created: mode not specified)");
 			else if (mkdir(path, S_IRWXU))
 				(void)printf(" (not created: %s)",
 				    strerror(errno));
@@ -150,7 +166,7 @@ miss(p, tail)
 		if (!create)
 			continue;
 		if (chown(path, p->st_uid, p->st_gid)) {
-			(void)printf("%s: owner/group/mode not modified: %s\n",
+			(void)printf("%s: user/group/mode not modified: %s\n",
 			    path, strerror(errno));
 			continue;
 		}
