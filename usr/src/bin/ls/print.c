c45b29478eb70fad9fcfcd3a695cadb807358380
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1991-12-13 01:39:00 -0800

    missing static declaration
    SCCS-vsn: bin/ls/print.c 5.27

--- usr/src/bin/ls/print.c
+++ usr/src/bin/ls/print.c
@@ -9,7 +9,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)print.c	5.26 (Berkeley) %G%";
+static char sccsid[] = "@(#)print.c	5.27 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/param.h>
@@ -224,7 +224,7 @@ printlink(name)
 	(void)printf(" -> %s", path);
 }
 
-char *
+static char *
 flags_from_fid(flags)
 	long flags;
 {
