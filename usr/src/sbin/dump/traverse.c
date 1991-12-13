27996a83d2bcdd3eb0e6b61e0edf89032d184535
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1991-12-13 01:26:27 -0800

    ignore files with NODUMP flag set
    SCCS-vsn: sbin/dump/traverse.c 5.16

--- usr/src/sbin/dump/traverse.c
+++ usr/src/sbin/dump/traverse.c
@@ -6,14 +6,13 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)traverse.c	5.15 (Berkeley) %G%";
+static char sccsid[] = "@(#)traverse.c	5.16 (Berkeley) %G%";
 #endif /* not lint */
 
 #ifdef sunos
 #include <stdio.h>
 #include <ctype.h>
 #include <sys/param.h>
-#include <sys/stat.h>
 #include <sys/time.h>
 #include <sys/dir.h>
 #include <sys/vnode.h>
@@ -25,6 +24,7 @@ static char sccsid[] = "@(#)traverse.c	5.15 (Berkeley) %G%";
 #include <ufs/ufs/dinode.h>
 #include <ufs/ffs/fs.h>
 #endif
+#include <sys/stat.h>
 #include <protocols/dumprestore.h>
 #ifdef __STDC__
 #include <unistd.h>
@@ -99,8 +99,9 @@ mapfiles(maxino, tapesize)
 		SETINO(ino, usedinomap);
 		if (mode == IFDIR)
 			SETINO(ino, dumpdirmap);
-		if (dp->di_mtime >= spcl.c_ddate ||
-		    dp->di_ctime >= spcl.c_ddate) {
+		if ((dp->di_mtime >= spcl.c_ddate ||
+		    dp->di_ctime >= spcl.c_ddate) &&
+		    (dp->di_flags & NODUMP) != NODUMP) {
 			SETINO(ino, dumpinomap);
 			if (mode != IFREG && mode != IFDIR && mode != IFLNK) {
 				*tapesize += 1;
