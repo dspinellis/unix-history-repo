d0d19e9563ac2f0ff03fc587b22a486e486b2e2c
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-12-13 20:41:24 -0800

    no normative chnage, trivial cleanup
    SCCS-vsn: sys/ufs/lfs/lfs_bio.c 7.3

--- usr/src/sys/ufs/lfs/lfs_bio.c
+++ usr/src/sys/ufs/lfs/lfs_bio.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)lfs_bio.c	7.2 (Berkeley) %G%
+ *	@(#)lfs_bio.c	7.3 (Berkeley) %G%
  */
 
 #include <sys/param.h>
@@ -23,7 +23,6 @@ lfs_bwrite(bp)
 printf("lfs_bwrite\n");
 #endif
 	/*
-	 *
 	 * LFS version of bawrite, bdwrite, bwrite.  Set the delayed write
 	 * flag and use reassignbuf to move the buffer from the clean list
 	 * to the dirty one, then unlock the buffer.  Note, we set the
@@ -35,8 +34,8 @@ printf("lfs_bwrite\n");
 	 * No accounting for the cost of the write is currently done.
 	 * This is almost certainly wrong for synchronous operations, i.e. NFS.
 	 */
-	bp->b_flags &= ~(B_READ | B_DONE | B_ERROR);
 	bp->b_flags |= B_DELWRI | B_LOCKED;
+	bp->b_flags &= ~(B_READ | B_DONE | B_ERROR);
 	reassignbuf(bp, bp->b_vp);
 	brelse(bp);
 	return (0);
