ba7b1677218781a0e56f56e791b6b1aa0d25f8b5
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-12-15 02:46:24 -0800

    inline lfs_iset in lfs_segment.c
    SCCS-vsn: sys/ufs/lfs/lfs_extern.h 7.5

--- usr/src/sys/ufs/lfs/lfs_extern.h
+++ usr/src/sys/ufs/lfs/lfs_extern.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)lfs_extern.h	7.4 (Berkeley) %G%
+ *	@(#)lfs_extern.h	7.5 (Berkeley) %G%
  */
 
 struct fid;
@@ -27,7 +27,6 @@ int	 lfs_fsync
 DINODE	*lfs_ifind __P((struct lfs *, ino_t, void *));
 int	 lfs_inactive __P((VNODE *, struct proc *));
 int	 lfs_init __P((void));
-void	 lfs_iset __P((INODE *, daddr_t, time_t));
 daddr_t	 lfs_itod __P((struct lfs *, ino_t));
 int	 lfs_makeinode __P((int, struct nameidata *, struct inode **));
 int	 lfs_mount
