ba5bad1bb61abaa0fde45ebb5c64510be6060d7c
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1991-12-16 23:52:34 -0800

    free dynamically allocated part
    SCCS-vsn: sys/ufs/mfs/mfs_vnops.c 7.26

--- usr/src/sys/ufs/mfs/mfs_vnops.c
+++ usr/src/sys/ufs/mfs/mfs_vnops.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)mfs_vnops.c	7.25 (Berkeley) %G%
+ *	@(#)mfs_vnops.c	7.26 (Berkeley) %G%
  */
 
 #include <sys/param.h>
@@ -15,6 +15,7 @@
 #include <sys/buf.h>
 #include <sys/map.h>
 #include <sys/vnode.h>
+#include <sys/malloc.h>
 
 #include <machine/vmparam.h>
 #include <machine/mtpr.h>
@@ -315,6 +316,19 @@ mfs_inactive(vp, p)
 
 	if (VTOMFS(vp)->mfs_buflist != (struct buf *)(-1))
 		panic("mfs_inactive: not inactive");
+	return (0);
+}
+
+/*
+ * Reclaim a memory filesystem devvp so that it can be reused.
+ */
+int
+mfs_reclaim(vp)
+	register struct vnode *vp;
+{
+
+	FREE(vp->v_data, M_MFSNODE);
+	vp->v_data = NULL;
 	return (0);
 }
 
