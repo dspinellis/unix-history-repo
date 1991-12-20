d535a4bfa61f33b6245bb5049c9971396e78838d
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1991-12-19 21:00:33 -0800

    select di_size based on _NOQUAD support
    SCCS-vsn: sys/ufs/ffs/dinode.h 7.13
    SCCS-vsn: sys/ufs/ufs/dinode.h 7.13

--- usr/src/sys/ufs/ufs/dinode.h
+++ usr/src/sys/ufs/ufs/dinode.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)dinode.h	7.12 (Berkeley) %G%
+ *	@(#)dinode.h	7.13 (Berkeley) %G%
  */
 
 /*
@@ -28,7 +28,7 @@ struct dinode {
 	short	di_nlink;	/*  2: number of links to file */
 	uid_t	di_uid;		/*  4: owner's user id */
 	gid_t	di_gid;		/*  6: owner's group id */
-	u_quad	di_qsize;	/*  8: number of bytes in file */
+	u_quad_t di_qsize;	/*  8: number of bytes in file */
 	time_t	di_atime;	/* 16: time last accessed */
 	long	di_atspare;
 	time_t	di_mtime;	/* 24: time last modified */
@@ -43,14 +43,15 @@ struct dinode {
 	long	di_spare[4];	/* 112: reserved, currently unused */
 };
 
-#ifdef BYTE_ORDER
-#if BYTE_ORDER == LITTLE_ENDIAN || defined(tahoe) /* ugh! -- must be fixed */
-#define	di_size		di_qsize.val[0]
-#else /* BYTE_ORDER == BIG_ENDIAN */
-#define	di_size		di_qsize.val[1]
-#endif
+#ifdef _NOQUAD
+#define di_size	di_qsize.val[_QUAD_LOWWORD]
 #else
-#define	di_size		BYTE_ORDER_UNDEFINED
+#define di_size	di_qsize
+#endif
+
+#if defined(tahoe) /* ugh! -- must be fixed */
+#undef di_size
+#define	di_size		di_qsize.val[0]
 #endif
 
 #define	di_rdev		di_db[0]
