d1944850d6dd5d7c7ab8503d671084dfe46d3736
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1991-12-31 23:04:00 -0800

    sync no longer needs single-threading code, it's done by the system call
    SCCS-vsn: sys/ufs/lfs/lfs_vfsops.c 7.70

--- usr/src/sys/ufs/lfs/lfs_vfsops.c
+++ usr/src/sys/ufs/lfs/lfs_vfsops.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)lfs_vfsops.c	7.69 (Berkeley) %G%
+ *	@(#)lfs_vfsops.c	7.70 (Berkeley) %G%
  */
 
 #include <sys/param.h>
@@ -380,7 +380,6 @@ lfs_sync(mp, waitfor)
 	int waitfor;
 {
 	extern int crashandburn, syncprt;
-	static int sync_lock, sync_want;
 	int error;
 
 #ifdef VERBOSE
@@ -391,17 +390,6 @@ lfs_sync(mp, waitfor)
 	if (crashandburn)
 		return (0);
 #endif
-	/*
-	 * Meta data blocks are only marked dirty, not busy, so LFS syncs
-	 * must be single threaded.
-	 */
-	while (sync_lock) {
-		sync_want = 1;
-		if (error = tsleep(&sync_lock, PLOCK | PCATCH, "lfs sync", 0))
-			return (error);
-	}
-	sync_lock = 1;
-
 	if (syncprt)
 		ufs_bufstats();
 
@@ -410,11 +398,6 @@ lfs_sync(mp, waitfor)
 #ifdef QUOTA
 	qsync(mp);
 #endif
-	sync_lock = 0;
-	if (sync_want) {
-		sync_want = 0;
-		wakeup(&sync_lock);
-	}
 	return (error);
 }
 
