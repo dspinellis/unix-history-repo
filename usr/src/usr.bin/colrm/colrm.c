c052eddd7a1e5051049c848768a9c31318e4faee
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-01-08 20:41:35 -0800

    welcome to the 90's (or at least the 80's); complete rewrite.
    SCCS-vsn: usr.bin/colrm/colrm.c 5.5
    SCCS-vsn: usr.bin/colrm/colrm.1 6.7

--- usr/src/usr.bin/colrm/colrm.c
+++ usr/src/usr.bin/colrm/colrm.c
@@ -1,5 +1,5 @@
-/*
- * Copyright (c) 1980 Regents of the University of California.
+/*-
+ * Copyright (c) 1991 The Regents of the University of California.
  * All rights reserved.
  *
  * %sccs.include.redist.c%
@@ -7,94 +7,135 @@
 
 #ifndef lint
 char copyright[] =
-"@(#) Copyright (c) 1980 Regents of the University of California.\n\
+"@(#) Copyright (c) 1991 The Regents of the University of California.\n\
  All rights reserved.\n";
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)colrm.c	5.4 (Berkeley) %G%";
+static char sccsid[] = "@(#)colrm.c	5.5 (Berkeley) %G%";
 #endif /* not lint */
 
+#include <sys/types.h>
+#include <limits.h>
+#include <errno.h>
 #include <stdio.h>
-/*
-COLRM removes unwanted columns from a file
-	Jeff Schriebman  UC Berkeley 11-74
-*/
+#include <stdlib.h>
+#include <string.h>
 
+#define	TAB	8
 
-main(argc,argv)
-char **argv;
+void err __P((const char *, ...));
+void check __P((FILE *));
+void usage __P((void));
+
+int
+main(argc, argv)
+	int argc;
+	char *argv[];
 {
-	register c, ct, first, last;
+	register u_long column, start, stop;
+	register int ch;
+	char *p;
 
-	first = 0;
-	last = 0;
-	if (argc > 1)
-		first = getn(*++argv);
-	if (argc > 2)
-		last = getn(*++argv);
-
-start:
-	ct = 0;
-loop1:
-	c = getc(stdin);
-	if (feof(stdin))
-		goto fin;
-	if (c == '\t')
-		ct = (ct + 8) & ~7;
-	else if (c == '\b')
-		ct = ct ? ct - 1 : 0;
-	else
-		ct++;
-	if (c == '\n') {
-		putc(c, stdout);
-		goto start;
-	}
-	if (!first || ct < first) {
-		putc(c, stdout);
-		goto loop1;
-	}
-
-/* Loop getting rid of characters */
-	while (!last || ct < last) {
-		c = getc(stdin);
-		if (feof(stdin))
-			goto fin;
-		if (c == '\n') {
-			putc(c, stdout);
-			goto start;
+	while ((ch = getopt(argc, argv, "")) != EOF)
+		switch(ch) {
+		case '?':
+		default:
+			usage();
 		}
-		if (c == '\t')
-			ct = (ct + 8) & ~7;
-		else if (c == '\b')
-			ct = ct ? ct - 1 : 0;
-		else
-			ct++;
+	argc -= optind;
+	argv += optind;
+
+	start = stop = 0;
+	switch(argc) {
+	case 2:
+		stop = strtol(argv[1], &p, 10);
+		if (stop <= 0 || *p)
+			err("illegal column -- %s", argv[1]);
+		/* FALLTHROUGH */
+	case 1:
+		start = strtol(argv[0], &p, 10);
+		if (start <= 0 || *p)
+			err("illegal column -- %s", argv[0]);
+		break;
+	case 0:
+		break;
+	default:
+		usage();
 	}
 
-/* Output last of the line */
-	for (;;) {
-		c = getc(stdin);
-		if (feof(stdin))
+	if (stop && start > stop)
+		err("illegal start and stop columns");
+
+	for (column = 0;;) {
+		switch (ch = getchar()) {
+		case EOF:
+			check(stdin);
 			break;
-		putc(c, stdout);
-		if (c == '\n')
-			goto start;
+		case '\b':
+			if (column)
+				--column;
+			break;
+		case '\n':
+			column = 0;
+			break;
+		case '\t':
+			column = (column + TAB) & ~(TAB - 1);
+			break;
+		default:
+			++column;
+			break;
+		}
+
+		if ((!start || column < start || stop && column > stop) &&
+		    putchar(ch) == EOF)
+			check(stdout);
 	}
-fin:
-	fflush(stdout);
-	exit(0);
 }
 
-getn(ap)
-char *ap;
+void
+check(stream)
+	FILE *stream;
 {
-	register int n,c;
-	register char *p;
+	if (feof(stream))
+		exit(0);
+	if (ferror(stream))
+		err("%s: %s",
+		    stream == stdin ? "stdin" : "stdout", strerror(errno));
+}
 
-	p = ap;
-	n = 0;
-	while ((c = *p++) >= '0' && c <= '9')
-		n = n*10 + c - '0';
-	return(n);
+void
+usage()
+{
+	(void)fprintf(stderr, "usage: colrm [start [stop]]\n");
+	exit(1);
+}
+
+#if __STDC__
+#include <stdarg.h>
+#else
+#include <varargs.h>
+#endif
+
+void
+#if __STDC__
+err(const char *fmt, ...)
+#else
+err(fmt, va_alist)
+	char *fmt;
+        va_dcl
+#endif
+{
+	va_list ap;
+#if __STDC__
+	va_start(ap, fmt);
+#else
+	va_start(ap);
+#endif
+	(void)fprintf(stderr, "colrm: ");
+	(void)vfprintf(stderr, fmt, ap);
+	va_end(ap);
+	(void)fprintf(stderr, "\n");
+	exit(1);
+	/* NOTREACHED */
 }
