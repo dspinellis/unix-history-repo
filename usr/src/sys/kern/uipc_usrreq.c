4d22b3c49e27a21c2c7a0db0049b72b20c8b5722
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1992-01-14 20:25:09 -0800

    lease check when binding unix domain sockets
    SCCS-vsn: sys/kern/uipc_usrreq.c 7.28

--- usr/src/sys/kern/uipc_usrreq.c
+++ usr/src/sys/kern/uipc_usrreq.c
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 1982, 1986 Regents of the University of California.
  *
- *	@(#)uipc_usrreq.c	7.27 (Berkeley) %G%
+ *	@(#)uipc_usrreq.c	7.28 (Berkeley) %G%
  */
 
 #include "param.h"
