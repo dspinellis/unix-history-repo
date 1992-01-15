3604a380acf4f923bc2b4c76a937137a5a0efa49
Marc Teitelbaum <marc@ucbvax.Berkeley.EDU>
1992-01-14 20:11:54 -0800

    account for context switches
    SCCS-vsn: sys/sys/ktrace.h 7.5

--- usr/src/sys/sys/ktrace.h
+++ usr/src/sys/sys/ktrace.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)ktrace.h	7.4 (Berkeley) %G%
+ *	@(#)ktrace.h	7.5 (Berkeley) %G%
  */
 
 /*
@@ -34,7 +34,8 @@ struct ktr_header {
 /*
  * Test for kernel trace point
  */
-#define KTRPOINT(p, type)	((p)->p_traceflag & (1<<(type)))
+#define KTRPOINT(p, type)	\
+	(((p)->p_traceflag & ((1<<(type))|KTRFAC_ACTIVE)) == (1<<(type)))
 
 /*
  * ktrace record types
@@ -93,6 +94,15 @@ struct ktr_psig {
 };
 
 /*
+ * KTR_CSW - trace context switches
+ */
+#define KTR_CSW		6
+struct ktr_csw {
+	int	out;	/* 1 if switch out, 0 if switch in */
+	int	user;	/* 1 if usermode (ivcsw), 0 if kernel (vcsw) */
+};
+
+/*
  * kernel trace points (in p_traceflag)
  */
 #define KTRFAC_MASK	0x00ffffff
@@ -101,11 +111,13 @@ struct ktr_psig {
 #define KTRFAC_NAMEI	(1<<KTR_NAMEI)
 #define KTRFAC_GENIO	(1<<KTR_GENIO)
 #define	KTRFAC_PSIG	(1<<KTR_PSIG)
+#define KTRFAC_CSW	(1<<KTR_CSW)
 /*
  * trace flags (also in p_traceflags)
  */
 #define KTRFAC_ROOT	0x80000000	/* root set this trace */
 #define KTRFAC_INHERIT	0x40000000	/* pass trace flags to children */
+#define KTRFAC_ACTIVE	0x20000000	/* ktrace logging in progress, ignore */
 
 #ifndef	KERNEL
 
