98ef172f13d3c16260b94f9f71c23a65743852e2
Elan Amir <elan@ucbvax.Berkeley.EDU>
1992-01-30 00:23:01 -0800

    Changed print routines to conform to new ls(1) inplementation using fts(3) routines.
    SCCS-vsn: bin/ls/print.c 5.28

--- usr/src/bin/ls/print.c
+++ usr/src/bin/ls/print.c
@@ -9,11 +9,12 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)print.c	5.27 (Berkeley) %G%";
+static char sccsid[] = "@(#)print.c	5.28 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/param.h>
 #include <sys/stat.h>
+#include <fts.h>
 #include <time.h>
 #include <errno.h>
 #include <grp.h>
@@ -23,68 +24,81 @@ static char sccsid[] = "@(#)print.c	5.27 (Berkeley) %G%";
 #include <stdio.h>
 #include "ls.h"
 #include "extern.h"
-
-static int	printaname __P((LS *));
-static void	printlink __P((char *));
-static void	printtime __P((time_t));
-static int	printtype __P((u_int));
+	
 static char	*flags_from_fid __P((long));
+static int	 printaname __P((FTSENT *));
+static void	 printlink __P((FTSENT *));
+static void	 printtime __P((time_t));
+static int	 printtype __P((u_int));
 
+#define	IS_NOPRINT(e)	((e)->fts_number == NO_PRINT)
+
+/* ARGUSED */
 void
-printscol(stats, num)
-	register LS *stats;
-	register int num;
+printscol(dlist, entries, btotal, maxlen)
+	FTSENT *dlist;
+	int entries, maxlen;
+	u_long btotal;
 {
-	for (; num--; ++stats) {
-		(void)printaname(stats);
-		(void)putchar('\n');
-	}
+	register FTSENT *p;
+
+	for (p = dlist; p; p = p->fts_link)
+		if (!IS_NOPRINT(p)) {
+			(void)printaname(p);
+			(void)putchar('\n');
+		}
 }
 
+/* ARGUSED */
 void
-printlong(stats, num)
-	LS *stats;
-	register int num;
+printlong(dlist, entries, btotal, maxlen)
+	FTSENT *dlist;
+	int entries, maxlen;
+	u_long btotal;
 {
+	register FTSENT *p;
+	register struct stat *sp;
 	char modep[15], *user_from_uid(), *group_from_gid();
-
-	if (f_total)
-		(void)printf("total %lu\n", f_kblocks ?
-		    howmany(stats[0].lstat.st_btotal, 2) :
-		    stats[0].lstat.st_btotal);
-	for (; num--; ++stats) {
+	
+	
+	if (dlist->fts_level != FTS_ROOTLEVEL && (f_longform || f_size))
+		(void)printf("total %lu\n",
+		    f_kblocks ? howmany(btotal, 2) : btotal);
+	for (p = dlist; p; p = p->fts_link) {
+		if (IS_NOPRINT(p))
+			continue;
+		sp = p->fts_statp;
 		if (f_inode)
-			(void)printf("%6lu ", stats->lstat.st_ino);
+			(void)printf("%6lu ", sp->st_ino);
 		if (f_size)
 			(void)printf("%4ld ", f_kblocks ?
-			    howmany(stats->lstat.st_blocks, 2) :
-			    stats->lstat.st_blocks);
-		(void)strmode(stats->lstat.st_mode, modep);
-		(void)printf("%s %3u %-*s ", modep, stats->lstat.st_nlink,
-		    UT_NAMESIZE, user_from_uid(stats->lstat.st_uid, 0));
+			    howmany(sp->st_blocks, 2) : sp->st_blocks);
+		(void)strmode(sp->st_mode, modep);
+		(void)printf("%s %3u %-*s ", modep, sp->st_nlink,
+		    UT_NAMESIZE, user_from_uid(sp->st_uid, 0));
 		if (f_group)
-			(void)printf("%-*s ", UT_NAMESIZE,
-			    group_from_gid(stats->lstat.st_gid, 0));
+			(void)printf("%-*s ",
+			    UT_NAMESIZE, group_from_gid(sp->st_gid, 0));
 		if (f_flags)
-			(void)printf("%-*s ", FLAGSWIDTH,
-			    flags_from_fid(stats->lstat.st_flags));
-		if (S_ISCHR(stats->lstat.st_mode) ||
-		    S_ISBLK(stats->lstat.st_mode))
-			(void)printf("%3d, %3d ", major(stats->lstat.st_rdev),
-			    minor(stats->lstat.st_rdev));
+			(void)printf("%-*s ",
+			    FLAGSWIDTH, flags_from_fid(sp->st_flags));
+		if (S_ISCHR(sp->st_mode) ||
+		    S_ISBLK(sp->st_mode))
+			(void)printf("%3d, %3d ",
+			    major(sp->st_rdev), minor(sp->st_rdev));
 		else
-			(void)printf("%8ld ", stats->lstat.st_size);
+			(void)printf("%8ld ", sp->st_size);
 		if (f_accesstime)
-			printtime(stats->lstat.st_atime);
+			printtime(sp->st_atime);
 		else if (f_statustime)
-			printtime(stats->lstat.st_ctime);
+			printtime(sp->st_ctime);
 		else
-			printtime(stats->lstat.st_mtime);
-		(void)printf("%s", stats->name);
+			printtime(sp->st_mtime);
+		(void)printf("%s", p->fts_name);
 		if (f_type)
-			(void)printtype(stats->lstat.st_mode);
-		if (S_ISLNK(stats->lstat.st_mode))
-			printlink(stats->name);
+			(void)printtype(sp->st_mode);
+		if (S_ISLNK(sp->st_mode))
+			printlink(p);
 		(void)putchar('\n');
 	}
 }
@@ -92,15 +106,34 @@ printlong(stats, num)
 #define	TAB	8
 
 void
-printcol(stats, num)
-	LS *stats;
-	int num;
+printcol(dlist, entries, btotal, maxlen)
+	FTSENT *dlist;
+	u_long btotal;
+	int entries, maxlen;
 {
 	extern int termwidth;
-	register int base, chcnt, cnt, col, colwidth;
+	static FTSENT **array;
+	static int lastentries = -1;
+	register FTSENT *p;
+	register int base, chcnt, cnt, col, colwidth, num;
 	int endcol, numcols, numrows, row;
 
-	colwidth = stats[0].lstat.st_maxlen;
+	/*
+	 * Have to do random access in the linked list -- build a table
+	 * of pointers.
+	 */
+	if (entries > lastentries) {
+		lastentries = entries;
+		if ((array = (FTSENT **) realloc(array, entries * sizeof(FTSENT *))) == NULL) {
+			err(0, "%s", strerror(errno));
+			printscol(dlist, entries, btotal, maxlen);
+		}
+	}
+	for (p = dlist, num = 0; p; p = p->fts_link)
+		if (p->fts_number != NO_PRINT)
+			array[num++] = p;
+
+	colwidth = maxlen;
 	if (f_inode)
 		colwidth += 6;
 	if (f_size)
@@ -110,7 +143,7 @@ printcol(stats, num)
 
 	colwidth = (colwidth + TAB) & ~(TAB - 1);
 	if (termwidth < 2 * colwidth) {
-		printscol(stats, num);
+		printscol(dlist, 0, 0, 0);
 		return;
 	}
 
@@ -119,14 +152,13 @@ printcol(stats, num)
 	if (num % numcols)
 		++numrows;
 
-	if (f_size && f_total)
-		(void)printf("total %lu\n", f_kblocks ?
-		    howmany(stats[0].lstat.st_btotal, 2) :
-		    stats[0].lstat.st_btotal);
+	if (dlist->fts_level != FTS_ROOTLEVEL && (f_longform || f_size))
+		(void)printf("total %lu\n",
+		    f_kblocks ? howmany(btotal, 2) : btotal);
 	for (row = 0; row < numrows; ++row) {
 		endcol = colwidth;
 		for (base = row, chcnt = col = 0; col < numcols; ++col) {
-			chcnt += printaname(stats + base);
+			chcnt += printaname(array[base]);
 			if ((base += numrows) >= num)
 				break;
 			while ((cnt = (chcnt + TAB & ~(TAB - 1))) <= endcol) {
@@ -135,29 +167,31 @@ printcol(stats, num)
 			}
 			endcol += colwidth;
 		}
-		putchar('\n');
+		(void)putchar('\n');
 	}
 }
 
 /*
  * print [inode] [size] name
- * return # of characters printed, no trailing characters
+ * return # of characters printed, no trailing characters.
  */
 static int
-printaname(lp)
-	LS *lp;
+printaname(p)
+	register FTSENT *p;
 {
+	struct stat *sp;
 	int chcnt;
 
+	sp = p->fts_statp;
 	chcnt = 0;
 	if (f_inode)
-		chcnt += printf("%5lu ", lp->lstat.st_ino);
+		chcnt += printf("%5lu ", sp->st_ino);
 	if (f_size)
 		chcnt += printf("%4ld ", f_kblocks ?
-		    howmany(lp->lstat.st_blocks, 2) : lp->lstat.st_blocks);
-	chcnt += printf("%s", lp->name);
+		    howmany(sp->st_blocks, 2) : sp->st_blocks);
+	chcnt += printf("%s", p->fts_name);
 	if (f_type)
-		chcnt += printtype(lp->lstat.st_mode);
+		chcnt += printtype(sp->st_mode);
 	return (chcnt);
 }
 
@@ -210,12 +244,14 @@ printtype(mode)
 }
 
 static void
-printlink(name)
-	char *name;
+printlink(p)
+	FTSENT *p;
 {
 	int lnklen;
-	char path[MAXPATHLEN + 1];
-
+	char name[MAXPATHLEN + 1], path[MAXPATHLEN + 1];
+	
+	(void)snprintf(name,
+	    MAXPATHLEN, "%s/%s", p->fts_parent->fts_name,  p->fts_name);
 	if ((lnklen = readlink(name, path, MAXPATHLEN)) == -1) {
 		(void)fprintf(stderr, "\nls: %s: %s\n", name, strerror(errno));
 		return;
