fc02ecd2bab0581891336286014089e1cf95dab7
Elan Amir <elan@ucbvax.Berkeley.EDU>
1992-01-30 00:14:56 -0800

    New version of using fts(3) routines.  This version is more robust /
    SCCS-vsn: bin/ls/ls.c 5.53

--- usr/src/bin/ls/ls.c
+++ usr/src/bin/ls/ls.c
@@ -15,14 +15,14 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)ls.c	5.52 (Berkeley) %G%";
+static char sccsid[] = "@(#)ls.c	5.53 (Berkeley) %G%";
 #endif /* not lint */
 
-#include <sys/param.h>
+#include <sys/types.h>	
 #include <sys/stat.h>
 #include <sys/ioctl.h>
-#include <dirent.h>
 #include <unistd.h>
+#include <fts.h>
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
@@ -30,10 +30,12 @@ static char sccsid[] = "@(#)ls.c	5.52 (Berkeley) %G%";
 #include "ls.h"
 #include "extern.h"
 
-static void	displaydir __P((LS *, int));
-static void	doargs __P((int, char **));
-static void	subdir __P((LS *));
-static int	tabdir __P((LS *, LS **, char **));
+static void	display __P((int, FTSENT *, FTSENT *));
+static int	mastercmp __P((const FTSENT **, const FTSENT **));
+static void	traverse __P((int, char **, int));
+
+static void (*printfcn) __P((FTSENT *, int, u_long, int));
+static int (*sortfcn) __P((const FTSENT *, const FTSENT *));
 
 int termwidth = 80;		/* default terminal width */
 
@@ -49,7 +51,7 @@ int f_listalldot;		/* list . and .. as well */
 int f_listdir;			/* list actual directory, not contents */
 int f_listdot;			/* list files beginning with . */
 int f_longform;			/* long listing format */
-int f_needstat;			/* if need to stat files */
+int f_needstat;                 /* need stat(2) information */
 int f_newline;			/* if precede with newline */
 int f_nonprint;			/* show unprintables as ? */
 int f_nosort;			/* don't sort output */
@@ -61,21 +63,18 @@ int f_size;			/* list size in short listing */
 int f_statustime;		/* use time of last mode change */
 int f_dirname;			/* if precede with directory name */
 int f_timesort;			/* sort by time vice name */
-int f_total;			/* if precede with "total" line */
 int f_type;			/* add type character for non-regular files */
 
-void (*printfcn)();
-int (*statfcn)(), (*sortfcn)();
-
+void
 main(argc, argv)
 	int argc;
-	char **argv;
+	char *argv[];
 {
 	struct winsize win;
-	int ch;
+	int ch, fts_options;
 	char *p;
 
-	/* terminal defaults to -Cq, non-terminal defaults to -1 */
+	/* Terminal defaults to -Cq, non-terminal defaults to -1. */
 	if (isatty(1)) {
 		f_nonprint = 1;
 		if (ioctl(1, TIOCGWINSZ, &win) == -1 || !win.ws_col) {
@@ -88,15 +87,16 @@ main(argc, argv)
 	} else
 		f_singlecol = 1;
 
-	/* root is -A automatically */
+	/* Root is -A automatically. */
 	if (!getuid())
 		f_listdot = 1;
 
+	fts_options = 0;
 	while ((ch = getopt(argc, argv, "1ACFLRTacdfgikloqrstu")) != EOF) {
 		switch (ch) {
 		/*
-		 * -1, -C and -l all override each other
-		 * so shell aliasing works right
+		 * The -1, -C and -l options all override each other so shell
+		 * aliasing works right.
 		 */
 		case '1':
 			f_singlecol = 1;
@@ -129,6 +129,7 @@ main(argc, argv)
 			f_recursive = 1;
 			break;
 		case 'a':
+			fts_options |= FTS_SEEDOT;
 			f_listalldot = 1;
 			/* FALLTHROUGH */
 		case 'A':
@@ -177,15 +178,17 @@ main(argc, argv)
 	argc -= optind;
 	argv += optind;
 
-	/* -d turns off -R */
+	/* The -d option turns off the -R option. */
 	if (f_listdir)
 		f_recursive = 0;
 
-	/* if need to stat files */
-	f_needstat = f_longform || f_recursive || f_timesort ||
-	    f_size || f_type;
+	/* If options require that the files be stat'ed. */
+	f_needstat =
+	    f_longform || f_timesort || f_size || f_type;
+	if (!f_needstat)
+		fts_options |= FTS_NOSTAT;
 
-	/* select a sort function */
+	/* Select a sort function. */
 	if (f_reversesort) {
 		if (!f_timesort)
 			sortfcn = revnamecmp;
@@ -193,7 +196,7 @@ main(argc, argv)
 			sortfcn = revacccmp;
 		else if (f_statustime)
 			sortfcn = revstatcmp;
-		else /* use modification time */
+		else /* Use modification time. */
 			sortfcn = revmodcmp;
 	} else {
 		if (!f_timesort)
@@ -202,11 +205,11 @@ main(argc, argv)
 			sortfcn = acccmp;
 		else if (f_statustime)
 			sortfcn = statcmp;
-		else /* use modification time */
+		else /* Use modification time. */
 			sortfcn = modcmp;
 	}
 
-	/* select a print function */
+	/* Select a print function. */
 	if (f_singlecol)
 		printfcn = printscol;
 	else if (f_longform)
@@ -214,259 +217,208 @@ main(argc, argv)
 	else
 		printfcn = printcol;
 
-	/* if -l, -d or -F, and not ignoring the link, use lstat() */
-	statfcn =
-	    (f_longform || f_listdir || f_type) && !f_ignorelink ? lstat : stat;
+	/* If -l or -F, and not ignoring the link, use lstat(). */
+	fts_options |= (f_longform || f_type) && !f_ignorelink ?
+	    FTS_PHYSICAL : FTS_LOGICAL;
 
 	if (argc)
-		doargs(argc, argv);
+		traverse(argc, argv, fts_options);
 	else {
 		static char dot[] = ".", *dotav[] = { dot, NULL };
-		doargs(1, dotav);
+		traverse(1, dotav, fts_options);
 	}
 	exit(0);
 }
 
-static char path[MAXPATHLEN + 1];
-static char *endofpath = path;
+#define	IS_DDOT(name) \
+	((name)[0] == '.' && ((name)[1] == '\0' || \
+	    ((name)[1] == '.' && (name)[2] == '\0')))
 
+/*
+ * Traverse() walks the logical directory structure specified by the argv list
+ * in the order specified by the mastercmp() comparison function.  During the
+ * traversal it passes linked lists of structures to display() which represent
+ * a superset (may be exact set) of the files to be displayed.
+ */
 static void
-doargs(argc, argv)
+traverse(argc, argv, options)
+	int argc, options;
+	char *argv[];
+{
+	register FTS *ftsp;
+	register FTSENT *p;
+	register char *name;
+	register int is_ddot;
+	
+	if ((ftsp =
+	    fts_open(argv, options, f_nosort ? NULL : mastercmp)) == NULL)
+		err(1, "fts_open: %s", strerror(errno));
+	display(argc, NULL, fts_children(ftsp));
+	if (f_listdir)
+		return;
+	while (p = fts_read(ftsp))
+		switch(p->fts_info) {
+		case FTS_DC:
+			err(0, "%s: directory causes a cycle", p->fts_name);
+			break;
+		case FTS_DNR:
+		case FTS_ERR:
+			err(0, "%s: %s",
+			    p->fts_name, strerror(p->fts_errno));
+			break;
+		case FTS_D:
+			name = p->fts_name;
+			is_ddot = IS_DDOT(name);
+			if (!is_ddot ||
+			    (is_ddot && p->fts_level == FTS_ROOTLEVEL)) {
+				if (name[0] == '.' && !f_listdot && !is_ddot)
+					break;
+				display(argc, p, fts_children(ftsp));
+				if (!f_recursive)
+					(void)fts_set(ftsp, p, FTS_SKIP);
+			}
+			break;
+		}
+	(void)fts_close(ftsp);
+}
+
+/*
+ * Display() takes a linked list of FTSENT structures and based on the flags
+ * set on the command line passes the list along with any other necessary
+ * information to the print function (printfcn()).  P always points to the
+ * parent directory of the display list.
+ */
+static void
+display(argc, p, list)
 	int argc;
-	char **argv;
+	register FTSENT *p;
+	FTSENT *list;
 {
-	register LS *dstatp, *rstatp;
-	register int cnt, dircnt, maxlen, regcnt;
-	LS *dstats, *rstats;
-	struct stat sb;
-	char top[MAXPATHLEN + 1];
-	u_long blocks;
-
+	register FTSENT *cur;
+	u_long btotal;
+	int entries, maxlen;
+	
 	/*
-	 * walk through the operands, building separate arrays of LS
-	 * structures for directory and non-directory files.
+	 * If list is NULL there are two possibilities: that the parent
+	 * directory p has no children, or that fts_children() returned an
+	 * error.  We ignore the error case since, it will be replicated
+	 * on the next call to fts_read() on the post-order visit to the
+	 * directory p, and will be signalled in traverse().
 	 */
-	dstats = rstats = NULL;
-	for (dircnt = regcnt = 0; *argv; ++argv) {
-		if (statfcn(*argv, &sb) &&
-		    (statfcn == lstat || lstat(*argv, &sb))) {
-			if (errno == ENOENT) {
-				warn("%s: %s", *argv, strerror(errno));
-				continue;
-			}
-			err("%s: %s", *argv, strerror(errno));
-		}
-		if (S_ISDIR(sb.st_mode) && !f_listdir) {
-			if (!dstats)
-				dstatp = dstats =
-				    emalloc((u_int)argc * (sizeof(LS)));
-			dstatp->name = *argv;
-			dstatp->lstat = sb;
-			++dstatp;
-			++dircnt;
-		}
-		else {
-			if (!rstats) {
-				rstatp = rstats =
-				    emalloc((u_int)argc * (sizeof(LS)));
-				blocks = 0;
-				maxlen = -1;
-			}
-			rstatp->name = *argv;
-			rstatp->lstat = sb;
-
-			/* save name length for -C format */
-			rstatp->len = strlen(*argv);
-
-			if (f_nonprint)
-				prcopy(*argv, *argv, rstatp->len);
-
-			/* calculate number of blocks if -l/-s formats */
-			if (f_longform || f_size)
-				blocks += sb.st_blocks;
-
-			/* save max length if -C format */
-			if (f_column && maxlen < rstatp->len)
-				maxlen = rstatp->len;
-
-			++rstatp;
-			++regcnt;
-		}
-	}
-	/* display regular files */
-	if (regcnt) {
-		rstats[0].lstat.st_btotal = blocks;
-		rstats[0].lstat.st_maxlen = maxlen;
-		displaydir(rstats, regcnt);
-		f_newline = f_dirname = 1;
-	}
-	/* display directories */
-	if (dircnt) {
-		register char *p;
-
-		f_total = 1;
-		if (dircnt > 1) {
-			(void)getwd(top);
-			qsort((char *)dstats, dircnt, sizeof(LS), sortfcn);
-			f_dirname = 1;
-		}
-		for (cnt = 0; cnt < dircnt; ++dstats) {
-			for (endofpath = path, p = dstats->name;
-			    *endofpath = *p++; ++endofpath);
-			subdir(dstats);
-			f_newline = 1;
-			if (++cnt < dircnt && chdir(top))
-				err("%s: %s", top, strerror(errno));
-		}
-	}
-}
-
-static void
-displaydir(stats, num)
-	LS *stats;
-	register int num;
-{
-	register char *p, *savedpath;
-	LS *lp;
-
-	if (num > 1 && !f_nosort) {
-		u_long save1, save2;
-
-		save1 = stats[0].lstat.st_btotal;
-		save2 = stats[0].lstat.st_maxlen;
-		qsort((char *)stats, num, sizeof(LS), sortfcn);
-		stats[0].lstat.st_btotal = save1;
-		stats[0].lstat.st_maxlen = save2;
-	}
-
-	printfcn(stats, num);
-
-	if (f_recursive) {
-		savedpath = endofpath;
-		for (lp = stats; num--; ++lp) {
-			if (!S_ISDIR(lp->lstat.st_mode))
-				continue;
-			p = lp->name;
-			if (p[0] == '.' && (!p[1] || p[1] == '.' && !p[2]))
-				continue;
-			if (endofpath != path && endofpath[-1] != '/')
-				*endofpath++ = '/';
-			for (; *endofpath = *p++; ++endofpath);
-			f_newline = f_dirname = f_total = 1;
-			subdir(lp);
-			*(endofpath = savedpath) = '\0';
-		}
-	}
-}
-
-static void
-subdir(lp)
-	LS *lp;
-{
-	LS *stats;
-	int num;
-	char *names;
-
-	if (f_newline)
-		(void)putchar('\n');
-	if (f_dirname)
-		(void)printf("%s:\n", path);
-
-	if (chdir(lp->name)) {
-		warn("%s: %s", lp->name, strerror(errno));
+	if (list == NULL && (p->fts_level != FTS_ROOTLEVEL || argc != 1)) {
+		(void)printf("\n%s:\n", p->fts_path);
 		return;
 	}
-	if (num = tabdir(lp, &stats, &names)) {
-		displaydir(stats, num);
-		free(stats);
-		free(names);
+
+	/*
+	 * P can only be NULL if list is the argv list.  This list must be
+	 * handled slightly differently due to the fact that there does not
+	 * exist a proper parent directory and different rules apply to it.
+	 */
+	btotal = 0;
+	maxlen = 0;
+	if (p == NULL)
+		for (cur = list, entries = 0; cur; cur = cur->fts_link) {
+			if (cur->fts_info == FTS_ERR ||
+			    cur->fts_errno == ENOENT ||
+			    cur->fts_info == FTS_NS && f_needstat) {
+				err(0, "%s: %s",
+				    cur->fts_name, strerror(cur->fts_errno));
+				cur->fts_number = NO_PRINT;
+				continue;
+			}
+			/*
+			 * If cur is a directory, do not print it out now.  Its
+			 * contents will be printed out when fts_read() reads
+			 * it.
+			 */
+			if (cur->fts_info == FTS_D && !f_listdir) {
+				cur->fts_number = NO_PRINT;
+				continue;
+			}
+			/*
+			 * If file is dot file and no -a or -A is set,
+			 * do not display.
+			 */
+			if (cur->fts_name[0] == '.' && !f_listdot) {
+				cur->fts_number = NO_PRINT;
+				continue;
+			}
+			++entries;
+			if (f_nonprint)
+				prcopy(cur->fts_name, cur->fts_name,
+				    cur->fts_namelen);
+			if (f_column && cur->fts_namelen > maxlen)
+				maxlen = cur->fts_namelen;
+		}
+	else {
+		if (p->fts_level != FTS_ROOTLEVEL || argc != 1)
+			(void)printf("\n%s:\n", p->fts_path);
+		
+		for (cur = list, entries = 0; cur; cur = cur->fts_link) {
+			if (cur->fts_info == FTS_ERR ||
+			    cur->fts_errno == ENOENT ||
+			    cur->fts_info == FTS_NS && f_needstat) {
+				err(0, "%s: %s",
+				    cur->fts_name, strerror(cur->fts_errno));
+				cur->fts_number = NO_PRINT;
+				continue;
+			}
+			/*
+			 * If file is dot file and no -a or -A is set,
+			 * do not display.
+			 */
+			if (cur->fts_name[0] == '.' && !f_listdot) {
+				cur->fts_number = NO_PRINT;
+				continue;
+			}
+			++entries;
+			if (f_nonprint)
+				prcopy(cur->fts_name, cur->fts_name,
+				       cur->fts_namelen);
+			if (f_column && cur->fts_namelen > maxlen)
+				maxlen = cur->fts_namelen;
+			
+			if (f_longform || f_size)
+				btotal += cur->fts_statp->st_blocks;
+		}
 	}
-	if (chdir(".."))
-		err("..: %s", strerror(errno));
+
+	if (entries)
+		printfcn(list, entries, btotal, maxlen);
 }
 
+/*
+ * Ordering for mastercmp:
+ * If ordering the argv (fts_level = FTS_ROOTLEVEL) return non-directories
+ * as larger than directories.  Within either group, use the sort function.
+ * All other levels use the sort function.  Error entries remain unsorted.
+ */
 static int
-tabdir(lp, s_stats, s_names)
-	LS *lp, **s_stats;
-	char **s_names;
+mastercmp(a, b)
+	const FTSENT **a, **b;
 {
-	register DIR *dirp;
-	register int cnt, maxentry, maxlen;
-	register char *p, *names;
-	struct dirent *dp;
-	LS *stats;
-	u_long blocks;
+	register int a_info, b_info;
 
-	if (!(dirp = opendir("."))) {
-		warn("%s: %s", lp->name, strerror(errno));
+	a_info = (*a)->fts_info;
+	if (a_info == FTS_ERR)
 		return (0);
-	}
-	blocks = maxentry = maxlen = 0;
-	stats = NULL;
-	for (cnt = 0; dp = readdir(dirp);) {
-		/* this does -A and -a */
-		p = dp->d_name;
-		if (p[0] == '.') {
-			if (!f_listdot)
-				continue;
-			if (!f_listalldot && (!p[1] || p[1] == '.' && !p[2]))
-				continue;
-		}
-		if (cnt == maxentry) {
-			if (!maxentry)
-				*s_names = names =
-				    emalloc((u_int)lp->lstat.st_size);
-#define	DEFNUM	256
-			maxentry += DEFNUM;
-			if (!(*s_stats = stats = realloc((char *)stats,
-			    (u_int)maxentry * sizeof(LS))))
-				err("%s", strerror(errno));
-		}
-		if (f_needstat && statfcn(dp->d_name, &stats[cnt].lstat) &&
-		    statfcn == stat && lstat(dp->d_name, &stats[cnt].lstat)) {
-			/*
-			 * don't exit -- this could be an NFS mount that has
-			 * gone away.  Flush stdout so the messages line up.
-			 */
-			(void)fflush(stdout);
-			warn("%s: %s", dp->d_name, strerror(errno));
-			continue;
-		}
-		stats[cnt].name = names;
+	b_info = (*b)->fts_info;
+	if (b_info == FTS_ERR)
+		return (0);
+	if (f_needstat && (a_info == FTS_NS || b_info == FTS_NS))
+		return (namecmp(*a, *b));
 
-		if (f_nonprint)
-			prcopy(dp->d_name, names, (int)dp->d_namlen);
+	if (a_info == b_info)  
+		return (sortfcn(*a, *b));
+
+	if ((*a)->fts_level == FTS_ROOTLEVEL)
+		if (a_info == FTS_D)        
+			return (1);
+		else if (b_info == FTS_D)
+			return (-1);
 		else
-			bcopy(dp->d_name, names, (int)dp->d_namlen);
-		names += dp->d_namlen;
-		*names++ = '\0';
-
-		/*
-		 * get the inode from the directory, so the -f flag
-		 * works right.
-		 */
-		stats[cnt].lstat.st_ino = dp->d_ino;
-
-		/* save name length for -C format */
-		stats[cnt].len = dp->d_namlen;
-
-		/* calculate number of blocks if -l/-s formats */
-		if (f_longform || f_size)
-			blocks += stats[cnt].lstat.st_blocks;
-
-		/* save max length if -C format */
-		if (f_column && maxlen < (int)dp->d_namlen)
-			maxlen = dp->d_namlen;
-		++cnt;
-	}
-	(void)closedir(dirp);
-
-	if (cnt) {
-		stats[0].lstat.st_btotal = blocks;
-		stats[0].lstat.st_maxlen = maxlen;
-	} else if (stats) {
-		free(stats);
-		free(names);
-	}
-	return (cnt);
+			return (sortfcn(*a, *b));
+	else
+		return (sortfcn(*a, *b));
 }
