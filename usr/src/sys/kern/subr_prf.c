0b60130a4541904cce3706a6fd7a56a9973fad03
Marc Teitelbaum <marc@ucbvax.Berkeley.EDU>
1992-02-06 03:32:08 -0800

    panic can use %r, does not return; fix comments describing %r; eliminate gcc warning
    SCCS-vsn: sys/kern/subr_prf.c 7.34

--- usr/src/sys/kern/subr_prf.c
+++ usr/src/sys/kern/subr_prf.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)subr_prf.c	7.33 (Berkeley) %G%
+ *	@(#)subr_prf.c	7.34 (Berkeley) %G%
  */
 
 #include <sys/param.h>
@@ -50,7 +50,7 @@ int	(*v_putc)() = cnputc;		/* routine to putc on virtual console */
 void  logpri __P((int level));
 static void  putchar __P((int ch, int flags, struct tty *tp));
 static char *ksprintn __P((u_long num, int base, int *len));
-void kprintf __P((const char *fmt, int flags, struct tty *tp, va_list ap, ...));
+void kprintf __P((const char *fmt, int flags, struct tty *tp, va_list ap));
 
 int consintr = 1;			/* Ok to handle console interrupts? */
 
@@ -70,6 +70,9 @@ const char *panicstr;
  * and then reboots.  If we are called twice, then we avoid trying to sync
  * the disks as this often leads to recursive panics.
  */
+#ifdef __GNUC__
+volatile			/* panic() does not return */
+#endif
 void
 #ifdef __STDC__
 panic(const char *fmt, ...)
@@ -78,7 +81,7 @@ panic(fmt /*, va_alist */)
 	char *fmt;
 #endif
 {
-	int bootopt, savintr;
+	int bootopt;
 	va_list ap;
 
 	bootopt = RB_AUTOBOOT | RB_DUMP;
@@ -87,15 +90,9 @@ panic(fmt /*, va_alist */)
 	else
 		panicstr = fmt;
 
-	savintr = consintr;		/* disable interrupts */
-	consintr = 0;
-
 	va_start(ap, fmt);
-	kprintf("panic: ", TOCONS | TOLOG, NULL, ap);
-	kprintf(fmt, TOCONS | TOLOG, NULL, ap);
+	printf("panic: %r\n", fmt, ap);
 	va_end(ap);
-
-	consintr = savintr;		/* reenable interrupts */
 
 #ifdef KGDB
 	kgdb_panic();
@@ -316,7 +313,7 @@ printf(fmt, va_alist)
  * The format %b is supported to decode error registers.
  * Its usage is:
  *
- *	kprintf("reg=%b\n", regval, "<base><arg>*");
+ *	printf("reg=%b\n", regval, "<base><arg>*");
  *
  * where <base> is the output base expressed as a control character, e.g.
  * \10 gives octal; \20 gives hex.  Each arg is a sequence of characters,
@@ -337,7 +334,7 @@ printf(fmt, va_alist)
  * {
  *	va_list ap;
  *	va_start(ap, fmt);
- *	kprintf("prefix: %r: suffix\n", flags, tp, fmt, ap);
+ *	printf("prefix: %r: suffix\n", fmt, ap);
  *	va_end(ap);
  * }
  *
@@ -345,15 +342,11 @@ printf(fmt, va_alist)
  * formats only.
  */
 void
-#ifdef __STDC__
-kprintf(const char *fmt, int flags, struct tty *tp, va_list ap, ...)
-#else
-kprintf(fmt, flags, tp)
+kprintf(fmt, flags, tp, ap)
 	register const char *fmt;
 	int flags;
 	struct tty *tp;
 	va_list ap;
-#endif
 {
 	register char *p;
 	register int ch, n;
