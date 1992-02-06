07f16b119480416c1ab4dafc7172e224dd8f3eff
Chris Torek <torek@ucbvax.Berkeley.EDU>
1992-02-06 03:25:57 -0800

    fix up prototypes
    SCCS-vsn: sys/sys/systm.h 7.19

--- usr/src/sys/sys/systm.h
+++ usr/src/sys/sys/systm.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)systm.h	7.18 (Berkeley) %G%
+ *	@(#)systm.h	7.19 (Berkeley) %G%
  */
 
 extern const char *panicstr;	/* panic message */
@@ -56,8 +56,12 @@ int	enxio __P((void));
 int	eopnotsupp __P((void));
 int	seltrue __P((dev_t dev, int which, struct proc *p));
 
+#ifdef __GNUC__
+volatile void	panic __P((const char *, ...));
+#else
 void	panic __P((const char *, ...));
-void	tablefull __P((char *));
+#endif
+void	tablefull __P((const char *));
 void	addlog __P((const char *, ...));
 void	log __P((int, const char *, ...));
 void	printf __P((const char *, ...));
