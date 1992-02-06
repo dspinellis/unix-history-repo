a4907b1367aa27366f406b152703d326a478c4c2
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1992-02-06 06:41:43 -0800

    date and time created 92/02/05 22:41:43 by mckusick
    SCCS-vsn: sys/pmax/include/ptrace.h 7.1

---
+++ usr/src/sys/pmax/include/ptrace.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 1992 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.redist.c%
+ *
+ *	@(#)ptrace.h	7.1 (Berkeley) %G%
+ */
+
+/*
+ * Machine dependent trace commands.
+ *
+ * None for the pmax at this time.
+ */
