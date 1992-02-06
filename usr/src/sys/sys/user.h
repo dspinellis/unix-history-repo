0f2b10e0fa69bac72ebe9a84e906b4417298f9bb
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1992-02-06 00:29:50 -0800

    p_regs moves to p_md.md_regs
    SCCS-vsn: sys/sys/user.h 7.20

--- usr/src/sys/sys/user.h
+++ usr/src/sys/sys/user.h
@@ -47,7 +47,7 @@ struct	user {
  * This subterfuge brought to you by coredump() and procxmt().
  * These fields are *only* valid at those times!
  */
-#define	U_ar0	u_kproc.kp_proc.p_regs	/* copy of curproc->p_regs */
+#define	U_ar0	u_kproc.kp_proc.p_md.md_regs /* copy of curproc->p_md.md_regs */
 #define	U_tsize	u_kproc.kp_eproc.e_vm.vm_tsize
 #define	U_dsize	u_kproc.kp_eproc.e_vm.vm_dsize
 #define	U_ssize	u_kproc.kp_eproc.e_vm.vm_ssize
