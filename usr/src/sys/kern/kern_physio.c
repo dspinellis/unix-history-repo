7f6a380f5c219675fb869203f1bc5a5f66493905
Chris Torek <torek@ucbvax.Berkeley.EDU>
1992-02-06 07:41:31 -0800

    misc cleanups and speedups
    SCCS-vsn: sys/kern/kern_physio.c 7.24

--- usr/src/sys/kern/kern_physio.c
+++ usr/src/sys/kern/kern_physio.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.proprietary.c%
  *
- *	@(#)kern_physio.c	7.23 (Berkeley) %G%
+ *	@(#)kern_physio.c	7.24 (Berkeley) %G%
  */
 
 #include <sys/param.h>
@@ -41,7 +41,7 @@ physio(strat, bp, dev, rw, mincnt, uio)
 	struct uio *uio;
 {
 	register struct iovec *iov;
-	register int requested, done;
+	register int requested = 0, done = 0;
 	register struct proc *p = curproc;
 	char *a;
 	int s, allocbuf = 0, error = 0;
@@ -52,6 +52,8 @@ physio(strat, bp, dev, rw, mincnt, uio)
 	}
 	for (; uio->uio_iovcnt; uio->uio_iov++, uio->uio_iovcnt--) {
 		iov = uio->uio_iov;
+		if (iov->iov_len == 0)
+			continue;
 		if (!useracc(iov->iov_base, (u_int)iov->iov_len,
 		    rw == B_READ ? B_WRITE : B_READ)) {
 			error = EFAULT;
