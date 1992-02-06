1d49796e9e72f7689fb6ac323e9136508bc90cf5
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-02-05 18:47:40 -0800

    compiling without a 386 (no -DNNPX) makes npxproc undefined; from Christos Zoulas; put correct copyright notice in place
    SCCS-vsn: sys/i386/i386/vm_machdep.c 7.5

--- usr/src/sys/i386/i386/vm_machdep.c
+++ usr/src/sys/i386/i386/vm_machdep.c
@@ -7,38 +7,7 @@
  * the Systems Programming Group of the University of Utah Computer
  * Science Department, and William Jolitz.
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by the University of
- *	California, Berkeley and its contributors.
- * 4. Neither the name of the University nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- *
- *	@(#)vm_machdep.c	7.3 (Berkeley) 5/13/91
- */
-
-/*
+ *	@(#)vm_machdep.c	7.5 (Berkeley) %G%
  *	Utah $Hdr: vm_machdep.c 1.16.1.1 89/06/23$
  */
 
@@ -114,7 +83,9 @@ cpu_fork(p1, p2)
 	return (0);
 }
 
+#if NNPX > 0
 extern struct proc *npxproc;
+#endif
 
 #ifdef notyet
 /*
@@ -137,8 +108,10 @@ cpu_exit(p)
 {
 	static struct pcb nullpcb;	/* pcb to overwrite on last swtch */
 
+#if NNPX > 0
 	/* free cporcessor (if we have it) */
 	if( p == npxproc) npxproc =0;
+#endif
 
 	/* move to inactive space and stack, passing arg accross */
 	p = swtch_to_inactive(p);
@@ -157,7 +130,9 @@ cpu_exit(p)
 {
 	
 	/* free coprocessor (if we have it) */
+#if NNPX > 0
 	if( p == npxproc) npxproc =0;
+#endif
 
 	curproc = p;
 	swtch();
