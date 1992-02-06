f453ba960645b5990fecf4bad738086081b3932f
Chris Torek <torek@ucbvax.Berkeley.EDU>
1992-02-06 03:33:57 -0800

    add prototypes
    SCCS-vsn: sys/sys/mbuf.h 7.15

--- usr/src/sys/sys/mbuf.h
+++ usr/src/sys/sys/mbuf.h
@@ -3,7 +3,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)mbuf.h	7.14 (Berkeley) %G%
+ *	@(#)mbuf.h	7.15 (Berkeley) %G%
  */
 
 #ifndef M_WAITOK
@@ -324,9 +324,17 @@ int	max_linkhdr;			/* largest link-level header */
 int	max_protohdr;			/* largest protocol header */
 int	max_hdr;			/* largest link+protocol header */
 int	max_datalen;			/* MHLEN - max_hdr */
-struct	mbuf *m_get(), *m_gethdr(), *m_getclr(), *m_retry(), *m_retryhdr();
-struct	mbuf *m_free(), *m_copym(), *m_pullup(), *m_prepend();
-int	m_clalloc();
+struct	mbuf *m_get __P((int, int));
+struct	mbuf *m_gethdr __P((int, int));
+struct	mbuf *m_getclr __P((int, int));
+struct	mbuf *m_retry __P((int, int));
+struct	mbuf *m_retryhdr __P((int, int));
+struct	mbuf *m_free __P((struct mbuf *));
+void	m_freem __P((struct mbuf *));
+struct	mbuf *m_copym __P((struct mbuf *, int, int, int));
+struct	mbuf *m_pullup __P((struct mbuf *, int));
+struct	mbuf *m_prepend __P((struct mbuf *, int, int));
+int	m_clalloc __P((int, int));
 extern	int mbtypes[];			/* XXX */
 
 #ifdef MBTYPES
