327df1d1e18a6e3ccdffcd539986900e1e061f41
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1992-02-16 06:49:33 -0800

    have to zero mount structures when they are allocated
    SCCS-vsn: sys/ufs/ffs/ffs_vfsops.c 7.63

--- usr/src/sys/ufs/ffs/ffs_vfsops.c
+++ usr/src/sys/ufs/ffs/ffs_vfsops.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)ffs_vfsops.c	7.62 (Berkeley) %G%
+ *	@(#)ffs_vfsops.c	7.63 (Berkeley) %G%
  */
 
 #include <sys/param.h>
@@ -226,6 +226,7 @@ ffs_mountfs(devvp, mp, p)
 		goto out;
 	}
 	ump = malloc(sizeof *ump, M_UFSMNT, M_WAITOK);
+	bzero((caddr_t)ump, sizeof *ump);
 	ump->um_fs = malloc((u_long)fs->fs_sbsize, M_UFSMNT,
 	    M_WAITOK);
 	bcopy((caddr_t)bp->b_un.b_addr, (caddr_t)ump->um_fs,
