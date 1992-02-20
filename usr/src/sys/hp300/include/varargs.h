13d49e3bdde7e39f3e9a379c60681116e6016320
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-02-20 01:07:22 -0800

    break into separate machine-dependent files for Sparc port varargs.h moved from /usr/src/include
    SCCS-vsn: sys/hp300/include/varargs.h 5.4
    SCCS-vsn: sys/hp300/include/stdarg.h 7.3

--- usr/src/sys/hp300/include/varargs.h
+++ usr/src/sys/hp300/include/varargs.h
@@ -4,19 +4,28 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)varargs.h	5.3 (Berkeley) %G%
+ *	@(#)varargs.h	5.4 (Berkeley) %G%
  */
 
 #ifndef _VARARGS_H_
 #define	_VARARGS_H_
 
-#include <stdarg.h>
+typedef char *va_list;
 
-#undef	va_dcl
 #define	va_dcl	int va_alist;
 
-#undef	va_start
 #define	va_start(ap) \
 	ap = (char *)&va_alist
+
+#ifdef KERNEL
+#define	va_arg(ap, type) \
+	((type *)(ap += sizeof(type)))[-1]
+#else
+#define	va_arg(ap, type) \
+	((type *)(ap += sizeof(type) < sizeof(int) ? \
+		(abort(), 0) : sizeof(type)))[-1]
+#endif
+
+#define	va_end(ap)
 
 #endif /* !_VARARGS_H_ */
