30c34cfdff00de7747ad1650994f8151fdb49b4e
CSRG <csrg@ucbvax.Berkeley.EDU>
1992-02-21 01:21:00 -0800

    BSD 4_4 development Work on file usr/src/contrib/libg++-2.3/libiberty/configure.bat Work on file usr/src/contrib/gdb-4.7.lbl/libiberty/configure.bat
    Synthesized-from: CSRG/cd3/4.4

---
+++ usr/src/contrib/gdb-4.7.lbl/libiberty/configure.bat
@@ -0,0 +1,16 @@
+@echo off
+if "%1" == "go32" goto h8300
+if "%1" == "h8/300" goto h8300
+echo Specify one of [ go32 h8/300 ] on command line
+goto exit
+
+:go32
+echo Configuring libiberty for go32
+copy Makefile.dos Makefile
+goto exit
+
+:h8300
+echo Configuring libiberty for H8/300
+copy Makefile.dos Makefile
+
+:exit
