85c715049ee0dbab164b3b337efd37f5445b7345
Keith Sklower <sklower@ucbvax.Berkeley.EDU>
1992-02-20 18:41:56 -0800

    make it possible to have different metrics for different networks on same phys interface
    SCCS-vsn: sys/netns/ns.c 7.10

--- usr/src/sys/netns/ns.c
+++ usr/src/sys/netns/ns.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)ns.c	7.9 (Berkeley) %G%
+ *	@(#)ns.c	7.10 (Berkeley) %G%
  */
 
 #include "param.h"
@@ -292,6 +292,7 @@ ns_ifinit(ifp, ia, sns, scrub)
 		splx(s);
 		return (EINVAL);
 	}
+	ia->ia_ifa.ifa_metric = ifp->if_metric;
 	/*
 	 * Add route for the network.
 	 */
