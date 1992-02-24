5604fb3d4f89206c7c371a8c796b1179a89f6088
William F. Jolitz <wjolitz@soda.berkeley.edu>
1992-02-24 14:40:20 -0800

    386BSD 0.0 development Work on file usr/src/sys.386bsd/i386/conf/SMALL Work on file usr/src/sys.386bsd/compile/SMALL/ioconf.c Work on file usr/src/sys.386bsd/compile/SMALL/pty.h Work on file usr/src/sys.386bsd/compile/SMALL/devpager.h Work on file usr/src/sys.386bsd/compile/SMALL/vnodepager.h Work on file usr/src/sys.386bsd/compile/SMALL/swappager.h Work on file usr/src/sys.386bsd/compile/SMALL/vector.s Work on file usr/src/sys.386bsd/compile/SMALL/ether.h Work on file usr/src/sys.386bsd/compile/SMALL/we.h Work on file usr/src/sys.386bsd/compile/SMALL/fd.h Work on file usr/src/sys.386bsd/compile/SMALL/isa.h Work on file usr/src/sys.386bsd/compile/SMALL/pc.h Work on file usr/src/sys.386bsd/compile/SMALL/com.h Work on file usr/src/sys.386bsd/compile/SMALL/loop.h Work on file usr/src/sys.386bsd/compile/SMALL/sl.h Work on file usr/src/sys.386bsd/compile/SMALL/npx.h Work on file usr/src/sys.386bsd/compile/SMALL/wd.h Work on file usr/src/sys.386bsd/compile/SMALL/swap386bsd.c
    Co-Authored-By: Lynne Greer Jolitz <ljolitz@cardio.ucsf.edu>
    Synthesized-from: 386BSD-0.0/src

---
+++ usr/src/sys.386bsd/compile/SMALL/com.h
@@ -0,0 +1,1 @@
+#define NCOM 1
---
+++ usr/src/sys.386bsd/compile/SMALL/swap386bsd.c
@@ -0,0 +1,10 @@
+#include "sys/param.h"
+#include "sys/conf.h"
+
+dev_t	rootdev = makedev(0, 0);
+dev_t	dumpdev = makedev(0, 1);
+
+struct	swdevt swdevt[] = {
+	{ makedev(0, 1),	0,	0 },	/* dk0b */
+	{ 0, 0, 0 }
+};
