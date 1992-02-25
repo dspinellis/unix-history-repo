b7c1dedbd395611b1de93d82f43e6e75da7e8908
William F. Jolitz <wjolitz@soda.berkeley.edu>
1992-02-25 10:31:56 -0800

    386BSD 0.0 development Work on file usr/src/usr.bin/gdb/init.c
    Co-Authored-By: Lynne Greer Jolitz <ljolitz@cardio.ucsf.edu>
    Synthesized-from: 386BSD-0.0/src

---
+++ usr/src/usr.bin/gdb/init.c
@@ -0,0 +1,22 @@
+void initialize_all_files () {
+_initialize_i386bsd_dep();
+_initialize_blockframe ();
+_initialize_breakpoint ();
+_initialize_command ();
+_initialize_copying ();
+_initialize_core();
+_initialize_dbxread ();
+_initialize_expread ();
+_initialize_infcmd ();
+_initialize_inflow ();
+_initialize_infrun ();
+_initialize_printcmd ();
+_initialize_remote();
+_initialize_source ();
+_initialize_stack ();
+_initialize_symmisc ();
+_initialize_symtab ();
+_initialize_utils ();
+_initialize_valprint ();
+_initialize_values ();
+}
