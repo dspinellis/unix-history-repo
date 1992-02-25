8bb4f17fc520bf8a39264c9678f7a91df4add449
William F. Jolitz <wjolitz@soda.berkeley.edu>
1992-02-25 13:36:31 -0800

    386BSD 0.1 development Work on file usr/othersrc/public/bc-1.01/COPYING Work on file usr/othersrc/public/bc-1.01/bc.y Work on file usr/othersrc/public/bc-1.01/Install Work on file usr/othersrc/public/bc-1.01/const.h Work on file usr/othersrc/public/bc-1.01/global.c Work on file usr/othersrc/public/bc-1.01/execute.c Work on file usr/othersrc/public/bc-1.01/version.h Work on file usr/othersrc/public/bc-1.01/load.c Work on file usr/othersrc/public/bc-1.01/global.h Work on file usr/othersrc/public/bc-1.01/main.c Work on file usr/othersrc/public/bc-1.01/storage.c Work on file usr/othersrc/public/bc-1.01/number.c Work on file usr/othersrc/public/bc-1.01/proto.h Work on file usr/othersrc/public/bc-1.01/number.h Work on file usr/othersrc/public/bc-1.01/util.c Work on file usr/othersrc/public/bc-1.01/vfprintf.c Work on file usr/othersrc/public/bc-1.01/scan.l Work on file usr/othersrc/public/bc-1.01/README Work on file usr/othersrc/public/bc-1.01/bc.1 Work on file usr/othersrc/public/bc-1.01/fix_math.h Work on file usr/othersrc/public/bc-1.01/sbc.y Work on file usr/othersrc/public/bc-1.01/configure Work on file usr/othersrc/public/bc-1.01/libmath.b Work on file usr/othersrc/public/bc-1.01/ChangeLog Work on file usr/othersrc/public/bc-1.01/Examples/ckbook.b Work on file usr/othersrc/public/bc-1.01/Examples/primes.b Work on file usr/othersrc/public/bc-1.01/Test/aryprm.b Work on file usr/othersrc/public/bc-1.01/Test/atan.b Work on file usr/othersrc/public/bc-1.01/Test/array.b Work on file usr/othersrc/public/bc-1.01/Test/exp.b Work on file usr/othersrc/public/bc-1.01/Test/checklib.b Work on file usr/othersrc/public/bc-1.01/Test/div.b Work on file usr/othersrc/public/bc-1.01/Test/fact.b Work on file usr/othersrc/public/bc-1.01/Test/ln.b Work on file usr/othersrc/public/bc-1.01/Test/jn.b Work on file usr/othersrc/public/bc-1.01/Test/raise.b Work on file usr/othersrc/public/bc-1.01/Test/sqrt.b Work on file usr/othersrc/public/bc-1.01/Test/mul.b Work on file usr/othersrc/public/bc-1.01/Test/sine.b Work on file usr/othersrc/public/bc-1.01/Test/timetest Work on file usr/othersrc/public/bc-1.01/Test/testfn.b Work on file usr/othersrc/public/bc-1.01/config.h Work on file usr/othersrc/public/bc-1.01/scan.c Work on file usr/src/usr.bin/sed/README Work on file usr/src/usr.bin/sed/ChangeLog Work on file usr/src/usr.bin/sed/regex.h Work on file usr/src/usr.bin/sed/getopt.h Work on file usr/src/usr.bin/sed/sed.c Work on file usr/src/usr.bin/sed/utils.c Work on file usr/src/usr.bin/sed/regex.c Work on file usr/src/usr.bin/sed/getopt.c Work on file usr/src/usr.bin/sed/getopt1.c Work on file usr/src/usr.bin/sed/Makefile.gnu Work on file usr/src/usr.bin/tar/create.c Work on file usr/src/usr.bin/tar/extract.c Work on file usr/src/usr.bin/tar/update.c Work on file usr/src/usr.bin/tar/getoldopt.c Work on file usr/src/usr.bin/tar/mangle.c Work on file usr/src/usr.bin/tar/gnu.c Work on file usr/src/usr.bin/tar/names.c Work on file usr/src/usr.bin/tar/version.c Work on file usr/src/usr.bin/tar/list.c Work on file usr/src/usr.bin/tar/diffarch.c Work on file usr/src/usr.bin/tar/wildmat.c Work on file usr/src/usr.bin/tar/port.c Work on file usr/src/usr.bin/tar/getopt1.c Work on file usr/src/usr.bin/tar/getopt.c Work on file usr/src/usr.bin/tar/README Work on file usr/src/usr.bin/tar/regex.c Work on file usr/src/usr.bin/tar/ChangeLog Work on file usr/src/usr.bin/tar/rmt.h Work on file usr/src/usr.bin/tar/port.h Work on file usr/src/usr.bin/tar/open3.h Work on file usr/src/usr.bin/tar/regex.h Work on file usr/src/usr.bin/tar/getopt.h Work on file usr/src/usr.bin/tar/rtape_lib.c Work on file usr/src/usr.bin/tar/getdate.y
    Co-Authored-By: Lynne Greer Jolitz <ljolitz@cardio.ucsf.edu>
    Synthesized-from: 386BSD-0.1

---
+++ usr/othersrc/public/bc-1.01/version.h
@@ -0,0 +1,3 @@
+#define BC_VERSION  \
+  "bc 1.01 (Nov 25, 1991), Copyright (C) 1991 Free Software Foundation, Inc."
+
---
+++ usr/src/usr.bin/tar/version.c
@@ -0,0 +1,20 @@
+/* Version info for tar.
+   Copyright (C) 1989, Free Software Foundation.
+
+This file is part of GNU Tar.
+
+GNU Tar is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 1, or (at your option)
+any later version.
+
+GNU Tar is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GNU Tar; see the file COPYING.  If not, write to
+the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
+   
+char version_string[] = "GNU tar version 1.10";
