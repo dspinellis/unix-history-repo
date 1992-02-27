c550a89349ab83d0830ef2a058eb48d1556efaa0
William F. Jolitz <wjolitz@soda.berkeley.edu>
1992-02-27 00:31:20 -0800

    386BSD 0.1 development Work on file usr/othersrc/public/ghostscript-2.4.1/gslp.bat Work on file usr/othersrc/public/ghostscript-2.4.1/gsdj.bat Work on file usr/othersrc/public/ghostscript-2.4.1/gst.bat Work on file usr/othersrc/public/ghostscript-2.4.1/font2c.bat
    Co-Authored-By: Lynne Greer Jolitz <ljolitz@cardio.ucsf.edu>
    Synthesized-from: 386BSD-0.1

---
+++ usr/othersrc/public/ghostscript-2.4.1/gsdj.bat
@@ -0,0 +1,1 @@
+@gs -q -sDEVICE=deskjet -r300 -dDISKFONTS -dNOPAUSE -- gslp.ps %1 %2 %3 %4 %5 %6 %7 %8 %9
---
+++ usr/othersrc/public/ghostscript-2.4.1/gslp.bat
@@ -0,0 +1,1 @@
+@gs -q -sDEVICE=epson -r180 -dDISKFONTS -dNOPAUSE -- gslp.ps %1 %2 %3 %4 %5 %6 %7 %8 %9
