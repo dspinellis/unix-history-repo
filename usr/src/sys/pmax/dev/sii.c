0c7a22576b0c70063919475ad6484048e7ec955d
Ralph Campbell <ralph@ucbvax.Berkeley.EDU>
1992-03-01 01:33:10 -0800

    fix syntax error
    SCCS-vsn: sys/pmax/dev/sii.c 7.3

--- usr/src/sys/pmax/dev/sii.c
+++ usr/src/sys/pmax/dev/sii.c
@@ -7,10 +7,10 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)sii.c	7.2 (Berkeley) %G%
+ *	@(#)sii.c	7.3 (Berkeley) %G%
  *
  * from: $Header: /sprite/src/kernel/dev/ds3100.md/RCS/devSII.c,
- *	v 9.2 89/09/14 13:37:41 jhh Exp $ SPRITE (DECWRL)"; */
+ *	v 9.2 89/09/14 13:37:41 jhh Exp $ SPRITE (DECWRL)";
  */
 
 #include "sii.h"
