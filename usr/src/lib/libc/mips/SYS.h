3c8579225ed0493c53b701c368995410259eb6a0
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-02-29 20:27:10 -0800

    date and time created 92/02/29 12:27:10 by bostic
    SCCS-vsn: lib/libc/mips/SYS.h 5.1

---
+++ usr/src/lib/libc/mips/SYS.h
@@ -0,0 +1,24 @@
+/*-
+ * Copyright (c) 1991 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to Berkeley by
+ * Ralph Campbell.
+ *
+ * %sccs.include.redist.c%
+ *
+ *	@(#)SYS.h	5.1 (Berkeley) %G%
+ */
+
+#include <sys/syscall.h>
+#include "DEFS.h"
+
+/* vax/tahoe compat */
+#define	ret
+#define	r0	v0
+#define	r1	v1
+
+#define	SYSCALL(x)	LEAF(x); li v0,SYS_/**/x; syscall; bne a3,zero,err; \
+			j ra; err: j _cerror; END(x);
+#define	PSEUDO(x,y)	LEAF(x); li v0,SYS_/**/y; syscall; bne a3,zero,err; \
+			j ra; err: j _cerror; END(x);
