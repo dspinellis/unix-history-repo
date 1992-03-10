b091bb02272a983b3dcf62c4a5843c8772294126
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-03-10 00:15:28 -0800

    explain what is really happening
    SCCS-vsn: sys/hp300/include/limits.h 7.4

--- usr/src/sys/hp300/include/limits.h
+++ usr/src/sys/hp300/include/limits.h
@@ -4,19 +4,29 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)limits.h	7.3 (Berkeley) %G%
+ *	@(#)limits.h	7.4 (Berkeley) %G%
  */
 
 #define	CHAR_BIT	8		/* number of bits in a char */
-#define	CLK_TCK		60		/* ticks per second */
 #define	MB_LEN_MAX	1		/* no multibyte characters */
 
+#define	CLK_TCK		60		/* ticks per second */
+
+/*
+ * According to ANSI (section 2.2.4.2), the values below must be usable by
+ * #if preprocessing directives.  Additionally, the expression must have the
+ * same type as would an expression that is an object of the corresponding
+ * type converted according to the integral promotions.  The subtraction for
+ * INT_MIN and LONG_MIN is so the value is not unsigned; 2147483648 is an
+ * unsigned int for 32-bit two's complement ANSI compilers (section 3.1.3.2).
+ * These numbers work for pcc as well.
+ */
 #define	SCHAR_MAX	127		/* min value for a signed char */
 #define	SCHAR_MIN	-128		/* max value for a signed char */
 
 #define	UCHAR_MAX	255		/* max value for an unsigned char */
 #define	CHAR_MAX	127		/* max value for a char */
-#define	CHAR_MIN	128		/* min value for a char */
+#define	CHAR_MIN	-128		/* min value for a char */
 
 #define	USHRT_MAX	65535		/* max value for an unsigned short */
 #define	SHRT_MAX	32767		/* max value for a short */
