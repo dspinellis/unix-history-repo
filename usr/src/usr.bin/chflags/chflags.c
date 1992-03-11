6026e36d498be3f094bedfc5ba78f9c523b7b669
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-03-11 06:16:27 -0800

    make fts(3) work again, welcome to the wonderful world of ANSI!
    SCCS-vsn: usr.bin/chflags/chflags.c 5.2

--- usr/src/usr.bin/chflags/chflags.c
+++ usr/src/usr.bin/chflags/chflags.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1989 The Regents of the University of California.
+ * Copyright (c) 1992 The Regents of the University of California.
  * All rights reserved.
  *
  * %sccs.include.redist.c%
@@ -7,44 +7,56 @@
 
 #ifndef lint
 char copyright[] =
-"@(#) Copyright (c) 1989 The Regents of the University of California.\n\
+"@(#) Copyright (c) 1992 The Regents of the University of California.\n\
  All rights reserved.\n";
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)chflags.c	5.1 (Berkeley) %G%";
+static char sccsid[] = "@(#)chflags.c	5.2 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <errno.h>
 #include <fts.h>
+#include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-extern int errno;
+typedef struct cmdset {
+	long clrbits;
+	long setbits;
+} CMDS;
+CMDS cmds;
 int retval;
 
+void	 err __P((const char *, ...));
+void	 error __P((char *));
+int	 getflags __P((CMDS *, int));
+void	*setflags __P((char *));
+void	 usage __P((void));
+
+int
 main(argc, argv)
 	int argc;
-	char **argv;
+	char *argv[];
 {
-	extern int optind;
 	register FTS *fts;
 	register FTSENT *p;
-	register int oct, oflags;
+	register long oflags;
+	register int oct;
 	register char *flags;
-	void *set, *setflags();
 	struct stat sb;
-	int ch, fflag, rflag;
+	void *set;
+	int ch, rflag;
+	char *ep;
 
-	fflag = rflag = 0;
-	while ((ch = getopt(argc, argv, "Rf")) != EOF)
+	rflag = 0;
+	while ((ch = getopt(argc, argv, "R")) != EOF)
 		switch((char)ch) {
 		case 'R':
 			rflag = 1;
-			break;
-		case 'f':		/* no longer documented */
-			fflag = 1;
 			break;
 		case '?':
 		default:
@@ -58,24 +70,21 @@ main(argc, argv)
 
 	flags = *argv;
 	if (*flags >= '0' && *flags <= '7') {
-		oflags = (int)strtol(flags, (char **)NULL, 8);
-		oct = 1;
+		oflags = (int)strtol(flags, &ep, 8);
+                if (oflags < 0 || *ep)
+                        err("invalid flags: %s", flags);
+                oct = 1;
 	} else {
-		if (!(set = setflags(flags))) {
-			(void)fprintf(stderr, "chflags: invalid flags.\n");
-			exit(1);
-		}
+		if ((set = setflags(flags)) == NULL)
+                        err("invalid flags: %s", flags);
 		oct = 0;
 	}
 
 	retval = 0;
 	if (rflag) {
 		if (!(fts = fts_open(++argv,
-		    oct ? FTS_NOSTAT|FTS_PHYSICAL : FTS_PHYSICAL, 0))) {
-			(void)fprintf(stderr, "chflags: %s.\n",
-			    strerror(errno));
-			exit(1);
-		}
+		    oct ? FTS_NOSTAT|FTS_PHYSICAL : FTS_PHYSICAL, 0)))
+			err("%s", strerror(errno));
 		while (p = fts_read(fts))
 			switch(p->fts_info) {
 			case FTS_D:
@@ -83,13 +92,10 @@ main(argc, argv)
 			case FTS_DNR:
 			case FTS_ERR:
 			case FTS_NS:
-				(void)fprintf(stderr, "chflags: %s: %s.\n",
-				    p->fts_path, strerror(errno));
-				exit(1);
+				err("%s: %s", p->fts_path, strerror(errno));
 			default:
 				if (chflags(p->fts_accpath, oct ? oflags :
-				    getflags(set, p->fts_statb.st_flags)) &&
-				    !fflag)
+				    getflags(set, p->fts_statp->st_flags)))
 					error(p->fts_path);
 				break;
 			}
@@ -97,43 +103,24 @@ main(argc, argv)
 	}
 	if (oct) {
 		while (*++argv)
-			if (chflags(*argv, oflags) && !fflag)
+			if (chflags(*argv, oflags))
 				error(*argv);
 	} else
 		while (*++argv)
-			if ((lstat(*argv, &sb) ||
-			    chflags(*argv, getflags(set, sb.st_flags))) &&
-			    !fflag)
+			if (lstat(*argv, &sb) ||
+			    chflags(*argv, getflags(set, sb.st_flags)))
 				error(*argv);
 	exit(retval);
-}
-
-error(name)
-	char *name;
-{
-	(void)fprintf(stderr, "chflags: %s: %s.\n", name, strerror(errno));
-	retval = 1;
-}
-
-usage()
-{
-	(void)fprintf(stderr, "Usage: chflags [-R] flags file ...\n");
-	exit(1);
 }
 
 /*
  * These are analogous to the setmode/getmode routines in the C library.
  */
-struct cmdset {
-	long	clrbits;
-	long	setbits;
-} cmds;
-
 void *
 setflags(cp)
 	char *cp;
 {
-	register struct cmdset *fset;
+	register CMDS *fset;
 	register char *arg;
 
 	fset = &cmds;
@@ -152,12 +139,57 @@ setflags(cp)
 	return (fset);
 }
 
+int
 getflags(fset, oflags)
-	register struct cmdset *fset;
+	register CMDS *fset;
 	register int oflags;
 {
 
 	oflags &= ~fset->clrbits;
 	oflags |= fset->setbits;
 	return (oflags);
+}
+
+#if __STDC__
+#include <stdarg.h>
+#else
+#include <varargs.h>
+#endif
+
+void
+#if __STDC__
+err(const char *fmt, ...)
+#else
+err(fmt, va_alist)
+	char *fmt;
+        va_dcl
+#endif
+{
+	va_list ap;
+#if __STDC__
+	va_start(ap, fmt);
+#else
+	va_start(ap);
+#endif
+	(void)fprintf(stderr, "chflags: ");
+	(void)vfprintf(stderr, fmt, ap);
+	va_end(ap);
+	(void)fprintf(stderr, "\n");
+	exit(1);
+	/* NOTREACHED */
+}
+
+void
+error(name)
+	char *name;
+{
+	(void)fprintf(stderr, "chflags: %s: %s.\n", name, strerror(errno));
+	retval = 1;
+}
+
+void
+usage()
+{
+	(void)fprintf(stderr, "usage: chflags [-R] flags file ...\n");
+	exit(1);
 }
