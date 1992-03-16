4d57325f07626f841344299bdadedf83862cef59
Chris Torek <torek@ucbvax.Berkeley.EDU>
1992-03-15 21:35:57 -0800

    must include systm.h to get decl for panic (for gcc)
    SCCS-vsn: sys/kern/uipc_usrreq.c 7.32

--- usr/src/sys/kern/uipc_usrreq.c
+++ usr/src/sys/kern/uipc_usrreq.c
@@ -1,10 +1,11 @@
 /*
  * Copyright (c) 1982, 1986 Regents of the University of California.
  *
- *	@(#)uipc_usrreq.c	7.31 (Berkeley) %G%
+ *	@(#)uipc_usrreq.c	7.32 (Berkeley) %G%
  */
 
 #include "param.h"
+#include "systm.h"
 #include "proc.h"
 #include "filedesc.h"
 #include "domain.h"
