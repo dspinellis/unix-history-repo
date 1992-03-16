ad64f11de0f88d9b9d28be736fdabe05ad0c1d48
Ralph Campbell <ralph@ucbvax.Berkeley.EDU>
1992-03-16 03:25:10 -0800

    make code more like hp300
    SCCS-vsn: lib/libc/mips/gen/modf.s 5.3

--- usr/src/lib/libc/mips/gen/modf.s
+++ usr/src/lib/libc/mips/gen/modf.s
@@ -11,7 +11,7 @@
 #include "DEFS.h"
 
 #if defined(LIBC_SCCS) && !defined(lint)
-	ASMSTR("@(#)modf.s	5.2 (Berkeley) %G%")
+	ASMSTR("@(#)modf.s	5.3 (Berkeley) %G%")
 #endif /* LIBC_SCCS and not lint */
 
 /*
@@ -20,27 +20,15 @@
  * returns: xxx and n (in *iptr) where val == n.xxx
  */
 LEAF(modf)
-	li.d	$f6, 4503599627370496.0	# check for value out of range (2**52)
-	abs.d	$f0, $f12
-	c.lt.d	$f0, $f6
-	mfc1	t0, $f13		# get the sign & exponent part
-	bc1f	3f			# val is not less than maxint
-	add.d	$f2, $f0, $f6		# logical shift right
-	sub.d	$f2, $f2, $f6		# logical shift left
-	c.le.d	$f2, $f0
-	bc1t	1f
-	li.d	$f6, 1.0		# adjust due to rounding
-	sub.d	$f2, $f2, $f6
-1:
-	bge	t0, zero, 2f		# jump if val >= 0
-	neg.d	$f2, $f0		# negate integer part if val < 0
-2:
-	s.d	$f2, 0(a2)		# save the integer part
-	sub.d	$f0, $f12, $f2		# return the fractional part
-	j	ra
-3:
-	mtc1	zero, $f0		# val was too big so
-	mtc1	zero, $f1		#   return fraction of zero
-	s.d	$f12, 0(a2)		#   and the original number.
+	cfc1	t0, $31			# get the control register
+	cfc1	t0, $31			# get the control register
+	or	t1, t0, 0x3		# set rounding mode to round to zero
+	xor	t1, t1, 0x2		#  (i.e., 01)
+	ctc1	t1, $31
+	cvt.w.d	$f0, $f12		# convert val to integer
+	cvt.d.w	$f0, $f0		# convert back to double
+	ctc1	t0, $31			# restore old rounding mode
+	s.d	$f0, 0(a2)		# save the integer part
+	sub.d	$f0, $f12, $f0		# subtract val - integer part
 	j	ra
 END(modf)
