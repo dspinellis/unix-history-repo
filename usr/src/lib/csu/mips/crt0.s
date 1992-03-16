2439a24aa184e34b194f020c02a7014874b1f518
Ralph Campbell <ralph@ucbvax.Berkeley.EDU>
1992-03-16 03:22:58 -0800

    use C style comments not # in column one
    SCCS-vsn: lib/csu/mips/crt0.s 5.2

--- usr/src/lib/csu/mips/crt0.s
+++ usr/src/lib/csu/mips/crt0.s
@@ -1,15 +1,14 @@
-#!/bin/sh -
-#
-# Copyright (c) 1991 The Regents of the University of California.
-# All rights reserved.
-#
-# This code is derived from software contributed to Berkeley by
-# Ralph Campbell.
-#
-# %sccs.include.redist.sh%
-#
-#	@(#)crt0.s	5.1 (Berkeley) %G%
-#
+/*
+ * Copyright (c) 1991 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to Berkeley by
+ * Ralph Campbell.
+ *
+ * %sccs.include.redist.sh%
+ *
+ *	@(#)crt0.s	5.2 (Berkeley) %G%
+ */
 
 #include <machine/regdef.h>
 #include "../libc/mips/DEFS.h"
