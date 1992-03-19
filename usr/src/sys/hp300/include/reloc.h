c187af1ebdfddb11c1e2f850fcec4db23d73bcfe
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-03-19 00:33:37 -0800

    date and time created 92/03/18 16:33:37 by bostic
    SCCS-vsn: sys/hp300/include/reloc.h 7.1

---
+++ usr/src/sys/hp300/include/reloc.h
@@ -0,0 +1,18 @@
+/*-
+ * Copyright (c) 1992 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.redist.c%
+ *
+ *	@(#)reloc.h	7.1 (Berkeley) %G%
+ */
+
+/* Relocation format. */
+struct relocation_info {
+	int r_address;			/* offset in text or data segment */
+	unsigned int r_symbolnum : 24,	/* ordinal number of add symbol */
+			 r_pcrel :  1,	/* 1 if value should be pc-relative */
+			r_length :  2,	/* log base 2 of value's width */
+			r_extern :  1,	/* 1 if need to add symbol to value */
+				 :  4;	/* reserved */
+};
