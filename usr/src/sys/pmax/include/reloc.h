d175df021622d0a14bac4b0d08613346453a46a4
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-03-19 00:34:25 -0800

    date and time created 92/03/18 16:34:25 by bostic
    SCCS-vsn: sys/pmax/include/reloc.h 7.1

---
+++ usr/src/sys/pmax/include/reloc.h
@@ -0,0 +1,12 @@
+/*-
+ * Copyright (c) 1992 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.redist.c%
+ *
+ *	@(#)reloc.h	7.1 (Berkeley) %G%
+ */
+
+/*
+ * There is currently no relocation information for the MIPS.
+ */
