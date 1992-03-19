f60d0f3febcf254ba3c29819043ebb562c988ae4
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1992-03-19 02:04:57 -0800

    add structure to locate ps straings
    SCCS-vsn: sys/sys/exec.h 7.11

--- usr/src/sys/sys/exec.h
+++ usr/src/sys/sys/exec.h
@@ -4,8 +4,28 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)exec.h	7.10 (Berkeley) %G%
+ *	@(#)exec.h	7.11 (Berkeley) %G%
  */
 
-/* Compatibility file only. */
 #include <machine/exec.h>
+
+/*
+ * The following structure is found at the top of the user stack of each
+ * user process. The ps program uses it to locate argv and environment
+ * strings. Programs that wish ps to display other information may modify
+ * it; normally ps_argvstr points to the text for argv[0], and ps_nargvstr
+ * is the same as the program's argc. The fields ps_envstr and ps_nenvstr
+ * are the equivalent for the environment.
+ */
+struct ps_strings {
+	char	*ps_argvstr;	/* first of 0 or more argument strings */
+	int	ps_nargvstr;	/* the number of argument strings */
+	char	*ps_envstr;	/* first of 0 or more environment strings */
+	int	ps_nenvstr;	/* the number of environment strings */
+};
+
+/*
+ * Address of ps_strings structure (in user space).
+ */
+#define	PS_STRINGS \
+	((struct ps_strings *)(USRSTACK - sizeof(struct ps_strings)))
