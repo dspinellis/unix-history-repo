6f9fdb70cdae8a387d79f21d9ff7db5c0d7b23a5
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-03-18 23:11:32 -0800

    Don't log user passwords when debug is turned on with -d. Set "home" to "/" when logging in as a guest. Don't syslog() trailing CR-LF when using -d. Don't timestamp connect message, syslog() already does that. Don't timestamp timeout message, syslog() already does that. Minor whitespace cleanups. from Craig Leres (leres@ee.lbl.gov)
    SCCS-vsn: libexec/ftpd/ftpd.c 5.41
    SCCS-vsn: libexec/ftpd/ftpcmd.y 5.25

--- usr/src/libexec/ftpd/ftpd.c
+++ usr/src/libexec/ftpd/ftpd.c
@@ -12,7 +12,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)ftpd.c	5.40 (Berkeley) %G%";
+static char sccsid[] = "@(#)ftpd.c	5.41 (Berkeley) %G%";
 #endif /* not lint */
 
 /*
@@ -479,6 +479,7 @@ pass(passwd)
 		if (logging)
 			syslog(LOG_INFO, "ANONYMOUS FTP LOGIN FROM %s, %s",
 			    remotehost, passwd);
+		home = "/";		/* guest home dir for globbing */
 	} else {
 		reply(230, "User %s logged in.", pw->pw_name);
 #ifdef SETPROCTITLE
@@ -488,8 +489,8 @@ pass(passwd)
 		if (logging)
 			syslog(LOG_INFO, "FTP LOGIN FROM %s, %s",
 			    remotehost, pw->pw_name);
+		home = pw->pw_dir;	/* home dir for globbing */
 	}
-	home = pw->pw_dir;		/* home dir for globbing */
 	(void) umask(defumask);
 	return;
 bad:
@@ -538,7 +539,7 @@ retrieve(cmd, name)
 				}
 				if (c == '\n')
 					i++;
-			}	
+			}
 		} else if (lseek(fileno(fin), restart_point, L_SET) < 0) {
 			perror_reply(550, name);
 			goto done;
@@ -589,7 +590,7 @@ store(name, mode, unique)
 				}
 				if (c == '\n')
 					i++;
-			}	
+			}
 			/*
 			 * We must do this seek to "current" position
 			 * because we are changing from reading to
@@ -1130,8 +1131,6 @@ dolog(sin)
 {
 	struct hostent *hp = gethostbyaddr((char *)&sin->sin_addr,
 		sizeof (struct in_addr), AF_INET);
-	time_t t, time();
-	extern char *ctime();
 
 	if (hp)
 		(void) strncpy(remotehost, hp->h_name, sizeof (remotehost));
@@ -1143,11 +1142,8 @@ dolog(sin)
 	setproctitle(proctitle);
 #endif /* SETPROCTITLE */
 
-	if (logging) {
-		t = time((time_t *) 0);
-		syslog(LOG_INFO, "connection from %s at %s",
-		    remotehost, ctime(&t));
-	}
+	if (logging)
+		syslog(LOG_INFO, "connection from %s", remotehost);
 }
 
 /*
@@ -1196,8 +1192,8 @@ myoob()
 
 /*
  * Note: a response of 425 is not mentioned as a possible response to
- * 	the PASV command in RFC959. However, it has been blessed as
- * 	a legitimate response by Jon Postel in a telephone conversation
+ *	the PASV command in RFC959. However, it has been blessed as
+ *	a legitimate response by Jon Postel in a telephone conversation
  *	with Rick Adams on 25 Jan 89.
  */
 passive()
