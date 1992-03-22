f1254a5c4d94fe7e8689bc288bc3b7be08411fa9
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-03-21 18:44:42 -0800

    bring rev back into the main-line source tree reimplement it to work like the rest of the programs
    SCCS-vsn: usr.bin/rev/rev.1 6.3
    SCCS-vsn: usr.bin/rev/rev.c 5.1

--- usr/src/usr.bin/rev/rev.c
+++ usr/src/usr.bin/rev/rev.c
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 1987 The Regents of the University of California.
+ * Copyright (c) 1987, 1992 The Regents of the University of California.
  * All rights reserved.
  *
  * %sccs.include.redist.c%
@@ -7,37 +7,101 @@
 
 #ifndef lint
 char copyright[] =
-"@(#) Copyright (c) 1987 The Regents of the University of California.\n\
+"@(#) Copyright (c) 1987, 1992 The Regents of the University of California.\n\
  All rights reserved.\n";
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)rev.c	4.5 (Berkeley) %G%";
+static char sccsid[] = "@(#)rev.c	5.1 (Berkeley) %G%";
 #endif /* not lint */
 
+#include <sys/types.h>
+#include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+void usage __P((void));
+void warn __P((const char *, ...));
+
+int
 main(argc, argv)
 	int argc;
-	char **argv;
+	char *argv[];
 {
-	register char *t, *bp;
-	char buf[BUFSIZ];
+	register char *filename, *p, *t;
+	FILE *fp;
+	size_t len;
+	int ch, rval;
 
-	bp = buf;
-	do {
-		if (argc > 1 && !freopen(*++argv, "r", stdin)) {
-			fprintf(stderr, "rev: cannot open %s.\n", *argv);
-			exit(1);
+	while ((ch = getopt(argc, argv, "")) != EOF)
+		switch(ch) {
+		case '?':
+		default:
+			usage();
 		}
-		while (fgets(bp, sizeof(buf), stdin)) {
-			for (t = bp; *t; ++t);
-			if (*--t == '\n')
-				--t;
-			for (; t >= bp; --t)
+
+	argc -= optind;
+	argv += optind;
+
+	fp = stdin;
+	filename = "stdin";
+	rval = 0;
+	do {
+		if (*argv) {
+			if ((fp = fopen(*argv, "r")) == NULL) {
+				warn("%s: %s", *argv, strerror(errno));
+				rval = 1;
+				++argv;
+				continue;
+			}
+			filename = *argv++;
+		}
+		while (p = fgetline(fp, &len)) {
+			t = p + len - 1;
+			for (t = p + len - 1; t >= p; --t)
 				putchar(*t);
 			putchar('\n');
 		}
-	} while(--argc > 1);
+		if (ferror(fp)) {
+			warn("%s: %s", filename, strerror(errno));
+			rval = 1;
+		}
+		(void)fclose(fp);
+	} while(*argv);
 	exit(0);
+}
+
+#if __STDC__
+#include <stdarg.h>
+#else
+#include <varargs.h>
+#endif
+
+void
+#if __STDC__
+warn(const char *fmt, ...)
+#else
+warn(fmt, va_alist)
+	char *fmt;
+        va_dcl
+#endif
+{
+	va_list ap;
+#if __STDC__
+	va_start(ap, fmt);
+#else
+	va_start(ap);
+#endif
+	(void)fprintf(stderr, "rev: ");
+	(void)vfprintf(stderr, fmt, ap);
+	va_end(ap);
+	(void)fprintf(stderr, "\n");
+}
+
+void
+usage()
+{
+	(void)fprintf(stderr, "usage: rev [file ...]\n");
+	exit(1);
 }
