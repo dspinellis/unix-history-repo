ee919b50cc192596548ffeb23a6c80053592d34d
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-03-29 15:17:34 -0800

    gethostbyname should be const char, not char
    SCCS-vsn: include/netdb.h 5.16

--- usr/src/include/netdb.h
+++ usr/src/include/netdb.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)netdb.h	5.15 (Berkeley) %G%
+ *	@(#)netdb.h	5.16 (Berkeley) %G%
  */
 
 #ifndef _NETDB_H_
@@ -72,7 +72,7 @@ void		endnetent __P((void));
 void		endprotoent __P((void));
 void		endservent __P((void));
 struct hostent	*gethostbyaddr __P((const char *, int, int));
-struct hostent	*gethostbyname __P((char *));
+struct hostent	*gethostbyname __P((const char *));
 struct hostent *gethostent __P((void));
 struct netent	*getnetbyaddr __P((long, int)); /* u_long? */
 struct netent	*getnetbyname __P((const char *));
