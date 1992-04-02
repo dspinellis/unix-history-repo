c983b9aff37e4f10b8493e847fe36d7191c3d36a
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1992-04-02 00:52:45 -0800

    update from Steve McCanne for Sun compatible kvm
    SCCS-vsn: include/kvm.h 5.7

--- usr/src/include/kvm.h
+++ usr/src/include/kvm.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)kvm.h	5.6 (Berkeley) %G%
+ *	@(#)kvm.h	5.7 (Berkeley) %G%
  */
 
 #ifndef _KVM_H_
@@ -14,20 +14,27 @@
 #define	VRS_SYM		"_version"
 #define	VRS_KEY		"VERSION"
 
+#include <nlist.h>
 #include <sys/cdefs.h>
 
 __BEGIN_DECLS
-struct proc;
-struct user;
-char		*kvm_getargs __P((const struct proc *, const struct user *));
-struct eproc	*kvm_geteproc __P((const struct proc *));
-char		*kvm_geterr __P((void));
-int		 kvm_getprocs __P((int, int));
-struct user	*kvm_getu __P((const struct proc *));
-struct proc	*kvm_nextproc __P((void));
-int		 kvm_nlist __P((struct nlist *));
-int		 kvm_openfiles __P((const char *, const char *, const char *));
-int		 kvm_read __P((void *, void *, int));
+
+typedef struct __kvm kvm_t;
+
+kvm_t	*kvm_open __P((const char *, const char *, const char *, int,
+		       const char *));
+kvm_t	*kvm_openfiles __P((const char *, const char *, const char *, int,
+			    char *));
+int	 kvm_close __P((kvm_t *));
+int	 kvm_read __P((kvm_t *, unsigned long, char *, unsigned int));
+int	 kvm_write __P((kvm_t *, unsigned long, const char *, unsigned int));
+int	 kvm_nlist __P((kvm_t *, struct nlist *));
+char	*kvm_geterr __P((kvm_t *));
+
+struct kinfo_proc *kvm_getprocs __P((kvm_t *, int, int, int *));
+char	**kvm_getargv __P((kvm_t *, const struct kinfo_proc *, int));
+char	**kvm_getenvv __P((kvm_t *, const struct kinfo_proc *, int));
+
 __END_DECLS
 
 #endif /* !_KVM_H_ */
