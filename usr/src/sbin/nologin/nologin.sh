d4d42d5eefcba9119d22b97ef52648aee0f23697
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-04-02 21:05:25 -0800

    date and time created 92/04/02 14:05:25 by bostic
    SCCS-vsn: sbin/nologin/nologin.sh 5.1

---
+++ usr/src/sbin/nologin/nologin.sh
@@ -0,0 +1,12 @@
+#!/bin/sh -
+#
+# Copyright (c) 1992 The Regents of the University of California.
+# All rights reserved.
+#
+# %sccs.include.redist.sh%
+#
+#	@(#)nologin.sh	5.1 (Berkeley) %G%
+#
+
+echo 'This account is currently not available.'
+exit 1
