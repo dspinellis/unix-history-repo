7e09e5abdc2cbf6bf733a6c79a5d620caf4fcee5
William F. Jolitz <wjolitz@soda.berkeley.edu>
1992-04-10 12:54:42 -0800

    386BSD 0.1 development Work on file usr/othersrc/public/ghostscript-2.4.1/gxlum.h Work on file usr/othersrc/public/ghostscript-2.4.1/gzcolor.h
    Co-Authored-By: Lynne Greer Jolitz <ljolitz@cardio.ucsf.edu>
    Synthesized-from: 386BSD-0.1

---
+++ usr/othersrc/public/ghostscript-2.4.1/gxlum.h
@@ -0,0 +1,27 @@
+/* Copyright (C) 1992 Aladdin Enterprises.  All rights reserved.
+   Distributed by Free Software Foundation, Inc.
+
+This file is part of Ghostscript.
+
+Ghostscript is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY.  No author or distributor accepts responsibility
+to anyone for the consequences of using it or for whether it serves any
+particular purpose or works at all, unless he says so in writing.  Refer
+to the Ghostscript General Public License for full details.
+
+Everyone is granted permission to copy, modify and redistribute
+Ghostscript, but only under the conditions described in the Ghostscript
+General Public License.  A copy of this license is supposed to have been
+given to you along with Ghostscript so you can know your rights and
+responsibilities.  It should be in a file named COPYING.  Among other
+things, the copyright notice and this notice must be preserved on all
+copies.  */
+
+/* gxlum.h */
+/* Luminance computation parameters for Ghostscript */
+
+/* Color weights used for computing luminance. */
+#define lum_red_weight	299
+#define lum_green_weight	587
+#define lum_blue_weight	114
+#define lum_all_weights	(lum_red_weight + lum_green_weight + lum_blue_weight)
