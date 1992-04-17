436c3ae0bc75fa01c18e8f7304a00af6468c492a
Marc Teitelbaum <marc@ucbvax.Berkeley.EDU>
1992-04-16 21:36:58 -0800

    check if root has dot in PATH and issue warning
    SCCS-vsn: bin/sh/exec.c 5.3

--- usr/src/bin/sh/exec.c
+++ usr/src/bin/sh/exec.c
@@ -5,11 +5,37 @@
  * This code is derived from software contributed to Berkeley by
  * Kenneth Almquist.
  *
- * %sccs.include.redist.c%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the University of
+ *	California, Berkeley and its contributors.
+ * 4. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)exec.c	5.2 (Berkeley) %G%";
+static char sccsid[] = "@(#)exec.c	5.2 (Berkeley) 3/13/91";
 #endif /* not lint */
 
 /*
@@ -542,12 +568,15 @@ changepath(newval)
 	int index;
 	int firstchange;
 	int bltin;
+	int hasdot;
 
 	old = pathval();
 	new = newval;
 	firstchange = 9999;	/* assume no change */
-	index = 0;
+	index = hasdot = 0;
 	bltin = -1;
+	if (*new == ':')
+		hasdot++;
 	for (;;) {
 		if (*old != *new) {
 			firstchange = index;
@@ -561,10 +590,17 @@ changepath(newval)
 		if (*new == '%' && bltin < 0 && prefix("builtin", new + 1))
 			bltin = index;
 		if (*new == ':') {
+			char c = *(new+1);
+
 			index++;
+			if (c == ':' || c == '\0' || (c == '.' && 
+			   ((c = *(new+2)) == ':' || c == '\0')))
+				hasdot++;
 		}
 		new++, old++;
 	}
+	if (hasdot && geteuid() == 0)
+		out2str("sh: warning: running as root with dot in PATH\n");
 	if (builtinloc < 0 && bltin >= 0)
 		builtinloc = bltin;		/* zap builtins */
 	if (builtinloc >= 0 && bltin < 0)
