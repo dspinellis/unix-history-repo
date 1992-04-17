8121e7d5714cce77522d5fef4e28b87806a444b9
Eric Allman <eric@ucbvax.Berkeley.EDU>
1992-04-17 01:39:49 -0800

    properly set up envelope when returning email
    SCCS-vsn: usr.sbin/sendmail/src/savemail.c 5.16

--- usr/src/usr.sbin/sendmail/src/savemail.c
+++ usr/src/usr.sbin/sendmail/src/savemail.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)savemail.c	5.15 (Berkeley) %G%";
+static char sccsid[] = "@(#)savemail.c	5.16 (Berkeley) %G%";
 #endif /* not lint */
 
 # include <sys/types.h>
