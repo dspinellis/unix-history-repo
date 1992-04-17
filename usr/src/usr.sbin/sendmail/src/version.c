aa80b85b238c081b3e991457f39d348ad141b0bb
Eric Allman <eric@ucbvax.Berkeley.EDU>
1992-04-17 01:40:56 -0800

    properly set up envelope when returning email; try to eliminate some of the CurEnv globals
    SCCS-vsn: usr.sbin/sendmail/src/version.c 5.96

--- usr/src/usr.sbin/sendmail/src/version.c
+++ usr/src/usr.sbin/sendmail/src/version.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)version.c	5.95 (Berkeley) %G%";
+static char sccsid[] = "@(#)version.c	5.96 (Berkeley) %G%";
 #endif /* not lint */
 
-char	Version[] = "5.95";
+char	Version[] = "5.96";
