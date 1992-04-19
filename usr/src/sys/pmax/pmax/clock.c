e588525df52ce928d1e4efa1cda6381b17cd0858
Ralph Campbell <ralph@ucbvax.Berkeley.EDU>
1992-04-19 15:56:20 -0800

    fix clock read/write for leap year and to match ULTRIX.
    SCCS-vsn: sys/pmax/pmax/clock.c 7.3

--- usr/src/sys/pmax/pmax/clock.c
+++ usr/src/sys/pmax/pmax/clock.c
@@ -11,7 +11,7 @@
  *
  * from: Utah $Hdr: clock.c 1.18 91/01/21$
  *
- *	@(#)clock.c	7.2 (Berkeley) %G%
+ *	@(#)clock.c	7.3 (Berkeley) %G%
  */
 
 #include "param.h"
@@ -42,7 +42,7 @@ startrtclock()
 	extern int tickadj;
 
 	tick = 15625;		/* number of micro-seconds between interrupts */
-	hz = 1000000 / 15625;	/* about 64 */
+	hz = 1000000 / 15625;	/* 64 Hz */
 	tickadj = 240000 / (60000000 / 15625);
 	c = (volatile struct chiptime *)MACH_CLOCK_ADDR;
 	c->rega = REGA_TIME_BASE | SELECTED_RATE;
@@ -62,21 +62,23 @@ static short dayyr[12] = {
  * from a filesystem.  Base provides the time to within six months,
  * and the time of year clock (if any) provides the rest.
  */
+void
 inittodr(base)
 	time_t base;
 {
 	register volatile struct chiptime *c;
 	register int days, yr;
 	int sec, min, hour, day, mon, year;
-	long deltat, badbase = 0;
-	int s;
+	long deltat;
+	int badbase, s;
 
 	if (base < 5*SECYR) {
-		printf("WARNING: preposterous time in file system\n");
+		printf("WARNING: preposterous time in file system");
 		/* read the system clock anyway */
 		base = 6*SECYR + 186*SECDAY + SECDAY/2;
 		badbase = 1;
-	}
+	} else
+		badbase = 0;
 
 	c = (volatile struct chiptime *)MACH_CLOCK_ADDR;
 	/* don't read clock registers while they are being updated */
@@ -88,21 +90,22 @@ inittodr(base)
 	hour = c->hour;
 	day = c->day;
 	mon = c->mon;
-	year = c->year + 19;
+	year = c->year + 20; /* must be multiple of 4 because chip knows leap */
 	splx(s);
 
 	/* simple sanity checks */
 	if (year < 70 || mon < 1 || mon > 12 || day < 1 || day > 31 ||
 	    hour > 23 || min > 59 || sec > 59) {
-		printf("WARNING: preposterous clock chip time");
 		/*
 		 * Believe the time in the file system for lack of
 		 * anything better, resetting the TODR.
 		 */
 		time.tv_sec = base;
-		if (!badbase)
+		if (!badbase) {
+			printf("WARNING: preposterous clock chip time\n");
 			resettodr();
-		return (0);
+		}
+		goto bad;
 	}
 	days = 0;
 	for (yr = 70; yr < year; yr++)
@@ -126,6 +129,7 @@ inittodr(base)
 		printf("WARNING: clock %s %d days",
 		    time.tv_sec < base ? "lost" : "gained", deltat / SECDAY);
 	}
+bad:
 	printf(" -- CHECK AND RESET THE DATE!\n");
 }
 
@@ -139,43 +143,48 @@ inittodr(base)
 resettodr()
 {
 	register volatile struct chiptime *c;
-	register int t, t2, t3;
+	register int t, t2;
+	int sec, min, hour, day, mon, year;
 	int s;
 
 	/* compute the year */
 	t2 = time.tv_sec / SECDAY;
-	t = 69;
+	year = 69;
 	while (t2 >= 0) {	/* whittle off years */
-		t3 = t2;
-		t++;
-		t2 -= LEAPYEAR(t) ? 366 : 365;
+		t = t2;
+		year++;
+		t2 -= LEAPYEAR(year) ? 366 : 365;
 	}
 
-	c = (volatile struct chiptime *)MACH_CLOCK_ADDR;
-	s = splclock();
-	c->regb |= REGB_SET_TIME;
-	c->year = t - 19;
-
-	/* t3 = month + day; separate */
-	t = LEAPYEAR(t);
-	for (t2 = 1; t2 < 12; t2++)
-		if (t3 < dayyr[t2] + (t && t2 > 1))
+	/* t = month + day; separate */
+	t2 = LEAPYEAR(year);
+	for (mon = 1; mon < 12; mon++)
+		if (t < dayyr[mon] + (t2 && mon > 1))
 			break;
 
-	/* t2 is month */
-	c->mon = t2;
-	t3 = t3 - dayyr[t2 - 1] + 1;
-	if (t && t2 > 2)
-		t3--;
-	c->day = t3;
+	day = t - dayyr[mon - 1] + 1;
+	if (t2 && mon > 2)
+		day--;
 
 	/* the rest is easy */
 	t = time.tv_sec % SECDAY;
-	c->hour = t / 3600;
+	hour = t / 3600;
 	t %= 3600;
-	c->min = t / 60;
-	c->sec = t % 60;
-	c->regb &= ~REGB_SET_TIME;
+	min = t / 60;
+	sec = t % 60;
+
+	c = (volatile struct chiptime *)MACH_CLOCK_ADDR;
+	s = splclock();
+	t = c->regb;
+	c->regb = t | REGB_SET_TIME;
+	MachEmptyWriteBuffer();
+	c->sec = sec;
+	c->min = min;
+	c->hour = hour;
+	c->day = day;
+	c->mon = mon;
+	c->year = year - 20; /* must be multiple of 4 because chip knows leap */
+	c->regb = t;
 	MachEmptyWriteBuffer();
 	splx(s);
 }
