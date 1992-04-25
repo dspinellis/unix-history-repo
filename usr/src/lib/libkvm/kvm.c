a6f8f548b1efcfea7877fe39fc86185a07286bea
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-04-24 22:08:44 -0800

    move the kernel file to /var/db
    SCCS-vsn: lib/libkvm/kvm.c 5.25

--- usr/src/lib/libkvm/kvm.c
+++ usr/src/lib/libkvm/kvm.c
@@ -6,7 +6,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)kvm.c	5.24 (Berkeley) %G%";
+static char sccsid[] = "@(#)kvm.c	5.25 (Berkeley) %G%";
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/param.h>
