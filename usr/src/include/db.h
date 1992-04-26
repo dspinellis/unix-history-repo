10531a68be8693bb521b3cb0eb734e6ad246587e
Ralph Campbell <ralph@ucbvax.Berkeley.EDU>
1992-04-26 03:22:10 -0800

    added sys/types.h since its needed.
    SCCS-vsn: include/db.h 5.18

--- usr/src/include/db.h
+++ usr/src/include/db.h
@@ -4,12 +4,13 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)db.h	5.17 (Berkeley) %G%
+ *	@(#)db.h	5.18 (Berkeley) %G%
  */
 
 #ifndef _DB_H_
 #define	_DB_H_
 
+#include <sys/types.h>
 #include <sys/cdefs.h>
 
 #define	RET_ERROR	-1		/* Return values. */
