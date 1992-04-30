4a3e0b4bc275dfd195ed4343f4be11c82b946d16
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-04-29 18:52:12 -0800

    update from LBL (Chris, Craig and Steve)
    SCCS-vsn: lib/libkvm/kvm.3 5.2
    SCCS-vsn: lib/libkvm/kvm.c 5.25
    SCCS-vsn: lib/libkvm/kvm_geterr.3 5.2
    SCCS-vsn: lib/libkvm/kvm_getprocs.3 5.2
    SCCS-vsn: lib/libkvm/kvm_hp300.c 5.24
    SCCS-vsn: lib/libkvm/kvm_nlist.3 5.4
    SCCS-vsn: lib/libkvm/kvm_open.3 5.2
    SCCS-vsn: lib/libkvm/kvm_sparc.c 5.2

--- usr/src/lib/libkvm/kvm_hp300.c
+++ usr/src/lib/libkvm/kvm_hp300.c
@@ -6,11 +6,11 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)kvm_hp300.c	5.23 (Berkeley) %G%";
+static char sccsid[] = "@(#)kvm_hp300.c	5.24 (Berkeley) %G%";
 #endif /* LIBC_SCCS and not lint */
 
 /*
- * hp300 machine depedent routines for kvm.  Hopefully, the forthcoming 
+ * Hp300 machine depedent routines for kvm.  Hopefully, the forthcoming 
  * vm code will one day obsolete this module.
  */
 
@@ -25,7 +25,7 @@ static char sccsid[] = "@(#)kvm_hp300.c	5.23 (Berkeley) %G%";
 #include <vm/vm_param.h>
 
 #include <limits.h>
-#include <ndbm.h>
+#include <db.h>
 
 #include "kvm_private.h"
 
