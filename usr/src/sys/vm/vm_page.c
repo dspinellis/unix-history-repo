2ab9a7fa1a969761b2112a61094e37d725629718
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-05-05 00:06:03 -0800

    new include format
    SCCS-vsn: sys/vm/vm_page.c 7.13
    SCCS-vsn: sys/vm/vm_kern.c 7.9

--- usr/src/sys/vm/vm_page.c
+++ usr/src/sys/vm/vm_page.c
@@ -7,7 +7,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)vm_page.c	7.12 (Berkeley) %G%
+ *	@(#)vm_page.c	7.13 (Berkeley) %G%
  *
  *
  * Copyright (c) 1987, 1990 Carnegie-Mellon University.
@@ -40,12 +40,13 @@
  *	Resident memory management module.
  */
 
-#include "param.h"
+#include <sys/param.h>
+#include <sys/systm.h>
 
-#include "vm.h"
-#include "vm_map.h"
-#include "vm_page.h"
-#include "vm_pageout.h"
+#include <vm/vm.h>
+#include <vm/vm_page.h>
+#include <vm/vm_map.h>
+#include <vm/vm_pageout.h>
 
 /*
  *	Associated with page of user-allocatable memory is a
