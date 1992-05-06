1d1aca8f39edb2351f00a2c55d21a60d0785143d
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-05-05 21:33:59 -0800

    set no chdir bit no matter where fts_build called from
    SCCS-vsn: lib/libc/gen/fts.c 5.38

--- usr/src/lib/libc/gen/fts.c
+++ usr/src/lib/libc/gen/fts.c
@@ -6,7 +6,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)fts.c	5.37 (Berkeley) %G%";
+static char sccsid[] = "@(#)fts.c	5.38 (Berkeley) %G%";
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/param.h>
@@ -573,10 +573,9 @@ fts_build(sp, type)
 	 */
 	if (nlinks || type == BREAD)
 		if (FCHDIR(sp, dirfd(dirp))) {
-			if (nlinks && type == BREAD) {
-				cur->fts_flags |= FTS_DONTCHDIR;
+			if (nlinks && type == BREAD)
 				cur->fts_errno = errno;
-			}
+			cur->fts_flags |= FTS_DONTCHDIR;
 			descend = 0;
 			cderrno = errno;
 		} else {
