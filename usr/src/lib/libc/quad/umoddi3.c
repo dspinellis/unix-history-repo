0a7edd361d24874368cdd9ff49a7087477650e65
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-05-12 22:20:11 -0800

    put on a Berkeley copyright notice -- original didn't have a GNU copyright, I'll add one as soon as I find out which one to add date and time created 92/05/12 15:20:11 by bostic
    SCCS-vsn: lib/libc/quad/umoddi3.c 5.1

---
+++ usr/src/lib/libc/quad/umoddi3.c
@@ -0,0 +1,39 @@
+/*-
+ * Copyright (c) 1992 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.redist.c%
+ */
+
+#if defined(LIBC_SCCS) && !defined(lint)
+static char sccsid[] = "@(#)umoddi3.c	5.1 (Berkeley) %G%";
+#endif /* LIBC_SCCS and not lint */
+
+#include "longlong.h"
+
+extern void __bdiv ();
+
+long long 
+__umoddi3 (u, v)
+     long long u, v;
+{
+  unsigned long a[2][2], b[2], q[2], r[2];
+  long_long w;
+  long_long uu, vv;
+
+  uu.ll = u;
+  vv.ll = v;
+
+  a[HIGH][HIGH] = 0;
+  a[HIGH][LOW] = 0;
+  a[LOW][HIGH] = uu.s.high;
+  a[LOW][LOW] = uu.s.low;
+  b[HIGH] = vv.s.high;
+  b[LOW] = vv.s.low;
+
+  __bdiv (a, b, q, r, sizeof a, sizeof b);
+
+  w.s.high = r[HIGH];
+  w.s.low = r[LOW];
+  return w.ll;
+}
