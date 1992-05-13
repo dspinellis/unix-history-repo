9891c657cd3cc878aac355c05bf07fd10ada2b0d
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-05-13 01:16:26 -0800

    SItype becomes explicit `long int'
    SCCS-vsn: lib/libc/quad/cmpdi2.c 5.2
    SCCS-vsn: lib/libc/quad/ucmpdi2.c 5.2

--- usr/src/lib/libc/quad/ucmpdi2.c
+++ usr/src/lib/libc/quad/ucmpdi2.c
@@ -6,12 +6,12 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)ucmpdi2.c	5.1 (Berkeley) %G%";
+static char sccsid[] = "@(#)ucmpdi2.c	5.2 (Berkeley) %G%";
 #endif /* LIBC_SCCS and not lint */
 
 #include "longlong.h"
 
-SItype
+long int
 __ucmpdi2 (a, b)
      long long a, b;
 {
