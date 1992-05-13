c2562f15427012f717c5191cc37ed79c267e273e
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-05-13 01:15:26 -0800

    use BSD conventions for determining ENDIAN; clean up longlong union
    SCCS-vsn: lib/libc/quad/quad.h 5.3

--- usr/src/lib/libc/quad/quad.h
+++ usr/src/lib/libc/quad/quad.h
@@ -4,75 +4,48 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)quad.h	5.2 (Berkeley) %G%
+ *	@(#)quad.h	5.3 (Berkeley) %G%
  */
 
 /* More subroutines needed by GCC output code on some machines.  */
 /* Compile this one with gcc.  */
 #include <sys/param.h>
 
-#include <stddef.h>
-
 #define	BITS_PER_WORD	(NBBY * sizeof(long))
-
-#ifndef SItype
-#define SItype long int
-#endif
-
-/* long long ints are pairs of long ints in the order determined by
-   WORDS_BIG_ENDIAN.  */
-
-#ifdef WORDS_BIG_ENDIAN
-  struct longlong {long high, low;};
-#else
-  struct longlong {long low, high;};
-#endif
 
 /* We need this union to unpack/pack longlongs, since we don't have
    any arithmetic yet.  Incoming long long parameters are stored
    into the `ll' field, and the unpacked result is read from the struct
    longlong.  */
 
-typedef union
-{
-  struct longlong s;
-  long long ll;
-  SItype i[2];
-  unsigned SItype ui[2];
+typedef union {
+	long long ll;
+	struct { long val[2]; } s;
 } long_long;
+#define high val[_QUAD_HIGHWORD]
+#define low val[_QUAD_LOWWORD]
+
+#define HIGH _QUAD_HIGHWORD
+#define LOW _QUAD_LOWWORD
 
 /* Internally, long long ints are strings of unsigned shorts in the
-   order determined by BYTES_BIG_ENDIAN.  */
+   order determined by BYTE_ORDER.  */
 
 #define B 0x10000
 #define low16 (B - 1)
 
-#ifdef BYTES_BIG_ENDIAN
-
-/* Note that HIGH and LOW do not describe the order
-   of words in a long long.  They describe the order of words
-   in vectors ordered according to the byte order.  */
-
-#define HIGH 0
-#define LOW 1
-
+#if BYTE_ORDER == BIG_ENDIAN
 #define big_end(n)	0 
 #define little_end(n)	((n) - 1)
 #define next_msd(i)	((i) - 1)
 #define next_lsd(i)	((i) + 1)
 #define is_not_msd(i,n)	((i) >= 0)
 #define is_not_lsd(i,n)	((i) < (n))
-
 #else
-
-#define LOW 0
-#define HIGH 1
-
 #define big_end(n)	((n) - 1)
 #define little_end(n)	0 
 #define next_msd(i)	((i) + 1)
 #define next_lsd(i)	((i) - 1)
 #define is_not_msd(i,n)	((i) < (n))
 #define is_not_lsd(i,n)	((i) >= 0)
-
 #endif
