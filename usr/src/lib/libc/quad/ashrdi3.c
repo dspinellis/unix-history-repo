0fd52f7dccbe0c1ba913fe297366e392eb160444
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-05-12 22:19:57 -0800

    put on a Berkeley copyright notice -- original didn't have a GNU copyright, I'll add one as soon as I find out which one to add date and time created 92/05/12 15:19:57 by bostic
    SCCS-vsn: lib/libc/quad/ashrdi3.c 5.1
    SCCS-vsn: lib/libc/quad/cmpdi2.c 5.1

---
+++ usr/src/lib/libc/quad/ashrdi3.c
@@ -0,0 +1,44 @@
+/*-
+ * Copyright (c) 1992 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.redist.c%
+ */
+
+#if defined(LIBC_SCCS) && !defined(lint)
+static char sccsid[] = "@(#)ashrdi3.c	5.1 (Berkeley) %G%";
+#endif /* LIBC_SCCS and not lint */
+
+#include "longlong.h"
+
+long long
+__ashrdi3 (u, b1)
+     long long u;
+     long long b1;
+{
+  long_long w;
+  unsigned long carries;
+  int bm;
+  long_long uu;
+  int b = b1;
+
+  if (b == 0)
+    return u;
+
+  uu.ll = u;
+
+  bm = (sizeof (int) * BITS_PER_UNIT) - b;
+  if (bm <= 0)
+    {
+      w.s.high = uu.s.high >> 31; /* just to make w.s.high 1..1 or 0..0 */
+      w.s.low = uu.s.high >> -bm;
+    }
+  else
+    {
+      carries = (unsigned long)uu.s.high << bm;
+      w.s.high = uu.s.high >> b;
+      w.s.low = ((unsigned long)uu.s.low >> b) | carries;
+    }
+
+  return w.ll;
+}
