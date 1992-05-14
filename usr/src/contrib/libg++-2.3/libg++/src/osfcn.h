dc9983007b70188db8e65cfb5a4b8cdc40dfc505
CSRG <csrg@ucbvax.Berkeley.EDU>
1992-05-13 21:36:11 -0800

    BSD 4_4_Lite2 development Work on file usr/src/contrib/libg++-2.3/libg++/src/osfcn.h
    Synthesized-from: CSRG/cd3/4.4BSD-Lite2

---
+++ usr/src/contrib/libg++-2.3/libg++/src/osfcn.h
@@ -0,0 +1,17 @@
+
+#ifndef OSFCN_H
+#define OSFCN_H 1
+
+#include <std.h>
+#include <time.h>
+#include <sys/types.h>
+#if _G_HAVE_SYS_SOCKET
+#include <sys/socket.h>
+#endif
+#include <sys/time.h>
+#if _G_HAVE_SYS_RESOURCE
+#include <sys/resource.h>
+#endif
+
+
+#endif
