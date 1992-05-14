20370a5a49247b8e6d7ebf0855815b5a1f8d48c6
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1992-05-13 23:49:05 -0800

    cannot follow a pointer after structure that contains it is freed
    SCCS-vsn: sys/kern/uipc_usrreq.c 7.33

--- usr/src/sys/kern/uipc_usrreq.c
+++ usr/src/sys/kern/uipc_usrreq.c
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 1982, 1986 Regents of the University of California.
  *
- *	@(#)uipc_usrreq.c	7.32 (Berkeley) %G%
+ *	@(#)uipc_usrreq.c	7.33 (Berkeley) %G%
  */
 
 #include "param.h"
@@ -611,7 +611,7 @@ extern	struct domain unixdomain;
 
 unp_gc()
 {
-	register struct file *fp;
+	register struct file *fp, *nextfp;
 	register struct socket *so;
 
 	if (unp_gcing)
@@ -660,7 +660,8 @@ restart:
 			unp_scan(so->so_rcv.sb_mb, unp_mark);
 		}
 	} while (unp_defer);
-	for (fp = filehead; fp; fp = fp->f_filef) {
+	for (fp = filehead; fp; fp = nextfp) {
+		nextfp = fp->f_filef;
 		if (fp->f_count == 0)
 			continue;
 		if (fp->f_count == fp->f_msgcount && (fp->f_flag & FMARK) == 0)
