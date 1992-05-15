ef3b17e6f3fbba87b9b4ae937bf2d3e21a32b1ac
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-05-14 20:26:28 -0800

    date and time created 92/05/14 13:26:28 by bostic
    SCCS-vsn: games/caesar/rot13.sh 5.1

---
+++ usr/src/games/caesar/rot13.sh
@@ -0,0 +1,11 @@
+#!/bin/sh -
+#
+# Copyright (c) 1992 The Regents of the University of California.
+# All rights reserved.
+#
+# %sccs.include.redist.sh%
+#
+#	@(#)rot13.sh	5.1 (Berkeley) %G%
+#
+
+caesar 13 $*
