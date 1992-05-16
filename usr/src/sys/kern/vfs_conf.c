4eae2a0ab742ddde4f1d06706e375b5058c95420
John Heidemann <heideman@ucbvax.Berkeley.EDU>
1992-05-15 19:23:55 -0800

    new vnode interface config tables added (ops and opvs)
    SCCS-vsn: sys/kern/vfs_conf.c 7.8

--- usr/src/sys/kern/vfs_conf.c
+++ usr/src/sys/kern/vfs_conf.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)vfs_conf.c	7.7 (Berkeley) %G%
+ *	@(#)vfs_conf.c	7.8 (Berkeley) %G%
  */
 
 #include <sys/param.h>
@@ -67,3 +67,147 @@ struct vfsops *vfssw[] = {
 	NULL,			/* 4 = MOUNT_PC */
 	LFS_VFSOPS,		/* 5 = MOUNT_LFS */
 };
+
+
+/*
+ * Vnode_op_descs lists all vnode operations supported.
+ * At boot time vfs_op_init examines this list
+ * to configure vnode operations vectors.
+ */
+
+extern struct vnodeop_desc
+	vop_lookup_desc,
+	vop_create_desc,
+	vop_mknod_desc,
+	vop_open_desc,
+	vop_close_desc,
+	vop_access_desc,
+	vop_getattr_desc,
+	vop_setattr_desc,
+	vop_read_desc,
+	vop_write_desc,
+	vop_ioctl_desc,
+	vop_select_desc,
+	vop_mmap_desc,
+	vop_fsync_desc,
+	vop_seek_desc,
+	vop_remove_desc,
+	vop_link_desc,
+	vop_rename_desc,
+	vop_mkdir_desc,
+	vop_rmdir_desc,
+	vop_symlink_desc,
+	vop_readdir_desc,
+	vop_readlink_desc,
+	vop_abortop_desc,
+	vop_inactive_desc,
+	vop_reclaim_desc,
+	vop_lock_desc,
+	vop_unlock_desc,
+	vop_bmap_desc,
+	vop_strategy_desc,
+	vop_print_desc,
+	vop_islocked_desc,
+	vop_advlock_desc,
+	vop_blkatoff_desc,
+	vop_vget_desc,
+	vop_valloc_desc,
+	vop_vfree_desc,
+	vop_truncate_desc,
+	vop_update_desc,
+	vop_bwrite_desc,
+/* and the default */
+	vop_default_desc;
+
+struct vnodeop_desc *vfs_op_descs[] = {
+	&vop_default_desc,   /* must be first */
+	&vop_lookup_desc,
+	&vop_create_desc,
+	&vop_mknod_desc,
+	&vop_open_desc,
+	&vop_close_desc,
+	&vop_access_desc,
+	&vop_getattr_desc,
+	&vop_setattr_desc,
+	&vop_read_desc,
+	&vop_write_desc,
+	&vop_ioctl_desc,
+	&vop_select_desc,
+	&vop_mmap_desc,
+	&vop_fsync_desc,
+	&vop_seek_desc,
+	&vop_remove_desc,
+	&vop_link_desc,
+	&vop_rename_desc,
+	&vop_mkdir_desc,
+	&vop_rmdir_desc,
+	&vop_symlink_desc,
+	&vop_readdir_desc,
+	&vop_readlink_desc,
+	&vop_abortop_desc,
+	&vop_inactive_desc,
+	&vop_reclaim_desc,
+	&vop_lock_desc,
+	&vop_unlock_desc,
+	&vop_bmap_desc,
+	&vop_strategy_desc,
+	&vop_print_desc,
+	&vop_islocked_desc,
+	&vop_advlock_desc,
+	&vop_blkatoff_desc,
+	&vop_vget_desc,
+	&vop_valloc_desc,
+	&vop_vfree_desc,
+	&vop_truncate_desc,
+	&vop_update_desc,
+	&vop_bwrite_desc,
+	NULL
+};
+
+
+
+
+/*
+ *
+ * vfs_opv_descs enumerates the list of vnode classes,
+ * each with it's own vnode operation vector.
+ * It is consulted at system
+ * boot to build operation vectors.
+ * It's also null terminated.
+ *
+ * Out-of-kernel, someone else (more knowlegable about what file
+ * systems live in this address space) must specify this table.
+ */
+
+extern struct vnodeopv_desc ffs_vnodeop_opv_desc;
+extern struct vnodeopv_desc ffs_specop_opv_desc;
+extern struct vnodeopv_desc ffs_fifoop_opv_desc;
+extern struct vnodeopv_desc lfs_vnodeop_opv_desc;
+extern struct vnodeopv_desc lfs_specop_opv_desc;
+extern struct vnodeopv_desc lfs_fifoop_opv_desc;
+extern struct vnodeopv_desc mfs_vnodeop_opv_desc;
+extern struct vnodeopv_desc dead_vnodeop_opv_desc;
+extern struct vnodeopv_desc fifo_vnodeop_opv_desc;
+extern struct vnodeopv_desc spec_vnodeop_opv_desc;
+extern struct vnodeopv_desc nfsv2_vnodeop_opv_desc;
+extern struct vnodeopv_desc spec_nfsv2nodeop_opv_desc;
+extern struct vnodeopv_desc fifo_nfsv2nodeop_opv_desc;
+
+
+struct vnodeopv_desc *vfs_opv_descs[] = {
+	&ffs_vnodeop_opv_desc,
+	&ffs_specop_opv_desc,
+	&ffs_fifoop_opv_desc,
+	&lfs_vnodeop_opv_desc,
+	&lfs_specop_opv_desc,
+	&lfs_fifoop_opv_desc,
+	&mfs_vnodeop_opv_desc,
+	&dead_vnodeop_opv_desc,
+	&fifo_vnodeop_opv_desc,
+	&spec_vnodeop_opv_desc,
+	&nfsv2_vnodeop_opv_desc,
+	&spec_nfsv2nodeop_opv_desc,
+	&fifo_nfsv2nodeop_opv_desc,
+	NULL
+};
+
