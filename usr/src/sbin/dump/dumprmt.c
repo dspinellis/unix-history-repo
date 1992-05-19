b5a59d7f30f2d1a5df04fabe498cf1284ae4da95
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1992-05-19 01:24:05 -0800

    need additional include
    SCCS-vsn: sbin/dump/dumprmt.c 5.14

--- usr/src/sbin/dump/dumprmt.c
+++ usr/src/sbin/dump/dumprmt.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)dumprmt.c	5.13 (Berkeley) %G%";
+static char sccsid[] = "@(#)dumprmt.c	5.14 (Berkeley) %G%";
 #endif /* not lint */
 
 #ifdef sunos
@@ -26,6 +26,7 @@ static char sccsid[] = "@(#)dumprmt.c	5.13 (Berkeley) %G%";
 #include <sys/mtio.h>
 #include <sys/ioctl.h>
 #include <sys/socket.h>
+#include <sys/time.h>
 #include <ufs/ufs/dinode.h>
 #include <stdio.h>
 #endif
