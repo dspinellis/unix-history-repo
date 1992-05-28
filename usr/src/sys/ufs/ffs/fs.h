b77b7b2d83b54b5ac88af68996940154e6e16ac4
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1992-05-28 05:58:18 -0800

    declarations needed by utilities moved from ffs_extern.h to fs.h
    SCCS-vsn: sys/ufs/ffs/fs.h 7.15
    SCCS-vsn: sys/ufs/ffs/ffs_extern.h 7.11

--- usr/src/sys/ufs/ffs/fs.h
+++ usr/src/sys/ufs/ffs/fs.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)fs.h	7.14 (Berkeley) %G%
+ *	@(#)fs.h	7.15 (Berkeley) %G%
  */
 
 /*
@@ -404,3 +404,6 @@ struct	ocg {
  * NINDIR is the number of indirects in a file system block.
  */
 #define	NINDIR(fs)	((fs)->fs_nindir)
+
+extern int inside[], around[];
+extern u_char *fragtbl[];
