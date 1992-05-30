9c9912cf87f4df4760d8ce1f1239a7f0fb91b6d6
Eric Allman <eric@ucbvax.Berkeley.EDU>
1992-05-29 17:38:40 -0800

    allow configuration of GECOS matching; do regular aliasing after a GECOS match
    SCCS-vsn: usr.sbin/sendmail/src/recipient.c 5.28
    SCCS-vsn: usr.sbin/sendmail/src/conf.h 5.25
    SCCS-vsn: usr.sbin/sendmail/src/sendmail.h 5.32
    SCCS-vsn: usr.sbin/sendmail/src/readcf.c 5.38

--- usr/src/usr.sbin/sendmail/src/conf.h
+++ usr/src/usr.sbin/sendmail/src/conf.h
@@ -5,7 +5,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)conf.h	5.24 (Berkeley) %G%
+ *	@(#)conf.h	5.25 (Berkeley) %G%
  */
 
 /*
--- usr/src/usr.sbin/sendmail/src/sendmail.h
+++ usr/src/usr.sbin/sendmail/src/sendmail.h
@@ -5,7 +5,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)sendmail.h	5.31 (Berkeley) %G%
+ *	@(#)sendmail.h	5.32 (Berkeley) %G%
  */
 
 /*
@@ -15,7 +15,7 @@
 # ifdef _DEFINE
 # define EXTERN
 # ifndef lint
-static char SmailSccsId[] =	"@(#)sendmail.h	5.31		%G%";
+static char SmailSccsId[] =	"@(#)sendmail.h	5.32		%G%";
 # endif lint
 # else  _DEFINE
 # define EXTERN extern
