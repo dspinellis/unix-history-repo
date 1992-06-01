1c9bb951783bd9a2dd79ec61cc78cdeaf97a6cca
Jan-Simon Pendry <pendry@ucbvax.Berkeley.EDU>
1992-06-01 04:22:11 -0800

    add types for LOFS, FDESC, PORTAL file systems
    SCCS-vsn: sys/sys/mount.h 7.32

--- usr/src/sys/sys/mount.h
+++ usr/src/sys/sys/mount.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)mount.h	7.31 (Berkeley) %G%
+ *	@(#)mount.h	7.32 (Berkeley) %G%
  */
 
 #ifndef KERNEL
@@ -56,7 +56,10 @@ struct statfs {
 #define	MOUNT_MFS	3
 #define	MOUNT_PC	4
 #define	MOUNT_LFS	5
-#define	MOUNT_MAXTYPE	5
+#define	MOUNT_LOFS	6
+#define	MOUNT_FDESC	7
+#define	MOUNT_PORTAL	8
+#define	MOUNT_MAXTYPE	8
 
 /*
  * Structure per mounted file system.  Each mounted file system has an
