97c6769d4a564ef72fbe1ca3465e4a6311283152
William F. Jolitz <wjolitz@soda.berkeley.edu>
1992-06-02 00:07:49 -0800

    386BSD 0.1 development Work on file usr/othersrc/public/ghostscript-2.4.1/arch.h
    Co-Authored-By: Lynne Greer Jolitz <ljolitz@cardio.ucsf.edu>
    Synthesized-from: 386BSD-0.1

---
+++ usr/othersrc/public/ghostscript-2.4.1/arch.h
@@ -0,0 +1,9 @@
+/* Parameters derived from machine and compiler architecture */
+
+#define arch_is_big_endian 0
+#define arch_sizeof_short 2
+#define arch_sizeof_int 4
+#define arch_sizeof_long 4
+#define arch_floats_are_IEEE 1
+#define arch_arith_rshift 2
+#define arch_can_shift_full_long 0
