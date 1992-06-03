30a9acf67a55f05fba2be944f8ef01c7dac65466
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1992-06-03 04:17:25 -0800

    avoid null arguments
    SCCS-vsn: sys/kern/vfs_subr.c 7.77

--- usr/src/sys/kern/vfs_subr.c
+++ usr/src/sys/kern/vfs_subr.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)vfs_subr.c	7.76 (Berkeley) %G%
+ *	@(#)vfs_subr.c	7.77 (Berkeley) %G%
  */
 
 /*
@@ -1143,7 +1143,7 @@ kinfo_vnode(op, where, acopysize, arg, aneeded)
 	struct mount *omp;
 	struct vnode *vp;
 	register char *bp = where, *savebp;
-	char *ewhere = where + *acopysize;
+	char *ewhere;
 	int error;
 
 #define VPTRSZ	sizeof (struct vnode *)
@@ -1152,6 +1152,7 @@ kinfo_vnode(op, where, acopysize, arg, aneeded)
 		*aneeded = (numvnodes + KINFO_VNODESLOP) * (VPTRSZ + VNODESZ);
 		return (0);
 	}
+	ewhere = where + *acopysize;
 		
 	do {
 		if (vfs_busy(mp)) {
