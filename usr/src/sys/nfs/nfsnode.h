bd4cd0679bacc0ec4dc1078759107979b715074f
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1992-06-02 21:29:27 -0800

    quad's need 8-byte alignment
    SCCS-vsn: sys/nfs/nfsnode.h 7.24

--- usr/src/sys/nfs/nfsnode.h
+++ usr/src/sys/nfs/nfsnode.h
@@ -7,7 +7,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)nfsnode.h	7.23 (Berkeley) %G%
+ *	@(#)nfsnode.h	7.24 (Berkeley) %G%
  */
 
 /*
@@ -34,8 +34,8 @@ struct nfsnode {
 	nfsv2fh_t n_fh;			/* NFS File Handle */
 	long	n_flag;			/* Flag for locking.. */
 	struct	vnode *n_vnode;		/* vnode associated with this node */
-	time_t	n_attrstamp;		/* Time stamp for cached attributes */
 	struct	vattr n_vattr;		/* Vnode attribute cache */
+	time_t	n_attrstamp;		/* Time stamp for cached attributes */
 	struct	sillyrename *n_sillyrename; /* Ptr to silly rename struct */
 	off_t	n_size;			/* Current size of file */
 	int	n_error;		/* Save write error value */
