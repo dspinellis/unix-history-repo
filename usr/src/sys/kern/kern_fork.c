2f0099df3645a7e217ad110499759c361ee023f3
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1992-06-03 04:28:48 -0800

    NULL out p_wmesg
    SCCS-vsn: sys/kern/kern_fork.c 7.35

--- usr/src/sys/kern/kern_fork.c
+++ usr/src/sys/kern/kern_fork.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)kern_fork.c	7.34 (Berkeley) %G%
+ *	@(#)kern_fork.c	7.35 (Berkeley) %G%
  */
 
 #include "param.h"
@@ -151,6 +151,7 @@ again:
 	    (unsigned) ((caddr_t)&p2->p_endzero - (caddr_t)&p2->p_startzero));
 	bcopy(&p1->p_startcopy, &p2->p_startcopy,
 	    (unsigned) ((caddr_t)&p2->p_endcopy - (caddr_t)&p2->p_startcopy));
+	p2->p_wmesg = NULL;	/* XXX - should be in zero range */
 	p2->p_spare[0] = 0;	/* XXX - should be in zero range */
 	p2->p_spare[1] = 0;	/* XXX - should be in zero range */
 	p2->p_spare[2] = 0;	/* XXX - should be in zero range */
