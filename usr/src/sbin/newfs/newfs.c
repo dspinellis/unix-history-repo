9bf9a6dfaefb705d905702bf58b092485c191f6f
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1992-06-05 06:04:56 -0800

    include <unistd.h> to get lseek prototype
    SCCS-vsn: sbin/newfs/newfs.c 6.31

--- usr/src/sbin/newfs/newfs.c
+++ usr/src/sbin/newfs/newfs.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)newfs.c	6.30 (Berkeley) %G%";
+static char sccsid[] = "@(#)newfs.c	6.31 (Berkeley) %G%";
 #endif /* not lint */
 
 #ifndef lint
@@ -33,6 +33,7 @@ char copyright[] =
 #else
 #include <varargs.h>
 #endif
+#include <unistd.h>
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
