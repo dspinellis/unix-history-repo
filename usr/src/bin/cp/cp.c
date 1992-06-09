4241cdd1de5105c81e401e7cd8534a9a5de8fe47
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-06-08 23:19:42 -0800

    whitespace cleanups, no real changes
    SCCS-vsn: bin/cp/cp.c 5.33
    SCCS-vsn: bin/cp/extern.h 5.5

--- usr/src/bin/cp/cp.c
+++ usr/src/bin/cp/cp.c
@@ -15,28 +15,29 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)cp.c	5.32 (Berkeley) %G%";
+static char sccsid[] = "@(#)cp.c	5.33 (Berkeley) %G%";
 #endif /* not lint */
 
 /*
- * cp copies source files to target files.
+ * Cp copies source files to target files.
  * 
- * The global PATH_T structures "to" always containa the path to the
- * current target file, respectively.  Since fts(3) does not change
- * directories, this path can be either absolute or dot-realative.
+ * The global PATH_T structure "to" always contains the path to the
+ * current target file.  Since fts(3) does not change directories,
+ * this path can be either absolute or dot-realative.
  * 
  * The basic algorithm is to initialize "to" and use fts(3) to traverse
  * the file hierarchy rooted in the argument list.  A trivial case is the
- * case of 'cp file1 file2'. The more interesing case is the case of
+ * case of 'cp file1 file2'.  The more interesting case is the case of
  * 'cp file1 file2 ... fileN dir' where the hierarchy is traversed and the
- * path (relative to the root of the traversal) is appended to dir 
- * (stored in "to") to form the final target path.
+ * path (relative to the root of the traversal) is appended to dir (stored
+ * in "to") to form the final target path.
  */
 
 #include <sys/param.h>
 #include <sys/stat.h>
 #include <sys/mman.h>
 #include <sys/time.h>
+
 #include <dirent.h>
 #include <fcntl.h>
 #include <errno.h>
@@ -47,47 +48,39 @@ static char sccsid[] = "@(#)cp.c	5.32 (Berkeley) %G%";
 #include <fts.h>
 #include "extern.h"
 
+#define	STRIP_TRAILING_SLASH(p) {					\
+        while ((p).p_end > (p).p_path && (p).p_end[-1] == '/')		\
+                *--(p).p_end = 0;					\
+}
 
-#define FILE_TO_FILE 1
-#define FILE_TO_DIR  2
-#define DIR_TO_DNE  3
+static void	copy __P((FTS *));
+static int	mastercmp __P((const FTSENT **, const FTSENT **));
 
-#define STRIP_TRAILING_SLASH(p) { \
-        while ((p).p_end > (p).p_path && (p).p_end[-1] == '/') \
-                *--(p).p_end = 0; \
-		}
-
-static void copy __P((int, FTS *));
-static int mastercmp __P((const FTSENT **, const FTSENT **));
 PATH_T to = { to.p_path, "" };
 
 uid_t myuid;
 int exit_val, myumask;
-int iflag, pflag;
+int iflag, orflag, pflag, rflag;
 char *progname;
 
-static int rflag, orflag;
+static enum { FILE_TO_FILE, FILE_TO_DIR, DIR_TO_DNE } type;
+
 int
 main(argc, argv)
 	int argc;
-	char **argv;
+	char *argv[];
 {
-	extern int optind;
 	struct stat to_stat, tmp_stat;
-	register int c, r;
-	char *p;
-	char *target;
-	int fts_options;
-	int type;
 	FTS *ftsp;
-	int hflag, Hflag;
+	register int c, r;
+	int fts_options, Hflag, hflag;
+	char *p, *target;
 
 	/*
 	 * The utility cp(1) is used by mv(1) -- except for usage statements,
 	 * print the "called as" program name.
 	 */
 	progname = (p = rindex(*argv,'/')) ? ++p : *argv;
-
 
 	/*
          * Symbolic link handling is as follows:
@@ -99,7 +92,7 @@ main(argc, argv)
          *     all symbolic links on the command line.
          * 
          */
-	Hflag = hflag = rflag = orflag = 0;
+	Hflag = hflag = 0;
 	fts_options = FTS_NOCHDIR | FTS_LOGICAL;
 	while ((c = getopt(argc, argv, "HRfhipr")) != EOF) 
 		switch ((char)c) {
@@ -140,24 +133,31 @@ main(argc, argv)
 	if (argc < 2)
 		usage();
 
+	if (orflag) {
+		if (rflag) {
+			(void)fprintf(stderr,
+	    "cp: the -R and -r options are mutually exclusive.\n");
+			exit(1);
+		}
+		if (Hflag || hflag) {
+			(void)fprintf(stderr,
+	    "cp: the -r and the -H and -h options are mutually exclusive.\n");
+			exit(1);
+		}
+	}
+
 	if (hflag) {
 		fts_options &= ~FTS_PHYSICAL;
 		fts_options |= FTS_LOGICAL;
 	}
 
-	if (rflag && orflag) {
-		(void)fprintf(stderr,
-		    "cp: the -R and -r options are mutually exclusive.\n");
-		exit(1);
-	}
-
 	myuid = getuid();
 
-	/* copy the umask for explicit mode setting */
+	/* Copy the umask for explicit mode setting. */
 	myumask = umask(0);
 	(void)umask(myumask);
 
-	/* Save the target base in "to" */
+	/* Save the target base in "to". */
 	target = argv[--argc];
 	if (strlen(target) > MAXPATHLEN) {
 		err("%s: name too long", target);
@@ -172,14 +172,14 @@ main(argc, argv)
         STRIP_TRAILING_SLASH(to);
 	to.target_end = to.p_end;
 
-	/* Want to know when end of list for fts. */
+	/* Set end of argument list for fts(3). */
 	argv[argc] = NULL;     
 	
 	/*
 	 * Cp has two distinct cases:
 	 *
-	 * % cp [-rip] source target
-	 * % cp [-rip] source1 ... directory
+	 * cp [-R] source target
+	 * cp [-R] source1 ... sourceN directory
 	 *
 	 * In both cases, source can be either a file or a directory.
 	 *
@@ -189,7 +189,6 @@ main(argc, argv)
 	 *
 	 * In (2), the real target is not directory, but "directory/source".
 	 */
-
 	r = stat(to.p_path, &to_stat);
 	if (r == -1 && errno != ENOENT) {
 		err("%s: %s", to.p_path, strerror(errno));
@@ -205,10 +204,10 @@ main(argc, argv)
 		}
 		/*
 		 * Need to detect the case:
-		 * % cp -R dir foo
-		 * Where dir is a directory and foo does not exist, because 
-		 * in this special case we want pathname concatenations
-		 * turned on but not for the initial mkdir().
+		 *	cp -R dir foo
+		 * Where dir is a directory and foo does not exist, where
+		 * we want pathname concatenations turned on but not for
+		 * the initial mkdir().
 		 */
 		if (r == -1) {
 			if (orflag || (rflag && (hflag || Hflag)))
@@ -222,8 +221,7 @@ main(argc, argv)
 				type = FILE_TO_FILE;
 		} else
 			type = FILE_TO_FILE;
-	}
-	else
+	} else
 		/*
 		 * Case (2).  Target is a directory.
 		 */
@@ -233,37 +231,36 @@ main(argc, argv)
 		err("%s", strerror(errno)); 
 	        exit(1);
 	}
-	copy(type, ftsp);
+	copy(ftsp);
 	fts_close(ftsp);
 
 	exit(exit_val);
 }
 
 static void
-copy(type, ftsp)
-	int type;
+copy(ftsp)
 	FTS *ftsp;
 {
+	register FTSENT *curr;
+	register int base, nlen;
 	struct stat to_stat;
 	int dne;
-	register FTSENT *curr;
-	char *c;
-	char *n;
-	register int base, nlen;
+	char *c, *n;
 
-	while(curr = fts_read(ftsp)) {
-		if (curr->fts_info == FTS_ERR || curr->fts_info == FTS_NS) {
-			err("%s: %s", curr->fts_path, 
-			    strerror(curr->fts_errno));
+	while (curr = fts_read(ftsp)) {
+		switch(curr->fts_info) {
+		case FTS_NS:
+		case FTS_ERR:
+			err("%s: %s",
+			    curr->fts_path, strerror(curr->fts_errno));
 			exit_val = 1;
 			continue;
-		}
-		if (curr->fts_info == FTS_DP)
-			continue;
-		if (curr->fts_info == FTS_DC) {
+		case FTS_DC:
 			err("%s: directory causes a cycle", curr->fts_path);
 			exit_val = 1;
-			return;
+			continue;
+		case FTS_DP:
+			continue;
 		}
 
 		/*
@@ -271,44 +268,39 @@ copy(type, ftsp)
                  * source name to the target name.  
                  */
 		if (type != FILE_TO_FILE) {
-			if ((curr->fts_namelen + to.target_end - to.p_path + 1)
-			    > MAXPATHLEN) {
+			if ((curr->fts_namelen +
+			    to.target_end - to.p_path + 1) > MAXPATHLEN) {
 				err("%s/%s: name too long (not copied)", 
 				    to.p_path, curr->fts_name);
 				continue;
 			}
 
 			/*
-			 * We need to remember roots of traversals so we can
-			 * create correct pathnames.  In the case where
-			 * we have a directory copied to a non-existent one:
-			 * % cp -R a/dir noexist
-			 * we want the path names to be noexist/foo rather than
-			 * noexist/dir/foo, where foo is a file in dir, as 
-			 * opposed to the case where the target exists.
+			 * Need to remember the roots of traversals to create
+			 * correct pathnames.  If there's a directory being
+			 * copied to a non-existent directory, e.g.
+			 *	cp -R a/dir noexist
+			 * the resulting path name should be noexist/foo, not
+			 * noexist/dir/foo (where foo is a file in dir), which
+			 * is the case where the target exists.
+			 *
+			 * Also, check for "..".  This is for correct path
+			 * concatentation for paths ending in "..", e.g.
+			 *	cp -R .. /tmp
+			 * Paths ending in ".." are changed to ".".  This is
+			 * tricky, but seems the easiest way to fix the problem.
 			 */
-			if (curr->fts_level == FTS_ROOTLEVEL) {
+			if (curr->fts_level == FTS_ROOTLEVEL)
 				if (type != DIR_TO_DNE) {
 					c = rindex(curr->fts_path, '/');
-					base = (c == NULL)? 0 : 
+					base = (c == NULL) ? 0 : 
 						(int) (c - curr->fts_path + 1);
 
-				/*
-				 * This is necessary to assure that
-				 * correct concatentation of paths ending
-				 * in ".." occurs.  This is somewhat
-				 * tricky.  Note that the old
-				 * cp(1) did not handle these cases 
-				 * correctly.  This is sort of a hack
-				 * but it is the easiest way to
-				 * fix the problem.
-				 */
 					if (!strcmp(&curr->fts_path[base], 
 					    ".."))
 						base += 1;
 				} else
 					base = curr->fts_pathlen;
-			}
 
 			if (to.target_end[-1] != '/') {
 				*to.target_end = '/';
@@ -330,7 +322,7 @@ copy(type, ftsp)
 			if (to_stat.st_dev == curr->fts_statp->st_dev &&
 			    to_stat.st_ino == curr->fts_statp->st_ino) {
 				(void)fprintf(stderr,
-			            "%s: %s and %s are identical (not copied).\n",
+			    "%s: %s and %s are identical (not copied).\n",
 				    progname, to.p_path, curr->fts_path);
 				exit_val = 1;
 				continue;
@@ -338,7 +330,7 @@ copy(type, ftsp)
 			dne = 0;
 		}
 
-		switch(curr->fts_statp->st_mode & S_IFMT) {
+		switch (curr->fts_statp->st_mode & S_IFMT) {
 		case S_IFLNK:
 			copy_link(curr, !dne);
 			break;
@@ -365,8 +357,7 @@ copy(type, ftsp)
 					    strerror(errno));
 					return;
 		                }
-			}
-			else if (!S_ISDIR(to_stat.st_mode)) {
+			} else if (!S_ISDIR(to_stat.st_mode)) {
 				(void)fprintf(stderr, 
 				    "%s: %s: not a directory.\n", progname, 
 				    to.p_path);
@@ -405,39 +396,28 @@ copy(type, ftsp)
 }
 
 /*
- * Mastercmp() is the comparison function for the copy order.  The order is
- * to copy non-directory files before directory files.  There are two reasons
- * to do directories last.  The first is efficiency.  Files tend to be in the 
- * same cylinder group as their parent, whereas directories tend not to be.
- * Copying files all at once reduces seeking.  Second, deeply nested trees
- * could use up all the file descriptors if we didn't close one directory 
- * before recursively starting on the next.
+ * mastercmp --
+ *	The comparison function for the copy order.  The order is to copy
+ *	non-directory files before directory files.  The reason for this
+ *	is because files tend to be in the same cylinder group as their
+ *	parent directory, whereas directories tend not to be.  Copying the
+ *	files first reduces seeking.
  */
-
 static int
 mastercmp(a, b)
 	const FTSENT **a, **b;
 {
-	int a_info, b_info;
+	register int a_info, b_info;
 
 	a_info = (*a)->fts_info;
 	if (a_info == FTS_ERR || a_info == FTS_NS || a_info == FTS_DNR)
-		return 0;
+		return (0);
 	b_info = (*b)->fts_info;
 	if (b_info == FTS_ERR || b_info == FTS_NS || b_info == FTS_DNR)
-		return 0;
-
+		return (0);
 	if (a_info == FTS_D)
-		return -1;
+		return (-1);
 	if (b_info == FTS_D)
-		return 1;
-	
-	return 0;
+		return (1);
+	return (0);
 }
-
-
-
-
-
-
-
