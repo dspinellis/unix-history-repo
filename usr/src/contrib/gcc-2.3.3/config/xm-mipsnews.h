9538a1f0ed3a44f7f097af34ea8d9aaf59b1d413
CSRG <csrg@ucbvax.Berkeley.EDU>
1992-06-10 06:47:39 -0800

    BSD 4_4_Lite1 development Work on file usr/src/contrib/gcc-2.3.3/config/xm-mipsnews.h
    Synthesized-from: CSRG/cd2/4.4BSD-Lite1

---
+++ usr/src/contrib/gcc-2.3.3/config/xm-mipsnews.h
@@ -0,0 +1,6 @@
+/* This file is for the Sony Mips News running "NewsOS Version 5",
+   which is really System V.  */
+#include "xm-umips.h"
+
+/* Sony has a funny name for this symbol.  */
+#define sys_siglist _sys_siglist
