da20c3949fb81d7e1cdd3aa80b661956404b5721
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-06-18 18:49:21 -0800

    It's an ANSI kind of morning!  (Getopt, too.)
    SCCS-vsn: bin/rmdir/rmdir.c 5.4

--- usr/src/bin/rmdir/rmdir.c
+++ usr/src/bin/rmdir/rmdir.c
@@ -1,5 +1,5 @@
-/*
- * Copyright (c) 1983 The Regents of the University of California.
+/*-
+ * Copyright (c) 1992 The Regents of the University of California.
  * All rights reserved.
  *
  * %sccs.include.redist.c%
@@ -7,34 +7,53 @@
 
 #ifndef lint
 char copyright[] =
-"@(#) Copyright (c) 1983 The Regents of the University of California.\n\
+"@(#) Copyright (c) 1992 The Regents of the University of California.\n\
  All rights reserved.\n";
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)rmdir.c	5.3 (Berkeley) %G%";
+static char sccsid[] = "@(#)rmdir.c	5.4 (Berkeley) %G%";
 #endif /* not lint */
 
-/*
- * Remove directory
- */
+#include <errno.h>
+#include <unistd.h>
+#include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
+void usage __P((void));
+
+int
 main(argc, argv)
 	int argc;
-	char **argv;
+	char *argv[];
 {
-	int errors;
+	int ch, errors;
 
-	if (argc < 2) {
-		fprintf(stderr, "usage: rmdir directory ...\n");
-		exit(1);
-	}
-	for (errors = 0; *++argv;)
+	while ((ch = getopt(argc, argv, "")) != EOF)
+		switch(ch) {
+		case '?':
+		default:
+			usage();
+		}
+	argc -= optind;
+	argv += optind;
+
+	if (argc == 0)
+		usage();
+
+	for (errors = 0; *argv; ++argv)
 		if (rmdir(*argv) < 0) {
-			fprintf(stderr, "rmdir: ");
-			perror(*argv);
+			(void)fprintf(stderr,
+			    "rmdir: %s: %s\n", *argv, strerror(errno));
 			errors = 1;
 		}
 	exit(errors);
+}
+
+void
+usage()
+{
+	(void)fprintf(stderr, "usage: rmdir directory ...\n");
+	exit(1);
 }
