a31be2ad21e12d267402aca27cb8c74ec07c4521
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1992-06-19 01:54:51 -0800

    lint
    SCCS-vsn: sbin/dump/unctime.c 5.5

--- usr/src/sbin/dump/unctime.c
+++ usr/src/sbin/dump/unctime.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)unctime.c	5.4 (Berkeley) %G%";
+static char sccsid[] = "@(#)unctime.c	5.5 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -47,7 +47,7 @@ unctime(str)
 
 	if (strlen(str) != 25)
 		str[25] = 0;
-	strcpy(dbuf, str);
+	(void) strcpy(dbuf, str);
 	dbuf[E_MONTH+3] = 0;
 	if ((then.tm_mon = lookup(&dbuf[E_MONTH])) < 0)
 		return (-1);
