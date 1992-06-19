d5c958b7813b2c3adc320ebd7f26e1a105c9c29a
Andrew Cherenson <andrew@ucbvax.Berkeley.EDU>
1992-06-18 23:09:18 -0800

    fix MDTM comparisons, fix mcase conversion in mget
    SCCS-vsn: usr.bin/ftp/cmds.c 5.28

--- usr/src/usr.bin/ftp/cmds.c
+++ usr/src/usr.bin/ftp/cmds.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)cmds.c	5.27 (Berkeley) %G%";
+static char sccsid[] = "@(#)cmds.c	5.28 (Berkeley) %G%";
 #endif /* not lint */
 
 /*
@@ -625,22 +625,17 @@ usage:
 					tm->tm_mon++;
 					if (tm->tm_year > yy%100)
 						return (1);
-					else if (tm->tm_year == yy%100) {
-						if (tm->tm_mon > mo)
-							return (1);
-					} else if (tm->tm_mon == mo) {
-						if (tm->tm_mday > day)
-							return (1);
-					} else if (tm->tm_mday == day) {
-						if (tm->tm_hour > hour)
-							return (1);
-					} else if (tm->tm_hour == hour) {
-						if (tm->tm_min > min)
-							return (1);
-					} else if (tm->tm_min == min) {
-						if (tm->tm_sec > sec)
-							return (1);
-					}
+					if ((tm->tm_year == yy%100 && 
+					    tm->tm_mon > mo) ||
+					   (tm->tm_mon == mo && 
+					    tm->tm_mday > day) ||
+					   (tm->tm_mday == day && 
+					    tm->tm_hour > hour) ||
+					   (tm->tm_hour == hour && 
+					    tm->tm_min > min) ||
+					   (tm->tm_min == min && 
+					    tm->tm_sec > sec))
+						return (1);
 				} else {
 					printf("%s\n", reply_string);
 					verbose = overbose;
@@ -686,7 +681,7 @@ mget(argc, argv)
 {
 	extern jmp_buf jabort;
 	sig_t oldintr;
-	int ointer;
+	int ch, ointer;
 	char *cp, *tp, *tp2, tmpbuf[MAXPATHLEN];
 	void mabort();
 
@@ -707,20 +702,8 @@ mget(argc, argv)
 		if (mflag && confirm(argv[0], cp)) {
 			tp = cp;
 			if (mcase) {
-				while (*tp && !islower(*tp)) {
-					tp++;
-				}
-				if (!*tp) {
-					tp = cp;
-					tp2 = tmpbuf;
-					while ((*tp2 = *tp) != NULL) {
-						if (isupper(*tp2)) {
-							*tp2 = 'a' + *tp2 - 'A';
-						}
-						tp++;
-						tp2++;
-					}
-				}
+				for (tp2 = tmpbuf; ch = *tp++;)
+					*tp2++ = isupper(ch) ? tolower(ch) : ch;
 				tp = tmpbuf;
 			}
 			if (ntflag) {
