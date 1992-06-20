834b693c10473e741bd27a18a1d93d7f2cadff11
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-06-19 22:16:17 -0800

    use long's explicitly, off_t's are now quads
    SCCS-vsn: include/ranlib.h 5.3

--- usr/src/include/ranlib.h
+++ usr/src/include/ranlib.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)ranlib.h	5.2 (Berkeley) %G%
+ *	@(#)ranlib.h	5.3 (Berkeley) %G%
  */
 
 #ifndef _RANLIB_H_
@@ -15,10 +15,10 @@
 
 struct ranlib {
 	union {
-		off_t ran_strx;		/* string table index */
+		long ran_strx;		/* string table index */
 		char *ran_name;		/* in memory symbol name */
 	} ran_un;
-	off_t ran_off;			/* archive file offset */
+	long ran_off;			/* archive file offset */
 };
 
 #endif /* !_RANLIB_H_ */
