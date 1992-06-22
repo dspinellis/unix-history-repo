8fb2fd4f574e8d604d4d15b21e0f4edff7e88975
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-06-21 17:36:29 -0800

    libkern has its own include file with prototypes
    SCCS-vsn: sys/sys/systm.h 7.23

--- usr/src/sys/sys/systm.h
+++ usr/src/sys/sys/systm.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)systm.h	7.22 (Berkeley) %G%
+ *	@(#)systm.h	7.23 (Berkeley) %G%
  */
 
 extern const char *panicstr;	/* panic message */
@@ -68,8 +68,6 @@ void	ttyprintf __P((struct tty *, const char *, ...));
 void	bcopy __P((void *from, void *to, u_int len));
 void	ovbcopy __P((void *from, void *to, u_int len));
 void	bzero __P((void *buf, u_int len));
-int	bcmp __P((void *str1, void *str2, u_int len));
-size_t	strlen __P((const char *string));
 
 int	copystr __P((void *kfaddr, void *kdaddr, u_int len, u_int *done));
 int	copyinstr __P((void *udaddr, void *kaddr, u_int len, u_int *done));
@@ -88,7 +86,4 @@ int	fuiword __P((void *base));
 int	suword __P((void *base, int word));
 int	suiword __P((void *base, int word));
 
-int	scanc __P((unsigned size, u_char *cp, u_char *table, int mask));
-int	skpc __P((int mask, int size, char *cp));
-int	locc __P((int mask, char *cp, unsigned size));
-int	ffs __P((long value));
+#include <libkern/libkern.h>
