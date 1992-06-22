f300b8b03de6ece0fbc87801d513376c81e08674
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-06-21 21:27:13 -0800

    lseek takes an off_t
    SCCS-vsn: usr.bin/ftp/ftp.c 5.39

--- usr/src/usr.bin/ftp/ftp.c
+++ usr/src/usr.bin/ftp/ftp.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)ftp.c	5.38 (Berkeley) %G%";
+static char sccsid[] = "@(#)ftp.c	5.39 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/param.h>
@@ -22,13 +22,16 @@ static char sccsid[] = "@(#)ftp.c	5.38 (Berkeley) %G%";
 #include <arpa/ftp.h>
 #include <arpa/telnet.h>
 
-#include <stdio.h>
 #include <signal.h>
-#include <errno.h>
 #include <netdb.h>
 #include <fcntl.h>
+#include <errno.h>
 #include <pwd.h>
 #include <varargs.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 #include "ftp_var.h"
 
@@ -38,10 +41,8 @@ int	data = -1;
 int	abrtflag = 0;
 int	ptflag = 0;
 struct	sockaddr_in myctladdr;
-uid_t	getuid();
 sig_t	lostpeer();
 
-extern char *strerror();
 extern int connected, errno;
 
 FILE	*cin, *cout;
@@ -655,9 +656,7 @@ recvrequest(cmd, local, remote, lmode, printnames)
 	register int c, d;
 	struct timeval start, stop;
 	struct stat st;
-	off_t lseek();
 	void abortrecv();
-	char *malloc();
 
 	is_retr = strcmp(cmd, "RETR") == 0;
 	if (is_retr && verbose && printnames) {
