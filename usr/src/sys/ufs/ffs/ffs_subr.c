6738a3727b12e06677ecc6edfb956dd032b265b4
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1992-06-24 00:28:37 -0800

    move systm.h into KERNEL only defines
    SCCS-vsn: sys/ufs/ffs/ffs_subr.c 7.25

--- usr/src/sys/ufs/ffs/ffs_subr.c
+++ usr/src/sys/ufs/ffs/ffs_subr.c
@@ -4,14 +4,14 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)ffs_subr.c	7.24 (Berkeley) %G%
+ *	@(#)ffs_subr.c	7.25 (Berkeley) %G%
  */
 
 #include <sys/param.h>
-#include <sys/systm.h>
 #include <ufs/ffs/fs.h>
 
 #ifdef KERNEL
+#include <sys/systm.h>
 #include <sys/vnode.h>
 #include <ufs/ffs/ffs_extern.h>
 #include <sys/buf.h>
