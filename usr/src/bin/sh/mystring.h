cc0975e609f053c875735c62ccd65d65e19a4bdd
Marc Teitelbaum <marc@ucbvax.Berkeley.EDU>
1992-06-24 01:05:51 -0800

    remove unneded prototypes
    SCCS-vsn: bin/sh/mystring.h 5.2

--- usr/src/bin/sh/mystring.h
+++ usr/src/bin/sh/mystring.h
@@ -7,37 +7,22 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)mystring.h	5.1 (Berkeley) %G%
+ *	@(#)mystring.h	5.2 (Berkeley) %G%
  */
 
-#ifndef SYSV
-#define strchr mystrchr
-#endif
+#include <string.h>
 
 #ifdef __STDC__
 void scopyn(const char *, char *, int);
-char *strchr(const char *, int);
-void mybcopy(const pointer, pointer, int);
 int prefix(const char *, const char *);
 int number(const char *);
 int is_number(const char *);
-int strcmp(const char *, const char *);	/* from C library */
-char *strcpy(char *, const char *);	/* from C library */
-int strlen(const char *);		/* from C library */
-char *strcat(char *, const char *);	/* from C library */
 #else
 void scopyn();
-char *strchr();
-void mybcopy();
 int prefix();
 int number();
 int is_number();
-int strcmp();
-char *strcpy();
-int strlen();
-char *strcat();
 #endif
 
 #define equal(s1, s2)	(strcmp(s1, s2) == 0)
 #define scopy(s1, s2)	((void)strcpy(s2, s1))
-#define bcopy(src, dst, n)	mybcopy((pointer)(src), (pointer)(dst), n)
