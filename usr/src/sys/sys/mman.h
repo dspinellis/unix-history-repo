692577dd6f4d993a39ff4b97785a36e32866f2bc
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1992-06-24 00:06:48 -0800

    renumber constants to match Sun's naming and numbering scheme; eliminate MAP_FILE (make it the default as Sun does)
    SCCS-vsn: sys/sys/mman.h 7.7

--- usr/src/sys/sys/mman.h
+++ usr/src/sys/sys/mman.h
@@ -4,38 +4,38 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)mman.h	7.6 (Berkeley) %G%
+ *	@(#)mman.h	7.7 (Berkeley) %G%
  */
 
 /*
  * Protections are chosen from these bits, or-ed together
  */
-#define	PROT_READ	0x04	/* pages can be read */
+#define	PROT_READ	0x01	/* pages can be read */
 #define	PROT_WRITE	0x02	/* pages can be written */
-#define	PROT_EXEC	0x01	/* pages can be executed */
+#define	PROT_EXEC	0x04	/* pages can be executed */
 
 /*
- * Flags contain mapping type, sharing type and options.
- * Mapping type; choose one
+ * Flags contain sharing type and options.
+ * Sharing types; choose one.
  */
-#define	MAP_FILE	0x0001	/* mapped from a file or device */
-#define	MAP_ANON	0x0002	/* allocated from memory, swap space */
-#define	MAP_TYPE	0x000f	/* mask for type field */
-
-/*
- * Sharing types; choose one
- */
-#define	MAP_COPY	0x0020	/* "copy" region at mmap time */
-#define	MAP_SHARED	0x0010	/* share changes */
-#define	MAP_PRIVATE	0x0000	/* changes are private */
+#define	MAP_SHARED	0x0001	/* share changes */
+#define	MAP_PRIVATE	0x0002	/* changes are private */
+#define	MAP_COPY	0x0004	/* "copy" region at mmap time */
 
 /*
  * Other flags
  */
-#define	MAP_FIXED	 0x0100	/* map addr must be exactly as requested */
-#define	MAP_NOEXTEND	 0x0200	/* for MAP_FILE, don't change file size */
-#define	MAP_HASSEMAPHORE 0x0400	/* region may contain semaphores */
-#define	MAP_INHERIT	 0x0800	/* region is retained after exec */
+#define	MAP_FIXED	 0x0010	/* map addr must be exactly as requested */
+#define	MAP_RENAME	 0x0020	/* Sun: rename private pages to file */
+#define	MAP_NORESERVE	 0x0040	/* Sun: don't reserve needed swap area */
+#define	MAP_INHERIT	 0x0080	/* region is retained after exec */
+#define	MAP_NOEXTEND	 0x0100	/* for MAP_FILE, don't change file size */
+#define	MAP_HASSEMAPHORE 0x0200	/* region may contain semaphores */
+
+/*
+ * Mapping type; default is map from file.
+ */
+#define	MAP_ANON	0x1000	/* allocated from memory, swap space */
 
 /*
  * Advice to madvise
@@ -53,9 +53,9 @@
 __BEGIN_DECLS
 /* Some of these int's should probably be size_t's */
 caddr_t	mmap __P((caddr_t, size_t, int, int, int, off_t));
-int	mprotect __P((caddr_t, int, int));
-int	munmap __P((caddr_t, int));
-int	msync __P((caddr_t, int));
+int	mprotect __P((caddr_t, size_t, int));
+int	munmap __P((caddr_t, size_t));
+int	msync __P((caddr_t, size_t));
 __END_DECLS
 
 #endif /* !KERNEL */
