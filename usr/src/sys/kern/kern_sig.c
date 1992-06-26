1a71e6e687baa95c8e51fcd10b379f99656b68d7
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1992-06-25 23:16:26 -0800

    nothing to do in sigaltstack if no new stack given
    SCCS-vsn: sys/kern/kern_sig.c 7.47

--- usr/src/sys/kern/kern_sig.c
+++ usr/src/sys/kern/kern_sig.c
@@ -3,7 +3,7 @@
  * All rights reserved.  The Berkeley software License Agreement
  * specifies the terms and conditions for redistribution.
  *
- *	@(#)kern_sig.c	7.46 (Berkeley) %G%
+ *	@(#)kern_sig.c	7.47 (Berkeley) %G%
  */
 
 #define	SIGPROP		/* include signal properties table */
@@ -414,8 +414,9 @@ sigaltstack(p, uap, retval)
 	if (uap->oss && (error = copyout((caddr_t)&psp->ps_sigstk,
 	    (caddr_t)uap->oss, sizeof (struct sigaltstack))))
 		return (error);
-	if (uap->nss && (error = copyin((caddr_t)uap->nss, (caddr_t)&ss,
-	    sizeof (ss))))
+	if (uap->nss == 0)
+		return (0);
+	if (error = copyin((caddr_t)uap->nss, (caddr_t)&ss, sizeof (ss)))
 		return (error);
 	if (ss.ss_flags & SA_DISABLE) {
 		if (psp->ps_sigstk.ss_flags & SA_ONSTACK)
