54105ac65974b116fd69a2693db8d0d18ffbdba7
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-06-30 01:58:29 -0800

    quad fixes, but some general cleanup too
    SCCS-vsn: usr.bin/gcore/md-sparc.c 5.2

--- usr/src/usr.bin/gcore/md-sparc.c
+++ usr/src/usr.bin/gcore/md-sparc.c
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 1992 Regents of the University of California.
+ * Copyright (c) 1992 The Regents of the University of California.
  * All rights reserved.
  *
  * This software was developed by the Computer Systems Engineering group
@@ -9,8 +9,10 @@
  * %sccs.include.redist.c%
  */
 
-#include <stdio.h>
-#include <kvm.h>
+#ifndef lint
+static char sccsid[] = "@(#)md-sparc.c	5.2 (Berkeley) %G%";
+#endif /* not lint */
+
 #include <sys/param.h>
 #include <sys/time.h>
 #include <sys/file.h>
@@ -20,6 +22,12 @@
 #include <sys/kinfo.h>
 #include <sys/kinfo_proc.h>
 #include <machine/vmparam.h>
+
+#include <kvm.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include "extern.h"
 
 #ifndef offsetof
 #define	offsetof(s, f) ((int)&((s *)0)->f)
@@ -33,11 +41,11 @@ shift_page(fd, off, ssize)
 {
 	char buffer[NBPG];
 
-	(void)lseek(fd, -NBPG, SEEK_END);
+	(void)lseek(fd, (off_t)-NBPG, SEEK_END);
 	for (; ssize > 0; ssize -= NBPG) {
-		read(fd, buffer, NBPG);
-		write(fd, buffer, NBPG);
-		(void)lseek(fd, -2 * NBPG, SEEK_CUR);
+		(void)read(fd, buffer, NBPG);
+		(void)write(fd, buffer, NBPG);
+		(void)lseek(fd, (off_t)-2 * NBPG, SEEK_CUR);
 	}
 }
 
@@ -47,7 +55,7 @@ shift_page(fd, off, ssize)
  * Also, we need to get the trap frame and possible floating point state
  * from the top of the kernel stack and store it in the pcb.
  */
-int
+void
 md_core(kd, fd, ki)
 	kvm_t *kd;
 	int fd;
@@ -68,21 +76,19 @@ md_core(kd, fd, ki)
 		      /* XXX */
 		      (void *)&tf, sizeof(tf));
 	if (cc < 0)
-		error("kvm_read: %s (reading kernel trapframe)",
+		err(1, "kvm_read: %s (reading kernel trapframe)",
 		      kvm_geterr(kd));
 	if (cc != sizeof(tf))
-		error("cannot read kernel trapframe");
+		err(1, "cannot read kernel trapframe");
 
 	/*
 	 * Write out the real trap frame.
 	 */
 	off = offsetof(struct user, u_md);
 	off += offsetof(struct md_coredump, md_tf);
-	if (lseek(fd, off, SEEK_SET) == -1) {
-		perror("lseek");
-		exit(1);
-	}
-	(void)write(fd, (char *)&tf, sizeof(tf));
+	if (lseek(fd, off, SEEK_SET) == -1)
+		err(1, "lseek: %s", strerror(errno));
+	(void)write(fd, &tf, sizeof(tf));
 
 	if (ki->kp_proc.p_md.md_fpstate != 0) {
 		/*
@@ -93,32 +99,29 @@ md_core(kd, fd, ki)
 		cc = kvm_read(kd, (u_long)ki->kp_proc.p_md.md_fpstate,
 			      (void *)&fs, sizeof(fs));
 		if (cc < 0)
-			error("kvm_read: %s (fpu state)", kvm_geterr(kd));
+			err(1, "kvm_read: %s (fpu state)", kvm_geterr(kd));
 		if (cc != sizeof(fs))
-			error("cannot read fpu state");
+			err(1, "cannot read fpu state");
 		(void)write(fd, (char *)&fs, sizeof(fs));
 	}
 	/*
 	 * Read pcb.
 	 */
-	if (lseek(fd, offsetof(struct user, u_pcb), SEEK_SET) == -1) {
-		perror("lseek");
-		exit(1);
-	}
+	if (lseek(fd, (off_t)offsetof(struct user, u_pcb), SEEK_SET) == -1)
+		err(1, "lseek: %s", strerror(errno));
 	cc = read(fd, (char *)&pcb, sizeof(pcb));
-	if (cc < 0) {
-		perror("read");
-		exit(1);
+	if (cc != sizeof(pcb)) {
+		if (cc < 0)
+			err(1, "read: %s", strerror(errno));
+		err(1, "couldn't read pcb from core file");
 	}
-	if (cc != sizeof(pcb))
-		error("couldn't read pcb from core file");
 
 	/*
 	 * Write any unsaved windows to the appropriate stack locations.
 	 */
 	nsaved = pcb.pcb_nsaved;
 	if (nsaved == 0)
-		return (0);
+		return;
 
 	rw = &pcb.pcb_rw[0];
 	off = ctob(UPAGES + ki->kp_eproc.e_vm.vm_dsize);
@@ -131,7 +134,7 @@ md_core(kd, fd, ki)
 		s = ssize - (USRSTACK - sp);
 		if (s < 0) {
 			if (s < -NBPG)
-				error("cannot copy pcb windows to stack");
+				err(1, "cannot copy pcb windows to stack");
 			/*
 			 * It's possible to be missing the bottomost
 			 * page because a stack page hasn't been allocated
@@ -143,18 +146,16 @@ md_core(kd, fd, ki)
 			ssize += NBPG;
 			s += NBPG;
 			++ki->kp_eproc.e_vm.vm_ssize;
-			(void)lseek(fd, offsetof(struct user,
-						 u_kproc.kp_eproc.e_vm),
-				    SEEK_SET);
-			(void)write(fd, (char *)&ki->kp_eproc.e_vm, 
-				    sizeof(ki->kp_eproc.e_vm));
+			(void)lseek(fd,
+			    (off_t)offsetof(struct user, u_kproc.kp_eproc.e_vm),
+			    SEEK_SET);
+			(void)write(fd,
+			    &ki->kp_eproc.e_vm, sizeof(ki->kp_eproc.e_vm));
 		}
 		if (lseek(fd, off + s, SEEK_SET) == -1)
-			error("cannot copy pcb windows to stack");
+			err(1, "cannot copy pcb windows to stack");
 
-		write(fd, (char *)rw, sizeof(*rw));
+		(void)write(fd, rw, sizeof(*rw));
 		sp = rw->rw_in[6];
 	}
-	return (0);
 }
-
