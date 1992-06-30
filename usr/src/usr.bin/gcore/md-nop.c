4306356b857c3c722d12e55d97c8f2eecf62eb1c
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-06-30 01:57:19 -0800

    cleanup and ANSI-ficatoin
    SCCS-vsn: usr.bin/gcore/md-nop.c 5.2

--- usr/src/usr.bin/gcore/md-nop.c
+++ usr/src/usr.bin/gcore/md-nop.c
@@ -5,17 +5,20 @@
  * %sccs.include.redist.c%
  */
 
-#include <stdio.h>
-#include <kvm.h>
 #include <sys/param.h>
 #include <sys/kinfo.h>
 #include <sys/kinfo_proc.h>
 
+#include <stdio.h>
+#include <kvm.h>
+#include "extern.h"
+
+void
 md_core(kd, fd, ki)
 	kvm_t *kd;
 	int fd;
 	struct kinfo_proc *ki;
 {
 	/* Don't need to fix anything for this architecture. */
-	return (0);
+	return;
 }
