3b58ec91db328bdd72c2390f5bee00ac65a3d821
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-06-29 21:43:42 -0800

    move fnmatch function declaration from unistd.h
    SCCS-vsn: include/fnmatch.h 5.2

--- usr/src/include/fnmatch.h
+++ usr/src/include/fnmatch.h
@@ -4,11 +4,24 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)fnmatch.h	5.1 (Berkeley) %G%
+ *	@(#)fnmatch.h	5.2 (Berkeley) %G%
  */
+
+#ifndef	_FNMATCH_H_
+#define	_FNMATCH_H_
 
 #define	FNM_NOMATCH	1	/* Match failed. */
 
 #define	FNM_NOESCAPE	0x01	/* Disable backslash escaping. */
 #define	FNM_PATHNAME	0x02	/* Slash must be matched by slash. */
 #define	FNM_PERIOD	0x04	/* Period must be matched by period. */
+
+#include <sys/cdefs.h>
+
+__BEGIN_DECLS
+#ifndef	_POSIX_SOURCE
+int	 fnmatch __P((const char *, const char *, int));
+#endif
+__END_DECLS
+
+#endif /* !_FNMATCH_H_ */
