4c76c203fa2a7fec90377fa6e92f6f0d09546413
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-07-01 17:21:46 -0800

    fix seek calls for quads
    SCCS-vsn: lib/libutil/login.c 5.5
    SCCS-vsn: lib/libutil/logout.c 5.6

--- usr/src/lib/libutil/login.c
+++ usr/src/lib/libutil/login.c
@@ -6,11 +6,14 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)login.c	5.4 (Berkeley) %G%";
+static char sccsid[] = "@(#)login.c	5.5 (Berkeley) %G%";
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/types.h>
-#include <sys/file.h>
+
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
 #include <utmp.h>
 #include <stdio.h>
 
@@ -20,16 +23,15 @@ login(ut)
 {
 	register int fd;
 	int tty;
-	off_t lseek();
 
 	tty = ttyslot();
 	if (tty > 0 && (fd = open(_PATH_UTMP, O_WRONLY|O_CREAT, 0644)) >= 0) {
-		(void)lseek(fd, (long)(tty * sizeof(struct utmp)), L_SET);
-		(void)write(fd, (char *)ut, sizeof(struct utmp));
+		(void)lseek(fd, (off_t)(tty * sizeof(struct utmp)), L_SET);
+		(void)write(fd, ut, sizeof(struct utmp));
 		(void)close(fd);
 	}
 	if ((fd = open(_PATH_WTMP, O_WRONLY|O_APPEND, 0)) >= 0) {
-		(void)write(fd, (char *)ut, sizeof(struct utmp));
+		(void)write(fd, ut, sizeof(struct utmp));
 		(void)close(fd);
 	}
 }
--- usr/src/lib/libutil/logout.c
+++ usr/src/lib/libutil/logout.c
@@ -6,36 +6,39 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)logout.c	5.5 (Berkeley) %G%";
+static char sccsid[] = "@(#)logout.c	5.6 (Berkeley) %G%";
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/types.h>
-#include <sys/file.h>
 #include <sys/time.h>
+
+#include <fcntl.h>
 #include <utmp.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
 
 typedef struct utmp UTMP;
 
+int
 logout(line)
 	register char *line;
 {
 	register int fd;
 	UTMP ut;
 	int rval;
-	off_t lseek();
-	time_t time();
 
-	if ((fd = open(_PATH_UTMP, O_RDWR)) < 0)
+	if ((fd = open(_PATH_UTMP, O_RDWR, 0)) < 0)
 		return(0);
 	rval = 0;
-	while (read(fd, (char *)&ut, sizeof(UTMP)) == sizeof(UTMP)) {
+	while (read(fd, &ut, sizeof(UTMP)) == sizeof(UTMP)) {
 		if (!ut.ut_name[0] || strncmp(ut.ut_line, line, UT_LINESIZE))
 			continue;
 		bzero(ut.ut_name, UT_NAMESIZE);
 		bzero(ut.ut_host, UT_HOSTSIZE);
 		(void)time(&ut.ut_time);
-		(void)lseek(fd, -(long)sizeof(UTMP), L_INCR);
-		(void)write(fd, (char *)&ut, sizeof(UTMP));
+		(void)lseek(fd, -(off_t)sizeof(UTMP), L_INCR);
+		(void)write(fd, &ut, sizeof(UTMP));
 		rval = 1;
 	}
 	(void)close(fd);
