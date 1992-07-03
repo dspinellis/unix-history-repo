3ebca32ab2c872182150f04b80adbb3238853b92
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1992-07-03 00:21:41 -0800

    add d_type fields in directories
    SCCS-vsn: sys/ufs/ffs/dir.h 7.11
    SCCS-vsn: sys/ufs/ufs/dir.h 7.11

--- usr/src/sys/ufs/ffs/dir.h
+++ usr/src/sys/ufs/ffs/dir.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)dir.h	7.10 (Berkeley) %G%
+ *	@(#)dir.h	7.11 (Berkeley) %G%
  */
 
 #ifndef _DIR_H_
@@ -23,9 +23,9 @@
  * with null bytes.  All names are guaranteed null terminated.
  * The maximum length of a name in a directory is MAXNAMLEN.
  *
- * The macro DIRSIZ(dp) gives the amount of space required to represent
+ * The macro DIRSIZ(fmt, dp) gives the amount of space required to represent
  * a directory entry.  Free space in a directory is represented by
- * entries which have dp->d_reclen > DIRSIZ(dp).  All DIRBLKSIZ bytes
+ * entries which have dp->d_reclen > DIRSIZ(fmt, dp).  All DIRBLKSIZ bytes
  * in a directory block are claimed by the directory entries.  This
  * usually results in the last entry in a directory having a large
  * dp->d_reclen.  When entries are deleted from a directory, the
@@ -41,9 +41,28 @@
 struct	direct {
 	u_long	d_ino;			/* inode number of entry */
 	u_short	d_reclen;		/* length of this record */
-	u_short	d_namlen;		/* length of string in d_name */
+	u_char	d_type; 		/* file type, see below */
+	u_char	d_namlen;		/* length of string in d_name */
 	char	d_name[MAXNAMLEN + 1];	/* name with length <= MAXNAMLEN */
 };
+
+/*
+ * File types
+ */
+#define	DT_UNKNOWN	 0
+#define	DT_FIFO		 1
+#define	DT_CHR		 2
+#define	DT_DIR		 4
+#define	DT_BLK		 6
+#define	DT_REG		 8
+#define	DT_LNK		10
+#define	DT_SOCK		12
+
+/*
+ * Convert between stat structure types and directory types.
+ */
+#define	IFTODT(mode)	(((mode) & 0170000) >> 12)
+#define	DTTOIF(dirtype)	((dirtype) << 12)
 
 /*
  * The DIRSIZ macro gives the minimum record length which will hold
@@ -51,8 +70,15 @@ struct	direct {
  * without the d_name field, plus enough space for the name with a terminating
  * null byte (dp->d_namlen+1), rounded up to a 4 byte boundary.
  */
-#define DIRSIZ(dp) \
+#if (BYTE_ORDER == LITTLE_ENDIAN)
+#define DIRSIZ(oldfmt, dp) \
+    ((oldfmt) ? \
+    ((sizeof (struct direct) - (MAXNAMLEN+1)) + (((dp)->d_type+1 + 3) &~ 3)) : \
+    ((sizeof (struct direct) - (MAXNAMLEN+1)) + (((dp)->d_namlen+1 + 3) &~ 3)))
+#else
+#define DIRSIZ(oldfmt, dp) \
     ((sizeof (struct direct) - (MAXNAMLEN+1)) + (((dp)->d_namlen+1 + 3) &~ 3))
+#endif
 
 /*
  * Template for manipulating directories.
@@ -62,11 +88,27 @@ struct	direct {
 struct dirtemplate {
 	u_long	dot_ino;
 	short	dot_reclen;
-	short	dot_namlen;
+	u_char	dot_type;
+	u_char	dot_namlen;
 	char	dot_name[4];		/* must be multiple of 4 */
 	u_long	dotdot_ino;
 	short	dotdot_reclen;
-	short	dotdot_namlen;
+	u_char	dotdot_type;
+	u_char	dotdot_namlen;
+	char	dotdot_name[4];		/* ditto */
+};
+
+/*
+ * This is the old format of directories, sanz type element.
+ */
+struct odirtemplate {
+	u_long	dot_ino;
+	short	dot_reclen;
+	u_short	dot_namlen;
+	char	dot_name[4];		/* must be multiple of 4 */
+	u_long	dotdot_ino;
+	short	dotdot_reclen;
+	u_short	dotdot_namlen;
 	char	dotdot_name[4];		/* ditto */
 };
 #endif /* !_DIR_H_ */
