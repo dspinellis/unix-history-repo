394693e807a1016376caed324a7fc658a6674839
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1992-07-03 08:42:42 -0800

    add parameter declaration
    SCCS-vsn: sys/ufs/ffs/ufs_inode.c 7.48
    SCCS-vsn: sys/ufs/ufs/ufs_inode.c 7.48

--- usr/src/sys/ufs/ufs/ufs_inode.c
+++ usr/src/sys/ufs/ufs/ufs_inode.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)ufs_inode.c	7.47 (Berkeley) %G%
+ *	@(#)ufs_inode.c	7.48 (Berkeley) %G%
  */
 
 #include <sys/param.h>
@@ -59,8 +59,10 @@ ufs_iput(ip)
  * Reclaim an inode so that it can be used for other purposes.
  */
 int
-ufs_reclaim (ap)
-	struct vop_reclaim_args *ap;
+ufs_reclaim(ap)
+	struct vop_reclaim_args /* {
+		struct vnode *a_vp;
+	} */ *ap;
 {
 	register struct vnode *vp = ap->a_vp;
 	register struct inode *ip;
