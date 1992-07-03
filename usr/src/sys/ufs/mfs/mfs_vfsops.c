41da5ab059c788c1360939b37297a2b34432436e
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1992-07-03 08:52:28 -0800

    use ffs_vget
    SCCS-vsn: sys/ufs/mfs/mfs_vfsops.c 7.26

--- usr/src/sys/ufs/mfs/mfs_vfsops.c
+++ usr/src/sys/ufs/mfs/mfs_vfsops.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)mfs_vfsops.c	7.25 (Berkeley) %G%
+ *	@(#)mfs_vfsops.c	7.26 (Berkeley) %G%
  */
 
 #include <sys/param.h>
@@ -47,6 +47,7 @@ struct vfsops mfs_vfsops = {
 	ufs_quotactl,
 	mfs_statfs,
 	ffs_sync,
+	ffs_vget,
 	ffs_fhtovp,
 	ffs_vptofh,
 	mfs_init,
