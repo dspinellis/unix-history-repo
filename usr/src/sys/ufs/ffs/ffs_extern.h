ebb2cef6588c5664621db3b2d466382279468549
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1992-07-03 08:45:25 -0800

    ffs_vget moves from VOP to VFS
    SCCS-vsn: sys/ufs/ffs/ffs_extern.h 7.13

--- usr/src/sys/ufs/ffs/ffs_extern.h
+++ usr/src/sys/ufs/ffs/ffs_extern.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)ffs_extern.h	7.12 (Berkeley) %G%
+ *	@(#)ffs_extern.h	7.13 (Berkeley) %G%
  */
 
 struct buf;
@@ -53,7 +53,7 @@ int	ffs_unmount __P((struct mount *, int, struct proc *));
 int	ffs_update __P((struct vop_update_args *));
 int	ffs_valloc __P((struct vop_valloc_args *));
 int	ffs_vfree __P((struct vop_vfree_args *));
-int	ffs_vget __P((struct vop_vget_args *));
+int	ffs_vget __P((struct mount *, ino_t, struct vnode **));
 int	ffs_vptofh __P((struct vnode *, struct fid *));
 int	ffs_write __P((struct vop_write_args *));
 
