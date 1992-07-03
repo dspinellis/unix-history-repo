2c930d2f013fb43d19cc42058709523f7945ae63
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1992-07-03 08:57:46 -0800

    delete USES
    SCCS-vsn: sys/nfs/nfs_bio.c 7.29
    SCCS-vsn: sys/nfs/nfs_nqlease.c 7.9

--- usr/src/sys/nfs/nfs_nqlease.c
+++ usr/src/sys/nfs/nfs_nqlease.c
@@ -7,7 +7,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)nfs_nqlease.c	7.8 (Berkeley) %G%
+ *	@(#)nfs_nqlease.c	7.9 (Berkeley) %G%
  */
 
 /*
@@ -140,9 +140,6 @@ nqsrv_getlease(vp, duration, flags, nd, nam, cachablep, frev, cred)
 	u_quad_t *frev;
 	struct ucred *cred;
 {
-	USES_VOP_GETATTR;
-	USES_VOP_LOCK;
-	USES_VOP_UNLOCK;
 	register struct nqlease *lp;
 	register struct nqhost *lph;
 	struct nqlease *tlp = (struct nqlease *)0;
@@ -678,7 +675,6 @@ nqnfsrv_getlease(nfsd, mrep, md, dpos, cred, nam, mrq)
 	struct ucred *cred;
 	struct mbuf *nam, **mrq;
 {
-	USES_VOP_GETATTR;
 	register struct nfsv2_fattr *fp;
 	struct vattr va;
 	register struct vattr *vap = &va;
@@ -997,7 +993,6 @@ nqnfs_clientd(nmp, cred, ncd, flag, argp, p)
 	caddr_t argp;
 	struct proc *p;
 {
-	USES_VOP_FSYNC;
 	register struct nfsnode *np;
 	struct vnode *vp;
 	int error, vpid;
