26fe7a9ae78ed48f23a505ccf9033510e622af4b
Chris Torek <torek@ucbvax.Berkeley.EDU>
1992-07-03 17:17:37 -0800

    declare static functions as static before defining as static (for ANSI C)
    SCCS-vsn: lib/libcompat/4.3/ruserpass.c 5.10

--- usr/src/lib/libcompat/4.3/ruserpass.c
+++ usr/src/lib/libcompat/4.3/ruserpass.c
@@ -22,7 +22,8 @@ static char sccsid[] = "@(#)ruserpass.c	5.10 (Berkeley) %G%";
 static void blkencrypt(), enblkclr(), enblknot(), nbssetkey(), rnetrc();
 static int token();
 static char *renvlook();
-struct	utmp *getutmp();
+static struct utmp *getutmp();
+
 static	FILE *cfile;
 
 ruserpass(host, aname, apass)
@@ -243,8 +244,8 @@ token()
 }
 /* rest is nbs.c stolen from berknet */
 
-char *deblknot(), *deblkclr();
-char *nbs8decrypt(), *nbs8encrypt();
+static char *deblknot(), *deblkclr();
+static char *nbs8decrypt(), *nbs8encrypt();
 static char	E[48];
 
 /*
