39250b71e338d6045a3b9622f26a12854e163ae4
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1992-07-07 00:12:56 -0800

    put into KNF
    SCCS-vsn: sys/kern/vfs_init.c 7.2

--- usr/src/sys/kern/vfs_init.c
+++ usr/src/sys/kern/vfs_init.c
@@ -9,7 +9,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)vfs_init.c	7.1 (Berkeley) %G%
+ *	@(#)vfs_init.c	7.2 (Berkeley) %G%
  */
 
 
@@ -25,15 +25,14 @@
 #include <sys/errno.h>
 #include <sys/malloc.h>
 
-
-/* Sigh, such primitive tools are these... */
+/*
+ * Sigh, such primitive tools are these...
+ */
 #if 0
 #define DODEBUG(A) A
 #else
 #define DODEBUG(A)
 #endif
-
-
 
 extern struct vnodeopv_desc *vfs_opv_descs[];
 				/* a list of lists of vnodeops defns */
@@ -49,8 +48,6 @@ int vfs_opv_numops;
 
 typedef (*PFI)();   /* the standard Pointer to a Function returning an Int */
 
-
-
 /*
  * A miscellaneous routine.
  * A generic "default" routine that just returns an error.
@@ -58,32 +55,26 @@ typedef (*PFI)();   /* the standard Pointer to a Function returning an Int */
 int
 vn_default_error()
 {
-	return EOPNOTSUPP;
+
+	return (EOPNOTSUPP);
 }
-
-
-
 
 /*
  * vfs_init.c
  *
  * Allocate and fill in operations vectors.
  *
- * An undocumented feature of this approach to defining
- * operations is that there can be multiple entries in
- * vfs_opv_descs for the same operations vector.
- * This allows third parties to extend the set of operations
- * supported by another layer in a binary compatibile way.
- * For example, assume that NFS needed to be modified to support
- * Ficus.  NFS has an entry (probably nfs_vnopdeop_decls)
- * declaring all the operations NFS supports by default.
- * Ficus could add another entry (ficus_nfs_vnodeop_decl_entensions)
- * listing those new operations Ficus adds to NFS, all without
- * modifying the NFS code.  (Of couse, the OTW NFS protocol
- * still needs to be munged, but that's a(whole)nother story.)
- * This is a feature.
+ * An undocumented feature of this approach to defining operations is that
+ * there can be multiple entries in vfs_opv_descs for the same operations
+ * vector. This allows third parties to extend the set of operations
+ * supported by another layer in a binary compatibile way. For example,
+ * assume that NFS needed to be modified to support Ficus. NFS has an entry
+ * (probably nfs_vnopdeop_decls) declaring all the operations NFS supports by
+ * default. Ficus could add another entry (ficus_nfs_vnodeop_decl_entensions)
+ * listing those new operations Ficus adds to NFS, all without modifying the
+ * NFS code. (Of couse, the OTW NFS protocol still needs to be munged, but
+ * that is a(whole)nother story.) This is a feature.
  */
-
 void
 vfs_opv_init()
 {
@@ -107,8 +98,9 @@ vfs_opv_init()
 			MALLOC(*opv_desc_vector_p, PFI*,
 			       vfs_opv_numops*sizeof(PFI), M_VNODE, M_WAITOK);
 			bzero (*opv_desc_vector_p, vfs_opv_numops*sizeof(PFI));
-			DODEBUG(printf("vector at %x allocated\n", opv_desc_vector_p));
-		};
+			DODEBUG(printf("vector at %x allocated\n",
+			    opv_desc_vector_p));
+		}
 		opv_desc_vector = *opv_desc_vector_p;
 		for (j=0; vfs_opv_descs[i]->opv_desc_ops[j].opve_op; j++) {
 			opve_descp = &(vfs_opv_descs[i]->opv_desc_ops[j]);
@@ -121,8 +113,7 @@ vfs_opv_init()
 			 * first, it should be the only one with a zero
 			 * offset.  Any other operation with a zero
 			 * offset is probably not listed in
-			 * vfs_op_descs,
-			 * and so is probably an error.
+			 * vfs_op_descs, and so is probably an error.
 			 *
 			 * A panic here means the layer programmer
 			 * has committed the all-too common bug
@@ -134,72 +125,62 @@ vfs_opv_init()
 			if (opve_descp->opve_op->vdesc_offset == 0 &&
 				    opve_descp->opve_op->vdesc_offset !=
 				    	VOFFSET(vop_default)) {
-				printf ("error: operation %s not listed in vfs_op_descs.\n", opve_descp->opve_op->vdesc_name);
+				printf("operation %s not listed in %s.\n",
+				    opve_descp->opve_op->vdesc_name,
+				    "vfs_op_descs");
 				panic ("vfs_opv_init: bad operation");
-			};
+			}
 			/*
 			 * Fill in this entry.
 			 */
-			opv_desc_vector[opve_descp->opve_op->vdesc_offset]=
+			opv_desc_vector[opve_descp->opve_op->vdesc_offset] =
 					opve_descp->opve_impl;
-		};
-	};
-
+		}
+	}
 	/*
 	 * Finally, go back and replace unfilled routines
 	 * with their default.  (Sigh, an O(n^3) algorithm.  I
 	 * could make it better, but that'd be work, and n is small.)
 	 */
-	for (i=0; vfs_opv_descs[i]; i++) {
+	for (i = 0; vfs_opv_descs[i]; i++) {
 		opv_desc_vector = *(vfs_opv_descs[i]->opv_desc_vector_p);
 		/*
 		 * Force every operations vector to have a default routine.
 		 */
 		if (opv_desc_vector[VOFFSET(vop_default)]==NULL) {
 			panic("vfs_opv_init: operation vector without default routine.");
-		};
-		for (k=0; k<vfs_opv_numops; k++)
+		}
+		for (k = 0; k<vfs_opv_numops; k++)
 			if (opv_desc_vector[k] == NULL)
 				opv_desc_vector[k] = 
 					opv_desc_vector[VOFFSET(vop_default)];
-	};
+	}
 }
 
-
-
-
+/*
+ * Initialize known vnode operations vectors.
+ */
 void
 vfs_op_init()
 {
 	int i, j;
 
-	DODEBUG(printf ("Vnode_interface_init.\n"));
-
+	DODEBUG(printf("Vnode_interface_init.\n"));
 	/*
 	 * Set all vnode vectors to a well known value.
 	 */
-	for (i=0; vfs_opv_descs[i]; i++)
+	for (i = 0; vfs_opv_descs[i]; i++)
 		*(vfs_opv_descs[i]->opv_desc_vector_p) = NULL;
-
-	vfs_opv_numops = 0;
-
 	/*
 	 * Figure out how many ops there are by counting the table,
 	 * and assign each its offset.
 	 */
-	for (i=0; vfs_op_descs[i]; i++) {
+	for (vfs_opv_numops = 0, i = 0; vfs_op_descs[i]; i++) {
 		vfs_op_descs[i]->vdesc_offset = vfs_opv_numops;
 		vfs_opv_numops++;
-	};
-
+	}
 	DODEBUG(printf ("vfs_opv_numops=%d\n", vfs_opv_numops));
 }
-
-
-
-
-
-
 
 /*
  * Routines having to do with the management of the vnode table.
