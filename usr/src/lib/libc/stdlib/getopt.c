737de4d40396519763a16ca29357547b04b49337
Marc Teitelbaum <marc@ucbvax.Berkeley.EDU>
1992-07-06 20:48:47 -0800

    add optreset global for resetting state of getopt (for using multiple times in the same program).
    SCCS-vsn: lib/libc/stdlib/getopt.3 6.17
    SCCS-vsn: lib/libc/stdlib/getopt.c 5.1

--- usr/src/lib/libc/stdlib/getopt.c
+++ usr/src/lib/libc/stdlib/getopt.c
@@ -6,7 +6,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)getopt.c	4.13 (Berkeley) %G%";
+static char sccsid[] = "@(#)getopt.c	5.1 (Berkeley) %G%";
 #endif /* LIBC_SCCS and not lint */
 
 #include <stdio.h>
@@ -18,10 +18,12 @@ static char sccsid[] = "@(#)getopt.c	4.13 (Berkeley) %G%";
  */
 int	opterr = 1,		/* if error message should be printed */
 	optind = 1,		/* index into parent argv vector */
-	optopt;			/* character checked for validity */
+	optopt,			/* character checked for validity */
+	optreset;		/* reset getopt */
 char	*optarg;		/* argument associated with option */
 
 #define	BADCH	(int)'?'
+#define	BADARG	(int)':'
 #define	EMSG	""
 
 int
@@ -34,7 +36,8 @@ getopt(nargc, nargv, ostr)
 	register char *oli;			/* option letter list index */
 	char *p;
 
-	if (!*place) {				/* update scanning pointer */
+	if (optreset || !*place) {		/* update scanning pointer */
+		optreset = 0;
 		if (optind >= nargc || *(place = nargv[optind]) != '-') {
 			place = EMSG;
 			return(EOF);
@@ -55,7 +58,7 @@ getopt(nargc, nargv, ostr)
 			return(EOF);
 		if (!*place)
 			++optind;
-		if (opterr) {
+		if (opterr && *ostr != ':') {
 			if (!(p = rindex(*nargv, '/')))
 				p = *nargv;
 			else
@@ -79,6 +82,8 @@ getopt(nargc, nargv, ostr)
 				p = *nargv;
 			else
 				++p;
+			if (*ostr == ':')
+				return(BADARG);
 			if (opterr)
 				(void)fprintf(stderr,
 				    "%s: option requires an argument -- %c\n",
