3c297ff0f547c0740548df4a931741b59aa55506
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1992-07-07 23:15:52 -0800

    return a quad -1
    SCCS-vsn: lib/libc/hp300/sys/cerror.s 5.2

--- usr/src/lib/libc/hp300/sys/cerror.s
+++ usr/src/lib/libc/hp300/sys/cerror.s
@@ -10,7 +10,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-	.asciz "@(#)cerror.s	5.1 (Berkeley) %G%"
+	.asciz "@(#)cerror.s	5.2 (Berkeley) %G%"
 #endif /* LIBC_SCCS and not lint */
 
 #include "SYS.h"
