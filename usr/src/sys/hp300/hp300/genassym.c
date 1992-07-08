541b4566530d6c21c9a5a3912cfdc1096d933827
Chris Torek <torek@ucbvax.Berkeley.EDU>
1992-07-08 08:07:37 -0800

    major overhaul for legibility
    SCCS-vsn: sys/hp300/hp300/genassym.c 7.11

--- usr/src/sys/hp300/hp300/genassym.c
+++ usr/src/sys/hp300/hp300/genassym.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)genassym.c	7.10 (Berkeley) %G%
+ *	@(#)genassym.c	7.11 (Berkeley) %G%
  */
 
 #define KERNEL
@@ -25,145 +25,165 @@
 #include "sys/user.h"
 #include "pte.h"
 
+#include <errno.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <string.h>
+#include <unistd.h>
+
+extern int errno;
+
+void
+def(what, val)
+	char *what;
+	int val;
+{
+
+	if (printf("#define\t%s\t%d\n", what, val) < 0) {
+		(void)fprintf(stderr, "genassym: printf: %s\n",
+		    strerror(errno));
+		exit(1);
+	}
+}
+
+void
+flush()
+{
+
+	if (fflush(stdout) || fsync(fileno(stdout)) < 0) {
+		(void)fprintf(stderr, "genassym: flush stdout: %s\n",
+		    strerror(errno));
+		exit(1);
+	}
+}
+
+#define	off(what, s, m)	def(what, (int)offsetof(s, m))
+
 main()
 {
-	register struct proc *p = (struct proc *)0;
-	register struct vmmeter *vm = (struct vmmeter *)0;
-	register struct user *up = (struct user *)0;
-	register struct rusage *rup = (struct rusage *)0;
-	struct vmspace *vms = (struct vmspace *)0;
-	pmap_t pmap = (pmap_t)0;
-	struct pcb *pcb = (struct pcb *)0;
 	register unsigned i;
 
-	printf("#define\tP_LINK %d\n", &p->p_link);
-	printf("#define\tP_RLINK %d\n", &p->p_rlink);
-	printf("#define\tP_VMSPACE %d\n", &p->p_vmspace);
-	printf("#define\tVM_PMAP %d\n", &vms->vm_pmap);
-	printf("#define\tPM_STCHG %d\n", &pmap->pm_stchanged);
-	printf("#define\tP_ADDR %d\n", &p->p_addr);
-	printf("#define\tP_PRI %d\n", &p->p_pri);
-	printf("#define\tP_STAT %d\n", &p->p_stat);
-	printf("#define\tP_WCHAN %d\n", &p->p_wchan);
-	printf("#define\tP_FLAG %d\n", &p->p_flag);
-	printf("#define\tSSLEEP %d\n", SSLEEP);
-	printf("#define\tSRUN %d\n", SRUN);
-	printf("#define\tV_SWTCH %d\n", &vm->v_swtch);
-	printf("#define\tV_TRAP %d\n", &vm->v_trap);
-	printf("#define\tV_SYSCALL %d\n", &vm->v_syscall);
-	printf("#define\tV_INTR %d\n", &vm->v_intr);
-	printf("#define\tV_SOFT %d\n", &vm->v_soft);
-	printf("#define\tV_FAULTS %d\n", &vm->v_faults);
-	printf("#define\tUPAGES %d\n", UPAGES);
-	printf("#define\tHIGHPAGES %d\n", HIGHPAGES);
-	printf("#define\tP1PAGES %d\n", P1PAGES);
-	printf("#define\tCLSIZE %d\n", CLSIZE);
-	printf("#define\tNBPG %d\n", NBPG);
-	printf("#define\tNPTEPG %d\n", NPTEPG);
-	printf("#define\tPGSHIFT %d\n", PGSHIFT);
-	printf("#define\tSYSPTSIZE %d\n", SYSPTSIZE);
-	printf("#define\tUSRPTSIZE %d\n", USRPTSIZE);
-	printf("#define\tUSRIOSIZE %d\n", USRIOSIZE);
-#ifdef SYSVSHM
-	printf("#define\tSHMMAXPGS %d\n", SHMMAXPGS);
-#endif
-	printf("#define\tUSRSTACK %d\n", USRSTACK);
-	printf("#define\tMSGBUFPTECNT %d\n", btoc(sizeof (struct msgbuf)));
-	printf("#define\tNMBCLUSTERS %d\n", NMBCLUSTERS);
-	printf("#define\tMCLBYTES %d\n", MCLBYTES);
-	printf("#define\tNKMEMCLUSTERS %d\n", NKMEMCLUSTERS);
-	printf("#define\tU_PROF %d\n", &up->u_stats.p_prof);
-	printf("#define\tU_PROFSCALE %d\n", &up->u_stats.p_prof.pr_scale);
-	printf("#define\tRU_MINFLT %d\n", &rup->ru_minflt);
-	printf("#define\tT_BUSERR %d\n", T_BUSERR);
-	printf("#define\tT_ADDRERR %d\n", T_ADDRERR);
-	printf("#define\tT_ILLINST %d\n", T_ILLINST);
-	printf("#define\tT_ZERODIV %d\n", T_ZERODIV);
-	printf("#define\tT_CHKINST %d\n", T_CHKINST);
-	printf("#define\tT_TRAPVINST %d\n", T_TRAPVINST);
-	printf("#define\tT_PRIVINST %d\n", T_PRIVINST);
-	printf("#define\tT_TRACE %d\n", T_TRACE);
-	printf("#define\tT_MMUFLT %d\n", T_MMUFLT);
-	printf("#define\tT_SSIR %d\n", T_SSIR);
-	printf("#define\tT_FMTERR %d\n", T_FMTERR);
-	printf("#define\tT_COPERR %d\n", T_COPERR);
-	printf("#define\tT_FPERR %d\n", T_FPERR);
-	printf("#define\tT_ASTFLT %d\n", T_ASTFLT);
-	printf("#define\tT_TRAP15 %d\n", T_TRAP15);
-	printf("#define\tT_FPEMULI %d\n", T_FPEMULI);
-	printf("#define\tT_FPEMULD %d\n", T_FPEMULD);
-	printf("#define\tPSL_S %d\n", PSL_S);
-	printf("#define\tPSL_IPL7 %d\n", PSL_IPL7);
-	printf("#define\tPSL_LOWIPL %d\n", PSL_LOWIPL);
-	printf("#define\tPSL_HIGHIPL %d\n", PSL_HIGHIPL);
-	printf("#define\tPSL_USER %d\n", PSL_USER);
-	printf("#define\tSPL1 %d\n", PSL_S | PSL_IPL1);
-	printf("#define\tSPL2 %d\n", PSL_S | PSL_IPL2);
-	printf("#define\tSPL3 %d\n", PSL_S | PSL_IPL3);
-	printf("#define\tSPL4 %d\n", PSL_S | PSL_IPL4);
-	printf("#define\tSPL5 %d\n", PSL_S | PSL_IPL5);
-	printf("#define\tSPL6 %d\n", PSL_S | PSL_IPL6);
-	printf("#define\tFC_USERD %d\n", FC_USERD);
-	printf("#define\tFC_PURGE %d\n", FC_PURGE);
-	printf("#define\tMAXADDR %d\n", MAXADDR);
-	printf("#define\tIIOMAPSIZE %d\n", IIOMAPSIZE);
-	printf("#define\tEIOMAPSIZE %d\n", EIOMAPSIZE);
-	printf("#define\tINTIOBASE %d\n", INTIOBASE);
-	printf("#define\tMMUBASE %d\n", MMUBASE);
-	printf("#define\tMMUSTAT %d\n", MMUSTAT);
-	printf("#define\tMMUCMD %d\n", MMUCMD);
-	printf("#define\tMMUSSTP %d\n", MMUSSTP);
-	printf("#define\tMMUUSTP %d\n", MMUUSTP);
-	printf("#define\tMMUTBINVAL %d\n", MMUTBINVAL);
-	printf("#define\tMMU_BERR %d\n", MMU_BERR);
-	printf("#define\tMMU_ENAB %d\n", MMU_ENAB);
-	printf("#define\tMMU_FAULT %d\n", MMU_FAULT);
-	printf("#define\tMMU_CEN %d\n", MMU_CEN);
-	printf("#define\tMMU_IEN %d\n", MMU_IEN);
-	printf("#define\tMMU_FPE %d\n", MMU_FPE);
-	printf("#define\tCACHE_ON %d\n", CACHE_ON);
-	printf("#define\tCACHE_OFF %d\n", CACHE_OFF);
-	printf("#define\tCACHE_CLR %d\n", CACHE_CLR);
-	printf("#define\tIC_CLEAR %d\n", IC_CLEAR);
-	printf("#define\tDC_CLEAR %d\n", DC_CLEAR);
-	printf("#define\tPG_V %d\n", PG_V);
-	printf("#define\tPG_NV %d\n", PG_NV);
-	printf("#define\tPG_RO %d\n", PG_RO);
-	printf("#define\tPG_RW %d\n", PG_RW);
-	printf("#define\tPG_CI %d\n", PG_CI);
-	printf("#define\tPG_PROT %d\n", PG_PROT);
-	printf("#define\tPG_FRAME %d\n", PG_FRAME);
-	printf("#define\tSG_V %d\n", SG_V);
-	printf("#define\tSG_NV %d\n", SG_NV);
-	printf("#define\tSG_RW %d\n", SG_RW);
-	printf("#define\tSG_FRAME %d\n", SG_FRAME);
-	printf("#define\tSG_ISHIFT %d\n", SG_ISHIFT);
-	printf("#define\tPCB_FLAGS %d\n", &pcb->pcb_flags);
-	printf("#define\tPCB_PS %d\n", &pcb->pcb_ps);
-	printf("#define\tPCB_USTP %d\n", &pcb->pcb_ustp);
-	printf("#define\tPCB_USP %d\n", &pcb->pcb_usp);
-	printf("#define\tPCB_REGS %d\n", pcb->pcb_regs);
-	printf("#define\tPCB_ONFAULT %d\n", &pcb->pcb_onfault);
-	printf("#define\tPCB_FPCTX %d\n", &pcb->pcb_fpregs);
-	printf("#define\tSIZEOF_PCB %d\n", sizeof(struct pcb));
-	printf("#define\tSP %d\n", SP);
-	printf("#define\tB_READ %d\n", B_READ);
-	printf("#define\tENOENT %d\n", ENOENT);
-	printf("#define\tEFAULT %d\n", EFAULT);
-	printf("#define\tENAMETOOLONG %d\n", ENAMETOOLONG);
-	printf("#define\tCLKBASE %d\n", CLKBASE);
-	printf("#define\tCLKCR1 %d\n", CLKCR1);
-	printf("#define\tCLKCR2 %d\n", CLKCR2);
-	printf("#define\tCLKCR3 %d\n", CLKCR3);
-	printf("#define\tCLKSR %d\n", CLKSR);
-	printf("#define\tCLKMSB1 %d\n", CLKMSB1);
-	printf("#define\tCLKMSB2 %d\n", CLKMSB2);
-	printf("#define\tCLKMSB3 %d\n", CLKMSB3);
-	printf("#define\tSYS_exit %d\n", SYS_exit);
-	printf("#define\tSYS_execve %d\n", SYS_execve);
-	printf("#define\tSYS_sigreturn %d\n", SYS_sigreturn);
-	for (i = 0; i < 32; i++)
-		if ((1 << i) & PCB_HPUXTRACE)
-			printf("#define\tPCB_TRCB %d\n", i);
+	/* general constants */
+	def("UPAGES", UPAGES);
+	def("NBPG", NBPG);
+	def("PGSHIFT", PGSHIFT);
+	def("USRSTACK", USRSTACK);
+	def("MAXADDR", MAXADDR);
+
+	/* proc fields and values */
+	off("P_LINK", struct proc, p_link);
+	off("P_RLINK", struct proc, p_rlink);
+	off("P_VMSPACE", struct proc, p_vmspace);
+	off("P_ADDR", struct proc, p_addr);
+	off("P_PRI", struct proc, p_pri);
+	off("P_STAT", struct proc, p_stat);
+	off("P_WCHAN", struct proc, p_wchan);
+	off("P_FLAG", struct proc, p_flag);
+	def("SSLEEP", SSLEEP);
+	def("SRUN", SRUN);
+
+	/* VM structure fields */
+	off("VM_PMAP", struct vmspace, vm_pmap);
+	off("PM_STCHG", struct pmap, pm_stchanged);
+
+	/* interrupt/fault metering */
+	off("V_SWTCH", struct vmmeter, v_swtch);
+	off("V_INTR", struct vmmeter, v_intr);
+
+	/* trap types (should just include trap.h?) */
+	def("T_BUSERR", T_BUSERR);
+	def("T_ADDRERR", T_ADDRERR);
+	def("T_ILLINST", T_ILLINST);
+	def("T_ZERODIV", T_ZERODIV);
+	def("T_CHKINST", T_CHKINST);
+	def("T_TRAPVINST", T_TRAPVINST);
+	def("T_PRIVINST", T_PRIVINST);
+	def("T_TRACE", T_TRACE);
+	def("T_MMUFLT", T_MMUFLT);
+	def("T_SSIR", T_SSIR);
+	def("T_FMTERR", T_FMTERR);
+	def("T_COPERR", T_COPERR);
+	def("T_FPERR", T_FPERR);
+	def("T_ASTFLT", T_ASTFLT);
+	def("T_TRAP15", T_TRAP15);
+	def("T_FPEMULI", T_FPEMULI);
+	def("T_FPEMULD", T_FPEMULD);
+
+	/* PSL values (should just include psl.h?) */
+	def("PSL_S", PSL_S);
+	def("PSL_IPL7", PSL_IPL7);
+	def("PSL_LOWIPL", PSL_LOWIPL);
+	def("PSL_HIGHIPL", PSL_HIGHIPL);
+	def("PSL_USER", PSL_USER);
+	def("SPL1", PSL_S | PSL_IPL1);
+	def("SPL2", PSL_S | PSL_IPL2);
+	def("SPL3", PSL_S | PSL_IPL3);
+	def("SPL4", PSL_S | PSL_IPL4);
+	def("SPL5", PSL_S | PSL_IPL5);
+	def("SPL6", PSL_S | PSL_IPL6);
+
+	/* magic */
+	def("FC_USERD", FC_USERD);
+	def("FC_PURGE", FC_PURGE);
+	def("INTIOBASE", INTIOBASE);
+	def("MMUBASE", MMUBASE);
+	def("MMUSTAT", MMUSTAT);
+	def("MMUCMD", MMUCMD);
+	def("MMUSSTP", MMUSSTP);
+	def("MMUUSTP", MMUUSTP);
+	def("MMUTBINVAL", MMUTBINVAL);
+	def("MMU_BERR", MMU_BERR);
+	def("MMU_ENAB", MMU_ENAB);
+	def("MMU_FAULT", MMU_FAULT);
+	def("MMU_CEN", MMU_CEN);
+	def("MMU_IEN", MMU_IEN);
+	def("MMU_FPE", MMU_FPE);
+	def("CACHE_ON", CACHE_ON);
+	def("CACHE_OFF", CACHE_OFF);
+	def("CACHE_CLR", CACHE_CLR);
+	def("IC_CLEAR", IC_CLEAR);
+	def("DC_CLEAR", DC_CLEAR);
+
+	/* pte/ste bits */
+	def("PG_V", PG_V);
+	def("PG_NV", PG_NV);
+	def("PG_RO", PG_RO);
+	def("PG_RW", PG_RW);
+	def("PG_CI", PG_CI);
+	def("PG_PROT", PG_PROT);
+	def("PG_FRAME", PG_FRAME);
+	def("SG_V", SG_V);
+	def("SG_NV", SG_NV);
+	def("SG_RW", SG_RW);
+	def("SG_FRAME", SG_FRAME);
+	def("SG_ISHIFT", SG_ISHIFT);
+
+	/* pcb fields */
+	off("PCB_FLAGS", struct pcb, pcb_flags);
+	off("PCB_PS", struct pcb, pcb_ps);
+	off("PCB_USTP", struct pcb, pcb_ustp);
+	off("PCB_USP", struct pcb, pcb_usp);
+	off("PCB_REGS", struct pcb, pcb_regs);
+	off("PCB_ONFAULT", struct pcb, pcb_onfault);
+	off("PCB_FPCTX", struct pcb, pcb_fpregs);
+	def("SIZEOF_PCB", sizeof(struct pcb));
+
+	/* system calls */
+	def("SYS_exit", SYS_exit);
+	def("SYS_execve", SYS_execve);
+	def("SYS_sigreturn", SYS_sigreturn);
+
+	/* errno */
+	def("EFAULT", EFAULT);
+	def("ENAMETOOLONG", ENAMETOOLONG);
+
+	/* clock registers */
+	def("CLKSR", CLKSR);
+	def("CLKMSB1", CLKMSB1);
+	def("CLKMSB3", CLKMSB3);
+
+	/* HP-UX trace bit */
+	def("PCB_TRCB", ffs(PCB_HPUXTRACE) - 1);
+
 	exit(0);
 }
