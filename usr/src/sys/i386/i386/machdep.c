d4557e638ed8f13e48c326911c967c3e0e1e6455
Chris Torek <torek@ucbvax.Berkeley.EDU>
1992-07-11 01:00:11 -0800

    ANSIfy syscall arguments
    SCCS-vsn: sys/i386/i386/machdep.c 7.14
    SCCS-vsn: sys/i386/i386/sys_machdep.c 5.6

--- usr/src/sys/i386/i386/machdep.c
+++ usr/src/sys/i386/i386/machdep.c
@@ -7,7 +7,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)machdep.c	7.13 (Berkeley) %G%
+ *	@(#)machdep.c	7.14 (Berkeley) %G%
  */
 
 #include "param.h"
@@ -395,11 +395,12 @@ sendsig(catcher, sig, mask, code)
  * psl to gain improper priviledges or to cause
  * a machine fault.
  */
+struct sigreturn_args {
+	struct sigcontext *sigcntxp;
+};
 sigreturn(p, uap, retval)
 	struct proc *p;
-	struct args {
-		struct sigcontext *sigcntxp;
-	} *uap;
+	struct sigreturn_args *uap;
 	int *retval;
 {
 	register struct sigcontext *scp;
