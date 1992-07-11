9f323b8ca161e2a18a0a35e09ec090ab6ea027f2
Chris Torek <torek@ucbvax.Berkeley.EDU>
1992-07-11 13:54:47 -0800

    fix swapper run stats
    SCCS-vsn: sys/kern/init_main.c 7.51

--- usr/src/sys/kern/init_main.c
+++ usr/src/sys/kern/init_main.c
@@ -2,7 +2,7 @@
  * Copyright (c) 1982, 1986 Regents of the University of California.
  * All rights reserved.  The Berkeley software License Agreement
  * specifies the terms and conditions for redistribution.
- *	@(#)init_main.c	7.50 (Berkeley) %G%
+ *	@(#)init_main.c	7.51 (Berkeley) %G%
  */
 
 #include "param.h"
@@ -218,8 +218,7 @@ main()
 	 * Now can look at time, having had a chance
 	 * to verify the time from the file system.
 	 */
-	runtime = mono_time = boottime = time;
-	p->p_stats->p_start = p->p_rtime = runtime;
+	p->p_stats->p_start = runtime = mono_time = boottime = time;
 
 	/*
 	 * make init process
