586a5d1b5576fd43c62f49846a3e4f9fdff6babc
Chris Torek <torek@ucbvax.Berkeley.EDU>
1992-07-11 01:07:14 -0800

    ANSIfy syscall args
    SCCS-vsn: sys/news3400/news3400/machdep.c 7.4

--- usr/src/sys/news3400/news3400/machdep.c
+++ usr/src/sys/news3400/news3400/machdep.c
@@ -11,7 +11,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)machdep.c	7.3 (Berkeley) %G%
+ *	@(#)machdep.c	7.4 (Berkeley) %G%
  */
 
 /* from: Utah $Hdr: machdep.c 1.63 91/04/24$ */
@@ -538,12 +538,13 @@ sendsig(catcher, sig, mask, code)
  * psl to gain improper priviledges or to cause
  * a machine fault.
  */
+struct sigreturn_args {
+	struct sigcontext *sigcntxp;
+};
 /* ARGSUSED */
 sigreturn(p, uap, retval)
 	struct proc *p;
-	struct args {
-		struct sigcontext *sigcntxp;
-	} *uap;
+	struct sigreturn_args *uap;
 	int *retval;
 {
 	register struct sigcontext *scp;
