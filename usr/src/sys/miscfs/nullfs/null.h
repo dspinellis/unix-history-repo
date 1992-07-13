d00d1635247e21b600d26ddddd90f7c11fc7c554
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1992-07-12 22:10:19 -0800

    move to rev 7
    SCCS-vsn: sys/miscfs/fdesc/fdesc.h 7.1
    SCCS-vsn: sys/miscfs/fdesc/fdesc_vfsops.c 7.1
    SCCS-vsn: sys/miscfs/fdesc/fdesc_vnops.c 7.1
    SCCS-vsn: sys/miscfs/nullfs/null.h 7.1

--- usr/src/sys/miscfs/nullfs/null.h
+++ usr/src/sys/miscfs/nullfs/null.h
@@ -8,7 +8,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)null.h	1.7 (Berkeley) %G%
+ *	@(#)null.h	7.1 (Berkeley) %G%
  *
  * $Id: lofs.h,v 1.8 1992/05/30 10:05:43 jsp Exp jsp $
  */
@@ -48,4 +48,3 @@ extern struct vnode *null_checkvp __P((struct vnode *vp, char *fil, int lno));
 extern int (**null_vnodeop_p)();
 extern struct vfsops null_vfsops;
 #endif /* KERNEL */
-
