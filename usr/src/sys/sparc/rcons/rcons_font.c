85a601907b27d1a35e3a57b7a56a4069896f6c5c
Chris Torek <torek@ucbvax.Berkeley.EDU>
1992-07-13 07:43:47 -0800

    date and time created 92/07/13 00:43:47 by torek
    SCCS-vsn: sys/sparc/rcons/raster_text.c 7.1
    SCCS-vsn: sys/sparc/rcons/rcons_font.c 7.1

---
+++ usr/src/sys/sparc/rcons/rcons_font.c
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 1991 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This software was developed by the Computer Systems Engineering group
+ * at Lawrence Berkeley Laboratory under DARPA contract BG 91-66 and
+ * contributed to Berkeley.
+ *
+ * %sccs.include.redist.c%
+ *
+ *	@(#)rcons_font.c	7.1 (Berkeley) %G%
+ *
+ * from: $Header: rcons_font.c,v 1.8 92/06/17 06:23:37 torek Exp $
+ */
+
+#ifdef KERNEL
+#include "sys/param.h"
+#include "sys/kernel.h"
+#include "sys/fbio.h"
+#include "sys/device.h"
+#include "machine/fbvar.h"
+#else
+#include <sys/types.h>
+#include "myfbdevice.h"
+#endif
+
+#include "raster.h"
+
+#include "gallant19.h"
+
+void
+rcons_font(fb)
+	register struct fbdevice *fb;
+{
+
+	/* XXX really rather get this from the prom */
+	fb->fb_font = &gallant19;
+
+	/* Get distance to top and bottom of font from font origin */
+	fb->fb_font_ascent = -(fb->fb_font->chars)['a'].homey;
+}
