ce9819734e28ff6b4a38da158e7d4abe7034f7fd
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-07-13 03:10:05 -0800

    make the min/max functions inlines
    SCCS-vsn: sys/libkern/libkern.h 7.4

--- usr/src/sys/libkern/libkern.h
+++ usr/src/sys/libkern/libkern.h
@@ -4,22 +4,64 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)libkern.h	7.3 (Berkeley) %G%
+ *	@(#)libkern.h	7.4 (Berkeley) %G%
  */
 
 #include <sys/types.h>
 
-/* Prototypes for non-quad routines. */
+static inline int
+imax(a, b)
+	int a, b;
+{
+	return (a > b ? a : b);
+}
+static inline int
+imin(a, b)
+	int a, b;
+{
+	return (a < b ? a : b);
+}
+static inline long
+lmax(a, b)
+	long a, b;
+{
+	return (a > b ? a : b);
+}
+static inline long
+lmin(a, b)
+	long a, b;
+{
+	return (a < b ? a : b);
+}
+static inline u_int
+max(a, b)
+	u_int a, b;
+{
+	return (a > b ? a : b);
+}
+static inline u_int
+min(a, b)
+	u_int a, b;
+{
+	return (a < b ? a : b);
+}
+static inline u_long
+ulmax(a, b)
+	u_long a, b;
+{
+	return (a > b ? a : b);
+}
+static inline u_long
+ulmin(a, b)
+	u_long a, b;
+{
+	return (a < b ? a : b);
+}
 
+/* Prototypes for non-quad routines. */
 int	 bcmp __P((const void *, const void *, size_t));
 int	 ffs __P((int));
-int	 imax __P((int, int));
-int	 imin __P((int, int));
-long	 lmax __P((long, long));
-long	 lmin __P((long, long));
 int	 locc __P((int, char *, u_int));
-u_int	 max __P((u_int, u_int));
-u_int	 min __P((u_int, u_int));
 u_long	 random __P((void));
 int	 scanc __P((u_int, u_char *, u_char *, int));
 int	 skpc __P((int, int, char *));
@@ -27,5 +69,3 @@ char	*strcat __P((char *, const char *));
 char	*strcpy __P((char *, const char *));
 size_t	 strlen __P((const char *));
 char	*strncpy __P((char *, const char *, size_t));
-u_long	 ulmax __P((u_long, u_long));
-u_long	 ulmin __P((u_long, u_long));
