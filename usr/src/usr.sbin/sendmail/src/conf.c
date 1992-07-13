199041c135a81f39741a783f34bb16a4fda5d9e5
Eric Allman <eric@ucbvax.Berkeley.EDU>
1992-07-12 19:34:49 -0800

    change setproctitle so it will print sendmail: in 4.4bsd
    SCCS-vsn: usr.sbin/sendmail/src/version.c 5.105
    SCCS-vsn: usr.sbin/sendmail/src/conf.c 5.38

--- usr/src/usr.sbin/sendmail/src/conf.c
+++ usr/src/usr.sbin/sendmail/src/conf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)conf.c	5.37 (Berkeley) %G%";
+static char sccsid[] = "@(#)conf.c	5.38 (Berkeley) %G%";
 #endif /* not lint */
 
 # include <sys/ioctl.h>
