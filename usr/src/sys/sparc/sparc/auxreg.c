dd182f26937f56a163a9e2e9904343ed62d91e5f
Chris Torek <torek@ucbvax.Berkeley.EDU>
1992-07-13 07:44:28 -0800

    date and time created 92/07/13 00:44:28 by torek
    SCCS-vsn: sys/sparc/sparc/auxreg.c 7.1
    SCCS-vsn: sys/sparc/sparc/auxreg.h 7.1

---
+++ usr/src/sys/sparc/sparc/auxreg.c
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 1992 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This software was developed by the Computer Systems Engineering group
+ * at Lawrence Berkeley Laboratory under DARPA contract BG 91-66 and
+ * contributed to Berkeley.
+ *
+ * %sccs.include.redist.c%
+ *
+ *	@(#)auxreg.c	7.1 (Berkeley) %G%
+ *
+ * from: $Header: auxreg.c,v 1.7 92/06/17 05:21:54 torek Exp $ (LBL)
+ */
+
+#include "sys/param.h"
+#include "sys/device.h"
+#include "sys/kernel.h"
+
+#include "machine/autoconf.h"
+
+#include "vaddrs.h"
+#include "auxreg.h"
+
+static void auxregattach __P((struct device *, struct device *, void *));
+struct cfdriver auxregcd =
+    { NULL, "auxiliary-io", matchbyname, auxregattach,
+      DV_DULL, sizeof(struct device) };
+
+#ifdef BLINK
+static int
+blink(zero)
+	void *zero;
+{
+	register int s;
+	register fixpt_t lav;
+
+	s = splhigh();
+	*AUXIO_REG = (*AUXIO_REG | AUXIO_MB1) ^ AUXIO_LED;
+	splx(s);
+	/*
+	 * Blink rate is:
+	 *	full cycle every second if completely idle (loadav = 0)
+	 *	full cycle every 2 seconds if loadav = 1
+	 *	full cycle every 3 seconds if loadav = 2
+	 * etc.
+	 */
+	s = (((averunnable[0] + FSCALE) * hz) >> (FSHIFT + 1));
+	timeout(blink, (caddr_t)0, s);
+}
+#endif
+
+/* ARGSUSED */
+static void
+auxregattach(parent, self, aux)
+	struct device *parent, *self;
+	void *aux;
+{
+	struct romaux *ra = aux;
+
+	(void)mapdev(ra->ra_paddr, AUXREG_VA, sizeof(long));
+	printf("\n");
+#ifdef BLINK
+	blink((caddr_t)0);
+#endif
+}
