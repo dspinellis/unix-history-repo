683875c6f5679eb4ea77dc96e12329da5f750944
Chris Torek <torek@ucbvax.Berkeley.EDU>
1992-07-13 07:44:40 -0800

    date and time created 92/07/13 00:44:40 by torek
    SCCS-vsn: sys/sparc/sparc/memreg.c 7.1
    SCCS-vsn: sys/sparc/sparc/memreg.h 7.1
    SCCS-vsn: sys/sparc/sparc/pmap.c 7.1

---
+++ usr/src/sys/sparc/sparc/memreg.c
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 1992 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This software was developed by the Computer Systems Engineering group
+ * at Lawrence Berkeley Laboratory under DARPA contract BG 91-66 and
+ * contributed to Berkeley.
+ *
+ * %sccs.include.redist.c%
+ *
+ *	@(#)memreg.c	7.1 (Berkeley) %G%
+ *
+ * from: $Header: memreg.c,v 1.4 92/06/17 05:22:17 torek Exp $ (LBL)
+ */
+
+#include "sys/param.h"
+#include "sys/device.h"
+
+#include "machine/autoconf.h"
+
+#include "ctlreg.h"
+#include "memreg.h"
+
+static void memregattach __P((struct device *, struct device *, void *));
+struct cfdriver memregcd =
+    { NULL, "memory-error", matchbyname, memregattach,
+      DV_DULL, sizeof(struct device) };
+
+/* ARGSUSED */
+static void
+memregattach(parent, self, aux)
+	struct device *parent, *self;
+	void *aux;
+{
+	struct romaux *ra = aux;
+
+	par_err_reg = ra->ra_vaddr ? (volatile int *)ra->ra_vaddr :
+	    (volatile int *)mapiodev(ra->ra_paddr, sizeof(int));
+	printf("\n");
+}
+
+/*
+ * Synchronous and asynchronous memory error handler.
+ * (This is the level 15 interrupt, which is not vectored.)
+ * Should kill the process that got its bits clobbered,
+ * and take the page out of the page pool, but for now...
+ */
+void
+memerr(issync, ser, sva, aer, ava)
+	int issync, ser, sva, aer, ava;
+{
+
+	printf("%ssync mem err: ser=%b sva=%x aer=%b ava=%x\n",
+	    issync ? "" : "a", ser, SER_BITS, sva, aer & 0xff, AER_BITS, ava);
+	if (par_err_reg)
+		printf("parity error register = %b\n", *par_err_reg, PER_BITS);
+#ifdef DEBUG
+	callrom();
+#else
+	panic("memory error");		/* XXX */
+#endif
+}
