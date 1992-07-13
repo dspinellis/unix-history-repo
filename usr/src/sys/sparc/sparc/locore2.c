6b3b2e6e72f3aac801fae49923a7cbab22449ef4
Chris Torek <torek@ucbvax.Berkeley.EDU>
1992-07-13 07:44:38 -0800

    date and time created 92/07/13 00:44:38 by torek
    SCCS-vsn: sys/sparc/sparc/locore2.c 7.1

---
+++ usr/src/sys/sparc/sparc/locore2.c
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 1992 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This software was developed by the Computer Systems Engineering group
+ * at Lawrence Berkeley Laboratory under DARPA contract BG 91-66 and
+ * contributed to Berkeley.
+ *
+ * %sccs.include.redist.c%
+ *
+ *	@(#)locore2.c	7.1 (Berkeley) %G%
+ *
+ * from: $Header: locore2.c,v 1.7 92/06/20 08:47:28 mccanne Exp $ (LBL)
+ */
+
+/*
+ * Primitives which are in locore.s on other machines,
+ * but which have no reason to be assembly-coded on SPARC.
+ */
+
+#include "param.h"
+#include "proc.h"
+#include "resourcevar.h"
+
+int	whichqs;
+
+/*
+ * Put process p on the run queue indicated by its priority.
+ * Calls should be made at splstatclock(), and p->p_stat should be SRUN.
+ */
+setrq(p)
+	register struct proc *p;
+{
+	register struct prochd *q;
+	register struct proc *oldlast;
+	register int which = p->p_pri >> 2;
+
+	if (p->p_rlink != NULL)
+		panic("setrq");
+	q = &qs[which];
+	whichqs |= 1 << which;
+	p->p_link = (struct proc *)q;
+	p->p_rlink = oldlast = q->ph_rlink;
+	q->ph_rlink = p;
+	oldlast->p_link = p;
+}
+
+/*
+ * Remove process p from its run queue, which should be the one
+ * indicated by its priority.  Calls should be made at splstatclock().
+ */
+remrq(p)
+	register struct proc *p;
+{
+	register int which = p->p_pri >> 2;
+	register struct prochd *q;
+
+	if ((whichqs & (1 << which)) == 0)
+		panic("remrq");
+	p->p_link->p_rlink = p->p_rlink;
+	p->p_rlink->p_link = p->p_link;
+	p->p_rlink = NULL;
+	q = &qs[which];
+	if (q->ph_link == (struct proc *)q)
+		whichqs &= ~(1 << which);
+}
