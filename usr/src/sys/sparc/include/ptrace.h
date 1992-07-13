0e1975517b18ff7da0045fdf5e9dd3050ae5f0b6
Chris Torek <torek@ucbvax.Berkeley.EDU>
1992-07-13 07:43:05 -0800

    date and time created 92/07/13 00:43:05 by torek
    SCCS-vsn: sys/sparc/include/psl.h 7.1
    SCCS-vsn: sys/sparc/include/pte.h 7.1
    SCCS-vsn: sys/sparc/include/ptrace.h 7.1

---
+++ usr/src/sys/sparc/include/ptrace.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 1992 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This software was developed by the Computer Systems Engineering group
+ * at Lawrence Berkeley Laboratory under DARPA contract BG 91-66 and
+ * contributed to Berkeley.
+ *
+ * %sccs.include.redist.c%
+ *
+ *	@(#)ptrace.h	7.1 (Berkeley) %G%
+ *
+ * from: $Header: ptrace.h,v 1.5 92/06/17 06:10:26 torek Exp $ (LBL)
+ */
+
+/*
+ * SPARC-dependent ptrace definitions.
+ */
+#define	PT_GETREGS	(PT_FIRSTMACH + 0)
+#define	PT_SETREGS	(PT_FIRSTMACH + 1)
+#define	PT_GETFPREGS	(PT_FIRSTMACH + 2)
+#define	PT_SETFPREGS	(PT_FIRSTMACH + 3)
+
