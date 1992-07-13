5be84a962943e932ad97977ee468c13df8a3d876
Thomas Ferrin <tef@ucbvax.Berkeley.EDU>
1992-07-13 05:57:56 -0800

    add -s, -l and -p options for better conformance with RFC1196
    SCCS-vsn: libexec/fingerd/fingerd.c 5.9

--- usr/src/libexec/fingerd/fingerd.c
+++ usr/src/libexec/fingerd/fingerd.c
@@ -12,7 +12,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)fingerd.c	5.8 (Berkeley) %G%";
+static char sccsid[] = "@(#)fingerd.c	5.9 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <errno.h>
@@ -20,12 +20,18 @@ static char sccsid[] = "@(#)fingerd.c	5.8 (Berkeley) %G%";
 #include <stdio.h>
 #include <stdlib.h>
 #include <strings.h>
+#include <syslog.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 #include "pathnames.h"
 
 void err __P((const char *, ...));
 
 int
-main()
+main(int argc, char **argv)
 {
 	register FILE *fp;
 	register int ch;
@@ -33,16 +39,40 @@ main()
 	int p[2];
 #define	ENTRIES	50
 	char **ap, *av[ENTRIES + 1], **comp, line[1024];
-
-#ifdef LOGGING					/* Unused for now. */
-#include <netinet/in.h>
+	char *prog = _PATH_FINGER;
+	struct hostent *hp;
 	struct sockaddr_in sin;
-	int sval;
+	int sval = sizeof(sin);
+	int secure=0, logging=0;
+	extern char *optarg;
 
-	sval = sizeof(sin);
-	if (getpeername(0, &sin, &sval) < 0)
-		err("getpeername: %s", strerror(errno));
-#endif
+	openlog("fingerd", LOG_PID|LOG_CONS, LOG_DAEMON);
+	while ((ch=getopt(argc, argv, "slp:")) != EOF)
+		switch (ch) {
+		case 's':
+			secure++;
+			break;
+		case 'l':
+			logging++;
+			break;
+		case 'p':
+			prog = optarg;
+			break;
+		case '?':
+		default:
+			syslog(LOG_ERR, "unknown option: %c", ch);
+		}
+
+	if (logging) {
+		if (getpeername(0, (struct sockaddr *)&sin, &sval) < 0)
+			err("getpeername: %s", strerror(errno));
+		if (hp = gethostbyaddr((char *)&sin.sin_addr.s_addr,
+				sizeof(sin.sin_addr.s_addr), AF_INET))
+			lp = hp->h_name;
+		else
+			lp = inet_ntoa(sin.sin_addr);
+		syslog(LOG_NOTICE, "query from %s", lp);
+	}
 
 	if (!fgets(line, sizeof(line), stdin))
 		exit(1);
@@ -50,8 +80,18 @@ main()
 	comp = &av[1];
 	for (lp = line, ap = &av[2];;) {
 		*ap = strtok(lp, " \t\r\n");
-		if (*ap == NULL)
+		if (!*ap) {
+			if (secure && ap == &av[2]) {
+				puts("must provide username\r\n");
+				exit(1);
+			}
 			break;
+		}
+		if (secure && strchr(*ap, '@')) {
+			puts("fowarding service denied\r\n");
+			exit(1);
+		}
+
 		/* RFC742: "/[Ww]" == "-l" */
 		if ((*ap)[0] == '/' && ((*ap)[1] == 'W' || (*ap)[1] == 'w')) {
 			av[1] = "-l";
@@ -61,8 +101,11 @@ main()
 			break;
 		lp = NULL;
 	}
-	*comp = "finger";
 
+	if (lp = strrchr(prog, '/'))
+		*comp = ++lp;
+	else
+		*comp = prog;
 	if (pipe(p) < 0)
 		err("pipe: %s", strerror(errno));
 
@@ -73,8 +116,8 @@ main()
 			(void)dup2(p[1], 1);
 			(void)close(p[1]);
 		}
-		execv(_PATH_FINGER, comp);
-		err("execv: %s: %s", _PATH_FINGER, strerror(errno));
+		execv(prog, comp);
+		err("execv: %s: %s", prog, strerror(errno));
 		_exit(1);
 	case -1:
 		err("fork: %s", strerror(errno));
@@ -111,10 +154,8 @@ err(fmt, va_alist)
 #else
 	va_start(ap);
 #endif
-	(void)fprintf(stderr, "fingerd: ");
-	(void)vfprintf(stderr, fmt, ap);
+	(void)vsyslog(LOG_ERR, fmt, ap);
 	va_end(ap);
-	(void)fprintf(stderr, "\r\n");
 	exit(1);
 	/* NOTREACHED */
 }
