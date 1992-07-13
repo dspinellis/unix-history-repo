93bd60bb5a436440e31a7b26bdf4fb689bd350aa
Jan-Simon Pendry <pendry@ucbvax.Berkeley.EDU>
1992-07-13 00:34:55 -0800

    need to include <sys/systm.h>
    SCCS-vsn: sys/nfs/nfs_bio.c 7.31
    SCCS-vsn: sys/nfs/nfs_socket.c 7.35

--- usr/src/sys/nfs/nfs_bio.c
+++ usr/src/sys/nfs/nfs_bio.c
@@ -7,10 +7,11 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)nfs_bio.c	7.30 (Berkeley) %G%
+ *	@(#)nfs_bio.c	7.31 (Berkeley) %G%
  */
 
 #include <sys/param.h>
+#include <sys/systm.h>
 #include <sys/resourcevar.h>
 #include <sys/proc.h>
 #include <sys/buf.h>
