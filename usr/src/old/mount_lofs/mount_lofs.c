dd29c964851bf72ece77b03434a2c9d404db6772
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-07-12 22:14:12 -0800

    move file systems into misclfs
    SCCS-vsn: old/mount_lofs/mount_lofs.c 5.3

--- usr/src/old/mount_lofs/mount_lofs.c
+++ usr/src/old/mount_lofs/mount_lofs.c
@@ -8,12 +8,12 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)mount_lofs.c	5.2 (Berkeley) %G%
+ *	@(#)mount_lofs.c	5.3 (Berkeley) %G%
  */
 
 #include <sys/param.h>
 #include <sys/mount.h>
-#include <lofs/lofs.h>
+#include <miscfs/lofs/lofs.h>
 
 #include <errno.h>
 #include <stdio.h>
