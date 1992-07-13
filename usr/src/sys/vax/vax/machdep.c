d8b9020cb55a2ab8646fbc65392e156a2a590307
Chris Torek <torek@ucbvax.Berkeley.EDU>
1992-07-12 22:42:56 -0800

    ANSIfy syscall args
    SCCS-vsn: sys/tahoe/tahoe/sys_machdep.c 7.7
    SCCS-vsn: sys/tahoe/tahoe/machdep.c 7.16
    SCCS-vsn: sys/vax/vax/machdep.c 7.33
    SCCS-vsn: sys/vax/vax/sys_machdep.c 7.8

--- usr/src/sys/vax/vax/machdep.c
+++ usr/src/sys/vax/vax/machdep.c
@@ -3,7 +3,7 @@
  * All rights reserved.  The Berkeley software License Agreement
  * specifies the terms and conditions for redistribution.
  *
- *	@(#)machdep.c	7.32 (Berkeley) %G%
+ *	@(#)machdep.c	7.33 (Berkeley) %G%
  */
 
 #include "sys/param.h"
@@ -423,12 +423,13 @@ sendsig(catcher, sig, mask, code)
  * psl to gain improper priviledges or to cause
  * a machine fault.
  */
+struct sigreturn_args {
+	struct sigcontext *sigcntxp;
+};
 /* ARGSUSED */
 sigreturn(p, uap, retval)
 	struct proc *p;
-	struct args {
-		struct sigcontext *sigcntxp;
-	} *uap;
+	struct sigreturn_args *uap;
 	int *retval;
 {
 	register struct sigcontext *scp;
