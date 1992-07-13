4f1870d8046aeb5d09c829f1260107846294f95a
Eric Allman <eric@ucbvax.Berkeley.EDU>
1992-07-12 21:44:09 -0800

    lint
    SCCS-vsn: usr.sbin/sendmail/src/headers.c 5.21
    SCCS-vsn: usr.sbin/sendmail/src/parseaddr.c 5.21
    SCCS-vsn: usr.sbin/sendmail/src/daemon.c 5.47
    SCCS-vsn: usr.sbin/sendmail/src/deliver.c 5.61
    SCCS-vsn: usr.sbin/sendmail/src/queue.c 5.44
    SCCS-vsn: usr.sbin/sendmail/src/mci.c 5.3

--- usr/src/usr.sbin/sendmail/src/deliver.c
+++ usr/src/usr.sbin/sendmail/src/deliver.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)deliver.c	5.60 (Berkeley) %G%";
+static char sccsid[] = "@(#)deliver.c	5.61 (Berkeley) %G%";
 #endif /* not lint */
 
 #include "sendmail.h"
@@ -705,8 +705,6 @@ openmailer(m, pvp, ctladdr, clever, pmfile, prfile)
 		 strcmp(m->m_mailer, "[TCP]") == 0)
 	{
 #ifdef DAEMON
-		register STAB *st;
-		extern STAB *stab();
 		register int i;
 		register u_short port;
 		int nmx;
