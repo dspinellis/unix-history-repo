7963ffe22a90a872768ac085a17701c6f1c3e823
Eric Allman <eric@ucbvax.Berkeley.EDU>
1992-07-12 19:47:43 -0800

    fix botch in filling out to LastArgv
    SCCS-vsn: usr.sbin/sendmail/src/conf.c 5.39

--- usr/src/usr.sbin/sendmail/src/conf.c
+++ usr/src/usr.sbin/sendmail/src/conf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)conf.c	5.38 (Berkeley) %G%";
+static char sccsid[] = "@(#)conf.c	5.39 (Berkeley) %G%";
 #endif /* not lint */
 
 # include <sys/ioctl.h>
