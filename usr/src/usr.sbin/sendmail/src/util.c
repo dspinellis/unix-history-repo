bb3fc191d7b55b9fa8ea6e21cc7673907a1c1423
Eric Allman <eric@ucbvax.Berkeley.EDU>
1992-07-14 19:58:46 -0800

    fix small bug in buildfname
    SCCS-vsn: usr.sbin/sendmail/src/version.c 5.108
    SCCS-vsn: usr.sbin/sendmail/src/util.c 5.28

--- usr/src/usr.sbin/sendmail/src/util.c
+++ usr/src/usr.sbin/sendmail/src/util.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)util.c	5.27 (Berkeley) %G%";
+static char sccsid[] = "@(#)util.c	5.28 (Berkeley) %G%";
 #endif /* not lint */
 
 # include <stdio.h>
@@ -370,7 +370,7 @@ buildfname(gecos, login, buf)
 	/* now fill in buf */
 	if (quoteit)
 		*bp++ = '"';
-	while (*p != '\0' && *p != ',' && *p != ';' && *p != '%')
+	for (p = gecos; *p != '\0' && *p != ',' && *p != ';' && *p != '%'; p++)
 	{
 		if (*p == '&')
 		{
@@ -378,10 +378,9 @@ buildfname(gecos, login, buf)
 			*bp = toupper(*bp);
 			while (*bp != '\0')
 				bp++;
-			p++;
 		}
 		else
-			*bp++ = *p++;
+			*bp++ = *p;
 	}
 	if (quoteit)
 		*bp++ = '"';
