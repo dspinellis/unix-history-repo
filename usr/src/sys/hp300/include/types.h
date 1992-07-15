367c08b2dd47b707ce8cf3a5fe99c2861f468217
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-07-14 21:21:34 -0800

    add basic data types list
    SCCS-vsn: sys/hp300/include/types.h 7.10

--- usr/src/sys/hp300/include/types.h
+++ usr/src/sys/hp300/include/types.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)types.h	7.9 (Berkeley) %G%
+ *	@(#)types.h	7.10 (Berkeley) %G%
  */
 
 #ifndef	_MACHTYPES_H_
@@ -14,11 +14,24 @@ typedef struct _physadr {
 	short r[1];
 } *physadr;
 
-typedef struct label_t {		/* consistent with HP-UX */
+typedef struct label_t {			/* Consistent with HP-UX. */
 	int val[15];
 } label_t;
 
 typedef	u_long	vm_offset_t;
 typedef	u_long	vm_size_t;
+
+#ifdef	__GNUC__
+typedef	char			s1byte_t;	/* Basic data types. */
+typedef	unsigned char		u1byte_t;
+typedef	short			s2byte_t;
+typedef	unsigned short		u2byte_t;
+typedef	long			s4byte_t;
+typedef	unsigned long		u4byte_t;
+typedef	long long		s8byte_t;
+typedef	unsigned long long	u8byte_t;
+typedef	float			f4byte_t;
+typedef	double			f8byte_t;
+#endif
 
 #endif	/* _MACHTYPES_H_ */
