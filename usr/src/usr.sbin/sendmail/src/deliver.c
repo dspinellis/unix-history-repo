c140a82cf05dfc03a6be5377f93d6f342cc12281
Eric Allman <eric@ucbvax.Berkeley.EDU>
1992-07-14 20:09:03 -0800

    fix "user open" setproctitle message
    SCCS-vsn: usr.sbin/sendmail/src/deliver.c 5.64

--- usr/src/usr.sbin/sendmail/src/deliver.c
+++ usr/src/usr.sbin/sendmail/src/deliver.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)deliver.c	5.63 (Berkeley) %G%";
+static char sccsid[] = "@(#)deliver.c	5.64 (Berkeley) %G%";
 #endif /* not lint */
 
 #include "sendmail.h"
