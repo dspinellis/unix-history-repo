98f4f527b95de828e1350c1480e2f05df2189cf0
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-07-17 16:52:09 -0800

    lint
    SCCS-vsn: lib/libc/db/hash/hash_page.c 5.22
    SCCS-vsn: lib/libc/db/btree/bt_open.c 5.18
    SCCS-vsn: lib/libc/db/recno/rec_seq.c 5.4

--- usr/src/lib/libc/db/recno/rec_seq.c
+++ usr/src/lib/libc/db/recno/rec_seq.c
@@ -6,14 +6,16 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)rec_seq.c	5.3 (Berkeley) %G%";
+static char sccsid[] = "@(#)rec_seq.c	5.4 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/types.h>
+
 #include <errno.h>
 #include <db.h>
 #include <limits.h>
 #include <stdio.h>
+#include <strings.h>
 #include "recno.h"
 
 /*
