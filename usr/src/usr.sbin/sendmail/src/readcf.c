26cc38da412158931338cee05bcdd4c12dc9db5a
Eric Allman <eric@ucbvax.Berkeley.EDU>
1992-07-19 21:47:09 -0800

    option E has to be in a safe environment; otherwise you could point at a file that you can't otherwise read
    SCCS-vsn: usr.sbin/sendmail/src/readcf.c 5.44

--- usr/src/usr.sbin/sendmail/src/readcf.c
+++ usr/src/usr.sbin/sendmail/src/readcf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)readcf.c	5.43 (Berkeley) %G%";
+static char sccsid[] = "@(#)readcf.c	5.44 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
