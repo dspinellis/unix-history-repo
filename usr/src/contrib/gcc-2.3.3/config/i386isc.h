5da92b90f1d359b695f57b2bd59c379899c8e4df
CSRG <csrg@ucbvax.Berkeley.EDU>
1992-07-21 00:57:41 -0800

    BSD 4_4_Lite2 development Work on file usr/src/contrib/gcc-2.3.3/config/i386isc.h
    Synthesized-from: CSRG/cd3/4.4BSD-Lite2

---
+++ usr/src/contrib/gcc-2.3.3/config/i386isc.h
@@ -0,0 +1,10 @@
+/* Definitions for Intel 386 running Interactive Unix System V.
+   Specifically, this is for recent versions that support POSIX;
+   for version 2.0.2, use configuration option i386-sysv instead.  */
+
+/* Mostly it's like AT&T Unix System V. */
+
+#include "i386v.h"
+
+/* But with a few changes.  */
+#include "isc.h"
