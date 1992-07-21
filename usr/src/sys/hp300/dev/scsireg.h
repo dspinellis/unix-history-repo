60e2879af04d6c7ff1a1f872a8cc97f98e5a42a6
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-07-20 19:06:58 -0800

    shouldn't include ioctl.h here
    SCCS-vsn: sys/hp300/dev/scsireg.h 7.5

--- usr/src/sys/hp300/dev/scsireg.h
+++ usr/src/sys/hp300/dev/scsireg.h
@@ -7,7 +7,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)scsireg.h	7.4 (Berkeley) %G%
+ *	@(#)scsireg.h	7.5 (Berkeley) %G%
  */
 
 /*
@@ -418,13 +418,6 @@ struct scsi_cache {
 	u_char	minimum_prefetch_multiplier;
 	u_char	reserved[8];
 };
-
-/*
- * Driver ioctl's for various scsi operations.
- */
-#ifndef _IOCTL_
-#include "ioctl.h"
-#endif
 
 /*
  * Control for SCSI "format" mode.
