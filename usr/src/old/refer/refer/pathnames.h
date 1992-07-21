6a58ea90d65db269a4114eca72566f50ff7a67d6
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-07-21 04:07:23 -0800

    update paths, Ind* now installed in /usr/old
    SCCS-vsn: old/refer/refer/pathnames.h 5.5

--- usr/src/old/refer/refer/pathnames.h
+++ usr/src/old/refer/refer/pathnames.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)pathnames.h	5.4 (Berkeley) %G%
+ *	@(#)pathnames.h	5.5 (Berkeley) %G%
  */
 
 #include <paths.h>
@@ -12,7 +12,7 @@
 #define	_PATH_ALL	"/usr/dict/lookall/All"
 #define	_PATH_EIGN	"/usr/dict/eign"
 #define	_PATH_HUNT	"/usr/old/libexec/hunt"
-#define	_PATH_IND	"/var/db/Ind"
+#define	_PATH_IND	"/usr/old/dict/papers/Ind"
 #define	_PATH_LIB	"/usr/old/libexec"
 #define	_PATH_MKEY	"/usr/old/libexec/mkey"
 #define	_PATH_PWD	"/bin/pwd"
