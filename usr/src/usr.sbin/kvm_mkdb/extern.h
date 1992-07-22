3e43c127cbb216cf8387545b79d9789ce6cf936e
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-07-21 21:03:04 -0800

    date and time created 92/07/21 14:03:04 by bostic
    SCCS-vsn: usr.sbin/kvm_mkdb/extern.h 5.1

---
+++ usr/src/usr.sbin/kvm_mkdb/extern.h
@@ -0,0 +1,12 @@
+/*-
+ * Copyright (c) 1992 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.redist.c%
+ *
+ *	@(#)extern.h	5.1 (Berkeley) %G%
+ */
+
+void	create_knlist __P((char *, DB *));
+void	error __P((char *));
+int	testdb __P(());
