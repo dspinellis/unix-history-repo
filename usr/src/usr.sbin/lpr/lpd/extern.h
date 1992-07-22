c57b454439d38a842878fab2f1adf0e927b29cc7
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-07-21 17:29:34 -0800

    date and time created 92/07/21 10:29:34 by bostic
    SCCS-vsn: usr.sbin/lpr/lpd/extern.h 5.1

---
+++ usr/src/usr.sbin/lpr/lpd/extern.h
@@ -0,0 +1,11 @@
+/*-
+ * Copyright (c) 1992 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.redist.c%
+ *
+ *	@(#)extern.h	5.1 (Berkeley) %G%
+ */
+
+void       printjob __P((void));
+int        recvjob __P((void));
