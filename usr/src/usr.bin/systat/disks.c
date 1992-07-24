e35483e0820be985ddbd8f2bf266b4eea2250078
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-07-24 00:57:34 -0800

    update copyright/header notice
    SCCS-vsn: usr.bin/systat/cmdtab.c 5.3
    SCCS-vsn: usr.bin/systat/disks.c 5.12

--- usr/src/usr.bin/systat/disks.c
+++ usr/src/usr.bin/systat/disks.c
@@ -1,12 +1,13 @@
-/*
- * Copyright (c) 1980 Regents of the University of California.
- * All rights reserved.  The Berkeley software License Agreement
- * specifies the terms and conditions for redistribution.
+/*-
+ * Copyright (c) 1980, 1992 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.proprietary.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)disks.c	5.11 (Berkeley) %G%";
-#endif not lint
+static char sccsid[] = "@(#)disks.c	5.12 (Berkeley) %G%";
+#endif /* not lint */
 
 #include <sys/types.h>
 #include <sys/buf.h>
