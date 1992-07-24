3c5e290cf52a1dcd5340a5fc28e413207c11a3f2
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-07-23 21:59:11 -0800

    date and time created 92/07/23 14:59:11 by bostic
    SCCS-vsn: libexec/rbootd/pathnames.h 5.1

---
+++ usr/src/libexec/rbootd/pathnames.h
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 1992 Regents of the University of California.
+ * Copyright (c) 1988, 1992 The University of Utah and the Center
+ *	for Software Science (CSS).
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to Berkeley by
+ * the Center for Software Science of the University of Utah Computer
+ * Science Department.  CSS requests users of this software to return
+ * to css-dist@cs.utah.edu any improvements that they make and grant
+ * CSS redistribution rights.
+ *
+ * %sccs.include.redist.c%
+ *
+ *	@(#)pathnames.h	5.1 (Berkeley) %G%
+ *
+ * Utah $Hdr: pathnames.h 3.1 92/07/06$
+ * Author: Jeff Forys, University of Utah CSS
+ */
+
+#define	_PATH_RBOOTDCONF	"/etc/rbootd.conf"
+#define	_PATH_RBOOTDPID		"/etc/rbootd.pid"
+#define	_PATH_RBOOTDLIB		"/usr/lib/rbootd"
+#define	_PATH_RBOOTDDBG		"/usr/tmp/rbootd.dbg"
+
+#define	_PATH_BPF		"/dev/bpf%d"
