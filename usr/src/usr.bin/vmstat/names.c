5bf199c8cc5401c5014d5063666e94a8b51c685b
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-07-24 19:44:39 -0800

    make names.c useable by systat; read_names now returns a value instead of exiting, add support (completely untested) for Sun's
    SCCS-vsn: usr.bin/vmstat/names.c 5.5
    SCCS-vsn: usr.bin/vmstat/vmstat.c 5.35

--- usr/src/usr.bin/vmstat/names.c
+++ usr/src/usr.bin/vmstat/names.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)names.c	5.4 (Berkeley) %G%
+ *	@(#)names.c	5.5 (Berkeley) %G%
  */
 
 #if !defined(hp300) && !defined(tahoe) && !defined(vax)
@@ -16,7 +16,7 @@ char *defdrives[] = { 0 };
 
 char *defdrives[] = { "sd0", "sd1", "sd2", "rd0", "rd1", "rd2", 0 };
 
-void
+int
 read_names()
 {
 	register char *p;
@@ -28,9 +28,9 @@ read_names()
 
 	hp = nl[X_HPDINIT].n_value;
 	if (hp == 0) {
-		(void) fprintf(stderr,
-		    "vmstat: disk init info not in namelist\n");
-		exit(1);
+		(void)fprintf(stderr,
+		    "disk init info not in namelist\n");
+		return (0);
 	}
 	p = buf;
 	for (;; hp += sizeof hdev) {
@@ -45,6 +45,7 @@ read_names()
 		dr_name[hdev.hp_dk] = p;
 		p += sprintf(p, "%s%d", name, hdev.hp_unit) + 1;
 	}
+	return (1);
 }
 #endif /* hp300 */
 
@@ -53,7 +54,7 @@ read_names()
 
 char *defdrives[] = { "dk0", "dk1", "dk2", 0 };
 
-void
+int
 read_names()
 {
 	register char *p;
@@ -65,8 +66,8 @@ read_names()
 	up = (struct vba_device *) nl[X_VBDINIT].n_value;
 	if (up == 0) {
 		(void) fprintf(stderr,
-		    "vmstat: disk init info not in namelist\n");
-		exit(1);
+		    "disk init info not in namelist\n");
+		return (0);
 	}
 	p = buf;
 	for (;; up += sizeof udev) {
@@ -80,6 +81,7 @@ read_names()
 		dr_name[udev.ui_dk] = p;
 		p += sprintf(p, "%s%d", name, udev.ui_unit);
 	}
+	return (1);
 }
 #endif /* tahoe */
 
@@ -89,7 +91,7 @@ read_names()
 
 char *defdrives[] = { "hp0", "hp1", "hp2", 0 };
 
-void
+int
 read_names()
 {
 	register char *p;
@@ -104,32 +106,69 @@ read_names()
 	mp = nl[X_MBDINIT].n_value;
 	up = nl[X_UBDINIT].n_value;
 	if (mp == 0 && up == 0) {
-		(void) fprintf(stderr,
-		    "vmstat: disk init info not in namelist\n");
-		exit(1);
+		(void)fprintf(stderr,
+		    "disk init info not in namelist\n");
+		return (0);
 	}
 	p = buf;
-	if (mp) for (;; mp += sizeof mdev) {
-		(void)kvm_read(kd, mp, &mdev, sizeof mdev);
-		if (mdev.mi_driver == 0)
-			break;
-		if (mdev.mi_dk < 0 || mdev.mi_alive == 0)
-			continue;
-		(void)kvm_read(kd, mdev.mi_driver, &mdrv, sizeof mdrv);
-		(void)kvm_rea(kd, mdrv.md_dname, name, sizeof name);
-		dr_name[mdev.mi_dk] = p;
-		p += sprintf(p, "%s%d", name, mdev.mi_unit);
-	}
-	if (up) for (;; up += sizeof udev) {
-		(void)kvm_read(kd, up, &udev, sizeof udev);
-		if (udev.ui_driver == 0)
-			break;
-		if (udev.ui_dk < 0 || udev.ui_alive == 0)
-			continue;
-		(void)kvm_read(kd, udev.ui_driver, &udrv, sizeof udrv);
-		(void)kvm_read(kd, udrv.ud_dname, name, sizeof name);
-		dr_name[udev.ui_dk] = p;
-		p += sprintf(p, "%s%d", name, udev.ui_unit);
-	}
+	if (mp)
+		for (;; mp += sizeof mdev) {
+			(void)kvm_read(kd, mp, &mdev, sizeof mdev);
+			if (mdev.mi_driver == 0)
+				break;
+			if (mdev.mi_dk < 0 || mdev.mi_alive == 0)
+				continue;
+			(void)kvm_read(kd, mdev.mi_driver, &mdrv, sizeof mdrv);
+			(void)kvm_rea(kd, mdrv.md_dname, name, sizeof name);
+			dr_name[mdev.mi_dk] = p;
+			p += sprintf(p, "%s%d", name, mdev.mi_unit);
+		}
+	if (up)
+		for (;; up += sizeof udev) {
+			(void)kvm_read(kd, up, &udev, sizeof udev);
+			if (udev.ui_driver == 0)
+				break;
+			if (udev.ui_dk < 0 || udev.ui_alive == 0)
+				continue;
+			(void)kvm_read(kd, udev.ui_driver, &udrv, sizeof udrv);
+			(void)kvm_read(kd, udrv.ud_dname, name, sizeof name);
+			dr_name[udev.ui_dk] = p;
+			p += sprintf(p, "%s%d", name, udev.ui_unit);
+		}
+	return (1);
 }
 #endif /* vax */
+
+#ifdef sun
+#include <sundev/mbvar.h>
+
+int
+read_names()
+{
+	static int once = 0;
+	struct mb_device mdev;
+	struct mb_driver mdrv;
+	short two_char;
+	char *cp = (char *) &two_char;
+	register struct mb_device *mp;
+
+	mp = (struct mb_device *)nl[X_MBDINIT].n_value;
+	if (mp == 0) {
+		(void)fprintf(stderr,
+		    "disk init info not in namelist\n");
+		return (0);
+	}
+	for (;; ++mp) {
+		(void)kvm_read(kd, mp++, &mdev, sizeof(mdev));
+		if (mdev.md_driver == 0)
+			break;
+		if (mdev.md_dk < 0 || mdev.md_alive == 0)
+			continue;
+		(void)kvm_read(kd, mdev.md_driver, &mdrv, sizeof(mdrv));
+		(void)kvm_read(kd, mdrv.mdr_dname, &two_char, sizeof(two_char));
+		(void)sprintf(dr_name[mdev.md_dk],
+		    "%c%c%d", cp[0], cp[1], mdev.md_unit);
+	}
+	return(1);
+}
+#endif /* sun */
