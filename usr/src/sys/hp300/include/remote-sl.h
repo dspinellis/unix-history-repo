6793b383c938c83c659c03ec0ed9bc3643e2d5bf
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1992-07-24 22:33:56 -0800

    update to latest revision of kgdb debugging protocol
    SCCS-vsn: sys/hp300/hp300/kgdb_stub.c 7.13
    SCCS-vsn: sys/hp300/include/remote-sl.h 7.4

--- usr/src/sys/hp300/include/remote-sl.h
+++ usr/src/sys/hp300/include/remote-sl.h
@@ -7,32 +7,26 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)remote-sl.h	7.3 (Berkeley) %G%
+ *	@(#)remote-sl.h	7.4 (Berkeley) %G%
  *
- * $Header: remote-sl.h,v 1.3 91/03/22 15:34:27 mccanne Exp $ (LBL)
+ * $Header: remote-sl.h,v 1.2 92/07/23 19:38:20 mccanne Exp $ (LBL)
  */
 
+#define FRAME_START		0xc1		/* Frame End */
 #define FRAME_END		0xc0		/* Frame End */
 #define FRAME_ESCAPE		0xdb		/* Frame Esc */
-#define TRANS_FRAME_END		0xdc		/* transposed frame end */
+#define TRANS_FRAME_START	0xde		/* transposed frame start */
+#define TRANS_FRAME_END		0xdc		/* transposed frame esc */
 #define TRANS_FRAME_ESCAPE	0xdd		/* transposed frame esc */
 
 /*
- * Error codes.
+ * Message limits.  SL_MAXDATA is the maximum number of bytes that can
+ * be read or written.  SL_RPCSIZE is the maximum message size for
+ * the serial link.  The actual MTU is two times the max message (since
+ * each byte might be escaped), plus the two framing bytes.  We add two 
+ * to the message length to account for the type byte and checksum.
  */
-#define EKGDB_CSUM	1
-#define EKGDB_2BIG	2
-#define EKGDB_RUNT	3
-
-/*
- * Message limits.  SL_MAXMSG is the longest message that can be passed
- * down to the serial link.  The actual MTU is two times the max message
- * (since each byte might be escaped), plus the two framing bytes.  We add 
- * two to the message length to account for the type byte and check sum.
- * SL_BUFSIZE is one character larger than SL_MAXMSG so we can stuff
- * a checksum into the input buffer without special casing.
- */
-#define SL_MAXMSG 64
-#define SL_BUFSIZE (SL_MAXMSG + 1)
-#define SL_MTU ((2 * (SL_MAXMSG + 2) + 2))
+#define SL_MAXDATA 62			/* max data that can be read */
+#define SL_RPCSIZE (1 + SL_MAXDATA)	/* errno byte + data */
+#define SL_MTU ((2 * (SL_RPCSIZE + 2) + 2))
 
