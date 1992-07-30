31b9a2c6ecf254b31610899d64de86a039d7d328
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-07-29 18:24:41 -0800

    open the file for writing, not appending; fix from Chris Torek
    SCCS-vsn: games/rogue/score.c 5.6

--- usr/src/games/rogue/score.c
+++ usr/src/games/rogue/score.c
@@ -9,7 +9,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)score.c	5.5 (Berkeley) %G%";
+static char sccsid[] = "@(#)score.c	5.6 (Berkeley) %G%";
 #endif /* not lint */
 
 /*
@@ -180,7 +180,8 @@ short other;
 
 	md_lock(1);
 
-	if ((fp = fopen(_PATH_SCOREFILE, "a+")) == NULL) {
+	if ((fp = fopen(_PATH_SCOREFILE, "r+")) == NULL &&
+	    (fp = fopen(_PATH_SCOREFILE, "w+")) == NULL) {
 		message("cannot read/write/create score file", 0);
 		sf_error();
 	}
