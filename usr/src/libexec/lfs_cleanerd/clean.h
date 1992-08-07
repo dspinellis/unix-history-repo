41b1167e4bdaa9de6e20208a9fcae800b8d6943c
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-08-06 19:27:22 -0800

    Version 2 cleaner
    SCCS-vsn: libexec/lfs_cleanerd/clean.h 1.2
    SCCS-vsn: libexec/lfs_cleanerd/library.c 1.2

--- usr/src/libexec/lfs_cleanerd/clean.h
+++ usr/src/libexec/lfs_cleanerd/clean.h
@@ -4,6 +4,11 @@
  * for finding and parsing LFS segments.
  */
 
+#define DUMP_SUM_HEADER		0x0001
+#define DUMP_INODE_ADDRS	0x0002
+#define DUMP_FINFOS		0x0004
+#define	DUMP_ALL		0xFFFF
+
 #define IFILE_NAME "ifile"
 
 #ifndef TRUE
@@ -11,237 +16,124 @@
 #define FALSE	(0)
 #endif
 
+/*
+ * Cleaner parameters
+ *	BUSY_LIM: lower bound of the number of segments currently available
+ *		as a percentage of the total number of free segments possibly
+ *		available.
+ *	IDLE_LIM: Same as BUSY_LIM but used when the system is idle.
+ *	MIN_SEGS: Minimum number of segments you should always have.
+ *		I have no idea what this should be, but it should probably
+ *		be a function of lfsp.
+ *	NUM_TO_CLEAN: Number of segments to clean at once.  Again, this
+ *		should probably be based on the file system size and how
+ *		full or empty the segments being cleaned are.
+ */
+
+#define	BUSY_LIM	0.50
+#define	IDLE_LIM	0.90
+#define	MIN_SEGS(lfsp)	(3)
+#define	NUM_TO_CLEAN(fsp)	(5)
+
+#define MAXLOADS	3
+#define	ONE_MIN	0
+#define	FIVE_MIN 1
+#define	FIFTEEN_MIN 2
+
 typedef struct fs_info {
 	struct	statfs	*fi_statfsp;	/* fsstat info from getfsstat */
-	struct	lfs	*fi_lfsp;	/* superblock */
-					/*
-					 * shared cleaner info data 
-					 * (from top of ifile) 
-					 */
-	CLEANERINFO	*fi_cip;
+	struct	lfs	fi_lfs;		/* superblock */
+	CLEANERINFO	*fi_cip;	/* Cleaner info from ifile */
 	SEGUSE	*fi_segusep;		/* segment usage table (from ifile) */
 	IFILE	*fi_ifilep;		/* ifile table (from ifile) */
 	u_long	fi_daddr_shift;		/* shift to get byte offset of daddr */
 	u_long	fi_ifile_count;		/* # entries in the ifile table */
-	u_long	fi_ifile_length;	/* length of the ifile */
+	off_t	fi_ifile_length;	/* length of the ifile */
 } FS_INFO;
-
-
-#define fsid		(fsp->fi_statfsp->f_fsid)
-#define statfsp		(fsp->fi_statfsp)
-#define lfsp		(fsp->fi_lfsp)
-#define cip		(fsp->fi_cip)
-#define segusep		(fsp->fi_segusep)
-#define ifilep		(fsp->fi_ifilep)
-#define ifile_count	(fsp->fi_ifile_count)
-#define ifile_length	(fsp->fi_ifile_length)
 
 /* 
  * XXX: size (in bytes) of a segment
  *	should lfs_bsize be fsbtodb(fs,1), blksize(fs), or lfs_dsize? 
  */
-#define seg_size(fs) ((fs)->lfs_ssize<<(fs)->lfs_bshift)
+#define seg_size(fs) ((fs)->lfs_ssize << (fs)->lfs_bshift)
 
 /* daddr -> byte offset */
-#define datobyte(fs, da) ((da)<<(fs)->fi_daddr_shift)
-#define bytetoda(fs, byte) ((byte)>>(fs)->fi_daddr_shift)
+#define datobyte(fs, da) ((da) << (fs)->fi_daddr_shift)
+#define bytetoda(fs, byte) ((byte) >> (fs)->fi_daddr_shift)
 
-#define CLEANSIZE(fs)	(CLEANSIZE_SU(fs) << fs->lfs_bshift)
-#define SEGTABSIZE(fs)	(SEGTABSIZE_SU(fs) << fs->lfs_bshift)
+#define CLEANSIZE(fsp)	(fsp->fi_lfs.lfs_cleansz << fsp->fi_lfs.lfs_bshift)
+#define SEGTABSIZE(fsp)	(fsp->fi_lfs.lfs_segtabsz << fsp->fi_lfs.lfs_bshift)
 
-#define IFILE_ENTRY(fs, if, i)	((IFILE*)((caddr_t)(if) + \
-	(fs)->lfs_bsize*((i)/(fs)->lfs_ifpb) + \
-	sizeof(IFILE)*((i)%(fs)->lfs_ifpb)))
-#define SEGUSE_ENTRY(fs, su, i) ((SEGUSE*)((caddr_t)(su) + \
-	(fs)->lfs_bsize*((i)/(fs)->lfs_sepb) + \
-	sizeof(IFILE)*((i)%(fs)->lfs_sepb)))
+#define IFILE_ENTRY(fs, if, i) \
+	((IFILE *)((caddr_t)(if) + ((i) / (fs)->lfs_ifpb << (fs)->lfs_bshift)) \
+	+ (i) % (fs)->lfs_ifpb)
 
-/*
- * fs_getmntinfo:
- *
- *    This function will get information on all mounted file systems
- * with the given type.  It will return the number of mounted file
- * systems with the right type.  It will return in *buf a pointer to
- * the array of statfs structures.
- */
-extern int
-fs_getmntinfo __P((struct statfs **buf, int type));
+#define SEGUSE_ENTRY(fs, su, i) \
+	((SEGUSE *)((caddr_t)(su) + (fs)->lfs_bsize * ((i) / (fs)->lfs_sepb)) +\
+	(i) % (fs)->lfs_sepb)
 
-/*
- * get_fs_info:
- *
- * get all the information available on a file system
- */
-extern int
-get_fs_info __P((struct statfs *lstatfsp, FS_INFO **fspp, int count));
-
-extern void
-free_fs_info __P((FS_INFO *fsp, int count));
-
-/* 
- * get_superblock: 
- *    gets the superblock from disk (possibly in face of errors) 
- */
-extern int
-get_superblock __P((FS_INFO *fsp, struct lfs *sbp));
-
-
-/* 
- * get_ifile: 
- *    This function will map the ifile into memory.  It returns
- * NULL on failure.
- */
-extern int
-get_ifile __P((FS_INFO *fsp));
+__BEGIN_DECLS
+int	 dump_summary __P((struct lfs *, SEGSUM *, u_long, daddr_t **));
+void	 err __P((const int, const char *, ...));
+int	 fs_getmntinfo __P((struct statfs **, int));
+int	 get __P((int, off_t, void *, size_t));
+FS_INFO	*get_fs_info __P((struct statfs *, int));
+int 	 lfs_segmapv __P((FS_INFO *, int, caddr_t, BLOCK_INFO **, int *,
+	     INODE_INFO **, int *));
+int	 mmap_segment __P((FS_INFO *, int, caddr_t *));
+void	 munmap_segment __P((FS_INFO *, caddr_t));
+void	 reread_fs_info __P((FS_INFO *, int));
+void	 toss __P((void *, int *, size_t,
+	      int (*)(const void *, const void *, const void *), void *));
 
 /*
- * segmapv:
- *
- *   This function will scan a segment and return a list of
- * <inode, blocknum> pairs which indicate which blocks were
- * contained as live data within the segment at some point
- * (it may have "died" since then).  Any given pair will be 
- * listed at most once.
+ * USEFUL DEBUGGING FUNCTIONS:
  */
-extern int 
-lfs_segmapv __P((FS_INFO *fsp, int seg, caddr_t seg_buf, 
-		BLOCK_INFO **blocks, int *bcount, 
-		INODE_INFO **inodes, int *icount));
+#ifdef VERBOSE
+#define PRINT_FINFO(fp, ip) { \
+	(void)printf("    %s %s%d version %d nblocks %d\n", \
+	    (ip)->if_version > (fp)->fi_version ? "TOSSING" : "KEEPING", \
+	    "FINFO for inode: ", (fp)->fi_ino, \
+	    (fp)->fi_version, (fp)->fi_nblocks); \
+	fflush(stdout); \
+}
 
-/* 
- * this will parse a partial segment and create a vector of block_info's
- * for live data blocks for live inodes.  It will not include blocks or 
- * inodes from files with new version numbers.  
- */
-extern void
-pseg_blocks __P((FS_INFO *fsp, int seg, SEGSUM *s, caddr_t seg_buf, 
-		BLOCK_INFO **blocks, int *count));
+#define PRINT_IINFO(b, iip) { \
+	(void) printf("\t%s inode: %d daddr: 0x%lx create: %s\n", \
+	    b ? "KEEPING" : "TOSSING", (iip)->ii_inode, (iip)->ii_daddr, \
+	    ctime((time_t *)&(iip)->ii_segcreate)); \
+	fflush(stdout); \
+}
 
-/* 
- * this will parse a partial segment and create a vector of inode_info's
- * for live inodes.  It will not include blocks or inodes from files 
- * with new version numbers.  
- */
-extern void
-pseg_inodes __P((FS_INFO *fsp, int seg, SEGSUM *s, caddr_t seg_buf, 
-		INODE_INFO **inodes, int *count));
+#define PRINT_BINFO(bip) { \
+	(void)printf("\tinode: %d lbn: %d daddr: 0x%lx create: %s\n", \
+	    (bip)->bi_inode, (bip)->bi_lbn, (bip)->bi_daddr, \
+	    ctime((time_t *)&(bip)->bi_segcreate)); \
+	fflush(stdout); \
+}
 
-/* 
- * return the size of the partial segment in bytes. 
- */
-extern u_long
-pseg_size __P((FS_INFO *fsp, SEGSUM *s));
+#define PRINT_SEGUSE(sup, n) { \
+	(void)printf("Segment %d nbytes=%lu\tflags=%c%c%c ninos=%d nsums=%d lastmod: %s\n", \
+			n, (sup)->su_nbytes, \
+			(sup)->su_flags & SEGUSE_DIRTY ? 'D' : 'C', \
+			(sup)->su_flags & SEGUSE_ACTIVE ? 'A' : ' ', \
+			(sup)->su_flags & SEGUSE_SUPERBLOCK ? 'S' : ' ', \
+			(sup)->su_ninos, (sup)->su_nsums, \
+			ctime((time_t *)&(sup)->su_lastmod)); \
+	fflush(stdout); \
+}
 
+void	 dump_super __P((struct lfs *));
+void	 dump_cleaner_info __P((void *));
+void	 print_SEGSUM __P(( struct lfs *, SEGSUM *));
+void	 print_CLEANERINFO __P((CLEANERINFO *));
+#else
+#define	PRINT_FINFO(fp, ip)
+#define	PRINT_IINFO(b, iip)
+#define PRINT_BINFO(bip)
+#define	PRINT_SEGUSE(sup, n)
+#define	dump_cleaner_info(cip)
+#define	dump_super(lfsp)
+#endif
 
-/* 
- * join block list b with list a (eliminating duplicates), leaving result
- * in list a.
- */
-extern void
-pseg_bjoin __P((FS_INFO *fsp, BLOCK_INFO **ablocks, int *acount, 
-		BLOCK_INFO *bblocks, int bcount));
-
-
-/* 
- * join inode list b with list a (eliminating duplicates), leaving result
- * in list a.
- */
-extern void
-pseg_ijoin __P((FS_INFO *fsp, INODE_INFO **ainodes, int *acount, 
-		INODE_INFO *binodes, int bcount));
-
-
-/* is the segsum block valid? return TRUE if it is, FALSE otherwise */
-extern int 
-segsum_valid __P((FS_INFO *fsp, SEGSUM *ssp));
-
-
-/*
- * pseg_valid:
- *
- * returns 1 if the partial segment is valid, and 0 if it is invalid.
- * it uses the checksums to verify validity.
- */	 
-extern int
-pseg_valid __P((FS_INFO *fsp, SEGSUM *ssp));
-
-
-/* 
- * pseg_finfos:
- * 
- * get array of FINFO pointers for partial segment
- * return the array in finfos, and the size of the array in count
- */
-extern void
-pseg_finfos __P((FS_INFO *fsp, SEGSUM *ssp, FINFO ***finfos, int *count));
-
-/*
- * blocksize:
- *
- * returns the size (in bytes) of a (logical) block.
- * this is used because lfs uses different block sizes, depending
- * on the logical # of the block.  Lfs uses various sizes so
- * it doesn't need fragments.
- */ 
-extern u_long
-blocksize __P((FS_INFO *fsp, int index));
-
-/*
- * finfo_size:
- *
- * returns the size in bytes of an FINFO structure 
- */
-extern u_long
-finfo_size __P((FINFO *finfop));
-	
-/*
- * Simple, general purpose, fast checksum.  Data must be short-aligned.
- * Returns a u_long in case we ever want to do something more rigorous.
- *
- * XXX
- * Use the TCP/IP checksum instead.
- */
-extern u_long
-cksum __P((register void *str, register size_t len));
-
-/* 
- * read a segment into a memory buffer
- */
-extern int
-mmap_segment __P((FS_INFO *fsp, int segment, caddr_t *seg_buf));
-
-extern void
-munmap_segment __P((FS_INFO *fsp, caddr_t seg_buf));
-
-
-/*
- * USEFUL DEBUGGING TOOLS:
- */
-
-extern void
-print_IFILE __P((IFILE *p));
-
-extern void
-print_SEGUSE __P((SEGUSE *p));
-
-extern void
-print_CLEANERINFO __P((CLEANERINFO *p));
-
-extern void
-print_SEGSUM __P((SEGSUM *p));
-
-extern void
-print_time_t __P((time_t t));
-
-extern void
-print_BLOCK_INFO __P((BLOCK_INFO *p));
-
-extern void
-print_INODE_INFO __P((INODE_INFO *p));
-
-extern void
-print_FINFO __P((FINFO *p));
-
-extern void
-print_lfs __P((struct lfs *p));
-
+__END_DECLS
