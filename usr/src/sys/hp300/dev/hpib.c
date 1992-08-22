3491978121a401c7d6b649b320981c9b4f09731b
Carl Staelin <staelin@ucbvax.Berkeley.EDU>
1992-08-21 23:40:09 -0800

    increase the hpibidtimeout so hpib devices are correctly autoconfigured
    SCCS-vsn: sys/hp300/dev/hpib.c 7.6

--- usr/src/sys/hp300/dev/hpib.c
+++ usr/src/sys/hp300/dev/hpib.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)hpib.c	7.5 (Berkeley) %G%
+ *	@(#)hpib.c	7.6 (Berkeley) %G%
  */
 
 /*
@@ -33,7 +33,7 @@ struct	isr hpib_isr[NHPIB];
 int	nhpibppoll(), fhpibppoll();
 
 int	hpibtimeout = 100000;	/* # of status tests before we give up */
-int	hpibidtimeout = 20000;	/* # of status tests for hpibid() calls */
+int	hpibidtimeout = 100000;	/* # of status tests for hpibid() calls */
 int	hpibdmathresh = 3;	/* byte count beyond which to attempt dma */
 
 hpibinit(hc)
