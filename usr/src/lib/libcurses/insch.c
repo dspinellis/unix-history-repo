326ddc773aea583ec9f3a627471e416951028a6c
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-08-23 18:10:45 -0800

    KNF
    SCCS-vsn: lib/libcurses/insch.c 5.5
    SCCS-vsn: lib/libcurses/insertln.c 5.5

--- usr/src/lib/libcurses/insch.c
+++ usr/src/lib/libcurses/insch.c
@@ -7,36 +7,35 @@
 
 #ifndef lint
 static char sccsid[] = "@(#)insch.c	5.5 (Berkeley) %G%";
-#endif /* not lint */
+#endif	/* not lint */
 
-# include	"curses.ext"
+#include <curses.h>
 
 /*
- *	This routine performs an insert-char on the line, leaving
- * (_cury,_curx) unchanged.
- *
+ * winsch --
+ *	Do an insert-char on the line, leaving (_cury,_curx) unchanged.
  */
-winsch(win, c)
-reg WINDOW	*win;
-char		c; {
+int
+winsch(win, ch)
+	register WINDOW *win;
+	int ch;
+{
 
-	reg char	*temp1, *temp2;
-	reg char	*end;
+	register char *end, *temp1, *temp2;
 
 	end = &win->_y[win->_cury][win->_curx];
 	temp1 = &win->_y[win->_cury][win->_maxx - 1];
 	temp2 = temp1 - 1;
 	while (temp1 > end)
 		*temp1-- = *temp2--;
-	*temp1 = c;
+	*temp1 = ch;
 	touchline(win, win->_cury, win->_curx, win->_maxx - 1);
-	if (win->_cury == LINES - 1 && win->_y[LINES-1][COLS-1] != ' ')
+	if (win->_cury == LINES - 1 && win->_y[LINES - 1][COLS - 1] != ' ')
 		if (win->_scroll) {
 			wrefresh(win);
 			scroll(win);
 			win->_cury--;
-		}
-		else
-			return ERR;
-	return OK;
+		} else
+			return (ERR);
+	return (OK);
 }
