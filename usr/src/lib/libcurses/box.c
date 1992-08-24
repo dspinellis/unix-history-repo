085265b849c1f0120fb59b80be9fe6e6152b3591
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-08-23 17:02:13 -0800

    KNF
    SCCS-vsn: lib/libcurses/box.c 5.5

--- usr/src/lib/libcurses/box.c
+++ usr/src/lib/libcurses/box.c
@@ -7,22 +7,22 @@
 
 #ifndef lint
 static char sccsid[] = "@(#)box.c	5.5 (Berkeley) %G%";
-#endif /* not lint */
+#endif	/* not lint */
 
-# include	"curses.ext"
+#include <curses.h>
 
 /*
- *	This routine draws a box around the given window with "vert"
- * as the vertical delimiting char, and "hor", as the horizontal one.
- *
+ * box --
+ *	Draw a box around the given window with "vert" as the vertical
+ *	delimiting char, and "hor", as the horizontal one.
  */
+int
 box(win, vert, hor)
-reg WINDOW	*win;
-char		vert, hor; {
-
-	reg int		i;
-	reg int		endy, endx;
-	reg char	*fp, *lp;
+	register WINDOW *win;
+	int vert, hor;
+{
+	register int endy, endx, i;
+	register char *fp, *lp;
 
 	endx = win->_maxx;
 	endy = win->_maxy - 1;
@@ -33,7 +33,8 @@ char		vert, hor; {
 	endx--;
 	for (i = 0; i <= endy; i++)
 		win->_y[i][0] = (win->_y[i][endx] = vert);
-	if (!win->_scroll && (win->_flags&_SCROLLWIN))
+	if (!win->_scroll && (win->_flags & _SCROLLWIN))
 		fp[0] = fp[endx] = lp[0] = lp[endx] = ' ';
 	touchwin(win);
+	return (OK);
 }
