390cf3358befa34e545d9ba47f1a4ca05b6dfac5
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-08-23 16:57:42 -0800

    forgot to compile it before checking it in; typo
    SCCS-vsn: lib/libcurses/addch.c 5.7

--- usr/src/lib/libcurses/addch.c
+++ usr/src/lib/libcurses/addch.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)addch.c	5.6 (Berkeley) %G%";
+static char sccsid[] = "@(#)addch.c	5.7 (Berkeley) %G%";
 #endif	/* not lint */
 
 #include <curses.h>
@@ -17,7 +17,7 @@ static char sccsid[] = "@(#)addch.c	5.6 (Berkeley) %G%";
  *
  */
 int
-waddch(win, c)
+waddch(win, ch)
 	WINDOW *win;
 	int ch;
 {
