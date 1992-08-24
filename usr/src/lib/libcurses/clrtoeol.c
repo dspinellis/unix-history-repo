2594dcb6bc7167ce1823b30adc2458a7c0c1b8bc
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-08-23 17:11:45 -0800

    KNF, return a value
    SCCS-vsn: lib/libcurses/clrtoeol.c 5.5

--- usr/src/lib/libcurses/clrtoeol.c
+++ usr/src/lib/libcurses/clrtoeol.c
@@ -7,21 +7,20 @@
 
 #ifndef lint
 static char sccsid[] = "@(#)clrtoeol.c	5.5 (Berkeley) %G%";
-#endif /* not lint */
+#endif	/* not lint */
 
-# include	"curses.ext"
+#include <curses.h>
 
 /*
- *	This routine clears up to the end of line
- *
+ * wclrtoeol --
+ *	Clear up to the end of line.
  */
+int
 wclrtoeol(win)
-reg WINDOW	*win; {
-
-	reg char	*sp, *end;
-	reg int		y, x;
-	reg char	*maxx;
-	reg int		minx;
+	register WINDOW *win;
+{
+	register int minx, x, y;
+	register char *end, *maxx, *sp;
 
 	y = win->_cury;
 	x = win->_curx;
@@ -35,11 +34,10 @@ reg WINDOW	*win; {
 				minx = sp - win->_y[y];
 			*sp = ' ';
 		}
-	/*
-	 * update firstch and lastch for the line
-	 */
-	touchline(win, y, win->_curx, win->_maxx - 1);
-# ifdef DEBUG
-	fprintf(outf, "CLRTOEOL: minx = %d, maxx = %d, firstch = %d, lastch = %d\n", minx, maxx - win->_y[y], win->_firstch[y], win->_lastch[y]);
-# endif
+#ifdef DEBUG
+	__TRACE("CLRTOEOL: minx = %d, maxx = %d, firstch = %d, lastch = %d\n",
+	    minx, maxx - win->_y[y], win->_firstch[y], win->_lastch[y]);
+#endif
+	/* Update firstch and lastch for the line. */
+	return (touchline(win, y, win->_curx, win->_maxx - 1));
 }
