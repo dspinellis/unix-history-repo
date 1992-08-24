0095f07e13e7d33231222bd4e7fcff0673fa43ee
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-08-23 21:39:08 -0800

    fold mv routines in; lint
    SCCS-vsn: lib/libcurses/printw.c 5.10
    SCCS-vsn: lib/libcurses/scanw.c 5.9

--- usr/src/lib/libcurses/scanw.c
+++ usr/src/lib/libcurses/scanw.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)scanw.c	5.8 (Berkeley) %G%";
+static char sccsid[] = "@(#)scanw.c	5.9 (Berkeley) %G%";
 #endif	/* not lint */
 
 /*
@@ -21,10 +21,13 @@ static char sccsid[] = "@(#)scanw.c	5.8 (Berkeley) %G%";
 #include <varargs.h>
 #endif
 
+static int __sscans __P((WINDOW *, const char *, ...));
+
 /*
  * scanw --
  *	Implement a scanf on the standard screen.
  */
+int
 #if __STDC__
 scanw(const char *fmt, ...)
 #else
@@ -50,6 +53,7 @@ scanw(fmt, va_alist)
  * wscanw --
  *	Implements a scanf on the given window.
  */
+int
 #if __STDC__
 wscanw(WINDOW *win, const char *fmt, ...)
 #else
@@ -73,19 +77,78 @@ wscanw(win, fmt, va_alist)
 }
 
 /*
+ * mvscanw, mvwscanw -- 
+ *	Implement the mvscanw commands.  Due to the variable number of
+ *	arguments, they cannot be macros.  Another sigh....
+ */
+int
+#if __STDC__
+mvscanw(register int y, register int x, const char *fmt,...)
+#else
+mvscanw(y, x, fmt, va_alist)
+	register int y, x;
+	char *fmt;
+	va_dcl
+#endif
+{
+	va_list ap;
+	int ret;
+
+	if (move(y, x) != OK)
+		return (ERR);
+#if __STDC__
+	va_start(ap, fmt);
+#else
+	va_start(ap);
+#endif
+	ret = __sscans(stdscr, fmt, ap);
+	va_end(ap);
+	return (ret);
+}
+
+int
+#if __STDC__
+mvwscanw(register WINDOW * win, register int y, register int x,
+    const char *fmt, ...)
+#else
+mvwscanw(win, y, x, fmt, va_alist)
+	register WINDOW *win;
+	register int y, x;
+	char *fmt;
+	va_dcl
+#endif
+{
+	va_list ap;
+	int ret;
+
+	if (move(y, x) != OK)
+		return (ERR);
+#if __STDC__
+	va_start(ap, fmt);
+#else
+	va_start(ap);
+#endif
+	ret = __sscans(win, fmt, ap);
+	va_end(ap);
+	return (ret);
+}
+
+/*
  * __sscans --
  *	This routine actually executes the scanf from the window.
  *	THIS SHOULD BE RENAMED vwscanw AND EXPORTED
  */
+static int
+#if __STDC__
+__sscans(WINDOW *win, const char *fmt, ...)
+#else
 __sscans(win, fmt, ap)
 	WINDOW *win;
-#if __STDC__
-	const char *fmt;
-#else
 	char *fmt;
+	va_dcl;
 #endif
-	va_list ap;
 {
+	va_list ap;
 	char buf[1024];
 
 	return (wgetstr(win, buf) == OK ? vsscanf(buf, fmt, ap) : ERR);
