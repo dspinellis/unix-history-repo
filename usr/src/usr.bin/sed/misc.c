d205e8bdde9c20459994164dc6d9342e68902e34
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-08-25 01:42:18 -0800

    we can't survive some of the compile errors, just die
    SCCS-vsn: usr.bin/sed/misc.c 5.2

--- usr/src/usr.bin/sed/misc.c
+++ usr/src/usr.bin/sed/misc.c
@@ -108,14 +108,8 @@ err(severity, fmt, va_alist)
 	(void)vfprintf(stderr, fmt, ap);
 	va_end(ap);
 	(void)fprintf(stderr, "\n");
-	switch (severity) {
-	case COMPILE:
-		compile_errors++;
-#define	MAX_COMPILE_ERRS	20
-		if (compile_errors > MAX_COMPILE_ERRS)
-			err(FATAL, "too many compilation errors; exiting");
-	case FATAL:
-		exit(1);
-	}
+	if (severity == WARNING)
+		return;
+	exit(1);
 	/* NOTREACHED */
 }
