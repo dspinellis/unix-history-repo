1ba475dafa8d419dd95815984cbd8aaf9a23423f
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-08-25 01:38:24 -0800

    always die, we can't survive some of the compile errors
    SCCS-vsn: usr.bin/sed/misc.c 5.2

--- usr/src/usr.bin/sed/misc.c
+++ usr/src/usr.bin/sed/misc.c
@@ -10,7 +10,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)misc.c	5.1 (Berkeley) %G%";
+static char sccsid[] = "@(#)misc.c	5.2 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/types.h>
