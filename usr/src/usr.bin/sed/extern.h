324c31e45ec0b34420452a6d4f8ab7429378ac91
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-08-28 22:26:15 -0800

    change calling convention of mf_fgets, cspace; new externs match and maxnsub
    SCCS-vsn: usr.bin/sed/extern.h 5.4

--- usr/src/usr.bin/sed/extern.h
+++ usr/src/usr.bin/sed/extern.h
@@ -8,11 +8,13 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)extern.h	5.3 (Berkeley) %G%
+ *	@(#)extern.h	5.4 (Berkeley) %G%
  */
 
 extern struct s_command *prog;
 extern struct s_appends *appends;
+extern regmatch_t *match;
+extern size_t maxnsub;
 extern u_long linenum;
 extern int appendnum;
 extern int lastline;
@@ -22,9 +24,10 @@ extern char *fname;
 void	 compile __P((void));
 char	*cu_fgets __P((char *, int));
 void	 err __P((int, const char *, ...));
-char	*mf_fgets __P((size_t *));
+int	 mf_fgets __P((SPACE *, enum e_spflag));
 void	 process __P((void));
 char	*strregerror __P((int, regex_t *));
 void	*xmalloc __P((u_int));
 void	*xrealloc __P((void *, u_int));
 void	 cfclose __P((struct s_command *));
+void	 cspace __P((SPACE *, char *, size_t, enum e_spflag));
