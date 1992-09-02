57dac99c741bd8cdaa0eef32cd6268a54b5bf724
Elan Amir <elan@ucbvax.Berkeley.EDU>
1992-09-01 19:09:20 -0800

    vtroff = psroff
    SCCS-vsn: usr.bin/vgrind/vgrind.sh 5.8

--- usr/src/usr.bin/vgrind/vgrind.sh
+++ usr/src/usr.bin/vgrind/vgrind.sh
@@ -3,9 +3,35 @@
 # Copyright (c) 1980 The Regents of the University of California.
 # All rights reserved.
 #
-# %sccs.include.redist.sh%
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 3. All advertising materials mentioning features or use of this software
+#    must display the following acknowledgement:
+#	This product includes software developed by the University of
+#	California, Berkeley and its contributors.
+# 4. Neither the name of the University nor the names of its contributors
+#    may be used to endorse or promote products derived from this software
+#    without specific prior written permission.
 #
-#	@(#)vgrind.sh	5.7 (Berkeley) %G%
+# THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+#
+#	@(#)vgrind.sh	5.8 (Berkeley) 8/3/92
 #
 
 set voptions=
@@ -90,10 +116,10 @@ if (-r index) then
     else
 	if ("$head" != "") then
 	    $vf $options -h "$head" $files | \
-		sh -c "vtroff -rx1 $voptions -i -mvgrind 2>> xindex"
+		sh -c "psroff -rx1 $voptions -i -mvgrind 2>> xindex"
 	else
 	    $vf $options $files | \
-		sh -c "vtroff -rx1 $voptions -i -mvgrind 2>> xindex"
+		sh -c "psroff -rx1 $voptions -i -mvgrind 2>> xindex"
 	endif
     endif
     sort -df +0 -2 xindex >index
@@ -107,9 +133,9 @@ else
 	endif
     else
 	if ("$head" != "") then
-	    $vf $options -h "$head" $files | vtroff -i $voptions -mvgrind
+	    $vf $options -h "$head" $files | psroff -i $voptions -mvgrind
 	else
-	    $vf $options $files | vtroff -i $voptions -mvgrind
+	    $vf $options $files | psroff -i $voptions -mvgrind
 	endif
     endif
 endif
