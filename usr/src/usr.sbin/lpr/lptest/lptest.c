b4d6d67285c6a14c6f286a010a096a5eb7ea6694
Elan Amir <elan@ucbvax.Berkeley.EDU>
1992-09-16 17:54:44 -0800

    copyright[] -> static
    SCCS-vsn: usr.sbin/lpr/lptest/lptest.c 5.8

--- usr/src/usr.sbin/lpr/lptest/lptest.c
+++ usr/src/usr.sbin/lpr/lptest/lptest.c
@@ -7,13 +7,13 @@
  */
 
 #ifndef lint
-char copyright[] =
+static char copyright[] =
 "@(#) Copyright (c) 1983 The Regents of the University of California.\n\
  All rights reserved.\n";
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)lptest.c	5.7 (Berkeley) %G%";
+static char sccsid[] = "@(#)lptest.c	5.8 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <stdlib.h>
