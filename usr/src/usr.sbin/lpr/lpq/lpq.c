831b2291ba1ff07d5c5311e2367617137d07d883
Elan Amir <elan@ucbvax.Berkeley.EDU>
1992-09-16 17:53:36 -0800

    copyright[] -> static
    SCCS-vsn: usr.sbin/lpr/lpq/lpq.c 5.11
    SCCS-vsn: usr.sbin/lpr/lpr/lpr.c 5.14

--- usr/src/usr.sbin/lpr/lpq/lpq.c
+++ usr/src/usr.sbin/lpr/lpq/lpq.c
@@ -7,13 +7,13 @@
  */
 
 #ifndef lint
-char copyright[] =
+static char copyright[] =
 "@(#) Copyright (c) 1983 The Regents of the University of California.\n\
  All rights reserved.\n";
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)lpq.c	5.10 (Berkeley) %G%";
+static char sccsid[] = "@(#)lpq.c	5.11 (Berkeley) %G%";
 #endif /* not lint */
 
 /*
