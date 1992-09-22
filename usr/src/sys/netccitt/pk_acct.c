ea0bf9406b2a79875ee3bb392f675a78600290e4
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1992-09-21 21:17:09 -0800

    get rid of USES keywords
    SCCS-vsn: sys/netccitt/pk_acct.c 7.9

--- usr/src/sys/netccitt/pk_acct.c
+++ usr/src/sys/netccitt/pk_acct.c
@@ -9,7 +9,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)pk_acct.c	7.8 (Berkeley) %G%
+ *	@(#)pk_acct.c	7.9 (Berkeley) %G%
  */
 
 #include "param.h"
@@ -38,7 +38,6 @@ struct	vnode *pkacctp;
 pk_accton (path)
 	char *path;
 {
-	USES_VOP_UNLOCK;
 	register struct vnode *vp = NULL;
 	struct nameidata nd;
 	struct vnode *oacctp = pkacctp;
