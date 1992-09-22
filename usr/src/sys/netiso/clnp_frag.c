0e7e1a32663d20b9037608fda6d47d7569fbe133
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1992-09-22 02:06:06 -0800

    need to include systm.h to get min and max
    SCCS-vsn: sys/netiso/clnp_frag.c 7.13
    SCCS-vsn: sys/netiso/iso.c 7.19
    SCCS-vsn: sys/netiso/tp_subr.c 7.19
    SCCS-vsn: sys/netiso/tp_emit.c 7.15

--- usr/src/sys/netiso/clnp_frag.c
+++ usr/src/sys/netiso/clnp_frag.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)clnp_frag.c	7.12 (Berkeley) %G%
+ *	@(#)clnp_frag.c	7.13 (Berkeley) %G%
  */
 
 /***********************************************************
@@ -37,6 +37,7 @@ SOFTWARE.
 /* $Source: /var/src/sys/netiso/RCS/clnp_frag.c,v $ */
 
 #include "param.h"
+#include "systm.h"
 #include "mbuf.h"
 #include "domain.h"
 #include "protosw.h"
