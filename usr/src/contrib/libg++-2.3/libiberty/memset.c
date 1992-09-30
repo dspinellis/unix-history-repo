7b129aa03eb70a7eab88b72886db1a9e7c6a6ef7
CSRG <csrg@ucbvax.Berkeley.EDU>
1992-09-29 21:16:41 -0800

    BSD 4_4 development Work on file usr/src/contrib/libg++-2.3/libiberty/basename.c Work on file usr/src/contrib/libg++-2.3/libiberty/bcmp.c Work on file usr/src/contrib/libg++-2.3/libiberty/bcopy.c Work on file usr/src/contrib/libg++-2.3/libiberty/bzero.c Work on file usr/src/contrib/libg++-2.3/libiberty/concat.c Work on file usr/src/contrib/libg++-2.3/libiberty/fdmatch.c Work on file usr/src/contrib/libg++-2.3/libiberty/getcwd.c Work on file usr/src/contrib/libg++-2.3/libiberty/getopt1.c Work on file usr/src/contrib/libg++-2.3/libiberty/insque.c Work on file usr/src/contrib/libg++-2.3/libiberty/memcmp.c Work on file usr/src/contrib/libg++-2.3/libiberty/memcpy.c Work on file usr/src/contrib/libg++-2.3/libiberty/memmove.c Work on file usr/src/contrib/libg++-2.3/libiberty/memset.c Work on file usr/src/contrib/libg++-2.3/libiberty/obstack.c Work on file usr/src/contrib/libg++-2.3/libiberty/sigsetmask.c Work on file usr/src/contrib/libg++-2.3/libiberty/spaces.c Work on file usr/src/contrib/libg++-2.3/libiberty/strchr.c Work on file usr/src/contrib/libg++-2.3/libiberty/strrchr.c Work on file usr/src/contrib/libg++-2.3/libiberty/strstr.c Work on file usr/src/contrib/libg++-2.3/libiberty/vfork.c Work on file usr/src/contrib/libg++-2.3/libiberty/vsprintf.c
    Synthesized-from: CSRG/cd3/4.4

---
+++ usr/src/contrib/libg++-2.3/libiberty/memset.c
@@ -0,0 +1,35 @@
+/* memset
+   Copyright (C) 1991 Free Software Foundation, Inc.
+
+This file is part of the libiberty library.
+Libiberty is free software; you can redistribute it and/or
+modify it under the terms of the GNU Library General Public
+License as published by the Free Software Foundation; either
+version 2 of the License, or (at your option) any later version.
+
+Libiberty is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Library General Public License for more details.
+
+You should have received a copy of the GNU Library General Public
+License along with libiberty; see the file COPYING.LIB.  If
+not, write to the Free Software Foundation, Inc., 675 Mass Ave,
+Cambridge, MA 02139, USA.  */
+
+#include <ansidecl.h>
+#ifdef __STDC__
+#include <stddef.h>
+#else
+#define size_t unsigned long
+#endif
+
+PTR
+DEFUN(memset, (dest, val, len),
+      PTR dest AND register int val AND register size_t len)
+{
+  register unsigned char *ptr = (unsigned char*)dest;
+  while (len-- > 0)
+    *ptr++ = val;
+  return dest;
+}
