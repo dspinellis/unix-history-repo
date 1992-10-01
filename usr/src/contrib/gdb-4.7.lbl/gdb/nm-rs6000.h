b11a8f7fec86f0cbef21f15dd4257038c1897c16
CSRG <csrg@ucbvax.Berkeley.EDU>
1992-10-01 07:49:09 -0800

    BSD 4_4 development Work on file usr/src/contrib/gdb-4.7.lbl/gdb/nm-rs6000.h Work on file usr/src/contrib/gdb-4.7.lbl/gdb/rs6000-nat.c
    Synthesized-from: CSRG/cd3/4.4

---
+++ usr/src/contrib/gdb-4.7.lbl/gdb/nm-rs6000.h
@@ -0,0 +1,30 @@
+/* IBM RS/6000 native-dependent macros for GDB, the GNU debugger.
+   Copyright 1986, 1987, 1989, 1991, 1992 Free Software Foundation, Inc.
+
+This file is part of GDB.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  */
+
+/* Do implement the attach and detach commands.  */
+
+#define ATTACH_DETACH
+
+#define PTRACE_ATTACH PT_ATTACH
+#define PTRACE_DETACH PT_DETACH
+
+/* Override copies of {fetch,store}_inferior_registers in infptrace.c.  */
+
+#define FETCH_INFERIOR_REGISTERS
+
