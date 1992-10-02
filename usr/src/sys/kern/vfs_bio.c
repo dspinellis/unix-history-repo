3aa3de2bba10c8f72d551c44b887ae45150abb79
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1992-10-02 08:10:50 -0800

    modernize the management of the hash and free lists
    SCCS-vsn: sys/kern/vfs_bio.c 7.54
    SCCS-vsn: sys/kern/vfs_cluster.c 7.54

--- usr/src/sys/kern/vfs_bio.c
+++ usr/src/sys/kern/vfs_bio.c
@@ -6,7 +6,7 @@
  * Use and redistribution is subject to the Berkeley Software License
  * Agreement and your Software Agreement with AT&T (Western Electric).
  *
- *	@(#)vfs_bio.c	7.53 (Berkeley) %G%
+ *	@(#)vfs_bio.c	7.54 (Berkeley) %G%
  */
 
 #include <sys/param.h>
