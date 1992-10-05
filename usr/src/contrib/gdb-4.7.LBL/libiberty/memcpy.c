62136400fb1c857f718e6a91593647a8784e9ec0
CSRG <csrg@ucbvax.Berkeley.EDU>
1992-10-05 02:43:06 -0800

    BSD 4_4_Lite1 development Work on file usr/src/contrib/gdb-4.7.LBL/libiberty/argv.c Work on file usr/src/contrib/gdb-4.7.LBL/libiberty/basename.c Work on file usr/src/contrib/gdb-4.7.LBL/libiberty/bcmp.c Work on file usr/src/contrib/gdb-4.7.LBL/libiberty/bcopy.c Work on file usr/src/contrib/gdb-4.7.LBL/libiberty/concat.c Work on file usr/src/contrib/gdb-4.7.LBL/libiberty/configure.in Work on file usr/src/contrib/gdb-4.7.LBL/libiberty/bzero.c Work on file usr/src/contrib/gdb-4.7.LBL/libiberty/cplus-dem.c Work on file usr/src/contrib/gdb-4.7.LBL/libiberty/fdmatch.c Work on file usr/src/contrib/gdb-4.7.LBL/libiberty/getopt1.c Work on file usr/src/contrib/gdb-4.7.LBL/libiberty/getopt.c Work on file usr/src/contrib/gdb-4.7.LBL/libiberty/getcwd.c Work on file usr/src/contrib/gdb-4.7.LBL/libiberty/getpagesize.c Work on file usr/src/contrib/gdb-4.7.LBL/libiberty/insque.c Work on file usr/src/contrib/gdb-4.7.LBL/libiberty/memcpy.c Work on file usr/src/contrib/gdb-4.7.LBL/libiberty/memmove.c Work on file usr/src/contrib/gdb-4.7.LBL/libiberty/memcmp.c Work on file usr/src/contrib/gdb-4.7.LBL/libiberty/memset.c Work on file usr/src/contrib/gdb-4.7.LBL/libiberty/sigsetmask.c Work on file usr/src/contrib/gdb-4.7.LBL/libiberty/obstack.c Work on file usr/src/contrib/gdb-4.7.LBL/libiberty/spaces.c Work on file usr/src/contrib/gdb-4.7.LBL/libiberty/strchr.c Work on file usr/src/contrib/gdb-4.7.LBL/libiberty/strrchr.c Work on file usr/src/contrib/gdb-4.7.LBL/libiberty/strstr.c Work on file usr/src/contrib/gdb-4.7.LBL/libiberty/vfork.c Work on file usr/src/contrib/gdb-4.7.LBL/libiberty/vsprintf.c Work on file usr/src/contrib/gdb-4.7.LBL/libiberty/config/mh-aix Work on file usr/src/contrib/gdb-4.7.LBL/opcodes/z8k-dis.c
    Synthesized-from: CSRG/cd2/4.4BSD-Lite1

---
+++ usr/src/contrib/gdb-4.7.LBL/libiberty/memcpy.c
@@ -0,0 +1,49 @@
+/* memcpy
+   Copyright (C) 1991 Free Software Foundation, Inc.
+
+This file is part of the libiberty library.
+Libiberty is free software; you can redistribute it and/or
+modify it under the terms of the GNU Library General Public
+License as published by the Free Software Foundation; either
+version 2 of the License, or (at your option) any later version.
+
+Libiberty is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Library General Public License for more details.
+
+You should have received a copy of the GNU Library General Public
+License along with libiberty; see the file COPYING.LIB.  If
+not, write to the Free Software Foundation, Inc., 675 Mass Ave,
+Cambridge, MA 02139, USA.  */
+
+
+/*
+
+NAME
+
+	memcpy -- copy memory regions of arbitary length
+
+SYNOPSIS
+
+	void* memcpy (void *out, const void *in, size_t n);
+
+DESCRIPTION
+
+	Copy LENGTH bytes from memory region pointed to by IN to memory
+	region pointed to by OUT.
+*/
+
+#include <ansidecl.h>
+#ifdef __STDC__
+#include <stddef.h>
+#else
+#define size_t unsigned long
+#endif
+
+PTR
+DEFUN(memcpy, (out, in, length), PTR out AND CONST PTR in AND size_t length)
+{
+    bcopy(in, out, length);
+    return out;
+}
---
+++ usr/src/contrib/gdb-4.7.LBL/libiberty/strrchr.c
@@ -0,0 +1,56 @@
+/* Portable version of strrchr().
+   Copyright (C) 1991 Free Software Foundation, Inc.
+
+This file is part of the libiberty library.
+Libiberty is free software; you can redistribute it and/or
+modify it under the terms of the GNU Library General Public
+License as published by the Free Software Foundation; either
+version 2 of the License, or (at your option) any later version.
+
+Libiberty is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Library General Public License for more details.
+
+You should have received a copy of the GNU Library General Public
+License along with libiberty; see the file COPYING.LIB.  If
+not, write to the Free Software Foundation, Inc., 675 Mass Ave,
+Cambridge, MA 02139, USA.  */
+
+/*
+
+NAME
+
+	strrchr -- return pointer to last occurance of a character
+
+SYNOPSIS
+
+	char *strrchr (const char *s, int c)
+
+DESCRIPTION
+
+	Returns a pointer to the last occurance of character C in
+	string S, or a NULL pointer if no occurance is found.
+	
+BUGS
+
+	Behavior when character is the null character is implementation
+	dependent.
+
+*/
+
+#include <ansidecl.h>
+
+char *
+strrchr (s, c)
+  register CONST char *s;
+  int c;
+{
+  char *rtnval = 0;
+
+  do {
+    if (*s == c)
+      rtnval = s;
+  } while (*s++);
+  return (rtnval);
+}
