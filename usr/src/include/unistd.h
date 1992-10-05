7f45ed66a7231dc1d9f8a531e24eaa26971894b8
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-10-04 19:50:36 -0800

    nfssvc has two arguments, second is a caddr_t
    SCCS-vsn: include/unistd.h 5.22

--- usr/src/include/unistd.h
+++ usr/src/include/unistd.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)unistd.h	5.21 (Berkeley) %G%
+ *	@(#)unistd.h	5.22 (Berkeley) %G%
  */
 
 #ifndef _UNISTD_H_
@@ -108,7 +108,7 @@ int	 iruserok __P((u_long, int, const char *, const char *));
 int	 mknod __P((const char *, mode_t, dev_t));
 int	 mkstemp __P((char *));
 char	*mktemp __P((char *));
-int	 nfssvc __P((int));
+int	 nfssvc __P((int, caddr_t));
 int	 nice __P((int));
 void	 psignal __P((u_int, const char *));
 extern char *sys_siglist[];
