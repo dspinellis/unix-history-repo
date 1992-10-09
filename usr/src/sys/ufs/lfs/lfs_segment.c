c6e8e743f0b208b256ba1d846b240b224c908300
Margo Seltzer <margo@ucbvax.Berkeley.EDU>
1992-10-08 21:46:07 -0800

    Use new queue structures.  Count finfos before you collect them in case you have to write the segment in the middle.  Mark newly created buffers B_DONE so bread will use them.
    SCCS-vsn: sys/ufs/lfs/lfs_segment.c 7.36

--- usr/src/sys/ufs/lfs/lfs_segment.c
+++ usr/src/sys/ufs/lfs/lfs_segment.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)lfs_segment.c	7.35 (Berkeley) %G%
+ *	@(#)lfs_segment.c	7.36 (Berkeley) %G%
  */
 
 #include <sys/param.h>
@@ -28,6 +28,7 @@
 #include <ufs/ufs/inode.h>
 #include <ufs/ufs/dir.h>
 #include <ufs/ufs/ufsmount.h>
+#include <ufs/ufs/ufs_extern.h>
 
 #include <ufs/lfs/lfs.h>
 #include <ufs/lfs/lfs_extern.h>
@@ -111,7 +112,7 @@ lfs_vflush(vp)
 	do {
 		lfs_initseg(fs, sp);
 		do {
-			if (vp->v_dirtyblkhd != NULL)
+			if (vp->v_dirtyblkhd.le_next != NULL)
 				lfs_writefile(fs, sp, vp);
 		} while (lfs_writeinode(fs, sp, ip));
 
@@ -176,9 +177,9 @@ loop:	for (vp = mp->mnt_mounth; vp; vp = vp->v_mountf) {
 		 */
 		ip = VTOI(vp);
 		if ((ip->i_flag & (IMOD | IACC | IUPD | ICHG) ||
-		    vp->v_dirtyblkhd != NULL) &&
+		    vp->v_dirtyblkhd.le_next != NULL) &&
 		    ip->i_number != LFS_IFILE_INUM) {
-			if (vp->v_dirtyblkhd != NULL)
+			if (vp->v_dirtyblkhd.le_next != NULL)
 				lfs_writefile(fs, sp, vp);
 			(void) lfs_writeinode(fs, sp, ip);
 		}
@@ -281,7 +282,7 @@ redo:
 		vp = fs->lfs_ivnode;
 		while (vget(vp));
 		ip = VTOI(vp);
-		if (vp->v_dirtyblkhd != NULL)
+		if (vp->v_dirtyblkhd.le_next != NULL)
 			lfs_writefile(fs, sp, vp);
 		(void)lfs_writeinode(fs, sp, ip);
 		vput(vp);
@@ -342,6 +343,7 @@ lfs_writefile(fs, sp, vp)
 		lfs_initseg(fs, sp);
 	}
 	sp->sum_bytes_left -= sizeof(struct finfo) - sizeof(daddr_t);
+	++((SEGSUM *)(sp->segsum))->ss_nfinfo;
 
 	fip = sp->fip;
 	fip->fi_nblocks = 0;
@@ -367,13 +369,14 @@ lfs_writefile(fs, sp, vp)
 	printf("lfs_writefile: adding %d blocks\n", fip->fi_nblocks);
 #endif
 	if (fip->fi_nblocks != 0) {
-		++((SEGSUM *)(sp->segsum))->ss_nfinfo;
 		sp->fip =
 		    (struct finfo *)((caddr_t)fip + sizeof(struct finfo) +
 		    sizeof(daddr_t) * (fip->fi_nblocks - 1));
 		sp->start_lbp = &sp->fip->fi_blocks[0];
-	} else
+	} else {
 		sp->sum_bytes_left += sizeof(struct finfo) - sizeof(daddr_t);
+		--((SEGSUM *)(sp->segsum))->ss_nfinfo;
+	}
 }
 
 int
@@ -494,16 +497,14 @@ lfs_gatherblock(sp, bp, sptr)
 			splx(*sptr);
 		lfs_updatemeta(sp);
 
-		/* Add the current file to the segment summary. */
-		++((SEGSUM *)(sp->segsum))->ss_nfinfo;
-
 		version = sp->fip->fi_version;
 		(void) lfs_writeseg(fs, sp);
 		lfs_initseg(fs, sp);
 
 		sp->fip->fi_version = version;
 		sp->fip->fi_ino = VTOI(sp->vp)->i_number;
-
+		/* Add the current file to the segment summary. */
+		++((SEGSUM *)(sp->segsum))->ss_nfinfo;
 		sp->sum_bytes_left -= 
 		    sizeof(struct finfo) - sizeof(daddr_t);
 
@@ -534,7 +535,7 @@ lfs_gather(fs, sp, vp, match)
 
 	sp->vp = vp;
 	s = splbio();
-loop:	for (bp = vp->v_dirtyblkhd; bp; bp = bp->b_blockf) {
+loop:	for (bp = vp->v_dirtyblkhd.le_next; bp; bp = bp->b_vnbufs.qe_next) {
 		if (bp->b_flags & B_BUSY || !match(fs, bp) ||
 		    bp->b_flags & B_GATHERED)
 			continue;
@@ -565,7 +566,7 @@ lfs_updatemeta(sp)
 	struct buf *bp;
 	struct lfs *fs;
 	struct vnode *vp;
-	INDIR a[NIADDR], *ap;
+	struct indir a[NIADDR + 2], *ap;
 	struct inode *ip;
 	daddr_t daddr, lbn, off;
 	int db_per_fsb, error, i, nblocks, num;
@@ -590,8 +591,8 @@ lfs_updatemeta(sp)
 		(*sp->start_bpp)->b_blkno = off = fs->lfs_offset;
 		fs->lfs_offset += db_per_fsb;
 
-		if (error = lfs_bmaparray(vp, lbn, &daddr, a, &num))
-			panic("lfs_updatemeta: lfs_bmaparray %d", error);
+		if (error = ufs_bmaparray(vp, lbn, &daddr, a, &num, NULL))
+			panic("lfs_updatemeta: ufs_bmaparray %d", error);
 		ip = VTOI(vp);
 		switch (num) {
 		case 0:
@@ -867,6 +868,7 @@ lfs_writeseg(fs, sp)
 				free(bp, M_SEGMENT);
 			} else {
 				bremfree(bp);
+				bp->b_flags |= B_DONE;
 				reassignbuf(bp, bp->b_vp);
 				brelse(bp);
 			}
