0a063b216b66da4120b21027181de162c94d4456
Keith Sklower <sklower@ucbvax.Berkeley.EDU>
1992-10-09 08:51:46 -0800

    date and time created 92/10/09 00:51:46 by sklower
    SCCS-vsn: sys/netiso/tuba_table.h 7.1

---
+++ usr/src/sys/netiso/tuba_table.h
@@ -0,0 +1,27 @@
+/*-
+ * Copyright (c) 1991 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.redist.c%
+ *
+ *	@(#)tuba_table.h	7.1 (Berkeley) %G%
+ */
+
+struct tuba_cache {
+	struct	radix_node tc_nodes[2];		/* convenient lookup */
+	int	tc_refcnt;
+	int	tc_time;			/* last looked up */
+	int	tc_flags;
+#define TCF_PERM	1
+	int	tc_index;
+	u_short	tc_sum_in;			/* for inbound cksum */
+	u_short	tc_sum_out;			/* for outbound cksum */
+	struct	iso_addr tc_addr;
+};
+
+#define ICKSUM(a, b) ((a = (b) % 0xffff), (a == 0 ? a = 0xffff : a))
+
+#ifdef KERNEL
+extern struct tuba_cache **tuba_table;
+struct radix_node_head *tuba_tree;
+#endif
