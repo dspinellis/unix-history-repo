455703f0fa9a537e8228c422a22b8f9d5864e8ff
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1992-10-22 23:43:43 -0800

    track allocation sizes
    SCCS-vsn: sys/kern/kern_malloc.c 7.36

--- usr/src/sys/kern/kern_malloc.c
+++ usr/src/sys/kern/kern_malloc.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)kern_malloc.c	7.35 (Berkeley) %G%
+ *	@(#)kern_malloc.c	7.36 (Berkeley) %G%
  */
 
 #include <sys/param.h>
@@ -96,6 +96,7 @@ malloc(size, type, flags)
 			ksp->ks_limblocks++;
 		tsleep((caddr_t)ksp, PSWP+2, memname[type], 0);
 	}
+	ksp->ks_size |= 1 << indx;
 #endif
 #ifdef DIAGNOSTIC
 	copysize = 1 << indx < MAX_COPY ? 1 << indx : MAX_COPY;
