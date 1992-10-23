7f18efae08633c990ec53f36ded941c106c1b2a5
CSRG <csrg@ucbvax.Berkeley.EDU>
1992-10-22 17:56:30 -0800

    BSD 4_4_Lite2 development Work on file usr/src/contrib/libg++-2.3/libiberty/functions.def Work on file usr/src/contrib/libg++-2.3/libiberty/rename.c
    Synthesized-from: CSRG/cd3/4.4BSD-Lite2

---
+++ usr/src/contrib/libg++-2.3/libiberty/rename.c
@@ -0,0 +1,38 @@
+/* rename -- rename a file
+   Copyright (C) 1992 Free Software Foundation, Inc.
+
+This file is part of the libiberty library.
+Libiberty is free software; you can redistribute it and/or
+modify it under the terms of the GNU Library General Public
+License as published by the Free Software Foundation; either
+version 2 of the License, or (at your option) any later version.
+
+Libiberty is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Library General Public License for more details.
+
+You should have received a copy of the GNU Library General Public
+License along with libiberty; see the file COPYING.LIB.  If
+not, write to the Free Software Foundation, Inc., 675 Mass Ave,
+Cambridge, MA 02139, USA.  */
+
+/* Rename a file.  */
+
+#include <errno.h>
+
+int
+rename (zfrom, zto)
+     char *zfrom;
+     char *zto;
+{
+  if (link (zfrom, zto) < 0)
+    {
+      if (errno != EEXIST)
+	return -1;
+      if (unlink (zto) < 0
+	  || link (zfrom, zto) < 0)
+	return -1;
+    }
+  return unlink (zfrom);
+}
