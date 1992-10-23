6f6e07f0c2750bacede8cec8aea23bf269cec466
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1992-10-22 23:41:53 -0800

    add new statistics
    SCCS-vsn: sys/sys/malloc.h 7.33

--- usr/src/sys/sys/malloc.h
+++ usr/src/sys/sys/malloc.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)malloc.h	7.32 (Berkeley) %G%
+ *	@(#)malloc.h	7.33 (Berkeley) %G%
  */
 
 #ifndef _MALLOC_H_
@@ -155,6 +155,8 @@ struct kmemstats {
 	u_short	ks_mapblocks;	/* number of times blocked for kernel map */
 	long	ks_maxused;	/* maximum number ever used */
 	long	ks_limit;	/* most that are allowed to exist */
+	long	ks_size;	/* sizes of this thing that are allocated */
+	long	ks_spare;
 };
 
 /*
