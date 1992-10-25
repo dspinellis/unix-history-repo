83fea046305ef4d59ad64754a463d1964c0f679b
Margo Seltzer <margo@ucbvax.Berkeley.EDU>
1992-10-24 18:00:44 -0800

    Check all flags, not just B_CACHE to determine if block was in cache.
    SCCS-vsn: sys/ufs/lfs/lfs_syscalls.c 7.26

--- usr/src/sys/ufs/lfs/lfs_syscalls.c
+++ usr/src/sys/ufs/lfs/lfs_syscalls.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)lfs_syscalls.c	7.25 (Berkeley) %G%
+ *	@(#)lfs_syscalls.c	7.26 (Berkeley) %G%
  */
 
 #include <sys/param.h>
@@ -177,7 +177,7 @@ lfs_markv(p, uap, retval)
 			    blkp->bi_bp);
 		else {
 			bp = getblk(vp, blkp->bi_lbn, bsize);
-			if (!(bp->b_flags & B_CACHE) &&
+			if (!(bp->b_flags & (B_DELWRI | B_DONE | B_CACHE)) &&
 			    (error = copyin(blkp->bi_bp, bp->b_un.b_addr,
 			    bsize)))
 				goto err2;
