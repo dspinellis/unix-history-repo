37ac96008df20f0a82125d2b678098a180188c73
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-11-04 16:19:08 -0800

    date and time created 92/11/04 08:19:08 by bostic
    SCCS-vsn: sys/pmax/include/stdarg.h 7.1

---
+++ usr/src/sys/pmax/include/stdarg.h
@@ -0,0 +1,29 @@
+/*-
+ * Copyright (c) 1992 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.redist.c%
+ *
+ *	@(#)stdarg.h	7.1 (Berkeley) %G%
+ */
+
+typedef char *va_list;
+
+#define	__va_promote(type) \
+	(((sizeof(type) + sizeof(int) - 1) / sizeof(int)) * sizeof(int))
+
+#define	va_start(ap, last) \
+	(ap = ((char *)&(last) + __va_promote(last)))
+
+#ifdef KERNEL
+#define	va_arg(ap, type) \
+	((type *)(ap += sizeof(type)))[-1]
+#else
+#define	va_arg(ap, type) \
+	((type *)(ap += sizeof(type) == sizeof(int) ? sizeof(type) : \
+		sizeof(type) > sizeof(int) ? \
+		(-(int)(ap) & ~(sizeof(type) - 1)) + sizeof(type) : \
+		(abort(), 0)))[-1]
+#endif
+
+#define	va_end(ap)
