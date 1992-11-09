245ea7c3cd531e8564639c6ebe5675ccbdced9cf
CSRG <csrg@ucbvax.Berkeley.EDU>
1992-11-09 04:18:02 -0800

    BSD 4_4_Lite1 development Work on file usr/src/contrib/gcc-2.3.3/config/xm-3b1.h
    Synthesized-from: CSRG/cd2/4.4BSD-Lite1

---
+++ usr/src/contrib/gcc-2.3.3/config/xm-3b1.h
@@ -0,0 +1,9 @@
+#define USG
+
+#include "xm-m68k.h"
+
+#define bcopy(a,b,c) memcpy (b,a,c)
+#define bzero(a,b) memset (a,0,b)
+#define bcmp(a,b,c) memcmp (a,b,c)
+#define index strchr
+#define rindex strrchr
