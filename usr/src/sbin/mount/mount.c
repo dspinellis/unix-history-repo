5e8ea92c7b390f0e08355cad816a1b5f6b42c2a6
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1992-11-16 07:43:35 -0800

    -f flag (fake) becomes -d (debug); -f used to force downgrade from read/write to read-only mount mode
    SCCS-vsn: sbin/mount/mount.c 5.51

--- usr/src/sbin/mount/mount.c
+++ usr/src/sbin/mount/mount.c
@@ -12,7 +12,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)mount.c	5.50 (Berkeley) %G%";
+static char sccsid[] = "@(#)mount.c	5.51 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/param.h>
@@ -37,7 +37,7 @@ static char sccsid[] = "@(#)mount.c	5.50 (Berkeley) %G%";
 #define	SETTYPE(type) \
 	(!strcmp(type, FSTAB_RW) || !strcmp(type, FSTAB_RQ))
 
-int fake, verbose, updateflg, mnttype;
+int debug, force, verbose, updateflg, mnttype;
 char *mntname, **envp;
 char **vfslist, **makevfslist();
 static void prmount();
@@ -61,13 +61,16 @@ main(argc, argv, arge)
 	type = NULL;
 	mnttype = MOUNT_UFS;
 	mntname = "ufs";
-	while ((ch = getopt(argc, argv, "afrwuvt:o:")) != EOF)
+	while ((ch = getopt(argc, argv, "adfrwuvt:o:")) != EOF)
 		switch((char)ch) {
 		case 'a':
 			all = 1;
 			break;
+		case 'd':
+			debug = 1;
+			break;
 		case 'f':
-			fake = 1;
+			force = 1;
 			break;
 		case 'r':
 			type = FSTAB_RO;
@@ -118,7 +121,7 @@ main(argc, argv, arge)
 	}
 
 	if (argc == 0) {
-		if (verbose || fake || type)
+		if (verbose || debug || type)
 			usage();
 		if ((mntsize = getmntinfo(&mntbuf, MNT_NOWAIT)) == 0) {
 			(void) fprintf(stderr,
@@ -240,6 +243,8 @@ mountfs(spec, name, flags, type, options, mntopts)
 		getstdopts(options, &flags);
 	if (type)
 		getstdopts(type, &flags);
+	if (force)
+		flags |= MNT_FORCE;
 	switch (mnttype) {
 	case MOUNT_LFS:
 	case MOUNT_UFS:
@@ -280,7 +285,7 @@ mountfs(spec, name, flags, type, options, mntopts)
 				(void)printf(" %s", argv[i]);
 			(void)printf("\n");
 		}
-		if (fake)
+		if (debug)
 			break;
 		if (pid = vfork()) {
 			if (pid == -1) {
@@ -302,7 +307,7 @@ mountfs(spec, name, flags, type, options, mntopts)
 		/* NOTREACHED */
 
 	}
-	if (!fake && mount(mnttype, name, flags, argp)) {
+	if (!debug && mount(mnttype, name, flags, argp)) {
 		(void) fprintf(stderr, "%s on %s: ", spec, name);
 		switch (errno) {
 		case EMFILE:
