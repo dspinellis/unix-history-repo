c460695e8164b9271dce1e239fa5c80eb1d52c28
Chris Torek <torek@ucbvax.Berkeley.EDU>
1992-11-17 07:07:59 -0800

    rm bogus variable to fix "send" command (jonathan@CS.Stanford.EDU)
    SCCS-vsn: usr.bin/telnet/commands.c 5.7
    SCCS-vsn: usr.bin/telnet/commands.c 5.7

--- usr/src/usr.bin/telnet/commands.c
+++ usr/src/usr.bin/telnet/commands.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)commands.c	5.6 (Berkeley) %G%";
+static char sccsid[] = "@(#)commands.c	5.7 (Berkeley) %G%";
 #endif /* not lint */
 
 #if	defined(unix)
@@ -265,7 +265,6 @@ sendcmd(argc, argv)
     int  argc;
     char **argv;
 {
-    int what;		/* what we are sending this time */
     int count;		/* how many bytes we are going to need to send */
     int i;
     int question = 0;	/* was at least one argument a question */
@@ -338,8 +337,8 @@ sendcmd(argc, argv)
 				  (s->narg > 1) ? argv[i+2] : 0);
 	    i += s->narg;
 	} else {
-	    NET2ADD(IAC, what);
-	    printoption("SENT", IAC, what);
+	    NET2ADD(IAC, s->what);
+	    printoption("SENT", IAC, s->what);
 	}
     }
     return (count == success);
