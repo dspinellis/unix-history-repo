0ed47d8bba23a934cd9cb8fe67f481db846fa2a9
Chris Torek <torek@ucbvax.Berkeley.EDU>
1992-11-24 22:55:55 -0800

    handle 2K mbufs gracefully: round user buffer spec up, not down. allow more than 3 packets of window by default (3 is particularly bad with current ack policy).  (from van)
    SCCS-vsn: sys/netinet/tcp_input.c 7.29

--- usr/src/sys/netinet/tcp_input.c
+++ usr/src/sys/netinet/tcp_input.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)tcp_input.c	7.28 (Berkeley) %G%
+ *	@(#)tcp_input.c	7.29 (Berkeley) %G%
  */
 
 #include <sys/param.h>
@@ -1441,8 +1441,10 @@ tcp_mss(tp, offer)
 		if (bufsize < mss)
 			mss = bufsize;
 		else {
-			bufsize = min(bufsize, SB_MAX) / mss * mss;
-			(void) sbreserve(&so->so_snd, bufsize);
+			bufsize = roundup(bufsize, mss);
+			if (bufsize > SB_MAX)
+				bufsize = SB_MAX;
+			(void)sbreserve(&so->so_snd, bufsize);
 		}
 		tp->t_maxseg = mss;
 
@@ -1451,8 +1453,10 @@ tcp_mss(tp, offer)
 #endif
 			bufsize = so->so_rcv.sb_hiwat;
 		if (bufsize > mss) {
-			bufsize = min(bufsize, SB_MAX) / mss * mss;
-			(void) sbreserve(&so->so_rcv, bufsize);
+			bufsize = roundup(bufsize, mss);
+			if (bufsize > SB_MAX)
+				bufsize = SB_MAX;
+			(void)sbreserve(&so->so_rcv, bufsize);
 		}
 	}
 	tp->snd_cwnd = mss;
