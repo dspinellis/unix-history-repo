d023d4d993c4cb728fa7e6222e56f9080384c8a2
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-12-03 02:17:56 -0800

    date and time created 92/12/02 18:17:56 by bostic
    SCCS-vsn: usr.bin/compress/zcat.sh 5.1

---
+++ usr/src/usr.bin/compress/zcat.sh
@@ -0,0 +1,11 @@
+#!/bin/sh -
+#
+# Copyright (c) 1992 The Regents of the University of California.
+# All rights reserved.
+#
+# %sccs.include.redist.sh%
+#
+#	@(#)zcat.sh	5.1 (Berkeley) %G%
+#
+
+uncompress -c $*
