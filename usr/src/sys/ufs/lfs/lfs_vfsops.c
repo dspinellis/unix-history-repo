3c32216a968e61f95de12b06826ec33ab7e9a993
Margo Seltzer <margo@ucbvax.Berkeley.EDU>
1992-12-10 22:19:32 -0800

    Fix call to segwrite (pass SYNC and CKP flags instead of 1).
    SCCS-vsn: sys/ufs/lfs/lfs_vfsops.c 7.89

--- usr/src/sys/ufs/lfs/lfs_vfsops.c
+++ usr/src/sys/ufs/lfs/lfs_vfsops.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)lfs_vfsops.c	7.88 (Berkeley) %G%
+ *	@(#)lfs_vfsops.c	7.89 (Berkeley) %G%
  */
 
 #include <sys/param.h>
@@ -227,9 +227,6 @@ lfs_mountfs(devvp, mp, p)
 		error = EINVAL;		/* XXX needs translation */
 		goto out;
 	}
-#ifdef DEBUG
-	lfs_dump_super(fs);
-#endif
 
 	/* Allocate the mount structure, copy the superblock into it. */
 	ump = (struct ufsmount *)malloc(sizeof *ump, M_UFSMNT, M_WAITOK);
@@ -422,7 +419,7 @@ lfs_sync(mp, waitfor, cred, p)
 	int error;
 
 	/* All syncs must be checkpoints until roll-forward is implemented. */
-	error = lfs_segwrite(mp, 1);
+	error = lfs_segwrite(mp, SEGM_CKP | (waitfor ? SEGM_SYNC : 0));
 #ifdef QUOTA
 	qsync(mp);
 #endif
