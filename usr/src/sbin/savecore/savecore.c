e58a240444f7ddc1b07887273a9a0c1bc214fd0f
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-12-11 19:35:35 -0800

    Kirk wants the crash dump mode to be 644
    SCCS-vsn: sbin/savecore/savecore.c 5.36

--- usr/src/sbin/savecore/savecore.c
+++ usr/src/sbin/savecore/savecore.c
@@ -12,7 +12,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)savecore.c	5.35 (Berkeley) %G%";
+static char sccsid[] = "@(#)savecore.c	5.36 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/param.h>
@@ -305,7 +305,7 @@ err1:			syslog(LOG_WARNING, "%s: %s", path, strerror(errno));
 			exit(1);
 		}
 	} else
-		ofd = Create(path, S_IRUSR | S_IWUSR);
+		ofd = Create(path, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
 
 	/* Open the raw device. */
 	rawp = rawname(ddname);
