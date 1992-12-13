a9126e20f206441e63f042d3daa306780e25015c
Akito Fujita <akito@ucbvax.Berkeley.EDU>
1992-12-13 11:50:09 -0800

    date and time created 92/12/13 03:50:09 by akito
    SCCS-vsn: sys/luna68k/stand/boot.c 7.1
    SCCS-vsn: sys/luna68k/stand/conf.c 7.1

---
+++ usr/src/sys/luna68k/stand/boot.c
@@ -0,0 +1,195 @@
+/*
+ * Copyright (c) 1992 OMRON Corporation.
+ * Copyright (c) 1992 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to Berkeley by
+ * OMRON Corporation.
+ *
+ * %sccs.include.redist.c%
+ *
+ *	@(#)boot.c	7.1 (Berkeley) %G%
+ */
+
+/*
+ * boot.c -- boot program
+ * by A.Fujita, MAR-01-1992
+ */
+
+#include <sys/param.h>
+#include <sys/reboot.h>
+#include <sys/exec.h>
+#include <machine/stinger.h>
+#include <luna68k/stand/saio.h>
+#include <luna68k/stand/status.h>
+
+extern struct KernInter	*kiff;
+
+int howto;
+int devtype = MAKEBOOTDEV(4, 0, 0, 6, 0);
+
+char *copyunix();
+
+struct exec header;
+char default_file[] = "sd(0,0)vmunix";
+
+char *how_to_info[] = {
+"RB_ASKNAME	ask for file name to reboot from",
+"RB_SINGLE	reboot to single user only",
+"RB_NOSYNC	dont sync before reboot",
+"RB_HALT	don't reboot, just halt",
+"RB_INITNAME	name given for /etc/init (unused)",
+"RB_DFLTROOT	use compiled-in rootdev",
+"RB_KDB		give control to kernel debugger",
+"RB_RDONLY	mount root fs read-only"
+};
+
+int
+how_to_boot(argc, argv)
+	int   argc;
+	char *argv[];
+{
+	int i, h = howto;
+
+	if (argc < 2) {
+		printf("howto: 0x%s\n\n", hexstr(howto, 2));
+
+		if (h == 0) {
+			printf("\t%s\n", "RB_AUTOBOOT	flags for system auto-booting itself");
+		} else {
+			for (i = 0; i < 8; i++, h >>= 1) {
+				if (h & 0x01) {
+					printf("\t%s\n", how_to_info[i]);
+				}
+			}
+		}
+
+		printf("\n");
+	}
+}
+
+int
+boot(argc, argv)
+	int   argc;
+	char *argv[];
+{
+	register int io;
+	char *line;
+
+	if (argc < 2)
+		line = default_file;
+	else
+		line = argv[1];
+
+	printf("Booting %s\n", line);
+
+	io = open(line, 0);
+	if (io >= 0) {
+		bootunix(howto, devtype, io);
+		close(io);
+	}
+}
+
+int
+load(argc, argv)
+	int   argc;
+	char *argv[];
+{
+	register int io;
+	char *line;
+
+	if (argc < 2)
+		line = default_file;
+	else
+		line = argv[1];
+
+	printf("loading %s\n", line);
+
+	io = open(line, 0);
+	if (io >= 0) {
+		copyunix(io);
+		printf("\n");
+		close(io);
+	}
+}
+
+int
+bootunix(howto, devtype, io)
+	register howto;		/* d7 contains boot flags */
+	register devtype;	/* d6 contains boot device */
+	register io;
+{
+	register char *load;	/* a5 contains load addr for unix */
+
+	load = copyunix(io);
+
+	printf(" start 0x%x\n", load);
+	asm("	movl %0,d7" : : "d" (howto));
+	asm("	movl %0,d6" : : "d" (devtype));
+	asm("	movl %0,a5" : : "a" (kiff));
+	(*((int (*)()) load))();
+}
+
+char *
+copyunix(io)
+	register io;
+{
+
+	register int i;
+	register char *load;	/* a5 contains load addr for unix */
+	register char *addr;
+
+	/*
+	 * Read a.out file header
+	 */
+
+	i = read(io, (char *)&header, sizeof(struct exec));
+	if (i != sizeof(struct exec) ||
+	    (header.a_magic != 0407 && header.a_magic != 0413 && header.a_magic != 0410)) {
+		printf("illegal magic number ... 0x%x\n");
+		printf("Bad format\n");
+		return(0);
+	}
+
+	load = addr = (char *) (header.a_entry & 0x00FFFFFF);
+
+	printf("%d", header.a_text);
+	if (header.a_magic == 0413 && lseek(io, 0x400, 0) == -1)
+		goto shread;
+
+	/*
+	 * Load TEXT Segment
+	 */
+
+	if (read(io, (char *)addr, header.a_text) != header.a_text)
+		goto shread;
+	addr += header.a_text;
+	if (header.a_magic == 0413 || header.a_magic == 0410)
+		while ((int)addr & CLOFSET)
+			*addr++ = 0;
+
+	/*
+	 * Load DATA Segment
+	 */
+
+	printf("+%d", header.a_data);
+	if (read(io, addr, header.a_data) != header.a_data)
+		goto shread;
+
+	/*
+	 * Clear BSS Segment
+	 */
+
+	addr += header.a_data;
+	printf("+%d", header.a_bss);
+	header.a_bss += 128*512;	/* slop */
+	for (i = 0; i < header.a_bss; i++)
+		*addr++ = 0;
+
+	return(load);
+
+shread:
+	printf("   Short read\n");
+	return(0);
+}
+
