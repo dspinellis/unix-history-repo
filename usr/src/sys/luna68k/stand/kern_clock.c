c0d3f59749afc93cb0d83000d03bf2ecb2cb8d39
Akito Fujita <akito@ucbvax.Berkeley.EDU>
1992-12-13 11:50:17 -0800

    date and time created 92/12/13 03:50:17 by akito
    SCCS-vsn: sys/luna68k/stand/kern_clock.c 7.1
    SCCS-vsn: sys/luna68k/stand/locore.s 7.1

---
+++ usr/src/sys/luna68k/stand/kern_clock.c
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 1992 OMRON Corporation.
+ * Copyright (c) 1992 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to Berkeley by
+ * OMRON Corporation.
+ *
+ * %sccs.include.redist.c%
+ *
+ *	@(#)kern_clock.c	7.1 (Berkeley) %G%
+ */
+
+#include <sys/types.h>
+
+/*
+ * The hz hardware interval timer.
+ * We update the events relating to real time.
+ * If this timer is also being used to gather statistics,
+ * we run through the statistics gathering routine as well.
+ */
+/*ARGSUSED*/
+hardclock(pc, ps)
+	caddr_t pc;
+	int ps;
+{
+}
