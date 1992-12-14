8f6600d0640db503db0af26b5dd20fd9b3aa6e8e
Keith Muller <muller@ucbvax.Berkeley.EDU>
1992-12-14 06:51:35 -0800

    date and time created 92/12/13 22:51:35 by muller
    SCCS-vsn: bin/pax/ftree.h 1.1
    SCCS-vsn: bin/pax/options.h 1.1

---
+++ usr/src/bin/pax/ftree.h
@@ -0,0 +1,24 @@
+/*-
+ * Copyright (c) 1992 Keith Muller.
+ * Copyright (c) 1992 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to Berkeley by
+ * Keith Muller of the University of California, San Diego.
+ *
+ * %sccs.include.redist.c%
+ *
+ *	@(#)ftree.h	1.1 (Berkeley) %G%
+ */
+
+/*
+ * Data structure used by the ftree.c routines to store the file args to be
+ * handed to fts(). It keeps a reference count of which args generated a
+ * "selected" member
+ */
+
+typedef struct ftree {
+	char		*fname;		/* file tree name */
+	int		refcnt;		/* has tree had a selected file? */
+	struct ftree	*fow;		/* pointer to next entry on list */
+} FTREE;
