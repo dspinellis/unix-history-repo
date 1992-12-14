89465b7e34c470fee9d44fd206ce8431c11933b2
Keith Muller <muller@ucbvax.Berkeley.EDU>
1992-12-14 06:51:39 -0800

    date and time created 92/12/13 22:51:39 by muller
    SCCS-vsn: bin/pax/tables.h 1.1
    SCCS-vsn: bin/pax/tar.h 1.1

---
+++ usr/src/bin/pax/tar.h
@@ -0,0 +1,90 @@
+/*-
+ * Copyright (c) 1992 Keith Muller.
+ * Copyright (c) 1992 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to Berkeley by
+ * Keith Muller of the University of California, San Diego.
+ *
+ * %sccs.include.redist.c%
+ *
+ *	@(#)tar.h	1.1 (Berkeley) %G%
+ */
+
+/*
+ * defines and data structures common to all tar formats
+ */
+#define NULLCNT		2		/* number of null blks in trailer */
+#define TNMSZ		100		/* size of name field */
+#define CHK_OFFSET	148		/* start of chksum field */
+#define CHK_LEN		8		/* length of checksum field */
+#define BLNKSUM		256L		/* sum of checksum field using ' ' */
+
+/*
+ * Pad with a bit mask, much faster than doing a mod but only works on powers
+ * of 2. Macro below is for block of 512 bytes.
+ */
+#define TAR_PAD(x)	((512 - ((x) & 511)) & 511)
+
+/*
+ * -o options for telling tar to not write directories to the archive
+ */
+#define TAR_NODIR	"nodir"
+#define TAR_OPTION	"write_opt"
+
+/*
+ * structure of an old tar header as appeared in BSD releases
+ */
+typedef struct {
+	char name[TNMSZ];		/* name of entry */
+	char mode[8]; 			/* mode */
+	char uid[8]; 			/* uid */
+	char gid[8];			/* gid */
+	char size[12];			/* size */
+	char mtime[12];			/* modification time */
+	char chksum[8];			/* checksum */
+	char linkflag;			/* norm, hard, or sym. */
+	char linkname[TNMSZ];		/* linked to name */
+} HD_TAR;
+
+/*
+ * Data Interchange Format - Extended tar Format - POSIX 1003.1-1990
+ */
+#define TPFSZ		155
+#define	TMAGIC		"ustar"		/* ustar and a null */
+#define	TMAGLEN		6
+#define	TVERSION	"00"		/* 00 and no null */
+#define	TVERSLEN	2
+
+typedef struct {
+	char name[TNMSZ];		/* name of entry */
+	char mode[8]; 			/* mode */
+	char uid[8]; 			/* uid */
+	char gid[8];			/* gid */
+	char size[12];			/* size */
+	char mtime[12];			/* modification time */
+	char chksum[8];			/* checksum */
+	char typeflag;			/* type of file. */
+	char linkname[TNMSZ];		/* linked to name */
+	char magic[TMAGLEN];		/* magic cookie */
+	char version[TVERSLEN];		/* version */
+	char uname[32];			/* ascii owner name */
+	char gname[32];			/* ascii group name */
+	char devmajor[8];		/* major device number */
+	char devminor[8];		/* minor device number */
+	char prefix[TPFSZ];		/* linked to name */
+} HD_USTAR;
+
+/*
+ * Values used in typeflag field
+ * (only REGTYPE, LNKTYPE and SYMTYPE are used in old tar headers)
+ */
+#define	REGTYPE		'0'		/* Regular File */
+#define	AREGTYPE	'\0'		/* Regular File */
+#define	LNKTYPE		'1'		/* Link */
+#define	SYMTYPE		'2'		/* Symlink */
+#define	CHRTYPE		'3'		/* Character Special File */
+#define	BLKTYPE		'4'		/* Block Special File */
+#define	DIRTYPE		'5'		/* Directory */
+#define	FIFOTYPE	'6'		/* FIFO */
+#define	CONTTYPE	'7'		/* high perf file */
