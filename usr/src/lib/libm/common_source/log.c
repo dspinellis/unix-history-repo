2b5ae2bd1cb75f10633fddb6a288242d218b2b7c
Peter McIlroy <mcilroy@ucbvax.Berkeley.EDU>
1992-12-17 04:01:52 -0800

    (no message)
    SCCS-vsn: lib/libm/common_source/log.c 5.9

--- usr/src/lib/libm/common_source/log.c
+++ usr/src/lib/libm/common_source/log.c
@@ -6,14 +6,14 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)log.c	5.8 (Berkeley) %G%";
+static char sccsid[] = "@(#)log.c	5.9 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <math.h>
 #include <errno.h>
 
 #include "log_table.h"
-#include "dmath.h"
+#include "mathimpl.h"
 
 /* Table-driven natural logarithm.
  *
