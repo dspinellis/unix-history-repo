af1552ee4525f0cf7f5ed74178468369ec0ceae1
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1992-12-17 01:31:20 -0800

    proper acknowledgement
    SCCS-vsn: sys/i386/i386/kgdb_glue.c 7.4
    SCCS-vsn: sys/i386/i386/kgdb_proto.h 7.4
    SCCS-vsn: sys/i386/i386/kgdb_stub.c 7.5
    SCCS-vsn: sys/hp300/hp300/kgdb_glue.c 7.3
    SCCS-vsn: sys/hp300/hp300/kgdb_proto.h 7.2
    SCCS-vsn: sys/hp300/hp300/kgdb_stub.c 7.15

--- usr/src/sys/i386/i386/kgdb_stub.c
+++ usr/src/sys/i386/i386/kgdb_stub.c
@@ -2,9 +2,18 @@
  * Copyright (c) 1990 Regents of the University of California.
  * All rights reserved.
  *
+ * This software was developed by the Computer Systems Engineering group
+ * at Lawrence Berkeley Laboratory under DARPA contract BG 91-66 and
+ * contributed to Berkeley.
+ *
+ * All advertising materials mentioning features or use of this software
+ * must display the following acknowledgement:
+ *	This product includes software developed by the University of
+ *	California, Lawrence Berkeley Laboratories.
+ *
  * %sccs.include.redist.c%
  *
- *	@(#)kgdb_stub.c	7.4 (Berkeley) %G%
+ *	@(#)kgdb_stub.c	7.5 (Berkeley) %G%
  */
 /*
  * "Stub" to allow remote cpu to debug over a serial line using gdb.
