a20c0541ab9fa85c94e2734803a8c3b916bec933
Eric Allman <eric@ucbvax.Berkeley.EDU>
1992-12-18 20:03:21 -0800

    add NIS map type; log message-id in same line as from logging
    SCCS-vsn: usr.sbin/sendmail/src/headers.c 5.29
    SCCS-vsn: usr.sbin/sendmail/src/version.c 5.141
    SCCS-vsn: usr.sbin/sendmail/src/sendmail.h 5.46
    SCCS-vsn: usr.sbin/sendmail/src/conf.c 5.48
    SCCS-vsn: usr.sbin/sendmail/src/map.c 5.6

--- usr/src/usr.sbin/sendmail/src/sendmail.h
+++ usr/src/usr.sbin/sendmail/src/sendmail.h
@@ -5,7 +5,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)sendmail.h	5.45 (Berkeley) %G%
+ *	@(#)sendmail.h	5.46 (Berkeley) %G%
  */
 
 /*
@@ -15,7 +15,7 @@
 # ifdef _DEFINE
 # define EXTERN
 # ifndef lint
-static char SmailSccsId[] =	"@(#)sendmail.h	5.45		%G%";
+static char SmailSccsId[] =	"@(#)sendmail.h	5.46		%G%";
 # endif lint
 # else /*  _DEFINE */
 # define EXTERN extern
