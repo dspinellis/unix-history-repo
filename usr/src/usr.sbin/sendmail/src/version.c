e99b84fcfc51c907600726ddab67d73cdfd792aa
Eric Allman <eric@ucbvax.Berkeley.EDU>
1992-12-20 19:13:42 -0800

    change random constants to be #defines; getopt-ify main.c
    SCCS-vsn: usr.sbin/sendmail/src/version.c 5.142

--- usr/src/usr.sbin/sendmail/src/version.c
+++ usr/src/usr.sbin/sendmail/src/version.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)version.c	5.141 (Berkeley) %G%";
+static char sccsid[] = "@(#)version.c	5.142 (Berkeley) %G%";
 #endif /* not lint */
 
-char	Version[] = "ALPHA-5.141";
+char	Version[] = "ALPHA-5.142";
