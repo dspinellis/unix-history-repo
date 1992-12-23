1388336eeb392584ccd7a49f0483e6f1d6e44de3
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-12-22 23:00:18 -0800

    variables don't need to be static
    SCCS-vsn: lib/libcurses/addch.c 5.11

--- usr/src/lib/libcurses/addch.c
+++ usr/src/lib/libcurses/addch.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)addch.c	5.10 (Berkeley) %G%";
+static char sccsid[] = "@(#)addch.c	5.11 (Berkeley) %G%";
 #endif	/* not lint */
 
 #include <curses.h>
@@ -16,17 +16,16 @@ static char sccsid[] = "@(#)addch.c	5.10 (Berkeley) %G%";
  *	Add the character to the current position in the given window.
  *
  */
-
 int
 waddch(win, ch)
 	WINDOW *win;
 	int ch;
 {
-	static __LDATA buf;
+	__LDATA buf;
 
 	buf.ch = ch;
 	buf.attr = 0;
-	return(__waddch(win, &buf));
+	return (__waddch(win, &buf));
 }
 
 int
@@ -34,7 +33,7 @@ __waddch(win, dp)
 	WINDOW *win;
 	__LDATA *dp;
 {
-	static char buf[2];
+	char buf[2];
 
 	buf[0] = dp->ch;
 	return (__waddbytes(win, buf, 1, dp->attr & __STANDOUT));
