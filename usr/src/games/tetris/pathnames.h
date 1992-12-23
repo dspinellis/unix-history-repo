df89bab82d4877c5af69413f3986188431fc3ee1
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-12-22 19:50:37 -0800

    date and time created 92/12/22 11:50:37 by bostic
    SCCS-vsn: games/tetris/pathnames.h 5.1

---
+++ usr/src/games/tetris/pathnames.h
@@ -0,0 +1,10 @@
+/*-
+ * Copyright (c) 1992 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.redist.c%
+ *
+ *	@(#)pathnames.h	5.1 (Berkeley) %G%
+ */
+
+#define _PATH_SCOREFILE	"/var/games/tetris.scores"
