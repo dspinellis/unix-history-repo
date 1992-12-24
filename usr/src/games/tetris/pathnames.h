f9edc336c8deca60b7fae0ba03be466a8600515b
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1992-12-23 18:16:49 -0800

    update copyright notice with contrib notice
    SCCS-vsn: games/tetris/input.c 5.2
    SCCS-vsn: games/tetris/input.h 5.2
    SCCS-vsn: games/tetris/pathnames.h 5.2
    SCCS-vsn: games/tetris/scores.c 5.2
    SCCS-vsn: games/tetris/scores.h 5.2
    SCCS-vsn: games/tetris/screen.c 5.2
    SCCS-vsn: games/tetris/screen.h 5.2
    SCCS-vsn: games/tetris/shapes.c 5.2
    SCCS-vsn: games/tetris/tetris.c 5.2
    SCCS-vsn: games/tetris/tetris.h 5.2

--- usr/src/games/tetris/pathnames.h
+++ usr/src/games/tetris/pathnames.h
@@ -2,9 +2,12 @@
  * Copyright (c) 1992 The Regents of the University of California.
  * All rights reserved.
  *
+ * This code is derived from software contributed to Berkeley by
+ * Chris Torek and Darren F. Provine.
+ *
  * %sccs.include.redist.c%
  *
- *	@(#)pathnames.h	5.1 (Berkeley) %G%
+ *	@(#)pathnames.h	5.2 (Berkeley) %G%
  */
 
 #define _PATH_SCOREFILE	"/var/games/tetris.scores"
