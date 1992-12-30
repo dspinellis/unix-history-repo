48abeebc55dc589b2ffdb4d69faf169af7794c0a
CSRG <csrg@ucbvax.Berkeley.EDU>
1992-12-30 10:41:28 -0800

    BSD 4_4_Lite1 development Work on file usr/src/contrib/news/trn3/INTERN.h
    Synthesized-from: CSRG/cd2/4.4BSD-Lite1

---
+++ usr/src/contrib/news/trn3/INTERN.h
@@ -0,0 +1,26 @@
+/* $Id: INTERN.h,v 3.0 1991/09/09 20:18:23 davison Trn $
+ */
+/* This software is Copyright 1991 by Stan Barber. 
+ *
+ * Permission is hereby granted to copy, reproduce, redistribute or otherwise
+ * use this software as long as: there is no monetary profit gained
+ * specifically from the use or reproduction of this software, it is not
+ * sold, rented, traded or otherwise marketed, and this copyright notice is
+ * included prominently in any copy made. 
+ *
+ * The author make no claims as to the fitness or correctness of this software
+ * for any use whatsoever, and it is provided as is. Any use of this software
+ * is at the user's own risk. 
+ */
+
+#undef EXT
+#define EXT
+
+#undef INIT
+#ifdef xenix
+#define INIT(x) =x
+#else
+#define INIT(x) = x
+#endif
+
+#define DOINIT
