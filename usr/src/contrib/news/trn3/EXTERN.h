2de43cc616c6948c153779320ae5c90063754795
CSRG <csrg@ucbvax.Berkeley.EDU>
1992-12-30 10:41:11 -0800

    BSD 4_4 development Work on file usr/src/contrib/news/trn3/EXTERN.h
    Synthesized-from: CSRG/cd3/4.4

---
+++ usr/src/contrib/news/trn3/EXTERN.h
@@ -0,0 +1,22 @@
+/* $Id: EXTERN.h,v 3.0 1991/09/09 20:18:23 davison Trn $
+ */
+/* This software is Copyright 1991 by Stan Barber. 
+ *
+ * Permission is hereby granted to copy, reproduce, redistribute or otherwise
+ * use this software as long as: there is no monetary profit gained
+ * specifically from the use or reproduction of this software, it is not
+ * sold, rented, traded or otherwise marketed, and this copyright notice is
+ * included prominently in any copy made. 
+ *
+ * The author make no claims as to the fitness or correctness of this software
+ * for any use whatsoever, and it is provided as is. Any use of this software
+ * is at the user's own risk. 
+ */
+
+#undef EXT
+#define EXT extern
+
+#undef INIT
+#define INIT(x)
+
+#undef DOINIT
