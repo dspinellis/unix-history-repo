35143c3001bd95dc5ade42340974c40a704aaef5
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-01-01 21:17:46 -0800

    pass timeouts to sfgets; check for control characters in addresses; fixed deferred error reporting; report duplicate aliases; handle mixed case recursive aliases; misc fixes
    SCCS-vsn: usr.sbin/sendmail/src/version.c 6.3

--- usr/src/usr.sbin/sendmail/src/version.c
+++ usr/src/usr.sbin/sendmail/src/version.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)version.c	6.2 (Berkeley) %G%";
+static char sccsid[] = "@(#)version.c	6.3 (Berkeley) %G%";
 #endif /* not lint */
 
-char	Version[] = "ALPHA-6.2";
+char	Version[] = "ALPHA-6.3";
