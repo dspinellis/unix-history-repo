bb495e443ac903c8bfe72ff1fceac6bfdd153fda
CSRG <csrg@ucbvax.Berkeley.EDU>
1993-01-06 00:55:49 -0800

    BSD 4_4 development Work on file usr/src/contrib/bind-4.9/contrib/host/type.h
    Synthesized-from: CSRG/cd3/4.4

---
+++ usr/src/contrib/bind-4.9/contrib/host/type.h
@@ -0,0 +1,31 @@
+#ifndef T_TXT
+#define T_TXT		16
+#endif
+
+#ifndef T_RP
+#define T_RP		17
+#endif
+
+#ifndef T_AFSDB
+#define T_AFSDB		18
+#endif
+
+#ifndef T_X25
+#define T_X25		19
+#endif
+
+#ifndef T_ISDN
+#define T_ISDN		20
+#endif
+
+#ifndef T_RT
+#define T_RT		21
+#endif
+
+#ifndef T_NSAP
+#define T_NSAP		22
+#endif
+
+#ifndef T_NSAPPTR
+#define T_NSAPPTR	23
+#endif
