e0f5e491ca04d44fd74c05d04839b82aa4797a08
Julian Elischer <julian@dialix.oz.au>
1993-01-11 00:00:00 +0000

    MULTIPLE PROCESSES ON ONE PTY
    	Two processes each write to the same pty in a special case, each
    sleeps waiting on theoutput queue.  Unfortunatly, the pty code sleeps
    and wakes up on this same address because the sub-field it seeks on is
    the first in the output queue structure, and thus has the same address
    as the queue as a whole. The solution is to make the pty code sleep and
    wakeup on another sub-field of the structure.  The result of the
    confusion is that the two processes writing to the pty simply keep
    waking each other up (at high priority, and then going to sleep
    themselves at high priority)  As a result, all other work on the
    processor comes to a stop.
    
    AUTHOR:		Julian Elischer (julian@ref.tfs.com)
    386BSD-Patchkit: patch00070

--- usr/src/sys.386bsd/kern/tty_pty.c
+++ usr/src/sys.386bsd/kern/tty_pty.c
@@ -34,7 +34,7 @@
  *
  * PATCHES MAGIC                LEVEL   PATCH THAT GOT US HERE
  * --------------------         -----   ----------------------
- * CURRENT PATCH LEVEL:         3       00067
+ * CURRENT PATCH LEVEL:         4       00070
  * --------------------         -----   ----------------------
  *
  * 11 Dec 92	Williams Jolitz		Fixed tty handling
@@ -42,6 +42,8 @@
  * 28 Nov 1991	Warren Toomey		Cleaned up the use of COMPAT_43
  *					in the 386BSD kernel.	 
  * 6 Oct 1992	Holger Veit		Fixed 'hanging console' bug
+ * 11 Jan 93	Julian Elischer		Fixes multiple processes on one
+ *					pty bug
  */
 static char rcsid[] = "$Header: /usr/bill/working/sys/kern/RCS/tty_pty.c,v 1.3 92/01/21 21:31:23 william Exp $";
 
@@ -236,7 +238,7 @@ ptcwakeup(tp, flag)
 			pti->pt_selr = 0;
 			pti->pt_flags &= ~PF_RCOLL;
 		}
-		wakeup((caddr_t)&tp->t_out.rb_hd);
+		wakeup((caddr_t)&tp->t_out.rb_tl);
 	}
 	if (flag & FWRITE) {
 		if (pti->pt_selw) {
@@ -336,7 +338,7 @@ ptcread(dev, uio, flag)
 			return (0);	/* EOF */
 		if (flag & IO_NDELAY)
 			return (EWOULDBLOCK);
-		if (error = tsleep((caddr_t)&tp->t_out.rb_hd, TTIPRI | PCATCH,
+		if (error = tsleep((caddr_t)&tp->t_out.rb_tl, TTIPRI | PCATCH,
 		    ttyin, 0))
 			return (error);
 	}
