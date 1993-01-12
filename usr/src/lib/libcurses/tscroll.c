98034dbc69f842404d84d23e2a35a6e3f4a66a62
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-01-11 20:17:22 -0800

    tscroll -> __tscroll (for now)
    SCCS-vsn: lib/libcurses/refresh.c 5.28
    SCCS-vsn: lib/libcurses/tscroll.c 5.4

--- usr/src/lib/libcurses/tscroll.c
+++ usr/src/lib/libcurses/tscroll.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)tscroll.c	5.3 (Berkeley) %G%";
+static char sccsid[] = "@(#)tscroll.c	5.4 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <stdio.h>
@@ -36,7 +36,7 @@ static char sccsid[] = "@(#)tscroll.c	5.3 (Berkeley) %G%";
  * all other characters are ``self-inserting''.
  */
 char *
-tscroll(cap, n)
+__tscroll(cap, n)
 	const char *cap;
 	int n;
 {
