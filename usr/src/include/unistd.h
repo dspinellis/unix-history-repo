26dee5edf1607a6a92de9c8ba2c4e116f0f52f42
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-01-12 18:28:41 -0800

    move cuserid to libcompat, POSIX no longer requires it
    SCCS-vsn: include/unistd.h 5.23

--- usr/src/include/unistd.h
+++ usr/src/include/unistd.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)unistd.h	5.22 (Berkeley) %G%
+ *	@(#)unistd.h	5.23 (Berkeley) %G%
  */
 
 #ifndef _UNISTD_H_
@@ -30,7 +30,6 @@ u_int	 alarm __P((u_int));
 int	 chdir __P((const char *));
 int	 chown __P((const char *, uid_t, gid_t));
 int	 close __P((int));
-char	*cuserid __P((char *));
 int	 dup __P((int));
 int	 dup2 __P((int, int));
 int	 execl __P((const char *, const char *, ...));
