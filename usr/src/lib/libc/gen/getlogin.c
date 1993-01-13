41069b004024753b49f5ee00524103ed955a8ea7
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-01-12 18:32:26 -0800

    move cuserid() to libcompat, POSIX no longer requires it
    SCCS-vsn: lib/libc/gen/getlogin.c 5.10

--- usr/src/lib/libc/gen/getlogin.c
+++ usr/src/lib/libc/gen/getlogin.c
@@ -6,7 +6,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)getlogin.c	5.9 (Berkeley) %G%";
+static char sccsid[] = "@(#)getlogin.c	5.10 (Berkeley) %G%";
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/param.h>
@@ -29,22 +29,4 @@ getlogin()
 		_logname_valid = 1;
 	}
 	return (*logname ? logname : (char *)NULL);
-}
-
-char *
-cuserid(s)
-	char *s;
-{
-	register struct passwd *pwd;
-
-	if ((pwd = getpwuid(geteuid())) == NULL) {
-		if (s)
-			*s = '\0';
-		return (s);
-	}
-	if (s) {
-		(void)strncpy(s, pwd->pw_name, L_cuserid);
-		return (s);
-	}
-	return (pwd->pw_name);
 }
