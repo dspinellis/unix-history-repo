df5ac75c2d78253ad6e8cf66529d8c59db8f966f
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-01-14 16:50:20 -0800

    fix minor debugging botch
    SCCS-vsn: usr.sbin/sendmail/src/parseaddr.c 6.7

--- usr/src/usr.sbin/sendmail/src/parseaddr.c
+++ usr/src/usr.sbin/sendmail/src/parseaddr.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)parseaddr.c	6.6 (Berkeley) %G%";
+static char sccsid[] = "@(#)parseaddr.c	6.7 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -635,9 +635,9 @@ rewrite(pvp, ruleset)
 			if (tTd(21, 35))
 			{
 				printf("operator=");
-				xputs(ap);
-				printf(", token=");
 				xputs(rp);
+				printf(", token=");
+				xputs(ap);
 				printf("\n");
 			}
 			if (rp == NULL)
