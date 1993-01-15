38dde045fe840bce76f682d7f4155c4601aa4b30
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-01-14 18:33:40 -0800

    revert debugging to old values
    SCCS-vsn: usr.sbin/sendmail/src/parseaddr.c 6.8

--- usr/src/usr.sbin/sendmail/src/parseaddr.c
+++ usr/src/usr.sbin/sendmail/src/parseaddr.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)parseaddr.c	6.7 (Berkeley) %G%";
+static char sccsid[] = "@(#)parseaddr.c	6.8 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -634,9 +634,9 @@ rewrite(pvp, ruleset)
 			rp = *rvp;
 			if (tTd(21, 35))
 			{
-				printf("operator=");
+				printf("rp=");
 				xputs(rp);
-				printf(", token=");
+				printf(", ap=");
 				xputs(ap);
 				printf("\n");
 			}
