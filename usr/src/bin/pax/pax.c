af62b40c5373ffd629b63cd4ca1a9f8c061d8ced
Keith Muller <muller@ucbvax.Berkeley.EDU>
1993-01-17 00:37:34 -0800

    made copyright a static
    SCCS-vsn: bin/pax/pax.c 1.3
    SCCS-vsn: bin/pax/extern.h 1.3

--- usr/src/bin/pax/pax.c
+++ usr/src/bin/pax/pax.c
@@ -10,13 +10,13 @@
  */
 
 #ifndef lint
-char copyright[] =
+static char copyright[] =
 "@(#) Copyright (c) 1992 The Regents of the University of California.\n\
  All rights reserved.\n";
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)pax.c	1.2 (Berkeley) %G%";
+static char sccsid[] = "@(#)pax.c	1.3 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <stdio.h>
