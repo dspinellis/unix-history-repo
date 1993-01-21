332d87b0285d50ba2eb3805bb225fe71b52b60cb
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-01-20 21:24:10 -0800

    delete extra copyright notice
    SCCS-vsn: lib/libtelnet/getent.c 5.3

--- usr/src/lib/libtelnet/getent.c
+++ usr/src/lib/libtelnet/getent.c
@@ -6,27 +6,8 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)getent.c	5.2 (Berkeley) %G%";
+static char sccsid[] = "@(#)getent.c	5.3 (Berkeley) %G%";
 #endif /* not lint */
-
-/*
- * Copyright (c) 1991 Regents of the University of California.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms are permitted provided
- * that: (1) source distributions retain this entire copyright notice and
- * comment, and (2) distributions including binaries display the following
- * acknowledgement:  ``This product includes software developed by the
- * University of California, Berkeley and its contributors'' in the
- * documentation or other materials provided with the distribution and in
- * all advertising materials mentioning features or use of this software.
- * Neither the name of the University nor the names of its contributors may
- * be used to endorse or promote products derived from this software without
- * specific prior written permission.
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR IMPLIED
- * WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
- */
 
 /*ARGSUSED*/
 getent(cp, name)
