c285c47eca0cade2521751d023be0cbf9e8125f4
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-01-23 01:20:21 -0800

    fix -e compat flag; a few compilation warnings; improve error messages
    SCCS-vsn: usr.sbin/sendmail/src/version.c 6.13
    SCCS-vsn: usr.sbin/sendmail/src/daemon.c 6.3

--- usr/src/usr.sbin/sendmail/src/version.c
+++ usr/src/usr.sbin/sendmail/src/version.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)version.c	6.12 (Berkeley) %G%";
+static char sccsid[] = "@(#)version.c	6.13 (Berkeley) %G%";
 #endif /* not lint */
 
-char	Version[] = "ALPHA-6.12";
+char	Version[] = "ALPHA-6.13";
