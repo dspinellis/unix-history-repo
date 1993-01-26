56b0f533a7dc3730579731cf841c13e4e6ac5a69
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1993-01-25 20:49:48 -0800

    may miss dumping last inode
    SCCS-vsn: sbin/dump/traverse.c 5.22

--- usr/src/sbin/dump/traverse.c
+++ usr/src/sbin/dump/traverse.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)traverse.c	5.21 (Berkeley) %G%";
+static char sccsid[] = "@(#)traverse.c	5.22 (Berkeley) %G%";
 #endif /* not lint */
 
 #ifdef sunos
@@ -89,7 +89,7 @@ mapfiles(maxino, tapesize)
 	register struct dinode *dp;
 	int anydirskipped = 0;
 
-	for (ino = 0; ino < maxino; ino++) {
+	for (ino = 0; ino <= maxino; ino++) {
 		dp = getino(ino);
 		if ((mode = (dp->di_mode & IFMT)) == 0)
 			continue;
