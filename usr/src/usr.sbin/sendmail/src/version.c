dd001511059e21af9f5e02f78fd6a9e90e9bd53e
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-01-28 16:05:32 -0800

    fix mci_state after sending RSET command
    SCCS-vsn: usr.sbin/sendmail/src/usersmtp.c 6.4
    SCCS-vsn: usr.sbin/sendmail/src/version.c 6.16

--- usr/src/usr.sbin/sendmail/src/version.c
+++ usr/src/usr.sbin/sendmail/src/version.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)version.c	6.15 (Berkeley) %G%";
+static char sccsid[] = "@(#)version.c	6.16 (Berkeley) %G%";
 #endif /* not lint */
 
-char	Version[] = "ALPHA-6.15";
+char	Version[] = "ALPHA-6.16";
