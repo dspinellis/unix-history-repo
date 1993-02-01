7217f7466fb157de2765b1e0c286df2a8b519314
CSRG <csrg@ucbvax.Berkeley.EDU>
1993-02-01 00:46:28 -0800

    BSD 4_4_Lite2 development Work on file usr/src/contrib/X11R5-luna68k/mit/server/ddx/sol/solUtils.c
    Synthesized-from: CSRG/cd3/4.4BSD-Lite2

---
+++ usr/src/contrib/X11R5-luna68k/mit/server/ddx/sol/solUtils.c
@@ -0,0 +1,26 @@
+/*
+ * solUtils.c --
+ *
+ *	remade by A.Fujita, DEC-16-1992
+ */
+
+#include "sol.h"
+
+int
+ddxProcessArgument (argc, argv, i)
+int argc;
+char *argv[];
+int i;
+{
+#ifdef	DEBUG
+	fprintf(stderr, "solUtils.c: ddxProcessArgument()\n");
+#endif
+}
+
+void
+ddxUseMsg()
+{
+#ifdef	DEBUG
+	fprintf(stderr, "solUtils.c: ddxUseMsg()\n");
+#endif
+}
