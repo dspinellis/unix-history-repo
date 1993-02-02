27a05a600baf1f646a78cd7407a2581efaeeb000
Chris Torek <torek@ucbvax.Berkeley.EDU>
1993-02-01 19:17:29 -0800

    rm bitfields
    SCCS-vsn: sys/dev/scsi/processor.h 5.3

--- usr/src/sys/dev/scsi/processor.h
+++ usr/src/sys/dev/scsi/processor.h
@@ -13,9 +13,9 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)processor.h	5.2 (Berkeley) %G%
+ *	@(#)processor.h	5.3 (Berkeley) %G%
  *
- * from: $Header: processor.h,v 1.2 92/05/15 11:24:01 torek Exp $ (LBL)
+ * from: $Header: processor.h,v 1.3 92/12/02 03:52:27 torek Exp $ (LBL)
  */
 
 /*
@@ -29,8 +29,7 @@
  */
 struct scsi_cdb_rs {
 	u_char	cdb_cmd,		/* 0x8 or 0xa */
-		cdb_lun:3,		/* logical unit number */
-		cdb_xxx:5,		/* reserved */
+		cdb_lun_xxx,		/* logical unit number + reserved */
 		cdb_lenh,		/* buffer or data length (MSB) */
 		cdb_lenm,		/* buffer or data length */
 		cdb_lenl,		/* buffer or data length (LSB) */
