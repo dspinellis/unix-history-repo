e974d50f8a5422e8b1f03c83472757d012b288b2
Chris Torek <torek@ucbvax.Berkeley.EDU>
1993-02-01 19:28:43 -0800

    rm bitfields; move mode select to common
    SCCS-vsn: sys/dev/scsi/tape.h 5.3

--- usr/src/sys/dev/scsi/tape.h
+++ usr/src/sys/dev/scsi/tape.h
@@ -13,13 +13,15 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)tape.h	5.2 (Berkeley) %G%
+ *	@(#)tape.h	5.3 (Berkeley) %G%
  *
- * from: $Header: tape.h,v 1.2 92/05/15 11:24:06 torek Exp $ (LBL)
+ * from: $Header: tape.h,v 1.3 92/12/02 03:53:14 torek Exp $ (LBL)
  */
 
 /*
  * SCSI definitions for Sequential Access Devices (tapes).
+ *
+ * Commands defined in common headers (scsi.h or disktape.h) are omitted.
  */
 #define	CMD_REWIND		0x01	/* rewind */
 #define	CMD_READ_BLOCK_LIMITS	0x05	/* read block limits */
@@ -31,12 +33,10 @@
 #define	CMD_SPACE		0x11	/* space */
 #define	CMD_VERIFY		0x13	/* verify */
 #define	CMD_RBD			0x14	/* recover buffered data */
-#define	CMD_MODE_SELECT		0x15	/* mode select */
 #define	CMD_RESERVE_UNIT	0x16	/* reserve unit */
 #define	CMD_RELEASE_UNIT	0x17	/* release unit */
 /*	CMD_COPY		0x18	   copy (common to all scsi devs) */
 #define	CMD_ERASE		0x19	/* erase */
-#define	CMD_MODE_SENSE		0x1a	/* mode sense */
 #define	CMD_LOAD_UNLOAD		0x1b	/* load/unload */
 #define	CMD_PAMR		0x1e	/* prevent/allow medium removal */
 
@@ -47,23 +47,21 @@
  */
 struct scsi_cdb_rw {
 	u_char	cdb_cmd,	/* 0x08 or 0x0a or 0x0f or 0x13 or 0x14 */
-		cdb_lun:3,	/* logical unit number */
-		cdb_xxx:3,	/* reserved */
-		cdb_bytecmp:1,	/* byte-by-byte comparison (VERIFY only) */
-		cdb_fixed:1,	/* fixed length blocks */
+		cdb_lun_bf,	/* LUN + reserved + bytecmp + fixed */
 		cdb_lenh,	/* transfer length (MSB) */
 		cdb_lenm,	/* transfer length */
 		cdb_lenl,	/* transfer length (LSB) */
 		cdb_ctrl;	/* control byte */
 };
+#define	SCSI_RW_BYTECMP		0x02	/* byte compare flag if verify */
+#define	SCSI_RW_FIXEDBLK	0x01	/* fixed block size for read/write */
 
 /*
  * Structure of a TRACK SELECT command.
  */
 struct scsi_cdb_ts {
 	u_char	cdb_cmd,	/* 0x0b */
-		cdb_lun:3,	/* logical unit number */
-		cdb_xxx0:5,	/* reserved */
+		cdb_lun_xxx,	/* logical unit number + reserved */
 		cdb_xxx1,	/* reserved */
 		cdb_xxx2,	/* reserved */
 		cdb_track,	/* track value */
@@ -75,8 +73,7 @@ struct scsi_cdb_ts {
  */
 struct scsi_cdb_wfm {
 	u_char	cdb_cmd,	/* 0x0b */
-		cdb_lun:3,	/* logical unit number */
-		cdb_xxx0:5,	/* reserved */
+		cdb_lun_xxx,	/* logical unit number + reserved */
 		cdb_nfh,	/* number of filemarks (MSB) */
 		cdb_nfm,	/* number of filemarks */
 		cdb_nfl,	/* number of filemarks (LSB) */
@@ -88,9 +85,7 @@ struct scsi_cdb_wfm {
  */
 struct scsi_cdb_space {
 	u_char	cdb_cmd,	/* 0x0b */
-		cdb_lun:3,	/* logical unit number */
-		cdb_xxx0:3,	/* reserved */
-		cdb_code:2,	/* code (see below) */
+		cdb_lun_code,	/* LUN + reserved + 2-bit code */
 		cdb_counth,	/* count (MSB) */
 		cdb_countm,	/* count */
 		cdb_countl,	/* count (LSB) */
@@ -102,58 +97,11 @@ struct scsi_cdb_space {
 #define	SCSI_CMD_SPACE_PEOD	3	/* skip to physical end of data */
 
 /*
- * Mode Select parameters (data).
- */
-struct scsi_msel {
-	u_short	msel_xxx0;	/* reserved */
-	u_char	msel_xxx1:1,	/* reserved */
-		msel_bm:3,	/* buffered mode */
-		msel_speed:4,	/* speed */
-		msel_bdl;	/* block descriptor length */
-	struct scsi_msel_bdesc {
-		u_char	dc,	/* density code */
-			nbh,	/* number of blocks (MSB) */
-			nbm,	/* number of blocks */
-			nbl,	/* number of blocks (LSB) */
-			xxx,	/* reserved */
-			blh,	/* block length (MSB) */
-			blm,	/* block length */
-			bll;	/* block length (LSB) */
-	} msel_bd[1];		/* actually longer */
-	/* followed by Vendor Unique bytes */
-};
-
-/* buffered mode and speed */
-#define	SCSI_MSEL_BM_UNBUFFERED	0	/* unbuffered writes */
-#define	SCSI_MSEL_BM_BUFFERED	1	/* buffered writes allowed */
-#define	SCSI_MSEL_SPEED_DEFAULT	0	/* use device default speed */
-#define	SCSI_MSEL_SPEED_LOW	1	/* use lowest speed */
-#define	SCSI_MSEL_SPEED_HIGH	15	/* use highest speed */
-
-/* density codes */
-#define	SCSI_MSEL_DC_DEFAULT	0	/* use device default density */
-#define	SCSI_MSEL_DC_9T_800BPI	1	/* 9 track, 800 bpi */
-#define	SCSI_MSEL_DC_9T_1600BPI	2	/* 9 track, 1600 bpi */
-#define	SCSI_MSEL_DC_9T_6250BPI	3	/* 9 track, 6250 bpi */
-#define	SCSI_MSEL_DC_QIC_XX1	4	/* QIC-11? 4 or 9 track, 8000 bpi */
-#define	SCSI_MSEL_DC_QIC_XX2	5	/* QIC-11? 4 or 9 track, 8000 bpi */
-#define	SCSI_MSEL_DC_9T_3200BPI	6	/* 9 track, 3200 bpi */
-#define	SCSI_MSEL_DC_QIC_XX3	7	/* QIC, 4 track, 6400 bpi */
-#define	SCSI_MSEL_DC_CS_XX4	8	/* cassette 4 track, 8000 bpi 8/
-#define	SCSI_MSEL_DC_HIC_XX5	9	/* half inch cartridge, 18 track */
-#define	SCSI_MSEL_DC_HIC_XX6	10	/* HIC, 22 track, 6667 bpi */
-#define	SCSI_MSEL_DC_QIC_XX7	11	/* QIC, 4 track, 1600 bpi */
-#define	SCSI_MSEL_DC_HIC_XX8	12	/* HIC, 24 track, 12690 bpi */
-#define	SCSI_MSEL_DC_HIC_XX9	13	/* HIC, 24 track, 25380 bpi */
-
-/*
  * Structure of an ERASE command.
  */
 struct scsi_cdb_erase {
 	u_char	cdb_cmd,	/* 0x0b */
-		cdb_lun:3,	/* logical unit number */
-		cdb_xxx0:4,	/* reserved */
-		cdb_long:1,	/* long erase */
+		cdb_lun_long,	/* LUN + reserved + long-erase flag */
 		cdb_xxx1,	/* reserved */
 		cdb_xxx2,	/* reserved */
 		cdb_xxx3,	/* reserved */
@@ -165,13 +113,12 @@ struct scsi_cdb_erase {
  */
 struct scsi_cdb_lu {
 	u_char	cdb_cmd,	/* 0x1b */
-		cdb_lun:3,	/* logical unit number */
-		cdb_xxx0:4,	/* reserved */
-		cdb_immed:1,	/* return status immediately */
+		cdb_lun_immed,	/* LUN + reserved + immediate flag */
 		cdb_xxx1,	/* reserved */
 		cdb_xxx2,	/* reserved */
-		cdb_xxx3:6,	/* reserved */
-		cdb_reten:1,	/* retension tape */
-		cdb_load:1,	/* load (else unload) */
+		cdb_rl,		/* reserved + retension flag + load flag */
 		cdb_ctrl;	/* control byte */
 };
+#define	SCSI_LU_RL_RETEN	0x02	/* retension */
+#define	SCSI_LU_RL_LOAD		0x01	/* load */
+#define	SCSI_LU_RL_UNLOAD	0x00	/* unload (pseudo flag) */
