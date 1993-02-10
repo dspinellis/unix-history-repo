92152a4dc84c5bcfa707897f507a01b55aca322e
Akito Fujita <akito@ucbvax.Berkeley.EDU>
1993-02-10 02:16:57 -0800

    local verison update
    SCCS-vsn: sys/luna68k/stand/init_main.c 7.5

--- usr/src/sys/luna68k/stand/init_main.c
+++ usr/src/sys/luna68k/stand/init_main.c
@@ -8,7 +8,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)init_main.c	7.4 (Berkeley) %G%
+ *	@(#)init_main.c	7.5 (Berkeley) %G%
  */
 
 #include <sys/param.h>
@@ -23,7 +23,7 @@ extern int dipsw1, dipsw2;
 
 extern char default_file[];
 
-#define	VERS_LOCAL	"Phase-28"
+#define	VERS_LOCAL	"Phase-29"
 
 extern int howto;
 extern int devtype;
