0b011310219a389737841ffbeaa4eb67591870f3
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1993-02-11 03:41:22 -0800

    additional information for uname
    SCCS-vsn: sys/conf/newvers.sh 7.6

--- usr/src/sys/conf/newvers.sh
+++ usr/src/sys/conf/newvers.sh
@@ -1,11 +1,11 @@
 #!/bin/sh -
 #
-# Copyright (c) 1984, 1986, 1990 The Regents of the University of California.
+# Copyright (c) 1984,1986,1990,1993 The Regents of the University of California.
 # All rights reserved.
 #
 # %sccs.include.redist.sh%
 #
-#	@(#)newvers.sh	7.5 (Berkeley) %G%
+#	@(#)newvers.sh	7.6 (Berkeley) %G%
 #
 
 if [ ! -r version ]
@@ -15,7 +15,9 @@ fi
 
 touch version
 v=`cat version` u=${USER-root} d=`pwd` h=`hostname` t=`date`
-( echo "char sccs[] = \"@(#)4.4BSD-Alpha #${v}: ${t} (${u}@${h}:${d})\\n\";" ;
-  echo "char version[] = \"4.4BSD-Alpha UNIX #${v}: ${t}\\n    ${u}@${h}:${d}\\n\";"
-) > vers.c
+echo "char ostype[] = \"4.4BSD\";" > vers.c
+echo "char osrelease[] = \"4.4BSD-Alpha\";" >> vers.c
+echo "char sccs[4] = { '@', '(', '#', ')' };" >>vers.c
+echo "char version[] = \"4.4BSD-Alpha UNIX #${v}: ${t}\\n    ${u}@${h}:${d}\\n\";" >>vers.c
+
 echo `expr ${v} + 1` > version
