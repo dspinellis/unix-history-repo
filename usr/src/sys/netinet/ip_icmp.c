a9cd698eba2bd80f604b1a95b4319a9bfa7f67fd
Keith Sklower <sklower@ucbvax.Berkeley.EDU>
1993-02-12 03:46:11 -0800

    replies to some ICMP broadcasts were themselves mistaken broadcast due to non-clearing of an mbuf-flag.  Fix due to Mike Karels
    SCCS-vsn: sys/netinet/ip_icmp.c 7.20

--- usr/src/sys/netinet/ip_icmp.c
+++ usr/src/sys/netinet/ip_icmp.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)ip_icmp.c	7.19 (Berkeley) %G%
+ *	@(#)ip_icmp.c	7.20 (Berkeley) %G%
  */
 
 #include <sys/param.h>
@@ -491,6 +491,7 @@ icmp_reflect(m)
 		bcopy((caddr_t)ip + optlen, (caddr_t)(ip + 1),
 			 (unsigned)(m->m_len - sizeof(struct ip)));
 	}
+	m->m_flags &= ~(M_BCAST|M_MCAST);
 	icmp_send(m, opts);
 	if (opts)
 		(void)m_free(opts);
