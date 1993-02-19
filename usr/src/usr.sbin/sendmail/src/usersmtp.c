d8b98cc141127d65d8736383970af8383671427b
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-02-18 23:11:49 -0800

    change logging of 4yz error codes
    SCCS-vsn: usr.sbin/sendmail/src/usersmtp.c 6.9

--- usr/src/usr.sbin/sendmail/src/usersmtp.c
+++ usr/src/usr.sbin/sendmail/src/usersmtp.c
@@ -10,9 +10,9 @@
 
 #ifndef lint
 #ifdef SMTP
-static char sccsid[] = "@(#)usersmtp.c	6.8 (Berkeley) %G% (with SMTP)";
+static char sccsid[] = "@(#)usersmtp.c	6.9 (Berkeley) %G% (with SMTP)";
 #else
-static char sccsid[] = "@(#)usersmtp.c	6.8 (Berkeley) %G% (without SMTP)";
+static char sccsid[] = "@(#)usersmtp.c	6.9 (Berkeley) %G% (without SMTP)";
 #endif
 #endif /* not lint */
 
@@ -548,7 +548,7 @@ reply(m)
 
 		/* save temporary failure messages for posterity */
 		if (SmtpReplyBuffer[0] == '4' && SmtpError[0] == '\0')
-			(void) strcpy(SmtpError, &SmtpReplyBuffer[4]);
+			(void) strcpy(SmtpError, SmtpReplyBuffer);
 
 		return (r);
 	}
