3ec41b65915f7e752624367e44346a67b7cfa63d
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-02-20 00:40:18 -0800

    many minor bug fixes; add "c" mailer flag to strip comments; improve YP support; move metacharacters from C0 to C1; change method of doing sender duplicate deletion
    SCCS-vsn: usr.sbin/sendmail/src/version.c 6.24

--- usr/src/usr.sbin/sendmail/src/version.c
+++ usr/src/usr.sbin/sendmail/src/version.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)version.c	6.23 (Berkeley) %G%";
+static char sccsid[] = "@(#)version.c	6.24 (Berkeley) %G%";
 #endif /* not lint */
 
-char	Version[] = "ALPHA-6.23";
+char	Version[] = "ALPHA-6.24";
