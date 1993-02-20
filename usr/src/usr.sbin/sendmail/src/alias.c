585dfa825a507ca9aa3ad8f86805102f65fc1692
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-02-19 18:37:15 -0800

    make duplicate deletion more global
    SCCS-vsn: usr.sbin/sendmail/src/recipient.c 6.12
    SCCS-vsn: usr.sbin/sendmail/src/alias.c 6.13
    SCCS-vsn: usr.sbin/sendmail/src/udb.c 6.8

--- usr/src/usr.sbin/sendmail/src/alias.c
+++ usr/src/usr.sbin/sendmail/src/alias.c
@@ -29,15 +29,15 @@ ERROR: DBM is no longer supported -- use NDBM instead.
 #ifndef lint
 #ifdef NEWDB
 #ifdef NDBM
-static char sccsid[] = "@(#)alias.c	6.12 (Berkeley) %G% (with NEWDB and NDBM)";
+static char sccsid[] = "@(#)alias.c	6.13 (Berkeley) %G% (with NEWDB and NDBM)";
 #else
-static char sccsid[] = "@(#)alias.c	6.12 (Berkeley) %G% (with NEWDB)";
+static char sccsid[] = "@(#)alias.c	6.13 (Berkeley) %G% (with NEWDB)";
 #endif
 #else
 #ifdef NDBM
-static char sccsid[] = "@(#)alias.c	6.12 (Berkeley) %G% (with NDBM)";
+static char sccsid[] = "@(#)alias.c	6.13 (Berkeley) %G% (with NDBM)";
 #else
-static char sccsid[] = "@(#)alias.c	6.12 (Berkeley) %G% (without NEWDB or NDBM)";
+static char sccsid[] = "@(#)alias.c	6.13 (Berkeley) %G% (without NEWDB or NDBM)";
 #endif
 #endif
 #endif /* not lint */
@@ -149,6 +149,15 @@ alias(a, sendq, e)
 	AliasLevel++;
 	sendtolist(p, a, sendq, e);
 	AliasLevel--;
+	if (!bitset(QSELFREF, a->q_flags))
+	{
+		if (tTd(27, 5))
+		{
+			printf("alias: QDONTSEND ");
+			printaddr(a, FALSE);
+		}
+		a->q_flags |= QDONTSEND;
+	}
 }
 /*
 **  ALIASLOOKUP -- look up a name in the alias file.
--- usr/src/usr.sbin/sendmail/src/recipient.c
+++ usr/src/usr.sbin/sendmail/src/recipient.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)recipient.c	6.11 (Berkeley) %G%";
+static char sccsid[] = "@(#)recipient.c	6.12 (Berkeley) %G%";
 #endif /* not lint */
 
 # include <sys/types.h>
@@ -93,17 +93,6 @@ sendtolist(list, ctladdr, sendq, e)
 			ctladdr->q_flags |= QSELFREF;
 		al = a;
 		firstone = FALSE;
-	}
-
-	/* if this alias doesn't include itself, delete ctladdr */
-	if (ctladdr != NULL && !bitset(QSELFREF, ctladdr->q_flags))
-	{
-		if (tTd(25, 5))
-		{
-			printf("sendtolist: QDONTSEND ");
-			printaddr(ctladdr, FALSE);
-		}
-		ctladdr->q_flags |= QDONTSEND;
 	}
 
 	/* arrange to send to everyone on the local send list */
@@ -221,10 +210,12 @@ recipient(a, sendq, e)
 				printf("%s in sendq: ", a->q_paddr);
 				printaddr(q, FALSE);
 			}
-			if (!bitset(QDONTSEND, a->q_flags))
-				message(Arpa_Info, "duplicate suppressed");
 			if (!bitset(QPRIMARY, q->q_flags))
+			{
+				if (!bitset(QDONTSEND, a->q_flags))
+					message(Arpa_Info, "duplicate suppressed");
 				q->q_flags |= a->q_flags;
+			}
 			return (q);
 		}
 	}
@@ -678,6 +669,15 @@ include(fname, forwarding, ctladdr, sendq, e)
 		AliasLevel++;
 		sendtolist(buf, ctladdr, sendq, e);
 		AliasLevel--;
+	}
+	if (!bitset(QSELFREF, ctladdr->q_flags))
+	{
+		if (tTd(27, 5))
+		{
+			printf("include: QDONTSEND ");
+			printaddr(ctladdr, FALSE);
+		}
+		ctladdr->q_flags |= QDONTSEND;
 	}
 
 	(void) fclose(fp);
