69d992bee50d4e03bdcdae4ec9ef7250d822dd90
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-02-20 22:21:26 -0800

    syslog when low on disk space
    SCCS-vsn: usr.sbin/sendmail/src/conf.c 6.19

--- usr/src/usr.sbin/sendmail/src/conf.c
+++ usr/src/usr.sbin/sendmail/src/conf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)conf.c	6.18 (Berkeley) %G%";
+static char sccsid[] = "@(#)conf.c	6.19 (Berkeley) %G%";
 #endif /* not lint */
 
 # include <sys/ioctl.h>
