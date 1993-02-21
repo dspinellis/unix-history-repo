68c7cd67f813c0b8069c597a229ed7c37ba31b7c
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-02-21 15:42:42 -0800

    add MAXIPADDR
    SCCS-vsn: usr.sbin/sendmail/src/conf.h 6.8

--- usr/src/usr.sbin/sendmail/src/conf.h
+++ usr/src/usr.sbin/sendmail/src/conf.h
@@ -5,7 +5,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)conf.h	6.7 (Berkeley) %G%
+ *	@(#)conf.h	6.8 (Berkeley) %G%
  */
 
 /*
@@ -32,6 +32,7 @@
 # define MAXKEY		128		/* maximum size of a database key */
 # define MEMCHUNKSIZE	1024		/* chunk size for memory allocation */
 # define MAXUSERENVIRON	100		/* max envariables saved */
+# define MAXIPADDR	16		/* max # of IP addrs for this host */
 # define PSBUFSIZE	(MAXNAME * 4)	/* size of prescan buffer */
 
 # ifndef QUEUESIZE
