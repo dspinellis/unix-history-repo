e8248240e1e15a7066b031e9673f0308244bfad7
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-02-21 17:57:27 -0800

    don't "lose" queue runs; completely reset state on RSET; send errors from <> and don't reply to such messages; explicitly check for [a.b.c.d] syntax referencing yourself; generalize read timeouts
    SCCS-vsn: usr.sbin/sendmail/src/version.c 6.26

--- usr/src/usr.sbin/sendmail/src/version.c
+++ usr/src/usr.sbin/sendmail/src/version.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)version.c	6.25 (Berkeley) %G%";
+static char sccsid[] = "@(#)version.c	6.26 (Berkeley) %G%";
 #endif /* not lint */
 
-char	Version[] = "ALPHA-6.25";
+char	Version[] = "ALPHA-6.26";
