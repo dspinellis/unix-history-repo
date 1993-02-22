d6f6f1b34c16c2a2508abcb28a98b0221231970a
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-02-21 22:12:33 -0800

    clean up shouldqueue function
    SCCS-vsn: usr.sbin/sendmail/src/queue.c 6.16
    SCCS-vsn: usr.sbin/sendmail/src/conf.c 6.23

--- usr/src/usr.sbin/sendmail/src/conf.c
+++ usr/src/usr.sbin/sendmail/src/conf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)conf.c	6.22 (Berkeley) %G%";
+static char sccsid[] = "@(#)conf.c	6.23 (Berkeley) %G%";
 #endif /* not lint */
 
 # include <sys/ioctl.h>
@@ -759,6 +759,8 @@ shouldqueue(pri, ctime)
 {
 	if (CurrentLA < QueueLA)
 		return (FALSE);
+	if (CurrentLA >= RefuseLA)
+		return (TRUE);
 	return (pri > (QueueFactor / (CurrentLA - QueueLA + 1)));
 }
 /*
@@ -780,7 +782,7 @@ bool
 refuseconnections()
 {
 	/* this is probably too simplistic */
-	return (CurrentLA > RefuseLA);
+	return (CurrentLA >= RefuseLA);
 }
 /*
 **  SETPROCTITLE -- set process title for ps
--- usr/src/usr.sbin/sendmail/src/queue.c
+++ usr/src/usr.sbin/sendmail/src/queue.c
@@ -10,9 +10,9 @@
 
 #ifndef lint
 #ifdef QUEUE
-static char sccsid[] = "@(#)queue.c	6.15 (Berkeley) %G% (with queueing)";
+static char sccsid[] = "@(#)queue.c	6.16 (Berkeley) %G% (with queueing)";
 #else
-static char sccsid[] = "@(#)queue.c	6.15 (Berkeley) %G% (without queueing)";
+static char sccsid[] = "@(#)queue.c	6.16 (Berkeley) %G% (without queueing)";
 #endif
 #endif /* not lint */
 
@@ -388,7 +388,7 @@ runqueue(forkflag)
 
 	CurrentLA = getla();	/* get load average */
 
-	if (shouldqueue(-100000000L, curtime()))
+	if (shouldqueue(0L, curtime()))
 	{
 		if (Verbose)
 			printf("Skipping queue run -- load average too high\n");
