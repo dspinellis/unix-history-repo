f70d2393382e3f1b31a8adf660a31cb69be3e04e
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-02-21 20:44:26 -0800

    insist on level 4 config file to send errors from <>
    SCCS-vsn: usr.sbin/sendmail/src/main.c 6.22
    SCCS-vsn: usr.sbin/sendmail/src/savemail.c 6.9

--- usr/src/usr.sbin/sendmail/src/savemail.c
+++ usr/src/usr.sbin/sendmail/src/savemail.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)savemail.c	6.8 (Berkeley) %G%";
+static char sccsid[] = "@(#)savemail.c	6.9 (Berkeley) %G%";
 #endif /* not lint */
 
 # include <sys/types.h>
