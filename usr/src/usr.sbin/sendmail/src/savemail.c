f630fab03c721612a28534f7fa556f946f00bcf1
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-02-23 00:59:53 -0800

    add route pruning
    SCCS-vsn: usr.sbin/sendmail/src/sendmail.h 6.18
    SCCS-vsn: usr.sbin/sendmail/src/savemail.c 6.10

--- usr/src/usr.sbin/sendmail/src/savemail.c
+++ usr/src/usr.sbin/sendmail/src/savemail.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)savemail.c	6.9 (Berkeley) %G%";
+static char sccsid[] = "@(#)savemail.c	6.10 (Berkeley) %G%";
 #endif /* not lint */
 
 # include <sys/types.h>
