46607a41f8235c9d01937f8e7d5a603cb7802c07
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-02-23 18:06:41 -0800

    allow definition of _PATH_SENDMAILPID in Makefile
    SCCS-vsn: usr.sbin/sendmail/src/pathnames.h 6.4

--- usr/src/usr.sbin/sendmail/src/pathnames.h
+++ usr/src/usr.sbin/sendmail/src/pathnames.h
@@ -4,13 +4,15 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)pathnames.h	6.3 (Berkeley) %G%
+ *	@(#)pathnames.h	6.4 (Berkeley) %G%
  */
 
 #define	_PATH_SENDMAILCF	"/etc/sendmail.cf"
 #define	_PATH_SENDMAILFC	"/etc/sendmail.fc"
-#ifdef BSD4_4
-#define _PATH_SENDMAILPID	"/var/run/sendmail.pid"
-#else
-#define _PATH_SENDMAILPID	"/etc/sendmail.pid"
+#ifndef _PATH_SENDMAILPID
+# ifdef BSD4_4
+#  define _PATH_SENDMAILPID	"/var/run/sendmail.pid"
+# else
+#  define _PATH_SENDMAILPID	"/etc/sendmail.pid"
+# endif
 #endif
