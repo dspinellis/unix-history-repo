36178f9a946a19f5f2b3340261371c00c1782c76
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-02-24 13:38:57 -0800

    propagate alias owner information to envelope sender; delete trusted users (it was too easy to defeat); delete -R flag; add route-addr pruning per RFC 1123-5.3.3; misc bug fixes & compatibility changes
    SCCS-vsn: usr.sbin/sendmail/src/version.c 6.27

--- usr/src/usr.sbin/sendmail/src/version.c
+++ usr/src/usr.sbin/sendmail/src/version.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)version.c	6.26 (Berkeley) %G%";
+static char sccsid[] = "@(#)version.c	6.27 (Berkeley) %G%";
 #endif /* not lint */
 
-char	Version[] = "ALPHA-6.26";
+char	Version[] = "ALPHA-6.27";
