343db3f0aadaacfafff1cc0fb80d4e67c4ff2ea1
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-02-23 17:19:10 -0800

    fix botch in SMTP VRFY code (assumed all users were local)
    SCCS-vsn: usr.sbin/sendmail/src/recipient.c 6.17
    SCCS-vsn: usr.sbin/sendmail/src/sendmail.h 6.20
    SCCS-vsn: usr.sbin/sendmail/src/alias.c 6.17
    SCCS-vsn: usr.sbin/sendmail/src/udb.c 6.12

--- usr/src/usr.sbin/sendmail/src/sendmail.h
+++ usr/src/usr.sbin/sendmail/src/sendmail.h
@@ -5,7 +5,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)sendmail.h	6.19 (Berkeley) %G%
+ *	@(#)sendmail.h	6.20 (Berkeley) %G%
  */
 
 /*
@@ -15,7 +15,7 @@
 # ifdef _DEFINE
 # define EXTERN
 # ifndef lint
-static char SmailSccsId[] =	"@(#)sendmail.h	6.19		%G%";
+static char SmailSccsId[] =	"@(#)sendmail.h	6.20		%G%";
 # endif lint
 # else /*  _DEFINE */
 # define EXTERN extern
@@ -101,6 +101,7 @@ typedef struct address ADDRESS;
 # define QSENT		000040	/* has been successfully delivered */
 # define QNOTREMOTE	000100	/* not an address for remote forwarding */
 # define QSELFREF	000200	/* this address references itself */
+# define QVERIFIED	000400	/* verified, but not expanded */
 /*
 **  Mailer definition structure.
 **	Every mailer known to the system is declared in this
