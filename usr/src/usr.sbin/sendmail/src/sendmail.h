c8158ccbd402050bce40b62f33160e960e876131
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-02-23 22:45:28 -0800

    delete trusted users code; it's too easy to circumvent
    SCCS-vsn: usr.sbin/sendmail/src/envelope.c 6.13
    SCCS-vsn: usr.sbin/sendmail/src/conf.h 6.10
    SCCS-vsn: usr.sbin/sendmail/src/sendmail.h 6.21
    SCCS-vsn: usr.sbin/sendmail/src/readcf.c 6.12

--- usr/src/usr.sbin/sendmail/src/sendmail.h
+++ usr/src/usr.sbin/sendmail/src/sendmail.h
@@ -5,7 +5,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)sendmail.h	6.20 (Berkeley) %G%
+ *	@(#)sendmail.h	6.21 (Berkeley) %G%
  */
 
 /*
@@ -15,7 +15,7 @@
 # ifdef _DEFINE
 # define EXTERN
 # ifndef lint
-static char SmailSccsId[] =	"@(#)sendmail.h	6.20		%G%";
+static char SmailSccsId[] =	"@(#)sendmail.h	6.21		%G%";
 # endif lint
 # else /*  _DEFINE */
 # define EXTERN extern
