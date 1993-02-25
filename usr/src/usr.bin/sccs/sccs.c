c011763201a0f13e700c69dfd643c0a123852626
Chris Torek <torek@ucbvax.Berkeley.EDU>
1993-02-24 19:40:11 -0800

    quick fix for sys_siglist redeclaration. this really should be reworked
    SCCS-vsn: usr.bin/sccs/sccs.c 5.12

--- usr/src/usr.bin/sccs/sccs.c
+++ usr/src/usr.bin/sccs/sccs.c
@@ -12,7 +12,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)sccs.c	5.11 (Berkeley) %G%";
+static char sccsid[] = "@(#)sccs.c	5.12 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/cdefs.h>
@@ -242,7 +242,9 @@ bool	Debug;			/* turn on tracing */
 extern char	*getenv();
 # endif V6
 
+# ifdef DECLARE_SIGLIST
 extern char	*sys_siglist[];
+# endif
 
 char *gstrcat(), *strcat();
 char *gstrncat(), *strncat();
