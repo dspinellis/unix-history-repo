d7c771e8d81110d706495fe09b1d5ce6ae8d3610
Keith Sklower <sklower@ucbvax.Berkeley.EDU>
1993-02-26 00:39:39 -0800

    make conform to MIB II
    SCCS-vsn: sys/net/if_types.h 7.5

--- usr/src/sys/net/if_types.h
+++ usr/src/sys/net/if_types.h
@@ -4,17 +4,22 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)if_types.h	7.4 (Berkeley) %G%
+ *	@(#)if_types.h	7.5 (Berkeley) %G%
  */
 
 
-/* interface types for benefit of parsing media address headers */
+/*
+ * Interface types for benefit of parsing media address headers.
+ * This list is derived from the SNMP list of ifTypes, currently
+ * documented in RFC1123.
+ */
+
 #define IFT_OTHER	0x1		/* none of the following */
 #define IFT_1822	0x2		/* old-style arpanet imp */
 #define IFT_HDH1822	0x3		/* HDH arpanet imp */
 #define IFT_X25DDN	0x4		/* x25 to imp */
-#define IFT_X25		0x5		/* PDN X25 interface */
-#define	IFT_ETHER	0x6		/* Ethernet I or II */
+#define IFT_X25		0x5		/* PDN X25 interface (RFC877) */
+#define	IFT_ETHER	0x6		/* Ethernet CSMACD */
 #define	IFT_ISO88023	0x7		/* CMSA CD */
 #define	IFT_ISO88024	0x8		/* Token Bus */
 #define	IFT_ISO88025	0x9		/* Token Ring */
@@ -27,10 +32,11 @@
 #define IFT_LAPB	0x10
 #define IFT_SDLC	0x11
 #define IFT_T1		0x12
-#define IFT_CEPT	0x13
+#define IFT_CEPT	0x13		/* E1 - european T1 */
 #define IFT_ISDNBASIC	0x14
 #define IFT_ISDNPRIMARY	0x15
-#define IFT_PTPSERIAL	0x16
+#define IFT_PTPSERIAL	0x16		/* Proprietary PTP serial */
+#define IFT_PPP		0x17		/* RFC 1331 */
 #define	IFT_LOOP	0x18		/* loopback */
 #define IFT_EON		0x19		/* ISO over IP */
 #define	IFT_XETHER	0x1a		/* obsolete 3MB experimental ethernet */
@@ -40,3 +46,4 @@
 #define	IFT_DS3		0x1e		/* Generic T3 */
 #define	IFT_SIP		0x1f		/* SMDS */
 #define	IFT_FRELAY	0x20		/* Frame Relay */
+#define IFT_RS232	0x21		/* Used elsewhere by SNMP as index */
