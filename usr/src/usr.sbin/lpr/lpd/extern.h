dea361bc55f545a86e1a679c7f8b529f7b3a5aa2
Chris Torek <torek@ucbvax.Berkeley.EDU>
1993-02-26 19:40:03 -0800

    rm __BEGIN_DECLS, __END_DECLS; the latter was misspelled and these are only for C++ to use C code anyway.  gcc1 was certainly forgiving about underscores!
    SCCS-vsn: usr.sbin/lpr/lpd/extern.h 5.4

--- usr/src/usr.sbin/lpr/lpd/extern.h
+++ usr/src/usr.sbin/lpr/lpd/extern.h
@@ -2,15 +2,12 @@
  * Copyright (c) 1989 The Regents of the University of California.
  * All rights reserved.
  *
- *
  * %sccs.include.redist.c%
  *
- *      @(#)extern.h	5.3 (Berkeley) %G%
+ *      @(#)extern.h	5.4 (Berkeley) %G%
  */
 
 #include <sys/cdefs.h>
 
-__BEGIN_DECLS
 void       printjob __P((void));
 void       recvjob __P((void));
-___END_DECLS
