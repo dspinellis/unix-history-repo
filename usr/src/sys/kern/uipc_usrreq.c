0da90ee22b23ec1adf55a4b92a5fe4acb483038e
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1993-02-26 19:27:15 -0800

    avoid duplicate closef's
    SCCS-vsn: sys/kern/uipc_usrreq.c 7.39

--- usr/src/sys/kern/uipc_usrreq.c
+++ usr/src/sys/kern/uipc_usrreq.c
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 1982, 1986 Regents of the University of California.
  *
- *	@(#)uipc_usrreq.c	7.38 (Berkeley) %G%
+ *	@(#)uipc_usrreq.c	7.39 (Berkeley) %G%
  */
 
 #include <sys/param.h>
@@ -772,6 +772,8 @@ unp_discard(fp)
 	struct file *fp;
 {
 
+	if (fp->f_msgcount == 0)
+		return;
 	fp->f_msgcount--;
 	unp_rights--;
 	(void) closef(fp, (struct proc *)NULL);
