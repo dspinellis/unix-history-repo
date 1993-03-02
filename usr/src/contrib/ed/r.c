2f657cb55c059b6222ad5f42127c52f54719af11
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-03-02 01:03:22 -0800

    make it 8-bit clean
    SCCS-vsn: contrib/ed/r.c 5.5

--- usr/src/contrib/ed/r.c
+++ usr/src/contrib/ed/r.c
@@ -9,7 +9,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)r.c	5.4 (Berkeley) %G%";
+static char sccsid[] = "@(#)r.c	5.5 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -39,12 +39,9 @@ r(inputt, errnum)
 	FILE *inputt;
 	int *errnum;
 {
-	struct exec l_magic;
-	struct stat l_s_buf;
 	FILE *l_fp;
 	long l_num;
 	char *l_filename_read=NULL, *l_temp;
-	int l_srv;
 
 	if (filename_flag == 1) {
 		sigspecial++;
@@ -82,36 +79,15 @@ r(inputt, errnum)
 	/*
 	 * Determine if the file can be read.  If not set the help message to
 	 * something descriptive that the user should understand.
+	 * We're now allowing ed to read directory and executable files
+	 * for as much as it can, if there are NULL's in the file it
+	 * is guaranteed to be different since ed doesn't do NULL's.
 	 */
-	if (((l_srv = stat(l_filename_read, &l_s_buf)) == -1) ||
-	    (l_s_buf.st_mode & S_IFDIR)) {
-		if (l_srv == -1)
-			strcpy(help_msg, strerror(errno));
-		else
-			strcpy(help_msg,
-			    "filename is directory, not a text file");
-		printf("?%s\n", l_filename_read);
-		*errnum = 0;
-		return;
-	}
 	if ((l_fp = fopen(l_filename_read, "r")) == NULL) {
 		strcpy(help_msg, "permission lacking to read file");
 		printf("?%s\n", l_filename_read);
 		*errnum = 0;
 		return;
-	}
-	/*
-	 * There is permission to read the file, but if it's an executable
-	 * file of the object code and linked type, we really don't want to
-	 * look at it (according to ed spec's).
-	 */
-	if (fread(&l_magic, sizeof(struct exec), 1, l_fp) != 0) {
-		if (!(N_BADMAG(l_magic))) {
-			strcpy(help_msg, "unable to read executable file");
-			printf("?%s\n", l_filename_read);
-			*errnum = 0;
-			return;
-		}
 	}
 	fseek(l_fp, (off_t)0, 0);
 	if (g_flag == 0)
