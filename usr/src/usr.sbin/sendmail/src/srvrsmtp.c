a80727e0aeff2615357baba1f02af5fffe48eacf
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-03-01 21:31:57 -0800

    have HELO/EHLO announce SIZE option
    SCCS-vsn: usr.sbin/sendmail/src/srvrsmtp.c 6.22

--- usr/src/usr.sbin/sendmail/src/srvrsmtp.c
+++ usr/src/usr.sbin/sendmail/src/srvrsmtp.c
@@ -10,9 +10,9 @@
 
 #ifndef lint
 #ifdef SMTP
-static char sccsid[] = "@(#)srvrsmtp.c	6.21 (Berkeley) %G% (with SMTP)";
+static char sccsid[] = "@(#)srvrsmtp.c	6.22 (Berkeley) %G% (with SMTP)";
 #else
-static char sccsid[] = "@(#)srvrsmtp.c	6.21 (Berkeley) %G% (without SMTP)";
+static char sccsid[] = "@(#)srvrsmtp.c	6.22 (Berkeley) %G% (without SMTP)";
 #endif
 #endif /* not lint */
 
