401a5c02e67f8ab948e0d1338b48dee54308eea1
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-03-02 22:27:53 -0800

    fix some uninitialized variables; <unistd.h> everywhere; logging changes; accept host name inside [...] to avoid MX lookup
    SCCS-vsn: usr.sbin/sendmail/src/version.c 6.32

--- usr/src/usr.sbin/sendmail/src/version.c
+++ usr/src/usr.sbin/sendmail/src/version.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)version.c	6.31 (Berkeley) %G%";
+static char sccsid[] = "@(#)version.c	6.32 (Berkeley) %G%";
 #endif /* not lint */
 
-char	Version[] = "ALPHA-6.31";
+char	Version[] = "ALPHA-6.32";
