6da14acfeb5a1b2d634ee67f053f176146ba44f4
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1993-03-03 02:23:59 -0800

    add proc parameter to net_sysctl
    SCCS-vsn: sys/kern/uipc_domain.c 7.13

--- usr/src/sys/kern/uipc_domain.c
+++ usr/src/sys/kern/uipc_domain.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)uipc_domain.c	7.12 (Berkeley) %G%
+ *	@(#)uipc_domain.c	7.13 (Berkeley) %G%
  */
 
 #include <sys/param.h>
@@ -109,13 +109,14 @@ found:
 	return (maybe);
 }
 
-net_sysctl(name, namelen, oldp, oldlenp, newp, newlen)
+net_sysctl(name, namelen, oldp, oldlenp, newp, newlen, p)
 	int *name;
 	u_int namelen;
 	void *oldp;
 	u_int *oldlenp;
 	void *newp;
 	u_int newlen;
+	struct proc *p;
 {
 	register struct domain *dp;
 	register struct protosw *pr;
