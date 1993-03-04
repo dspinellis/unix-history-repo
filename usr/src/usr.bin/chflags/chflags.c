55c9190ebd3392567df1a88ffcf85dc10912344b
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-03-04 04:35:34 -0800

    use err routines from C library add sappend, schange, uappend, uchange flags, use routine from ls(1) source
    SCCS-vsn: usr.bin/chflags/Makefile 5.2
    SCCS-vsn: usr.bin/chflags/chflags.1 5.5
    SCCS-vsn: usr.bin/chflags/chflags.c 5.4

--- usr/src/usr.bin/chflags/chflags.c
+++ usr/src/usr.bin/chflags/chflags.c
@@ -12,30 +12,21 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)chflags.c	5.3 (Berkeley) %G%";
+static char sccsid[] = "@(#)chflags.c	5.4 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <errno.h>
+
+#include <err.h>
 #include <fts.h>
-#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
-typedef struct cmdset {
-	long clrbits;
-	long setbits;
-} CMDS;
-CMDS cmds;
-int retval;
-
-void	 err __P((const char *, ...));
-void	 error __P((char *));
-int	 getflags __P((CMDS *, int));
-void	*setflags __P((char *));
-void	 usage __P((void));
+u_long	string_to_flags __P((char **, u_long *, u_long *));
+void	usage __P((void));
 
 int
 main(argc, argv)
@@ -44,14 +35,10 @@ main(argc, argv)
 {
 	register FTS *ftsp;
 	register FTSENT *p;
-	register long oflags;
 	register int oct;
-	register char *flags;
-	struct stat sb;
-	void *set;
-	int ch, rflag, hflag, Hflag;
-	char *ep;
-	int fts_options;
+	u_long clear, set;
+	int ch, fts_options, retval, rflag, hflag, Hflag;
+	char *flags, *ep;
 
 	rflag = hflag = Hflag = 0;
 	fts_options = FTS_PHYSICAL;
@@ -81,22 +68,22 @@ main(argc, argv)
 
 	flags = *argv;
 	if (*flags >= '0' && *flags <= '7') {
-		oflags = (int)strtol(flags, &ep, 8);
-                if (oflags < 0 || *ep)
-                        err("invalid flags: %s", flags);
+		set = (int)strtol(flags, &ep, 8);
+                if (set < 0 || *ep)
+                        errx(1, "invalid flags: %s", flags);
+		fts_options |= FTS_NOSTAT;
                 oct = 1;
 	} else {
-		if ((set = setflags(flags)) == NULL)
-                        err("invalid flags: %s", flags);
+		if (string_to_flags(&flags, &set, &clear))
+                        errx(1, "invalid flag: %s", flags);
+		clear = ~clear;
 		oct = 0;
 	}
 
-	retval = 0;
-	if (oct)
-		fts_options |= FTS_NOSTAT;
-	if (!(ftsp = fts_open(++argv, fts_options , 0)))
-		err("%s", strerror(errno));
-	while (p = fts_read(ftsp))
+	if ((ftsp = fts_open(++argv, fts_options , 0)) == NULL)
+		err(1, "");
+
+	for (retval = 0; p = fts_read(ftsp);)
 		switch(p->fts_info) {
 		case FTS_D:
 			if (!rflag)
@@ -105,92 +92,27 @@ main(argc, argv)
 		case FTS_DNR:
 		case FTS_ERR:
 		case FTS_NS:
-			err("%s: %s", p->fts_path, strerror(errno));
+			err(1, "%s", p->fts_path);
 		default:
                         if (p->fts_info == FTS_SL &&
                             !(hflag ||
                             (Hflag && p->fts_level == FTS_ROOTLEVEL)))
 				continue;
-			if (chflags(p->fts_accpath, oct ? oflags :
-			    getflags(set, p->fts_statp->st_flags)))
-				error(p->fts_path);
+			if (oct) {
+				if (!chflags(p->fts_accpath, set))
+					break;
+			} else {
+				p->fts_statp->st_flags |= set;
+				p->fts_statp->st_flags &= clear;
+				if (!chflags(p->fts_accpath,
+				    p->fts_statp->st_flags))
+					break;
+			}
+			warn("%s", p->fts_path);
+			retval = 1;
 			break;
 		}
 	exit(retval);
-}
-
-/*
- * These are analogous to the setmode/getmode routines in the C library.
- */
-void *
-setflags(cp)
-	char *cp;
-{
-	register CMDS *fset;
-	register char *arg;
-
-	fset = &cmds;
-	fset->clrbits = 0;
-	fset->setbits = 0;
-	while (cp) {
-		while ((arg = strsep(&cp, ",")) != NULL && *arg == '\0')
-			/* void */;
-		if (!strcasecmp(arg, "dump"))
-			fset->clrbits |= NODUMP;
-		else if (!strcasecmp(arg, "nodump"))
-			fset->setbits |= NODUMP;
-		else
-			return (NULL);
-	}
-	return (fset);
-}
-
-int
-getflags(fset, oflags)
-	register CMDS *fset;
-	register int oflags;
-{
-
-	oflags &= ~fset->clrbits;
-	oflags |= fset->setbits;
-	return (oflags);
-}
-
-#if __STDC__
-#include <stdarg.h>
-#else
-#include <varargs.h>
-#endif
-
-void
-#if __STDC__
-err(const char *fmt, ...)
-#else
-err(fmt, va_alist)
-	char *fmt;
-        va_dcl
-#endif
-{
-	va_list ap;
-#if __STDC__
-	va_start(ap, fmt);
-#else
-	va_start(ap);
-#endif
-	(void)fprintf(stderr, "chflags: ");
-	(void)vfprintf(stderr, fmt, ap);
-	va_end(ap);
-	(void)fprintf(stderr, "\n");
-	exit(1);
-	/* NOTREACHED */
-}
-
-void
-error(name)
-	char *name;
-{
-	(void)fprintf(stderr, "chflags: %s: %s.\n", name, strerror(errno));
-	retval = 1;
 }
 
 void
