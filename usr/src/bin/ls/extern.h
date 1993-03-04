20de1ad12fb9b3eef205c88a99e0294a6b2d9aca
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-03-04 04:36:44 -0800

    add flags_to_string declaration
    SCCS-vsn: bin/ls/extern.h 5.4

--- usr/src/bin/ls/extern.h
+++ usr/src/bin/ls/extern.h
@@ -4,21 +4,22 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)extern.h	5.3 (Berkeley) %G%
+ *	@(#)extern.h	5.4 (Berkeley) %G%
  */
 
-int acccmp __P((const FTSENT *, const FTSENT *));
-int revacccmp __P((const FTSENT *, const FTSENT *));
-int modcmp __P((const FTSENT *, const FTSENT *));
-int revmodcmp __P((const FTSENT *, const FTSENT *));
-int namecmp __P((const FTSENT *, const FTSENT *));
-int revnamecmp __P((const FTSENT *, const FTSENT *));
-int statcmp __P((const FTSENT *, const FTSENT *));
-int revstatcmp __P((const FTSENT *, const FTSENT *));
+int	 acccmp __P((const FTSENT *, const FTSENT *));
+int	 revacccmp __P((const FTSENT *, const FTSENT *));
+int	 modcmp __P((const FTSENT *, const FTSENT *));
+int	 revmodcmp __P((const FTSENT *, const FTSENT *));
+int	 namecmp __P((const FTSENT *, const FTSENT *));
+int	 revnamecmp __P((const FTSENT *, const FTSENT *));
+int	 statcmp __P((const FTSENT *, const FTSENT *));
+int	 revstatcmp __P((const FTSENT *, const FTSENT *));
 
-void err __P((int, const char *, ...));
-void prcopy __P((char *, char *, int));
-void printcol __P((DISPLAY *));
-void printlong __P((DISPLAY *));
-void printscol __P((DISPLAY *));
-void usage __P((void));
+void	 err __P((int, const char *, ...));
+char	*flags_to_string __P((u_long, char *));
+void	 prcopy __P((char *, char *, int));
+void	 printcol __P((DISPLAY *));
+void	 printlong __P((DISPLAY *));
+void	 printscol __P((DISPLAY *));
+void	 usage __P((void));
