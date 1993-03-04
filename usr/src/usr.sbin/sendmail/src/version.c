c092f71a494f7a49e0ee5675437978593d0bc2bf
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-03-03 18:27:38 -0800

    allow prescan to return a pointer to an empty list; clean up some prescan error reporting; changes to getcanonname: don't assume that T_ANY returns everything (it's not T_ALL, and caching can confuse it).
    SCCS-vsn: usr.sbin/sendmail/src/version.c 6.33

--- usr/src/usr.sbin/sendmail/src/version.c
+++ usr/src/usr.sbin/sendmail/src/version.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)version.c	6.32 (Berkeley) %G%";
+static char sccsid[] = "@(#)version.c	6.33 (Berkeley) %G%";
 #endif /* not lint */
 
-char	Version[] = "ALPHA-6.32";
+char	Version[] = "ALPHA-6.33";
