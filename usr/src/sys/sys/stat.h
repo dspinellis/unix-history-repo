44e4f1374e619cc988d8d54b1e3962ad7dfee460
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-03-04 17:53:23 -0800

    I messed up the #ifdef's for _POSIX_SOURCE
    SCCS-vsn: sys/sys/stat.h 7.18

--- usr/src/sys/sys/stat.h
+++ usr/src/sys/sys/stat.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)stat.h	7.17 (Berkeley) %G%
+ *	@(#)stat.h	7.18 (Berkeley) %G%
  */
 
 #include <sys/time.h>
@@ -77,11 +77,8 @@ struct stat {
 #define	S_IWOTH	0000002			/* W for other */
 #define	S_IXOTH	0000001			/* X for other */
 
-					/* 0666 */
-#define	DEFFILEMODE	(S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP|S_IROTH|S_IWOTH)
-
 #ifndef _POSIX_SOURCE
-#define	S_IFMT	 0170000		/* type of file */
+#define	S_IFMT	 0170000		/* type of file mask */
 #define	S_IFIFO	 0010000		/* named pipe (fifo) */
 #define	S_IFCHR	 0020000		/* character special */
 #define	S_IFDIR	 0040000		/* directory */
@@ -89,10 +86,8 @@ struct stat {
 #define	S_IFREG	 0100000		/* regular */
 #define	S_IFLNK	 0120000		/* symbolic link */
 #define	S_IFSOCK 0140000		/* socket */
-
 #define	S_ISVTX	 0001000		/* save swapped text even after use */
-
-#define S_BLKSIZE	512		/* block size used in the stat struct */
+#endif
 
 #define	S_ISDIR(m)	((m & 0170000) == 0040000)	/* directory */
 #define	S_ISCHR(m)	((m & 0170000) == 0020000)	/* char special */
@@ -103,6 +98,12 @@ struct stat {
 #define	S_ISLNK(m)	((m & 0170000) == 0120000)	/* symbolic link */
 #define	S_ISSOCK(m)	((m & 0170000) == 0140000)	/* socket */
 #endif
+
+#ifndef _POSIX_SOURCE
+							/* 0666 */
+#define	DEFFILEMODE	(S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP|S_IROTH|S_IWOTH)
+
+#define S_BLKSIZE	512		/* block size used in the stat struct */
 
 /*
  * Definitions of flags stored in file flags word.
