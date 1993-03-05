6871393a5a9dcd90fffc674a2c559868b3a2f411
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-03-05 15:28:22 -0800

    fix excess # args to syslog; fix pid log file open; consider addrs "different" if uids differ; exit in main on bad args; fix null ptr refs through RealHostName; diagnose null return from hostsignature
    SCCS-vsn: usr.sbin/sendmail/src/version.c 6.34

--- usr/src/usr.sbin/sendmail/src/version.c
+++ usr/src/usr.sbin/sendmail/src/version.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)version.c	6.33 (Berkeley) %G%";
+static char sccsid[] = "@(#)version.c	6.34 (Berkeley) %G%";
 #endif /* not lint */
 
-char	Version[] = "ALPHA-6.33";
+char	Version[] = "ALPHA-6.34";
