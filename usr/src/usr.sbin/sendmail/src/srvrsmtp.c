1e496e5182b107516caf60b80899d402f9e6d126
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-03-06 17:32:10 -0800

    log numeric IP addr in Received: lines per 1123 section 5.2.8
    SCCS-vsn: usr.sbin/sendmail/src/srvrsmtp.c 6.24

--- usr/src/usr.sbin/sendmail/src/srvrsmtp.c
+++ usr/src/usr.sbin/sendmail/src/srvrsmtp.c
@@ -10,9 +10,9 @@
 
 #ifndef lint
 #ifdef SMTP
-static char sccsid[] = "@(#)srvrsmtp.c	6.23 (Berkeley) %G% (with SMTP)";
+static char sccsid[] = "@(#)srvrsmtp.c	6.24 (Berkeley) %G% (with SMTP)";
 #else
-static char sccsid[] = "@(#)srvrsmtp.c	6.23 (Berkeley) %G% (without SMTP)";
+static char sccsid[] = "@(#)srvrsmtp.c	6.24 (Berkeley) %G% (without SMTP)";
 #endif
 #endif /* not lint */
 
@@ -109,6 +109,7 @@ smtp(e)
 	auto char *delimptr;
 	char inp[MAXLINE];
 	char cmdbuf[MAXLINE];
+	char hostbuf[MAXNAME];
 	extern char Version[];
 	extern char *macvalue();
 	extern ADDRESS *recipient();
@@ -205,15 +206,16 @@ smtp(e)
 					MyHostName);
 				break;
 			}
+			(void) strcpy(hostbuf, p);
+			(void) strcat(hostbuf, " (");
+			(void) strcat(hostbuf, inet_ntoa(RealHostAddr.sin_addr));
 			if (strcasecmp(p, RealHostName) != 0)
 			{
-				char hostbuf[MAXNAME];
-
-				(void) sprintf(hostbuf, "%s (%s)", p, RealHostName);
-				sendinghost = newstr(hostbuf);
+				(void) strcat(hostbuf, "; ");
+				(void) strcat(hostbuf, RealHostName);
 			}
-			else
-				sendinghost = newstr(p);
+			(void) strcat(hostbuf, ")");
+			sendinghost = newstr(hostbuf);
 
 			/* send ext. message -- old systems must ignore */
 			message("250-%s Hello %s, pleased to meet you",
