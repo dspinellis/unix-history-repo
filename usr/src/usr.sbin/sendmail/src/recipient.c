c1f57005df865b5c1cf5cd53acf86b732f551107
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-03-06 18:52:59 -0800

    don't automatically print errno in usrerr -- it can be too misleading
    SCCS-vsn: usr.sbin/sendmail/src/sysexits.c 6.2
    SCCS-vsn: usr.sbin/sendmail/src/recipient.c 6.24
    SCCS-vsn: usr.sbin/sendmail/src/err.c 6.7
    SCCS-vsn: usr.sbin/sendmail/src/deliver.c 6.39

--- usr/src/usr.sbin/sendmail/src/recipient.c
+++ usr/src/usr.sbin/sendmail/src/recipient.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)recipient.c	6.23 (Berkeley) %G%";
+static char sccsid[] = "@(#)recipient.c	6.24 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -600,7 +600,7 @@ include(fname, forwarding, ctladdr, sendq, e)
 		int ret = errno;
 
 		clrevent(ev);
-		usrerr("550 Cannot open %s", fname);
+		usrerr("550 Cannot open %s: %s", fname, errstring(ret));
 		return ret;
 	}
 
--- usr/src/usr.sbin/sendmail/src/sysexits.c
+++ usr/src/usr.sbin/sendmail/src/sysexits.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)sysexits.c	6.1 (Berkeley) %G%";
+static char sccsid[] = "@(#)sysexits.c	6.2 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sysexits.h>
@@ -18,45 +18,19 @@ static char sccsid[] = "@(#)sysexits.c	6.1 (Berkeley) %G%";
 char *SysExMsg[] = {
 	/* 64 USAGE */		"500 Bad usage",
 	/* 65 DATAERR */	"501 Data format error",
-	/* 66 NOINPUT */	"550 Cannot open input",
+	/* 66 NOINPUT */	"550 Cannot open input: %s",
 	/* 67 NOUSER */		"550 User unknown",
 	/* 68 NOHOST */		"550 Host unknown",
 	/* 69 UNAVAILABLE */	"554 Service unavailable",
-	/* 70 SOFTWARE */	"554 Internal error",
-	/* 71 OSERR */		"451 Operating system error",
-	/* 72 OSFILE */		"554 System file missing",
-	/* 73 CANTCREAT */	"550 Can't create output",
-	/* 74 IOERR */		"451 I/O error",
+	/* 70 SOFTWARE */	"554 Internal error: %s",
+	/* 71 OSERR */		"451 Operating system error: %s",
+	/* 72 OSFILE */		"554 System file missing: %s",
+	/* 73 CANTCREAT */	"550 Can't create output: %s",
+	/* 74 IOERR */		"451 I/O error: %s",
 	/* 75 TEMPFAIL */	"250 Deferred",
 	/* 76 PROTOCOL */	"554 Remote protocol error",
-	/* 77 NOPERM */		"550 Insufficient permission",
+	/* 77 NOPERM */		"550 Insufficient permission: %s",
 	/* 78 CONFIG */		"554 Local configuration error",
 };
 
 int N_SysEx = sizeof(SysExMsg) / sizeof(SysExMsg[0]);
-
-/*
- *  STATSTRING -- return string corresponding to an error status
- *
- *	Parameters:
- *		stat -- the status to decode.
- *
- *	Returns:
- *		The string corresponding to that status
- *
- *	Side Effects:
- *		none.
- */
-char *
-statstring(stat)
-	int stat;
-{
-	static char ebuf[50];
-
-	stat -= EX__BASE;
-	if (stat < 0 || stat >= N_SysEx) {
-		(void)sprintf(ebuf, "554 Unknown status %d", stat + EX__BASE);
-		return(ebuf);
-	}
-	return(SysExMsg[stat]);
-}
