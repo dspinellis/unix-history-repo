568d597537cf6da6c8fd0839418015ac6f5ddb66
Chris Torek <torek@ucbvax.Berkeley.EDU>
1993-03-07 07:42:02 -0800

    stop if we reach out of band mark while peeking; otherwise we will go into an infinite loop copying 0 bytes each time.  (From Tom Truscott <trt@cs.duke.edu>)
    SCCS-vsn: sys/kern/uipc_socket.c 7.39

--- usr/src/sys/kern/uipc_socket.c
+++ usr/src/sys/kern/uipc_socket.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)uipc_socket.c	7.38 (Berkeley) %G%
+ *	@(#)uipc_socket.c	7.39 (Berkeley) %G%
  */
 
 #include <sys/param.h>
@@ -682,8 +682,11 @@ dontblock:
 					so->so_state |= SS_RCVATMARK;
 					break;
 				}
-			} else
+			} else {
 				offset += len;
+				if (offset == so->so_oobmark)
+					break;
+			}
 		}
 		if (flags & MSG_EOR)
 			break;
