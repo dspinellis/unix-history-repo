1a9c0f8f40eee3437d83e6e226f60416fbb3ce3f
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-03-09 02:17:29 -0800

    allow local mailer to specify $@host; don't add e_fromdomain on sender addresses (conflicts with owner- code); include numeric IP address in Received: lines per 1123; use transienterror in more places; don't have usrerr autoprint errno; undo patch allowing prescan to return null argv; etc.
    SCCS-vsn: usr.sbin/sendmail/src/version.c 6.36

--- usr/src/usr.sbin/sendmail/src/version.c
+++ usr/src/usr.sbin/sendmail/src/version.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)version.c	6.35 (Berkeley) %G%";
+static char sccsid[] = "@(#)version.c	6.36 (Berkeley) %G%";
 #endif /* not lint */
 
-char	Version[] = "ALPHA-6.35";
+char	Version[] = "ALPHA-6.36";
