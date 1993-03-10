e346ebefe1c62c2a888128026ade979255d4a3d1
Kazumasa Utashiro <utashiro@ucbvax.Berkeley.EDU>
1993-03-10 07:56:07 -0800

    cleanup header and machine dependency.
    SCCS-vsn: sys/news3400/news3400/swapgeneric.c 7.3

--- usr/src/sys/news3400/news3400/swapgeneric.c
+++ usr/src/sys/news3400/news3400/swapgeneric.c
@@ -8,10 +8,8 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)swapgeneric.c	7.2 (Berkeley) %G%
+ *	@(#)swapgeneric.c	7.3 (Berkeley) %G%
  */
-
-#include <machine/fix_machine_type.h>
 
 #include <sysparam.h>
 #include <sysconf.h>
