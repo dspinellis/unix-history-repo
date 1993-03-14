f644e5a82699c55a676c438b69a8229cd648646a
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-03-14 06:50:43 -0800

    fix SysExMsg printing; setproctitle on getmxrr
    SCCS-vsn: usr.sbin/sendmail/src/deliver.c 6.41

--- usr/src/usr.sbin/sendmail/src/deliver.c
+++ usr/src/usr.sbin/sendmail/src/deliver.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)deliver.c	6.40 (Berkeley) %G%";
+static char sccsid[] = "@(#)deliver.c	6.41 (Berkeley) %G%";
 #endif /* not lint */
 
 #include "sendmail.h"
@@ -974,6 +974,7 @@ giveresponse(stat, m, mci, e)
 	extern int h_errno;
 #endif
 	char buf[MAXLINE];
+	extern char *errstring();
 
 	/*
 	**  Compute status message from code.
@@ -990,23 +991,15 @@ giveresponse(stat, m, mci, e)
 	}
 	else if (stat == EX_TEMPFAIL)
 	{
-		(void) strcpy(buf, SysExMsg[i]);
+		(void) strcpy(buf, SysExMsg[i] + 1);
 #ifdef NAMED_BIND
 		if (h_errno == TRY_AGAIN)
-		{
-			extern char *errstring();
-
 			statmsg = errstring(h_errno+MAX_ERRNO);
-		}
 		else
 #endif
 		{
 			if (errno != 0)
-			{
-				extern char *errstring();
-
 				statmsg = errstring(errno);
-			}
 			else
 			{
 #ifdef SMTP
@@ -1028,6 +1021,11 @@ giveresponse(stat, m, mci, e)
 	else
 	{
 		statmsg = SysExMsg[i];
+		if (*statmsg++ == ':')
+		{
+			(void) sprintf(buf, "%s: %s", statmsg, errstring(errno));
+			statmsg = buf;
+		}
 	}
 
 	/*
