a332213cefc4e0387ecdfedc11de6beac27a1722
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-03-14 06:52:27 -0800

    change "received from" logging on internal error response
    SCCS-vsn: usr.sbin/sendmail/src/headers.c 6.18

--- usr/src/usr.sbin/sendmail/src/headers.c
+++ usr/src/usr.sbin/sendmail/src/headers.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)headers.c	6.17 (Berkeley) %G%";
+static char sccsid[] = "@(#)headers.c	6.18 (Berkeley) %G%";
 #endif /* not lint */
 
 # include <errno.h>
@@ -323,7 +323,9 @@ eatheader(e, queuejob)
 		char hbuf[MAXNAME];
 		char sbuf[MAXLINE];
 
-		if (RealHostName[0] == '[')
+		if (bitset(EF_RESPONSE, e->e_flags))
+			name = "[RESPONSE]";
+		else if (RealHostName[0] == '[')
 			name = RealHostName;
 		else
 		{
