d4c3b835983a755b71cf005a27e5f975168ca093
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-03-14 16:31:26 -0800

    back out setproctitle changes; they trash argv before everything has been properly saved
    SCCS-vsn: usr.sbin/sendmail/src/daemon.c 6.18
    SCCS-vsn: usr.sbin/sendmail/src/deliver.c 6.42
    SCCS-vsn: usr.sbin/sendmail/src/conf.c 6.35

--- usr/src/usr.sbin/sendmail/src/conf.c
+++ usr/src/usr.sbin/sendmail/src/conf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)conf.c	6.34 (Berkeley) %G%";
+static char sccsid[] = "@(#)conf.c	6.35 (Berkeley) %G%";
 #endif /* not lint */
 
 # include <sys/ioctl.h>
--- usr/src/usr.sbin/sendmail/src/deliver.c
+++ usr/src/usr.sbin/sendmail/src/deliver.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)deliver.c	6.41 (Berkeley) %G%";
+static char sccsid[] = "@(#)deliver.c	6.42 (Berkeley) %G%";
 #endif /* not lint */
 
 #include "sendmail.h"
