2c4e70d7955cb6842308765edf023d5cfdbaf2e1
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-03-17 21:02:04 -0800

    minor glitch -- add paren
    SCCS-vsn: usr.sbin/sendmail/src/conf.c 6.37

--- usr/src/usr.sbin/sendmail/src/conf.c
+++ usr/src/usr.sbin/sendmail/src/conf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)conf.c	6.36 (Berkeley) %G%";
+static char sccsid[] = "@(#)conf.c	6.37 (Berkeley) %G%";
 #endif /* not lint */
 
 # include <sys/ioctl.h>
