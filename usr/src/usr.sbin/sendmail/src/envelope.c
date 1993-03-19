6958b272e7b42f59f031171dc5dd561658e397d8
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-03-19 03:37:08 -0800

    leading blanks in aliases can cause "unparsable" from addresses
    SCCS-vsn: usr.sbin/sendmail/src/envelope.c 6.24

--- usr/src/usr.sbin/sendmail/src/envelope.c
+++ usr/src/usr.sbin/sendmail/src/envelope.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)envelope.c	6.23 (Berkeley) %G%";
+static char sccsid[] = "@(#)envelope.c	6.24 (Berkeley) %G%";
 #endif /* not lint */
 
 #include "sendmail.h"
@@ -422,7 +422,7 @@ setsender(from, e, delimptr, internal)
 	register char **pvp;
 	char *realname = NULL;
 	register struct passwd *pw;
-	char *delimchar = NULL;
+	char delimchar;
 	char buf[MAXNAME];
 	char pvpbuf[PSBUFSIZE];
 	extern struct passwd *getpwnam();
@@ -450,8 +450,9 @@ setsender(from, e, delimptr, internal)
 /*
 	SuprErrs = TRUE;
 */
+	delimchar = internal ? '\0' : ' ';
 	if (from == NULL ||
-	    parseaddr(from, &e->e_from, 1, ' ', delimptr, e) == NULL)
+	    parseaddr(from, &e->e_from, 1, delimchar, delimptr, e) == NULL)
 	{
 		/* log garbage addresses for traceback */
 			syslog(LOG_ERR, "Unparseable user %s wants to be %s",
