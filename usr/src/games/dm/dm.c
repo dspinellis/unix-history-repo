00b01b755150a4e1309a007b3631ba6b40e0f4c1
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-03-22 13:53:36 -0800

    lint
    SCCS-vsn: games/arithmetic/arithmetic.c 5.6
    SCCS-vsn: games/dm/dm.c 5.17

--- usr/src/games/dm/dm.c
+++ usr/src/games/dm/dm.c
@@ -12,18 +12,23 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)dm.c	5.16 (Berkeley) %G%";
+static char sccsid[] = "@(#)dm.c	5.17 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/param.h>
 #include <sys/file.h>
 #include <sys/time.h>
 #include <sys/resource.h>
-#include <pwd.h>
-#include <utmp.h>
-#include <nlist.h>
-#include <stdio.h>
+
 #include <ctype.h>
+#include <nlist.h>
+#include <pwd.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+#include <utmp.h>
+
 #include "pathnames.h"
 
 extern int errno;
@@ -32,13 +37,12 @@ static int	priority = 0;		/* priority game runs at */
 static char	*game,			/* requested game */
 		*gametty;		/* from tty? */
 
-/*ARGSUSED*/
+int
 main(argc, argv)
 	int argc;
-	char **argv;
+	char *argv[];
 {
-	char *cp, *rindex(), *ttyname();
-	time_t time();
+	char *cp;
 
 	nogamefile();
 	game = (cp = rindex(*argv, '/')) ? ++cp : *argv;
@@ -63,7 +67,7 @@ main(argc, argv)
 play(args)
 	char **args;
 {
-	char pbuf[MAXPATHLEN], *strcpy(), *strerror();
+	char pbuf[MAXPATHLEN];
 
 	(void)strcpy(pbuf, _PATH_HIDE);
 	(void)strcpy(pbuf + sizeof(_PATH_HIDE) - 1, game);
@@ -150,7 +154,6 @@ c_tty(tty)
 {
 	static int first = 1;
 	static char *p_tty;
-	char *rindex();
 
 	if (first) {
 		p_tty = rindex(gametty, '/');
@@ -272,11 +275,10 @@ hour(h)
  */
 logfile()
 {
-	struct passwd *pw, *getpwuid();
+	struct passwd *pw;
 	FILE *lp;
 	uid_t uid;
 	int lock_cnt;
-	char *ctime();
 
 	if (lp = fopen(_PATH_LOG, "a")) {
 		for (lock_cnt = 0;; ++lock_cnt) {
