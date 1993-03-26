49299ab058a48da15e7f9f8b4d7193c4b133c7eb
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-03-25 17:10:40 -0800

    catch "unable to create locked temp file" condition
    SCCS-vsn: usr.sbin/sendmail/src/queue.c 6.39

--- usr/src/usr.sbin/sendmail/src/queue.c
+++ usr/src/usr.sbin/sendmail/src/queue.c
@@ -10,9 +10,9 @@
 
 #ifndef lint
 #ifdef QUEUE
-static char sccsid[] = "@(#)queue.c	6.38 (Berkeley) %G% (with queueing)";
+static char sccsid[] = "@(#)queue.c	6.39 (Berkeley) %G% (with queueing)";
 #else
-static char sccsid[] = "@(#)queue.c	6.38 (Berkeley) %G% (without queueing)";
+static char sccsid[] = "@(#)queue.c	6.39 (Berkeley) %G% (without queueing)";
 #endif
 #endif /* not lint */
 
@@ -99,6 +99,7 @@ queueup(df)
 			{
 				if (errno == EEXIST)
 					continue;
+notemp:
 				syserr("!queueup: cannot create temp file %s", tf);
 			}
 
@@ -106,7 +107,10 @@ queueup(df)
 				break;
 
 			close(fd);
+			sleep(i);
 		}
+		if (fd < 0)
+			goto notemp;
 
 		tfp = fdopen(fd, "w");
 	}
