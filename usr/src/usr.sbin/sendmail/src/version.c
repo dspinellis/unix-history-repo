0bc520681546c9945d8f80b49800cdc1c45303b2
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-03-26 16:20:35 -0800

    fix HELO problem in srvrsmtp (HELO was required); fall back to other MX records if error occurs on MAIL or DATA; prototype portability; change $~ to just match one word; fix some problems with multi-word matches
    SCCS-vsn: usr.sbin/sendmail/src/version.c 6.46

--- usr/src/usr.sbin/sendmail/src/version.c
+++ usr/src/usr.sbin/sendmail/src/version.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)version.c	6.45 (Berkeley) %G%";
+static char sccsid[] = "@(#)version.c	6.46 (Berkeley) %G%";
 #endif /* not lint */
 
-char	Version[] = "ALPHA-6.45";
+char	Version[] = "ALPHA-6.46";
