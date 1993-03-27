835dcfcaec6000d0dc9716b3ee7568efad41a9cb
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-03-26 16:06:45 -0800

    MATCHZERO doesn't assign a $N value
    SCCS-vsn: usr.sbin/sendmail/src/readcf.c 6.22

--- usr/src/usr.sbin/sendmail/src/readcf.c
+++ usr/src/usr.sbin/sendmail/src/readcf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)readcf.c	6.21 (Berkeley) %G%";
+static char sccsid[] = "@(#)readcf.c	6.22 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -241,7 +241,6 @@ readcf(cfname)
 					  case MATCHONE:
 					  case MATCHCLASS:
 					  case MATCHNCLASS:
-					  case CANONHOST:
 						nfuzzy++;
 						break;
 
