bf26980dd2a814c669c4be0ae352cda5ae644ece
Ralph Campbell <ralph@ucbvax.Berkeley.EDU>
1993-03-28 00:43:57 -0800

    remove printf's so boot prog will fit into 15 sectors.
    SCCS-vsn: sys/pmax/stand/boot.c 7.7

--- usr/src/sys/pmax/stand/boot.c
+++ usr/src/sys/pmax/stand/boot.c
@@ -7,7 +7,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)boot.c	7.6 (Berkeley) %G%
+ *	@(#)boot.c	7.7 (Berkeley) %G%
  */
 
 #include <sys/param.h>
@@ -30,7 +30,6 @@ main(argc, argv)
 {
 	register char *cp;
 	int ask, entry;
-	char *boot = "boot";
 
 #ifdef JUSTASK
 	ask = 1;
@@ -96,39 +95,30 @@ loadfile(fname)
 	struct exec aout;
 
 	if ((fd = open(fname, 0)) < 0) {
-		printf("Can't open '%s'\n", fname);
 		goto err;
 	}
 
 	/* read the exec header */
 	i = read(fd, (char *)&aout, sizeof(aout));
 	if (i != sizeof(aout)) {
-		printf("No a.out header\n");
 		goto cerr;
 	} else if (aout.a_magic != OMAGIC) {
-		printf("A.out? magic 0%o size %d+%d+%d\n", aout.a_magic,
-			aout.a_text, aout.a_data, aout.a_bss);
 		goto cerr;
 	}
 
 	/* read the code and initialized data */
 	printf("Size: %d+%d", aout.a_text, aout.a_data);
 	if (lseek(fd, (off_t)N_TXTOFF(aout), 0) < 0) {
-		printf("\nSeek error\n");
 		goto cerr;
 	}
 	i = aout.a_text + aout.a_data;
-#ifndef TEST
 	n = read(fd, (char *)aout.a_entry, i);
-#else
-	n = i;
-#endif
+#ifndef SMALL
 	(void) close(fd);
+#endif
 	if (n < 0) {
-		printf("\nRead error\n");
 		goto err;
 	} else if (n != i) {
-		printf("\nShort read (%d)\n", n);
 		goto err;
 	}
 
@@ -139,7 +129,10 @@ loadfile(fname)
 	return ((int)aout.a_entry);
 
 cerr:
+#ifndef SMALL
 	(void) close(fd);
+#endif
 err:
+	printf("Can't boot '%s'\n", fname);
 	return (-1);
 }
