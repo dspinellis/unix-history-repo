87e4deb4bf2d924c39271eb9a9b328db709eaea5
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-03-29 18:49:22 -0800

    add FallBackMX
    SCCS-vsn: usr.sbin/sendmail/src/sendmail.h 6.39

--- usr/src/usr.sbin/sendmail/src/sendmail.h
+++ usr/src/usr.sbin/sendmail/src/sendmail.h
@@ -5,7 +5,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)sendmail.h	6.38 (Berkeley) %G%
+ *	@(#)sendmail.h	6.39 (Berkeley) %G%
  */
 
 /*
@@ -15,7 +15,7 @@
 # ifdef _DEFINE
 # define EXTERN
 # ifndef lint
-static char SmailSccsId[] =	"@(#)sendmail.h	6.38		%G%";
+static char SmailSccsId[] =	"@(#)sendmail.h	6.39		%G%";
 # endif lint
 # else /*  _DEFINE */
 # define EXTERN extern
