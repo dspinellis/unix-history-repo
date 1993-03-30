edff83f9fb5a6779730860c33baa61eebf35568c
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-03-29 19:42:22 -0800

    make heuristic on when -d doesn't have an argument more like old behaviour
    SCCS-vsn: usr.sbin/sendmail/src/main.c 6.49

--- usr/src/usr.sbin/sendmail/src/main.c
+++ usr/src/usr.sbin/sendmail/src/main.c
@@ -13,7 +13,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)main.c	6.48 (Berkeley) %G%";
+static char sccsid[] = "@(#)main.c	6.49 (Berkeley) %G%";
 #endif /* not lint */
 
 #define	_DEFINE
@@ -1407,7 +1407,7 @@ obsolete(argv)
 
 		/* if -d doesn't have an argument, use 0-99.1 */
 		if (ap[1] == 'd' && ap[2] == '\0' &&
-		    (argv[1] == NULL || argv[1][0] == '-'))
+		    (argv[1] == NULL || !isdigit(argv[1][0])))
 			*argv = "-d0-99.1";
 	}
 }
