48c8624688ebfdbbe8f9d669e7701935ae3ada29
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-03-30 22:49:36 -0800

    attempt to get better handling of recipients going
    SCCS-vsn: usr.sbin/sendmail/src/srvrsmtp.c 6.35.1.1

--- usr/src/usr.sbin/sendmail/src/srvrsmtp.c
+++ usr/src/usr.sbin/sendmail/src/srvrsmtp.c
@@ -10,9 +10,9 @@
 
 #ifndef lint
 #ifdef SMTP
-static char sccsid[] = "@(#)srvrsmtp.c	6.35 (Berkeley) %G% (with SMTP)";
+static char sccsid[] = "@(#)srvrsmtp.c	6.35.1.1 (Berkeley) %G% (with SMTP)";
 #else
-static char sccsid[] = "@(#)srvrsmtp.c	6.35 (Berkeley) %G% (without SMTP)";
+static char sccsid[] = "@(#)srvrsmtp.c	6.35.1.1 (Berkeley) %G% (without SMTP)";
 #endif
 #endif /* not lint */
 
@@ -372,7 +372,6 @@ smtp(e)
 			}
 			QuickAbort = TRUE;
 			LogUsrErrs = TRUE;
-
 			p = skipword(p, "to");
 			if (p == NULL)
 				break;
@@ -380,6 +379,8 @@ smtp(e)
 			if (a == NULL)
 				break;
 			a->q_flags |= QPRIMARY;
+			if (!Verbose)
+				e->e_flags |= EF_VRFYONLY;
 			a = recipient(a, &e->e_sendqueue, e);
 			if (Errors != 0)
 				break;
@@ -441,17 +442,23 @@ smtp(e)
 			id = e->e_id;
 
 			/* send to all recipients */
-			sendall(e, SM_DEFAULT);
+			sendall(e, Verbose ? SM_DELIVER : SM_QUEUE);
 			e->e_to = NULL;
 
 			/* save statistics */
 			markstats(e, (ADDRESS *) NULL);
+
+			unlockqueue(e);
 
 			/* issue success if appropriate and reset */
 			if (Errors == 0 || HoldErrs)
 				message("250 %s Message accepted for delivery", id);
 			else
 				e->e_flags &= ~EF_FATALERRS;
+
+			/* now make it really happen */
+			if (!Verbose && e->e_sendmode != SM_QUEUE)
+				dowork(id, TRUE, e);
 
 			/* if in a child, pop back to our parent */
 			if (InChild)
