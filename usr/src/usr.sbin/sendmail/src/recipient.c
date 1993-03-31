d27ddc7eeec56733280d0157bbc2ace1e04fad18
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-03-30 19:00:59 -0800

    mark include() address as verified for possible later expansion
    SCCS-vsn: usr.sbin/sendmail/src/recipient.c 6.32

--- usr/src/usr.sbin/sendmail/src/recipient.c
+++ usr/src/usr.sbin/sendmail/src/recipient.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)recipient.c	6.31 (Berkeley) %G%";
+static char sccsid[] = "@(#)recipient.c	6.32 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -694,6 +694,7 @@ include(fname, forwarding, ctladdr, sendq, e)
 	if (bitset(EF_VRFYONLY, e->e_flags))
 	{
 		/* don't do any more now */
+		ctladdr->q_flags |= QVERIFIED;
 		xfclose(fp, "include", fname);
 		return 0;
 	}
