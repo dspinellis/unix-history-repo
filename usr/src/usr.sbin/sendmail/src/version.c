2556612cf3861d367f7affc3d220f11dbb6f6c4a
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-03-30 22:56:52 -0800

    fix incorrect infinite loop diagnosis & improve diagnostics; fix bogus CurHostAddr printing; don't warn about -f if setting sender to yourself; "probe" SMTP connections using RSET instead of NOOP
    SCCS-vsn: usr.sbin/sendmail/src/version.c 6.48

--- usr/src/usr.sbin/sendmail/src/version.c
+++ usr/src/usr.sbin/sendmail/src/version.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)version.c	6.47 (Berkeley) %G%";
+static char sccsid[] = "@(#)version.c	6.48 (Berkeley) %G%";
 #endif /* not lint */
 
-char	Version[] = "ALPHA-6.47";
+char	Version[] = "ALPHA-6.48";
