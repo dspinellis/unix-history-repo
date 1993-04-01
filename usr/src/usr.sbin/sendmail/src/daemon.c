2c8d140524c91f8c55b9d69f574270403b9840f5
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-04-01 15:06:19 -0800

    set CurHostName any time you create a connection to that host
    SCCS-vsn: usr.sbin/sendmail/src/daemon.c 6.30

--- usr/src/usr.sbin/sendmail/src/daemon.c
+++ usr/src/usr.sbin/sendmail/src/daemon.c
@@ -12,9 +12,9 @@
 
 #ifndef lint
 #ifdef DAEMON
-static char sccsid[] = "@(#)daemon.c	6.29 (Berkeley) %G% (with daemon mode)";
+static char sccsid[] = "@(#)daemon.c	6.30 (Berkeley) %G% (with daemon mode)";
 #else
-static char sccsid[] = "@(#)daemon.c	6.29 (Berkeley) %G% (without daemon mode)";
+static char sccsid[] = "@(#)daemon.c	6.30 (Berkeley) %G% (without daemon mode)";
 #endif
 #endif /* not lint */
 
@@ -501,6 +501,7 @@ makeconnection(host, port, outfile, infile, usesecureport)
 #endif
 	errno = 0;
 	bzero(&CurHostAddr, sizeof CurHostAddr);
+	CurHostName = host;
 
 	if (host[0] == '[')
 	{
