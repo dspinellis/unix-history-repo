ea48625010aecb6fbcb73bdf9a3501657cb33563
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-04-01 20:34:21 -0800

    diagnose incorrect privacy flags; quote backslashes in _phrase_; some FallBackMX fixes; don't ignore rest of queue if one job is locked; always respond "quickly" to RCPT; accept email for mailing lists with errors on the list
    SCCS-vsn: usr.sbin/sendmail/src/version.c 6.49

--- usr/src/usr.sbin/sendmail/src/version.c
+++ usr/src/usr.sbin/sendmail/src/version.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)version.c	6.48 (Berkeley) %G%";
+static char sccsid[] = "@(#)version.c	6.49 (Berkeley) %G%";
 #endif /* not lint */
 
-char	Version[] = "ALPHA-6.48";
+char	Version[] = "ALPHA-6.49";
