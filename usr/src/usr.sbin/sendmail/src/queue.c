a33015a90f4ae4759cbc4546d5ee9314da5690ad
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-04-01 20:54:19 -0800

    do immediate requeue if expanding aliases during queue run (from SMTP)
    SCCS-vsn: usr.sbin/sendmail/src/srvrsmtp.c 6.37
    SCCS-vsn: usr.sbin/sendmail/src/queue.c 6.43

--- usr/src/usr.sbin/sendmail/src/queue.c
+++ usr/src/usr.sbin/sendmail/src/queue.c
@@ -10,9 +10,9 @@
 
 #ifndef lint
 #ifdef QUEUE
-static char sccsid[] = "@(#)queue.c	6.42 (Berkeley) %G% (with queueing)";
+static char sccsid[] = "@(#)queue.c	6.43 (Berkeley) %G% (with queueing)";
 #else
-static char sccsid[] = "@(#)queue.c	6.42 (Berkeley) %G% (without queueing)";
+static char sccsid[] = "@(#)queue.c	6.43 (Berkeley) %G% (without queueing)";
 #endif
 #endif /* not lint */
 
@@ -394,7 +394,7 @@ runqueue(forkflag)
 			continue;
 		}
 
-		dowork(w->w_name + 2, ForkQueueRuns, e);
+		dowork(w->w_name + 2, ForkQueueRuns, FALSE, e);
 		free(w->w_name);
 		free((char *) w);
 	}
@@ -660,6 +660,8 @@ workcmpf(a, b)
 **	Parameters:
 **		id -- the ID of the job to run.
 **		forkflag -- if set, run this in background.
+**		requeueflag -- if set, reinstantiate the queue quickly.
+**			This is used when expanding aliases in the queue.
 **		e - the envelope in which to run it.
 **
 **	Returns:
@@ -669,9 +671,10 @@ workcmpf(a, b)
 **		The work request is satisfied if possible.
 */
 
-dowork(id, forkflag, e)
+dowork(id, forkflag, requeueflag, e)
 	char *id;
 	bool forkflag;
+	bool requeueflag;
 	register ENVELOPE *e;
 {
 	register int i;
