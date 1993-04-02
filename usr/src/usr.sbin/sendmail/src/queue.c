cc8b1f688dcb545a79ed0f673953e359cabb48c1
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-04-01 22:04:35 -0800

    free work queue entry even if skipping the job
    SCCS-vsn: usr.sbin/sendmail/src/queue.c 6.45

--- usr/src/usr.sbin/sendmail/src/queue.c
+++ usr/src/usr.sbin/sendmail/src/queue.c
@@ -10,9 +10,9 @@
 
 #ifndef lint
 #ifdef QUEUE
-static char sccsid[] = "@(#)queue.c	6.44 (Berkeley) %G% (with queueing)";
+static char sccsid[] = "@(#)queue.c	6.45 (Berkeley) %G% (with queueing)";
 #else
-static char sccsid[] = "@(#)queue.c	6.44 (Berkeley) %G% (without queueing)";
+static char sccsid[] = "@(#)queue.c	6.45 (Berkeley) %G% (without queueing)";
 #endif
 #endif /* not lint */
 
@@ -478,10 +478,11 @@ runqueue(forkflag)
 		{
 			if (Verbose)
 				printf("\nSkipping %s\n", w->w_name + 2);
-			continue;
 		}
-
-		dowork(w->w_name + 2, ForkQueueRuns, FALSE, e);
+		else
+		{
+			dowork(w->w_name + 2, ForkQueueRuns, FALSE, e);
+		}
 		free(w->w_name);
 		free((char *) w);
 	}
