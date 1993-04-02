b8a306aa3444092280d861398c441a4dad7ae752
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-04-01 20:29:34 -0800

    strip leading spaces off of aliases
    SCCS-vsn: usr.sbin/sendmail/src/alias.c 6.30

--- usr/src/usr.sbin/sendmail/src/alias.c
+++ usr/src/usr.sbin/sendmail/src/alias.c
@@ -26,15 +26,15 @@ ERROR: DBM is no longer supported -- use NDBM instead.
 #ifndef lint
 #ifdef NEWDB
 #ifdef NDBM
-static char sccsid[] = "@(#)alias.c	6.29 (Berkeley) %G% (with NEWDB and NDBM)";
+static char sccsid[] = "@(#)alias.c	6.30 (Berkeley) %G% (with NEWDB and NDBM)";
 #else
-static char sccsid[] = "@(#)alias.c	6.29 (Berkeley) %G% (with NEWDB)";
+static char sccsid[] = "@(#)alias.c	6.30 (Berkeley) %G% (with NEWDB)";
 #endif
 #else
 #ifdef NDBM
-static char sccsid[] = "@(#)alias.c	6.29 (Berkeley) %G% (with NDBM)";
+static char sccsid[] = "@(#)alias.c	6.30 (Berkeley) %G% (with NDBM)";
 #else
-static char sccsid[] = "@(#)alias.c	6.29 (Berkeley) %G% (without NEWDB or NDBM)";
+static char sccsid[] = "@(#)alias.c	6.30 (Berkeley) %G% (without NEWDB or NDBM)";
 #endif
 #endif
 #endif /* not lint */
@@ -655,6 +655,8 @@ readaliases(aliasfile, init, e)
 		**		list maintainer.
 		*/
 
+		while (isascii(*p) && isspace(*p))
+			p++;
 		if (*p == ':')
 		{
 			ADDRESS *maint;
