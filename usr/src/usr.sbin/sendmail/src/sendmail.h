5ae5dbc41464eb8ef32038a65b7a21632ed8a92a
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-04-04 23:13:52 -0800

    add map_rebuild for program to use for auto-rebuild
    SCCS-vsn: usr.sbin/sendmail/src/sendmail.h 6.44

--- usr/src/usr.sbin/sendmail/src/sendmail.h
+++ usr/src/usr.sbin/sendmail/src/sendmail.h
@@ -5,7 +5,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)sendmail.h	6.43 (Berkeley) %G%
+ *	@(#)sendmail.h	6.44 (Berkeley) %G%
  */
 
 /*
@@ -15,7 +15,7 @@
 # ifdef _DEFINE
 # define EXTERN
 # ifndef lint
-static char SmailSccsId[] =	"@(#)sendmail.h	6.43		%G%";
+static char SmailSccsId[] =	"@(#)sendmail.h	6.44		%G%";
 # endif
 # else /*  _DEFINE */
 # define EXTERN extern
