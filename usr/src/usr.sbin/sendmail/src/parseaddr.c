280b164855a6298af98571de296ebdd829f097fb
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-04-05 15:22:01 -0800

    clean up error return messages
    SCCS-vsn: usr.sbin/sendmail/src/parseaddr.c 6.36
    SCCS-vsn: usr.sbin/sendmail/src/savemail.c 6.29

--- usr/src/usr.sbin/sendmail/src/parseaddr.c
+++ usr/src/usr.sbin/sendmail/src/parseaddr.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)parseaddr.c	6.35 (Berkeley) %G%";
+static char sccsid[] = "@(#)parseaddr.c	6.36 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -121,7 +121,7 @@ parseaddr(addr, a, copyf, delim, delimptr, e)
 	**  Build canonical address from pvp.
 	*/
 
-	a = buildaddr(pvp, a);
+	a = buildaddr(pvp, a, e);
 	if (a == NULL)
 		return (NULL);
 
@@ -1062,6 +1062,7 @@ rewrite(pvp, ruleset)
 **		tv -- token vector.
 **		a -- pointer to address descriptor to fill.
 **			If NULL, one will be allocated.
+**		e -- the current envelope.
 **
 **	Returns:
 **		NULL if there was an error.
@@ -1091,9 +1092,10 @@ struct errcodes
 };
 
 ADDRESS *
-buildaddr(tv, a)
+buildaddr(tv, a, e)
 	register char **tv;
 	register ADDRESS *a;
+	register ENVELOPE *e;
 {
 	struct mailer **mp;
 	register struct mailer *m;
@@ -1136,6 +1138,8 @@ buildaddr(tv, a)
 		cataddr(++tv, NULL, buf, sizeof buf, ' ');
 		stripquotes(buf);
 		usrerr(buf);
+		if (e->e_message == NULL)
+			e->e_message = newstr(buf);
 		return (NULL);
 	}
 
@@ -1578,7 +1582,7 @@ maplocaluser(a, sendq, e)
 		return;
 
 	/* if non-null, mailer destination specified -- has it changed? */
-	a1 = buildaddr(pvp, NULL);
+	a1 = buildaddr(pvp, NULL, e);
 	if (a1 == NULL || sameaddr(a, a1))
 		return;
 
--- usr/src/usr.sbin/sendmail/src/savemail.c
+++ usr/src/usr.sbin/sendmail/src/savemail.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)savemail.c	6.28 (Berkeley) %G%";
+static char sccsid[] = "@(#)savemail.c	6.29 (Berkeley) %G%";
 #endif /* not lint */
 
 # include <pwd.h>
@@ -219,8 +219,7 @@ savemail(e)
 				state = ESM_POSTMASTER;
 				break;
 			}
-			if (returntosender(e->e_message != NULL ? e->e_message :
-					   "Unable to deliver mail",
+			if (returntosender(e->e_message,
 					   q, (e->e_class >= 0), e) == 0)
 			{
 				state = ESM_DONE;
@@ -243,8 +242,7 @@ savemail(e)
 				state = ESM_USRTMP;
 				break;
 			}
-			if (returntosender(e->e_message != NULL ? e->e_message :
-					   "Unable to deliver mail",
+			if (returntosender(e->e_message,
 					   q, (e->e_class >= 0), e) == 0)
 			{
 				state = ESM_DONE;
