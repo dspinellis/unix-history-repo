0b32d17aade69c376cd1a56e7f52c8d61137947c
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1993-04-04 20:47:33 -0800

    date and time created 93/04/04 13:47:33 by mckusick
    SCCS-vsn: lib/libc/compat-43/gethostid.c 5.1
    SCCS-vsn: lib/libc/compat-43/sethostid.c 5.1

---
+++ usr/src/lib/libc/compat-43/gethostid.c
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 1989 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.redist.c%
+ */
+
+#if defined(LIBC_SCCS) && !defined(lint)
+static char sccsid[] = "@(#)gethostid.c	5.1 (Berkeley) %G%";
+#endif /* LIBC_SCCS and not lint */
+
+#include <sys/param.h>
+#include <sys/sysctl.h>
+
+#if __STDC__
+long
+gethostid(void)
+#else
+long
+gethostid()
+#endif
+{
+	int mib[2], size;
+	long value;
+
+	mib[0] = CTL_KERN;
+	mib[1] = KERN_HOSTID;
+	size = sizeof value;
+	if (sysctl(mib, 2, &value, &size, NULL, 0) == -1)
+		return (-1);
+	return (value);
+}
