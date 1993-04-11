64c57c4b763a47d0d646de60d22c47539f5b4ae9
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-04-10 16:27:40 -0800

    fix loopback detection
    SCCS-vsn: usr.sbin/sendmail/src/usersmtp.c 6.24

--- usr/src/usr.sbin/sendmail/src/usersmtp.c
+++ usr/src/usr.sbin/sendmail/src/usersmtp.c
@@ -10,9 +10,9 @@
 
 #ifndef lint
 #ifdef SMTP
-static char sccsid[] = "@(#)usersmtp.c	6.23 (Berkeley) %G% (with SMTP)";
+static char sccsid[] = "@(#)usersmtp.c	6.24 (Berkeley) %G% (with SMTP)";
 #else
-static char sccsid[] = "@(#)usersmtp.c	6.23 (Berkeley) %G% (without SMTP)";
+static char sccsid[] = "@(#)usersmtp.c	6.24 (Berkeley) %G% (without SMTP)";
 #endif
 #endif /* not lint */
 
@@ -143,10 +143,10 @@ smtpinit(m, pvp)
 	**  to connect to an echo server.
 	*/
 
-	p = strchr(SmtpReplyBuffer, ' ');
+	p = strchr(&SmtpReplyBuffer[4], ' ');
 	if (p != NULL)
 		*p == '\0';
-	if (strcasecmp(SmtpReplyBuffer, MyHostName) == 0)
+	if (strcasecmp(&SmtpReplyBuffer[4], MyHostName) == 0)
 	{
 		syserr("553 %s config error: mail loops back to myself",
 			MyHostName);
