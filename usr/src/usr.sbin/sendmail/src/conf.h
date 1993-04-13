d1516c5ebe9e2e5ab4e6d1bc063a195224bfbba4
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-04-13 15:05:11 -0800

    change comment on MAXUSERENVIRON to emphasize min value
    SCCS-vsn: usr.sbin/sendmail/src/conf.h 6.16

--- usr/src/usr.sbin/sendmail/src/conf.h
+++ usr/src/usr.sbin/sendmail/src/conf.h
@@ -5,7 +5,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)conf.h	6.15 (Berkeley) %G%
+ *	@(#)conf.h	6.16 (Berkeley) %G%
  */
 
 /*
@@ -31,7 +31,7 @@
 # define SMTPLINELIM	990		/* maximum SMTP line length */
 # define MAXKEY		128		/* maximum size of a database key */
 # define MEMCHUNKSIZE	1024		/* chunk size for memory allocation */
-# define MAXUSERENVIRON	100		/* max envariables saved */
+# define MAXUSERENVIRON	100		/* max envars saved, must be >= 3 */
 # define MAXIPADDR	16		/* max # of IP addrs for this host */
 # define PSBUFSIZE	(MAXNAME * 4)	/* size of prescan buffer */
 
