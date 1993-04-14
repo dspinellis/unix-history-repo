e75223a596f63168ee58e86aad963865e67eb214
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-04-14 14:34:27 -0800

    move IDENTPROTO #define into conf.h; default it off for ULTRIX
    SCCS-vsn: usr.sbin/sendmail/src/conf.h 6.17
    SCCS-vsn: usr.sbin/sendmail/src/daemon.c 6.36

--- usr/src/usr.sbin/sendmail/src/daemon.c
+++ usr/src/usr.sbin/sendmail/src/daemon.c
@@ -12,9 +12,9 @@
 
 #ifndef lint
 #ifdef DAEMON
-static char sccsid[] = "@(#)daemon.c	6.35 (Berkeley) %G% (with daemon mode)";
+static char sccsid[] = "@(#)daemon.c	6.36 (Berkeley) %G% (with daemon mode)";
 #else
-static char sccsid[] = "@(#)daemon.c	6.35 (Berkeley) %G% (without daemon mode)";
+static char sccsid[] = "@(#)daemon.c	6.36 (Berkeley) %G% (without daemon mode)";
 #endif
 #endif /* not lint */
 
@@ -751,8 +751,6 @@ myhostname(hostbuf, size)
 **	Side Effects:
 **		Sets RealHostName to the name of the host at the other end.
 */
-
-#define IDENTPROTO	1
 
 #ifdef IDENTPROTO
 
