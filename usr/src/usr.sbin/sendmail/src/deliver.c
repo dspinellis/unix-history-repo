782098e2a6d5030293fa3a393414861007dfbb9a
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-04-14 14:48:01 -0800

    let the $@ clause of an SMTP mailer be a colon separated list of hosts; these will be searched like MX records
    SCCS-vsn: usr.sbin/sendmail/src/deliver.c 6.62

--- usr/src/usr.sbin/sendmail/src/deliver.c
+++ usr/src/usr.sbin/sendmail/src/deliver.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)deliver.c	6.61 (Berkeley) %G%";
+static char sccsid[] = "@(#)deliver.c	6.62 (Berkeley) %G%";
 #endif /* not lint */
 
 #include "sendmail.h"
