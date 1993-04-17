f3fae6d26b5b8d306022772fa81e99c0f398e37a
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-04-17 15:48:20 -0800

    turn off DEFNAMES and DNSRCH in hostsignature for old config file compatibility
    SCCS-vsn: usr.sbin/sendmail/src/deliver.c 6.63

--- usr/src/usr.sbin/sendmail/src/deliver.c
+++ usr/src/usr.sbin/sendmail/src/deliver.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)deliver.c	6.62 (Berkeley) %G%";
+static char sccsid[] = "@(#)deliver.c	6.63 (Berkeley) %G%";
 #endif /* not lint */
 
 #include "sendmail.h"
