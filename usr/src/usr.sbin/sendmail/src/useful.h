d2bd88e25df9262be63acc17d40292e75c599761
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-04-18 16:47:27 -0800

    oops...  fix botch in previous delta
    SCCS-vsn: usr.sbin/sendmail/src/useful.h 6.5

--- usr/src/usr.sbin/sendmail/src/useful.h
+++ usr/src/usr.sbin/sendmail/src/useful.h
@@ -4,16 +4,18 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)useful.h	6.4 (Berkeley) %G%
+ *	@(#)useful.h	6.5 (Berkeley) %G%
  */
 
 # include <sys/types.h>
 
 /* support for ANSI prototypes (or not, as the case may be) */
-#if defined(__STDC__) && defined(_FORGIVING_CC_) && !defined(__P)
-#define __P(protos)	protos
-#else
-#define __P(protos)	()
+#ifndef __P
+# if defined(__STDC__) && defined(_FORGIVING_CC_)
+#  define __P(protos)	protos
+# else
+#  define __P(protos)	()
+# endif
 #endif
 
 /* support for bool type */
