d1eb16cb87415f7e9dda196a39ff800f73da40b0
Chris Torek <torek@ucbvax.Berkeley.EDU>
1993-04-20 13:35:20 -0800

    update from van: add backlog, renumber ioctls for mixing with SunOS
    SCCS-vsn: sys/sparc/include/bsd_audioio.h 7.3

--- usr/src/sys/sparc/include/bsd_audioio.h
+++ usr/src/sys/sparc/include/bsd_audioio.h
@@ -9,13 +9,13 @@
  * All advertising materials mentioning features or use of this software
  * must display the following acknowledgement:
  *	This product includes software developed by the University of
- *	California, Lawrence Berkeley Laboratories.
+ *	California, Lawrence Berkeley Laboratory.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)bsd_audioio.h	7.2 (Berkeley) %G%
+ *	@(#)bsd_audioio.h	7.3 (Berkeley) %G%
  *
- * from: $Header: bsd_audioio.h,v 1.4 92/07/13 00:31:22 torek Exp $ (LBL)
+ * from: $Header: bsd_audioio.h,v 1.5 92/11/21 20:46:46 van Exp $ (LBL)
  */
 
 #ifndef _BSD_AUDIOIO_H_
@@ -24,11 +24,11 @@
 /*
  * /dev/audio ioctls.  needs comments!
  */
-#define AUDIO_MIN_GAIN 0
-#define AUDIO_MAX_GAIN 255
+#define AUDIO_MIN_GAIN (0)
+#define AUDIO_MAX_GAIN (255)
 
-#define AUDIO_ENCODING_ULAW 1
-#define AUDIO_ENCODING_ALAW 2
+#define AUDIO_ENCODING_ULAW (1)
+#define AUDIO_ENCODING_ALAW (2)
 
 struct audio_prinfo {
 	u_int	sample_rate;
@@ -37,15 +37,18 @@ struct audio_prinfo {
 	u_int	encoding;
 	u_int	gain;
 	u_int	port;
+	u_long	seek;		/* BSD extension */
+	u_int	ispare[3];
 	u_int	samples;
+	u_int	eof;
 
 	u_char	pause;
 	u_char	error;
 	u_char	waiting;
+	u_char	cspare[3];
 	u_char	open;
+	u_char	active;
 
-	/* BSD extensions */
-	u_long	seek;
 };
 
 struct audio_info {
@@ -56,6 +59,7 @@ struct audio_info {
 	u_int	blocksize;	/* input blocking threshold */
 	u_int	hiwat;		/* output high water mark */
 	u_int	lowat;		/* output low water mark */
+	u_int	backlog;	/* samples of output backlog to gen. */
 };
 typedef struct audio_info audio_info_t;
 
@@ -63,23 +67,23 @@ typedef struct audio_info audio_info_t;
 	(void)memset((void *)(p), 0xff, sizeof(struct audio_info))
 
 #if (defined(sun) || defined(ibm032)) && !defined(__GNUC__)
-#define AUDIO_GETINFO	_IOR(A, 1, struct audio_info)
-#define AUDIO_SETINFO	_IOWR(A, 2, struct audio_info)
-#define AUDIO_DRAIN	_IO(A, 3)
-#define AUDIO_FLUSH	_IO(A, 4)
-#define AUDIO_WSEEK	_IOR(A, 5, u_long)
-#define AUDIO_RERROR	_IOR(A, 6, int)
-#define AUDIO_GETMAP	_IOR(A, 20, struct mapreg)
-#define	AUDIO_SETMAP	_IOW(A, 21, struct mapreg)
+#define AUDIO_GETINFO	_IOR(A, 21, struct audio_info)
+#define AUDIO_SETINFO	_IOWR(A, 22, struct audio_info)
+#define AUDIO_DRAIN	_IO(A, 23)
+#define AUDIO_FLUSH	_IO(A, 24)
+#define AUDIO_WSEEK	_IOR(A, 25, u_long)
+#define AUDIO_RERROR	_IOR(A, 26, int)
+#define AUDIO_GETMAP	_IOR(A, 27, struct mapreg)
+#define	AUDIO_SETMAP	_IOW(A, 28, struct mapreg)
 #else
-#define AUDIO_GETINFO	_IOR('A', 1, struct audio_info)
-#define AUDIO_SETINFO	_IOWR('A', 2, struct audio_info)
-#define AUDIO_DRAIN	_IO('A', 3)
-#define AUDIO_FLUSH	_IO('A', 4)
-#define AUDIO_WSEEK	_IOR('A', 5, u_long)
-#define AUDIO_RERROR	_IOR('A', 6, int)
-#define AUDIO_GETMAP	_IOR('A', 20, struct mapreg)
-#define	AUDIO_SETMAP	_IOW('A', 21, struct mapreg)
+#define AUDIO_GETINFO	_IOR('A', 21, struct audio_info)
+#define AUDIO_SETINFO	_IOWR('A', 22, struct audio_info)
+#define AUDIO_DRAIN	_IO('A', 23)
+#define AUDIO_FLUSH	_IO('A', 24)
+#define AUDIO_WSEEK	_IOR('A', 25, u_long)
+#define AUDIO_RERROR	_IOR('A', 26, int)
+#define AUDIO_GETMAP	_IOR('A', 27, struct mapreg)
+#define	AUDIO_SETMAP	_IOW('A', 28, struct mapreg)
 #endif
 
 #define AUDIO_SPEAKER   	1
