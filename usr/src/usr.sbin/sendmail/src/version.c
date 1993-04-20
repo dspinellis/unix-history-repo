f0854008e519968f26c62b0384ed6ddb944769a1
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-04-19 23:53:18 -0800

    properly handle \! hack; improve error message printing; XLA extensions; additional hooks for tempfail detection
    SCCS-vsn: usr.sbin/sendmail/src/version.c 6.54

--- usr/src/usr.sbin/sendmail/src/version.c
+++ usr/src/usr.sbin/sendmail/src/version.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)version.c	6.53 (Berkeley) %G%";
+static char sccsid[] = "@(#)version.c	6.54 (Berkeley) %G%";
 #endif /* not lint */
 
-char	Version[] = "ALPHA-6.53";
+char	Version[] = "ALPHA-6.54";
