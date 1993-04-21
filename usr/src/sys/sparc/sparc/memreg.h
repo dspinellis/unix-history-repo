a1df4be37a6681832c06ef77915899c424682927
Chris Torek <torek@ucbvax.Berkeley.EDU>
1993-04-21 06:37:31 -0800

    spelling
    SCCS-vsn: sys/sparc/sparc/memreg.h 7.3

--- usr/src/sys/sparc/sparc/memreg.h
+++ usr/src/sys/sparc/sparc/memreg.h
@@ -9,13 +9,13 @@
  * All advertising materials mentioning features or use of this software
  * must display the following acknowledgement:
  *	This product includes software developed by the University of
- *	California, Lawrence Berkeley Laboratories.
+ *	California, Lawrence Berkeley Laboratory.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)memreg.h	7.2 (Berkeley) %G%
+ *	@(#)memreg.h	7.3 (Berkeley) %G%
  *
- * from: $Header: memreg.h,v 1.5 92/06/17 05:22:18 torek Exp $ (LBL)
+ * from: $Header: memreg.h,v 1.6 92/11/26 03:05:05 torek Exp $ (LBL)
  */
 
 /*
