433507826b5b6e36cc3ecdc1952d29277a43ef9d
Chris Torek <torek@ucbvax.Berkeley.EDU>
1993-04-21 03:54:47 -0800

    spelling
    SCCS-vsn: sys/sparc/sparc/asm.h 7.3

--- usr/src/sys/sparc/sparc/asm.h
+++ usr/src/sys/sparc/sparc/asm.h
@@ -9,13 +9,13 @@
  * All advertising materials mentioning features or use of this software
  * must display the following acknowledgement:
  *	This product includes software developed by the University of
- *	California, Lawrence Berkeley Laboratories.
+ *	California, Lawrence Berkeley Laboratory.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)asm.h	7.2 (Berkeley) %G%
+ *	@(#)asm.h	7.3 (Berkeley) %G%
  *
- * from: $Header: asm.h,v 1.4 92/06/17 05:21:52 torek Exp $
+ * from: $Header: asm.h,v 1.5 92/11/26 03:04:42 torek Exp $
  */
 
 /*
