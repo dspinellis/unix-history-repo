8b42d8459029b871ebb6daf65721fa079f0928cf
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-04-27 14:03:00 -0800

    add x400-received to list of H_TRACE fields
    SCCS-vsn: usr.sbin/sendmail/src/conf.c 6.48

--- usr/src/usr.sbin/sendmail/src/conf.c
+++ usr/src/usr.sbin/sendmail/src/conf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)conf.c	6.47 (Berkeley) %G%";
+static char sccsid[] = "@(#)conf.c	6.48 (Berkeley) %G%";
 #endif /* not lint */
 
 # include <sys/ioctl.h>
@@ -85,6 +85,7 @@ struct hdrinfo	HdrInfo[] =
 
 		/* trace fields */
 	"received",		H_TRACE|H_FORCE,
+	"x400-received",	H_TRACE|H_FORCE,
 	"via",			H_TRACE|H_FORCE,
 	"mail-from",		H_TRACE|H_FORCE,
 
