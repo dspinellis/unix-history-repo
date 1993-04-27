bdfdad9a9ec20baddb24803a2f19552e8a0c799b
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-04-26 20:33:19 -0800

    quote need not be in DELIMCHARS either
    SCCS-vsn: usr.sbin/sendmail/src/parseaddr.c 6.45

--- usr/src/usr.sbin/sendmail/src/parseaddr.c
+++ usr/src/usr.sbin/sendmail/src/parseaddr.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)parseaddr.c	6.44 (Berkeley) %G%";
+static char sccsid[] = "@(#)parseaddr.c	6.45 (Berkeley) %G%";
 #endif /* not lint */
 
 #include "sendmail.h"
@@ -66,7 +66,7 @@ char	*DelimChar;		/* set to point to the delimiter */
 */
 
 /* following delimiters are inherent to the internal algorithms */
-# define DELIMCHARS	"()<>,;\"\r\n"	/* default word delimiters */
+# define DELIMCHARS	"()<>,;\r\n"	/* default word delimiters */
 
 ADDRESS *
 parseaddr(addr, a, copyf, delim, delimptr, e)
