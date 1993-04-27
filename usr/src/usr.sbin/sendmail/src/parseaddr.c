3cd5fca6285ff6b9a70c4ac2c746181c34bb9e3d
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-04-26 20:30:04 -0800

    fix some problems with backslash handling
    SCCS-vsn: usr.sbin/sendmail/src/parseaddr.c 6.44

--- usr/src/usr.sbin/sendmail/src/parseaddr.c
+++ usr/src/usr.sbin/sendmail/src/parseaddr.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)parseaddr.c	6.43 (Berkeley) %G%";
+static char sccsid[] = "@(#)parseaddr.c	6.44 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -54,7 +54,7 @@ static char sccsid[] = "@(#)parseaddr.c	6.43 (Berkeley) %G%";
 */
 
 /* following delimiters are inherent to the internal algorithms */
-# define DELIMCHARS	"\201()<>,;\\\"\r\n"	/* word delimiters */
+# define DELIMCHARS	"()<>,;\"\r\n"	/* default word delimiters */
 
 ADDRESS *
 parseaddr(addr, a, copyf, delim, delimptr, e)
@@ -335,7 +335,7 @@ prescan(addr, delim, pvpbuf, delimptr)
 		for (;;)
 		{
 			/* store away any old lookahead character */
-			if (c != NOCHAR)
+			if (c != NOCHAR && !bslashmode)
 			{
 				/* see if there is room */
 				if (q >= &pvpbuf[PSBUFSIZE - 5])
@@ -396,8 +396,6 @@ prescan(addr, delim, pvpbuf, delimptr)
 			if (c == '\\')
 			{
 				bslashmode = TRUE;
-				c = NOCHAR;
-				continue;
 			}
 			else if (state == QST)
 			{
