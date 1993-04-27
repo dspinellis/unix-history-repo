14a78da106ed3170d76fd0768599de9aa601ef9e
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-04-26 19:44:22 -0800

    print more fields in printaddr
    SCCS-vsn: usr.sbin/sendmail/src/parseaddr.c 6.43

--- usr/src/usr.sbin/sendmail/src/parseaddr.c
+++ usr/src/usr.sbin/sendmail/src/parseaddr.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)parseaddr.c	6.42 (Berkeley) %G%";
+static char sccsid[] = "@(#)parseaddr.c	6.43 (Berkeley) %G%";
 #endif /* not lint */
 
 #include "sendmail.h"
