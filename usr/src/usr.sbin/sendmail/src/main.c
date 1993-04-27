d5a02058efbb4871daf48808c69f7d72011573a3
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-04-26 23:08:46 -0800

    clean up setsid handling
    SCCS-vsn: usr.sbin/sendmail/src/main.c 6.58
    SCCS-vsn: usr.sbin/sendmail/src/conf.c 6.47

--- usr/src/usr.sbin/sendmail/src/main.c
+++ usr/src/usr.sbin/sendmail/src/main.c
@@ -13,7 +13,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)main.c	6.57 (Berkeley) %G%";
+static char sccsid[] = "@(#)main.c	6.58 (Berkeley) %G%";
 #endif /* not lint */
 
 #define	_DEFINE
