b4e6cfaf4070e530d03cbd9e7ab0ba2ff71c7090
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-04-27 22:21:06 -0800

    use strftime, don't roll our own
    SCCS-vsn: usr.bin/w/pr_time.c 5.3

--- usr/src/usr.bin/w/pr_time.c
+++ usr/src/usr.bin/w/pr_time.c
@@ -6,74 +6,70 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)pr_time.c	5.2 (Berkeley) %G%";
+static char sccsid[] = "@(#)pr_time.c	5.3 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/types.h>
 #include <sys/time.h>
+
 #include <stdio.h>
 #include <string.h>
+#include <tzfile.h>
 
-#define	HR	(60 * 60)
-#define	DAY	(24 * HR)
-#define	MON	(30 * DAY)
+#include "extern.h"
 
-static time_t now;
 /*
- * prttime prints a time in hours and minutes or minutes and seconds.
- * The character string tail is printed at the end, obvious
- * strings to pass are "", " ", or "am".
+ * pr_attime --
+ *	Print the time since the user logged in. 
  */
-static char *
-prttime(tim, tail)
-	time_t tim;
-	char *tail;
+void
+pr_attime(started, now)
+	time_t *started, *now;
 {
-	int mins;
-	static char timebuf[32];
+	static char buf[256];
+	struct tm *tp;
+	time_t diff;
 
-	if (tim >= 60) {
-		mins = tim % 60;
-		(void) sprintf(timebuf, "%2d:%02d%s", (int)(tim / 60), mins,
-		    tail);
-	} else if (tim >= 0)
-		(void) sprintf(timebuf, "    %2d%s", (int)tim, tail);
+	tp = localtime(started);
+	diff = *now - *started;
+
+	/* If more than a week, use day-month-year. */
+	if (diff > SECSPERDAY * DAYSPERWEEK)
+		(void)strftime(buf, sizeof(buf), "%d%b%y", tp);
+
+	/* If not today, use day-hour-am/pm. */
+	else if (*now / SECSPERDAY != *started / SECSPERDAY)
+		(void)strftime(buf, sizeof(buf), "%a5.3p", tp);
+
+	/* If more than an hour, use hh:mm{am,pm}. */
+	else if (diff > SECSPERHOUR)
+		(void)strftime(buf, sizeof(buf), "%l:pr_time.cp", tp);
+
+	/* Default is mm. */
 	else
-		(void) strcpy(timebuf, tail);
+		(void)strftime(buf, sizeof(buf), "    %M", tp);
 
-	return (timebuf);
+	(void)printf("%s", buf);
 }
 
-static char *weekday[] = { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
-static char *month[] = { "Jan", "Feb", "Mar", "Apr", "May", "Jun",
-		"Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
-
-/* prtat prints a 12 hour time given a pointer to a time of day */
-char *
-attime(started)
-	time_t *started;
+/*
+ * pr_idle --
+ *	Display the idle time.
+ */
+void
+pr_idle(idle)
+	time_t idle;
 {
-	struct tm *p;
-	register int hr, pm;
-	static char prbuff[64];
+	/* If idle more than 36 hours, print as a number of days. */
+	if (idle >= 36 * SECSPERHOUR)
+		(void)printf(" %ddays ", idle / SECSPERDAY);
 
-	if (now == 0)
-		(void) time(&now);
-	p = localtime(started);
-	hr = p->tm_hour;
-	pm = (hr > 11);
-	if (hr > 11)
-		hr -= 12;
-	if (hr == 0)
-		hr = 12;
-	if (now - *started <= 18 * HR)
-		return (prttime((time_t)hr * 60 + p->tm_min, pm ? "pm" : "am"));
-	if (now - *started <= 7 * DAY)
-		(void) sprintf(prbuff, "%*s%d%s", hr < 10 ? 4 : 3,
-			weekday[p->tm_wday], hr, pm ? "pm" : "am");
+	/* If idle more than an hour, print as HH:MM. */
+	else if (idle >= SECSPERHOUR)
+		(void)printf(" %2d:%02d ",
+		    idle / SECSPERHOUR, (idle % SECSPERHOUR) / SECSPERMIN);
+
+	/* Else print the minutes idle. */
 	else
-		(void) sprintf(prbuff, "%2d%s%2d", p->tm_mday,
-			month[p->tm_mon], p->tm_year);
-
-	return (prbuff);
+		(void)printf("    %2d ", idle / SECSPERMIN);
 }
