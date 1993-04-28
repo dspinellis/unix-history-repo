e29557d46a4a2521b12c407ca6e5aae5a905f748
Chris Torek <torek@ucbvax.Berkeley.EDU>
1993-04-28 01:36:30 -0800

    date and time created 93/04/27 18:36:30 by torek
    SCCS-vsn: usr.sbin/eeprom/eeprom.c 5.1
    SCCS-vsn: usr.sbin/eeprom/pathnames.h 5.1

---
+++ usr/src/usr.sbin/eeprom/pathnames.h
@@ -0,0 +1,19 @@
+/*-
+ * Copyright (c) 1992 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This software was developed by the Computer Systems Engineering group
+ * at Lawrence Berkeley Laboratory under DARPA contract BG 91-66 and
+ * contributed to Berkeley.
+ *
+ * All advertising materials mentioning features or use of this software
+ * must display the following acknowledgement:
+ *	This product includes software developed by the University of
+ *	California, Lawrence Berkeley Laboratory.
+ *
+ * %sccs.include.redist.c%
+ *
+ *	@(#)pathnames.h	5.1 (Berkeley) %G%
+ */
+
+#define _PATH_OPENPROM "/dev/openprom"
