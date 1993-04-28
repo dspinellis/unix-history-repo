ca84b21c799eba7aee129744b00dcf1611d99357
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-04-27 22:26:53 -0800

    move kmem version of getloadavg to kvm library
    SCCS-vsn: lib/libc/gen/getloadavg.c 6.5
    SCCS-vsn: lib/libc/gen/getloadavg.3 6.5

--- usr/src/lib/libc/gen/getloadavg.c
+++ usr/src/lib/libc/gen/getloadavg.c
@@ -6,75 +6,39 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)getloadavg.c	6.4 (Berkeley) %G%";
+static char sccsid[] = "@(#)getloadavg.c	6.5 (Berkeley) %G%";
 #endif /* LIBC_SCCS and not lint */
 
-#include <sys/param.h>
 #include <sys/types.h>
-#include <sys/file.h>
-#include <sys/kernel.h>
+#include <sys/time.h>
+#include <sys/resource.h>
 #include <sys/sysctl.h>
 #include <vm/vm_param.h>
-#include <nlist.h>
 
-static struct nlist nl[] = {
-	{ "_averunnable" },
-#define	X_AVERUNNABLE	0
-	{ "_fscale" },
-#define	X_FSCALE	1
-	{ "" },
-};
+#include <stdlib.h>
 
 /*
- *  getloadavg() -- Get system load averages.
+ * getloadavg() -- Get system load averages.
  *
- *  Put `nelem' samples into `loadavg' array.
- *  Return number of samples retrieved, or -1 on error.
+ * Put `nelem' samples into `loadavg' array.
+ * Return number of samples retrieved, or -1 on error.
  */
+int
 getloadavg(loadavg, nelem)
 	double loadavg[];
 	int nelem;
 {
-	static int need_nlist = 1;
 	struct loadavg loadinfo;
-	int size, kmemfd, i;
-	int alreadyopen = 1;
-	int mib[2], fscale;
+	int size, i, mib[2];
 
-	size = sizeof(loadinfo);
 	mib[0] = CTL_VM;
 	mib[1] = VM_LOADAVG;
-	if (sysctl(mib, 2, &loadinfo, &size, NULL, 0) < 0) {
-		if ((alreadyopen = kvm_openfiles(NULL, NULL, NULL)) == -1)
-			return (-1);
-		/* 
-		 * cache nlist 
-		 */
-		if (need_nlist) {
-			if (kvm_nlist(nl) != 0)
-				goto bad;
-			need_nlist = 0;
-		}
-		if (kvm_read((off_t)nl[X_AVERUNNABLE].n_value,
-		    (char *)&loadinfo, sizeof(loadinfo)) != size)
-			goto bad;
-		/*
-		 * Old kernel have fscale separately; if not found assume
-		 * running new format.
-		 */
-		if (kvm_read( (off_t)nl[X_FSCALE].n_value, (char *)&fscale, 
-		    sizeof(fscale)) == sizeof(fscale))
-			loadinfo.fscale = fscale;
-	}
+	size = sizeof(loadinfo);
+	if (sysctl(mib, 2, &loadinfo, &size, NULL, 0) < 0)
+		return (-1);
+
 	nelem = MIN(nelem, sizeof(loadinfo.ldavg) / sizeof(fixpt_t));
 	for (i = 0; i < nelem; i++)
 		loadavg[i] = (double) loadinfo.ldavg[i] / loadinfo.fscale;
-	if (!alreadyopen)
-		kvm_close();
 	return (nelem);
-
-bad:
-	if (!alreadyopen)
-		kvm_close();
-	return (-1);
 }
