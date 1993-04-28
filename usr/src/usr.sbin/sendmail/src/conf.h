78097078c39736a1f0084d4c2657739a0f5aa6e8
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-04-28 00:27:08 -0800

    need sys/stat.h
    SCCS-vsn: usr.sbin/sendmail/src/conf.h 6.25

--- usr/src/usr.sbin/sendmail/src/conf.h
+++ usr/src/usr.sbin/sendmail/src/conf.h
@@ -5,7 +5,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)conf.h	6.24 (Berkeley) %G%
+ *	@(#)conf.h	6.25 (Berkeley) %G%
  */
 
 /*
@@ -13,6 +13,7 @@
 */
 
 # include <sys/param.h>
+# include <sys/stat.h>
 # include <fcntl.h>
 
 /*
