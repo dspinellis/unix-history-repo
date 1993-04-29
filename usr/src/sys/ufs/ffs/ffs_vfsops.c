a31d5281da568759a35af2dfae7a2b0cbdaf0b20
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1993-04-29 00:49:18 -0800

    pass special credential to allow opening of disks
    SCCS-vsn: sys/ufs/ffs/ffs_vfsops.c 7.85
    SCCS-vsn: sys/ufs/lfs/lfs_vfsops.c 7.91

--- usr/src/sys/ufs/ffs/ffs_vfsops.c
+++ usr/src/sys/ufs/ffs/ffs_vfsops.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)ffs_vfsops.c	7.84 (Berkeley) %G%
+ *	@(#)ffs_vfsops.c	7.85 (Berkeley) %G%
  */
 
 #include <sys/param.h>
@@ -350,7 +350,7 @@ ffs_mountfs(devvp, mp, p)
 		return (error);
 
 	ronly = (mp->mnt_flag & MNT_RDONLY) != 0;
-	if (error = VOP_OPEN(devvp, ronly ? FREAD : FREAD|FWRITE, NOCRED, p))
+	if (error = VOP_OPEN(devvp, ronly ? FREAD : FREAD|FWRITE, FSCRED, p))
 		return (error);
 	if (VOP_IOCTL(devvp, DIOCGPART, (caddr_t)&dpart, FREAD, NOCRED, p) != 0)
 		size = DEV_BSIZE;
--- usr/src/sys/ufs/lfs/lfs_vfsops.c
+++ usr/src/sys/ufs/lfs/lfs_vfsops.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)lfs_vfsops.c	7.90 (Berkeley) %G%
+ *	@(#)lfs_vfsops.c	7.91 (Berkeley) %G%
  */
 
 #include <sys/param.h>
@@ -197,7 +197,7 @@ lfs_mountfs(devvp, mp, p)
 		return (error);
 
 	ronly = (mp->mnt_flag & MNT_RDONLY) != 0;
-	if (error = VOP_OPEN(devvp, ronly ? FREAD : FREAD|FWRITE, NOCRED, p))
+	if (error = VOP_OPEN(devvp, ronly ? FREAD : FREAD|FWRITE, FSCRED, p))
 		return (error);
 
 	if (VOP_IOCTL(devvp, DIOCGPART, (caddr_t)&dpart, FREAD, NOCRED, p) != 0)
