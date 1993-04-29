eaedb83050d2d75b4e7025c8cfc5b99eeb6d528e
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-04-29 00:23:02 -0800

    getbsize doesn't need the program name any longer
    SCCS-vsn: bin/ls/ls.c 5.74
    SCCS-vsn: usr.bin/du/du.c 5.21

--- usr/src/usr.bin/du/du.c
+++ usr/src/usr.bin/du/du.c
@@ -15,7 +15,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)du.c	5.20 (Berkeley) %G%";
+static char sccsid[] = "@(#)du.c	5.21 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/param.h>
@@ -29,7 +29,7 @@ static char sccsid[] = "@(#)du.c	5.20 (Berkeley) %G%";
 #include <stdlib.h>
 #include <string.h>
 
-char	*getbsize __P((char *, int *, long *));
+char	*getbsize __P((int *, long *));
 int	 linkchk __P((FTSENT *));
 void	 usage __P((void));
 
@@ -89,7 +89,7 @@ main(argc, argv)
 		argv[1] = NULL;
 	}
 
-	(void)getbsize("du", &notused, &blocksize);
+	(void)getbsize(&notused, &blocksize);
 	blocksize /= 512;
 
 	if ((fts = fts_open(argv, ftsoptions, NULL)) == NULL)
