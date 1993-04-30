b6ce159d7f1c8beba3e1d11e9fcee6a1f080aed7
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-04-29 19:30:57 -0800

    random cleanups, lint, for 4.4BSD freeze
    SCCS-vsn: bin/ln/ln.c 5.1

--- usr/src/bin/ln/ln.c
+++ usr/src/bin/ln/ln.c
@@ -12,29 +12,35 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)ln.c	4.15 (Berkeley) %G%";
+static char sccsid[] = "@(#)ln.c	5.1 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/param.h>
 #include <sys/stat.h>
-#include <stdio.h>
+
+#include <err.h>
 #include <errno.h>
-#include <string.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
-static int	dirflag,			/* undocumented force flag */
-		sflag,				/* symbolic, not hard, link */
-		(*linkf)();			/* system link call */
-static linkit(), usage();
+int	dirflag,			/* Undocumented force flag. */
+	sflag,				/* Symbolic, not hard, link. */
+					/* System link call. */
+	(*linkf) __P((const char *, const char *));
 
+static int	linkit __P((char *, char *, int));
+static void	usage __P((void));
+
+int
 main(argc, argv)
 	int argc;
-	char **argv;
+	char *argv[];
 {
 	extern int optind;
-	struct stat buf;
-	int ch, exitval, link(), symlink();
+	struct stat sb;
+	int ch, exitval;
 	char *sourcedir;
 
 	while ((ch = getopt(argc, argv, "Fs")) != EOF)
@@ -62,62 +68,57 @@ main(argc, argv)
 		exit(linkit(argv[0], ".", 1));
 	case 2:				/* ln target source */
 		exit(linkit(argv[0], argv[1], 0));
-	default:			/* ln target1 target2 directory */
-		sourcedir = argv[argc - 1];
-		if (stat(sourcedir, &buf)) {
-			(void)fprintf(stderr,
-			    "ln: %s: %s\n", sourcedir, strerror(errno));
-			exit(1);
-		}
-		if (!S_ISDIR(buf.st_mode))
-			usage();
-		for (exitval = 0; *argv != sourcedir; ++argv)
-			exitval |= linkit(*argv, sourcedir, 1);
-		exit(exitval);
 	}
-	/* NOTREACHED */
+					/* ln target1 target2 directory */
+	sourcedir = argv[argc - 1];
+	if (stat(sourcedir, &sb))
+		err(1, "%s", sourcedir);
+	if (!S_ISDIR(sb.st_mode))
+		usage();
+	for (exitval = 0; *argv != sourcedir; ++argv)
+		exitval |= linkit(*argv, sourcedir, 1);
+	exit(exitval);
 }
 
-static
+static int
 linkit(target, source, isdir)
 	char *target, *source;
 	int isdir;
 {
-	struct stat buf;
-	char path[MAXPATHLEN], *cp;
+	struct stat sb;
+	char path[MAXPATHLEN], *p;
 
 	if (!sflag) {
 		/* if target doesn't exist, quit now */
-		if (stat(target, &buf)) {
-			(void)fprintf(stderr,
-			    "ln: %s: %s\n", target, strerror(errno));
-			return(1);
+		if (stat(target, &sb)) {
+			warn("%s", target);
+			return (1);
 		}
 		/* only symbolic links to directories, unless -F option used */
-		if (!dirflag && (buf.st_mode & S_IFMT) == S_IFDIR) {
-			(void)printf("ln: %s is a directory.\n", target);
-			return(1);
+		if (!dirflag && (sb.st_mode & S_IFMT) == S_IFDIR) {
+			warnx("%s: is a directory", target);
+			return (1);
 		}
 	}
 
 	/* if the source is a directory, append the target's name */
-	if (isdir || !stat(source, &buf) && (buf.st_mode & S_IFMT) == S_IFDIR) {
-		if (!(cp = rindex(target, '/')))
-			cp = target;
+	if (isdir || !stat(source, &sb) && (sb.st_mode & S_IFMT) == S_IFDIR) {
+		if (!(p = strrchr(target, '/')))
+			p = target;
 		else
-			++cp;
-		(void)sprintf(path, "%s/%s", source, cp);
+			++p;
+		(void)snprintf(path, sizeof(path), "%s/%s", source, p);
 		source = path;
 	}
 
 	if ((*linkf)(target, source)) {
-		(void)fprintf(stderr, "ln: %s: %s\n", source, strerror(errno));
-		return(1);
+		warn("%s", source);
+		return (1);
 	}
-	return(0);
+	return (0);
 }
 
-static
+static void
 usage()
 {
 	(void)fprintf(stderr,
