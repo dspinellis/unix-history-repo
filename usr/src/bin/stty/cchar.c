a41ab9c0b76fb7764d72beef5405c1b87ee4cccc
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-04-29 22:17:06 -0800

    use C library err/warn routines; make GCC 2 happy
    SCCS-vsn: bin/stty/cchar.c 5.7

--- usr/src/bin/stty/cchar.c
+++ usr/src/bin/stty/cchar.c
@@ -6,13 +6,16 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)cchar.c	5.6 (Berkeley) %G%";
+static char sccsid[] = "@(#)cchar.c	5.7 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/types.h>
+
+#include <err.h>
 #include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
+
 #include "stty.h"
 #include "extern.h"
 
@@ -24,37 +27,37 @@ static char sccsid[] = "@(#)cchar.c	5.6 (Berkeley) %G%";
  * command line.
  */
 struct cchar cchars1[] = {
-	"discard",	VDISCARD, 	CDISCARD,
-	"dsusp", 	VDSUSP,		CDSUSP,
-	"eof",		VEOF,		CEOF,
-	"eol",		VEOL,		CEOL,
-	"eol2",		VEOL2,		CEOL,
-	"erase",	VERASE,		CERASE,
-	"intr",		VINTR,		CINTR,
-	"kill",		VKILL,		CKILL,
-	"lnext",	VLNEXT,		CLNEXT,
-	"quit",		VQUIT,		CQUIT,
-	"reprint",	VREPRINT, 	CREPRINT,
-	"start",	VSTART,		CSTART,
-	"status",	VSTATUS, 	CSTATUS,
-	"stop",		VSTOP,		CSTOP,
-	"susp",		VSUSP,		CSUSP,
-	"werase",	VWERASE,	CWERASE,
-	NULL,
+	{ "discard",	VDISCARD, 	CDISCARD },
+	{ "dsusp", 	VDSUSP,		CDSUSP },
+	{ "eof",	VEOF,		CEOF },
+	{ "eol",	VEOL,		CEOL },
+	{ "eol2",	VEOL2,		CEOL },
+	{ "erase",	VERASE,		CERASE },
+	{ "intr",	VINTR,		CINTR },
+	{ "kill",	VKILL,		CKILL },
+	{ "lnext",	VLNEXT,		CLNEXT },
+	{ "quit",	VQUIT,		CQUIT },
+	{ "reprint",	VREPRINT, 	CREPRINT },
+	{ "start",	VSTART,		CSTART },
+	{ "status",	VSTATUS, 	CSTATUS },
+	{ "stop",	VSTOP,		CSTOP },
+	{ "susp",	VSUSP,		CSUSP },
+	{ "werase",	VWERASE,	CWERASE },
+	{ NULL },
 };
 
 struct cchar cchars2[] = {
-	"brk",		VEOL,		CEOL,
-	"flush",	VDISCARD, 	CDISCARD,
-	"rprnt",	VREPRINT, 	CREPRINT,
-	NULL,
+	{ "brk",	VEOL,		CEOL },
+	{ "flush",	VDISCARD, 	CDISCARD },
+	{ "rprnt",	VREPRINT, 	CREPRINT },
+	{ NULL },
 };
 
+int
 csearch(argvp, ip)
 	char ***argvp;
 	struct info *ip;
 {
-	extern char *usage;
 	register struct cchar *cp;
 	struct cchar tmp;
 	char *arg, *name;
@@ -68,11 +71,13 @@ csearch(argvp, ip)
 	    c_cchar)) && !(cp = (struct cchar *)bsearch(&tmp, cchars1,
 	    sizeof(cchars1)/sizeof(struct cchar) - 1, sizeof(struct cchar),
 	    c_cchar)))
-		return(0);
+		return (0);
 
 	arg = *++*argvp;
-	if (!arg)
-		err("option requires an argument -- %s\n%s", name, usage);
+	if (!arg) {
+		warnx("option requires an argument -- %s", name);
+		usage();
+	}
 
 #define CHK(s)  (*arg == s[0] && !strcmp(arg, s))
 	if (CHK("undef") || CHK("<undef>"))
@@ -83,12 +88,12 @@ csearch(argvp, ip)
 	else
 		ip->t.c_cc[cp->sub] = arg[0];
 	ip->set = 1;
-	return(1);
+	return (1);
 }
 
-static
+static int
 c_cchar(a, b)
         const void *a, *b;
 {
-        return(strcmp(((struct cchar *)a)->name, ((struct cchar *)b)->name));
+        return (strcmp(((struct cchar *)a)->name, ((struct cchar *)b)->name));
 }
