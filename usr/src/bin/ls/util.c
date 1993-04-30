d716f98d953f397b5dc00ef721031629d75c7bf6
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-04-29 19:39:44 -0800

    use err/warn(3) from C library
    SCCS-vsn: bin/ls/util.c 5.13

--- usr/src/bin/ls/util.c
+++ usr/src/bin/ls/util.c
@@ -9,17 +9,18 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)util.c	5.12 (Berkeley) %G%";
+static char sccsid[] = "@(#)util.c	5.13 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <stdio.h>
-#include <stdlib.h>
+
 #include <ctype.h>
 #include <fts.h>
-#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+
 #include "ls.h"
 #include "extern.h"
 
@@ -41,34 +42,4 @@ usage()
 {
 	(void)fprintf(stderr, "usage: ls [-1ACFLRTacdfiklqrstu] [file ...]\n");
 	exit(1);
-}
-
-#if __STDC__
-#include <stdarg.h>
-#else
-#include <varargs.h>
-#endif
-
-void
-#if __STDC__
-err(int fatal, const char *fmt, ...)
-#else
-err(fatal, fmt, va_alist)
-	int fatal;
-	char *fmt;
-	va_dcl
-#endif
-{
-	va_list ap;
-#if __STDC__
-	va_start(ap, fmt);
-#else
-	va_start(ap);
-#endif
-	(void)fprintf(stderr, "ls: ");
-	(void)vfprintf(stderr, fmt, ap);
-	va_end(ap);
-	(void)fprintf(stderr, "\n");
-	if (fatal)
-		exit(1);
 }
