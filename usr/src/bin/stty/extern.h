faf85e2f2a81fbafaed47f0f86f4d8af765acc03
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-04-29 22:17:20 -0800

    use C library err/warn routines
    SCCS-vsn: bin/stty/extern.h 5.5

--- usr/src/bin/stty/extern.h
+++ usr/src/bin/stty/extern.h
@@ -4,23 +4,19 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)extern.h	5.4 (Berkeley) %G%
+ *	@(#)extern.h	5.5 (Berkeley) %G%
  */
 
-__BEGIN_DECLS
 int	c_cchars __P((const void *, const void *));
 int	c_modes __P((const void *, const void *));
 int	csearch __P((char ***, struct info *));
 void	checkredirect __P((void));
-void	err __P((const char *, ...));
 void	gprint __P((struct termios *, struct winsize *, int));
 void	gread __P((struct termios *, char *));
 int	ksearch __P((char ***, struct info *));
 int	msearch __P((char ***, struct info *));
 void	optlist __P((void));
 void	print __P((struct termios *, struct winsize *, int, enum FMT));
-void	warn __P((const char *, ...));
-__END_DECLS
+void	usage __P((void));
 
 extern struct cchar cchars1[], cchars2[];
-extern char *usage;
