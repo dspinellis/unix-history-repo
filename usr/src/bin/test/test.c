887b2111044957bd3966383a509865cbd8e6f440
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-04-30 18:43:51 -0800

    lint
    SCCS-vsn: bin/test/test.c 5.6

--- usr/src/bin/test/test.c
+++ usr/src/bin/test/test.c
@@ -15,18 +15,20 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)test.c	5.5 (Berkeley) %G%";
+static char sccsid[] = "@(#)test.c	5.6 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/types.h>
 #include <sys/stat.h>
 
+#include <ctype.h>
 #include <err.h>
 #include <errno.h>
 #include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #include "operators.h"
 
