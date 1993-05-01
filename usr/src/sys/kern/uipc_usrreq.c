e0a0b9b26d552cc1ff2a7e668f01daa5f19784a4
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1993-05-01 02:07:21 -0800

    a better approach than 7.39 to avoid duplicate closef's (from karels@bsdi.com)
    SCCS-vsn: sys/kern/uipc_usrreq.c 7.40

--- usr/src/sys/kern/uipc_usrreq.c
+++ usr/src/sys/kern/uipc_usrreq.c
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 1982, 1986 Regents of the University of California.
  *
- *	@(#)uipc_usrreq.c	7.39 (Berkeley) %G%
+ *	@(#)uipc_usrreq.c	7.40 (Berkeley) %G%
  */
 
 #include <sys/param.h>
@@ -334,8 +334,17 @@ unp_detach(unp)
 	unp->unp_socket->so_pcb = 0;
 	m_freem(unp->unp_addr);
 	(void) m_free(dtom(unp));
-	if (unp_rights)
+	if (unp_rights) {
+		/*
+		 * Normally the receive buffer is flushed later,
+		 * in sofree, but if our receive buffer holds references
+		 * to descriptors that are now garbage, we will dispose
+		 * of those descriptor references after the garbage collector
+		 * gets them (resulting in a "panic: closef: count < 0").
+		 */
+		sorflush(unp->unp_socket);
 		unp_gc();
+	}
 }
 
 unp_bind(unp, nam, p)
@@ -772,8 +781,6 @@ unp_discard(fp)
 	struct file *fp;
 {
 
-	if (fp->f_msgcount == 0)
-		return;
 	fp->f_msgcount--;
 	unp_rights--;
 	(void) closef(fp, (struct proc *)NULL);
