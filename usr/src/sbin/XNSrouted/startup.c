2f7d8622c9d6b2360cd12d2f673049484370b860
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-04-30 19:42:16 -0800

    sysctl.h needs <sys/param.h>
    SCCS-vsn: sbin/XNSrouted/startup.c 5.14

--- usr/src/sbin/XNSrouted/startup.c
+++ usr/src/sbin/XNSrouted/startup.c
@@ -9,17 +9,21 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)startup.c	5.13 (Berkeley) %G%";
+static char sccsid[] = "@(#)startup.c	5.14 (Berkeley) %G%";
 #endif /* not lint */
 
 /*
  * Routing Table Management Daemon
  */
 #include "defs.h"
+
+#include <sys/param.h>
 #include <sys/ioctl.h>
 #include <sys/sysctl.h>
+
 #include <net/if.h>
 #include <net/if_dl.h>
+
 #include <nlist.h>
 #include <stdlib.h>
 
