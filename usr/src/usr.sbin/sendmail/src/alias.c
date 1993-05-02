585bb8ca2c2ba9842a2034794060eab38bbfc177
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-05-01 19:47:39 -0800

    oops...  can't #undef __P on vangogh
    SCCS-vsn: usr.sbin/sendmail/src/map.c 6.13
    SCCS-vsn: usr.sbin/sendmail/src/alias.c 6.37

--- usr/src/usr.sbin/sendmail/src/alias.c
+++ usr/src/usr.sbin/sendmail/src/alias.c
@@ -15,7 +15,6 @@ ERROR: DBM is no longer supported -- use NDBM instead.
 # endif
 
 # ifdef NEWDB
-# undef __P
 # include <db.h>
 # endif
 
@@ -26,15 +25,15 @@ ERROR: DBM is no longer supported -- use NDBM instead.
 #ifndef lint
 #ifdef NEWDB
 #ifdef NDBM
-static char sccsid[] = "@(#)alias.c	6.36 (Berkeley) %G% (with NEWDB and NDBM)";
+static char sccsid[] = "@(#)alias.c	6.37 (Berkeley) %G% (with NEWDB and NDBM)";
 #else
-static char sccsid[] = "@(#)alias.c	6.36 (Berkeley) %G% (with NEWDB)";
+static char sccsid[] = "@(#)alias.c	6.37 (Berkeley) %G% (with NEWDB)";
 #endif
 #else
 #ifdef NDBM
-static char sccsid[] = "@(#)alias.c	6.36 (Berkeley) %G% (with NDBM)";
+static char sccsid[] = "@(#)alias.c	6.37 (Berkeley) %G% (with NDBM)";
 #else
-static char sccsid[] = "@(#)alias.c	6.36 (Berkeley) %G% (without NEWDB or NDBM)";
+static char sccsid[] = "@(#)alias.c	6.37 (Berkeley) %G% (without NEWDB or NDBM)";
 #endif
 #endif
 #endif /* not lint */
