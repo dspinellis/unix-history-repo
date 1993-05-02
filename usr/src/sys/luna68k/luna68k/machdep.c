6f6e9c7080f0578a2d5e73af3f4473e0f406c381
Akito Fujita <akito@ucbvax.Berkeley.EDU>
1993-05-02 12:39:20 -0800

    comment off kstackatbase related statment
    SCCS-vsn: sys/luna68k/luna68k/machdep.c 7.9

--- usr/src/sys/luna68k/luna68k/machdep.c
+++ usr/src/sys/luna68k/luna68k/machdep.c
@@ -13,7 +13,7 @@
  * from: Utah $Hdr: machdep.c 1.63 91/04/24$
  * from: hp300/hp300/machdep.c   7.36 (Berkeley) 2/10/93
  *
- *	@(#)machdep.c	7.8 (Berkeley) %G%
+ *	@(#)machdep.c	7.9 (Berkeley) %G%
  */
 
 #include <sys/param.h>
@@ -1044,11 +1044,14 @@ badkstack(oflow, fr)
 	int oflow;
 	struct frame fr;
 {
+#ifdef	notdef
 	extern char kstackatbase[];
 
 	printf("%s: sp should be %x\n", 
 	       oflow ? oflowmsg : uflowmsg,
 	       kstackatbase - (exframesize[fr.f_format] + 8));
+#endif
+	printf("%s: sp should be ????????\n", oflow ? oflowmsg : uflowmsg);
 	regdump(&fr, 0);
 	panic(oflow ? oflowmsg : uflowmsg);
 }
