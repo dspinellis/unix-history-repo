1956e9d31adb98ce40b7cef6ca591ff76dab1214
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1993-05-02 18:12:24 -0800

    add prototype for lseek to make programs casting second arg to off_t work
    SCCS-vsn: sys/sys/types.h 7.27

--- usr/src/sys/sys/types.h
+++ usr/src/sys/sys/types.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)types.h	7.26 (Berkeley) %G%
+ *	@(#)types.h	7.27 (Berkeley) %G%
  */
 
 #ifndef _TYPES_H_
@@ -39,6 +39,18 @@ typedef	short		pid_t;		/* process id */
 typedef	long		segsz_t;	/* segment size */
 typedef	long		swblk_t;	/* swap offset */
 typedef	unsigned long	uid_t;		/* user id */
+
+/*
+ * This belongs in unistd.h, but is placed here to ensure that programs
+ * casting the second parameter of lseek to off_t will get the correct
+ * version of lseek.
+ */
+#ifndef KERNEL
+#define	 lseek __lseek
+__BEGIN_DECLS
+off_t	 lseek __P((int, off_t, int));
+__END_DECLS
+#endif
 
 #ifndef _POSIX_SOURCE
 #define	major(x)	((int)(((u_int)(x) >> 8)&0xff))	/* major number */
