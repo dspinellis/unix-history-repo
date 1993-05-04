495171e4f764b17d7640f61b6d47bb346a779b72
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-05-03 19:26:38 -0800

    first cut at updated alias implementation
    SCCS-vsn: usr.sbin/sendmail/src/main.c 6.61
    SCCS-vsn: usr.sbin/sendmail/src/srvrsmtp.c 6.47
    SCCS-vsn: usr.sbin/sendmail/src/readcf.c 6.32
    SCCS-vsn: usr.sbin/sendmail/src/conf.c 6.51
    SCCS-vsn: usr.sbin/sendmail/src/alias.c 6.39

--- usr/src/usr.sbin/sendmail/src/conf.c
+++ usr/src/usr.sbin/sendmail/src/conf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)conf.c	6.50 (Berkeley) %G%";
+static char sccsid[] = "@(#)conf.c	6.51 (Berkeley) %G%";
 #endif /* not lint */
 
 # include <sys/ioctl.h>
