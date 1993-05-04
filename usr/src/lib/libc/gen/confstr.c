a52a47612090f4e38b68bd49d0fa8edb52962059
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-05-03 23:54:43 -0800

    add /usr/sbin, so user's will find chown
    SCCS-vsn: lib/libc/gen/confstr.c 5.2

--- usr/src/lib/libc/gen/confstr.c
+++ usr/src/lib/libc/gen/confstr.c
@@ -6,7 +6,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)confstr.c	5.1 (Berkeley) %G%";
+static char sccsid[] = "@(#)confstr.c	5.2 (Berkeley) %G%";
 #endif /* LIBC_SCCS and not lint */
 
 #include <errno.h>
@@ -22,10 +22,10 @@ confstr(name, buf, len)
 	switch (name) {
 	case _CS_PATH:
 		if (len != 0 && buf != NULL) {
-			(void)strncpy(buf, _PATH_DEFPATH, len - 1);
+			(void)strncpy(buf, _PATH_STDPATH, len - 1);
 			buf[len - 1] = '\0';
 		}
-		return (sizeof(_PATH_DEFPATH));
+		return (sizeof(_PATH_STDPATH));
 	default:
 		errno = EINVAL;
 		return (0);
