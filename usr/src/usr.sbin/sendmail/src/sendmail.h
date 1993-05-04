f41e437b6dfdfb378e6869ec282e840739c49e98
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-05-03 18:45:53 -0800

    restore NoAlias
    SCCS-vsn: usr.sbin/sendmail/src/sendmail.h 6.58

--- usr/src/usr.sbin/sendmail/src/sendmail.h
+++ usr/src/usr.sbin/sendmail/src/sendmail.h
@@ -5,7 +5,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)sendmail.h	6.57 (Berkeley) %G%
+ *	@(#)sendmail.h	6.58 (Berkeley) %G%
  */
 
 /*
@@ -15,7 +15,7 @@
 # ifdef _DEFINE
 # define EXTERN
 # ifndef lint
-static char SmailSccsId[] =	"@(#)sendmail.h	6.57		%G%";
+static char SmailSccsId[] =	"@(#)sendmail.h	6.58		%G%";
 # endif
 # else /*  _DEFINE */
 # define EXTERN extern
@@ -721,6 +721,7 @@ EXTERN bool	SuperSafe;	/* be extra careful, even if expensive */
 EXTERN bool	ForkQueueRuns;	/* fork for each job when running the queue */
 EXTERN bool	AutoRebuild;	/* auto-rebuild the alias database as needed */
 EXTERN bool	CheckAliases;	/* parse addresses during newaliases */
+EXTERN bool	NoAlias;	/* suppress aliasing */
 EXTERN bool	UseNameServer;	/* use internet domain name server */
 EXTERN bool	EightBit;	/* try to preserve 8-bit data */
 EXTERN int	SafeAlias;	/* minutes to wait until @:@ in alias file */
