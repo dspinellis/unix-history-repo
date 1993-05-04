0c256f3d39cf19793e35eb147e04b36b0da74eec
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-05-04 02:12:12 -0800

    change option 8 to option 7 (and invert semantics)
    SCCS-vsn: usr.sbin/sendmail/src/srvrsmtp.c 6.49
    SCCS-vsn: usr.sbin/sendmail/src/util.c 6.17
    SCCS-vsn: usr.sbin/sendmail/src/readcf.c 6.33
    SCCS-vsn: usr.sbin/sendmail/src/conf.c 6.52

--- usr/src/usr.sbin/sendmail/src/conf.c
+++ usr/src/usr.sbin/sendmail/src/conf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)conf.c	6.51 (Berkeley) %G%";
+static char sccsid[] = "@(#)conf.c	6.52 (Berkeley) %G%";
 #endif /* not lint */
 
 # include <sys/ioctl.h>
