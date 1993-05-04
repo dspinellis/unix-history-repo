7edbe1b128577f7afa09de66af671d8d1ccac0db
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-05-03 23:52:11 -0800

    date and time created 93/05/03 16:52:11 by bostic
    SCCS-vsn: lib/libc/gen/confstr.c 5.1

---
+++ usr/src/lib/libc/gen/confstr.c
@@ -0,0 +1,34 @@
+/*-
+ * Copyright (c) 1993 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.redist.c%
+ */
+
+#if defined(LIBC_SCCS) && !defined(lint)
+static char sccsid[] = "@(#)confstr.c	5.1 (Berkeley) %G%";
+#endif /* LIBC_SCCS and not lint */
+
+#include <errno.h>
+#include <paths.h>
+#include <unistd.h>
+
+size_t
+confstr(name, buf, len)
+	int name;
+	char *buf;
+	size_t len;
+{
+	switch (name) {
+	case _CS_PATH:
+		if (len != 0 && buf != NULL) {
+			(void)strncpy(buf, _PATH_DEFPATH, len - 1);
+			buf[len - 1] = '\0';
+		}
+		return (sizeof(_PATH_DEFPATH));
+	default:
+		errno = EINVAL;
+		return (0);
+	}
+	/* NOTREACHED */
+}
