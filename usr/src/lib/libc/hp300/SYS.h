0ce18319ad094245df9c05dffcf1ba5f2fb4a2bc
Mike Hibler <hibler@ucbvax.Berkeley.EDU>
1993-05-03 23:17:54 -0800

    add STDC versions for both gcc1 and gcc2, eliminate non-gcc versions
    SCCS-vsn: lib/libc/hp300/DEFS.h 5.2
    SCCS-vsn: lib/libc/hp300/SYS.h 5.6
    SCCS-vsn: lib/libc/hp300/sys/sigreturn.s 5.2

--- usr/src/lib/libc/hp300/SYS.h
+++ usr/src/lib/libc/hp300/SYS.h
@@ -8,27 +8,44 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)SYS.h	5.5 (Berkeley) %G%
+ *	@(#)SYS.h	5.6 (Berkeley) %G%
  */
 
 #include <sys/syscall.h>
 
+#ifdef __STDC__
 #ifdef PROF
-#ifdef __GNUC__
+#if __GNUC__ >= 2
+#define	ENTRY(x)	.globl _ ## x; .even; _ ## x:; .data; PROF ## x:; \
+			.long 0; .text; link a6,\#0; lea PROF ## x,a0; \
+			jbsr mcount; unlk a6
+#else
+#define	ENTRY(x)	.globl _ ## x; .even; _ ## x:; .data; PROF ## x:; \
+			.long 0; .text; link a6,#0; lea PROF ## x,a0; \
+			jbsr mcount; unlk a6
+#endif
+#else
+#define	ENTRY(x)	.globl _ ## x; .even; _ ## x:
+#endif
+#if __GNUC__ >= 2
+#define SYSTRAP(x)	movl \#SYS_ ## x,d0; trap \#0
+#else
+#define SYSTRAP(x)	movl #SYS_ ## x,d0; trap #0
+#endif
+#else
+#ifdef PROF
 #define	ENTRY(x)	.globl _/**/x; .even; _/**/x:; .data; PROF/**/x:; \
 			.long 0; .text; link a6,#0; lea PROF/**/x,a0; \
 			jbsr mcount; unlk a6
 #else
-#define	ENTRY(x)	.globl _/**/x; .even; _/**/x:; .data; PROF/**/x:; \
-			.long 0; .text; lea PROF/**/x,a0; jbsr mcount
-#endif
-#else
 #define	ENTRY(x)	.globl _/**/x; .even; _/**/x:
-#endif PROF
-#define	SYSCALL(x)	.even; err: jmp cerror; ENTRY(x); movl #SYS_/**/x,d0; \
-			trap #0; jcs err
+#endif
+#define SYSTRAP(x)	movl #SYS_/**/x,d0; trap #0
+#endif
+
+#define	SYSCALL(x)	.even; err: jmp cerror; ENTRY(x); SYSTRAP(x); jcs err
 #define	RSYSCALL(x)	SYSCALL(x); rts
-#define	PSEUDO(x,y)	ENTRY(x); movl #SYS_/**/y,d0; trap #0; rts
+#define	PSEUDO(x,y)	ENTRY(x); SYSTRAP(y); rts
 
 #define	ASMSTR		.asciz
 
