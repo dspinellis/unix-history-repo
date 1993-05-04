694f41ad72f04e12513a3d62566757b974d80f48
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-05-04 02:29:57 -0800

    add e_msgboundary for MIME support
    SCCS-vsn: usr.sbin/sendmail/src/sendmail.h 6.60

--- usr/src/usr.sbin/sendmail/src/sendmail.h
+++ usr/src/usr.sbin/sendmail/src/sendmail.h
@@ -5,7 +5,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)sendmail.h	6.59 (Berkeley) %G%
+ *	@(#)sendmail.h	6.60 (Berkeley) %G%
  */
 
 /*
@@ -15,7 +15,7 @@
 # ifdef _DEFINE
 # define EXTERN
 # ifndef lint
-static char SmailSccsId[] =	"@(#)sendmail.h	6.59		%G%";
+static char SmailSccsId[] =	"@(#)sendmail.h	6.60		%G%";
 # endif
 # else /*  _DEFINE */
 # define EXTERN extern
