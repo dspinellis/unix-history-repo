18ef0102b94a90875a8748329b8f7d896a4b2a98
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-05-03 19:25:14 -0800

    clean up extra "C" line in qf file; first cut at new alias scheme
    SCCS-vsn: usr.sbin/sendmail/src/queue.c 6.54

--- usr/src/usr.sbin/sendmail/src/queue.c
+++ usr/src/usr.sbin/sendmail/src/queue.c
@@ -10,9 +10,9 @@
 
 #ifndef lint
 #ifdef QUEUE
-static char sccsid[] = "@(#)queue.c	6.53 (Berkeley) %G% (with queueing)";
+static char sccsid[] = "@(#)queue.c	6.54 (Berkeley) %G% (with queueing)";
 #else
-static char sccsid[] = "@(#)queue.c	6.53 (Berkeley) %G% (without queueing)";
+static char sccsid[] = "@(#)queue.c	6.54 (Berkeley) %G% (without queueing)";
 #endif
 #endif /* not lint */
 
@@ -158,7 +158,7 @@ notemp:
 	fprintf(f, "S%s\n", CurEnv->e_from.q_paddr);
 
 	/* output list of error recipients */
-	printctladdr(NULL, tfp);
+	printctladdr(NULL, NULL);
 	for (q = e->e_errorqueue; q != NULL; q = q->q_next)
 	{
 		if (!bitset(QDONTSEND|QBADADDR, q->q_flags))
@@ -239,9 +239,9 @@ printctladdr(a, tfp)
 	extern ADDRESS *getctladdr();
 
 	/* initialization */
-	if (a == NULL)
+	if (a == NULL || tfp == NULL)
 	{
-		if (lastctladdr != NULL)
+		if (lastctladdr != NULL && tfp != NULL)
 			fprintf(tfp, "C\n");
 		lastctladdr = NULL;
 		lastuid = 0;
@@ -378,7 +378,7 @@ runqueue(forkflag)
 	**  Make sure the alias database is open.
 	*/
 
-	initaliases(AliasFile, FALSE, e);
+	initaliases(FALSE, e);
 
 	/*
 	**  Start making passes through the queue.
