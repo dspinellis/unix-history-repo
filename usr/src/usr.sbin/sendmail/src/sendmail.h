de6623588c24fa2724bf374a70055ae1e0ae24b8
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-05-04 02:29:57 -0800

    add e_msgboundary for MIME support
    SCCS-vsn: usr.sbin/sendmail/src/sendmail.h 6.60

--- usr/src/usr.sbin/sendmail/src/sendmail.h
+++ usr/src/usr.sbin/sendmail/src/sendmail.h
@@ -5,7 +5,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)sendmail.h	6.59 (Berkeley) %G%
+ *	@(#)sendmail.h	6.60 (Berkeley) %G%
  */
 
 /*
@@ -15,7 +15,7 @@
 # ifdef _DEFINE
 # define EXTERN
 # ifndef lint
-static char SmailSccsId[] =	"@(#)sendmail.h	6.59		%G%";
+static char SmailSccsId[] =	"@(#)sendmail.h	6.60		%G%";
 # endif
 # else /*  _DEFINE */
 # define EXTERN extern
@@ -294,6 +294,7 @@ ENVELOPE
 	FILE		*e_qfp;		/* queue control file */
 	char		*e_message;	/* error message */
 	char		*e_statmsg;	/* stat msg (changes per delivery) */
+	char		*e_msgboundary;	/* MIME-style message part boundary */
 	char		*e_macro[128];	/* macro definitions */
 };
 
