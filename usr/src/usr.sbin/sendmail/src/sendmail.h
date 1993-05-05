f1ccc6fa36383f70b2cbf8c3a909b2b0cad4bcf3
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-05-04 19:17:52 -0800

    putbody takes one more parameter
    SCCS-vsn: usr.sbin/sendmail/src/sendmail.h 6.62

--- usr/src/usr.sbin/sendmail/src/sendmail.h
+++ usr/src/usr.sbin/sendmail/src/sendmail.h
@@ -5,7 +5,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)sendmail.h	6.61 (Berkeley) %G%
+ *	@(#)sendmail.h	6.62 (Berkeley) %G%
  */
 
 /*
@@ -15,7 +15,7 @@
 # ifdef _DEFINE
 # define EXTERN
 # ifndef lint
-static char SmailSccsId[] =	"@(#)sendmail.h	6.61		%G%";
+static char SmailSccsId[] =	"@(#)sendmail.h	6.62		%G%";
 # endif
 # else /*  _DEFINE */
 # define EXTERN extern
