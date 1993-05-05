2097d467ee61a3272074838647ad38d4cee61ad7
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-05-05 03:41:47 -0800

    adjust setproctitle for 4.4BSD (to avoid problem with warn())
    SCCS-vsn: usr.sbin/sendmail/src/conf.c 6.53

--- usr/src/usr.sbin/sendmail/src/conf.c
+++ usr/src/usr.sbin/sendmail/src/conf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)conf.c	6.52 (Berkeley) %G%";
+static char sccsid[] = "@(#)conf.c	6.53 (Berkeley) %G%";
 #endif /* not lint */
 
 # include <sys/ioctl.h>
