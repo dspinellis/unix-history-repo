b87031fc7a8f8c46ab1e3e10034100e0acf55955
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-05-04 19:10:02 -0800

    use sysctl(3), don't roll our own
    SCCS-vsn: lib/libc/gen/confstr.c 5.3

--- usr/src/lib/libc/gen/confstr.c
+++ usr/src/lib/libc/gen/confstr.c
@@ -6,11 +6,15 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)confstr.c	5.2 (Berkeley) %G%";
+static char sccsid[] = "@(#)confstr.c	5.3 (Berkeley) %G%";
 #endif /* LIBC_SCCS and not lint */
+
+#include <sys/param.h>
+#include <sys/sysctl.h>
 
 #include <errno.h>
 #include <paths.h>
+#include <stdlib.h>
 #include <unistd.h>
 
 size_t
@@ -19,13 +23,34 @@ confstr(name, buf, len)
 	char *buf;
 	size_t len;
 {
+	size_t tlen;
+	int mib[2], sverrno;
+	char *p;
+
 	switch (name) {
 	case _CS_PATH:
+		mib[0] = CTL_USER;
+		mib[1] = USER_CS_PATH;
+		if (sysctl(mib, 2, NULL, &tlen, NULL, 0) == -1)
+			return (-1);
 		if (len != 0 && buf != NULL) {
-			(void)strncpy(buf, _PATH_STDPATH, len - 1);
+			if ((p = malloc(tlen)) == NULL)
+				return (-1);
+			if (sysctl(mib, 2, p, &tlen, NULL, 0) == -1) {
+				sverrno = errno;
+				free(p);
+				errno = sverrno;
+				return (-1);
+			}
+			/*
+			 * POSIX 1003.2 requires partial return of
+			 * the string -- that should be *real* useful.
+			 */
+			(void)strncpy(buf, p, len - 1);
 			buf[len - 1] = '\0';
+			free(p);
 		}
-		return (sizeof(_PATH_STDPATH));
+		return (tlen);
 	default:
 		errno = EINVAL;
 		return (0);
