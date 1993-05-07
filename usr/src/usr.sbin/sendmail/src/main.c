396d55416a1104782736408ad9c7900f10a18c38
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-05-06 17:24:24 -0800

    fix bug causing mail to be returned in -bv mode
    SCCS-vsn: usr.sbin/sendmail/src/main.c 6.62

--- usr/src/usr.sbin/sendmail/src/main.c
+++ usr/src/usr.sbin/sendmail/src/main.c
@@ -13,7 +13,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)main.c	6.61 (Berkeley) %G%";
+static char sccsid[] = "@(#)main.c	6.62 (Berkeley) %G%";
 #endif /* not lint */
 
 #define	_DEFINE
@@ -955,7 +955,10 @@ main(argc, argv, envp)
 		finis();
 	}
 	if (OpMode == MD_VERIFY)
+	{
 		CurEnv->e_sendmode = SM_VERIFY;
+		CurEnv->e_errormode = EM_QUIET;
+	}
 
 	/*
 	**  Scan argv and deliver the message to everyone.
@@ -998,7 +1001,8 @@ main(argc, argv, envp)
 	sendall(CurEnv, SM_DEFAULT);
 
 	/*
-	** All done.
+	**  All done.
+	**	Don't send return error message if in VERIFY mode.
 	*/
 
 	finis();
