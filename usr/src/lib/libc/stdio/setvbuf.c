02f609f6ba2fba65d4e410d346bf3527501da345
Chris Torek <torek@ucbvax.Berkeley.EDU>
1993-05-07 05:48:01 -0800

    rewrite: always allocate buffer immediately; always honor user mode flags; set fseek optimization iff new buffer is proper size
    SCCS-vsn: lib/libc/stdio/setvbuf.c 5.5

--- usr/src/lib/libc/stdio/setvbuf.c
+++ usr/src/lib/libc/stdio/setvbuf.c
@@ -9,7 +9,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)setvbuf.c	5.4 (Berkeley) %G%";
+static char sccsid[] = "@(#)setvbuf.c	5.5 (Berkeley) %G%";
 #endif /* LIBC_SCCS and not lint */
 
 #include <stdio.h>
@@ -27,81 +27,97 @@ setvbuf(fp, buf, mode, size)
 	register size_t size;
 {
 	register int ret, flags;
+	size_t iosize;
+	int ttyflag;
 
 	/*
 	 * Verify arguments.  The `int' limit on `size' is due to this
-	 * particular implementation.
+	 * particular implementation.  Note, buf and size are ignored
+	 * when setting _IONBF.
 	 */
-	if ((mode != _IOFBF && mode != _IOLBF && mode != _IONBF) ||
-	    (int)size < 0)
-		return (EOF);
+	if (mode != _IONBF)
+		if ((mode != _IOFBF && mode != _IOLBF) || (int)size < 0)
+			return (EOF);
 
 	/*
-	 * OK so far.  Write current buffer, if any; drop read count, if
-	 * any.  Make sure putc() will not think fp is line buffered.  Free
-	 * old buffer if it was from malloc().  Clear line and non-buffer
-	 * flags, and clear malloc flag.
+	 * Write current buffer, if any.  Discard unread input, cancel
+	 * line buffering, and free old buffer if malloc()ed.
 	 */
 	ret = 0;
-	(void) __sflush(fp);
-	fp->_r = 0;
-	fp->_lbfsize = 0;
+	(void)__sflush(fp);
+	fp->_r = fp->_lbfsize = 0;
 	flags = fp->_flags;
 	if (flags & __SMBF)
 		free((void *)fp->_bf._base);
-	flags &= ~(__SLBF | __SNBF | __SMBF);
+	flags &= ~(__SLBF | __SNBF | __SMBF | __SOPT | __SNPT);
 
-	if (size == 0)
-		buf = NULL;	/* we will make a real one later */
-	else if (buf == NULL) {
-		/*
-		 * Caller wants specific buffering mode and size but did
-		 * not provide a buffer.  Produce one of the given size.
-		 * If that fails, set the size to 0 and continue, so that
-		 * we will try again later with a system-supplied size
-		 * (failure here is probably from someone with the bogus
-		 * idea that larger is always better, asking for many MB),
-		 * but return EOF to indicate failure.
-		 */
+	/* If setting unbuffered mode, skip all the hard work. */
+	if (mode == _IONBF)
+		goto nbf;
+
+	/*
+	 * Find optimal I/O size for seek optimization.  This also returns
+	 * a `tty flag' to suggest that we check isatty(fd), but we do not
+	 * care since our caller told us how to buffer.
+	 */
+	flags |= __swhatbuf(fp, &iosize, &ttyflag);
+	if (size == 0) {
+		buf = NULL;	/* force local allocation */
+		size = iosize;
+	}
+
+	/* Allocate buffer if needed. */
+	if (buf == NULL) {
 		if ((buf = malloc(size)) == NULL) {
+			/*
+			 * Unable to honor user's request.  We will return
+			 * failure, but try again with file system size.
+			 */
 			ret = EOF;
-			size = 0;
-		} else
-			flags |= __SMBF;
+			if (size != iosize) {
+				size = iosize;
+				buf = malloc(size);
+			}
+		}
+		if (buf == NULL) {
+			/* No luck; switch to unbuffered I/O. */
+nbf:
+			fp->_flags = flags | __SNBF;
+			fp->_w = 0;
+			fp->_bf._base = fp->_p = fp->_nbuf;
+			fp->_bf._size = 1;
+			return (ret);
+		}
+		flags |= __SMBF;
 	}
 
 	/*
-	 * Now put back whichever flag is needed, and fix _lbfsize if line
-	 * buffered.  Ensure output flush on exit if the stream will be
-	 * buffered at all.
+	 * Kill any seek optimization if the buffer is not the
+	 * right size.
+	 *
+	 * SHOULD WE ALLOW MULTIPLES HERE (i.e., ok iff (size % iosize) == 0)?
 	 */
-	switch (mode) {
+	if (size != iosize)
+		flags |= __SNPT;
 
-	case _IONBF:
-		flags |= __SNBF;
-		fp->_bf._base = fp->_p = fp->_nbuf;
-		fp->_bf._size = 1;
-		break;
-
-	case _IOLBF:
+	/*
+	 * Fix up the FILE fields, and set __cleanup for output flush on
+	 * exit (since we are buffered in some way).  Note that _w can
+	 * always be set to 0 safely here---it should be 0 in read mode
+	 * or the `indeterminate' state, and 0 for line buffered---so
+	 * using the new buffer size for fully-buffered streams in write
+	 * mode is merely a tiny optimization.
+	 */
+	if (mode == _IOLBF) {
 		flags |= __SLBF;
 		fp->_lbfsize = -size;
-		/* FALLTHROUGH */
-
-	case _IOFBF:
-		/* no flag */
-		__cleanup = _cleanup;
-		fp->_bf._base = fp->_p = (unsigned char *)buf;
-		fp->_bf._size = size;
-		break;
-	}
-
-	/*
-	 * Patch up write count if necessary.
-	 */
-	if (flags & __SWR)
-		fp->_w = flags & (__SLBF | __SNBF) ? 0 : size;
+		fp->_w = 0;
+	} else
+		fp->_w = flags & __SWR ? size : 0;
 	fp->_flags = flags;
+	fp->_bf._base = fp->_p = (unsigned char *)buf;
+	fp->_bf._size = size;
+	__cleanup = _cleanup;
 
 	return (ret);
 }
