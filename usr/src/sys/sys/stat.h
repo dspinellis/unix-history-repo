c405f7baa566efbf2451b6583948781c9dd527d6
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1993-05-10 22:01:20 -0800

    make definitions of user and super-user changeable flags
    SCCS-vsn: sys/sys/stat.h 7.20

--- usr/src/sys/sys/stat.h
+++ usr/src/sys/sys/stat.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)stat.h	7.19 (Berkeley) %G%
+ *	@(#)stat.h	7.20 (Berkeley) %G%
  */
 
 #include <sys/time.h>
@@ -108,14 +108,16 @@ struct stat {
 /*
  * Definitions of flags stored in file flags word.
  *
- * Low 16-bits: super-user and owner settable.
+ * Super-user and owner changeable flags.
  */
+#define	UF_SETTABLE	0x0000ffff	/* mask of owner changeable flags */
 #define	UF_NODUMP	0x00000001	/* do not dump file */
 #define	UF_IMMUTABLE	0x00000002	/* file may not be changed */
 #define	UF_APPEND	0x00000004	/* writes to file may only append */
 /*
- * High 16-bits: super-user settable.
+ * Super-user changeable flags.
  */
+#define	SF_SETTABLE	0xffff0000	/* mask of superuser changeable flags */
 #define	SF_ARCHIVED	0x00010000	/* file is archived */
 #define	SF_IMMUTABLE	0x00020000	/* file may not be changed */
 #define	SF_APPEND	0x00040000	/* writes to file may only append */
