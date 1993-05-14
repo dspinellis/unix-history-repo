a26636bdb5fa2d2157ea981af6fd156ae7d19bb2
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-05-13 16:41:15 -0800

    fix SMTP display bug; allow SMTP greeting failure to go to next MX host; tweak MIME headers; fix problem causing buffer overruns in map lookups; fix split header/envelope rewriting rule parsing; disable VERB if PRIV_NOEXPN set
    SCCS-vsn: usr.sbin/sendmail/src/version.c 6.58

--- usr/src/usr.sbin/sendmail/src/version.c
+++ usr/src/usr.sbin/sendmail/src/version.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)version.c	6.57 (Berkeley) %G%";
+static char sccsid[] = "@(#)version.c	6.58 (Berkeley) %G%";
 #endif /* not lint */
 
-char	Version[] = "ALPHA-6.57";
+char	Version[] = "ALPHA-6.58";
