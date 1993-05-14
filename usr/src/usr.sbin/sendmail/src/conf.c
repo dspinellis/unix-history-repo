1594a8c70b42f27ff9c9897fc1de3860ac0b9dc4
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-05-13 17:43:46 -0800

    try to make setproctitle port to NET/bsd and BSD/386 better
    SCCS-vsn: usr.sbin/sendmail/src/conf.c 6.55

--- usr/src/usr.sbin/sendmail/src/conf.c
+++ usr/src/usr.sbin/sendmail/src/conf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)conf.c	6.54 (Berkeley) %G%";
+static char sccsid[] = "@(#)conf.c	6.55 (Berkeley) %G%";
 #endif /* not lint */
 
 # include <sys/ioctl.h>
