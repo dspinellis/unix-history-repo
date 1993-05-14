b7dc3011874318c90eda6b46fdac17ecb3f274aa
Chris Torek <torek@ucbvax.Berkeley.EDU>
1993-05-13 23:50:49 -0800

    obey local timezone file (a bit gross, but it seems to work)
    SCCS-vsn: lib/libcompat/4.1/ftime.c 5.4

--- usr/src/lib/libcompat/4.1/ftime.c
+++ usr/src/lib/libcompat/4.1/ftime.c
@@ -6,7 +6,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)ftime.c	5.3 (Berkeley) %G%";
+static char sccsid[] = "@(#)ftime.c	5.4 (Berkeley) %G%";
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/types.h>
@@ -29,11 +29,23 @@ ftime(tp)
 {
 	struct timeval t;
 	struct timezone tz;
+	struct tm *tm;
+	time_t zero;
 
 	if (gettimeofday(&t, &tz) < 0)
 		return (-1);
 	tp->time = t.tv_sec;
 	tp->millitm = t.tv_usec / 1000;
-	tp->timezone = tz.tz_minuteswest;
-	tp->dstflag = tz.tz_dsttime;
+	if ((tm = localtime(&tp->time)) == NULL) {
+		/* in the absence of anything better, use kernel's timezone */
+		tp->timezone = tz.tz_minuteswest;
+		tp->dstflag = tz.tz_dsttime;
+	} else {
+		tp->dstflag = tm->tm_isdst;
+		if (tm->tm_isdst) {	/* tm_gmtoff has an offset applied */
+			zero = 0;	/* try 0 and hope for the best */
+			tp->timezone = -localtime(&zero)->tm_gmtoff / 60;
+		} else
+			tp->timezone = -tm->tm_gmtoff / 60;
+	}
 }
