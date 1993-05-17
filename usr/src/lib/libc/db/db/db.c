20f8f8ea4a43c488e28089b1c14c49a932854f3f
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-05-16 23:19:43 -0800

    was going to add a flags argument to the sync call, rewrote it instead to just cast the return-error routine
    SCCS-vsn: lib/libc/db/db/db.c 5.5

--- usr/src/lib/libc/db/db/db.c
+++ usr/src/lib/libc/db/db/db.c
@@ -37,11 +37,11 @@ dbopen(fname, flags, mode, type, openinfo)
 	return (NULL);
 }
 
-static int __db_edel __P((const DB *, const DBT *, u_int));
-static int __db_eget __P((const DB *, const DBT *, DBT *, u_int));
-static int __db_eput __P((const DB *dbp, DBT *, const DBT *, u_int));
-static int __db_eseq __P((const DB *, DBT *, DBT *, u_int));
-static int __db_esync __P((const DB *));
+static int
+__dberr()
+{
+	return (RET_ERROR);
+}
 
 /*
  * __DBPANIC -- Stop.
@@ -54,54 +54,9 @@ __dbpanic(dbp)
 	DB *dbp;
 {
 	/* The only thing that can succeed is a close. */
-	dbp->del = __db_edel;
-	dbp->get = __db_eget;
-	dbp->put = __db_eput;
-	dbp->seq = __db_eseq;
-	dbp->sync = __db_esync;
-}
-
-static int
-__db_edel(dbp, key, flags)
-	const DB *dbp;
-	const DBT *key;
-	u_int flags;
-{
-	return (RET_ERROR);
-}
-
-static int
-__db_eget(dbp, key, data, flag)
-	const DB *dbp;
-	const DBT *key;
-	DBT *data;
-	u_int flag;
-{
-	return (RET_ERROR);
-}
-
-static int
-__db_eput(dbp, key, data, uflags)
-	const DB *dbp;
-	DBT *key;
-	const DBT *data;
-	u_int uflags;
-{
-	return (RET_ERROR);
-}
-
-static int
-__db_eseq(dbp, key, data, flags)
-	const DB *dbp;
-	DBT *key, *data;
-	u_int flags;
-{
-	return (RET_ERROR);
-}
-
-static int
-__db_esync(dbp)
-	const DB *dbp;
-{
-	return (RET_ERROR);
+	dbp->del = (int (*)())__dberr;
+	dbp->get = (int (*)())__dberr;
+	dbp->put = (int (*)())__dberr;
+	dbp->seq = (int (*)())__dberr;
+	dbp->sync = (int (*)())__dberr;
 }
