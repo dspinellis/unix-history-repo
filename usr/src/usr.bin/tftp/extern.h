c2aee344cc994132554232e185b1bee90a0f75ed
Chris Torek <torek@ucbvax.Berkeley.EDU>
1993-05-16 23:10:21 -0800

    date and time created 93/05/16 16:10:21 by torek
    SCCS-vsn: usr.bin/tftp/extern.h 5.1

---
+++ usr/src/usr.bin/tftp/extern.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 1993 Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.redist.c%
+ *
+ *	@(#)extern.h	5.1 (Berkeley) %G%
+ */
+
+void	recvfile __P((int, char *, char *));
+void	sendfile __P((int, char *, char *));
