1ef6e61a6915bd67d8a7cd211fce21437ebb8f5b
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-05-17 17:56:18 -0800

    type conflicts
    SCCS-vsn: usr.sbin/sendmail/src/main.c 6.64
    SCCS-vsn: usr.sbin/sendmail/src/err.c 6.19
    SCCS-vsn: usr.sbin/sendmail/src/deliver.c 6.77

--- usr/src/usr.sbin/sendmail/src/main.c
+++ usr/src/usr.sbin/sendmail/src/main.c
@@ -13,7 +13,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)main.c	6.63 (Berkeley) %G%";
+static char sccsid[] = "@(#)main.c	6.64 (Berkeley) %G%";
 #endif /* not lint */
 
 #define	_DEFINE
