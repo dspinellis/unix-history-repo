903afb6ef5c5160d2a8b934a36510f5ff13360c5
Chris Torek <torek@ucbvax.Berkeley.EDU>
1993-05-18 06:01:44 -0800

    hack to make it compile: declare lseek only #ifndef lseek
    SCCS-vsn: usr.bin/uucp/includes/uucp.h 5.21

--- usr/src/usr.bin/uucp/includes/uucp.h
+++ usr/src/usr.bin/uucp/includes/uucp.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.proprietary.c%
  *
- *	@(#)uucp.h	5.20 (Berkeley) %G%
+ *	@(#)uucp.h	5.21 (Berkeley) %G%
  */
 
 #include <stdio.h>
@@ -399,7 +399,9 @@ extern	char DLocal[], DLocalX[], *subfile(), *subdir();
 extern	char *ttyname(), *strcpy(), *strcat(), *index(), *rindex(),
 		*fgets(), *calloc(), *malloc(), *fdig(), *ttyname(),
 		*cfgets(), *getwd(), *strpbrk(), *strncpy();
+#ifndef lseek		/* XXX */
 extern	long lseek();
+#endif
 extern time_t time();
 
 extern char _FAILED[], CANTOPEN[], DEVNULL[];
