20b98a958aae67844751ebbd57449ec2ad806bc9
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-05-17 17:04:12 -0800

    lint, ANSI C prototypes, index->strchr
    SCCS-vsn: libexec/bugfiler/bugfiler.c 5.18
    SCCS-vsn: libexec/bugfiler/error.c 5.9
    SCCS-vsn: libexec/bugfiler/gethead.c 5.11
    SCCS-vsn: libexec/bugfiler/process.c 5.10
    SCCS-vsn: libexec/bugfiler/redist.c 5.13
    SCCS-vsn: libexec/bugfiler/reply.c 5.10

--- usr/src/libexec/bugfiler/error.c
+++ usr/src/libexec/bugfiler/error.c
@@ -6,15 +6,19 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)error.c	5.8 (Berkeley) %G%";
+static char sccsid[] = "@(#)error.c	5.9 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/param.h>
+
 #include <dirent.h>
-#include <syslog.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <syslog.h>
+
 #include "bug.h"
+#include "extern.h"
 
 static short	err_redir;			/* stderr redirected */
 
@@ -22,6 +26,7 @@ static short	err_redir;			/* stderr redirected */
  * seterr --
  *	redirect stderr for error processing
  */
+void
 seterr()
 {
 	if (!freopen(ERROR_FILE, "a", stderr))
@@ -33,12 +38,12 @@ seterr()
  * error --
  *	write errors to log file and die
  */
+void
 error(fmt, arg)
 	register char	*fmt,
 			*arg;
 {
 	static char	logmsg[MAXLINELEN];	/* syslog message */
-	char	*strcpy(), *strcat();
 
 	if (err_redir) {
 		/* don't combine these, "fmt" may not require "arg" */
--- usr/src/libexec/bugfiler/process.c
+++ usr/src/libexec/bugfiler/process.c
@@ -6,32 +6,37 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)process.c	5.9 (Berkeley) %G%";
+static char sccsid[] = "@(#)process.c	5.10 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/param.h>
 #include <sys/time.h>
-#include <fcntl.h>
-#include <dirent.h>
-#include <unistd.h>
-#include <stdio.h>
+
 #include <ctype.h>
+#include <dirent.h>
+#include <fcntl.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+
 #include "bug.h"
+#include "extern.h"
 
 char	pfile[MAXPATHLEN];			/* permanent file name */
+
+static int getnext __P((void));
 
 /*
  * process --
  *	copy report to permanent file,
  *	update summary file.
  */
+int
 process()
 {
 	register int	rval;			/* read return value */
 	struct timeval	tp;			/* time of day */
 	int	lfd;				/* lock file descriptor */
-	static int getnext();
 
 	if (access(LOCK_FILE, R_OK) || (lfd = open(LOCK_FILE, O_RDONLY, 0)) < 0)
 		error("can't find lock file %s.", LOCK_FILE);
@@ -52,7 +57,9 @@ process()
 		error("can't append to summary file %s.", bfr);
 	if (gettimeofday(&tp, (struct timezone *)NULL))
 		error("can't get time of day.", CHN);
-	printf("\n%s\t\t%s\t%s\t%s\tOwner: Bugs Bunny\n\tStatus: Received\n", pfile, ctime(&tp.tv_sec), mailhead[INDX_TAG].line, mailhead[SUBJ_TAG].found ? mailhead[SUBJ_TAG].line : "Subject:\n");
+	printf("\n%s\t\t%s\t%s\t%s\tOwner: Bugs Bunny\n\tStatus: Received\n",
+	    pfile, ctime(&tp.tv_sec), mailhead[INDX_TAG].line,
+	    mailhead[SUBJ_TAG].found ? mailhead[SUBJ_TAG].line : "Subject:\n");
 	(void)flock(lfd, LOCK_UN);
 	(void)fclose(stdout);
 }
