abdaeaabd65c38fd2a613861741eb5afb4f289c9
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1993-05-17 22:30:43 -0800

    old get/setrlimt are COMPAT_43
    SCCS-vsn: sys/hp/hpux/hpux_compat.c 7.35

--- usr/src/sys/hp/hpux/hpux_compat.c
+++ usr/src/sys/hp/hpux/hpux_compat.c
@@ -11,7 +11,7 @@
  *
  * from: Utah $Hdr: hpux_compat.c 1.55 92/12/26$
  *
- *	@(#)hpux_compat.c	7.34 (Berkeley) %G%
+ *	@(#)hpux_compat.c	7.35 (Berkeley) %G%
  */
 
 /*
@@ -1456,7 +1456,7 @@ hpuxgetrlimit(p, uap, retval)
 		return (EINVAL);
 	if (uap->which == HPUXRLIMIT_NOFILE)
 		uap->which = RLIMIT_NOFILE;
-	return (getrlimit(p, uap, retval));
+	return (ogetrlimit(p, uap, retval));
 }
 
 hpuxsetrlimit(p, uap, retval)
@@ -1468,7 +1468,7 @@ hpuxsetrlimit(p, uap, retval)
 		return (EINVAL);
 	if (uap->which == HPUXRLIMIT_NOFILE)
 		uap->which = RLIMIT_NOFILE;
-	return (setrlimit(p, uap, retval));
+	return (osetrlimit(p, uap, retval));
 }
 
 /*
