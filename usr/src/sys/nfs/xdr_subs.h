05377dac41052dbdaefac76aeb2064924e220338
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1993-05-17 21:10:27 -0800

    update from Torek/Macklem for machines requiring quad alignment
    SCCS-vsn: sys/nfs/xdr_subs.h 7.7

--- usr/src/sys/nfs/xdr_subs.h
+++ usr/src/sys/nfs/xdr_subs.h
@@ -7,80 +7,46 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)xdr_subs.h	7.6 (Berkeley) %G%
+ *	@(#)xdr_subs.h	7.7 (Berkeley) %G%
  */
 
 /*
  * Macros used for conversion to/from xdr representation by nfs...
  * These use the MACHINE DEPENDENT routines ntohl, htonl
  * As defined by "XDR: External Data Representation Standard" RFC1014
+ *
+ * To simplify the implementation, we use ntohl/htonl even on big-endian
+ * machines, and count on them being `#define'd away.  Some of these
+ * might be slightly more efficient as quad_t copies on a big-endian,
+ * but we cannot count on their alignment anyway.
  */
-#if BYTE_ORDER == LITTLE_ENDIAN
-#define fxdr_unsigned(t, v)	((t)ntohl((long)(v)))
+
+#define	fxdr_unsigned(t, v)	((t)ntohl((long)(v)))
+#define	txdr_unsigned(v)	(htonl((long)(v)))
+
 #define	fxdr_nfstime(f, t) { \
-	(t)->ts_sec = \
-		ntohl(((struct nfsv2_time *)(f))->nfs_sec); \
-	(t)->ts_nsec = 1000 * \
-		ntohl(((struct nfsv2_time *)(f))->nfs_usec); \
+	(t)->ts_sec = ntohl(((struct nfsv2_time *)(f))->nfs_sec); \
+	(t)->ts_nsec = 1000 * ntohl(((struct nfsv2_time *)(f))->nfs_usec); \
+}
+#define	txdr_nfstime(f, t) { \
+	((struct nfsv2_time *)(t))->nfs_sec = htonl((f)->ts_sec); \
+	((struct nfsv2_time *)(t))->nfs_usec = htonl((f)->ts_nsec) / 1000; \
 }
 
 #define	fxdr_nqtime(f, t) { \
-	(t)->ts_sec = \
-		ntohl(((struct nqnfs_time *)(f))->nq_sec); \
-	(t)->ts_nsec = \
-		ntohl(((struct nqnfs_time *)(f))->nq_nsec); \
+	(t)->ts_sec = ntohl(((struct nqnfs_time *)(f))->nq_sec); \
+	(t)->ts_nsec = ntohl(((struct nqnfs_time *)(f))->nq_nsec); \
+}
+#define	txdr_nqtime(f, t) { \
+	((struct nqnfs_time *)(t))->nq_sec = htonl((f)->ts_sec); \
+	((struct nqnfs_time *)(t))->nq_nsec = htonl((f)->ts_nsec); \
 }
 
-/*
- * To handle quad conversions, define a struct of two longs and use
- * ntohl and htonl. Maybe someday there should be ntohq and htonq?
- */
-union _hq {
-	quad_t	hq;
-	struct {
-		long val[2];
-	} lq;
-};
 #define	fxdr_hyper(f, t) { \
-	((union _hq *)(t))->lq.val[1] = ntohl(((union _hq *)(f))->lq.val[0]); \
-	((union _hq *)(t))->lq.val[0] = ntohl(((union _hq *)(f))->lq.val[1]); \
+	((long *)(t))[_QUAD_HIGHWORD] = ntohl(((long *)(f))[0]); \
+	((long *)(t))[_QUAD_LOWWORD] = ntohl(((long *)(f))[1]); \
 }
 #define	txdr_hyper(f, t) { \
-	((union _hq *)(t))->lq.val[0] = htonl(((union _hq *)(f))->lq.val[1]); \
-	((union _hq *)(t))->lq.val[1] = htonl(((union _hq *)(f))->lq.val[0]); \
+	((long *)(t))[0] = htonl(((long *)(f))[_QUAD_HIGHWORD]); \
+	((long *)(t))[1] = htonl(((long *)(f))[_QUAD_LOWWORD]); \
 }
-
-#define	txdr_unsigned(v)	(htonl((long)(v)))
-#define	txdr_nqtime(f, t) { \
-	((struct nqnfs_time *)(t))->nq_sec = \
-		htonl((f)->ts_sec); \
-	((struct nqnfs_time *)(t))->nq_nsec = \
-		htonl((f)->ts_nsec); \
-}
-#define	txdr_nfstime(f, t) { \
-	((struct nfsv2_time *)(t))->nfs_sec = \
-		htonl((f)->ts_sec); \
-	((struct nfsv2_time *)(t))->nfs_usec = \
-		htonl((f)->ts_nsec) / 1000; \
-}
-#else	/* BIG_ENDIAN */
-#define fxdr_unsigned(t, v)	((t)(v))
-#define	fxdr_nqtime(f, t) \
-	*(t) = *((struct timespec *)(f))
-#define	fxdr_nfstime(f, t) { \
-	(t)->ts_sec = ((struct nfsv2_time *)(f))->nfs_sec; \
-	(t)->ts_nsec = ((struct nfsv2_time *)(f))->nfs_usec * 1000; \
-}
-#define	fxdr_hyper(f, t) \
-	*((quad_t *)(t)) = *((quad_t *)(f))
-
-#define	txdr_unsigned(v)	((long)(v))
-#define	txdr_nqtime(f, t) \
-	*(t) = *((struct nqnfs_time *)(f))
-#define	txdr_nfstime(f, t) { \
-	((struct nfsv2_time *)(t))->nfs_sec = (f)->ts_sec; \
-	((struct nfsv2_time *)(t))->nfs_usec = (f)->ts_nsec / 1000; \
-}
-#define	txdr_hyper(f, t) \
-	*((quad_t *)(t)) = *((quad_t *)(f))
-#endif	/* ENDIAN */
