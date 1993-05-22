901fa64fa8b00f0d172749b3938613c3d1838802
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-05-21 20:03:59 -0800

    Henry Spencer's Alpha 3.0 regex release
    SCCS-vsn: include/regex.h 5.3

--- usr/src/include/regex.h
+++ usr/src/include/regex.h
@@ -8,7 +8,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)regex.h	5.2 (Berkeley) %G%
+ *	@(#)regex.h	5.3 (Berkeley) %G%
  */
 
 #ifndef _REGEX_H_
@@ -20,6 +20,7 @@ typedef off_t regoff_t;
 typedef struct {
 	int re_magic;
 	size_t re_nsub;		/* number of parenthesized subexpressions */
+	const char *re_endp;	/* end pointer for REG_PEND */
 	struct re_guts *re_g;	/* none of your business :-) */
 } regex_t;
 
@@ -29,35 +30,42 @@ typedef struct {
 } regmatch_t;
 
 /* regcomp() flags */
-#define	REG_EXTENDED	001
-#define	REG_ICASE	002
-#define	REG_NOSUB	004
-#define	REG_NEWLINE	010
+#define	REG_BASIC	0000
+#define	REG_EXTENDED	0001
+#define	REG_ICASE	0002
+#define	REG_NOSUB	0004
+#define	REG_NEWLINE	0010
+#define	REG_NOSPEC	0020
+#define	REG_PEND	0040
+#define	REG_DUMP	0200
+
+/* regerror() flags */
+#define	REG_NOMATCH	 1
+#define	REG_BADPAT	 2
+#define	REG_ECOLLATE	 3
+#define	REG_ECTYPE	 4
+#define	REG_EESCAPE	 5
+#define	REG_ESUBREG	 6
+#define	REG_EBRACK	 7
+#define	REG_EPAREN	 8
+#define	REG_EBRACE	 9
+#define	REG_BADBR	10
+#define	REG_ERANGE	11
+#define	REG_ESPACE	12
+#define	REG_BADRPT	13
+#define	REG_EMPTY	14
+#define	REG_ASSERT	15
+#define	REG_INVARG	16
+#define	REG_ATOI	255	/* convert name to number (!) */
+#define	REG_ITOA	0400	/* convert number to name (!) */
 
 /* regexec() flags */
 #define	REG_NOTBOL	00001
 #define	REG_NOTEOL	00002
 #define	REG_STARTEND	00004
-#define	REG_TRACE	00400	/* debugging tracing */
-#define	REG_LARGE	01000	/* force large state model for debug */
-#define	REG_BACKR	02000
-
-/* errors */
-#define	REG_NOMATCH	(1)
-#define	REG_BADPAT	(2)
-#define	REG_ECOLLATE	(3)
-#define	REG_ECTYPE	(4)
-#define	REG_EESCAPE	(5)
-#define	REG_ESUBREG	(6)
-#define	REG_EBRACK	(7)
-#define	REG_EPAREN	(8)
-#define	REG_EBRACE	(9)
-#define	REG_BADBR	(10)
-#define	REG_ERANGE	(11)
-#define	REG_ESPACE	(12)
-#define	REG_BADRPT	(13)
-#define	REG_EMPTY	(14)	/* empty component */
-#define	REG_ASSERT	(15)	/* assertion failure */
+#define	REG_TRACE	00400	/* tracing of execution */
+#define	REG_LARGE	01000	/* force large representation */
+#define	REG_BACKR	02000	/* force use of backref code */
 
 #include <sys/cdefs.h>
 
