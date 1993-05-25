c3240c097ab977fc65d039be05f30823a5020f55
CSRG <csrg@ucbvax.Berkeley.EDU>
1993-05-25 03:54:44 -0800

    BSD 4_4_Lite1 development Work on file usr/src/contrib/gzip-1.2.4/primos/include/errno.h
    Synthesized-from: CSRG/cd2/4.4BSD-Lite1

---
+++ usr/src/contrib/gzip-1.2.4/primos/include/errno.h
@@ -0,0 +1,17 @@
+/*
+** errno.h
+**
+** Emulation of the Unix errno.h header file for PRIMOS
+**
+** Author: Peter Eriksson <pen@lysator.liu.se>
+*/
+
+#ifndef __ERRNO_H__
+#define __ERRNO_H__
+
+#include <errd.h>
+
+#define ENOENT e$fntf
+
+#endif
+
