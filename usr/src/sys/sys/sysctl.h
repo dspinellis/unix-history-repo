58bae0f8c15f11d3ac9f40d1e1318dc5dc81aa7b
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-05-24 22:26:26 -0800

    per-process limits are resources, not sysctl(3) variables
    SCCS-vsn: sys/sys/sysctl.h 7.27
    SCCS-vsn: lib/libc/gen/sysctl.3 6.10

--- usr/src/sys/sys/sysctl.h
+++ usr/src/sys/sys/sysctl.h
@@ -7,7 +7,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)sysctl.h	7.26 (Berkeley) %G%
+ *	@(#)sysctl.h	7.27 (Berkeley) %G%
  */
 
 #ifndef _SYS_SYSCTL_H_
@@ -110,9 +110,7 @@ struct ctlname {
 #define	KERN_NO_TRUNC		28	/* int: no path truncation */
 #define	KERN_VDISABLE		29	/* int: terminal character disable */
 #define	KERN_BOOTTIME		30	/* struct: time kernel was booted */
-#define	KERN_MAXUPROC	 	31	/* int: max processes per uid */
-#define	KERN_MAXUFILES	 	32	/* int: max open files per uid */
-#define	KERN_MAXID		33	/* number of valid kern ids */
+#define	KERN_MAXID		31	/* number of valid kern ids */
 
 #define CTL_KERN_NAMES { \
 	{ 0, 0 }, \
@@ -146,8 +144,6 @@ struct ctlname {
 	{ "no_trunc", CTLTYPE_INT }, \
 	{ "vdisable", CTLTYPE_INT }, \
 	{ "boottime", CTLTYPE_STRUCT }, \
-	{ "maxuproc", CTLTYPE_INT }, \
-	{ "maxufiles", CTLTYPE_INT }, \
 }
 
 /* 
