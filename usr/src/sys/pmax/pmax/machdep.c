dd9c1d4644857fc273e270257da4b330c8ca278b
Ralph Campbell <ralph@ucbvax.Berkeley.EDU>
1993-05-25 01:24:52 -0800

    added cpu_sysctl support for console
    SCCS-vsn: sys/pmax/pmax/machdep.c 7.21

--- usr/src/sys/pmax/pmax/machdep.c
+++ usr/src/sys/pmax/pmax/machdep.c
@@ -10,7 +10,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)machdep.c	7.20 (Berkeley) %G%
+ *	@(#)machdep.c	7.21 (Berkeley) %G%
  */
 
 /* from: Utah $Hdr: machdep.c 1.63 91/04/24$ */
@@ -30,8 +30,11 @@
 #include <sys/malloc.h>
 #include <sys/mbuf.h>
 #include <sys/msgbuf.h>
+#include <sys/ioctl.h>
+#include <sys/tty.h>
 #include <sys/user.h>
 #include <sys/exec.h>
+#include <sys/sysctl.h>
 #ifdef SYSVSHM
 #include <sys/shm.h>
 #endif
@@ -883,6 +886,33 @@ cpu_startup()
 	 * Configure the system.
 	 */
 	configure();
+}
+
+/*
+ * machine dependent system variables.
+ */
+cpu_sysctl(name, namelen, oldp, oldlenp, newp, newlen, p)
+	int *name;
+	u_int namelen;
+	void *oldp;
+	size_t *oldlenp;
+	void *newp;
+	size_t newlen;
+	struct proc *p;
+{
+
+	/* all sysctl names at this level are terminal */
+	if (namelen != 1)
+		return (ENOTDIR);		/* overloaded */
+
+	switch (name[0]) {
+	case CPU_CONSDEV:
+		return (sysctl_rdstruct(oldp, oldlenp, newp, &cn_tab.cn_dev,
+		    sizeof cn_tab.cn_dev));
+	default:
+		return (EOPNOTSUPP);
+	}
+	/* NOTREACHED */
 }
 
 /*
