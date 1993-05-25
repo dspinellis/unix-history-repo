de4604f173c64b3ee8589cf11c1981b2520c6cc1
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1993-05-25 04:23:00 -0800

    update NIC host name
    SCCS-vsn: usr.bin/whois/whois.c 5.12

--- usr/src/usr.bin/whois/whois.c
+++ usr/src/usr.bin/whois/whois.c
@@ -12,7 +12,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)whois.c	5.11 (Berkeley) %G%";
+static char sccsid[] = "@(#)whois.c	5.12 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -21,7 +21,7 @@ static char sccsid[] = "@(#)whois.c	5.11 (Berkeley) %G%";
 #include <netdb.h>
 #include <stdio.h>
 
-#define	NICHOST	"nic.ddn.mil"
+#define	NICHOST	"whois.internic.net"
 
 main(argc, argv)
 	int argc;
