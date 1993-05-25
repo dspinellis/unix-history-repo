cc9c275fe1e685c59276db83c19612298ded0dc4
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1993-05-25 01:24:12 -0800

    provide both pieces of getty info, roundup wait time on getty sleep
    SCCS-vsn: sbin/init/init.c 6.17

--- usr/src/sbin/init/init.c
+++ usr/src/sbin/init/init.c
@@ -15,7 +15,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)init.c	6.16 (Berkeley) %G%";
+static char sccsid[] = "@(#)init.c	6.17 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/param.h>
@@ -888,7 +888,8 @@ new_session(sprev, session_index, typ)
 	sp->se_device = malloc(sizeof(_PATH_DEV) + strlen(typ->ty_name));
 	(void) sprintf(sp->se_device, "%s%s", _PATH_DEV, typ->ty_name);
 
-	sp->se_getty = strdup(typ->ty_getty);
+	sp->se_getty = malloc(strlen(typ->ty_getty) + strlen(typ->ty_name) + 2);
+	(void) sprintf(sp->se_getty, "%s %s", typ->ty_getty, typ->ty_name);
 	sp->se_getty_argv = construct_argv(sp->se_getty);
 	if (sp->se_getty_argv == 0) {
 		warning("can't parse getty for port %s",
@@ -1014,7 +1015,7 @@ start_getty(sp)
 	    current_time - sp->se_started < GETTY_SPACING) {
 		warning("getty repeating too quickly on port %s, sleeping",
 		        sp->se_device);
-		sleep((unsigned) GETTY_SPACING -
+		sleep((unsigned) GETTY_SPACING + 1 -
 		      (current_time - sp->se_started));
 	}
 
