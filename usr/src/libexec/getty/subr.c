2a45c0f658d8c7df823380d62b74c4013e21bb61
Elan Amir <elan@ucbvax.Berkeley.EDU>
1993-05-27 23:08:07 -0800

    deleted getttytab.c; turn off debugging
    SCCS-vsn: libexec/getty/Makefile 5.13
    SCCS-vsn: libexec/getty/subr.c 5.13

--- usr/src/libexec/getty/subr.c
+++ usr/src/libexec/getty/subr.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)subr.c	5.12 (Berkeley) %G%";
+static char sccsid[] = "@(#)subr.c	5.13 (Berkeley) %G%";
 #endif /* not lint */
 
 /*
@@ -62,7 +62,6 @@ gettable(name, buf)
 			fp->value = 1 ^ fp->invrt;
 		}
 	}
-#define DEBUG
 #ifdef DEBUG
 	printf("name=\"%s\", buf=\"%s\"\n", name, buf);
 	for (sp = gettystrs; sp->field; sp++)
