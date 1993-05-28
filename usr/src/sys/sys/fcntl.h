f77ec6d638713f9336348e230a0e98e2d88fb7be
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1993-05-28 08:41:50 -0800

    reorganize to better alignment
    SCCS-vsn: sys/sys/fcntl.h 5.16

--- usr/src/sys/sys/fcntl.h
+++ usr/src/sys/sys/fcntl.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)fcntl.h	5.15 (Berkeley) %G%
+ *	@(#)fcntl.h	5.16 (Berkeley) %G%
  */
 
 #ifndef _FCNTL_H_
@@ -126,11 +126,11 @@
  * information passed to system by user
  */
 struct flock {
-	short	l_type;		/* lock type: read/write, etc. */
-	short	l_whence;	/* type of l_start */
 	off_t	l_start;	/* starting offset */
 	off_t	l_len;		/* len = 0 means until end of file */
 	pid_t	l_pid;		/* lock owner */
+	short	l_type;		/* lock type: read/write, etc. */
+	short	l_whence;	/* type of l_start */
 };
 
 
