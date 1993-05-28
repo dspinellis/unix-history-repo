202d157dd950e1ba7b148bb47b3658f1069b732b
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-05-28 15:09:19 -0800

    fix recursive syserr; lock NEWDB files during gets; fix host map overwrites alias conflict; defer opening of map files; some Solaris 2.1 updates (although it still doesn't work)
    SCCS-vsn: usr.sbin/sendmail/src/version.c 6.62

--- usr/src/usr.sbin/sendmail/src/version.c
+++ usr/src/usr.sbin/sendmail/src/version.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)version.c	6.61 (Berkeley) %G%";
+static char sccsid[] = "@(#)version.c	6.62 (Berkeley) %G%";
 #endif /* not lint */
 
-char	Version[] = "ALPHA-6.61";
+char	Version[] = "BETA-6.62";
