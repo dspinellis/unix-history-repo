5ef08c9784be68e5e3f712bfb7331570747d9b53
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-05-27 20:37:17 -0800

    put a backward compatible ctype in place, for old binaries
    SCCS-vsn: lib/libc/locale/ctype.c 5.9
    SCCS-vsn: lib/libc/locale/Makefile.inc 5.4

--- usr/src/lib/libc/locale/ctype.c
+++ usr/src/lib/libc/locale/ctype.c
@@ -6,12 +6,17 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)ctype.c	5.8 (Berkeley) %G%";
+static char sccsid[] = "@(#)ctype.c	5.9 (Berkeley) %G%";
 #endif /* LIBC_SCCS and not lint */
 
-#include <ctype.h>
-
-char *_ctype_ = __ctype;		/* Backward compatibility. */
+#define	_U	0x01
+#define	_L	0x02
+#define	_N	0x04
+#define	_S	0x08
+#define	_P	0x10
+#define	_C	0x20
+#define	_X	0x40
+#define	_B	0x80
 
 char __ctype[1 + 256] = {
 	0,
@@ -32,3 +37,5 @@ char __ctype[1 + 256] = {
 	_L,	_L,	_L,	_L,	_L,	_L,	_L,	_L,
 	_L,	_L,	_L,	_P,	_P,	_P,	_P,	_C
 };
+
+char *_ctype_ = __ctype;		/* Backward compatibility. */
