8e6c8494675cdab8ef4a4ea9c5b83bcdd31bf6b4
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-05-28 21:43:46 -0800

    catch wierd waitpid infinite loop
    SCCS-vsn: usr.sbin/sendmail/src/conf.c 6.61

--- usr/src/usr.sbin/sendmail/src/conf.c
+++ usr/src/usr.sbin/sendmail/src/conf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)conf.c	6.60 (Berkeley) %G%";
+static char sccsid[] = "@(#)conf.c	6.61 (Berkeley) %G%";
 #endif /* not lint */
 
 # include <sys/ioctl.h>
