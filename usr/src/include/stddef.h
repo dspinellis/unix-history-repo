b2a7761144c1c040879eb0af0e9bd15791eb68e0
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-05-28 19:25:01 -0800

    don't typedef run_t if _ANSI_SOURCE defined
    SCCS-vsn: include/stdlib.h 5.28
    SCCS-vsn: include/stddef.h 5.8

--- usr/src/include/stddef.h
+++ usr/src/include/stddef.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)stddef.h	5.7 (Berkeley) %G%
+ *	@(#)stddef.h	5.8 (Berkeley) %G%
  */
 
 #ifndef _STDDEF_H_
@@ -20,8 +20,10 @@ typedef	_BSD_SIZE_T_	size_t;
 #endif
 
 #ifdef	_BSD_WCHAR_T_
-typedef	_BSD_WCHAR_T_	wchar_t;
+#ifndef _ANSI_SOURCE
 typedef	_BSD_WCHAR_T_	rune_t;
+#endif
+typedef	_BSD_WCHAR_T_	wchar_t;
 #undef	_BSD_WCHAR_T_
 #endif
 
