639db12f75a5897506e31f13ab5d1ca6810c2b30
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-05-30 15:55:27 -0800

    set RES_DEBUG earlier
    SCCS-vsn: usr.sbin/sendmail/src/main.c 6.69

--- usr/src/usr.sbin/sendmail/src/main.c
+++ usr/src/usr.sbin/sendmail/src/main.c
@@ -13,7 +13,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)main.c	6.68 (Berkeley) %G%";
+static char sccsid[] = "@(#)main.c	6.69 (Berkeley) %G%";
 #endif /* not lint */
 
 #define	_DEFINE
@@ -501,11 +501,6 @@ main(argc, argv, envp)
 		}
 	}
 	}
-
-#ifdef NAMED_BIND
-	if (tTd(8, 8))
-		_res.options |= RES_DEBUG;
-#endif
 
 	/*
 	**  Do basic initialization.
