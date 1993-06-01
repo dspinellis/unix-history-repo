046142151477f2ae43bf7b5621468e18645f3901
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-01 00:42:23 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: games/pom/pom.6 8.1
    SCCS-vsn: games/pom/pom.c 8.1

--- usr/src/games/pom/pom.c
+++ usr/src/games/pom/pom.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1989 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1989, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software posted to USENET.
  *
@@ -8,13 +8,13 @@
  */
 
 #ifndef lint
-char copyright[] =
-"@(#) Copyright (c) 1989 The Regents of the University of California.\n\
- All rights reserved.\n";
+static char copyright[] =
+"@(#) Copyright (c) 1989, 1993\n\
+	The Regents of the University of California.  All rights reserved.\n";
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)pom.c	5.3 (Berkeley) %G%";
+static char sccsid[] = "@(#)pom.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 /*
