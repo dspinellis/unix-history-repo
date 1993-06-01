28094dd4faa95401acc0c560adf17d06b3dabf76
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-01 00:44:50 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: games/quiz/pathnames.h 8.1
    SCCS-vsn: games/quiz/quiz.6 8.1
    SCCS-vsn: games/quiz/quiz.c 8.1
    SCCS-vsn: games/quiz/quiz.h 8.1
    SCCS-vsn: games/quiz/rxp.c 8.1
    SCCS-vsn: games/rain/rain.6 8.1
    SCCS-vsn: games/rain/rain.c 8.1

--- usr/src/games/quiz/quiz.c
+++ usr/src/games/quiz/quiz.c
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1991 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1991, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Jim R. Oldroyd at The Instruction Set and Keith Gabryelski at
@@ -10,13 +10,13 @@
  */
 
 #ifndef lint
-char copyright[] =
-"@(#) Copyright (c) 1991 The Regents of the University of California.\n\
- All rights reserved.\n";
+static char copyright[] =
+"@(#) Copyright (c) 1991, 1993\n\
+	The Regents of the University of California.  All rights reserved.\n";
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)quiz.c	5.3 (Berkeley) %G%";
+static char sccsid[] = "@(#)quiz.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/types.h>
--- usr/src/games/quiz/quiz.h
+++ usr/src/games/quiz/quiz.h
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1991 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1991, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Jim R. Oldroyd at The Instruction Set and Keith Gabryelski at
@@ -8,7 +8,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)quiz.h	5.2 (Berkeley) %G%
+ *	@(#)quiz.h	8.1 (Berkeley) %G%
  */
 
 #define	TRUE		1
