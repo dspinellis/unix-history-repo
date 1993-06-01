b5ff552d7fcb4824a8aad684909d48290fa97cad
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-05-31 23:21:40 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: old/boggle/boggle.6 8.1
    SCCS-vsn: old/boggle/boggle.c 8.1
    SCCS-vsn: old/boggle/comp.c 8.1
    SCCS-vsn: old/boggle/pathnames.h 8.1
    SCCS-vsn: games/caesar/caesar.6 8.1
    SCCS-vsn: games/caesar/caesar.c 8.1
    SCCS-vsn: games/caesar/rot13.sh 8.1

--- usr/src/games/caesar/rot13.sh
+++ usr/src/games/caesar/rot13.sh
@@ -1,11 +1,11 @@
 #!/bin/sh -
 #
-# Copyright (c) 1992 The Regents of the University of California.
-# All rights reserved.
+# Copyright (c) 1992, 1993
+#	The Regents of the University of California.  All rights reserved.
 #
 # %sccs.include.redist.sh%
 #
-#	@(#)rot13.sh	5.1 (Berkeley) %G%
+#	@(#)rot13.sh	8.1 (Berkeley) %G%
 #
 
 caesar 13 $*
