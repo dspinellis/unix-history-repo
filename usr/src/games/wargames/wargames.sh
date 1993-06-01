28c46da260439c7bf08da1ac2db03d6f15b03595
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-01 01:02:22 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: games/wargames/wargames.sh 8.1

--- usr/src/games/wargames/wargames.sh
+++ usr/src/games/wargames/wargames.sh
@@ -1,11 +1,11 @@
 #!/bin/sh -
 #
-# Copyright (c) 1985 The Regents of the University of California.
-# All rights reserved.
+# Copyright (c) 1985, 1993
+#	The Regents of the University of California.  All rights reserved.
 #
 # %sccs.include.redist.sh%
 #
-#	@(#)wargames.sh	5.4 (Berkeley) %G%
+#	@(#)wargames.sh	8.1 (Berkeley) %G%
 #
 echo -n "Would you like to play a game? "
 read x
