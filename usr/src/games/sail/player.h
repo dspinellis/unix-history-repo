eec7864a4b1e95d5f0a9ae902c46aa7c712bb85a
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-01 00:51:20 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: games/sail/pl_5.c 8.1
    SCCS-vsn: games/sail/pl_6.c 8.1
    SCCS-vsn: games/sail/pl_7.c 8.1
    SCCS-vsn: games/sail/pl_main.c 8.1
    SCCS-vsn: games/sail/player.h 8.1
    SCCS-vsn: games/sail/sync.c 8.1
    SCCS-vsn: games/sail/version.c 8.1
    SCCS-vsn: games/sail/sail.6 8.1

--- usr/src/games/sail/player.h
+++ usr/src/games/sail/player.h
@@ -1,10 +1,10 @@
 /*
- * Copyright (c) 1983 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1983, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)player.h	5.5 (Berkeley) %G%
+ *	@(#)player.h	8.1 (Berkeley) %G%
  */
 
 #include <curses.h>
--- usr/src/games/sail/version.c
+++ usr/src/games/sail/version.c
@@ -1,12 +1,12 @@
 /*
- * Copyright (c) 1983 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1983, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)version.c	5.4 (Berkeley) %G%";
+static char sccsid[] = "@(#)version.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
-char version[] = "Wooden Ships and Iron Men, Version 5.4 (90/06/01)";
+char version[] = "Wooden Ships and Iron Men, Version 8.1 (93/05/31)";
