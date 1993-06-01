c8bf86dc1713a5adc8225422e93a4cf52666fee2
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-05-31 22:26:18 -0800

    add copyright notices, SCCS headers, yank to 8.1
    SCCS-vsn: bin/sh/alias.c 8.1
    SCCS-vsn: bin/sh/alias.h 8.1
    SCCS-vsn: bin/sh/arith.y 8.1
    SCCS-vsn: bin/sh/arith_lex.l 8.1
    SCCS-vsn: bin/sh/mail.c 8.2
    SCCS-vsn: bin/sh/alias.h 1.2

--- usr/src/bin/sh/alias.h
+++ usr/src/bin/sh/alias.h
@@ -1,3 +1,15 @@
+/*-
+ * Copyright (c) 1993 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to Berkeley by
+ * Kenneth Almquist.
+ *
+ * %sccs.include.redist.c%
+ *
+ *	@(#)alias.h	1.2 (Berkeley) %G%
+ */
+
 #define ALIASINUSE	1
 
 struct alias {
