8aab8b617f1cca6fed53e1519eda7c4867232f3f
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-05-31 23:04:24 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: games/atc/def.h 8.1
    SCCS-vsn: games/atc/extern.c 8.1
    SCCS-vsn: games/atc/extern.h 8.1
    SCCS-vsn: games/atc/grammar.y 8.1
    SCCS-vsn: games/atc/graphics.c 8.1
    SCCS-vsn: games/atc/include.h 8.1
    SCCS-vsn: games/atc/input.c 8.1
    SCCS-vsn: games/atc/list.c 8.1
    SCCS-vsn: games/atc/log.c 8.1
    SCCS-vsn: games/atc/main.c 8.1
    SCCS-vsn: games/atc/pathnames.h 8.1
    SCCS-vsn: games/atc/struct.h 8.1
    SCCS-vsn: games/atc/tunable.c 8.1
    SCCS-vsn: games/atc/tunable.h 8.1
    SCCS-vsn: games/atc/update.c 8.1
    SCCS-vsn: games/atc/atc.6 8.1

--- usr/src/games/atc/extern.h
+++ usr/src/games/atc/extern.h
@@ -1,13 +1,13 @@
 /*-
- * Copyright (c) 1990 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Ed James.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)extern.h	5.4 (Berkeley) %G%
+ *	@(#)extern.h	8.1 (Berkeley) %G%
  */
 
 /*
--- usr/src/games/atc/log.c
+++ usr/src/games/atc/log.c
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1990 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Ed James.
@@ -18,7 +18,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)log.c	5.7 (Berkeley) %G%";
+static char sccsid[] = "@(#)log.c	8.1 (Berkeley) %G%";
 #endif not lint
 
 #include "include.h"
