e656c79543800978fde8688def45a1b1638c0d6d
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-05-31 21:50:31 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: bin/expr/expr.1 8.1
    SCCS-vsn: bin/hostname/hostname.1 8.1
    SCCS-vsn: bin/hostname/hostname.c 8.1
    SCCS-vsn: bin/kill/kill.1 8.1
    SCCS-vsn: bin/kill/kill.c 8.1
    SCCS-vsn: bin/ls/cmp.c 8.1
    SCCS-vsn: bin/ls/extern.h 8.1
    SCCS-vsn: bin/ls/ls.1 8.1
    SCCS-vsn: bin/ls/ls.c 8.1
    SCCS-vsn: bin/ls/ls.h 8.1
    SCCS-vsn: bin/ls/print.c 8.1
    SCCS-vsn: bin/ls/stat_flags.c 8.1
    SCCS-vsn: bin/ls/util.c 8.1
    SCCS-vsn: bin/mkdir/mkdir.1 8.1
    SCCS-vsn: bin/mkdir/mkdir.c 8.1
    SCCS-vsn: bin/mv/mv.1 8.1
    SCCS-vsn: bin/mv/mv.c 8.1
    SCCS-vsn: bin/mv/pathnames.h 8.1
    SCCS-vsn: bin/pax/ar_io.c 8.1
    SCCS-vsn: bin/pax/ar_io.c 8.1

--- usr/src/bin/mkdir/mkdir.c
+++ usr/src/bin/mkdir/mkdir.c
@@ -1,18 +1,18 @@
 /*
- * Copyright (c) 1983, 1992 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1983, 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-char copyright[] =
-"@(#) Copyright (c) 1983, 1992 Regents of the University of California.\n\
- All rights reserved.\n";
+static char copyright[] =
+"@(#) Copyright (c) 1983, 1992, 1993\n\
+	The Regents of the University of California.  All rights reserved.\n";
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)mkdir.c	5.9 (Berkeley) %G%";
+static char sccsid[] = "@(#)mkdir.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/types.h>
