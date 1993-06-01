0bc230a92bd60df5ff0e7b8d9db46e7a1c559050
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-01 00:53:05 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: games/snake/snscore/snscore.c 8.1

--- usr/src/games/snake/snscore/snscore.c
+++ usr/src/games/snake/snscore/snscore.c
@@ -12,7 +12,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)snscore.c	5.7 (Berkeley) %G%";
+static char sccsid[] = "@(#)snscore.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/types.h>
