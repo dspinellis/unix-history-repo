4fd7e99ea1d1dd287be924ddfae39f47108a0b12
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-05-31 22:37:23 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: bin/sh/show.c 8.1
    SCCS-vsn: bin/sh/builtins.def 8.1

--- usr/src/bin/sh/show.c
+++ usr/src/bin/sh/show.c
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1991 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1991, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Kenneth Almquist.
@@ -9,7 +9,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)show.c	5.4 (Berkeley) %G%";
+static char sccsid[] = "@(#)show.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <stdio.h>
