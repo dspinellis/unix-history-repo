e0e560d42362204f362f03790f72c2bc2754e0a4
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-05-31 22:08:21 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: bin/pwd/pwd.1 8.1
    SCCS-vsn: bin/pwd/pwd.c 8.1
    SCCS-vsn: bin/rcp/extern.h 8.1
    SCCS-vsn: bin/rcp/pathnames.h 8.1
    SCCS-vsn: bin/rcp/rcp.1 8.1
    SCCS-vsn: bin/rcp/rcp.c 8.1
    SCCS-vsn: bin/rcp/util.c 8.1

--- usr/src/bin/rcp/util.c
+++ usr/src/bin/rcp/util.c
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1992 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)util.c	5.3 (Berkeley) %G%";
+static char sccsid[] = "@(#)util.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/param.h>
