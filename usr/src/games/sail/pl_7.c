ff3864b1b6b41b49f4fc4a62d5e1b95694465f02
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-01 00:51:20 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: games/sail/pl_5.c 8.1
    SCCS-vsn: games/sail/pl_6.c 8.1
    SCCS-vsn: games/sail/pl_7.c 8.1
    SCCS-vsn: games/sail/pl_main.c 8.1
    SCCS-vsn: games/sail/player.h 8.1
    SCCS-vsn: games/sail/sync.c 8.1
    SCCS-vsn: games/sail/version.c 8.1
    SCCS-vsn: games/sail/sail.6 8.1

--- usr/src/games/sail/pl_7.c
+++ usr/src/games/sail/pl_7.c
@@ -1,12 +1,12 @@
 /*
- * Copyright (c) 1983 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1983, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)pl_7.c	5.10 (Berkeley) %G%";
+static char sccsid[] = "@(#)pl_7.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/ttydefaults.h>
