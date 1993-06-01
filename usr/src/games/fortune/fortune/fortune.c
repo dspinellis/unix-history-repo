5afa59fbca0e655aa0849775ebf599fa8e16604a
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-01 00:01:46 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: games/fortune/fortune/fortune.6 8.1
    SCCS-vsn: games/fortune/fortune/fortune.c 8.1
    SCCS-vsn: games/fortune/fortune/pathnames.h 8.1

--- usr/src/games/fortune/fortune/fortune.c
+++ usr/src/games/fortune/fortune/fortune.c
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1986 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1986, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Ken Arnold.
@@ -9,13 +9,13 @@
  */
 
 #ifndef lint
-char copyright[] =
-"@(#) Copyright (c) 1986 The Regents of the University of California.\n\
- All rights reserved.\n";
+static char copyright[] =
+"@(#) Copyright (c) 1986, 1993\n\
+	The Regents of the University of California.  All rights reserved.\n";
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)fortune.c	5.14 (Berkeley) %G%";
+static char sccsid[] = "@(#)fortune.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 # include	<sys/param.h>
