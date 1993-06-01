4da9f9998736b5b92682a06739d6e11b5910ae75
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-05-31 22:10:27 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: bin/rm/rm.1 8.1
    SCCS-vsn: bin/rm/rm.c 8.1
    SCCS-vsn: bin/rmail/rmail.c 8.1

--- usr/src/bin/rm/rm.c
+++ usr/src/bin/rm/rm.c
@@ -12,7 +12,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)rm.c	5.1 (Berkeley) %G%";
+static char sccsid[] = "@(#)rm.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/types.h>
