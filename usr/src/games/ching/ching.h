dc55818030bb078104447cc3f42421fc5107dea1
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-05-31 23:46:52 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: games/ching/ching.h 8.1
    SCCS-vsn: games/ching/ching.sh 8.1

--- usr/src/games/ching/ching.h
+++ usr/src/games/ching/ching.h
@@ -1,13 +1,13 @@
 /*
- * Copyright (c) 1988 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1988, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Guy Harris.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)ching.h	5.2 (Berkeley) %G%
+ *	@(#)ching.h	8.1 (Berkeley) %G%
  */
 
 #define OYIN	6		/* yin (broken) moving to yang (solid) */
