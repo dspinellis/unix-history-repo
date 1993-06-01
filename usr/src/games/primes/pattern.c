61582ef01bf9edf297b5a6f909bcd9d73e22688e
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-01 00:43:20 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: games/primes/pattern.c 8.1
    SCCS-vsn: games/primes/pr_tbl.c 8.1
    SCCS-vsn: games/primes/primes.c 8.1
    SCCS-vsn: games/primes/primes.h 8.1

--- usr/src/games/primes/pattern.c
+++ usr/src/games/primes/pattern.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1989 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1989, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Landon Curt Noll.
@@ -9,7 +9,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)pattern.c	5.2 (Berkeley) %G%";
+static char sccsid[] = "@(#)pattern.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 /*
--- usr/src/games/primes/primes.h
+++ usr/src/games/primes/primes.h
@@ -1,13 +1,13 @@
 /*
- * Copyright (c) 1989 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1989, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Landon Curt Noll.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)primes.h	5.2 (Berkeley) %G%
+ *	@(#)primes.h	8.1 (Berkeley) %G%
  */
 
 /*
