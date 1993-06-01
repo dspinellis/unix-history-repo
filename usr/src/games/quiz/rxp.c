c83d9e703de5984508cc435a6a356ca57c932e6f
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-01 00:44:50 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: games/quiz/pathnames.h 8.1
    SCCS-vsn: games/quiz/quiz.6 8.1
    SCCS-vsn: games/quiz/quiz.c 8.1
    SCCS-vsn: games/quiz/quiz.h 8.1
    SCCS-vsn: games/quiz/rxp.c 8.1
    SCCS-vsn: games/rain/rain.6 8.1
    SCCS-vsn: games/rain/rain.c 8.1

--- usr/src/games/quiz/rxp.c
+++ usr/src/games/quiz/rxp.c
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1991 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1991, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Jim R. Oldroyd at The Instruction Set and Keith Gabryelski at
@@ -10,7 +10,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)rxp.c	5.3 (Berkeley) %G%";
+static char sccsid[] = "@(#)rxp.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 /*
