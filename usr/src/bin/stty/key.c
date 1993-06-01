b3eea26cefc86479ba2b49705f07cd789e434fab
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-05-31 22:48:01 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: bin/stty/cchar.c 8.1
    SCCS-vsn: bin/stty/extern.h 8.1
    SCCS-vsn: bin/stty/gfmt.c 8.1
    SCCS-vsn: bin/stty/key.c 8.1
    SCCS-vsn: bin/stty/modes.c 8.1
    SCCS-vsn: bin/stty/print.c 8.1
    SCCS-vsn: bin/stty/stty.1 8.1
    SCCS-vsn: bin/stty/stty.c 8.1
    SCCS-vsn: bin/stty/stty.h 8.1
    SCCS-vsn: bin/stty/util.c 8.1

--- usr/src/bin/stty/key.c
+++ usr/src/bin/stty/key.c
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1991 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1991, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)key.c	5.5 (Berkeley) %G%";
+static char sccsid[] = "@(#)key.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/types.h>
