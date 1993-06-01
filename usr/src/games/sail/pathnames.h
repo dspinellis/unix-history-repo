b20df55227db74a79ea6ae5e9b3c734fa3e1920d
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-01 00:50:51 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: games/sail/assorted.c 8.1
    SCCS-vsn: games/sail/dr_1.c 8.1
    SCCS-vsn: games/sail/dr_2.c 8.1
    SCCS-vsn: games/sail/dr_3.c 8.1
    SCCS-vsn: games/sail/dr_4.c 8.1
    SCCS-vsn: games/sail/dr_5.c 8.1
    SCCS-vsn: games/sail/dr_main.c 8.1
    SCCS-vsn: games/sail/driver.h 8.1
    SCCS-vsn: games/sail/extern.h 8.1
    SCCS-vsn: games/sail/game.c 8.1
    SCCS-vsn: games/sail/globals.c 8.1
    SCCS-vsn: games/sail/lo_main.c 8.1
    SCCS-vsn: games/sail/machdep.h 8.1
    SCCS-vsn: games/sail/main.c 8.1
    SCCS-vsn: games/sail/misc.c 8.1
    SCCS-vsn: games/sail/parties.c 8.1
    SCCS-vsn: games/sail/pathnames.h 8.1
    SCCS-vsn: games/sail/pl_1.c 8.1
    SCCS-vsn: games/sail/pl_2.c 8.1
    SCCS-vsn: games/sail/pl_3.c 8.1
    SCCS-vsn: games/sail/pl_4.c 8.1

--- usr/src/games/sail/pathnames.h
+++ usr/src/games/sail/pathnames.h
@@ -1,10 +1,10 @@
 /*-
- * Copyright (c) 1990 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)pathnames.h	5.1 (Berkeley) %G%
+ *	@(#)pathnames.h	8.1 (Berkeley) %G%
  */
 
 #define	_PATH_LOGFILE	"/var/games/saillog"
