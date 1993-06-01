dc1cc9eda915f0face4f642a7ecaa6ed6a349275
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-05-31 23:13:39 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: games/battlestar/battlestar.6 8.1
    SCCS-vsn: games/battlestar/battlestar.c 8.1
    SCCS-vsn: games/battlestar/com1.c 8.1
    SCCS-vsn: games/battlestar/com2.c 8.1
    SCCS-vsn: games/battlestar/com3.c 8.1
    SCCS-vsn: games/battlestar/com4.c 8.1
    SCCS-vsn: games/battlestar/com5.c 8.1
    SCCS-vsn: games/battlestar/com6.c 8.1
    SCCS-vsn: games/battlestar/com7.c 8.1
    SCCS-vsn: games/battlestar/cypher.c 8.1
    SCCS-vsn: games/battlestar/dayfile.c 8.1
    SCCS-vsn: games/battlestar/dayobjs.c 8.1
    SCCS-vsn: games/battlestar/extern.h 8.1
    SCCS-vsn: games/battlestar/fly.c 8.1
    SCCS-vsn: games/battlestar/getcom.c 8.1
    SCCS-vsn: games/battlestar/globals.c 8.1
    SCCS-vsn: games/battlestar/init.c 8.1
    SCCS-vsn: games/battlestar/misc.c 8.1
    SCCS-vsn: games/battlestar/nightfile.c 8.1
    SCCS-vsn: games/battlestar/nightobjs.c 8.1
    SCCS-vsn: games/battlestar/parse.c 8.1
    SCCS-vsn: games/battlestar/pathnames.h 8.1
    SCCS-vsn: games/battlestar/room.c 8.1
    SCCS-vsn: games/battlestar/save.c 8.1
    SCCS-vsn: games/battlestar/words.c 8.1

--- usr/src/games/battlestar/com1.c
+++ usr/src/games/battlestar/com1.c
@@ -1,12 +1,12 @@
 /*
- * Copyright (c) 1983 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1983, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)com1.c	5.3 (Berkeley) %G%";
+static char sccsid[] = "@(#)com1.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 #include "externs.h"
