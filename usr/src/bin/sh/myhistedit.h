4a13d81afa7fcc64cc877f91b33f88afc657c39f
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-05-31 22:39:00 -0800

    fix copyright notice, SCCS header
    SCCS-vsn: bin/sh/myhistedit.h 1.4

--- usr/src/bin/sh/myhistedit.h
+++ usr/src/bin/sh/myhistedit.h
@@ -1,4 +1,14 @@
+/*-
+ * Copyright (c) 1993
+ *	The Regents of the University of California.  All rights reserved.
+ *
+ * %sccs.include.redist.c%
+ *
+ *	@(#)myhistedit.h	1.4 (Berkeley) %G%
+ */
+
 #include <histedit.h>
+
 extern History *hist;
 extern EditLine *el;
 extern int displayhist;
