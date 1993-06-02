608f5508f09f84baf02805595e621b0ce23111ff
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-02 07:04:41 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: lib/libc/compat-43/creat.2 8.1
    SCCS-vsn: lib/libc/compat-43/creat.c 8.1
    SCCS-vsn: lib/libc/compat-43/gethostid.3 8.1
    SCCS-vsn: lib/libc/compat-43/gethostid.c 8.1
    SCCS-vsn: lib/libc/compat-43/getwd.c 8.1
    SCCS-vsn: lib/libc/compat-43/killpg.2 8.1
    SCCS-vsn: lib/libc/compat-43/killpg.c 8.1
    SCCS-vsn: lib/libc/compat-43/regex.c 8.1
    SCCS-vsn: lib/libc/compat-43/sethostid.c 8.1
    SCCS-vsn: lib/libc/compat-43/setpgrp.c 8.1
    SCCS-vsn: lib/libc/compat-43/setregid.2 8.1
    SCCS-vsn: lib/libc/compat-43/setregid.c 8.1
    SCCS-vsn: lib/libc/compat-43/setreuid.2 8.1
    SCCS-vsn: lib/libc/compat-43/setreuid.c 8.1
    SCCS-vsn: lib/libc/compat-43/setrgid.c 8.1
    SCCS-vsn: lib/libc/compat-43/setruid.3 8.1
    SCCS-vsn: lib/libc/compat-43/setruid.c 8.1
    SCCS-vsn: lib/libc/compat-43/sigblock.2 8.1
    SCCS-vsn: lib/libc/compat-43/sigcompat.c 8.1
    SCCS-vsn: lib/libc/compat-43/sigpause.2 8.1
    SCCS-vsn: lib/libc/compat-43/sigsetmask.2 8.1
    SCCS-vsn: lib/libc/compat-43/sigvec.2 8.1

--- usr/src/lib/libc/compat-43/regex.c
+++ usr/src/lib/libc/compat-43/regex.c
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1992 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * James da Silva at the University of Maryland at College Park.
@@ -16,7 +16,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)regex.c	5.1 (Berkeley) %G%";
+static char sccsid[] = "@(#)regex.c	8.1 (Berkeley) %G%";
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/types.h>
