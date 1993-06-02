eb14b7a11180e7568fd388cadd13bdae62ca5c23
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-02 06:15:14 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: local/kerberosIV/register/pathnames.h 8.1
    SCCS-vsn: local/kerberosIV/register/register.c 8.1
    SCCS-vsn: local/kerberosIV/register/register_proto.h 8.1
    SCCS-vsn: local/kerberosIV/register/register.1 8.1

--- usr/src/local/kerberosIV/register/register_proto.h
+++ usr/src/local/kerberosIV/register/register_proto.h
@@ -1,10 +1,10 @@
 /*-
- * Copyright (c) 1989 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1989, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)register_proto.h	5.1 (Berkeley) %G%
+ *	@(#)register_proto.h	8.1 (Berkeley) %G%
  */
 
 #define	APPEND_DB	0x01
