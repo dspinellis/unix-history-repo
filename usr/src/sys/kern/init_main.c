7861004e1838dd079b76d43c775ba9232cf6888c
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1993-06-02 01:11:02 -0800

    update copyright on bootup
    SCCS-vsn: sys/kern/init_main.c 7.60

--- usr/src/sys/kern/init_main.c
+++ usr/src/sys/kern/init_main.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)init_main.c	7.59 (Berkeley) %G%
+ *	@(#)init_main.c	7.60 (Berkeley) %G%
  */
 
 #include <sys/param.h>
@@ -32,10 +32,10 @@
 
 #ifdef HPFPLIB
 char	copyright[] =
-"Copyright (c) 1982,1986,1989,1991 The Regents of the University of California.\nCopyright (c) 1992 Hewlett-Packard Company\nCopyright (c) 1992 Motorola Inc.\nAll rights reserved.\n\n";
+"Copyright (c) 1982, 1986, 1989, 1991, 1993\n\tThe Regents of the University of California.\nCopyright (c) 1992 Hewlett-Packard Company\nCopyright (c) 1992 Motorola Inc.\nAll rights reserved.\n\n";
 #else
 char	copyright[] =
-"Copyright (c) 1982,1986,1989,1991 The Regents of the University of California.\nAll rights reserved.\n\n";
+"Copyright (c) 1982, 1986, 1989, 1991, 1993\n\tThe Regents of the University of California.  All rights reserved.\n\n";
 #endif
 
 /*
