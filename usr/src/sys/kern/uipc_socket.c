e6bbeb9c4b646f703c30fd96019c9f84c1186b99
Mike Karels <karels@ucbvax.Berkeley.EDU>
1993-06-02 17:45:57 -0800

    deliver data to non-atomic protocols in regular-sized chunks even on machines without MAPPED_MBUFS, or TCP will send 968-byte packets
    SCCS-vsn: sys/kern/uipc_socket.c 7.41

--- usr/src/sys/kern/uipc_socket.c
+++ usr/src/sys/kern/uipc_socket.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)uipc_socket.c	7.40 (Berkeley) %G%
+ *	@(#)uipc_socket.c	7.41 (Berkeley) %G%
  */
 
 #include <sys/param.h>
@@ -366,7 +366,7 @@ restart:
 #ifdef	MAPPED_MBUFS
 				len = min(MCLBYTES, resid);
 #else
-				if (top == 0) {
+				if (atomic && top == 0) {
 					len = min(MCLBYTES - max_hdr, resid);
 					m->m_data += max_hdr;
 				} else
