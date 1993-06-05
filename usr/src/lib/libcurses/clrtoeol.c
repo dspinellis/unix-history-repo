cb50557ba5f756b160179ca66e07eb3e5b39c7fa
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-04 23:47:38 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: lib/libcurses/addbytes.c 8.1
    SCCS-vsn: lib/libcurses/addch.c 8.1
    SCCS-vsn: lib/libcurses/addnstr.c 8.1
    SCCS-vsn: lib/libcurses/box.c 8.1
    SCCS-vsn: lib/libcurses/clear.c 8.1
    SCCS-vsn: lib/libcurses/clrtobot.c 8.1
    SCCS-vsn: lib/libcurses/clrtoeol.c 8.1
    SCCS-vsn: lib/libcurses/cr_put.c 8.1
    SCCS-vsn: lib/libcurses/ctrace.c 8.1
    SCCS-vsn: lib/libcurses/curses.3 8.1
    SCCS-vsn: lib/libcurses/curses.c 8.1
    SCCS-vsn: lib/libcurses/curses.h 8.1

--- usr/src/lib/libcurses/clrtoeol.c
+++ usr/src/lib/libcurses/clrtoeol.c
@@ -1,12 +1,12 @@
 /*
- * Copyright (c) 1981 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1981, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)clrtoeol.c	5.12 (Berkeley) %G%";
+static char sccsid[] = "@(#)clrtoeol.c	8.1 (Berkeley) %G%";
 #endif	/* not lint */
 
 #include <curses.h>
