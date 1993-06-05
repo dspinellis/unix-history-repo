aefbc421347a330dc0f0f8226ac1940a9af4a7e4
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-04 22:45:32 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: lib/libc/vax/gen/_setjmp.s 8.1
    SCCS-vsn: lib/libc/vax/gen/alloca.s 8.1
    SCCS-vsn: lib/libc/vax/gen/fabs.s 8.1
    SCCS-vsn: lib/libc/vax/gen/fixdfsi.s 8.1
    SCCS-vsn: lib/libc/vax/gen/fixunsdfsi.s 8.1
    SCCS-vsn: lib/libc/vax/gen/ldexp.s 8.1
    SCCS-vsn: lib/libc/vax/gen/lshlsi3.s 8.1
    SCCS-vsn: lib/libc/vax/gen/lshrsi3.s 8.1
    SCCS-vsn: lib/libc/vax/gen/modf.s 8.1
    SCCS-vsn: lib/libc/vax/gen/modsi3.s 8.1
    SCCS-vsn: lib/libc/vax/gen/setjmp.s 8.1
    SCCS-vsn: lib/libc/vax/gen/udiv.s 8.1
    SCCS-vsn: lib/libc/vax/gen/udivsi3.s 8.1
    SCCS-vsn: lib/libc/vax/gen/umodsi3.s 8.1
    SCCS-vsn: lib/libc/vax/gen/umulsi3.s 8.1
    SCCS-vsn: lib/libc/vax/gen/urem.s 8.1
    SCCS-vsn: lib/libc/vax/sys/cache.lib/Ovfork.c 8.1
    SCCS-vsn: lib/libc/vax/sys/cache.lib/fork.c 8.1
    SCCS-vsn: lib/libc/vax/sys/cache.lib/getdtablesize.c 8.1
    SCCS-vsn: lib/libc/vax/sys/cache.lib/getgid.c 8.1
    SCCS-vsn: lib/libc/vax/sys/cache.lib/gethostid.c 8.1
    SCCS-vsn: lib/libc/vax/sys/cache.lib/getpid.c 8.1
    SCCS-vsn: lib/libc/vax/sys/cache.lib/getppid.c 8.1
    SCCS-vsn: lib/libc/vax/sys/cache.lib/getuid.c 8.1
    SCCS-vsn: lib/libc/vax/sys/cache.lib/x.c 8.1

--- usr/src/lib/libc/vax/sys/cache.lib/getppid.c
+++ usr/src/lib/libc/vax/sys/cache.lib/getppid.c
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1983 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1983, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-	.asciz "@(#)getppid.c	5.2 (Berkeley) %G%"
+	.asciz "@(#)getppid.c	8.1 (Berkeley) %G%"
 #endif /* LIBC_SCCS and not lint */
 
 #include "SYS.h"
