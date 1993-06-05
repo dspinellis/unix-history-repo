ab145c18997100ecaef584445ea41215dbd7e401
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-05 00:19:47 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: lib/libplot/aed/arc.c 8.1
    SCCS-vsn: lib/libplot/aed/box.c 8.1
    SCCS-vsn: lib/libplot/aed/circle.c 8.1
    SCCS-vsn: lib/liby/main.c 8.1
    SCCS-vsn: lib/libplot/aed/close.c 8.1
    SCCS-vsn: lib/liby/yyerror.c 8.1
    SCCS-vsn: lib/libplot/aed/cont.c 8.1

--- usr/src/lib/liby/main.c
+++ usr/src/lib/liby/main.c
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1990 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)main.c	5.3 (Berkeley) %G%";
+static char sccsid[] = "@(#)main.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 main()
