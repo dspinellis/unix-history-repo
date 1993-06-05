fe9800d1e3f482e8ec7a12f78f7ec3dbd6c402d8
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-04 22:25:43 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: lib/libc/db/recno/rec_get.c 8.1
    SCCS-vsn: lib/libc/db/recno/rec_open.c 8.1
    SCCS-vsn: lib/libc/db/recno/rec_put.c 8.1
    SCCS-vsn: lib/libc/db/recno/rec_search.c 8.1
    SCCS-vsn: lib/libc/db/recno/rec_seq.c 8.1
    SCCS-vsn: lib/libc/db/recno/rec_utils.c 8.1
    SCCS-vsn: lib/libc/db/recno/recno.h 8.1

--- usr/src/lib/libc/db/recno/rec_seq.c
+++ usr/src/lib/libc/db/recno/rec_seq.c
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1991 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1991, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)rec_seq.c	5.9 (Berkeley) %G%";
+static char sccsid[] = "@(#)rec_seq.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/types.h>
--- usr/src/lib/libc/db/recno/rec_utils.c
+++ usr/src/lib/libc/db/recno/rec_utils.c
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1990 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)rec_utils.c	5.7 (Berkeley) %G%";
+static char sccsid[] = "@(#)rec_utils.c	8.1 (Berkeley) %G%";
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/param.h>
