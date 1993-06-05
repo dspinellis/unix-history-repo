e315d83000ba54e9089167bce69c3b19af0686a9
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-04 20:19:27 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: lib/libc/sys/listen.2 8.1
    SCCS-vsn: lib/libc/tahoe/gen/_setjmp.s 8.1
    SCCS-vsn: lib/libc/sys/lseek.2 8.1
    SCCS-vsn: lib/libc/tahoe/gen/alloca.s 8.1
    SCCS-vsn: lib/libc/sys/lseek.c 8.1
    SCCS-vsn: lib/libc/tahoe/gen/fabs.s 8.1
    SCCS-vsn: lib/libc/sys/madvise.2 8.1
    SCCS-vsn: lib/libc/tahoe/gen/fixdfsi.s 8.1
    SCCS-vsn: lib/libc/sys/mincore.2 8.1
    SCCS-vsn: lib/libc/tahoe/gen/fixunsdfsi.s 8.1
    SCCS-vsn: lib/libc/sys/mkdir.2 8.1
    SCCS-vsn: lib/libc/tahoe/gen/frexp.c 8.1
    SCCS-vsn: lib/libc/sys/mkfifo.2 8.1
    SCCS-vsn: lib/libc/tahoe/gen/isinf.c 8.1
    SCCS-vsn: lib/libc/sys/mknod.2 8.1
    SCCS-vsn: lib/libc/tahoe/gen/ldexp.s 8.1
    SCCS-vsn: lib/libc/sys/mlock.2 8.1
    SCCS-vsn: lib/libc/tahoe/gen/modf.s 8.1
    SCCS-vsn: lib/libc/sys/mmap.2 8.1
    SCCS-vsn: lib/libc/tahoe/gen/modsi3.s 8.1
    SCCS-vsn: lib/libc/sys/mmap.c 8.1
    SCCS-vsn: lib/libc/tahoe/gen/setjmp.s 8.1
    SCCS-vsn: lib/libc/sys/mount.2 8.1
    SCCS-vsn: lib/libc/tahoe/gen/udiv.s 8.1
    SCCS-vsn: lib/libc/tahoe/gen/udivsi3.s 8.1
    SCCS-vsn: lib/libc/sys/mprotect.2 8.1
    SCCS-vsn: lib/libc/tahoe/gen/umodsi3.s 8.1
    SCCS-vsn: lib/libc/sys/msync.2 8.1
    SCCS-vsn: lib/libc/tahoe/gen/umulsi3.s 8.1
    SCCS-vsn: lib/libc/sys/munmap.2 8.1
    SCCS-vsn: lib/libc/tahoe/gen/urem.s 8.1
    SCCS-vsn: lib/libc/sys/nfssvc.2 8.1
    SCCS-vsn: lib/libc/sys/open.2 8.1
    SCCS-vsn: lib/libc/sys/pathconf.2 8.1
    SCCS-vsn: lib/libc/sys/pipe.2 8.1
    SCCS-vsn: lib/libc/sys/profil.2 8.1
    SCCS-vsn: lib/libc/sys/ptrace.2 8.1
    SCCS-vsn: lib/libc/sys/quotactl.2 8.1
    SCCS-vsn: lib/libc/sys/read.2 8.1
    SCCS-vsn: lib/libc/sys/readlink.2 8.1
    SCCS-vsn: lib/libc/sys/reboot.2 8.1
    SCCS-vsn: lib/libc/sys/recv.2 8.1
    SCCS-vsn: lib/libc/sys/rename.2 8.1
    SCCS-vsn: lib/libc/sys/revoke.2 8.1
    SCCS-vsn: lib/libc/sys/rmdir.2 8.1
    SCCS-vsn: lib/libc/tahoe/net/htonl.s 8.1
    SCCS-vsn: lib/libc/sys/select.2 8.1
    SCCS-vsn: lib/libc/tahoe/net/htons.s 8.1
    SCCS-vsn: lib/libc/sys/send.2 8.1
    SCCS-vsn: lib/libc/tahoe/net/ntohl.s 8.1
    SCCS-vsn: lib/libc/sys/setgroups.2 8.1
    SCCS-vsn: lib/libc/tahoe/net/ntohs.s 8.1

--- usr/src/lib/libc/tahoe/gen/setjmp.s
+++ usr/src/lib/libc/tahoe/gen/setjmp.s
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1988 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1988, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Computer Consoles Inc.
@@ -9,7 +9,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-	.asciz "@(#)setjmp.s	5.1 (Berkeley) %G%"
+	.asciz "@(#)setjmp.s	8.1 (Berkeley) %G%"
 #endif /* LIBC_SCCS and not lint */
 
 /*
--- usr/src/lib/libc/tahoe/net/htonl.s
+++ usr/src/lib/libc/tahoe/net/htonl.s
@@ -1,12 +1,12 @@
 /*
- * Copyright (c) 1988 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1988, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-	.asciz "@(#)htonl.s	5.1 (Berkeley) %G%"
+	.asciz "@(#)htonl.s	8.1 (Berkeley) %G%"
 #endif /* LIBC_SCCS and not lint */
 
 /* netorder = htonl(hostorder) */
