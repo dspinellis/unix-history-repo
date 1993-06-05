7a7d97b7dc2e583047c12f2a8ecc96afec8034a1
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-05 02:03:14 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: libexec/rbootd/pathnames.h 8.1
    SCCS-vsn: libexec/rbootd/bpf.c 8.1
    SCCS-vsn: libexec/rbootd/conf.c 8.1
    SCCS-vsn: libexec/rbootd/defs.h 8.1
    SCCS-vsn: libexec/rbootd/parseconf.c 8.1
    SCCS-vsn: libexec/rbootd/rbootd.8 8.1
    SCCS-vsn: libexec/rbootd/rbootd.c 8.1
    SCCS-vsn: libexec/rbootd/rmp.h 8.1
    SCCS-vsn: libexec/rbootd/rmp_var.h 8.1
    SCCS-vsn: libexec/rbootd/rmpproto.c 8.1
    SCCS-vsn: libexec/rbootd/utils.c 8.1

--- usr/src/libexec/rbootd/pathnames.h
+++ usr/src/libexec/rbootd/pathnames.h
@@ -1,8 +1,8 @@
 /*
  * Copyright (c) 1988, 1992 The University of Utah and the Center
  *	for Software Science (CSS).
- * Copyright (c) 1992 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * the Center for Software Science of the University of Utah Computer
@@ -12,7 +12,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)pathnames.h	5.3 (Berkeley) %G%
+ *	@(#)pathnames.h	8.1 (Berkeley) %G%
  *
  * Utah $Hdr: pathnames.h 3.1 92/07/06$
  * Author: Jeff Forys, University of Utah CSS
--- usr/src/libexec/rbootd/rbootd.c
+++ usr/src/libexec/rbootd/rbootd.c
@@ -12,14 +12,14 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)rbootd.c	5.3 (Berkeley) %G%
+ *	@(#)rbootd.c	8.1 (Berkeley) %G%
  *
  * Utah $Hdr: rbootd.c 3.1 92/07/06$
  * Author: Jeff Forys, University of Utah CSS
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)rbootd.c	5.3 (Berkeley) %G%";
+static char sccsid[] = "@(#)rbootd.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/param.h>
