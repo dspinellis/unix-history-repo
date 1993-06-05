6243f81190e6af161117d73f744b54bf87ef95f5
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-04 23:40:14 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: lib/libcurses/EXAMPLES/ex1.c 8.1
    SCCS-vsn: lib/libcurses/PSD.doc/intro.6 8.1

--- usr/src/lib/libcurses/EXAMPLES/ex1.c
+++ usr/src/lib/libcurses/EXAMPLES/ex1.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1992 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  *
  * %sccs.include.redist.c%
@@ -8,12 +8,12 @@
 
 #ifndef lint
 static char copyright[] =
-"@(#) Copyright (c) 1992 The Regents of the University of California.\n\
- All rights reserved.\n";
+"@(#) Copyright (c) 1992, 1993\n\
+	The Regents of the University of California.  All rights reserved.\n";
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)ex1.c	1.1 (Berkeley) %G%";
+static char sccsid[] = "@(#)ex1.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 #include <sys/types.h>
 #include <curses.h>
