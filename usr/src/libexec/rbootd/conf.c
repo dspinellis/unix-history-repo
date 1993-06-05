7866c3f515d563435fee106cfa790eacf40443a8
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-05 01:58:44 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: libexec/rbootd/bpf.c 8.1
    SCCS-vsn: libexec/rbootd/conf.c 8.1
    SCCS-vsn: libexec/rbootd/defs.h 8.1
    SCCS-vsn: libexec/rbootd/parseconf.c 8.1
    SCCS-vsn: libexec/rbootd/pathnames.h 8.1
    SCCS-vsn: libexec/rbootd/rbootd.8 8.1
    SCCS-vsn: libexec/rexecd/rexecd.8 8.1
    SCCS-vsn: libexec/rbootd/rbootd.c 8.1
    SCCS-vsn: libexec/rexecd/rexecd.c 8.1
    SCCS-vsn: libexec/rbootd/rmp.h 8.1

--- usr/src/libexec/rbootd/conf.c
+++ usr/src/libexec/rbootd/conf.c
@@ -12,14 +12,14 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)conf.c	5.2 (Berkeley) %G%
+ *	@(#)conf.c	8.1 (Berkeley) %G%
  *
  * Utah $Hdr: conf.c 3.1 92/07/06$
  * Author: Jeff Forys, University of Utah CSS
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)conf.c	5.2 (Berkeley) %G%";
+static char sccsid[] = "@(#)conf.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/param.h>
