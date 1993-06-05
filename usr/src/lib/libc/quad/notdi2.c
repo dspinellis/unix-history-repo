1d323fec54ba6e6df6868c5383c2f78ef41944c2
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-04 19:57:29 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: lib/libc/quad/adddi3.c 8.1
    SCCS-vsn: lib/libc/quad/anddi3.c 8.1
    SCCS-vsn: lib/libc/quad/ashldi3.c 8.1
    SCCS-vsn: lib/libc/quad/ashrdi3.c 8.1
    SCCS-vsn: lib/libc/quad/cmpdi2.c 8.1
    SCCS-vsn: lib/libc/quad/divdi3.c 8.1
    SCCS-vsn: lib/libc/quad/fixdfdi.c 8.1
    SCCS-vsn: lib/libc/quad/fixunsdfdi.c 8.1
    SCCS-vsn: lib/libc/quad/fixunssfdi.c 8.1
    SCCS-vsn: lib/libc/quad/floatdidf.c 8.1
    SCCS-vsn: lib/libc/quad/floatdisf.c 8.1
    SCCS-vsn: lib/libc/quad/floatunsdidf.c 8.1
    SCCS-vsn: lib/libc/quad/iordi3.c 8.1
    SCCS-vsn: lib/libc/quad/lshldi3.c 8.1
    SCCS-vsn: lib/libc/quad/lshrdi3.c 8.1
    SCCS-vsn: lib/libc/quad/moddi3.c 8.1
    SCCS-vsn: lib/libc/quad/muldi3.c 8.1
    SCCS-vsn: lib/libc/quad/negdi2.c 8.1
    SCCS-vsn: lib/libc/quad/notdi2.c 8.1
    SCCS-vsn: lib/libc/quad/qdivrem.c 8.1
    SCCS-vsn: lib/libc/quad/quad.h 8.1
    SCCS-vsn: lib/libc/quad/subdi3.c 8.1
    SCCS-vsn: lib/libc/quad/ucmpdi2.c 8.1
    SCCS-vsn: lib/libc/quad/udivdi3.c 8.1
    SCCS-vsn: lib/libc/quad/umoddi3.c 8.1
    SCCS-vsn: lib/libc/quad/xordi3.c 8.1

--- usr/src/lib/libc/quad/notdi2.c
+++ usr/src/lib/libc/quad/notdi2.c
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1992 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This software was developed by the Computer Systems Engineering group
  * at Lawrence Berkeley Laboratory under DARPA contract BG 91-66 and
@@ -10,7 +10,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)notdi2.c	5.4 (Berkeley) %G%";
+static char sccsid[] = "@(#)notdi2.c	8.1 (Berkeley) %G%";
 #endif /* LIBC_SCCS and not lint */
 
 #include "quad.h"
