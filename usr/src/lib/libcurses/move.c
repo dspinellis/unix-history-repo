546957785e1611cb5aaf66ff9ec8eec7d935c5d3
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-04 23:48:34 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: lib/libcurses/getch.c 8.1
    SCCS-vsn: lib/libcurses/getstr.c 8.1
    SCCS-vsn: lib/libcurses/cur_hash.c 8.1
    SCCS-vsn: lib/libcurses/id_subwins.c 8.1
    SCCS-vsn: lib/libcurses/idlok.c 8.1
    SCCS-vsn: lib/libcurses/initscr.c 8.1
    SCCS-vsn: lib/libcurses/insch.c 8.1
    SCCS-vsn: lib/libcurses/insertln.c 8.1
    SCCS-vsn: lib/libcurses/longname.c 8.1
    SCCS-vsn: lib/libcurses/move.c 8.1

--- usr/src/lib/libcurses/move.c
+++ usr/src/lib/libcurses/move.c
@@ -1,12 +1,12 @@
 /*
- * Copyright (c) 1981 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1981, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)move.c	5.13 (Berkeley) %G%";
+static char sccsid[] = "@(#)move.c	8.1 (Berkeley) %G%";
 #endif	/* not lint */
 
 #include <curses.h>
