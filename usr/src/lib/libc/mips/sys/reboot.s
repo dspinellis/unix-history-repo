1a54725bd4c4bad8c0d9582ecb5757181474f02b
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-04 19:48:09 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: lib/libc/mips/sys/fork.s 8.1
    SCCS-vsn: lib/libc/mips/sys/pipe.s 8.1
    SCCS-vsn: lib/libc/mips/sys/ptrace.s 8.1
    SCCS-vsn: lib/libc/mips/sys/reboot.s 8.1
    SCCS-vsn: lib/libc/mips/sys/sbrk.s 8.1
    SCCS-vsn: lib/libc/mips/SYS.h 8.1
    SCCS-vsn: lib/libc/mips/sys/setlogin.s 8.1
    SCCS-vsn: lib/libc/mips/sys/sigpending.s 8.1
    SCCS-vsn: lib/libc/mips/sys/sigprocmask.s 8.1
    SCCS-vsn: lib/libc/mips/sys/sigreturn.s 8.1
    SCCS-vsn: lib/libc/mips/sys/sigsuspend.s 8.1
    SCCS-vsn: lib/libc/mips/sys/syscall.s 8.1

--- usr/src/lib/libc/mips/sys/reboot.s
+++ usr/src/lib/libc/mips/sys/reboot.s
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1991 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1991, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Ralph Campbell.
@@ -11,7 +11,7 @@
 #include "SYS.h"
 
 #if defined(LIBC_SCCS) && !defined(lint)
-	ASMSTR("@(#)reboot.s	5.1 (Berkeley) %G%")
+	ASMSTR("@(#)reboot.s	8.1 (Berkeley) %G%")
 #endif /* LIBC_SCCS and not lint */
 
 LEAF(reboot)
--- usr/src/lib/libc/mips/sys/syscall.s
+++ usr/src/lib/libc/mips/sys/syscall.s
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1991 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1991, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Ralph Campbell.
@@ -11,7 +11,7 @@
 #include "SYS.h"
 
 #if defined(LIBC_SCCS) && !defined(lint)
-	ASMSTR("@(#)syscall.s	5.2 (Berkeley) %G%")
+	ASMSTR("@(#)syscall.s	8.1 (Berkeley) %G%")
 #endif /* LIBC_SCCS and not lint */
 
 #define SYS_syscall	0
