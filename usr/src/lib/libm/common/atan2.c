e4d12b1231e7c0099d9fba8b4185c86eef56e707
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-05 00:00:26 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: lib/libm/README 8.1
    SCCS-vsn: lib/libm/common/atan2.c 8.1
    SCCS-vsn: lib/libm/common/sincos.c 8.1
    SCCS-vsn: lib/libm/common/tan.c 8.1
    SCCS-vsn: lib/libm/common/trig.h 8.1
    SCCS-vsn: lib/libm/common_source/acos.3 8.1
    SCCS-vsn: lib/libm/common_source/acosh.3 8.1
    SCCS-vsn: lib/libm/common_source/acosh.c 8.1
    SCCS-vsn: lib/libm/common_source/asin.3 8.1
    SCCS-vsn: lib/libm/ieee/cabs.c 8.1
    SCCS-vsn: lib/libm/common_source/asincos.c 8.1
    SCCS-vsn: lib/libm/ieee/cbrt.c 8.1
    SCCS-vsn: lib/libm/common_source/asinh.3 8.1
    SCCS-vsn: lib/libm/ieee/support.c 8.1
    SCCS-vsn: lib/libm/common_source/asinh.c 8.1
    SCCS-vsn: lib/libm/common_source/atan.3 8.1
    SCCS-vsn: lib/libm/common_source/atan.c 8.1
    SCCS-vsn: lib/libm/common_source/atan2.3 8.1

--- usr/src/lib/libm/common/atan2.c
+++ usr/src/lib/libm/common/atan2.c
@@ -1,12 +1,12 @@
 /*
- * Copyright (c) 1985 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1985, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)atan2.c	5.6 (Berkeley) %G%";
+static char sccsid[] = "@(#)atan2.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 /* ATAN2(Y,X)
--- usr/src/lib/libm/common/trig.h
+++ usr/src/lib/libm/common/trig.h
@@ -1,10 +1,10 @@
 /*
- * Copyright (c) 1987 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1987, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)trig.h	5.6 (Berkeley) %G%
+ *	@(#)trig.h	8.1 (Berkeley) %G%
  */
 
 #include "mathimpl.h"
