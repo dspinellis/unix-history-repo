7b3f44760cb5b9d76f20c75c3f47b1df017f8bb0
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-04 23:15:27 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: lib/libc/vax/net/htonl.s 8.1
    SCCS-vsn: lib/libc/vax/net/htons.s 8.1
    SCCS-vsn: lib/libc/vax/net/ntohl.s 8.1
    SCCS-vsn: lib/libc/vax/net/ntohs.s 8.1
    SCCS-vsn: lib/libc/vax/stdlib/abs.s 8.1

--- usr/src/lib/libc/vax/net/htons.s
+++ usr/src/lib/libc/vax/net/htons.s
@@ -1,12 +1,12 @@
 /*
- * Copyright (c) 1983 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1983, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-	.asciz "@(#)htons.s	5.6 (Berkeley) %G%"
+	.asciz "@(#)htons.s	8.1 (Berkeley) %G%"
 #endif /* LIBC_SCCS and not lint */
 
 /* hostorder = htons(netorder) */
--- usr/src/lib/libc/vax/stdlib/abs.s
+++ usr/src/lib/libc/vax/stdlib/abs.s
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1991 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1991, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-	.asciz "@(#)abs.s	5.6 (Berkeley) %G%"
+	.asciz "@(#)abs.s	8.1 (Berkeley) %G%"
 #endif /* LIBC_SCCS and not lint */
 
 #include "DEFS.h"
