0b3c31258378a5d1d2158f7a9e73e43a66142535
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-05 00:22:52 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: lib/libplot/bitgraph/open.c 8.1

--- usr/src/lib/libplot/bitgraph/open.c
+++ usr/src/lib/libplot/bitgraph/open.c
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1980 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1980, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.proprietary.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)open.c	5.4 (Berkeley) %G%";
+static char sccsid[] = "@(#)open.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 /*
