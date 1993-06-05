ea65304704507b381fbf4783b4879fdf5cc8403d
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-04 23:28:27 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: lib/libcompat/4.3/tahoe/insque.s 8.1
    SCCS-vsn: lib/libcompat/4.3/tahoe/nargs.s 8.1
    SCCS-vsn: lib/libcompat/4.3/tahoe/remque.s 8.1
    SCCS-vsn: lib/libcompat/4.3/vax/insque.s 8.1
    SCCS-vsn: lib/libcompat/4.3/vax/nargs.s 8.1
    SCCS-vsn: lib/libcompat/4.3/vax/remque.s 8.1
    SCCS-vsn: lib/libcompat/4.3/hp300/insque.s 8.1
    SCCS-vsn: lib/libcompat/4.3/hp300/remque.s 8.1

--- usr/src/lib/libcompat/4.3/tahoe/nargs.s
+++ usr/src/lib/libcompat/4.3/tahoe/nargs.s
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1988 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1988, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
@@ -9,7 +9,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-	.asciz "@(#)nargs.s	1.5 (Berkeley) %G%"
+	.asciz "@(#)nargs.s	8.1 (Berkeley) %G%"
 #endif /* LIBC_SCCS and not lint */
 
 /* C library -- nargs */
