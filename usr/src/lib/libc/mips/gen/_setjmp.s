e1bee4a4a92105ca00f187c2be0dcf4c6d587e34
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-04 19:46:34 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: lib/libc/mips/gen/_setjmp.s 8.1
    SCCS-vsn: lib/libc/mips/gen/frexp.c 8.1
    SCCS-vsn: lib/libc/mips/gen/isinf.s 8.1
    SCCS-vsn: lib/libc/mips/gen/ldexp.s 8.1

--- usr/src/lib/libc/mips/gen/_setjmp.s
+++ usr/src/lib/libc/mips/gen/_setjmp.s
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1991 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1991, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Ralph Campbell.
@@ -12,7 +12,7 @@
 #include <machine/machAsmDefs.h>
 
 #if defined(LIBC_SCCS) && !defined(lint)
-	ASMSTR("@(#)_setjmp.s	5.7 (Berkeley) %G%")
+	ASMSTR("@(#)_setjmp.s	8.1 (Berkeley) %G%")
 #endif /* LIBC_SCCS and not lint */
 
 /*
--- usr/src/lib/libc/mips/gen/isinf.s
+++ usr/src/lib/libc/mips/gen/isinf.s
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1993 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Ralph Campbell.
@@ -11,7 +11,7 @@
 #include <machine/machAsmDefs.h>
 
 #if defined(LIBC_SCCS) && !defined(lint)
-	ASMSTR("@(#)isinf.s	5.1 (Berkeley) %G%")
+	ASMSTR("@(#)isinf.s	8.1 (Berkeley) %G%")
 #endif /* LIBC_SCCS and not lint */
 
 
