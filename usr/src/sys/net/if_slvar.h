b56b00369347840b1aae23a4d2081331f10dbba6
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-05 00:31:46 -0800

    prototype everything
    SCCS-vsn: sys/net/if_slvar.h 7.10
    SCCS-vsn: sys/net/bpfdesc.h 7.6

--- usr/src/sys/net/if_slvar.h
+++ usr/src/sys/net/if_slvar.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)if_slvar.h	7.9 (Berkeley) %G%
+ *	@(#)if_slvar.h	7.10 (Berkeley) %G%
  *
  * $Header: if_slvar.h,v 1.3 89/05/31 02:25:18 van Exp $
  */
@@ -39,3 +39,15 @@ struct sl_softc {
 
 /* this stuff doesn't belong here... */
 #define	SLIOCGUNIT	_IOR('t', 88, int)	/* get slip unit number */
+
+#ifdef KERNEL
+void	 slattach __P((void));
+void	 slclose __P((struct tty *));
+void	 slinput __P((int, struct tty *));
+int	 slioctl __P((struct ifnet *, int, caddr_t));
+int	 slopen __P((dev_t, struct tty *));
+int	 sloutput __P((struct ifnet *,
+	    struct mbuf *, struct sockaddr *, struct rtentry *));
+void	 slstart __P((struct tty *));
+int	 sltioctl __P((struct tty *, int, caddr_t, int));
+#endif
