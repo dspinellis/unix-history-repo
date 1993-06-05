33b6eafe9b637e4a64e0e823f19383704eb79e61
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-05 00:34:33 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: lib/libplot/f77plot/box.c 8.1
    SCCS-vsn: lib/libterm/termcap.3 8.1
    SCCS-vsn: lib/libplot/f77plot/circle.c 8.1
    SCCS-vsn: lib/libplot/f77plot/clospl.c 8.1
    SCCS-vsn: lib/libplot/f77plot/cont.c 8.1
    SCCS-vsn: lib/libplot/f77plot/dot.c 8.1
    SCCS-vsn: lib/libplot/f77plot/erase.c 8.1
    SCCS-vsn: lib/libplot/f77plot/label.c 8.1

--- usr/src/lib/libplot/f77plot/box.c
+++ usr/src/lib/libplot/f77plot/box.c
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1980 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1980, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.proprietary.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)box.c	5.3 (Berkeley) %G%";
+static char sccsid[] = "@(#)box.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 box_(x0, y0, x1, y1)
