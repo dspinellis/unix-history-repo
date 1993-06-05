a8fc356211e169d4732bf93277f804fa0f01cdc7
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-05 00:45:17 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: lib/libplot/imagen/erase.c 8.1
    SCCS-vsn: lib/libplot/imagen/imPcodes.h 8.1
    SCCS-vsn: lib/libplot/imagen/imp.h 8.1
    SCCS-vsn: lib/libplot/imagen/label.c 8.1
    SCCS-vsn: lib/libplot/imagen/line.c 8.1
    SCCS-vsn: lib/libplot/imagen/linemod.c 8.1
    SCCS-vsn: lib/libplot/imagen/move.c 8.1
    SCCS-vsn: lib/libplot/imagen/open.c 8.1
    SCCS-vsn: lib/libplot/imagen/point.c 8.1
    SCCS-vsn: lib/libplot/imagen/scale.c 8.1
    SCCS-vsn: lib/libplot/imagen/space.c 8.1

--- usr/src/lib/libplot/imagen/label.c
+++ usr/src/lib/libplot/imagen/label.c
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1985 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1985, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.proprietary.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)label.c	5.2 (Berkeley) %G%";
+static char sccsid[] = "@(#)label.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 #include "imPcodes.h"
