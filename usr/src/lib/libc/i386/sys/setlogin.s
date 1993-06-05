fea6be11e9822149bcf1c84e5b3761f4049143f1
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-04 19:44:46 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: lib/libc/i386/sys/Ovfork.s 8.1
    SCCS-vsn: lib/libc/i386/sys/brk.s 8.1
    SCCS-vsn: lib/libc/i386/sys/cerror.s 8.1
    SCCS-vsn: lib/libc/i386/sys/exect.s 8.1
    SCCS-vsn: lib/libc/i386/sys/fork.s 8.1
    SCCS-vsn: lib/libc/i386/sys/mount.s 8.1
    SCCS-vsn: lib/libc/i386/sys/pipe.s 8.1
    SCCS-vsn: lib/libc/i386/sys/ptrace.s 8.1
    SCCS-vsn: lib/libc/i386/sys/reboot.s 8.1
    SCCS-vsn: lib/libc/i386/DEFS.h 8.1
    SCCS-vsn: lib/libc/i386/sys/sbrk.s 8.1
    SCCS-vsn: lib/libc/i386/SYS.h 8.1
    SCCS-vsn: lib/libc/i386/sys/setlogin.s 8.1
    SCCS-vsn: lib/libc/i386/sys/sigpending.s 8.1
    SCCS-vsn: lib/libc/i386/sys/sigprocmask.s 8.1
    SCCS-vsn: lib/libc/i386/sys/sigreturn.s 8.1
    SCCS-vsn: lib/libc/i386/sys/sigsuspend.s 8.1
    SCCS-vsn: lib/libc/i386/sys/syscall.s 8.1
    SCCS-vsn: lib/libc/locale/ansi.c 8.1
    SCCS-vsn: lib/libc/locale/ctype.c 8.1
    SCCS-vsn: lib/libc/locale/euc.c 8.1
    SCCS-vsn: lib/libc/locale/frune.c 8.1
    SCCS-vsn: lib/libc/locale/isctype.c 8.1
    SCCS-vsn: lib/libc/locale/lconv.c 8.1
    SCCS-vsn: lib/libc/locale/localeconv.c 8.1
    SCCS-vsn: lib/libc/locale/mbrune.c 8.1
    SCCS-vsn: lib/libc/locale/none.c 8.1
    SCCS-vsn: lib/libc/locale/rune.c 8.1
    SCCS-vsn: lib/libc/locale/setlocale.c 8.1
    SCCS-vsn: lib/libc/locale/table.c 8.1
    SCCS-vsn: lib/libc/locale/utf2.c 8.1
    SCCS-vsn: lib/libc/locale/ctype.3 8.1
    SCCS-vsn: lib/libc/locale/isalnum.3 8.1
    SCCS-vsn: lib/libc/locale/isalpha.3 8.1
    SCCS-vsn: lib/libc/locale/isascii.3 8.1
    SCCS-vsn: lib/libc/locale/isblank.3 8.1
    SCCS-vsn: lib/libc/locale/iscntrl.3 8.1
    SCCS-vsn: lib/libc/locale/isdigit.3 8.1
    SCCS-vsn: lib/libc/locale/isgraph.3 8.1
    SCCS-vsn: lib/libc/locale/islower.3 8.1
    SCCS-vsn: lib/libc/locale/isprint.3 8.1
    SCCS-vsn: lib/libc/locale/ispunct.3 8.1
    SCCS-vsn: lib/libc/locale/isspace.3 8.1
    SCCS-vsn: lib/libc/locale/isupper.3 8.1
    SCCS-vsn: lib/libc/locale/isxdigit.3 8.1
    SCCS-vsn: lib/libc/locale/mbrune.3 8.1
    SCCS-vsn: lib/libc/locale/multibyte.3 8.1
    SCCS-vsn: lib/libc/locale/rune.3 8.1
    SCCS-vsn: lib/libc/locale/setlocale.3 8.1
    SCCS-vsn: lib/libc/locale/toascii.3 8.1
    SCCS-vsn: lib/libc/locale/tolower.3 8.1
    SCCS-vsn: lib/libc/locale/toupper.3 8.1

--- usr/src/lib/libc/i386/sys/setlogin.s
+++ usr/src/lib/libc/i386/sys/setlogin.s
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1991 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1991, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * William Jolitz.
@@ -9,7 +9,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-	.asciz "@(#)setlogin.s	5.2 (Berkeley) %G%"
+	.asciz "@(#)setlogin.s	8.1 (Berkeley) %G%"
 #endif /* LIBC_SCCS and not lint */
 
 #include "SYS.h"
--- usr/src/lib/libc/locale/frune.c
+++ usr/src/lib/libc/locale/frune.c
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1993 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Paul Borman at Krystal Technologies.
@@ -9,7 +9,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)frune.c	5.1 (Berkeley) %G%";
+static char sccsid[] = "@(#)frune.c	8.1 (Berkeley) %G%";
 #endif /* LIBC_SCCS and not lint */
 
 #include <limits.h>
