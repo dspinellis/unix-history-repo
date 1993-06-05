747d84742db26160284f62d5ad9984609f1cac07
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-05 01:56:24 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: libexec/mail.local/mail.local.8 8.1
    SCCS-vsn: libexec/mail.local/mail.local.c 8.1
    SCCS-vsn: libexec/mail.local/pathnames.h 8.1

--- usr/src/libexec/mail.local/mail.local.c
+++ usr/src/libexec/mail.local/mail.local.c
@@ -1,18 +1,18 @@
 /*-
- * Copyright (c) 1990 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-char copyright[] =
-"@(#) Copyright (c) 1990 The Regents of the University of California.\n\
- All rights reserved.\n";
+static char copyright[] =
+"@(#) Copyright (c) 1990, 1993\n\
+	The Regents of the University of California.  All rights reserved.\n";
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)mail.local.c	5.13 (Berkeley) %G%";
+static char sccsid[] = "@(#)mail.local.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/param.h>
