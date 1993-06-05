498ff63a1edf9e6bb95c09730c77f80e88363df9
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-05 00:43:33 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: lib/libplot/hp2648/arc.c 8.1
    SCCS-vsn: lib/libplot/hp2648/box.c 8.1
    SCCS-vsn: lib/libplot/hp2648/circle.c 8.1
    SCCS-vsn: lib/libplot/hp2648/close.c 8.1
    SCCS-vsn: lib/libplot/hp2648/cont.c 8.1
    SCCS-vsn: lib/libplot/hp2648/dot.c 8.1
    SCCS-vsn: lib/libplot/hp2648/erase.c 8.1
    SCCS-vsn: lib/libplot/hp2648/hp2648.h 8.1
    SCCS-vsn: lib/libplot/hp2648/label.c 8.1
    SCCS-vsn: lib/libplot/hp2648/line.c 8.1
    SCCS-vsn: lib/libplot/hp2648/linemod.c 8.1
    SCCS-vsn: lib/libplot/hp2648/move.c 8.1
    SCCS-vsn: lib/libplot/hp2648/open.c 8.1
    SCCS-vsn: lib/libplot/hp2648/point.c 8.1
    SCCS-vsn: lib/libplot/hp7221/arc.c 8.1
    SCCS-vsn: lib/libplot/hp2648/space.c 8.1
    SCCS-vsn: lib/libplot/hp7221/box.c 8.1
    SCCS-vsn: lib/libplot/hp2648/subr.c 8.1

--- usr/src/lib/libplot/hp2648/point.c
+++ usr/src/lib/libplot/hp2648/point.c
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1980 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1980, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.proprietary.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)point.c	5.2 (Berkeley) %G%";
+static char sccsid[] = "@(#)point.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 #include "hp2648.h"
