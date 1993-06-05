32badf819ae2906a094de1fc9c57b98c0d769242
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-05 00:49:35 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: lib/libplot/plot/arc.c 8.1
    SCCS-vsn: lib/libplot/plot/box.c 8.1
    SCCS-vsn: lib/libplot/plot/circle.c 8.1
    SCCS-vsn: lib/libplot/plot/close.c 8.1
    SCCS-vsn: lib/libplot/t300/arc.c 8.1
    SCCS-vsn: lib/libplot/plot/cont.c 8.1
    SCCS-vsn: lib/libplot/t300/box.c 8.1
    SCCS-vsn: lib/libplot/plot/dot.c 8.1
    SCCS-vsn: lib/libplot/t300/circle.c 8.1
    SCCS-vsn: lib/libplot/plot/erase.c 8.1
    SCCS-vsn: lib/libplot/t300/close.c 8.1
    SCCS-vsn: lib/libplot/plot/label.c 8.1
    SCCS-vsn: lib/libplot/t300/con.h 8.1
    SCCS-vsn: lib/libplot/plot/line.c 8.1
    SCCS-vsn: lib/libplot/t300/dot.c 8.1
    SCCS-vsn: lib/libplot/plot/linmod.c 8.1
    SCCS-vsn: lib/libplot/t300/erase.c 8.1
    SCCS-vsn: lib/libplot/plot/move.c 8.1
    SCCS-vsn: lib/libplot/plot/open.c 8.1
    SCCS-vsn: lib/libplot/t300/label.c 8.1
    SCCS-vsn: lib/libplot/plot/plot.3 8.1
    SCCS-vsn: lib/libplot/t300/line.c 8.1
    SCCS-vsn: lib/libplot/plot/point.c 8.1
    SCCS-vsn: lib/libplot/t300/linmod.c 8.1
    SCCS-vsn: lib/libplot/plot/putsi.c 8.1
    SCCS-vsn: lib/libplot/t300/move.c 8.1
    SCCS-vsn: lib/libplot/plot/space.c 8.1
    SCCS-vsn: lib/libplot/t300/open.c 8.1

--- usr/src/lib/libplot/t300/linmod.c
+++ usr/src/lib/libplot/t300/linmod.c
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1983 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1983, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.proprietary.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)linmod.c	4.2 (Berkeley) %G%";
+static char sccsid[] = "@(#)linmod.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 linemod(){
