9c6a96ecd2f2d1e4e55cd6da8cd978905eb1a9a8
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-04 23:28:27 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: lib/libcompat/4.3/tahoe/insque.s 8.1
    SCCS-vsn: lib/libcompat/4.3/tahoe/nargs.s 8.1
    SCCS-vsn: lib/libcompat/4.3/tahoe/remque.s 8.1
    SCCS-vsn: lib/libcompat/4.3/vax/insque.s 8.1
    SCCS-vsn: lib/libcompat/4.3/vax/nargs.s 8.1
    SCCS-vsn: lib/libcompat/4.3/vax/remque.s 8.1
    SCCS-vsn: lib/libcompat/4.3/hp300/insque.s 8.1
    SCCS-vsn: lib/libcompat/4.3/hp300/remque.s 8.1

--- usr/src/lib/libcompat/4.3/vax/insque.s
+++ usr/src/lib/libcompat/4.3/vax/insque.s
@@ -1,12 +1,12 @@
 /*
- * Copyright (c) 1983 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1983, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-	.asciz "@(#)insque.s	5.6 (Berkeley) %G%"
+	.asciz "@(#)insque.s	8.1 (Berkeley) %G%"
 #endif /* LIBC_SCCS and not lint */
 
 /* insque(new, pred) */
--- usr/src/lib/libcompat/4.3/vax/nargs.s
+++ usr/src/lib/libcompat/4.3/vax/nargs.s
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1984 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1984, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.proprietary.c%
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-	.asciz "@(#)nargs.s	5.4 (Berkeley) %G%"
+	.asciz "@(#)nargs.s	8.1 (Berkeley) %G%"
 #endif /* LIBC_SCCS and not lint */
 
 #include "DEFS.h"
