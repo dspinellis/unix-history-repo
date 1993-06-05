2a9ef6dad6e8d36bbf8e6f20fd7963c341d80d86
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-05 00:32:29 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: lib/libplot/dumb/point.c 8.1
    SCCS-vsn: lib/libplot/dumb/space.c 8.1
    SCCS-vsn: lib/libplot/dumb/subr.c 8.1
    SCCS-vsn: lib/libterm/pathnames.h 8.1
    SCCS-vsn: lib/libterm/termcap.c 8.1
    SCCS-vsn: lib/libterm/tgoto.c 8.1

--- usr/src/lib/libterm/termcap.c
+++ usr/src/lib/libterm/termcap.c
@@ -1,12 +1,12 @@
 /*
- * Copyright (c) 1980 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1980, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)termcap.c	5.9 (Berkeley) %G%";
+static char sccsid[] = "@(#)termcap.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 #define	PBUFSIZ		512	/* max length of filename path */
--- usr/src/lib/libterm/tgoto.c
+++ usr/src/lib/libterm/tgoto.c
@@ -1,12 +1,12 @@
 /*
- * Copyright (c) 1980 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1980, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)tgoto.c	5.4 (Berkeley) %G%";
+static char sccsid[] = "@(#)tgoto.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 #define	CTRL(c)	((c) & 037)
