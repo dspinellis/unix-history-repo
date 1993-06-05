4b9b66abf88eac8d74861e218db668938e8b9c8e
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-05 01:50:22 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: libexec/getty/extern.h 8.1
    SCCS-vsn: libexec/getty/getty.8 8.1
    SCCS-vsn: libexec/getty/gettytab.5 8.1
    SCCS-vsn: libexec/kpasswdd/kpasswdd.8 8.1
    SCCS-vsn: libexec/getty/gettytab.h 8.1
    SCCS-vsn: libexec/kpasswdd/kpasswdd.c 8.1

--- usr/src/libexec/getty/gettytab.h
+++ usr/src/libexec/getty/gettytab.h
@@ -1,10 +1,10 @@
 /*
- * Copyright (c) 1983 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1983, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)gettytab.h	5.6 (Berkeley) %G%
+ *	@(#)gettytab.h	8.1 (Berkeley) %G%
  */
 
 /*
