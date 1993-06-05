40268f71dc20017f512dd6a39a0ad963b73bf237
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-05 00:51:14 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: lib/libplot/t300s/arc.c 8.1
    SCCS-vsn: lib/libplot/t300s/box.c 8.1
    SCCS-vsn: lib/libplot/t300s/circle.c 8.1
    SCCS-vsn: lib/libplot/t300s/close.c 8.1
    SCCS-vsn: lib/libplot/t300s/con.h 8.1
    SCCS-vsn: lib/libplot/t300s/dot.c 8.1
    SCCS-vsn: lib/libplot/t300s/erase.c 8.1
    SCCS-vsn: lib/libplot/t300s/label.c 8.1
    SCCS-vsn: lib/libplot/t300s/line.c 8.1
    SCCS-vsn: lib/libplot/t300s/linmod.c 8.1
    SCCS-vsn: lib/libplot/t300s/move.c 8.1
    SCCS-vsn: lib/libplot/t4013/arc.c 8.1
    SCCS-vsn: lib/libplot/t300s/open.c 8.1
    SCCS-vsn: lib/libplot/t4013/box.c 8.1
    SCCS-vsn: lib/libplot/t300s/point.c 8.1
    SCCS-vsn: lib/libplot/t4013/circle.c 8.1
    SCCS-vsn: lib/libplot/t300s/space.c 8.1
    SCCS-vsn: lib/libplot/t4013/close.c 8.1
    SCCS-vsn: lib/libplot/t300s/subr.c 8.1

--- usr/src/lib/libplot/t4013/box.c
+++ usr/src/lib/libplot/t4013/box.c
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1985 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1985, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.proprietary.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)box.c	5.2 (Berkeley) %G%";
+static char sccsid[] = "@(#)box.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 box(x0, y0, x1, y1)
