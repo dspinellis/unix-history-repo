53d7a8a385293d555a181715edaeef98c79cd1bf
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-04 22:25:43 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: lib/libc/db/recno/rec_get.c 8.1
    SCCS-vsn: lib/libc/db/recno/rec_open.c 8.1
    SCCS-vsn: lib/libc/db/recno/rec_put.c 8.1
    SCCS-vsn: lib/libc/db/recno/rec_search.c 8.1
    SCCS-vsn: lib/libc/db/recno/rec_seq.c 8.1
    SCCS-vsn: lib/libc/db/recno/rec_utils.c 8.1
    SCCS-vsn: lib/libc/db/recno/recno.h 8.1

--- usr/src/lib/libc/db/recno/rec_get.c
+++ usr/src/lib/libc/db/recno/rec_get.c
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1990 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)rec_get.c	5.10 (Berkeley) %G%";
+static char sccsid[] = "@(#)rec_get.c	8.1 (Berkeley) %G%";
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/types.h>
--- usr/src/lib/libc/db/recno/recno.h
+++ usr/src/lib/libc/db/recno/recno.h
@@ -1,10 +1,10 @@
 /*-
- * Copyright (c) 1991 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1991, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)recno.h	5.1 (Berkeley) %G%
+ *	@(#)recno.h	8.1 (Berkeley) %G%
  */
 
 enum SRCHOP { SDELETE, SINSERT, SEARCH};	/* Rec_search operation. */
