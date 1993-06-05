0671a0723ebf069e2c3b54470fb815476d2f8b9b
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-05 01:58:57 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: libexec/rbootd/rmp_var.h 8.1
    SCCS-vsn: libexec/rbootd/rmpproto.c 8.1
    SCCS-vsn: libexec/rbootd/utils.c 8.1

--- usr/src/libexec/rbootd/rmpproto.c
+++ usr/src/libexec/rbootd/rmpproto.c
@@ -12,14 +12,14 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)rmpproto.c	5.2 (Berkeley) %G%
+ *	@(#)rmpproto.c	8.1 (Berkeley) %G%
  *
  * Utah $Hdr: rmpproto.c 3.1 92/07/06$
  * Author: Jeff Forys, University of Utah CSS
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)rmpproto.c	5.2 (Berkeley) %G%";
+static char sccsid[] = "@(#)rmpproto.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/param.h>
