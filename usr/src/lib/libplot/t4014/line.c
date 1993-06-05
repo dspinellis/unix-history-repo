f214c322072db38cdf82c7e15bb52860cd60b009
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-05 01:16:26 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: lib/libplot/t4014/erase.c 8.1
    SCCS-vsn: lib/libplot/t4014/label.c 8.1
    SCCS-vsn: lib/libplot/t4014/line.c 8.1
    SCCS-vsn: lib/libplot/t4014/linemod.c 8.1
    SCCS-vsn: lib/libplot/t4014/move.c 8.1
    SCCS-vsn: lib/libplot/t4014/open.c 8.1
    SCCS-vsn: lib/libplot/t4014/point.c 8.1
    SCCS-vsn: lib/libplot/t4014/scale.c 8.1
    SCCS-vsn: lib/libplot/t4014/space.c 8.1
    SCCS-vsn: lib/libplot/t4014/subr.c 8.1

--- usr/src/lib/libplot/t4014/line.c
+++ usr/src/lib/libplot/t4014/line.c
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1983 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1983, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.proprietary.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)line.c	4.2 (Berkeley) %G%";
+static char sccsid[] = "@(#)line.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 line(x0,y0,x1,y1){
