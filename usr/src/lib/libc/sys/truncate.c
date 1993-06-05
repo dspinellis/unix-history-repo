38cf370e7caeed518d7fa0ab4a12b5445c8877d7
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-04 20:20:56 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: lib/libc/sys/setpgid.2 8.1
    SCCS-vsn: lib/libc/sys/setsid.2 8.1
    SCCS-vsn: lib/libc/sys/setuid.2 8.1
    SCCS-vsn: lib/libc/sys/shutdown.2 8.1
    SCCS-vsn: lib/libc/sys/sigaction.2 8.1
    SCCS-vsn: lib/libc/sys/sigaltstack.2 8.1
    SCCS-vsn: lib/libc/sys/sigpending.2 8.1
    SCCS-vsn: lib/libc/sys/sigprocmask.2 8.1
    SCCS-vsn: lib/libc/sys/sigreturn.2 8.1
    SCCS-vsn: lib/libc/sys/sigstack.2 8.1
    SCCS-vsn: lib/libc/sys/sigsuspend.2 8.1
    SCCS-vsn: lib/libc/sys/socket.2 8.1
    SCCS-vsn: lib/libc/sys/socketpair.2 8.1
    SCCS-vsn: lib/libc/sys/stat.2 8.1
    SCCS-vsn: lib/libc/sys/statfs.2 8.1
    SCCS-vsn: lib/libc/sys/swapon.2 8.1
    SCCS-vsn: lib/libc/sys/symlink.2 8.1
    SCCS-vsn: lib/libc/sys/sync.2 8.1
    SCCS-vsn: lib/libc/sys/syscall.2 8.1
    SCCS-vsn: lib/libc/sys/truncate.2 8.1
    SCCS-vsn: lib/libc/sys/truncate.c 8.1
    SCCS-vsn: lib/libc/sys/umask.2 8.1
    SCCS-vsn: lib/libc/sys/unlink.2 8.1
    SCCS-vsn: lib/libc/sys/utimes.2 8.1
    SCCS-vsn: lib/libc/sys/vfork.2 8.1
    SCCS-vsn: lib/libc/sys/wait.2 8.1
    SCCS-vsn: lib/libc/sys/write.2 8.1

--- usr/src/lib/libc/sys/truncate.c
+++ usr/src/lib/libc/sys/truncate.c
@@ -6,7 +6,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)truncate.c	5.2 (Berkeley) %G%";
+static char sccsid[] = "@(#)truncate.c	8.1 (Berkeley) %G%";
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/types.h>
