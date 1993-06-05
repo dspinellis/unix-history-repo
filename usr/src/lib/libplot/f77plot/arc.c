017187f2c7a91c2600957c4b5678b3f80236eb4e
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-05 00:33:58 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: lib/libplot/f77plot/arc.c 8.1

--- usr/src/lib/libplot/f77plot/arc.c
+++ usr/src/lib/libplot/f77plot/arc.c
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1980 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1980, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.proprietary.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)arc.c	5.2 (Berkeley) %G%";
+static char sccsid[] = "@(#)arc.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 arc_(x,y,x0,y0,x1,y1)
