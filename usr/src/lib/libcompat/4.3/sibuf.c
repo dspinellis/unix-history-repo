7dec9fa82cadd5546afeef577dbdc918f69e5004
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-04 23:26:48 -0800

    4.4BSD snapshot (revision 8.1)
    SCCS-vsn: lib/libcompat/4.3/sibuf.c 8.1
    SCCS-vsn: lib/libcompat/4.3/sobuf.c 8.1
    SCCS-vsn: lib/libcompat/4.3/strout.c 8.1

--- usr/src/lib/libcompat/4.3/sibuf.c
+++ usr/src/lib/libcompat/4.3/sibuf.c
@@ -3,7 +3,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)sibuf.c	5.3 (Berkeley) %G%";
+static char sccsid[] = "@(#)sibuf.c	8.1 (Berkeley) %G%";
 #endif /* LIBC_SCCS and not lint */
 
 #include <stdio.h>
--- usr/src/lib/libcompat/4.3/strout.c
+++ usr/src/lib/libcompat/4.3/strout.c
@@ -3,7 +3,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)strout.c	5.3 (Berkeley) %G%";
+static char sccsid[] = "@(#)strout.c	8.1 (Berkeley) %G%";
 #endif /* LIBC_SCCS and not lint */
 
 #include	<stdio.h>
