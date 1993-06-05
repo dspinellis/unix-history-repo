25e909710a8e1e8978fbf4e8b4ed80770c0c4846
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-05 00:34:59 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: lib/libplot/f77plot/point.c 8.1
    SCCS-vsn: lib/libtelnet/auth-proto.h 8.1
    SCCS-vsn: lib/libplot/f77plot/space.c 8.1

--- usr/src/lib/libplot/f77plot/space.c
+++ usr/src/lib/libplot/f77plot/space.c
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1980 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1980, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.proprietary.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)space.c	5.2 (Berkeley) %G%";
+static char sccsid[] = "@(#)space.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 space_(x0,y0,x1,y1)
