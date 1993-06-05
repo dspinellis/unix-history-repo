85cf8afaa2ab1a0bf328616a833d67fd3328d84f
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-05 00:35:40 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: lib/libtelnet/auth.c 8.1
    SCCS-vsn: lib/libtelnet/auth.h 8.1
    SCCS-vsn: lib/libtelnet/enc-proto.h 8.1
    SCCS-vsn: lib/libtelnet/enc_des.c 8.1
    SCCS-vsn: lib/libtelnet/encrypt.c 8.1
    SCCS-vsn: lib/libtelnet/encrypt.h 8.1
    SCCS-vsn: lib/libtelnet/genget.c 8.1

--- usr/src/lib/libtelnet/encrypt.h
+++ usr/src/lib/libtelnet/encrypt.h
@@ -1,10 +1,10 @@
 /*-
- * Copyright (c) 1991 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1991, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)encrypt.h	5.4 (Berkeley) %G%
+ *	@(#)encrypt.h	8.1 (Berkeley) %G%
  */
 
 /*
