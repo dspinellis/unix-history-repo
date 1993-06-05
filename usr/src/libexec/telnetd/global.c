4aec58aea6433a51975f6381a0b2b7e2b6de2700
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-05 02:07:34 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: libexec/telnetd/authenc.c 8.1
    SCCS-vsn: libexec/telnetd/defs.h 8.1
    SCCS-vsn: libexec/telnetd/ext.h 8.1
    SCCS-vsn: libexec/telnetd/global.c 8.1
    SCCS-vsn: libexec/telnetd/pathnames.h 8.1
    SCCS-vsn: libexec/telnetd/slc.c 8.1
    SCCS-vsn: libexec/telnetd/state.c 8.1
    SCCS-vsn: libexec/telnetd/sys_term.c 8.1
    SCCS-vsn: libexec/telnetd/telnetd.8 8.1
    SCCS-vsn: libexec/telnetd/telnetd.c 8.1
    SCCS-vsn: libexec/telnetd/telnetd.h 8.1
    SCCS-vsn: libexec/telnetd/termstat.c 8.1
    SCCS-vsn: libexec/telnetd/utility.c 8.1

--- usr/src/libexec/telnetd/global.c
+++ usr/src/libexec/telnetd/global.c
@@ -1,12 +1,12 @@
 /*
- * Copyright (c) 1989 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1989, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)global.c	5.2 (Berkeley) %G%";
+static char sccsid[] = "@(#)global.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 /*
--- usr/src/libexec/telnetd/telnetd.h
+++ usr/src/libexec/telnetd/telnetd.h
@@ -1,10 +1,10 @@
 /*
- * Copyright (c) 1989 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1989, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)telnetd.h	5.3 (Berkeley) %G%
+ *	@(#)telnetd.h	8.1 (Berkeley) %G%
  */
 
 
