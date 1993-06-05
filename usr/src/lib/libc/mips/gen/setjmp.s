7c6dc1dfe293e57b2c09de521234d1dc5a424192
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-04 19:46:48 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: lib/libc/mips/net/htonl.s 8.1
    SCCS-vsn: lib/libc/mips/gen/modf.s 8.1
    SCCS-vsn: lib/libc/mips/net/htons.s 8.1
    SCCS-vsn: lib/libc/mips/gen/nlist.c 8.1
    SCCS-vsn: lib/libc/mips/gen/setjmp.s 8.1

--- usr/src/lib/libc/mips/gen/setjmp.s
+++ usr/src/lib/libc/mips/gen/setjmp.s
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1991 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1991, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Ralph Campbell.
@@ -13,7 +13,7 @@
 #include <machine/machAsmDefs.h>
 
 #if defined(LIBC_SCCS) && !defined(lint)
-	ASMSTR("@(#)setjmp.s	5.6 (Berkeley) %G%")
+	ASMSTR("@(#)setjmp.s	8.1 (Berkeley) %G%")
 #endif /* LIBC_SCCS and not lint */
 
 /*
