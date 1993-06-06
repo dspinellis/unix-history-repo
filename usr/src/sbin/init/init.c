ce72dd49b1ceab1192054ea4bd27b0ff13c9c115
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-05 18:00:34 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: sbin/init/init.8 8.1
    SCCS-vsn: sbin/init/init.c 8.1
    SCCS-vsn: sbin/init/pathnames.h 8.1

--- usr/src/sbin/init/init.c
+++ usr/src/sbin/init/init.c
@@ -15,7 +15,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)init.c	6.23 (Berkeley) %G%";
+static char sccsid[] = "@(#)init.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/param.h>
