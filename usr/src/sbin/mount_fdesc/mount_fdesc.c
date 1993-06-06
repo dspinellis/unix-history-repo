dd6f70958313886b48b66fb7558ac71a5f435d2a
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-05 18:02:07 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: sbin/mount_fdesc/mount_fdesc.8 8.1
    SCCS-vsn: sbin/mount_fdesc/mount_fdesc.c 8.1

--- usr/src/sbin/mount_fdesc/mount_fdesc.c
+++ usr/src/sbin/mount_fdesc/mount_fdesc.c
@@ -1,14 +1,14 @@
 /*
  * Copyright (c) 1990, 1992 Jan-Simon Pendry
- * Copyright (c) 1992 The Regents of the University of California
- * All rights reserved.
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Jan-Simon Pendry.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)mount_fdesc.c	5.2 (Berkeley) %G%
+ *	@(#)mount_fdesc.c	8.1 (Berkeley) %G%
  */
 
 #include <sys/param.h>
