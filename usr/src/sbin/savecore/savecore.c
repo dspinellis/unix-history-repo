079071cf1a37f746392344ceaf27519b66d3bb3f
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-05 18:17:40 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: sbin/savecore/savecore.8 8.1
    SCCS-vsn: sbin/savecore/savecore.c 8.1

--- usr/src/sbin/savecore/savecore.c
+++ usr/src/sbin/savecore/savecore.c
@@ -1,18 +1,18 @@
 /*-
- * Copyright (c) 1986, 1992 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1986, 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-char copyright[] =
-"@(#) Copyright (c) 1986, 1992 The Regents of the University of California.\n\
- All rights reserved.\n";
+static char copyright[] =
+"@(#) Copyright (c) 1986, 1992, 1993\n\
+	The Regents of the University of California.  All rights reserved.\n";
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)savecore.c	5.38 (Berkeley) %G%";
+static char sccsid[] = "@(#)savecore.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/param.h>
