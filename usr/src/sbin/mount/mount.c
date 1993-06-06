f53dd5e3a3694cdb3ebaac26ce4e96e157c03866
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-05 18:01:29 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: sbin/mount/mount.8 8.1
    SCCS-vsn: sbin/mount/mount.c 8.1
    SCCS-vsn: sbin/mount/pathnames.h 8.1

--- usr/src/sbin/mount/mount.c
+++ usr/src/sbin/mount/mount.c
@@ -1,18 +1,18 @@
 /*
- * Copyright (c) 1980, 1989 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1980, 1989, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-char copyright[] =
-"@(#) Copyright (c) 1980, 1989 The Regents of the University of California.\n\
- All rights reserved.\n";
+static char copyright[] =
+"@(#) Copyright (c) 1980, 1989, 1993\n\
+	The Regents of the University of California.  All rights reserved.\n";
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)mount.c	5.52 (Berkeley) %G%";
+static char sccsid[] = "@(#)mount.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/param.h>
