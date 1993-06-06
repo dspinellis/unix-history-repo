ae1f94170d7f4a569fde37dedaf692729aa3d88d
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-05 18:07:22 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: sbin/newlfs/config.h 8.1
    SCCS-vsn: sbin/newlfs/extern.h 8.1
    SCCS-vsn: sbin/newlfs/lfs.c 8.1
    SCCS-vsn: sbin/newlfs/misc.c 8.1
    SCCS-vsn: sbin/newlfs/newfs.c 8.1

--- usr/src/sbin/newlfs/newfs.c
+++ usr/src/sbin/newlfs/newfs.c
@@ -1,18 +1,18 @@
 /*-
- * Copyright (c) 1989, 1992 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1989, 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-char copyright[] =
-"@(#) Copyright (c) 1989, 1992 The Regents of the University of California.\n\
- All rights reserved.\n";
+static char copyright[] =
+"@(#) Copyright (c) 1989, 1992, 1993\n\
+	The Regents of the University of California.  All rights reserved.\n";
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)newfs.c	5.7 (Berkeley) %G%";
+static char sccsid[] = "@(#)newfs.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 /*
