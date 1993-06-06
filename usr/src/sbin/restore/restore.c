c13358f6b8ce5507761582279ef89c532f41eaed
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-05 18:12:20 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: sbin/restore/dirs.c 8.1
    SCCS-vsn: sbin/restore/extern.h 8.1
    SCCS-vsn: sbin/restore/interactive.c 8.1
    SCCS-vsn: sbin/restore/main.c 8.1
    SCCS-vsn: sbin/restore/pathnames.h 8.1
    SCCS-vsn: sbin/restore/restore.8 8.1
    SCCS-vsn: sbin/restore/restore.c 8.1
    SCCS-vsn: sbin/restore/restore.h 8.1
    SCCS-vsn: sbin/restore/symtab.c 8.1
    SCCS-vsn: sbin/restore/tape.c 8.1
    SCCS-vsn: sbin/restore/utilities.c 8.1

--- usr/src/sbin/restore/restore.c
+++ usr/src/sbin/restore/restore.c
@@ -1,12 +1,12 @@
 /*
- * Copyright (c) 1983 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1983, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)restore.c	5.11 (Berkeley) %G%";
+static char sccsid[] = "@(#)restore.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/types.h>
--- usr/src/sbin/restore/utilities.c
+++ usr/src/sbin/restore/utilities.c
@@ -1,12 +1,12 @@
 /*
- * Copyright (c) 1983 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1983, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)utilities.c	5.11 (Berkeley) %G%";
+static char sccsid[] = "@(#)utilities.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/param.h>
