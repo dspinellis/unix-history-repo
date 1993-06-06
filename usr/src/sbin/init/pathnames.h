bc5774f9caee1fb48d677d2d77471d65f5baaac8
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-05 18:00:34 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: sbin/init/init.8 8.1
    SCCS-vsn: sbin/init/init.c 8.1
    SCCS-vsn: sbin/init/pathnames.h 8.1

--- usr/src/sbin/init/pathnames.h
+++ usr/src/sbin/init/pathnames.h
@@ -1,13 +1,13 @@
 /*-
- * Copyright (c) 1991 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1991, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Donn Seeley at Berkeley Software Design, Inc.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)pathnames.h	6.2 (Berkeley) %G%
+ *	@(#)pathnames.h	8.1 (Berkeley) %G%
  */
 
 #include <paths.h>
