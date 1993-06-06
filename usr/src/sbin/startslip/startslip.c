ce97255551d076edf817396ab2611a68fc703763
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-05 18:22:05 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: sbin/startslip/startslip.1 8.1
    SCCS-vsn: sbin/startslip/startslip.c 8.1

--- usr/src/sbin/startslip/startslip.c
+++ usr/src/sbin/startslip/startslip.c
@@ -1,19 +1,19 @@
 
 /*-
- * Copyright (c) 1990 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1990, 1991, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-char copyright[] =
-"@(#) Copyright (c) 1990, 1991 The Regents of the University of California.\n\
- All rights reserved.\n";
+static char copyright[] =
+"@(#) Copyright (c) 1990, 1991, 1993\n\
+	The Regents of the University of California.  All rights reserved.\n";
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)startslip.c	5.10 (Berkeley) %G%";
+static char sccsid[] = "@(#)startslip.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/param.h>
