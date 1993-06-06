a16db8cbefb84b061bca06fada573834038956c9
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-06-05 16:10:10 -0800

    don't send warning messages in response to warning messages
    SCCS-vsn: usr.sbin/sendmail/src/deliver.c 6.81

--- usr/src/usr.sbin/sendmail/src/deliver.c
+++ usr/src/usr.sbin/sendmail/src/deliver.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)deliver.c	6.80 (Berkeley) %G%";
+static char sccsid[] = "@(#)deliver.c	6.81 (Berkeley) %G%";
 #endif /* not lint */
 
 #include "sendmail.h"
@@ -1268,7 +1268,8 @@ markfailure(e, q, rcode)
 		    curtime() > e->e_ctime + TimeOuts.to_q_warning)
 		{
 			if (!bitset(EF_WARNING, e->e_flags) &&
-			    e->e_class >= 0)
+			    e->e_class >= 0 &&
+			    strcmp(e->e_from.q_paddr, "<>") != 0)
 			{
 				(void) sprintf(buf,
 					"warning: cannot send message for %s",
