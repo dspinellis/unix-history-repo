f42fcadd7a654fc252c666d9c3cb9131ce116182
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-06 21:41:10 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.bin/ftp/cmdtab.c 8.1
    SCCS-vsn: usr.bin/ftp/domacro.c 8.1
    SCCS-vsn: usr.bin/gcore/extern.h 8.1
    SCCS-vsn: usr.bin/ftp/ftp.1 8.1
    SCCS-vsn: usr.bin/gcore/gcore.1 8.1
    SCCS-vsn: usr.bin/gcore/gcore.c 8.1
    SCCS-vsn: usr.bin/ftp/ftp.c 8.1
    SCCS-vsn: usr.bin/gcore/md-sparc.c 8.1

--- usr/src/usr.bin/gcore/md-sparc.c
+++ usr/src/usr.bin/gcore/md-sparc.c
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1992 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This software was developed by the Computer Systems Engineering group
  * at Lawrence Berkeley Laboratory under DARPA contract BG 91-66 and
@@ -10,7 +10,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)md-sparc.c	5.4 (Berkeley) %G%";
+static char sccsid[] = "@(#)md-sparc.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/param.h>
