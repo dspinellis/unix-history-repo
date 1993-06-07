9d054362b748c57f582ea479a555265216a8b0f9
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-06 21:40:04 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.bin/fpr/fpr.1 8.1
    SCCS-vsn: usr.bin/fpr/fpr.c 8.1

--- usr/src/usr.bin/fpr/fpr.c
+++ usr/src/usr.bin/fpr/fpr.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1989 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1989, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Robert Corbett.
@@ -9,13 +9,13 @@
  */
 
 #ifndef lint
-char copyright[] =
-"@(#) Copyright (c) 1989 The Regents of the University of California.\n\
- All rights reserved.\n";
+static char copyright[] =
+"@(#) Copyright (c) 1989, 1993\n\
+	The Regents of the University of California.  All rights reserved.\n";
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)fpr.c	5.4 (Berkeley) %G%";
+static char sccsid[] = "@(#)fpr.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <stdio.h>
