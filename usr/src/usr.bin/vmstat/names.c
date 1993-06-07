382c164292bc525a7933b89d4fc52a851256d91a
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-07 05:22:21 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.bin/vmstat/names.c 8.1
    SCCS-vsn: usr.bin/vmstat/vmstat.8 8.1

--- usr/src/usr.bin/vmstat/names.c
+++ usr/src/usr.bin/vmstat/names.c
@@ -1,10 +1,10 @@
 /*-
- * Copyright (c) 1991 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1991, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)names.c	5.8 (Berkeley) %G%
+ *	@(#)names.c	8.1 (Berkeley) %G%
  */
 
 #if !defined(hp300) && !defined(tahoe) && !defined(vax) && \
