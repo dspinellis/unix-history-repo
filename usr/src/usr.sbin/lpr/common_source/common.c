b0d6ece00fa5e66a9fd3ad24f0b4ac23d22dc1bf
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-06 20:59:33 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.sbin/lpr/common_source/common.c 8.1
    SCCS-vsn: usr.sbin/lpr/common_source/displayq.c 8.1
    SCCS-vsn: usr.sbin/lpr/common_source/lp.h 8.1
    SCCS-vsn: usr.sbin/lpr/common_source/lp.local.h 8.1
    SCCS-vsn: usr.sbin/lpr/common_source/pathnames.h 8.1
    SCCS-vsn: usr.sbin/lpr/common_source/printcap.c 8.1
    SCCS-vsn: usr.sbin/lpr/common_source/rmjob.c 8.1
    SCCS-vsn: usr.sbin/lpr/common_source/startdaemon.c 8.1
    SCCS-vsn: usr.sbin/lpr/filters/lpf.c 8.1

--- usr/src/usr.sbin/lpr/common_source/common.c
+++ usr/src/usr.sbin/lpr/common_source/common.c
@@ -1,12 +1,12 @@
 /*
- * Copyright (c) 1983 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1983, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)common.c	5.10 (Berkeley) %G%";
+static char sccsid[] = "@(#)common.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/param.h>
