276971b6e06e2c6e83d47fb0d3e06ced563ae0c8
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-06 21:07:27 -0800

    4.4BSD snapshot (revision 8.1)
    SCCS-vsn: usr.sbin/sysctl/pathconf.c 8.1
    SCCS-vsn: usr.sbin/sysctl/Makefile 8.1

--- usr/src/usr.sbin/sysctl/pathconf.c
+++ usr/src/usr.sbin/sysctl/pathconf.c
@@ -12,7 +12,7 @@ static char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)pathconf.c	5.1 (Berkeley) %G%";
+static char sccsid[] = "@(#)pathconf.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/param.h>
