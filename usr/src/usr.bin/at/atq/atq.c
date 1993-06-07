1191fc68d4b16a210a74ed6f9307e5774124bd6c
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-06 21:21:21 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.bin/at/atq/atq.1 8.1
    SCCS-vsn: usr.bin/at/atq/atq.c 8.1
    SCCS-vsn: usr.bin/at/atq/pathnames.h 8.1

--- usr/src/usr.bin/at/atq/atq.c
+++ usr/src/usr.bin/at/atq/atq.c
@@ -1,18 +1,18 @@
 /*-
- * Copyright (c) 1983 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1983, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.proprietary.c%
  */
 
 #ifndef lint
-char copyright[] =
-"@(#) Copyright (c) 1983 The Regents of the University of California.\n\
- All rights reserved.\n";
+static char copyright[] =
+"@(#) Copyright (c) 1983, 1993\n\
+	The Regents of the University of California.  All rights reserved.\n";
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)atq.c	5.8 (Berkeley) %G%";
+static char sccsid[] = "@(#)atq.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 /*
