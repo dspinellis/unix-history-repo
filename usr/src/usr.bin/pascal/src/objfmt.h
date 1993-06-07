c1dd26b1ed364b33f9a627e86e5954df02339fc0
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-06 23:09:59 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.bin/pascal/src/func.c 8.1
    SCCS-vsn: usr.bin/pascal/src/gen.c 8.1
    SCCS-vsn: usr.bin/pascal/src/hash.c 8.1
    SCCS-vsn: usr.bin/pascal/src/iorec.h 8.1
    SCCS-vsn: usr.bin/pascal/src/lab.c 8.1
    SCCS-vsn: usr.bin/pascal/src/lookup.c 8.1
    SCCS-vsn: usr.bin/pascal/src/lval.c 8.1
    SCCS-vsn: usr.bin/rlogin/des_rw.c 8.1
    SCCS-vsn: usr.bin/pascal/src/main.c 8.1
    SCCS-vsn: usr.bin/rlogin/kcmd.c 8.1
    SCCS-vsn: usr.bin/pascal/src/nl.c 8.1
    SCCS-vsn: usr.bin/rlogin/krb.h 8.1
    SCCS-vsn: usr.bin/rlogin/krcmd.c 8.1
    SCCS-vsn: usr.bin/pascal/src/objfmt.h 8.1
    SCCS-vsn: usr.bin/rlogin/rlogin.1 8.1
    SCCS-vsn: usr.bin/pascal/src/opc.c 8.1
    SCCS-vsn: usr.bin/rlogin/rlogin.c 8.1
    SCCS-vsn: usr.bin/pascal/src/p2put.c 8.1

--- usr/src/usr.bin/pascal/src/objfmt.h
+++ usr/src/usr.bin/pascal/src/objfmt.h
@@ -1,10 +1,10 @@
 /*-
- * Copyright (c) 1980 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1980, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)objfmt.h	5.4 (Berkeley) %G%
+ *	@(#)objfmt.h	8.1 (Berkeley) %G%
  */
 
 /*
