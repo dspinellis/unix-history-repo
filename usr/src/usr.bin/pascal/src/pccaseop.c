81d57fafb5ccc1ee1a77fc3ef8a2bd0d3ff4a5dc
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-06 23:10:49 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.bin/pascal/src/pTree.h 8.1
    SCCS-vsn: usr.bin/pascal/src/pc.h 8.1
    SCCS-vsn: usr.bin/pascal/src/pccaseop.c 8.1
    SCCS-vsn: usr.bin/pascal/src/pcfunc.c 8.1
    SCCS-vsn: usr.bin/pascal/src/pclval.c 8.1
    SCCS-vsn: usr.bin/pascal/src/pcproc.c 8.1
    SCCS-vsn: usr.bin/pascal/src/pcwhoami.h 8.1
    SCCS-vsn: usr.bin/pascal/src/pic.c 8.1
    SCCS-vsn: usr.bin/rs/rs.1 8.1
    SCCS-vsn: usr.bin/pascal/src/piwhoami.h 8.1
    SCCS-vsn: usr.bin/rs/rs.c 8.1
    SCCS-vsn: usr.bin/pascal/src/proc.c 8.1

--- usr/src/usr.bin/pascal/src/pccaseop.c
+++ usr/src/usr.bin/pascal/src/pccaseop.c
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1980 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1980, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
