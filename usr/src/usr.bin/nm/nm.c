061762ad66b4792a299fefbc25a0a361b60e4de6
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-06 22:48:16 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.bin/pascal/pdx/test/args.p 8.1
    SCCS-vsn: usr.bin/pascal/pdx/test/block.p 8.1
    SCCS-vsn: usr.bin/pascal/pdx/test/bug.p 8.1
    SCCS-vsn: usr.bin/nm/nm.1 8.1
    SCCS-vsn: usr.bin/pascal/pdx/test/call.p 8.1
    SCCS-vsn: usr.bin/pascal/pdx/test/case.p 8.1
    SCCS-vsn: usr.bin/nm/nm.c 8.1
    SCCS-vsn: usr.bin/pascal/pdx/test/enum.p 8.1

--- usr/src/usr.bin/nm/nm.c
+++ usr/src/usr.bin/nm/nm.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1989 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1989, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Hans Huebner.
@@ -9,13 +9,13 @@
  */
 
 #ifndef lint
-char copyright[] =
-"@(#) Copyright (c) 1989 The Regents of the University of California.\n\
- All rights reserved.\n";
+static char copyright[] =
+"@(#) Copyright (c) 1989, 1993\n\
+	The Regents of the University of California.  All rights reserved.\n";
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)nm.c	5.8 (Berkeley) %G%";
+static char sccsid[] = "@(#)nm.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/types.h>
--- usr/src/usr.bin/pascal/pdx/test/bug.p
+++ usr/src/usr.bin/pascal/pdx/test/bug.p
@@ -1,10 +1,10 @@
 (*
- * Copyright (c) 1980 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1980, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)bug.p	5.1 (Berkeley) %G%
+ *	@(#)bug.p	8.1 (Berkeley) %G%
  *)
 
 program test (output);
