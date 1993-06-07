42c7de29c6731ec1b31b310085b5fc4c72e2bb12
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-06 23:29:04 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.bin/systat/iostat.c 8.1

--- usr/src/usr.bin/systat/iostat.c
+++ usr/src/usr.bin/systat/iostat.c
@@ -1,12 +1,12 @@
 /*
- * Copyright (c) 1980, 1992 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1980, 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)iostat.c	5.13 (Berkeley) %G%";
+static char sccsid[] = "@(#)iostat.c	8.1 (Berkeley) %G%";
 #endif not lint
 
 #include <sys/param.h>
