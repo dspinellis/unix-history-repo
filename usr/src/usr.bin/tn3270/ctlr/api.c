f4f2498bac80c43eebd67a9fa825674ddc1bebcf
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-06 23:39:26 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.bin/tn3270/ctlr/3180.kbd 8.1
    SCCS-vsn: usr.bin/tn3270/ctlr/3270pc.kbd 8.1
    SCCS-vsn: usr.bin/tn3270/ctlr/api.c 8.1
    SCCS-vsn: usr.bin/tn3270/ctlr/api.h 8.1
    SCCS-vsn: usr.bin/tty/tty.1 8.1
    SCCS-vsn: usr.bin/tn3270/ctlr/declare.h 8.1
    SCCS-vsn: usr.bin/tty/tty.c 8.1

--- usr/src/usr.bin/tn3270/ctlr/api.c
+++ usr/src/usr.bin/tn3270/ctlr/api.c
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1988 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1988, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)api.c	4.5 (Berkeley) %G%";
+static char sccsid[] = "@(#)api.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 /*
