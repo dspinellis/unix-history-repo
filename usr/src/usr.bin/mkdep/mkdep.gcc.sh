adb07fb14eb3f3149db61262247b3ffbc5e9da81
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-06 22:31:26 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.bin/pascal/pcexterns/pcexterns.awk 8.1
    SCCS-vsn: usr.bin/mkdep/mkdep.append 8.1
    SCCS-vsn: usr.bin/mkdep/mkdep.gcc.sh 8.1
    SCCS-vsn: usr.bin/mkdep/mkdep.old.compiler 8.1
    SCCS-vsn: usr.bin/mkdep/mkdep.sh 8.1
    SCCS-vsn: usr.bin/mkdep/mkdep.ultrix 8.1
    SCCS-vsn: usr.bin/mkdep/mkdep.1 8.1

--- usr/src/usr.bin/mkdep/mkdep.gcc.sh
+++ usr/src/usr.bin/mkdep/mkdep.gcc.sh
@@ -1,11 +1,11 @@
 #!/bin/sh -
 #
-# Copyright (c) 1991 The Regents of the University of California.
-# All rights reserved.
+# Copyright (c) 1991, 1993
+#	The Regents of the University of California.  All rights reserved.
 #
 # %sccs.include.redist.sh%
 #
-#	@(#)mkdep.gcc.sh	5.6 (Berkeley) %G%
+#	@(#)mkdep.gcc.sh	8.1 (Berkeley) %G%
 #
 
 PATH=/bin:/usr/bin:/usr/ucb
--- usr/src/usr.bin/mkdep/mkdep.sh
+++ usr/src/usr.bin/mkdep/mkdep.sh
@@ -1,11 +1,11 @@
 #!/bin/sh -
 #
-# Copyright (c) 1991 The Regents of the University of California.
-# All rights reserved.
+# Copyright (c) 1991, 1993
+#	The Regents of the University of California.  All rights reserved.
 #
 # %sccs.include.redist.sh%
 #
-#	@(#)mkdep.sh	5.21 (Berkeley) %G%
+#	@(#)mkdep.sh	8.1 (Berkeley) %G%
 #
 
 PATH=/bin:/usr/bin:/usr/ucb:/usr/old/bin
