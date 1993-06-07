5a80e53e85d018c8fe49219a5410387703fb215a
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-06 20:50:31 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.sbin/arp/arp.8 8.1
    SCCS-vsn: usr.sbin/arp/arp.c 8.1

--- usr/src/usr.sbin/arp/arp.c
+++ usr/src/usr.sbin/arp/arp.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1984 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1984, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Sun Microsystems, Inc.
@@ -9,13 +9,13 @@
  */
 
 #ifndef lint
-char copyright[] =
-"@(#) Copyright (c) 1984 Regents of the University of California.\n\
- All rights reserved.\n";
+static char copyright[] =
+"@(#) Copyright (c) 1984, 1993\n\
+	The Regents of the University of California.  All rights reserved.\n";
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)arp.c	5.17 (Berkeley) %G%";
+static char sccsid[] = "@(#)arp.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 /*
