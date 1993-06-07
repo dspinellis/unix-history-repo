009633f8d0e3dda229fb269db3b5a1551b7d715a
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-06 21:39:47 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.bin/fmt/fmt.1 8.1
    SCCS-vsn: usr.bin/fmt/fmt.c 8.1

--- usr/src/usr.bin/fmt/fmt.c
+++ usr/src/usr.bin/fmt/fmt.c
@@ -12,7 +12,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)fmt.c	5.10 (Berkeley) %G%";
+static char sccsid[] = "@(#)fmt.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <stdio.h>
