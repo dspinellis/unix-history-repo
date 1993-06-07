a4fffe7b34fde674833ad4134255f38cc92ed904
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-06 21:19:48 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.bin/ar/append.c 8.1
    SCCS-vsn: usr.bin/ar/ar.1 8.1
    SCCS-vsn: usr.bin/ar/ar.5.5 8.1
    SCCS-vsn: usr.bin/ar/ar.c 8.1
    SCCS-vsn: usr.bin/ar/archive.c 8.1
    SCCS-vsn: usr.bin/ar/archive.h 8.1
    SCCS-vsn: usr.bin/ar/contents.c 8.1
    SCCS-vsn: usr.bin/ar/delete.c 8.1
    SCCS-vsn: usr.bin/ar/extern.h 8.1
    SCCS-vsn: usr.bin/ar/extract.c 8.1
    SCCS-vsn: usr.bin/ar/misc.c 8.1
    SCCS-vsn: usr.bin/ar/move.c 8.1
    SCCS-vsn: usr.bin/ar/pathnames.h 8.1
    SCCS-vsn: usr.bin/ar/print.c 8.1
    SCCS-vsn: usr.bin/ar/replace.c 8.1

--- usr/src/usr.bin/ar/pathnames.h
+++ usr/src/usr.bin/ar/pathnames.h
@@ -1,13 +1,13 @@
 /*-
- * Copyright (c) 1991 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1991, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Hugh Smith at The University of Guelph.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)pathnames.h	5.4 (Berkeley) %G%
+ *	@(#)pathnames.h	8.1 (Berkeley) %G%
  */
 
 #define	_NAME_ARTMP	"ar.XXXXXX"
--- usr/src/usr.bin/ar/replace.c
+++ usr/src/usr.bin/ar/replace.c
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1990 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Hugh Smith at The University of Guelph.
@@ -9,7 +9,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)replace.c	5.8 (Berkeley) %G%";
+static char sccsid[] = "@(#)replace.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/param.h>
