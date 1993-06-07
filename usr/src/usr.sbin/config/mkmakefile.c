55caad513008333979cbadbd6931753fc463b973
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-06 20:53:07 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.sbin/config/config.8 8.1
    SCCS-vsn: usr.sbin/config/config.h 8.1
    SCCS-vsn: usr.sbin/config/config.y 8.1
    SCCS-vsn: usr.sbin/config/lang.l 8.1
    SCCS-vsn: usr.sbin/config/main.c 8.1
    SCCS-vsn: usr.sbin/config/mkglue.c 8.1
    SCCS-vsn: usr.sbin/config/mkheaders.c 8.1
    SCCS-vsn: usr.sbin/config/mkioconf.c 8.1
    SCCS-vsn: usr.sbin/config/mkmakefile.c 8.1
    SCCS-vsn: usr.sbin/config/mkswapconf.c 8.1
    SCCS-vsn: usr.sbin/config/mkubglue.c 8.1

--- usr/src/usr.sbin/config/mkmakefile.c
+++ usr/src/usr.sbin/config/mkmakefile.c
@@ -1,12 +1,12 @@
 /*
- * Copyright (c) 1980,1990 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1993, 19801990
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)mkmakefile.c	5.39 (Berkeley) %G%";
+static char sccsid[] = "@(#)mkmakefile.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 /*
