ad9904f11db84c21614a323b390da95a70373fd2
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-07 05:27:38 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.bin/window/alias.h 8.1
    SCCS-vsn: usr.bin/window/char.c 8.1
    SCCS-vsn: usr.bin/window/char.h 8.1

--- usr/src/usr.bin/window/alias.h
+++ usr/src/usr.bin/window/alias.h
@@ -1,13 +1,13 @@
 /*
- * Copyright (c) 1983 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1983, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Edward Wang at The University of California, Berkeley.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)alias.h	3.8 (Berkeley) %G%
+ *	@(#)alias.h	8.1 (Berkeley) %G%
  */
 
 #define alias var
