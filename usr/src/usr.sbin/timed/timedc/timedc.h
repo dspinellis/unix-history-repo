1f914df9e6fe15a03fe08954dc55ad88a5855944
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-06 21:09:42 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.sbin/timed/timed/acksend.c 8.1
    SCCS-vsn: usr.sbin/timed/timed/byteorder.c 8.1
    SCCS-vsn: usr.sbin/timed/timed/candidate.c 8.1
    SCCS-vsn: usr.sbin/timed/timed/cksum.c 8.1
    SCCS-vsn: usr.sbin/timed/timed/correct.c 8.1
    SCCS-vsn: usr.sbin/timed/timedc/cmds.c 8.1
    SCCS-vsn: usr.sbin/timed/timed/extern.h 8.1
    SCCS-vsn: usr.sbin/timed/timedc/cmdtab.c 8.1
    SCCS-vsn: usr.sbin/timed/timed/globals.h 8.1
    SCCS-vsn: usr.sbin/timed/timedc/extern.h 8.1
    SCCS-vsn: usr.sbin/timed/timed/master.c 8.1
    SCCS-vsn: usr.sbin/timed/timedc/timedc.8 8.1
    SCCS-vsn: usr.sbin/timed/timedc/timedc.c 8.1
    SCCS-vsn: usr.sbin/timed/timed/measure.c 8.1
    SCCS-vsn: usr.sbin/timed/timedc/timedc.h 8.1
    SCCS-vsn: usr.sbin/timed/timed/networkdelta.c 8.1

--- usr/src/usr.sbin/timed/timedc/timedc.h
+++ usr/src/usr.sbin/timed/timedc/timedc.h
@@ -1,10 +1,10 @@
 /*-
- * Copyright (c) 1985, 1993 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1985, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)timedc.h	5.1 (Berkeley) %G%
+ *	@(#)timedc.h	8.1 (Berkeley) %G%
  */
 
 #include <sys/param.h>
