5a4647a9dd7ee57a89600c98b3a6272aac9193d0
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-06 21:00:32 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.sbin/lpr/lpd/lpdchar.c 8.1
    SCCS-vsn: usr.sbin/lpr/lpd/printjob.c 8.1
    SCCS-vsn: usr.sbin/lpr/lpq/lpq.1 8.1
    SCCS-vsn: usr.sbin/lpr/lpd/recvjob.c 8.1
    SCCS-vsn: usr.sbin/lpr/lpq/lpq.c 8.1

--- usr/src/usr.sbin/lpr/lpd/printjob.c
+++ usr/src/usr.sbin/lpr/lpd/printjob.c
@@ -1,19 +1,19 @@
 /*
- * Copyright (c) 1983 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1983, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-char copyright[] =
-"@(#) Copyright (c) 1983 The Regents of the University of California.\n\
- All rights reserved.\n";
+static char copyright[] =
+"@(#) Copyright (c) 1983, 1993\n\
+	The Regents of the University of California.  All rights reserved.\n";
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)printjob.c	5.17 (Berkeley) %G%";
+static char sccsid[] = "@(#)printjob.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 
