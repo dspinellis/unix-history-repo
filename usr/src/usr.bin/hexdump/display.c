ebcd34d10a0cb2200a5e6dedc8f05b34bc6892d1
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-06 21:50:14 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.bin/hexdump/conv.c 8.1
    SCCS-vsn: usr.bin/hexdump/display.c 8.1
    SCCS-vsn: usr.bin/hexdump/hexdump.1 8.1
    SCCS-vsn: usr.bin/hexdump/hexdump.c 8.1
    SCCS-vsn: usr.bin/hexdump/hexdump.h 8.1
    SCCS-vsn: usr.bin/hexdump/hexsyntax.c 8.1
    SCCS-vsn: usr.bin/hexdump/od.1 8.1
    SCCS-vsn: usr.bin/hexdump/odsyntax.c 8.1
    SCCS-vsn: usr.bin/hexdump/parse.c 8.1

--- usr/src/usr.bin/hexdump/display.c
+++ usr/src/usr.bin/hexdump/display.c
@@ -1,12 +1,12 @@
 /*
- * Copyright (c) 1989 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1989, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)display.c	5.15 (Berkeley) %G%";
+static char sccsid[] = "@(#)display.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/param.h>
--- usr/src/usr.bin/hexdump/hexdump.c
+++ usr/src/usr.bin/hexdump/hexdump.c
@@ -1,18 +1,18 @@
 /*
- * Copyright (c) 1989 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1989, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-char copyright[] =
-"@(#) Copyright (c) 1989 The Regents of the University of California.\n\
- All rights reserved.\n";
+static char copyright[] =
+"@(#) Copyright (c) 1989, 1993\n\
+	The Regents of the University of California.  All rights reserved.\n";
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)hexdump.c	5.6 (Berkeley) %G%";
+static char sccsid[] = "@(#)hexdump.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/types.h>
