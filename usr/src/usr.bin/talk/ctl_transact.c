dbdf835c5206c4fbb06b9fea180f059dd0530484
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-06 23:31:18 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.bin/talk/ctl.c 8.1
    SCCS-vsn: usr.bin/talk/ctl_transact.c 8.1
    SCCS-vsn: usr.bin/tcopy/pathnames.h 8.1
    SCCS-vsn: usr.bin/talk/display.c 8.1
    SCCS-vsn: usr.bin/tcopy/tcopy.1 8.1
    SCCS-vsn: usr.bin/talk/get_addrs.c 8.1
    SCCS-vsn: usr.bin/tcopy/tcopy.c 8.1

--- usr/src/usr.bin/talk/ctl_transact.c
+++ usr/src/usr.bin/talk/ctl_transact.c
@@ -1,12 +1,12 @@
 /*
- * Copyright (c) 1983 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1983, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)ctl_transact.c	5.8 (Berkeley) %G%";
+static char sccsid[] = "@(#)ctl_transact.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/types.h>
