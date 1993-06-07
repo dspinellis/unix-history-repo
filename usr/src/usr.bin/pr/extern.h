7591eeabcd260d768ea892f589c68c3eaacfde6a
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-06 22:57:09 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.bin/pr/egetopt.c 8.1
    SCCS-vsn: usr.bin/pr/extern.h 8.1
    SCCS-vsn: usr.bin/pr/pr.1 8.1
    SCCS-vsn: usr.bin/pr/pr.c 8.1
    SCCS-vsn: usr.bin/pr/pr.h 8.1

--- usr/src/usr.bin/pr/extern.h
+++ usr/src/usr.bin/pr/extern.h
@@ -1,14 +1,14 @@
 /*-
  * Copyright (c) 1991 Keith Muller.
- * Copyright (c) 1993 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Keith Muller of the University of California, San Diego.
  *
  * %sccs.include.redist.c%
  *
- *      @(#)extern.h	5.2 (Berkeley) %G%
+ *      @(#)extern.h	8.1 (Berkeley) %G%
  */
 
 extern int eoptind;
