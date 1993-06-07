829446b0724701736de52f7600c11e74272f3a6b
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-07 05:09:48 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.bin/uucp/libacu/att2224.c 8.1
    SCCS-vsn: usr.bin/uucp/libacu/bsdtcp.c 8.1
    SCCS-vsn: usr.bin/uucp/libacu/cds224.c 8.1
    SCCS-vsn: usr.bin/uucp/libacu/df12.c 8.1
    SCCS-vsn: usr.bin/uucp/libacu/df2.c 8.1
    SCCS-vsn: usr.bin/uucp/libacu/dk.c 8.1
    SCCS-vsn: usr.bin/uucp/libacu/dn.c 8.1
    SCCS-vsn: usr.bin/uucp/libacu/hys.c 8.1
    SCCS-vsn: usr.bin/uucp/libacu/hys24.c 8.1
    SCCS-vsn: usr.bin/uucp/libacu/hysq.c 8.1
    SCCS-vsn: usr.bin/uucp/libacu/mic.c 8.1
    SCCS-vsn: usr.bin/uucp/libacu/nov.c 8.1
    SCCS-vsn: usr.bin/uucp/libacu/pen.c 8.1
    SCCS-vsn: usr.bin/uucp/libacu/pnet.c 8.1
    SCCS-vsn: usr.bin/uucp/libacu/rvmacs.c 8.1
    SCCS-vsn: usr.bin/uucp/libacu/sy.c 8.1
    SCCS-vsn: usr.bin/uucp/libacu/unet.c 8.1
    SCCS-vsn: usr.bin/uucp/libacu/va212.c 8.1
    SCCS-vsn: usr.bin/uucp/libacu/va811.c 8.1
    SCCS-vsn: usr.bin/uucp/libacu/va820.c 8.1
    SCCS-vsn: usr.bin/uucp/libacu/vad.c 8.1
    SCCS-vsn: usr.bin/uucp/libacu/vent.c 8.1
    SCCS-vsn: usr.bin/uucp/libacu/vmacs.c 8.1

--- usr/src/usr.bin/uucp/libacu/va820.c
+++ usr/src/usr.bin/uucp/libacu/va820.c
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1985 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1985, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.proprietary.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)va820.c	4.6 (Berkeley) %G%";
+static char sccsid[] = "@(#)va820.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 #include "condevs.h"
