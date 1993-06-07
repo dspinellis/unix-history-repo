b966e7b91915b14e436c6b1251ff0ebed3620c5b
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-06 23:30:37 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.bin/tail/misc.c 8.1
    SCCS-vsn: usr.bin/tail/read.c 8.1
    SCCS-vsn: usr.bin/tail/reverse.c 8.1
    SCCS-vsn: usr.bin/tail/tail.1 8.1
    SCCS-vsn: usr.bin/tail/tail.c 8.1

--- usr/src/usr.bin/tail/reverse.c
+++ usr/src/usr.bin/tail/reverse.c
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1991 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1991, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Edward Sze-Tyan Wang.
@@ -9,7 +9,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)reverse.c	5.8 (Berkeley) %G%";
+static char sccsid[] = "@(#)reverse.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/param.h>
