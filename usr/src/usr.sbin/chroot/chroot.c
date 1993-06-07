d703a9386925b2c227f0b8f2fd471eedf446dff2
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-06 20:52:05 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.sbin/chroot/chroot.8 8.1
    SCCS-vsn: usr.sbin/chroot/chroot.c 8.1

--- usr/src/usr.sbin/chroot/chroot.c
+++ usr/src/usr.sbin/chroot/chroot.c
@@ -12,7 +12,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)chroot.c	5.9 (Berkeley) %G%";
+static char sccsid[] = "@(#)chroot.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/types.h>
