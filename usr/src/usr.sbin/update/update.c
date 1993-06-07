38b10cf596c21b872e7a9316ae57087da2b9ad83
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-06 21:12:03 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.sbin/update/update.8 8.1
    SCCS-vsn: usr.sbin/update/update.c 8.1

--- usr/src/usr.sbin/update/update.c
+++ usr/src/usr.sbin/update/update.c
@@ -1,18 +1,18 @@
 /*-
- * Copyright (c) 1987, 1990 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1987, 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-char copyright[] =
-"@(#) Copyright (c) 1987, 1990 The Regents of the University of California.\n\
- All rights reserved.\n";
+static char copyright[] =
+"@(#) Copyright (c) 1987, 1990, 1993\n\
+	The Regents of the University of California.  All rights reserved.\n";
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)update.c	5.1 (Berkeley) %G%";
+static char sccsid[] = "@(#)update.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/time.h>
