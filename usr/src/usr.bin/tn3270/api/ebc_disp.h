489b3149355715b04e62cc5688092d757374c216
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-06 23:37:23 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.bin/tn3270/api/ebc_disp.c 8.1
    SCCS-vsn: usr.bin/tn3270/api/ebc_disp.h 8.1
    SCCS-vsn: usr.bin/tput/clear.sh 8.1
    SCCS-vsn: usr.bin/tput/tput.1 8.1
    SCCS-vsn: usr.bin/tput/tput.c 8.1

--- usr/src/usr.bin/tn3270/api/ebc_disp.h
+++ usr/src/usr.bin/tn3270/api/ebc_disp.h
@@ -1,10 +1,10 @@
 /*-
- * Copyright (c) 1988 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1988, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)ebc_disp.h	4.2 (Berkeley) %G%
+ *	@(#)ebc_disp.h	8.1 (Berkeley) %G%
  */
 
 extern unsigned char
--- usr/src/usr.bin/tput/clear.sh
+++ usr/src/usr.bin/tput/clear.sh
@@ -1,11 +1,11 @@
 #!/bin/sh -
 #
-# Copyright (c) 1989 The Regents of the University of California.
-# All rights reserved.
+# Copyright (c) 1989, 1993
+#	The Regents of the University of California.  All rights reserved.
 #
 # %sccs.include.redist.sh%
 #
-#	@(#)clear.sh	5.2 (Berkeley) %G%
+#	@(#)clear.sh	8.1 (Berkeley) %G%
 #
 
 exec tput clear
