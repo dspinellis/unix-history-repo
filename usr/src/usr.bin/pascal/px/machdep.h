7fae293a186ed724123feaee43854b5bc5eedd91
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-06 22:58:17 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.bin/pascal/px/utilities.c 8.1
    SCCS-vsn: usr.bin/pascal/px/version.c 8.1
    SCCS-vsn: usr.bin/pascal/px/machdep.h 8.1
    SCCS-vsn: usr.bin/pascal/px/vars.h 8.1
    SCCS-vsn: usr.bin/printf/printf.1 8.1
    SCCS-vsn: usr.bin/printf/printf.c 8.1

--- usr/src/usr.bin/pascal/px/machdep.h
+++ usr/src/usr.bin/pascal/px/machdep.h
@@ -1,10 +1,10 @@
 /*-
- * Copyright (c) 1980 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1980, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)machdep.h	5.4 (Berkeley) %G%
+ *	@(#)machdep.h	8.1 (Berkeley) %G%
  */
 
 #ifdef ADDR32
--- usr/src/usr.bin/pascal/px/vars.h
+++ usr/src/usr.bin/pascal/px/vars.h
@@ -1,10 +1,10 @@
 /*-
- * Copyright (c) 1980 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1980, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)vars.h	5.4 (Berkeley) %G%
+ *	@(#)vars.h	8.1 (Berkeley) %G%
  */
 
 #include <stdio.h>
