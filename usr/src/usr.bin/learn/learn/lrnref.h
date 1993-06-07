a59ea125442db6718bb557e0feb4d198fb609df9
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-06 21:56:33 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.bin/learn/learn/copy.c 8.1
    SCCS-vsn: usr.bin/learn/learn/dounit.c 8.1
    SCCS-vsn: usr.bin/learn/learn/getlesson.c 8.1
    SCCS-vsn: usr.bin/learn/learn/learn.1 8.1
    SCCS-vsn: usr.bin/learn/learn/learn.c 8.1
    SCCS-vsn: usr.bin/leave/leave.1 8.1
    SCCS-vsn: usr.bin/learn/learn/list.c 8.1
    SCCS-vsn: usr.bin/leave/leave.c 8.1
    SCCS-vsn: usr.bin/learn/learn/lrnref.h 8.1

--- usr/src/usr.bin/learn/learn/lrnref.h
+++ usr/src/usr.bin/learn/learn/lrnref.h
@@ -1,10 +1,10 @@
 /*-
- * Copyright (c) 1986 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1986, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.proprietary.c%
  *
- *	@(#)lrnref.h	4.5 (Berkeley) %G%
+ *	@(#)lrnref.h	8.1 (Berkeley) %G%
  */
 
 #define	READY	0
