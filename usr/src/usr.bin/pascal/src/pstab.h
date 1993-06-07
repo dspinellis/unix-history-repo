0d536ec2bd41686ee992045aff9283c501ca9f63
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-06 23:11:19 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.bin/pascal/src/pstab.h 8.1
    SCCS-vsn: usr.bin/pascal/src/put.c 8.1
    SCCS-vsn: usr.bin/rsh/pathnames.h 8.1
    SCCS-vsn: usr.bin/rsh/rsh.1 8.1
    SCCS-vsn: usr.bin/pascal/src/rec.c 8.1
    SCCS-vsn: usr.bin/rsh/rsh.c 8.1
    SCCS-vsn: usr.bin/pascal/src/rval.c 8.1

--- usr/src/usr.bin/pascal/src/pstab.h
+++ usr/src/usr.bin/pascal/src/pstab.h
@@ -1,10 +1,10 @@
 /*-
- * Copyright (c) 1980 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1980, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)pstab.h	5.2 (Berkeley) %G%
+ *	@(#)pstab.h	8.1 (Berkeley) %G%
  */
 
     /*
