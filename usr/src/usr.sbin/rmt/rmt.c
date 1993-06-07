e45ab4e34c6d97624d9ae7f1f6fa1ed8f9745e8e
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-06 21:05:23 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.sbin/rmt/rmt.8 8.1
    SCCS-vsn: usr.sbin/rmt/rmt.c 8.1
    SCCS-vsn: usr.sbin/rwhod/rwhod.8 8.1

--- usr/src/usr.sbin/rmt/rmt.c
+++ usr/src/usr.sbin/rmt/rmt.c
@@ -1,18 +1,18 @@
 /*
- * Copyright (c) 1983 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1983, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-char copyright[] =
-"@(#) Copyright (c) 1983 Regents of the University of California.\n\
- All rights reserved.\n";
+static char copyright[] =
+"@(#) Copyright (c) 1983, 1993\n\
+	The Regents of the University of California.  All rights reserved.\n";
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)rmt.c	5.8 (Berkeley) %G%";
+static char sccsid[] = "@(#)rmt.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 /*
