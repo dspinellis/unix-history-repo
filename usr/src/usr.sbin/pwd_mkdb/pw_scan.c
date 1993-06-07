d5e522e52421d08d61ca67a577924094be2625b2
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-06 21:04:49 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.sbin/pwd_mkdb/pw_scan.c 8.1
    SCCS-vsn: usr.sbin/pwd_mkdb/pwd_mkdb.8 8.1
    SCCS-vsn: usr.sbin/quot/quot.8 8.1
    SCCS-vsn: usr.sbin/pwd_mkdb/pwd_mkdb.c 8.1

--- usr/src/usr.sbin/pwd_mkdb/pw_scan.c
+++ usr/src/usr.sbin/pwd_mkdb/pw_scan.c
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1990 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)pw_scan.c	5.1 (Berkeley) %G%";
+static char sccsid[] = "@(#)pw_scan.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 /*
