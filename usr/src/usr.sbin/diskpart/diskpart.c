987208365926f9c37e17318330b1877a9a8b7d70
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-06 20:56:19 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.sbin/diskpart/diskpart.8 8.1
    SCCS-vsn: usr.sbin/diskpart/diskpart.c 8.1

--- usr/src/usr.sbin/diskpart/diskpart.c
+++ usr/src/usr.sbin/diskpart/diskpart.c
@@ -1,18 +1,18 @@
 /*
- * Copyright (c) 1983, 1988 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1983, 1988, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-char copyright[] =
-"@(#) Copyright (c) 1983, 1988 Regents of the University of California.\n\
- All rights reserved.\n";
+static char copyright[] =
+"@(#) Copyright (c) 1983, 1988, 1993\n\
+	The Regents of the University of California.  All rights reserved.\n";
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)diskpart.c	5.11 (Berkeley) %G%";
+static char sccsid[] = "@(#)diskpart.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 /*
