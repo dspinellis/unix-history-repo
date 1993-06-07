6ff8c7dfdc91ddf876da4c216bb21f3835fde068
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-06 21:51:14 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.bin/id/groups.sh 8.1
    SCCS-vsn: usr.bin/id/whoami.sh 8.1
    SCCS-vsn: usr.bin/id/groups.1 8.1
    SCCS-vsn: usr.bin/id/id.1 8.1
    SCCS-vsn: usr.bin/id/id.c 8.1
    SCCS-vsn: usr.bin/id/whoami.1 8.1

--- usr/src/usr.bin/id/groups.sh
+++ usr/src/usr.bin/id/groups.sh
@@ -1,11 +1,11 @@
 #!/bin/sh -
 #
-# Copyright (c) 1991 The Regents of the University of California.
-# All rights reserved.
+# Copyright (c) 1991, 1993
+#	The Regents of the University of California.  All rights reserved.
 #
 # %sccs.include.redist.sh%
 #
-#	@(#)groups.sh	5.1 (Berkeley) %G%
+#	@(#)groups.sh	8.1 (Berkeley) %G%
 #
 
 id -Gn $*
--- usr/src/usr.bin/id/whoami.sh
+++ usr/src/usr.bin/id/whoami.sh
@@ -1,11 +1,11 @@
 #!/bin/sh -
 #
-# Copyright (c) 1991 The Regents of the University of California.
-# All rights reserved.
+# Copyright (c) 1991, 1993
+#	The Regents of the University of California.  All rights reserved.
 #
 # %sccs.include.redist.sh%
 #
-#	@(#)whoami.sh	5.1 (Berkeley) %G%
+#	@(#)whoami.sh	8.1 (Berkeley) %G%
 #
 
 id -un
