6f339beeec78faafa523675f105afc152c8673cc
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-06 23:38:19 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.bin/tn3270/ascii/default.map 8.1
    SCCS-vsn: usr.bin/tn3270/ascii/map3270.c 8.1
    SCCS-vsn: usr.bin/tn3270/ascii/map3270.h 8.1
    SCCS-vsn: usr.bin/tr/extern.h 8.1
    SCCS-vsn: usr.bin/tn3270/ascii/mset.c 8.1
    SCCS-vsn: usr.bin/tr/str.c 8.1
    SCCS-vsn: usr.bin/tn3270/ascii/state.h 8.1
    SCCS-vsn: usr.bin/tr/tr.1 8.1
    SCCS-vsn: usr.bin/tn3270/ascii/termin.c 8.1
    SCCS-vsn: usr.bin/tr/tr.c 8.1

--- usr/src/usr.bin/tn3270/ascii/termin.c
+++ usr/src/usr.bin/tn3270/ascii/termin.c
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1988 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1988, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)termin.c	4.2 (Berkeley) %G%";
+static char sccsid[] = "@(#)termin.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 /* this takes characters from the keyboard, and produces 3270 keystroke
