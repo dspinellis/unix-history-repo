f349a8a47ad18fdd9c98f6341e44b1c253ecf165
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-07 05:31:12 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.bin/window/wwalloc.c 8.1
    SCCS-vsn: usr.bin/window/wwbox.c 8.1

--- usr/src/usr.bin/window/wwalloc.c
+++ usr/src/usr.bin/window/wwalloc.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1983 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1983, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Edward Wang at The University of California, Berkeley.
@@ -9,7 +9,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)wwalloc.c	3.11 (Berkeley) %G%";
+static char sccsid[] = "@(#)wwalloc.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 #include "ww.h"
