52dcc4f561d8e8cc28b006314514cd22471a6bce
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-07 05:18:00 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.bin/uuencode/uuencode.1 8.1
    SCCS-vsn: usr.bin/uuencode/uuencode.c 8.1
    SCCS-vsn: usr.bin/uuencode/uuencode.format.5 8.1

--- usr/src/usr.bin/uuencode/uuencode.c
+++ usr/src/usr.bin/uuencode/uuencode.c
@@ -1,12 +1,12 @@
 /*
- * Copyright (c) 1983 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1983, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)uuencode.c	5.9 (Berkeley) %G%";
+static char sccsid[] = "@(#)uuencode.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 /*
