3e35d875794476a1c77a4f522295b2314514990e
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-06 22:29:06 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.bin/pascal/pc2/langpats.c 8.1
    SCCS-vsn: usr.bin/pascal/pc3/pc3.c 8.1
    SCCS-vsn: usr.bin/pascal/pc3/pc3.h 8.1

--- usr/src/usr.bin/pascal/pc2/langpats.c
+++ usr/src/usr.bin/pascal/pc2/langpats.c
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1979, 1984 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1979, 1984, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)langpats.c	5.6 (Berkeley) %G%";
+static char sccsid[] = "@(#)langpats.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 #include "inline.h"
