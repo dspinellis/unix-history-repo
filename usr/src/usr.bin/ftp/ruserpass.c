54f57a80ff1387d27048b3bbb90af7c402ecd1ea
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-06 21:41:30 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.bin/ftp/ftp_var.h 8.1
    SCCS-vsn: usr.bin/ftp/main.c 8.1
    SCCS-vsn: usr.bin/ftp/pathnames.h 8.1
    SCCS-vsn: usr.bin/ftp/ruserpass.c 8.1

--- usr/src/usr.bin/ftp/ruserpass.c
+++ usr/src/usr.bin/ftp/ruserpass.c
@@ -1,12 +1,12 @@
 /*
- * Copyright (c) 1985 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1985, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)ruserpass.c	5.4 (Berkeley) %G%";
+static char sccsid[] = "@(#)ruserpass.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/types.h>
