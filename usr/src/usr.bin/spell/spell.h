4035be21d23ff851faf04a346e6cb6c8452b2171
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-06 23:24:42 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.bin/spell/spell.1 8.1
    SCCS-vsn: usr.bin/spell/spell.c 8.1
    SCCS-vsn: usr.bin/spell/spell.h 8.1
    SCCS-vsn: usr.bin/spell/spell.sh 8.1
    SCCS-vsn: usr.bin/spell/spellin.c 8.1
    SCCS-vsn: usr.bin/spell/spellout.c 8.1
    SCCS-vsn: usr.bin/split/split.1 8.1
    SCCS-vsn: usr.bin/split/split.c 8.1

--- usr/src/usr.bin/spell/spell.h
+++ usr/src/usr.bin/spell/spell.h
@@ -1,10 +1,10 @@
 /*-
- * Copyright (c) 1991 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1991, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.proprietary.c%
  *
- *	@(#)spell.h	4.3 (Berkeley) %G%
+ *	@(#)spell.h	8.1 (Berkeley) %G%
  */
 
 #include <stdio.h>
