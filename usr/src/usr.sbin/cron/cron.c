580cdccafe8c2c1f7a6ea3457d3fe2fba286a3ba
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-06 20:55:47 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.sbin/cron/cron.8 8.1
    SCCS-vsn: usr.sbin/cron/cron.c 8.1
    SCCS-vsn: usr.sbin/cron/crontab.5 8.1
    SCCS-vsn: usr.sbin/cron/pathnames.h 8.1

--- usr/src/usr.sbin/cron/cron.c
+++ usr/src/usr.sbin/cron/cron.c
@@ -1,18 +1,18 @@
 /*-
- * Copyright (c) 1986 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1986, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.proprietary.c%
  */
 
 #ifndef lint
-char copyright[] =
-"@(#) Copyright (c) 1986 The Regents of the University of California.\n\
- All rights reserved.\n";
+static char copyright[] =
+"@(#) Copyright (c) 1986, 1993\n\
+	The Regents of the University of California.  All rights reserved.\n";
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)cron.c	4.18 (Berkeley) %G%";
+static char sccsid[] = "@(#)cron.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/types.h>
