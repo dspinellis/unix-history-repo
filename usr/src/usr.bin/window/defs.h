5685cfe36a41d274f1b5b7e0d5f9068797b99ab9
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-07 05:28:35 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.bin/window/cmd7.c 8.1
    SCCS-vsn: usr.bin/xargs/pathnames.h 8.1
    SCCS-vsn: usr.bin/window/compress.c 8.1
    SCCS-vsn: usr.bin/xargs/xargs.1 8.1
    SCCS-vsn: usr.bin/xargs/xargs.c 8.1
    SCCS-vsn: usr.bin/window/context.c 8.1
    SCCS-vsn: usr.bin/window/context.h 8.1
    SCCS-vsn: usr.bin/window/defs.h 8.1
    SCCS-vsn: usr.bin/window/error.c 8.1

--- usr/src/usr.bin/window/defs.h
+++ usr/src/usr.bin/window/defs.h
@@ -1,13 +1,13 @@
 /*
- * Copyright (c) 1983 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1983, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Edward Wang at The University of California, Berkeley.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)defs.h	3.21 (Berkeley) %G%
+ *	@(#)defs.h	8.1 (Berkeley) %G%
  */
 
 #include "ww.h"
