645fea4ebea81194cf21c675afe0c050440814f8
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-06 23:31:38 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.bin/talk/get_names.c 8.1
    SCCS-vsn: usr.bin/talk/init_disp.c 8.1
    SCCS-vsn: usr.bin/tee/tee.1 8.1
    SCCS-vsn: usr.bin/talk/invite.c 8.1
    SCCS-vsn: usr.bin/tee/tee.c 8.1
    SCCS-vsn: usr.bin/talk/io.c 8.1

--- usr/src/usr.bin/talk/init_disp.c
+++ usr/src/usr.bin/talk/init_disp.c
@@ -1,12 +1,12 @@
 /*
- * Copyright (c) 1983 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1983, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)init_disp.c	5.5 (Berkeley) %G%";
+static char sccsid[] = "@(#)init_disp.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 /*
