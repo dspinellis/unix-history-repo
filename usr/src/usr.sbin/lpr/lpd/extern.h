7fc73a524e4629d60ee930e0c5133530977d17a6
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-06 21:00:12 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.sbin/lpr/lpc/cmds.c 8.1
    SCCS-vsn: usr.sbin/lpr/lpc/cmdtab.c 8.1
    SCCS-vsn: usr.sbin/lpr/lpc/extern.h 8.1
    SCCS-vsn: usr.sbin/lpr/lpd/extern.h 8.1
    SCCS-vsn: usr.sbin/lpr/lpc/lpc.8 8.1
    SCCS-vsn: usr.sbin/lpr/lpd/lpd.8 8.1
    SCCS-vsn: usr.sbin/lpr/lpc/lpc.c 8.1
    SCCS-vsn: usr.sbin/lpr/lpd/lpd.c 8.1
    SCCS-vsn: usr.sbin/lpr/lpc/lpc.h 8.1

--- usr/src/usr.sbin/lpr/lpd/extern.h
+++ usr/src/usr.sbin/lpr/lpd/extern.h
@@ -1,10 +1,10 @@
 /*
- * Copyright (c) 1989 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1989, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *      @(#)extern.h	5.4 (Berkeley) %G%
+ *      @(#)extern.h	8.1 (Berkeley) %G%
  */
 
 #include <sys/cdefs.h>
