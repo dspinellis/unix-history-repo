46b7cbcfe9e4cea9ee300b444755ef2bb933aed9
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-06 21:02:07 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.sbin/lpr/pac/pac.8 8.1
    SCCS-vsn: usr.sbin/lpr/pac/pac.c 8.1

--- usr/src/usr.sbin/lpr/pac/pac.c
+++ usr/src/usr.sbin/lpr/pac/pac.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1983 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1983, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  *
  * %sccs.include.redist.c%
@@ -8,12 +8,12 @@
 
 #ifndef lint
 static char copyright[] =
-"@(#) Copyright (c) 1983 The Regents of the University of California.\n\
- All rights reserved.\n";
+"@(#) Copyright (c) 1983, 1993\n\
+	The Regents of the University of California.  All rights reserved.\n";
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)pac.c	5.10 (Berkeley) %G%";
+static char sccsid[] = "@(#)pac.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 /*
