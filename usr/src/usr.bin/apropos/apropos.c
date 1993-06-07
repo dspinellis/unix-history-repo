5384bd2a2f50dad197f3dbe55331dcede1c99000
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-06 22:21:31 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.bin/apropos/apropos.1 8.1
    SCCS-vsn: usr.bin/apropos/apropos.c 8.1

--- usr/src/usr.bin/apropos/apropos.c
+++ usr/src/usr.bin/apropos/apropos.c
@@ -1,18 +1,18 @@
 /*
- * Copyright (c) 1987 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1987, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-char copyright[] =
-"@(#) Copyright (c) 1987 Regents of the University of California.\n\
- All rights reserved.\n";
+static char copyright[] =
+"@(#) Copyright (c) 1987, 1993\n\
+	The Regents of the University of California.  All rights reserved.\n";
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)apropos.c	5.13 (Berkeley) %G%";
+static char sccsid[] = "@(#)apropos.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/param.h>
