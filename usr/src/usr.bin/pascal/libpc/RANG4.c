d92590c8ed87eb02df7e93d0288a07384e69f2c7
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-06 22:18:22 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.bin/pascal/libpc/PERROR.c 8.1
    SCCS-vsn: usr.bin/pascal/libpc/PFCLOSE.c 8.1
    SCCS-vsn: usr.bin/pascal/libpc/PFLUSH.c 8.1
    SCCS-vsn: usr.bin/pascal/libpc/PMFLUSH.c 8.1
    SCCS-vsn: usr.bin/pascal/libpc/PRED.c 8.1
    SCCS-vsn: usr.bin/pascal/libpc/PUT.c 8.1
    SCCS-vsn: usr.bin/pascal/libpc/RANDOM.c 8.1
    SCCS-vsn: usr.bin/pascal/libpc/RANG4.c 8.1
    SCCS-vsn: usr.bin/pascal/libpc/READ4.c 8.1

--- usr/src/usr.bin/pascal/libpc/RANG4.c
+++ usr/src/usr.bin/pascal/libpc/RANG4.c
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1979 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1979, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)RANG4.c	1.6 (Berkeley) %G%";
+static char sccsid[] = "@(#)RANG4.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 char ERANG[] = "Value of %D is out of range\n";
