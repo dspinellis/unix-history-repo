2f9da41ab9371afbcaf40017402ec12e6cb0111a
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-06 23:52:32 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.bin/tn3270/tools/mkmake.y 8.1
    SCCS-vsn: usr.bin/tn3270/tools/prt3270.c 8.1
    SCCS-vsn: usr.bin/tn3270/general/genbsubs.c 8.1
    SCCS-vsn: usr.bin/tn3270/general/general.h 8.1
    SCCS-vsn: usr.bin/tn3270/general/globals.c 8.1
    SCCS-vsn: usr.bin/tn3270/general/globals.h 8.1
    SCCS-vsn: usr.bin/tn3270/general/vaxbsubs.s 8.1
    SCCS-vsn: usr.bin/tn3270/mset/map3270.5 8.1
    SCCS-vsn: usr.bin/tn3270/mset/mset.1 8.1

--- usr/src/usr.bin/tn3270/general/globals.h
+++ usr/src/usr.bin/tn3270/general/globals.h
@@ -1,10 +1,10 @@
 /*-
- * Copyright (c) 1988 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1988, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)globals.h	4.2 (Berkeley) %G%
+ *	@(#)globals.h	8.1 (Berkeley) %G%
  */
 
 /*
