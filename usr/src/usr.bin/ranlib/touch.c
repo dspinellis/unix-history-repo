085ee225e6face99b526329576ef72af49aefebf
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-06 23:04:02 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.bin/ranlib/build.c 8.1
    SCCS-vsn: usr.bin/ranlib/misc.c 8.1
    SCCS-vsn: usr.bin/ranlib/pathnames.h 8.1
    SCCS-vsn: usr.bin/ranlib/ranlib.1 8.1
    SCCS-vsn: usr.bin/ranlib/ranlib.5.5 8.1
    SCCS-vsn: usr.bin/ranlib/ranlib.c 8.1
    SCCS-vsn: usr.bin/ranlib/touch.c 8.1

--- usr/src/usr.bin/ranlib/touch.c
+++ usr/src/usr.bin/ranlib/touch.c
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1990 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Hugh Smith at The University of Guelph.
@@ -9,7 +9,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)touch.c	5.3 (Berkeley) %G%";
+static char sccsid[] = "@(#)touch.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/types.h>
