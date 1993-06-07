3003fa202b4d69175195ff9306500203624085c0
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-07 05:22:13 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.bin/vis/foldit.c 8.1
    SCCS-vsn: usr.bin/vis/vis.1 8.1
    SCCS-vsn: usr.bin/vis/vis.c 8.1

--- usr/src/usr.bin/vis/foldit.c
+++ usr/src/usr.bin/vis/foldit.c
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1990 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)foldit.c	5.1 (Berkeley) %G%";
+static char sccsid[] = "@(#)foldit.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 foldit(chunk, col, max)
