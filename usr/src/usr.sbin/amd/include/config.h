47d03084fca8ecb34c9abf9b499e1841991b0e5a
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-06 19:00:46 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.sbin/amd/include/am.h 8.1
    SCCS-vsn: usr.sbin/amd/include/config.h 8.1
    SCCS-vsn: usr.sbin/amd/include/fstype.h 8.1
    SCCS-vsn: usr.sbin/amd/include/uwait.h 8.1
    SCCS-vsn: usr.sbin/amd/include/am.h 5.6
    SCCS-vsn: usr.sbin/amd/mk-amd-map/mk-amd-map.c 8.1

--- usr/src/usr.sbin/amd/include/config.h
+++ usr/src/usr.sbin/amd/include/config.h
@@ -1,15 +1,15 @@
 /*
  * Copyright (c) 1990 Jan-Simon Pendry
  * Copyright (c) 1990 Imperial College of Science, Technology & Medicine
- * Copyright (c) 1990 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Jan-Simon Pendry at Imperial College, London.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)config.h	5.3 (Berkeley) %G%
+ *	@(#)config.h	8.1 (Berkeley) %G%
  *
  * $Id: config.h,v 5.2.2.1 1992/02/09 15:09:56 jsp beta $
  *
