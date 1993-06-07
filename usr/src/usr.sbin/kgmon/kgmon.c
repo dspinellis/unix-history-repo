6d635369999b2b58ff5154a42f9c5e46868aa9f9
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-06 20:57:44 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.sbin/kgmon/kgmon.8 8.1
    SCCS-vsn: usr.sbin/kgmon/kgmon.c 8.1

--- usr/src/usr.sbin/kgmon/kgmon.c
+++ usr/src/usr.sbin/kgmon/kgmon.c
@@ -1,18 +1,18 @@
 /*
- * Copyright (c) 1983, 1992 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1983, 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-char copyright[] =
-"@(#) Copyright (c) 1983, 1992 The Regents of the University of California.\n\
- All rights reserved.\n";
+static char copyright[] =
+"@(#) Copyright (c) 1983, 1992, 1993\n\
+	The Regents of the University of California.  All rights reserved.\n";
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)kgmon.c	5.17 (Berkeley) %G%";
+static char sccsid[] = "@(#)kgmon.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/param.h>
