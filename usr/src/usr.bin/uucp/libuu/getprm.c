c0d6c8c5461cb9b8d3b9eea58817f0e8d7069c35
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-07 05:11:39 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.bin/uucp/libuu/anlwrk.c 8.1
    SCCS-vsn: usr.bin/uucp/libuu/anyread.c 8.1
    SCCS-vsn: usr.bin/uucp/libuu/cfgets.c 8.1
    SCCS-vsn: usr.bin/uucp/libuu/chkpth.c 8.1
    SCCS-vsn: usr.bin/uucp/libuu/cpmv.c 8.1
    SCCS-vsn: usr.bin/uucp/libuu/expfile.c 8.1
    SCCS-vsn: usr.bin/uucp/man5/L-devices.5 8.1
    SCCS-vsn: usr.bin/uucp/libuu/gename.c 8.1
    SCCS-vsn: usr.bin/uucp/man5/L-dialcodes.5 8.1
    SCCS-vsn: usr.bin/uucp/libuu/getargs.c 8.1
    SCCS-vsn: usr.bin/uucp/man5/L.aliases.5 8.1
    SCCS-vsn: usr.bin/uucp/libuu/getprm.c 8.1
    SCCS-vsn: usr.bin/uucp/man5/L.cmds.5 8.1
    SCCS-vsn: usr.bin/uucp/libuu/getpwinfo.c 8.1
    SCCS-vsn: usr.bin/uucp/man5/L.sys.5 8.1
    SCCS-vsn: usr.bin/uucp/libuu/gnxseq.c 8.1
    SCCS-vsn: usr.bin/uucp/libuu/lastpart.c 8.1
    SCCS-vsn: usr.bin/uucp/man5/USERFILE.5 8.1
    SCCS-vsn: usr.bin/uucp/libuu/logent.c 8.1
    SCCS-vsn: usr.bin/uucp/libuu/mailst.c 8.1
    SCCS-vsn: usr.bin/uucp/libuu/prefix.c 8.1
    SCCS-vsn: usr.bin/uucp/libuu/setline.c 8.1
    SCCS-vsn: usr.bin/uucp/libuu/subdir.c 8.1
    SCCS-vsn: usr.bin/uucp/libuu/sysacct.c 8.1
    SCCS-vsn: usr.bin/uucp/libuu/systat.c 8.1
    SCCS-vsn: usr.bin/uucp/libuu/ulockf.c 8.1
    SCCS-vsn: usr.bin/uucp/libuu/uucpdefs.c 8.1
    SCCS-vsn: usr.bin/uucp/libuu/uucpname.c 8.1
    SCCS-vsn: usr.bin/uucp/libuu/versys.c 8.1
    SCCS-vsn: usr.bin/uucp/libuu/xqt.c 8.1

--- usr/src/usr.bin/uucp/libuu/getprm.c
+++ usr/src/usr.bin/uucp/libuu/getprm.c
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1985 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1985, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.proprietary.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)getprm.c	5.5 (Berkeley) %G%";
+static char sccsid[] = "@(#)getprm.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 #include "uucp.h"
