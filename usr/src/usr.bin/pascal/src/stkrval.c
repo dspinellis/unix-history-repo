6c20f2e758e7e69d611677ba0ff24a5d4c783cdb
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-06 23:12:03 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.bin/pascal/src/stat.c 8.1
    SCCS-vsn: usr.bin/pascal/src/stklval.c 8.1
    SCCS-vsn: usr.bin/rwho/rwho.1 8.1
    SCCS-vsn: usr.bin/pascal/src/stkrval.c 8.1
    SCCS-vsn: usr.bin/rwho/rwho.c 8.1

--- usr/src/usr.bin/pascal/src/stkrval.c
+++ usr/src/usr.bin/pascal/src/stkrval.c
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1980 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1980, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)stkrval.c	5.3 (Berkeley) %G%";
+static char sccsid[] = "@(#)stkrval.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 #include "whoami.h"
