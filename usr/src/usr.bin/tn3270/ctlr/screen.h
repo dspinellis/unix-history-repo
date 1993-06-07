3a4912374268e65fa61db0ed359c35676225c722
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-06 23:40:41 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.bin/tn3270/ctlr/screen.h 8.1
    SCCS-vsn: usr.bin/tn3270/ctlr/scrnctlr.h 8.1
    SCCS-vsn: usr.bin/tn3270/ctlr/unix.kbd 8.1
    SCCS-vsn: usr.bin/unifdef/unifdef.1 8.1
    SCCS-vsn: usr.bin/unifdef/unifdef.c 8.1

--- usr/src/usr.bin/tn3270/ctlr/screen.h
+++ usr/src/usr.bin/tn3270/ctlr/screen.h
@@ -1,10 +1,10 @@
 /*-
- * Copyright (c) 1988 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1988, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)screen.h	4.3 (Berkeley) %G%
+ *	@(#)screen.h	8.1 (Berkeley) %G%
  */
 
 #define	INCLUDED_SCREEN
