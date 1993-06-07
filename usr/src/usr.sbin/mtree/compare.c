8ed04f4558d7fc87e00f264bc0b8ab241bb036b2
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-06 21:03:24 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.sbin/mtree/compare.c 8.1
    SCCS-vsn: usr.sbin/mtree/create.c 8.1
    SCCS-vsn: usr.sbin/mtree/extern.h 8.1
    SCCS-vsn: usr.sbin/mtree/misc.c 8.1
    SCCS-vsn: usr.sbin/mtree/mtree.8 8.1
    SCCS-vsn: usr.sbin/mtree/mtree.c 8.1
    SCCS-vsn: usr.sbin/mtree/mtree.h 8.1
    SCCS-vsn: usr.sbin/mtree/spec.c 8.1
    SCCS-vsn: usr.sbin/mtree/verify.c 8.1

--- usr/src/usr.sbin/mtree/compare.c
+++ usr/src/usr.sbin/mtree/compare.c
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1989 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1989, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)compare.c	5.13 (Berkeley) %G%";
+static char sccsid[] = "@(#)compare.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/param.h>
--- usr/src/usr.sbin/mtree/spec.c
+++ usr/src/usr.sbin/mtree/spec.c
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1989 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1989, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)spec.c	5.18 (Berkeley) %G%";
+static char sccsid[] = "@(#)spec.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/types.h>
