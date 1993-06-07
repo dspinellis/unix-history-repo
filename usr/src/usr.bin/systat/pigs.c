0750c3c604a5993ef8240cf1c5e8c811c6cb6d5a
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-06 23:29:31 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.bin/systat/pigs.c 8.1

--- usr/src/usr.bin/systat/pigs.c
+++ usr/src/usr.bin/systat/pigs.c
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1980, 1992 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1980, 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)pigs.c	5.16 (Berkeley) %G%";
+static char sccsid[] = "@(#)pigs.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 /*
