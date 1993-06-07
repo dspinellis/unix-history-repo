3dd9cf8f877519a6f6abfa64986d3ce29a5aa2b3
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-06 21:47:07 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.bin/gcore/gcore.1 8.1
    SCCS-vsn: usr.bin/gcore/md-nop.c 8.1
    SCCS-vsn: usr.bin/gprof/arcs.c 8.1
    SCCS-vsn: usr.bin/gprof/dfn.c 8.1
    SCCS-vsn: usr.bin/gprof/gprof.1 8.1
    SCCS-vsn: usr.bin/gprof/gprof.c 8.1
    SCCS-vsn: usr.bin/gprof/gprof.h 8.1
    SCCS-vsn: usr.bin/gprof/hertz.c 8.1
    SCCS-vsn: usr.bin/gprof/hp300.h 8.1
    SCCS-vsn: usr.bin/gprof/i386.h 8.1
    SCCS-vsn: usr.bin/gprof/lookup.c 8.1
    SCCS-vsn: usr.bin/gprof/mips.c 8.1
    SCCS-vsn: usr.bin/gprof/mips.h 8.1
    SCCS-vsn: usr.bin/gprof/pathnames.h 8.1
    SCCS-vsn: usr.bin/gprof/printgprof.c 8.1
    SCCS-vsn: usr.bin/gprof/printlist.c 8.1
    SCCS-vsn: usr.bin/gprof/sparc.c 8.1
    SCCS-vsn: usr.bin/gprof/sparc.h 8.1
    SCCS-vsn: usr.bin/gprof/tahoe.c 8.1
    SCCS-vsn: usr.bin/gprof/tahoe.h 8.1
    SCCS-vsn: usr.bin/gprof/vax.c 8.1
    SCCS-vsn: usr.bin/gprof/vax.h 8.1

--- usr/src/usr.bin/gprof/printgprof.c
+++ usr/src/usr.bin/gprof/printgprof.c
@@ -1,12 +1,12 @@
 /*
- * Copyright (c) 1983 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1983, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)printgprof.c	5.9 (Berkeley) %G%";
+static char sccsid[] = "@(#)printgprof.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 #include "gprof.h"
--- usr/src/usr.bin/gprof/vax.h
+++ usr/src/usr.bin/gprof/vax.h
@@ -1,10 +1,10 @@
 /*
- * Copyright (c) 1983 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1983, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)vax.h	5.4 (Berkeley) %G%
+ *	@(#)vax.h	8.1 (Berkeley) %G%
  */
 
     /*
