53f9b176f1b1606683f90741c505a888c8dfbe26
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-06 23:28:56 -0800

    4.4BSD snapshot (revision 8.1)
    SCCS-vsn: usr.bin/struct/struct/1.hash.c 8.1

--- usr/src/usr.bin/struct/struct/1.hash.c
+++ usr/src/usr.bin/struct/struct/1.hash.c
@@ -3,7 +3,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)1.hash.c	4.3 (Berkeley) %G%";
+static char sccsid[] = "@(#)1.hash.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <stdio.h>
