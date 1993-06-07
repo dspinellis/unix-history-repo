34914973bee36f76b621804768eb61caf27f8222
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-06 23:28:45 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.bin/systat/cmdtab.c 8.1

--- usr/src/usr.bin/systat/cmdtab.c
+++ usr/src/usr.bin/systat/cmdtab.c
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1980, 1992 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1980, 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)cmdtab.c	5.4 (Berkeley) %G%";
+static char sccsid[] = "@(#)cmdtab.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 #include "systat.h"
