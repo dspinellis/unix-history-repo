a2b011dc43558e6adff96b8da2a71a81085f3be4
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-07 05:30:37 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.bin/window/tttermcap.c 8.1
    SCCS-vsn: usr.bin/window/tttvi925.c 8.1
    SCCS-vsn: usr.bin/window/ttwyse60.c 8.1
    SCCS-vsn: usr.bin/window/ttwyse75.c 8.1
    SCCS-vsn: usr.bin/window/ttzapple.c 8.1
    SCCS-vsn: usr.bin/window/ttzentec.c 8.1

--- usr/src/usr.bin/window/ttzapple.c
+++ usr/src/usr.bin/window/ttzapple.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1989 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1989, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Edward Wang at The University of California, Berkeley.
@@ -9,7 +9,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)ttzapple.c	3.13 (Berkeley) %G%";
+static char sccsid[] = "@(#)ttzapple.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 #include "ww.h"
