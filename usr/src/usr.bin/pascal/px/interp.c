912bf5863a2f88049faa3af663356840b2531eac
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-06 22:57:54 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.bin/pascal/px/int.c 8.1
    SCCS-vsn: usr.bin/pascal/px/interp.c 8.1
    SCCS-vsn: usr.bin/printenv/printenv.1 8.1
    SCCS-vsn: usr.bin/printenv/printenv.c 8.1

--- usr/src/usr.bin/pascal/px/interp.c
+++ usr/src/usr.bin/pascal/px/interp.c
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1991 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1991, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)interp.c	5.8 (Berkeley) %G%";
+static char sccsid[] = "@(#)interp.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <math.h>
