6e4ff592c8a971103308f0ecff6ca65a377aede6
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-06 21:30:02 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.bin/ctags/C.c 8.1
    SCCS-vsn: usr.bin/ctags/ctags.1 8.1
    SCCS-vsn: usr.bin/ctags/ctags.c 8.1
    SCCS-vsn: usr.bin/ctags/ctags.h 8.1
    SCCS-vsn: usr.bin/ctags/fortran.c 8.1
    SCCS-vsn: usr.bin/ctags/lisp.c 8.1
    SCCS-vsn: usr.bin/ctags/print.c 8.1
    SCCS-vsn: usr.bin/ctags/tree.c 8.1
    SCCS-vsn: usr.bin/cut/cut.1 8.1
    SCCS-vsn: usr.bin/ctags/yacc.c 8.1

--- usr/src/usr.bin/ctags/yacc.c
+++ usr/src/usr.bin/ctags/yacc.c
@@ -1,12 +1,12 @@
 /*
- * Copyright (c) 1987 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1987, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)yacc.c	5.6 (Berkeley) %G%";
+static char sccsid[] = "@(#)yacc.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <stdio.h>
