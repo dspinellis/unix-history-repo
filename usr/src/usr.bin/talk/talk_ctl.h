9fa020fcdaf2ab322a7e3250a382f8331b5264fb
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-06 23:32:01 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.bin/talk/talk_ctl.h 8.1

--- usr/src/usr.bin/talk/talk_ctl.h
+++ usr/src/usr.bin/talk/talk_ctl.h
@@ -1,10 +1,10 @@
 /*
- * Copyright (c) 1983 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1983, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)talk_ctl.h	5.6 (Berkeley) %G%
+ *	@(#)talk_ctl.h	8.1 (Berkeley) %G%
  */
 
 extern	struct sockaddr_in daemon_addr;
