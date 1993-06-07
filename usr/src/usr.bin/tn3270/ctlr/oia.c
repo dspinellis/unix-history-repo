943e41d24a23b855a879b3a865d98050b069c14f
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-06 23:40:04 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.bin/tn3270/ctlr/inbound.c 8.1
    SCCS-vsn: usr.bin/tn3270/ctlr/oia.c 8.1
    SCCS-vsn: usr.bin/uname/uname.1 8.1
    SCCS-vsn: usr.bin/tn3270/ctlr/oia.h 8.1
    SCCS-vsn: usr.bin/uname/uname.c 8.1

--- usr/src/usr.bin/tn3270/ctlr/oia.c
+++ usr/src/usr.bin/tn3270/ctlr/oia.c
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1988 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1988, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)oia.c	4.2 (Berkeley) %G%";
+static char sccsid[] = "@(#)oia.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 /*
