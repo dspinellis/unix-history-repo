3292715ea22887e1c6dd8e1f3b5da3b6caf32a19
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-07 05:30:57 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.bin/window/var.c 8.1
    SCCS-vsn: usr.bin/window/var.h 8.1
    SCCS-vsn: usr.bin/window/win.c 8.1
    SCCS-vsn: usr.bin/window/window.1 8.1

--- usr/src/usr.bin/window/var.h
+++ usr/src/usr.bin/window/var.h
@@ -1,13 +1,13 @@
 /*
- * Copyright (c) 1983 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1983, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Edward Wang at The University of California, Berkeley.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)var.h	3.11 (Berkeley) %G%
+ *	@(#)var.h	8.1 (Berkeley) %G%
  */
 
 struct var {
