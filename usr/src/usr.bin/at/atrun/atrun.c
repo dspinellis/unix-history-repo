b91914b442014eb5090f467a3aace5070890ee1f
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-06 21:22:19 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.bin/at/atrun/atrun.c 8.1
    SCCS-vsn: usr.bin/at/atrun/pathnames.h 8.1

--- usr/src/usr.bin/at/atrun/atrun.c
+++ usr/src/usr.bin/at/atrun/atrun.c
@@ -12,7 +12,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)atrun.c	5.9 (Berkeley) %G%";
+static char sccsid[] = "@(#)atrun.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 /*
