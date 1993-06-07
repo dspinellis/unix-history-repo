7c43157dc7d197274cb851b827bc3d050577bbe8
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-07 04:58:29 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: include/unistd.h 8.1
    SCCS-vsn: include/signal.h 8.1
    SCCS-vsn: include/stdio.h 8.1

--- usr/src/include/signal.h
+++ usr/src/include/signal.h
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1991 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1991, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
