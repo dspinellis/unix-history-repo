090742a7bb4052b2f04490da2a080b0df920843e
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-06 23:23:14 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: contrib/sort/append.c 8.1
    SCCS-vsn: contrib/sort/extern.h 8.1
    SCCS-vsn: contrib/sort/fields.c 8.1
    SCCS-vsn: contrib/sort/files.c 8.1
    SCCS-vsn: contrib/sort/fsort.c 8.1
    SCCS-vsn: contrib/sort/fsort.h 8.1
    SCCS-vsn: contrib/sort/init.c 8.1
    SCCS-vsn: contrib/sort/msort.c 8.1
    SCCS-vsn: contrib/sort/pathnames.h 8.1
    SCCS-vsn: contrib/sort/sort.1 8.1
    SCCS-vsn: contrib/sort/sort.c 8.1
    SCCS-vsn: contrib/sort/sort.h 8.1

--- usr/src/contrib/sort/fsort.h
+++ usr/src/contrib/sort/fsort.h
@@ -1,13 +1,13 @@
 /*-
- * Copyright (c) 1993 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Peter McIlroy.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)fsort.h	5.1 (Berkeley) %G%
+ *	@(#)fsort.h	8.1 (Berkeley) %G%
  */
 
 #define POW 20			/* exponent for buffer size */
