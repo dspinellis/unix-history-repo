821f3e2039ce6fe09bb7f2159b49eba13d0e763b
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-06 19:04:20 -0800

    it's a main(), throw in the copyright string
    SCCS-vsn: usr.sbin/amd/fsinfo/fsinfo.c 5.5

--- usr/src/usr.sbin/amd/fsinfo/fsinfo.c
+++ usr/src/usr.sbin/amd/fsinfo/fsinfo.c
@@ -9,11 +9,17 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)fsinfo.c	8.1 (Berkeley) %G%
+ *	@(#)fsinfo.c	5.5 (Berkeley) %G%
  *
  * $Id: fsinfo.c,v 5.2.2.1 1992/02/09 15:09:33 jsp beta $
  *
  */
+
+#ifndef lint
+char copyright[] =
+"@(#) Copyright (c) 1993 The Regents of the University of California.\n\
+ All rights reserved.\n";
+#endif /* not lint */
 
 /*
  * fsinfo
