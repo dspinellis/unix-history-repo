1787be72daef5233473609b2e7956c122522f44a
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-06 22:35:03 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.bin/mkstr/mkstr.1 8.1
    SCCS-vsn: usr.bin/mkstr/mkstr.c 8.1

--- usr/src/usr.bin/mkstr/mkstr.c
+++ usr/src/usr.bin/mkstr/mkstr.c
@@ -1,18 +1,18 @@
 /*
- * Copyright (c) 1980 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1980, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-char copyright[] =
-"@(#) Copyright (c) 1980 Regents of the University of California.\n\
- All rights reserved.\n";
+static char copyright[] =
+"@(#) Copyright (c) 1980, 1993\n\
+	The Regents of the University of California.  All rights reserved.\n";
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)mkstr.c	5.4 (Berkeley) %G%";
+static char sccsid[] = "@(#)mkstr.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <stdio.h>
