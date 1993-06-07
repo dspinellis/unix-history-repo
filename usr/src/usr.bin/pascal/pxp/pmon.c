0d11c9b51b67fafdae49bfe704c05e5b104797ba
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-06 23:02:41 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.bin/pascal/pxp/lval.c 8.1
    SCCS-vsn: usr.bin/pascal/pxp/main.c 8.1
    SCCS-vsn: usr.bin/pascal/pxp/pmon.c 8.1
    SCCS-vsn: usr.bin/ptx/pathnames.h 8.1
    SCCS-vsn: usr.bin/pascal/pxp/pp.c 8.1
    SCCS-vsn: usr.bin/ptx/ptx.1 8.1
    SCCS-vsn: usr.bin/pascal/pxp/proc.c 8.1
    SCCS-vsn: usr.bin/ptx/ptx.c 8.1
    SCCS-vsn: usr.bin/pascal/pxp/pxp.1 8.1

--- usr/src/usr.bin/pascal/pxp/pmon.c
+++ usr/src/usr.bin/pascal/pxp/pmon.c
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1980 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1980, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)pmon.c	5.4 (Berkeley) %G%";
+static char sccsid[] = "@(#)pmon.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 /*
