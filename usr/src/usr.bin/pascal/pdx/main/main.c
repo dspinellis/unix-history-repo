5411483942aae35f9f814883f7e7a6e5a1e14105
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-06 22:40:54 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.bin/pascal/pdx/main/main.c 8.1
    SCCS-vsn: usr.bin/pascal/pdx/mappings/filetab.h 8.1
    SCCS-vsn: usr.bin/pascal/pdx/mappings/functab.c 8.1
    SCCS-vsn: usr.bin/pascal/pdx/mappings/linetab.h 8.1
    SCCS-vsn: usr.bin/pascal/pdx/mappings/objaddr.c 8.1
    SCCS-vsn: usr.bin/pascal/pdx/mappings/srcfile.c 8.1
    SCCS-vsn: usr.bin/pascal/pdx/mappings/srcline.c 8.1
    SCCS-vsn: usr.bin/pascal/pdx/mc68000/cerror.s 5.3

--- usr/src/usr.bin/pascal/pdx/main/main.c
+++ usr/src/usr.bin/pascal/pdx/main/main.c
@@ -1,18 +1,18 @@
 /*-
- * Copyright (c) 1980 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1980, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-char copyright[] =
-"@(#) Copyright (c) 1980 The Regents of the University of California.\n\
- All rights reserved.\n";
+static char copyright[] =
+"@(#) Copyright (c) 1980, 1993\n\
+	The Regents of the University of California.  All rights reserved.\n";
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)main.c	5.3 (Berkeley) %G%";
+static char sccsid[] = "@(#)main.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 /*
