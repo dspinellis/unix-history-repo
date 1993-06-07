bbdfcbec552365879b8007169352f2c13e609fff
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-06 23:40:19 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.bin/tn3270/ctlr/options.c 8.1
    SCCS-vsn: usr.bin/tn3270/ctlr/options.h 8.1
    SCCS-vsn: usr.bin/unexpand/unexpand.c 8.1
    SCCS-vsn: usr.bin/tn3270/ctlr/outbound.c 8.1

--- usr/src/usr.bin/tn3270/ctlr/options.h
+++ usr/src/usr.bin/tn3270/ctlr/options.h
@@ -1,10 +1,10 @@
 /*-
- * Copyright (c) 1988 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1988, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)options.h	4.2 (Berkeley) %G%
+ *	@(#)options.h	8.1 (Berkeley) %G%
  */
 
 /*
