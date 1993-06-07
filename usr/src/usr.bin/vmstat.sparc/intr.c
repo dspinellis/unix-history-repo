1ba820be9886d56d0ea2e1bc79b5b679e13f89a2
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-07 05:22:51 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.bin/vmstat/vmstat.c 8.1
    SCCS-vsn: usr.bin/vmstat.sparc/extern.h 8.1
    SCCS-vsn: usr.bin/vmstat.sparc/getdev.c 8.1
    SCCS-vsn: usr.bin/vmstat.sparc/getdev.h 8.1
    SCCS-vsn: usr.bin/vmstat.sparc/intr.c 8.1
    SCCS-vsn: usr.bin/vmstat.sparc/main.c 8.1
    SCCS-vsn: usr.bin/vmstat.sparc/mem.c 8.1
    SCCS-vsn: usr.bin/vmstat.sparc/names.c 8.1
    SCCS-vsn: usr.bin/vmstat.sparc/sum.c 8.1

--- usr/src/usr.bin/vmstat.sparc/intr.c
+++ usr/src/usr.bin/vmstat.sparc/intr.c
@@ -1,12 +1,12 @@
 /*
- * Copyright (c) 1993 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)intr.c	5.1 (Berkeley) %G%";
+static char sccsid[] = "@(#)intr.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/types.h>
--- usr/src/usr.bin/vmstat.sparc/names.c
+++ usr/src/usr.bin/vmstat.sparc/names.c
@@ -1,10 +1,10 @@
 /*-
- * Copyright (c) 1993 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)names.c	5.1 (Berkeley) %G%
+ *	@(#)names.c	8.1 (Berkeley) %G%
  */
 
 #ifdef hp300
