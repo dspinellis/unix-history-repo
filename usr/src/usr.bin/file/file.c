10dee91fc5e48b67d840918a15aa1eecdc1a930a
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-06 21:38:34 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.bin/file/file.1 8.1
    SCCS-vsn: usr.bin/file/file.c 8.1

--- usr/src/usr.bin/file/file.c
+++ usr/src/usr.bin/file/file.c
@@ -1,18 +1,18 @@
 /*-
- * Copyright (c) 1991 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1991, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.proprietary.c%
  */
 
 #ifndef lint
-char copyright[] =
-"@(#) Copyright (c) 1991 The Regents of the University of California.\n\
- All rights reserved.\n";
+static char copyright[] =
+"@(#) Copyright (c) 1991, 1993\n\
+	The Regents of the University of California.  All rights reserved.\n";
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)file.c	5.5 (Berkeley) %G%";
+static char sccsid[] = "@(#)file.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 /*
