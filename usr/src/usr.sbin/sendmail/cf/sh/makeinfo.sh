999d1015113d30f20bd7dbb0d38cb9d5b21a03dd
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-07 17:12:44 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.sbin/sendmail/cf/sh/makeinfo.sh 8.1

--- usr/src/usr.sbin/sendmail/cf/sh/makeinfo.sh
+++ usr/src/usr.sbin/sendmail/cf/sh/makeinfo.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 #
 # Copyright (c) 1983 Eric P. Allman
-# Copyright (c) 1988 The Regents of the University of California.
-# All rights reserved.
+# Copyright (c) 1988, 1993
+#	The Regents of the University of California.  All rights reserved.
 #
 # %sccs.include.redist.sh%
 #
-#	@(#)makeinfo.sh	6.2 (Berkeley) %G%
+#	@(#)makeinfo.sh	8.1 (Berkeley) %G%
 #
 
 echo '#####' built by `whoami` on `date`
