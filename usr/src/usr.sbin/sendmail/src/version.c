cba1c6616b139df4543a967c8d90181bf9f18979
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-07 17:54:47 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.sbin/sendmail/src/version.c 6.66

--- usr/src/usr.sbin/sendmail/src/version.c
+++ usr/src/usr.sbin/sendmail/src/version.c
@@ -1,13 +1,13 @@
 /*
  * Copyright (c) 1983 Eric P. Allman
- * Copyright (c) 1988 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1988, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)version.c	8.1 (Berkeley) %G%";
+static char sccsid[] = "@(#)version.c	6.66 (Berkeley) %G%";
 #endif /* not lint */
 
-char	Version[] = "BETA-8.1";
+char	Version[] = "6.66";
