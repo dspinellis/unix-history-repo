12a8d49cab4b5e0001d2d589ee30ffc6519140d2
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-07 17:22:25 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.sbin/sendmail/makemap/makemap.8 8.1
    SCCS-vsn: usr.sbin/sendmail/makemap/makemap.c 8.1

--- usr/src/usr.sbin/sendmail/makemap/makemap.c
+++ usr/src/usr.sbin/sendmail/makemap/makemap.c
@@ -1,13 +1,13 @@
 /*
  * Copyright (c) 1992 Eric P. Allman.
- * Copyright (c) 1992 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)makemap.c	6.2 (Berkeley) %G%";
+static char sccsid[] = "@(#)makemap.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <stdio.h>
