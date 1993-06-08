c9c6068336307f54bfaf33b253a28d537800f88b
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-08 03:54:12 -0800

    4.4BSD snapshot (revision 8.1)
    SCCS-vsn: usr.bin/more/main.c 8.1

--- usr/src/usr.bin/more/main.c
+++ usr/src/usr.bin/more/main.c
@@ -14,7 +14,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)main.c	5.14 (Berkeley) %G%";
+static char sccsid[] = "@(#)main.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 /*
