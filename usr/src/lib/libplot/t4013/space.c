75ee17d6134e2d3b3f8892ba25c9e7726c69b0a0
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-08 03:53:12 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: lib/libplot/t4013/point.c 8.1
    SCCS-vsn: lib/libplot/t4013/space.c 8.1
    SCCS-vsn: lib/libplot/t4014/box.c 8.1
    SCCS-vsn: lib/libplot/t4014/close.c 8.1
    SCCS-vsn: usr.bin/more/main.c 5.14

--- usr/src/lib/libplot/t4013/space.c
+++ usr/src/lib/libplot/t4013/space.c
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1985 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1985, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.proprietary.c%
  */
--- usr/src/usr.bin/more/main.c
+++ usr/src/usr.bin/more/main.c
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 1988 Mark Nudleman
- * Copyright (c) 1988 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1988, 1993
+ *	Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
@@ -9,12 +9,12 @@
 #ifndef lint
 char copyright[] =
 "@(#) Copyright (c) 1988 Mark Nudleman.\n\
-@(#) Copyright (c) 1988 Regents of the University of California.\n\
- All rights reserved.\n";
+@(#) Copyright (c) 1988, 1993
+	Regents of the University of California.  All rights reserved.\n";
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)main.c	8.1 (Berkeley) %G%";
+static char sccsid[] = "@(#)main.c	5.14 (Berkeley) %G%";
 #endif /* not lint */
 
 /*
