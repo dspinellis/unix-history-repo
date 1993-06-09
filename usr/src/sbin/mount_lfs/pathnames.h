1c8031284ff341b0bb58f2a980221bf7bb327842
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-09 02:13:42 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: sbin/mount_lfs/mount_lfs.8 8.1
    SCCS-vsn: sbin/mount_lfs/mount_lfs.c 8.1
    SCCS-vsn: sbin/mount_lfs/pathnames.h 8.1

--- usr/src/sbin/mount_lfs/pathnames.h
+++ usr/src/sbin/mount_lfs/pathnames.h
@@ -1,10 +1,10 @@
 /*-
- * Copyright (c) 1993 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)pathnames.h	5.1 (Berkeley) %G%
+ *	@(#)pathnames.h	8.1 (Berkeley) %G%
  */
 
 #define _PATH_LFS_CLEANERD	"/usr/libexec/lfs_cleanerd"
