be3e6f2ffddbbfdf711745e4fb0922e519acd340
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-09 01:36:42 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: lib/libcurses/PSD.doc/Master 8.1
    SCCS-vsn: lib/libcurses/PSD.doc/appen.A 8.1
    SCCS-vsn: lib/libcurses/PSD.doc/appen.B 8.1
    SCCS-vsn: lib/libcurses/PSD.doc/appen.C 8.1
    SCCS-vsn: lib/libcurses/PSD.doc/macros 8.1
    SCCS-vsn: share/doc/psd/20.ipctut/dgramread.c 8.1
    SCCS-vsn: share/doc/psd/20.ipctut/dgramsend.c 8.1
    SCCS-vsn: share/doc/psd/20.ipctut/pipe.c 8.1
    SCCS-vsn: share/doc/psd/20.ipctut/socketpair.c 8.1
    SCCS-vsn: share/doc/psd/20.ipctut/strchkread.c 8.1
    SCCS-vsn: share/doc/psd/20.ipctut/streamread.c 8.1
    SCCS-vsn: share/doc/psd/20.ipctut/streamwrite.c 8.1
    SCCS-vsn: share/doc/psd/20.ipctut/udgramread.c 8.1
    SCCS-vsn: share/doc/psd/20.ipctut/udgramsend.c 8.1
    SCCS-vsn: share/doc/psd/20.ipctut/ustreamread.c 8.1
    SCCS-vsn: share/doc/psd/20.ipctut/ustreamwrite.c 8.1
    SCCS-vsn: share/doc/psd/20.ipctut/tutor.me 8.1

--- usr/src/share/doc/psd/20.ipctut/ustreamread.c
+++ usr/src/share/doc/psd/20.ipctut/ustreamread.c
@@ -1,9 +1,9 @@
-.\" Copyright (c) 1986 The Regents of the University of California.
-.\" All rights reserved.
+.\" Copyright (c) 1986, 1993
+.\"	The Regents of the University of California.  All rights reserved.
 .\"
 .\" %sccs.include.redist.roff%
 .\"
-.\"	@(#)ustreamread.c	6.5 (Berkeley) %G%
+.\"	@(#)ustreamread.c	8.1 (Berkeley) %G%
 .\"
 #include <sys/types.h>
 #include <sys/socket.h>
