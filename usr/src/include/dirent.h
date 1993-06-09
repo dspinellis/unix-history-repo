7e6664bf315fa0992e84ce94e493f7bbd63d3a9b
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-09 00:57:26 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: include/dirent.h 8.1
    SCCS-vsn: lib/libc/gen/rewinddir.c 8.1
    SCCS-vsn: lib/libc/gen/opendir.c 8.1

--- usr/src/include/dirent.h
+++ usr/src/include/dirent.h
@@ -1,10 +1,10 @@
 /*-
- * Copyright (c) 1989 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1989, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)dirent.h	5.20 (Berkeley) %G%
+ *	@(#)dirent.h	8.1 (Berkeley) %G%
  */
 
 #ifndef _DIRENT_H_
