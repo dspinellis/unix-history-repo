555771c3680231e6ed178ef20f1683b53e7a47a6
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-09 00:57:26 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: include/dirent.h 8.1
    SCCS-vsn: lib/libc/gen/rewinddir.c 8.1
    SCCS-vsn: lib/libc/gen/opendir.c 8.1

--- usr/src/lib/libc/gen/rewinddir.c
+++ usr/src/lib/libc/gen/rewinddir.c
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1990 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)rewinddir.c	5.2 (Berkeley) %G%";
+static char sccsid[] = "@(#)rewinddir.c	8.1 (Berkeley) %G%";
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/types.h>
