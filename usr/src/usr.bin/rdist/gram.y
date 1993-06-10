df2de95921c2db8e0ff45cc51dfbba00ff49abbc
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-10 05:50:13 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.bin/rdist/defs.h 8.1
    SCCS-vsn: usr.bin/rdist/docmd.c 8.1
    SCCS-vsn: usr.bin/rdist/expand.c 8.1
    SCCS-vsn: usr.bin/rdist/gram.y 8.1
    SCCS-vsn: usr.bin/rdist/lookup.c 8.1
    SCCS-vsn: usr.bin/rdist/main.c 8.1
    SCCS-vsn: usr.bin/rdist/pathnames.h 8.1
    SCCS-vsn: usr.bin/rdist/rdist.1 8.1
    SCCS-vsn: usr.bin/rdist/server.c 8.1

--- usr/src/usr.bin/rdist/gram.y
+++ usr/src/usr.bin/rdist/gram.y
@@ -1,13 +1,13 @@
 %{
 /*
- * Copyright (c) 1983 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1983, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)gram.y	5.7 (Berkeley) %G%";
+static char sccsid[] = "@(#)gram.y	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 #include "defs.h"
--- usr/src/usr.bin/rdist/server.c
+++ usr/src/usr.bin/rdist/server.c
@@ -1,12 +1,12 @@
 /*
- * Copyright (c) 1983 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1983, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)server.c	5.17 (Berkeley) %G%";
+static char sccsid[] = "@(#)server.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/wait.h>
