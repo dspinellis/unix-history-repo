9ea485118ea70e022faba63eed07a647227e19f1
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-10 05:45:27 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.bin/login/klogin.c 8.1
    SCCS-vsn: usr.bin/login/login.1 8.1
    SCCS-vsn: usr.bin/login/login.c 8.1
    SCCS-vsn: usr.bin/login/pathnames.h 8.1
    SCCS-vsn: usr.bin/logname/logname.1 8.1
    SCCS-vsn: usr.bin/logname/logname.c 8.1

--- usr/src/usr.bin/login/klogin.c
+++ usr/src/usr.bin/login/klogin.c
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1990 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)klogin.c	5.11 (Berkeley) %G%";
+static char sccsid[] = "@(#)klogin.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 #ifdef KERBEROS
