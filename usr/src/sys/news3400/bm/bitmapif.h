4425bd90d1d4e59940042a0230330377fb523c41
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-11 06:49:25 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: sys/news3400/bm/bitmapif.c 8.1
    SCCS-vsn: sys/news3400/bm/bitmapif.h 8.1
    SCCS-vsn: sys/news3400/bm/bmcons.c 8.1
    SCCS-vsn: sys/news3400/bm/fnt.c 8.1
    SCCS-vsn: sys/news3400/bm/fnt24.c 8.1
    SCCS-vsn: sys/news3400/bm/kb_ctrl.c 8.1
    SCCS-vsn: sys/news3400/bm/kb_encode.c 8.1

--- usr/src/sys/news3400/bm/bitmapif.h
+++ usr/src/sys/news3400/bm/bitmapif.h
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1992 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Sony Corp. and Kazumasa Utashiro of Software Research Associates, Inc.
@@ -9,7 +9,7 @@
  *
  * from: $Hdr: bitmapif.h,v 4.300 91/06/09 06:14:42 root Rel41 $ SONY
  *
- *	@(#)bitmapif.h	7.1 (Berkeley) %G%
+ *	@(#)bitmapif.h	8.1 (Berkeley) %G%
  */
 
 /*
