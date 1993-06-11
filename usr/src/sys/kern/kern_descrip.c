60ebe536aa859664a156f6c6e5c61cee4d0e005d
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-11 04:56:56 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: sys/kern/init_main.c 8.1
    SCCS-vsn: sys/kern/kern_acct.c 8.1
    SCCS-vsn: sys/kern/kern_clock.c 8.1
    SCCS-vsn: sys/kern/kern_descrip.c 8.1

--- usr/src/sys/kern/kern_descrip.c
+++ usr/src/sys/kern/kern_descrip.c
@@ -1,10 +1,10 @@
 /*
- * Copyright (c) 1982, 1986, 1989, 1991 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1982, 1986, 1989, 1991, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)kern_descrip.c	7.42 (Berkeley) %G%
+ *	@(#)kern_descrip.c	8.1 (Berkeley) %G%
  */
 
 #include <sys/param.h>
