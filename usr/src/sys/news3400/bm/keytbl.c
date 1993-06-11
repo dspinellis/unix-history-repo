f89037a93f58f0616ce4b7d18e774c741550b383
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-11 06:49:34 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: sys/news3400/bm/keytbl.c 8.1

--- usr/src/sys/news3400/bm/keytbl.c
+++ usr/src/sys/news3400/bm/keytbl.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1992 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Sony Corp. and Kazumasa Utashiro of Software Research Associates, Inc.
@@ -9,7 +9,7 @@
  *
  * from: $Hdr: keytbl.c,v 4.300 91/06/09 06:14:54 root Rel41 $ SONY
  *
- *	@(#)keytbl.c	7.3 (Berkeley) %G%
+ *	@(#)keytbl.c	8.1 (Berkeley) %G%
  */
 
 #include <news3400/iop/keyboard.h>
