897990d061ac19c16635f96cb597dfd657739461
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-11 04:24:02 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: sys/hp/dev/hil_keymaps.c 8.1
    SCCS-vsn: sys/hp/dev/hil_subr.c 8.1
    SCCS-vsn: sys/hp/dev/hilioctl.h 8.1
    SCCS-vsn: sys/hp/dev/hilreg.h 8.1

--- usr/src/sys/hp/dev/hil_keymaps.c
+++ usr/src/sys/hp/dev/hil_keymaps.c
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 1988 University of Utah.
- * Copyright (c) 1990 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * the Systems Programming Group of the University of Utah Computer
@@ -11,7 +11,7 @@
  *
  * from: Utah $Hdr: hil_keymaps.c 1.1 90/07/09$
  *
- *	@(#)hil_keymaps.c	7.4 (Berkeley) %G%
+ *	@(#)hil_keymaps.c	8.1 (Berkeley) %G%
  */
 
 /*
