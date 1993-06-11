517890651c6c68833dbb2b13bc940fc41f8db2de
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-11 06:30:45 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: sys/pmax/stand/libsa/callvec.c 8.1
    SCCS-vsn: sys/pmax/stand/libsa/devopen.c 8.1
    SCCS-vsn: sys/pmax/stand/libsa/getenv.c 8.1
    SCCS-vsn: sys/pmax/stand/libsa/gets.c 8.1
    SCCS-vsn: sys/pmax/stand/libsa/strcat.c 8.1
    SCCS-vsn: sys/pmax/stand/libsa/strcmp.c 8.1
    SCCS-vsn: sys/pmax/stand/libsa/strcpy.c 8.1

--- usr/src/sys/pmax/stand/libsa/gets.c
+++ usr/src/sys/pmax/stand/libsa/gets.c
@@ -1,13 +1,13 @@
 /*-
- * Copyright (c) 1992 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Ralph Campbell.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)gets.c	7.1 (Berkeley) %G%
+ *	@(#)gets.c	8.1 (Berkeley) %G%
  */
 
 #include <pmax/stand/dec_prom.h>
