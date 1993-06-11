f16f7c0f64943a26000c5381e4113f4dcaf611bd
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-11 07:43:30 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: sys/net/route.c 8.1
    SCCS-vsn: sys/news3400/fb/x.c 8.1
    SCCS-vsn: sys/news3400/fb/x.c 8.1
    SCCS-vsn: sys/news3400/fb/x.c 8.1
    SCCS-vsn: sys/news3400/fb/x.c 8.1
    SCCS-vsn: sys/news3400/fb/x.c 8.1
    SCCS-vsn: sys/news3400/fb/x.c 8.1
    SCCS-vsn: sys/news3400/fb/x.c 8.1
    SCCS-vsn: sys/news3400/fb/fb_mfbs.c 8.1
    SCCS-vsn: sys/news3400/fb/fb_probe.c 8.1
    SCCS-vsn: sys/news3400/fb/fb_probe.c 8.1
    SCCS-vsn: sys/news3400/fb/fb_start.c 8.1
    SCCS-vsn: sys/news3400/fb/fbbm_253.c 8.1
    SCCS-vsn: sys/news3400/fb/fbbm_lcdm.c 8.1
    SCCS-vsn: sys/news3400/fb/fbdefs.h 8.1

--- usr/src/sys/news3400/fb/fb_probe.c
+++ usr/src/sys/news3400/fb/fb_probe.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1992 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Sony Corp. and Kazumasa Utashiro of Software Research Associates, Inc.
@@ -9,7 +9,7 @@
  *
  * from: $Hdr: fb_probe.c,v 4.300 91/06/09 06:32:57 root Rel41 $ SONY
  *
- *	@(#)fb_probe.c	7.3 (Berkeley) %G%
+ *	@(#)fb_probe.c	8.1 (Berkeley) %G%
  */
 
 #ifdef IPC_MRX
--- usr/src/sys/news3400/fb/fbdefs.h
+++ usr/src/sys/news3400/fb/fbdefs.h
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1992 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Sony Corp. and Kazumasa Utashiro of Software Research Associates, Inc.
@@ -9,7 +9,7 @@
  *
  * from: $Hdr: fbdefs.h,v 4.300 91/06/09 06:33:54 root Rel41 $ SONY
  *
- *	@(#)fbdefs.h	7.4 (Berkeley) %G%
+ *	@(#)fbdefs.h	8.1 (Berkeley) %G%
  */
 
 #ifndef mips
