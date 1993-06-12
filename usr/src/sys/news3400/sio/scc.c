123ec4fb18d414e219281a1710a774b483d9810a
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-11 22:05:53 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: sys/news3400/sio/scc.c 8.1
    SCCS-vsn: sys/news3400/sio/scc.h 8.1
    SCCS-vsn: sys/news3400/sio/sccparam.h 8.1
    SCCS-vsn: sys/news3400/sio/sccreg.h 8.1
    SCCS-vsn: sys/news3400/sio/scc.conf 7.3

--- usr/src/sys/news3400/sio/scc.c
+++ usr/src/sys/news3400/sio/scc.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1992 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Sony Corp. and Kazumasa Utashiro of Software Research Associates, Inc.
@@ -9,7 +9,7 @@
  *
  * from: $Hdr: scc.c,v 4.300 91/06/09 06:44:53 root Rel41 $ SONY
  *
- *	@(#)scc.c	7.4 (Berkeley) %G%
+ *	@(#)scc.c	8.1 (Berkeley) %G%
  */
 
 /*
--- usr/src/sys/news3400/sio/sccreg.h
+++ usr/src/sys/news3400/sio/sccreg.h
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1992 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Sony Corp. and Kazumasa Utashiro of Software Research Associates, Inc.
@@ -9,7 +9,7 @@
  *
  * from: $Hdr: sccreg.h,v 4.300 91/06/09 06:44:58 root Rel41 $ SONY
  *
- *	@(#)sccreg.h	7.1 (Berkeley) %G%
+ *	@(#)sccreg.h	8.1 (Berkeley) %G%
  */
 
 #ifndef _SCCREG_
