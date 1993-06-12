0b3f38f12a9e5b307c62e74db9dcff4dc50b2d69
CSRG <csrg@ucbvax.Berkeley.EDU>
1993-06-12 02:10:55 -0800

    BSD 4_4_Lite2 development Work on file usr/src/contrib/X11R5-lib/X11/Composite.h Work on file usr/src/contrib/X11R5-lib/X11/keysym.h Work on file usr/src/contrib/X11R5-lib/X11/Xutil.h Work on file usr/src/contrib/X11R5-lib/X11/Xresource.h Work on file usr/src/contrib/X11R5-lib/X11/ap_keysym.h Work on file usr/src/contrib/X11R5-lib/X11/Xprotostr.h Work on file usr/src/contrib/X11R5-lib/X11/cursorfont.h Work on file usr/src/contrib/X11R5-lib/X11/Xmd.h Work on file usr/src/contrib/X11R5-lib/X11/Xlocale.h Work on file usr/src/contrib/X11R5-lib/X11/Xfuncproto.h Work on file usr/src/contrib/X11R5-lib/X11/Xfuncs.h Work on file usr/src/contrib/X11R5-lib/X11/Xosdefs.h Work on file usr/src/contrib/X11R5-lib/X11/Xlibint.h Work on file usr/src/contrib/X11R5-lib/X11/Xproto.h Work on file usr/src/contrib/X11R5-lib/X11/Xos.h Work on file usr/src/contrib/X11R5-lib/X11/X.h Work on file usr/src/contrib/X11R5-lib/X11/Vendor.h Work on file usr/src/contrib/X11R5-lib/X11/Xauth.h Work on file usr/src/contrib/X11R5-lib/X11/StringDefs.h Work on file usr/src/contrib/X11R5-lib/X11/Xatom.h Work on file usr/src/contrib/X11R5-lib/X11/X10.h Work on file usr/src/contrib/X11R5-lib/X11/Xcms.h Work on file usr/src/contrib/X11R5-lib/X11/VendorP.h Work on file usr/src/contrib/X11R5-lib/X11/XWDFile.h Work on file usr/src/contrib/X11R5-lib/X11/Xdmcp.h Work on file usr/src/contrib/X11R5-lib/X11/Sunkeysym.h Work on file usr/src/contrib/X11R5-lib/X11/ObjectP.h Work on file usr/src/contrib/X11R5-lib/X11/RectObj.h Work on file usr/src/contrib/X11R5-lib/X11/IntrinsicP.h Work on file usr/src/contrib/X11R5-lib/X11/ShellP.h Work on file usr/src/contrib/X11R5-lib/X11/RectObjP.h Work on file usr/src/contrib/X11R5-lib/X11/Object.h Work on file usr/src/contrib/X11R5-lib/X11/Shell.h Work on file usr/src/contrib/X11R5-lib/X11/Constraint.h Work on file usr/src/contrib/X11R5-lib/X11/DECkeysym.h Work on file usr/src/contrib/X11R5-lib/X11/ConstrainP.h Work on file usr/src/contrib/X11R5-lib/X11/CoreP.h Work on file usr/src/contrib/X11R5-lib/X11/CompositeP.h Work on file usr/src/contrib/X11R5-lib/X11/Core.h Work on file usr/src/contrib/X11R5-lib/X11/Intrinsic.h Work on file usr/src/contrib/X11R5-lib/X11/keysymdef.h
    Synthesized-from: CSRG/cd3/4.4BSD-Lite2

---
+++ usr/src/contrib/X11R5-lib/X11/Xfuncs.h
@@ -0,0 +1,55 @@
+/*
+ * $XConsortium: Xfuncs.h,v 1.8 91/04/17 09:27:52 rws Exp $
+ * 
+ * Copyright 1990 by the Massachusetts Institute of Technology
+ *
+ * Permission to use, copy, modify, and distribute this software and its
+ * documentation for any purpose and without fee is hereby granted, provided 
+ * that the above copyright notice appear in all copies and that both that 
+ * copyright notice and this permission notice appear in supporting 
+ * documentation, and that the name of M.I.T. not be used in advertising
+ * or publicity pertaining to distribution of the software without specific, 
+ * written prior permission. M.I.T. makes no representations about the 
+ * suitability of this software for any purpose.  It is provided "as is"
+ * without express or implied warranty.
+ *
+ */
+
+#ifndef _XFUNCS_H_
+#define _XFUNCS_H_
+
+#include <X11/Xosdefs.h>
+
+#ifdef X_USEBFUNCS
+void bcopy();
+void bzero();
+int bcmp();
+#else
+#if (__STDC__ && !defined(X_NOT_STDC_ENV) && !defined(sun) && !defined(macII)) || defined(SVR4) || defined(hpux) || defined(_IBMR2)
+#include <string.h>
+#define bcopy(b1,b2,len) memmove(b2, b1, (size_t)(len))
+#define bzero(b,len) memset(b, 0, (size_t)(len))
+#define bcmp(b1,b2,len) memcmp(b1, b2, (size_t)(len))
+#else
+#ifdef sgi
+#include <bstring.h>
+#else
+#ifdef SYSV
+#include <memory.h>
+#if defined(_XBCOPYFUNC) && !defined(macII)
+#define bcopy _XBCOPYFUNC
+#define _XNEEDBCOPYFUNC
+#endif
+void bcopy();
+#define bzero(b,len) memset(b, 0, len)
+#define bcmp(b1,b2,len) memcmp(b1, b2, len)
+#else /* bsd */
+void bcopy();
+void bzero();
+int bcmp();
+#endif /* SYSV */
+#endif /* sgi */
+#endif /* __STDC__ and relatives */
+#endif /* X_USEBFUNCS */
+
+#endif /* _XFUNCS_H_ */
