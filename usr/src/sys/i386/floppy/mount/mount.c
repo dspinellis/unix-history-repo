a943599dca74c72243da11a67588c956669ee8cc
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-11 22:29:49 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: sys/i386/floppy/mount/mount.c 8.1

--- usr/src/sys/i386/floppy/mount/mount.c
+++ usr/src/sys/i386/floppy/mount/mount.c
@@ -1,10 +1,10 @@
 /*-
- * Copyright (c) 1991 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1991, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)mount.c	5.1 (Berkeley) %G%
+ *	@(#)mount.c	8.1 (Berkeley) %G%
  */
 
 #include <sys/param.h>
