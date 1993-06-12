5c2f7e8c6d6ed8db4ea823c2a32877d6a8fdf649
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1993-06-11 21:35:13 -0800

    date and time created 93/06/11 14:35:13 by mckusick
    SCCS-vsn: sys/stand/dev.c 7.1
    SCCS-vsn: sys/stand/disklabel.c 7.1
    SCCS-vsn: sys/stand/getfile.c 7.1

---
+++ usr/src/sys/stand/getfile.c
@@ -0,0 +1,23 @@
+/*-
+ * Copyright (c) 1993 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.redist.c%
+ *
+ *	@(#)getfile.c	7.1 (Berkeley) %G%
+ */
+
+getfile(prompt, mode)
+	char *prompt;
+	int mode;
+{
+	int fd;
+	char buf[100];
+
+	do {
+		printf("%s: ", prompt);
+		gets(buf);
+	} while ((fd = open(buf, mode)) < 0);
+
+	return (fd);
+}
