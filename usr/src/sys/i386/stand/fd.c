4ae6cce5f7b49154be486ad3048747ee61798853
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-11 22:58:14 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: sys/i386/stand/boot.c 8.1
    SCCS-vsn: sys/i386/stand/bootxx.c 8.1
    SCCS-vsn: sys/i386/stand/cga.c 8.1
    SCCS-vsn: sys/i386/stand/conf.c 8.1
    SCCS-vsn: sys/i386/stand/confxx.c 8.1
    SCCS-vsn: sys/i386/stand/fd.c 8.1
    SCCS-vsn: sys/i386/stand/fdbootblk.c 8.1
    SCCS-vsn: sys/i386/stand/kbd.c 8.1
    SCCS-vsn: sys/i386/stand/prf.c 8.1
    SCCS-vsn: sys/i386/stand/srt0.c 8.1
    SCCS-vsn: sys/i386/stand/wd.c 8.1
    SCCS-vsn: sys/i386/stand/wdbootblk.c 8.1

--- usr/src/sys/i386/stand/fd.c
+++ usr/src/sys/i386/stand/fd.c
@@ -1,13 +1,13 @@
 /*-
- * Copyright (c) 1990 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Don Ahn.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)fd.c	7.4 (Berkeley) %G%
+ *	@(#)fd.c	8.1 (Berkeley) %G%
  */
 
 /****************************************************************************/
--- usr/src/sys/i386/stand/srt0.c
+++ usr/src/sys/i386/stand/srt0.c
@@ -1,13 +1,13 @@
 /*-
- * Copyright (c) 1990 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * William Jolitz.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)srt0.c	7.3 (Berkeley) %G%
+ *	@(#)srt0.c	8.1 (Berkeley) %G%
  */
 
 /*
