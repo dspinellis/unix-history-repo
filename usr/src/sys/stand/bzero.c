644ed3bd80be1710dd2acedae3f39c1f21b64390
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1993-06-11 21:35:11 -0800

    date and time created 93/06/11 14:35:11 by mckusick
    SCCS-vsn: sys/stand/bzero.c 7.1
    SCCS-vsn: sys/stand/cat.c 7.1

---
+++ usr/src/sys/stand/bzero.c
@@ -0,0 +1,24 @@
+/*-
+ * Copyright (c) 1993 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.redist.c%
+ *
+ *	@(#)bzero.c	7.1 (Berkeley) %G%
+ */
+
+/*
+ * This is designed to be small, not fast.
+ */
+void
+bzero(s1, n)
+	void *s1;
+	unsigned n;
+{
+	register char *t = s1;
+
+	while (n != 0) {
+		*t++ = 0;
+		n--;
+	}
+}
