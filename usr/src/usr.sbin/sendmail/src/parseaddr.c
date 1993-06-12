7318e1f497e5525aa4bb313d71387e33b30e842e
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-06-12 15:35:11 -0800

    fix bug causing class backups to jump two tokens instead of one
    SCCS-vsn: usr.sbin/sendmail/src/parseaddr.c 6.61

--- usr/src/usr.sbin/sendmail/src/parseaddr.c
+++ usr/src/usr.sbin/sendmail/src/parseaddr.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)parseaddr.c	8.1 (Berkeley) %G%";
+static char sccsid[] = "@(#)parseaddr.c	6.61 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -826,7 +826,7 @@ rewrite(pvp, ruleset, e)
 				if ((*rp & 0377) == MATCHCLASS)
 				{
 					/* extend binding and try again */
-					mlp->last = avp++;
+					mlp->last = avp;
 					goto extendclass;
 				}
 			}
