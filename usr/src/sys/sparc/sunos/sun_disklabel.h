e0367c4706cf289665d64281b4aa2cfe505a0f65
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-11 22:19:38 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: sys/sparc/sunos/sun_disklabel.c 8.1
    SCCS-vsn: sys/sparc/sunos/sun_disklabel.h 8.1
    SCCS-vsn: sys/sparc/sunos/sun_ioctl.c 8.1
    SCCS-vsn: sys/sparc/sunos/sun_misc.c 8.1

--- usr/src/sys/sparc/sunos/sun_disklabel.h
+++ usr/src/sys/sparc/sunos/sun_disklabel.h
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1992 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This software was developed by the Computer Systems Engineering group
  * at Lawrence Berkeley Laboratory under DARPA contract BG 91-66 and
@@ -13,7 +13,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)sun_disklabel.h	7.3 (Berkeley) %G%
+ *	@(#)sun_disklabel.h	8.1 (Berkeley) %G%
  *
  * from: $Header: sun_disklabel.h,v 1.4 92/06/17 07:04:13 torek Exp $
  */
