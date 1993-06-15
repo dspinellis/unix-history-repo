5304fa209929498933b091d518abb4c46549d723
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-14 23:12:57 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: sys/kern/kern_exit.c 8.1

--- usr/src/sys/kern/kern_exit.c
+++ usr/src/sys/kern/kern_exit.c
@@ -1,10 +1,10 @@
 /*
- * Copyright (c) 1982, 1986, 1989, 1991 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1982, 1986, 1989, 1991, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)kern_exit.c	7.56 (Berkeley) %G%
+ *	@(#)kern_exit.c	8.1 (Berkeley) %G%
  */
 
 #include <sys/param.h>
