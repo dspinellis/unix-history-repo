fdc1f2204a9071ea88f9591db86f43544adb43e8
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-14 22:20:43 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: sys/kern/vfs_syscalls.c 8.1

--- usr/src/sys/kern/vfs_syscalls.c
+++ usr/src/sys/kern/vfs_syscalls.c
@@ -1,10 +1,10 @@
 /*
- * Copyright (c) 1989 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1989, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)vfs_syscalls.c	7.113 (Berkeley) %G%
+ *	@(#)vfs_syscalls.c	8.1 (Berkeley) %G%
  */
 
 #include <sys/param.h>
