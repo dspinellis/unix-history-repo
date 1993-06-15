8be35f755ffe3677e05e24eb1a7fb9985264292a
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1993-06-15 01:58:08 -0800

    update for new manual format
    SCCS-vsn: libexec/getNAME/getNAME.c 5.6

--- usr/src/libexec/getNAME/getNAME.c
+++ usr/src/libexec/getNAME/getNAME.c
@@ -12,7 +12,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)getNAME.c	8.1 (Berkeley) %G%";
+static char sccsid[] = "@(#)getNAME.c	5.6 (Berkeley) %G%";
 #endif /* not lint */
 
 /*
@@ -27,6 +27,7 @@ static char sccsid[] = "@(#)getNAME.c	8.1 (Berkeley) %G%";
 
 int tocrc;
 int intro;
+int typeflag;
 
 void doname __P((char *));
 void dorefname __P((char *));
@@ -43,13 +44,16 @@ main(argc, argv)
 	extern int optind;
 	int ch;
 
-	while ((ch = getopt(argc, argv, "it")) != EOF)
+	while ((ch = getopt(argc, argv, "itw")) != EOF)
 		switch(ch) {
 		case 'i':
 			intro = 1;
 			break;
 		case 't':
 			tocrc = 1;
+			break;
+		case 'w':
+			typeflag = 1;
 			break;
 		case '?':
 		default:
@@ -67,26 +71,44 @@ main(argc, argv)
 }
 
 void
-getfrom(name)
-	char *name;
+getfrom(pathname)
+	char *pathname;
 {
 	int i = 0;
+	char *name, *loc;
 	char headbuf[BUFSIZ];
 	char linbuf[BUFSIZ];
 
-	if (freopen(name, "r", stdin) == 0) {
-		perror(name);
+	if (freopen(pathname, "r", stdin) == 0) {
+		perror(pathname);
 		return;
 	}
+	if (name = strrchr(pathname, '/'))
+		name++;
+	else
+		name = pathname;
 	for (;;) {
-		if (fgets(headbuf, sizeof headbuf, stdin) == NULL)
+		if (fgets(headbuf, sizeof headbuf, stdin) == NULL) {
+			if (typeflag)
+				printf("%-60s	UNKNOWN\n", pathname);
 			return;
+		}
 		if (headbuf[0] != '.')
 			continue;
-		if (headbuf[1] == 'T' && headbuf[2] == 'H')
+		if ((headbuf[1] == 'T' && headbuf[2] == 'H') ||
+		    (headbuf[1] == 't' && headbuf[2] == 'h'))
 			break;
-		if (headbuf[1] == 't' && headbuf[2] == 'h')
-			break;
+		if (headbuf[1] == 'D' && headbuf[2] == 't') {
+			if (typeflag) {
+				printf("%-60s	NEW\n", pathname);
+				return;
+			}
+			goto newman;
+		}
+	}
+	if (typeflag) {
+		printf("%-60s	OLD\n", pathname);
+		return;
 	}
 	for (;;) {
 		if (fgets(linbuf, sizeof linbuf, stdin) == NULL)
@@ -103,26 +125,71 @@ getfrom(name)
 		doname(name);
 	if (!intro)
 		printf("%s\t", headbuf);
+	linbuf[0] = '\0';
 	for (;;) {
-		if (fgets(linbuf, sizeof linbuf, stdin) == NULL)
+		if (fgets(headbuf, sizeof headbuf, stdin) == NULL)
 			break;
-		if (linbuf[0] == '.') {
-			if (linbuf[1] == 'S' && linbuf[2] == 'H')
+		if (headbuf[0] == '.') {
+			if (headbuf[1] == 'S' && headbuf[2] == 'H')
 				break;
-			if (linbuf[1] == 's' && linbuf[2] == 'h')
+			if (headbuf[1] == 's' && headbuf[2] == 'h')
 				break;
-		}
-		trimln(linbuf);
-		if (intro) {
-			split(linbuf, name);
-			continue;
 		}
 		if (i != 0)
-			printf(" ");
+			strcat(linbuf, " ");
 		i++;
-		printf("%s", linbuf);
+		trimln(headbuf);
+		strcat(linbuf, headbuf);
 	}
-	printf("\n");
+	if (intro)
+		split(linbuf, name);
+	else
+		printf("%s\n", linbuf);
+	return;
+
+newman:
+	for (;;) {
+		if (fgets(linbuf, sizeof linbuf, stdin) == NULL)
+			return;
+		if (linbuf[0] != '.')
+			continue;
+		if (linbuf[1] == 'S' && linbuf[2] == 'h')
+			break;
+	}
+	trimln(headbuf);
+	if (tocrc)
+		doname(name);
+	if (!tocrc && !intro)
+		printf(".TH%s\t", &headbuf[3]);
+	linbuf[0] = '\0';
+	for (;;) {
+		if (fgets(headbuf, sizeof headbuf, stdin) == NULL)
+			break;
+		if (headbuf[0] == '.') {
+			if (headbuf[1] == 'S' && headbuf[2] == 'h')
+				break;
+		}
+		if (i != 0)
+			strcat(linbuf, " ");
+		i++;
+		trimln(headbuf);
+		for (loc = headbuf; loc; loc = strchr(loc, ' '))
+			if (loc[1] == ',')
+				strcpy(loc, &loc[1]);
+			else
+				loc++;
+		if (headbuf[0] != '.') {
+			strcat(linbuf, headbuf);
+		} else {
+			if (headbuf[1] == 'N' && headbuf[2] == 'd')
+				strcat(linbuf, "\\- ");
+			strcat(linbuf, &headbuf[4]);
+		}
+	}
+	if (intro)
+		split(linbuf, name);
+	else
+		printf("%s\n", linbuf);
 }
 
 void
