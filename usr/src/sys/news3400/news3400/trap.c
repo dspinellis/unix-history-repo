f62121528dca3c17926dbdaa0a76020c0ba88298
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-16 23:48:38 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: sys/news3400/news3400/trap.c 8.1

--- usr/src/sys/news3400/news3400/trap.c
+++ usr/src/sys/news3400/news3400/trap.c
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 1988 University of Utah.
- * Copyright (c) 1992 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * the Systems Programming Group of the University of Utah Computer
@@ -12,7 +12,7 @@
  *
  * from: Utah $Hdr: trap.c 1.32 91/04/06$
  *
- *	@(#)trap.c	7.11 (Berkeley) %G%
+ *	@(#)trap.c	8.1 (Berkeley) %G%
  */
 
 #include <sys/param.h>
