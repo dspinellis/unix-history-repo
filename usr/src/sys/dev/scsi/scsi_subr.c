b1e958588cb667488fd9704717ef5d56ffe5191b
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-16 21:48:11 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: sys/dev/scsi/scsi_subr.c 8.1
    SCCS-vsn: share/man/man7/mailaddr.7 8.1
    SCCS-vsn: usr.sbin/sendmail/src/aliases.5 8.1
    SCCS-vsn: usr.sbin/sendmail/src/sendmail.8 8.1

--- usr/src/sys/dev/scsi/scsi_subr.c
+++ usr/src/sys/dev/scsi/scsi_subr.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1992 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This software was developed by the Computer Systems Engineering group
  * at Lawrence Berkeley Laboratory under DARPA contract BG 91-66 and
@@ -13,7 +13,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)scsi_subr.c	5.5 (Berkeley) %G%
+ *	@(#)scsi_subr.c	8.1 (Berkeley) %G%
  *
  * from: $Header: scsi_subr.c,v 1.10 93/02/01 19:21:58 torek Exp $ (LBL)
  */
