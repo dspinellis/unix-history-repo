18a2a8f018f710fdc940cf4cce2f654aa0e0f9b7
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-17 14:17:35 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: lib/libc/sys/ftruncate.c 8.1
    SCCS-vsn: lib/libc/sys/lseek.c 8.1
    SCCS-vsn: lib/libc/sys/mmap.c 8.1
    SCCS-vsn: lib/libc/sys/truncate.c 8.1

--- usr/src/lib/libc/sys/ftruncate.c
+++ usr/src/lib/libc/sys/ftruncate.c
@@ -1,12 +1,12 @@
 /*
- * Copyright (c) 1992 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)ftruncate.c	5.3 (Berkeley) %G%";
+static char sccsid[] = "@(#)ftruncate.c	8.1 (Berkeley) %G%";
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/types.h>
