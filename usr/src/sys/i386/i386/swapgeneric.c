2380a25c37fa7c65b61603d85446e64039da5642
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-17 02:30:15 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: sys/i386/i386/swapgeneric.c 8.1

--- usr/src/sys/i386/i386/swapgeneric.c
+++ usr/src/sys/i386/i386/swapgeneric.c
@@ -1,13 +1,13 @@
 /*-
- * Copyright (c) 1990 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * William Jolitz.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)swapgeneric.c	5.7 (Berkeley) %G%
+ *	@(#)swapgeneric.c	8.1 (Berkeley) %G%
  */
 
 #include <machine/pte.h>
