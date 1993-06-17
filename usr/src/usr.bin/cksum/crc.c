44a90bb0b822d4c17c6a8741e155dadc0050c31d
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-17 13:25:13 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: usr.bin/cksum/crc.c 8.1

--- usr/src/usr.bin/cksum/crc.c
+++ usr/src/usr.bin/cksum/crc.c
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1991 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1991, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * James W. Williams of NASA Goddard Space Flight Center.
@@ -9,7 +9,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)crc.c	5.6 (Berkeley) %G%";
+static char sccsid[] = "@(#)crc.c	8.1 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/types.h>
