6eaa07b68f6389cc8abfccd852d1986029042383
Nate Willams <nate@FreeBSD.org>
1993-06-29 02:59:34 +0000

    Added Bob Wilcox's fixes to ls (from NetBSD which are from the News)
    (Net/2 version will not fly, since it requires alot of new includes files
    that are still not released)

--- bin/ls/print.c
+++ bin/ls/print.c
@@ -36,6 +36,7 @@
 
 #ifndef lint
 static char sccsid[] = "@(#)print.c	5.24 (Berkeley) 10/19/90";
+static char rcsid[] = "$Header: /b/source/CVS/src/bin/ls/print.c,v 1.3 1993/03/23 00:26:10 cgd Exp $";
 #endif /* not lint */
 
 #include <sys/param.h>
