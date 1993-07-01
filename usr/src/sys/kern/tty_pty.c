bdef913d0d5977219eba13d6b89e1788bab7674b
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-06-30 22:13:01 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: sys/kern/tty_pty.c 8.1

--- usr/src/sys/kern/tty_pty.c
+++ usr/src/sys/kern/tty_pty.c
@@ -1,10 +1,10 @@
 /*
- * Copyright (c) 1982, 1986, 1989 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1982, 1986, 1989, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)tty_pty.c	7.30 (Berkeley) %G%
+ *	@(#)tty_pty.c	8.1 (Berkeley) %G%
  */
 
 /*
