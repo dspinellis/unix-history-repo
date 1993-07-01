228fe7df88bcd5353b970a06797e9d7b917af33a
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1993-06-30 22:12:19 -0800

    missing ptcwakeup needed for line-mode telnet
    SCCS-vsn: sys/kern/tty_pty.c 7.30

--- usr/src/sys/kern/tty_pty.c
+++ usr/src/sys/kern/tty_pty.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)tty_pty.c	8.1 (Berkeley) %G%
+ *	@(#)tty_pty.c	7.30 (Berkeley) %G%
  */
 
 /*
@@ -533,14 +533,14 @@ ptyioctl(dev, cmd, data, flag, p)
 		if (*(int *)data) {
 			if (pti->pt_flags & PF_PKT) {
 				pti->pt_send |= TIOCPKT_IOCTL;
-				ptcwakeup(tp);
+				ptcwakeup(tp, FREAD);
 			}
 			tp->t_lflag |= EXTPROC;
 		} else {
 			if ((tp->t_state & EXTPROC) &&
 			    (pti->pt_flags & PF_PKT)) {
 				pti->pt_send |= TIOCPKT_IOCTL;
-				ptcwakeup(tp);
+				ptcwakeup(tp, FREAD);
 			}
 			tp->t_lflag &= ~EXTPROC;
 		}
@@ -639,6 +639,7 @@ ptyioctl(dev, cmd, data, flag, p)
 		case TIOCLSET:
 #endif
 			pti->pt_send |= TIOCPKT_IOCTL;
+			ptcwakeup(tp, FREAD);
 		default:
 			break;
 		}
