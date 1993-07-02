cd627f6f06fcda7aa5e855f7f47c00e805f3a904
CSRG <csrg@ucbvax.Berkeley.EDU>
1993-07-02 12:54:42 -0800

    BSD 4_4_Lite1 development Work on file usr/src/contrib/emacs-18.57/src/m-sparcbsd.h
    Synthesized-from: CSRG/cd2/4.4BSD-Lite1

---
+++ usr/src/contrib/emacs-18.57/src/m-sparcbsd.h
@@ -0,0 +1,109 @@
+/* m- file for Sun 4 SPARC.
+   Copyright (C) 1987 Free Software Foundation, Inc.
+
+This file is part of GNU Emacs.
+
+GNU Emacs is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 1, or (at your option)
+any later version.
+
+GNU Emacs is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GNU Emacs; see the file COPYING.  If not, write to
+the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
+
+
+/* The following three symbols give information on
+ the size of various data types.  */
+
+#define SHORTBITS 16		/* Number of bits in a short */
+
+#define INTBITS 32		/* Number of bits in an int */
+
+#define LONGBITS 32		/* Number of bits in a long */
+
+/* SPARC has lowest-numbered byte as most significant */
+
+#define BIG_ENDIAN 4321
+
+/* Define NO_ARG_ARRAY if you cannot take the address of the first of a
+ * group of arguments and treat it as an array of the arguments.  */
+
+#define NO_ARG_ARRAY
+
+/* Define how to take a char and sign-extend into an int.
+   On machines where char is signed, this is a no-op.  */
+
+#define SIGN_EXTEND_CHAR(c) (c)
+
+/* Say this machine is a sparc */
+
+#ifndef sparc
+#define sparc
+#endif
+
+/* This level of optimization is reported to work.  */
+
+#ifdef __GNUC__
+# define C_OPTIMIZE_SWITCH -O
+#else
+     /* This level of optimization is reported to work.  */
+# define C_OPTIMIZE_SWITCH -O2
+#endif
+
+/* Use type int rather than a union, to represent Lisp_Object */
+
+#define NO_UNION_TYPE
+
+/* XINT must explicitly sign-extend */
+
+#define EXPLICIT_SIGN_EXTEND
+
+/* Data type of load average, as read out of kmem.  */
+
+#define LOAD_AVE_TYPE double
+
+/* Convert that into an integer that is 100 for a load average of 1.0  */
+
+#define LOAD_AVE_CVT(x) ((int) ((x) * 100.0))
+
+/* Define C_ALLOCA if this machine does not support a true alloca
+   and the one written in C should be used instead.
+   Define HAVE_ALLOCA to say that the system provides a properly
+   working alloca function and it should be used.
+   Define neither one if an assembler-language alloca
+   in the file alloca.s should be used.  */
+
+#define HAVE_ALLOCA
+#define alloca __builtin_alloca
+
+/* Must use the system's termcap.  It does special things.  */
+
+#define LIBS_TERMCAP -ltermcap
+
+/* Mask for address bits within a memory segment */
+
+#define SEGMENT_MASK (__LDPGSZ - 1) /* loader pages are 8k for sun4/4c compat */
+
+/* Arrange to link with sun windows, if requested.  */
+/* For details on emacstool and sunfns, see etc/SUN-SUPPORT */
+/* These programs require Sun UNIX 4.2 Release 3.2 or greater */
+
+#ifdef HAVE_SUN_WINDOWS
+#define OTHER_FILES  ${etcdir}emacstool
+#define LIBS_MACHINE -lsuntool -lsunwindow -lpixrect
+#define OBJECTS_MACHINE sunfns.o
+#define SYMS_MACHINE syms_of_sunfns ()
+#endif
+#define PURESIZE 180000
+
+/* Say that the text segment of a.out includes the header;
+   the header actually occupies the first few bytes of the text segment
+   and is counted in hdr.a_text.  */
+
+#define A_TEXT_OFFSET(HDR) sizeof (HDR)
