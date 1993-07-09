1ce703724af15fc565242ea18c731a8d2a3429a2
Mike Hibler <hibler@ucbvax.Berkeley.EDU>
1993-07-08 17:54:57 -0800

    hack to get around bad framebuffer sizes from HW
    SCCS-vsn: sys/hp300/dev/ite_dv.c 7.9
    SCCS-vsn: sys/hp300/dev/ite_rb.c 7.9
    SCCS-vsn: sys/hp300/stand/ite.c 7.6

--- usr/src/sys/hp300/dev/ite_dv.c
+++ usr/src/sys/hp300/dev/ite_dv.c
@@ -9,9 +9,9 @@
  *
  * %sccs.include.redist.c%
  *
- * from: Utah $Hdr: ite_dv.c 1.9 92/12/20$
+ * from: Utah $Hdr: ite_dv.c 1.10 93/06/25$
  *
- *	@(#)ite_dv.c	8.1 (Berkeley) %G%
+ *	@(#)ite_dv.c	7.9 (Berkeley) %G%
  */
 
 #include "ite.h"
@@ -52,6 +52,17 @@ dvbox_init(ip)
 		ip->fbheight = gp->g_display.gd_fbheight;
 		ip->dwidth = gp->g_display.gd_dwidth;
 		ip->dheight = gp->g_display.gd_dheight;
+		/*
+		 * XXX some displays (e.g. the davinci) appear
+		 * to return a display height greater than the
+		 * returned FB height.  Guess we should go back
+		 * to getting the display dimensions from the
+		 * fontrom...
+		 */
+		if (ip->dwidth > ip->fbwidth)
+			ip->dwidth = ip->fbwidth;
+		if (ip->dheight > ip->fbheight)
+			ip->dheight = ip->fbheight;
 	}
 
 	dv_reset(ip->regbase);
