b8238394a4c76d30de8fbed036ebfb0cb751cf01
Paul Richards <paul@isl.cf.ac.uk>
1993-07-09 02:55:37 +0000

    Removed tahoe copy of cksum.i386.c and linked /usr/src/sys.386bsd/i386/i386/in_cksum.c to cksum.i386.c

--- usr.sbin/timed/timed/cksum.i386.c
+++ usr.sbin/timed/timed/cksum.i386.c
@@ -1,5 +1,5 @@
-/*
- * Copyright (c) 1986 Regents of the University of California.
+/*-
+ * Copyright (c) 1990 The Regents of the University of California.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -29,13 +29,22 @@
  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
+ *
+ * from tahoe:	in_cksum.c	1.2	86/01/05
+ *	@(#)in_cksum.c	1.3 (Berkeley) 1/19/91
+ *
+ * PATCHES MAGIC                LEVEL   PATCH THAT GOT US HERE
+ * --------------------         -----   ----------------------
+ * CURRENT PATCH LEVEL:         1       00104
+ * --------------------         -----   ----------------------
+ *
+ * 24 Jul 92	Bakul Shah		Optimized some more
+ *
+ * 920724 	i386 changes by Bakul Shah <bvs@bitblocks.com>
  */
 
-#ifndef lint
-static char sccsid[] = "@(#)cksum.tahoe.c	2.4 (Berkeley) 6/1/90";
-#endif /* not lint */
-
-#include <sys/types.h>
+#include "param.h"
+#include "sys/mbuf.h"
 
 /*
  * Checksum routine for Internet Protocol family headers.
@@ -43,54 +52,130 @@ static char sccsid[] = "@(#)cksum.tahoe.c	2.4 (Berkeley) 6/1/90";
  * This routine is very heavily used in the network
  * code and should be modified for each CPU to be as fast as possible.
  * 
- * This implementation is TAHOE version.
+ * This implementation is 386 version.
  */
 
 #undef	ADDCARRY
-#define ADDCARRY(sum) { \
-	if (sum & 0xffff0000) {	\
-		sum &= 0xffff; \
-		sum++; \
-	} \
-}
+#define ADDCARRY(x)     if ((x) > 0xffff) (x) -= 0xffff
+#define REDUCE          {sum = (sum & 0xffff) + (sum >> 16); ADDCARRY(sum);}
 
-in_cksum(addr, len)
-	register u_short *addr;
+/*
+ * Thanks to gcc we don't have to guess
+ * which registers contain sum & w.
+ */
+#define CLC     asm("clc")
+#define ADD(n)  asm("adcl " #n "(%2), %0": "=r"(sum): "0"(sum), "r"(w))
+#define MOP     asm("adcl $0, %0":         "=r"(sum): "0"(sum))
+
+in_cksum(m, len)
+	register struct mbuf *m;
 	register int len;
 {
-	union word {
-		char	c[2];
-		u_short	s;
-	} u;
-	register int sum = 0;
+	register u_short *w;
+	register unsigned sum = 0;
+	register int mlen = 0;
+	int byte_swapped = 0;
+	union { char	c[2]; u_short	s; } su;
 
-	while (len > 0) {
-		/*
-		 * add by words.
-		 */
-		while ((len -= 2) >= 0) {
-			if ((int)addr & 0x1) {
-				/* word is not aligned */
-				u.c[0] = *(char *)addr;
-				u.c[1] = *((char *)addr+1);
-				sum += u.s;
-				addr++;
-			} else
-				sum += *addr++;
-			ADDCARRY(sum);
-		}
-		if (len == -1)
+	for (;m && len; m = m->m_next) {
+		if (m->m_len == 0)
+			continue;
+		w = mtod(m, u_short *);
+		if (mlen == -1) {
 			/*
-			 * Odd number of bytes. 
+			 * The first byte of this mbuf is the continuation
+			 * of a word spanning between this mbuf and the
+			 * last mbuf.
 			 */
-			u.c[0] = *(u_char *)addr;
+
+			/* su.c[0] is already saved when scanning previous 
+			 * mbuf.  sum was REDUCEd when we found mlen == -1
+			 */
+			su.c[1] = *(u_char *)w;
+			sum += su.s;
+			w = (u_short *)((char *)w + 1);
+			mlen = m->m_len - 1;
+			len--;
+		} else
+			mlen = m->m_len;
+		if (len < mlen)
+			mlen = len;
+		len -= mlen;
+		/*
+		 * Force to long boundary so we do longword aligned
+		 * memory operations
+		 */
+		if (3 & (int) w) {
+			REDUCE;
+			if ((1 & (int) w) && (mlen > 0)) {
+				sum <<= 8;
+				su.c[0] = *(char *)w;
+				w = (u_short *)((char *)w + 1);
+				mlen--;
+				byte_swapped = 1;
+			}
+			if ((2 & (int) w) && (mlen >= 2)) {
+				sum += *w++;
+				mlen -= 2;
+			}
+		}
+		/*
+		 * Do as much of the checksum as possible 32 bits at at time.
+		 * In fact, this loop is unrolled to make overhead from
+		 * branches &c small.
+		 */
+		while ((mlen -= 32) >= 0) {
+			/*
+			 * Clear the carry flag, add with carry 16 words
+			 * and fold-in last carry by adding a 0 with carry.
+			 */
+			CLC;
+			ADD(0);  ADD(4);  ADD(8);  ADD(12);
+			ADD(16); ADD(20); ADD(24); ADD(28);
+			MOP; w += 16;
+		}
+		mlen += 32;
+		while ((mlen -= 8) >= 0) {
+			CLC;
+			ADD(0); ADD(4);
+			MOP;
+			w += 4;
+		}
+		mlen += 8;
+		if (mlen == 0 && byte_swapped == 0)
+			continue;       /* worth 1% maybe ?? */
+		REDUCE;
+		while ((mlen -= 2) >= 0) {
+			sum += *w++;
+		}
+		if (byte_swapped) {
+			sum <<= 8;
+			byte_swapped = 0;
+			if (mlen == -1) {
+				su.c[1] = *(char *)w;
+				sum += su.s;
+				mlen = 0;
+			} else
+				mlen = -1;
+		} else if (mlen == -1)
+			/*
+			 * This mbuf has odd number of bytes.
+			 * There could be a word split betwen
+			 * this mbuf and the next mbuf.
+			 * Save the last byte (to prepend to next mbuf).
+			 */
+			su.c[0] = *(char *)w;
 	}
-	if (len == -1) {
+
+	if (len)
+		printf("cksum: out of data\n");
+	if (mlen == -1) {
 		/* The last mbuf has odd # of bytes. Follow the
 		   standard (the odd byte is shifted left by 8 bits) */
-		u.c[1] = 0;
-		sum += u.s;
-		ADDCARRY(sum);
+		su.c[1] = 0;
+		sum += su.s;
 	}
+	REDUCE;
 	return (~sum & 0xffff);
 }
+
