2522081183d7772cc3127316f827dff3f62d500a
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-07-08 18:32:54 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: sys/hp300/dev/ite_rb.c 8.1
    SCCS-vsn: sys/hp300/dev/ite_dv.c 8.1
    SCCS-vsn: sys/hp300/stand/ite.c 8.1
    SCCS-vsn: sys/hp/hpux/hpux.h 8.1
    SCCS-vsn: sys/hp/hpux/hpux_compat.c 8.1
    SCCS-vsn: sys/hp/hpux/hpux_tty.c 8.1

--- usr/src/sys/hp/hpux/hpux.h
+++ usr/src/sys/hp/hpux/hpux.h
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 1988 University of Utah.
- * Copyright (c) 1990 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * the Systems Programming Group of the University of Utah Computer
@@ -11,7 +11,7 @@
  *
  * from: Utah $Hdr: hpux.h 1.30 93/06/28$
  *
- *	@(#)hpux.h	7.9 (Berkeley) %G%
+ *	@(#)hpux.h	8.1 (Berkeley) %G%
  */
 
 #include <hp/hpux/hpux_exec.h>
