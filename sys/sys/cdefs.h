9fdaece63f0b23850ff91db236e4f4a39b92c771
Paul Richards <paul@isl.cf.ac.uk>
1993-07-10 14:54:26 +0000

    Removed gcc hacks from cdefs.h Send any flames to me not the list :-)

--- sys/sys/cdefs.h
+++ sys/sys/cdefs.h
@@ -61,34 +61,5 @@
 #define	__CONCAT(x,y)	x/**/y
 #define	__STRING(x)	"x"
 
-#ifdef __GNUC__
-#define	const		__const		/* GCC: ANSI C with -traditional */
-#define	inline		__inline
-#define	signed		__signed
-#define	volatile	__volatile
-
-#else	/* !__GNUC__ */
-#define	const				/* delete ANSI C keywords */
-#define	inline
-#define	signed
-#define	volatile
-#endif	/* !__GNUC__ */
 #endif	/* !(__STDC__ || __cplusplus) */
-
-/*
- * GCC has extensions for declaring functions as `pure' (always returns
- * the same value given the same inputs, i.e., has no external state and
- * no side effects) and `dead' (nonreturning).  These mainly affect
- * optimization and warnings.  Unfortunately, GCC complains if these are
- * used under strict ANSI mode (`gcc -ansi -pedantic'), hence we need to
- * define them only if compiling without this.
- */
-#if defined(__GNUC__) && !defined(__STRICT_ANSI__)
-#define __dead __volatile
-#define __pure __const
-#else
-#define __dead
-#define __pure
-#endif
-
 #endif /* !_CDEFS_H_ */
