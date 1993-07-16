8f985aa80632e4e8cfb971cd3a0e917429d3f25c
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-07-16 00:00:01 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: sys/vm/vm_glue.c 8.1
    SCCS-vsn: sys/vm/vm_swap.c 8.1
    SCCS-vsn: sys/hp300/stand/ct.c 8.1
    SCCS-vsn: sys/hp300/stand/rd.c 8.1
    SCCS-vsn: sys/hp300/stand/mkboot.c 8.1

--- usr/src/sys/vm/vm_swap.c
+++ usr/src/sys/vm/vm_swap.c
@@ -1,10 +1,10 @@
 /*
- * Copyright (c) 1982, 1986, 1989 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1982, 1986, 1989, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)vm_swap.c	7.30 (Berkeley) %G%
+ *	@(#)vm_swap.c	8.1 (Berkeley) %G%
  */
 
 #include <sys/param.h>
