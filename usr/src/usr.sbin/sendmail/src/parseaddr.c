c49e060af035f7d2f3197010f9419d2239c384d5
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-07-17 20:48:18 -0800

    allow 6xx codes to mean "warnings sent to postmaster"
    SCCS-vsn: usr.sbin/sendmail/src/sendmail.h 8.5
    SCCS-vsn: usr.sbin/sendmail/src/parseaddr.c 8.5
    SCCS-vsn: usr.sbin/sendmail/src/err.c 8.4

--- usr/src/usr.sbin/sendmail/src/parseaddr.c
+++ usr/src/usr.sbin/sendmail/src/parseaddr.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)parseaddr.c	8.4 (Berkeley) %G%";
+static char sccsid[] = "@(#)parseaddr.c	8.5 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -368,18 +368,18 @@ prescan(addr, delim, pvpbuf, delimptr)
 				/* diagnose and patch up bad syntax */
 				if (state == QST)
 				{
-					usrerr("553 Unbalanced '\"'");
+					usrerr("653 Unbalanced '\"'");
 					c = '"';
 				}
 				else if (cmntcnt > 0)
 				{
-					usrerr("553 Unbalanced '('");
+					usrerr("653 Unbalanced '('");
 					c = ')';
 				}
 				else if (anglecnt > 0)
 				{
 					c = '>';
-					usrerr("553 Unbalanced '<'");
+					usrerr("653 Unbalanced '<'");
 				}
 				else
 					break;
@@ -421,7 +421,7 @@ prescan(addr, delim, pvpbuf, delimptr)
 			{
 				if (cmntcnt <= 0)
 				{
-					usrerr("553 Unbalanced ')'");
+					usrerr("653 Unbalanced ')'");
 					c = NOCHAR;
 				}
 				else
@@ -435,7 +435,7 @@ prescan(addr, delim, pvpbuf, delimptr)
 			{
 				if (anglecnt <= 0)
 				{
-					usrerr("553 Unbalanced '>'");
+					usrerr("653 Unbalanced '>'");
 					c = NOCHAR;
 				}
 				else
