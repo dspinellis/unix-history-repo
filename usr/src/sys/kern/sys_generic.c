5340cf9ba347cd9cf566d2a64c658b0e95a21b60
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-07-19 07:17:37 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: sys/kern/sys_generic.c 8.1

--- usr/src/sys/kern/sys_generic.c
+++ usr/src/sys/kern/sys_generic.c
@@ -1,10 +1,10 @@
 /*
- * Copyright (c) 1982, 1986, 1989 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1982, 1986, 1989, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)sys_generic.c	7.39 (Berkeley) %G%
+ *	@(#)sys_generic.c	8.1 (Berkeley) %G%
  */
 
 #include <sys/param.h>
