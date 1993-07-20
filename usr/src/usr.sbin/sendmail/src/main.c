7c4e91807d17a168a43295675a219fa731d547d7
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-07-19 23:30:45 -0800

    include M flag on Ultrix for DECnet compatibility
    SCCS-vsn: usr.sbin/sendmail/src/main.c 8.7

--- usr/src/usr.sbin/sendmail/src/main.c
+++ usr/src/usr.sbin/sendmail/src/main.c
@@ -13,7 +13,7 @@ static char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)main.c	8.6 (Berkeley) %G%";
+static char sccsid[] = "@(#)main.c	8.7 (Berkeley) %G%";
 #endif /* not lint */
 
 #define	_DEFINE
@@ -236,9 +236,13 @@ main(argc, argv, envp)
 	*/
 	nothaw = FALSE;
 #if defined(__osf__) || defined(_AIX3)
-#define OPTIONS		"B:b:C:cd:e:F:f:h:Iimno:p:q:r:sTtvX:x"
+# define OPTIONS	"B:b:C:cd:e:F:f:h:Iimno:p:q:r:sTtvX:x"
 #else
-#define OPTIONS		"B:b:C:cd:e:F:f:h:Iimno:p:q:r:sTtvX:"
+# if defined(ultrix)
+#  define OPTIONS	"B:b:C:cd:e:F:f:h:IiM:mno:p:q:r:sTtvX:"
+# else
+#  define OPTIONS	"B:b:C:cd:e:F:f:h:Iimno:p:q:r:sTtvX:"
+# endif
 #endif
 	while ((j = getopt(argc, argv, OPTIONS)) != EOF)
 	{
@@ -568,6 +572,9 @@ main(argc, argv, envp)
 			break;
 
 		  case 'e':	/* error message disposition */
+# if defined(ultrix)
+		  case 'M':	/* define macro */
+# endif
 			setoption(j, optarg, FALSE, TRUE, CurEnv);
 			break;
 
