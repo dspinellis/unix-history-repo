f0a6613d87732d106146b784f6d6c7e48a611c7a
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1993-07-19 20:45:15 -0800

    fix prototype for igmp_input (from mccanne@horse.ee.lbl.gov)
    SCCS-vsn: sys/netinet/igmp_var.h 7.3

--- usr/src/sys/netinet/igmp_var.h
+++ usr/src/sys/netinet/igmp_var.h
@@ -8,7 +8,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)igmp_var.h	8.1 (Berkeley) %G%
+ *	@(#)igmp_var.h	7.3 (Berkeley) %G%
  */
 
 /*
@@ -52,7 +52,7 @@ struct igmpstat igmpstat;
 	)
 
 void	igmp_init __P(());
-void	igmp_input __P((struct mbuf *, struct ifnet *));
+void	igmp_input __P((struct mbuf *, int));
 void	igmp_joingroup __P((struct in_multi *));
 void	igmp_leavegroup __P((struct in_multi *));
 void	igmp_fasttimo __P(());
