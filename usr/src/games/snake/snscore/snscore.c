f40afeab185c1cc3d49691c49bc1ac63197f9e29
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-07-19 22:06:09 -0800

    An uninitialized variable could make it print out null scores From: Craig Leres <leres@ee.lbl.gov>
    SCCS-vsn: games/snake/snscore/snscore.c 5.8

--- usr/src/games/snake/snscore/snscore.c
+++ usr/src/games/snake/snscore/snscore.c
@@ -12,7 +12,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)snscore.c	8.1 (Berkeley) %G%";
+static char sccsid[] = "@(#)snscore.c	5.8 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -30,9 +30,9 @@ struct	player	{
 	char	*name;
 } players[MAXPLAYERS], temp;
 
+int
 main()
 {
-	char	buf[80], cp;
 	short	uid, score;
 	FILE	*fd;
 	int	noplayers;
@@ -49,7 +49,8 @@ main()
 	printf("Snake players scores to date\n");
 	fread(&whoallbest, sizeof(short), 1, fd);
 	fread(&allbest, sizeof(short), 1, fd);
-	for (uid=2;;uid++) {
+	noplayers = 0;
+	for (uid = 2; ;uid++) {
 		if(fread(&score, sizeof(short), 1, fd) == 0)
 			break;
 		if (score > 0) {
@@ -63,29 +64,29 @@ main()
 			if (p == NULL)
 				continue;
 			q = p -> pw_name;
-			players[noplayers].name = malloc(strlen(q)+1);
+			players[noplayers].name = malloc(strlen(q) + 1);
 			strcpy(players[noplayers].name, q);
 			noplayers++;
 		}
 	}
 
 	/* bubble sort scores */
-	for (notsorted=1; notsorted; ) {
+	for (notsorted = 1; notsorted; ) {
 		notsorted = 0;
-		for (i=0; i<noplayers-1; i++)
-			if (players[i].scores < players[i+1].scores) {
+		for (i = 0; i < noplayers - 1; i++)
+			if (players[i].scores < players[i + 1].scores) {
 				temp = players[i];
-				players[i] = players[i+1];
-				players[i+1] = temp;
+				players[i] = players[i + 1];
+				players[i + 1] = temp;
 				notsorted++;
 			}
 	}
 
 	j = 1;
-	for (i=0; i<noplayers; i++) {
+	for (i = 0; i < noplayers; i++) {
 		printf("%d:\t$%d\t%s\n", j, players[i].scores, players[i].name);
-		if (players[i].scores > players[i+1].scores)
-			j = i+2;
+		if (players[i].scores > players[i + 1].scores)
+			j = i + 2;
 	}
 	exit(0);
 }
