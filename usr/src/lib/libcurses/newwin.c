e437b5bb39b456282f5000689bdc93ea317223f4
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-07-20 14:24:02 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: lib/libcurses/newwin.c 8.1
    SCCS-vsn: lib/libcurses/refresh.c 8.1

--- usr/src/lib/libcurses/newwin.c
+++ usr/src/lib/libcurses/newwin.c
@@ -1,12 +1,12 @@
 /*
- * Copyright (c) 1981 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1981, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)newwin.c	5.19 (Berkeley) %G%";
+static char sccsid[] = "@(#)newwin.c	8.1 (Berkeley) %G%";
 #endif	/* not lint */
 
 #include <curses.h>
