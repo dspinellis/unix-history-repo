18759747d3d4273ef8174d6e84ffb3c0db00f266
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-07-20 01:34:30 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: sys/sparc/sparc/cache.c 8.1
    SCCS-vsn: sys/sparc/dev/cons.c 8.1
    SCCS-vsn: sys/sparc/dev/zs.c 8.1

--- usr/src/sys/sparc/dev/zs.c
+++ usr/src/sys/sparc/dev/zs.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1992 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This software was developed by the Computer Systems Engineering group
  * at Lawrence Berkeley Laboratory under DARPA contract BG 91-66 and
@@ -13,7 +13,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)zs.c	7.6 (Berkeley) %G%
+ *	@(#)zs.c	8.1 (Berkeley) %G%
  *
  * from: $Header: zs.c,v 1.30 93/07/19 23:44:42 torek Exp $
  */
--- usr/src/sys/sparc/sparc/cache.c
+++ usr/src/sys/sparc/sparc/cache.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1992 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This software was developed by the Computer Systems Engineering group
  * at Lawrence Berkeley Laboratory under DARPA contract BG 91-66 and
@@ -13,7 +13,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)cache.c	7.7 (Berkeley) %G%
+ *	@(#)cache.c	8.1 (Berkeley) %G%
  *
  * from: $Header: cache.c,v 1.10 93/07/18 06:23:51 torek Exp $ (LBL)
  */
