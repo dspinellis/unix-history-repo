ced607fbbf0f7f764956e64de0513bf5a34b57d1
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1993-07-20 18:35:00 -0800

    fix length adjustment of m_pkthdr.len (from karels@bsdi.com)
    SCCS-vsn: sys/kern/uipc_mbuf.c 7.26

--- usr/src/sys/kern/uipc_mbuf.c
+++ usr/src/sys/kern/uipc_mbuf.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)uipc_mbuf.c	8.1 (Berkeley) %G%
+ *	@(#)uipc_mbuf.c	7.26 (Berkeley) %G%
  */
 
 #include <sys/param.h>
@@ -398,8 +398,8 @@ m_adj(mp, req_len)
 		}
 		if (m->m_len >= len) {
 			m->m_len -= len;
-			if ((mp = m)->m_flags & M_PKTHDR)
-				m->m_pkthdr.len -= len;
+			if (mp->m_flags & M_PKTHDR)
+				mp->m_pkthdr.len -= len;
 			return;
 		}
 		count -= len;
