52e6307710d7321516a014d1174feb78c6d1c188
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-07-21 19:10:01 -0800

    fix botch with EXPN continuation lines
    SCCS-vsn: usr.sbin/sendmail/src/srvrsmtp.c 8.8

--- usr/src/usr.sbin/sendmail/src/srvrsmtp.c
+++ usr/src/usr.sbin/sendmail/src/srvrsmtp.c
@@ -10,9 +10,9 @@
 
 #ifndef lint
 #ifdef SMTP
-static char sccsid[] = "@(#)srvrsmtp.c	8.7 (Berkeley) %G% (with SMTP)";
+static char sccsid[] = "@(#)srvrsmtp.c	8.8 (Berkeley) %G% (with SMTP)";
 #else
-static char sccsid[] = "@(#)srvrsmtp.c	8.7 (Berkeley) %G% (without SMTP)";
+static char sccsid[] = "@(#)srvrsmtp.c	8.8 (Berkeley) %G% (without SMTP)";
 #endif
 #endif /* not lint */
 
@@ -609,6 +609,10 @@ smtp(e)
 			}
 			while (vrfyqueue != NULL)
 			{
+				a = vrfyqueue;
+				while ((a = a->q_next) != NULL &&
+				       bitset(QDONTSEND|QBADADDR, a->q_flags))
+					continue;
 				if (!bitset(QDONTSEND|QBADADDR, vrfyqueue->q_flags))
 					printvrfyaddr(vrfyqueue, a == NULL);
 				vrfyqueue = vrfyqueue->q_next;
