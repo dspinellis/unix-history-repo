29ea0d65d89f6e659afcd4018f322070b24271a0
CSRG <csrg@ucbvax.Berkeley.EDU>
1993-07-21 23:13:05 -0800

    BSD 4_4 development Work on file usr/src/contrib/gzip-1.0.7/_match.s
    Synthesized-from: CSRG/cd3/4.4

---
+++ usr/src/contrib/gzip-1.0.7/_match.s
@@ -0,0 +1,43 @@
+# 1 "./match.S"
+ 
+
+
+
+
+
+
+
+
+ 
+ 
+
+ 
+
+
+# 28 "./match.S"
+
+
+
+
+
+
+# 156 "./match.S"
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+# 374 "./match.S"
+
+ error: this asm version is for 386 or 680x0 only
+
+
