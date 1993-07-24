35a2dc26d484e0424ccf8f6640ab8996f2e26e46
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-07-24 15:05:54 -0800

    prefer HASGETDTABLESIZE over _SC_OPEN_MAX: a disturbingly large number of systems define _SC_OPEN_MAX in the header files but then don't implement the sysconf() syscall.
    SCCS-vsn: usr.sbin/sendmail/src/conf.c 8.11

--- usr/src/usr.sbin/sendmail/src/conf.c
+++ usr/src/usr.sbin/sendmail/src/conf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)conf.c	8.10 (Berkeley) %G%";
+static char sccsid[] = "@(#)conf.c	8.11 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
