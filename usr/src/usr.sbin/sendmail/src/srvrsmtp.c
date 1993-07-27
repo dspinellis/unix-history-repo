7e9e5fbee70c49b028515807e6feeb7cd168cf7e
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-07-26 16:20:20 -0800

    fix problem causing errors during collect to supress 250 message
    SCCS-vsn: usr.sbin/sendmail/src/srvrsmtp.c 8.9

--- usr/src/usr.sbin/sendmail/src/srvrsmtp.c
+++ usr/src/usr.sbin/sendmail/src/srvrsmtp.c
@@ -10,14 +10,13 @@
 
 #ifndef lint
 #ifdef SMTP
-static char sccsid[] = "@(#)srvrsmtp.c	8.8 (Berkeley) %G% (with SMTP)";
+static char sccsid[] = "@(#)srvrsmtp.c	8.9 (Berkeley) %G% (with SMTP)";
 #else
-static char sccsid[] = "@(#)srvrsmtp.c	8.8 (Berkeley) %G% (without SMTP)";
+static char sccsid[] = "@(#)srvrsmtp.c	8.9 (Berkeley) %G% (without SMTP)";
 #endif
 #endif /* not lint */
 
 # include <errno.h>
-# include <signal.h>
 
 # ifdef SMTP
 
@@ -478,8 +477,6 @@ smtp(e)
 
 			/* collect the text of the message */
 			collect(TRUE, doublequeue, e);
-			if (Errors != 0)
-				goto abortmessage;
 
 			/*
 			**  Arrange to send to everyone.
