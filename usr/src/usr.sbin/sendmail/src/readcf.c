83d4d383e866a6469ae62e502ebf4a9249199497
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-08-07 04:17:18 -0800

    expand C lines so that $X can work
    SCCS-vsn: usr.sbin/sendmail/src/readcf.c 8.6

--- usr/src/usr.sbin/sendmail/src/readcf.c
+++ usr/src/usr.sbin/sendmail/src/readcf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)readcf.c	8.5 (Berkeley) %G%";
+static char sccsid[] = "@(#)readcf.c	8.6 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -365,7 +365,8 @@ readcf(cfname)
 
 		  case 'C':		/* word class */
 			/* scan the list of words and set class for all */
-			for (p = &bp[2]; *p != '\0'; )
+			expand(&bp[2], exbuf, &exbuf[sizeof exbuf], e);
+			for (p = exbuf; *p != '\0'; )
 			{
 				register char *wd;
 				char delim;
