8d809d0d40793ba2f41a88122de5506e7c20d468
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-08-07 20:25:35 -0800

    only let command line -oL flag raise (never lower) logging level
    SCCS-vsn: usr.sbin/sendmail/src/readcf.c 8.8

--- usr/src/usr.sbin/sendmail/src/readcf.c
+++ usr/src/usr.sbin/sendmail/src/readcf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)readcf.c	8.7 (Berkeley) %G%";
+static char sccsid[] = "@(#)readcf.c	8.8 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -1187,7 +1187,8 @@ setoption(opt, val, sticky)
 		break;
 
 	  case 'L':		/* log level */
-		LogLevel = atoi(val);
+		if (safe || LogLevel < atoi(val))
+			LogLevel = atoi(val);
 		break;
 
 	  case 'M':		/* define macro */
