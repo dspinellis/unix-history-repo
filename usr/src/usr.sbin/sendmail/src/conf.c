aea003954fe02dcbcccadf26caa291735adef63a
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-08-07 18:58:14 -0800

    work around lockf bug in SunOS
    SCCS-vsn: usr.sbin/sendmail/src/conf.c 8.15

--- usr/src/usr.sbin/sendmail/src/conf.c
+++ usr/src/usr.sbin/sendmail/src/conf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)conf.c	8.14 (Berkeley) %G%";
+static char sccsid[] = "@(#)conf.c	8.15 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
