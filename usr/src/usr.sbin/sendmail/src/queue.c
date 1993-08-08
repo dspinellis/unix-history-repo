47c8e4cd8e5b64b29f08b08298dede2c0e164fb6
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-08-07 18:58:51 -0800

    don't delete apparently bogus queue files so eagerly
    SCCS-vsn: usr.sbin/sendmail/src/queue.c 8.10

--- usr/src/usr.sbin/sendmail/src/queue.c
+++ usr/src/usr.sbin/sendmail/src/queue.c
@@ -10,9 +10,9 @@
 
 #ifndef lint
 #ifdef QUEUE
-static char sccsid[] = "@(#)queue.c	8.9 (Berkeley) %G% (with queueing)";
+static char sccsid[] = "@(#)queue.c	8.10 (Berkeley) %G% (with queueing)";
 #else
-static char sccsid[] = "@(#)queue.c	8.9 (Berkeley) %G% (without queueing)";
+static char sccsid[] = "@(#)queue.c	8.10 (Berkeley) %G% (without queueing)";
 #endif
 #endif /* not lint */
 
@@ -937,7 +937,7 @@ readqf(e, announcefile)
 		return FALSE;
 	}
 
-	if (st.st_uid != geteuid() || (st.st_mode & 07777) != FileMode)
+	if (st.st_uid != geteuid())
 	{
 # ifdef LOG
 		if (LogLevel > 0)
