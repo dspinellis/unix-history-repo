9ea3eb227c5edefb8dace8577452c5947849e6ff
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-08-08 22:21:44 -0800

    avoid null pointer dereference on h->h_value
    SCCS-vsn: usr.sbin/sendmail/src/headers.c 8.9

--- usr/src/usr.sbin/sendmail/src/headers.c
+++ usr/src/usr.sbin/sendmail/src/headers.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)headers.c	8.8 (Berkeley) %G%";
+static char sccsid[] = "@(#)headers.c	8.9 (Berkeley) %G%";
 #endif /* not lint */
 
 # include <errno.h>
@@ -318,8 +318,15 @@ eatheader(e, full)
 	msgid = "<none>";
 	for (h = e->e_header; h != NULL; h = h->h_link)
 	{
+		if (h->h_value == NULL)
+		{
+			if (tTd(32, 1))
+				printf("%s: <NULL>\n", h->h_field);
+			continue;
+		}
+
 		/* do early binding */
-		if (bitset(H_DEFAULT, h->h_flags) && h->h_value != NULL)
+		if (bitset(H_DEFAULT, h->h_flags))
 		{
 			expand(h->h_value, buf, &buf[sizeof buf], e);
 			if (buf[0] != '\0')
@@ -352,8 +359,7 @@ eatheader(e, full)
 		}
 
 		/* save the message-id for logging */
-		if (full && h->h_value != NULL &&
-		    strcasecmp(h->h_field, "message-id") == 0)
+		if (full && strcasecmp(h->h_field, "message-id") == 0)
 		{
 			msgid = h->h_value;
 			while (isascii(*msgid) && isspace(*msgid))
