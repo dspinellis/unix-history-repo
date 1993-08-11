e82258f33e8d1c432f9d98622539eaa1d4d31112
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1993-08-10 23:07:32 -0800

    avoid mbuf leak
    SCCS-vsn: sys/kern/uipc_syscalls.c 8.2

--- usr/src/sys/kern/uipc_syscalls.c
+++ usr/src/sys/kern/uipc_syscalls.c
@@ -3,7 +3,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)uipc_syscalls.c	8.1 (Berkeley) %G%
+ *	@(#)uipc_syscalls.c	8.2 (Berkeley) %G%
  */
 
 #include <sys/param.h>
@@ -1125,11 +1125,11 @@ getpeername1(p, uap, retval)
 	so = (struct socket *)fp->f_data;
 	if ((so->so_state & (SS_ISCONNECTED|SS_ISCONFIRMING)) == 0)
 		return (ENOTCONN);
+	if (error = copyin((caddr_t)uap->alen, (caddr_t)&len, sizeof (len)))
+		return (error);
 	m = m_getclr(M_WAIT, MT_SONAME);
 	if (m == NULL)
 		return (ENOBUFS);
-	if (error = copyin((caddr_t)uap->alen, (caddr_t)&len, sizeof (len)))
-		return (error);
 	if (error = (*so->so_proto->pr_usrreq)(so, PRU_PEERADDR, 0, m, 0))
 		goto bad;
 	if (len > m->m_len)
