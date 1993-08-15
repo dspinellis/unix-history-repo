732c8b73fabb24d2dcbf8e19bcb563d8563ec610
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-08-15 15:39:04 -0800

    predefine $e, $l, $n, $o, and $q
    SCCS-vsn: usr.sbin/sendmail/src/main.c 8.18

--- usr/src/usr.sbin/sendmail/src/main.c
+++ usr/src/usr.sbin/sendmail/src/main.c
@@ -13,7 +13,7 @@ static char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)main.c	8.17 (Berkeley) %G%";
+static char sccsid[] = "@(#)main.c	8.18 (Berkeley) %G%";
 #endif /* not lint */
 
 #define	_DEFINE
@@ -320,7 +320,7 @@ main(argc, argv, envp)
 	if (readconfig)
 	{
 		/* initialize some macros, etc. */
-		initmacros();
+		initmacros(CurEnv);
 
 		/* version */
 		define('v', Version, CurEnv);
@@ -1172,7 +1172,8 @@ struct metamac	MetaMacros[] =
 	'\0'
 };
 
-initmacros()
+initmacros(e)
+	register ENVELOPE *e;
 {
 	register struct metamac *m;
 	char buf[5];
@@ -1182,15 +1183,22 @@ initmacros()
 	{
 		buf[0] = m->metaval;
 		buf[1] = '\0';
-		define(m->metaname, newstr(buf), CurEnv);
+		define(m->metaname, newstr(buf), e);
 	}
 	buf[0] = MATCHREPL;
 	buf[2] = '\0';
 	for (c = '0'; c <= '9'; c++)
 	{
 		buf[1] = c;
-		define(c, newstr(buf), CurEnv);
+		define(c, newstr(buf), e);
 	}
+
+	/* set defaults for some macros sendmail will use later */
+	define('e', "\201j Sendmail \201v ready at \201b", e);
+	define('l', "From \201g  \201d", e);
+	define('n', "MAILER-DAEMON", e);
+	define('o', ".:@[]", e);
+	define('q', "<\201g>", e);
 }
 /*
 **  FREEZE -- freeze BSS & allocated memory
