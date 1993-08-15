673a8fd03a212a2b6a4146ba2378d895f8740f1d
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-08-15 02:14:30 -0800

    add TimeOuts.to_ident to set IDENT protocol timeout
    SCCS-vsn: usr.sbin/sendmail/src/sendmail.h 8.12
    SCCS-vsn: usr.sbin/sendmail/src/daemon.c 8.7
    SCCS-vsn: usr.sbin/sendmail/src/readcf.c 8.9

--- usr/src/usr.sbin/sendmail/src/readcf.c
+++ usr/src/usr.sbin/sendmail/src/readcf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)readcf.c	8.8 (Berkeley) %G%";
+static char sccsid[] = "@(#)readcf.c	8.9 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -1472,6 +1472,7 @@ makemapentry(line)
 **		Initializes the TimeOuts structure
 */
 
+#define SECONDS
 #define MINUTES	* 60
 #define HOUR	* 3600
 
@@ -1494,6 +1495,7 @@ settimeouts(val)
 		TimeOuts.to_quit = (time_t) 2 MINUTES;
 		TimeOuts.to_nextcommand = (time_t) 1 HOUR;
 		TimeOuts.to_miscshort = (time_t) 2 MINUTES;
+		TimeOuts.to_ident = (time_t) 30 SECONDS;
 		return;
 	}
 
@@ -1554,6 +1556,8 @@ settimeouts(val)
 				TimeOuts.to_quit = to;
 			else if (strcasecmp(val, "misc") == 0)
 				TimeOuts.to_miscshort = to;
+			else if (strcasecmp(val, "ident") == 0)
+				TimeOuts.to_ident = to;
 			else
 				syserr("settimeouts: invalid timeout %s", val);
 		}
