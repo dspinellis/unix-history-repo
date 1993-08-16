eb4cf9d8cc917ca7601f9c68793a610683a13385
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1993-08-15 20:17:03 -0800

    final update from Akito Fujita
    SCCS-vsn: sys/luna68k/stand/bmd.c 8.2
    SCCS-vsn: sys/luna68k/stand/init_main.c 8.2

--- usr/src/sys/luna68k/stand/init_main.c
+++ usr/src/sys/luna68k/stand/init_main.c
@@ -8,7 +8,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)init_main.c	8.1 (Berkeley) %G%
+ *	@(#)init_main.c	8.2 (Berkeley) %G%
  */
 
 #include <sys/param.h>
@@ -23,7 +23,7 @@ extern int dipsw1, dipsw2;
 
 extern char default_file[];
 
-#define	VERS_LOCAL	"Phase-30"
+#define	VERS_LOCAL	"Phase-31"
 
 extern int howto;
 extern int devtype;
