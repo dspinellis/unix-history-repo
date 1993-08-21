d724d659377f26491310987569c1797e4bbb033d
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-08-21 14:23:00 -0800

    more NeXT portability changes; use initgroups when reading .forward files
    SCCS-vsn: usr.sbin/sendmail/src/conf.h 8.25
    SCCS-vsn: usr.sbin/sendmail/src/deliver.c 8.15
    SCCS-vsn: usr.sbin/sendmail/src/conf.c 8.23
    SCCS-vsn: usr.sbin/sendmail/src/alias.c 8.10

--- usr/src/usr.sbin/sendmail/src/conf.c
+++ usr/src/usr.sbin/sendmail/src/conf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)conf.c	8.22 (Berkeley) %G%";
+static char sccsid[] = "@(#)conf.c	8.23 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
