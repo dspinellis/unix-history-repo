cca1c28502c3d35146ac38e1de944929d1dfaadc
Andrew Moore <alm@FreeBSD.org>
1993-08-21 06:01:24 +0000

    update to sun-lamp's

--- share/mk/bsd.doc.mk
+++ share/mk/bsd.doc.mk
@@ -15,22 +15,59 @@ TBL?=		tbl
 
 .PATH: ${.CURDIR}
 
+all:	${DOC}.${PRINTER}
+
 .if !target(print)
-print: paper.${PRINTER}
-	lpr -P${PRINTER} paper.${PRINTER}
+print: ${DOC}.${PRINTER}
+	lpr -P${PRINTER} ${DOC}.${PRINTER}
+.endif
+
+.if !target(obj)
+.if defined(NOOBJ)
+obj:
+.else
+obj:
+	@cd ${.CURDIR}; rm -f obj > /dev/null 2>&1 || true; \
+	here=`pwd`; subdir=`echo $$here | sed 's,^/usr/src/,,'`; \
+	if test $$here != $$subdir ; then \
+		dest=/usr/obj/$$subdir ; \
+		echo "$$here -> $$dest"; ln -s $$dest obj; \
+		if test -d /usr/obj -a ! -d $$dest; then \
+			mkdir -p $$dest; \
+		else \
+			true; \
+		fi; \
+	else \
+		true ; \
+		dest=$$here/obj ; \
+		echo "making $$here/obj" ; \
+		if test ! -d obj ; then \
+			mkdir $$here/obj; \
+		fi ; \
+	fi;
+.endif
 .endif
 
 clean cleandir:
-	rm -f paper.* [eE]rrs mklog ${CLEANFILES}
+	rm -f ${DOC}.* [eE]rrs mklog ${CLEANFILES}
 	rm -rf obj
 
 FILES?=	${SRCS}
 install:
-	install ${COPY} -o ${BINOWN} -g ${BINGRP} -m 444 \
-	    Makefile ${FILES} ${EXTRA} ${DESTDIR}${BINDIR}/${DIR}
+	@if [ ! -d "${DESTDIR}${BINDIR}/${DIR}" ]; then \
+                /bin/rm -f ${DESTDIR}${BINDIR}/${DIR}  ; \
+                mkdir -p ${DESTDIR}${BINDIR}/${DIR}  ; \
+                chown root.wheel ${DESTDIR}${BINDIR}/${DIR}  ; \
+                chmod 755 ${DESTDIR}${BINDIR}/${DIR}  ; \
+        else \
+                true ; \
+        fi
+	( cd ${.CURDIR} ; install ${COPY} -o ${BINOWN} -g ${BINGRP} -m 444 \
+	    Makefile ${FILES} ${EXTRA} ${DESTDIR}${BINDIR}/${DIR} )
 
 spell: ${SRCS}
-		spell ${SRCS} | sort | comm -23 - spell.ok > paper.spell
+	(cd ${.CURDIR};  spell ${SRCS} ) | sort | \
+		comm -23 - ${.CURDIR}/spell.ok > ${DOC}.spell
 
 BINDIR?=	/usr/share/doc
 BINGRP?=	bin
