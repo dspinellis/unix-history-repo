2b5c0e14253ae3751ffec9689abb67c440433651
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-08-22 23:52:19 -0800

    add PRIV_RESTRICTQRUN flag
    SCCS-vsn: usr.sbin/sendmail/src/sendmail.h 8.16

--- usr/src/usr.sbin/sendmail/src/sendmail.h
+++ usr/src/usr.sbin/sendmail/src/sendmail.h
@@ -5,7 +5,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)sendmail.h	8.15 (Berkeley) %G%
+ *	@(#)sendmail.h	8.16 (Berkeley) %G%
  */
 
 /*
@@ -15,7 +15,7 @@
 # ifdef _DEFINE
 # define EXTERN
 # ifndef lint
-static char SmailSccsId[] =	"@(#)sendmail.h	8.15		%G%";
+static char SmailSccsId[] =	"@(#)sendmail.h	8.16		%G%";
 # endif
 # else /*  _DEFINE */
 # define EXTERN extern
@@ -682,7 +682,8 @@ EXTERN char	OpMode;		/* operation mode, see below */
 #define PRIV_NOEXPN		00010	/* disallow EXPN command entirely */
 #define PRIV_NOVRFY		00020	/* disallow VRFY command entirely */
 #define PRIV_AUTHWARNINGS	00040	/* flag possible authorization probs */
-#define PRIV_RESTRMAILQ		01000	/* restrict mailq command */
+#define PRIV_RESTRICTMAILQ	01000	/* restrict mailq command */
+#define PRIV_RESTRICTQRUN	02000	/* restrict queue run */
 #define PRIV_GOAWAY		00777	/* don't give no info, anyway, anyhow */
 
 /* struct defining such things */
