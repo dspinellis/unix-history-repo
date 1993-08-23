8d30cc9e629042085e9f78a9433f4af5500f83ef
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-08-22 23:48:36 -0800

    SCO fixes (old bug snuck back in)
    SCCS-vsn: usr.sbin/sendmail/src/conf.c 8.24

--- usr/src/usr.sbin/sendmail/src/conf.c
+++ usr/src/usr.sbin/sendmail/src/conf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)conf.c	8.23 (Berkeley) %G%";
+static char sccsid[] = "@(#)conf.c	8.24 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -1303,7 +1303,7 @@ freespace(dir, bsize)
 #  else
 	struct statfs fs;
 #   define FSBLOCKSIZE	fs.f_bsize
-#   if defined(_SCO_UNIX_)
+#   if defined(_SCO_unix_)
 #    define f_bavail f_bfree
 #   endif
 #  endif
