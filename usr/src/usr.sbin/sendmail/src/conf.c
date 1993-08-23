a5b84bd6d22f546677c9fb26c2503204bcac9fb8
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-08-23 15:53:52 -0800

    freespace portability fixes for IRIX
    SCCS-vsn: usr.sbin/sendmail/src/conf.c 8.27

--- usr/src/usr.sbin/sendmail/src/conf.c
+++ usr/src/usr.sbin/sendmail/src/conf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)conf.c	8.26 (Berkeley) %G%";
+static char sccsid[] = "@(#)conf.c	8.27 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
