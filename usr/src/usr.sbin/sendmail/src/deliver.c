40d277167c6930ac2d9628af2792dbdbfd4242ca
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-08-30 19:35:15 -0800

    fix missing trailing \0 problem
    SCCS-vsn: usr.sbin/sendmail/src/deliver.c 8.18

--- usr/src/usr.sbin/sendmail/src/deliver.c
+++ usr/src/usr.sbin/sendmail/src/deliver.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)deliver.c	8.17 (Berkeley) %G%";
+static char sccsid[] = "@(#)deliver.c	8.18 (Berkeley) %G%";
 #endif /* not lint */
 
 #include "sendmail.h"
