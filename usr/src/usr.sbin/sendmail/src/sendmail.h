18ae7f4c2930f3c4ec5438a15ec250c95f270b75
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-09-02 22:25:04 -0800

    add MF_LOCKED flag
    SCCS-vsn: usr.sbin/sendmail/src/sendmail.h 8.18

--- usr/src/usr.sbin/sendmail/src/sendmail.h
+++ usr/src/usr.sbin/sendmail/src/sendmail.h
@@ -5,7 +5,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)sendmail.h	8.17 (Berkeley) %G%
+ *	@(#)sendmail.h	8.18 (Berkeley) %G%
  */
 
 /*
@@ -15,7 +15,7 @@
 # ifdef _DEFINE
 # define EXTERN
 # ifndef lint
-static char SmailSccsId[] =	"@(#)sendmail.h	8.17		%G%";
+static char SmailSccsId[] =	"@(#)sendmail.h	8.18		%G%";
 # endif
 # else /*  _DEFINE */
 # define EXTERN extern
@@ -520,6 +520,7 @@ MAP
 # define MF_ALIAS	0x0080		/* this is an alias file */
 # define MF_TRY0NULL	0x0100		/* try with no null byte */
 # define MF_TRY1NULL	0x0200		/* try with the null byte */
+# define MF_LOCKED	0x0400		/* this map is currently locked */
 # define MF_IMPL_HASH	0x1000		/* implicit: underlying hash database */
 # define MF_IMPL_NDBM	0x2000		/* implicit: underlying NDBM database */
 
