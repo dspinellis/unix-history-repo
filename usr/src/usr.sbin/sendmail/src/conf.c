e8e597555f27bd003456e9b1ddf285bc3fea6e8b
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-09-03 00:15:08 -0800

    fix syntax problem
    SCCS-vsn: usr.sbin/sendmail/src/conf.c 8.33

--- usr/src/usr.sbin/sendmail/src/conf.c
+++ usr/src/usr.sbin/sendmail/src/conf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)conf.c	8.32 (Berkeley) %G%";
+static char sccsid[] = "@(#)conf.c	8.33 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -1606,8 +1606,8 @@ lockfile(fd, filename, ext, type)
 #  endif
 		syserr("cannot flock(%s%s, fd=%d, type=%o, omode=%o, euid=%d)",
 			filename, ext, fd, type, omode, geteuid());
-# endif
 	}
+# endif
 	if (tTd(55, 60))
 		printf("FAIL\n");
 	return FALSE;
