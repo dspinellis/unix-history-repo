0a045dae7a3de97919f8ad54ebd54a6a9b5a7fef
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-09-05 01:02:23 -0800

    allow null defaults in $( ... $) lookups
    SCCS-vsn: usr.sbin/sendmail/src/parseaddr.c 8.11

--- usr/src/usr.sbin/sendmail/src/parseaddr.c
+++ usr/src/usr.sbin/sendmail/src/parseaddr.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)parseaddr.c	8.10 (Berkeley) %G%";
+static char sccsid[] = "@(#)parseaddr.c	8.11 (Berkeley) %G%";
 #endif /* not lint */
 
 #include "sendmail.h"
@@ -1271,6 +1271,7 @@ backup:
 			char *pvpb1[MAXATOM + 1];
 			char *argvect[10];
 			char pvpbuf[PSBUFSIZE];
+			char *nullpvp[1];
 			bool match, defaultpart;
 			char begintype;
 			char db = '\0';
