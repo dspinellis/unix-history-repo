5dd2f53bb5d3dbd140afcf46a4396355d6355161
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-09-07 18:49:28 -0800

    if DB_LOCK not set, pin page across calls and don't copy key/data pair
    SCCS-vsn: lib/libc/db/btree/bt_put.c 8.2
    SCCS-vsn: lib/libc/db/btree/bt_seq.c 8.2
    SCCS-vsn: lib/libc/db/btree/bt_get.c 8.2

--- usr/src/lib/libc/db/btree/bt_put.c
+++ usr/src/lib/libc/db/btree/bt_put.c
@@ -9,7 +9,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)bt_put.c	8.1 (Berkeley) %G%";
+static char sccsid[] = "@(#)bt_put.c	8.2 (Berkeley) %G%";
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/types.h>
@@ -55,6 +55,12 @@ __bt_put(dbp, key, data, flags)
 	char *dest, db[NOVFLSIZE], kb[NOVFLSIZE];
 
 	t = dbp->internal;
+
+	/* Toss any page pinned across calls. */
+	if (t->bt_pinned != NULL) {
+		mpool_put(t->bt_mp, t->bt_pinned, 0);
+		t->bt_pinned = NULL;
+	}
 
 	switch (flags) {
 	case R_CURSOR:
