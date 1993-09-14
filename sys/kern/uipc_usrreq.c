62e41d2370e31f7b99abd3dc8b2b324ccbb1de85
Rod Grimes <rgrimes@FreeBSD.org>
1993-09-14 04:26:40 +0000

    >From: =?ISO-2022-JP?B?GyRCQT5FRCEhRS9HNxsoQg==?= > - Noriyuki Soda <soda@sra.co.jp> Description:         recvmsg(2) always returns -1 with errno==EMSGSIZE, when trying         to pass file descriptors through UNIX domain socket.
            For example, following program can be used to test:
            ftp.uu.net:~ftp/published/books/stevens.advprog.tar.Z
            apue/lib.44/{sendfd.c,recvfd.c}
            apue/{open,open.fe,opend,opend.fe}/*

--- sys/kern/uipc_usrreq.c
+++ sys/kern/uipc_usrreq.c
@@ -30,7 +30,8 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)uipc_usrreq.c	7.26 (Berkeley) 6/3/91
+ *	from:	@(#)uipc_usrreq.c	7.26 (Berkeley) 6/3/91
+ *	$Id$
  */
 
 #include "param.h"
@@ -591,7 +592,7 @@ unp_externalize(rights)
 	int newfds = (cm->cmsg_len - sizeof(*cm)) / sizeof (int);
 	int f;
 
-	if (fdavail(p, newfds)) {
+	if (!fdavail(p, newfds)) {
 		for (i = 0; i < newfds; i++) {
 			fp = *rp;
 			unp_discard(fp);
