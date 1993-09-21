190e166ab1715a8e457330915605952916cddc5d
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-09-21 14:14:23 -0800

    changes for 4.4BSD-Lite requested by USL
    SCCS-vsn: sys/sys/proc.h 8.3
    SCCS-vsn: sys/sys/tty.h 8.3
    SCCS-vsn: sys/sys/buf.h 8.2

--- usr/src/sys/sys/tty.h
+++ usr/src/sys/sys/tty.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)tty.h	8.2 (Berkeley) %G%
+ *	@(#)tty.h	8.3 (Berkeley) %G%
  */
 
 #include <sys/termios.h>
@@ -29,43 +29,46 @@ struct clist {
  */
 struct tty {
 	struct	clist t_rawq;		/* Device raw input queue. */
+	long	t_rawcc;		/* Raw input queue statistics. */
 	struct	clist t_canq;		/* Device canonical queue. */
+	long	t_cancc;		/* Canonical queue statistics. */
 	struct	clist t_outq;		/* Device output queue. */
-	void	(*t_oproc)();		/* Device. */
-#ifdef sun4c
-	void	(*t_stop)();		/* Device. */
-#endif
-	int	(*t_param)();		/* Device. */
+	long	t_outcc;		/* Output queue statistics. */
+	char	t_line;			/* Interface to device drivers. */
+	dev_t	t_dev;			/* Device. */
+	int	t_state;		/* Device and driver (TS*) state. */
+	int	t_flags;		/* Tty flags. */
+	struct	pgrp *t_pgrp;		/* Foreground process group. */
+	struct	session *t_session;	/* Enclosing session. */
 	struct	selinfo t_rsel;		/* Tty read/oob select. */
 	struct	selinfo t_wsel;		/* Tty write select. */
-	caddr_t	T_LINEP; 		/* XXX */
-	caddr_t	t_addr;			/* XXX */
-	dev_t	t_dev;			/* Device. */
-	int	t_flags;		/* (compat) some of both */
-	int	t_state;		/* Device and driver internal state. */
-	struct	session *t_session;	/* tty */
-	struct	pgrp *t_pgrp;		/* Foreground process group. */
-	char	t_line;			/* Glue. */
-	short	t_col;			/* Tty. */
+	struct	termios t_termios;	/* Termios state. */
+	struct	winsize t_winsize;	/* Window size. */
+					/* Start output. */
+	void	(*t_oproc) __P((struct tty *));
+#ifdef sun4c
+					/* Stop output. */
+	void	(*t_stop) __P((struct tty *, int));
+#endif
+					/* Set hardware state. */
+	int	(*t_param) __P((struct tty *, struct termios *));
+	void	*t_sc;			/* XXX: net/if_sl.c:sl_softc. */
+	short	t_column;		/* Tty output column. */
 	short	t_rocount, t_rocol;	/* Tty. */
 	short	t_hiwat;		/* High water mark. */
 	short	t_lowat;		/* Low water mark. */
-	struct	winsize t_winsize;	/* Window size. */
-	struct	termios t_termios;	/* Termios state. */
-#define	t_iflag		t_termios.c_iflag
-#define	t_oflag		t_termios.c_oflag
-#define	t_cflag		t_termios.c_cflag
-#define	t_lflag		t_termios.c_lflag
-#define	t_min		t_termios.c_min
-#define	t_time		t_termios.c_time
-#define	t_cc		t_termios.c_cc
-#define	t_ispeed	t_termios.c_ispeed
-#define	t_ospeed	t_termios.c_ospeed
-	long	t_cancc;		/* Statistics. */
-	long	t_rawcc;
-	long	t_outcc;
 	short	t_gen;			/* Generation number. */
 };
+
+#define	t_cc		t_termios.c_cc
+#define	t_cflag		t_termios.c_cflag
+#define	t_iflag		t_termios.c_iflag
+#define	t_ispeed	t_termios.c_ispeed
+#define	t_lflag		t_termios.c_lflag
+#define	t_min		t_termios.c_min
+#define	t_oflag		t_termios.c_oflag
+#define	t_ospeed	t_termios.c_ospeed
+#define	t_time		t_termios.c_time
 
 #define	TTIPRI	25			/* Sleep priority for tty reads. */
 #define	TTOPRI	26			/* Sleep priority for tty writes. */
@@ -79,29 +82,28 @@ struct tty {
 #define	TTMINHIWAT	roundup(100, CBSIZE)
 #define	TTMAXLOWAT	256
 #define	TTMINLOWAT	32
-#endif /* KERNEL */
+#endif
 
-/* Internal state bits. */
-#define	TS_TIMEOUT	0x000001	/* Delay timeout in progress. */
-#define	TS_WOPEN	0x000002	/* Waiting for open to complete. */
-#define	TS_ISOPEN	0x000004	/* Indicates the device is open. */
-#define	TS_FLUSH	0x000008	/* Outq has been flushed during DMA. */
-#define	TS_CARR_ON	0x000010	/* Software image of carrier-present. */
-#define	TS_BUSY		0x000020	/* Indicates output is in progress. */
-#define	TS_ASLEEP	0x000040	/* Wakeup when output done. */
-#define	TS_XCLUDE	0x000080	/* Exclusive-use flag against open. */
-#define	TS_TTSTOP	0x000100	/* Output stopped by ctl-s. */
-/* was	TS_HUPCLS	0x000200 	 * Hang up upon last close. */
-#define	TS_TBLOCK	0x000400	/* Tandem queue blocked. */
-#define	TS_ASYNC	0x004000	/* Tty in async i/o mode. */
+/* These flags are kept in t_state. */
+#define	TS_ASLEEP	0x00001		/* Process waiting for tty. */
+#define	TS_ASYNC	0x00002		/* Tty in async I/O mode. */
+#define	TS_BUSY		0x00004		/* Draining output. */
+#define	TS_CARR_ON	0x00008		/* Carrier is present. */
+#define	TS_FLUSH	0x00010		/* Outq has been flushed during DMA. */
+#define	TS_ISOPEN	0x00020		/* Open has completed. */
+#define	TS_TBLOCK	0x00040		/* Further input blocked. */
+#define	TS_TIMEOUT	0x00080		/* Wait for output char processing. */
+#define	TS_TTSTOP	0x00100		/* Output paused. */
+#define	TS_WOPEN	0x00200		/* Open in progress. */
+#define	TS_XCLUDE	0x00400		/* Tty requires exclusivity. */
+
 /* State for intra-line fancy editing work. */
-#define	TS_BKSL		0x010000	/* State for lowercase \ work. */
-#define	TS_ERASE	0x040000	/* Within a \.../ for PRTRUB. */
-#define	TS_LNCH		0x080000	/* Next character is literal. */
-#define	TS_TYPEN	0x100000	/* Retyping suspended input (PENDIN). */
-#define	TS_CNTTB	0x200000	/* Counting tab width, ignore FLUSHO. */
-
-#define	TS_LOCAL	(TS_BKSL|TS_ERASE|TS_LNCH|TS_TYPEN|TS_CNTTB)
+#define	TS_BKSL		0x00800		/* State for lowercase \ work. */
+#define	TS_CNTTB	0x01000		/* Counting tab width, ignore FLUSHO. */
+#define	TS_ERASE	0x02000		/* Within a \.../ for PRTRUB. */
+#define	TS_LNCH		0x04000		/* Next character is literal. */
+#define	TS_TYPEN	0x08000		/* Retyping suspended input (PENDIN). */
+#define	TS_LOCAL	(TS_BKSL | TS_CNTTB | TS_ERASE | TS_LNCH | TS_TYPEN)
 
 /* Character type information. */
 #define	ORDINARY	0
@@ -113,30 +115,30 @@ struct tty {
 #define	RETURN		6
 
 struct speedtab {
-        int sp_speed;			/* Speed. */
-        int sp_code;			/* Code. */
+	int sp_speed;			/* Speed. */
+	int sp_code;			/* Code. */
 };
-
-/* Flags on a character passed to ttyinput. */
-#define	TTY_CHARMASK    0x000000ff      /* Character mask */
-#define	TTY_QUOTE       0x00000100      /* Character quoted */
-#define	TTY_ERRORMASK   0xff000000      /* Error mask */
-#define	TTY_FE          0x01000000      /* Framing error or BREAK condition */
-#define	TTY_PE          0x02000000      /* Parity error */
-
-/* Is tp controlling terminal for p? */
-#define	isctty(p, tp) \
-	((p)->p_session == (tp)->t_session && (p)->p_flag&SCTTY)
-
-/* Is p in background of tp? */
-#define	isbackground(p, tp) \
-	(isctty((p), (tp)) && (p)->p_pgrp != (tp)->t_pgrp)
 
 /* Modem control commands (driver). */
 #define	DMSET		0
 #define	DMBIS		1
 #define	DMBIC		2
 #define	DMGET		3
+
+/* Flags on a character passed to ttyinput. */
+#define	TTY_CHARMASK	0x000000ff	/* Character mask */
+#define	TTY_QUOTE	0x00000100	/* Character quoted */
+#define	TTY_ERRORMASK	0xff000000	/* Error mask */
+#define	TTY_FE		0x01000000	/* Framing error or BREAK condition */
+#define	TTY_PE		0x02000000	/* Parity error */
+
+/* Is tp controlling terminal for p? */
+#define	isctty(p, tp)							\
+	((p)->p_session == (tp)->t_session && (p)->p_flag & SCTTY)
+
+/* Is p in background of tp? */
+#define	isbackground(p, tp)						\
+	(isctty((p), (tp)) && (p)->p_pgrp != (tp)->t_pgrp)
 
 #ifdef KERNEL
 extern	struct ttychars ttydefaults;
