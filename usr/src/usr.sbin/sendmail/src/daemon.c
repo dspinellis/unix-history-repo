fd2cb3f7653c5f74f33d803d8efe4ea5ba2c2e6c
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-09-22 17:03:08 -0800

    compile cleanup on various systems (mostly AUX)
    SCCS-vsn: usr.sbin/sendmail/src/conf.h 8.34
    SCCS-vsn: usr.sbin/sendmail/src/sendmail.h 8.21
    SCCS-vsn: usr.sbin/sendmail/src/daemon.c 8.15
    SCCS-vsn: usr.sbin/sendmail/src/conf.c 8.37

--- usr/src/usr.sbin/sendmail/src/daemon.c
+++ usr/src/usr.sbin/sendmail/src/daemon.c
@@ -11,9 +11,9 @@
 
 #ifndef lint
 #ifdef DAEMON
-static char sccsid[] = "@(#)daemon.c	8.14 (Berkeley) %G% (with daemon mode)";
+static char sccsid[] = "@(#)daemon.c	8.15 (Berkeley) %G% (with daemon mode)";
 #else
-static char sccsid[] = "@(#)daemon.c	8.14 (Berkeley) %G% (without daemon mode)";
+static char sccsid[] = "@(#)daemon.c	8.15 (Berkeley) %G% (without daemon mode)";
 #endif
 #endif /* not lint */
 
@@ -142,7 +142,7 @@ getrequests()
 	if (TcpRcvBufferSize > 0)
 	{
 		if (setsockopt(DaemonSocket, SOL_SOCKET, SO_RCVBUF,
-			       &TcpRcvBufferSize,
+			       (char *) &TcpRcvBufferSize,
 			       sizeof(TcpRcvBufferSize)) < 0)
 			syserr("getrequests: setsockopt(SO_RCVBUF)");
 	}
@@ -678,7 +678,7 @@ gothostent:
 		if (TcpSndBufferSize > 0)
 		{
 			if (setsockopt(s, SOL_SOCKET, SO_SNDBUF,
-				       &TcpSndBufferSize,
+				       (char *) &TcpSndBufferSize,
 				       sizeof(TcpSndBufferSize)) < 0)
 				syserr("makeconnection: setsockopt(SO_SNDBUF)");
 		}
