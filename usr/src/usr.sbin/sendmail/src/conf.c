6839796311a1bd021e319760544971bd39fce8f8
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-09-24 14:14:22 -0800

    hooks for future features
    SCCS-vsn: usr.sbin/sendmail/src/usersmtp.c 8.9
    SCCS-vsn: usr.sbin/sendmail/src/conf.c 8.38

--- usr/src/usr.sbin/sendmail/src/conf.c
+++ usr/src/usr.sbin/sendmail/src/conf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)conf.c	8.37 (Berkeley) %G%";
+static char sccsid[] = "@(#)conf.c	8.38 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
