d7f740a4d3405d20ea77000ea8107ce75fa89a97
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-09-26 17:15:17 -0800

    compute return message size more cleverly if NoReturn is set
    SCCS-vsn: usr.sbin/sendmail/src/savemail.c 8.13

--- usr/src/usr.sbin/sendmail/src/savemail.c
+++ usr/src/usr.sbin/sendmail/src/savemail.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)savemail.c	8.12 (Berkeley) %G%";
+static char sccsid[] = "@(#)savemail.c	8.13 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
