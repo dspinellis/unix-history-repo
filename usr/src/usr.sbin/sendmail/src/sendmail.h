7358aa55480401ba3a0cd7d23a6470bbfb114322
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-09-27 02:59:32 -0800

    add RunExpensiveJobs
    SCCS-vsn: usr.sbin/sendmail/src/sendmail.h 8.24

--- usr/src/usr.sbin/sendmail/src/sendmail.h
+++ usr/src/usr.sbin/sendmail/src/sendmail.h
@@ -5,7 +5,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)sendmail.h	8.23 (Berkeley) %G%
+ *	@(#)sendmail.h	8.24 (Berkeley) %G%
  */
 
 /*
@@ -15,7 +15,7 @@
 # ifdef _DEFINE
 # define EXTERN
 # ifndef lint
-static char SmailSccsId[] =	"@(#)sendmail.h	8.23		%G%";
+static char SmailSccsId[] =	"@(#)sendmail.h	8.24		%G%";
 # endif
 # else /*  _DEFINE */
 # define EXTERN extern
