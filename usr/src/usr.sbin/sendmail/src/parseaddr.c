cbedf0c882cf6ef7f948bdcd1c28626ad57f0e79
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-09-27 16:23:32 -0800

    repair numeric codes on $#error lines (broken a few days ago)
    SCCS-vsn: usr.sbin/sendmail/src/parseaddr.c 8.12

--- usr/src/usr.sbin/sendmail/src/parseaddr.c
+++ usr/src/usr.sbin/sendmail/src/parseaddr.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)parseaddr.c	8.11 (Berkeley) %G%";
+static char sccsid[] = "@(#)parseaddr.c	8.12 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -1231,7 +1231,21 @@ badaddr:
 			syserr("554 buildaddr: error: no user");
 		cataddr(++tv, NULL, buf, sizeof buf, ' ');
 		stripquotes(buf);
-		usrerr("%s", buf);
+		if (isascii(buf[0]) && isdigit(buf[0]) &&
+		    isascii(buf[1]) && isdigit(buf[1]) &&
+		    isascii(buf[2]) && isdigit(buf[2]) &&
+		    buf[3] == ' ')
+		{
+			char fmt[10];
+
+			strncpy(fmt, buf, 3);
+			strcpy(&fmt[3], " %s");
+			usrerr(fmt, buf + 4);
+		}
+		else
+		{
+			usrerr("%s", buf);
+		}
 		goto badaddr;
 	}
 
