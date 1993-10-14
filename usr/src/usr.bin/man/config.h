22d91172fdb0c117bdb01a0764eb9e0d4945ad94
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-10-13 21:25:13 -0800

    date and time created 93/10/13 13:25:13 by bostic
    SCCS-vsn: usr.bin/man/config.h 8.1

---
+++ usr/src/usr.bin/man/config.h
@@ -0,0 +1,22 @@
+/*-
+ * Copyright (c) 1993 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.redist.c%
+ *
+ *	@(#)config.h	8.1 (Berkeley) %G%
+ */
+
+typedef struct _entry {
+	struct queue_entry	list;		/* List of items. */
+	struct queue_entry	tags;		/* List of tags. */
+	char *s;				/* Associated string. */
+	size_t len;				/* Length of 's'. */
+} ENTRY;
+
+extern struct queue_entry head;
+
+ENTRY	*addlist __P((char *));
+void	 config __P((void));
+void	 debug __P((char *));
+ENTRY	*getlist __P((char *));
