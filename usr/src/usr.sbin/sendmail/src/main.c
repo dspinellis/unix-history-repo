5590cb403b4acf26e7f14f4409af2055728e5c62
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-10-21 15:19:13 -0800

    drop X-Authentication-Warning: about using -f for the time being
    SCCS-vsn: usr.sbin/sendmail/src/main.c 8.31

--- usr/src/usr.sbin/sendmail/src/main.c
+++ usr/src/usr.sbin/sendmail/src/main.c
@@ -13,7 +13,7 @@ static char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)main.c	8.30 (Berkeley) %G%";
+static char sccsid[] = "@(#)main.c	8.31 (Berkeley) %G%";
 #endif /* not lint */
 
 #define	_DEFINE
@@ -601,9 +601,11 @@ main(argc, argv, envp)
 	if (warn_C_flag)
 		auth_warning(CurEnv, "Processed by %s with -C %s",
 			RealUserName, ConfFile);
+/*
 	if (warn_f_flag != '\0')
 		auth_warning(CurEnv, "%s set sender to %s using -%c",
 			RealUserName, from, warn_f_flag);
+*/
 	if (Warn_Q_option)
 		auth_warning(CurEnv, "Processed from queue %s", QueueDir);
 
