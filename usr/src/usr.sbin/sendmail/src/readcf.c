3ee8c081fc0c5764cab51b75faff527743e746bc
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-10-21 20:28:53 -0800

    consider option "c" to be safe
    SCCS-vsn: usr.sbin/sendmail/src/readcf.c 8.14

--- usr/src/usr.sbin/sendmail/src/readcf.c
+++ usr/src/usr.sbin/sendmail/src/readcf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)readcf.c	8.13 (Berkeley) %G%";
+static char sccsid[] = "@(#)readcf.c	8.14 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
