77d67be958eb6e4be8f46695a00611546d2f5e12
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-10-24 00:45:34 -0800

    fix a problem causing C mailer flag to be ignored if the message was queued
    SCCS-vsn: usr.sbin/sendmail/src/queue.c 8.24

--- usr/src/usr.sbin/sendmail/src/queue.c
+++ usr/src/usr.sbin/sendmail/src/queue.c
@@ -10,9 +10,9 @@
 
 #ifndef lint
 #ifdef QUEUE
-static char sccsid[] = "@(#)queue.c	8.23 (Berkeley) %G% (with queueing)";
+static char sccsid[] = "@(#)queue.c	8.24 (Berkeley) %G% (with queueing)";
 #else
-static char sccsid[] = "@(#)queue.c	8.23 (Berkeley) %G% (without queueing)";
+static char sccsid[] = "@(#)queue.c	8.24 (Berkeley) %G% (without queueing)";
 #endif
 #endif /* not lint */
 
@@ -246,7 +246,7 @@ queueup(e, queueall, announce)
 
 	bzero((char *) &nullmailer, sizeof nullmailer);
 	nullmailer.m_re_rwset = nullmailer.m_rh_rwset =
-			nullmailer.m_se_rwset = nullmailer.m_sh_rwset = -1;
+			nullmailer.m_se_rwset = nullmailer.m_sh_rwset = 0;
 	nullmailer.m_eol = "\n";
 
 	define('g', "\201f", e);
