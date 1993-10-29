c7de389f2651358abf127f8d3901e10ec36b439e
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-10-29 03:38:40 -0800

    it's not the case that unbalanced conditions are always fixed
    SCCS-vsn: usr.sbin/sendmail/src/parseaddr.c 8.16

--- usr/src/usr.sbin/sendmail/src/parseaddr.c
+++ usr/src/usr.sbin/sendmail/src/parseaddr.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)parseaddr.c	8.15 (Berkeley) %G%";
+static char sccsid[] = "@(#)parseaddr.c	8.16 (Berkeley) %G%";
 #endif /* not lint */
 
 #include "sendmail.h"
@@ -451,18 +451,18 @@ prescan(addr, delim, pvpbuf, delimptr)
 				/* diagnose and patch up bad syntax */
 				if (state == QST)
 				{
-					usrerr("653 Unbalanced '\"' (fixed)");
+					usrerr("653 Unbalanced '\"'");
 					c = '"';
 				}
 				else if (cmntcnt > 0)
 				{
-					usrerr("653 Unbalanced '(' (fixed)");
+					usrerr("653 Unbalanced '('");
 					c = ')';
 				}
 				else if (anglecnt > 0)
 				{
 					c = '>';
-					usrerr("653 Unbalanced '<' (fixed)");
+					usrerr("653 Unbalanced '<'");
 				}
 				else
 					break;
@@ -512,7 +512,7 @@ prescan(addr, delim, pvpbuf, delimptr)
 			{
 				if (cmntcnt <= 0)
 				{
-					usrerr("653 Unbalanced ')' (fixed)");
+					usrerr("653 Unbalanced ')'");
 					c = NOCHAR;
 				}
 				else
@@ -526,7 +526,7 @@ prescan(addr, delim, pvpbuf, delimptr)
 			{
 				if (anglecnt <= 0)
 				{
-					usrerr("653 Unbalanced '>' (fixed)");
+					usrerr("653 Unbalanced '>'");
 					c = NOCHAR;
 				}
 				else
