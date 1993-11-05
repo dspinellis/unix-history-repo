9d49ae264038f7cabbda4e8804ad6b60dbbd45eb
Andreas Schulz <ats@g386bsd.first.bmd.de>
1993-11-05 10:18:09 +0000

    Changed the version number again from 1.1a to 1.0.1, because we should better use minor numbers instead of greek characters.

--- sys/conf/newvers.sh
+++ sys/conf/newvers.sh
@@ -32,7 +32,7 @@
 # SUCH DAMAGE.
 #
 #	from: @(#)newvers.sh	7.4 (Berkeley) 12/7/90
-#	$Id: newvers.sh,v 1.3 1993/10/16 16:31:53 rgrimes Exp $
+#	$Id: newvers.sh,v 1.4 1993/11/03 10:48:26 ats Exp $
 #
 
 if [ ! -r version ]
@@ -44,7 +44,7 @@ fi
 
 touch version
 
-kernvers="FreeBSD 1.1a"
+kernvers="FreeBSD 1.0.1"
 v=`cat version` t=`date "+ %m/%d/%y %H:%M"`
 t=`date`
 user=${USER-root}
