86dd288a231ae4bcf2e0ec570b7b1d6802a6111b
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-11-08 17:54:09 -0800

    be much less trusting about setuid in mailfile -- still works for the .forward case because ctladdr != NULL there
    SCCS-vsn: usr.sbin/sendmail/src/deliver.c 8.38

--- usr/src/usr.sbin/sendmail/src/deliver.c
+++ usr/src/usr.sbin/sendmail/src/deliver.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)deliver.c	8.37 (Berkeley) %G%";
+static char sccsid[] = "@(#)deliver.c	8.38 (Berkeley) %G%";
 #endif /* not lint */
 
 #include "sendmail.h"
@@ -1731,9 +1731,7 @@ mailfile(filename, ctladdr, e)
 
 		if (bitset(0111, stb.st_mode))
 			exit(EX_CANTCREAT);
-		if (ctladdr == NULL)
-			ctladdr = &e->e_from;
-		else
+		if (ctladdr != NULL)
 		{
 			/* ignore setuid and setgid bits */
 			mode &= ~(S_ISGID|S_ISUID);
@@ -1752,7 +1750,7 @@ mailfile(filename, ctladdr, e)
 
 		if (!bitset(S_ISGID, mode) || setgid(stb.st_gid) < 0)
 		{
-			if (ctladdr->q_uid == 0)
+			if (ctladdr == NULL || ctladdr->q_uid == 0)
 			{
 				(void) initgroups(DefUser, DefGid);
 			}
@@ -1765,7 +1763,7 @@ mailfile(filename, ctladdr, e)
 		}
 		if (!bitset(S_ISUID, mode) || setuid(stb.st_uid) < 0)
 		{
-			if (ctladdr->q_uid == 0)
+			if (ctladdr == NULL || ctladdr->q_uid == 0)
 				(void) setuid(DefUid);
 			else
 				(void) setuid(ctladdr->q_uid);
