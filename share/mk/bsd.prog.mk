f82f7c8f63198c6cec2e1de6e1161fceec708fc0
Rod Grimes <rgrimes@FreeBSD.org>
1993-11-10 11:43:15 +0000

    Remove LIBRPC, it is now part of libc.  Picked up this from Chris D. commit on sun-lamp

--- share/mk/bsd.prog.mk
+++ share/mk/bsd.prog.mk
@@ -31,7 +31,6 @@ LIBMP?=		/usr/lib/libmp.a
 LIBPC?=		/usr/lib/libpc.a
 LIBPLOT?=	/usr/lib/libplot.a
 LIBRESOLV?=	/usr/lib/libresolv.a
-LIBRPC?=	/usr/lib/librpc.a
 LIBRPCSVC?=	/usr/lib/librpcsvc.a
 LIBTELNET?=	/usr/lib/libtelnet.a
 LIBTERM?=	/usr/lib/libterm.a
