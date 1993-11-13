7b80dd46d670022b1a9d5622e6f9f4baf77b4b77
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-11-12 20:52:32 -0800

    add null -O flag for NeXT -- Mail.app uses this, but why?
    SCCS-vsn: usr.sbin/sendmail/src/main.c 8.36

--- usr/src/usr.sbin/sendmail/src/main.c
+++ usr/src/usr.sbin/sendmail/src/main.c
@@ -13,7 +13,7 @@ static char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)main.c	8.35 (Berkeley) %G%";
+static char sccsid[] = "@(#)main.c	8.36 (Berkeley) %G%";
 #endif /* not lint */
 
 #define	_DEFINE
@@ -230,12 +230,15 @@ main(argc, argv, envp)
 
 #if defined(__osf__) || defined(_AIX3)
 # define OPTIONS	"B:b:C:cd:e:F:f:h:Iimno:p:q:r:sTtvX:x"
-#else
-# if defined(ultrix)
-#  define OPTIONS	"B:b:C:cd:e:F:f:h:IiM:mno:p:q:r:sTtvX:"
-# else
-#  define OPTIONS	"B:b:C:cd:e:F:f:h:Iimno:p:q:r:sTtvX:"
-# endif
+#endif
+#if defined(ultrix)
+# define OPTIONS	"B:b:C:cd:e:F:f:h:IiM:mno:p:q:r:sTtvX:"
+#endif
+#if defined(NeXT)
+# define OPTIONS	"B:b:C:cd:e:F:f:h:IimnOo:p:q:r:sTtvX:"
+#endif
+#ifndef OPTIONS
+# define OPTIONS	"B:b:C:cd:e:F:f:h:Iimno:p:q:r:sTtvX:"
 #endif
 	while ((j = getopt(argc, argv, OPTIONS)) != EOF)
 	{
@@ -563,6 +566,10 @@ main(argc, argv, envp)
 
 # if defined(__osf__) || defined(_AIX3)
 		  case 'x':	/* random flag that OSF/1 & AIX mailx passes */
+			break;
+# endif
+# if defined(NeXT)
+		  case 'O':	/* random flag that NeXT Mail.app passes */
 			break;
 # endif
 
