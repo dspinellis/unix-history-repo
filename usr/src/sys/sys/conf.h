9472a7bdae76cfb094500f51ed916824a49617c7
Mike Hibler <hibler@ucbvax.Berkeley.EDU>
1993-11-14 21:45:50 -0800

    convert sw_freed to sw_flags for SEQSWAP support
    SCCS-vsn: sys/sys/conf.h 8.2

--- usr/src/sys/sys/conf.h
+++ usr/src/sys/sys/conf.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)conf.h	8.1 (Berkeley) %G%
+ *	@(#)conf.h	8.2 (Berkeley) %G%
  */
 
 /*
@@ -79,10 +79,13 @@ extern struct linesw linesw[];
 
 struct swdevt {
 	dev_t	sw_dev;
-	int	sw_freed;
+	int	sw_flags;
 	int	sw_nblks;
 	struct	vnode *sw_vp;
 };
+#define	SW_FREED	0x01
+#define	SW_SEQUENTIAL	0x02
+#define sw_freed	sw_flags	/* XXX compat */
 
 #ifdef KERNEL
 extern struct swdevt swdevt[];
