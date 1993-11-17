e9ae66c8dc4e4e53f05f193b812a7d24bd3167c2
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-11-16 20:36:16 -0800

    Error in the table; From: db@diana.ocunix.on.ca (Dyane Bruce)
    SCCS-vsn: games/bcd/bcd.c 8.2

--- usr/src/games/bcd/bcd.c
+++ usr/src/games/bcd/bcd.c
@@ -15,7 +15,7 @@ static char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)bcd.c	8.1 (Berkeley) %G%";
+static char sccsid[] = "@(#)bcd.c	8.2 (Berkeley) %G%";
 #endif /* not lint */
 
 /*
