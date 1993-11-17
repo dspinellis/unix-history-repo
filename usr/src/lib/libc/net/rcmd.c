21c79fd8133ba8c06f8439e6dcd61b9defda771d
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-11-16 19:59:23 -0800

    While installing rdist v6, I noticed that a file descriptor leak in rcmd.c documented in the READM still existed in the bsd source. A context diff is appended. From: Craig Leres <leres@ee.lbl.gov>
    SCCS-vsn: lib/libc/net/rcmd.c 8.2

--- usr/src/lib/libc/net/rcmd.c
+++ usr/src/lib/libc/net/rcmd.c
@@ -6,7 +6,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)rcmd.c	8.1 (Berkeley) %G%";
+static char sccsid[] = "@(#)rcmd.c	8.2 (Berkeley) %G%";
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/param.h>
