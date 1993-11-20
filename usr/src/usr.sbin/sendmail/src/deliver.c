9e454ef394a5680e46d70c3e9fd3f3c6bb9cf835
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-11-20 14:52:29 -0800

    curhost can be null if running SMTP to a program: avoid core dumps
    SCCS-vsn: usr.sbin/sendmail/src/deliver.c 8.42

--- usr/src/usr.sbin/sendmail/src/deliver.c
+++ usr/src/usr.sbin/sendmail/src/deliver.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)deliver.c	8.41 (Berkeley) %G%";
+static char sccsid[] = "@(#)deliver.c	8.42 (Berkeley) %G%";
 #endif /* not lint */
 
 #include "sendmail.h"
@@ -1568,6 +1568,7 @@ logdelivery(m, mci, stat, ctladdr, e)
 			(void) strcat(bp, p);
 		}
 	}
+	bp += strlen(bp);
 		
 	syslog(LOG_INFO, "%s: to=%s%s, stat=%s",
 	       e->e_id, e->e_to, buf, stat);
