06fd85f7ecfbe7615f2e0bce082ea2241a2b9a08
J.T. Conklin <jtc@FreeBSD.org>
1993-11-22 23:54:24 +0000

    POSIX.2ification, from my work with NetBSD.

--- bin/mv/pathnames.h
+++ bin/mv/pathnames.h
@@ -30,7 +30,8 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)pathnames.h	5.2 (Berkeley) 5/31/90
+ *	from: @(#)pathnames.h	5.2 (Berkeley) 5/31/90
+ *	$Id: pathnames.h,v 1.4 1993/08/01 18:59:16 mycroft Exp $
  */
 
 #define	_PATH_RM	"/bin/rm"
