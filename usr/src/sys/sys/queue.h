8845883d0b28f612e0f0915d1346d0b9c763673d
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1993-11-23 15:12:45 -0800

    revised version for use by applications as well as the kernel
    SCCS-vsn: sys/sys/queue.h 8.2

--- usr/src/sys/sys/queue.h
+++ usr/src/sys/sys/queue.h
@@ -1,128 +1,190 @@
 /* 
- * Copyright (c) 1991, 1993
+ * Copyright (c) 1993
  *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)queue.h	8.1 (Berkeley) %G%
+ *	@(#)queue.h	8.2 (Berkeley) %G%
  */
 
 #ifndef	_QUEUE_H_
 #define	_QUEUE_H_
 
 /*
- * This file defines two types of data structures, lists and queues.
- *
- * A list is headed by a single forward pointer (or an array of forward
- * pointers for a hash table header). The elements are doubly linked
- * so that an arbitrary element can be removed without a need to
- * traverse the list. New elements can be added to the list after
- * an existing element or at the head of the list.
- *
- * A queue is headed by a pair of pointers, one to the head of the list
- * and the other to the tail of the list. The elements are doubly linked
- * so that an arbitrary element can be removed without a need to
- * traverse the list. New elements can be added to the list after
- * an existing element, at the head of the list, or at the end of
- * the list.
- *
- * Note that the implementation used here avoids the need to special
- * case inserting into an empty list, deleting the last element from
- * a list, or inserting at the beginning or end of a list. The drawback
- * to this method is that it is not possible to traverse a list or
- * queue backwards.
+ * List definitions.
  */
-
-struct queue_entry {
-	void	*qe_next;	/* next element */
-	void	**qe_prev;	/* address of previous next element */
-};
-
-struct list_entry {
-	void	*le_next;	/* next element */
-};
-
-/*
- * Value for pointers on removed entries.
- */
-#define	NOLIST	(void *)0xdead
-
-/*
- * Queue functions.
- */
-#define	queue_init(head) { \
-	(head)->qe_next = 0; \
-	(head)->qe_prev = &(head)->qe_next; \
+#define LIST_HEAD(name, type)						\
+struct name {								\
+	struct type *lh_first;	/* first element */			\
 }
 
-#define queue_enter_tail(head, elm, type, field) { \
-	(elm)->field.qe_next = 0; \
-	(elm)->field.qe_prev = (head)->qe_prev; \
-	*(head)->qe_prev = (elm); \
-	(head)->qe_prev = &(elm)->field.qe_next; \
-}
-
-#define queue_enter_head(head, elm, type, field) { \
-	type queue_ptr; \
-	if (queue_ptr = (head)->qe_next) \
-		queue_ptr->field.qe_prev = &(elm)->field.qe_next; \
-	else \
-		(head)->qe_prev = &(elm)->field.qe_next; \
-	(head)->qe_next = (elm); \
-	(elm)->field.qe_next = queue_ptr; \
-	(elm)->field.qe_prev = &(head)->qe_next; \
-}
-
-#define queue_insert_after(head, listelm, elm, type, field) { \
-	type queue_ptr; \
-	if (queue_ptr = (listelm)->qe_next) \
-		queue_ptr->field.qe_prev = &(elm)->field.qe_next; \
-	else \
-		(head)->qe_prev = &(elm)->field.qe_next; \
-	(listelm)->qe_next = (elm); \
-	(elm)->field.qe_next = queue_ptr; \
-	(elm)->field.qe_prev = &(listelm)->qe_next; \
-}
-
-#define queue_remove(head, elm, type, field) { \
-	type queue_ptr; \
-	if (queue_ptr = (elm)->field.qe_next) \
-		queue_ptr->field.qe_prev = (elm)->field.qe_prev; \
-	else \
-		(head)->qe_prev = (elm)->field.qe_prev; \
-	*(elm)->field.qe_prev = queue_ptr; \
-	(elm)->field.qe_next = NOLIST; \
-	(elm)->field.qe_prev = NOLIST; \
+#define LIST_ENTRY(type)						\
+struct {								\
+	struct type *le_next;	/* next element */			\
+	struct type **le_prev;	/* address of previous next element */	\
 }
 
 /*
  * List functions.
  */
-#define list_enter_head(head, elm, type, field) { \
-	type queue_ptr; \
-	if (queue_ptr = (head)->le_next) \
-		queue_ptr->field.qe_prev = &(elm)->field.qe_next; \
-	(head)->le_next = (elm); \
-	(elm)->field.qe_next = queue_ptr; \
-	(elm)->field.qe_prev = &(head)->le_next; \
+#define	LIST_INIT(head) {						\
+	(head)->lh_first = NULL;					\
 }
 
-#define list_insert_after(listelm, elm, type, field) { \
-	type queue_ptr; \
-	if (queue_ptr = (listelm)->qe_next) \
-		queue_ptr->field.qe_prev = &(elm)->field.qe_next; \
-	(listelm)->qe_next = (elm); \
-	(elm)->field.qe_next = queue_ptr; \
-	(elm)->field.qe_prev = &(listelm)->qe_next; \
+#define LIST_INSERT_AFTER(listelm, elm, field) {			\
+	if (((elm)->field.le_next = (listelm)->field.le_next) != NULL)	\
+		(listelm)->field.le_next->field.le_prev =		\
+		    &(elm)->field.le_next;				\
+	(listelm)->field.le_next = (elm);				\
+	(elm)->field.le_prev = &(listelm)->field.le_next;		\
 }
 
-#define list_remove(elm, type, field) { \
-	type queue_ptr; \
-	if (queue_ptr = (elm)->field.qe_next) \
-		queue_ptr->field.qe_prev = (elm)->field.qe_prev; \
-	*(elm)->field.qe_prev = queue_ptr; \
-	(elm)->field.qe_next = NOLIST; \
-	(elm)->field.qe_prev = NOLIST; \
+#define LIST_INSERT_HEAD(head, elm, field) {				\
+	if (((elm)->field.le_next = (head)->lh_first) != NULL)		\
+		(head)->lh_first->field.le_prev = &(elm)->field.le_next;\
+	(head)->lh_first = (elm);					\
+	(elm)->field.le_prev = &(head)->lh_first;			\
 }
 
+#define LIST_REMOVE(elm, field) {					\
+	if ((elm)->field.le_next != NULL)				\
+		(elm)->field.le_next->field.le_prev = 			\
+		    (elm)->field.le_prev;				\
+	*(elm)->field.le_prev = (elm)->field.le_next;			\
+}
+
+/*
+ * Tail queue definitions.
+ */
+#define TAILQ_HEAD(name, type)						\
+struct name {								\
+	struct type *tqh_first;	/* first element */			\
+	struct type **tqh_last;	/* addr of last next element */		\
+}
+
+#define TAILQ_ENTRY(type)						\
+struct {								\
+	struct type *tqe_next;	/* next element */			\
+	struct type **tqe_prev;	/* address of previous next element */	\
+}
+
+/*
+ * Tail queue functions.
+ */
+#define	TAILQ_INIT(head) {						\
+	(head)->tqh_first = NULL;					\
+	(head)->tqh_last = &(head)->tqh_first;				\
+}
+
+#define TAILQ_INSERT_HEAD(head, elm, field) {				\
+	if (((elm)->field.tqe_next = (head)->tqh_first) != NULL)	\
+		(elm)->field.tqe_next->field.tqe_prev =			\
+		    &(elm)->field.tqe_next;				\
+	else								\
+		(head)->tqh_last = &(elm)->field.tqe_next;		\
+	(head)->tqh_first = (elm);					\
+	(elm)->field.tqe_prev = &(head)->tqh_first;			\
+}
+
+#define TAILQ_INSERT_TAIL(head, elm, field) {				\
+	(elm)->field.tqe_next = NULL;					\
+	(elm)->field.tqe_prev = (head)->tqh_last;			\
+	*(head)->tqh_last = (elm);					\
+	(head)->tqh_last = &(elm)->field.tqe_next;			\
+}
+
+#define TAILQ_INSERT_AFTER(head, listelm, elm, field) {			\
+	if (((elm)->field.tqe_next = (listelm)->field.tqe_next) != NULL)\
+		(elm)->field.tqe_next->field.tqe_prev = 		\
+		    &(elm)->field.tqe_next;				\
+	else								\
+		(head)->tqh_last = &(elm)->field.tqe_next;		\
+	(listelm)->field.tqe_next = (elm);				\
+	(elm)->field.tqe_prev = &(listelm)->field.tqe_next;		\
+}
+
+#define TAILQ_REMOVE(head, elm, field) {				\
+	if (((elm)->field.tqe_next) != NULL)				\
+		(elm)->field.tqe_next->field.tqe_prev = 		\
+		    (elm)->field.tqe_prev;				\
+	else								\
+		(head)->tqh_last = (elm)->field.tqe_prev;		\
+	*(elm)->field.tqe_prev = (elm)->field.tqe_next;			\
+}
+
+/*
+ * Circular queue definitions.
+ */
+#define CIRCLEQ_HEAD(name, type)					\
+struct name {								\
+	struct type *cqh_first;		/* first element */		\
+	struct type *cqh_last;		/* last element */		\
+}
+
+#define CIRCLEQ_ENTRY(type)						\
+struct {								\
+	struct type *cqe_next;		/* next element */		\
+	struct type *cqe_prev;		/* previous element */		\
+}
+
+/*
+ * Circular queue functions.
+ */
+#define	CIRCLEQ_INIT(head) {						\
+	(head)->cqh_first = (void *)(head);				\
+	(head)->cqh_last = (void *)(head);				\
+}
+
+#define CIRCLEQ_INSERT_AFTER(head, listelm, elm, field) {		\
+	(elm)->field.cqe_next = (listelm)->field.cqe_next;		\
+	(elm)->field.cqe_prev = (listelm);				\
+	if ((listelm)->field.cqe_next == (void *)(head))		\
+		(head)->cqh_last = (elm);				\
+	else								\
+		(listelm)->field.cqe_next->field.cqe_prev = (elm);	\
+	(listelm)->field.cqe_next = (elm);				\
+}
+
+#define CIRCLEQ_INSERT_BEFORE(head, listelm, elm, field) {		\
+	(elm)->field.cqe_next = (listelm);				\
+	(elm)->field.cqe_prev = (listelm)->field.cqe_prev;		\
+	if ((listelm)->field.cqe_prev == (void *)(head))		\
+		(head)->cqh_first = (elm);				\
+	else								\
+		(listelm)->field.cqe_prev->field.cqe_next = (elm);	\
+	(listelm)->field.cqe_prev = (elm);				\
+}
+
+#define CIRCLEQ_INSERT_HEAD(head, elm, field) {				\
+	(elm)->field.cqe_next = (head)->cqh_first;			\
+	(elm)->field.cqe_prev = (void *)(head);				\
+	if ((head)->cqh_last == (void *)(head))				\
+		(head)->cqh_last = (elm);				\
+	else								\
+		(head)->cqh_first->field.cqe_prev = (elm);		\
+	(head)->cqh_first = (elm);					\
+}
+
+#define CIRCLEQ_INSERT_TAIL(head, elm, field) {				\
+	(elm)->field.cqe_next = (void *)(head);				\
+	(elm)->field.cqe_prev = (head)->cqh_last;			\
+	if ((head)->cqh_first == (void *)(head))			\
+		(head)->cqh_first = (elm);				\
+	else								\
+		(head)->cqh_last->field.cqe_next = (elm);		\
+	(head)->cqh_last = (elm);					\
+}
+
+#define	CIRCLEQ_REMOVE(head, elm, field) {				\
+	if ((elm)->field.cqe_next == (void *)(head))			\
+		(head)->cqh_last = (elm)->field.cqe_prev;		\
+	else								\
+		(elm)->field.cqe_next->field.cqe_prev =			\
+		    (elm)->field.cqe_prev;				\
+	if ((elm)->field.cqe_prev == (void *)(head))			\
+		(head)->cqh_first = (elm)->field.cqe_next;		\
+	else								\
+		(elm)->field.cqe_prev->field.cqe_next =			\
+		    (elm)->field.cqe_next;				\
+}
 #endif	/* !_QUEUE_H_ */
