b395d81a801f0ac7f6014e97c4f51d6054cbfef8
Andreas Schulz <ats@g386bsd.first.bmd.de>
1993-11-30 21:54:25 +0000

    Fixed the error, that makes g++ fail in compiling groff/libbib/index.cc. I don't understand why g++ uses the struct buf definition from types.h instead of the local char *buf definition, but it seems to happen. Error in scope handling in g++ ?

--- sys/sys/types.h
+++ sys/sys/types.h
@@ -31,7 +31,7 @@
  * SUCH DAMAGE.
  *
  *	from: @(#)types.h	7.17 (Berkeley) 5/6/91
- *	$Id: types.h,v 1.2 1993/10/16 17:18:12 rgrimes Exp $
+ *	$Id: types.h,v 1.3 1993/11/28 14:23:11 ats Exp $
  */
 
 #ifndef _TYPES_H_
@@ -127,7 +127,9 @@ struct	pgrp;
 struct	ucred;
 struct	rusage;
 struct	file;
+#ifdef KERNEL
 struct	buf;
+#endif
 struct	tty;
 struct	uio;
 #endif
