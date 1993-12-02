dad5ff72cc2cf9f8e8fd08664a43bb1ccd2a96ad
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-12-02 15:29:33 -0800

    print $j and check that $j is in $=w on SIGUSR1
    SCCS-vsn: usr.sbin/sendmail/src/main.c 8.41

--- usr/src/usr.sbin/sendmail/src/main.c
+++ usr/src/usr.sbin/sendmail/src/main.c
@@ -13,7 +13,7 @@ static char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)main.c	8.40 (Berkeley) %G%";
+static char sccsid[] = "@(#)main.c	8.41 (Berkeley) %G%";
 #endif /* not lint */
 
 #define	_DEFINE
