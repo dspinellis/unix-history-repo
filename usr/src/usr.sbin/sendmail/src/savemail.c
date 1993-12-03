81ef2e769213532ce878da78b687962d5708dc78
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-12-03 02:31:33 -0800

    try to clean up warning messages
    SCCS-vsn: usr.sbin/sendmail/src/savemail.c 8.20.1.1

--- usr/src/usr.sbin/sendmail/src/savemail.c
+++ usr/src/usr.sbin/sendmail/src/savemail.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)savemail.c	8.20 (Berkeley) %G%";
+static char sccsid[] = "@(#)savemail.c	8.20.1.1 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
