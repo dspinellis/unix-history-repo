822baf17ca9b5f7ba62f69fe8c0148b13efa4af2
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-12-10 20:42:19 -0800

    up size of maximum short string
    SCCS-vsn: usr.sbin/sendmail/src/util.c 8.21

--- usr/src/usr.sbin/sendmail/src/util.c
+++ usr/src/usr.sbin/sendmail/src/util.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)util.c	8.20 (Berkeley) %G%";
+static char sccsid[] = "@(#)util.c	8.21 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -1362,7 +1362,7 @@ printit:
 */
 
 #ifndef MAXSHORTSTR
-# define MAXSHORTSTR	83
+# define MAXSHORTSTR	203
 #endif
 
 char *
