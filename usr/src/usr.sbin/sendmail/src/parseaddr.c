120ee12bc51aabe5652f9d63cce6bef3d5c7cdf3
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-12-10 20:42:42 -0800

    avoid core dump on subroutines that return null addr lists
    SCCS-vsn: usr.sbin/sendmail/src/parseaddr.c 8.21

--- usr/src/usr.sbin/sendmail/src/parseaddr.c
+++ usr/src/usr.sbin/sendmail/src/parseaddr.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)parseaddr.c	8.20 (Berkeley) %G%";
+static char sccsid[] = "@(#)parseaddr.c	8.21 (Berkeley) %G%";
 #endif /* not lint */
 
 #include "sendmail.h"
