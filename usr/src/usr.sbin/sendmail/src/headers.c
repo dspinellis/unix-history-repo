5f3c55754504da1584ba2c6be265bd0c33beba3c
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-12-10 20:31:19 -0800

    accept Full-Name: header on input
    SCCS-vsn: usr.sbin/sendmail/src/headers.c 8.15

--- usr/src/usr.sbin/sendmail/src/headers.c
+++ usr/src/usr.sbin/sendmail/src/headers.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)headers.c	8.14 (Berkeley) %G%";
+static char sccsid[] = "@(#)headers.c	8.15 (Berkeley) %G%";
 #endif /* not lint */
 
 # include <errno.h>
@@ -296,6 +296,11 @@ eatheader(e, full)
 	else
 		define('u', NULL, e);
 
+	/* full name of from person */
+	p = hvalue("full-name", e);
+	if (p != NULL)
+		define('x', p, e);
+
 	if (tTd(32, 1))
 		printf("----- collected header -----\n");
 	msgid = "<none>";
@@ -377,11 +382,6 @@ eatheader(e, full)
 		e->e_msgpriority = e->e_msgsize
 				 - e->e_class * WkClassFact
 				 + e->e_nrcpts * WkRecipFact;
-
-	/* full name of from person */
-	p = hvalue("full-name", e);
-	if (p != NULL)
-		define('x', p, e);
 
 	/* date message originated */
 	p = hvalue("posted-date", e);
