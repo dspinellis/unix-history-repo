1ab48b686a99f977e21ac00a79cd487d369a1666
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-12-10 20:43:18 -0800

    avoid stupid failures if 421 error is returned during connection startup
    SCCS-vsn: usr.sbin/sendmail/src/usersmtp.c 8.15

--- usr/src/usr.sbin/sendmail/src/usersmtp.c
+++ usr/src/usr.sbin/sendmail/src/usersmtp.c
@@ -10,9 +10,9 @@
 
 #ifndef lint
 #ifdef SMTP
-static char sccsid[] = "@(#)usersmtp.c	8.14 (Berkeley) %G% (with SMTP)";
+static char sccsid[] = "@(#)usersmtp.c	8.15 (Berkeley) %G% (with SMTP)";
 #else
-static char sccsid[] = "@(#)usersmtp.c	8.14 (Berkeley) %G% (without SMTP)";
+static char sccsid[] = "@(#)usersmtp.c	8.15 (Berkeley) %G% (without SMTP)";
 #endif
 #endif /* not lint */
 
@@ -183,8 +183,13 @@ smtpinit(m, pvp)
 			goto tempfail2;
 	}
 
-	mci->mci_state = MCIS_OPEN;
-	return;
+	if (mci->mci_state != MCIS_CLOSED)
+	{
+		mci->mci_state = MCIS_OPEN;
+		return;
+	}
+
+	/* got a 421 error code during startup */
 
   tempfail1:
   tempfail2:
