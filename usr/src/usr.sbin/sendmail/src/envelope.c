5c355c0ea5a2acb4c700651594de809e7f67f6e8
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-12-10 20:41:58 -0800

    avoid sending WARNING message on return-receipts
    SCCS-vsn: usr.sbin/sendmail/src/savemail.c 8.21
    SCCS-vsn: usr.sbin/sendmail/src/envelope.c 8.22

--- usr/src/usr.sbin/sendmail/src/envelope.c
+++ usr/src/usr.sbin/sendmail/src/envelope.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)envelope.c	8.21 (Berkeley) %G%";
+static char sccsid[] = "@(#)envelope.c	8.22 (Berkeley) %G%";
 #endif /* not lint */
 
 #include "sendmail.h"
@@ -189,6 +189,7 @@ dropenvelope(e)
 
 		(void) sendtolist(e->e_receiptto, NULLADDR, &rlist, e);
 		(void) returntosender("Return receipt", rlist, FALSE, e);
+		e->e_flags &= ~EF_SENDRECEIPT;
 	}
 
 	/*
