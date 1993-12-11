113408272a36abedeaccbfe194f43d8091f26a95
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1993-12-11 01:46:37 -0800

    add cpu_set_init_frame to initialize init's syscall frame
    SCCS-vsn: sys/pmax/include/cpu.h 8.3

--- usr/src/sys/pmax/include/cpu.h
+++ usr/src/sys/pmax/include/cpu.h
@@ -7,7 +7,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)cpu.h	8.2 (Berkeley) %G%
+ *	@(#)cpu.h	8.3 (Berkeley) %G%
  */
 
 #ifndef _CPU_H_
@@ -25,10 +25,10 @@
  */
 #define	COPY_SIGCODE		/* copy sigcode above user stack in exec */
 
-#define	cpu_exec(p)	(p->p_md.md_ss_addr = 0) /* init single step */
-#define	cpu_wait(p)	/* nothing */
-#define cpu_setstack(p, ap) \
-	(p)->p_md.md_regs[SP] = ap
+#define	cpu_exec(p)		(p->p_md.md_ss_addr = 0) /* init single step */
+#define	cpu_wait(p)		/* nothing */
+#define cpu_setstack(p, ap)	(p)->p_md.md_regs[SP] = ap
+#define cpu_set_init_frame(p, fp) (p)->p_md.md_regs = fp
 
 /*
  * Arguments to hardclock and gatherstats encapsulate the previous
