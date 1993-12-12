0c66c5289391c461d19444769518995b70d14302
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-12-12 01:31:09 -0800

    Solaris compiler fixes
    SCCS-vsn: usr.sbin/sendmail/src/conf.c 8.51

--- usr/src/usr.sbin/sendmail/src/conf.c
+++ usr/src/usr.sbin/sendmail/src/conf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)conf.c	8.50 (Berkeley) %G%";
+static char sccsid[] = "@(#)conf.c	8.51 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
