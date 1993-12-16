b436a91e65b543d4651ac45ab0d0b85b66577ceb
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-12-16 13:38:52 -0800

    adjust declaration of getopt for ANSI C compilers
    SCCS-vsn: usr.sbin/sendmail/src/conf.c 8.52

--- usr/src/usr.sbin/sendmail/src/conf.c
+++ usr/src/usr.sbin/sendmail/src/conf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)conf.c	8.51 (Berkeley) %G%";
+static char sccsid[] = "@(#)conf.c	8.52 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
