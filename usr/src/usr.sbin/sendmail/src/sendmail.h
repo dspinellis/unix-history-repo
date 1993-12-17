46c0278a4d71d0cf7347793c9c56534129b9d40e
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-12-16 18:18:08 -0800

    add EF_8BITDATA flag to indicate that 8-bit data has been seen
    SCCS-vsn: usr.sbin/sendmail/src/sendmail.h 8.36

--- usr/src/usr.sbin/sendmail/src/sendmail.h
+++ usr/src/usr.sbin/sendmail/src/sendmail.h
@@ -5,7 +5,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)sendmail.h	8.35 (Berkeley) %G%
+ *	@(#)sendmail.h	8.36 (Berkeley) %G%
  */
 
 /*
@@ -15,7 +15,7 @@
 # ifdef _DEFINE
 # define EXTERN
 # ifndef lint
-static char SmailSccsId[] =	"@(#)sendmail.h	8.35		%G%";
+static char SmailSccsId[] =	"@(#)sendmail.h	8.36		%G%";
 # endif
 # else /*  _DEFINE */
 # define EXTERN extern
