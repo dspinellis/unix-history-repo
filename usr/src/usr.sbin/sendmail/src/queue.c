86b402d51062cd555f376f179be40ac63c41c6f2
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-12-17 16:14:42 -0800

    don't output null headers into queue file
    SCCS-vsn: usr.sbin/sendmail/src/queue.c 8.32

--- usr/src/usr.sbin/sendmail/src/queue.c
+++ usr/src/usr.sbin/sendmail/src/queue.c
@@ -10,9 +10,9 @@
 
 #ifndef lint
 #ifdef QUEUE
-static char sccsid[] = "@(#)queue.c	8.31 (Berkeley) %G% (with queueing)";
+static char sccsid[] = "@(#)queue.c	8.32 (Berkeley) %G% (with queueing)";
 #else
-static char sccsid[] = "@(#)queue.c	8.31 (Berkeley) %G% (without queueing)";
+static char sccsid[] = "@(#)queue.c	8.32 (Berkeley) %G% (without queueing)";
 #endif
 #endif /* not lint */
 
