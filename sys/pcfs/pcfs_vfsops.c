be86f13396e7a4f56d7a899407d7d6fd32797662
Andrey Chernov <ache@FreeBSD.org>
1993-12-19 02:07:58 +0000

    Warning "root directory is not a multiple  of the clustersize... " #ifdefed PCFSDEBUG, because it is usual case for floppy formats 720, 800, 820, etc.

--- sys/pcfs/pcfs_vfsops.c
+++ sys/pcfs/pcfs_vfsops.c
@@ -15,7 +15,7 @@
  *
  *  October 1992
  *
- *	$Id: pcfs_vfsops.c,v 1.3 1993/10/16 19:29:37 rgrimes Exp $
+ *	$Id: pcfs_vfsops.c,v 1.4 1993/11/25 01:37:13 wollman Exp $
  */
 
 #include "param.h"
@@ -296,9 +296,10 @@ mountpcfs(devvp, mp, p)
 		pmp->pm_fatblocksize = MAXBSIZE;
 	pmp->pm_fatblocksec = pmp->pm_fatblocksize / pmp->pm_BytesPerSec;
 		
+#if defined(PCFSDEBUG)
 	if ((pmp->pm_rootdirsize % pmp->pm_SectPerClust) != 0)
 		printf("mountpcfs(): root directory is not a multiple of the clustersize in length\n");
-
+#endif
 /*
  *  Compute mask and shift value for isolating cluster relative
  *  byte offsets and cluster numbers from a file offset.
