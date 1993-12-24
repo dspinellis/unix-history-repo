1051540f9349cf241e2b099b47ee70bb94ca0d28
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-12-24 15:57:07 -0800

    go ahead and actually implement getusershell in compat mode; too many systems don't have it
    SCCS-vsn: usr.sbin/sendmail/src/recipient.c 8.32
    SCCS-vsn: usr.sbin/sendmail/src/conf.c 8.54
    SCCS-vsn: usr.sbin/sendmail/src/READ_ME 8.41

--- usr/src/usr.sbin/sendmail/src/conf.c
+++ usr/src/usr.sbin/sendmail/src/conf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)conf.c	8.53 (Berkeley) %G%";
+static char sccsid[] = "@(#)conf.c	8.54 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
--- usr/src/usr.sbin/sendmail/src/recipient.c
+++ usr/src/usr.sbin/sendmail/src/recipient.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)recipient.c	8.31 (Berkeley) %G%";
+static char sccsid[] = "@(#)recipient.c	8.32 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -399,7 +399,6 @@ recipient(a, sendq, e)
 			buildfname(pw->pw_gecos, pw->pw_name, nbuf);
 			if (nbuf[0] != '\0')
 				a->q_fullname = newstr(nbuf);
-#ifndef NEEDGETUSERSHELL
 			if (pw->pw_shell != NULL && pw->pw_shell[0] != '\0')
 			{
 				extern char *getusershell();
@@ -412,7 +411,6 @@ recipient(a, sendq, e)
 				if (p == NULL)
 					a->q_flags |= QBOGUSSHELL;
 			}
-#endif
 			if (!quoted)
 				forward(a, sendq, e);
 		}
