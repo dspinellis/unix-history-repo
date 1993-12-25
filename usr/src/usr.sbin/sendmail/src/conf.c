58bfd9c92e61708c77695a248ee66e9621ea0181
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-12-24 18:11:59 -0800

    make NEEDGETUSERSHELL a 0/1 flag instead of a def/ndef flag
    SCCS-vsn: usr.sbin/sendmail/src/conf.c 8.55
    SCCS-vsn: usr.sbin/sendmail/src/READ_ME 8.42

--- usr/src/usr.sbin/sendmail/src/conf.c
+++ usr/src/usr.sbin/sendmail/src/conf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)conf.c	8.54 (Berkeley) %G%";
+static char sccsid[] = "@(#)conf.c	8.55 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
