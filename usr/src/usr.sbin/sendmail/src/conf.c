fa1bb8572ba7c80df954fac2fca0233e9e316a7c
Eric Allman <eric@ucbvax.Berkeley.EDU>
1993-12-27 22:54:50 -0800

    minor comment typo
    SCCS-vsn: usr.sbin/sendmail/src/conf.c 8.58

--- usr/src/usr.sbin/sendmail/src/conf.c
+++ usr/src/usr.sbin/sendmail/src/conf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)conf.c	8.57 (Berkeley) %G%";
+static char sccsid[] = "@(#)conf.c	8.58 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
