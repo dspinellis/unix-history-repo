ba353e11d7495a2a1163ac0f48f1e0e411c3ecff
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1994-01-02 22:17:49 -0800

    -w should set iflag, so the output looks like the old 'netstat 1' From: "Chris G. Demetriou" <cgd@vangogh.CS.Berkeley.EDU>
    SCCS-vsn: usr.bin/netstat/main.c 8.2

--- usr/src/usr.bin/netstat/main.c
+++ usr/src/usr.bin/netstat/main.c
@@ -12,7 +12,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)main.c	8.1 (Berkeley) %G%";
+static char sccsid[] = "@(#)main.c	8.2 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/param.h>
@@ -284,6 +284,7 @@ main(argc, argv)
 			break;
 		case 'w':
 			interval = atoi(optarg);
+			iflag = 1;
 			break;
 		case '?':
 		default:
