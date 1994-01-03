8ada9804bd1b24c304101cc3ea0af3b2fa1671e2
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1994-01-02 18:41:26 -0800

    rewrite man to make the configuration file more flexible
    SCCS-vsn: usr.bin/man/pathnames.h 8.2

--- usr/src/usr.bin/man/pathnames.h
+++ usr/src/usr.bin/man/pathnames.h
@@ -4,9 +4,14 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)pathnames.h	8.1 (Berkeley) %G%
+ *	@(#)pathnames.h	8.2 (Berkeley) %G%
  */
 
+#ifdef DEBUG
+#define	_PATH_MANCONF	"./man.conf"
+#else
 #define	_PATH_MANCONF	"/etc/man.conf"
+#endif
 #define	_PATH_PAGER	"/usr/bin/more -s"
 #define	_PATH_WHATIS	"whatis.db"
+#define	_PATH_TMP	"/tmp/man.XXXXXX"
