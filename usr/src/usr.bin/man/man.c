d5349a3a1f84c00f5e9e3372765b09dc7bf74e8b
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1994-01-02 18:30:13 -0800

    rewrite man to make the configuration file more flexible
    SCCS-vsn: usr.bin/man/man.c 8.3

--- usr/src/usr.bin/man/man.c
+++ usr/src/usr.bin/man/man.c
@@ -12,59 +12,77 @@ static char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)man.c	8.2 (Berkeley) %G%";
+static char sccsid[] = "@(#)man.c	8.3 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/param.h>
+#include <sys/queue.h>
 
 #include <ctype.h>
+#include <err.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <fnmatch.h>
+#include <glob.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
+#include "config.h"
 #include "pathnames.h"
 
-extern int errno;
+int f_all, f_where;
 
-int f_all, f_cat, f_how, f_where;
-char *command, *machine, *p_augment, *p_path, *pager, *progname;
-extern char **arorder, *pathbuf;
+static void	 build_page __P((char *, char *));
+static void	 cat __P((char *));
+static char	*check_pager __P((char *));
+static void	 cleanup __P((void));
+static void	 how __P((char *));
+static void	 jump __P((char **, char *, char *));
+static int	 manual __P((char *, ENTRY *, glob_t *));
+static void	 onsig __P((int));
+static void	 usage __P((void));
 
+int
 main(argc, argv)
 	int argc;
-	register char **argv;
+	char *argv[];
 {
 	extern char *optarg;
 	extern int optind;
-	int ch, res;
-	char *section[2], *check_pager(), *getpath(), **getorder(), *tmp;
+	ENTRY *defp, *defnewp, *intmpp;
+	ENTRY *section, *sectp, *sectnewp, *subp, *tp;
+	glob_t pg;
+	size_t len;
+	int ch, f_cat, f_how, found;
+	char **ap, *cmd, *machine, *p, *p_add, *p_path, *pager, *slashp;
+	char buf[MAXPATHLEN * 2];
 
-	progname = "man";
+	f_cat = f_how = 0;
 	while ((ch = getopt(argc, argv, "-acfhkM:m:P:w")) != EOF)
-		switch((char)ch) {
+		switch (ch) {
 		case 'a':
 			f_all = 1;
 			break;
 		case 'c':
-		case '-':		/* deprecated */
+		case '-':		/* Deprecated. */
 			f_cat = 1;
 			break;
 		case 'h':
 			f_how = 1;
 			break;
 		case 'm':
-			p_augment = optarg;
+			p_add = optarg;
 			break;
 		case 'M':
-		case 'P':		/* backward compatibility */
+		case 'P':		/* Backward compatibility. */
 			p_path = optarg;
 			break;
 		/*
-		 * "man -f" and "man -k" are backward compatible, undocumented
-		 * ways of calling whatis(1) and apropos(1).
+		 * The -f and -k options are backward compatible,
+		 * undocumented ways of calling whatis(1) and apropos(1).
 		 */
 		case 'f':
 			jump(argv, "-f", "whatis");
@@ -93,104 +111,457 @@ main(argc, argv)
 		else
 			pager = _PATH_PAGER;
 
-	if (!(machine = getenv("MACHINE")))
+	/* Read the configuration file. */
+	config();
+
+	/* If there's no _default list, create an empty one. */
+	if ((defp = getlist("_default")) == NULL)
+		defp = addlist("_default");
+
+	/* Get the machine type. */
+	if ((machine = getenv("MACHINE")) == NULL)
 		machine = MACHINE;
 
-	/* see if checking in a specific section */
-	if (argc > 1 && getsection(*argv)) {
-		section[0] = *argv++;
-		section[1] = (char *)NULL;
-	} else {
-		section[0] = "_default";
-		section[1] = (char *)NULL;
-	}
-
-	arorder = getorder();
-	if (p_path || (p_path = getenv("MANPATH"))) {
-		char buf[MAXPATHLEN], **av;
-
-		tmp = strtok(p_path, ":");
-		while (tmp) {
-			(void)snprintf(buf, sizeof(buf), "%s/", tmp);
-			for (av = arorder; *av; ++av)
-				cadd(buf, strlen(buf), *av);
-			tmp = strtok(NULL, ":");
+	/*
+	 * 1: If the user specified a MANPATH variable, or set the -M
+	 *    option, we replace the _default list with the user's list,
+	 *    appending the entries in the _subdir list and the machine.
+	 */
+	if (p_path == NULL)
+		p_path = getenv("MANPATH");
+	if (p_path != NULL) {
+		while ((tp = defp->list.qe_next) != NULL) {
+			free(tp->s);
+			queue_remove(&defp->list, tp, ENTRY *, list);
 		}
-		p_path = pathbuf;
-	} else if (!(p_path = getpath(section)) && !p_augment) {
-		(void)fprintf(stderr,
-			"man: no place to search for those manual pages.\n");
-		exit(1);
+		for (p = strtok(p_path, ":");
+		    p != NULL; p = strtok(NULL, ":")) {
+			slashp = p[strlen(p) - 1] == '/' ? "" : "/";
+			subp = getlist("_subdir");
+			if (subp != NULL)
+				subp = subp->list.qe_next;
+			for (; subp != NULL; subp = subp->list.qe_next) {
+				(void)snprintf(buf, sizeof(buf), "%s%s%s{/%s,}",
+				    p, slashp, subp->s, machine);
+				if ((tp = malloc(sizeof(ENTRY))) == NULL ||
+				    (tp->s = strdup(buf)) == NULL)
+					err(1, NULL);
+				queue_enter_tail(&defp->list,
+				    tp, ENTRY *, list);
+			}
+		}
 	}
 
-	for (; *argv; ++argv) {
-		if (p_augment)
-			res = manual(p_augment, *argv);
-		res = manual(p_path, *argv);
-		if (!res && !f_where)
-			(void)fprintf(stderr,
-			    "man: no entry for %s in the manual.\n", *argv);
+	/*
+	 * 2: If the user did not specify MANPATH, -M or a section, rewrite
+	 *    the _default list to include the _subdir list and the machine.
+	 */
+	if ((section = getlist(*argv)) != NULL)
+		++argv;
+	if (p_path == NULL && section == NULL) {
+		defnewp = addlist("_default_new");
+		if (defp->list.qe_next != NULL)
+			defp = defp->list.qe_next;
+		for (; defp != NULL; defp = defp->list.qe_next) {
+			slashp = defp->s[strlen(defp->s) - 1] == '/' ? "" : "/";
+			subp = getlist("_subdir");
+			if (subp != NULL)
+				subp = subp->list.qe_next;
+			for (; subp != NULL; subp = subp->list.qe_next) {
+				(void)snprintf(buf, sizeof(buf), "%s%s%s{/%s,}",
+				defp->s, slashp, subp->s, machine);
+				if ((tp = malloc(sizeof(ENTRY))) == NULL ||
+				    (tp->s = strdup(buf)) == NULL)
+					err(1, NULL);
+				queue_enter_tail(&defnewp->list,
+				    tp, ENTRY *, list);
+			}
+		}
+		defp = getlist("_default");
+		while ((tp = defp->list.qe_next) != NULL) {
+			free(tp->s);
+			queue_remove(&defp->list, tp, ENTRY *, list);
+		}
+		free(defp->s);
+		queue_remove(&defp->tags, defp, ENTRY *, tags);
+		defnewp = getlist("_default_new");
+		free(defnewp->s);
+		defnewp->s = "_default";
+		defp = defnewp;
 	}
 
-	/* use system(3) in case someone's pager is "pager arg1 arg2" */
-	if (command)
-		(void)system(command);
+	/*
+	 * 3: If the user set the -m option, insert the user's list before
+	 *    whatever list we have, again appending the _subdir list and
+	 *    the machine.
+	 */
+	if (p_add != NULL)
+		for (p = strtok(p_add, ":"); p != NULL; p = strtok(NULL, ":")) {
+			slashp = p[strlen(p) - 1] == '/' ? "" : "/";
+			subp = getlist("_subdir");
+			if (subp != NULL)
+				subp = subp->list.qe_next;
+			for (; subp != NULL; subp = subp->list.qe_next) {
+				(void)snprintf(buf, sizeof(buf), "%s%s%s{/%s,}",
+				    p, slashp, subp->s, machine);
+				if ((tp = malloc(sizeof(ENTRY))) == NULL ||
+				    (tp->s = strdup(buf)) == NULL)
+					err(1, NULL);
+				queue_enter_head(&defp->list,
+				    tp, ENTRY *, list);
+			}
+		}
+
+	/*
+	 * 4: If none of MANPATH, -M, or -m were specified, and a section was,
+	 *    rewrite the section's paths (if they have a trailing slash) to
+	 *    append the _subdir list and the machine.  This then becomes the
+	 *    _default list.
+	 */
+	if (p_path == NULL && p_add == NULL && section != NULL) {
+		sectnewp = addlist("_section_new");
+		if ((sectp = section)->list.qe_next != NULL)
+			sectp = sectp->list.qe_next;
+		while (sectp != NULL) {
+			if (sectp->s[strlen(sectp->s) - 1] != '/') {
+				tp = sectp;
+				sectp = sectp->list.qe_next;
+				queue_remove(&section->list, tp, ENTRY *, list);
+				queue_enter_tail(&sectnewp->list,
+				    tp, ENTRY *, list);
+				continue;
+			}
+			subp = getlist("_subdir");
+			if (subp != NULL)
+				subp = subp->list.qe_next;
+			for (; subp != NULL; subp = subp->list.qe_next) {
+				(void)snprintf(buf, sizeof(buf),
+				    "%s%s{/%s,}", sectp->s, subp->s, machine);
+				if ((tp = malloc(sizeof(ENTRY))) == NULL ||
+				    (tp->s = strdup(buf)) == NULL)
+					err(1, NULL);
+				queue_enter_tail(&sectnewp->list,
+				    tp, ENTRY *, list);
+			}
+			sectp = sectp->list.qe_next;
+		}
+		sectnewp->s = section->s;
+		defp = sectnewp;
+		queue_remove(&section->tags, section, ENTRY *, tags);
+	}
+
+	/*
+	 * 5: Search for the files.  Set up an interrupt handler, so the
+	 *    temporary files go away.
+	 */
+	(void)signal(SIGINT, onsig);
+
+	memset(&pg, 0, sizeof(pg));
+	for (found = 0; *argv; ++argv)
+		if (manual(*argv, defp, &pg))
+			found = 1;
+
+	/*
+	 * 7: If nothing found, we're done.
+	 */
+	if (!found) {
+		cleanup();
+		exit (1);
+	}
+
+	/* 8: If it's simple, display it fast. */
+	if (f_cat) {
+		found = 0;
+		for (ap = pg.gl_pathv; *ap != NULL; ++ap) {
+			if (*ap == '\0')
+				continue;
+			cat(*ap);
+			if (!f_all) {
+				found = 1;
+				break;
+			}
+		}
+		if (!found) {
+			if (intmpp != NULL)
+				intmpp = intmpp->list.qe_next;
+			for (; intmpp != NULL; intmpp = intmpp->list.qe_next) {
+				cat(intmpp->s);
+				if (!f_all)
+					break;
+			}
+		}
+		cleanup();
+		exit (0);
+	}
+	if (f_how) {
+		found = 0;
+		for (ap = pg.gl_pathv; *ap != NULL; ++ap) {
+			if (*ap == '\0')
+				continue;
+			how(*ap);
+			if (!f_all) {
+				found = 1;
+				break;
+			}
+		}
+		if (!found) {
+			intmpp = getlist("_intmp");
+			if (intmpp != NULL)
+				intmpp = intmpp->list.qe_next;
+			for (; intmpp != NULL; intmpp = intmpp->list.qe_next) {
+				how(intmpp->s);
+				if (!f_all)
+					break;
+			}
+		}
+		cleanup();
+		exit (0);
+	}
+	if (f_where) {
+		for (ap = pg.gl_pathv; *ap != NULL; ++ap) {
+			if (*ap == '\0')
+				continue;
+			(void)printf("%s\n", *ap);
+		}
+		intmpp = getlist("_intmp");
+		if (intmpp != NULL)
+			intmpp = intmpp->list.qe_next;
+		for (; intmpp != NULL; intmpp = intmpp->list.qe_next)
+			(void)printf("%s\n", intmpp->s);
+		cleanup();
+		exit (0);
+	}
+		
+	/*
+	 * 9: We display things in a single command; build a list of things
+	 *    to display.
+	 */
+	found = 0;
+	for (ap = pg.gl_pathv, len = strlen(pager) + 1; *ap != NULL; ++ap) {
+		if (**ap == '\0')
+			continue;
+		len += strlen(*ap) + 1;
+		if (!f_all) {
+			found = 1;
+			break;
+		}
+	}
+	if (!found) {
+		intmpp = getlist("_intmp");
+		if (intmpp != NULL)
+			intmpp = intmpp->list.qe_next;
+		for (; intmpp != NULL; intmpp = intmpp->list.qe_next) {
+			len += strlen(intmpp->s);
+			if (!f_all)
+				break;
+		}
+	}
+
+	if ((cmd = malloc(len)) == NULL) {
+		cleanup();
+		err(1, NULL);
+	}
+	p = cmd;
+	len = strlen(pager);
+	memmove(p, pager, len);
+	p += len;
+	*p++ = ' ';
+	found = 0;
+	for (ap = pg.gl_pathv; *ap != NULL; ++ap) {
+		if (**ap == '\0')
+			continue;
+		len = strlen(*ap);
+		memmove(p, *ap, len);
+		p += len;
+		*p++ = ' ';
+		if (!f_all) {
+			found = 1;
+			break;
+		}
+	}
+	if (!found) {
+		intmpp = getlist("_intmp");
+		if (intmpp != NULL)
+			intmpp = intmpp->list.qe_next;
+		for (; intmpp != NULL; intmpp = intmpp->list.qe_next) {
+			len = strlen(intmpp->s);
+			memmove(p, intmpp->s, len);
+			p += len;
+			*p++ = ' ';
+		}
+	}
+	*p = '\0';
+
+	/* Use system(3) in case someone's pager is "pager arg1 arg2". */
+	(void)system(cmd);
+
+	cleanup();
 	exit(0);
 }
 
 /*
  * manual --
- *	given a path, a directory list and a file name, find a file
- *	that matches; check ${directory}/${dir}/{file name} and
- *	${directory}/${dir}/${machine}/${file name}.
+ *	Search the manuals for the pages.
  */
-manual(path, name)
-	char *path, *name;
+static int
+manual(page, list, pg)
+	char *page;
+	ENTRY *list;
+	glob_t *pg;
 {
-	register int res;
-	register char *cp;
-	char fname[MAXPATHLEN + 1];
+	ENTRY *listp, *missp, *sufp, *tp;
+	int anyfound, cnt, found;
+	char *p, buf[128];
 
-	for (res = 0; path != NULL && *path != '\0'; path = cp) {
-		if (cp = strchr(path, ':')) {
-			if (cp == path + 1) {		/* foo::bar */
-				++cp;
+	anyfound = 0;
+	buf[0] = '*';
+
+	/* For each element in the list... */
+	if (list != NULL)
+		list = list->list.qe_next;
+	for (listp = list; listp != NULL; listp = listp->list.qe_next) {
+		(void)snprintf(buf, sizeof(buf), "%s/%s.*", listp->s, page);
+		if (glob(buf,
+		    GLOB_APPEND | GLOB_NOSORT | GLOB_BRACE, NULL, pg)) {
+			cleanup();
+			err(1, "globbing");
+		}
+		if (pg->gl_matchc == 0)
+			continue;
+
+		/* Find out if it's really a man page. */
+		for (cnt = 1; cnt <= pg->gl_matchc; ++cnt) {
+
+			/* Try the _suffix key words first. */
+			sufp = getlist("_suffix");
+			if (sufp != NULL)
+				sufp = sufp->list.qe_next;
+			for (found = 0;
+			    sufp != NULL; sufp = sufp->list.qe_next) {
+				(void)snprintf(buf,
+				     sizeof(buf), "*%s", sufp->s);
+				if (!fnmatch(buf,
+				    pg->gl_pathv[pg->gl_pathc - cnt], 0)) {
+					found = 1;
+					break;
+				}
+			}
+			if (found) {
+				anyfound = 1;
+				if (!f_all)
+					break;
 				continue;
 			}
-			*cp = '\0';
-		}
-		(void)snprintf(fname, sizeof(fname), "%s/%s.0", path, name);
-		if (access(fname, R_OK)) {
-			(void)snprintf(fname, sizeof(fname),
-			    "%s/%s/%s.0", path, machine, name);
-			if (access(fname, R_OK)) {
-				if (cp != NULL)
-					*cp++ = ':';
+
+			/* Try the _build key words next. */
+			sufp = getlist("_build");
+			if (sufp != NULL)
+				sufp = sufp->list.qe_next;
+			for (found = 0;
+			    sufp != NULL; sufp = sufp->list.qe_next) {
+				for (p = sufp->s;
+				    *p != '\0' && !isspace(*p); ++p);
+				if (*p == '\0')
+					continue;
+				*p = '\0';
+				(void)snprintf(buf,
+				     sizeof(buf), "*%s", sufp->s);
+				if (!fnmatch(buf,
+				    pg->gl_pathv[pg->gl_pathc - cnt], 0)) {
+					if (!f_where) {
+						build_page(p + 1,
+						    pg->gl_pathv[pg->gl_pathc -
+						    cnt]);
+						pg->gl_pathv[pg->gl_pathc -
+						    cnt] = "";
+					}
+					*p = ' ';
+					found = 1;
+					break;
+				}
+				*p = ' ';
+			}
+			if (found) {
+				anyfound = 1;
+				if (!f_all)
+					break;
 				continue;
 			}
+
+			/* It's not a man page, forget about it. */
+			pg->gl_pathv[pg->gl_pathc - cnt] = "";
 		}
 
-		if (f_where)
-			(void)printf("man: found in %s.\n", fname);
-		else if (f_cat)
-			cat(fname);
-		else if (f_how)
-			how(fname);
-		else
-			add(fname);
-		if (!f_all)
-			return(1);
-		res = 1;
-		if (cp != NULL)
-			*cp++ = ':';
+		if (anyfound && !f_all)
+			break;
 	}
-	return(res);
+
+	/* If not found, enter onto the missing list. */
+	if (!anyfound) {
+		if ((missp = getlist("_missing")) == NULL)
+			missp = addlist("_missing");
+		if ((tp = malloc(sizeof(ENTRY))) == NULL ||
+		    (tp->s = strdup(page)) == NULL) {
+			cleanup();
+			err(1, NULL);
+		}
+		queue_enter_tail(&missp->list, tp, ENTRY *, list);
+	}
+	return (anyfound);
+}
+
+/* 
+ * build_page --
+ *	Build a man page for display.
+ */
+static void
+build_page(fmt, path)
+	char *fmt, *path;
+{
+	static int warned;
+	ENTRY *intmpp, *tp;
+	int fd;
+	char buf[MAXPATHLEN], cmd[MAXPATHLEN], tpath[sizeof(_PATH_TMP)];
+
+	/* Let the user know this may take awhile. */
+	if (!warned) {
+		warned = 1;
+		warnx("Formatting manual page...");
+	}
+
+	/* Add an "in tmp" list. */
+	if ((intmpp = getlist("_intmp")) == NULL)
+		intmpp = addlist("_intmp");
+
+	/* Move to the printf(3) format string. */
+	for (; *fmt && isspace(*fmt); ++fmt);
+
+	/*
+	 * Get a temporary file and build a version of the file to display.
+	 * Link the built file into the list.
+	 */
+	(void)strcpy(tpath, _PATH_TMP);
+	if ((fd = mkstemp(tpath)) == -1) {
+		cleanup();
+		err(1, "%s", tpath);
+	}
+	(void)snprintf(buf, sizeof(buf), "%s > %s", fmt, tpath);
+	(void)snprintf(cmd, sizeof(cmd), buf, path);
+	(void)system(cmd);
+	(void)close(fd);
+	if ((tp = malloc(sizeof(ENTRY))) == NULL ||
+	    (tp->s = strdup(tpath)) == NULL) {
+		cleanup();
+		err(1, NULL);
+	}
+	queue_enter_tail(&intmpp->list, tp, ENTRY *, list);
 }
 
 /*
  * how --
  *	display how information
  */
+static void
 how(fname)
 	char *fname;
 {
@@ -201,8 +572,8 @@ how(fname)
 	char buf[BUFSIZ];
 
 	if (!(fp = fopen(fname, "r"))) {
-		(void)fprintf(stderr, "man: %s: %s\n", fname, strerror(errno));
-		exit(1);
+		cleanup();
+		err(1, "%s", fname);
 	}
 #define	S1	"SYNOPSIS"
 #define	S2	"S\bSY\bYN\bNO\bOP\bPS\bSI\bIS\bS"
@@ -229,10 +600,12 @@ how(fname)
 	}
 	(void)fclose(fp);
 }
+
 /*
  * cat --
  *	cat out the file
  */
+static void
 cat(fname)
 	char *fname;
 {
@@ -240,57 +613,26 @@ cat(fname)
 	char buf[BUFSIZ];
 
 	if ((fd = open(fname, O_RDONLY, 0)) < 0) {
-		(void)fprintf(stderr, "man: %s: %s\n", fname, strerror(errno));
-		exit(1);
+		cleanup();
+		err(1, "%s", fname);
 	}
 	while ((n = read(fd, buf, sizeof(buf))) > 0)
-		if (write(1, buf, n) != n) {
-			(void)fprintf(stderr,
-			    "man: write: %s\n", strerror(errno));
-			exit(1);
+		if (write(STDOUT_FILENO, buf, n) != n) {
+			cleanup();
+			err(1, "write");
 		}
 	if (n == -1) {
-		(void)fprintf(stderr, "man: read: %s\n", strerror(errno));
-		exit(1);
+		cleanup();
+		err(1, "read");
 	}
 	(void)close(fd);
-}
-
-/*
- * add --
- *	add a file name to the list for future paging
- */
-add(fname)
-	char *fname;
-{
-	static u_int buflen;
-	static int len;
-	static char *cp;
-	int flen;
-
-	if (!command) {
-		if (!(command = malloc(buflen = 1024)))
-			enomem();
-		len = strlen(strcpy(command, pager));
-		cp = command + len;
-	}
-	flen = strlen(fname);
-	if (len + flen + 2 > buflen) {		/* +2 == space, EOS */
-		if (!(command = realloc(command, buflen += 1024)))
-			enomem();
-		cp = command + len;
-	}
-	*cp++ = ' ';
-	len += flen + 1;			/* +1 = space */
-	(void)strcpy(cp, fname);
-	cp += flen;
 }
 
 /*
  * check_pager --
  *	check the user supplied page information
  */
-char *
+static char *
 check_pager(name)
 	char *name;
 {
@@ -312,7 +654,7 @@ check_pager(name)
 		/* allocate space to add the "-s" */
 		if (!(name =
 		    malloc((u_int)(strlen(save) + sizeof("-s") + 1))))
-			enomem();
+			err(1, NULL);
 		(void)sprintf(name, "%s %s", save, "-s");
 	}
 	return(name);
@@ -322,11 +664,11 @@ check_pager(name)
  * jump --
  *	strip out flag argument and jump
  */
+static void
 jump(argv, flag, name)
-	char **argv, *name;
-	register char *flag;
+	char **argv, *flag, *name;
 {
-	register char **arg;
+	char **arg;
 
 	argv[0] = name;
 	for (arg = argv + 1; *arg; ++arg)
@@ -339,10 +681,53 @@ jump(argv, flag, name)
 	exit(1);
 }
 
+/* 
+ * onsig --
+ *	If signaled, delete the temporary files.
+ */
+static void
+onsig(signo)
+	int signo;
+{
+	cleanup();
+
+	(void)signal(SIGINT, SIG_DFL);
+	(void)kill(getpid(), SIGINT);
+}
+
+/*
+ * cleanup --
+ *	Clean up temporary files, show any error messages.
+ */
+static void
+cleanup()
+{
+	ENTRY *intmpp, *missp;
+	int sverrno;
+
+	sverrno = errno;
+
+	missp = getlist("_missing");
+	if (missp != NULL)
+		missp = missp->list.qe_next;
+	if (missp != NULL)
+		for (; missp != NULL; missp = missp->list.qe_next)
+			warnx("no entry for %s in the manual.", missp->s);
+
+	intmpp = getlist("_intmp");
+	if (intmpp != NULL)
+		intmpp = intmpp->list.qe_next;
+	for (; intmpp != NULL; intmpp = intmpp->list.qe_next)
+		(void)unlink(intmpp->s);
+
+	errno = sverrno;
+}
+
 /*
  * usage --
  *	print usage message and die
  */
+static void
 usage()
 {
 	(void)fprintf(stderr,
