9c3da884671626918fb740ddd6141fde14648a0f
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1994-01-02 18:29:47 -0800

    argc needs to be decremented, as it's tested later
    SCCS-vsn: usr.bin/man/man.c 8.2

--- usr/src/usr.bin/man/man.c
+++ usr/src/usr.bin/man/man.c
@@ -12,7 +12,7 @@ static char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)man.c	8.1 (Berkeley) %G%";
+static char sccsid[] = "@(#)man.c	8.2 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/param.h>
@@ -79,6 +79,7 @@ main(argc, argv)
 		default:
 			usage();
 		}
+	argc -= optind;
 	argv += optind;
 
 	if (!*argv)
