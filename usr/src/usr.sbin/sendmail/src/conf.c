b884ec589b2aff366a514d765552df811b7ff4e2
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-01-03 17:31:11 -0800

    Convex portability
    SCCS-vsn: usr.sbin/sendmail/src/conf.c 8.59

--- usr/src/usr.sbin/sendmail/src/conf.c
+++ usr/src/usr.sbin/sendmail/src/conf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)conf.c	8.58 (Berkeley) %G%";
+static char sccsid[] = "@(#)conf.c	8.59 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
