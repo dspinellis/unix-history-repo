17d3b3b3679913dc46ae6d65994a27610b7dc0a9
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1994-01-05 01:14:33 -0800

    do not need to do anything for cpu_set_init_frame() as the PMAX stores registers in u.u_pcb rather than on stack (i.e., fork() sets p_md.md_regs)
    SCCS-vsn: sys/pmax/include/cpu.h 8.4

--- usr/src/sys/pmax/include/cpu.h
+++ usr/src/sys/pmax/include/cpu.h
@@ -7,7 +7,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)cpu.h	8.3 (Berkeley) %G%
+ *	@(#)cpu.h	8.4 (Berkeley) %G%
  */
 
 #ifndef _CPU_H_
@@ -28,7 +28,7 @@
 #define	cpu_exec(p)		(p->p_md.md_ss_addr = 0) /* init single step */
 #define	cpu_wait(p)		/* nothing */
 #define cpu_setstack(p, ap)	(p)->p_md.md_regs[SP] = ap
-#define cpu_set_init_frame(p, fp) (p)->p_md.md_regs = fp
+#define cpu_set_init_frame(p, fp) /* nothing */
 
 /*
  * Arguments to hardclock and gatherstats encapsulate the previous
