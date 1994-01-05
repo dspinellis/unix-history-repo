3ab7d35ee68cf508b40dfc8994540fd17c840641
Wiljo Heinen <wiljo@freeside.ki.open.de>
1994-01-05 03:30:41 +0000

    Replaced makefile with BSD Makefile. Removed z_abs.c because we have the functions in our math library. Changed function f_cabs in cabs.c to call cabs(). Removed checksums.

--- lib/libF77/cabs.c
+++ lib/libF77/cabs.c
@@ -7,21 +7,8 @@ double f__cabs(real, imag) double real, imag;
 double f__cabs(double real, double imag)
 #endif
 {
-double temp;
-
-if(real < 0)
-	real = -real;
-if(imag < 0)
-	imag = -imag;
-if(imag > real){
-	temp = real;
-	real = imag;
-	imag = temp;
-}
-if((real+imag) == real)
-	return(real);
-
-temp = imag/real;
-temp = real*sqrt(1.0 + temp*temp);  /*overflow!!*/
-return(temp);
+struct {double x, y;} z;
+z.x = real;
+z.y = imag;
+return cabs(z);
 }
