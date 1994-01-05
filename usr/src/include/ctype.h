e9b66ff1ea8c676d5eb2b83c2227ed48fdb32d01
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1994-01-04 23:44:27 -0800

    runetype.h is ANSI safe, need to include it so that __isctype works
    SCCS-vsn: include/ctype.h 8.3

--- usr/src/include/ctype.h
+++ usr/src/include/ctype.h
@@ -7,15 +7,13 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)ctype.h	8.2 (Berkeley) %G%
+ *	@(#)ctype.h	8.3 (Berkeley) %G%
  */
 
 #ifndef	_CTYPE_H_
 #define _CTYPE_H_
 
-#ifndef _ANSI_SOURCE
 #include <runetype.h>
-#endif
 
 #define	_A	0x00000100L		/* Alpha */
 #define	_C	0x00000200L		/* Control */
