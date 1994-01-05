b1142a5a792cadb0d2f5b00649c03a5b95f0d3ca
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-01-04 23:02:44 -0800

    make sure route-addrs have <angle brackets> in envelope
    SCCS-vsn: usr.sbin/sendmail/src/parseaddr.c 8.29

--- usr/src/usr.sbin/sendmail/src/parseaddr.c
+++ usr/src/usr.sbin/sendmail/src/parseaddr.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)parseaddr.c	8.28 (Berkeley) %G%";
+static char sccsid[] = "@(#)parseaddr.c	8.29 (Berkeley) %G%";
 #endif /* not lint */
 
 #include "sendmail.h"
