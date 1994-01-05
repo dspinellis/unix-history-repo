a6f32e4c6f128f73152a3494ee4307c4a66e080a
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1994-01-04 21:39:37 -0800

    include <machine/limits.h>; POSIX.1 requires time.h to have CLK_TCK
    SCCS-vsn: include/time.h 8.2

--- usr/src/include/time.h
+++ usr/src/include/time.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)time.h	8.1 (Berkeley) %G%
+ *	@(#)time.h	8.2 (Berkeley) %G%
  */
 
 #ifndef _TIME_H_
@@ -44,6 +44,8 @@ struct tm {
 	long	tm_gmtoff;	/* offset from CUT in seconds */
 	char	*tm_zone;	/* timezone abbreviation */
 };
+
+#include <machine/limits.h>	/* Include file containing CLK_TCK. */
 
 #include <sys/cdefs.h>
 
