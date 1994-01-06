1267597335642ddc865ee1f095022e2d186cef9b
Jan-Simon Pendry <pendry@ucbvax.Berkeley.EDU>
1994-01-06 07:57:59 -0800

    don't need vget to lock the vnode
    SCCS-vsn: sys/miscfs/procfs/procfs_subr.c 8.2

--- usr/src/sys/miscfs/procfs/procfs_subr.c
+++ usr/src/sys/miscfs/procfs/procfs_subr.c
@@ -8,7 +8,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)procfs_subr.c	8.1 (Berkeley) %G%
+ *	@(#)procfs_subr.c	8.2 (Berkeley) %G%
  *
  * From:
  *	$Id: procfs_subr.c,v 3.2 1993/12/15 09:40:17 jsp Exp $
@@ -68,9 +68,8 @@ loop:
 		if (pfs->pfs_pid == pid &&
 		    pfs->pfs_type == pfs_type &&
 		    PFSTOV(pfs)->v_mount == mp) {
-			if (vget(pfs->pfs_vnode, 1))
+			if (vget(pfs->pfs_vnode, 0))
 				goto loop;
-			VOP_UNLOCK(pfs->pfs_vnode);
 			*vpp = pfs->pfs_vnode;
 			return (0);
 		}
