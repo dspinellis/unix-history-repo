53798164802276883ad859822f0444bb17b6df21
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-01-05 17:20:54 -0800

    allow * in /etc/shells to match anything
    SCCS-vsn: usr.sbin/sendmail/src/conf.c 8.60

--- usr/src/usr.sbin/sendmail/src/conf.c
+++ usr/src/usr.sbin/sendmail/src/conf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)conf.c	8.59 (Berkeley) %G%";
+static char sccsid[] = "@(#)conf.c	8.60 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -1378,7 +1378,7 @@ usershellok(shell)
 
 	setusershell();
 	while ((p = getusershell()) != NULL)
-		if (strcmp(p, shell) == 0)
+		if (strcmp(p, shell) == 0 || strcmp(p, "*") == 0)
 			break;
 	endusershell();
 	return p != NULL;
@@ -1407,7 +1407,7 @@ usershellok(shell)
 		while (*p != '\0' && *p != '#' && !isspace(*p))
 			p++;
 		*p = '\0';
-		if (strcmp(shell, q) == 0)
+		if (strcmp(shell, q) == 0 || strcmp("*", q) == 0)
 		{
 			fclose(shellf);
 			return TRUE;
