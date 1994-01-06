12325116cd4f3e96d2d3e3601af6fae0443c8ccb
Jan-Simon Pendry <pendry@ucbvax.Berkeley.EDU>
1994-01-05 20:30:35 -0800

    remove debug code, general cleanup.  KNF.
    SCCS-vsn: sys/deprecated/lofs/lofs.h 8.4
    SCCS-vsn: sys/deprecated/lofs/lofs_subr.c 8.4
    SCCS-vsn: sys/deprecated/lofs/lofs_vfsops.c 8.2
    SCCS-vsn: sys/deprecated/lofs/lofs_vnops.c 8.2

--- usr/src/sys/deprecated/lofs/lofs.h
+++ usr/src/sys/deprecated/lofs/lofs.h
@@ -8,7 +8,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)lofs.h	8.3 (Berkeley) %G%
+ *	@(#)lofs.h	8.4 (Berkeley) %G%
  *
  * $Id: lofs.h,v 1.8 1992/05/30 10:05:43 jsp Exp jsp $
  */
@@ -33,16 +33,12 @@ struct lofsnode {
 	struct vnode	*a_vnode;	/* Back pointer to vnode/lofsnode */
 };
 
+extern int lofs_init __P((void));
 extern int make_lofs __P((struct mount *mp, struct vnode **vpp));
 
 #define	VFSTOLOFS(mp) ((struct lofsmount *)((mp)->mnt_data))
 #define	LOFSP(vp) ((struct lofsnode *)(vp)->v_data)
-#ifdef LOFS_DIAGNOSTIC
-extern struct vnode *lofs_checkvp __P((struct vnode *vp, char *fil, int lno));
-#define	LOFSVP(vp) lofs_checkvp(vp, __FILE__, __LINE__)
-#else
 #define	LOFSVP(vp) (LOFSP(vp)->a_lofsvp)
-#endif
 
 extern int (**lofs_vnodeop_p)();
 extern struct vfsops lofs_vfsops;
--- usr/src/sys/deprecated/lofs/lofs_subr.c
+++ usr/src/sys/deprecated/lofs/lofs_subr.c
@@ -8,7 +8,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)lofs_subr.c	8.3 (Berkeley) %G%
+ *	@(#)lofs_subr.c	8.4 (Berkeley) %G%
  *
  * $Id: lofs_subr.c,v 1.11 1992/05/30 10:05:43 jsp Exp jsp $
  */
@@ -48,15 +48,15 @@ static struct lofscache lofscache[NLOFSCACHE];
 /*
  * Initialise cache headers
  */
+int
 lofs_init()
 {
 	struct lofscache *ac;
-#ifdef LOFS_DIAGNOSTIC
-	printf("lofs_init\n");		/* printed during system boot */
-#endif
 
 	for (ac = lofscache; ac < lofscache + NLOFSCACHE; ac++)
 		ac->ac_forw = ac->ac_back = (struct lofsnode *) ac;
+
+	return (0);
 }
 
 /*
@@ -66,6 +66,7 @@ static struct lofscache *
 lofs_hash(targetvp)
 struct vnode *targetvp;
 {
+
 	return (&lofscache[LOFS_NHASH(targetvp)]);
 }
 
@@ -82,10 +83,6 @@ lofs_alloc(vp, targetvp)
 	struct lofscache *hd;
 	struct lofsnode *a;
 
-#ifdef LOFS_DIAGNOSTIC
-	printf("lofs_alloc(%x, %x)\n", vp, targetvp);
-#endif
-
 	MALLOC(a, struct lofsnode *, sizeof(struct lofsnode), M_TEMP, M_WAITOK);
 	a->a_vnode = vp;
 	vp->v_data = a;
@@ -94,44 +91,8 @@ lofs_alloc(vp, targetvp)
 	hd = lofs_hash(targetvp);
 	insque(a, hd);
 
-#ifdef LOFS_DIAGNOSTIC
-	vprint("alloc vp", vp);
-	vprint("alloc targetvp", targetvp);
-#endif
 }
 
-#ifdef LOFS_DIAGNOSTIC
-void
-lofs_flushmp(mp)
-	struct mount *mp;
-{
-	struct lofscache *ac;
-	int i = 0;
-	struct lofsnode *roota;
-
-	printf("lofs_flushmp(%x)\n", mp);
-
-	roota = LOFSP(VFSTOLOFS(mp)->rootvp);
-
-	for (ac = lofscache; ac < lofscache + NLOFSCACHE; ac++) {
-		struct lofsnode *a = ac->ac_forw;
-		while (a != (struct lofsnode *) ac) {
-			if (a != roota && a->a_vnode->v_mount == mp) {
-				struct vnode *vp = a->a_lofsvp;
-				if (vp) {
-					a->a_lofsvp = 0;
-					vprint("would vrele", vp);
-					/*vrele(vp);*/
-					i++;
-				}
-			}
-			a = a->a_forw;
-		}
-	}
-	if (i > 0)
-		printf("lofsnode: vrele'd %d aliases\n", i);
-}
-#endif
 
 /*
  * Return alias for target vnode if already exists, else 0.
@@ -144,10 +105,6 @@ lofs_find(mp, targetvp)
 	struct lofscache *hd;
 	struct lofsnode *a;
 
-#ifdef LOFS_DIAGNOSTIC
-	printf("lofs_find(mp = %x, target = %x)\n", mp, targetvp);
-#endif
-
 	/*
 	 * Find hash base, and then search the (two-way) linked
 	 * list looking for a lofsnode structure which is referencing
@@ -158,17 +115,9 @@ lofs_find(mp, targetvp)
 
 	for (a = hd->ac_forw; a != (struct lofsnode *) hd; a = a->a_forw) {
 		if (a->a_lofsvp == targetvp && a->a_vnode->v_mount == mp) {
-#ifdef LOFS_DIAGNOSTIC
-			printf("lofs_find(%x): found (%x,%x)->%x\n",
-				targetvp, mp, a->a_vnode, targetvp);
-#endif
 			return (a);
 		}
 	}
-
-#ifdef LOFS_DIAGNOSTIC
-	printf("lofs_find(%x, %x): NOT found\n", mp, targetvp);
-#endif
 
 	return (0);
 }
@@ -193,9 +142,6 @@ lofs_alias(mp, targetvp, newvpp)
 		/*
 		 * Take another reference to the alias vnode
 		 */
-#ifdef LOFS_DIAGNOSTIC
-		vprint("lofs_alias: exists", ap->a_vnode);
-#endif
 		aliasvp = ap->a_vnode;
 		VREF(aliasvp);
 	} else {
@@ -204,9 +150,6 @@ lofs_alias(mp, targetvp, newvpp)
 		/*
 		 * Get new vnode.
 		 */
-#ifdef LOFS_DIAGNOSTIC
-		printf("lofs_alias: create new alias vnode\n");
-#endif
 		if (error = getnewvnode(VT_LOFS, mp, lofs_vnodeop_p, &aliasvp))
 			return (error);
 
@@ -227,11 +170,6 @@ lofs_alias(mp, targetvp, newvpp)
 
 	vrele(targetvp);
 
-#ifdef LOFS_DIAGNOSTIC
-	vprint("lofs_alias alias", aliasvp);
-	vprint("lofs_alias target", targetvp);
-#endif
-
 	*newvpp = aliasvp;
 	return (0);
 }
@@ -241,25 +179,17 @@ lofs_alias(mp, targetvp, newvpp)
  * to it, otherwise make a new lofsnode vnode which
  * contains a reference to the target vnode.
  */
+int
 make_lofs(mp, vpp)
 	struct mount *mp;
 	struct vnode **vpp;
 {
 	struct vnode *targetvp;
 
-#ifdef LOFS_DIAGNOSTIC
-	printf("make_lofs(mp = %x, vp = %x\n", mp, *vpp);
-#endif
-
 	/*
 	 * (targetvp) is locked at this point.
 	 */
 	targetvp = *vpp;
-
-#ifdef LOFS_DIAGNOSTIC
-	if (targetvp == 0)
-		panic("make_lofs: null vp");
-#endif
 
 	/*
 	 * Try to find an existing reference to the target vnodes.
@@ -267,29 +197,3 @@ make_lofs(mp, vpp)
 	return (lofs_alias(mp, targetvp, vpp));
 }
 
-#ifdef LOFS_DIAGNOSTIC
-struct vnode *
-lofs_checkvp(vp, fil, lno)
-	struct vnode *vp;
-	char *fil;
-	int lno;
-{
-	struct lofsnode *a = LOFSP(vp);
-	if (a->a_lofsvp == 0) {
-		int i; u_long *p;
-		printf("vp = %x, ZERO ptr\n", vp);
-#ifdef notdef
-		for (p = (u_long *) a, i = 0; i < 8; i++)
-			printf(" %x", p[i]);
-		printf("\n");
-		DELAY(2000000);
-		panic("lofs_checkvp");
-#endif
-	}
-	printf("aliasvp %x/%d -> %x/%d [%s, %d]\n",
-		a->a_vnode, a->a_vnode->v_usecount,
-		a->a_lofsvp, a->a_lofsvp ? a->a_lofsvp->v_usecount : -42,
-		fil, lno);
-	return a->a_lofsvp;
-}
-#endif
