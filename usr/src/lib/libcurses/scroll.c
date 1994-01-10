efa68b981d21fa2fa427e374024057cda9e78bff
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1994-01-09 22:40:36 -0800

    don't need termios.h any longer
    SCCS-vsn: lib/libcurses/cr_put.c 8.2
    SCCS-vsn: lib/libcurses/scroll.c 8.2

--- usr/src/lib/libcurses/scroll.c
+++ usr/src/lib/libcurses/scroll.c
@@ -6,11 +6,10 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)scroll.c	8.1 (Berkeley) %G%";
+static char sccsid[] = "@(#)scroll.c	8.2 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <curses.h>
-#include <termios.h>
 
 /*
  * scroll --
