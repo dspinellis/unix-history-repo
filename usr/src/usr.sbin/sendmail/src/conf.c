ce382bdf9bfa9b475067adbc35c71ea9d703ea9a
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-01-09 19:09:45 -0800

    HP-UX compatibility: use correct /etc/shells default
    SCCS-vsn: usr.sbin/sendmail/src/conf.c 8.62

--- usr/src/usr.sbin/sendmail/src/conf.c
+++ usr/src/usr.sbin/sendmail/src/conf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)conf.c	8.61 (Berkeley) %G%";
+static char sccsid[] = "@(#)conf.c	8.62 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -1358,14 +1358,29 @@ vsprintf(s, fmt, ap)
 **		FALSE -- if the shell is restricted.
 */
 
-#ifndef _PATH_SHELLS
-# define _PATH_SHELLS	"/etc/shells"
+#if !HASGETUSERSHELL
+
+# ifndef _PATH_SHELLS
+#  define _PATH_SHELLS	"/etc/shells"
+# endif
+
+char	*DefaultUserShells[] =
+{
+	"/bin/sh",
+	"/usr/bin/sh",
+	"/bin/csh",
+	"/usr/bin/csh",
+#ifdef __hpux
+	"/bin/rsh",
+	"/bin/ksh",
+	"/bin/rksh",
+	"/bin/pam",
+	"/usr/bin/keysh",
+	"/bin/posix/sh",
 #endif
-#ifndef _PATH_BSHELL
-# define _PATH_BSHELL	"/bin/sh"
-#endif
-#ifndef _PATH_CSHELL
-# define _PATH_CSHELL	"/bin/csh"
+	NULL
+};
+
 #endif
 
 bool
@@ -1390,8 +1405,14 @@ usershellok(shell)
 	if (shellf == NULL)
 	{
 		/* no /etc/shells; see if it is one of the std shells */
-		return (strcmp(shell, _PATH_CSHELL) == 0 ||
-			strcmp(shell, _PATH_BSHELL) == 0);
+		char **d;
+
+		for (d = DefaultUserShells; *d != NULL; d++)
+		{
+			if (strcmp(shell, *d) == 0)
+				return TRUE;
+		}
+		return FALSE;
 	}
 
 	while (fgets(buf, sizeof buf, shellf) != NULL)
