60b5c41e565783ed931519372dc189005327be88
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-01-13 23:36:45 -0800

    patch to protocol logging for short syslog buffer sizes
    SCCS-vsn: usr.sbin/sendmail/src/headers.c 8.22

--- usr/src/usr.sbin/sendmail/src/headers.c
+++ usr/src/usr.sbin/sendmail/src/headers.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)headers.c	8.21 (Berkeley) %G%";
+static char sccsid[] = "@(#)headers.c	8.22 (Berkeley) %G%";
 #endif /* not lint */
 
 # include <errno.h>
@@ -486,7 +486,7 @@ logsender(e, msgid)
 	p = macvalue('r', e);
 	if (p != NULL)
 	{
-		sprintf(sbp, " proto=%s,", e->e_id, p);
+		sprintf(sbp, " proto=%s,", p);
 		sbp += strlen(sbp);
 	}
 	syslog(LOG_INFO, "%s relay=%s", sbuf, name);
