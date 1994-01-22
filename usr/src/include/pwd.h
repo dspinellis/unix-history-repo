678685082fbb560ae8252fbbf07b6b57a801faeb
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1994-01-22 00:53:07 -0800

    add USL's copyright notice
    SCCS-vsn: include/ar.h 8.2
    SCCS-vsn: include/assert.h 8.2
    SCCS-vsn: include/ctype.h 8.4
    SCCS-vsn: include/grp.h 8.2
    SCCS-vsn: include/nlist.h 8.2
    SCCS-vsn: include/pwd.h 8.2
    SCCS-vsn: include/setjmp.h 8.2
    SCCS-vsn: include/time.h 8.3
    SCCS-vsn: include/utmp.h 8.2

--- usr/src/include/pwd.h
+++ usr/src/include/pwd.h
@@ -1,10 +1,15 @@
 /*-
  * Copyright (c) 1989, 1993
  *	The Regents of the University of California.  All rights reserved.
+ * (c) UNIX System Laboratories, Inc.
+ * All or some portions of this file are derived from material licensed
+ * to the University of California by American Telephone and Telegraph
+ * Co. or Unix System Laboratories, Inc. and are reproduced herein with
+ * the permission of UNIX System Laboratories, Inc.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)pwd.h	8.1 (Berkeley) %G%
+ *	@(#)pwd.h	8.2 (Berkeley) %G%
  */
 
 #ifndef _PWD_H_
