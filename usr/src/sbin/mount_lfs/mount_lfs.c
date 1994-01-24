22fadb8e1e6cf203617b12de66288ec92c377319
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1994-01-23 20:29:43 -0800

    match rev 8.6 of /sys/sys/mount.h from Mike Hibler reorganization for generic support of export information
    SCCS-vsn: sbin/fsck/main.c 8.2
    SCCS-vsn: sbin/mount_lfs/mount_lfs.c 8.2

--- usr/src/sbin/mount_lfs/mount_lfs.c
+++ usr/src/sbin/mount_lfs/mount_lfs.c
@@ -12,7 +12,7 @@ static char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)mount_lfs.c	8.1 (Berkeley) %G%";
+static char sccsid[] = "@(#)mount_lfs.c	8.2 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/param.h>
@@ -73,11 +73,11 @@ main(argc, argv)
 	fs_name = argv[1];	/* the mount point */
 
 	/* copied from mount's UFS code */
-	args.exroot = DEFAULT_ROOTUID;
+	args.export.ex_root = DEFAULT_ROOTUID;
 	if (mntflags & MNT_RDONLY)
-		args.exflags = MNT_EXRDONLY;
+		args.export.ex_flags = MNT_EXRDONLY;
 	else
-		args.exflags = 0;
+		args.export.ex_flags = 0;
 
 	if (mount(MOUNT_LFS, fs_name, mntflags, &args)) {
 		(void)fprintf(stderr, "mount_lfs: %s\n", strerror(errno));
