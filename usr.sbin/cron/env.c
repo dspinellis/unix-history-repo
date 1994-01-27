0866dde1d230821f713dc677930406bd8c9068cd
Nate Willams <nate@FreeBSD.org>
1994-01-27 19:18:58 +0000

    This is Patch01 to Cron-3.0.  Please apply it.  Changes are:
    	Some of the documentation has been reformatted to fit in 80 char lines;
    	it has also been corrected on some NeXT system and general environment
    	variable details.
    
    	The EDITOR (or VISUAL) environment variable is now allowed to have
    	multiple tokens in it when running "crontab -e".
    
    	Some more code has been reformatted to the BSD style.
    
    	A bug was fixed in the way environment variables were searched.
    
    [ Nate's comments:
    
    I also chose to add the FEATURES file to our distribution as it
    documents some features that are not present in previous BSD cron's
    
    ]

--- usr.sbin/cron/env.c
+++ usr.sbin/cron/env.c
@@ -17,7 +17,8 @@
  */
 
 #if !defined(lint) && !defined(LINT)
-static char rcsid[] = "$Header: $";
+static char netid[] = "From Id: env.c,v 2.7 1994/01/26 02:25:50 vixie Exp vixie $";
+static char rcsid[] = "$Id: env.c,v 2.7 1994/01/26 02:25:50 vixie Exp vixie $";
 #endif
 
 
@@ -30,7 +31,7 @@ env_init()
 	register char	**p = (char **) malloc(sizeof(char **));
 
 	p[0] = NULL;
-	return p;
+	return (p);
 }
 
 
@@ -59,7 +60,7 @@ env_copy(envp)
 	for (i = 0;  i < count;  i++)
 		p[i] = strdup(envp[i]);
 	p[count] = NULL;
-	return p;
+	return (p);
 }
 
 
@@ -89,7 +90,7 @@ env_set(envp, envstr)
 		 */
 		free(envp[found]);
 		envp[found] = strdup(envstr);
-		return envp;
+		return (envp);
 	}
 
 	/*
@@ -101,7 +102,7 @@ env_set(envp, envstr)
 			      (unsigned) ((count+1) * sizeof(char **)));
 	p[count] = p[count-1];
 	p[count-1] = strdup(envstr);
-	return p;
+	return (p);
 }
 
 
@@ -123,7 +124,7 @@ load_env(envstr, f)
 	fileline = LineNumber;
 	skip_comments(f);
 	if (EOF == get_string(envstr, MAX_ENVSTR, f, "\n"))
-		return ERR;
+		return (ERR);
 
 	Debug(DPARS, ("load_env, read <%s>\n", envstr))
 
@@ -133,7 +134,7 @@ load_env(envstr, f)
 		Debug(DPARS, ("load_env, not 2 fields (%d)\n", fields))
 		fseek(f, filepos, 0);
 		Set_LineNum(fileline);
-		return FALSE;
+		return (FALSE);
 	}
 
 	/* 2 fields from scanf; looks like an env setting
@@ -157,7 +158,7 @@ load_env(envstr, f)
 
 	(void) sprintf(envstr, "%s=%s", name, val);
 	Debug(DPARS, ("load_env, <%s> <%s> -> <%s>\n", name, val, envstr))
-	return TRUE;
+	return (TRUE);
 }
 
 
@@ -166,10 +167,14 @@ env_get(name, envp)
 	register char	*name;
 	register char	**envp;
 {
-	for (;  *envp;  envp++) {
-		register char	*p = strchr(*envp, '=');
-		if (p && !strncmp(*envp, name, p - *envp))
-			return p+1;
+	register int	len = strlen(name);
+	register char	*p, *q;
+
+	while (p = *envp++) {
+		if (!(q = strchr(p, '=')))
+			continue;
+		if ((q - p) == len && !strncmp(p, name, len))
+			return (q+1);
 	}
-	return NULL;
+	return (NULL);
 }
