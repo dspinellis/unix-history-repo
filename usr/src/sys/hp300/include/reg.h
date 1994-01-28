e7f2c5fa98a07bcbb7d9696ee73eebc35f519a3b
Jan-Simon Pendry <pendry@ucbvax.Berkeley.EDU>
1994-01-27 19:33:10 -0800

    add struct fpreg
    SCCS-vsn: sys/hp300/include/reg.h 8.3

--- usr/src/sys/hp300/include/reg.h
+++ usr/src/sys/hp300/include/reg.h
@@ -11,7 +11,7 @@
  *
  * from: Utah $Hdr: reg.h 1.1 90/07/09$
  *
- *	@(#)reg.h	8.2 (Berkeley) %G%
+ *	@(#)reg.h	8.3 (Berkeley) %G%
  */
 
 /*
@@ -53,6 +53,13 @@ struct reg {
         int     r_regs[16];	/* numbered as above */
 	int	r_pc;
 	int	r_sr;
+};
+
+/*
+ * Register set accessible via /proc/$pid/fpreg
+ */
+struct fpreg {
+	int	fpr_xxx;	/* not implemented */
 };
 
 
