4cf2cdbb3cb8f5ea75843cdb384c304dc074d606
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-02-01 00:59:53 -0800

    cleanup of _POSIX_CHOWN_RESTRICTED stuff
    SCCS-vsn: usr.sbin/sendmail/src/recipient.c 8.42
    SCCS-vsn: usr.sbin/sendmail/src/conf.c 8.67

--- usr/src/usr.sbin/sendmail/src/conf.c
+++ usr/src/usr.sbin/sendmail/src/conf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)conf.c	8.66 (Berkeley) %G%";
+static char sccsid[] = "@(#)conf.c	8.67 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
