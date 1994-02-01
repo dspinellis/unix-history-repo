f298c639348e8a096bf06b580bf8f5d9099e8a1a
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-02-01 01:01:15 -0800

    be a bit less tricky to avoid confusing old C compilers
    SCCS-vsn: usr.sbin/sendmail/src/collect.c 8.9

--- usr/src/usr.sbin/sendmail/src/collect.c
+++ usr/src/usr.sbin/sendmail/src/collect.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)collect.c	8.8 (Berkeley) %G%";
+static char sccsid[] = "@(#)collect.c	8.9 (Berkeley) %G%";
 #endif /* not lint */
 
 # include <errno.h>
@@ -288,8 +288,11 @@ readerr:
 			    "collect: %s on connection from %s, sender=%s: %m\n",
 			    problem, host, e->e_from.q_paddr);
 # endif
-		(feof(InChannel) ? usrerr : syserr)
-			("451 collect: %s on connection from %s, from=%s",
+		if (feof(InChannel))
+			usrerr("451 collect: %s on connection from %s, from=%s",
+				problem, host, e->e_from.q_paddr);
+		else
+			syserr("451 collect: %s on connection from %s, from=%s",
 				problem, host, e->e_from.q_paddr);
 
 		/* don't return an error indication */
