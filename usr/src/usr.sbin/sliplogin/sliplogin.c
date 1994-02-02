32b0c10a691099df5e3014e9bd759f43475bc233
Chris G. Demetriou <cgd@ucbvax.Berkeley.EDU>
1994-02-02 06:18:18 -0800

    get SLIOCGUNIT definition from <net/slip.h>, kill unneeded includes
    SCCS-vsn: usr.sbin/sliplogin/sliplogin.c 8.2

--- usr/src/usr.sbin/sliplogin/sliplogin.c
+++ usr/src/usr.sbin/sliplogin/sliplogin.c
@@ -12,7 +12,7 @@ static char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)sliplogin.c	8.1 (Berkeley) %G%";
+static char sccsid[] = "@(#)sliplogin.c	8.2 (Berkeley) %G%";
 #endif /* not lint */
 
 /*
@@ -58,9 +58,7 @@ static char sccsid[] = "@(#)sliplogin.c	8.1 (Berkeley) %G%";
 #else
 #include <sgtty.h>
 #endif
-#include <netinet/in.h>
-#include <net/if.h>
-#include <net/if_slvar.h>
+#include <net/slip.h>
 
 #include <stdio.h>
 #include <errno.h>
