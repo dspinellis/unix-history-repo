c1064becd81cff98d76b563686a1d375a45556da
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-02-03 01:46:36 -0800

    fix botch in HP-UX version of chownsafe -- condition reversed
    SCCS-vsn: usr.sbin/sendmail/src/conf.c 8.70

--- usr/src/usr.sbin/sendmail/src/conf.c
+++ usr/src/usr.sbin/sendmail/src/conf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)conf.c	8.69 (Berkeley) %G%";
+static char sccsid[] = "@(#)conf.c	8.70 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
