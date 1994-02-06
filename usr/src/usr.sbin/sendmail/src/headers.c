7b644b39dd5ded5572ba8b4b8df281349582903e
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-02-05 20:56:24 -0800

    wrap headers and lines more intelligently (important for very short line lengths
    SCCS-vsn: usr.sbin/sendmail/src/headers.c 8.26
    SCCS-vsn: usr.sbin/sendmail/src/util.c 8.30

--- usr/src/usr.sbin/sendmail/src/headers.c
+++ usr/src/usr.sbin/sendmail/src/headers.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)headers.c	8.25 (Berkeley) %G%";
+static char sccsid[] = "@(#)headers.c	8.26 (Berkeley) %G%";
 #endif /* not lint */
 
 # include <errno.h>
@@ -942,6 +942,7 @@ commaize(h, p, oldstyle, mci, e)
 {
 	register char *obp;
 	int opos;
+	int omax;
 	bool firstone = TRUE;
 	char obuf[MAXLINE + 3];
 
@@ -957,6 +958,9 @@ commaize(h, p, oldstyle, mci, e)
 	(void) sprintf(obp, "%s: ", h->h_field);
 	opos = strlen(h->h_field) + 2;
 	obp += opos;
+	omax = 78;
+	if (mci->mci_mailer->m_linelimit - 2 < omax)
+		omax = mci->mci_mailer->m_linelimit - 2;
 
 	/*
 	**  Run through the list of values.
@@ -1031,7 +1035,7 @@ commaize(h, p, oldstyle, mci, e)
 		opos += strlen(name);
 		if (!firstone)
 			opos += 2;
-		if (opos > 78 && !firstone)
+		if (opos > omax && !firstone)
 		{
 			(void) strcpy(obp, ",\n");
 			putline(obuf, mci);
