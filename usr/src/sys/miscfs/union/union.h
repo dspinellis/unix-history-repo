0bd085eef8e4c8ed6cccce2dd28db093fbab8893
Jan-Simon Pendry <pendry@ucbvax.Berkeley.EDU>
1994-02-08 00:13:18 -0800

    checkpoint
    SCCS-vsn: sys/miscfs/union/union.h 1.7
    SCCS-vsn: sys/miscfs/union/union_subr.c 1.7
    SCCS-vsn: sys/miscfs/union/union_vnops.c 1.7
    SCCS-vsn: sys/miscfs/union/union_vfsops.c 1.7

--- usr/src/sys/miscfs/union/union.h
+++ usr/src/sys/miscfs/union/union.h
@@ -8,7 +8,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)union.h	1.6 (Berkeley) %G%
+ *	@(#)union.h	1.7 (Berkeley) %G%
  */
 
 struct union_args {
@@ -42,9 +42,12 @@ struct union_node {
 	struct vnode	        *un_lowervp;	/* underlying object */
 	struct vnode		*un_dirvp;	/* Parent dir of uppervp */
 	char			*un_path;	/* saved component name */
-	int			un_open;	/* # of opens on lowervp */
+	int			un_hash;	/* saved un_path hash value */
+	int			un_openl;	/* # of opens on lowervp */
 	int			un_flags;
+#ifdef DIAGNOSTIC
 	pid_t			un_pid;
+#endif
 };
 
 #define UN_WANT 0x01
@@ -60,6 +63,7 @@ extern int union_mkshadow __P((struct union_mount *, struct vnode *,
 				struct componentname *, struct vnode **));
 extern int union_vn_create __P((struct vnode **, struct union_node *,
 				struct proc *));
+extern void union_removed_upper __P((struct union_node *un));
 
 #define	MOUNTTOUNIONMOUNT(mp) ((struct union_mount *)((mp)->mnt_data))
 #define	VTOUNION(vp) ((struct union_node *)(vp)->v_data)
