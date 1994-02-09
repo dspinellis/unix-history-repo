f2f113263a836f627b84953ce8d90a3c86f46697
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-02-08 16:54:14 -0800

    change a daemon child process title ASAP
    SCCS-vsn: usr.sbin/sendmail/src/daemon.c 8.37

--- usr/src/usr.sbin/sendmail/src/daemon.c
+++ usr/src/usr.sbin/sendmail/src/daemon.c
@@ -12,9 +12,9 @@
 
 #ifndef lint
 #ifdef DAEMON
-static char sccsid[] = "@(#)daemon.c	8.36 (Berkeley) %G% (with daemon mode)";
+static char sccsid[] = "@(#)daemon.c	8.37 (Berkeley) %G% (with daemon mode)";
 #else
-static char sccsid[] = "@(#)daemon.c	8.36 (Berkeley) %G% (without daemon mode)";
+static char sccsid[] = "@(#)daemon.c	8.37 (Berkeley) %G% (without daemon mode)";
 #endif
 #endif /* not lint */
 
