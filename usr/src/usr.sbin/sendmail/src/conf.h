fecd142471a40e9de8ec1b84150b6856bb3ee513
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-02-09 15:39:55 -0800

    DG/UX doesn't have getusershell(3)
    SCCS-vsn: usr.sbin/sendmail/src/conf.h 8.86

--- usr/src/usr.sbin/sendmail/src/conf.h
+++ usr/src/usr.sbin/sendmail/src/conf.h
@@ -5,7 +5,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)conf.h	8.85 (Berkeley) %G%
+ *	@(#)conf.h	8.86 (Berkeley) %G%
  */
 
 /*
@@ -238,6 +238,7 @@ extern char		*getenv();
 # define HASUNAME	1	/* use System V uname(2) system call */
 # define HASSETSID	1	/* has Posix setsid(2) call */
 # define HASINITGROUPS	1	/* has initgroups(3) call */
+# define HASGETUSERSHELL 0	/* does not have getusershell(3) */
 # ifndef IDENTPROTO
 #  define IDENTPROTO	0	/* TCP/IP implementation is broken */
 # endif
