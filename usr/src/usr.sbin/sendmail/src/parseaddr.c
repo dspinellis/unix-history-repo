8a4740c8a08a43d12eff90233fa12ccb5cf8953f
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-02-09 19:32:19 -0800

    make default $#error number 553 instead of 501
    SCCS-vsn: usr.sbin/sendmail/src/parseaddr.c 8.30

--- usr/src/usr.sbin/sendmail/src/parseaddr.c
+++ usr/src/usr.sbin/sendmail/src/parseaddr.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)parseaddr.c	8.29 (Berkeley) %G%";
+static char sccsid[] = "@(#)parseaddr.c	8.30 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -1306,7 +1306,7 @@ badaddr:
 		}
 		else
 		{
-			usrerr("%s", buf);
+			usrerr("553 %s", buf);
 		}
 		goto badaddr;
 	}
