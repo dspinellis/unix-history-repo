25958b84fa7ce4d6e1b76ba607301b965a487558
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1994-02-12 16:47:12 -0800

    typo
    SCCS-vsn: lib/libc/gen/opendir.c 8.2

--- usr/src/lib/libc/gen/opendir.c
+++ usr/src/lib/libc/gen/opendir.c
@@ -6,10 +6,11 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)opendir.c	8.1 (Berkeley) %G%";
+static char sccsid[] = "@(#)opendir.c	8.2 (Berkeley) %G%";
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/param.h>
+
 #include <dirent.h>
 #include <fcntl.h>
 #include <stdlib.h>
