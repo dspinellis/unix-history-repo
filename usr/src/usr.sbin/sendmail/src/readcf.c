b6d9f1575277bf4ce4313bd46f24944245cab7ca
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-02-16 16:46:26 -0800

    clean up debugging in fileclass()
    SCCS-vsn: usr.sbin/sendmail/src/readcf.c 8.21

--- usr/src/usr.sbin/sendmail/src/readcf.c
+++ usr/src/usr.sbin/sendmail/src/readcf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)readcf.c	8.20 (Berkeley) %G%";
+static char sccsid[] = "@(#)readcf.c	8.21 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -572,6 +572,9 @@ fileclass(class, filename, fmt, safe, optional)
 	struct stat stbuf;
 	char buf[MAXLINE];
 
+	if (tTd(37, 2))
+		printf("fileclass(%s, fmt=%s)\n", filename, fmt);
+
 	if (filename[0] == '|')
 	{
 		syserr("fileclass: pipes (F%c%s) not supported due to security problems",
@@ -580,6 +583,8 @@ fileclass(class, filename, fmt, safe, optional)
 	}
 	if (stat(filename, &stbuf) < 0)
 	{
+		if (tTd(37, 2))
+			printf("  cannot stat (%s)\n", errstring(errno));
 		if (!optional)
 			syserr("fileclass: cannot stat %s", filename);
 		return;
@@ -637,8 +642,7 @@ fileclass(class, filename, fmt, safe, optional)
 				*p++ = '\0';
 
 			/* enter the word in the symbol table */
-			s = stab(q, ST_CLASS, ST_ENTER);
-			setbitn(class, s->s_class);
+			setclass(class, q);
 		}
 	}
 
