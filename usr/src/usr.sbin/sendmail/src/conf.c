62254a9426ac3e94433fc599da0720dbe1a1070a
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-02-19 16:41:49 -0800

    replace strtol implementation with one that works
    SCCS-vsn: usr.sbin/sendmail/src/conf.c 8.77

--- usr/src/usr.sbin/sendmail/src/conf.c
+++ usr/src/usr.sbin/sendmail/src/conf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)conf.c	8.76 (Berkeley) %G%";
+static char sccsid[] = "@(#)conf.c	8.77 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -1978,46 +1978,104 @@ setvendor(vendor)
 **  STRTOL -- convert string to long integer
 **
 **	For systems that don't have it in the C library.
+**
+**	This is taken verbatim from the 4.4-Lite C library.
 */
 
 #ifdef NEEDSTRTOL
 
+#if defined(LIBC_SCCS) && !defined(lint)
+static char sccsid[] = "@(#)strtol.c	8.1 (Berkeley) 6/4/93";
+#endif /* LIBC_SCCS and not lint */
+
+#include <limits.h>
+
+/*
+ * Convert a string to a long integer.
+ *
+ * Ignores `locale' stuff.  Assumes that the upper and lower case
+ * alphabets and digits are each contiguous.
+ */
+
 long
-strtol(p, ep, b)
-	char *p;
-	char **ep;
-	int b;
+strtol(nptr, endptr, base)
+	const char *nptr;
+	char **endptr;
+	register int base;
 {
-	long l = 0;
-	char c;
-	char maxd;
-	int neg = 1;
+	register const char *s = nptr;
+	register unsigned long acc;
+	register int c;
+	register unsigned long cutoff;
+	register int neg = 0, any, cutlim;
 
-	maxd = (b > 10) ? '9' : b + '0';
+	/*
+	 * Skip white space and pick up leading +/- sign if any.
+	 * If base is 0, allow 0x for hex and 0 for octal, else
+	 * assume decimal; if base is already 16, allow 0x.
+	 */
+	do {
+		c = *s++;
+	} while (isspace(c));
+	if (c == '-') {
+		neg = 1;
+		c = *s++;
+	} else if (c == '+')
+		c = *s++;
+	if ((base == 0 || base == 16) &&
+	    c == '0' && (*s == 'x' || *s == 'X')) {
+		c = s[1];
+		s += 2;
+		base = 16;
+	}
+	if (base == 0)
+		base = c == '0' ? 8 : 10;
 
-	if (p && *p == '-') {
-		neg = -1;
-		p++;
-	}
-	while (p && (c = *p)) {
-		if (c >= '0' && c <= maxd) {
-			l = l*b + *p++ - '0';
-			continue;
+	/*
+	 * Compute the cutoff value between legal numbers and illegal
+	 * numbers.  That is the largest legal value, divided by the
+	 * base.  An input number that is greater than this value, if
+	 * followed by a legal input character, is too big.  One that
+	 * is equal to this value may be valid or not; the limit
+	 * between valid and invalid numbers is then based on the last
+	 * digit.  For instance, if the range for longs is
+	 * [-2147483648..2147483647] and the input base is 10,
+	 * cutoff will be set to 214748364 and cutlim to either
+	 * 7 (neg==0) or 8 (neg==1), meaning that if we have accumulated
+	 * a value > 214748364, or equal but the next digit is > 7 (or 8),
+	 * the number is too big, and we will return a range error.
+	 *
+	 * Set any if any `digits' consumed; make it negative to indicate
+	 * overflow.
+	 */
+	cutoff = neg ? -(unsigned long)LONG_MIN : LONG_MAX;
+	cutlim = cutoff % (unsigned long)base;
+	cutoff /= (unsigned long)base;
+	for (acc = 0, any = 0;; c = *s++) {
+		if (isdigit(c))
+			c -= '0';
+		else if (isalpha(c))
+			c -= isupper(c) ? 'A' - 10 : 'a' - 10;
+		else
+			break;
+		if (c >= base)
+			break;
+		if (any < 0 || acc > cutoff || acc == cutoff && c > cutlim)
+			any = -1;
+		else {
+			any = 1;
+			acc *= base;
+			acc += c;
 		}
-		if (c >= 'A' && c <= 'Z')
-			c -= 'A' + 'a';
-		c = c - 'a' + 10;
-		if (b > c) {
-			l = l*b + c;
-			p++;
-			continue;
-		}
-		break;
 	}
-	l *= neg;
-	if (ep)
-		*ep = p;
-	return l;
+	if (any < 0) {
+		acc = neg ? LONG_MIN : LONG_MAX;
+		errno = ERANGE;
+	} else if (neg)
+		acc = -acc;
+	if (endptr != 0)
+		*endptr = (char *)(any ? s - 1 : nptr);
+	return (acc);
 }
 
 #endif
