ade4dc19f1f4d442b50519afad808b302c19b529
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1994-02-21 17:35:28 -0800

    change send() to return a ssize_t and take a size_t as a length
    SCCS-vsn: lib/libc/net/send.c 8.2

--- usr/src/lib/libc/net/send.c
+++ usr/src/lib/libc/net/send.c
@@ -6,16 +6,19 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)send.c	8.1 (Berkeley) %G%";
+static char sccsid[] = "@(#)send.c	8.2 (Berkeley) %G%";
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <stdio.h>
 
+#include <stddef.h>
+
+ssize_t
 send(s, msg, len, flags)
-	int s, len, flags;
+	int s, flags;
+	size_t len;
 	const void *msg;
 {
-	return(sendto(s, msg, len, flags, (struct sockaddr *)NULL, 0));
+	return (sendto(s, msg, len, flags, NULL, 0));
 }
