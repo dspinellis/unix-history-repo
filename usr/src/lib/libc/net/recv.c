9fb49e30c00e6d97bccbdc4c0849953328bd03b0
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1994-02-21 17:35:40 -0800

    change recv() to return a ssize_t and take a size_t as a length
    SCCS-vsn: lib/libc/net/recv.c 8.2

--- usr/src/lib/libc/net/recv.c
+++ usr/src/lib/libc/net/recv.c
@@ -6,16 +6,19 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)recv.c	8.1 (Berkeley) %G%";
+static char sccsid[] = "@(#)recv.c	8.2 (Berkeley) %G%";
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <stdio.h>
 
+#include <stddef.h>
+
+ssize_t
 recv(s, buf, len, flags)
-	int s, len, flags;
+	int s, flags;
+	size_t len;
 	void *buf;
 {
-	return(recvfrom(s, buf, len, flags, (struct sockaddr *)NULL, 0));
+	return (recvfrom(s, buf, len, flags, NULL, 0));
 }
