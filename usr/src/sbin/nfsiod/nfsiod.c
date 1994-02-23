6d1617b59c4317da8e930e588e2904b4f9ac4e9c
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1994-02-23 00:02:38 -0800

    use -n, not a trailing argument; use getopt even if no arguments; don't drop core if NFS not compiled into the kernel; KNF
    SCCS-vsn: sbin/nfsiod/nfsiod.c 8.2
    SCCS-vsn: sbin/nfsiod/nfsiod.8 8.2

--- usr/src/sbin/nfsiod/nfsiod.c
+++ usr/src/sbin/nfsiod/nfsiod.c
@@ -15,19 +15,25 @@ static char copyright[] =
 #endif not lint
 
 #ifndef lint
-static char sccsid[] = "@(#)nfsiod.c	8.1 (Berkeley) %G%";
+static char sccsid[] = "@(#)nfsiod.c	8.2 (Berkeley) %G%";
 #endif not lint
 
-#include <stdio.h>
-#include <signal.h>
-#include <fcntl.h>
-#include <sys/syslog.h>
 #include <sys/param.h>
 #include <sys/ioctl.h>
-#include <sys/wait.h>
+#include <sys/syslog.h>
 #include <sys/ucred.h>
+#include <sys/wait.h>
+
 #include <nfs/nfsv2.h>
 #include <nfs/nfs.h>
+
+#include <err.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 /* Global defs */
 #ifdef DEBUG
@@ -35,42 +41,101 @@ int debug = 1;
 #else
 int debug = 0;
 #endif
-extern int errno;
-void reapchild();
+
+void nonfs __P((int));
+void reapchild __P((int));
+void usage __P((void));
 
 /*
  * Nfsiod does asynchronous buffered I/O on behalf of the NFS client.
- * It does not have to be running for correct operation, but will improve
- * throughput. The one optional argument is the number of children to fork.
+ * It does not have to be running for correct operation, but will
+ * improve throughput.
  */
+int
 main(argc, argv)
 	int argc;
 	char *argv[];
 {
-	register int i;
-	int cnt;
+	int ch, num_servers;
 
-	if (argc != 2 || (cnt = atoi(argv[1])) <= 0 || cnt > 20)
-		cnt = 1;
+#define	MAXNFSDCNT      20
+#define	DEFNFSDCNT       4
+	num_servers = DEFNFSDCNT;
+	while ((ch = getopt(argc, argv, "n:")) != EOF)
+		switch (ch) {
+		case 'n':
+			num_servers = atoi(optarg);
+			if (num_servers < 1 || num_servers > MAXNFSDCNT) {
+				warnx("nfsiod count %d; reset to %d",
+				    DEFNFSDCNT);
+				num_servers = DEFNFSDCNT;
+			}
+			break;
+		case '?':
+		default:
+			usage();
+		}
+	argc -= optind;
+	argv += optind;
+
+	/*
+	 * XXX
+	 * Backward compatibility, trailing number is the count of daemons.
+	 */
+	if (argc > 1)
+		usage();
+	if (argc == 1) {
+		num_servers = atoi(argv[0]);
+		if (num_servers < 1 || num_servers > MAXNFSDCNT) {
+			warnx("nfsiod count %d; reset to %d", DEFNFSDCNT);
+			num_servers = DEFNFSDCNT;
+		}
+	}
+
 	if (debug == 0) {
 		daemon(0, 0);
-		signal(SIGINT, SIG_IGN);
-		signal(SIGQUIT, SIG_IGN);
-		signal(SIGHUP, SIG_IGN);
+		(void)signal(SIGHUP, SIG_IGN);
+		(void)signal(SIGINT, SIG_IGN);
+		(void)signal(SIGQUIT, SIG_IGN);
+		(void)signal(SIGSYS, nonfs);
 	}
-	signal(SIGCHLD, reapchild);
+	(void)signal(SIGCHLD, reapchild);
+
 	openlog("nfsiod:", LOG_PID, LOG_DAEMON);
-	for (i = 1; i < cnt; i++)
-		if (fork() == 0)
-			break;
-	if (nfssvc(NFSSVC_BIOD, (char *)0) < 0)
-		syslog(LOG_ERR, "nfssvc failed %m");
+
+	while (num_servers--)
+		switch (fork()) {
+		case -1:
+			syslog(LOG_ERR, "fork: %m");
+			exit (1);
+		case 0:
+			if (nfssvc(NFSSVC_BIOD, NULL) < 0) {
+				syslog(LOG_ERR, "nfssvc: %m");
+				exit (1);
+			}
+			exit(0);
+		}
+	exit (0);
 }
 
 void
-reapchild()
+nonfs(signo)
+	int signo;
+{
+	syslog(LOG_ERR, "missing system call: NFS not available.");
+}
+
+void
+reapchild(signo)
+	int signo;
 {
 
-	while (wait3((int *) NULL, WNOHANG, (struct rusage *) NULL))
-		;
+	while (wait3(NULL, WNOHANG, NULL));
+}
+
+void
+usage()
+{
+	(void)fprintf(stderr, "usage: nfsiod [-n num_servers]\n");
+	exit(1);
 }
