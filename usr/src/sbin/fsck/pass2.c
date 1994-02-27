529b1554c65bb1907b6ee753a6b13a8846fb5c75
Marshall Kirk McKusick <mkm@ucbvax.Berkeley.EDU>
1994-02-27 08:03:32 -0800

    (no message)
    SCCS-vsn: sbin/fsck/pass2.c 8.2

--- usr/src/sbin/fsck/pass2.c
+++ usr/src/sbin/fsck/pass2.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)pass2.c	8.1 (Berkeley) %G%";
+static char sccsid[] = "@(#)pass2.c	8.2 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/param.h>
