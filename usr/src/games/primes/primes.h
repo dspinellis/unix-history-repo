06e1d94cf6f81a7218b977d897ee29bcd24928a9
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1994-03-01 14:25:24 -0800

    primes did not support numbers in the 2^31 - 2^32 range; redefine the upper limit, and change the %ld's to %lu's appears to fix it. From: Arne Juul <arnej@dsl.unit.no>
    SCCS-vsn: games/primes/primes.c 8.2
    SCCS-vsn: games/primes/primes.h 8.2

--- usr/src/games/primes/primes.h
+++ usr/src/games/primes/primes.h
@@ -7,24 +7,20 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)primes.h	8.1 (Berkeley) %G%
+ *	@(#)primes.h	8.2 (Berkeley) %G%
  */
 
 /*
  * primes - generate a table of primes between two values
  *
- * By: Landon Curt Noll   chongo@toad.com,   ...!{sun,tolsoft}!hoptoad!chongo
+ * By: Landon Curt Noll chongo@toad.com, ...!{sun,tolsoft}!hoptoad!chongo
  *
- *   chongo <for a good prime call: 391581 * 2^216193 - 1> /\oo/\
+ * chongo <for a good prime call: 391581 * 2^216193 - 1> /\oo/\
  */
 
 /* ubig is the type that holds a large unsigned value */
-typedef unsigned long ubig;           /* must be >=32 bit unsigned value */
+typedef unsigned long ubig;		/* must be >=32 bit unsigned value */
+#define	BIG		ULONG_MAX	/* largest value will sieve */
 
-/*
- * sieve parameters
- */
-#define BIG ((ubig)0xffffffff)        /* highest value we will sieve */
-#define SEMIBIG ((ubig)0x7fffffff)    /* highest signed value */
-#define NEG_SEMIBIG ((ubig)0x80000000) /* lowest signed value */
-#define TABSIZE 256*1024 /* bytes in sieve table (must be > 3*5*7*11) */
+/* bytes in sieve table (must be > 3*5*7*11) */
+#define	TABSIZE		256*1024
