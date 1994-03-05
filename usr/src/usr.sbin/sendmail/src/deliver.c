39087b8d72c8b9fb07a192c4b1dc9e4523ea328f
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-03-04 16:48:18 -0800

    make sure that you don't get bogus addresses queued up on name server problems and split envelopes
    SCCS-vsn: usr.sbin/sendmail/src/deliver.c 8.75

--- usr/src/usr.sbin/sendmail/src/deliver.c
+++ usr/src/usr.sbin/sendmail/src/deliver.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)deliver.c	8.74 (Berkeley) %G%";
+static char sccsid[] = "@(#)deliver.c	8.75 (Berkeley) %G%";
 #endif /* not lint */
 
 #include "sendmail.h"
@@ -223,10 +223,16 @@ sendall(e, mode)
 			
 			for (q = e->e_sendqueue; q != NULL; q = q->q_next)
 				if (q->q_owner == owner)
+				{
 					q->q_flags |= QDONTSEND;
+					q->q_flags &= ~QQUEUEUP;
+				}
 			for (q = ee->e_sendqueue; q != NULL; q = q->q_next)
 				if (q->q_owner != owner)
+				{
 					q->q_flags |= QDONTSEND;
+					q->q_flags &= ~QQUEUEUP;
+				}
 
 			if (e->e_df != NULL && mode != SM_VERIFY)
 			{
