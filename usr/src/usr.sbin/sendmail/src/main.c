1afc7f81fce351565569fc54d5e5b1de1717267e
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-03-07 18:46:19 -0800

    fix handling of -d command line flag
    SCCS-vsn: usr.sbin/sendmail/src/main.c 8.50

--- usr/src/usr.sbin/sendmail/src/main.c
+++ usr/src/usr.sbin/sendmail/src/main.c
@@ -13,7 +13,7 @@ static char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)main.c	8.49 (Berkeley) %G%";
+static char sccsid[] = "@(#)main.c	8.50 (Berkeley) %G%";
 #endif /* not lint */
 
 #define	_DEFINE
