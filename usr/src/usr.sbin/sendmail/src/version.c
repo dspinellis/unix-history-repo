84ad6cde3774057e2f050169abf3937db483adf8
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-03-14 17:42:16 -0800

    SECURITY PROBLEM: wierd values to -d could give you root!
    SCCS-vsn: usr.sbin/sendmail/src/trace.c 8.2
    SCCS-vsn: usr.sbin/sendmail/src/version.c 8.6.7.1
    SCCS-vsn: usr.sbin/sendmail/RELEASE_NOTES 8.6.7.1

--- usr/src/usr.sbin/sendmail/src/version.c
+++ usr/src/usr.sbin/sendmail/src/version.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)version.c	8.6.6.1 (Berkeley) %G%";
+static char sccsid[] = "@(#)version.c	8.6.7.1 (Berkeley) %G%";
 #endif /* not lint */
 
-char	Version[] = "8.6.6";
+char	Version[] = "8.6.7";
