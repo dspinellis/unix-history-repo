f3d3148a8a5967b470eef2aa61e13daece7ac698
David Greenman <davidg@Root.COM>
1994-03-16 07:28:35 +0000

    Added call to setlogin().

--- libexec/rexecd/rexecd.c
+++ libexec/rexecd/rexecd.c
@@ -208,6 +208,7 @@ doit(f, fromp)
 		pwd->pw_shell = _PATH_BSHELL;
 	if (f > 2)
 		(void) close(f);
+	(void) setlogin(pwd->pw_name);
 	(void) setgid((gid_t)pwd->pw_gid);
 	initgroups(pwd->pw_name, pwd->pw_gid);
 	(void) setuid((uid_t)pwd->pw_uid);
