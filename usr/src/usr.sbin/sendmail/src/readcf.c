35d04a9423b183f823eba91675c7c9582b8f6ff7
Mike Corrigan <corrigan@ucbvax.Berkeley.EDU>
1994-03-18 23:30:05 -0800

    The 'E' option is not safe From: "Michael J. Corrigan" <corrigan@UCSD.EDU>
    SCCS-vsn: usr.sbin/sendmail/src/readcf.c 8.23

--- usr/src/usr.sbin/sendmail/src/readcf.c
+++ usr/src/usr.sbin/sendmail/src/readcf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)readcf.c	8.22 (Berkeley) %G%";
+static char sccsid[] = "@(#)readcf.c	8.23 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
