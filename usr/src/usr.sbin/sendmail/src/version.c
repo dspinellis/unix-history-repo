3f50ed7709d36dbd7736e37ec046489dcab8fb64
Mike Corrigan <corrigan@ucbvax.Berkeley.EDU>
1994-03-18 23:36:46 -0800

    SECURITY PROBLEM: -oe option can read any file From: "Michael J. Corrigan" <corrigan@UCSD.EDU>
    SCCS-vsn: usr.sbin/sendmail/src/version.c 8.6.8.1

--- usr/src/usr.sbin/sendmail/src/version.c
+++ usr/src/usr.sbin/sendmail/src/version.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)version.c	8.6.7.1 (Berkeley) %G%";
+static char sccsid[] = "@(#)version.c	8.6.8.1 (Berkeley) %G%";
 #endif /* not lint */
 
-char	Version[] = "8.6.7";
+char	Version[] = "8.6.8";
