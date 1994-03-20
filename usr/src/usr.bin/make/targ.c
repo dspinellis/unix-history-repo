b48ba4482655f90985891302419565be4a5bd559
Christos Zoulas <christos@ucbvax.Berkeley.EDU>
1994-03-19 23:57:53 -0800

    fix indentation
    SCCS-vsn: usr.bin/make/targ.c 8.2

--- usr/src/usr.bin/make/targ.c
+++ usr/src/usr.bin/make/targ.c
@@ -11,7 +11,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)targ.c	8.1 (Berkeley) %G%";
+static char sccsid[] = "@(#)targ.c	8.2 (Berkeley) %G%";
 #endif /* not lint */
 
 /*-
@@ -123,7 +123,7 @@ Targ_NewGN (name)
     gn->preds =     	Lst_Init(FALSE);
     gn->context =   	Lst_Init (FALSE);
     gn->commands =  	Lst_Init (FALSE);
-    gn->suffix = 	NULL;
+    gn->suffix =	NULL;
 
     return (gn);
 }
