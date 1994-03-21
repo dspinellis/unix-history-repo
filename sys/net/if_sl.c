d41835fbf632caeb0499d53befee37320a3b4462
Andrey Chernov <ache@FreeBSD.org>
1994-03-21 21:29:50 +0000

    Remove ttywflush from slclose, it cause dead hang in tsleep when carier dropped. Moreover, it is incorrect to deal anything with t_out & t_raw here (like ttywflush does), we already change line discipline.

--- sys/net/if_sl.c
+++ sys/net/if_sl.c
@@ -31,7 +31,7 @@
  * SUCH DAMAGE.
  *
  *	from: @(#)if_sl.c	7.22 (Berkeley) 4/20/91
- *	$Id: if_sl.c,v 1.7 1993/12/20 19:31:32 wollman Exp $
+ *	$Id: if_sl.c,v 1.8 1994/03/02 20:28:55 guido Exp $
  */
 
 /*
@@ -65,7 +65,7 @@
  * interrupts and network activity; thus, splimp must be >= spltty.
  */
 
-/* $Id: if_sl.c,v 1.7 1993/12/20 19:31:32 wollman Exp $ */
+/* $Id: if_sl.c,v 1.8 1994/03/02 20:28:55 guido Exp $ */
 /* from if_sl.c,v 1.11 84/10/04 12:54:47 rick Exp */
 
 #include "sl.h"
@@ -332,7 +332,6 @@ slclose(tp)
 	register struct sl_softc *sc;
 	int s;
 
-	ttywflush(tp);
 	s = splimp();		/* actually, max(spltty, splnet) */
 	tp->t_line = 0;
 	sc = (struct sl_softc *)tp->t_sc;
