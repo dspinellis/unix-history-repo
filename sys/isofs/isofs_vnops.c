4cdd6105c05e1961bbf71146df6d9a8a429f36cf
David Greenman <davidg@Root.COM>
1994-03-25 22:26:22 +0000

    Patch from mrl@teleport.com (Mostyn R. Lewis), with the comment improved by me:
    Below is a fix to isofs_vnops.c in /usr/src/sys/isofs to make find traverse
    directories in the usual way. The va_nlink field needs either to be correct,
    i.e the number of directories and the path or else 1 to fool fts.
    
    (Note that this is correct for non-RR FS, but needs to be re-done in the
    presence of RR - but there isn't any higher level support for this yet.
    -DG)

--- sys/isofs/isofs_vnops.c
+++ sys/isofs/isofs_vnops.c
@@ -1,5 +1,5 @@
 /*
- *	$Id: isofs_vnops.c,v 1.3 1993/11/25 01:32:31 wollman Exp $
+ *	$Id: isofs_vnops.c,v 1.4 1993/12/19 00:51:08 wollman Exp $
  */
 #include "param.h"
 #include "systm.h"
@@ -82,10 +82,13 @@ isofs_getattr(vp, vap, cred, p)
 
 	vap->va_fsid = ip->i_dev;
 	vap->va_fileid = ip->i_number;
-	if (vp->v_type == VDIR)
-		vap->va_nlink = 2;
-	else
-		vap->va_nlink = 1;
+	/*
+	 * This should be set properly if a RR filesystem, but this is
+	 * the safest value for now. Note that previously this was conditionally
+	 * set to 2 if the file is a directory, but this causes problems
+	 * with find.
+	 */
+	vap->va_nlink = 1;
 
 	vap->va_mode = ip->inode.iso_mode;
 	vap->va_uid  = ip->inode.iso_uid;
