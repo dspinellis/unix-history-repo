cf3f65dfdc948a80b7b907008ad5d1fec09051ee
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1994-03-27 15:31:47 -0800

    Jan-Simon Pendry and my new mount code mount_cd9660 pretty much completely rewritten for cleanliness, KNF, and ANSI
    SCCS-vsn: sbin/mount_cd9660/Makefile 8.2
    SCCS-vsn: sbin/mount_cd9660/mount_cd9660.8 8.3
    SCCS-vsn: sbin/mount_cd9660/mount_cd9660.c 8.4

--- usr/src/sbin/mount_cd9660/mount_cd9660.c
+++ usr/src/sbin/mount_cd9660/mount_cd9660.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1992, 1993
+ * Copyright (c) 1992, 1993, 1994
  *      The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley
@@ -9,88 +9,96 @@
  *
  * %sccs.include.redist.c%
  *
- *      @(#)mount_cd9660.c	8.3 (Berkeley) %G%
+ *      @(#)mount_cd9660.c	8.4 (Berkeley) %G%
  */
 
 #ifndef lint
 static char copyright[] =
-"@(#) Copyright (c) 1992, 1993\n\
+"@(#) Copyright (c) 1992, 1993, 1994\n\
         The Regents of the University of California.  All rights reserved.\n";
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)mount_cd9660.c	8.3 (Berkeley) %G%";
+static char sccsid[] = "@(#)mount_cd9660.c	8.4 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/param.h>
 #define CD9660
 #include <sys/mount.h>
 
-#include <errno.h>
+#include <err.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 
-#define DEFAULT_ROOTUID	-2
+#include "mntopts.h"
 
-void
-usage()
-{
+struct mntopt mopts[] = {
+	MOPT_STDOPTS,
+	MOPT_UPDATE,
+	{ NULL }
+};
 
-	fprintf(stderr,
-		"usage: mount_cd9660 [-F fsoptions] [-norrip] [-gen] [-extattr] special node\n");
-	exit(1);
-}
-		
+void	usage __P((void));
+
 int
 main(argc, argv)
 	int argc;
 	char **argv;
 {
-	char *dev;
-	char *dir;
 	struct iso_args args;
-	int mntflags = 0, opts = 0;
+	int ch, mntflags, opts;
+	char *dev, *dir, *options;
 
-	argc--;
-	argv++;
-	while (argc > 2) {
-		if (!strcmp("-F", argv[0])) {
-			argc--; argv++;
-			mntflags |= atoi(argv[0]);
-			argc--; argv++;
-		} else if (!strcmp(argv[0], "-norrip")) {
-			opts |= ISOFSMNT_NORRIP;
-			argc--; argv++;
-		} else if (!strcmp(argv[0], "-gen")) {
-			opts |= ISOFSMNT_GENS;
-			argc--; argv++;
-		} else if (!strcmp(argv[0], "-extattr")) {
+	options = NULL;
+	mntflags = opts = 0;
+	while ((ch = getopt(argc, argv, "ego:r")) != EOF)
+		switch (ch) {
+		case 'e':
 			opts |= ISOFSMNT_EXTATT;
-			argc--; argv++;
-		} else if (!strcmp(argv[0], "-notrans")) {
-			opts |= ISOFSMNT_NOTRANS;
-			argc--; argv++;
-		} else
+			break;
+		case 'g':
+			opts |= ISOFSMNT_GENS;
+			break;
+		case 'o':
+			getmntopts(options, mopts, &mntflags);
+			break;
+		case 'r':
+			opts |= ISOFSMNT_NORRIP;
+			break;
+		case '?':
+		default:
 			usage();
-	}
+		}
+	argc -= optind;
+	argv += optind;
+
+	if (argc != 2)
+		usage();
 
 	dev = argv[0];
 	dir = argv[1];
 
+#define DEFAULT_ROOTUID	-2
 	args.fspec = dev;
 	args.export.ex_root = DEFAULT_ROOTUID;
+
 	if (mntflags & MNT_RDONLY)
 		args.export.ex_flags = MNT_EXRDONLY;
 	else
 		args.export.ex_flags = 0;
 	args.flags = opts;
 
-	if (mount(MOUNT_CD9660, dir, mntflags, &args) < 0) {
-		(void)fprintf(stderr, "mount_cd9660: %s\n", strerror(errno));
-		exit(1);
-	}
-
+	if (mount(MOUNT_CD9660, dir, mntflags, &args) < 0)
+		err(1, NULL);
 	exit(0);
+}
+
+void
+usage()
+{
+	(void)fprintf(stderr,
+		"usage: mount_cd9660 [-egrt] [-o options] special node\n");
+	exit(1);
 }
