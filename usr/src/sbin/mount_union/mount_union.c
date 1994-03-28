f5837701ae5bd447e182abc87335c95acbb35548
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1994-03-27 18:57:32 -0800

    Jan-Simon Pendry's and my new mount code
    SCCS-vsn: sbin/mount_union/Makefile 8.3
    SCCS-vsn: sbin/mount_union/mount_union.8 8.5
    SCCS-vsn: sbin/mount_union/mount_union.c 8.4

--- usr/src/sbin/mount_union/mount_union.c
+++ usr/src/sbin/mount_union/mount_union.c
@@ -6,22 +6,38 @@
  * Jan-Simon Pendry.
  *
  * %sccs.include.redist.c%
- *
- *	@(#)mount_union.c	8.3 (Berkeley) %G%
  */
+
+#ifndef lint
+char copyright[] =
+"@(#) Copyright (c) 1992, 1993, 1994\n\
+	The Regents of the University of California.  All rights reserved.\n";
+#endif /* not lint */
+
+#ifndef lint
+static char sccsid[] = "@(#)mount_union.c	8.4 (Berkeley) %G%";
+#endif /* not lint */
 
 #include <sys/param.h>
 #include <sys/mount.h>
+
 #include <miscfs/union/union.h>
 
-#include <errno.h>
+#include <err.h>
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
-static int subdir __P((const char *, const char *));
-void usage __P((void));
+#include "mntopts.h"
+
+struct mntopt mopts[] = {
+	MOPT_STDOPTS,
+	{ NULL }
+};
+
+int	subdir __P((const char *, const char *));
+void	usage __P((void));
 
 int
 main(argc, argv)
@@ -31,81 +47,59 @@ main(argc, argv)
 	struct union_args args;
 	int ch, mntflags;
 	char target[MAXPATHLEN];
-	int error = 0;
 
 	mntflags = 0;
 	args.mntflags = UNMNT_ABOVE;
 
-	while ((ch = getopt(argc, argv, "F:a:b:r:")) != EOF) {
-		switch(ch) {
-		case 'F':
-			mntflags = atoi(optarg);
-			break;
+	while ((ch = getopt(argc, argv, "a:b:o:r:")) != EOF) {
+		switch (ch) {
 		case 'a':
-			if (strcmp(optarg, "bove") == 0) {
-				args.mntflags &= ~UNMNT_OPMASK;
-				args.mntflags |= UNMNT_ABOVE;
-			} else {
-				error = 1;
-			}
+			if (strcmp(optarg, "bove") != 0)
+				usage();
+			args.mntflags &= ~UNMNT_OPMASK;
+			args.mntflags |= UNMNT_ABOVE;
 			break;
-
 		case 'b':
-			if (strcmp(optarg, "elow") == 0) {
-				args.mntflags &= ~UNMNT_OPMASK;
-				args.mntflags |= UNMNT_BELOW;
-			} else {
-				error = 1;
-			}
+			if (strcmp(optarg, "elow") != 0)
+				usage();
+			args.mntflags &= ~UNMNT_OPMASK;
+			args.mntflags |= UNMNT_BELOW;
 			break;
-
+		case 'o':
+			getmntopts(optarg, mopts, &mntflags);
+			break;
 		case 'r':
-			if (strcmp(optarg, "eplace") == 0) {
-				args.mntflags &= ~UNMNT_OPMASK;
-				args.mntflags |= UNMNT_REPLACE;
-			} else {
-				error = 1;
-			}
+			if (strcmp(optarg, "eplace") != 0)
+				usage();
+			args.mntflags &= ~UNMNT_OPMASK;
+			args.mntflags |= UNMNT_REPLACE;
 			break;
-
 		case '?':
 		default:
-			error = 1;
-			break;
+			usage();
 		}
 	}
 	argc -= optind;
 	argv += optind;
 
 	if (argc != 2)
-		error = 1;
-
-	if (error)
 		usage();
 
-	if (realpath(argv[0], target) == 0) {
-		(void)fprintf(stderr, "mount_union: %s: %s\n",
-				target, strerror(errno));
-		exit(1);
-	}
+	if (realpath(argv[0], target) == 0)
+		err(1, "%s", target);
 
-	if (subdir(target, argv[1]) || subdir(argv[1], target)) {
-		(void)fprintf(stderr,
-			"mount_union: %s (%s) and %s are not distinct paths\n",
-				argv[0], target, argv[1]);
-		exit(1);
-	}
+	if (subdir(target, argv[1]) || subdir(argv[1], target))
+		errx(1, "%s (%s) and %s are not distinct paths",
+		    argv[0], target, argv[1]);
 
 	args.target = target;
 
-	if (mount(MOUNT_UNION, argv[1], mntflags, &args)) {
-		(void)fprintf(stderr, "mount_union: %s\n", strerror(errno));
-		exit(1);
-	}
+	if (mount(MOUNT_UNION, argv[1], mntflags, &args))
+		err(1, NULL);
 	exit(0);
 }
 
-static int
+int
 subdir(p, dir)
 	const char *p;
 	const char *dir;
@@ -126,6 +120,6 @@ void
 usage()
 {
 	(void)fprintf(stderr,
-	    "usage: mount_union [ -F fsoptions ] target_fs mount_point\n");
+		"usage: mount_union [-o options] target_fs mount_point\n");
 	exit(1);
 }
