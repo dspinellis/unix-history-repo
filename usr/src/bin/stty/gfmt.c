03cfc097fb1204d0d8d29e8c5244a3a4d3919027
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1994-03-28 15:20:17 -0800

    use the cchars1 array instead of listing the options From: Charles Hannum <mycroft@sun-lamp.cs.berkeley.edu>
    SCCS-vsn: bin/stty/gfmt.c 8.4

--- usr/src/bin/stty/gfmt.c
+++ usr/src/bin/stty/gfmt.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)gfmt.c	8.3 (Berkeley) %G%";
+static char sccsid[] = "@(#)gfmt.c	8.4 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -41,12 +41,12 @@ gread(tp, s)
 	char *s;
 {
 	register char *ep, *p;
+	register struct cchar *cp;
 	long tmp;
 
-#define	CHK(s)	(*p == s[0] && !strcmp(p, s))
-	if (!(s = strchr(s, ':')))
+	if ((s = strchr(s, ':')) == NULL)
 		gerr(NULL);
-	for (++s; s;) {
+	for (++s; s != NULL;) {
 		p = strsep(&s, ":\0");
 		if (!p || !*p)
 			break;
@@ -54,40 +54,14 @@ gread(tp, s)
 			gerr(p);
 		*ep++ = '\0';
 		(void)sscanf(ep, "%lx", &tmp);
+
+#define	CHK(s)	(*p == s[0] && !strcmp(p, s))
 		if (CHK("cflag")) {
 			tp->c_cflag = tmp;
 			continue;
 		}
-		if (CHK("discard")) {
-			tp->c_cc[VDISCARD] = tmp;
-			continue;
-		}
-		if (CHK("dsusp")) {
-			tp->c_cc[VDSUSP] = tmp;
-			continue;
-		}
-		if (CHK("eof")) {
-			tp->c_cc[VEOF] = tmp;
-			continue;
-		}
-		if (CHK("eol")) {
-			tp->c_cc[VEOL] = tmp;
-			continue;
-		}
-		if (CHK("eol2")) {
-			tp->c_cc[VEOL2] = tmp;
-			continue;
-		}
-		if (CHK("erase")) {
-			tp->c_cc[VERASE] = tmp;
-			continue;
-		}
 		if (CHK("iflag")) {
 			tp->c_iflag = tmp;
-			continue;
-		}
-		if (CHK("intr")) {
-			tp->c_cc[VINTR] = tmp;
 			continue;
 		}
 		if (CHK("ispeed")) {
@@ -95,21 +69,8 @@ gread(tp, s)
 			tp->c_ispeed = tmp;
 			continue;
 		}
-		if (CHK("kill")) {
-			tp->c_cc[VKILL] = tmp;
-			continue;
-		}
 		if (CHK("lflag")) {
 			tp->c_lflag = tmp;
-			continue;
-		}
-		if (CHK("lnext")) {
-			tp->c_cc[VLNEXT] = tmp;
-			continue;
-		}
-		if (CHK("min")) {
-			(void)sscanf(ep, "%ld", &tmp);
-			tp->c_cc[VMIN] = tmp;
 			continue;
 		}
 		if (CHK("oflag")) {
@@ -121,40 +82,15 @@ gread(tp, s)
 			tp->c_ospeed = tmp;
 			continue;
 		}
-		if (CHK("quit")) {
-			tp->c_cc[VQUIT] = tmp;
-			continue;
-		}
-		if (CHK("reprint")) {
-			tp->c_cc[VREPRINT] = tmp;
-			continue;
-		}
-		if (CHK("start")) {
-			tp->c_cc[VSTART] = tmp;
-			continue;
-		}
-		if (CHK("status")) {
-			tp->c_cc[VSTATUS] = tmp;
-			continue;
-		}
-		if (CHK("stop")) {
-			tp->c_cc[VSTOP] = tmp;
-			continue;
-		}
-		if (CHK("susp")) {
-			tp->c_cc[VSUSP] = tmp;
-			continue;
-		}
-		if (CHK("time")) {
-			(void)sscanf(ep, "%ld", &tmp);
-			tp->c_cc[VTIME] = tmp;
-			continue;
-		}
-		if (CHK("werase")) {
-			tp->c_cc[VWERASE] = tmp;
-			continue;
-		}
-		gerr(p);
+		for (cp = cchars1; cp->name != NULL; ++cp)
+			if (CHK(cp->name)) {
+				if (cp->sub == VMIN || cp->sub == VTIME)
+					(void)sscanf(ep, "%ld", &tmp);
+				tp->c_cc[cp->sub] = tmp;
+				break;
+			}
+		if (cp->name == NULL)
+			gerr(p);
 	}
 }
 
