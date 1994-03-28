73232b491f35ba3a5d1b68a18cdd32927bfad279
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1994-03-27 21:59:47 -0800

    UFS supports sync, sync
    SCCS-vsn: sbin/mount/mount_ufs.c 8.2

--- usr/src/sbin/mount/mount_ufs.c
+++ usr/src/sbin/mount/mount_ufs.c
@@ -12,7 +12,7 @@ static char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)mount_ufs.c	8.1 (Berkeley) %G%";
+static char sccsid[] = "@(#)mount_ufs.c	8.2 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/param.h>
@@ -31,6 +31,8 @@ void	ufs_usage __P((void));
 
 static struct mntopt mopts[] = {
 	MOPT_STDOPTS,
+	MOPT_ASYNC,
+	MOPT_SYNC,
 	MOPT_UPDATE,
 	{ NULL }
 };
