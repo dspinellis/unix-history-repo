28d3cb386ca1285c692e86ce3fc13f04d27ceb95
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1994-03-31 23:32:04 -0800

    POSIX 1003.2B/D9 symbolic links
    SCCS-vsn: bin/chmod/chmod.1 8.3
    SCCS-vsn: bin/chmod/chmod.c 8.4

--- usr/src/bin/chmod/chmod.c
+++ usr/src/bin/chmod/chmod.c
@@ -12,7 +12,7 @@ static char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)chmod.c	8.3 (Berkeley) %G%";
+static char sccsid[] = "@(#)chmod.c	8.4 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -37,28 +37,39 @@ main(argc, argv)
 	register FTSENT *p;
 	register int oct, omode;
 	mode_t *set;
-	int ch, fflag, rflag, hflag, Hflag;
-	int fts_options, retval;
+	int Hflag, Lflag, Pflag, Rflag, ch, fflag, fts_options, hflag, rval;
 	char *ep, *mode;
 
-	fts_options = FTS_PHYSICAL;
-	fflag = rflag = hflag = Hflag = 0;
-	while ((ch = getopt(argc, argv, "HRXfghorstuwx")) != EOF)
+	Hflag = Lflag = Pflag = Rflag = fflag = hflag = 0;
+	while ((ch = getopt(argc, argv, "HLPRXfgorstuwx")) != EOF)
 		switch (ch) {
 		case 'H':
 			Hflag = 1;
-			fts_options |= FTS_COMFOLLOW;
+			Lflag = Pflag = 0;
+			break;
+		case 'L':
+			Lflag = 1;
+			Hflag = Pflag = 0;
+			break;
+		case 'P':
+			Pflag = 1;
+			Hflag = Lflag = 0;
 			break;
 		case 'R':
-			rflag = 1;
+			Rflag = 1;
 			break;
-		case 'f':		/* XXX: no longer documented. */
+		case 'f':		/* XXX: undocumented. */
 			fflag = 1;
 			break;
 		case 'h':
+			/*
+			 * In System V (and probably POSIX.2) the -h option
+			 * causes chmod to change the mode of the symbolic
+			 * link.  4.4BSD's symbolic links don't have modes,
+			 * so it's an undocumented noop.  Do syntax checking,
+			 * though.
+			 */
 			hflag = 1;
-			fts_options &= ~FTS_PHYSICAL;
-			fts_options |= FTS_LOGICAL;
 			break;
 		/*
 		 * XXX
@@ -83,6 +94,19 @@ done:	argv += optind;
 	if (argc < 2)
 		usage();
 
+	fts_options = FTS_PHYSICAL;
+	if (Rflag) {
+		if (hflag)
+			errx(1,
+		"the -R and -h options may not be specified together.");
+		if (Hflag)
+			fts_options |= FTS_COMFOLLOW;
+		if (Lflag) {
+			fts_options &= ~FTS_PHYSICAL;
+			fts_options |= FTS_LOGICAL;
+		}
+	}
+
 	mode = *argv;
 	if (*mode >= '0' && *mode <= '7') {
 		omode = (int)strtol(mode, &ep, 8);
@@ -95,38 +119,52 @@ done:	argv += optind;
 		oct = 0;
 	}
 
-	retval = 0;
 	if ((ftsp = fts_open(++argv, fts_options, 0)) == NULL)
-		err(1, "");
-	while ((p = fts_read(ftsp)) != NULL)
+		err(1, NULL);
+	for (rval = 0; (p = fts_read(ftsp)) != NULL;) {
 		switch (p->fts_info) {
 		case FTS_D:
-			if (!rflag)
-				fts_set(ftsp, p, FTS_SKIP);
-		case FTS_SL:
-		case FTS_SLNONE:
-			break;
-		case FTS_DNR:
-		case FTS_ERR:
-		case FTS_NS:
-			err(1, "%s", p->fts_path);
-		default:	
-			if (p->fts_info == FTS_SL && !(hflag || 
-			    (Hflag && p->fts_level == FTS_ROOTLEVEL)))
+			if (Rflag)		/* Change it at FTS_DP. */
 				continue;
-			if (chmod(p->fts_accpath, oct ? omode :
-			    getmode(set, p->fts_statp->st_mode)) && !fflag) {
-				warn(p->fts_path);
-				retval = 1;
-			}
+			fts_set(ftsp, p, FTS_SKIP);
+			break;
+		case FTS_DC:			/* Ignore. */
+			continue;
+		case FTS_DNR:			/* Warn, chmod, continue. */
+			errno = p->fts_errno;
+			warn("%s", p->fts_path);
+			rval = 1;
+			break;
+		case FTS_ERR:			/* Warn, continue. */
+		case FTS_NS:
+			errno = p->fts_errno;
+			warn("%s", p->fts_path);
+			rval = 1;
+			continue;
+		case FTS_SL:			/* Ignore. */
+		case FTS_SLNONE:
+			/*
+			 * The only symlinks that end up here are ones that
+			 * don't point to anything and ones that we found
+			 * doing a physical walk.
+			 */
+			continue;
+		default:
 			break;
 		}
-	exit(retval);
+		if (chmod(p->fts_accpath, oct ? omode :
+		    getmode(set, p->fts_statp->st_mode)) && !fflag) {
+			warn(p->fts_path);
+			rval = 1;
+		}
+	}
+	exit(rval);
 }
 
 void
 usage()
 {
-	(void)fprintf(stderr, "usage: chmod [-HRh] mode file ...\n");
+	(void)fprintf(stderr,
+	    "usage: chmod [-R [-H | -L | -P]] mode file ...\n");
 	exit(1);
 }
