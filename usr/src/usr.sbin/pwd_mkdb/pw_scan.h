e0260baed55c75db0b930cc61132f60de643125d
Jan-Simon Pendry <pendry@ucbvax.Berkeley.EDU>
1994-04-01 14:08:20 -0800

    date and time created 94/04/01 07:08:20 by pendry
    SCCS-vsn: usr.sbin/pwd_mkdb/pw_scan.h 8.1

---
+++ usr/src/usr.sbin/pwd_mkdb/pw_scan.h
@@ -0,0 +1,10 @@
+/*-
+ * Copyright (c) 1994
+ *	The Regents of the University of California.  All rights reserved.
+ *
+ * %sccs.include.redist.c%
+ *
+ *	@(#)pw_scan.h	8.1 (Berkeley) %G%
+ */
+
+extern int	pw_scan __P((char *, struct passwd *));
