d0bbde96a3d986c3ed7cbf49bbaa601ca8f20b72
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1994-04-01 17:17:41 -0800

    POSIX 1003.2B/D9 symbolic links make cp use err/warn, and pass errors back to the calling routine clean up fts_read() usage, fix errno calls make file-copy failed error clean up after itself immediately
    SCCS-vsn: bin/cp/cp.1 8.2
    SCCS-vsn: bin/cp/cp.c 8.2
    SCCS-vsn: bin/cp/extern.h 8.2
    SCCS-vsn: bin/cp/utils.c 8.3

--- usr/src/bin/cp/extern.h
+++ usr/src/bin/cp/extern.h
@@ -1,10 +1,10 @@
 /*-
- * Copyright (c) 1991, 1993
+ * Copyright (c) 1991, 1993, 1994
  *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)extern.h	8.1 (Berkeley) %G%
+ *	@(#)extern.h	8.2 (Berkeley) %G%
  */
 
 typedef struct {
@@ -13,20 +13,17 @@ typedef struct {
 	char p_path[MAXPATHLEN + 1];	/* pointer to the start of a path */
 } PATH_T;
 
-extern char *progname;			/* program name */
 extern PATH_T to;
-extern int iflag, pflag;
 extern uid_t myuid;
-extern int exit_val, myumask;
+extern int iflag, pflag, myumask;
 
 #include <sys/cdefs.h>
 
 __BEGIN_DECLS
-void copy_fifo __P((struct stat *, int));
-void copy_file __P((FTSENT *, int));
-void copy_link __P((FTSENT *, int));
-void copy_special __P((struct stat *, int));
-void err __P((const char *fmt, ...));
-void setfile __P((struct stat *, int));
-void usage __P((void));
+int	copy_fifo __P((struct stat *, int));
+int	copy_file __P((FTSENT *, int));
+int	copy_link __P((FTSENT *, int));
+int	copy_special __P((struct stat *, int));
+int	setfile __P((struct stat *, int));
+void	usage __P((void));
 __END_DECLS
