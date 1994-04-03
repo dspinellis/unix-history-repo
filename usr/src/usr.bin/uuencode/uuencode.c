183f4ef8005c2aedf6bae5ac3dd5efb04d0be1da
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1994-04-02 17:19:14 -0800

    copyright cleanup
    SCCS-vsn: usr.bin/uuencode/uuencode.c 8.2
    SCCS-vsn: usr.bin/uudecode/uudecode.c 8.2
    SCCS-vsn: usr.bin/window/main.c 8.2

--- usr/src/usr.bin/uuencode/uuencode.c
+++ usr/src/usr.bin/uuencode/uuencode.c
@@ -1,4 +1,4 @@
-/*
+/*-
  * Copyright (c) 1983, 1993
  *	The Regents of the University of California.  All rights reserved.
  *
@@ -6,7 +6,13 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)uuencode.c	8.1 (Berkeley) %G%";
+char copyright[] =
+"@(#) Copyright (c) 1983, 1993\n\
+	The Regents of the University of California.  All rights reserved.\n";
+#endif /* not lint */
+
+#ifndef lint
+static char sccsid[] = "@(#)uuencode.c	8.2 (Berkeley) %G%";
 #endif /* not lint */
 
 /*
@@ -16,11 +22,13 @@ static char sccsid[] = "@(#)uuencode.c	8.1 (Berkeley) %G%";
  */
 #include <sys/types.h>
 #include <sys/stat.h>
+
 #include <stdio.h>
 
+int
 main(argc, argv)
 	int argc;
-	char **argv;
+	char *argv[];
 {
 	extern int optind;
 	extern int errno;
--- usr/src/usr.bin/window/main.c
+++ usr/src/usr.bin/window/main.c
@@ -9,7 +9,13 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)main.c	8.1 (Berkeley) %G%";
+char copyright[] =
+"@(#) Copyright (c) 1983, 1993\n\
+	The Regents of the University of California.  All rights reserved.\n";
+#endif /* not lint */
+
+#ifndef lint
+static char sccsid[] = "@(#)main.c	8.2 (Berkeley) %G%";
 #endif /* not lint */
 
 #include "defs.h"
