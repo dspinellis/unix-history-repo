8b9a5e7abd7a6cd32718e39164ed745f8471dd07
Jan-Simon Pendry <pendry@ucbvax.Berkeley.EDU>
1994-04-02 16:50:32 -0800

    date and time created 94/04/02 09:50:32 by pendry
    SCCS-vsn: usr.bin/passwd/extern.h 8.1

---
+++ usr/src/usr.bin/passwd/extern.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 1994
+ *	The Regents of the University of California.  All rights reserved.
+ *
+ * %sccs.include.redist.c%
+ *
+ *	@(#)extern.h	8.1 (Berkeley) %G%
+ */
+
+int	krb_passwd __P((void));
+int	local_passwd __P((char *));
