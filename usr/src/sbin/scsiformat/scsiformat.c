6581b5bd1bac4166c82eb91d554e5bef6c60a451
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1994-04-02 17:00:59 -0800

    copyright cleanup
    SCCS-vsn: sbin/scsiformat/scsiformat.c 5.5

--- usr/src/sbin/scsiformat/scsiformat.c
+++ usr/src/sbin/scsiformat/scsiformat.c
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1992 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This software was developed by the Computer Systems Engineering group
  * at Lawrence Berkeley Laboratory under DARPA contract BG 91-66 and
@@ -8,17 +8,17 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)scsiformat.c	5.4 (Berkeley) %G%
+ *	@(#)scsiformat.c	5.5 (Berkeley) %G%
  */
 
 #ifndef lint
 char copyright[] =
-"@(#) Copyright (c) 1993 The Regents of the University of California.\n\
- All rights reserved.\n";
+"@(#) Copyright (c) 1992, 1993\n\
+	The Regents of the University of California.  All rights reserved.\n";
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)scsiformat.c	5.4 (Berkeley) %G%";
+static char sccsid[] = "@(#)scsiformat.c	5.5 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/param.h>
