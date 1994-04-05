59fc1ff1689df0eb7e469225b9b00950d970c15a
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1994-04-04 16:08:29 -0800

    non-proprietary version from Jan-Simon Pendry
    SCCS-vsn: usr.bin/apply/apply.c 8.2

--- usr/src/usr.bin/apply/apply.c
+++ usr/src/usr.bin/apply/apply.c
@@ -12,7 +12,7 @@ static char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)apply.c	8.1 (Berkeley) %G%";
+static char sccsid[] = "@(#)apply.c	8.2 (Berkeley) %G%";
 #endif /* not lint */
 
 /*
