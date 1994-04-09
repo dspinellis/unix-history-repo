27b6dcce96fc70750145acd7706429a0b611752c
Andreas Schulz <ats@g386bsd.first.bmd.de>
1994-04-09 13:03:39 +0000

    Comment out a private INT_MAX/INT_MIN definition to shut up the compiler.

--- gnu/usr.bin/groff/xditview/device.c
+++ gnu/usr.bin/groff/xditview/device.c
@@ -23,8 +23,10 @@ searching for device and font description files. */
 #define WS " \t\r\n"
 
 /* Minimum and maximum values a `signed int' can hold.  */
+/*
 #define INT_MIN (-INT_MAX-1)
 #define INT_MAX 2147483647
+*/
 
 #define CHAR_TABLE_SIZE 307
 
