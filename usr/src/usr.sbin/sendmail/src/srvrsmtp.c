bdf6d00462480b5da0f01a096f3cc287637c7f56
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-04-12 21:38:14 -0800

    make HELO and EHLO response consistent
    SCCS-vsn: usr.sbin/sendmail/src/srvrsmtp.c 8.34

--- usr/src/usr.sbin/sendmail/src/srvrsmtp.c
+++ usr/src/usr.sbin/sendmail/src/srvrsmtp.c
@@ -10,9 +10,9 @@
 
 #ifndef lint
 #ifdef SMTP
-static char sccsid[] = "@(#)srvrsmtp.c	8.33 (Berkeley) %G% (with SMTP)";
+static char sccsid[] = "@(#)srvrsmtp.c	8.34 (Berkeley) %G% (with SMTP)";
 #else
-static char sccsid[] = "@(#)srvrsmtp.c	8.33 (Berkeley) %G% (without SMTP)";
+static char sccsid[] = "@(#)srvrsmtp.c	8.34 (Berkeley) %G% (without SMTP)";
 #endif
 #endif /* not lint */
 
@@ -256,7 +256,7 @@ smtp(e)
 			
 			/* print extended message and brag */
 			message("250-%s Hello %s, pleased to meet you",
-				MyHostName, p);
+				MyHostName, CurSmtpClient);
 			if (!bitset(PRIV_NOEXPN, PrivacyFlags))
 				message("250-EXPN");
 			if (MaxMessageSize > 0)
