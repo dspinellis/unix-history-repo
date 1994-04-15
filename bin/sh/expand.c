3c1ca1d3bb3cba11adb02ab71f0b3d0e6f23f409
Andrey Chernov <ache@FreeBSD.org>
1994-04-15 15:27:24 +0000

    Bump expdir limit to 16384, Taylor 1.05 configure fails instead

--- bin/sh/expand.c
+++ bin/sh/expand.c
@@ -664,10 +664,10 @@ expandmeta(str)
 		savelastp = exparg.lastp;
 		INTOFF;
 		if (expdir == NULL)
-			expdir = ckmalloc(8192); /* I hope this is big enough */
+			expdir = ckmalloc(16384); /* I hope this is big enough */
 		expmeta(expdir, str->text);
-		if(strlen(expdir) >= 8192)
-			error("malloc overflow in sh:expand.c in ckmalloc(8192)\n");
+		if(strlen(expdir) >= 16384)
+			error("malloc overflow in sh:expand.c in ckmalloc(16384)\n");
 		ckfree(expdir);
 		expdir = NULL;
 		INTON;
