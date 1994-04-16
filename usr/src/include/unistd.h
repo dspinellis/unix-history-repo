f973e598b7bb68f3b6cd30e69e27602821902109
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1994-04-16 15:43:18 -0800

    add 1994 to the copyright
    SCCS-vsn: include/unistd.h 8.9

--- usr/src/include/unistd.h
+++ usr/src/include/unistd.h
@@ -1,10 +1,10 @@
 /*-
- * Copyright (c) 1991, 1993
+ * Copyright (c) 1991, 1993, 1994
  *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)unistd.h	8.8 (Berkeley) %G%
+ *	@(#)unistd.h	8.9 (Berkeley) %G%
  */
 
 #ifndef _UNISTD_H_
