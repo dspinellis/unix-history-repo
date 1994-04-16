4cea2580815a58a46331e6e9ad01e40d23a881d4
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-04-15 18:31:44 -0800

    defer error messages until after the collect is done to get around a disagreement with MVS about how the SMTP protocol works
    SCCS-vsn: usr.sbin/sendmail/src/collect.c 8.12

--- usr/src/usr.sbin/sendmail/src/collect.c
+++ usr/src/usr.sbin/sendmail/src/collect.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)collect.c	8.11 (Berkeley) %G%";
+static char sccsid[] = "@(#)collect.c	8.12 (Berkeley) %G%";
 #endif /* not lint */
 
 # include <errno.h>
@@ -37,6 +37,9 @@ static char sccsid[] = "@(#)collect.c	8.11 (Berkeley) %G%";
 **		The from person may be set.
 */
 
+char	*CollectErrorMessage;
+bool	CollectErrno;
+
 collect(smtpmode, requeueflag, e)
 	bool smtpmode;
 	bool requeueflag;
@@ -50,6 +53,9 @@ collect(smtpmode, requeueflag, e)
 	register int workbuflen;
 	extern char *hvalue();
 	extern bool isheader(), flusheol();
+
+	CollectErrorMessage = NULL;
+	CollectErrno = 0;
 
 	/*
 	**  Create the temp file name and create the file.
@@ -249,13 +255,23 @@ readerr:
 		tferror(tf, e);
 	if (fsync(fileno(tf)) < 0 || fclose(tf) < 0)
 	{
-		syserr("cannot sync message data to disk (%s)", e->e_df);
+		tferror(tf, e);
 		finis();
 	}
 
-	/* An EOF when running SMTP is an error */
-	if (inputerr && (OpMode == MD_SMTP || OpMode == MD_DAEMON))
+	if (CollectErrorMessage != NULL && Errors <= 0)
 	{
+		if (CollectErrno != 0)
+		{
+			errno = CollectErrno;
+			syserr(CollectErrorMessage, e->e_df);
+			finis();
+		}
+		usrerr(CollectErrorMessage);
+	}
+	else if (inputerr && (OpMode == MD_SMTP || OpMode == MD_DAEMON))
+	{
+		/* An EOF when running SMTP is an error */
 		char *host;
 		char *problem;
 
@@ -359,14 +375,12 @@ flusheol(buf, fp)
 	FILE *fp;
 {
 	register char *p = buf;
-	bool printmsg = TRUE;
 	char junkbuf[MAXLINE];
 
 	while (strchr(p, '\n') == NULL)
 	{
-		if (printmsg)
-			usrerr("553 header line too long");
-		printmsg = FALSE;
+		CollectErrorMessage = "553 header line too long";
+		CollectErrno = 0;
 		if (sfgets(junkbuf, MAXLINE, fp, TimeOuts.to_datablock,
 				"long line flush") == NULL)
 			return (FALSE);
@@ -393,6 +407,7 @@ tferror(tf, e)
 	FILE *tf;
 	register ENVELOPE *e;
 {
+	CollectErrno = errno;
 	if (errno == ENOSPC)
 	{
 		struct stat st;
@@ -423,10 +438,12 @@ tferror(tf, e)
 			fprintf(tf, "*** Currently, %ld kilobytes are available for mail temp files.\n",
 				avail);
 		}
-		usrerr("452 Out of disk space for temp file");
+		CollectErrorMessage = "452 Out of disk space for temp file";
 	}
 	else
-		syserr("collect: Cannot write %s", e->e_df);
+	{
+		CollectErrorMessage = "cannot write message body to disk (%s)";
+	}
 	(void) freopen("/dev/null", "w", tf);
 }
 /*
