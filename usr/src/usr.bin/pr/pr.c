7bfa8485e3f90f713f3591e1dd2cd066ad7dd9a1
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1994-04-16 19:50:24 -0800

    historically, pr permitted -w without a column specification POSIX.2 only requires that it be ignored for single-column, not that it be disallowed. From: "John F. Woods" <jfw@jfwhome.funhouse.com>
    SCCS-vsn: usr.bin/pr/pr.c 8.2

--- usr/src/usr.bin/pr/pr.c
+++ usr/src/usr.bin/pr/pr.c
@@ -16,7 +16,7 @@ static char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)pr.c	8.1 (Berkeley) %G%";
+static char sccsid[] = "@(#)pr.c	8.2 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -1718,9 +1718,6 @@ setup(argc, argv)
 			pgwd = SPGWD;
 		else
 			pgwd = PGWD;
-	} else if (clcnt == 1) {
-		(void)fputs("pr: -w requires multiple columns\n", err);
-		return(1);
 	}
 	if (cflag || merge) {
 		if (!eflag) {
