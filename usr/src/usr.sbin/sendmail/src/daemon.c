4dc5a3072f69185ac89ccedf9aca7ca258129b93
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-04-18 13:48:20 -0800

    sleep between retries
    SCCS-vsn: usr.sbin/sendmail/src/daemon.c 8.48

--- usr/src/usr.sbin/sendmail/src/daemon.c
+++ usr/src/usr.sbin/sendmail/src/daemon.c
@@ -11,9 +11,9 @@
 
 #ifndef lint
 #ifdef DAEMON
-static char sccsid[] = "@(#)daemon.c	8.47 (Berkeley) %G% (with daemon mode)";
+static char sccsid[] = "@(#)daemon.c	8.48 (Berkeley) %G% (with daemon mode)";
 #else
-static char sccsid[] = "@(#)daemon.c	8.47 (Berkeley) %G% (without daemon mode)";
+static char sccsid[] = "@(#)daemon.c	8.48 (Berkeley) %G% (without daemon mode)";
 #endif
 #endif /* not lint */
 
@@ -332,6 +332,8 @@ opendaemonsocket(firsttime)
 
 	do
 	{
+		if (ntries > 0)
+			sleep(5);
 		if (firsttime || DaemonSocket < 0)
 		{
 			DaemonSocket = socket(DaemonAddr.sa.sa_family, SOCK_STREAM, 0);
