ccb1f197a5b89da34c6b890a1ee53d840e60b5ba
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-04-19 05:18:09 -0800

    see ../RELEASE_NOTES
    SCCS-vsn: usr.sbin/sendmail/src/version.c 8.6.9.1

--- usr/src/usr.sbin/sendmail/src/version.c
+++ usr/src/usr.sbin/sendmail/src/version.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)version.c	8.6.8.1 (Berkeley) %G%";
+static char sccsid[] = "@(#)version.c	8.6.9.1 (Berkeley) %G%";
 #endif /* not lint */
 
-char	Version[] = "8.6.8";
+char	Version[] = "8.6.9";
