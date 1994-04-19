9c81b2b99b3092a660428c8f6ebaedb7c5760254
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-04-18 22:30:08 -0800

    fix UNIXWARE
    SCCS-vsn: usr.sbin/sendmail/src/conf.c 8.89

--- usr/src/usr.sbin/sendmail/src/conf.c
+++ usr/src/usr.sbin/sendmail/src/conf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)conf.c	8.88 (Berkeley) %G%";
+static char sccsid[] = "@(#)conf.c	8.89 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
