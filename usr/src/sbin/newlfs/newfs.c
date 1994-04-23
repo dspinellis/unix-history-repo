bbb82d089fc920e2ef4b247df0bc59e4a4f61091
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1994-04-22 19:14:22 -0800

    delete inclusion of <ufs/ffs/fs.h>
    SCCS-vsn: sbin/newlfs/newfs.c 8.3

--- usr/src/sbin/newlfs/newfs.c
+++ usr/src/sbin/newlfs/newfs.c
@@ -12,7 +12,7 @@ static char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)newfs.c	8.2 (Berkeley) %G%";
+static char sccsid[] = "@(#)newfs.c	8.3 (Berkeley) %G%";
 #endif /* not lint */
 
 /*
@@ -28,7 +28,6 @@ static char sccsid[] = "@(#)newfs.c	8.2 (Berkeley) %G%";
 
 #include <ufs/ufs/dir.h>
 #include <ufs/ufs/dinode.h>
-#include <ufs/ffs/fs.h>
 
 #include <errno.h>
 #include <unistd.h>
