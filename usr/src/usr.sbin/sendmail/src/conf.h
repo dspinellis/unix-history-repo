e3ee27caab4e9834857afc56e69ee41ef8ff3eb2
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-05-12 14:00:16 -0800

    portability fix for Solaris 2
    SCCS-vsn: usr.sbin/sendmail/src/conf.h 8.105

--- usr/src/usr.sbin/sendmail/src/conf.h
+++ usr/src/usr.sbin/sendmail/src/conf.h
@@ -5,7 +5,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)conf.h	8.104 (Berkeley) %G%
+ *	@(#)conf.h	8.105 (Berkeley) %G%
  */
 
 /*
@@ -197,7 +197,7 @@ extern int	syslog(int, char *, ...);
 #  define gethostbyaddr	solaris_gethostbyaddr	/* get working version */
 #  define GIDSET_T	gid_t
 #  ifndef _PATH_UNIX
-#   define _PATH_UNIX	"/kernel/unix"
+#   define _PATH_UNIX	"/dev/ksyms"
 #  endif
 #  ifndef _PATH_SENDMAILCF
 #   define _PATH_SENDMAILCF	"/etc/mail/sendmail.cf"
