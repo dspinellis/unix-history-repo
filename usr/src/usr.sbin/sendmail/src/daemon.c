8f5c01e78af1085400358f79fef6f23df8e87b14
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-05-18 17:45:19 -0800

    close daemon socket early to limit possible race condition
    SCCS-vsn: usr.sbin/sendmail/src/daemon.c 8.51

--- usr/src/usr.sbin/sendmail/src/daemon.c
+++ usr/src/usr.sbin/sendmail/src/daemon.c
@@ -12,9 +12,9 @@
 
 #ifndef lint
 #ifdef DAEMON
-static char sccsid[] = "@(#)daemon.c	8.50 (Berkeley) %G% (with daemon mode)";
+static char sccsid[] = "@(#)daemon.c	8.51 (Berkeley) %G% (with daemon mode)";
 #else
-static char sccsid[] = "@(#)daemon.c	8.50 (Berkeley) %G% (without daemon mode)";
+static char sccsid[] = "@(#)daemon.c	8.51 (Berkeley) %G% (without daemon mode)";
 #endif
 #endif /* not lint */
 
