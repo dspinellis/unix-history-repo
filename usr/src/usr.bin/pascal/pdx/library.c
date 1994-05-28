ae9ff2a34e235260b9ee46152d133d53dd403b8b
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1994-05-27 22:22:07 -0800

    eliminate bogus sys_errlist declaration
    SCCS-vsn: usr.bin/pascal/pdx/library.c 8.2

--- usr/src/usr.bin/pascal/pdx/library.c
+++ usr/src/usr.bin/pascal/pdx/library.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)library.c	8.1 (Berkeley) %G%";
+static char sccsid[] = "@(#)library.c	8.2 (Berkeley) %G%";
 #endif /* not lint */
 
 /*
@@ -234,7 +234,6 @@ int pid;
  */
 
 extern int sys_nerr;
-extern char *sys_errlist[];
  
 /*
  * Before calling syserr, the integer errno is set to contain the
