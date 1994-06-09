fcad5ca383f94a50cf9a84d5b77525ef9d6c5474
Mike Hibler <hibler@ucbvax.Berkeley.EDU>
1994-06-08 23:00:16 -0800

    on write, be sure to flush the vnode from the VM object cache
    SCCS-vsn: sys/nfs/nfs_bio.c 8.6

--- usr/src/sys/nfs/nfs_bio.c
+++ usr/src/sys/nfs/nfs_bio.c
@@ -7,7 +7,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)nfs_bio.c	8.5 (Berkeley) %G%
+ *	@(#)nfs_bio.c	8.6 (Berkeley) %G%
  */
 
 #include <sys/param.h>
@@ -387,6 +387,11 @@ nfs_write(ap)
 	 */
 	biosize = nmp->nm_rsize;
 	do {
+
+		/*
+		 * XXX make sure we aren't cached in the VM page cache
+		 */
+		(void)vnode_pager_uncache(vp);
 
 		/*
 		 * Check for a valid write lease.
