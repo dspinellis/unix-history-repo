021fccd388dbf6bdc7629ea8dd531a1dde58f734
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-06-19 14:34:21 -0800

    avoid denial-of-service attacks caused by setting resource limits low before calling sendmail
    SCCS-vsn: usr.sbin/sendmail/src/READ_ME 8.66
    SCCS-vsn: usr.sbin/sendmail/src/conf.h 8.110
    SCCS-vsn: usr.sbin/sendmail/src/conf.c 8.92
    SCCS-vsn: usr.sbin/sendmail/src/alias.c 8.28

--- usr/src/usr.sbin/sendmail/src/conf.c
+++ usr/src/usr.sbin/sendmail/src/conf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)conf.c	8.91 (Berkeley) %G%";
+static char sccsid[] = "@(#)conf.c	8.92 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
--- usr/src/usr.sbin/sendmail/src/conf.h
+++ usr/src/usr.sbin/sendmail/src/conf.h
@@ -5,7 +5,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)conf.h	8.109 (Berkeley) %G%
+ *	@(#)conf.h	8.110 (Berkeley) %G%
  */
 
 /*
