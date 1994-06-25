5085d974eabdcfb46e31ac05d3eaaf844a2e77c5
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-06-24 19:50:16 -0800

    diagnose bogus HELO commands
    SCCS-vsn: usr.sbin/sendmail/src/srvrsmtp.c 8.41

--- usr/src/usr.sbin/sendmail/src/srvrsmtp.c
+++ usr/src/usr.sbin/sendmail/src/srvrsmtp.c
@@ -10,9 +10,9 @@
 
 #ifndef lint
 #ifdef SMTP
-static char sccsid[] = "@(#)srvrsmtp.c	8.40 (Berkeley) %G% (with SMTP)";
+static char sccsid[] = "@(#)srvrsmtp.c	8.41 (Berkeley) %G% (with SMTP)";
 #else
-static char sccsid[] = "@(#)srvrsmtp.c	8.40 (Berkeley) %G% (without SMTP)";
+static char sccsid[] = "@(#)srvrsmtp.c	8.41 (Berkeley) %G% (without SMTP)";
 #endif
 #endif /* not lint */
 
