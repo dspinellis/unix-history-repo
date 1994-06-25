36be3e055e16b95d330c2ef84d1c34e6ddeb2ffa
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-06-25 15:40:20 -0800

    relax chownsafe rules a bit
    SCCS-vsn: usr.sbin/sendmail/src/conf.c 8.94

--- usr/src/usr.sbin/sendmail/src/conf.c
+++ usr/src/usr.sbin/sendmail/src/conf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)conf.c	8.93 (Berkeley) %G%";
+static char sccsid[] = "@(#)conf.c	8.94 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
