e4a16db9be58f6040fc5cf7a5fa5dc4d46c1751c
Keith Sklower <sklower@ucbvax.Berkeley.EDU>
1994-07-01 19:56:09 -0800

    bugfix from karels & husemann in ifwithdstaddr()
    SCCS-vsn: sys/net/if.c 8.4

--- usr/src/sys/net/if.c
+++ usr/src/sys/net/if.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)if.c	8.3 (Berkeley) %G%
+ *	@(#)if.c	8.4 (Berkeley) %G%
  */
 
 #include <sys/param.h>
@@ -172,7 +172,8 @@ ifa_ifwithdstaddr(addr)
 	for (ifp = ifnet; ifp; ifp = ifp->if_next) 
 	    if (ifp->if_flags & IFF_POINTOPOINT)
 		for (ifa = ifp->if_addrlist; ifa; ifa = ifa->ifa_next) {
-			if (ifa->ifa_addr->sa_family != addr->sa_family)
+			if (ifa->ifa_addr->sa_family != addr->sa_family ||
+			    ifa->ifa_dstaddr == NULL)
 				continue;
 			if (equal(addr, ifa->ifa_dstaddr))
 				return (ifa);
