63b796546d0be999b57e6f498438ab41373e271e
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-07-03 18:48:37 -0800

    break out special local mail processing (e.g., mapping to the "prog" mailer) onto separate mailer flags
    SCCS-vsn: usr.sbin/sendmail/src/usersmtp.c 8.20
    SCCS-vsn: usr.sbin/sendmail/src/headers.c 8.33
    SCCS-vsn: usr.sbin/sendmail/src/recipient.c 8.46
    SCCS-vsn: usr.sbin/sendmail/src/main.c 8.62
    SCCS-vsn: usr.sbin/sendmail/src/envelope.c 8.36
    SCCS-vsn: usr.sbin/sendmail/src/parseaddr.c 8.34
    SCCS-vsn: usr.sbin/sendmail/src/collect.c 8.16
    SCCS-vsn: usr.sbin/sendmail/src/sendmail.h 8.45
    SCCS-vsn: usr.sbin/sendmail/src/savemail.c 8.32
    SCCS-vsn: usr.sbin/sendmail/src/TODO 8.24
    SCCS-vsn: usr.sbin/sendmail/src/srvrsmtp.c 8.42
    SCCS-vsn: usr.sbin/sendmail/src/deliver.c 8.87
    SCCS-vsn: usr.sbin/sendmail/src/conf.c 8.97
    SCCS-vsn: usr.sbin/sendmail/src/alias.c 8.29

--- usr/src/usr.sbin/sendmail/src/conf.c
+++ usr/src/usr.sbin/sendmail/src/conf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)conf.c	8.96 (Berkeley) %G%";
+static char sccsid[] = "@(#)conf.c	8.97 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -381,7 +381,7 @@ ttypath()
 **	message should be given using "usrerr" and 0 should
 **	be returned.
 **
-**	'NoReturn' can be set to suppress the return-to-sender
+**	EF_NORETURN can be set in e->e_flags to suppress the return-to-sender
 **	function; this should be done on huge messages.
 **
 **	Parameters:
@@ -412,11 +412,11 @@ checkcompat(to, e)
 	register STAB *s;
 
 	s = stab("arpa", ST_MAILER, ST_FIND);
-	if (s != NULL && e->e_from.q_mailer != LocalMailer &&
+	if (s != NULL && strcmp(e->e_from.q_mailer->m_name, "local") != 0 &&
 	    to->q_mailer == s->s_mailer)
 	{
 		usrerr("553 No ARPA mail through this machine: see your system administration");
-		/* NoReturn = TRUE; to supress return copy */
+		/* e->e_flags |= EF_NORETURN; to supress return copy */
 		return (EX_UNAVAILABLE);
 	}
 # endif /* EXAMPLE_CODE */
--- usr/src/usr.sbin/sendmail/src/savemail.c
+++ usr/src/usr.sbin/sendmail/src/savemail.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)savemail.c	8.31 (Berkeley) %G%";
+static char sccsid[] = "@(#)savemail.c	8.32 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -162,7 +162,7 @@ savemail(e)
 		switch (state)
 		{
 		  case ESM_QUIET:
-			if (e->e_from.q_mailer == LocalMailer)
+			if (bitnset(M_LOCALMAILER, e->e_from.q_mailer->m_flags))
 				state = ESM_DEADLETTER;
 			else
 				state = ESM_MAIL;
@@ -304,7 +304,7 @@ savemail(e)
 			*/
 
 			p = NULL;
-			if (e->e_from.q_mailer == LocalMailer)
+			if (bitnset(M_HASPWENT, e->e_from.q_mailer->m_flags))
 			{
 				if (e->e_from.q_home != NULL)
 					p = e->e_from.q_home;
@@ -669,7 +669,7 @@ errbody(mci, e)
 	**  Output text of original message
 	*/
 
-	if (NoReturn)
+	if (bitset(EF_NORETURN, e->e_flags))
 		SendBody = FALSE;
 	putline("", mci);
 	if (e->e_parent->e_df != NULL)
