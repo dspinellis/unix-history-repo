63a5f6fe9dd93d5e9101516c536c39d16c0c72f6
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1994-07-08 00:48:35 -0800

    since size_t is unsigned must check for overflow incrementally (from karels)
    SCCS-vsn: sys/kern/uipc_syscalls.c 8.5

--- usr/src/sys/kern/uipc_syscalls.c
+++ usr/src/sys/kern/uipc_syscalls.c
@@ -487,10 +487,9 @@ sendit(p, s, mp, flags, retsize)
 	auio.uio_resid = 0;
 	iov = mp->msg_iov;
 	for (i = 0; i < mp->msg_iovlen; i++, iov++) {
-		if (iov->iov_len < 0)
+		if (auio.uio_resid + iov->iov_len < auio.uio_resid)
 			return (EINVAL);
-		if ((auio.uio_resid += iov->iov_len) < 0)
-			return (EINVAL);
+		auio.uio_resid += iov->iov_len;
 	}
 	if (mp->msg_name) {
 		if (error = sockargs(&to, mp->msg_name, mp->msg_namelen,
@@ -752,10 +751,9 @@ recvit(p, s, mp, namelenp, retsize)
 	auio.uio_resid = 0;
 	iov = mp->msg_iov;
 	for (i = 0; i < mp->msg_iovlen; i++, iov++) {
-		if (iov->iov_len < 0)
+		if (auio.uio_resid + iov->iov_len < auio.uio_resid)
 			return (EINVAL);
-		if ((auio.uio_resid += iov->iov_len) < 0)
-			return (EINVAL);
+		auio.uio_resid += iov->iov_len;
 	}
 #ifdef KTRACE
 	if (KTRPOINT(p, KTR_GENIO)) {
