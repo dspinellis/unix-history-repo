050a889461b21d7a6bb6581071a799fc6e898814
Keith Sklower <sklower@ucbvax.Berkeley.EDU>
1994-07-19 17:36:07 -0800

    isop_laddr is a pointer to a sockaddr_iso, not the thing itself; change line about 231 to isop->laddr->siso_tlen
    SCCS-vsn: sys/netiso/iso_pcb.c 8.3

--- usr/src/sys/netiso/iso_pcb.c
+++ usr/src/sys/netiso/iso_pcb.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)iso_pcb.c	8.2 (Berkeley) %G%
+ *	@(#)iso_pcb.c	8.3 (Berkeley) %G%
  */
 
 /***********************************************************
@@ -202,7 +202,7 @@ iso_pcbbind(isop, nam)
 		register char *cp;
 noname:
 		cp = TSEL(isop->isop_laddr);
-		isop->isop_laddr.siso_tlen = 2;
+		isop->isop_laddr->siso_tlen = 2;
 	IFDEBUG(D_ISO)
 		printf("iso_pcbbind noname\n");
 	ENDDEBUG
