c02dcd1dfd33a73d2282c27b17f3a29add328b91
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-08-07 22:45:20 -0800

    BrokenSmtpPeers is now an option
    SCCS-vsn: usr.sbin/sendmail/src/sendmail.h 8.51

--- usr/src/usr.sbin/sendmail/src/sendmail.h
+++ usr/src/usr.sbin/sendmail/src/sendmail.h
@@ -5,7 +5,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)sendmail.h	8.50 (Berkeley) %G%
+ *	@(#)sendmail.h	8.51 (Berkeley) %G%
  */
 
 /*
@@ -15,7 +15,7 @@
 # ifdef _DEFINE
 # define EXTERN
 # ifndef lint
-static char SmailSccsId[] =	"@(#)sendmail.h	8.50		%G%";
+static char SmailSccsId[] =	"@(#)sendmail.h	8.51		%G%";
 # endif
 # else /*  _DEFINE */
 # define EXTERN extern
