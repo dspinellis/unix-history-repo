ff0895ffa891f1ae65fa98680daaf4a3f9ee2a9d
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-08-07 23:31:30 -0800

    allow V7 config files (to make long option names safe)
    SCCS-vsn: usr.sbin/sendmail/src/main.c 8.64

--- usr/src/usr.sbin/sendmail/src/main.c
+++ usr/src/usr.sbin/sendmail/src/main.c
@@ -13,7 +13,7 @@ static char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)main.c	8.63 (Berkeley) %G%";
+static char sccsid[] = "@(#)main.c	8.64 (Berkeley) %G%";
 #endif /* not lint */
 
 #define	_DEFINE
