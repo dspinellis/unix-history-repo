24675bee43f88018f188630a01400b0f289c6c73
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-08-08 01:44:49 -0800

    add EightBitMode as long option name
    SCCS-vsn: usr.sbin/sendmail/src/readcf.c 8.32

--- usr/src/usr.sbin/sendmail/src/readcf.c
+++ usr/src/usr.sbin/sendmail/src/readcf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)readcf.c	8.31 (Berkeley) %G%";
+static char sccsid[] = "@(#)readcf.c	8.32 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -1074,6 +1074,7 @@ struct optioninfo
 } OptionTab[] =
 {
 	"SevenBitInput",	'7',	TRUE,
+	"EightBitMode",		'8',	TRUE,
 	"AliasFile",		'A',	FALSE,
 	"AliasWait",		'a',	FALSE,
 	"BlankSub",		'B',	FALSE,
