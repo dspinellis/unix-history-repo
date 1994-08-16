2da7adfe87db43a06f4fb83fd3289c1dbb72ec9e
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-08-16 15:20:07 -0800

    fix initial daemon socket open
    SCCS-vsn: usr.sbin/sendmail/src/daemon.c 8.59

--- usr/src/usr.sbin/sendmail/src/daemon.c
+++ usr/src/usr.sbin/sendmail/src/daemon.c
@@ -11,9 +11,9 @@
 
 #ifndef lint
 #ifdef DAEMON
-static char sccsid[] = "@(#)daemon.c	8.58 (Berkeley) %G% (with daemon mode)";
+static char sccsid[] = "@(#)daemon.c	8.59 (Berkeley) %G% (with daemon mode)";
 #else
-static char sccsid[] = "@(#)daemon.c	8.58 (Berkeley) %G% (without daemon mode)";
+static char sccsid[] = "@(#)daemon.c	8.59 (Berkeley) %G% (without daemon mode)";
 #endif
 #endif /* not lint */
 
@@ -178,10 +178,9 @@ getrequests()
 		}
 
 		/* arrange to (re)open the socket if necessary */
-		if (DaemonSocket < 0)
-			(void) opendaemonsocket(FALSE);
 		if (refusingconnections)
 		{
+			(void) opendaemonsocket(FALSE);
 			setproctitle("accepting connections");
 			refusingconnections = FALSE;
 		}
