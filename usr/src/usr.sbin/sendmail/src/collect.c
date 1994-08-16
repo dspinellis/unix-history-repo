a6e11eeef6d0af75556d06ae696cc9f80f44fd4b
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-08-16 15:19:51 -0800

    fix a couple of bugs having to do with CRLF mapping
    SCCS-vsn: usr.sbin/sendmail/src/collect.c 8.23

--- usr/src/usr.sbin/sendmail/src/collect.c
+++ usr/src/usr.sbin/sendmail/src/collect.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)collect.c	8.22 (Berkeley) %G%";
+static char sccsid[] = "@(#)collect.c	8.23 (Berkeley) %G%";
 #endif /* not lint */
 
 # include <errno.h>
@@ -237,17 +237,18 @@ maketemp(from)
 				break;
 
 			  case IS_CR:
-				if (c != '\n')
+				if (c == '\n')
+					istate = IS_BOL;
+				else
 				{
 					ungetc(c, InChannel);
 					c = '\r';
+					istate = IS_NORM;
 				}
-				else if (!bitset(EF_CRLF_NOT_EOL, e->e_flags))
-					istate = IS_BOL;
-				break;
+				goto bufferchar;
 			}
 
-			if (c == '\r')
+			if (c == '\r' && !bitset(EF_CRLF_NOT_EOL, e->e_flags))
 			{
 				istate = IS_CR;
 				continue;
@@ -257,6 +258,7 @@ maketemp(from)
 			else
 				istate = IS_NORM;
 
+bufferchar:
 			if (mstate == MS_BODY)
 			{
 				/* just put the character out */
