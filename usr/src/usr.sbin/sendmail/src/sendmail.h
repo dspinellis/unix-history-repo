e32556ae13bd960ea7f21a8702efe93848370f01
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-08-17 16:49:01 -0800

    add some new configuration values (snitched from KJS)
    SCCS-vsn: usr.sbin/sendmail/src/sendmail.h 8.58

--- usr/src/usr.sbin/sendmail/src/sendmail.h
+++ usr/src/usr.sbin/sendmail/src/sendmail.h
@@ -5,7 +5,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)sendmail.h	8.57 (Berkeley) %G%
+ *	@(#)sendmail.h	8.58 (Berkeley) %G%
  */
 
 /*
@@ -15,7 +15,7 @@
 # ifdef _DEFINE
 # define EXTERN
 # ifndef lint
-static char SmailSccsId[] =	"@(#)sendmail.h	8.57		%G%";
+static char SmailSccsId[] =	"@(#)sendmail.h	8.58		%G%";
 # endif
 # else /*  _DEFINE */
 # define EXTERN extern
@@ -865,6 +865,9 @@ EXTERN char	*ForwardPath;	/* path to search for .forward files */
 EXTERN long	MinBlocksFree;	/* min # of blocks free on queue fs */
 EXTERN char	*FallBackMX;	/* fall back MX host */
 EXTERN long	MaxMessageSize;	/* advertised max size we will accept */
+EXTERN time_t	MaxHostStatAge;	/* max age of cached host status info */
+EXTERN time_t	MinQueueAge;	/* min delivery interval */
+EXTERN int	DeliveryNiceness;	/* how nice to be during delivery */
 EXTERN char	*PostMasterCopy;	/* address to get errs cc's */
 EXTERN int	CheckpointInterval;	/* queue file checkpoint interval */
 EXTERN bool	DontPruneRoutes;	/* don't prune source routes */
