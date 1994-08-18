8ccc1e1a606a93410db84112a6d033ce9ea9d233
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-08-17 16:49:01 -0800

    add some new configuration values (snitched from KJS)
    SCCS-vsn: usr.sbin/sendmail/src/sendmail.h 8.58

--- usr/src/usr.sbin/sendmail/src/sendmail.h
+++ usr/src/usr.sbin/sendmail/src/sendmail.h
@@ -5,7 +5,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)sendmail.h	8.57 (Berkeley) %G%
+ *	@(#)sendmail.h	8.58 (Berkeley) %G%
  */
 
 /*
@@ -15,7 +15,7 @@
 # ifdef _DEFINE
 # define EXTERN
 # ifndef lint
-static char SmailSccsId[] =	"@(#)sendmail.h	8.57		%G%";
+static char SmailSccsId[] =	"@(#)sendmail.h	8.58		%G%";
 # endif
 # else /*  _DEFINE */
 # define EXTERN extern
