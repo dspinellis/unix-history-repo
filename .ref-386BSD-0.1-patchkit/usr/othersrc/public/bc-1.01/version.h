e7e000d1b886c626fe49cc4c5d1cae70fa81893c
Garrett Wollman <wollman@FreeBSD.org>
1994-09-29 23:04:24 +0000

    xntp 3.4e from Dave Mills @ UDel

--- .ref-386BSD-0.1-patchkit/usr/othersrc/public/bc-1.01/version.h
+++
@@ -1,3 +0,0 @@
-#define BC_VERSION  \
-  "bc 1.01 (Nov 25, 1991), Copyright (C) 1991 Free Software Foundation, Inc."
-
--- .ref-386BSD-0.1-patchkit/usr/othersrc/public/ghostscript-2.4.1/gslp.bat
+++
@@ -1,1 +0,0 @@
-@gs -q -sDEVICE=epson -r180 -dDISKFONTS -dNOPAUSE -- gslp.ps %1 %2 %3 %4 %5 %6 %7 %8 %9
