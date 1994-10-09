20ce5de1ef32e3ac760dadc2ca5d7019b8033688
Keith Sklower <sklower@ucbvax.Berkeley.EDU>
1994-10-09 09:44:23 -0800

    this version corresponds to radix.c SID 8.2.2.1 making use of normal routes when present, but allowing non-contiguous masks, \
    SCCS-vsn: sys/net/radix.h 8.1.2.1

--- usr/src/sys/net/radix.h
+++ usr/src/sys/net/radix.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)radix.h	8.1.1.1 (Berkeley) %G%
+ *	@(#)radix.h	8.1.2.1 (Berkeley) %G%
  */
 
 #ifndef _RADIX_H_
@@ -15,7 +15,7 @@
  */
 
 struct radix_node {
-	struct	radix_node *rn_mklist;	/* list of masks contained in subtree */
+	struct	radix_mask *rn_mklist;	/* list of masks contained in subtree */
 	struct	radix_node *rn_p;	/* parent */
 	short	rn_b;			/* bit offset; -1-index(netmask) */
 	char	rn_bmask;		/* node: mask for bit test*/
@@ -49,6 +49,34 @@ struct radix_node {
 #define rn_l rn_u.rn_node.rn_L
 #define rn_r rn_u.rn_node.rn_R
 
+/*
+ * Annotations to tree concerning potential routes applying to subtrees.
+ */
+
+extern struct radix_mask {
+	short	rm_b;			/* bit offset; -1-index(netmask) */
+	char	rm_unused;		/* cf. rn_bmask */
+	u_char	rm_flags;		/* cf. rn_flags */
+	struct	radix_mask *rm_mklist;	/* more masks to try */
+	union	{
+		caddr_t	rmu_mask;		/* the mask */
+		struct	radix_node *rmu_leaf;	/* for normal routes */
+	}	rm_rmu;
+	int	rm_refs;		/* # of references to this struct */
+} *rn_mkfreelist;
+
+#define rm_mask rm_rmu.rmu_mask
+#define rm_leaf rm_rmu.rmu_leaf		/* extra field would make 32 bytes */
+
+#define MKGet(m) {\
+	if (rn_mkfreelist) {\
+		m = rn_mkfreelist; \
+		rn_mkfreelist = (m)->rm_mklist; \
+	} else \
+		R_Malloc(m, struct radix_mask *, sizeof (*(m))); }\
+
+#define MKFree(m) { (m)->rm_mklist = rn_mkfreelist; rn_mkfreelist = (m);}
+
 struct radix_node_head {
 	struct	radix_node *rnh_treetop;
 	int	rnh_addrsize;		/* permit, but not require fixed keys */
@@ -65,29 +93,19 @@ struct radix_node_head {
 		__P((void *v, void *mask, struct radix_node_head *head));
 	struct	radix_node *(*rnh_matchaddr)	/* locate based on sockaddr */
 		__P((void *v, struct radix_node_head *head));
+	struct	radix_node *(*rnh_lookup)	/* locate based on sockaddr */
+		__P((void *v, void *mask, struct radix_node_head *head));
 	struct	radix_node *(*rnh_matchpkt)	/* locate based on packet hdr */
 		__P((void *v, struct radix_node_head *head));
 	int	(*rnh_walktree)			/* traverse tree */
 		__P((struct radix_node_head *head, int (*f)(), void *w));
-/* mapping stuff */
-	struct	radix_index_table  {
-		short	limit;
-		short	offset;
-	}	*rnh_table;			/* how to re-order the bits */
-	int	rnh_offset;			/* for martialed keys */
-	int	(*rnh_bits_matched)		/* used in matching, insert */
-		__P((void *trial, void *ref,
-		     struct radix_node_head *head, int masklen));
-	int	(*rnh_set_mask)			/* used in insertion */
-		__P((int index,
-		     struct radix_node *rn, struct radix_node_head *rnh));
-/* the treetop itself */
 	struct	radix_node rnh_nodes[3];	/* empty tree for common case */
 };
 
 
 #ifndef KERNEL
 #define Bcmp(a, b, n) bcmp(((char *)(a)), ((char *)(b)), (n))
+#define Bcopy(a, b, n) bcopy(((char *)(a)), ((char *)(b)), (unsigned)(n))
 #define Bzero(p, n) bzero((char *)(p), (int)(n));
 #define R_Malloc(p, t, n) (p = (t) malloc((unsigned int)(n)))
 #define Free(p) free((char *)p);
@@ -113,16 +131,6 @@ struct radix_node
 	 *rn_match __P((void *, struct radix_node_head *)),
 	 *rn_newpair __P((void *, int, struct radix_node[2])),
 	 *rn_search __P((void *, struct radix_node *)),
-	 *rn_search_unmapped __P((void *, struct radix_node_head *));
-
-int	 rn_set_unmapped_mask
-		__P((int, struct radix_node *, struct radix_node_head *)),
-	 rn_set_mapped_mask
-		__P((int, struct radix_node *, struct radix_node_head *)),
-	 rn_mapped_bits_matched
-		__P((void *, void *, struct radix_node_head *, int)),
-	 rn_unmapped_bits_matched
-		__P((void *, void *, struct radix_node_head *, int));
-					
+	 *rn_search_m __P((void *, struct radix_node *, void *));
 
 #endif /* _RADIX_H_ */
