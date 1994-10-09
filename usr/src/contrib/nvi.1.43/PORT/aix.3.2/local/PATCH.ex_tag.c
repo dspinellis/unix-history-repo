6b31bbd77044d71ac43597fe7033b37394138d6d
CSRG <csrg@ucbvax.Berkeley.EDU>
1994-10-09 00:28:30 -0800

    BSD 4_4_Lite2 development Work on file usr/src/contrib/nvi.1.43/PORT/aix.3.2/local/PATCH.ex_tag.c
    Synthesized-from: CSRG/cd3/4.4BSD-Lite2

---
+++ usr/src/contrib/nvi.1.43/PORT/aix.3.2/local/PATCH.ex_tag.c
@@ -0,0 +1,12 @@
+*** ex_tag.c.orig	Tue Mar 22 12:24:23 1994
+--- ex_tag.c	Tue Mar 29 19:05:56 1994
+***************
+*** 39,44 ****
+--- 39,45 ----
+  #endif /* not lint */
+  
+  #include <sys/param.h>
++ #include <sys/types.h>
+  #include <sys/mman.h>
+  #include <sys/queue.h>
+  #include <sys/stat.h>
