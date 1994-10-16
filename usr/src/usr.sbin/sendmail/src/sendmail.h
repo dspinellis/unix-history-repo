176f7b1440da0fce7a280d93628fb7c3df78dcd7
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-10-15 16:55:43 -0800

    allow option to set default character set for MIME
    SCCS-vsn: usr.sbin/sendmail/src/sendmail.h 8.63
    SCCS-vsn: usr.sbin/sendmail/src/readcf.c 8.40
    SCCS-vsn: usr.sbin/sendmail/src/deliver.c 8.100

--- usr/src/usr.sbin/sendmail/src/sendmail.h
+++ usr/src/usr.sbin/sendmail/src/sendmail.h
@@ -5,7 +5,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)sendmail.h	8.62 (Berkeley) %G%
+ *	@(#)sendmail.h	8.63 (Berkeley) %G%
  */
 
 /*
@@ -15,7 +15,7 @@
 # ifdef _DEFINE
 # define EXTERN
 # ifndef lint
-static char SmailSccsId[] =	"@(#)sendmail.h	8.62		%G%";
+static char SmailSccsId[] =	"@(#)sendmail.h	8.63		%G%";
 # endif
 # else /*  _DEFINE */
 # define EXTERN extern
