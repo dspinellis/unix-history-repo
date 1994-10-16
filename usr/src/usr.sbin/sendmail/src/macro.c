8a0a7129982b24e1e625bc9a3177238a302d0f94
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-10-15 17:50:13 -0800

    check some possible errors on long macro names
    SCCS-vsn: usr.sbin/sendmail/src/macro.c 8.5

--- usr/src/usr.sbin/sendmail/src/macro.c
+++ usr/src/usr.sbin/sendmail/src/macro.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)macro.c	8.4 (Berkeley) %G%";
+static char sccsid[] = "@(#)macro.c	8.5 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
