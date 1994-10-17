22485b143e78a52a79181611ab0c4a4481282c18
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-10-17 04:26:18 -0800

    allow "u" option to take "user:group" as a value
    SCCS-vsn: usr.sbin/sendmail/src/readcf.c 8.42

--- usr/src/usr.sbin/sendmail/src/readcf.c
+++ usr/src/usr.sbin/sendmail/src/readcf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)readcf.c	8.41 (Berkeley) %G%";
+static char sccsid[] = "@(#)readcf.c	8.42 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -1391,6 +1391,7 @@ setoption(opt, val, sticky)
 		break;
 
 	  case 'g':		/* default gid */
+  g_opt:
 		if (isascii(*val) && isdigit(*val))
 			DefGid = atoi(val);
 		else
@@ -1400,7 +1401,8 @@ setoption(opt, val, sticky)
 			DefGid = -1;
 			gr = getgrnam(val);
 			if (gr == NULL)
-				syserr("readcf: option g: unknown group %s", val);
+				syserr("readcf: option %c: unknown group %s",
+					opt, val);
 			else
 				DefGid = gr->gr_gid;
 		}
@@ -1592,6 +1594,14 @@ setoption(opt, val, sticky)
 		break;
 
 	  case 'u':		/* set default uid */
+		for (p = val; *p != '\0'; p++)
+		{
+			if (*p == '.' || *p == '/' || *p == ':')
+			{
+				*p++ = '\0';
+				break;
+			}
+		}
 		if (isascii(*val) && isdigit(*val))
 			DefUid = atoi(val);
 		else
@@ -1603,10 +1613,18 @@ setoption(opt, val, sticky)
 			if (pw == NULL)
 				syserr("readcf: option u: unknown user %s", val);
 			else
+			{
 				DefUid = pw->pw_uid;
+				DefGid = pw->pw_gid;
+			}
 		}
 		setdefuser();
-		break;
+
+		/* handle the group if it is there */
+		if (*p == '\0')
+			break;
+		val = p;
+		goto g_opt;
 
 	  case 'V':		/* fallback MX host */
 		FallBackMX = newstr(val);
