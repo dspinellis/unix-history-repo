82366afc4279eaaee8c521acbb0659a6c8b47874
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-10-17 16:24:32 -0800

    use -s flag on dequote map to indicate space substitution
    SCCS-vsn: usr.sbin/sendmail/src/parseaddr.c 8.38

--- usr/src/usr.sbin/sendmail/src/parseaddr.c
+++ usr/src/usr.sbin/sendmail/src/parseaddr.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)parseaddr.c	8.37 (Berkeley) %G%";
+static char sccsid[] = "@(#)parseaddr.c	8.38 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -1840,6 +1840,10 @@ dequote_init(map, args)
 		  case 'a':
 			map->map_app = ++p;
 			break;
+
+		  case 's':
+			map->map_coldelim = *++p;
+			break;
 		}
 		while (*p != '\0' && !(isascii(*p) && isspace(*p)))
 			p++;
@@ -1876,19 +1880,13 @@ dequote_map(map, name, av, statp)
 	register char *p;
 	register char *q;
 	register char c;
-	int anglecnt;
-	int cmntcnt;
-	int quotecnt;
-	int spacecnt;
-	bool quotemode;
-	bool bslashmode;
-
-	anglecnt = 0;
-	cmntcnt = 0;
-	quotecnt = 0;
-	spacecnt = 0;
-	quotemode = FALSE;
-	bslashmode = FALSE;
+	int anglecnt = 0;
+	int cmntcnt = 0;
+	int quotecnt = 0;
+	int spacecnt = 0;
+	bool quotemode = FALSE;
+	bool bslashmode = FALSE;
+	char spacesub = map->map_coldelim;
 
 	for (p = q = name; (c = *p++) != '\0'; )
 	{
@@ -1899,8 +1897,8 @@ dequote_map(map, name, av, statp)
 			continue;
 		}
 
-		if (c == ' ' && SpaceSub != '\0')
-			c = SpaceSub;
+		if (c == ' ' && spacesub != '\0')
+			c = spacesub;
 
 		switch (c)
 		{
