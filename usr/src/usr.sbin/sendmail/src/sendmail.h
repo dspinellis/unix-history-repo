ca50ea0f122b987bf1202fc87b440d22449c0c21
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-10-25 01:04:47 -0800

    add ServiceSwitchFile configuration option
    SCCS-vsn: usr.sbin/sendmail/src/sendmail.h 8.65

--- usr/src/usr.sbin/sendmail/src/sendmail.h
+++ usr/src/usr.sbin/sendmail/src/sendmail.h
@@ -5,7 +5,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)sendmail.h	8.64 (Berkeley) %G%
+ *	@(#)sendmail.h	8.65 (Berkeley) %G%
  */
 
 /*
@@ -15,7 +15,7 @@
 # ifdef _DEFINE
 # define EXTERN
 # ifndef lint
-static char SmailSccsId[] =	"@(#)sendmail.h	8.64		%G%";
+static char SmailSccsId[] =	"@(#)sendmail.h	8.65		%G%";
 # endif
 # else /*  _DEFINE */
 # define EXTERN extern
