85d2abebc4f14dc5b9bcfc31ec5b3878a20bd844
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-10-26 01:19:38 -0800

    compile problems on various platforms
    SCCS-vsn: usr.sbin/sendmail/src/main.c 8.69
    SCCS-vsn: usr.sbin/sendmail/src/conf.c 8.112
    SCCS-vsn: usr.sbin/sendmail/src/map.c 8.34

--- usr/src/usr.sbin/sendmail/src/conf.c
+++ usr/src/usr.sbin/sendmail/src/conf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)conf.c	8.111 (Berkeley) %G%";
+static char sccsid[] = "@(#)conf.c	8.112 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
--- usr/src/usr.sbin/sendmail/src/main.c
+++ usr/src/usr.sbin/sendmail/src/main.c
@@ -13,7 +13,7 @@ static char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)main.c	8.68 (Berkeley) %G%";
+static char sccsid[] = "@(#)main.c	8.69 (Berkeley) %G%";
 #endif /* not lint */
 
 #define	_DEFINE
