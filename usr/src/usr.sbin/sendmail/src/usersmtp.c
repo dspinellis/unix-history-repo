f23690544df7a7797f016da6e321319edf9dcbfb
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-10-26 17:01:11 -0800

    include host name we are chatting with when diagnosing protocol error
    SCCS-vsn: usr.sbin/sendmail/src/usersmtp.c 8.22

--- usr/src/usr.sbin/sendmail/src/usersmtp.c
+++ usr/src/usr.sbin/sendmail/src/usersmtp.c
@@ -10,9 +10,9 @@
 
 #ifndef lint
 #ifdef SMTP
-static char sccsid[] = "@(#)usersmtp.c	8.21 (Berkeley) %G% (with SMTP)";
+static char sccsid[] = "@(#)usersmtp.c	8.22 (Berkeley) %G% (with SMTP)";
 #else
-static char sccsid[] = "@(#)usersmtp.c	8.21 (Berkeley) %G% (without SMTP)";
+static char sccsid[] = "@(#)usersmtp.c	8.22 (Berkeley) %G% (without SMTP)";
 #endif
 #endif /* not lint */
 
@@ -392,8 +392,8 @@ smtpmailfrom(m, mci, e)
 #ifdef LOG
 	if (LogLevel > 1)
 	{
-		syslog(LOG_CRIT, "%s: SMTP MAIL protocol error: %s",
-			e->e_id, SmtpReplyBuffer);
+		syslog(LOG_CRIT, "%s: %s: SMTP MAIL protocol error: %s",
+			e->e_id, mci->mci_host, SmtpReplyBuffer);
 	}
 #endif
 
@@ -443,8 +443,8 @@ smtprcpt(to, m, mci, e)
 #ifdef LOG
 	if (LogLevel > 1)
 	{
-		syslog(LOG_CRIT, "%s: SMTP RCPT protocol error: %s",
-			e->e_id, SmtpReplyBuffer);
+		syslog(LOG_CRIT, "%s: %s: SMTP RCPT protocol error: %s",
+			e->e_id, mci->mci_host, SmtpReplyBuffer);
 	}
 #endif
 
@@ -504,8 +504,8 @@ smtpdata(m, mci, e)
 #ifdef LOG
 		if (LogLevel > 1)
 		{
-			syslog(LOG_CRIT, "%s: SMTP DATA-1 protocol error: %s",
-				e->e_id, SmtpReplyBuffer);
+			syslog(LOG_CRIT, "%s: %s: SMTP DATA-1 protocol error: %s",
+				e->e_id, mci->mci_host, SmtpReplyBuffer);
 		}
 #endif
 		smtprset(m, mci, e);
@@ -592,8 +592,8 @@ smtpdata(m, mci, e)
 #ifdef LOG
 	if (LogLevel > 1)
 	{
-		syslog(LOG_CRIT, "%s: SMTP DATA-2 protocol error: %s",
-			e->e_id, SmtpReplyBuffer);
+		syslog(LOG_CRIT, "%s: %s: SMTP DATA-2 protocol error: %s",
+			e->e_id, mci->mci_host, SmtpReplyBuffer);
 	}
 #endif
 	return (EX_PROTOCOL);
