35af774fe58f7a9e1d8a426d9f93022063572e2e
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1994-10-28 03:51:22 -0800

    don't calculate stop time until all the data is sent or received (from mcn@.c3.lanl.gov (Michael Neuman))
    SCCS-vsn: usr.bin/ftp/ftp.c 8.6

--- usr/src/usr.bin/ftp/ftp.c
+++ usr/src/usr.bin/ftp/ftp.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)ftp.c	8.5 (Berkeley) %G%";
+static char sccsid[] = "@(#)ftp.c	8.6 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/param.h>
@@ -620,10 +620,10 @@ sendrequest(cmd, local, remote, printnames)
 		}
 		break;
 	}
-	(void) gettimeofday(&stop, (struct timezone *)0);
 	if (closefunc != NULL)
 		(*closefunc)(fin);
 	(void) fclose(dout);
+	(void) gettimeofday(&stop, (struct timezone *)0);
 	(void) getreply(0);
 	(void) signal(SIGINT, oldintr);
 	if (oldintp)
@@ -632,7 +632,6 @@ sendrequest(cmd, local, remote, printnames)
 		ptransfer("sent", bytes, &start, &stop);
 	return;
 abort:
-	(void) gettimeofday(&stop, (struct timezone *)0);
 	(void) signal(SIGINT, oldintr);
 	if (oldintp)
 		(void) signal(SIGPIPE, oldintp);
@@ -650,6 +649,7 @@ abort:
 	code = -1;
 	if (closefunc != NULL && fin != NULL)
 		(*closefunc)(fin);
+	(void) gettimeofday(&stop, (struct timezone *)0);
 	if (bytes > 0)
 		ptransfer("sent", bytes, &start, &stop);
 }
@@ -929,8 +929,8 @@ break2:
 	(void) signal(SIGINT, oldintr);
 	if (oldintp)
 		(void) signal(SIGPIPE, oldintp);
-	(void) gettimeofday(&stop, (struct timezone *)0);
 	(void) fclose(din);
+	(void) gettimeofday(&stop, (struct timezone *)0);
 	(void) getreply(0);
 	if (bytes > 0 && is_retr)
 		ptransfer("received", bytes, &start, &stop);
@@ -939,7 +939,6 @@ abort:
 
 /* abort using RFC959 recommended IP,SYNC sequence  */
 
-	(void) gettimeofday(&stop, (struct timezone *)0);
 	if (oldintp)
 		(void) signal(SIGPIPE, oldintr);
 	(void) signal(SIGINT, SIG_IGN);
@@ -959,6 +958,7 @@ abort:
 		(*closefunc)(fout);
 	if (din)
 		(void) fclose(din);
+	(void) gettimeofday(&stop, (struct timezone *)0);
 	if (bytes > 0)
 		ptransfer("received", bytes, &start, &stop);
 	(void) signal(SIGINT, oldintr);
