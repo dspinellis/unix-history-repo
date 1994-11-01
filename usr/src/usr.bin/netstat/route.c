3b238b9f149e6bf4d4b869753de939b8eeba7a10
Keith Sklower <sklower@ucbvax.Berkeley.EDU>
1994-10-31 21:45:49 -0800

    optimization for normal masks requires keeping ptr to associated leaf,
    SCCS-vsn: usr.bin/netstat/route.c 8.4

--- usr/src/usr.bin/netstat/route.c
+++ usr/src/usr.bin/netstat/route.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)route.c	8.3 (Berkeley) %G%";
+static char sccsid[] = "@(#)route.c	8.4 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/param.h>
