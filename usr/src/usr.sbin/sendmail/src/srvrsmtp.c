911c4c4d732b3d1713aa5afe370f1693fc731e55
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-11-05 18:43:56 -0800

    announce DSN extension as X-DSN-0 instead of DSN until it is standardized
    SCCS-vsn: usr.sbin/sendmail/src/srvrsmtp.c 8.46

--- usr/src/usr.sbin/sendmail/src/srvrsmtp.c
+++ usr/src/usr.sbin/sendmail/src/srvrsmtp.c
@@ -10,9 +10,9 @@
 
 #ifndef lint
 #ifdef SMTP
-static char sccsid[] = "@(#)srvrsmtp.c	8.45 (Berkeley) %G% (with SMTP)";
+static char sccsid[] = "@(#)srvrsmtp.c	8.46 (Berkeley) %G% (with SMTP)";
 #else
-static char sccsid[] = "@(#)srvrsmtp.c	8.45 (Berkeley) %G% (without SMTP)";
+static char sccsid[] = "@(#)srvrsmtp.c	8.46 (Berkeley) %G% (without SMTP)";
 #endif
 #endif /* not lint */
 
