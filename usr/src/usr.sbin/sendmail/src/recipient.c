3f807c27d0c500007e4a0e6abe9f3a776bf32e1b
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-11-08 18:06:29 -0800

    copy buffer in senttolist() to avoid conflict with maps
    SCCS-vsn: usr.sbin/sendmail/src/recipient.c 8.52

--- usr/src/usr.sbin/sendmail/src/recipient.c
+++ usr/src/usr.sbin/sendmail/src/recipient.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)recipient.c	8.51 (Berkeley) %G%";
+static char sccsid[] = "@(#)recipient.c	8.52 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -53,6 +53,9 @@ sendtolist(list, ctladdr, sendq, e)
 	char delimiter;		/* the address delimiter */
 	int naddrs;
 	char *oldto = e->e_to;
+	static char *bufp = NULL;
+	static int buflen;
+	char buf[MAXNAME + 1];
 
 	if (list == NULL)
 	{
@@ -79,7 +82,22 @@ sendtolist(list, ctladdr, sendq, e)
 	al = NULL;
 	naddrs = 0;
 
-	for (p = list; *p != '\0'; )
+	if (buf == NULL)
+	{
+		bufp = buf;
+		buflen = sizeof buf - 1;
+	}
+	if (strlen(list) > buflen)
+	{
+		/* allocate additional space */
+		if (bufp != buf)
+			free(bufp);
+		buflen = strlen(list);
+		bufp = malloc(buflen + 1);
+	}
+	strcpy(bufp, list);
+
+	for (p = bufp; *p != '\0'; )
 	{
 		auto char *delimptr;
 		register ADDRESS *a;
