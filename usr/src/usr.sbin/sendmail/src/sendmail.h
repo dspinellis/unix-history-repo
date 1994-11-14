856757ad791f6dd49581ea2dd6c4e3f434458f96
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-11-13 23:42:57 -0800

    add MCF_OPTFILE flag
    SCCS-vsn: usr.sbin/sendmail/src/sendmail.h 8.70

--- usr/src/usr.sbin/sendmail/src/sendmail.h
+++ usr/src/usr.sbin/sendmail/src/sendmail.h
@@ -5,7 +5,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)sendmail.h	8.69 (Berkeley) %G%
+ *	@(#)sendmail.h	8.70 (Berkeley) %G%
  */
 
 /*
@@ -15,7 +15,7 @@
 # ifdef _DEFINE
 # define EXTERN
 # ifndef lint
-static char SmailSccsId[] =	"@(#)sendmail.h	8.69		%G%";
+static char SmailSccsId[] =	"@(#)sendmail.h	8.70		%G%";
 # endif
 # else /*  _DEFINE */
 # define EXTERN extern
