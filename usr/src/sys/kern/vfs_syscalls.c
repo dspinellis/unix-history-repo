f003be7b4e006bac8838acd86159068dbd23287d
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1994-11-22 01:42:10 -0800

    return EBUSY if reference count on mount point is greater than 1 to avoid problems in lookup from programs referencing the hidden vnode
    SCCS-vsn: sys/kern/vfs_syscalls.c 8.25

--- usr/src/sys/kern/vfs_syscalls.c
+++ usr/src/sys/kern/vfs_syscalls.c
@@ -9,7 +9,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)vfs_syscalls.c	8.24 (Berkeley) %G%
+ *	@(#)vfs_syscalls.c	8.25 (Berkeley) %G%
  */
 
 #include <sys/param.h>
@@ -135,6 +135,10 @@ mount(p, uap, retval)
 		vput(vp);
 		return (ENODEV);
 	}
+	if (vp->v_usecount > 1 || vp->v_mountedhere != NULL) {
+		vput(vp);
+		return (EBUSY);
+	}
 
 	/*
 	 * Allocate and initialize the file system.
@@ -147,12 +151,6 @@ mount(p, uap, retval)
 		free((caddr_t)mp, M_MOUNT);
 		vput(vp);
 		return (error);
-	}
-	if (vp->v_mountedhere != NULL) {
-		vfs_unlock(mp);
-		free((caddr_t)mp, M_MOUNT);
-		vput(vp);
-		return (EBUSY);
 	}
 	vp->v_mountedhere = mp;
 	mp->mnt_vnodecovered = vp;
