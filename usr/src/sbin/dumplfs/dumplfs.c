d229c0829ef593f927e992b01604d6a0690fe1b7
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1994-11-30 19:03:49 -0800

    lint
    SCCS-vsn: sbin/fsck/pass5.c 8.6
    SCCS-vsn: sbin/newfs/mkfs.c 8.7
    SCCS-vsn: sbin/dumplfs/dumplfs.c 8.2
    SCCS-vsn: usr.sbin/diskpart/diskpart.c 8.3

--- usr/src/sbin/dumplfs/dumplfs.c
+++ usr/src/sbin/dumplfs/dumplfs.c
@@ -12,7 +12,7 @@ static char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)dumplfs.c	8.1 (Berkeley) %G%";
+static char sccsid[] = "@(#)dumplfs.c	8.2 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/param.h>
@@ -353,7 +353,7 @@ dump_sum(fd, lfsp, sp, segnum, addr)
 	daddr_t addr;
 {
 	FINFO *fp;
-	long *dp;
+	daddr_t *dp;
 	int i, j;
 	int ck;
 	int numblocks;
