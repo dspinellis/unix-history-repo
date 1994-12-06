c9c6cf1d930daf07eeacc23c992541a47ca53e23
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1994-12-06 02:33:03 -0800

    return ENOSYS rather than EINVAL for non-existent system call
    SCCS-vsn: sys/kern/kern_sig.c 8.11

--- usr/src/sys/kern/kern_sig.c
+++ usr/src/sys/kern/kern_sig.c
@@ -3,7 +3,7 @@
  * All rights reserved.  The Berkeley software License Agreement
  * specifies the terms and conditions for redistribution.
  *
- *	@(#)kern_sig.c	8.10 (Berkeley) %G%
+ *	@(#)kern_sig.c	8.11 (Berkeley) %G%
  */
 
 #define	SIGPROP		/* include signal properties table */
