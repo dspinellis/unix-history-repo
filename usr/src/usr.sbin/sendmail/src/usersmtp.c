1675dc128f0e30e4f12e49d650b3f75caae83e41
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-12-10 19:57:02 -0800

    don't let an error in one envelope from address blow away other jobs to the same host later in the queue run
    SCCS-vsn: usr.sbin/sendmail/src/usersmtp.c 8.32

--- usr/src/usr.sbin/sendmail/src/usersmtp.c
+++ usr/src/usr.sbin/sendmail/src/usersmtp.c
@@ -10,9 +10,9 @@
 
 #ifndef lint
 #ifdef SMTP
-static char sccsid[] = "@(#)usersmtp.c	8.31 (Berkeley) %G% (with SMTP)";
+static char sccsid[] = "@(#)usersmtp.c	8.32 (Berkeley) %G% (with SMTP)";
 #else
-static char sccsid[] = "@(#)usersmtp.c	8.31 (Berkeley) %G% (without SMTP)";
+static char sccsid[] = "@(#)usersmtp.c	8.32 (Berkeley) %G% (without SMTP)";
 #endif
 #endif /* not lint */
 
@@ -404,13 +404,17 @@ smtpmailfrom(m, mci, e)
 	}
 	else if (r == 250)
 	{
-		mci->mci_exitstat = EX_OK;
 		return EX_OK;
+	}
+	else if (r == 501 || r == 553)
+	{
+		/* syntax error in arguments */
+		smtpquit(m, mci, e);
+		return EX_DATAERR;
 	}
 	else if (r == 552)
 	{
 		/* signal service unavailable */
-		mci->mci_exitstat = EX_UNAVAILABLE;
 		smtpquit(m, mci, e);
 		return EX_UNAVAILABLE;
 	}
@@ -426,7 +430,6 @@ smtpmailfrom(m, mci, e)
 	/* protocol error -- close up */
 	smtpquit(m);
 	smtpquit(m, mci, e);
-	mci->mci_exitstat = EX_PROTOCOL;
 	return EX_PROTOCOL;
 }
 /*
