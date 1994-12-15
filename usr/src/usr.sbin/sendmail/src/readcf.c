519ffecc38a5168606b669ec38cee5e8574bb320
Eric Allman <eric@ucbvax.Berkeley.EDU>
1994-12-15 14:28:59 -0800

    compile glitch
    SCCS-vsn: usr.sbin/sendmail/src/readcf.c 8.54

--- usr/src/usr.sbin/sendmail/src/readcf.c
+++ usr/src/usr.sbin/sendmail/src/readcf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)readcf.c	8.53 (Berkeley) %G%";
+static char sccsid[] = "@(#)readcf.c	8.54 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -910,7 +910,7 @@ makemailer(line)
 
 	if (ConfigLevel < 6 && m->m_mtstype == NULL &&
 	    (strcmp(m->m_mailer, "[IPC]") == 0 ||
-	     strcmp(m->m_mailer, "[TCP]") == NULL))
+	     strcmp(m->m_mailer, "[TCP]") == 0))
 		m->m_mtstype = "Internet";
 
 	/* enter the mailer into the symbol table */
