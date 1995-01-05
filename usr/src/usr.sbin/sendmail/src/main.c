67c86c09ad34301c1566b6fa72cb85b66f98e9f1
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-01-05 00:33:12 -0800

    allow -M to define macros on all architectures (not just Ultrix)
    SCCS-vsn: usr.sbin/sendmail/src/main.c 8.74

--- usr/src/usr.sbin/sendmail/src/main.c
+++ usr/src/usr.sbin/sendmail/src/main.c
@@ -13,7 +13,7 @@ static char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)main.c	8.73 (Berkeley) %G%";
+static char sccsid[] = "@(#)main.c	8.74 (Berkeley) %G%";
 #endif /* not lint */
 
 #define	_DEFINE
