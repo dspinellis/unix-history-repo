299e368f3e508c8c89afca3feec5732305dc0990
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-01-05 00:33:29 -0800

    avoid error message on self-referencing alias
    SCCS-vsn: usr.sbin/sendmail/src/recipient.c 8.60

--- usr/src/usr.sbin/sendmail/src/recipient.c
+++ usr/src/usr.sbin/sendmail/src/recipient.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)recipient.c	8.59 (Berkeley) %G%";
+static char sccsid[] = "@(#)recipient.c	8.60 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
