3e1765d74362cf20f1c199db836221693189eb1f
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-01-07 19:18:43 -0800

    enable load average for AIX3
    SCCS-vsn: usr.sbin/sendmail/src/conf.h 8.129

--- usr/src/usr.sbin/sendmail/src/conf.h
+++ usr/src/usr.sbin/sendmail/src/conf.h
@@ -5,7 +5,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)conf.h	8.128 (Berkeley) %G%
+ *	@(#)conf.h	8.129 (Berkeley) %G%
  */
 
 /*
@@ -147,6 +147,7 @@ extern int	syslog(int, char *, ...);
 # define FORK		fork	/* no vfork primitive available */
 # define SFS_TYPE	SFS_STATFS	/* use <sys/statfs.h> statfs() impl */
 # define SPT_PADCHAR	'\0'	/* pad process title with nulls */
+# define LA_TYPE	LA_INT
 #endif
 
 
