80d7d4d7f47774f5023c11a247d4851f69c46f54
Jan-Simon Pendry <pendry@ucbvax.Berkeley.EDU>
1995-01-28 17:48:34 -0800

    fix error message
    SCCS-vsn: usr.bin/showmount/showmount.c 8.2

--- usr/src/usr.bin/showmount/showmount.c
+++ usr/src/usr.bin/showmount/showmount.c
@@ -15,7 +15,7 @@ static char copyright[] =
 #endif not lint
 
 #ifndef lint
-static char sccsid[] = "@(#)showmount.c	8.1 (Berkeley) %G%";
+static char sccsid[] = "@(#)showmount.c	8.2 (Berkeley) %G%";
 #endif not lint
 
 #include <sys/types.h>
