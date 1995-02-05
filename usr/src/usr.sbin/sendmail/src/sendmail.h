0482a8175a6ac6abbe1e51d248a3694a470ca17e
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-02-04 16:06:22 -0800

    delete things no longer needed with Message/RFC822-Headers
    SCCS-vsn: usr.sbin/sendmail/src/sendmail.h 8.82

--- usr/src/usr.sbin/sendmail/src/sendmail.h
+++ usr/src/usr.sbin/sendmail/src/sendmail.h
@@ -5,7 +5,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)sendmail.h	8.81 (Berkeley) %G%
+ *	@(#)sendmail.h	8.82 (Berkeley) %G%
  */
 
 /*
@@ -15,7 +15,7 @@
 # ifdef _DEFINE
 # define EXTERN
 # ifndef lint
-static char SmailSccsId[] =	"@(#)sendmail.h	8.81		%G%";
+static char SmailSccsId[] =	"@(#)sendmail.h	8.82		%G%";
 # endif
 # else /*  _DEFINE */
 # define EXTERN extern
@@ -616,13 +616,6 @@ struct prival
 #define SFF_MUSTOWN		0x0001	/* user must own this file */
 #define SFF_NOSLINK		0x0002	/* file cannot be a symbolic link */
 #define SFF_ROOTOK		0x0004	/* ok for root to own this file */
-
-
-/*
-**  Flags passed to putheader and putbody.
-*/
-
-#define PF_DELETEMIMEHDRS	0x0001	/* delete Content-Type: and C-T-E: */
 
 
 /*
