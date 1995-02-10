3a371b8c45397503e86da8a139cbda886563ea3d
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-02-10 15:45:05 -0800

    eliminate unnecessary declaration
    SCCS-vsn: usr.sbin/sendmail/src/readcf.c 8.64
    SCCS-vsn: usr.sbin/sendmail/src/readcf.c 8.23.1.2

--- usr/src/usr.sbin/sendmail/src/readcf.c
+++ usr/src/usr.sbin/sendmail/src/readcf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)readcf.c	8.23.1.1 (Berkeley) %G%";
+static char sccsid[] = "@(#)readcf.c	8.23.1.2 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
