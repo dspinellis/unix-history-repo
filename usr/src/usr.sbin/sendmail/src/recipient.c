f76e51f24efa8f233516383303c7e07cbc964327
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-02-10 15:47:26 -0800

    avoid conflicting buffer usage
    SCCS-vsn: usr.sbin/sendmail/src/recipient.c 8.44.1.2

--- usr/src/usr.sbin/sendmail/src/recipient.c
+++ usr/src/usr.sbin/sendmail/src/recipient.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)recipient.c	8.44.1.1 (Berkeley) %G%";
+static char sccsid[] = "@(#)recipient.c	8.44.1.2 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
