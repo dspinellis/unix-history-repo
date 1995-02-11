e8e73bb49d0b444d0d80959fcdcb04f979f04220
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-02-10 17:30:00 -0800

    more bogus newline deletion
    SCCS-vsn: usr.sbin/sendmail/src/main.c 8.55.1.2

--- usr/src/usr.sbin/sendmail/src/main.c
+++ usr/src/usr.sbin/sendmail/src/main.c
@@ -13,7 +13,7 @@ static char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)main.c	8.80 (Berkeley) %G%";
+static char sccsid[] = "@(#)main.c	8.55.1.2 (Berkeley) %G%";
 #endif /* not lint */
 
 #define	_DEFINE
