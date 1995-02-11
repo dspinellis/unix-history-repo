e51b88c3ce5b78a02b95b53f55cf2d71106f8839
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-02-10 18:48:28 -0800

    add logging on possible attacks
    SCCS-vsn: usr.sbin/sendmail/src/util.c 8.39.1.2

--- usr/src/usr.sbin/sendmail/src/util.c
+++ usr/src/usr.sbin/sendmail/src/util.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)util.c	8.48 (Berkeley) %G%";
+static char sccsid[] = "@(#)util.c	8.39.1.2 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
