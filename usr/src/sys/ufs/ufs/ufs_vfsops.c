f53765fe469e177f2e14eae30374821ac654ff8c
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1995-02-15 20:08:39 -0800

    SYNC may be done by anyone; select quota type properly (from karels)
    SCCS-vsn: sys/ufs/ufs/ufs_vfsops.c 8.5

--- usr/src/sys/ufs/ufs/ufs_vfsops.c
+++ usr/src/sys/ufs/ufs/ufs_vfsops.c
@@ -9,7 +9,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)ufs_vfsops.c	8.4 (Berkeley) %G%
+ *	@(#)ufs_vfsops.c	8.5 (Berkeley) %G%
  */
 
 #include <sys/param.h>
@@ -88,8 +88,9 @@ ufs_quotactl(mp, cmds, uid, arg, p)
 	cmd = cmds >> SUBCMDSHIFT;
 
 	switch (cmd) {
-	case Q_GETQUOTA:
 	case Q_SYNC:
+		break;
+	case Q_GETQUOTA:
 		if (uid == p->p_cred->p_ruid)
 			break;
 		/* fall through */
@@ -98,7 +99,7 @@ ufs_quotactl(mp, cmds, uid, arg, p)
 			return (error);
 	}
 
-	type = cmd & SUBCMDMASK;
+	type = cmds & SUBCMDMASK;
 	if ((u_int)type >= MAXQUOTAS)
 		return (EINVAL);
 
