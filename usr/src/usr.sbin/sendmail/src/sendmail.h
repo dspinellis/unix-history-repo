e71afa78b7cc61087820eb363895f025efe00b10
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-02-19 21:39:04 -0800

    add ST_RULESET for full-word rulesets
    SCCS-vsn: usr.sbin/sendmail/src/sendmail.h 8.88

--- usr/src/usr.sbin/sendmail/src/sendmail.h
+++ usr/src/usr.sbin/sendmail/src/sendmail.h
@@ -5,7 +5,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)sendmail.h	8.87 (Berkeley) %G%
+ *	@(#)sendmail.h	8.88 (Berkeley) %G%
  */
 
 /*
@@ -15,7 +15,7 @@
 # ifdef _DEFINE
 # define EXTERN
 # ifndef lint
-static char SmailSccsId[] =	"@(#)sendmail.h	8.87		%G%";
+static char SmailSccsId[] =	"@(#)sendmail.h	8.88		%G%";
 # endif
 # else /*  _DEFINE */
 # define EXTERN extern
