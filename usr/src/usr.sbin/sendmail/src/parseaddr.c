c0a76094a4686e3b1e27a3f2eb81d22f0fcdaa2b
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-02-20 15:34:00 -0800

    allow symbolic ruleset names
    SCCS-vsn: usr.sbin/sendmail/src/parseaddr.c 8.52
    SCCS-vsn: usr.sbin/sendmail/src/TODO 8.32
    SCCS-vsn: usr.sbin/sendmail/src/readcf.c 8.68

--- usr/src/usr.sbin/sendmail/src/parseaddr.c
+++ usr/src/usr.sbin/sendmail/src/parseaddr.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)parseaddr.c	8.51 (Berkeley) %G%";
+static char sccsid[] = "@(#)parseaddr.c	8.52 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -1155,11 +1155,19 @@ rewrite(pvp, ruleset, reclevel, e)
 			}
 			else
 			{
+				int ruleset;
+				STAB *s;
+
 				bcopy((char *) &npvp[2], (char *) pvp,
 					(int) (avp - npvp - 2) * sizeof *avp);
 				if (tTd(21, 3))
 					printf("-----callsubr %s\n", npvp[1]);
-				stat = rewrite(pvp, atoi(npvp[1]), reclevel, e);
+				s = stab(npvp[1], ST_RULESET, ST_FIND);
+				if (s == NULL)
+					ruleset = atoi(npvp[1]);
+				else
+					ruleset = s->s_ruleset;
+				stat = rewrite(pvp, ruleset, reclevel, e);
 				if (rstat == EX_OK || stat == EX_TEMPFAIL)
 					rstat = stat;
 				if (*pvp != NULL && (**pvp & 0377) == CANONNET)
