0dcef214dd0fdcbf7a3b4d8c0ffadaf970a48735
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-02-21 15:40:29 -0800

    woops -- botched the last patch
    SCCS-vsn: usr.sbin/sendmail/src/recipient.c 8.66

--- usr/src/usr.sbin/sendmail/src/recipient.c
+++ usr/src/usr.sbin/sendmail/src/recipient.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)recipient.c	8.65 (Berkeley) %G%";
+static char sccsid[] = "@(#)recipient.c	8.66 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
