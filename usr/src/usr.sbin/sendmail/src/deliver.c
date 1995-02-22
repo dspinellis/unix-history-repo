f4a76577c996370c6cab580245a50438edd71dc9
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-02-21 18:03:49 -0800

    compiler glitch -- variable used before set
    SCCS-vsn: usr.sbin/sendmail/src/deliver.c 8.123

--- usr/src/usr.sbin/sendmail/src/deliver.c
+++ usr/src/usr.sbin/sendmail/src/deliver.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)deliver.c	8.122 (Berkeley) %G%";
+static char sccsid[] = "@(#)deliver.c	8.123 (Berkeley) %G%";
 #endif /* not lint */
 
 #include "sendmail.h"
@@ -751,7 +751,6 @@ deliver(firstto, editfcn)
 	tobuf[0] = '\0';
 	e->e_to = tobuf;
 	ctladdr = NULL;
-	xstart = curtime();
 	firstsig = hostsignature(firstto->q_mailer, firstto->q_host, e);
 	for (; to != NULL; to = to->q_next)
 	{
