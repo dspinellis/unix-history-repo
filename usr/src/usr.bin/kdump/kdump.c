d49348aeeefca37e14887e6d7e571f71f5aad0e1
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1995-02-22 01:12:18 -0800

    ktr_narg => ktr_argsize
    SCCS-vsn: usr.bin/kdump/kdump.c 8.2

--- usr/src/usr.bin/kdump/kdump.c
+++ usr/src/usr.bin/kdump/kdump.c
@@ -12,7 +12,7 @@ static char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)kdump.c	8.1 (Berkeley) %G%";
+static char sccsid[] = "@(#)kdump.c	8.2 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/param.h>
@@ -218,15 +218,16 @@ static char *ptrace_ops[] = {
 ktrsyscall(ktr)
 	register struct ktr_syscall *ktr;
 {
-	register narg = ktr->ktr_narg;
-	register int *ip;
-	char *ioctlname();
+	register int *ip, narg;
+	char *ioctlname __P((int));
 
 	if (ktr->ktr_code >= nsyscalls || ktr->ktr_code < 0)
 		(void)printf("[%d]", ktr->ktr_code);
 	else
 		(void)printf("%s", syscallnames[ktr->ktr_code]);
 	ip = (int *)((char *)ktr + sizeof(struct ktr_syscall));
+
+	narg = ktr->ktr_argsize / sizeof(register_t);
 	if (narg) {
 		char c = '(';
 		if (fancy) {
