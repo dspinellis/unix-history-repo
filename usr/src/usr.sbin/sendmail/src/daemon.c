fcec15f6dd231fa216a7d46082049174d6d0fe4b
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-02-28 23:40:21 -0800

    ibuf type wrong
    SCCS-vsn: usr.sbin/sendmail/src/daemon.c 8.48.1.4

--- usr/src/usr.sbin/sendmail/src/daemon.c
+++ usr/src/usr.sbin/sendmail/src/daemon.c
@@ -12,9 +12,9 @@
 
 #ifndef lint
 #ifdef DAEMON
-static char sccsid[] = "@(#)daemon.c	8.71 (Berkeley) %G% (with daemon mode)";
+static char sccsid[] = "@(#)daemon.c	8.48.1.4 (Berkeley) %G% (with daemon mode)";
 #else
-static char sccsid[] = "@(#)daemon.c	8.71 (Berkeley) %G% (without daemon mode)";
+static char sccsid[] = "@(#)daemon.c	8.48.1.4 (Berkeley) %G% (without daemon mode)";
 #endif
 #endif /* not lint */
 
@@ -574,7 +574,7 @@ getauthinfo(fd)
 	EVENT *ev;
 	int nleft;
 	char ibuf[MAXNAME + 1];
-	int ibuf[MAXNAME + 1];
+	char ibuf[MAXNAME + 1];
 	static char hbuf[MAXNAME * 2 + 2];
 	extern char *hostnamebyanyaddr();
 	extern char RealUserName[];			/* main.c */
