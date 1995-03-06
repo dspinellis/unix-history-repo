92e4d2a54d4c74a4240719d478577194e4bd1c57
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-03-05 19:42:50 -0800

    add "strict" parameter to denlstring
    SCCS-vsn: usr.sbin/sendmail/src/sendmail.h 8.92

--- usr/src/usr.sbin/sendmail/src/sendmail.h
+++ usr/src/usr.sbin/sendmail/src/sendmail.h
@@ -5,7 +5,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)sendmail.h	8.43.1.3 (Berkeley) %G%
+ *	@(#)sendmail.h	8.92 (Berkeley) %G%
  */
 
 /*
@@ -15,7 +15,7 @@
 # ifdef _DEFINE
 # define EXTERN
 # ifndef lint
-static char SmailSccsId[] =	"@(#)sendmail.h	8.43.1.3		%G%";
+static char SmailSccsId[] =	"@(#)sendmail.h	8.92		%G%";
 # endif
 # else /*  _DEFINE */
 # define EXTERN extern
