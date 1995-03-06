4b81ebff6304ae829324e4656ff4065d8316e553
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-03-05 22:23:11 -0800

    integrate changes from *Hobbit*
    SCCS-vsn: usr.sbin/sendmail/src/sendmail.h 8.94

--- usr/src/usr.sbin/sendmail/src/sendmail.h
+++ usr/src/usr.sbin/sendmail/src/sendmail.h
@@ -5,7 +5,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)sendmail.h	8.93 (Berkeley) %G%
+ *	@(#)sendmail.h	8.94 (Berkeley) %G%
  */
 
 /*
@@ -15,7 +15,7 @@
 # ifdef _DEFINE
 # define EXTERN
 # ifndef lint
-static char SmailSccsId[] =	"@(#)sendmail.h	8.93		%G%";
+static char SmailSccsId[] =	"@(#)sendmail.h	8.94		%G%";
 # endif
 # else /*  _DEFINE */
 # define EXTERN extern
