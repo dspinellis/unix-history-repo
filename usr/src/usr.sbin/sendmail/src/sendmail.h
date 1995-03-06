75c005be688864d742d58275e69a672c9cb87eb7
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-03-05 22:23:11 -0800

    integrate changes from *Hobbit*
    SCCS-vsn: usr.sbin/sendmail/src/sendmail.h 8.94

--- usr/src/usr.sbin/sendmail/src/sendmail.h
+++ usr/src/usr.sbin/sendmail/src/sendmail.h
@@ -5,7 +5,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)sendmail.h	8.93 (Berkeley) %G%
+ *	@(#)sendmail.h	8.94 (Berkeley) %G%
  */
 
 /*
@@ -15,7 +15,7 @@
 # ifdef _DEFINE
 # define EXTERN
 # ifndef lint
-static char SmailSccsId[] =	"@(#)sendmail.h	8.93		%G%";
+static char SmailSccsId[] =	"@(#)sendmail.h	8.94		%G%";
 # endif
 # else /*  _DEFINE */
 # define EXTERN extern
@@ -940,6 +940,7 @@ EXTERN long	MaxMessageSize;	/* advertised max size we will accept */
 EXTERN time_t	MaxHostStatAge;	/* max age of cached host status info */
 EXTERN time_t	MinQueueAge;	/* min delivery interval */
 EXTERN time_t	DialDelay;	/* delay between dial-on-demand tries */
+EXTERN char	*SafeFileEnv;	/* chroot location for file delivery */
 EXTERN char	*ServiceSwitchFile;	/* backup service switch */
 EXTERN char	*DefaultCharSet;	/* default character set for MIME */
 EXTERN int	DeliveryNiceness;	/* how nice to be during delivery */
