4addb66e44fa6ff2c9c567900509e38eaf4f3184
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-03-05 17:37:11 -0800

    improved security for points where setuid has returned to sender
    SCCS-vsn: usr.sbin/sendmail/src/deliver.c 8.125

--- usr/src/usr.sbin/sendmail/src/deliver.c
+++ usr/src/usr.sbin/sendmail/src/deliver.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)deliver.c	8.84.1.2 (Berkeley) %G%";
+static char sccsid[] = "@(#)deliver.c	8.125 (Berkeley) %G%";
 #endif /* not lint */
 
 #include "sendmail.h"
