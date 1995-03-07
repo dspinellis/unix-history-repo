e008db5b24cad0446702f2ff0dda219c159e47ca
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-03-06 23:11:57 -0800

    fix some race conditions for file opens
    SCCS-vsn: usr.sbin/sendmail/src/recipient.c 8.70
    SCCS-vsn: usr.sbin/sendmail/src/conf.h 8.138
    SCCS-vsn: usr.sbin/sendmail/src/domain.c 8.32
    SCCS-vsn: usr.sbin/sendmail/src/sendmail.h 8.95
    SCCS-vsn: usr.sbin/sendmail/src/savemail.c 8.53
    SCCS-vsn: usr.sbin/sendmail/src/util.c 8.55

--- usr/src/usr.sbin/sendmail/src/conf.h
+++ usr/src/usr.sbin/sendmail/src/conf.h
@@ -5,7 +5,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)conf.h	8.137 (Berkeley) %G%
+ *	@(#)conf.h	8.138 (Berkeley) %G%
  */
 
 /*
--- usr/src/usr.sbin/sendmail/src/recipient.c
+++ usr/src/usr.sbin/sendmail/src/recipient.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)recipient.c	8.69 (Berkeley) %G%";
+static char sccsid[] = "@(#)recipient.c	8.70 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -364,7 +364,7 @@ recipient(a, sendq, aliaslevel, e)
 			usrerr("550 Address %s is unsafe for mailing to files",
 				a->q_alias->q_paddr);
 		}
-		else if (!writable(buf, getctladdr(a), SFF_ANYFILE))
+		else if (!writable(buf, getctladdr(a), SFF_CREAT))
 		{
 			a->q_flags |= QBADADDR;
 			giveresponse(EX_CANTCREAT, m, NULL, a->q_alias,
@@ -641,32 +641,9 @@ writable(filename, ctladdr, flags)
 	int bits;
 	register char *p;
 	char *uname;
-	struct stat stb;
-	extern char RealUserName[];
 
 	if (tTd(29, 5))
 		printf("writable(%s, %x)\n", filename, flags);
-
-#ifdef HASLSTAT
-	if ((bitset(SFF_NOSLINK, flags) ? lstat(filename, &stb)
-					: stat(filename, &stb)) < 0)
-#else
-	if (stat(filename, &stb) < 0)
-#endif
-	{
-		/* file does not exist -- see if directory is safe */
-		p = strrchr(filename, '/');
-		if (p == NULL)
-		{
-			errno = ENOTDIR;
-			return FALSE;
-		}
-		*p = '\0';
-		errno = safefile(filename, RealUid, RealGid, RealUserName,
-				 SFF_MUSTOWN, S_IWRITE|S_IEXEC);
-		*p = '/';
-		return errno == 0;
-	}
 
 #ifdef SUID_ROOT_FILES_OK
 	/* really ought to be passed down -- and not a good idea */
@@ -677,14 +654,6 @@ writable(filename, ctladdr, flags)
 	**  File does exist -- check that it is writable.
 	*/
 
-	if (bitset(0111, stb.st_mode))
-	{
-		if (tTd(29, 5))
-			printf("failed (mode %o: x bits)\n", stb.st_mode);
-		errno = EPERM;
-		return (FALSE);
-	}
-
 	if (ctladdr != NULL && geteuid() == 0)
 	{
 		euid = ctladdr->q_uid;
@@ -694,6 +663,8 @@ writable(filename, ctladdr, flags)
 #ifdef RUN_AS_REAL_UID
 	else
 	{
+		extern char RealUserName[];
+
 		euid = RealUid;
 		egid = RealGid;
 		uname = RealUserName;
@@ -717,23 +688,9 @@ writable(filename, ctladdr, flags)
 	if (egid == 0)
 		egid = DefGid;
 	if (geteuid() == 0)
-	{
-		if (bitset(S_ISUID, stb.st_mode) &&
-		    (stb.st_uid != 0 || bitset(SFF_ROOTOK, flags)))
-		{
-			euid = stb.st_uid;
-			uname = NULL;
-		}
-		if (bitset(S_ISGID, stb.st_mode) &&
-		    (stb.st_gid != 0 || bitset(SFF_ROOTOK, flags)))
-			egid = stb.st_gid;
-	}
+		flags |= SFF_SETUIDOK;
 
-	if (tTd(29, 5))
-		printf("\teu/gid=%d/%d, st_u/gid=%d/%d\n",
-			euid, egid, stb.st_uid, stb.st_gid);
-
-	errno = safefile(filename, euid, egid, uname, flags, S_IWRITE);
+	errno = safefile(filename, euid, egid, uname, flags, S_IWRITE, NULL);
 	return errno == 0;
 }
 /*
@@ -774,10 +731,6 @@ writable(filename, ctladdr, flags)
 
 static jmp_buf	CtxIncludeTimeout;
 static void	includetimeout();
-
-#ifndef S_IWOTH
-# define S_IWOTH	(S_IWRITE >> 6)
-#endif
 
 int
 include(fname, forwarding, ctladdr, sendq, aliaslevel, e)
@@ -887,7 +840,7 @@ include(fname, forwarding, ctladdr, sendq, aliaslevel, e)
 		ev = NULL;
 
 	/* the input file must be marked safe */
-	rval = safefile(fname, uid, gid, uname, sfflags, S_IREAD);
+	rval = safefile(fname, uid, gid, uname, sfflags, S_IREAD, NULL);
 	if (rval != 0)
 	{
 		/* don't use this :include: file */
