113bd9b04e1d2d765472121793ba2233a4030d21
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-03-07 00:38:47 -0800

    add MF_APPEND flag
    SCCS-vsn: usr.sbin/sendmail/src/sendmail.h 8.96

--- usr/src/usr.sbin/sendmail/src/sendmail.h
+++ usr/src/usr.sbin/sendmail/src/sendmail.h
@@ -5,7 +5,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)sendmail.h	8.95 (Berkeley) %G%
+ *	@(#)sendmail.h	8.96 (Berkeley) %G%
  */
 
 /*
@@ -15,7 +15,7 @@
 # ifdef _DEFINE
 # define EXTERN
 # ifndef lint
-static char SmailSccsId[] =	"@(#)sendmail.h	8.95		%G%";
+static char SmailSccsId[] =	"@(#)sendmail.h	8.96		%G%";
 # endif
 # else /*  _DEFINE */
 # define EXTERN extern
