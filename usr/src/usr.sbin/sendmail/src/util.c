a77af776a8b36e2480b787305dbb2985606e73b6
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-03-08 00:43:22 -0800

    fix bug in xputs -- wasn't properly printing \n, \r, or \t
    SCCS-vsn: usr.sbin/sendmail/src/util.c 8.56

--- usr/src/usr.sbin/sendmail/src/util.c
+++ usr/src/usr.sbin/sendmail/src/util.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)util.c	8.55 (Berkeley) %G%";
+static char sccsid[] = "@(#)util.c	8.56 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -277,9 +277,6 @@ xputs(s)
 		/* wasn't a meta-macro -- find another way to print it */
 		switch (c)
 		{
-		  case '\0':
-			continue;
-
 		  case '\n':
 			c = 'n';
 			break;
@@ -297,6 +294,8 @@ xputs(s)
 			(void) putchar(c ^ 0100);
 			continue;
 		}
+		(void) putchar('\\');
+		(void) putchar(c);
 	}
 	(void) fflush(stdout);
 }
