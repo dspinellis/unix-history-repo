cb5b6b2bc7754eeebf172ac33a168908f7f7c2b2
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-03-12 02:02:29 -0800

    more MIME cleanup
    SCCS-vsn: usr.sbin/sendmail/src/main.c 8.90
    SCCS-vsn: usr.sbin/sendmail/src/conf.h 8.140
    SCCS-vsn: usr.sbin/sendmail/src/sendmail.h 8.101
    SCCS-vsn: usr.sbin/sendmail/src/deliver.c 8.128
    SCCS-vsn: usr.sbin/sendmail/src/mime.c 8.11

--- usr/src/usr.sbin/sendmail/src/deliver.c
+++ usr/src/usr.sbin/sendmail/src/deliver.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)deliver.c	8.127 (Berkeley) %G%";
+static char sccsid[] = "@(#)deliver.c	8.128 (Berkeley) %G%";
 #endif /* not lint */
 
 #include "sendmail.h"
