7379244ddb8ba2df4dcbccbf55ae4691a46c997b
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-03-11 22:27:49 -0800

    add MAXMIMENESTING
    SCCS-vsn: usr.sbin/sendmail/src/conf.h 8.139

--- usr/src/usr.sbin/sendmail/src/conf.h
+++ usr/src/usr.sbin/sendmail/src/conf.h
@@ -5,7 +5,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)conf.h	8.138 (Berkeley) %G%
+ *	@(#)conf.h	8.139 (Berkeley) %G%
  */
 
 /*
@@ -40,6 +40,7 @@
 # define MAXALIASDB	12		/* max # of alias databases */
 # define MAXMAPSTACK	12		/* max # of stacked or sequenced maps */
 # define MAXTOCLASS	8		/* max # of message timeout classes */
+# define MAXMIMENESTING	20		/* max depth of MIME nesting */
 
 # ifndef QUEUESIZE
 # define QUEUESIZE	1000		/* max # of jobs per queue run */
