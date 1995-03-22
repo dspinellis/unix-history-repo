dc1764f13c73765eadaf810bd6893fe518eebf93
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-03-22 03:38:21 -0800

    name changes to make the documentation cleaner
    SCCS-vsn: usr.sbin/sendmail/src/readcf.c 8.75

--- usr/src/usr.sbin/sendmail/src/readcf.c
+++ usr/src/usr.sbin/sendmail/src/readcf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)readcf.c	8.74.1.1 (Berkeley) %G%";
+static char sccsid[] = "@(#)readcf.c	8.75 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -1257,7 +1257,7 @@ struct optioninfo
 	"MatchGECOS",		'G',		FALSE,
 	"HelpFile",		'H',		FALSE,
 	"MaxHopCount",		'h',		FALSE,
-	"NameServerOptions",	'I',		FALSE,
+	"ResolverOptions",	'I',		FALSE,
 	"IgnoreDots",		'i',		TRUE,
 	"ForwardPath",		'J',		FALSE,
 	"SendMimeErrors",	'j',		TRUE,
@@ -1287,9 +1287,9 @@ struct optioninfo
 	"QueueLA",		'x',		FALSE,
 	"RefuseLA",		'X',		FALSE,
 	"RecipientFactor",	'y',		FALSE,
-	"ForkQueueRuns",	'Y',		FALSE,
+	"ForkEachJob",		'Y',		FALSE,
 	"ClassFactor",		'z',		FALSE,
-	"TimeFactor",		'Z',		FALSE,
+	"RetryFactor",		'Z',		FALSE,
 #define O_BSP		0x80
 	"BrokenSmtpPeers",	O_BSP,		TRUE,
 #define O_QUEUESORTORD	0x81
@@ -1310,6 +1310,8 @@ struct optioninfo
 	"NoRecipientAction",	O_NORCPTACTION,	TRUE,
 #define O_SAFEFILEENV	0x89
 	"SafeFileEnvironment",	O_SAFEFILEENV,	FALSE,
+#define O_MAXMSGSIZE	0x8a
+	"MaxMessageSize",	O_MAXMSGSIZE,	FALSE,
 
 	NULL,			'\0',		FALSE,
 };
@@ -1920,6 +1922,10 @@ setoption(opt, val, sticky)
 
 	  case O_SAFEFILEENV:	/* chroot() environ for writing to files */
 		SafeFileEnv = newstr(val);
+		break;
+
+	  case O_MAXMSGSIZE:	/* maximum message size */
+		MaxMessageSize = atol(p);
 		break;
 
 	  default:
