5854d20f8385435a1d51098d9ec98d1e9724fe38
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-03-23 05:47:59 -0800

    compile problem if !HASFLOCK
    SCCS-vsn: usr.sbin/sendmail/src/deliver.c 8.134

--- usr/src/usr.sbin/sendmail/src/deliver.c
+++ usr/src/usr.sbin/sendmail/src/deliver.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)deliver.c	8.133 (Berkeley) %G%";
+static char sccsid[] = "@(#)deliver.c	8.134 (Berkeley) %G%";
 #endif /* not lint */
 
 #include "sendmail.h"
