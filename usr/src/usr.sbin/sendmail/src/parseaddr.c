c4a99df52f82ee5b371c460d6081d9af023025ae
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-03-25 15:39:37 -0800

    don't pass through NOTIFY= unless it was given in the SMTP RCPT
    SCCS-vsn: usr.sbin/sendmail/src/usersmtp.c 8.40
    SCCS-vsn: usr.sbin/sendmail/src/recipient.c 8.74
    SCCS-vsn: usr.sbin/sendmail/src/parseaddr.c 8.60
    SCCS-vsn: usr.sbin/sendmail/src/sendmail.h 8.106
    SCCS-vsn: usr.sbin/sendmail/src/srvrsmtp.c 8.61

--- usr/src/usr.sbin/sendmail/src/parseaddr.c
+++ usr/src/usr.sbin/sendmail/src/parseaddr.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)parseaddr.c	8.59 (Berkeley) %G%";
+static char sccsid[] = "@(#)parseaddr.c	8.60 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -1602,6 +1602,7 @@ struct qflags	AddressFlags[] =
 	"QPINGONSUCCESS",	QPINGONSUCCESS,
 	"QPINGONFAILURE",	QPINGONFAILURE,
 	"QPINGONDELAY",		QPINGONDELAY,
+	"QHASNOTIFY",		QHASNOTIFY,
 	"QRELAYED",		QRELAYED,
 	NULL
 };
