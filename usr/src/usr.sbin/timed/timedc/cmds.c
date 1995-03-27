1381b0b4a7f26046dea1dc34530d6d4b755af940
Vernon Schryver <vjs@ucbvax.Berkeley.EDU>
1995-03-26 21:21:38 -0800

    fix check for EINTR
    SCCS-vsn: usr.sbin/timed/timedc/cmds.c 8.2

--- usr/src/usr.sbin/timed/timedc/cmds.c
+++ usr/src/usr.sbin/timed/timedc/cmds.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)cmds.c	8.1 (Berkeley) %G%";
+static char sccsid[] = "@(#)cmds.c	8.2 (Berkeley) %G%";
 #endif /* not lint */
 
 #ifdef sgi
@@ -87,7 +87,7 @@ daydiff(hostname)
 			i = select(sock+1, &ready, (fd_set *)0,
 				   (fd_set *)0, &tout);
 			if (i < 0) {
-				if (errno = EINTR)
+				if (errno == EINTR)
 					continue;
 				perror("select(date read)");
 				return 0;
