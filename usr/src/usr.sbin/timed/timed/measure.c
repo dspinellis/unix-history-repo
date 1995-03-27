4e5032960e92b1140f1a9fe0694fd2eab0b1cfce
Vernon Schryver <vjs@ucbvax.Berkeley.EDU>
1995-03-26 21:24:57 -0800

    add missing htonl() which caused craziness on little endian machines
    SCCS-vsn: usr.sbin/timed/timed/measure.c 8.2

--- usr/src/usr.sbin/timed/timed/measure.c
+++ usr/src/usr.sbin/timed/timed/measure.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)measure.c	8.1 (Berkeley) %G%";
+static char sccsid[] = "@(#)measure.c	8.2 (Berkeley) %G%";
 #endif /* not lint */
 
 #ifdef sgi
@@ -125,8 +125,8 @@ measure(maxmsec, wmsec, hname, addr, print)
 		 */
 		if (trials < TRIALS) {
 			trials++;
-			oicp->icmp_otime = ((tcur.tv_sec % SECDAY) * 1000
-					    + tcur.tv_usec / 1000);
+			oicp->icmp_otime = htonl((tcur.tv_sec % SECDAY) * 1000
+						 + tcur.tv_usec / 1000);
 			oicp->icmp_cksum = 0;
 			oicp->icmp_cksum = in_cksum((u_short*)oicp,
 						    sizeof(*oicp));
