e0039cc15e5d43fc7c96e523d0e6ddbe8725a2f2
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1995-03-29 06:28:45 -0800

    doreallocblks moves into ffs
    SCCS-vsn: sys/kern/vfs_cluster.c 8.10

--- usr/src/sys/kern/vfs_cluster.c
+++ usr/src/sys/kern/vfs_cluster.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)vfs_cluster.c	8.9 (Berkeley) %G%
+ *	@(#)vfs_cluster.c	8.10 (Berkeley) %G%
  */
 
 #include <sys/param.h>
@@ -16,16 +16,6 @@
 #include <sys/malloc.h>
 #include <sys/resourcevar.h>
 #include <libkern/libkern.h>
-
-#ifdef DEBUG
-#include <vm/vm.h>
-#include <sys/sysctl.h>
-int doreallocblks = 1;
-struct ctldebug debug13 = { "doreallocblks", &doreallocblks };
-#else
-/* XXX for cluster_write */
-#define doreallocblks 1
-#endif
 
 /*
  * Local declarations
@@ -493,8 +483,7 @@ cluster_write(bp, filesize)
 			 * Otherwise try reallocating to make it sequential.
 			 */
 			cursize = vp->v_lastw - vp->v_cstart + 1;
-			if (!doreallocblks ||
-			    (lbn + 1) * bp->b_bcount != filesize ||
+			if ((lbn + 1) * bp->b_bcount != filesize ||
 			    lbn != vp->v_lastw + 1 || vp->v_clen <= cursize) {
 				cluster_wbuild(vp, NULL, bp->b_bcount,
 				    vp->v_cstart, cursize, lbn);
