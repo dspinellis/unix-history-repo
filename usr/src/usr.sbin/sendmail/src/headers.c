3b390c1cd36e78fbafe5d2b97459f9a4e5e70143
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-03-30 22:26:01 -0800

    make lines beginning with : look like non-headers instead of syntax errors
    SCCS-vsn: usr.sbin/sendmail/src/headers.c 8.52

--- usr/src/usr.sbin/sendmail/src/headers.c
+++ usr/src/usr.sbin/sendmail/src/headers.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)headers.c	8.51 (Berkeley) %G%";
+static char sccsid[] = "@(#)headers.c	8.52 (Berkeley) %G%";
 #endif /* not lint */
 
 # include <errno.h>
@@ -66,7 +66,7 @@ chompheader(line, def, e)
 			p = q;
 		}
 		else
-			usrerr("553 header syntax error, line \"%s\"", line);
+			syserr("553 header syntax error, line \"%s\"", line);
 		cond = TRUE;
 	}
 
@@ -310,10 +310,10 @@ hvalue(field, header)
 **	optional white space followed by a colon.
 **
 **	Parameters:
-**		s -- string to check for possible headerness.
+**		h -- string to check for possible headerness.
 **
 **	Returns:
-**		TRUE if s is a header.
+**		TRUE if h is a header.
 **		FALSE otherwise.
 **
 **	Side Effects:
@@ -321,11 +321,16 @@ hvalue(field, header)
 */
 
 bool
-isheader(s)
-	register char *s;
+isheader(h)
+	char *h;
 {
+	register char *s = h;
+
 	while (*s > ' ' && *s != ':' && *s != '\0')
 		s++;
+
+	if (h == s)
+		return FALSE;
 
 	/* following technically violates RFC822 */
 	while (isascii(*s) && isspace(*s))
