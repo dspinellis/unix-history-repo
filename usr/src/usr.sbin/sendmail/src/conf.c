64a1d78a0ef6747cb79526fd4f4b68830cf00014
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-03-31 22:05:59 -0800

    add local_hostname_length so Ultrix will link against BIND 4.9
    SCCS-vsn: usr.sbin/sendmail/src/conf.c 8.145

--- usr/src/usr.sbin/sendmail/src/conf.c
+++ usr/src/usr.sbin/sendmail/src/conf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)conf.c	8.144 (Berkeley) %G%";
+static char sccsid[] = "@(#)conf.c	8.145 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
