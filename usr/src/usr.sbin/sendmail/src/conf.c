7ef2ebebbcbd89cdc28aa098eca2dcd2d9058f40
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-03-31 22:35:20 -0800

    more type glitching for maximum portability
    SCCS-vsn: usr.sbin/sendmail/src/sendmail.h 8.112
    SCCS-vsn: usr.sbin/sendmail/src/conf.c 8.146

--- usr/src/usr.sbin/sendmail/src/conf.c
+++ usr/src/usr.sbin/sendmail/src/conf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)conf.c	8.145 (Berkeley) %G%";
+static char sccsid[] = "@(#)conf.c	8.146 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
