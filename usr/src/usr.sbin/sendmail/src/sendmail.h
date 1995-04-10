d410b67410f64e1b321c441d4080f685feebed02
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-04-10 15:26:12 -0800

    eliminate BrokenSmtpPeers; go for default one line greeting
    SCCS-vsn: usr.sbin/sendmail/src/sendmail.h 8.120

--- usr/src/usr.sbin/sendmail/src/sendmail.h
+++ usr/src/usr.sbin/sendmail/src/sendmail.h
@@ -5,7 +5,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)sendmail.h	8.119 (Berkeley) %G%
+ *	@(#)sendmail.h	8.120 (Berkeley) %G%
  */
 
 /*
@@ -15,7 +15,7 @@
 # ifdef _DEFINE
 # define EXTERN
 # ifndef lint
-static char SmailSccsId[] =	"@(#)sendmail.h	8.119		%G%";
+static char SmailSccsId[] =	"@(#)sendmail.h	8.120		%G%";
 # endif
 # else /*  _DEFINE */
 # define EXTERN extern
