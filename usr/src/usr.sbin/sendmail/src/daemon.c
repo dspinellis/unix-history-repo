7781182b4ebb7e9b0e80d7c449c6e96b0f947f74
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-04-11 13:52:37 -0800

    random minor code cleanup
    SCCS-vsn: usr.sbin/sendmail/src/daemon.c 8.79

--- usr/src/usr.sbin/sendmail/src/daemon.c
+++ usr/src/usr.sbin/sendmail/src/daemon.c
@@ -11,9 +11,9 @@
 
 #ifndef lint
 #ifdef DAEMON
-static char sccsid[] = "@(#)daemon.c	8.78 (Berkeley) %G% (with daemon mode)";
+static char sccsid[] = "@(#)daemon.c	8.79 (Berkeley) %G% (with daemon mode)";
 #else
-static char sccsid[] = "@(#)daemon.c	8.78 (Berkeley) %G% (without daemon mode)";
+static char sccsid[] = "@(#)daemon.c	8.79 (Berkeley) %G% (without daemon mode)";
 #endif
 #endif /* not lint */
 
@@ -1380,7 +1380,7 @@ anynet_ntoa(sap)
 
 #ifdef NETINET
 	  case AF_INET:
-		return inet_ntoa(((struct sockaddr_in *) sap)->sin_addr);
+		return inet_ntoa(sap->sin.sin_addr);
 #endif
 
 	  default:
