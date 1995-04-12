4a5cfa52aa272c81ddb9e1679de72a697aa8912e
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1995-04-11 19:56:19 -0800

    comments from cgd and mycroft
    SCCS-vsn: sys/sys/lock.h 8.3
    SCCS-vsn: sys/kern/kern_lock.c 8.3

--- usr/src/sys/sys/lock.h
+++ usr/src/sys/sys/lock.h
@@ -8,7 +8,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)lock.h	8.2 (Berkeley) %G%
+ *	@(#)lock.h	8.3 (Berkeley) %G%
  */
 
 #ifndef	_LOCK_H_
@@ -48,7 +48,32 @@ struct lock {
 	pid_t	lk_lockholder;		/* pid of exclusive lock holder */
 };
 /*
- * Lock request types.
+ * Lock request types:
+ *   LK_SHARED - get one of many possible shared locks. If a process
+ *	holding an exclusive lock requests a shared lock, the exclusive
+ *	lock(s) will be downgraded to shared locks.
+ *   LK_EXCLUSIVE - stop further shared locks, when they are cleared,
+ *	grant a pending upgrade if it exists, then grant an exclusive
+ *	lock. Only one exclusive lock may exist at a time, except that
+ *	a process holding an exclusive lock may get additional exclusive
+ *	locks if it explicitly sets the LK_CANRECURSE flag in the lock
+ *	request, or if the LK_CANRECUSE flag was set when the lock was
+ *	initialized.
+ *   LK_UPGRADE - the process must hold a shared lock that it wants to
+ *	have upgraded to an exclusive lock. Other processes may get
+ *	exclusive access to the resource between the time that the upgrade
+ *	is requested and the time that it is granted.
+ *   LK_EXCLUPGRADE - the process must hold a shared lock that it wants to
+ *	have upgraded to an exclusive lock. If the request succeeds, no
+ *	other processes will have gotten exclusive access to the resource
+ *	between the time that the upgrade is requested and the time that
+ *	it is granted. However, if another process has already requested
+ *	an upgrade, the request will fail (see error returns below).
+ *   LK_DOWNGRADE - the process must hold an exclusive lock that it wants
+ *	to have downgraded to a shared lock. If the process holds multiple
+ *	(recursive) exclusive locks, they will all be downgraded to shared
+ *	locks.
+ *   LK_RELEASE - release one instance of a lock.
  *
  * These are flags that are passed to the lockmgr routine.
  */
@@ -56,8 +81,9 @@ struct lock {
 #define LK_SHARED	0x00000001	/* shared lock */
 #define LK_EXCLUSIVE	0x00000002	/* exclusive lock */
 #define LK_UPGRADE	0x00000003	/* shared-to-exclusive upgrade */
-#define LK_DOWNGRADE	0x00000004	/* exclusive-to-shared downgrade */
-#define LK_RELEASE	0x00000005	/* release any type of lock */
+#define LK_EXCLUPGRADE	0x00000004	/* first shared-to-exclusive upgrade */
+#define LK_DOWNGRADE	0x00000005	/* exclusive-to-shared downgrade */
+#define LK_RELEASE	0x00000006	/* release any type of lock */
 /*
  * External lock flags.
  *
@@ -82,16 +108,17 @@ struct lock {
  *
  * Successfully obtained locks return 0. Locks will always succeed
  * unless one of the following is true:
+ *	LK_FORCEUPGRADE is requested and some other process has already
+ *	    requested a lock upgrade (returns EBUSY).
  *	LK_WAIT is set and a sleep would be required (returns EBUSY).
  *	LK_SLEEPFAIL is set and a sleep was done (returns ENOLCK).
  *	PCATCH is set in lock priority and a signal arrives (returns
  *	    either EINTR or ERESTART if system calls is to be restarted).
  *	Non-null lock timeout and timeout expires (returns EWOULDBLOCK).
- * A failed lock attempt always returns a non-zero error value.
- * If the lock acquisition caused the process to sleep, the LK_SLEPT
- * flag is set otherwise it is cleared.
+ * A failed lock attempt always returns a non-zero error value. No lock
+ * is held after an error return (in particular, a failed LK_UPGRADE
+ * or LK_FORCEUPGRADE will have released its shared access lock).
  */
-#define LK_SLEPT	0x00001000	/* process slept while acquiring lock */
 
 /*
  * Indicator that no process holds exclusive lock
@@ -100,7 +127,7 @@ struct lock {
 
 void	lock_init __P((struct lock *, int prio, char *wmesg, int timo,
 			int flags));
-int	lockmgr __P((struct lock *, struct proc *, u_int flags));
+int	lockmgr __P((volatile struct lock *, struct proc *, u_int flags));
 
 #if NCPUS > 1
 /*
@@ -121,7 +148,7 @@ atomic_lock_init(lkp)
 
 __inline void
 atomic_lock(lkp)
-	struct atomic_lk *lkp;
+	volatile struct atomic_lk *lkp;
 {
 
 	while (test_and_set(&lkp->lock_data))
@@ -149,7 +176,7 @@ atomic_lock_init(alp)
 
 __inline void
 atomic_lock(alp)
-	struct atomic_lk *alp;
+	volatile struct atomic_lk *alp;
 {
 
 	if (alp->lock_data == 1)
