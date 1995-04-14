5339510a37d22b6e269450d3fcf0a6135d441df2
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-04-14 00:36:11 -0800

    fix postmaster notifications -- it was getting the body in some inappropriate cases, and sometimes no message at all; clean up dead.letter processing
    SCCS-vsn: usr.sbin/sendmail/src/recipient.c 8.78
    SCCS-vsn: usr.sbin/sendmail/src/envelope.c 8.61
    SCCS-vsn: usr.sbin/sendmail/src/savemail.c 8.63

--- usr/src/usr.sbin/sendmail/src/recipient.c
+++ usr/src/usr.sbin/sendmail/src/recipient.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)recipient.c	8.77 (Berkeley) %G%";
+static char sccsid[] = "@(#)recipient.c	8.78 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -687,7 +687,7 @@ writable(filename, ctladdr, flags)
 	char *uname;
 
 	if (tTd(29, 5))
-		printf("writable(%s, %x)\n", filename, flags);
+		printf("writable(%s, 0x%x)\n", filename, flags);
 
 #ifdef SUID_ROOT_FILES_OK
 	/* really ought to be passed down -- and not a good idea */
@@ -704,8 +704,7 @@ writable(filename, ctladdr, flags)
 		egid = ctladdr->q_gid;
 		uname = ctladdr->q_user;
 	}
-#ifdef RUN_AS_REAL_UID
-	else
+	else if (bitset(SFF_RUNASREALUID, flags))
 	{
 		extern char RealUserName[];
 
@@ -713,7 +712,6 @@ writable(filename, ctladdr, flags)
 		egid = RealGid;
 		uname = RealUserName;
 	}
-#else
 	else if (FileMailer != NULL)
 	{
 		euid = FileMailer->m_uid;
@@ -723,7 +721,6 @@ writable(filename, ctladdr, flags)
 	{
 		euid = egid = 0;
 	}
-#endif
 	if (euid == 0)
 	{
 		euid = DefUid;
