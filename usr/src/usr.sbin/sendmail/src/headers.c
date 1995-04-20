5062a093afc9f38a44373b33ec5a92fbbf352112
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-04-20 13:35:14 -0800

    use xputs to print chompheader intro debugging output
    SCCS-vsn: usr.sbin/sendmail/src/headers.c 8.57

--- usr/src/usr.sbin/sendmail/src/headers.c
+++ usr/src/usr.sbin/sendmail/src/headers.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)headers.c	8.56 (Berkeley) %G%";
+static char sccsid[] = "@(#)headers.c	8.57 (Berkeley) %G%";
 #endif /* not lint */
 
 # include <errno.h>
@@ -50,7 +50,11 @@ chompheader(line, def, hdrp, e)
 	char buf[MAXNAME + 1];
 
 	if (tTd(31, 6))
-		printf("chompheader: %s\n", line);
+	{
+		printf("chompheader: ");
+		xputs(line);
+		printf("\n");
+	}
 
 	headeronly = hdrp != NULL;
 	if (!headeronly)
