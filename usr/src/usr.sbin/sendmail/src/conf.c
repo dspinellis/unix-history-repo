65543071c0fa89c5ab3ac2e235ec808d953b268d
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-04-21 17:58:07 -0800

    check for .cf files with version extensions
    SCCS-vsn: usr.sbin/sendmail/src/conf.c 8.152

--- usr/src/usr.sbin/sendmail/src/conf.c
+++ usr/src/usr.sbin/sendmail/src/conf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)conf.c	8.151 (Berkeley) %G%";
+static char sccsid[] = "@(#)conf.c	8.152 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
