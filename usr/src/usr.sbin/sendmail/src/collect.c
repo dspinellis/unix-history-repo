3e8702350f1e7615287cb4df638fec009176b42a
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-04-21 16:49:00 -0800

    better debugging message for message termination
    SCCS-vsn: usr.sbin/sendmail/src/collect.c 8.35

--- usr/src/usr.sbin/sendmail/src/collect.c
+++ usr/src/usr.sbin/sendmail/src/collect.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)collect.c	8.34 (Berkeley) %G%";
+static char sccsid[] = "@(#)collect.c	8.35 (Berkeley) %G%";
 #endif /* not lint */
 
 # include <errno.h>
@@ -353,7 +353,7 @@ readerr:
 	if ((feof(fp) && smtpmode) || ferror(fp))
 	{
 		if (tTd(30, 1))
-			printf("collect: read error\n");
+			printf("collect: %s\n", errstring(errno));
 		inputerr = TRUE;
 	}
 
