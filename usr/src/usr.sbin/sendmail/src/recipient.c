8befd72fa870025547e5f82851860a025eece7ef
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-04-22 21:55:42 -0800

    woops -- compile glitch
    SCCS-vsn: usr.sbin/sendmail/src/recipient.c 8.81

--- usr/src/usr.sbin/sendmail/src/recipient.c
+++ usr/src/usr.sbin/sendmail/src/recipient.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)recipient.c	8.80 (Berkeley) %G%";
+static char sccsid[] = "@(#)recipient.c	8.81 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -518,7 +518,7 @@ recipient(a, sendq, aliaslevel, e)
 		if (q == NULL)
 		{
 			a->q_flags |= QBADADDR;
-			a->q_flags = "5.4.6";
+			a->q_status = "5.4.6";
 			usrerr("554 aliasing/forwarding loop broken");
 		}
 	}
