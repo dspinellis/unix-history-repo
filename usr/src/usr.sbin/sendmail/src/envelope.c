266325b88b1aedfe63d3ea4e35b21558bd7d9888
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-04-22 22:58:55 -0800

    updates for new status system codes and Multipart/Report drafts
    SCCS-vsn: usr.sbin/sendmail/src/sysexits.c 8.4
    SCCS-vsn: usr.sbin/sendmail/src/envelope.c 8.63
    SCCS-vsn: usr.sbin/sendmail/src/savemail.c 8.67

--- usr/src/usr.sbin/sendmail/src/envelope.c
+++ usr/src/usr.sbin/sendmail/src/envelope.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)envelope.c	8.62 (Berkeley) %G%";
+static char sccsid[] = "@(#)envelope.c	8.63 (Berkeley) %G%";
 #endif /* not lint */
 
 #include "sendmail.h"
@@ -160,7 +160,10 @@ dropenvelope(e)
 		for (q = e->e_sendqueue; q != NULL; q = q->q_next)
 		{
 			if (bitset(QQUEUEUP, q->q_flags))
+			{
 				q->q_flags |= QBADADDR;
+				q->q_status = "4.4.7";
+			}
 		}
 	}
 	else if (TimeOuts.to_q_warning[e->e_timeoutclass] > 0 &&
