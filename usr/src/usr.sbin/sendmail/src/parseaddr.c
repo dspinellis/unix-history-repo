b99b0061cd5fc70bf68abd81428fac0963e15490
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-04-25 14:13:06 -0800

    fix problem sending to user@domain where "domain" has an entry but no A or MX records; problem only occurs if it matches as the last entry in the resolver search list
    SCCS-vsn: usr.sbin/sendmail/src/parseaddr.c 8.68
    SCCS-vsn: usr.sbin/sendmail/src/daemon.c 8.84

--- usr/src/usr.sbin/sendmail/src/parseaddr.c
+++ usr/src/usr.sbin/sendmail/src/parseaddr.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)parseaddr.c	8.67 (Berkeley) %G%";
+static char sccsid[] = "@(#)parseaddr.c	8.68 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -1156,7 +1156,7 @@ rewrite(pvp, ruleset, reclevel, e)
 						stat);
 
 				/* should recover if stat == EX_TEMPFAIL */
-				if (stat == EX_TEMPFAIL || stat == EX_UNAVAILABLE)
+				if (stat == EX_TEMPFAIL)
 				{
 					rstat = EX_TEMPFAIL;
 					if (tTd(50, 1))
