09773faf87c23b1a7ad8fa99b694931eb19ffc58
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1995-04-27 21:13:29 -0800

    POSIX.2 requires that the shell's dot builtin do a path search From: "J.T. Conklin" <jtc@sun-lamp.cs.berkeley.edu>
    SCCS-vsn: bin/sh/main.c 8.2

--- usr/src/bin/sh/main.c
+++ usr/src/bin/sh/main.c
@@ -15,9 +15,11 @@ static char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)main.c	8.1 (Berkeley) %G%";
+static char sccsid[] = "@(#)main.c	8.2 (Berkeley) %G%";
 #endif /* not lint */
 
+#include <sys/stat.h>
+#include <stdio.h>
 #include <signal.h>
 #include <fcntl.h>
 #include "shell.h"
@@ -36,6 +38,7 @@ static char sccsid[] = "@(#)main.c	8.1 (Berkeley) %G%";
 #include "error.h"
 #include "init.h"
 #include "mystring.h"
+#include "exec.h"
 
 #define PROFILE 0
 
@@ -242,14 +245,36 @@ readcmdfile(name)
 
 /*
  * Take commands from a file.  To be compatable we should do a path
- * search for the file, but a path search doesn't make any sense.
+ * search for the file, which is necessary to find sub-commands.
  */
+
+
+static char *
+find_dot_file(basename) char *basename; {
+	static char localname[FILENAME_MAX+1];
+	char *fullname;
+	char *path = pathval();
+	struct stat statb;
+
+	/* don't try this for absolute or relative paths */
+	if( strchr(basename, '/'))
+		return basename;
+
+	while ((fullname = padvance(&path, basename)) != NULL) {
+		strcpy(localname, fullname);
+		stunalloc(fullname);
+		if ((stat(fullname, &statb) == 0) && S_ISREG(statb.st_mode))
+			return localname;
+	}
+	return basename;
+}
 
 dotcmd(argc, argv)  char **argv; {
 	exitstatus = 0;
 	if (argc >= 2) {		/* That's what SVR2 does */
-		setinputfile(argv[1], 1);
-		commandname = argv[1];
+		char *fullname = find_dot_file(argv[1]);
+		setinputfile(fullname, 1);
+		commandname = fullname;
 		cmdloop(0);
 		popfile();
 	}
