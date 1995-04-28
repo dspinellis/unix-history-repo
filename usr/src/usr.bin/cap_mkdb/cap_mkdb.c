3a76578762b0d80ec4b840a2ce56949cdee5437f
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1995-04-27 22:31:49 -0800

    speedup for cap_mkdb when running over NFS From: Charles Hannum <mycroft@NetBSD.ORG>
    SCCS-vsn: usr.bin/cap_mkdb/cap_mkdb.c 8.2

--- usr/src/usr.bin/cap_mkdb/cap_mkdb.c
+++ usr/src/usr.bin/cap_mkdb/cap_mkdb.c
@@ -12,7 +12,7 @@ static char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)cap_mkdb.c	8.1 (Berkeley) %G%";
+static char sccsid[] = "@(#)cap_mkdb.c	8.2 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/param.h>
