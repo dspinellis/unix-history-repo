d453521f35c50d646b1db83871383037d0a533c9
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-04-29 14:43:26 -0800

    documentation
    SCCS-vsn: usr.sbin/sendmail/src/usersmtp.c 8.51

--- usr/src/usr.sbin/sendmail/src/usersmtp.c
+++ usr/src/usr.sbin/sendmail/src/usersmtp.c
@@ -10,9 +10,9 @@
 
 #ifndef lint
 #ifdef SMTP
-static char sccsid[] = "@(#)usersmtp.c	8.50 (Berkeley) %G% (with SMTP)";
+static char sccsid[] = "@(#)usersmtp.c	8.51 (Berkeley) %G% (with SMTP)";
 #else
-static char sccsid[] = "@(#)usersmtp.c	8.50 (Berkeley) %G% (without SMTP)";
+static char sccsid[] = "@(#)usersmtp.c	8.51 (Berkeley) %G% (without SMTP)";
 #endif
 #endif /* not lint */
 
@@ -774,9 +774,8 @@ smtpprobe(mci)
 **		mci -- the mailer connection info structure.
 **		e -- the current envelope.
 **		timeout -- the timeout for reads.
-**		pfunc -- processing function for second and subsequent
-**			lines of response -- if null, no special
-**			processing is done.
+**		pfunc -- processing function called on each line of response.
+**			If null, no special processing is done.
 **
 **	Returns:
 **		reply code it reads.
