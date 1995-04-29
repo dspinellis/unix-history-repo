1d6b63309a4ab37dd91872792cde038c3638a24a
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1995-04-28 16:45:40 -0800

    The sin_zero field of one of the sockaddr's wasn't being zeroed. From: Charles Hannum <mycroft@NetBSD.ORG>
    SCCS-vsn: bin/date/netdate.c 8.2

--- usr/src/bin/date/netdate.c
+++ usr/src/bin/date/netdate.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)netdate.c	8.1 (Berkeley) %G%";
+static char sccsid[] = "@(#)netdate.c	8.2 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/param.h>
@@ -56,6 +56,7 @@ netsettime(tval)
 		return (retval = 2);
 	}
 
+	memset(&dest, 0, sizeof(dest));
 	dest.sin_port = sp->s_port;
 	dest.sin_family = AF_INET;
 	dest.sin_addr.s_addr = htonl((u_long)INADDR_ANY);
