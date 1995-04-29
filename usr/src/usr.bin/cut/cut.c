9411766494987ac77fa48851652973bf3ed53a32
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1995-04-28 19:41:26 -0800

    fixed bug with -d leaving seperators; From: Chris Torek <torek@BSDI.COM>
    SCCS-vsn: usr.bin/cut/cut.c 8.2

--- usr/src/usr.bin/cut/cut.c
+++ usr/src/usr.bin/cut/cut.c
@@ -15,7 +15,7 @@ static char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)cut.c	8.1 (Berkeley) %G%";
+static char sccsid[] = "@(#)cut.c	8.2 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <ctype.h>
@@ -192,7 +192,8 @@ f_cut(fp, fname)
 	int output;
 	char lbuf[_POSIX2_LINE_MAX + 1];
 
-	for (sep = dchar, output = 0; fgets(lbuf, sizeof(lbuf), fp);) {
+	for (sep = dchar; fgets(lbuf, sizeof(lbuf), fp);) {
+		output = 0;
 		for (isdelim = 0, p = lbuf;; ++p) {
 			if (!(ch = *p))
 				err("%s: line too long.\n", fname);
