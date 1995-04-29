647704b71824e7be1b328c7af108dec17b7f4d32
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1995-04-28 21:56:59 -0800

    add the -o option to the usage statement From: "Charles M. Hannum" <mycroft@gnu.ai.mit.edu>
    SCCS-vsn: bin/ls/util.c 8.5

--- usr/src/bin/ls/util.c
+++ usr/src/bin/ls/util.c
@@ -9,7 +9,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)util.c	8.4 (Berkeley) %G%";
+static char sccsid[] = "@(#)util.c	8.5 (Berkeley) %G%";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -40,6 +40,7 @@ prcopy(src, dest, len)
 void
 usage()
 {
-	(void)fprintf(stderr, "usage: ls [-1ACFLRTWacdfiklqrstu] [file ...]\n");
+	(void)fprintf(stderr,
+	    "usage: ls [-1ACFLRTWacdfikloqrstu] [file ...]\n");
 	exit(1);
 }
