c7fcd84ee226fef6c6426b25da4cb708d97cfdc4
Christos Zoulas <christos@ucbvax.Berkeley.EDU>
1995-04-29 00:22:00 -0800

    upgraded to the latest NetBSD version
    SCCS-vsn: usr.bin/make/lst.lib/lstAppend.c 8.2
    SCCS-vsn: usr.bin/make/lst.lib/lstAtEnd.c 8.2
    SCCS-vsn: usr.bin/make/lst.lib/lstAtFront.c 8.2
    SCCS-vsn: usr.bin/make/lst.lib/lstClose.c 8.2
    SCCS-vsn: usr.bin/make/lst.lib/lstConcat.c 8.2
    SCCS-vsn: usr.bin/make/lst.lib/lstDatum.c 8.2
    SCCS-vsn: usr.bin/make/lst.lib/lstDeQueue.c 8.2
    SCCS-vsn: usr.bin/make/lst.lib/lstDestroy.c 8.2
    SCCS-vsn: usr.bin/make/lst.lib/lstDupl.c 8.2
    SCCS-vsn: usr.bin/make/lst.lib/lstEnQueue.c 8.2
    SCCS-vsn: usr.bin/make/lst.lib/lstFind.c 8.2
    SCCS-vsn: usr.bin/make/lst.lib/lstFindFrom.c 8.2
    SCCS-vsn: usr.bin/make/lst.lib/lstFirst.c 8.2
    SCCS-vsn: usr.bin/make/lst.lib/lstForEach.c 8.2
    SCCS-vsn: usr.bin/make/lst.lib/lstForEachFrom.c 8.2
    SCCS-vsn: usr.bin/make/lst.lib/lstInit.c 8.2
    SCCS-vsn: usr.bin/make/lst.lib/lstInsert.c 8.2
    SCCS-vsn: usr.bin/make/lst.lib/lstIsAtEnd.c 8.2
    SCCS-vsn: usr.bin/make/lst.lib/lstIsEmpty.c 8.2
    SCCS-vsn: usr.bin/make/lst.lib/lstLast.c 8.2
    SCCS-vsn: usr.bin/make/lst.lib/lstMember.c 8.2
    SCCS-vsn: usr.bin/make/lst.lib/lstNext.c 8.2
    SCCS-vsn: usr.bin/make/lst.lib/lstOpen.c 8.2
    SCCS-vsn: usr.bin/make/lst.lib/lstRemove.c 8.2
    SCCS-vsn: usr.bin/make/lst.lib/lstReplace.c 8.2
    SCCS-vsn: usr.bin/make/lst.lib/lstSucc.c 8.2

--- usr/src/usr.bin/make/lst.lib/lstFindFrom.c
+++ usr/src/usr.bin/make/lst.lib/lstFindFrom.c
@@ -9,7 +9,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)lstFindFrom.c	8.1 (Berkeley) %G%";
+static char sccsid[] = "@(#)lstFindFrom.c	8.2 (Berkeley) %G%";
 #endif /* not lint */
 
 /*-
@@ -39,7 +39,7 @@ Lst_FindFrom (l, ln, d, cProc)
     Lst		      	l;
     register LstNode    ln;
     register ClientData d;
-    register int	(*cProc)();
+    register int	(*cProc) __P((ClientData, ClientData));
 {
     register ListNode	tln;
     Boolean		found = FALSE;
