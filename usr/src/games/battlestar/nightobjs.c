e7ebcd6479e06eda19b60c53a977323f94e09b7e
Keith Bostic <bostic@ucbvax.Berkeley.EDU>
1995-04-28 22:42:22 -0800

    externs.h -> extern.h
    SCCS-vsn: games/battlestar/globals.c 8.2
    SCCS-vsn: games/battlestar/init.c 8.2
    SCCS-vsn: games/battlestar/misc.c 8.2
    SCCS-vsn: games/battlestar/nightfile.c 8.2
    SCCS-vsn: games/battlestar/nightobjs.c 8.2
    SCCS-vsn: games/battlestar/parse.c 8.2
    SCCS-vsn: games/battlestar/room.c 8.2
    SCCS-vsn: games/battlestar/save.c 8.2
    SCCS-vsn: games/battlestar/words.c 8.2

--- usr/src/games/battlestar/nightobjs.c
+++ usr/src/games/battlestar/nightobjs.c
@@ -6,10 +6,10 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)nightobjs.c	8.1 (Berkeley) %G%";
+static char sccsid[] = "@(#)nightobjs.c	8.2 (Berkeley) %G%";
 #endif /* not lint */
 
-#include "externs.h"
+#include "extern.h"
 
 struct objs nightobjs[] = {
 	{ 218,	PAJAMAS },
--- usr/src/games/battlestar/save.c
+++ usr/src/games/battlestar/save.c
@@ -6,10 +6,10 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)save.c	8.1 (Berkeley) %G%";
+static char sccsid[] = "@(#)save.c	8.2 (Berkeley) %G%";
 #endif /* not lint */
 
-#include "externs.h"
+#include "extern.h"
 
 restore()
 {
