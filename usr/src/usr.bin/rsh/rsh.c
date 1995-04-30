9c40d85d30295a32e9d4d62c77bb6eef44dfcd86
Vernon Schryver <vjs@ucbvax.Berkeley.EDU>
1995-04-29 17:35:03 -0800

    Andy Cherenson's hack to understand user@remote.host just like rcp Checked in for Vernon Schryver <vjs@calcite.rhyolite.com>.
    SCCS-vsn: usr.bin/rlogin/rlogin.1 8.2
    SCCS-vsn: usr.bin/rlogin/rlogin.c 8.4
    SCCS-vsn: usr.bin/rsh/rsh.1 8.2
    SCCS-vsn: usr.bin/rsh/rsh.c 8.4

--- usr/src/usr.bin/rsh/rsh.c
+++ usr/src/usr.bin/rsh/rsh.c
@@ -12,7 +12,7 @@ static char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)rsh.c	8.3 (Berkeley) %G%";
+static char sccsid[] = "@(#)rsh.c	8.4 (Berkeley) %G%";
 #endif /* not lint */
 
 /*
@@ -163,6 +163,16 @@ main(argc, argv)
 
 	if (!(pw = getpwuid(uid = getuid())))
 		errx(1, "unknown user id");
+	/* Accept user1@host format, though "-l user2" overrides user1 */
+	p = strchr(host, '@');
+	if (p) {
+		*p = '\0';
+		if (!user && p > host)
+			user = host;
+		host = p + 1;
+		if (*host == '\0')
+			usage();
+	}
 	if (!user)
 		user = pw->pw_name;
 
@@ -440,7 +450,7 @@ usage()
 {
 
 	(void)fprintf(stderr,
-	    "usage: rsh [-nd%s]%s[-l login] host [command]\n",
+	    "usage: rsh [-nd%s]%s[-l login] [login@]host [command]\n",
 #ifdef KERBEROS
 #ifdef CRYPT
 	    "x", " [-k realm] ");
