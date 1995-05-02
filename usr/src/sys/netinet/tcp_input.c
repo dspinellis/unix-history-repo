0a47612d99b9fb586ba9ed9860d831d4c8321678
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1995-05-02 04:41:22 -0800

    set TF_ACKNOW rather than TF_DELACK when TH_PUSH bit is set (from mycroft)
    SCCS-vsn: sys/netinet/tcp_input.c 8.9

--- usr/src/sys/netinet/tcp_input.c
+++ usr/src/sys/netinet/tcp_input.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)tcp_input.c	8.8 (Berkeley) %G%
+ *	@(#)tcp_input.c	8.9 (Berkeley) %G%
  */
 
 #ifndef TUBA_INCLUDE
@@ -61,7 +61,10 @@ extern u_long sb_max;
 	if ((ti)->ti_seq == (tp)->rcv_nxt && \
 	    (tp)->seg_next == (struct tcpiphdr *)(tp) && \
 	    (tp)->t_state == TCPS_ESTABLISHED) { \
-		tp->t_flags |= TF_DELACK; \
+		if ((ti)->ti_flags & TH_PUSH) \
+			tp->t_flags |= TF_ACKNOW; \
+		else \
+			tp->t_flags |= TF_DELACK; \
 		(tp)->rcv_nxt += (ti)->ti_len; \
 		flags = (ti)->ti_flags & TH_FIN; \
 		tcpstat.tcps_rcvpack++;\
@@ -474,7 +477,10 @@ findpcb:
 			m->m_len -= sizeof(struct tcpiphdr)+off-sizeof(struct tcphdr);
 			sbappend(&so->so_rcv, m);
 			sorwakeup(so);
-			tp->t_flags |= TF_DELACK;
+			if (ti->ti_flags & TH_PUSH)
+				tp->t_flags |= TF_ACKNOW;
+			else
+				tp->t_flags |= TF_DELACK;
 			return;
 		}
 	}
