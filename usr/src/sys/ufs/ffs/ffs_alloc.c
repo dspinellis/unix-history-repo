1038e688ce292e2455fbb2532c0b04d3d9520890
Kirk McKusick <mckusick@ucbvax.Berkeley.EDU>
1995-05-04 00:31:34 -0800

    give up on rotational layout if there is only one rotational position
    SCCS-vsn: sys/ufs/ffs/ffs_alloc.c 8.17

--- usr/src/sys/ufs/ffs/ffs_alloc.c
+++ usr/src/sys/ufs/ffs/ffs_alloc.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)ffs_alloc.c	8.16 (Berkeley) %G%
+ *	@(#)ffs_alloc.c	8.17 (Berkeley) %G%
  */
 
 #include <sys/param.h>
@@ -894,13 +894,7 @@ ffs_alloccgblk(fs, cgp, bpref)
 		bno = bpref;
 		goto gotit;
 	}
-	/*
-	 * check for a block available on the same cylinder
-	 */
-	cylno = cbtocylno(fs, bpref);
-	if (cg_blktot(cgp)[cylno] == 0)
-		goto norot;
-	if (fs->fs_cpc == 0) {
+	if (fs->fs_nrpos <= 1 || fs->fs_cpc == 0) {
 		/*
 		 * Block layout information is not available.
 		 * Leaving bpref unchanged means we take the
@@ -911,6 +905,12 @@ ffs_alloccgblk(fs, cgp, bpref)
 		 */
 		goto norot;
 	}
+	/*
+	 * check for a block available on the same cylinder
+	 */
+	cylno = cbtocylno(fs, bpref);
+	if (cg_blktot(cgp)[cylno] == 0)
+		goto norot;
 	/*
 	 * check the summary information to see if a block is 
 	 * available in the requested cylinder starting at the
