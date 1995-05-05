54b2fe0b55545db526a5105fc4fec30200f61d08
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-05-05 15:21:10 -0800

    fix MIME problem: did header munging on interior headers
    SCCS-vsn: usr.sbin/sendmail/src/headers.c 8.61
    SCCS-vsn: usr.sbin/sendmail/src/sendmail.h 8.131
    SCCS-vsn: usr.sbin/sendmail/src/mime.c 8.20

--- usr/src/usr.sbin/sendmail/src/headers.c
+++ usr/src/usr.sbin/sendmail/src/headers.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)headers.c	8.60 (Berkeley) %G%";
+static char sccsid[] = "@(#)headers.c	8.61 (Berkeley) %G%";
 #endif /* not lint */
 
 # include <errno.h>
@@ -1015,6 +1015,18 @@ putheader(mci, h, e)
 			xputs(h->h_value);
 		}
 
+		/* suppress Content-Transfer-Encoding: if we are MIMEing */
+		if (bitset(H_CTE, h->h_flags) &&
+		    bitset(MCIF_CVT8TO7|MCIF_INMIME, mci->mci_flags))
+		{
+			if (tTd(34, 11))
+				printf(" (skipped (content-transfer-encoding))\n");
+			continue;
+		}
+
+		if (bitset(MCIF_INMIME, mci->mci_flags))
+			goto vanilla;
+
 		if (bitset(H_CHECK|H_ACHECK, h->h_flags) &&
 		    !bitintersect(h->h_mflags, mci->mci_mailer->m_flags))
 		{
@@ -1037,15 +1049,6 @@ putheader(mci, h, e)
 		{
 			if (tTd(34, 11))
 				printf(" (skipped (receipt))\n");
-			continue;
-		}
-
-		/* suppress Content-Transfer-Encoding: if we are MIMEing */
-		if (bitset(H_CTE, h->h_flags) &&
-		    bitset(MCIF_CVT8TO7, mci->mci_flags))
-		{
-			if (tTd(34, 11))
-				printf(" (skipped (content-transfer-encoding))\n");
 			continue;
 		}
 
@@ -1086,6 +1089,7 @@ putheader(mci, h, e)
 			/* vanilla header line */
 			register char *nlp;
 
+vanilla:
 			(void) sprintf(obuf, "%s: ", h->h_field);
 			while ((nlp = strchr(p, '\n')) != NULL)
 			{
