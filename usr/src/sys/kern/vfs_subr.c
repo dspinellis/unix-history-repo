4afb461d2c58d92fed28ebcb6394d795659f806f
Jan-Simon Pendry <pendry@ucbvax.Berkeley.EDU>
1995-05-10 18:18:41 -0800

    VOP_REVOKE(vp, 0) should really be vgone(vp)
    SCCS-vsn: sys/kern/vfs_subr.c 8.22

--- usr/src/sys/kern/vfs_subr.c
+++ usr/src/sys/kern/vfs_subr.c
@@ -9,7 +9,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)vfs_subr.c	8.21 (Berkeley) %G%
+ *	@(#)vfs_subr.c	8.22 (Berkeley) %G%
  */
 
 /*
@@ -246,7 +246,7 @@ getnewvnode(tag, mp, vops, vpp)
 		vp->v_freelist.tqe_prev = (struct vnode **)0xdeadb;
 		vp->v_lease = NULL;
 		if (vp->v_type != VBAD)
-			VOP_REVOKE(vp, 0);
+			vgone(vp);
 #ifdef DIAGNOSTIC
 		if (vp->v_data)
 			panic("cleaned vnode isn't");
@@ -725,7 +725,7 @@ loop:
 		 * out the vnode data structures and we are done.
 		 */
 		if (vp->v_usecount == 0) {
-			VOP_REVOKE(vp, 0);
+			vgone(vp);
 			continue;
 		}
 		/*
@@ -735,7 +735,7 @@ loop:
 		 */
 		if (flags & FORCECLOSE) {
 			if (vp->v_type != VBLK && vp->v_type != VCHR) {
-				VOP_REVOKE(vp, 0);
+				vgone(vp);
 			} else {
 				vclean(vp, 0);
 				vp->v_op = spec_vnodeop_p;
