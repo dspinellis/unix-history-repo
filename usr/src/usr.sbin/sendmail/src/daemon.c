a9e0e9a8445198e2d53fa6e34683a5e0609988b6
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-05-13 15:19:32 -0800

    make DaemonPortOptions values case insensitive & include error on unrecognized values
    SCCS-vsn: usr.sbin/sendmail/src/daemon.c 8.85

--- usr/src/usr.sbin/sendmail/src/daemon.c
+++ usr/src/usr.sbin/sendmail/src/daemon.c
@@ -11,9 +11,9 @@
 
 #ifndef lint
 #ifdef DAEMON
-static char sccsid[] = "@(#)daemon.c	8.84 (Berkeley) %G% (with daemon mode)";
+static char sccsid[] = "@(#)daemon.c	8.85 (Berkeley) %G% (with daemon mode)";
 #else
-static char sccsid[] = "@(#)daemon.c	8.84 (Berkeley) %G% (without daemon mode)";
+static char sccsid[] = "@(#)daemon.c	8.85 (Berkeley) %G% (without daemon mode)";
 #endif
 #endif /* not lint */
 
@@ -471,6 +471,8 @@ setdaemonoptions(p)
 			continue;
 		while (isascii(*++v) && isspace(*v))
 			continue;
+		if (isascii(*f) && isupper(*f))
+			*f = tolower(*f);
 
 		switch (*f)
 		{
@@ -583,6 +585,9 @@ setdaemonoptions(p)
 		  case 'R':		/* receive buffer size */
 			TcpRcvBufferSize = atoi(v);
 			break;
+
+		  default:
+			syserr("554 DaemonPortOptions parameter \"%s\" unknown", f);
 		}
 	}
 }
