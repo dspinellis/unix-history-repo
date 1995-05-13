8660c2efde05afbb60f3d4901ea9185f93cbf546
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-05-13 15:18:56 -0800

    remove sendmail.cf versioning (except on compile flag) -- confusing
    SCCS-vsn: usr.sbin/sendmail/src/conf.c 8.158

--- usr/src/usr.sbin/sendmail/src/conf.c
+++ usr/src/usr.sbin/sendmail/src/conf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)conf.c	8.157 (Berkeley) %G%";
+static char sccsid[] = "@(#)conf.c	8.158 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
