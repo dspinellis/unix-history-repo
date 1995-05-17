26bc85a36240ff89bee626561dd06c620a8907ff
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-05-17 14:20:50 -0800

    handle AF_LINK addresses on 4.4BSD networking
    SCCS-vsn: usr.sbin/sendmail/src/conf.h 8.161
    SCCS-vsn: usr.sbin/sendmail/src/daemon.c 8.89
    SCCS-vsn: usr.sbin/sendmail/src/conf.c 8.168

--- usr/src/usr.sbin/sendmail/src/conf.c
+++ usr/src/usr.sbin/sendmail/src/conf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)conf.c	8.167 (Berkeley) %G%";
+static char sccsid[] = "@(#)conf.c	8.168 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
