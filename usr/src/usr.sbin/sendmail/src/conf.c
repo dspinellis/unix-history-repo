6bf06c0e53dff1bda77d3635bc57e04449d8bc68
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-05-17 16:12:16 -0800

    include NISPLUS in list of compile flags
    SCCS-vsn: usr.sbin/sendmail/src/conf.c 8.169

--- usr/src/usr.sbin/sendmail/src/conf.c
+++ usr/src/usr.sbin/sendmail/src/conf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)conf.c	8.168 (Berkeley) %G%";
+static char sccsid[] = "@(#)conf.c	8.169 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
