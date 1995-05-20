bdb756235180a9bd0bca12905d729b70d8579f5a
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-05-19 22:59:29 -0800

    fix simple self-references; more debugging
    SCCS-vsn: usr.sbin/sendmail/src/recipient.c 8.85

--- usr/src/usr.sbin/sendmail/src/recipient.c
+++ usr/src/usr.sbin/sendmail/src/recipient.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)recipient.c	8.84 (Berkeley) %G%";
+static char sccsid[] = "@(#)recipient.c	8.85 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -120,19 +120,34 @@ sendtolist(list, ctladdr, sendq, aliaslevel, e)
 			ADDRESS *b;
 			extern ADDRESS *self_reference();
 
-#if 0
 			/* self reference test */
 			if (sameaddr(ctladdr, a))
+			{
+				if (tTd(27, 5))
+				{
+					printf("sendtolist: QSELFREF ");
+					printaddr(ctladdr, FALSE);
+				}
 				ctladdr->q_flags |= QSELFREF;
-#endif
+			}
 
 			/* check for address loops */
 			b = self_reference(a, e);
 			if (b != NULL)
 			{
 				b->q_flags |= QSELFREF;
+				if (tTd(27, 5))
+				{
+					printf("sendtolist: QSELFREF ");
+					printaddr(b, FALSE);
+				}
 				if (a != b)
 				{
+					if (tTd(27, 5))
+					{
+						printf("sendtolist: QDONTSEND ");
+						printaddr(a, FALSE);
+					}
 					a->q_flags |= QDONTSEND;
 					continue;
 				}
