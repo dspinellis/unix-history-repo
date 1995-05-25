154497358540de8844b6448325206438a1e396b9
Margo Seltzer <margo@ucbvax.Berkeley.EDU>
1995-05-25 02:45:44 -0800

    Add support for fragments to make_lfs.
    SCCS-vsn: sbin/newlfs/extern.h 8.2

--- usr/src/sbin/newlfs/extern.h
+++ usr/src/sbin/newlfs/extern.h
@@ -4,15 +4,15 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)extern.h	8.1 (Berkeley) %G%
+ *	@(#)extern.h	8.2 (Berkeley) %G%
  */
 
 u_long	cksum __P((void *, size_t));
 u_short	dkcksum __P((struct disklabel *));
 void	fatal __P((const char *fmt, ...));
 u_int	log2 __P((u_int));
-int	make_lfs
-	    __P((int, struct disklabel *, struct partition *, int, int, int));
+int	make_lfs __P((int, struct disklabel *, struct partition *, int,
+		int, int, int));
 int	mkfs __P((struct partition *, char *, int, int));
 
 extern char	*progname;
