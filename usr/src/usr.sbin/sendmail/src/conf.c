5e38f905eb2bfe48ebe37054af31037cd41c2162
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-05-25 18:38:47 -0800

    fix some NIS problems
    SCCS-vsn: usr.sbin/sendmail/src/conf.c 8.176

--- usr/src/usr.sbin/sendmail/src/conf.c
+++ usr/src/usr.sbin/sendmail/src/conf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)conf.c	8.175 (Berkeley) %G%";
+static char sccsid[] = "@(#)conf.c	8.176 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
