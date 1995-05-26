8c6a5fae6e10e2afcf196aaf47e4d4bc53f02400
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-05-25 18:38:31 -0800

    MD_FREEZE is gone, but MD_INITALIAS is sensitive
    SCCS-vsn: usr.sbin/sendmail/src/readcf.c 8.92

--- usr/src/usr.sbin/sendmail/src/readcf.c
+++ usr/src/usr.sbin/sendmail/src/readcf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)readcf.c	8.91 (Berkeley) %G%";
+static char sccsid[] = "@(#)readcf.c	8.92 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -113,7 +113,7 @@ readcf(cfname)
 
 	if (OpMode != MD_TEST && bitset(S_IWGRP|S_IWOTH, statb.st_mode))
 	{
-		if (OpMode == MD_DAEMON || OpMode == MD_FREEZE)
+		if (OpMode == MD_DAEMON || OpMode == MD_INITALIAS)
 			fprintf(stderr, "%s: WARNING: dangerous write permissions\n",
 				FileName);
 #ifdef LOG
