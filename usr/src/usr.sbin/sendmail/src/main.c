a303256af8585f59f0f1ad8bcf779cba2576933c
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-05-27 14:45:59 -0800

    clean up test mode; putenv can fail
    SCCS-vsn: usr.sbin/sendmail/src/main.c 8.115

--- usr/src/usr.sbin/sendmail/src/main.c
+++ usr/src/usr.sbin/sendmail/src/main.c
@@ -13,7 +13,7 @@ static char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)main.c	8.114 (Berkeley) %G%";
+static char sccsid[] = "@(#)main.c	8.115 (Berkeley) %G%";
 #endif /* not lint */
 
 #define	_DEFINE
@@ -959,8 +959,11 @@ main(argc, argv, envp)
 			(void) fflush(stdout);
 			if (fgets(buf, sizeof buf, stdin) == NULL)
 				finis();
+			p = strchr(buf, '\n');
+			if (p != NULL)
+				*p = '\0';
 			if (!Verbose)
-				printf("> %s", buf);
+				printf("> %s\n", buf);
 			testmodeline(buf, CurEnv);
 		}
 	}
@@ -1527,7 +1530,8 @@ setuserenv(envar, value)
 	}
 
 	/* make sure it is in our environment as well */
-	putenv(p);
+	if (putenv(p) < 0)
+		syserr("setuserenv: putenv(%s) failed", p);
 }
 /*
 **  DUMPSTATE -- dump state
@@ -1622,9 +1626,6 @@ testmodeline(line, e)
 		switch (line[1])
 		{
 		  case 'D':
-			p = strchr(line, '\n');
-			if (p != NULL)
-				*p = '\0';
 			mid = macid(&line[2], &delimptr);
 			if (mid != '\0')
 				define(mid, newstr(delimptr), e);
@@ -1641,11 +1642,9 @@ testmodeline(line, e)
 				char *cp;
 				STAB *s;
 
-				if ((cp = strchr(line, '\n')) != NULL)
-					*cp = '\0';
-				if (cp == line+2)
+				if (line[2] == '\0')
 					return;
-				s = stab(line+2, ST_RULESET, ST_FIND);
+				s = stab(&line[2], ST_RULESET, ST_FIND);
 				if (s == NULL)
 				{
 					if (!isdigit(line[2]))
@@ -1691,10 +1690,7 @@ testmodeline(line, e)
 		switch (line[1])
 		{
 		  case 'd':
-			if (line[2] == '\n')
-				tTflag("");
-			else
-				tTflag(&line[2]);
+			tTflag(&line[2]);
 			break;
 
 		  default:
@@ -1718,12 +1714,9 @@ testmodeline(line, e)
 		return;
 
 	  case '/':		/* miscellaneous commands */
-		p = strchr(line, '\n');
-		if (p != NULL)
-		{
-			while (p >= line && isascii(*p) && isspace(*p))
-				*p-- = '\0';
-		}
+		p = &line[strlen(line)];
+		while (--p >= line && isascii(*p) && isspace(*p))
+			*p = '\0';
 		p = strpbrk(line, " \t");
 		if (p != NULL)
 		{
