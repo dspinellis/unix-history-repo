b84cbab2c75d265743d65f48701f25fbbd94c548
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-05-27 16:36:38 -0800

    make the maximum number of jobs in one queue run an option
    SCCS-vsn: usr.sbin/sendmail/src/conf.h 8.174
    SCCS-vsn: usr.sbin/sendmail/src/sendmail.h 8.135
    SCCS-vsn: usr.sbin/sendmail/src/readcf.c 8.94
    SCCS-vsn: usr.sbin/sendmail/src/queue.c 8.83

--- usr/src/usr.sbin/sendmail/src/sendmail.h
+++ usr/src/usr.sbin/sendmail/src/sendmail.h
@@ -5,7 +5,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)sendmail.h	8.134 (Berkeley) %G%
+ *	@(#)sendmail.h	8.135 (Berkeley) %G%
  */
 
 /*
@@ -15,7 +15,7 @@
 # ifdef _DEFINE
 # define EXTERN
 # ifndef lint
-static char SmailSccsId[] =	"@(#)sendmail.h	8.134		%G%";
+static char SmailSccsId[] =	"@(#)sendmail.h	8.135		%G%";
 # endif
 # else /*  _DEFINE */
 # define EXTERN extern
