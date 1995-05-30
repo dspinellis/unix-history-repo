e99156ec65fc968f3ce8950158cb5e17e962c7c6
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-05-30 15:08:33 -0800

    add trace flags in sm_gethostbyname
    SCCS-vsn: usr.sbin/sendmail/src/conf.c 8.185
    SCCS-vsn: usr.sbin/sendmail/src/TRACEFLAGS 8.10

--- usr/src/usr.sbin/sendmail/src/conf.c
+++ usr/src/usr.sbin/sendmail/src/conf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)conf.c	8.184 (Berkeley) %G%";
+static char sccsid[] = "@(#)conf.c	8.185 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
