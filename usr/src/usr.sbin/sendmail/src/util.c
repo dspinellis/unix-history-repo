e3afd8c03cdbb9200dfa24166912d06f8ac0fb29
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-05-30 14:08:41 -0800

    don't shorten names that have a trailing dot (except for the dot itself)
    SCCS-vsn: usr.sbin/sendmail/src/util.c 8.73

--- usr/src/usr.sbin/sendmail/src/util.c
+++ usr/src/usr.sbin/sendmail/src/util.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)util.c	8.72 (Berkeley) %G%";
+static char sccsid[] = "@(#)util.c	8.73 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -1635,11 +1635,15 @@ shorten_hostname(host)
 	register char *p;
 	char *mydom;
 	int i;
+	bool canon = FALSE;
 
 	/* strip off final dot */
 	p = &host[strlen(host) - 1];
 	if (*p == '.')
+	{
 		*p = '\0';
+		canon = TRUE;
+	}
 
 	/* see if there is any domain at all -- if not, we are done */
 	p = strchr(host, '.');
@@ -1651,7 +1655,7 @@ shorten_hostname(host)
 	if (mydom == NULL)
 		mydom = "";
 	i = strlen(++p);
-	if (strncasecmp(p, mydom, i) == 0 &&
+	if ((canon ? strcasecmp(p, mydom) : strncasecmp(p, mydom, i)) == 0 &&
 	    (mydom[i] == '.' || mydom[i] == '\0'))
 		*--p = '\0';
 }
