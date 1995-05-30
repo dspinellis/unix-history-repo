05db955c12804d5f1bf66e3795aac1241699c178
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-05-30 01:13:42 -0800

    fix a comment
    SCCS-vsn: usr.sbin/sendmail/src/readcf.c 8.96

--- usr/src/usr.sbin/sendmail/src/readcf.c
+++ usr/src/usr.sbin/sendmail/src/readcf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)readcf.c	8.95 (Berkeley) %G%";
+static char sccsid[] = "@(#)readcf.c	8.96 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -2136,8 +2136,7 @@ setclass(class, str)
 **		line -- the config file line
 **
 **	Returns:
-**		TRUE if it successfully entered the map entry.
-**		FALSE otherwise (usually syntax error).
+**		none.
 **
 **	Side Effects:
 **		Enters the map into the dictionary.
