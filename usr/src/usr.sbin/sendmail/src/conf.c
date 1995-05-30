dc9cff39d6da38be5ba78443fa294863661c925c
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-05-30 00:49:19 -0800

    put use of getconfattr() on a separate USEGETCONFATTR compile flag; in my opinion getconfattr() is fundamentally misdesigned
    SCCS-vsn: usr.sbin/sendmail/src/conf.c 8.182

--- usr/src/usr.sbin/sendmail/src/conf.c
+++ usr/src/usr.sbin/sendmail/src/conf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)conf.c	8.181 (Berkeley) %G%";
+static char sccsid[] = "@(#)conf.c	8.182 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
