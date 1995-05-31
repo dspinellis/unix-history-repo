f2a2dbe2f4e7985846079c7fcf215197fdfd891c
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-05-31 02:25:18 -0800

    allow symbolic ruleset names on M lines (S= and R=)
    SCCS-vsn: usr.sbin/sendmail/src/parseaddr.c 8.71
    SCCS-vsn: usr.sbin/sendmail/src/readcf.c 8.98

--- usr/src/usr.sbin/sendmail/src/parseaddr.c
+++ usr/src/usr.sbin/sendmail/src/parseaddr.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)parseaddr.c	8.70 (Berkeley) %G%";
+static char sccsid[] = "@(#)parseaddr.c	8.71 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -1249,11 +1249,7 @@ rewrite(pvp, ruleset, reclevel, e)
 					(int) (avp - npvp - 2) * sizeof *avp);
 				if (tTd(21, 3))
 					printf("-----callsubr %s\n", npvp[1]);
-				s = stab(npvp[1], ST_RULESET, ST_FIND);
-				if (s == NULL)
-					ruleset = atoi(npvp[1]);
-				else
-					ruleset = s->s_ruleset;
+				ruleset = strtorwset(npvp[1], NULL, ST_FIND);
 				stat = rewrite(pvp, ruleset, reclevel, e);
 				if (rstat == EX_OK || stat == EX_TEMPFAIL)
 					rstat = stat;
