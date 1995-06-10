3ee4e437196a4a73b92c20f793a9e92ac2adf615
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-06-10 14:37:19 -0800

    need MF_KEEPQUOTES bit in dequote map, else it is double-dequoted
    SCCS-vsn: usr.sbin/sendmail/src/parseaddr.c 8.72

--- usr/src/usr.sbin/sendmail/src/parseaddr.c
+++ usr/src/usr.sbin/sendmail/src/parseaddr.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)parseaddr.c	8.71 (Berkeley) %G%";
+static char sccsid[] = "@(#)parseaddr.c	8.72 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -1981,6 +1981,7 @@ dequote_init(map, args)
 {
 	register char *p = args;
 
+	map->map_mflags |= MF_KEEPQUOTES;
 	for (;;)
 	{
 		while (isascii(*p) && isspace(*p))
