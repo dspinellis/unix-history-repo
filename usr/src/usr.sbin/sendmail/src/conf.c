372254ff1a333e2f21846b0e159d39459f477f94
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-06-10 14:37:49 -0800

    print OS compile flags (sigh)
    SCCS-vsn: usr.sbin/sendmail/src/conf.c 8.189
    SCCS-vsn: usr.sbin/sendmail/src/main.c 8.122

--- usr/src/usr.sbin/sendmail/src/conf.c
+++ usr/src/usr.sbin/sendmail/src/conf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)conf.c	8.188 (Berkeley) %G%";
+static char sccsid[] = "@(#)conf.c	8.189 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
