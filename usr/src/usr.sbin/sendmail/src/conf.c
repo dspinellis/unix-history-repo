ed89cb7f1180d273682f135c0de983ddb131cd8f
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-06-10 19:09:05 -0800

    more clever connection refusals
    SCCS-vsn: usr.sbin/sendmail/src/conf.c 8.191

--- usr/src/usr.sbin/sendmail/src/conf.c
+++ usr/src/usr.sbin/sendmail/src/conf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)conf.c	8.190 (Berkeley) %G%";
+static char sccsid[] = "@(#)conf.c	8.191 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
