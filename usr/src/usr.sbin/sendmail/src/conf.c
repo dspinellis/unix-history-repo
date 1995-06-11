e04b05e9787f257e7e26a1f3f2f92b5afd1358d1
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-06-10 21:55:39 -0800

    fix definition for ANSI compilers
    SCCS-vsn: usr.sbin/sendmail/src/conf.c 8.193

--- usr/src/usr.sbin/sendmail/src/conf.c
+++ usr/src/usr.sbin/sendmail/src/conf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)conf.c	8.192 (Berkeley) %G%";
+static char sccsid[] = "@(#)conf.c	8.193 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
