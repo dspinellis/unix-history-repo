97a4d2dd1b01d28bf6400c43600eadd82df8279b
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-06-10 19:15:22 -0800

    TRY_VERSIONED_CF_NAME is a botch -- consider mailstats
    SCCS-vsn: usr.sbin/sendmail/src/conf.c 8.192

--- usr/src/usr.sbin/sendmail/src/conf.c
+++ usr/src/usr.sbin/sendmail/src/conf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)conf.c	8.191 (Berkeley) %G%";
+static char sccsid[] = "@(#)conf.c	8.192 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
