842fe529fa42b885387b9248d5c0e4b1a91c1898
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-06-11 19:01:29 -0800

    make sm_getpwuid definition match prototype; move some debugging
    SCCS-vsn: usr.sbin/sendmail/src/conf.c 8.194

--- usr/src/usr.sbin/sendmail/src/conf.c
+++ usr/src/usr.sbin/sendmail/src/conf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)conf.c	8.193 (Berkeley) %G%";
+static char sccsid[] = "@(#)conf.c	8.194 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
