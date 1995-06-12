ddf539189e2286c3a5037f1796c396b307ea48eb
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-06-12 13:16:40 -0800

    fix inverted debug output
    SCCS-vsn: usr.sbin/sendmail/src/readcf.c 8.103

--- usr/src/usr.sbin/sendmail/src/readcf.c
+++ usr/src/usr.sbin/sendmail/src/readcf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)readcf.c	8.102 (Berkeley) %G%";
+static char sccsid[] = "@(#)readcf.c	8.103 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -1670,7 +1670,7 @@ setoption(opt, val, sticky)
 		}
 		if (tTd(8, 2))
 			printf("_res.options = %x, HasWildcardMX = %d\n",
-				_res.options, !NoMXforCanon);
+				_res.options, NoMXforCanon);
 #else
 		usrerr("name server (I option) specified but BIND not compiled in");
 #endif
