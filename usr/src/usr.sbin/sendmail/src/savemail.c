f9558b2b62a76fa85b0ff1e21097a5d21c384add
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-06-14 14:04:22 -0800

    update for new DSN draft
    SCCS-vsn: usr.sbin/sendmail/src/savemail.c 8.75
    SCCS-vsn: usr.sbin/sendmail/src/srvrsmtp.c 8.82

--- usr/src/usr.sbin/sendmail/src/savemail.c
+++ usr/src/usr.sbin/sendmail/src/savemail.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)savemail.c	8.74 (Berkeley) %G%";
+static char sccsid[] = "@(#)savemail.c	8.75 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
