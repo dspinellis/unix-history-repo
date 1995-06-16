ea0b6c8e931c74fdf3669363296a2ddd89bb03a2
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-06-16 02:06:41 -0800

    don't alias \-quoted addresses
    SCCS-vsn: usr.sbin/sendmail/src/recipient.c 8.96

--- usr/src/usr.sbin/sendmail/src/recipient.c
+++ usr/src/usr.sbin/sendmail/src/recipient.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)recipient.c	8.95 (Berkeley) %G%";
+static char sccsid[] = "@(#)recipient.c	8.96 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -421,7 +421,8 @@ recipient(a, sendq, aliaslevel, e)
 	}
 
 	/* try aliasing */
-	if (!bitset(QDONTSEND, a->q_flags) && bitnset(M_ALIASABLE, m->m_flags))
+	if (!quoted && !bitset(QDONTSEND, a->q_flags) &&
+	    bitnset(M_ALIASABLE, m->m_flags))
 		alias(a, sendq, aliaslevel, e);
 
 # if USERDB
