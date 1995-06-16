02e564933d08905dadd2979d0d074665a2ce769c
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-06-15 21:04:43 -0800

    improved wildcard MX handling
    SCCS-vsn: usr.sbin/sendmail/src/domain.c 8.44

--- usr/src/usr.sbin/sendmail/src/domain.c
+++ usr/src/usr.sbin/sendmail/src/domain.c
@@ -10,9 +10,9 @@
 
 #ifndef lint
 #if NAMED_BIND
-static char sccsid[] = "@(#)domain.c	8.43 (Berkeley) %G% (with name server)";
+static char sccsid[] = "@(#)domain.c	8.44 (Berkeley) %G% (with name server)";
 #else
-static char sccsid[] = "@(#)domain.c	8.43 (Berkeley) %G% (without name server)";
+static char sccsid[] = "@(#)domain.c	8.44 (Berkeley) %G% (without name server)";
 #endif
 #endif /* not lint */
 
@@ -451,7 +451,7 @@ dns_getcanonname(host, hbsize, trymx, statp)
 
 	loopcnt = 0;
 cnameloop:
-	for (cp = host, n = 0; *cp; cp++)
+	for (cp = host, n = 0; *cp != '\0'; cp++)
 		if (*cp == '.')
 			n++;
 
@@ -486,6 +486,10 @@ cnameloop:
 		*cp = '\0';
 	}
 	*dp = NULL;
+
+	/* if we have a wildcard MX and no dots, try MX anyhow */
+	if (n == 0)
+		trymx = TRUE;
 
 	/*
 	**  Now run through the search list for the name in question.
@@ -534,13 +538,7 @@ cnameloop:
 				}
 			}
 
-			if (mxmatch != NULL)
-			{
-				/* we matched before -- use that one */
-				break;
-			}
-
-			/* otherwise, try the next name */
+			/* try the next name */
 			dp++;
 			qtype = T_ANY;
 			continue;
@@ -586,7 +584,7 @@ cnameloop:
 			{
 			  case T_MX:
 				gotmx = TRUE;
-				if (**dp != '\0')
+				if (**dp != '\0' || !HasWildcardMX)
 				{
 					/* got a match -- save that info */
 					if (trymx && mxmatch == NULL)
