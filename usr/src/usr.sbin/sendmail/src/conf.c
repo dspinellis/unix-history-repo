13bd3cf125bb1b7a61c291d15c1cfe2b8180b260
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-06-18 15:07:56 -0800

    try short host name for files hosts service (as well as nis)
    SCCS-vsn: usr.sbin/sendmail/src/conf.c 8.200

--- usr/src/usr.sbin/sendmail/src/conf.c
+++ usr/src/usr.sbin/sendmail/src/conf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)conf.c	8.199 (Berkeley) %G%";
+static char sccsid[] = "@(#)conf.c	8.200 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
