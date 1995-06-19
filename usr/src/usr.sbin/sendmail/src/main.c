9067f8c7be06985ea26d46518f58c3a5dc904b3f
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-06-19 14:39:36 -0800

    release HUP signal before re-execing sendmail
    SCCS-vsn: usr.sbin/sendmail/src/main.c 8.133
    SCCS-vsn: usr.sbin/sendmail/src/conf.c 8.201

--- usr/src/usr.sbin/sendmail/src/main.c
+++ usr/src/usr.sbin/sendmail/src/main.c
@@ -13,7 +13,7 @@ static char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)main.c	8.132 (Berkeley) %G%";
+static char sccsid[] = "@(#)main.c	8.133 (Berkeley) %G%";
 #endif /* not lint */
 
 #define	_DEFINE
