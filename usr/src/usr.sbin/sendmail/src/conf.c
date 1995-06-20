9b46433efdeec1d15dec61cf98874f3e2fbf932f
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-06-20 00:10:08 -0800

    back out check for /etc/sendmail.cf change -- instead, just always use sendmail.cf directly
    SCCS-vsn: usr.sbin/sendmail/src/conf.c 8.202

--- usr/src/usr.sbin/sendmail/src/conf.c
+++ usr/src/usr.sbin/sendmail/src/conf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)conf.c	8.201 (Berkeley) %G%";
+static char sccsid[] = "@(#)conf.c	8.202 (Berkeley) %G%";
 #endif /* not lint */
 
 # include "sendmail.h"
