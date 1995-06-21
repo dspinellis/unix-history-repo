0d376d305ad07164c1d3f78497c7e03c1c04ba96
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-06-20 16:37:19 -0800

    avoid infinite loop on /tryflags
    SCCS-vsn: usr.sbin/sendmail/src/main.c 8.135

--- usr/src/usr.sbin/sendmail/src/main.c
+++ usr/src/usr.sbin/sendmail/src/main.c
@@ -13,7 +13,7 @@ static char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)main.c	8.134 (Berkeley) %G%";
+static char sccsid[] = "@(#)main.c	8.135 (Berkeley) %G%";
 #endif /* not lint */
 
 #define	_DEFINE
@@ -1813,7 +1813,7 @@ testmodeline(line, e)
 		}
 		else if (strcasecmp(&line[1], "tryflags") == 0)
 		{
-			while (*p != '\n')
+			for (; *p != '\0'; p++)
 			{
 				switch (*p)
 				{
