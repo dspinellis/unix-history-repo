1b14bc8b170a2f85c6daf9f7cfb39974e6e056d0
Eric Allman <eric@ucbvax.Berkeley.EDU>
1995-06-21 14:08:30 -0800

    allow symbolic ruleset names in -bt mode
    SCCS-vsn: usr.sbin/sendmail/src/main.c 8.136

--- usr/src/usr.sbin/sendmail/src/main.c
+++ usr/src/usr.sbin/sendmail/src/main.c
@@ -13,7 +13,7 @@ static char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)main.c	8.135 (Berkeley) %G%";
+static char sccsid[] = "@(#)main.c	8.136 (Berkeley) %G%";
 #endif /* not lint */
 
 #define	_DEFINE
@@ -1872,11 +1872,14 @@ testmodeline(line, e)
 		while (*p != '\0')
 		{
 			int stat;
+			int rs = strtorwset(p, NULL, ST_FIND);
 
-			stat = rewrite(pvp, atoi(p), 0, e);
+			if (rs < 0)
+				break;
+			stat = rewrite(pvp, rs, 0, e);
 			if (stat != EX_OK)
-				printf("== Ruleset %s status %d\n",
-					p, stat);
+				printf("== Ruleset %s (%d) status %d\n",
+					p, rs, stat);
 			while (*p != '\0' && *p++ != ',')
 				continue;
 		}
