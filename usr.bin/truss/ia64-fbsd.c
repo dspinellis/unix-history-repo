dspinellis/unix-history-repo

commit b869246f894399d3d6324a262980735606fc6333

Add -c option to summarize number of calls, errors, and system time.
Reviewed by:	alfred


git-svn-id: svn+ssh://svn.freebsd.org/base/head@192025 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

usr.bin/truss/ia64-fbsd.c
@@ -294,7 +294,8 @@ ia64_syscall_exit(struct trussinfo *trussinfo, int syscall_num __unused)
    * but that complicates things considerably.
    */
 
-  print_syscall_ret(trussinfo, fsc.name, fsc.nargs, fsc.s_args, errorp, retval);
+  print_syscall_ret(trussinfo, fsc.name, fsc.nargs, fsc.s_args, errorp,
+		    fsc.sc, retval);
   clear_fsc();
 
   return (retval);

usr.bin/truss/amd64-fbsd.c
@@ -323,7 +323,8 @@ amd64_syscall_exit(struct trussinfo *trussinfo, int syscall_num __unused)
    * but that complicates things considerably.
    */
 
-  print_syscall_ret(trussinfo, fsc.name, fsc.nargs, fsc.s_args, errorp, retval);
+  print_syscall_ret(trussinfo, fsc.name, fsc.nargs, fsc.s_args, errorp,
+		    retval, fsc.sc);
   clear_fsc();
 
   return (retval);

usr.bin/truss/amd64-fbsd32.c
@@ -339,7 +339,8 @@ amd64_fbsd32_syscall_exit(struct trussinfo *trussinfo, int syscall_num __unused)
    * but that complicates things considerably.
    */
 
-  print_syscall_ret(trussinfo, fsc.name, fsc.nargs, fsc.s_args, errorp, retval);
+  print_syscall_ret(trussinfo, fsc.name, fsc.nargs, fsc.s_args, errorp,
+		    retval, fsc.sc);
   clear_fsc();
 
   return (retval);

usr.bin/truss/amd64-linux32.c
@@ -309,7 +309,7 @@ amd64_linux32_syscall_exit(struct trussinfo *trussinfo, int syscall_num __unused
   }
 
   print_syscall_ret(trussinfo, fsc.name, fsc.nargs, fsc.s_args, errorp,
-                    errorp ? i : retval);
+                    errorp ? i : retval, fsc.sc);
   clear_fsc();
 
   return (retval);

usr.bin/truss/i386-fbsd.c
@@ -329,7 +329,8 @@ i386_syscall_exit(struct trussinfo *trussinfo, int syscall_num __unused)
    * but that complicates things considerably.
    */
 
-  print_syscall_ret(trussinfo, fsc.name, fsc.nargs, fsc.s_args, errorp, retval);
+  print_syscall_ret(trussinfo, fsc.name, fsc.nargs, fsc.s_args, errorp,
+		    retval, fsc.sc);
   clear_fsc();
 
   return (retval);

usr.bin/truss/i386-linux.c
@@ -309,7 +309,7 @@ i386_linux_syscall_exit(struct trussinfo *trussinfo, int syscall_num __unused)
   }
 
   print_syscall_ret(trussinfo, fsc.name, fsc.nargs, fsc.s_args, errorp,
-                    errorp ? i : retval);
+                    errorp ? i : retval, fsc.sc);
   clear_fsc();
 
   return (retval);

usr.bin/truss/main.c
@@ -58,6 +58,7 @@ __FBSDID("$FreeBSD$");
 
 #include "truss.h"
 #include "extern.h"
+#include "syscall.h"
 
 #define MAXARGS 6
 
@@ -65,8 +66,8 @@ static void
 usage(void)
 {
 	fprintf(stderr, "%s\n%s\n",
-	    "usage: truss [-faedDS] [-o file] [-s strsize] -p pid",
-	    "       truss [-faedDS] [-o file] [-s strsize] command [args]");
+	    "usage: truss [-cfaedDS] [-o file] [-s strsize] -p pid",
+	    "       truss [-cfaedDS] [-o file] [-s strsize] command [args]");
 	exit(1);
 }
 
@@ -188,7 +189,7 @@ main(int ac, char **av)
 	trussinfo->pr_why = S_NONE;
 	trussinfo->curthread = NULL;
 	SLIST_INIT(&trussinfo->threadlist);
-	while ((c = getopt(ac, av, "p:o:faedDs:S")) != -1) {
+	while ((c = getopt(ac, av, "p:o:facedDs:S")) != -1) {
 		switch (c) {
 		case 'p':	/* specified pid */
 			trussinfo->pid = atoi(optarg);
@@ -203,6 +204,9 @@ main(int ac, char **av)
 			break;
 		case 'a': /* Print execve() argument strings. */
 			trussinfo->flags |= EXECVEARGS;
+			break;
+		case 'c': /* Count number of system calls and time. */
+			trussinfo->flags |= COUNTONLY;
 			break;
 		case 'e': /* Print execve() environment strings. */
 			trussinfo->flags |= EXECVEENVS;
@@ -337,6 +341,8 @@ START_TRACE:
 			free(signame);
 			break;
 		case S_EXIT:
+			if (trussinfo->flags & COUNTONLY)
+				break;
 			if (trussinfo->flags & FOLLOWFORKS)
 				fprintf(trussinfo->outfile, "%5d: ",
 				    trussinfo->pid);
@@ -360,12 +366,16 @@ START_TRACE:
 			break;
 		}
 	} while (trussinfo->pr_why != S_EXIT);
-	fflush(trussinfo->outfile);
 
 	if (trussinfo->flags & FOLLOWFORKS)
 		do {
 			childpid = wait(&status);
 		} while (childpid != -1);
+
+ 	if (trussinfo->flags & COUNTONLY)
+ 		print_summary(trussinfo);
+
+	fflush(trussinfo->outfile);
 
 	return (0);
 }

usr.bin/truss/powerpc-fbsd.c
@@ -331,7 +331,8 @@ powerpc_syscall_exit(struct trussinfo *trussinfo, int syscall_num __unused)
    * but that complicates things considerably.
    */
 
-  print_syscall_ret(trussinfo, fsc.name, fsc.nargs, fsc.s_args, errorp, retval);
+  print_syscall_ret(trussinfo, fsc.name, fsc.nargs, fsc.s_args, errorp,
+		    retval, fsc.sc);
   clear_fsc();
 
   return (retval);

commit 10edbbf3fe36d642a852efc9bc55a73955e79946

- Put arguments for print_syscall_ret() function in proper order.   Bug was introduced in r192025.
Approved by:	kib (mentor)

usr.bin/truss/ia64-fbsd.c
@@ -294,7 +294,7 @@ ia64_syscall_exit(struct trussinfo *trussinfo, int syscall_num __unused)
    */
 
   print_syscall_ret(trussinfo, fsc.name, fsc.nargs, fsc.s_args, errorp,
-		    fsc.sc, retval);
+		    retval, fsc.sc);
   clear_fsc();
 
   return (retval);