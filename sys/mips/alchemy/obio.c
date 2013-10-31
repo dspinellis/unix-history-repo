dspinellis/unix-history-repo

commit 274e421e5d659634b611e1640fb2ed885028ac6e

Devices that rely on hints or identify routines for discovery need to return BUS_PROBE_NOWILDCARD from their probe routines to avoid claiming wildcard devices on their parent bus. Do a sweep through the MIPS tree.
MFC after: 2 weeks


git-svn-id: svn+ssh://svn.freebsd.org/base/head@257338 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

sys/mips/alchemy/obio.c
@@ -158,7 +158,7 @@ static int
 obio_probe(device_t dev)
 {
 
-	return (0);
+	return (BUS_PROBE_NO_WILDCARD);
 }
 
 static int

sys/mips/adm5120/obio.c
@@ -157,7 +157,7 @@ static int
 obio_probe(device_t dev)
 {
 
-	return (0);
+	return (BUS_PROBE_NOWILDCARD);
 }
 
 static int

sys/mips/idt/obio.c
@@ -108,7 +108,7 @@ static int
 obio_probe(device_t dev)
 {
 
-	return (0);
+	return (BUS_PROBE_NOWILDCARD);
 }
 
 static int

sys/mips/rt305x/obio.c
@@ -148,7 +148,7 @@ static int
 obio_probe(device_t dev)
 {
 
-	return (0);
+	return (BUS_PROBE_NOWILDCARD);
 }
 
 static int

sys/mips/atheros/apb.c
@@ -103,7 +103,7 @@ static int
 apb_probe(device_t dev)
 {
 
-	return (0);
+	return (BUS_PROBE_NOWILDCARD);
 }
 
 static int

sys/mips/atheros/ar71xx_ehci.c
@@ -75,7 +75,7 @@ ar71xx_ehci_probe(device_t self)
 
 	device_set_desc(self, EHCI_HC_DEVSTR);
 
-	return (BUS_PROBE_DEFAULT);
+	return (BUS_PROBE_NOWILDCARD);
 }
 
 static int

sys/mips/atheros/ar71xx_pci.c
@@ -371,7 +371,7 @@ static int
 ar71xx_pci_probe(device_t dev)
 {
 
-	return (0);
+	return (BUS_PROBE_NOWILDCARD);
 }
 
 static int

sys/mips/atheros/ar71xx_spi.c
@@ -92,7 +92,7 @@ static int
 ar71xx_spi_probe(device_t dev)
 {
 	device_set_desc(dev, "AR71XX SPI");
-	return (0);
+	return (BUS_PROBE_NOWILDCARD);
 }
 
 static int

commit 0662ec6dbb737929aada53a04a3c220a45e4e638

Fix a typo introduced in r257338.

sys/mips/alchemy/obio.c
@@ -158,7 +158,7 @@ static int
 obio_probe(device_t dev)
 {
 
-	return (BUS_PROBE_NO_WILDCARD);
+	return (BUS_PROBE_NOWILDCARD);
 }
 
 static int