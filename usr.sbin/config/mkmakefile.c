dspinellis/unix-history-repo

commit 0aaf7648dc230d766c103d1f5c4368158a63494d

Don't believe we have a requirement until after we've checked all the known key words. This will make error messages slightly better in weird corner cases, but should otherwise be a nop.
git-svn-id: svn+ssh://svn.freebsd.org/base/head@261437 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

usr.sbin/config/mkmakefile.c
@@ -497,7 +497,6 @@ nextparam:
 		objprefix = ns(wd);
 		goto nextparam;
 	}
-	nreqs++;
 	if (eq(wd, "local")) {
 		filetype = LOCAL;
 		goto nextparam;
@@ -514,6 +513,7 @@ nextparam:
 		nowerror = 1;
 		goto nextparam;
 	}
+	nreqs++;
 	STAILQ_FOREACH(dp, &dtab, d_next)
 		if (eq(dp->d_name, wd)) {
 			dp->d_done |= DEVDONE;

commit 0e02fec6230af8a0a19036e0248f985da795a676

Fix a bug introduced in r261437 that failed to honor "optional profiling-routine" to work, since profiling-routine is not really an option or a device, but a special case elsewhere in the code.

usr.sbin/config/mkmakefile.c
@@ -477,18 +477,19 @@ nextparam:
 		filetype = NODEPEND;
 		goto nextparam;
 	}
-	if (eq(wd, "profiling-routine")) {
-		filetype = PROFILING;
-		goto nextparam;
-	}
 	if (eq(wd, "nowerror")) {
 		nowerror = 1;
+		goto nextparam;
+	}
+	nreqs++;
+	/* Hack to allow "optional profiling-routine" to work */
+	if (eq(wd, "profiling-routine")) {
+		filetype = PROFILING;
 		goto nextparam;
 	}
 	if (std)
 		errout("standard entry %s has optional inclusion specifier %s!\n",
 		    this, wd);
-	nreqs++;
 	STAILQ_FOREACH(dp, &dtab, d_next)
 		if (eq(dp->d_name, wd)) {
 			dp->d_done |= DEVDONE;