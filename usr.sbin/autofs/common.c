dspinellis/unix-history-repo

commit 1f83f478f26df4fa0b94f4320398b4d03400927f

Fix bug that made automount(8) never unmount stale autofs(5) mounts, ie mounts for entries that were there in auto_master(5), and then got removed.
MFC after:	1 month
Sponsored by:	The FreeBSD Foundation


git-svn-id: svn+ssh://svn.freebsd.org/base/head@275756 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

usr.sbin/autofs/common.c
@@ -676,8 +676,8 @@ node_print(const struct node *n)
 		node_print_indent(child, 0);
 }
 
-struct node *
-node_find(struct node *node, const char *path)
+static struct node *
+node_find_x(struct node *node, const char *path)
 {
 	struct node *child, *found;
 	char *tmp;
@@ -702,11 +702,22 @@ node_find(struct node *node, const char *path)
 	free(tmp);
 
 	TAILQ_FOREACH(child, &node->n_children, n_next) {
-		found = node_find(child, path);
+		found = node_find_x(child, path);
 		if (found != NULL)
 			return (found);
 	}
 
+	return (node);
+}
+
+struct node *
+node_find(struct node *root, const char *path)
+{
+	struct node *node;
+
+	node = node_find_x(root, path);
+	if (node == root)
+		return (NULL);
 	return (node);
 }
 

commit 3909a018c9e7f00b922807a24d8b5fcf592e686a

MFC r279845:
Fix handling of direct maps, broken in r275756.  Previously, running
automount(8) would unmount direct map trigger nodes every second time.

Sponsored by:	The FreeBSD Foundation

usr.sbin/autofs/common.c
@@ -661,29 +661,34 @@ node_find_x(struct node *node, const char *path)
 	char *tmp;
 	size_t tmplen;
 
-	//log_debugx("looking up %s in %s", path, node->n_key);
+	//log_debugx("looking up %s in %s", path, node_path(node));
 
-	tmp = node_path(node);
-	tmplen = strlen(tmp);
-	if (strncmp(tmp, path, tmplen) != 0) {
+	if (!node_is_direct_key(node)) {
+		tmp = node_path(node);
+		tmplen = strlen(tmp);
+		if (strncmp(tmp, path, tmplen) != 0) {
+			free(tmp);
+			return (NULL);
+		}
+		if (path[tmplen] != '/' && path[tmplen] != '\0') {
+			/*
+			 * If we have two map entries like 'foo' and 'foobar', make
+			 * sure the search for 'foobar' won't match 'foo' instead.
+			 */
+			free(tmp);
+			return (NULL);
+		}
 		free(tmp);
-		return (NULL);
 	}
-	if (path[tmplen] != '/' && path[tmplen] != '\0') {
-		/*
-		 * If we have two map entries like 'foo' and 'foobar', make
-		 * sure the search for 'foobar' won't match 'foo' instead.
-		 */
-		free(tmp);
-		return (NULL);
-	}
-	free(tmp);
 
 	TAILQ_FOREACH(child, &node->n_children, n_next) {
 		found = node_find_x(child, path);
 		if (found != NULL)
 			return (found);
 	}
+
+	if (node->n_parent == NULL || node_is_direct_key(node))
+		return (NULL);
 
 	return (node);
 }
@@ -693,9 +698,12 @@ node_find(struct node *root, const char *path)
 {
 	struct node *node;
 
+	assert(root->n_parent == NULL);
+
 	node = node_find_x(root, path);
-	if (node == root)
-		return (NULL);
+	if (node != NULL)
+		assert(node != root);
+
 	return (node);
 }
 