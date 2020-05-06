dspinellis/unix-history-repo

commit 700ad17065c4df9daa0d7b3c9992ba5daffe0fbb

Fix false CTL_UA_RES_RELEASE on secondary HA node.
git-svn-id: svn+ssh://svn.freebsd.org/base/head@287720 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

sys/cam/ctl/ctl.c
@@ -8404,7 +8404,7 @@ ctl_hndl_per_res_out_on_other_sc(union ctl_ha_msg *msg)
 	struct ctl_lun *lun;
 	struct ctl_softc *softc;
 	int i;
-	uint32_t targ_lun;
+	uint32_t residx, targ_lun;
 
 	softc = control_softc;
 	targ_lun = msg->hdr.nexus.targ_mapped_lun;
@@ -8420,6 +8420,7 @@ ctl_hndl_per_res_out_on_other_sc(union ctl_ha_msg *msg)
 		mtx_unlock(&lun->lun_lock);
 		return;
 	}
+	residx = ctl_get_initindex(&msg->hdr.nexus);
 	switch(msg->pr.pr_info.action) {
 	case CTL_PR_REG_KEY:
 		ctl_alloc_prkey(lun, msg->pr.pr_info.residx);
@@ -8484,8 +8485,9 @@ ctl_hndl_per_res_out_on_other_sc(union ctl_ha_msg *msg)
 		if (lun->res_type != SPR_TYPE_EX_AC
 		 && lun->res_type != SPR_TYPE_WR_EX) {
 			for (i = softc->init_min; i < softc->init_max; i++)
-				if (ctl_get_prkey(lun, i) != 0)
-					ctl_est_ua(lun, i, CTL_UA_RES_RELEASE);
+				if (i == residx || ctl_get_prkey(lun, i) == 0)
+					continue;
+				ctl_est_ua(lun, i, CTL_UA_RES_RELEASE);
 		}
 
 		lun->flags &= ~CTL_LUN_PR_RESERVED;

commit 2ca68aa78b2a988948a8ecb020746450d18b4c87

Redo r360682, now with only a minimal fix for misleading indentation:
MFC r333465 (partial, by lwhsu):

Fix build for platforms using GCC:

[omitted] - Remove unused or dead store variable
[omitted] - Remove unused function ctl_copyin_alloc
- Add missing curly brackets, this seems a regression in r287720

Reviewed by:	jhibbits
Differential Revision:	https://reviews.freebsd.org/D15383

sys/cam/ctl/ctl.c
@@ -8591,10 +8591,11 @@ ctl_hndl_per_res_out_on_other_sc(union ctl_io *io)
 		if (lun->pr_res_type != SPR_TYPE_EX_AC &&
 		    lun->pr_res_type != SPR_TYPE_WR_EX &&
 		    (lun->MODE_CTRL.queue_flags & SCP_NUAR) == 0) {
-			for (i = softc->init_min; i < softc->init_max; i++)
+			for (i = softc->init_min; i < softc->init_max; i++) {
 				if (i == residx || ctl_get_prkey(lun, i) == 0)
 					continue;
 				ctl_est_ua(lun, i, CTL_UA_RES_RELEASE);
+			}
 		}
 
 		lun->flags &= ~CTL_LUN_PR_RESERVED;