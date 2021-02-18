freebsd/freebsd-src

commit 1b6dd6d772cac39d314665609e54329c08da4e77

Print upper 32 bits in devmap table entries
If the devmap entry uses the upper 32 bits they wouldn't be printed in
devmap_dump_table(). This fixes that.

Submitted by:   Nicholas O'Brien <nickisobrien_gmail.com>
Sponsored by:   Axiado

sys/kern/subr_devmap.c
@@ -73,7 +73,7 @@ devmap_dump_table(int (*prfunc)(const char *, ...))
 
 	prfunc("Static device mappings:\n");
 	for (pd = devmap_table; pd->pd_size != 0; ++pd) {
-		prfunc("  0x%08x - 0x%08x mapped at VA 0x%08x\n",
+		prfunc("  0x%08jx - 0x%08jx mapped at VA 0x%08jx\n",
 		    pd->pd_pa, pd->pd_pa + pd->pd_size - 1, pd->pd_va);
 	}
 }

dspinellis/unix-history-repo

commit 43b2d58678edb67437dc8b2e92d42b3c20d1b82c

ddb: fix show devmap output on 32-bit arm
The output has been broken since 1b6dd6d772ca. Casting to uintmax_t
before the call to printf is necessary to ensure that 32-bit addresses
are interpreted correctly.

PR:		243236
MFC after:	3 days

sys/kern/subr_devmap.c
@@ -74,7 +74,9 @@ devmap_dump_table(int (*prfunc)(const char *, ...))
 	prfunc("Static device mappings:\n");
 	for (pd = devmap_table; pd->pd_size != 0; ++pd) {
 		prfunc("  0x%08jx - 0x%08jx mapped at VA 0x%08jx\n",
-		    pd->pd_pa, pd->pd_pa + pd->pd_size - 1, pd->pd_va);
+		    (uintmax_t)pd->pd_pa,
+		    (uintmax_t)(pd->pd_pa + pd->pd_size - 1),
+		    (uintmax_t)pd->pd_va);
 	}
 }
 