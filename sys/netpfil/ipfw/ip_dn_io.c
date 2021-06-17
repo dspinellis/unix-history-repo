freebsd/freebsd-src

VNETify dummynet
This moves dn_cfg and other parameters into per VNET variables.

The taskqueue and control state remains global.

Reviewed by:	kp
Differential Revision:	https://reviews.freebsd.org/D29274

sys/netpfil/ipfw/ip_dn_io.c
@@ -74,11 +74,10 @@ __FBSDID("$FreeBSD$");
 /*
  * We keep a private variable for the simulation time, but we could
  * probably use an existing one ("softticks" in sys/kern/kern_timeout.c)
- * instead of dn_cfg.curr_time
+ * instead of V_dn_cfg.curr_time
  */
-
-struct dn_parms dn_cfg;
-//VNET_DEFINE(struct dn_parms, _base_dn_cfg);
+VNET_DEFINE(struct dn_parms, dn_cfg);
+#define V_dn_cfg VNET(dn_cfg)
 
 /*
  * We use a heap to store entities for which we have pending timer events.
@@ -102,13 +101,13 @@ sysctl_hash_size(SYSCTL_HANDLER_ARGS)
 {
 	int error, value;
 
-	value = dn_cfg.hash_size;
+	value = V_dn_cfg.hash_size;
 	error = sysctl_handle_int(oidp, &value, 0, req);
 	if (error != 0 || req->newptr == NULL)
 		return (error);
 	if (value < 16 || value > 65536)
 		return (EINVAL);
-	dn_cfg.hash_size = value;
+	V_dn_cfg.hash_size = value;
 	return (0);
 }
 
@@ -119,9 +118,9 @@ sysctl_limits(SYSCTL_HANDLER_ARGS)
 	long value;
 
 	if (arg2 != 0)
-		value = dn_cfg.slot_limit;
+		value = V_dn_cfg.slot_limit;
 	else
-		value = dn_cfg.byte_limit;
+		value = V_dn_cfg.byte_limit;
 	error = sysctl_handle_long(oidp, &value, 0, req);
 
 	if (error != 0 || req->newptr == NULL)
@@ -129,11 +128,11 @@ sysctl_limits(SYSCTL_HANDLER_ARGS)
 	if (arg2 != 0) {
 		if (value < 1)
 			return (EINVAL);
-		dn_cfg.slot_limit = value;
+		V_dn_cfg.slot_limit = value;
 	} else {
 		if (value < 1500)
 			return (EINVAL);
-		dn_cfg.byte_limit = value;
+		V_dn_cfg.byte_limit = value;
 	}
 	return (0);
 }
@@ -151,9 +150,9 @@ static SYSCTL_NODE(_net_inet_ip, OID_AUTO, dummynet,
     "Dummynet");
 #endif
 
-/* wrapper to pass dn_cfg fields to SYSCTL_* */
-//#define DC(x)	(&(VNET_NAME(_base_dn_cfg).x))
-#define DC(x)	(&(dn_cfg.x))
+/* wrapper to pass V_dn_cfg fields to SYSCTL_* */
+#define DC(x)	(&(VNET_NAME(dn_cfg).x))
+
 /* parameters */
 
 SYSCTL_PROC(_net_inet_ip_dummynet, OID_AUTO, hash_size,
@@ -349,7 +348,7 @@ red_drops (struct dn_queue *q, int len)
 		 * XXX check wraps...
 		 */
 		if (q->avg) {
-			u_int t = div64((dn_cfg.curr_time - q->q_time), fs->lookup_step);
+			u_int t = div64((V_dn_cfg.curr_time - q->q_time), fs->lookup_step);
 
 			q->avg = (t < fs->lookup_depth) ?
 			    SCALE_MUL(q->avg, fs->w_q_lookup[t]) : 0;
@@ -524,7 +523,7 @@ dn_enqueue(struct dn_queue *q, struct mbuf* m, int drop)
 	return (0);
 
 drop:
-	dn_cfg.io_pkt_drop++;
+	V_dn_cfg.io_pkt_drop++;
 	q->ni.drops++;
 	ni->drops++;
 	FREE_PKT(m);
@@ -553,7 +552,7 @@ transmit_event(struct mq *q, struct delay_line *dline, uint64_t now)
 	}
 	if (m != NULL) {
 		dline->oid.subtype = 1; /* in heap */
-		heap_insert(&dn_cfg.evheap, pkt->output_time, dline);
+		heap_insert(&V_dn_cfg.evheap, pkt->output_time, dline);
 	}
 }
 
@@ -616,7 +615,7 @@ serve_sched(struct mq *q, struct dn_sch_inst *si, uint64_t now)
 			(m->m_pkthdr.len * 8 + extra_bits(m, s));
 		si->credit -= len_scaled;
 		/* Move packet in the delay line */
-		dn_tag_get(m)->output_time = dn_cfg.curr_time + s->link.delay ;
+		dn_tag_get(m)->output_time = V_dn_cfg.curr_time + s->link.delay ;
 		mq_append(&si->dline.mq, m);
 	}
 
@@ -634,7 +633,7 @@ serve_sched(struct mq *q, struct dn_sch_inst *si, uint64_t now)
 		if (m)
 			dn_tag_get(m)->output_time += t;
 		si->kflags |= DN_ACTIVE;
-		heap_insert(&dn_cfg.evheap, now + t, si);
+		heap_insert(&V_dn_cfg.evheap, now + t, si);
 	}
 	if (delay_line_idle && done)
 		transmit_event(q, &si->dline, now);
@@ -651,74 +650,85 @@ dummynet_task(void *context, int pending)
 {
 	struct timeval t;
 	struct mq q = { NULL, NULL }; /* queue to accumulate results */
+	struct epoch_tracker et;
 
-	CURVNET_SET((struct vnet *)context);
+	VNET_ITERATOR_DECL(vnet_iter);
+	VNET_LIST_RLOCK();
+	NET_EPOCH_ENTER(et);
 
-	DN_BH_WLOCK();
+	VNET_FOREACH(vnet_iter) {
+		memset(&q, 0, sizeof(struct mq));
+		CURVNET_SET(vnet_iter);
 
-	/* Update number of lost(coalesced) ticks. */
-	dn_cfg.tick_lost += pending - 1;
+		DN_BH_WLOCK();
 
-	getmicrouptime(&t);
-	/* Last tick duration (usec). */
-	dn_cfg.tick_last = (t.tv_sec - dn_cfg.prev_t.tv_sec) * 1000000 +
-	(t.tv_usec - dn_cfg.prev_t.tv_usec);
-	/* Last tick vs standard tick difference (usec). */
-	dn_cfg.tick_delta = (dn_cfg.tick_last * hz - 1000000) / hz;
-	/* Accumulated tick difference (usec). */
-	dn_cfg.tick_delta_sum += dn_cfg.tick_delta;
+		/* Update number of lost(coalesced) ticks. */
+		V_dn_cfg.tick_lost += pending - 1;
 
-	dn_cfg.prev_t = t;
+		getmicrouptime(&t);
+		/* Last tick duration (usec). */
+		V_dn_cfg.tick_last = (t.tv_sec - V_dn_cfg.prev_t.tv_sec) * 1000000 +
+		(t.tv_usec - V_dn_cfg.prev_t.tv_usec);
+		/* Last tick vs standard tick difference (usec). */
+		V_dn_cfg.tick_delta = (V_dn_cfg.tick_last * hz - 1000000) / hz;
+		/* Accumulated tick difference (usec). */
+		V_dn_cfg.tick_delta_sum += V_dn_cfg.tick_delta;
 
-	/*
-	* Adjust curr_time if the accumulated tick difference is
-	* greater than the 'standard' tick. Since curr_time should
-	* be monotonically increasing, we do positive adjustments
-	* as required, and throttle curr_time in case of negative
-	* adjustment.
-	*/
-	dn_cfg.curr_time++;
-	if (dn_cfg.tick_delta_sum - tick >= 0) {
-		int diff = dn_cfg.tick_delta_sum / tick;
+		V_dn_cfg.prev_t = t;
 
-		dn_cfg.curr_time += diff;
-		dn_cfg.tick_diff += diff;
-		dn_cfg.tick_delta_sum %= tick;
-		dn_cfg.tick_adjustment++;
-	} else if (dn_cfg.tick_delta_sum + tick <= 0) {
-		dn_cfg.curr_time--;
-		dn_cfg.tick_diff--;
-		dn_cfg.tick_delta_sum += tick;
-		dn_cfg.tick_adjustment++;
-	}
+		/*
+		* Adjust curr_time if the accumulated tick difference is
+		* greater than the 'standard' tick. Since curr_time should
+		* be monotonically increasing, we do positive adjustments
+		* as required, and throttle curr_time in case of negative
+		* adjustment.
+		*/
+		V_dn_cfg.curr_time++;
+		if (V_dn_cfg.tick_delta_sum - tick >= 0) {
+			int diff = V_dn_cfg.tick_delta_sum / tick;
 
-	/* serve pending events, accumulate in q */
-	for (;;) {
-		struct dn_id *p;    /* generic parameter to handler */
-
-		if (dn_cfg.evheap.elements == 0 ||
-		    DN_KEY_LT(dn_cfg.curr_time, HEAP_TOP(&dn_cfg.evheap)->key))
-			break;
-		p = HEAP_TOP(&dn_cfg.evheap)->object;
-		heap_extract(&dn_cfg.evheap, NULL);
-
-		if (p->type == DN_SCH_I) {
-			serve_sched(&q, (struct dn_sch_inst *)p, dn_cfg.curr_time);
-		} else { /* extracted a delay line */
-			transmit_event(&q, (struct delay_line *)p, dn_cfg.curr_time);
+			V_dn_cfg.curr_time += diff;
+			V_dn_cfg.tick_diff += diff;
+			V_dn_cfg.tick_delta_sum %= tick;
+			V_dn_cfg.tick_adjustment++;
+		} else if (V_dn_cfg.tick_delta_sum + tick <= 0) {
+			V_dn_cfg.curr_time--;
+			V_dn_cfg.tick_diff--;
+			V_dn_cfg.tick_delta_sum += tick;
+			V_dn_cfg.tick_adjustment++;
 		}
-	}
-	if (dn_cfg.expire && ++dn_cfg.expire_cycle >= dn_cfg.expire) {
-		dn_cfg.expire_cycle = 0;
-		dn_drain_scheduler();
-		dn_drain_queue();
-	}
 
+		/* serve pending events, accumulate in q */
+		for (;;) {
+			struct dn_id *p;    /* generic parameter to handler */
+
+			if (V_dn_cfg.evheap.elements == 0 ||
+			    DN_KEY_LT(V_dn_cfg.curr_time, HEAP_TOP(&V_dn_cfg.evheap)->key))
+				break;
+			p = HEAP_TOP(&V_dn_cfg.evheap)->object;
+			heap_extract(&V_dn_cfg.evheap, NULL);
+			if (p->type == DN_SCH_I) {
+				serve_sched(&q, (struct dn_sch_inst *)p, V_dn_cfg.curr_time);
+			} else { /* extracted a delay line */
+				transmit_event(&q, (struct delay_line *)p, V_dn_cfg.curr_time);
+			}
+		}
+		if (V_dn_cfg.expire && ++V_dn_cfg.expire_cycle >= V_dn_cfg.expire) {
+			V_dn_cfg.expire_cycle = 0;
+			dn_drain_scheduler();
+			dn_drain_queue();
+		}
+		DN_BH_WUNLOCK();
+		if (q.head != NULL)
+			dummynet_send(q.head);
+
+		CURVNET_RESTORE();
+	}
+	NET_EPOCH_EXIT(et);
+	VNET_LIST_RUNLOCK();
+
+	/* Schedule our next run. */
 	dn_reschedule();
-	DN_BH_WUNLOCK();
-	if (q.head != NULL)
-		dummynet_send(q.head);
-	CURVNET_RESTORE();
 }
 
 /*
@@ -834,7 +844,7 @@ tag_mbuf(struct mbuf *m, int dir, struct ip_fw_args *fwa)
 	dt->dn_dir = dir;
 	dt->ifp = fwa->flags & IPFW_ARGS_OUT ? fwa->ifp : NULL;
 	/* dt->output tame is updated as we move through */
-	dt->output_time = dn_cfg.curr_time;
+	dt->output_time = V_dn_cfg.curr_time;
 	dt->iphdr_off = (dir & PROTO_LAYER2) ? ETHER_HDR_LEN : 0;
 	return 0;
 }
@@ -866,12 +876,12 @@ dummynet_io(struct mbuf **m0, struct ip_fw_args *fwa)
 	else if (fwa->flags & IPFW_ARGS_IP6)
 		dir |= PROTO_IPV6;
 	DN_BH_WLOCK();
-	dn_cfg.io_pkt++;
+	V_dn_cfg.io_pkt++;
 	/* we could actually tag outside the lock, but who cares... */
 	if (tag_mbuf(m, dir, fwa))
 		goto dropit;
 	/* XXX locate_flowset could be optimised with a direct ref. */
-	fs = dn_ht_find(dn_cfg.fshash, fs_id, 0, NULL);
+	fs = dn_ht_find(V_dn_cfg.fshash, fs_id, 0, NULL);
 	if (fs == NULL)
 		goto dropit;	/* This queue/pipe does not exist! */
 	if (fs->sched == NULL)	/* should not happen */
@@ -894,7 +904,7 @@ dummynet_io(struct mbuf **m0, struct ip_fw_args *fwa)
 		m = *m0 = NULL;
 
 		/* dn_enqueue already increases io_pkt_drop */
-		dn_cfg.io_pkt_drop--;
+		V_dn_cfg.io_pkt_drop--;
 
 		goto dropit;
 	}
@@ -905,34 +915,34 @@ dummynet_io(struct mbuf **m0, struct ip_fw_args *fwa)
 	}
 
 	/* compute the initial allowance */
-	if (si->idle_time < dn_cfg.curr_time) {
+	if (si->idle_time < V_dn_cfg.curr_time) {
 	    /* Do this only on the first packet on an idle pipe */
 	    struct dn_link *p = &fs->sched->link;
 
-	    si->sched_time = dn_cfg.curr_time;
-	    si->credit = dn_cfg.io_fast ? p->bandwidth : 0;
+	    si->sched_time = V_dn_cfg.curr_time;
+	    si->credit = V_dn_cfg.io_fast ? p->bandwidth : 0;
 	    if (p->burst) {
-		uint64_t burst = (dn_cfg.curr_time - si->idle_time) * p->bandwidth;
+		uint64_t burst = (V_dn_cfg.curr_time - si->idle_time) * p->bandwidth;
 		if (burst > p->burst)
 			burst = p->burst;
 		si->credit += burst;
 	    }
 	}
 	/* pass through scheduler and delay line */
-	m = serve_sched(NULL, si, dn_cfg.curr_time);
+	m = serve_sched(NULL, si, V_dn_cfg.curr_time);
 
 	/* optimization -- pass it back to ipfw for immediate send */
 	/* XXX Don't call dummynet_send() if scheduler return the packet
 	 *     just enqueued. This avoid a lock order reversal.
 	 *     
 	 */
-	if (/*dn_cfg.io_fast &&*/ m == *m0 && (dir & PROTO_LAYER2) == 0 ) {
+	if (/*V_dn_cfg.io_fast &&*/ m == *m0 && (dir & PROTO_LAYER2) == 0 ) {
 		/* fast io, rename the tag * to carry reinject info. */
 		struct m_tag *tag = m_tag_first(m);
 
 		tag->m_tag_cookie = MTAG_IPFW_RULE;
 		tag->m_tag_id = 0;
-		dn_cfg.io_pkt_fast++;
+		V_dn_cfg.io_pkt_fast++;
 		if (m->m_nextpkt != NULL) {
 			printf("dummynet: fast io: pkt chain detected!\n");
 			m->m_nextpkt = NULL;
@@ -948,7 +958,7 @@ done:
 	return 0;
 
 dropit:
-	dn_cfg.io_pkt_drop++;
+	V_dn_cfg.io_pkt_drop++;
 	DN_BH_WUNLOCK();
 	if (m)
 		FREE_PKT(m);

dspinellis/unix-history-repo

(cherry picked from commit fe3bcfbda30e763a3ec56083b3a19cebbeaf8952)

sys/netpfil/ipfw/ip_dn_io.c
@@ -50,6 +50,7 @@ __FBSDID("$FreeBSD$");
 #include <sys/sysctl.h>
 
 #include <net/if.h>	/* IFNAMSIZ, struct ifaddr, ifq head, lock.h mutex.h */
+#include <net/if_var.h>
 #include <net/netisr.h>
 #include <net/vnet.h>
 
@@ -73,11 +74,10 @@ __FBSDID("$FreeBSD$");
 /*
  * We keep a private variable for the simulation time, but we could
  * probably use an existing one ("softticks" in sys/kern/kern_timeout.c)
- * instead of dn_cfg.curr_time
+ * instead of V_dn_cfg.curr_time
  */
-
-struct dn_parms dn_cfg;
-//VNET_DEFINE(struct dn_parms, _base_dn_cfg);
+VNET_DEFINE(struct dn_parms, dn_cfg);
+#define V_dn_cfg VNET(dn_cfg)
 
 /*
  * We use a heap to store entities for which we have pending timer events.
@@ -101,13 +101,13 @@ sysctl_hash_size(SYSCTL_HANDLER_ARGS)
 {
 	int error, value;
 
-	value = dn_cfg.hash_size;
+	value = V_dn_cfg.hash_size;
 	error = sysctl_handle_int(oidp, &value, 0, req);
 	if (error != 0 || req->newptr == NULL)
 		return (error);
 	if (value < 16 || value > 65536)
 		return (EINVAL);
-	dn_cfg.hash_size = value;
+	V_dn_cfg.hash_size = value;
 	return (0);
 }
 
@@ -118,9 +118,9 @@ sysctl_limits(SYSCTL_HANDLER_ARGS)
 	long value;
 
 	if (arg2 != 0)
-		value = dn_cfg.slot_limit;
+		value = V_dn_cfg.slot_limit;
 	else
-		value = dn_cfg.byte_limit;
+		value = V_dn_cfg.byte_limit;
 	error = sysctl_handle_long(oidp, &value, 0, req);
 
 	if (error != 0 || req->newptr == NULL)
@@ -128,11 +128,11 @@ sysctl_limits(SYSCTL_HANDLER_ARGS)
 	if (arg2 != 0) {
 		if (value < 1)
 			return (EINVAL);
-		dn_cfg.slot_limit = value;
+		V_dn_cfg.slot_limit = value;
 	} else {
 		if (value < 1500)
 			return (EINVAL);
-		dn_cfg.byte_limit = value;
+		V_dn_cfg.byte_limit = value;
 	}
 	return (0);
 }
@@ -147,9 +147,9 @@ SYSCTL_NODE(_net_inet_ip, OID_AUTO, dummynet, CTLFLAG_RW, 0, "Dummynet");
 static SYSCTL_NODE(_net_inet_ip, OID_AUTO, dummynet, CTLFLAG_RW, 0, "Dummynet");
 #endif
 
-/* wrapper to pass dn_cfg fields to SYSCTL_* */
-//#define DC(x)	(&(VNET_NAME(_base_dn_cfg).x))
-#define DC(x)	(&(dn_cfg.x))
+/* wrapper to pass V_dn_cfg fields to SYSCTL_* */
+#define DC(x)	(&(VNET_NAME(dn_cfg).x))
+
 /* parameters */
 
 
@@ -344,7 +344,7 @@ red_drops (struct dn_queue *q, int len)
 		 * XXX check wraps...
 		 */
 		if (q->avg) {
-			u_int t = div64((dn_cfg.curr_time - q->q_time), fs->lookup_step);
+			u_int t = div64((V_dn_cfg.curr_time - q->q_time), fs->lookup_step);
 
 			q->avg = (t < fs->lookup_depth) ?
 			    SCALE_MUL(q->avg, fs->w_q_lookup[t]) : 0;
@@ -519,7 +519,7 @@ dn_enqueue(struct dn_queue *q, struct mbuf* m, int drop)
 	return (0);
 
 drop:
-	dn_cfg.io_pkt_drop++;
+	V_dn_cfg.io_pkt_drop++;
 	q->ni.drops++;
 	ni->drops++;
 	FREE_PKT(m);
@@ -548,7 +548,7 @@ transmit_event(struct mq *q, struct delay_line *dline, uint64_t now)
 	}
 	if (m != NULL) {
 		dline->oid.subtype = 1; /* in heap */
-		heap_insert(&dn_cfg.evheap, pkt->output_time, dline);
+		heap_insert(&V_dn_cfg.evheap, pkt->output_time, dline);
 	}
 }
 
@@ -611,7 +611,7 @@ serve_sched(struct mq *q, struct dn_sch_inst *si, uint64_t now)
 			(m->m_pkthdr.len * 8 + extra_bits(m, s));
 		si->credit -= len_scaled;
 		/* Move packet in the delay line */
-		dn_tag_get(m)->output_time = dn_cfg.curr_time + s->link.delay ;
+		dn_tag_get(m)->output_time = V_dn_cfg.curr_time + s->link.delay ;
 		mq_append(&si->dline.mq, m);
 	}
 
@@ -629,7 +629,7 @@ serve_sched(struct mq *q, struct dn_sch_inst *si, uint64_t now)
 		if (m)
 			dn_tag_get(m)->output_time += t;
 		si->kflags |= DN_ACTIVE;
-		heap_insert(&dn_cfg.evheap, now + t, si);
+		heap_insert(&V_dn_cfg.evheap, now + t, si);
 	}
 	if (delay_line_idle && done)
 		transmit_event(q, &si->dline, now);
@@ -646,74 +646,85 @@ dummynet_task(void *context, int pending)
 {
 	struct timeval t;
 	struct mq q = { NULL, NULL }; /* queue to accumulate results */
+	struct epoch_tracker et;
 
-	CURVNET_SET((struct vnet *)context);
+	VNET_ITERATOR_DECL(vnet_iter);
+	VNET_LIST_RLOCK();
+	NET_EPOCH_ENTER_ET(et);
 
-	DN_BH_WLOCK();
+	VNET_FOREACH(vnet_iter) {
+		memset(&q, 0, sizeof(struct mq));
+		CURVNET_SET(vnet_iter);
 
-	/* Update number of lost(coalesced) ticks. */
-	dn_cfg.tick_lost += pending - 1;
+		DN_BH_WLOCK();
 
-	getmicrouptime(&t);
-	/* Last tick duration (usec). */
-	dn_cfg.tick_last = (t.tv_sec - dn_cfg.prev_t.tv_sec) * 1000000 +
-	(t.tv_usec - dn_cfg.prev_t.tv_usec);
-	/* Last tick vs standard tick difference (usec). */
-	dn_cfg.tick_delta = (dn_cfg.tick_last * hz - 1000000) / hz;
-	/* Accumulated tick difference (usec). */
-	dn_cfg.tick_delta_sum += dn_cfg.tick_delta;
+		/* Update number of lost(coalesced) ticks. */
+		V_dn_cfg.tick_lost += pending - 1;
 
-	dn_cfg.prev_t = t;
+		getmicrouptime(&t);
+		/* Last tick duration (usec). */
+		V_dn_cfg.tick_last = (t.tv_sec - V_dn_cfg.prev_t.tv_sec) * 1000000 +
+		(t.tv_usec - V_dn_cfg.prev_t.tv_usec);
+		/* Last tick vs standard tick difference (usec). */
+		V_dn_cfg.tick_delta = (V_dn_cfg.tick_last * hz - 1000000) / hz;
+		/* Accumulated tick difference (usec). */
+		V_dn_cfg.tick_delta_sum += V_dn_cfg.tick_delta;
 
-	/*
-	* Adjust curr_time if the accumulated tick difference is
-	* greater than the 'standard' tick. Since curr_time should
-	* be monotonically increasing, we do positive adjustments
-	* as required, and throttle curr_time in case of negative
-	* adjustment.
-	*/
-	dn_cfg.curr_time++;
-	if (dn_cfg.tick_delta_sum - tick >= 0) {
-		int diff = dn_cfg.tick_delta_sum / tick;
+		V_dn_cfg.prev_t = t;
 
-		dn_cfg.curr_time += diff;
-		dn_cfg.tick_diff += diff;
-		dn_cfg.tick_delta_sum %= tick;
-		dn_cfg.tick_adjustment++;
-	} else if (dn_cfg.tick_delta_sum + tick <= 0) {
-		dn_cfg.curr_time--;
-		dn_cfg.tick_diff--;
-		dn_cfg.tick_delta_sum += tick;
-		dn_cfg.tick_adjustment++;
-	}
+		/*
+		* Adjust curr_time if the accumulated tick difference is
+		* greater than the 'standard' tick. Since curr_time should
+		* be monotonically increasing, we do positive adjustments
+		* as required, and throttle curr_time in case of negative
+		* adjustment.
+		*/
+		V_dn_cfg.curr_time++;
+		if (V_dn_cfg.tick_delta_sum - tick >= 0) {
+			int diff = V_dn_cfg.tick_delta_sum / tick;
 
-	/* serve pending events, accumulate in q */
-	for (;;) {
-		struct dn_id *p;    /* generic parameter to handler */
-
-		if (dn_cfg.evheap.elements == 0 ||
-		    DN_KEY_LT(dn_cfg.curr_time, HEAP_TOP(&dn_cfg.evheap)->key))
-			break;
-		p = HEAP_TOP(&dn_cfg.evheap)->object;
-		heap_extract(&dn_cfg.evheap, NULL);
-
-		if (p->type == DN_SCH_I) {
-			serve_sched(&q, (struct dn_sch_inst *)p, dn_cfg.curr_time);
-		} else { /* extracted a delay line */
-			transmit_event(&q, (struct delay_line *)p, dn_cfg.curr_time);
+			V_dn_cfg.curr_time += diff;
+			V_dn_cfg.tick_diff += diff;
+			V_dn_cfg.tick_delta_sum %= tick;
+			V_dn_cfg.tick_adjustment++;
+		} else if (V_dn_cfg.tick_delta_sum + tick <= 0) {
+			V_dn_cfg.curr_time--;
+			V_dn_cfg.tick_diff--;
+			V_dn_cfg.tick_delta_sum += tick;
+			V_dn_cfg.tick_adjustment++;
 		}
-	}
-	if (dn_cfg.expire && ++dn_cfg.expire_cycle >= dn_cfg.expire) {
-		dn_cfg.expire_cycle = 0;
-		dn_drain_scheduler();
-		dn_drain_queue();
-	}
 
+		/* serve pending events, accumulate in q */
+		for (;;) {
+			struct dn_id *p;    /* generic parameter to handler */
+
+			if (V_dn_cfg.evheap.elements == 0 ||
+			    DN_KEY_LT(V_dn_cfg.curr_time, HEAP_TOP(&V_dn_cfg.evheap)->key))
+				break;
+			p = HEAP_TOP(&V_dn_cfg.evheap)->object;
+			heap_extract(&V_dn_cfg.evheap, NULL);
+			if (p->type == DN_SCH_I) {
+				serve_sched(&q, (struct dn_sch_inst *)p, V_dn_cfg.curr_time);
+			} else { /* extracted a delay line */
+				transmit_event(&q, (struct delay_line *)p, V_dn_cfg.curr_time);
+			}
+		}
+		if (V_dn_cfg.expire && ++V_dn_cfg.expire_cycle >= V_dn_cfg.expire) {
+			V_dn_cfg.expire_cycle = 0;
+			dn_drain_scheduler();
+			dn_drain_queue();
+		}
+		DN_BH_WUNLOCK();
+		if (q.head != NULL)
+			dummynet_send(q.head);
+
+		CURVNET_RESTORE();
+	}
+	NET_EPOCH_EXIT_ET(et);
+	VNET_LIST_RUNLOCK();
+
+	/* Schedule our next run. */
 	dn_reschedule();
-	DN_BH_WUNLOCK();
-	if (q.head != NULL)
-		dummynet_send(q.head);
-	CURVNET_RESTORE();
 }
 
 /*
@@ -827,7 +838,7 @@ tag_mbuf(struct mbuf *m, int dir, struct ip_fw_args *fwa)
 	dt->dn_dir = dir;
 	dt->ifp = fwa->oif;
 	/* dt->output tame is updated as we move through */
-	dt->output_time = dn_cfg.curr_time;
+	dt->output_time = V_dn_cfg.curr_time;
 	dt->iphdr_off = (dir & PROTO_LAYER2) ? ETHER_HDR_LEN : 0;
 	return 0;
 }
@@ -855,12 +866,12 @@ dummynet_io(struct mbuf **m0, int dir, struct ip_fw_args *fwa)
 	int fs_id = (fwa->rule.info & IPFW_INFO_MASK) +
 		((fwa->rule.info & IPFW_IS_PIPE) ? 2*DN_MAX_ID : 0);
 	DN_BH_WLOCK();
-	dn_cfg.io_pkt++;
+	V_dn_cfg.io_pkt++;
 	/* we could actually tag outside the lock, but who cares... */
 	if (tag_mbuf(m, dir, fwa))
 		goto dropit;
 	/* XXX locate_flowset could be optimised with a direct ref. */
-	fs = dn_ht_find(dn_cfg.fshash, fs_id, 0, NULL);
+	fs = dn_ht_find(V_dn_cfg.fshash, fs_id, 0, NULL);
 	if (fs == NULL)
 		goto dropit;	/* This queue/pipe does not exist! */
 	if (fs->sched == NULL)	/* should not happen */
@@ -883,7 +894,7 @@ dummynet_io(struct mbuf **m0, int dir, struct ip_fw_args *fwa)
 		m = *m0 = NULL;
 
 		/* dn_enqueue already increases io_pkt_drop */
-		dn_cfg.io_pkt_drop--;
+		V_dn_cfg.io_pkt_drop--;
 
 		goto dropit;
 	}
@@ -894,34 +905,34 @@ dummynet_io(struct mbuf **m0, int dir, struct ip_fw_args *fwa)
 	}
 
 	/* compute the initial allowance */
-	if (si->idle_time < dn_cfg.curr_time) {
+	if (si->idle_time < V_dn_cfg.curr_time) {
 	    /* Do this only on the first packet on an idle pipe */
 	    struct dn_link *p = &fs->sched->link;
 
-	    si->sched_time = dn_cfg.curr_time;
-	    si->credit = dn_cfg.io_fast ? p->bandwidth : 0;
+	    si->sched_time = V_dn_cfg.curr_time;
+	    si->credit = V_dn_cfg.io_fast ? p->bandwidth : 0;
 	    if (p->burst) {
-		uint64_t burst = (dn_cfg.curr_time - si->idle_time) * p->bandwidth;
+		uint64_t burst = (V_dn_cfg.curr_time - si->idle_time) * p->bandwidth;
 		if (burst > p->burst)
 			burst = p->burst;
 		si->credit += burst;
 	    }
 	}
 	/* pass through scheduler and delay line */
-	m = serve_sched(NULL, si, dn_cfg.curr_time);
+	m = serve_sched(NULL, si, V_dn_cfg.curr_time);
 
 	/* optimization -- pass it back to ipfw for immediate send */
 	/* XXX Don't call dummynet_send() if scheduler return the packet
 	 *     just enqueued. This avoid a lock order reversal.
 	 *     
 	 */
-	if (/*dn_cfg.io_fast &&*/ m == *m0 && (dir & PROTO_LAYER2) == 0 ) {
+	if (/*V_dn_cfg.io_fast &&*/ m == *m0 && (dir & PROTO_LAYER2) == 0 ) {
 		/* fast io, rename the tag * to carry reinject info. */
 		struct m_tag *tag = m_tag_first(m);
 
 		tag->m_tag_cookie = MTAG_IPFW_RULE;
 		tag->m_tag_id = 0;
-		dn_cfg.io_pkt_fast++;
+		V_dn_cfg.io_pkt_fast++;
 		if (m->m_nextpkt != NULL) {
 			printf("dummynet: fast io: pkt chain detected!\n");
 			m->m_nextpkt = NULL;
@@ -937,7 +948,7 @@ done:
 	return 0;
 
 dropit:
-	dn_cfg.io_pkt_drop++;
+	V_dn_cfg.io_pkt_drop++;
 	DN_BH_WUNLOCK();
 	if (m)
 		FREE_PKT(m);