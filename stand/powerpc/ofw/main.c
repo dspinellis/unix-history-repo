freebsd/freebsd-src

commit b4cb3fe0e39a31e60292018dc9fb7510b3de92bf

loader: implement mount/unmount rootfs
We want to keep our root file system open to preserve bcache segment
between file accesses, thus reducing physical disk IO.

Reviewed by:	imp, allanjude, kevans (previous version)
Differential Revision:	https://reviews.freebsd.org/D30848
MFC after:	1 month

stand/powerpc/ofw/main.c
@@ -137,6 +137,9 @@ main(int (*openfirm)(void *))
          */
 	cons_probe();
 
+	/* Set up currdev variable to have hooks in place. */
+	env_setenv("currdev", EV_VOLATILE, "", ofw_setcurrdev, env_nounset);
+
 	/*
 	 * March through the device switch probing for things.
 	 */

stand/efi/loader/main.c
@@ -187,15 +187,12 @@ static void
 set_currdev(const char *devname)
 {
 
-	/*
-	 * Don't execute hooks here; we may need to try setting these more than
-	 * once here if we're probing for the ZFS pool we're supposed to boot.
-	 * The currdev hook is intended to just validate user input anyways,
-	 * while the loaddev hook makes it immutable once we've determined what
-	 * the proper currdev is.
-	 */
-	env_setenv("currdev", EV_VOLATILE | EV_NOHOOK, devname, efi_setcurrdev,
+	env_setenv("currdev", EV_VOLATILE, devname, efi_setcurrdev,
 	    env_nounset);
+	/*
+	 * Don't execute hook here; the loaddev hook makes it immutable
+	 * once we've determined what the proper currdev is.
+	 */
 	env_setenv("loaddev", EV_VOLATILE | EV_NOHOOK, devname, env_noset,
 	    env_nounset);
 }
@@ -957,6 +954,9 @@ main(int argc, CHAR16 *argv[])
 		setenv("console", "comconsole", 1);
 #endif
 	cons_probe();
+
+	/* Set up currdev variable to have hooks in place. */
+	env_setenv("currdev", EV_VOLATILE, "", efi_setcurrdev, env_nounset);
 
 	/* Init the time source */
 	efi_time_init();

stand/i386/loader/main.c
@@ -164,6 +164,10 @@ main(void)
 	}
 	cons_probe();
 
+	/* Set up currdev variable to have hooks in place. */
+	env_setenv("currdev", EV_VOLATILE | EV_NOHOOK, "",
+	    i386_setcurrdev, env_nounset);
+
 	/*
 	 * Initialise the block cache. Set the upper limit.
 	 */

stand/powerpc/kboot/main.c
@@ -45,7 +45,6 @@ ssize_t kboot_copyout(vm_offset_t src, void *dest, const size_t len);
 ssize_t kboot_readin(readin_handle_t fd, vm_offset_t dest, const size_t len);
 int kboot_autoload(void);
 uint64_t kboot_loadaddr(u_int type, void *data, uint64_t addr);
-int kboot_setcurrdev(struct env_var *ev, int flags, const void *value);
 static void kboot_kseg_get(int *nseg, void **ptr);
 
 extern int command_fdt_internal(int argc, char *argv[]);

stand/uboot/common/main.c
@@ -475,6 +475,9 @@ main(int argc, char **argv)
 
 	meminfo();
 
+	/* Set up currdev variable to have hooks in place. */
+	env_setenv("currdev", EV_VOLATILE, "", uboot_setcurrdev, env_nounset);
+
 	/*
 	 * Enumerate U-Boot devices
 	 */

stand/userboot/userboot/main.c
@@ -159,6 +159,10 @@ loader_main(struct loader_callbacks *cb, void *arg, int version, int ndisks)
 	 */
 	cons_probe();
 
+	/* Set up currdev variable to have hooks in place. */
+	env_setenv("currdev", EV_VOLATILE, "",
+	    userboot_setcurrdev, env_nounset);
+
 	printf("\n%s", bootprog_info);
 #if 0
 	printf("Memory: %ld k\n", memsize() / 1024);

stand/common/bootstrap.h
@@ -29,6 +29,7 @@
 #ifndef _BOOTSTRAP_H_
 #define	_BOOTSTRAP_H_
 
+#include <stand.h>
 #include <sys/types.h>
 #include <sys/queue.h>
 #include <sys/linker_set.h>
@@ -399,6 +400,9 @@ int nvstore_set_var(void *, int, const char *, void *, size_t);
 int nvstore_set_var_from_string(void *, const char *, const char *,
     const char *);
 int nvstore_unset_var(void *, const char *);
+
+/* common code to set currdev variable. */
+extern int mount_currdev(struct env_var *, int, const void *);
 
 #ifndef CTASSERT
 #define	CTASSERT(x)	_Static_assert(x, "compile-time assertion failed")

stand/common/misc.c
@@ -179,3 +179,25 @@ dev_cleanup(void)
 	if (devsw[i]->dv_cleanup != NULL)
 	    (devsw[i]->dv_cleanup)();
 }
+
+/*
+ * mount new rootfs and unmount old, set "currdev" environment variable.
+ */
+int mount_currdev(struct env_var *ev, int flags, const void *value)
+{
+	int rv;
+
+	/* mount new rootfs */
+	rv = mount(value, "/", 0, NULL);
+	if (rv == 0) {
+		/*
+		 * Note we unmount any previously mounted fs only after
+		 * successfully mounting the new because we do not want to
+		 * end up with unmounted rootfs.
+		 */
+		if (ev->ev_value != NULL)
+			unmount(ev->ev_value, 0);
+		env_setenv(ev->ev_name, flags | EV_NOHOOK, value, NULL, NULL);
+	}
+	return (rv);
+}

dspinellis/unix-history-repo

commit 19529da3abcc3158575ee1c5a8f6146e5e71dba4

powerpc64: fix loader regression
After b4cb3fe0e39a, loader started crashing on PowerPC64, with a
Program Exception (700) error. The problem was that archsw was
used before being initialized, with the new mount feature. This
change fixes the issue by initializing archsw earlier, before
setting currdev, that triggers the mount.

Reviewed by:		tsoome
MFC after:		1 month
X-MFC-With:		b4cb3fe0e39a
Sponsored by:		Instituto de Pesquisas Eldorado (eldorado.org.br)
Differential Revision:	https://reviews.freebsd.org/D32027

stand/powerpc/ofw/main.c
@@ -137,6 +137,17 @@ main(int (*openfirm)(void *))
          */
 	cons_probe();
 
+	archsw.arch_getdev = ofw_getdev;
+	archsw.arch_copyin = ofw_copyin;
+	archsw.arch_copyout = ofw_copyout;
+	archsw.arch_readin = ofw_readin;
+#ifdef CAS
+	setenv("cas", "1", 0);
+	archsw.arch_autoload = ppc64_autoload;
+#else
+	archsw.arch_autoload = ofw_autoload;
+#endif
+
 	/* Set up currdev variable to have hooks in place. */
 	env_setenv("currdev", EV_VOLATILE, "", ofw_setcurrdev, env_nounset);
 
@@ -182,17 +193,6 @@ main(int (*openfirm)(void *))
 	 */
 	if (!(mfmsr() & PSL_DR))
 		setenv("usefdt", "1", 1);
-
-	archsw.arch_getdev = ofw_getdev;
-	archsw.arch_copyin = ofw_copyin;
-	archsw.arch_copyout = ofw_copyout;
-	archsw.arch_readin = ofw_readin;
-#ifdef CAS
-	setenv("cas", "1", 0);
-	archsw.arch_autoload = ppc64_autoload;
-#else
-	archsw.arch_autoload = ofw_autoload;
-#endif
 
 	interact();				/* doesn't return */
 