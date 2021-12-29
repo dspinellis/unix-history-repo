freebsd/freebsd-src

commit a3ba3d09c248cf2ec5ab2ae3668409ae4e5ab494

Make root mount wait mechanism smarter, by making it wait only if the root device doesn't yet exist.
Reviewed by:	kib@, marcel@
MFC after:	1 month
Sponsored by:	The FreeBSD Foundation
Differential Revision:	https://reviews.freebsd.org/D3709

sys/kern/vfs_mountroot.c
@@ -88,6 +88,7 @@ __FBSDID("$FreeBSD$");
 
 static int parse_mount(char **);
 static struct mntarg *parse_mountroot_options(struct mntarg *, const char *);
+static int vfs_mountroot_wait_if_neccessary(const char *fs, const char *dev);
 
 /*
  * The vnode of the system's root (/ in the filesystem, without chroot
@@ -673,7 +674,7 @@ parse_mount(char **conf)
 	char *errmsg;
 	struct mntarg *ma;
 	char *dev, *fs, *opts, *tok;
-	int delay, error, timeout;
+	int error;
 
 	error = parse_token(conf, &tok);
 	if (error)
@@ -710,20 +711,9 @@ parse_mount(char **conf)
 		goto out;
 	}
 
-	if (strcmp(fs, "zfs") != 0 && strstr(fs, "nfs") == NULL && 
-	    dev[0] != '\0' && !parse_mount_dev_present(dev)) {
-		printf("mountroot: waiting for device %s ...\n", dev);
-		delay = hz / 10;
-		timeout = root_mount_timeout * hz;
-		do {
-			pause("rmdev", delay);
-			timeout -= delay;
-		} while (timeout > 0 && !parse_mount_dev_present(dev));
-		if (timeout <= 0) {
-			error = ENODEV;
-			goto out;
-		}
-	}
+	error = vfs_mountroot_wait_if_neccessary(fs, dev);
+	if (error != 0)
+		goto out;
 
 	ma = NULL;
 	ma = mount_arg(ma, "fstype", fs, -1);
@@ -931,6 +921,51 @@ vfs_mountroot_wait(void)
 	}
 }
 
+static int
+vfs_mountroot_wait_if_neccessary(const char *fs, const char *dev)
+{
+	int delay, timeout;
+
+	/*
+	 * In case of ZFS and NFS we don't have a way to wait for
+	 * specific device.
+	 */
+	if (strcmp(fs, "zfs") == 0 || strstr(fs, "nfs") != NULL ||
+	    dev[0] == '\0') {
+		vfs_mountroot_wait();
+		return (0);
+	}
+
+	/*
+	 * Otherwise, no point in waiting if the device is already there.
+	 * Note that we must wait for GEOM to finish reconfiguring itself,
+	 * eg for geom_part(4) to finish tasting.
+	 */
+	DROP_GIANT();
+	g_waitidle();
+	PICKUP_GIANT();
+	if (parse_mount_dev_present(dev))
+		return (0);
+
+	/*
+	 * No luck.  Let's wait.  This code looks weird, but it's that way
+	 * to behave exactly as it used to work before.
+	 */
+	vfs_mountroot_wait();
+	printf("mountroot: waiting for device %s...\n", dev);
+	delay = hz / 10;
+	timeout = root_mount_timeout * hz;
+	do {
+		pause("rmdev", delay);
+		timeout -= delay;
+	} while (timeout > 0 && !parse_mount_dev_present(dev));
+
+	if (timeout <= 0)
+		return (ENODEV);
+
+	return (0);
+}
+
 void
 vfs_mountroot(void)
 {
@@ -941,8 +976,6 @@ vfs_mountroot(void)
 	int error;
 
 	td = curthread;
-
-	vfs_mountroot_wait();
 
 	sb = sbuf_new_auto();
 	vfs_mountroot_conf0(sb);

dspinellis/unix-history-repo

commit 2c8bf36598603b172ce15c9e2b75a8f11b6f26d5

vfs_mountroot: Check for root dev before waiting
If GEOM is idle but the root device is not yet present when we enter
vfs_mountroot_wait_if_necessary, we call vfs_mountroot_wait to wait
for root holds (e.g. CAM or USB initialization).  Upon returning from
vfs_mountroot_wait, we wait 100 ms at a time until the root device
shows up.

Since the root device most likely appeared during vfs_mountroot_wait
-- waiting for subsystems which may be responsible for the root
device is the whole purpose of that function -- it makes sense to
check if the device is now present rather than printing a warning
and pausing for 100 ms before checking.

Reviewed by:	trasz
Fixes: a3ba3d09c248 Make root mount wait mechanism smarter
Sponsored by:	https://www.patreon.com/cperciva
Differential Revision:	https://reviews.freebsd.org/D33593

sys/kern/vfs_mountroot.c
@@ -1040,6 +1040,8 @@ vfs_mountroot_wait_if_neccessary(const char *fs, const char *dev)
 	 * to behave exactly as it used to work before.
 	 */
 	vfs_mountroot_wait();
+	if (parse_mount_dev_present(dev))
+		return (0);
 	printf("mountroot: waiting for device %s...\n", dev);
 	delay = hz / 10;
 	timeout = root_mount_timeout * hz;