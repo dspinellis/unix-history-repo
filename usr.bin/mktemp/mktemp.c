freebsd/freebsd-src

mktemp: don't double up on trailing slashes for -t paths
This is a minor cosmetic change; re-organize slightly to set tmpdir to
_PATH_TMP if we didn't otherwise have a tmpdir candidate, then check the
trailing char before appending another slash.

While we're here, remove some bogus whitespace and add a test case for
this change.

Obtained from:	https://github.com/apple-oss-distributions/shell_cmds
Sponsored by:	Klara, Inc.

usr.bin/mktemp/mktemp.c
@@ -120,6 +120,7 @@ main(int argc, char **argv)
 
 	if (tflag) {
 		const char *envtmp;
+		size_t len;
 
 		envtmp = NULL;
 
@@ -131,7 +132,10 @@ main(int argc, char **argv)
 		if (envtmp != NULL)
 			tmpdir = envtmp;
 		if (tmpdir == NULL)
-			asprintf(&name, "%s%s.XXXXXXXXXX", _PATH_TMP, prefix);
+			tmpdir = _PATH_TMP;
+		len = strlen(tmpdir);
+		if (len > 0 && tmpdir[len - 1] == '/')
+			asprintf(&name, "%s%s.XXXXXXXXXX", tmpdir, prefix);
 		else
 			asprintf(&name, "%s/%s.XXXXXXXXXX", tmpdir, prefix);
 		/* if this fails, the program is in big trouble already */
@@ -142,7 +146,7 @@ main(int argc, char **argv)
 				errx(1, "cannot generate template");
 		}
 	}
-		
+
 	/* generate all requested files */
 	while (name != NULL || argc > 0) {
 		if (name == NULL) {

dspinellis/unix-history-repo

(cherry picked from commit a6346c02f646c6b74f007c64a9b546deb06182ae)

usr.bin/mktemp/mktemp.c
@@ -126,6 +126,7 @@ main(int argc, char **argv)
 
 	if (tflag) {
 		const char *envtmp;
+		size_t len;
 
 		envtmp = NULL;
 
@@ -137,7 +138,10 @@ main(int argc, char **argv)
 		if (envtmp != NULL)
 			tmpdir = envtmp;
 		if (tmpdir == NULL)
-			asprintf(&name, "%s%s.XXXXXXXX", _PATH_TMP, prefix);
+			tmpdir = _PATH_TMP;
+		len = strlen(tmpdir);
+		if (len > 0 && tmpdir[len - 1] == '/')
+			asprintf(&name, "%s%s.XXXXXXXX", tmpdir, prefix);
 		else
 			asprintf(&name, "%s/%s.XXXXXXXX", tmpdir, prefix);
 		/* if this fails, the program is in big trouble already */
@@ -148,7 +152,7 @@ main(int argc, char **argv)
 				errx(1, "cannot generate template");
 		}
 	}
-		
+
 	/* generate all requested files */
 	while (name != NULL || argc > 0) {
 		if (name == NULL) {