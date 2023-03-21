freebsd/freebsd-src

commit 14f102eacc8434a5a1f96466752578a4167140c9

tcp_wrappers: Use ANSI (c89) function definitions
Although this code is in contrib/ there is no active upstream.

Reviewed by:	brooks
Sponsored by:	The FreeBSD Foundation
Differential Revision: https://reviews.freebsd.org/D36047

contrib/tcp_wrappers/tli.c
@@ -63,8 +63,7 @@ static void tli_sink();
 
 /* tli_host - look up endpoint addresses and install conversion methods */
 
-void    tli_host(request)
-struct request_info *request;
+void    tli_host(struct request_info *request)
 {
 #ifdef INET6
     static struct sockaddr_storage client;
@@ -118,8 +117,7 @@ struct request_info *request;
 
 /* tli_cleanup - cleanup some dynamically-allocated data structures */
 
-static void tli_cleanup(request)
-struct request_info *request;
+static void tli_cleanup(struct request_info *request)
 {
     if (request->config != 0)
 	freenetconfigent(request->config);
@@ -131,8 +129,7 @@ struct request_info *request;
 
 /* tli_endpoints - determine TLI client and server endpoint information */
 
-static void tli_endpoints(request)
-struct request_info *request;
+static void tli_endpoints(struct request_info *request)
 {
     struct t_unitdata *server;
     struct t_unitdata *client;
@@ -240,8 +237,7 @@ int     fd;
 
 /* tli_hostaddr - map TLI transport address to printable address */
 
-static void tli_hostaddr(host)
-struct host_info *host;
+static void tli_hostaddr(struct host_info *host)
 {
     struct request_info *request = host->request;
     struct netconfig *config = request->config;
@@ -257,8 +253,7 @@ struct host_info *host;
 
 /* tli_hostname - map TLI transport address to hostname */
 
-static void tli_hostname(host)
-struct host_info *host;
+static void tli_hostname(struct host_info *host)
 {
     struct request_info *request = host->request;
     struct netconfig *config = request->config;
@@ -346,8 +341,7 @@ static char *tli_error()
 
 /* tli_sink - absorb unreceived datagram */
 
-static void tli_sink(fd)
-int     fd;
+static void tli_sink(int fd)
 {
     struct t_unitdata *unit;
     int     flags;

contrib/tcp_wrappers/clean_exit.c
@@ -21,8 +21,7 @@ extern void exit();
 
 /* clean_exit - clean up and exit */
 
-void    clean_exit(request)
-struct request_info *request;
+void    clean_exit(struct request_info *request)
 {
 
     /*

contrib/tcp_wrappers/diag.c
@@ -29,11 +29,7 @@ jmp_buf tcpd_buf;
 
 /* tcpd_diag - centralize error reporter */
 
-static void tcpd_diag(severity, tag, format, ap)
-int     severity;
-char   *tag;
-char   *format;
-va_list ap;
+static void tcpd_diag(int severity, char *tag, char *format, va_list ap)
 {
     char    fmt[BUFSIZ];
 

contrib/tcp_wrappers/environ.c
@@ -37,8 +37,7 @@ static int allocated = 0;		/* environ is, or is not, allocated */
 
 /* namelength - determine length of name in "name=whatever" */
 
-static int namelength(name)
-char   *name;
+static int namelength(char *name)
 {
     char   *equal;
 
@@ -48,9 +47,7 @@ char   *name;
 
 /* findenv - given name, locate name=value */
 
-static char **findenv(name, len)
-char   *name;
-int     len;
+static char **findenv(char *name, int len)
 {
     char  **envp;
 
@@ -62,8 +59,7 @@ int     len;
 
 /* getenv - given name, locate value */
 
-char   *getenv(name)
-char   *name;
+char   *getenv(char *name)
 {
     int     len = namelength(name);
     char  **envp = findenv(name, len);
@@ -73,8 +69,7 @@ char   *name;
 
 /* putenv - update or append environment (name,value) pair */
 
-int     putenv(nameval)
-char   *nameval;
+int     putenv(char *nameval)
 {
     char   *equal = strchr(nameval, '=');
     char   *value = (equal ? equal : "");
@@ -84,8 +79,7 @@ char   *nameval;
 
 /* unsetenv - remove variable from environment */
 
-void    unsetenv(name)
-char   *name;
+void    unsetenv(char *name)
 {
     char  **envp;
 
@@ -96,10 +90,7 @@ char   *name;
 
 /* setenv - update or append environment (name,value) pair */
 
-int     setenv(name, value, clobber)
-char   *name;
-char   *value;
-int     clobber;
+int     setenv(char *name, char *value, int clobber)
 {
     char   *destination;
     char  **envp;
@@ -133,9 +124,7 @@ int     clobber;
 
 /* cmalloc - malloc and copy block of memory */
 
-static char *cmalloc(new_len, old, old_len)
-char   *old;
-int     old_len;
+static char *cmalloc(int new_len, char *old, int old_len)
 {
     char   *new = malloc(new_len);
 
@@ -146,8 +135,7 @@ int     old_len;
 
 /* addenv - append environment entry */
 
-static int addenv(nameval)
-char   *nameval;
+static int addenv(char *nameval)
 {
     char  **envp;
     int     n_used;			/* number of environment entries */
@@ -190,9 +178,7 @@ static void printenv()
 	printf("%s\n", *envp);
 }
 
-int     main(argc, argv)
-int     argc;
-char  **argv;
+int     main(int argc, char **argv)
 {
     char   *cp;
     int     changed = 0;

contrib/tcp_wrappers/eval.c
@@ -42,8 +42,7 @@ char    paranoid[] = STRING_PARANOID;
 
 /* eval_user - look up user name */
 
-char   *eval_user(request)
-struct request_info *request;
+char   *eval_user(struct request_info *request)
 {
     if (request->user[0] == 0) {
 	strcpy(request->user, unknown);
@@ -55,8 +54,7 @@ struct request_info *request;
 
 /* eval_hostaddr - look up printable address */
 
-char   *eval_hostaddr(host)
-struct host_info *host;
+char   *eval_hostaddr(struct host_info *host)
 {
     if (host->addr[0] == 0) {
 	strcpy(host->addr, unknown);
@@ -68,8 +66,7 @@ struct host_info *host;
 
 /* eval_hostname - look up host name */
 
-char   *eval_hostname(host)
-struct host_info *host;
+char   *eval_hostname(struct host_info *host)
 {
     if (host->name[0] == 0) {
 	strcpy(host->name, unknown);
@@ -81,8 +78,7 @@ struct host_info *host;
 
 /* eval_hostinfo - return string with host name (preferred) or address */
 
-char   *eval_hostinfo(host)
-struct host_info *host;
+char   *eval_hostinfo(struct host_info *host)
 {
     char   *hostname;
 
@@ -100,8 +96,7 @@ struct host_info *host;
 
 /* eval_client - return string with as much about the client as we know */
 
-char   *eval_client(request)
-struct request_info *request;
+char   *eval_client(struct request_info *request)
 {
     static char both[2 * STRING_LENGTH];
     char   *hostinfo = eval_hostinfo(request->client);
@@ -120,8 +115,7 @@ struct request_info *request;
 
 /* eval_server - return string with as much about the server as we know */
 
-char   *eval_server(request)
-struct request_info *request;
+char   *eval_server(struct request_info *request)
 {
     static char both[2 * STRING_LENGTH];
     char   *host = eval_hostinfo(request->server);

contrib/tcp_wrappers/fakelog.c
@@ -17,20 +17,14 @@ static char sccsid[] = "@(#) fakelog.c 1.3 94/12/28 17:42:21";
 
 /* ARGSUSED */
 
-void openlog(name, logopt, facility)
-char   *name;
-int     logopt;
-int     facility;
+void openlog(char *name, int logopt, int facility)
 {
     /* void */
 }
 
 /* vsyslog - format one record */
 
-void vsyslog(severity, fmt, ap)
-int     severity;
-char   *fmt;
-va_list ap;
+void vsyslog(int severity, char *fmt, va_list ap)
 {
     char    buf[BUFSIZ];
 

contrib/tcp_wrappers/fix_options.c
@@ -35,8 +35,7 @@ static char sccsid[] = "@(#) fix_options.c 1.6 97/04/08 02:29:19";
 /* fix_options - get rid of IP-level socket options */
 
 void
-fix_options(request)
-struct request_info *request;
+fix_options(struct request_info *request)
 {
 #ifdef IP_OPTIONS
     unsigned char optbuf[BUFFER_SIZE / 3], *cp;

contrib/tcp_wrappers/fromhost.c
@@ -28,8 +28,7 @@ static char sccsid[] = "@(#) fromhost.c 1.17 94/12/28 17:42:23";
 
 /* fromhost - find out what network API we should use */
 
-void    fromhost(request)
-struct request_info *request;
+void    fromhost(struct request_info *request)
 {
 
     /*

dspinellis/unix-history-repo

commit 5d559ac27413798cf1a69ca00a71fd6cc676c871

tcp_wrappers: Use ANSI (c89) definitions for one more function
Fixes: 14f102eacc84 ("tcp_wrappers: Use ANSI (c89) function definit...")
Sponsored by: The FreeBSD Foundation

contrib/tcp_wrappers/tli.c
@@ -182,8 +182,7 @@ static void tli_endpoints(struct request_info *request)
 
 /* tli_transport - find out TLI transport type */
 
-static struct netconfig *tli_transport(fd)
-int     fd;
+static struct netconfig *tli_transport(int fd)
 {
     struct stat from_client;
     struct stat from_config;