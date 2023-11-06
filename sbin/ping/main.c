jlduran/freebsd-src

ping: Unify ping/ping6 statistics section
This is a first step towards a unification/simplification of ping/ping6
(internally).  The end goal is to produce a standardized user-facing
output.

Before (ping6):

    PING6(56=40+8+8 bytes) 2001:db8::1 --> 2001:db8::2
    16 bytes from ::1, icmp_seq=0 hlim=64 time=0.168 ms
    16 bytes from ::1, icmp_seq=1 hlim=64 time=0.068 ms

    --- 2001:db8::2 ping6 statistics ---
    round-trip min/avg/max/std-dev = 0.068/0.118/0.168/0.050 ms

After (ping6):

    PING(56=40+8+8 bytes) 2001:db8::1 --> 2001:db8::2
    16 bytes from ::1, icmp_seq=0 hlim=64 time=0.168 ms
    16 bytes from ::1, icmp_seq=1 hlim=64 time=0.068 ms

    --- 2001:db8::2 ping statistics ---
    round-trip min/avg/max/stddev = 0.068/0.118/0.168/0.050 ms

This has the nice side-effect of adding units to SIGINFO's statistics,
as printing numbers without units may not be of much help.  Also
mentions the fact that these times are round-trip.

Before (ping/ping6 SIGINFO):

    2/2 packets received (100.0%) 0.068 min / 0.118 avg / 0.168 max

After (ping/ping6 SIGINFO):

    --- <ipv4/ipv6 address> ping statistics ---
    2 packets transmitted, 2 packets received, 0.0% packet loss
    round-trip min/avg/max/stddev = 0.068/0.118/0.168/0.050 ms

In the case of a SIGINFO, the output will be printed to stderr, for both
ping and ping6.

Differential Revision:	https://reviews.freebsd.org/D39126

sbin/ping/main.c
@@ -37,6 +37,8 @@ __FBSDID("$FreeBSD$");
 #include <netinet/in.h>
 
 #include <err.h>
+#include <math.h>
+#include <signal.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -60,6 +62,28 @@ __FBSDID("$FreeBSD$");
 #else
 #error At least one of INET and INET6 is required
 #endif
+
+/* various options */
+u_int options;
+
+char *hostname;
+
+/* counters */
+long nreceived;		/* # of packets we got back */
+long nrepeats;		/* number of duplicates */
+long ntransmitted;	/* sequence # for outbound packets = #sent */
+long nrcvtimeout = 0;	/* # of packets we got back after waittime */
+
+/* nonzero if we've been told to finish up */
+volatile sig_atomic_t seenint;
+volatile sig_atomic_t seeninfo;
+
+/* timing */
+int timing;		/* flag to do timing */
+double tmin = 999999999.0;	/* minimum round trip time */
+double tmax = 0.0;	/* maximum round trip time */
+double tsum = 0.0;	/* sum of all times, for doing average */
+double tsumsq = 0.0;	/* sum of all times squared, for std. dev. */
 
 int
 main(int argc, char *argv[])
@@ -170,6 +194,65 @@ main(int argc, char *argv[])
 		return ping6(argc, argv);
 #endif /* INET6 */
 	errx(1, "Unknown host");
+}
+
+/*
+ * onsignal --
+ *	Set the global bit that causes the main loop to quit.
+ */
+void
+onsignal(int sig)
+{
+	switch (sig) {
+	case SIGALRM:
+	case SIGINT:
+		/*
+		 * When doing reverse DNS lookups, the seenint flag might not
+		 * be noticed for a while.  Just exit if we get a second SIGINT.
+		 */
+		if (!(options & F_HOSTNAME) && seenint != 0)
+			_exit(nreceived ? 0 : 2);
+		seenint++;
+		break;
+	case SIGINFO:
+		seeninfo++;
+		break;
+	}
+}
+
+/*
+ * pr_summary --
+ *	Print out summary statistics to the given output stream.
+ */
+void
+pr_summary(FILE * restrict stream)
+{
+	fprintf(stream, "\n--- %s ping statistics ---\n", hostname);
+	fprintf(stream, "%ld packets transmitted, ", ntransmitted);
+	fprintf(stream, "%ld packets received, ", nreceived);
+	if (nrepeats)
+		fprintf(stream, "+%ld duplicates, ", nrepeats);
+	if (ntransmitted) {
+		if (nreceived > ntransmitted)
+			fprintf(stream, "-- somebody's duplicating packets!");
+		else
+			fprintf(stream, "%.1f%% packet loss",
+			    ((((double)ntransmitted - nreceived) * 100.0) /
+			    ntransmitted));
+	}
+	if (nrcvtimeout)
+		fprintf(stream, ", %ld packets out of wait time", nrcvtimeout);
+	fputc('\n', stream);
+	if (nreceived && timing) {
+		/* Only display average to microseconds */
+		double num = nreceived + nrepeats;
+		double avg = tsum / num;
+		double dev = sqrt(fmax(0, tsumsq / num - avg * avg));
+		fprintf(stream,
+		    "round-trip min/avg/max/stddev = %.3f/%.3f/%.3f/%.3f ms\n",
+		    tmin, avg, tmax, dev);
+	}
+	fflush(stream);
 }
 
 void

dspinellis/unix-history-repo

Reviewed by:	markj
MFC after:	1 week
Pull Request:	https://github.com/freebsd/freebsd-src/pull/863
(cherry picked from commit 03d4d1c778091a08277d070185a9c60c7a6d57e0)

sbin/ping/main.c
@@ -35,6 +35,8 @@
 #include <netinet/in.h>
 
 #include <err.h>
+#include <math.h>
+#include <signal.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -58,6 +60,28 @@
 #else
 #error At least one of INET and INET6 is required
 #endif
+
+/* various options */
+u_int options;
+
+char *hostname;
+
+/* counters */
+long nreceived;		/* # of packets we got back */
+long nrepeats;		/* number of duplicates */
+long ntransmitted;	/* sequence # for outbound packets = #sent */
+long nrcvtimeout = 0;	/* # of packets we got back after waittime */
+
+/* nonzero if we've been told to finish up */
+volatile sig_atomic_t seenint;
+volatile sig_atomic_t seeninfo;
+
+/* timing */
+int timing;		/* flag to do timing */
+double tmin = 999999999.0;	/* minimum round trip time */
+double tmax = 0.0;	/* maximum round trip time */
+double tsum = 0.0;	/* sum of all times, for doing average */
+double tsumsq = 0.0;	/* sum of all times squared, for std. dev. */
 
 int
 main(int argc, char *argv[])
@@ -168,6 +192,65 @@ main(int argc, char *argv[])
 		return ping6(argc, argv);
 #endif /* INET6 */
 	errx(1, "Unknown host");
+}
+
+/*
+ * onsignal --
+ *	Set the global bit that causes the main loop to quit.
+ */
+void
+onsignal(int sig)
+{
+	switch (sig) {
+	case SIGALRM:
+	case SIGINT:
+		/*
+		 * When doing reverse DNS lookups, the seenint flag might not
+		 * be noticed for a while.  Just exit if we get a second SIGINT.
+		 */
+		if (!(options & F_HOSTNAME) && seenint != 0)
+			_exit(nreceived ? 0 : 2);
+		seenint++;
+		break;
+	case SIGINFO:
+		seeninfo++;
+		break;
+	}
+}
+
+/*
+ * pr_summary --
+ *	Print out summary statistics to the given output stream.
+ */
+void
+pr_summary(FILE * restrict stream)
+{
+	fprintf(stream, "\n--- %s ping statistics ---\n", hostname);
+	fprintf(stream, "%ld packets transmitted, ", ntransmitted);
+	fprintf(stream, "%ld packets received, ", nreceived);
+	if (nrepeats)
+		fprintf(stream, "+%ld duplicates, ", nrepeats);
+	if (ntransmitted) {
+		if (nreceived > ntransmitted)
+			fprintf(stream, "-- somebody's duplicating packets!");
+		else
+			fprintf(stream, "%.1f%% packet loss",
+			    ((((double)ntransmitted - nreceived) * 100.0) /
+			    ntransmitted));
+	}
+	if (nrcvtimeout)
+		fprintf(stream, ", %ld packets out of wait time", nrcvtimeout);
+	fputc('\n', stream);
+	if (nreceived && timing) {
+		/* Only display average to microseconds */
+		double num = nreceived + nrepeats;
+		double avg = tsum / num;
+		double stddev = sqrt(fmax(0, tsumsq / num - avg * avg));
+		fprintf(stream,
+		    "round-trip min/avg/max/stddev = %.3f/%.3f/%.3f/%.3f ms\n",
+		    tmin, avg, tmax, stddev);
+	}
+	fflush(stream);
 }
 
 void