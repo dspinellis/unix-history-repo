freebsd/freebsd-src

commit dcd5dea96509f6e483861350c9ef1d6a85b44ff6

cam: delete cam_sim_alloc_dev
cam_sim_alloc_dev was only used internally by the MMC system. That has
been convered to using xpt_path_device() and has stopped using this
interface, so this can be retired.

Reviewed by:		scottl@, mav@
Sponsored by:		Netflix
Differential Revision:	https://reviews.freebsd.org/D30858

sys/cam/cam_sim.h
@@ -104,7 +104,6 @@ struct cam_sim {
 	u_int32_t		flags;
 	struct cam_devq 	*devq;	/* Device Queue to use for this SIM */
 	int			refcount; /* References to the SIM. */
-	device_t		sim_dev; /* For attached peripherals. */
 };
 
 static __inline u_int32_t

sys/cam/cam_sim.c
@@ -116,7 +116,6 @@ cam_sim_alloc(sim_action_func sim_action, sim_poll_func sim_poll,
 	sim->sim_name = sim_name;
 	sim->softc = softc;
 	sim->path_id = CAM_PATH_ANY;
-	sim->sim_dev = NULL;	/* set only by cam_sim_alloc_dev */
 	sim->unit_number = unit;
 	sim->bus_id = 0;	/* set in xpt_bus_register */
 	sim->max_tagged_dev_openings = max_tagged_dev_transactions;
@@ -125,33 +124,6 @@ cam_sim_alloc(sim_action_func sim_action, sim_poll_func sim_poll,
 	sim->refcount = 1;
 	sim->devq = queue;
 	sim->mtx = mtx;
-	return (sim);
-}
-
-/**
- * @brief allocate a new sim and fill in the details with a device_t
- *
- * Just like @c cam_sim_alloc, but with an additional paramter.
- *
- * @param dev		A newbus device that's associated with the
- *			sim. Must be non-NULL.
- */
-struct cam_sim *
-cam_sim_alloc_dev(sim_action_func sim_action, sim_poll_func sim_poll,
-    const char *sim_name, void *softc, device_t dev, struct mtx *mtx,
-    int max_dev_transactions, int max_tagged_dev_transactions,
-    struct cam_devq *queue)
-{
-	struct cam_sim *sim;
-
-	KASSERT(dev != NULL, ("%s: dev is null for sim_name %s softc %p\n",
-	    __func__, sim_name, softc));
-
-	sim = cam_sim_alloc(sim_action, sim_poll, sim_name, softc,
-	    device_get_unit(dev), mtx, max_dev_transactions,
-	    max_tagged_dev_transactions, queue);
-	if (sim != NULL)
-		sim->sim_dev = dev;
 	return (sim);
 }
 

dspinellis/unix-history-repo

commit 49d5d11a9e6e659e8ea51b063d3be3027c9d5e9f

cam: Remove prototype for cam_sim_alloc_dev
The implementation was removed in dcd5dea96509, but the prototype was
not. Correct that oversight.

Fixes: dcd5dea96509
Sponsored by:		Netflix

sys/cam/cam_sim.h
@@ -60,15 +60,6 @@ struct cam_sim *  cam_sim_alloc(sim_action_func sim_action,
 				int max_dev_transactions,
 				int max_tagged_dev_transactions,
 				struct cam_devq *queue);
-struct cam_sim *  cam_sim_alloc_dev(sim_action_func sim_action,
-				sim_poll_func sim_poll,
-				const char *sim_name,
-				void *softc,
-				device_t dev,
-				struct mtx *mtx,
-				int max_dev_transactions,
-				int max_tagged_dev_transactions,
-				struct cam_devq *queue);
 void		  cam_sim_free(struct cam_sim *sim, int free_devq);
 void		  cam_sim_hold(struct cam_sim *sim);
 void		  cam_sim_release(struct cam_sim *sim);