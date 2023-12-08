freebsd/freebsd-src

pst: improve shutdown_post_sync handler
It is desirable to shut down the raid controller even in the face of a
panic. In the SCHEDULER_STOPPED() case, set the interrupt mask bits so
that we request a polled wait, rather than sleep(), from
iop_queue_wait_msg().

Tweak the function name and signature.

Reviewed by:	markj
MFC after:	2 weeks
Sponsored by:	The FreeBSD Foundation
Differential Revision:	https://reviews.freebsd.org/D42337

sys/dev/pst/pst-iop.c
@@ -432,7 +432,7 @@ iop_queue_wait_msg(struct iop_softc *sc, int mfa, struct i2o_basic_message *msg)
     int status, timeout = 10000;
 
     mtx_lock(&sc->mtx);
-    if (!(sc->reg->oqueue_intr_mask & 0x08)) {
+    if ((sc->reg->oqueue_intr_mask & I20_OUT_INTR_QUEUE) == 0) {
         msg->transaction_context = (u_int32_t)&request;
         msg->initiator_context = (u_int32_t)iop_done;
         sc->reg->iqueue = mfa;

dspinellis/unix-history-repo

(cherry picked from commit c4dacfa7f4b82f23ec0924d9db772860b2066f9b)
(cherry picked from commit f97aab79868cd7d891c52b14bd964523fa56f015)

sys/dev/pst/pst-iop.c
@@ -432,7 +432,7 @@ iop_queue_wait_msg(struct iop_softc *sc, int mfa, struct i2o_basic_message *msg)
     int status, timeout = 10000;
 
     mtx_lock(&sc->mtx);
-    if (!(sc->reg->oqueue_intr_mask & 0x08)) {
+    if ((sc->reg->oqueue_intr_mask & I2O_OUT_INTR_QUEUE) == 0) {
         msg->transaction_context = (u_int32_t)&request;
         msg->initiator_context = (u_int32_t)iop_done;
         sc->reg->iqueue = mfa;