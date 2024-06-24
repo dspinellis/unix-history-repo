freebsd/freebsd-src

commit 459dc61c8b05dedb61a2774348ffda7d7f8ea7c6

arm: Convert drivers to use device_set_desc(f)()
No functional change intended.

MFC after:	1 week

sys/arm/ti/am335x/am335x_dmtimer.c
@@ -248,7 +248,6 @@ am335x_dmtimer_tc_init(struct am335x_dmtimer_softc *sc)
 static int
 am335x_dmtimer_probe(device_t dev)
 {
-	char strbuf[32];
 	int tmr_num;
 	uint64_t rev_address;
 
@@ -276,8 +275,7 @@ am335x_dmtimer_probe(device_t dev)
 			return (ENXIO);
 	}
 
-	snprintf(strbuf, sizeof(strbuf), "AM335x DMTimer%d", tmr_num);
-	device_set_desc_copy(dev, strbuf);
+	device_set_descf("AM335x DMTimer%d", tmr_num);
 
 	return(BUS_PROBE_DEFAULT);
 }

sys/arm/nvidia/tegra_ahci.c
@@ -651,7 +651,7 @@ tegra_ahci_probe(device_t dev)
 	if (!ofw_bus_search_compatible(dev, compat_data)->ocd_data)
 		return (ENXIO);
 
-	device_set_desc_copy(dev, "AHCI SATA controller");
+	device_set_desc(dev, "AHCI SATA controller");
 	return (BUS_PROBE_DEFAULT);
 }
 

sys/arm/ti/am335x/am335x_dmtpps.c
@@ -383,7 +383,6 @@ static struct cdevsw dmtpps_cdevsw = {
 static int
 dmtpps_probe(device_t dev)
 {
-	char strbuf[64];
 	int tmr_num;
 	uint64_t rev_address;
 
@@ -435,9 +434,7 @@ dmtpps_probe(device_t dev)
 	if (dmtpps_tmr_num != tmr_num)
 		return (ENXIO);
 
-	snprintf(strbuf, sizeof(strbuf), "AM335x PPS-Capture DMTimer%d",
-	    tmr_num);
-	device_set_desc_copy(dev, strbuf);
+	device_set_descf("AM335x PPS-Capture DMTimer%d", tmr_num);
 
 	return(BUS_PROBE_DEFAULT);
 }

dspinellis/unix-history-repo

commit 056b644dc426406c222bbb8cd550d8bb64097597

ti/am335x: Fix the device_set_descf() call in am335x_dmtimer_probe()
Fixes: 459dc61c8b05 ("arm: Convert drivers to use device_set_desc(f)()")

Reviewed by:    markj, imp, manu
Approved by:    markj, imp, manu (mentor)
Differential Revision:  https://reviews.freebsd.org/D45699

sys/arm/ti/am335x/am335x_dmtimer.c
@@ -275,7 +275,7 @@ am335x_dmtimer_probe(device_t dev)
 			return (ENXIO);
 	}
 
-	device_set_descf("AM335x DMTimer%d", tmr_num);
+	device_set_descf(dev, "AM335x DMTimer%d", tmr_num);
 
 	return(BUS_PROBE_DEFAULT);
 }