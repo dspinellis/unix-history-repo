jlduran/freebsd-src

MAC: syscalls: Split mac_set_proc() into reusable pieces
This is in preparation for enabling the new setcred() system call to set
a process' MAC label.

No functional change (intended).

sys/security/mac/mac_syscalls.h
@@ -1,0 +1,33 @@
+/*-
+ * SPDX-License-Identifier: BSD-2-Clause
+ *
+ * Copyright (c) 2024 The FreeBSD Foundation
+ *
+ * This software was developed by Olivier Certner <olce.freebsd@certner.fr> at
+ * Kumacom SARL under sponsorship from the FreeBSD Foundation.
+ */
+
+/*
+ * Prototypes for functions used to implement system calls that must manipulate
+ * MAC labels.
+ */
+
+#ifndef _SECURITY_MAC_MAC_SYSCALLS_H_
+#define	_SECURITY_MAC_MAC_SYSCALLS_H_
+
+#ifndef _KERNEL
+#error "no user-serviceable parts inside"
+#endif
+
+int	mac_label_copyin(const struct mac *const u_mac, struct mac *const mac,
+	    char **const u_string);
+void	free_copied_label(const struct mac *const mac_p);
+
+int	mac_set_proc_prepare(struct thread *const td,
+	    const struct mac *const mac, void **const mac_set_proc_data);
+int	mac_set_proc_core(struct thread *const td, struct ucred *const newcred,
+	    void *const mac_set_proc_data);
+void	mac_set_proc_finish(struct thread *const td, bool proc_label_set,
+	    void *const mac_set_proc_data);
+
+#endif /* !_SECURITY_MAC_MAC_SYSCALLS_H_ */

dspinellis/unix-history-repo

MFC after:      2 weeks
Approved by:    markj (mentor)
Sponsored by:   The FreeBSD Foundation
Differential Revision:  https://reviews.freebsd.org/D46905
(cherry picked from commit 8a4d24a39098ed8170a37ca2aa83bf1da1976de1)

sys/security/mac/mac_syscalls.h
@@ -1,0 +1,33 @@
+/*-
+ * SPDX-License-Identifier: BSD-2-Clause
+ *
+ * Copyright (c) 2024 The FreeBSD Foundation
+ *
+ * This software was developed by Olivier Certner <olce.freebsd@certner.fr> at
+ * Kumacom SARL under sponsorship from the FreeBSD Foundation.
+ */
+
+/*
+ * Prototypes for functions used to implement system calls that must manipulate
+ * MAC labels.
+ */
+
+#ifndef _SECURITY_MAC_MAC_SYSCALLS_H_
+#define _SECURITY_MAC_MAC_SYSCALLS_H_
+
+#ifndef _KERNEL
+#error "no user-serviceable parts inside"
+#endif
+
+int	mac_label_copyin(const struct mac *const u_mac, struct mac *const mac,
+	    char **const u_string);
+void	free_copied_label(const struct mac *const mac);
+
+int	mac_set_proc_prepare(struct thread *const td,
+	    const struct mac *const mac, void **const mac_set_proc_data);
+int	mac_set_proc_core(struct thread *const td, struct ucred *const newcred,
+	    void *const mac_set_proc_data);
+void	mac_set_proc_finish(struct thread *const td, bool proc_label_set,
+	    void *const mac_set_proc_data);
+
+#endif /* !_SECURITY_MAC_MAC_SYSCALLS_H_ */