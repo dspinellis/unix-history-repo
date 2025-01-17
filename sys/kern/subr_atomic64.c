freebsd/freebsd-src

atomics: Constify loads: Fix powerpc build
Fixes:          f8212c7aeede ("atomics: Constify loads")
Sponsored by:   The FreeBSD Foundation

sys/kern/subr_atomic64.c
@@ -77,7 +77,7 @@ static struct mtx a64_mtx_pool[A64_POOL_SIZE];
 
 #define ATOMIC64_EMU_UN(op, rt, block, ret) \
     rt \
-    atomic_##op##_64(volatile uint64_t *p) {			\
+    atomic_##op##_64(const volatile uint64_t *p) {		\
 	uint64_t tmp __unused;					\
 	LOCK_A64();						\
 	block;							\

dspinellis/unix-history-repo

Fixes:          5e9a82e898d5 ("atomics: Constify loads")
(cherry picked from commit a9722e5ae8519a9a28e950dbd8d489178e9bc27e)

sys/kern/subr_atomic64.c
@@ -78,7 +78,7 @@ static struct mtx a64_mtx_pool[A64_POOL_SIZE];
 
 #define ATOMIC64_EMU_UN(op, rt, block, ret) \
     rt \
-    atomic_##op##_64(volatile u_int64_t *p) {			\
+    atomic_##op##_64(const volatile u_int64_t *p) {		\
 	u_int64_t tmp __unused;					\
 	LOCK_A64();						\
 	block;							\