freebsd/freebsd-src

commit a2e2178402afff939cb90e75c65ded0a9da8ac5c

riscv: parse memory regions from EFI map header
Using the newly isolated efi_map interface (subr_efi_map.c).

The primary goal is a greater adherence to the EFI firmware's view of
memory, and protection of its reserved regions. This is a better source
of truth than the device tree.

For example, regions marked as RuntimeServicesData must be excluded from
general memory allocations for EFI runtime services to eventually be
enabled on the platform (planned but not currently in progress).

The secondary impact is when loading a new/custom device tree via
loader(8); use of the EFI map ensures that reservations made by firmware
(OpenSBI, u-boot) are maintained.

Sponsored by:	The FreeBSD Foundation
Differential Revision:	https://reviews.freebsd.org/D49131

sys/riscv/riscv/machdep.c
@@ -45,6 +45,7 @@
 #include <sys/cons.h>
 #include <sys/cpu.h>
 #include <sys/devmap.h>
+#include <sys/efi_map.h>
 #include <sys/exec.h>
 #include <sys/imgact.h>
 #include <sys/kdb.h>
@@ -468,6 +469,7 @@ void
 initriscv(struct riscv_bootparams *rvbp)
 {
 	struct mem_region mem_regions[FDT_MEM_REGIONS];
+	struct efi_map_header *efihdr;
 	struct pcpu *pcpup;
 	int mem_regions_sz;
 	vm_offset_t lastaddr;
@@ -516,21 +518,40 @@ initriscv(struct riscv_bootparams *rvbp)
 	}
 	pcpup->pc_hart = boot_hart;
 
+	efihdr = (struct efi_map_header *)preload_search_info(preload_kmdp,
+	    MODINFO_METADATA | MODINFOMD_EFI_MAP);
+	if (efihdr != NULL) {
+		efi_map_add_entries(efihdr);
+		efi_map_exclude_entries(efihdr);
+	}
 #ifdef FDT
-	/*
-	 * Exclude reserved memory specified by the device tree. Typically,
-	 * this contains an entry for memory used by the runtime SBI firmware.
-	 */
-	if (fdt_get_reserved_mem(mem_regions, &mem_regions_sz) == 0) {
-		physmem_exclude_regions(mem_regions, mem_regions_sz,
+	else {
+		/* Exclude reserved memory specified by the device tree. */
+		if (fdt_get_reserved_mem(mem_regions, &mem_regions_sz) == 0) {
+			physmem_exclude_regions(mem_regions, mem_regions_sz,
+			    EXFLAG_NODUMP | EXFLAG_NOALLOC);
+		}
+
+		/* Grab physical memory regions information from device tree. */
+		if (fdt_get_mem_regions(mem_regions, &mem_regions_sz, NULL) != 0)
+			panic("Cannot get physical memory regions");
+		physmem_hardware_regions(mem_regions, mem_regions_sz);
+
+		/*
+		 * XXX: Unconditionally exclude the lowest 2MB of physical
+		 * memory, as this area is assumed to contain the SBI firmware,
+		 * and this is not properly reserved in all cases (e.g. in
+		 * older firmware like BBL).
+		 *
+		 * This is a little fragile, but it is consistent with the
+		 * platforms we support so far.
+		 *
+		 * TODO: remove this when the all regular booting methods
+		 * properly report their reserved memory in the device tree.
+		 */
+		physmem_exclude_region(mem_regions[0].mr_start, L2_SIZE,
 		    EXFLAG_NODUMP | EXFLAG_NOALLOC);
 	}
-
-	/* Grab physical memory regions information from device tree. */
-	if (fdt_get_mem_regions(mem_regions, &mem_regions_sz, NULL) != 0) {
-		panic("Cannot get physical memory regions");
-	}
-	physmem_hardware_regions(mem_regions, mem_regions_sz);
 #endif
 
 	/*
@@ -540,19 +561,6 @@ initriscv(struct riscv_bootparams *rvbp)
 
 	/* Do basic tuning, hz etc */
 	init_param1();
-
-#ifdef FDT
-	/*
-	 * XXX: Unconditionally exclude the lowest 2MB of physical memory, as
-	 * this area is assumed to contain the SBI firmware. This is a little
-	 * fragile, but it is consistent with the platforms we support so far.
-	 *
-	 * TODO: remove this when the all regular booting methods properly
-	 * report their reserved memory in the device tree.
-	 */
-	physmem_exclude_region(mem_regions[0].mr_start, L2_SIZE,
-	    EXFLAG_NODUMP | EXFLAG_NOALLOC);
-#endif
 
 	/* Bootstrap enough of pmap to enter the kernel proper */
 	kernlen = (lastaddr - KERNBASE);
@@ -588,8 +596,11 @@ initriscv(struct riscv_bootparams *rvbp)
 	if (env != NULL)
 		strlcpy(kernelname, env, sizeof(kernelname));
 
-	if (boothowto & RB_VERBOSE)
+	if (boothowto & RB_VERBOSE) {
+		if (efihdr != NULL)
+			efi_map_print_entries(efihdr);
 		physmem_print_tables();
+	}
 
 	early_boot = 0;
 

sys/conf/files.riscv
@@ -30,6 +30,7 @@ kern/msi_if.m			standard
 kern/pic_if.m			standard
 kern/subr_devmap.c		standard
 kern/subr_dummy_vdso_tc.c	standard
+kern/subr_efi_map.c		standard
 kern/subr_intr.c		standard
 kern/subr_physmem.c		standard
 libkern/bcopy.c			standard

dspinellis/unix-history-repo

commit 9aed0fd9b2dec464ee4d874e49646c22fffc7ac1

riscv: Exclude OpenSBI memory regions when booting with EFI
OpenSBI uses the first PMP entry to prevent buggy supervisor
software from overwriting the firmware [1]. However, this
region may not be properly marked as reserved in the EFI map, leading
to an access violation exception whenever the kernel
attempts to write to a page from that region.

Fix this by preemptively excluding first EFI memory map entry
if it is marked as "BootServicesData".

[1] https://github.com/riscv-non-isa/riscv-sbi-doc/pull/37

Reported by: tuexen
Tested by: tuexen
Fixes: a2e2178402af
Reviewed by:	imp, jrtc27
Differential Revision:	https://reviews.freebsd.org/D49839

sys/riscv/riscv/machdep.c
@@ -541,6 +541,22 @@ fdt_physmem_exclude_region_cb(const struct mem_region *mr, void *arg __unused)
 }
 #endif
 
+static void
+efi_exclude_sbi_pmp_cb(struct efi_md *p, void *argp)
+{
+	bool *first = (bool *)argp;
+
+	if (!*first)
+		return;
+
+	*first = false;
+	if (p->md_type == EFI_MD_TYPE_BS_DATA) {
+		physmem_exclude_region(p->md_phys,
+		    min(p->md_pages * EFI_PAGE_SIZE, L2_SIZE),
+		    EXFLAG_NOALLOC);
+	}
+}
+
 void
 initriscv(struct riscv_bootparams *rvbp)
 {
@@ -548,6 +564,7 @@ initriscv(struct riscv_bootparams *rvbp)
 	struct pcpu *pcpup;
 	vm_offset_t lastaddr;
 	vm_size_t kernlen;
+	bool first;
 	char *env;
 
 	TSRAW(&thread0, TS_ENTER, __func__, NULL);
@@ -577,11 +594,22 @@ initriscv(struct riscv_bootparams *rvbp)
 	if (efihdr != NULL) {
 		efi_map_add_entries(efihdr);
 		efi_map_exclude_entries(efihdr);
+
+		/*
+		 * OpenSBI uses the first PMP entry to prevent buggy supervisor
+		 * software from overwriting the firmware. However, this
+		 * region may not be properly marked as reserved, leading
+		 * to an access violation exception whenever the kernel
+		 * attempts to write to a page from that region.
+		 *
+		 * Fix this by excluding first EFI memory map entry
+		 * if it is marked as "BootServicesData".
+		 */
+		first = true;
+		efi_map_foreach_entry(efihdr, efi_exclude_sbi_pmp_cb, &first);
 	}
 #ifdef FDT
 	else {
-		bool first;
-
 		/* Exclude reserved memory specified by the device tree. */
 		fdt_foreach_reserved_mem(fdt_physmem_exclude_region_cb, NULL);
 