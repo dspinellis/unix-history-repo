freebsd/freebsd-src

commit fd186cd16eafabfa28aa95175d8df97cbf08b0c7

find: Fix items from the review I missed for printf
Minor fixes I missed in a review comment until after the commit.

Pointy-hat-to: imp
Fixes: 7b9c912c41f4
Sponsored by: Netflix

usr.bin/find/function.c
@@ -1450,7 +1450,10 @@ c_printf(OPTION *option, char ***argvp)
 	PLAN *new;
 
 	isoutput = 1;
-	ftsoptions &= ~FTS_NOSTAT;
+	/*
+	 * XXX We could scan the format looking for stat-dependent formats, and
+	 * turn off the stat if there's none: `%p`/`%f`/`%h` don't need a stat.
+	 */
 
 	new = palloc(option);
 	new->c_data = nextarg(option, argvp);

usr.bin/find/printf.c
@@ -279,7 +279,7 @@ do_printf(PLAN *plan, FTSENT *entry, FILE *fout)
 		case 'c': /* status change time ctime */
 			fp_ctime(fp, sb->st_ctime);
 			break;
-		case 'C': /* statuc change time with next char strftime format */
+		case 'C': /* status change time with next char strftime format */
 			fp_strftime(fp, sb->st_ctime, *fmt++);
 			break;
 		case 't': /* modification change time ctime */

dspinellis/unix-history-repo

commit 60bcffe5d9c280abe08abcaf6c6b2daa8cf9ffe3

find: Put back clearing the NOSTAT bit
In fd186cd16eaf I read the condition backwards. We want to stat all the
time until we can implement something to do it more inteligently as
Jiles suggested in the review.

Fixes: fd186cd16eaf
Noticed by: des
Sponsored by: Netflix

usr.bin/find/function.c
@@ -1449,11 +1449,12 @@ c_printf(OPTION *option, char ***argvp)
 {
 	PLAN *new;
 
-	isoutput = 1;
 	/*
 	 * XXX We could scan the format looking for stat-dependent formats, and
-	 * turn off the stat if there's none: `%p`/`%f`/`%h` don't need a stat.
+	 * turn off the nostat bit for trival cases: `%p`/`%f`/`%h`.
 	 */
+	isoutput = 1;
+	ftsoptions &= ~FTS_NOSTAT;
 
 	new = palloc(option);
 	new->c_data = nextarg(option, argvp);