freebsd/freebsd-src

commit 29a16ce065dbc28bc9e87c9bfadb08bb58b137e4

release: update stable/14 following branching from main
Approved by:	re (implicit)
Sponsored by:	GoFundMe https://www.gofundme.com/f/gjbbsd
Sponsored by:	PayPal https://paypal.me/gjbbsd

contrib/jemalloc/include/jemalloc/jemalloc_FreeBSD.h
@@ -5,7 +5,7 @@
 #undef JEMALLOC_OVERRIDE_VALLOC
 
 #ifndef MALLOC_PRODUCTION
-#define	JEMALLOC_DEBUG
+#define	MALLOC_PRODUCTION
 #endif
 
 #undef JEMALLOC_DSS

share/mk/src.opts.mk
@@ -126,7 +126,6 @@ __DEFAULT_YES_OPTIONS = \
     LLD \
     LLD_BOOTSTRAP \
     LLD_IS_LD \
-    LLVM_ASSERTIONS \
     LLVM_COV \
     LLVM_CXXFILT \
     LOADER_GELI \
@@ -162,6 +161,7 @@ __DEFAULT_YES_OPTIONS = \
     QUOTAS \
     RADIUS_SUPPORT \
     RBOOTD \
+    REPRODUCIBLE_BUILD \
     RESCUE \
     ROUTED \
     SENDMAIL \
@@ -205,11 +205,11 @@ __DEFAULT_NO_OPTIONS = \
     HESIOD \
     LOADER_VERBOSE \
     LOADER_VERIEXEC_PASS_MANIFEST \
+    LLVM_ASSERTIONS \
     LLVM_BINUTILS \
     MALLOC_PRODUCTION \
     OFED_EXTRA \
     OPENLDAP \
-    REPRODUCIBLE_BUILD \
     RPCBIND_WARMSTART_SUPPORT \
     SORT_THREADS \
     ZONEINFO_LEAPSECONDS_SUPPORT \

UPDATING
@@ -12,20 +12,8 @@ Items affecting the ports and packages system can be found in
 /usr/ports/UPDATING.  Please read that file before updating system packages
 and/or ports.
 
-NOTE TO PEOPLE WHO THINK THAT FreeBSD 14.x IS SLOW:
-	FreeBSD 14.x has many debugging features turned on, in both the kernel
-	and userland.  These features attempt to detect incorrect use of
-	system primitives, and encourage loud failure through extra sanity
-	checking and fail stop semantics.  They also substantially impact
-	system performance.  If you want to do performance measurement,
-	benchmarking, and optimization, you'll want to turn them off.  This
-	includes various WITNESS- related kernel options, INVARIANTS, malloc
-	debugging flags in userland, and various verbose features in the
-	kernel.  Many developers choose to disable these features on build
-	machines to maximize performance.  (To completely disable malloc
-	debugging, define WITH_MALLOC_PRODUCTION in /etc/src.conf and rebuild
-	world, or to merely disable the most expensive debugging functionality
-	at runtime, run "ln -s 'abort:false,junk:false' /etc/malloc.conf".)
+20230818:
+	__FreeBSD_version 1400095 after the branching stable/14 from main.
 
 20230817:
 	Serial communication (in boot loaders, kernel, and userland) has

libexec/rc/rc.conf
@@ -642,7 +642,7 @@ lpd_flags=""		# Flags to lpd (if enabled).
 nscd_enable="NO"	# Run the nsswitch caching daemon.
 chkprintcap_enable="NO"	# Run chkprintcap(8) before running lpd.
 chkprintcap_flags="-d"	# Create missing directories by default.
-dumpdev="AUTO"		# Device to crashdump to (device name, AUTO, or NO);
+dumpdev="NO"		# Device to crashdump to (device name, AUTO, or NO);
 			# this should be commented out here
 			# for stable branches to respect kenv.
 dumpon_flags=""		# Options to pass to dumpon(8), followed by dumpdev.

release/Makefile
@@ -186,7 +186,6 @@ disc1: packagesystem
 	ln -fs /tmp/bsdinstall_etc/resolv.conf ${.TARGET}/etc/resolv.conf
 	echo sendmail_enable=\"NONE\" > ${.TARGET}/etc/rc.conf
 	echo hostid_enable=\"NO\" >> ${.TARGET}/etc/rc.conf
-	echo debug.witness.trace=0 >> ${.TARGET}/etc/sysctl.conf
 	echo vfs.mountroot.timeout=\"10\" >> ${.TARGET}/boot/loader.conf
 	echo kernels_autodetect=\"NO\" >> ${.TARGET}/boot/loader.conf
 	echo loader_menu_multi_user_prompt=\"Installer\" >> ${.TARGET}/boot/loader.conf
@@ -223,7 +222,6 @@ bootonly: packagesystem
 	ln -fs /tmp/bsdinstall_etc/resolv.conf ${.TARGET}/etc/resolv.conf
 	echo sendmail_enable=\"NONE\" > ${.TARGET}/etc/rc.conf
 	echo hostid_enable=\"NO\" >> ${.TARGET}/etc/rc.conf
-	echo debug.witness.trace=0 >> ${.TARGET}/etc/sysctl.conf
 	echo vfs.mountroot.timeout=\"10\" >> ${.TARGET}/boot/loader.conf
 	echo kernels_autodetect=\"NO\" >> ${.TARGET}/boot/loader.conf
 	echo loader_menu_multi_user_prompt=\"Installer\" >> ${.TARGET}/boot/loader.conf
@@ -258,7 +256,6 @@ dvd: packagesystem
 	ln -fs /tmp/bsdinstall_etc/resolv.conf ${.TARGET}/etc/resolv.conf
 	echo sendmail_enable=\"NONE\" > ${.TARGET}/etc/rc.conf
 	echo hostid_enable=\"NO\" >> ${.TARGET}/etc/rc.conf
-	echo debug.witness.trace=0 >> ${.TARGET}/etc/sysctl.conf
 	echo vfs.mountroot.timeout=\"10\" >> ${.TARGET}/boot/loader.conf
 	echo kernels_autodetect=\"NO\" >> ${.TARGET}/boot/loader.conf
 	echo loader_menu_multi_user_prompt=\"Installer\" >> ${.TARGET}/boot/loader.conf

release/release.conf.sample
@@ -18,7 +18,7 @@ GITSRC="src.git"
 GITPORTS="ports.git"
 
 ## Set the src/, ports/, and doc/ branches or tags.
-SRCBRANCH="main"
+SRCBRANCH="stable/14"
 PORTBRANCH="main"
 
 ## Sample configuration for using git from ports.

sys/amd64/conf/GENERIC
@@ -92,19 +92,6 @@ options 	RCTL			# Resource limits
 # Debugging support.  Always need this:
 options 	KDB			# Enable kernel debugger support.
 options 	KDB_TRACE		# Print a stack trace for a panic.
-# For full debugger support use (turn off in stable branch):
-options 	BUF_TRACKING		# Track buffer history
-options 	DDB			# Support DDB.
-options 	FULL_BUF_TRACKING	# Track more buffer history
-options 	GDB			# Support remote GDB.
-options 	DEADLKRES		# Enable the deadlock resolver
-options 	INVARIANTS		# Enable calls of extra sanity checking
-options 	INVARIANT_SUPPORT	# Extra sanity checks of internal structures, required by INVARIANTS
-options 	QUEUE_MACRO_DEBUG_TRASH	# Trash queue(2) internal pointers on invalidation
-options 	WITNESS			# Enable checks to detect deadlocks and cycles
-options 	WITNESS_SKIPSPIN	# Don't run witness on spinlocks for speed
-options 	MALLOC_DEBUG_MAXZONES=8	# Separate malloc(9) zones
-options 	VERBOSE_SYSINIT=0	# Support debug.verbose_sysinit, off by default
 
 # Kernel Sanitizers
 #options 	COVERAGE		# Generic kernel coverage. Used by KCOV

sys/amd64/conf/GENERIC-NODEBUG
@@ -1,31 +1,0 @@
-#
-# GENERIC-NODEBUG -- WITNESS and INVARIANTS free kernel configuration file
-#		     for FreeBSD/amd64
-#
-# This configuration file removes several debugging options, including
-# WITNESS and INVARIANTS checking, which are known to have significant
-# performance impact on running systems.  When benchmarking new features
-# this kernel should be used instead of the standard GENERIC.
-# This kernel configuration should never appear outside of the HEAD
-# of the FreeBSD tree.
-#
-# For more information on this file, please read the config(5) manual page,
-# and/or the handbook section on Kernel Configuration Files:
-#
-#    https://docs.freebsd.org/en/books/handbook/kernelconfig/#kernelconfig-config
-#
-# The handbook is also available locally in /usr/share/doc/handbook
-# if you've installed the doc distribution, otherwise always see the
-# FreeBSD World Wide Web server (https://www.FreeBSD.org/) for the
-# latest information.
-#
-# An exhaustive list of options and more detailed explanations of the
-# device lines is also present in the ../../conf/NOTES and NOTES files.
-# If you are in doubt as to the purpose or necessity of a line, check first
-# in NOTES.
-#
-
-include GENERIC
-include "../../conf/std.nodebug"
-
-ident   GENERIC-NODEBUG

dspinellis/unix-history-repo

commit 688922a16a7b537e47d764f784d74139d2be29d9

jemalloc: Fix activating debug on WITHOUT_MALLOC_PRODUCTION
When stable/14 was branched, MALLOC_PRODUCTION wasn't moved from
__DEFAULT_NO_OPTIONS to __DEFAULT_YES_OPTIONS but instead
'jemalloc_FreeBSD.h' was changed to stop defining JEMALLOC_DEBUG (and
also, strangely, to start defining MALLOC_PRODUCTION always, although
that does not have any effect in jemalloc's code).

While this effectively turned off jemalloc debugging, it also prevents
people from turning it on again.  Additionally, leaving
MALLOC_PRODUCTION in __DEFAULT_NO_OPTIONS would be confusing.  Finally,
this puts stable/14 in line with what was done for stable/15.

This is a direct commit to stable/14.

Reviewed by:    markj
Fixes:          29a16ce065db ("release: update stable/14 following branching from main")
Sponsored by:   The FreeBSD Foundation
Differential Revision:  https://reviews.freebsd.org/D52850

contrib/jemalloc/include/jemalloc/jemalloc_FreeBSD.h
@@ -5,7 +5,7 @@
 #undef JEMALLOC_OVERRIDE_VALLOC
 
 #ifndef MALLOC_PRODUCTION
-#define	MALLOC_PRODUCTION
+#define	JEMALLOC_DEBUG
 #endif
 
 #undef JEMALLOC_DSS

share/mk/src.opts.mk
@@ -145,6 +145,7 @@ __DEFAULT_YES_OPTIONS = \
     MAIL \
     MAILWRAPPER \
     MAKE \
+    MALLOC_PRODUCTION \
     MLX5TOOL \
     NETCAT \
     NETGRAPH \
@@ -213,7 +214,6 @@ __DEFAULT_NO_OPTIONS = \
     LLVM_ASSERTIONS \
     LLVM_BINUTILS \
     LLVM_FULL_DEBUGINFO \
-    MALLOC_PRODUCTION \
     OFED_EXTRA \
     OPENLDAP \
     RPCBIND_WARMSTART_SUPPORT \